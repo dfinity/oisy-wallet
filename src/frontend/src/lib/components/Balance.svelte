aa4b62e8b540edb9631c44b44b984ad16e7bac69
David Dal Busco <david.dalbusco@outlook.com>
2023-08-09 12:59:02 +0200

    feat(frontend): fetch balance and transactions with ether.js (#7)

---
+++ src/frontend/src/lib/components/Balance.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { balance as balanceService } from '$lib/services/provider.services';
+	import { utils } from 'ethers';
+	import type { BigNumber } from '@ethersproject/bignumber';
+
+	let balance: BigNumber | undefined;
+
+	onMount(async () => {
+		// TODO: error
+		// TODO: watcher?
+		balance = await balanceService('0x6D1b7ceAd24FBaf153a3a18f09395Fd2f9C64912');
+	});
+</script>
+
+<p>Balance: <output>{utils.formatEther(`${balance ?? 0n}`)}</output></p>
---
+++ src/frontend/src/lib/components/Transactions.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { transactions as transactionsService } from '$lib/services/provider.services';
+	import type { TransactionResponse } from '@ethersproject/providers';
+	import { utils } from 'ethers';
+
+	let transactions: TransactionResponse[] = [];
+
+	onMount(async () => {
+		// TODO: error
+		// TODO: watcher?
+		transactions = await transactionsService('0x6D1b7ceAd24FBaf153a3a18f09395Fd2f9C64912');
+	});
+</script>
+
+{#each transactions as { from, to, value, blockNumber }, blockNumber}
+	<hr />
+	<p>From: <output>{from}</output></p>
+	<p>To: <output>{to}</output></p>
+	<p>Value: <output>{utils.formatEther(value.toString())}</output></p>
+{/each}
