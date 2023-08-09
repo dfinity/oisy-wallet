efc1d9693407ae9d93b5be6342b6c4814548dec9
David Dal Busco <david.dalbusco@outlook.com>
2023-08-09 07:23:45 +0200

    build: generate did types

--- src/declarations/backend/backend.factory.did.js
+++ src/declarations/backend/backend.factory.did.js
@@ -1,8 +1,21 @@
 // @ts-ignore
 export const idlFactory = ({ IDL }) => {
-	return IDL.Service({ greet: IDL.Func([IDL.Text], [IDL.Text], ['query']) });
+	const InitArg = IDL.Record({
+		ecdsa_key_name: IDL.Text,
+		chain_id: IDL.Nat
+	});
+	const Arg = IDL.Variant({ Upgrade: IDL.Null, Init: InitArg });
+	return IDL.Service({
+		caller_eth_address: IDL.Func([], [IDL.Text], []),
+		sign_transaction: IDL.Func([], [IDL.Text], [])
+	});
 };
 // @ts-ignore
 export const init = ({ IDL }) => {
-	return [];
+	const InitArg = IDL.Record({
+		ecdsa_key_name: IDL.Text,
+		chain_id: IDL.Nat
+	});
+	const Arg = IDL.Variant({ Upgrade: IDL.Null, Init: InitArg });
+	return [Arg];
 };
