17942aac19cc55b384bf60002d5e4999c80d418a
David Dal Busco <david.dalbusco@outlook.com>
2023-08-09 17:12:37 +0200

    feat(frontend): quick and dirty implementation of the eth address store to test an effective transaction

--- src/frontend/src/lib/components/Transactions.svelte
+++ src/frontend/src/lib/components/Transactions.svelte
@@ -3,13 +3,14 @@
 	import { transactions as transactionsService } from '$lib/services/provider.services';
 	import type { TransactionResponse } from '@ethersproject/providers';
 	import { utils } from 'ethers';
+	import { ethAddressStore } from '$lib/stores/eth.store';
 
 	let transactions: TransactionResponse[] = [];
 
 	onMount(async () => {
 		// TODO: error
 		// TODO: watcher?
-		transactions = await transactionsService('0x6D1b7ceAd24FBaf153a3a18f09395Fd2f9C64912');
+		transactions = await transactionsService($ethAddressStore!);
 	});
 </script>
 
