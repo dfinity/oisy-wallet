53ee4f1a4b633fe819588fc12117411d351c364e
Roman Kashitsyn <roman@dfinity.org>
2023-08-09 16:05:06 +0200

    feat(backend): implement sign_transaction method

--- src/declarations/backend/backend.factory.did.js
+++ src/declarations/backend/backend.factory.did.js
@@ -1,21 +1,23 @@
 // @ts-ignore
 export const idlFactory = ({ IDL }) => {
-	const InitArg = IDL.Record({
-		ecdsa_key_name: IDL.Text,
-		chain_id: IDL.Nat
-	});
+	const InitArg = IDL.Record({ ecdsa_key_name: IDL.Text });
 	const Arg = IDL.Variant({ Upgrade: IDL.Null, Init: InitArg });
+	const SignRequest = IDL.Record({
+		to: IDL.Text,
+		gas: IDL.Nat,
+		value: IDL.Nat,
+		chain_id: IDL.Nat,
+		nonce: IDL.Nat,
+		gas_price: IDL.Nat
+	});
 	return IDL.Service({
 		caller_eth_address: IDL.Func([], [IDL.Text], []),
-		sign_transaction: IDL.Func([], [IDL.Text], [])
+		sign_transaction: IDL.Func([SignRequest], [IDL.Text], [])
 	});
 };
 // @ts-ignore
 export const init = ({ IDL }) => {
-	const InitArg = IDL.Record({
-		ecdsa_key_name: IDL.Text,
-		chain_id: IDL.Nat
-	});
+	const InitArg = IDL.Record({ ecdsa_key_name: IDL.Text });
 	const Arg = IDL.Variant({ Upgrade: IDL.Null, Init: InitArg });
 	return [Arg];
 };
