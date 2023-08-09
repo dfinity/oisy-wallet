aa1e0cc572f2f4995510e0eb068f7c161b07fff0
David Dal Busco <david.dalbusco@outlook.com>
2023-08-09 12:58:32 +0200

    feat(frontend): fetch balance and transactions with ether.js

--- src/frontend/src/routes/+page.svelte
+++ src/frontend/src/routes/+page.svelte
@@ -2,10 +2,16 @@
 	import { authSignedInStore } from '$lib/stores/auth.store';
 	import { signIn, signOut } from '$lib/services/auth.services';
 	import EthAddress from '$lib/components/EthAddress.svelte';
+	import Balance from '$lib/components/Balance.svelte';
+	import Transactions from '$lib/components/Transactions.svelte';
 </script>
 
 {#if $authSignedInStore}
 	<EthAddress />
+
+	<Balance />
+
+	<Transactions />
 
 	<button on:click={signOut}>Sign-out</button>
 {:else}
