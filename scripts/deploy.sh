8c64348f211248a139c07d18a87aad6315a98396
David Dal Busco <david.dalbusco@outlook.com>
2023-08-09 17:13:13 +0200

    build: deploy backend arguments reviewed (#12)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,7 @@
 
 dfx deploy backend --argument "(variant {
   Init = record {
-       ecdsa_key_name = \"dfx_test_key\";
-       chain_id = 123_456;
+       ecdsa_key_name = \"dfx_test_key\"
    }
 })" --mode=reinstall
 
