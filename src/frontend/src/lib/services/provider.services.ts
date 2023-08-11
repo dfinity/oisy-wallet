adf725d742c7bf7fa5491ce0e7dad1d621d88a0c
David Dal Busco <david.dalbusco@outlook.com>
2023-08-11 12:07:24 +0200

    feat: send transaction (#17)

--- src/frontend/src/lib/services/provider.services.ts
+++ src/frontend/src/lib/services/provider.services.ts
@@ -1,17 +1,23 @@
 import type { ECDSA_PUBLIC_KEY } from '$lib/types/eth';
 import type { BigNumber } from '@ethersproject/bignumber';
-import { EtherscanProvider, type TransactionResponse } from '@ethersproject/providers';
+import {
+	EtherscanProvider,
+	type FeeData,
+	type TransactionResponse
+} from '@ethersproject/providers';
 
 // TODO: migration to ethers v6 issue https://github.com/ethers-io/ethers.js/issues/4303
 
-export const balance = (address: ECDSA_PUBLIC_KEY): Promise<BigNumber> => {
-	// TODO: api key as second parameter
-	const provider = new EtherscanProvider('sepolia');
-	return provider.getBalance(address);
-};
+const API_KEY = import.meta.env.VITE_EHTERSCAN_API_KEY;
+const provider = new EtherscanProvider('sepolia', API_KEY);
 
-export const transactions = (address: ECDSA_PUBLIC_KEY): Promise<TransactionResponse[]> => {
-	// TODO: api key as second parameter
-	const provider = new EtherscanProvider('sepolia');
-	return provider.getHistory(address);
-};
+export const balance = (address: ECDSA_PUBLIC_KEY): Promise<BigNumber> =>
+	provider.getBalance(address);
+
+export const transactions = (address: ECDSA_PUBLIC_KEY): Promise<TransactionResponse[]> =>
+	provider.getHistory(address);
+
+export const getFeeData = (): Promise<FeeData> => provider.getFeeData();
+
+export const sendTransaction = (signedTransaction: string): Promise<TransactionResponse> =>
+	provider.sendTransaction(signedTransaction);
