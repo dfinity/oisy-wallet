85f94ba3104969c665b86715c30785779c8683c1
David Dal Busco <david.dalbusco@outlook.com>
2023-08-14 11:01:36 +0200

    refactor(frontend): eth address -> address

---
+++ src/frontend/src/lib/stores/address.store.ts
@@ -0,0 +1,21 @@
+import type { ECDSA_PUBLIC_KEY } from '$lib/types/address';
+import { writable, type Readable } from 'svelte/store';
+
+export type AddressData = ECDSA_PUBLIC_KEY | undefined | null;
+
+export interface AddressStore extends Readable<AddressData> {
+	set: (address: ECDSA_PUBLIC_KEY) => void;
+	reset: () => void;
+}
+
+const initAddressStore = (): AddressStore => {
+	const { subscribe, set } = writable<AddressData>(undefined);
+
+	return {
+		set: (address: ECDSA_PUBLIC_KEY) => set(address),
+		reset: () => set(null),
+		subscribe
+	};
+};
+
+export const addressStore = initAddressStore();
