73a447e5649f2085f3edd213697eaec73bcc349b
David Dal Busco <david.dalbusco@outlook.com>
2023-08-15 10:18:07 +0200

    styles(fronted): utilities and signin (#29)

---
+++ src/frontend/src/lib/components/core/SignIn.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import splash from '$lib/assets/splash.png';
+	import { signIn } from '$lib/services/auth.services';
+</script>
+
+<section class="flex flex-col justify-center aligns-center">
+	<img src={splash} role="presentation" alt="" decoding="async" />
+
+	<h1 class="text-center py-4">Fully on-chain ETH Wallet built on the Internet Computer</h1>
+
+	<button on:click={signIn}>Sign-in</button>
+</section>
