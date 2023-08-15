865f8d109c144f5864e08f3e54053504b8de97cf
David Dal Busco <david.dalbusco@outlook.com>
2023-08-15 10:18:07 +0200

    styles(fronted): utilities and signin (#29)

--- src/frontend/src/routes/+page.svelte
+++ src/frontend/src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import { authSignedInStore } from '$lib/stores/auth.store';
-	import { signIn, signOut } from '$lib/services/auth.services';
+	import { signOut } from '$lib/services/auth.services';
 	import Address from '$lib/components/Address.svelte';
 	import Balance from '$lib/components/Balance.svelte';
 	import Transactions from '$lib/components/Transactions.svelte';
 	import Send from '$lib/components/Send.svelte';
 	import Sockets from '$lib/components/Sockets.svelte';
+	import SignIn from '$lib/components/core/SignIn.svelte';
 </script>
 
 {#if $authSignedInStore}
@@ -21,5 +22,5 @@
 
 	<button on:click={signOut}>Sign-out</button>
 {:else}
-	<button on:click={signIn}>Sign-in</button>
+	<SignIn />
 {/if}
