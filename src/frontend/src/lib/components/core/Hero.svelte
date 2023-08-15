7f31a0aade52dede35fb50f6affedb3c97f1e4cb
David Dal Busco <david.dalbusco@outlook.com>
2023-08-15 14:25:13 +0200

    style(frontend): add a hero slot

---
+++ src/frontend/src/lib/components/core/Hero.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import ETHIcon from '$lib/components/icons/ETHIcon.svelte';
+</script>
+
+<article class="bg-deep-violet text-ghost-white rounded-lg py-8 my-4 relative">
+	<div class="absolute" style="top: -3rem; left: 1rem;">
+		<ETHIcon />
+	</div>
+
+	<slot />
+</article>
