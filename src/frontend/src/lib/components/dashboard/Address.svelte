febf4367966c5d06e02985368552d94bdea0eac5
David Dal Busco <david.dalbusco@outlook.com>
2023-08-15 15:56:57 +0200

    feat: address (#33)

--- src/frontend/src/lib/components/dashboard/Address.svelte
+++ src/frontend/src/lib/components/dashboard/Address.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
 	import { addressStore } from '$lib/stores/address.store';
+	import { shortenWithMiddleEllipsis } from '$lib/utils/format.utils';
+	import { Copy } from '@dfinity/gix-components';
+	import { nonNullish } from '@dfinity/utils';
 </script>
 
-<output>{$addressStore}</output>
+<div class="text-ghost-white mt-8 flex gap-1">
+	{#if nonNullish($addressStore)}
+		<output>{shortenWithMiddleEllipsis($addressStore ?? '')}</output><Copy
+			value={shortenWithMiddleEllipsis($addressStore ?? '')}
+		/>
+	{/if}
+</div>
