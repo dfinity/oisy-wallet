38723f92c27172a960d25a2cc469450f37cbe5d4
David Dal Busco <david.dalbusco@outlook.com>
2023-08-16 14:37:09 +0200

    feat(frontend): receive

--- src/frontend/src/lib/components/actions/Receive.svelte
+++ src/frontend/src/lib/components/actions/Receive.svelte
@@ -2,12 +2,24 @@
 	import IconReceive from '$lib/components/icons/IconReceive.svelte';
 	import { addressStoreNotLoaded } from '$lib/stores/address.store';
 	import { isBusy } from '$lib/stores/busy.store';
+	import { Modal } from '@dfinity/gix-components';
 
 	let disabled;
 	$: disabled = $addressStoreNotLoaded || $isBusy;
+
+	let visible = false;
 </script>
 
-<button class="flex-1 secondary" {disabled} class:opacity-50={disabled}>
+<button
+	class="flex-1 secondary"
+	{disabled}
+	class:opacity-50={disabled}
+	on:click={() => (visible = true)}
+>
 	<IconReceive size="28" />
 	<span>Receive</span></button
 >
+
+<Modal {visible} on:nnsClose={() => (visible = false)}>
+	<svelte:fragment slot="title">Receive ETH</svelte:fragment>
+</Modal>
