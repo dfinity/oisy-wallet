2635f0f15aa962f6fdb1ea9e048f83973b38d0a9
David Dal Busco <david.dalbusco@outlook.com>
2023-08-16 14:59:27 +0200

    feat: qr code

--- src/frontend/src/lib/components/actions/Receive.svelte
+++ src/frontend/src/lib/components/actions/Receive.svelte
@@ -2,7 +2,7 @@
 	import IconReceive from '$lib/components/icons/IconReceive.svelte';
 	import { addressStore, addressStoreNotLoaded } from '$lib/stores/address.store';
 	import { isBusy } from '$lib/stores/busy.store';
-	import { Modal, QRCode } from '@dfinity/gix-components';
+	import { Copy, Modal, QRCode } from '@dfinity/gix-components';
 	import IconETHQRCode from '$lib/components/icons/IconETHQRCode.svelte';
 
 	let disabled;
@@ -23,6 +23,11 @@
 
 <Modal {visible} on:nnsClose={() => (visible = false)}>
 	<svelte:fragment slot="title">Receive ETH</svelte:fragment>
+
+	<p class="font-bold">Wallet address</p>
+	<p class="flex gap-1 mb-2 font-normal sm:items-center">
+		<output class="break-words">{$addressStore ?? ''}</output><Copy value={$addressStore ?? ''} />
+	</p>
 
 	<div
 		class="p-4 rounded-sm"
