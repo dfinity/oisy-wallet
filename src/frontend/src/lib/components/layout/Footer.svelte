fca4ba5d2b62b5f470ab5456391961a3e24df099
David Dal Busco <david.dalbusco@outlook.com>
2023-08-16 11:04:56 +0200

    feat(frontend): link to github (#41)

---
+++ src/frontend/src/lib/components/layout/Footer.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { IconGitHub } from '@dfinity/gix-components';
+</script>
+
+<footer class="flex justify-center mt-8 pb-4">
+	<a
+		href="https://github.com/dfinity/ic-eth-wallet"
+		rel="external noopener noreferrer"
+		target="_blank"
+		class="flex gap-1 items-center md:mt-8"
+		style="text-decoration: none"
+	>
+		<IconGitHub /> Source code on GitHub
+	</a>
+</footer>
