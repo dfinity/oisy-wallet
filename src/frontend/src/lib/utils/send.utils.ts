a61167cecfcc3b183212d2760d82b1abcb2335c4
David Dal Busco <david.dalbusco@outlook.com>
2023-08-21 16:02:57 +0200

    chore: format

--- src/frontend/src/lib/utils/send.utils.ts
+++ src/frontend/src/lib/utils/send.utils.ts
@@ -4,4 +4,4 @@ export const invalidDestination = (destination: string): boolean =>
 	isNullish(destination) || destination === '';
 
 export const invalidAmount = (amount: number | undefined): boolean =>
-    isNullish(amount) || amount <= 0;
+	isNullish(amount) || amount <= 0;
