b541e64d1571379da60ad649df92304ce620acd4
David Dal Busco <david.dalbusco@outlook.com>
2023-08-23 09:34:06 +0200

    feat: connectwallet modal

--- src/frontend/src/lib/components/wallet-connect/WalletConnect.svelte
+++ src/frontend/src/lib/components/wallet-connect/WalletConnect.svelte
@@ -1,25 +1,27 @@
 <script lang="ts">
-	import { Modal, QRCodeReaderModal } from '@dfinity/gix-components';
+	import { Input, Modal, QRCodeReader } from '@dfinity/gix-components';
 	import { toastsError } from '$lib/stores/toasts.store';
 	import type { WebSocketListener } from '$lib/types/listener';
-	import { isNullish } from '@dfinity/utils';
 	import { onDestroy } from 'svelte';
 	import { initWalletConnectListener } from '$lib/services/listener.services';
 	import { addressStore } from '$lib/stores/address.store';
+	import { fade } from 'svelte/transition';
 
 	let visible = false;
 	let renderQRCodeReader = false;
-	let qrCode = undefined;
 
 	const close = () => {
 		visible = false;
 		renderQRCodeReader = false;
 	};
 
-	const error = () =>
+	const error = () => {
+		renderQRCodeReader = false;
+
 		toastsError({
 			msg: { text: `Cannot read QR code.` }
 		});
+	};
 
 	let listener: WebSocketListener | undefined = undefined;
 
@@ -35,25 +37,81 @@
 	onDestroy(async () => await listener?.disconnect());
 
 	let uri: string = '';
+
 	const connect = async () => {
-		console.log(uri);
+		if (!uri) {
+			toastsError({
+				msg: { text: `An uri to connect to should be provided.` }
+			});
+			return;
+		}
+
 		await initListener(uri);
 	};
+
+	let invalid = true;
+	$: invalid = !uri;
 </script>
 
 <button on:click={() => (visible = true)} class="secondary text-deep-violet mt-2">
-	Wallet connect
+	WalletConnect
 </button>
 
-<input type="text" style="background: red" bind:value={uri} on:change={connect} />
+<Modal {visible} on:nnsClose={close}>
+	<svelte:fragment slot="title">WalletConnect</svelte:fragment>
 
-<Modal {visible} on:nnsClose={close} on:introend={() => (renderQRCodeReader = true)}>
-	<svelte:fragment slot="title">Scan QR Code</svelte:fragment>
+	<div class="rounded-lg qr-code">
+		{#if renderQRCodeReader}
+			<QRCodeReader on:nnsQRCode={async ({ detail }) => (uri = detail)} on:nnsQRCodeError={error} />
+		{/if}
 
-	{#if renderQRCodeReader}
-		<QRCodeReaderModal
-			on:nnsQRCode={async ({ detail: uri }) => await initListener(uri)}
-			on:nnsQRCodeError={error}
-		/>
-	{/if}
+		{#if !renderQRCodeReader}
+			<button
+				type="button"
+				class="tertiary inset-center"
+				out:fade
+				on:click={() => (renderQRCodeReader = true)}>Scan QR code</button
+			>
+		{/if}
+	</div>
+
+	<p class="text-center p-2 text-xs">or use walletconnect uri</p>
+
+	<Input
+		name="uri"
+		required
+		inputType="text"
+		placeholder="e.g. wc:a281567bb3e4..."
+		bind:value={uri}
+	/>
+
+	<div class="flex justify-end gap-1 mt-2">
+		<button class="primary" disabled={invalid} class:opacity-15={invalid} on:click={connect}>
+			Connect
+		</button>
+	</div>
 </Modal>
+
+<style lang="scss">
+	@use '../../../../../../node_modules/@dfinity/gix-components/dist/styles/mixins/media';
+
+	.qr-code {
+		position: relative;
+
+		border: 1px dashed var(--color-vampire-black);
+
+		margin: 0 auto;
+
+		width: 100%;
+
+		@include media.min-width(small) {
+			width: calc(var(--dialog-width) - (4 * var(--padding-8x)));
+		}
+
+		@media only screen and (hover: none) and (pointer: coarse) {
+			aspect-ratio: 1 / 1;
+		}
+
+		aspect-ratio: 4 / 3;
+	}
+</style>
