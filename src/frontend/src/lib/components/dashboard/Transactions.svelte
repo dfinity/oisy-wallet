9a35088249c7ae9efb6457c08bca2701bd91dfd6
David Dal Busco <david.dalbusco@outlook.com>
2023-08-28 15:25:58 +0200

    feat(frontend): tabs

--- src/frontend/src/lib/components/dashboard/Transactions.svelte
+++ src/frontend/src/lib/components/dashboard/Transactions.svelte
@@ -3,7 +3,14 @@
 	import Transaction from '$lib/components/dashboard/Transaction.svelte';
 </script>
 
-<p class="font-bold" class:mb-3={$sortedTransactionsStore.length > 0}>Activity</p>
+<div
+	class="font-bold flex"
+	class:mb-4={$sortedTransactionsStore.length > 0}
+	style="border-bottom: 1px solid var(--color-deep-violet)"
+>
+	<button class="flex-1 tab" style="border-bottom: 3px solid var(--color-deep-violet)">Tokens</button>
+	<button class="flex-1 tab" style="border-bottom: 3px solid transparent">Activity</button>
+</div>
 
 {#each $sortedTransactionsStore as transaction, index (transaction.hash)}
 	<Transaction {transaction} />
