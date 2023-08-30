3c300931b95ca5976f0dca0846280977ff1a8ff8
David Dal Busco <david.dalbusco@outlook.com>
2023-08-30 16:55:27 +0200

    feat(frontend): erc20 transfer (#86)
    * feat(frontend): erc20 transfer
    
    * feat: wip
    
    * feat: sign transaction with data
    
    * feat: sign transaction with data

--- src/frontend/src/lib/services/send.services.ts
+++ src/frontend/src/lib/services/send.services.ts
@@ -1,8 +1,13 @@
+import type { SignRequest } from '$declarations/backend/backend.did';
 import { signTransaction } from '$lib/api/backend.api';
 import { ETH_BASE_FEE, ETH_NETWORK_ID } from '$lib/constants/eth.constants';
+import { ETHEREUM_TOKEN_ID } from '$lib/constants/tokens.constants';
 import { SendStep } from '$lib/enums/send';
+import { populateTransaction } from '$lib/providers/etherscan-erc20.providers';
 import { getTransactionCount, sendTransaction } from '$lib/providers/etherscan.providers';
-import type { TokenId } from '$lib/types/token';
+import type { Erc20Token } from '$lib/types/erc20';
+import type { Token } from '$lib/types/token';
+import { isNullish } from '@dfinity/utils';
 import { Utils } from 'alchemy-sdk';
 
 export interface TransferParams {
@@ -13,28 +18,75 @@ export interface TransferParams {
 	maxFeePerGas: bigint;
 }
 
-export const send = async ({
-	progress,
-	tokenId,
-	from,
+const ethPrepareTransaction = async ({
 	to,
 	amount,
 	maxPriorityFeePerGas: max_priority_fee_per_gas,
-	maxFeePerGas: max_fee_per_gas
-}: TransferParams & { progress: (step: SendStep) => void; tokenId: TokenId }) => {
-	progress(SendStep.INITIALIZATION);
+	maxFeePerGas: max_fee_per_gas,
+	nonce
+}: TransferParams & { nonce: number }): Promise<SignRequest> => ({
+	to,
+	value: Utils.parseEther(`${amount}`).toBigInt(),
+	chain_id: ETH_NETWORK_ID,
+	nonce: BigInt(nonce),
+	gas: ETH_BASE_FEE,
+	max_fee_per_gas,
+	max_priority_fee_per_gas,
+	data: []
+});
 
-	const nonce = await getTransactionCount(from);
+const erc20PrepareTransaction = async ({
+	to,
+	amount,
+	maxPriorityFeePerGas: max_priority_fee_per_gas,
+	maxFeePerGas: max_fee_per_gas,
+	nonce,
+	token
+}: TransferParams & { nonce: number; token: Token }): Promise<SignRequest> => {
+	const { data } = await populateTransaction({
+		contract: token as Erc20Token,
+		address: to,
+		amount: Utils.parseEther(`${amount}`)
+	});
 
-	const transaction = {
+	if (isNullish(data)) {
+		throw new Error('Erc20 transaction Data cannot be undefined or null.');
+	}
+
+	return {
 		to,
-		value: Utils.parseEther(`${amount}`).toBigInt(),
 		chain_id: ETH_NETWORK_ID,
 		nonce: BigInt(nonce),
 		gas: ETH_BASE_FEE,
 		max_fee_per_gas,
-		max_priority_fee_per_gas
-	} as const;
+		max_priority_fee_per_gas,
+		value: 0n,
+		data: [data]
+	};
+};
+
+export const send = async ({
+	progress,
+	token,
+	from,
+	...rest
+}: TransferParams & { progress: (step: SendStep) => void; token: Token }) => {
+	progress(SendStep.INITIALIZATION);
+
+	const nonce = await getTransactionCount(from);
+
+	const transaction = await (token.id === ETHEREUM_TOKEN_ID
+		? ethPrepareTransaction({
+				...rest,
+				from,
+				nonce
+		  })
+		: erc20PrepareTransaction({
+				...rest,
+				from,
+				token,
+				nonce
+		  }));
 
 	progress(SendStep.SIGN);
 
