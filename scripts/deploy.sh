9244b23711c327012f52a059bcdbbb898dbc89e8
David Dal Busco <david.dalbusco@outlook.com>
2023-08-30 17:36:01 +0200

    build: remove reinstall

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ dfx deploy backend --argument "(variant {
   Init = record {
        ecdsa_key_name = \"dfx_test_key\"
    }
-})" --mode=reinstall
+})"
 
 dfx deploy internet_identity
 dfx deploy frontend
