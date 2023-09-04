4ff73d516e7522a501c8912e0ccf9f34751618b4
David Dal Busco <david.dalbusco@outlook.com>
2023-09-04 10:38:10 +0200

    fix: types

--- src/frontend/src/app.d.ts
+++ src/frontend/src/app.d.ts
@@ -1,17 +1,12 @@
-import type { MetamaskProvider } from '$lib/types/metamask';
-
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
 declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
 		// interface Platform {}
-	}
-
-	declare interface Window {
-		ethereum: MetamaskProvider;
 	}
 }
 
