f116e64b6d4a8414c3171708043ee14356af1a8f
David Dal Busco <david.dalbusco@outlook.com>
2023-09-04 16:24:08 +0200

    feat: deconstruct transaction hash

--- src/frontend/src/lib/providers/alchemy-erc20.providers.ts
+++ src/frontend/src/lib/providers/alchemy-erc20.providers.ts
@@ -2,6 +2,7 @@ import { ERC20_ABI } from '$lib/constants/erc20.constants';
 import type { ECDSA_PUBLIC_KEY } from '$lib/types/address';
 import type { Erc20Token } from '$lib/types/erc20';
 import type { WebSocketListener } from '$lib/types/listener';
+import type { BigNumber } from '@ethersproject/bignumber';
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { ethers } from 'ethers';
 
@@ -22,16 +23,23 @@ export const initPendingTransactionsListener = ({
 }): WebSocketListener => {
 	const erc20Contract = new ethers.Contract(contract.address, ERC20_ABI, provider);
 
+	const filterListener = async (
+		_from: string,
+		_address: string,
+		_value: BigNumber,
+		{ transactionHash }: { transactionHash: string }
+	) => await listener(transactionHash);
+
 	const filterFromAddress = erc20Contract.filters.Transfer(address, null);
-	erc20Contract.on(filterFromAddress, listener);
+	erc20Contract.on(filterFromAddress, filterListener);
 
 	const filterToAddress = erc20Contract.filters.Transfer(null, address);
-	erc20Contract.on(filterToAddress, listener);
+	erc20Contract.on(filterToAddress, filterListener);
 
 	return {
 		disconnect: async () => {
-			erc20Contract.off(filterFromAddress, listener);
-			erc20Contract.off(filterToAddress, listener);
+			erc20Contract.off(filterFromAddress, filterListener);
+			erc20Contract.off(filterToAddress, filterListener);
 		}
 	};
 };
