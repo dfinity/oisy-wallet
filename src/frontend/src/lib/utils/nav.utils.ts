8c05764a7e29ca90faaa4f79a27ff244c80d7436
David Dal Busco <david.dalbusco@outlook.com>
2023-09-06 16:37:19 +0200

    feat(frontend): encode and decode emojis in urls

--- src/frontend/src/lib/utils/nav.utils.ts
+++ src/frontend/src/lib/utils/nav.utils.ts
@@ -15,7 +15,10 @@ const tokenUrl = ({
 }: {
 	token: Token;
 	path: 'transactions/' | '/';
-}): string => `${path}?token=${encodeURIComponent(name)}`;
+}): string =>
+	`${path}?token=${encodeURIComponent(
+		name.replace(/\p{Emoji}/gu, (m, idx) => `\\u${m.codePointAt(0)?.toString(16)}`)
+	)}`;
 
 export const back = async (pop: boolean) => {
 	if (!pop) {
@@ -41,7 +44,17 @@ export const loadRouteToken = ($event: LoadEvent): RouteToken => {
 
 	const token = searchParams?.get('token');
 
+	const replaceEmoji = (input: string | null): string | null => {
+		if (input === null) {
+			return null;
+		}
+
+		return input.replace(/\\u([\dA-Fa-f]+)/g, (_match, hex) =>
+			String.fromCodePoint(Number(`0x${hex}`))
+		);
+	};
+
 	return {
-		token: nonNullish(token) ? decodeURIComponent(token) : null
+		token: nonNullish(token) ? replaceEmoji(decodeURIComponent(token)) : null
 	};
 };
