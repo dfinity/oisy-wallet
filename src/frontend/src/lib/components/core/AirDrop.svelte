d19912835e9c466b7e766f08b8a818f3af967173
David Dal Busco <david.dalbusco@outlook.com>
2023-09-06 08:40:51 +0200

    style: spacing

--- src/frontend/src/lib/components/core/AirDrop.svelte
+++ src/frontend/src/lib/components/core/AirDrop.svelte
@@ -2,7 +2,7 @@
 	import IconCelebrate from '$lib/components/icons/IconCelebrate.svelte';
 </script>
 
-<p class="font-bold text-center bg-white rounded-lg m-2 px-2 py-1.5">
+<p class="font-bold text-center bg-white rounded-lg m-3 px-2 py-1.5">
 	<IconCelebrate />
 	<span class="px-0.5"
 		>Participate in $100K ICP ERC-20 Airdrop by solving a quiz about the Internet Computer</span
