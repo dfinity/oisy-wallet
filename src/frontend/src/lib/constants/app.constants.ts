0f8e6466d79a5e6785ce2e2531e0edd4ec7d7d5f
David Dal Busco <david.dalbusco@outlook.com>
2023-09-06 15:14:24 +0200

    feat(frontend): one hour delegation length (#112)

--- src/frontend/src/lib/constants/app.constants.ts
+++ src/frontend/src/lib/constants/app.constants.ts
@@ -4,8 +4,8 @@ export const localIdentityCanisterId: string | null | undefined = import.meta.en
 	.VITE_INTERNET_IDENTITY_CANISTER_ID as string | null | undefined;
 
 // How long the delegation identity should remain valid?
-// e.g. BigInt(7 * 24 * 60 * 60 * 1000 * 1000 * 1000) = 7 days in nanoseconds
-export const AUTH_MAX_TIME_TO_LIVE = BigInt(7 * 24 * 60 * 60 * 1000 * 1000 * 1000);
+// e.g. BigInt(60 * 60 * 1000 * 1000 * 1000) = 1 hour in nanoseconds
+export const AUTH_MAX_TIME_TO_LIVE = BigInt(60 * 60 * 1000 * 1000 * 1000);
 
 export const AUTH_POPUP_WIDTH = 576;
 export const AUTH_POPUP_HEIGHT = 625;
