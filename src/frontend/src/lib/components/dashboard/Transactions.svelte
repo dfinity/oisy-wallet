1f53be187ca0c5f8a6807e041ff9a06dccbfce73
David Dal Busco <david.dalbusco@outlook.com>
2023-09-06 10:51:14 +0200

    feat: text

--- src/frontend/src/lib/components/dashboard/Transactions.svelte
+++ src/frontend/src/lib/components/dashboard/Transactions.svelte
@@ -20,7 +20,7 @@
 <h2 class="text-base mb-3 pb-0.5">Transactions</h2>
 
 {#if loading}
-	<p class="mt-1 text-cetacean-blue opacity-50">Loading...</p>
+	<p class="mt-1 text-cetacean-blue opacity-50">Transactions will appear here. Loading...</p>
 {:else}
 	{#each $sortedTransactions as transaction, _index (transaction.hash)}
 		<Transaction {transaction} />
