8fd67afd21b98c975e865f2dc06d1dd5b6e0f45d
David Dal Busco <david.dalbusco@outlook.com>
2023-09-06 07:42:32 +0200

    style(frontend): login page

---
+++ src/frontend/src/lib/components/core/Alpha.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import IconWarning from '$lib/components/icons/IconWarning.svelte';
+</script>
+
+<p
+	class="text-misty-rose flex gap-1 items-center justify-center text-center px-6 py-1"
+	style="border-radius: 30px; border: 2px solid var(--color-misty-rose)"
+>
+	<IconWarning /> This is Alpha version. Use at your own risk!
+</p>
