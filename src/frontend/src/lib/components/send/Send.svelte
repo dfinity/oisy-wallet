af6d76ea495321cefbfa68a0f43c81fa034b9df2
David Dal Busco <david.dalbusco@outlook.com>
2023-09-07 09:08:03 +0200

    feat(frontend): context fee (#116)

--- src/frontend/src/lib/components/send/Send.svelte
+++ src/frontend/src/lib/components/send/Send.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import { toastsError } from '$lib/stores/toasts.store';
-	import { getFeeData } from '$lib/providers/etherscan.providers';
-	import { getFeeData as getErc20FeeData } from '$lib/providers/etherscan-erc20.providers';
 	import { send as executeSend } from '$lib/services/send.services';
-	import { debounce, isNullish } from '@dfinity/utils';
+	import { isNullish } from '@dfinity/utils';
 	import IconSend from '$lib/components/icons/IconSend.svelte';
 	import { type WizardStep, type WizardSteps, WizardModal } from '@dfinity/gix-components';
 	import SendForm from '$lib/components/send/SendForm.svelte';
@@ -11,84 +9,36 @@
 	import { invalidAmount, invalidDestination } from '$lib/utils/send.utils';
 	import SendProgress from '$lib/components/send/SendProgress.svelte';
 	import { SendStep } from '$lib/enums/send';
-	import { onDestroy } from 'svelte';
-	import { initMinedTransactionsListener } from '$lib/services/listener.services';
-	import type { WebSocketListener } from '$lib/types/listener';
 	import { modalStore } from '$lib/stores/modal.store';
 	import { balanceEmpty } from '$lib/derived/balances.derived';
 	import { addressNotLoaded } from '$lib/derived/address.derived';
 	import { modalSend } from '$lib/derived/modal.derived';
 	import { addressStore } from '$lib/stores/address.store';
 	import { token } from '$lib/derived/token.derived';
-	import { ETHEREUM_TOKEN_ID } from '$lib/constants/tokens.constants';
-	import type { Erc20Token } from '$lib/types/erc20';
-	import type { TransactionFeeData } from '$lib/types/transaction';
-	import { BigNumber } from '@ethersproject/bignumber';
-	import { ETH_BASE_FEE } from '$lib/constants/eth.constants';
-	import { Utils } from 'alchemy-sdk';
+	import {
+		FEE_CONTEXT_KEY,
+		type FeeContext as FeeContextType,
+		initFeeStore
+	} from '$lib/stores/fee.store';
+	import { setContext } from 'svelte';
+	import FeeContext from '$lib/components/fee/FeeContext.svelte';
+
+	/**
+	 * Fee context store
+	 */
+
+	let storeFeeData = initFeeStore();
+
+	setContext<FeeContextType>(FEE_CONTEXT_KEY, {
+		store: storeFeeData
+	});
+
+	/**
+	 * Props
+	 */
 
 	let destination = '';
 	let amount: number | undefined = undefined;
-
-	/**
-	 * Fee data
-	 */
-
-	let feeData: TransactionFeeData | undefined;
-	let listener: WebSocketListener | undefined = undefined;
-
-	const updateFeeData = async () => {
-		try {
-			if ($token.id === ETHEREUM_TOKEN_ID) {
-				feeData = {
-					...(await getFeeData()),
-					gas: BigNumber.from(ETH_BASE_FEE)
-				};
-				return;
-			}
-
-			feeData = {
-				...(await getFeeData()),
-				gas: await getErc20FeeData({
-					contract: $token as Erc20Token,
-					address: destination !== '' ? destination : $addressStore!,
-					amount: Utils.parseEther(`${amount ?? '1'}`)
-				})
-			};
-		} catch (err: unknown) {
-			toastsError({
-				msg: { text: `Cannot fetch gas fee.` },
-				err
-			});
-		}
-	};
-
-	const debounceUpdateFeeData = debounce(updateFeeData);
-
-	const initFeeData = async (modalOpen: boolean) => {
-		await listener?.disconnect();
-
-		if (!modalOpen) {
-			return;
-		}
-
-		await updateFeeData();
-		listener = initMinedTransactionsListener(async () => debounceUpdateFeeData());
-	};
-
-	onDestroy(() => listener?.disconnect());
-
-	$: initFeeData($modalSend);
-
-	$: amount,
-		destination,
-		(() => {
-			if ($token.id === ETHEREUM_TOKEN_ID) {
-				return;
-			}
-
-			debounceUpdateFeeData();
-		})();
 
 	/**
 	 * Send
@@ -111,7 +61,7 @@
 			return;
 		}
 
-		if (isNullish(feeData)) {
+		if (isNullish($storeFeeData)) {
 			toastsError({
 				msg: { text: `Gas fees are not defined.` }
 			});
@@ -119,7 +69,7 @@
 		}
 
 		// https://github.com/ethers-io/ethers.js/discussions/2439#discussioncomment-1857403
-		const { maxFeePerGas, maxPriorityFeePerGas, gas } = feeData;
+		const { maxFeePerGas, maxPriorityFeePerGas, gas } = $storeFeeData;
 
 		// https://docs.ethers.org/v5/api/providers/provider/#Provider-getFeeData
 		// exceeds block gas limit
@@ -182,8 +132,6 @@
 		destination = '';
 		amount = undefined;
 
-		feeData = undefined;
-
 		sendProgressStep = SendStep.INITIALIZATION;
 	};
 </script>
@@ -202,12 +150,14 @@
 	<WizardModal {steps} bind:currentStep bind:this={modal} on:nnsClose={close}>
 		<svelte:fragment slot="title">{currentStep?.title ?? ''}</svelte:fragment>
 
-		{#if currentStep?.name === 'Review'}
-			<SendReview on:icBack={modal.back} on:icSend={send} bind:destination bind:amount {feeData} />
-		{:else if currentStep?.name === 'Sending'}
-			<SendProgress progressStep={sendProgressStep} />
-		{:else}
-			<SendForm on:icNext={modal.next} on:icClose={close} bind:destination bind:amount {feeData} />
-		{/if}
+		<FeeContext {amount} {destination} observe={$modalSend && currentStep?.name !== 'Sending'}>
+			{#if currentStep?.name === 'Review'}
+				<SendReview on:icBack={modal.back} on:icSend={send} bind:destination bind:amount />
+			{:else if currentStep?.name === 'Sending'}
+				<SendProgress progressStep={sendProgressStep} />
+			{:else}
+				<SendForm on:icNext={modal.next} on:icClose={close} bind:destination bind:amount />
+			{/if}
+		</FeeContext>
 	</WizardModal>
 {/if}
---
+++ src/frontend/src/lib/stores/fee.store.ts
@@ -0,0 +1,27 @@
+import type { TransactionFeeData } from '$lib/types/transaction';
+import type { Readable } from 'svelte/store';
+import { writable } from 'svelte/store';
+
+export type FeeStoreData = TransactionFeeData | undefined;
+
+export interface FeeStore extends Readable<FeeStoreData> {
+	setFee: (data: TransactionFeeData) => void;
+}
+
+export const initFeeStore = (): FeeStore => {
+	const { subscribe, set } = writable<FeeStoreData>(undefined);
+
+	return {
+		subscribe,
+
+		setFee(data: TransactionFeeData) {
+			set(data);
+		}
+	};
+};
+
+export interface FeeContext {
+	store: FeeStore;
+}
+
+export const FEE_CONTEXT_KEY = Symbol('fee');
