ea23bcd811ebe26ca0c415a20934b2e16d776a0d
David Dal Busco <david.dalbusco@outlook.com>
2023-09-11 17:24:42 +0200

    feat(frontend): display message to sign as utf-8 (#131)

--- src/frontend/src/lib/utils/wallet-connect.utils.ts
+++ src/frontend/src/lib/utils/wallet-connect.utils.ts
@@ -1,4 +1,18 @@
 import { isAddress } from '@ethersproject/address';
+import {Utils} from "alchemy-sdk";
 
 export const getSignParamsMessageHex = (params: string[]): string =>
 	params.filter((p) => !isAddress(p))[0];
+
+export const getSignParamsMessage = (params: string[]): string => {
+	const message = getSignParamsMessageHex(params);
+	return convertHexToUtf8(message);
+};
+
+export const convertHexToUtf8 = (value: string): string => {
+	if (Utils.isHexString(value)) {
+		return Utils.toUtf8String(value);
+	}
+
+	return value;
+};
