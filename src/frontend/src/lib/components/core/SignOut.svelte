180786a5d019de357f71bb58fedfb30485df4671
David Dal Busco <david.dalbusco@outlook.com>
2023-09-11 11:37:13 +0200

    feat(frontend): logout popover (#126)

---
+++ src/frontend/src/lib/components/core/SignOut.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { signOut } from '$lib/services/auth.services';
+	import { IconLogout } from '@dfinity/gix-components';
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	const logout = async () => {
+		dispatch('icLogoutTriggered');
+		await signOut();
+	};
+</script>
+
+<button data-tid="logout" on:click={logout} class="text">
+	<IconLogout />
+	Logout
+</button>
--- src/frontend/src/lib/components/core/UserPrincipal.svelte
+++ src/frontend/src/lib/components/core/UserPrincipal.svelte
@@ -1,13 +1,26 @@
 <script lang="ts">
-	import { signOut } from '$lib/services/auth.services';
 	import IconIcLogo from '$lib/components/icons/IconIcLogo.svelte';
 	import { authStore } from '$lib/stores/auth.store';
 	import { shortenWithMiddleEllipsis } from '$lib/utils/format.utils';
+	import { Popover } from '@dfinity/gix-components';
+	import SignOut from '$lib/components/core/SignOut.svelte';
+
+	let visible = false;
+	let button: HTMLButtonElement | undefined;
 </script>
 
-<button class="primary icon" on:click={signOut} aria-label="Sign-out">
+<button
+	class="primary icon"
+	bind:this={button}
+	on:click={() => (visible = true)}
+	aria-label="Sign-out"
+>
 	<IconIcLogo />
 	<span class="text-dark-slate-blue font-bold text-xs"
 		>{shortenWithMiddleEllipsis($authStore?.identity?.getPrincipal().toText() ?? '')}</span
 	>
 </button>
+
+<Popover bind:visible anchor={button} direction="rtl">
+	<SignOut on:icLogoutTriggered={() => (visible = false)} />
+</Popover>
