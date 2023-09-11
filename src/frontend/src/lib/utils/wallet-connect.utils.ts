2642369b91a1ccc9ea6807ad1a4867a68b0da4e8
David Dal Busco <david.dalbusco@outlook.com>
2023-09-11 17:18:26 +0200

    feat: sign message

--- src/frontend/src/lib/utils/wallet-connect.utils.ts
+++ src/frontend/src/lib/utils/wallet-connect.utils.ts
@@ -1,16 +1,4 @@
 import { isAddress } from '@ethersproject/address';
-import { Utils } from 'alchemy-sdk';
 
-export const getSignParamsMessage = (params: string[]): string => {
-	const message = params.filter((p) => !isAddress(p))[0];
-
-	return convertHexToUtf8(message);
-};
-
-export const convertHexToUtf8 = (value: string): string => {
-	if (Utils.isHexString(value)) {
-		return Utils.toUtf8String(value);
-	}
-
-	return value;
-};
+export const getSignParamsMessageHex = (params: string[]): string =>
+	params.filter((p) => !isAddress(p))[0];
