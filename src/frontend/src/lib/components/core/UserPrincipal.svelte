1b369710ab78faf4f8b1c0c2614ee8e8020a6ad4
David Dal Busco <david.dalbusco@outlook.com>
2023-09-11 13:06:57 +0200

    feat(frontend): external links in menu (#128)

--- src/frontend/src/lib/components/core/UserPrincipal.svelte
+++ src/frontend/src/lib/components/core/UserPrincipal.svelte
@@ -2,8 +2,9 @@
 	import IconIcLogo from '$lib/components/icons/IconIcLogo.svelte';
 	import { authStore } from '$lib/stores/auth.store';
 	import { shortenWithMiddleEllipsis } from '$lib/utils/format.utils';
-	import { Popover } from '@dfinity/gix-components';
+	import { IconGitHub, Popover } from '@dfinity/gix-components';
 	import SignOut from '$lib/components/core/SignOut.svelte';
+	import Hr from '$lib/components/ui/Hr.svelte';
 
 	let visible = false;
 	let button: HTMLButtonElement | undefined;
@@ -22,5 +23,35 @@
 </button>
 
 <Popover bind:visible anchor={button} direction="rtl">
-	<SignOut on:icLogoutTriggered={() => (visible = false)} />
+	<div class="flex flex-col gap-1">
+		<a
+			href="https://internetcomputer.org"
+			rel="external noopener noreferrer"
+			target="_blank"
+			class="flex gap-1 items-center no-underline"
+			aria-label="Open the Internet Computer portal"
+		>
+			<div
+				class="flex items-center justify-center rounded-50"
+				style="border: 1px solid var(--color-grey); zoom: 0.6;"
+			>
+				<IconIcLogo />
+			</div>
+			Internet Computer
+		</a>
+
+		<a
+			href="https://github.com/dfinity/ic-eth-wallet"
+			rel="external noopener noreferrer"
+			target="_blank"
+			class="flex gap-1 items-center no-underline"
+			aria-label="Source code on GitHub"
+		>
+			<IconGitHub /> Source code
+		</a>
+
+		<Hr />
+
+		<SignOut on:icLogoutTriggered={() => (visible = false)} />
+	</div>
 </Popover>
