55e6ab8ad0e8f9458516903f74dabdd6d743a16d
David Dal Busco <david.dalbusco@outlook.com>
2023-09-12 17:52:12 +0200

    chore: format

--- src/frontend/src/lib/utils/wallet-connect.utils.ts
+++ src/frontend/src/lib/utils/wallet-connect.utils.ts
@@ -1,7 +1,7 @@
+import type { WalletConnectEthSignTypedDataV4 } from '$lib/types/wallet-connect';
 import { isAddress } from '@ethersproject/address';
 import { Utils } from 'alchemy-sdk';
 import { utils } from 'ethers';
-import type {WalletConnectEthSignTypedDataV4} from "$lib/types/wallet-connect";
 
 export const getSignParamsMessageHex = (params: string[]): string =>
 	params.filter((p) => !isAddress(p))[0];
@@ -11,7 +11,9 @@ export const getSignParamsMessageUtf8 = (params: string[]): string => {
 	return convertHexToUtf8(message);
 };
 
-export const getSignParamsMessageTypedDataV4 = (params: string[]): WalletConnectEthSignTypedDataV4 => {
+export const getSignParamsMessageTypedDataV4 = (
+	params: string[]
+): WalletConnectEthSignTypedDataV4 => {
 	const message = getSignParamsMessageHex(params);
 	return JSON.parse(message);
 };
