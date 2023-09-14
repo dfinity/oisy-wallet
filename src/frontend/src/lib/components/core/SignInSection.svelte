c688d93f229a1324957d139ca1c5d2909e96d310
David Dal Busco <david.dalbusco@outlook.com>
2023-09-14 17:32:27 +0200

    feat(frontend): review login text

--- src/frontend/src/lib/components/core/SignInSection.svelte
+++ src/frontend/src/lib/components/core/SignInSection.svelte
@@ -4,13 +4,17 @@
 	import OisyWalletLogo from '$lib/components/icons/OisyWalletLogo.svelte';
 </script>
 
-<section class="flex flex-col justify-center items-center sm:pt-8 mt-8 relative z-1">
+<section class="flex flex-col justify-center items-center sm:pt-6 mt-6 relative z-1">
 	<OisyWalletLogo size="66" />
 
 	<h1 class="text-center py-4 xs:px-2 sm:px-4 mb-0.25" style="max-width: 600px">
-		Say goodbye to browser extensions—enjoy safe fully on-chain experience possible only on the
-		Internet Computer
+		Non-custodial, Browser-based Wallet, Internet Computer Secured
 	</h1>
+
+	<p class="text-center pb-4 xs:px-2 sm:px-4 mb-0.25" style="max-width: 600px">
+		Merging Ethereum and the Internet Computer, OISY provides a seamless on-chain experience,
+		eliminating the need for browser-extensions or mobile apps; it's passkey-enabled.
+	</p>
 
 	<ButtonIC on:click={async () => await signIn({})}>
 		<svelte:fragment slot="action">Connect with</svelte:fragment>
