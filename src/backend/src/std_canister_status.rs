dc31c04ffb777bf318f37cf36de7b4b7dc31dd55
Max Murphy <max@winning.black>
2023-09-14 09:58:48 +0200

    ++

--- src/backend/src/std_canister_status.rs
+++ src/backend/src/std_canister_status.rs
@@ -1,15 +1,15 @@
 //! Support for the standard CanisterStatusResultV2.
-//! 
+//!
 //! Note: This API is used my many canisters but the code is not packaged up in a portable way and typically uses old APIs to get the data.
-//! 
+//!
 //! The `ic_cdk` has a method called [`canister_status`](https://docs.rs/ic-cdk/0.10.0/ic_cdk/api/management_canister/main/fn.canister_status.html)
-//! with all the same data.  Perhaps the cycle management canister could support that.  In the meantime we convert the type used in the 
+//! with all the same data.  Perhaps the cycle management canister could support that.  In the meantime we convert the type used in the
 //! current `ic_cdk` into the currently requested `CanisterStatusResultV2`.
 
 use candid::{CandidType, Deserialize, Principal};
+use ic_cdk::api::management_canister::main::CanisterIdRecord;
 use ic_cdk::api::management_canister::main::CanisterStatusResponse;
 use ic_cdk::api::management_canister::main::CanisterStatusType;
-use ic_cdk::api::management_canister::main::CanisterIdRecord;
 use ic_cdk::api::management_canister::main::DefiniteCanisterSettings;
 
 /// Copy of the synonymous Rosetta type.
@@ -26,8 +26,9 @@ pub struct CanisterStatusResultV2 {
     freezing_threshold: candid::Nat,
 }
 
-impl From<CanisterStatusResponse> for CanisterStatusResultV2 {
-    fn from(value: CanisterStatusResponse) -> Self {
+impl TryFrom<CanisterStatusResponse> for CanisterStatusResultV2 {
+    type Error = &'static str;
+    fn try_from(value: CanisterStatusResponse) -> Result<Self, Self::Error> {
         let CanisterStatusResponse {
             status,
             module_hash,
@@ -37,19 +38,23 @@ impl From<CanisterStatusResponse> for CanisterStatusResultV2 {
             ..
         } = value;
 
-        let controller = settings.controllers.get(0).expect("This canister has not even one controller").clone();
+        let controller = settings
+            .controllers
+            .get(0)
+            .ok_or("This canister has not even one controller")?
+            .clone();
         let balance = vec![(vec![0], cycles.clone())];
         let freezing_threshold = settings.freezing_threshold.clone();
-        Self {
+        Ok(Self {
             status,
             module_hash,
             controller,
-            settings: settings.into(),
+            settings: settings.try_into()?,
             memory_size,
             cycles,
             balance,
             freezing_threshold,
-        }
+        })
     }
 }
 
@@ -61,7 +66,7 @@ impl From<CanisterStatusResponse> for CanisterStatusResultV2 {
 /// })`
 #[derive(CandidType, Deserialize, Debug, Eq, PartialEq)]
 pub struct DefiniteCanisterSettingsArgs {
-    controller: Principal,  // TODO: Upstream uses PrincipalId, bit cdk-rs uses Principal.  TODO: Confirm that these are identical on the wire.
+    controller: Principal, // TODO: Upstream uses PrincipalId, bit cdk-rs uses Principal.  TODO: Confirm that these are identical on the wire.
     controllers: Vec<Principal>,
     compute_allocation: candid::Nat,
     memory_allocation: candid::Nat,
@@ -70,7 +75,7 @@ pub struct DefiniteCanisterSettingsArgs {
 
 impl TryFrom<DefiniteCanisterSettings> for DefiniteCanisterSettingsArgs {
     type Error = &'static str;
-    fn from(value: DefiniteCanisterSettings) -> Result<Self, Self::Error> {
+    fn try_from(value: DefiniteCanisterSettings) -> Result<Self, Self::Error> {
         let DefiniteCanisterSettings {
             controllers,
             compute_allocation,
@@ -78,7 +83,10 @@ impl TryFrom<DefiniteCanisterSettings> for DefiniteCanisterSettingsArgs {
             freezing_threshold,
         } = value;
         Ok(Self {
-            controller: controllers.get(0).ok_or("This canister has not even one controller")?.clone(),
+            controller: controllers
+                .get(0)
+                .ok_or("This canister has not even one controller")?
+                .clone(),
             controllers,
             compute_allocation,
             memory_allocation,
@@ -89,12 +97,17 @@ impl TryFrom<DefiniteCanisterSettings> for DefiniteCanisterSettingsArgs {
 
 pub async fn get_canister_status_v2() -> CanisterStatusResultV2 {
     let canister_id = ic_cdk::api::id(); // Own canister ID.
-    ic_cdk::api::management_canister::main::canister_status(CanisterIdRecord{canister_id}).await
-    .map(|(canister_status_response,)|CanisterStatusResultV2::from(canister_status_response))
-    .unwrap_or_else(|err| {
-        panic!(
-            "Couldn't get canister_status of {}. Err: {:#?}",
-            canister_id, err
-        )
-    })
+    ic_cdk::api::management_canister::main::canister_status(CanisterIdRecord { canister_id })
+        .await
+        .map_err(|err| format!("Failed to get status: {:#?}", err))
+        .and_then(|(canister_status_response,)| {
+            CanisterStatusResultV2::try_from(canister_status_response)
+                .map_err(|str| format!("CanisterStatusResultV2::try_from failed: {}", str))
+        })
+        .unwrap_or_else(|err| {
+            panic!(
+                "Couldn't get canister_status of {}. Err: {}",
+                canister_id, err
+            )
+        })
 }
