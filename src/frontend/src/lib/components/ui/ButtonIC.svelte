eca8f1bb244e1a196edb2e2bc3b9b0229da4ddaa
David Dal Busco <david.dalbusco@outlook.com>
2023-09-14 14:35:55 +0200

    style(frontend): button spacing

--- src/frontend/src/lib/components/ui/ButtonIC.svelte
+++ src/frontend/src/lib/components/ui/ButtonIC.svelte
@@ -2,7 +2,11 @@
 	import IconIcLogo from '$lib/components/icons/IconIcLogo.svelte';
 </script>
 
-<button on:click class="primary labelled">
+<button
+	on:click
+	class="primary labelled"
+	style="padding: var(--padding) var(--padding-4x) var(--padding) var(--padding-2x)"
+>
 	<IconIcLogo />
 	<span>
 		<span><slot name="action" /></span>
