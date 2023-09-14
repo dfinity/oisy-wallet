59ae2de0d6169473818d51b7467547076491be18
David Dal Busco <david.dalbusco@outlook.com>
2023-09-14 17:34:33 +0200

    feat(frontend): remove aidrop banner

--- src/frontend/src/lib/components/layout/HeaderSignIn.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import AirDrop from '$lib/components/core/AirDrop.svelte';
-</script>
-
-<header class="flex justify-center sm:pb-4 md:px-2 relative z-1">
-	<AirDrop />
-</header>
--- src/frontend/src/lib/components/pages/SignIn.svelte
+++ src/frontend/src/lib/components/pages/SignIn.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-	import HeaderSignIn from '$lib/components/layout/HeaderSignIn.svelte';
 	import SignInSection from '$lib/components/core/SignInSection.svelte';
 	import SignInBackground from '$lib/components/core/SignInBackground.svelte';
 	import Footer from '$lib/components/layout/Footer.svelte';
 </script>
-
-<HeaderSignIn />
 
 <SignInSection />
 
