92faef1334f1787ddd1be85c46af6c1ce57a82e1
David Dal Busco <david.dalbusco@outlook.com>
2023-09-15 12:43:27 +0200

    feat(frontend): redo balance and token symbol in hero pane of transactions (#178)
    * feat(frontend): display balance and symbol in transactions route (redo)
    
    * feat: property instead of route in component
    
    * feat: ease transition

---
+++ src/frontend/src/lib/hero/Address.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { addressStore } from '$lib/stores/address.store';
+	import { shortenWithMiddleEllipsis } from '$lib/utils/format.utils';
+	import { nonNullish } from '@dfinity/utils';
+	import Copy from '$lib/components/ui/Copy.svelte';
+</script>
+
+<div class="text-off-white mt-3 flex gap-1 opacity-50" style="min-height: 30px">
+	{#if nonNullish($addressStore)}
+		<output class="break-words">{shortenWithMiddleEllipsis($addressStore ?? '')}</output><Copy
+			value={$addressStore ?? ''}
+		/>
+	{/if}
+</div>
