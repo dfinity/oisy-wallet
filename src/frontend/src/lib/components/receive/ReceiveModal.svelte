290e3096009ca152c5104b1a71a264cbd2fd9841
David Dal Busco <david.dalbusco@outlook.com>
2023-09-15 08:54:54 +0200

    feat(frontend): receive modal review

--- src/frontend/src/lib/components/receive/ReceiveModal.svelte
+++ src/frontend/src/lib/components/receive/ReceiveModal.svelte
@@ -5,14 +5,17 @@
 	import ReceiveQRCode from '$lib/components/receive/ReceiveQRCode.svelte';
 	import { Modal } from '@dfinity/gix-components';
 	import Copy from '$lib/components/ui/Copy.svelte';
+	import { shortenWithMiddleEllipsis } from '$lib/utils/format.utils';
 </script>
 
 <Modal visible={$modalReceive} on:nnsClose={modalStore.close}>
 	<svelte:fragment slot="title">Receive ETH</svelte:fragment>
 
-	<p class="font-bold">Wallet address</p>
-	<p class="flex gap-1 mb-2 font-normal sm:items-center">
-		<output class="break-words">{$addressStore ?? ''}</output><Copy value={$addressStore ?? ''} />
+	<p class="font-bold text-center">Wallet address:</p>
+	<p class="flex gap-1 mb-2 font-normal sm:items-center justify-center">
+		<output class="break-words">{shortenWithMiddleEllipsis($addressStore ?? '')}</output><Copy
+			value={$addressStore ?? ''}
+		/>
 	</p>
 
 	<ReceiveQRCode />
--- src/frontend/src/lib/components/ui/Copy.svelte
+++ src/frontend/src/lib/components/ui/Copy.svelte
@@ -17,7 +17,7 @@
 <button
 	on:click|preventDefault|stopPropagation={copyToClipboard}
 	aria-label={`Copy: ${value}`}
-	class="pb-1"
+	class="py-1"
 	style="height: var(--padding-4x); width: var(--padding-4x); min-width: var(--padding-4x);"
 >
 	<IconCopy />
