ab6663fdc912417f7bf381d0ae811a59c9f57711
David Dal Busco <david.dalbusco@outlook.com>
2023-09-15 07:21:35 +0200

    fix(frontend): approve property for component walletconnect review (#167)

--- src/frontend/src/lib/components/wallet-connect/WalletConnectActions.svelte
+++ src/frontend/src/lib/components/wallet-connect/WalletConnectActions.svelte
@@ -1,18 +1,16 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
 	import { isBusy } from '$lib/derived/busy.derived';
-	import type { Web3WalletTypes } from '@walletconnect/web3wallet';
-	import { acceptedContext } from '$lib/utils/wallet-connect.utils';
 
 	const dispatch = createEventDispatcher();
 
-	export let proposal: Web3WalletTypes.SessionProposal | undefined | null;
+	export let approve = true;
 </script>
 
 <div class="flex justify-end gap-1 mt-4">
 	<button class="primary" on:click={() => dispatch('icReject')} disabled={$isBusy}>Reject</button>
 
-	{#if acceptedContext(proposal?.verifyContext)}
+	{#if approve}
 		<button class="primary" on:click={() => dispatch('icApprove')} disabled={$isBusy}>
 			Approve
 		</button>
