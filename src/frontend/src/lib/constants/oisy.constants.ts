5ba104b5bddb7fda03b7f8df7083fa4873122a5f
David Dal Busco <david.dalbusco@outlook.com>
2023-09-15 16:59:38 +0200

    chore: no dev

---
+++ src/frontend/src/lib/constants/oisy.constants.ts
@@ -0,0 +1,4 @@
+export const OISY_NAME = import.meta.env.VITE_OISY_NAME;
+export const OISY_DESCRIPTION = import.meta.env.VITE_OISY_DESCRIPTION;
+export const OISY_URL = import.meta.env.VITE_OISY_URL;
+export const OISY_ICON = import.meta.env.VITE_OISY_ICON;
