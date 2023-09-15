507a9e7908259dfc89f82a897353815e6acebdce
David Dal Busco <david.dalbusco@outlook.com>
2023-09-15 17:40:57 +0200

    build(frontend): parse manifest and sitemap with proper env (#188)
    * build(frontend): update sitemap url
    
    * build: update manifest

--- scripts/build.metadata.mjs
+++ scripts/build.metadata.mjs
@@ -2,6 +2,7 @@
 
 import { config } from 'dotenv';
 import { readFileSync, writeFileSync } from 'node:fs';
+import { join } from 'node:path';
 import { findHtmlFiles } from './build.utils.mjs';
 
 const ENV =
@@ -13,13 +14,13 @@ const ENV =
 
 config({ path: `.env.${ENV}` });
 
+const replaceEnv = ({ html, pattern, value }) => {
+	const regex = new RegExp(pattern, 'g');
+	return html.replace(regex, value);
+};
+
 const buildMetadata = (htmlFile) => {
 	let indexHtml = readFileSync(htmlFile, 'utf-8');
-
-	const replaceEnv = ({ html, pattern, value }) => {
-		const regex = new RegExp(pattern, 'g');
-		return html.replace(regex, value);
-	};
 
 	const METADATA_KEYS = [
 		'VITE_OISY_NAME',
@@ -43,5 +44,20 @@ const buildMetadata = (htmlFile) => {
 	writeFileSync(htmlFile, indexHtml);
 };
 
+const buildUrl = (filePath) => {
+	let content = readFileSync(filePath, 'utf-8');
+
+	content = replaceEnv({
+		html: content,
+		pattern: `https:\/\/oisy\.com`,
+		value: process.env.VITE_OISY_URL
+	});
+
+	writeFileSync(filePath, content);
+};
+
 const htmlFiles = findHtmlFiles();
 htmlFiles.forEach((htmlFile) => buildMetadata(htmlFile));
+
+buildUrl(join(process.cwd(), 'build', 'sitemap.xml'));
+buildUrl(join(process.cwd(), 'build', 'manifest.webmanifest'));
