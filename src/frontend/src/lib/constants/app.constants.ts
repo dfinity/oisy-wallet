ec168fa67347e10d7fabd4d0a0267e4395d6364a
David Dal Busco <david.dalbusco@outlook.com>
2023-09-18 08:22:29 +0200

    build(frontend): staging and prod environment (#205)

--- src/frontend/src/lib/constants/app.constants.ts
+++ src/frontend/src/lib/constants/app.constants.ts
@@ -1,5 +1,9 @@
 export const APP_VERSION = VITE_APP_VERSION;
 
+export const MODE = VITE_DFX_NETWORK;
+export const STAGING = MODE === 'staging';
+export const PROD = MODE === 'ic';
+
 export const localIdentityCanisterId: string | null | undefined = import.meta.env
 	.VITE_INTERNET_IDENTITY_CANISTER_ID as string | null | undefined;
 
