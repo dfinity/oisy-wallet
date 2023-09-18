f9ceea01070befc6bf0cc8fda048c53edfa2301c
David Dal Busco <david.dalbusco@outlook.com>
2023-09-18 15:12:04 +0200

    chore: format

--- src/frontend/src/lib/services/erc20.services.ts
+++ src/frontend/src/lib/services/erc20.services.ts
@@ -4,7 +4,6 @@ import usdc from '$lib/assets/usdc.svg';
 import usdt from '$lib/assets/usdt.svg';
 import { ERC20_CONTRACTS_ADDRESSES } from '$lib/constants/erc20.constants';
 import { metadata } from '$lib/providers/etherscan-erc20.providers';
-import { balancesStore } from '$lib/stores/balances.store';
 import { erc20TokensStore } from '$lib/stores/erc20.store';
 import { toastsError } from '$lib/stores/toasts.store';
 
