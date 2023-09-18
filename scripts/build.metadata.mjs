f344dc6402a1c626773e50bc920ba16d25899dd1
David Dal Busco <david.dalbusco@outlook.com>
2023-09-18 11:51:46 +0200

    feat(frontend): update metadata description and allow SEO for production (#210)

--- scripts/build.metadata.mjs
+++ scripts/build.metadata.mjs
@@ -19,11 +19,12 @@ const replaceEnv = ({ html, pattern, value }) => {
 	return html.replace(regex, value);
 };
 
-const buildMetadata = (htmlFile) => {
-	let indexHtml = readFileSync(htmlFile, 'utf-8');
+const parseMetadata = (targetFile) => {
+	let content = readFileSync(targetFile, 'utf-8');
 
 	const METADATA_KEYS = [
 		'VITE_OISY_NAME',
+		'VITE_OISY_ONELINER',
 		'VITE_OISY_DESCRIPTION',
 		'VITE_OISY_URL',
 		'VITE_OISY_ICON'
@@ -31,20 +32,20 @@ const buildMetadata = (htmlFile) => {
 
 	METADATA_KEYS.forEach(
 		(key) =>
-			(indexHtml = replaceEnv({ html: indexHtml, pattern: `{{${key}}}`, value: process.env[key] }))
+			(content = replaceEnv({ html: content, pattern: `{{${key}}}`, value: process.env[key] }))
 	);
 
 	// Special use case. We need to build the dapp with a real URL within app.html other build fails.
-	indexHtml = replaceEnv({
-		html: indexHtml,
+	content = replaceEnv({
+		html: content,
 		pattern: `https:\/\/oisy\.com`,
 		value: process.env.VITE_OISY_URL
 	});
 
-	writeFileSync(htmlFile, indexHtml);
+	writeFileSync(targetFile, content);
 };
 
-const buildUrl = (filePath) => {
+const parseUrl = (filePath) => {
 	let content = readFileSync(filePath, 'utf-8');
 
 	content = replaceEnv({
@@ -56,8 +57,37 @@ const buildUrl = (filePath) => {
 	writeFileSync(filePath, content);
 };
 
-const htmlFiles = findHtmlFiles();
-htmlFiles.forEach((htmlFile) => buildMetadata(htmlFile));
+const updateRobotsTxt = () => {
+	if (ENV !== 'production') {
+		return;
+	}
 
-buildUrl(join(process.cwd(), 'build', 'sitemap.xml'));
-buildUrl(join(process.cwd(), 'build', 'manifest.webmanifest'));
+	const content = `User-agent: *
+Allow: /
+Sitemap: ${process.env.VITE_OISY_URL}/sitemap.xml
+Host: ${process.env.VITE_OISY_URL}`;
+
+	writeFileSync(join(process.cwd(), 'build', 'robots.txt'), content);
+};
+
+const removeMetaRobots = (targetFile) => {
+	if (ENV !== 'production') {
+		return;
+	}
+
+	let content = readFileSync(targetFile, 'utf-8');
+
+	const update = content.replace(/<meta\s+name="robots"\s+content="noindex"\s*\/>/, '');
+
+	writeFileSync(targetFile, update);
+};
+
+const htmlFiles = findHtmlFiles();
+htmlFiles.forEach((htmlFile) => parseMetadata(htmlFile));
+
+parseUrl(join(process.cwd(), 'build', 'sitemap.xml'));
+parseMetadata(join(process.cwd(), 'build', 'manifest.webmanifest'));
+
+// SEO
+htmlFiles.forEach((htmlFile) => removeMetaRobots(htmlFile));
+updateRobotsTxt();
--- src/frontend/src/lib/constants/oisy.constants.ts
+++ src/frontend/src/lib/constants/oisy.constants.ts
@@ -1,4 +1,5 @@
 export const OISY_NAME = import.meta.env.VITE_OISY_NAME;
+export const OISY_ONELINER = import.meta.env.VITE_OISY_ONELINER;
 export const OISY_DESCRIPTION = import.meta.env.VITE_OISY_DESCRIPTION;
 export const OISY_URL = import.meta.env.VITE_OISY_URL;
 export const OISY_ICON = import.meta.env.VITE_OISY_ICON;
