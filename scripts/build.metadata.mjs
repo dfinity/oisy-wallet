4ebe51acfaed6821c6cc8abc25e8f1bc25062e66
David Dal Busco <david.dalbusco@outlook.com>
2023-09-18 11:49:47 +0200

    build: update robots on build

--- scripts/build.metadata.mjs
+++ scripts/build.metadata.mjs
@@ -57,8 +57,37 @@ const parseUrl = (filePath) => {
 	writeFileSync(filePath, content);
 };
 
+const updateRobotsTxt = () => {
+	if (ENV !== 'production') {
+		return;
+	}
+
+	const content = `User-agent: *
+Allow: /
+Sitemap: ${process.env.VITE_OISY_URL}/sitemap.xml
+Host: ${process.env.VITE_OISY_URL}`;
+
+	writeFileSync(join(process.cwd(), 'build', 'robots.txt'), content);
+};
+
+const removeMetaRobots = (targetFile) => {
+	if (ENV !== 'production') {
+		return;
+	}
+
+	let content = readFileSync(targetFile, 'utf-8');
+
+	const update = content.replace(/<meta\s+name="robots"\s+content="noindex"\s*\/>/, '');
+
+	writeFileSync(targetFile, update);
+};
+
 const htmlFiles = findHtmlFiles();
 htmlFiles.forEach((htmlFile) => parseMetadata(htmlFile));
 
 parseUrl(join(process.cwd(), 'build', 'sitemap.xml'));
 parseMetadata(join(process.cwd(), 'build', 'manifest.webmanifest'));
+
+// SEO
+htmlFiles.forEach((htmlFile) => removeMetaRobots(htmlFile));
+updateRobotsTxt();
