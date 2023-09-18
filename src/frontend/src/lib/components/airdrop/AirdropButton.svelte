9c6bfe70667b2a4faf815884971082d7787c4230
David Dal Busco <david.dalbusco@outlook.com>
2023-09-18 16:59:14 +0200

    feat(frontend): init airdrop modal

--- src/frontend/src/lib/components/airdrop/AirdropButton.svelte
+++ src/frontend/src/lib/components/airdrop/AirdropButton.svelte
@@ -3,10 +3,14 @@
 	import IconCelebrate from '$lib/components/icons/IconCelebrate.svelte';
 	import { airdropAvailable } from '$lib/derived/airdrop.derived';
 	import { fade } from 'svelte/transition';
+	import {modalStore} from "$lib/stores/modal.store";
+	import AirdropModal from "$lib/components/airdrop/AirdropModal.svelte";
 </script>
 
 {#if AIRDROP && $airdropAvailable}
-	<button class="airdrop full center mt-3" in:fade
+	<button class="airdrop full center mt-3" in:fade on:click={modalStore.openAirdrop}
 		><IconCelebrate /> Airdrop status <span class="text-blue">Check</span></button
 	>
+
+	<AirdropModal />
 {/if}
---
+++ src/frontend/src/lib/components/airdrop/AirdropModal.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { modalAirdrop } from '$lib/derived/modal.derived';
+	import { modalStore } from '$lib/stores/modal.store';
+	import { Modal } from '@dfinity/gix-components';
+</script>
+
+<Modal visible={$modalAirdrop} on:nnsClose={modalStore.close}>
+	<svelte:fragment slot="title">Airdrop status</svelte:fragment>
+</Modal>
