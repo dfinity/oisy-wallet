2e348b534c4af210b29bc719d405a3cacc0f4c61
David Dal Busco <david.dalbusco@outlook.com>
2023-09-19 13:16:55 +0200

    feat(frontend): review text (#227)

--- src/frontend/src/lib/components/core/Alpha.svelte
+++ src/frontend/src/lib/components/core/Alpha.svelte
@@ -6,5 +6,5 @@
 	class="flex gap-1 items-center justify-center text-center px-3 sm:px-8 py-1 font-bold text-xxs sm:text-base"
 	style="color: var(--alpha-color, var(--color-misty-rose))"
 >
-	<IconWarning /> This is Alpha version. Use at your own risk!
+	<IconWarning /> This is alpha version. Use at your own risk!
 </p>
