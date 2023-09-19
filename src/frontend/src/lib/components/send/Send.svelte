504980a2aac3989cd954ff6ab8a355a3abc82156
David Dal Busco <david.dalbusco@outlook.com>
2023-09-19 14:57:20 +0200

    feat(frontend): send enabled even if balance is zero (#232)

--- src/frontend/src/lib/components/send/Send.svelte
+++ src/frontend/src/lib/components/send/Send.svelte
@@ -2,20 +2,19 @@
 	import { modalStore } from '$lib/stores/modal.store';
 	import IconSend from '$lib/components/icons/IconSend.svelte';
 	import { addressNotLoaded } from '$lib/derived/address.derived';
-	import { balanceEmpty } from '$lib/derived/balances.derived';
 	import { isBusy } from '$lib/derived/busy.derived';
 	import { modalSend } from '$lib/derived/modal.derived';
 	import SendModal from '$lib/components/send/SendModal.svelte';
 
 	let disabled: boolean;
-	$: disabled = $addressNotLoaded || $balanceEmpty || $isBusy;
+	$: disabled = $addressNotLoaded || $isBusy;
 </script>
 
 <button
 	class="flex-1 hero"
 	on:click={modalStore.openSend}
 	{disabled}
-	class:opacity-0={disabled}
+	class:opacity-50={disabled}
 >
 	<IconSend size="28" />
 	<span>Send</span></button
