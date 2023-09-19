e2fe5f9e7a2f95e9aaa1e68828d5e88a65d1e3a4
David Dal Busco <david.dalbusco@outlook.com>
2023-09-19 14:37:14 +0200

    feat(frontend): settings page (#229)
    * feat(frontend): settings page
    
    * feat: your principal

---
+++ src/frontend/src/lib/components/pages/Settings.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { shortenWithMiddleEllipsis } from '$lib/utils/format.utils';
+	import { KeyValuePairInfo } from '@dfinity/gix-components';
+	import { addressStore } from '$lib/stores/address.store';
+	import Copy from '$lib/components/ui/Copy.svelte';
+</script>
+
+<KeyValuePairInfo>
+	<svelte:fragment slot="key"><span class="font-bold">Your Principal:</span></svelte:fragment>
+	<svelte:fragment slot="value"
+		><output class="break-words">{shortenWithMiddleEllipsis($addressStore ?? '')}</output><Copy
+			value={$addressStore ?? ''}
+			text="Address copied to clipboard."
+		/></svelte:fragment
+	>
+	<svelte:fragment slot="info">
+		Your ID for the Oisy Wallet. Created by your Internet Identity.
+	</svelte:fragment>
+</KeyValuePairInfo>
