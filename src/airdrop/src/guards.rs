bd6f685ced1fd7e06a60292a49e307d477ff240d
David Dal Busco <david.dalbusco@outlook.com>
2023-09-20 17:42:32 +0200

    feat(airdrop): manager and admin guards (#258)
    * feat(airdrop): manager and admin guards
    
    * build: did files

---
+++ src/airdrop/src/guards.rs
@@ -0,0 +1,28 @@
+use crate::{PRINCIPALS_ADMIN, PRINCIPALS_MANAGERS};
+use ic_cdk::caller;
+
+pub fn caller_is_admin() -> Result<(), String> {
+    let caller = caller();
+
+    PRINCIPALS_ADMIN.with(|state| {
+        let admins = state.borrow();
+        if admins.contains(&caller) {
+            Ok(())
+        } else {
+            Err("Caller is not an authorized admin.".to_string())
+        }
+    })
+}
+
+pub fn caller_is_manager() -> Result<(), String> {
+    let caller = caller();
+
+    PRINCIPALS_MANAGERS.with(|state| {
+        let managers = state.borrow();
+        if managers.contains_key(&caller) {
+            Ok(())
+        } else {
+            Err("Caller is not an authorized manager.".to_string())
+        }
+    })
+}
