78e4590b529e2bcb30fc26c2bbcc8dd1e6d37277
David Dal Busco <david.dalbusco@outlook.com>
2023-09-20 19:50:07 +0200

    chore: format

--- src/frontend/src/lib/utils/airdrop.utils.ts
+++ src/frontend/src/lib/utils/airdrop.utils.ts
@@ -1,3 +1,4 @@
 import { OISY_URL } from '$lib/constants/oisy.constants';
 
-export const airdropCodeUrl = (code: string | undefined): string => `${OISY_URL}/?code=${code ?? ''}`;
+export const airdropCodeUrl = (code: string | undefined): string =>
+	`${OISY_URL}/?code=${code ?? ''}`;
