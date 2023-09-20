2fc6e4e598cedec749a11c40f27700f638d5db9f
David Dal Busco <david.dalbusco@outlook.com>
2023-09-20 10:28:31 +0200

    build(frontend): move Etherscan calls to Infura (except fetching transactions history) (#249)

---
+++ src/frontend/src/lib/providers/infura.providers.ts
@@ -0,0 +1,19 @@
+import type { ECDSA_PUBLIC_KEY } from '$lib/types/address';
+import type { BigNumber } from '@ethersproject/bignumber';
+import { InfuraProvider, type FeeData, type TransactionResponse } from '@ethersproject/providers';
+
+const API_KEY = import.meta.env.VITE_INFURA_API_KEY;
+const NETWORK = import.meta.env.VITE_INFURA_NETWORK;
+
+const provider = new InfuraProvider(NETWORK, API_KEY);
+
+export const balance = (address: ECDSA_PUBLIC_KEY): Promise<BigNumber> =>
+	provider.getBalance(address);
+
+export const getFeeData = (): Promise<FeeData> => provider.getFeeData();
+
+export const sendTransaction = (signedTransaction: string): Promise<TransactionResponse> =>
+	provider.sendTransaction(signedTransaction);
+
+export const getTransactionCount = (address: ECDSA_PUBLIC_KEY): Promise<number> =>
+	provider.getTransactionCount(address, 'pending');
