8f2cbd3f77aee7606a38bb55923e7c005c4bc2b5
David Dal Busco <david.dalbusco@outlook.com>
2023-09-20 18:47:47 +0200

    feat(airdrop): generate code (#263)

--- src/frontend/src/lib/api/airdrop.api.ts
+++ src/frontend/src/lib/api/airdrop.api.ts
@@ -1,4 +1,4 @@
-import type { Result_2 } from '$declarations/airdrop/airdrop.did';
+import type { Result_1, Result_2 } from '$declarations/airdrop/airdrop.did';
 import type { ECDSA_PUBLIC_KEY } from '$lib/types/address';
 import { getAirdropActor } from '$lib/utils/actor.utils';
 
@@ -10,6 +10,11 @@ export const getAirdropCode = async (): Promise<Result_2> => {
 export const isAirdropManager = async (): Promise<boolean> => {
 	const actor = await getAirdropActor();
 	return actor.is_manager();
+};
+
+export const generateAirdropCode = async (): Promise<Result_1> => {
+	const actor = await getAirdropActor();
+	return actor.generate_code();
 };
 
 export const redeemAirdropCode = async ({
