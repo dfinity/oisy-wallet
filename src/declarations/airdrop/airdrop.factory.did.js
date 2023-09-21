e0bbe8fd5004ae28b3c436a834b5c48d87a4f4a8
EnzoPlayer0ne <15707207+EnzoPlayer0ne@users.noreply.github.com>
2023-09-21 13:24:54 +0200

    chore(airdrop): Refactor to add a global state (#279)
    * save
    
    * refactor global state in lib.rs
    
    * refactor utils.rs to use STATE
    
    * finish refacting

--- src/declarations/airdrop/airdrop.factory.did.js
+++ src/declarations/airdrop/airdrop.factory.did.js
@@ -28,7 +28,6 @@ export const idlFactory = ({ IDL }) => {
 		children: IDL.Opt(IDL.Vec(IDL.Tuple(IDL.Text, IDL.Bool)))
 	});
 	const Result_2 = IDL.Variant({ Ok: Info, Err: CanisterError });
-	const Result_3 = IDL.Variant({ Ok: IDL.Nat64, Err: CanisterError });
 	return IDL.Service({
 		add_admin: IDL.Func([IDL.Principal], [Result], []),
 		add_codes: IDL.Func([IDL.Vec(IDL.Text)], [Result], []),
@@ -36,8 +35,6 @@ export const idlFactory = ({ IDL }) => {
 		bring_caninster_back_to_life: IDL.Func([], [Result], []),
 		generate_code: IDL.Func([], [Result_1], []),
 		get_code: IDL.Func([], [Result_2], ['query']),
-		get_total_code_issued: IDL.Func([], [Result_3], ['query']),
-		get_total_code_redeemed: IDL.Func([], [Result_3], ['query']),
 		is_manager: IDL.Func([], [IDL.Bool], ['query']),
 		kill_canister: IDL.Func([], [Result], []),
 		redeem_code: IDL.Func([IDL.Text], [Result_2], [])
