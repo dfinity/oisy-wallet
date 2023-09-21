e0a71e5b13fa7b1d8bbc306789c4ee3107f1032e
EnzoPlayer0ne <15707207+EnzoPlayer0ne@users.noreply.github.com>
2023-09-21 10:38:17 +0200

    chore(airdrop): Add `Arg` to `init` (#275)
    * add init args
    
    * remove not needed derive
    
    * fix: init annotation
    
    * add Init variant
    
    ---------
    
    Co-authored-by: David Dal Busco <david.dalbusco@outlook.com>

--- scripts/deploy.airdrop.sh
+++ scripts/deploy.airdrop.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
-DFX_PRINCIPAL=$(dfx identity get-principal)
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <backend_canister_id>"
+    exit 1
+fi
+
+BACKEND_CANISTER_ID=$1
 
 if [ -n "${ENV+1}" ]; then
   echo "TODO: to be implemented"
 else
-    dfx deploy airdrop --argument '(vec {principal"'${DFX_PRINCIPAL}'"})' --mode reinstall
+    dfx deploy airdrop --argument "(variant {
+      Init = record {
+        backend_canister_id = principal \"$BACKEND_CANISTER_ID\"
+      }
+    })" --mode reinstall
 
     "$(git rev-parse --show-toplevel)/scripts/generate-codes.sh" 20 1000
 fi
