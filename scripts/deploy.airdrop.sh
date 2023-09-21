fe9e857a44d36925a9f172f69cd65ad46218db26
David Dal Busco <david.dalbusco@outlook.com>
2023-09-21 17:33:01 +0200

    build: deploy airdrop locally

--- scripts/deploy.airdrop.sh
+++ scripts/deploy.airdrop.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <backend_canister_id>"
-    exit 1
-fi
-
-BACKEND_CANISTER_ID=$1
+BACKEND_ID=$(dfx canister id backend)
 
 if [ -n "${ENV+1}" ]; then
   echo "TODO: to be implemented"
 else
     dfx deploy airdrop --argument "(variant {
       Init = record {
-        backend_canister_id = principal \"$BACKEND_CANISTER_ID\"
+        backend_canister_id = principal \"$BACKEND_ID\"
       }
-    })" --mode reinstall
+    })"
 
     "$(git rev-parse --show-toplevel)/scripts/generate-codes.sh" 20 1000
 fi
