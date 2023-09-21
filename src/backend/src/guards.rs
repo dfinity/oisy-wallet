dd65a8005c9f7cbf654386362ca022a7bda54b83
David Dal Busco <david.dalbusco@outlook.com>
2023-09-21 12:57:07 +0200

    feat(backend): allowed callers for eth_address_of

--- src/backend/src/guards.rs
+++ src/backend/src/guards.rs
@@ -1,3 +1,4 @@
+use crate::read_state;
 use candid::Principal;
 use ic_cdk::caller;
 
@@ -6,5 +7,22 @@ pub fn caller_is_not_anonymous() -> Result<(), String> {
         Err("Anonymous caller not authorized.".to_string())
     } else {
         Ok(())
+    }
+}
+
+pub fn caller_is_allowed() -> Result<(), String> {
+    let caller = caller();
+    let allowed_callers = read_state(|s| s.allowed_callers.clone());
+
+    fn is_allowed_callers(caller: Principal, allowed_callers: &Vec<Principal>) -> bool {
+        allowed_callers
+            .iter()
+            .any(|allowed_caller| *allowed_caller == caller)
+    }
+
+    if is_allowed_callers(caller, &allowed_callers) {
+        Ok(())
+    } else {
+        Err("Caller is not allowed.".to_string())
     }
 }
