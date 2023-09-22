c8574858964ea8e386b171f70fa7300200c84ef2
EnzoPlayer0ne <15707207+EnzoPlayer0ne@users.noreply.github.com>
2023-09-22 19:13:17 +0200

    chore(airdrop): satisfy `clippy` (#314)
    * save
    
    * save
    
    * save

--- src/airdrop/src/guards.rs
+++ src/airdrop/src/guards.rs
@@ -1,5 +1,6 @@
-use crate::read_state;
 use ic_cdk::caller;
+
+use crate::read_state;
 
 pub fn caller_is_admin() -> Result<(), String> {
     let caller = caller();
@@ -8,7 +9,7 @@ pub fn caller_is_admin() -> Result<(), String> {
         if state.principals_admins.contains(&caller) {
             Ok(())
         } else {
-            Err("Caller is not an authorized admin".to_string())
+            Err("Caller is not an authorized admin".to_owned())
         }
     })
 }
@@ -20,7 +21,7 @@ pub fn caller_is_manager() -> Result<(), String> {
         if state.principals_managers.contains_key(&caller) {
             Ok(())
         } else {
-            Err("Caller is not an authorized manager".to_string())
+            Err("Caller is not an authorized manager".to_owned())
         }
     })
 }
