2770c0bb4dfd96460fc07432810c867ccee90a12
David Dal Busco <david.dalbusco@outlook.com>
2023-09-24 08:09:01 +0200

    chore: lint

--- src/frontend/src/lib/services/erc20.services.ts
+++ src/frontend/src/lib/services/erc20.services.ts
@@ -21,9 +21,8 @@ const mapErc20Icon = (symbol: string): string | undefined => {
 			return usdt;
 		case 'dai':
 			return dai;
-		// ICP in production
+		// ICP in production. ckICP was used on staging because the definitive name and symbol had not been decided.
 		case 'icp':
-		// ckICP was used on staging because the definitive name and symbol had not been decided
 		case 'ckicp':
 			return icp;
 		default:
