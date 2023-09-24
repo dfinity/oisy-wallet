f3754a388277f34fd1c09fbcc5f934c41e41ee2c
David Dal Busco <david.dalbusco@outlook.com>
2023-09-24 12:09:44 +0200

    refactor(frontend): move is icp account identifier (#340)

--- src/frontend/src/lib/utils/send.utils.ts
+++ src/frontend/src/lib/utils/send.utils.ts
@@ -5,3 +5,19 @@ export const invalidDestination = (destination: string): boolean =>
 
 export const invalidAmount = (amount: number | undefined): boolean =>
 	isNullish(amount) || amount < 0;
+
+export const isIcpAccountIdentifier = async (address: string | undefined): Promise<boolean> => {
+	if (isNullish(address)) {
+		return false;
+	}
+
+	try {
+		const { checkAccountId } = await import('@dfinity/nns');
+		checkAccountId(address);
+		return true;
+	} catch (_: unknown) {
+		// We do not parse the error
+	}
+
+	return false;
+};
