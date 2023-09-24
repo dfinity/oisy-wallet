b37ca785baaa255c363045ec4eae83a2bef8a135
David Dal Busco <david.dalbusco@outlook.com>
2023-09-24 12:53:22 +0200

    feat(frontend): map address with 0x (#347)

---
+++ src/frontend/src/lib/components/send/SendDestination.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { Input } from '@dfinity/gix-components';
+	import { mapAddressStartsWith0x } from '$lib/utils/send.utils';
+	import { isNullish } from '@dfinity/utils';
+
+	export let destination = '';
+
+	const onBlur = () => {
+		if (destination === '' || isNullish(destination)) {
+			return;
+		}
+
+		destination = mapAddressStartsWith0x(destination);
+	};
+</script>
+
+<label for="destination" class="font-bold px-1.25">Destination:</label>
+<Input
+	name="destination"
+	inputType="text"
+	required
+	bind:value={destination}
+	placeholder="Enter public address (0x)"
+	on:blur={onBlur}
+/>
