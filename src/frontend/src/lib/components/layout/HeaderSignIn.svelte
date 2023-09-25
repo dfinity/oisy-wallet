11373b5127805931efdbe2c7c17c3fc10180e0ef
David Dal Busco <david.dalbusco@outlook.com>
2023-09-25 09:18:57 +0200

    fix(frontend): not clickable alpha link on login screen (#370)

--- src/frontend/src/lib/components/layout/HeaderSignIn.svelte
+++ src/frontend/src/lib/components/layout/HeaderSignIn.svelte
@@ -2,6 +2,6 @@
 	import Alpha from '$lib/components/core/Alpha.svelte';
 </script>
 
-<header class="flex justify-center md:px-2 py-2 relative z-1 pointer-events-none">
+<header class="flex justify-center md:px-2 py-2 relative z-1">
 	<Alpha />
 </header>
