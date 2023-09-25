d638f11aa633b9a63738d4f6854c7db3a1d80526
David Dal Busco <david.dalbusco@outlook.com>
2023-09-25 17:30:43 +0200

    fix(frontend): agent-js incompatibility with ESM in web worker (#381)
    * feat(frontend): cjs for agent-js in web worker
    
    * chore: no dev
    
    * chore: no dev

--- src/frontend/src/lib/api/airdrop.api.ts
+++ src/frontend/src/lib/api/airdrop.api.ts
@@ -1,9 +1,8 @@
 import type { Result_1, Result_3 } from '$declarations/airdrop/airdrop.did';
 import { getAirdropActor } from '$lib/utils/actor.utils';
-import type { Identity } from '@dfinity/agent';
 
-export const getAirdropCode = async (identity?: Identity): Promise<Result_3> => {
-	const actor = await getAirdropActor(identity);
+export const getAirdropCode = async (): Promise<Result_3> => {
+	const actor = await getAirdropActor();
 	return actor.get_code();
 };
 
