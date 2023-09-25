eed4ddabb1c9b23e9d1ac384930f4a40d47d46a5
David Dal Busco <david.dalbusco@outlook.com>
2023-09-25 17:19:16 +0200

    feat(frontend): cjs for agent-js in web worker

---
+++ src/frontend/src/lib/worker-api/airdrop.worker-api.ts
@@ -0,0 +1,40 @@
+import type { _SERVICE as AirdropActor, Result_3 } from '$declarations/airdrop/airdrop.did';
+import { idlFactory as idlFactorAirdrop } from '$declarations/airdrop/airdrop.factory.did';
+import { getAgent } from '$lib/worker-utils/agent.worker-utils';
+import type { Identity } from '@dfinity/agent';
+import { Actor, type ActorMethod, type ActorSubclass } from '@dfinity/agent/lib/cjs/index';
+import type { IDL } from '@dfinity/candid';
+import type { Principal } from '@dfinity/principal';
+
+const getAirdropActor = async (identity: Identity): Promise<AirdropActor> => {
+	const canisterId = import.meta.env.VITE_AIRDROP_CANISTER_ID;
+
+	return createActor({
+		canisterId,
+		idlFactory: idlFactorAirdrop,
+		identity
+	});
+};
+
+const createActor = async <T = Record<string, ActorMethod>>({
+	canisterId,
+	idlFactory,
+	identity
+}: {
+	canisterId: string | Principal;
+	idlFactory: IDL.InterfaceFactory;
+	identity: Identity;
+}): Promise<ActorSubclass<T>> => {
+	const agent = await getAgent({ identity });
+
+	// Creates an actor with using the candid interface and the HttpAgent
+	return Actor.createActor(idlFactory, {
+		agent,
+		canisterId
+	});
+};
+
+export const getAirdropCode = async (identity: Identity): Promise<Result_3> => {
+	const actor = await getAirdropActor(identity);
+	return actor.get_code();
+};
