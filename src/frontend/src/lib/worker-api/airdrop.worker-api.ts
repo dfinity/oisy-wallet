be063bef9b76a5782a039466f85aa91b26a22dd3
David Dal Busco <david.dalbusco@outlook.com>
2023-09-25 17:22:27 +0200

    chore: no dev

--- src/frontend/src/lib/worker-api/airdrop.worker-api.ts
+++ src/frontend/src/lib/worker-api/airdrop.worker-api.ts
@@ -2,12 +2,14 @@ import type { _SERVICE as AirdropActor, Result_3 } from '$declarations/airdrop/a
 import { idlFactory as idlFactorAirdrop } from '$declarations/airdrop/airdrop.factory.did';
 import { getAgent } from '$lib/worker-utils/agent.worker-utils';
 import type { Identity } from '@dfinity/agent';
-import { Actor, type ActorMethod, type ActorSubclass } from '@dfinity/agent/lib/cjs/index';
+import { Actor, type ActorMethod, type ActorSubclass } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 import type { Principal } from '@dfinity/principal';
 
 const getAirdropActor = async (identity: Identity): Promise<AirdropActor> => {
 	const canisterId = import.meta.env.VITE_AIRDROP_CANISTER_ID;
+
+	console.log(canisterId)
 
 	return createActor({
 		canisterId,
