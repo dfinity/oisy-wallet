7978b8b91fd38c0ad94693b024790598a1f41a3b
David Dal Busco <david.dalbusco@outlook.com>
2023-09-25 17:30:43 +0200

    fix(frontend): agent-js incompatibility with ESM in web worker (#381)
    * feat(frontend): cjs for agent-js in web worker
    
    * chore: no dev
    
    * chore: no dev

--- src/frontend/src/lib/utils/actor.utils.ts
+++ src/frontend/src/lib/utils/actor.utils.ts
@@ -24,17 +24,17 @@ export const getBackendActor = async (): Promise<BackendActor> => {
 	});
 };
 
-export const getAirdropActor = async (identity?: Identity): Promise<AirdropActor> => {
-	const actorIdentity: Identity | undefined | null = identity ?? get(authStore).identity;
+export const getAirdropActor = async (): Promise<AirdropActor> => {
+	const identity: Identity | undefined | null = get(authStore).identity;
 
-	assertNonNullish(actorIdentity, 'No internet identity.');
+	assertNonNullish(identity, 'No internet identity.');
 
 	const canisterId = import.meta.env.VITE_AIRDROP_CANISTER_ID;
 
 	return createActor({
 		canisterId,
 		idlFactory: idlFactorAirdrop,
-		identity: actorIdentity
+		identity
 	});
 };
 
