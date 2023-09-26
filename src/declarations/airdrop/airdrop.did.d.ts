cc2a1adcb3a92ade78d51387943ab744d70ee7e5
David Dal Busco <david.dalbusco@outlook.com>
2023-09-26 18:13:40 +0200

    build(airdrop): did files with metrics

--- src/declarations/airdrop/airdrop.did.d.ts
+++ src/declarations/airdrop/airdrop.did.d.ts
@@ -18,16 +18,46 @@ export type CanisterError =
 	| { DuplicateKey: string }
 	| { NoCodeForII: null }
 	| { ManagersCannotParticipateInTheAirdrop: null };
+export interface CanisterStatusResultV2 {
+	controller: Principal;
+	status: CanisterStatusType;
+	freezing_threshold: bigint;
+	balance: Array<[Uint8Array | number[], bigint]>;
+	memory_size: bigint;
+	cycles: bigint;
+	settings: DefiniteCanisterSettingsArgs;
+	idle_cycles_burned_per_day: bigint;
+	module_hash: [] | [Uint8Array | number[]];
+}
+export type CanisterStatusType = { stopped: null } | { stopping: null } | { running: null };
 export interface CodeInfo {
 	codes_generated: bigint;
 	code: string;
 	codes_redeemed: bigint;
+}
+export interface DefiniteCanisterSettingsArgs {
+	controller: Principal;
+	freezing_threshold: bigint;
+	controllers: Array<Principal>;
+	memory_allocation: bigint;
+	compute_allocation: bigint;
 }
 export interface EthereumTransaction {
 	transferred: boolean;
 	reward_type: RewardType;
 	eth_address: string;
 	amount: bigint;
+}
+export interface HttpRequest {
+	url: string;
+	method: string;
+	body: Uint8Array | number[];
+	headers: Array<[string, string]>;
+}
+export interface HttpResponse {
+	body: Uint8Array | number[];
+	headers: Array<[string, string]>;
+	status_code: number;
 }
 export interface Info {
 	principal: Principal;
@@ -66,6 +96,7 @@ export interface _SERVICE {
 	clean_up: ActorMethod<[], Result>;
 	generate_code: ActorMethod<[], Result_1>;
 	get_airdrop: ActorMethod<[bigint], Result_2>;
+	get_canister_status: ActorMethod<[], CanisterStatusResultV2>;
 	get_code: ActorMethod<[], Result_3>;
 	get_logs: ActorMethod<[bigint], Result_4>;
 	get_state_admins: ActorMethod<[], Result_5>;
@@ -73,6 +104,7 @@ export interface _SERVICE {
 	get_state_parameters: ActorMethod<[], Result_7>;
 	get_state_rewards: ActorMethod<[], Result_8>;
 	get_stats: ActorMethod<[], Result_9>;
+	http_request: ActorMethod<[HttpRequest], HttpResponse>;
 	is_manager: ActorMethod<[], boolean>;
 	kill_canister: ActorMethod<[], Result>;
 	put_airdrop: ActorMethod<[BigUint64Array | bigint[]], Result>;
