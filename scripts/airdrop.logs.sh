baaebd9fc5f2422952b6c2e48a315e8880642755
EnzoPlayer0ne <15707207+EnzoPlayer0ne@users.noreply.github.com>
2023-09-26 03:41:48 +0200

    feat(airdrop): Update script to get logs #397

--- scripts/airdrop.logs.sh
+++ scripts/airdrop.logs.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Get the index as a parameter
+if [ -z "$1" ]; then
+  echo "Error: Missing index"
+  echo "Usage: $0 <index>"
+  exit 1
+fi
+
+INDEX=$1
+
 case $ENV in
   "staging")
     WALLET="cvthj-wyaaa-aaaad-aaaaq-cai"
@@ -12,7 +21,7 @@ case $ENV in
 esac
 
 if [ -n "${ENV+1}" ]; then
-    dfx canister call airdrop get_logs --network "$ENV" --wallet "$WALLET" | sed 's/"; "/"\n"/g' | sed 's/^.*vec { "//' | sed 's/";.*$//'
+    dfx canister call airdrop get_logs "($INDEX)" --network "$ENV" --wallet "$WALLET" |  sed 's/}; record/};\nrecord/g'
 else
-     dfx canister call airdrop get_logs | sed 's/"; "/"\n"/g' | sed 's/^.*vec { "//' | sed 's/";.*$//'
+     dfx canister call airdrop get_logs "($INDEX)" | sed 's/}; record/};\nrecord/g'
 fi
