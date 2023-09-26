daeee469fbd4a8fe3e05225a2e98d336d20d041b
David Dal Busco <david.dalbusco@outlook.com>
2023-09-26 08:47:58 +0200

    feat(frontend): es modules for workers (#405)

--- src/frontend/src/lib/api/backend.api.ts
+++ src/frontend/src/lib/api/backend.api.ts
@@ -1,23 +1,42 @@
 import type { SignRequest } from '$declarations/backend/backend.did';
 import type { ECDSA_PUBLIC_KEY } from '$lib/types/address';
+import type { OptionIdentity } from '$lib/types/identity';
 import { getBackendActor } from '$lib/utils/actor.utils';
 
-export const getEthAddress = async (): Promise<ECDSA_PUBLIC_KEY> => {
-	const actor = await getBackendActor();
+export const getEthAddress = async (identity: OptionIdentity): Promise<ECDSA_PUBLIC_KEY> => {
+	const actor = await getBackendActor(identity);
 	return actor.caller_eth_address();
 };
 
-export const signTransaction = async (transaction: SignRequest): Promise<string> => {
-	const actor = await getBackendActor();
+export const signTransaction = async ({
+	transaction,
+	identity
+}: {
+	transaction: SignRequest;
+	identity: OptionIdentity;
+}): Promise<string> => {
+	const actor = await getBackendActor(identity);
 	return actor.sign_transaction(transaction);
 };
 
-export const signMessage = async (message: string): Promise<string> => {
-	const actor = await getBackendActor();
+export const signMessage = async ({
+	message,
+	identity
+}: {
+	message: string;
+	identity: OptionIdentity;
+}): Promise<string> => {
+	const actor = await getBackendActor(identity);
 	return actor.personal_sign(message);
 };
 
-export const signPrehash = async (hash: string): Promise<string> => {
-	const actor = await getBackendActor();
+export const signPrehash = async ({
+	hash,
+	identity
+}: {
+	hash: string;
+	identity: OptionIdentity;
+}): Promise<string> => {
+	const actor = await getBackendActor(identity);
 	return actor.sign_prehash(hash);
 };
