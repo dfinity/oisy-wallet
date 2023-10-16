825f09152e2544b3128466e290726f27f7623a7d
David Dal Busco <david.dalbusco@outlook.com>
2023-10-16 20:28:03 +0200

    feat(frontend): tweak 1inch logo (#435)

--- src/frontend/src/lib/components/ui/Logo.svelte
+++ src/frontend/src/lib/components/ui/Logo.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import Img from '$lib/components/ui/Img.svelte';
-	import {isNullish, nonNullish} from '@dfinity/utils';
+	import { isNullish, nonNullish } from '@dfinity/utils';
 	import IconRandom from '$lib/components/icons/IconRandom.svelte';
 
 	export let src: string | undefined;
 	export let alt = '';
 	export let size: string;
-	export let color: 'dust' | 'off-white' = 'dust';
+	export let color: 'dust' | 'off-white' | 'white' = 'dust';
 
 	let loaded = isNullish(src);
 </script>
@@ -15,8 +15,9 @@
 	class="flex items-center justify-center rounded-50"
 	class:bg-dust={color === 'dust'}
 	class:bg-off-white={color === 'off-white'}
+	class:bg-white={color === 'white'}
 	class:opacity-15={!loaded}
-	style={`border: 1px solid var(--color-${color}); width: calc(${size} + 2px); height: calc(${size} + 2px); transition: opacity 0.15s ease-in;`}
+	style={`border: 1px solid var(--color-${color === "off-white" ? "off-white" : "dust"}); width: calc(${size} + 2px); height: calc(${size} + 2px); transition: opacity 0.15s ease-in;`}
 >
 	{#if nonNullish(src)}
 		<Img {src} {alt} width={size} height={size} on:load={() => (loaded = true)} />
