4fb65ae3328782cadc8167581658770d1b149b78
David Dal Busco <david.dalbusco@outlook.com>
2023-10-26 09:35:29 +0200

    feat(frontend): add token on settings page

--- src/frontend/src/lib/components/ui/RoundedIcon.svelte
+++ src/frontend/src/lib/components/ui/RoundedIcon.svelte
@@ -2,11 +2,12 @@
 	import type { ComponentType } from 'svelte';
 
 	export let icon: ComponentType;
+	export let iconSize = '3rem';
 	export let backgroundStyleClass = 'bg-dark-blue opacity-15';
 	export let iconStyleClass = '';
 </script>
 
 <div class="relative">
-	<div class={`rounded-50 ${backgroundStyleClass}`} style="width: 3rem; aspect-ratio: 1/1" />
+	<div class={`rounded-50 ${backgroundStyleClass}`} style={`width: ${iconSize}; aspect-ratio: 1/1`} />
 	<svelte:component this={icon} styleClass={`inset-center ${iconStyleClass}`} />
 </div>
