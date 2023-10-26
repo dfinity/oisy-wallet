693840105c3f0251c8551a0c861cf002e3e46629
David Dal Busco <david.dalbusco@outlook.com>
2023-10-26 07:29:32 +0200

    feat: contract address input

---
+++ src/frontend/src/lib/components/tokens/AddTokenForm.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { Input } from '@dfinity/gix-components';
+	import { invalidDestination } from '$lib/utils/send.utils';
+	import { createEventDispatcher } from 'svelte';
+
+	export let contractAddress = '';
+
+	let invalid = true;
+	$: invalid = invalidDestination(contractAddress);
+
+	const dispatch = createEventDispatcher();
+</script>
+
+<form on:submit={() => dispatch('icNext')} method="POST">
+	<label for="destination" class="font-bold px-1.25">Contract address:</label>
+	<Input
+		name="contractAddress"
+		inputType="text"
+		required
+		bind:value={contractAddress}
+		placeholder="Enter a contract address"
+	/>
+
+	<div class="flex justify-end gap-1">
+		<button type="button" class="secondary" on:click={() => dispatch('icClose')}>Cancel</button>
+		<button class="primary" type="submit" disabled={invalid} class:opacity-15={invalid}>
+			Next
+		</button>
+	</div>
+</form>
--- src/frontend/src/lib/components/tokens/AddTokenModal.svelte
+++ src/frontend/src/lib/components/tokens/AddTokenModal.svelte
@@ -2,7 +2,9 @@
 	import { WizardModal, type WizardStep, type WizardSteps } from '@dfinity/gix-components';
 	import { modalStore } from '$lib/stores/modal.store';
 
-    import {AddTokenStep} from "$lib/enums/steps";
+	import { AddTokenStep } from '$lib/enums/steps';
+	import AddTokenForm from '$lib/components/tokens/AddTokenForm.svelte';
+	import SendForm from '$lib/components/send/SendForm.svelte';
 
 	const steps: WizardSteps = [
 		{
@@ -29,8 +31,14 @@
 
 		sendProgressStep = AddTokenStep.INITIALIZATION;
 	};
+
+	let contractAddress = '';
 </script>
 
 <WizardModal {steps} bind:currentStep bind:this={modal} on:nnsClose={close}>
 	<svelte:fragment slot="title">{currentStep?.title ?? ''}</svelte:fragment>
+
+	{#if currentStep?.name === 'Review'}{:else if currentStep?.name === 'Saving'}{:else}
+		<AddTokenForm on:icNext={modal.next} on:icClose={close} bind:contractAddress />
+	{/if}
 </WizardModal>
