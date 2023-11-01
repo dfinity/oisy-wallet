ed9316166449c207168d4d2a4132cec9eff38da8
David Dal Busco <david.dalbusco@outlook.com>
2023-11-01 17:50:11 +0100

    style(frontend): review cta to add custom token

--- src/frontend/src/lib/components/ui/RoundedIcon.svelte
+++ src/frontend/src/lib/components/ui/RoundedIcon.svelte
@@ -5,12 +5,13 @@
 	export let iconSize = '2.9rem';
 	export let backgroundStyleClass = 'bg-dark-blue opacity-15';
 	export let iconStyleClass = '';
+	export let additionalStyleClass = '';
 </script>
 
 <div class="relative">
 	<div
 		class={`rounded-50 ${backgroundStyleClass}`}
-		style={`width: ${iconSize}; aspect-ratio: 1/1`}
+		style={`width: ${iconSize}; aspect-ratio: 1/1; ${additionalStyleClass}`}
 	/>
 	<svelte:component this={icon} styleClass={`inset-center ${iconStyleClass}`} />
 </div>
