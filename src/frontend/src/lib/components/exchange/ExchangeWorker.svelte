c29c15107efcd5fb6977342d30a2831ed8a710b7
David Dal Busco <david.dalbusco@outlook.com>
2023-12-04 12:57:35 +0100

    chore: clean import

--- src/frontend/src/lib/components/exchange/ExchangeWorker.svelte
+++ src/frontend/src/lib/components/exchange/ExchangeWorker.svelte
@@ -2,7 +2,6 @@
 	import { onDestroy, onMount } from 'svelte';
 	import type { ExchangeWorker } from '$lib/services/worker.exchange.services';
 	import { initExchangeWorker } from '$lib/services/worker.exchange.services';
-	import { exchangeStore } from '$lib/stores/exchange.store';
 
 	let worker: ExchangeWorker | undefined;
 
