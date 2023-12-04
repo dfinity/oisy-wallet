84298ebdde8cfd6e286fc83058cf3ef46ae3fdc8
David Dal Busco <david.dalbusco@outlook.com>
2023-12-04 16:33:39 +0100

    chore: format

--- src/frontend/src/lib/components/tokens/TokensLoader.svelte
+++ src/frontend/src/lib/components/tokens/TokensLoader.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import { erc20TokensNotInitialized } from '$lib/derived/erc20.derived';
 	import { fade } from 'svelte/transition';
-	import {ERC20_CONTRACTS_ADDRESSES} from "$lib/constants/erc20.constants";
+	import { ERC20_CONTRACTS_ADDRESSES } from '$lib/constants/erc20.constants';
 	import Card from '$lib/components/ui/Card.svelte';
 	import SkeletonText from '$lib/components/ui/SkeletonText.svelte';
 	import SkeletonLogo from '$lib/components/ui/SkeletonLogo.svelte';
 
-    let cards: number[];
-    $: cards = Array.from({ length: ERC20_CONTRACTS_ADDRESSES.length + 1 }, (_, i) => i);
+	let cards: number[];
+	$: cards = Array.from({ length: ERC20_CONTRACTS_ADDRESSES.length + 1 }, (_, i) => i);
 </script>
 
 {#if $erc20TokensNotInitialized}
@@ -22,6 +22,6 @@
 	{/each}
 {:else}
 	<div in:fade>
-        <slot />
-    </div>
+		<slot />
+	</div>
 {/if}
