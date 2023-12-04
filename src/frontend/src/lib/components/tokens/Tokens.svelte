5126f2af55152bdfaa71b90cd7b4864aa5133e14
David Dal Busco <david.dalbusco@outlook.com>
2023-12-04 16:07:26 +0100

    feat: skeleton loader

--- src/frontend/src/lib/components/tokens/Tokens.svelte
+++ src/frontend/src/lib/components/tokens/Tokens.svelte
@@ -6,15 +6,12 @@
 	import type { Token } from '$lib/types/token';
 	import { balancesStore } from '$lib/stores/balances.store';
 	import { transactionsUrl } from '$lib/utils/nav.utils';
-	import { erc20Tokens, erc20TokensNotInitialized } from '$lib/derived/erc20.derived';
+	import { erc20Tokens } from '$lib/derived/erc20.derived';
 	import Listener from '$lib/components/core/Listener.svelte';
 	import Logo from '$lib/components/ui/Logo.svelte';
 	import { AIRDROP } from '$lib/constants/airdrop.constants';
 	import AddToken from '$lib/components/tokens/AddToken.svelte';
-	import SkeletonText from '$lib/components/ui/SkeletonText.svelte';
-	import SkeletonLogo from '$lib/components/ui/SkeletonLogo.svelte';
-	import { ERC20_CONTRACTS_ADDRESSES } from '$lib/constants/erc20.constants';
-	import { fade } from 'svelte/transition';
+	import TokensLoader from '$lib/components/tokens/TokensLoader.svelte';
 
 	let tokens: [Token, ...Token[]] = [ETHEREUM_TOKEN];
 	$: tokens = [ETHEREUM_TOKEN, ...$erc20Tokens];
@@ -22,43 +19,32 @@
 
 <h2 class="text-base mb-6 pb-1" class:mt-12={AIRDROP} class:mt-16={!AIRDROP}>Tokens</h2>
 
-{#each tokens as token (token.id)}
-	{@const url = transactionsUrl(token)}
+<TokensLoader>
+	{#each tokens as token (token.id)}
+		{@const url = transactionsUrl(token)}
 
-	<Listener {token}>
-		<a
-			class="no-underline"
-			href={url}
-			aria-label={`Open the list of ${token.symbol} transactions`}
-			in:fade
-		>
-			<Card>
-				{token.name}
+		<Listener {token}>
+			<a
+				class="no-underline"
+				href={url}
+				aria-label={`Open the list of ${token.symbol} transactions`}
+			>
+				<Card>
+					{token.name}
 
-				<Logo src={token.icon} slot="icon" alt={`${token.name} logo`} size="46px" color="white" />
+					<Logo src={token.icon} slot="icon" alt={`${token.name} logo`} size="46px" color="white" />
 
-				<output class="break-all" slot="amount">
-					{formatTokenShort({
-						value: $balancesStore?.[token.id] ?? BigNumber.from(0n),
-						unitName: token.decimals
-					})}
-					{token.symbol}
-				</output>
-			</Card>
-		</a>
-	</Listener>
-{/each}
-
-{#if $erc20TokensNotInitialized}
-	{#each ERC20_CONTRACTS_ADDRESSES as _}
-		<Card>
-			<span class="inline-block max-w-full w-[120px] sm:w-[200px]"><SkeletonText /></span>
-
-			<span class="inline-block w-full max-w-[100px]" slot="amount"><SkeletonText /></span>
-
-			<SkeletonLogo slot="icon" />
-		</Card>
+					<output class="break-all" slot="amount">
+						{formatTokenShort({
+							value: $balancesStore?.[token.id] ?? BigNumber.from(0n),
+							unitName: token.decimals
+						})}
+						{token.symbol}
+					</output>
+				</Card>
+			</a>
+		</Listener>
 	{/each}
-{/if}
 
-<AddToken />
+	<AddToken />
+</TokensLoader>
