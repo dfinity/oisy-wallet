b08cd54a6e1c101a73073b858556eaa41cbe511b
David Dal Busco <david.dalbusco@outlook.com>
2023-12-06 08:11:12 +0100

    feat: load transactions with skeletons

--- src/frontend/src/lib/derived/token.derived.ts
+++ src/frontend/src/lib/derived/token.derived.ts
@@ -1,19 +1,20 @@
-import { page } from '$app/stores';
 import { ETHEREUM_TOKEN } from '$lib/constants/tokens.constants';
 import { erc20Tokens } from '$lib/derived/erc20.derived';
+import { routeToken } from '$lib/derived/nav.derived';
 import type { Token, TokenId } from '$lib/types/token';
 import { isNullish } from '@dfinity/utils';
 import { derived, type Readable } from 'svelte/store';
 
-export const token: Readable<Token> = derived([page, erc20Tokens], ([$page, $erc20Tokens]) => {
-	const { data } = $page;
+export const token: Readable<Token> = derived(
+	[routeToken, erc20Tokens],
+	([$routeToken, $erc20Tokens]) => {
+		if (isNullish($routeToken)) {
+			return ETHEREUM_TOKEN;
+		}
 
-	if (isNullish(data.token)) {
-		return ETHEREUM_TOKEN;
+		return $erc20Tokens.find(({ name }) => name === $routeToken) ?? ETHEREUM_TOKEN;
 	}
-
-	return $erc20Tokens.find(({ name }) => name === data.token) ?? ETHEREUM_TOKEN;
-});
+);
 
 export const tokenId: Readable<TokenId> = derived([token], ([{ id }]) => id);
 
