2aefa27100c2473e51738590f22a417537cfd6c8
David Dal Busco <david.dalbusco@dfinity.org>
2023-12-11 08:45:40 +0100

    feat: actor and agent singletons (#490)

--- src/frontend/src/lib/api/backend.api.ts
+++ src/frontend/src/lib/api/backend.api.ts
@@ -1,11 +1,11 @@
 import type { SignRequest, Token } from '$declarations/backend/backend.did';
+import { getBackendActor } from '$lib/ic/actor.ic';
 import type { ECDSA_PUBLIC_KEY } from '$lib/types/address';
 import type { OptionIdentity } from '$lib/types/identity';
-import { getBackendActor } from '$lib/utils/actor.utils';
 import type { Identity } from '@dfinity/agent';
 
 export const getEthAddress = async (identity: OptionIdentity): Promise<ECDSA_PUBLIC_KEY> => {
-	const { caller_eth_address } = await getBackendActor(identity);
+	const { caller_eth_address } = await getBackendActor({ identity });
 	return caller_eth_address();
 };
 
@@ -16,7 +16,7 @@ export const signTransaction = async ({
 	transaction: SignRequest;
 	identity: OptionIdentity;
 }): Promise<string> => {
-	const { sign_transaction } = await getBackendActor(identity);
+	const { sign_transaction } = await getBackendActor({ identity });
 	return sign_transaction(transaction);
 };
 
@@ -27,7 +27,7 @@ export const signMessage = async ({
 	message: string;
 	identity: OptionIdentity;
 }): Promise<string> => {
-	const { personal_sign } = await getBackendActor(identity);
+	const { personal_sign } = await getBackendActor({ identity });
 	return personal_sign(message);
 };
 
@@ -38,7 +38,7 @@ export const signPrehash = async ({
 	hash: string;
 	identity: OptionIdentity;
 }): Promise<string> => {
-	const { sign_prehash } = await getBackendActor(identity);
+	const { sign_prehash } = await getBackendActor({ identity });
 	return sign_prehash(hash);
 };
 
@@ -49,11 +49,11 @@ export const addUserToken = async ({
 	token: Token;
 	identity: Identity;
 }): Promise<void> => {
-	const { add_user_token } = await getBackendActor(identity);
+	const { add_user_token } = await getBackendActor({ identity });
 	return add_user_token(token);
 };
 
 export const listUserTokens = async ({ identity }: { identity: Identity }): Promise<Token[]> => {
-	const { list_user_tokens } = await getBackendActor(identity);
+	const { list_user_tokens } = await getBackendActor({ identity });
 	return list_user_tokens();
 };
