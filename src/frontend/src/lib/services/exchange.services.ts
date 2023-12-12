1f096d3244ef59fbee34895a6e87ed578e82d3bb
David Dal Busco <david.dalbusco@dfinity.org>
2023-12-12 09:20:57 +0100

    refactor: extract function to module to sync exchange store (#501)

--- src/frontend/src/lib/services/exchange.services.ts
+++ src/frontend/src/lib/services/exchange.services.ts
@@ -1,6 +1,12 @@
+import { ETHEREUM_TOKEN_ID } from '$lib/constants/tokens.constants';
+import { erc20Tokens } from '$lib/derived/erc20.derived';
 import { simplePrice, simpleTokenPrice } from '$lib/rest/goincecko.rest';
+import { exchangeStore } from '$lib/stores/exchange.store';
 import type { CoingeckoSimplePriceResponse } from '$lib/types/coingecko';
 import type { Erc20ContractAddress } from '$lib/types/erc20';
+import type { PostMessageDataResponseExchange } from '$lib/types/post-message';
+import { nonNullish } from '@dfinity/utils';
+import { get } from 'svelte/store';
 
 export const exchangeRateETHToUsd = async (): Promise<CoingeckoSimplePriceResponse | null> =>
 	simplePrice({
@@ -16,3 +22,19 @@ export const exchangeRateERC20ToUsd = async (
 		vs_currencies: 'usd',
 		contract_addresses: contractAddresses.map(({ address }) => address)
 	});
+
+export const syncExchange = (data: PostMessageDataResponseExchange | undefined) => {
+	exchangeStore.set([
+		{
+			tokenId: ETHEREUM_TOKEN_ID,
+			currentPrice: data?.currentEthPrice?.ethereum
+		},
+		...Object.entries(data?.currentErc20Prices ?? {})
+			.map(([key, currentPrice]) => {
+				const tokens = get(erc20Tokens);
+				const token = tokens.find(({ address }) => address.toLowerCase() === key.toLowerCase());
+				return nonNullish(token) ? { tokenId: token.id, currentPrice } : undefined;
+			})
+			.filter(nonNullish)
+	]);
+};
