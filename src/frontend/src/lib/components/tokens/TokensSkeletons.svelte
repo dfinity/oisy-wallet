ed2f156e47f1c6d715c8a1a37bf845d323eaa889
David Dal Busco <david.dalbusco@dfinity.org>
2023-12-13 08:19:53 +0100

    feat: use ICP usd exchange for ICP on ETH (#509)
    * feat: use ICP usd exchange for ICP on ETH
    
    * feat: better perf

--- src/frontend/src/lib/components/tokens/TokensSkeletons.svelte
+++ src/frontend/src/lib/components/tokens/TokensSkeletons.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import { erc20TokensNotInitialized } from '$lib/derived/erc20.derived';
 	import { fade } from 'svelte/transition';
-	import { ERC20_CONTRACTS_ADDRESSES } from '$lib/constants/erc20.constants';
+	import { ERC20_CONTRACTS } from '$lib/constants/erc20.constants';
 	import SkeletonCards from '$lib/components/ui/SkeletonCards.svelte';
 </script>
 
 {#if $erc20TokensNotInitialized}
-	<SkeletonCards rows={ERC20_CONTRACTS_ADDRESSES.length + 1} />
+	<SkeletonCards rows={ERC20_CONTRACTS.length + 1} />
 {:else}
 	<div in:fade>
 		<slot />
