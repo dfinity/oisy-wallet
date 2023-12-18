217e44daf2e5e3efa5f7213851dae10c125bb067
David Dal Busco <david.dalbusco@outlook.com>
2023-12-18 13:51:59 +0100

    feat: update idb last used timestamp

--- src/frontend/src/lib/api/idb.api.ts
+++ src/frontend/src/lib/api/idb.api.ts
@@ -1,7 +1,8 @@
 import { browser } from '$app/environment';
 import type { IdbEthAddress } from '$lib/types/idb';
 import type { Principal } from '@dfinity/principal';
-import { createStore, del, get, set, type UseStore } from 'idb-keyval';
+import { isNullish } from '@dfinity/utils';
+import { createStore, del, get, set, update, type UseStore } from 'idb-keyval';
 
 // There is no IndexedDB in SSG. Since this initialization occurs at the module's root, SvelteKit would encounter an error during the dapp bundling process, specifically a "ReferenceError [Error]: indexedDB is not defined". Therefore, the object for bundling on NodeJS side.
 const oisyEthAddressesStore = browser
@@ -15,6 +16,22 @@ export const setIdbEthAddress = ({
 	principal: Principal;
 	address: IdbEthAddress;
 }): Promise<void> => set(principal.toText(), address, oisyEthAddressesStore);
+
+export const updateIdbEthAddressLastUsage = (principal: Principal): Promise<void> =>
+	update(
+		principal.toText(),
+		(address) => {
+			if (isNullish(address)) {
+				return undefined;
+			}
+
+			return {
+				...address,
+				lastUsedTimestamp: Date.now()
+			};
+		},
+		oisyEthAddressesStore
+	);
 
 export const getIdbEthAddress = (principal: Principal): Promise<IdbEthAddress | undefined> =>
 	get(principal.toText(), oisyEthAddressesStore);
