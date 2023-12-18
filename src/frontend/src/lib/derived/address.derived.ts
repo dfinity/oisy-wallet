5239898fc5adee8bb0003e5d967399e4b4700903
David Dal Busco <david.dalbusco@outlook.com>
2023-12-18 12:38:51 +0100

    refactor: extract derived address store

--- src/frontend/src/lib/derived/address.derived.ts
+++ src/frontend/src/lib/derived/address.derived.ts
@@ -1,7 +1,12 @@
-import { addressStore } from '$lib/stores/address.store';
+import { addressStore, type AddressData } from '$lib/stores/address.store';
 import { isNullish } from '@dfinity/utils';
 import { derived, type Readable } from 'svelte/store';
 
 export const addressNotLoaded: Readable<boolean> = derived([addressStore], ([$addressStore]) =>
 	isNullish($addressStore)
+);
+
+export const address: Readable<AddressData> = derived(
+	[addressStore],
+	([$addressStore]) => $addressStore
 );
