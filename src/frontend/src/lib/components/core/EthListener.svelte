03e46381261a266236dcdc40e3c5c51964d87426
David Dal Busco <david.dalbusco@outlook.com>
2023-12-19 17:10:20 +0100

    chore: type

--- src/frontend/src/lib/components/core/EthListener.svelte
+++ src/frontend/src/lib/components/core/EthListener.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
 	import { isNullish } from '@dfinity/utils';
-	import { type AddressData } from '$lib/stores/address.store';
 	import { onDestroy } from 'svelte';
 	import { initTransactionsListener } from '$lib/services/eth-listener.services';
 	import type { WebSocketListener } from '$lib/types/listener';
 	import type { Token } from '$lib/types/token';
 	import { address } from '$lib/derived/address.derived';
+	import type { OptionAddress } from '$lib/types/address';
 
 	export let token: Token;
 
 	let listener: WebSocketListener | undefined = undefined;
 
-	const initListener = async ({ address }: { address: AddressData }) => {
+	const initListener = async ({ address }: { address: OptionAddress }) => {
 		await listener?.disconnect();
 
 		if (isNullish(address)) {
