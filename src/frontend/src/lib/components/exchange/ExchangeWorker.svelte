a3833a730c8c4f6ae8aca10d1af89820d5c2aea9
David Dal Busco <david.dalbusco@dfinity.org>
2023-12-20 08:53:48 +0100

    feat: disable exchange rate on local env (#531)

--- src/frontend/src/lib/components/exchange/ExchangeWorker.svelte
+++ src/frontend/src/lib/components/exchange/ExchangeWorker.svelte
@@ -3,10 +3,18 @@
 	import type { ExchangeWorker } from '$lib/services/worker.exchange.services';
 	import { initExchangeWorker } from '$lib/services/worker.exchange.services';
 	import { erc20TokensAddresses } from '$lib/derived/erc20.derived';
+	import { LOCAL } from '$lib/constants/app.constants';
 
 	let worker: ExchangeWorker | undefined;
 
-	onMount(async () => (worker = await initExchangeWorker()));
+	onMount(async () => {
+		if (LOCAL) {
+			// Using the exchange API during development if not necessary and given its limitation of calls per minute is annoying at it often throws errors on server reload.
+			return;
+		}
+
+		worker = await initExchangeWorker();
+	});
 
 	onDestroy(() => worker?.destroy());
 
