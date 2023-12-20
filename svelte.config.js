631d623ad2c7a9832cfba5244ad4d7c695892a35
David Dal Busco <david.dalbusco@dfinity.org>
2023-12-20 19:23:51 +0100

    build: redo SvelteKit v2 (#545)
    * build: redo SvelteKit v2
    
    * build: node types

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { readFileSync } from 'node:fs';
 import { fileURLToPath } from 'node:url';
 
