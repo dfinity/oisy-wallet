35acd4eabcdc14b6f5e03566be43e9c67b565f76
David Dal Busco <david.dalbusco@outlook.com>
2023-12-20 13:33:56 +0100

    feat: review test banner

--- src/frontend/src/lib/components/core/Banner.svelte
+++ src/frontend/src/lib/components/core/Banner.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-	import { IconClose } from '@dfinity/gix-components';
+	import { IconClose, IconWarning } from '@dfinity/gix-components';
 	import { STAGING } from '$lib/constants/app.constants';
 
 	let visible = true;
 
-	const banner = STAGING;
-	const NETWORK = import.meta.env.VITE_INFURA_NETWORK;
-
 	const close = () => (visible = false);
 </script>
 
-{#if banner && visible}
-	<div>
-		<h3 class="clamp-4">For testing purposes on <strong>{NETWORK.toUpperCase()}</strong> only.</h3>
+{#if STAGING && visible}
+	<div class="flex justify-between gap-4">
+		<span class="flex items-center justify-center gap-4">
+			<IconWarning size="48px" />
+			<h3 class="clamp-4">For testing purposes only!</h3>
+		</span>
 		<button on:click={close} aria-label="Close"><IconClose /></button>
 	</div>
 {/if}
@@ -26,38 +26,18 @@
 
 		z-index: calc(var(--overlay-z-index) + 10);
 
-		display: grid;
-		grid-template-columns: 20% 60% 20%;
-
-		justify-content: center;
-		align-items: center;
-
 		background: var(--negative-emphasis);
 
 		padding: var(--padding-2x) var(--padding-3x);
 		margin: var(--padding-3x) 0;
 
-		border-radius: var(--border-radius-lg);
+		border-radius: var(--border-radius);
+
+		border: 4px solid black;
 
 		width: calc(100% - var(--padding-4x));
 		max-width: 768px;
 
 		box-shadow: 0 4px 16px 0 rgba(0, 0, 0, 0.1215686275);
-	}
-
-	h3 {
-		margin: 0;
-
-		grid-column-start: 2;
-
-		text-align: center;
-
-		color: inherit;
-	}
-
-	button {
-		display: flex;
-		justify-self: flex-end;
-		margin: 0 var(--padding);
 	}
 </style>
