22811a0881f1ac5705cd894621cb9d55abfc8066
David Dal Busco <david.dalbusco@outlook.com>
2023-12-28 16:07:39 +0100

    refactor: extract common code to query workers

--- src/frontend/src/lib/workers/icrc-wallet.worker.ts
+++ src/frontend/src/lib/workers/icrc-wallet.worker.ts
@@ -1,22 +1,34 @@
-import { getTransactions } from '$lib/api/icrc-index.api';
-import { WALLET_TIMER_INTERVAL_MILLIS } from '$lib/constants/app.constants';
-import type {
-	PostMessage,
-	PostMessageDataRequestIcrc,
-	PostMessageDataResponseWallet
-} from '$lib/types/post-message';
-import {
-	TimerWorkerUtils,
-	type TimerWorkerUtilsJobData
-} from '$lib/worker-utils/timer.worker-utils';
+import { getTransactions as getTransactionsApi } from '$lib/api/icrc-index.api';
+import type { PostMessage, PostMessageDataRequestIcrc } from '$lib/types/post-message';
+import { type TimerWorkerUtilsJobData } from '$lib/worker-utils/timer.worker-utils';
+import { WalletWorkerUtils } from '$lib/worker-utils/wallet.worker-utils';
 import type {
 	IcrcGetTransactions,
 	IcrcTransaction,
 	IcrcTransactionWithId
 } from '@dfinity/ledger-icrc';
-import { assertNonNullish, jsonReplacer } from '@dfinity/utils';
+import { assertNonNullish } from '@dfinity/utils';
 
-const worker = new TimerWorkerUtils();
+const getTransactions = ({
+	identity,
+	data
+}: TimerWorkerUtilsJobData<PostMessageDataRequestIcrc>): Promise<IcrcGetTransactions> => {
+	assertNonNullish(data, 'No data - indexCanisterId - provided to fetch transactions.');
+
+	return getTransactionsApi({
+		identity,
+		owner: identity.getPrincipal(),
+		// We query tip to discover the new transactions
+		start: undefined,
+		...data
+	});
+};
+
+const worker: WalletWorkerUtils<
+	IcrcTransaction,
+	IcrcTransactionWithId,
+	PostMessageDataRequestIcrc
+> = new WalletWorkerUtils(getTransactions, 'syncIcrcWallet');
 
 onmessage = async ({ data: dataMsg }: MessageEvent<PostMessage<PostMessageDataRequestIcrc>>) => {
 	const { msg, data } = dataMsg;
@@ -26,71 +38,7 @@ onmessage = async ({ data: dataMsg }: MessageEvent<PostMessage<PostMessageDataRe
 			worker.stop();
 			return;
 		case 'startIcrcWalletTimer':
-			await worker.start<PostMessageDataRequestIcrc>({
-				interval: WALLET_TIMER_INTERVAL_MILLIS,
-				job: syncWallet,
-				data
-			});
+			await worker.start(data);
 			return;
 	}
 };
-
-let transactions: Record<string, IcrcTransaction> = {};
-let initialized = false;
-
-const syncWallet = async ({
-	identity,
-	data
-}: TimerWorkerUtilsJobData<PostMessageDataRequestIcrc>) => {
-	assertNonNullish(data, 'No data - indexCanisterId - provided to fetch transactions.');
-
-	const { transactions: fetchedTransactions, ...rest } = await getTransactions({
-		identity,
-		owner: identity.getPrincipal(),
-		// We query tip to discover the new transactions
-		start: undefined,
-		...data
-	});
-
-	const newTransactions = fetchedTransactions.filter(
-		({ id }) => !Object.keys(transactions).includes(`${id}`)
-	);
-
-	if (newTransactions.length === 0) {
-		// We execute postMessage at least once because developer may have no transaction at all so, we want to display the balance zero
-		if (!initialized) {
-			postMessageWallet({ transactions: newTransactions, ...rest });
-
-			initialized = true;
-		}
-
-		return;
-	}
-
-	transactions = {
-		...transactions,
-		...newTransactions.reduce(
-			(acc: Record<string, IcrcTransaction>, { id, transaction }: IcrcTransactionWithId) => ({
-				...acc,
-				[`${id}`]: transaction
-			}),
-			{}
-		)
-	};
-
-	postMessageWallet({ transactions: newTransactions, ...rest });
-};
-
-const postMessageWallet = ({ transactions: newTransactions, ...rest }: IcrcGetTransactions) =>
-	worker.postMsg<PostMessageDataResponseWallet<IcrcGetTransactions>>({
-		msg: 'syncIcrcWallet',
-		data: {
-			wallet: {
-				...rest,
-				newTransactions: JSON.stringify(
-					Object.entries(newTransactions).map(([_id, transaction]) => transaction),
-					jsonReplacer
-				)
-			}
-		}
-	});
