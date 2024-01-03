32b283277a545ddb68829af5c3cc492a9a4c9194
David Dal Busco <david.dalbusco@dfinity.org>
2024-01-03 16:15:58 +0100

    feat: Icrc ckETK support (no conversions) (#566)

--- src/frontend/src/lib/constants/icrc.constants.ts
+++ src/frontend/src/lib/constants/icrc.constants.ts
@@ -8,10 +8,23 @@ export const CKBTC_INDEX_CANISTER_ID =
 	(import.meta.env.VITE_CKBTC_INDEX_CANISTER_ID as string | null | undefined) ??
 	'n5wcd-faaaa-aaaar-qaaea-cai';
 
+export const CKETH_LEDGER_CANISTER_ID =
+	(import.meta.env.VITE_CKETH_LEDGER_CANISTER_ID as string | null | undefined) ??
+	'ss2fx-dyaaa-aaaar-qacoq-cai';
+
+export const CKETH_INDEX_CANISTER_ID =
+	(import.meta.env.VITE_CKETH_INDEX_CANISTER_ID as string | null | undefined) ??
+	's3zol-vqaaa-aaaar-qacpa-cai';
+
 export const ICRC_CANISTERS: IcInterface[] = [
 	{
 		ledgerCanisterId: CKBTC_LEDGER_CANISTER_ID,
 		indexCanisterId: CKBTC_INDEX_CANISTER_ID,
 		exchangeCoinId: 'bitcoin'
+	},
+	{
+		ledgerCanisterId: CKETH_LEDGER_CANISTER_ID,
+		indexCanisterId: CKETH_INDEX_CANISTER_ID,
+		exchangeCoinId: 'ethereum'
 	}
 ];
