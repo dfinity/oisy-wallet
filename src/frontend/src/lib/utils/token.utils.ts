810eeed0e8fbfc77504d84e1b918659cb8c61d57
David Dal Busco <david.dalbusco@outlook.com>
2024-01-05 08:16:26 +0100

    fix: loading data on navigation

--- src/frontend/src/lib/utils/token.utils.ts
+++ src/frontend/src/lib/utils/token.utils.ts
@@ -2,6 +2,3 @@ import { ERC20_ICP_SYMBOL } from '$lib/constants/erc20-icp.constants';
 import type { Token, TokenStandard } from '$lib/types/token';
 
 export const isErc20Icp = ({ symbol }: Token): boolean => symbol === ERC20_ICP_SYMBOL;
-
-export const isTokenStandardIc = (standard: TokenStandard): boolean =>
-	['icp', 'icrc'].includes(standard);
