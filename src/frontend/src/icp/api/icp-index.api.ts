5521180a3d0edc26ad1020284e9be2adea16dab2
David Dal Busco <david.dalbusco@outlook.com>
2024-01-08 08:23:52 +0100

    refactor: move $icp utils

--- src/frontend/src/icp/api/icp-index.api.ts
+++ src/frontend/src/icp/api/icp-index.api.ts
@@ -1,8 +1,8 @@
 import { ICP_INDEX_CANISTER_ID } from '$icp/constants/icp.constants';
+import { getAccountIdentifier } from '$icp/utils/icp-account.utils';
 import { getAgent } from '$lib/actors/agents.ic';
 import { WALLET_PAGINATION } from '$lib/constants/app.constants';
 import type { OptionIdentity } from '$lib/types/identity';
-import { getAccountIdentifier } from '$lib/utils/icp-account.utils';
 import { IndexCanister, type GetAccountIdentifierTransactionsResponse } from '@dfinity/ledger-icp';
 import { Principal } from '@dfinity/principal';
 import { assertNonNullish } from '@dfinity/utils';
--- src/frontend/src/icp/derived/ic.derived.ts
+++ src/frontend/src/icp/derived/ic.derived.ts
@@ -1,7 +1,7 @@
+import { getAccountIdentifier } from '$icp/utils/icp-account.utils';
+import { getIcrcAccount } from '$icp/utils/icrc-account.utils';
 import { tokenStandard } from '$lib/derived/token.derived';
 import { authStore } from '$lib/stores/auth.store';
-import { getAccountIdentifier } from '$lib/utils/icp-account.utils';
-import { getIcrcAccount } from '$lib/utils/icrc-account.utils';
 import type { AccountIdentifier } from '@dfinity/ledger-icp';
 import { encodeIcrcAccount, type IcrcAccount } from '@dfinity/ledger-icrc';
 import { nonNullish } from '@dfinity/utils';
