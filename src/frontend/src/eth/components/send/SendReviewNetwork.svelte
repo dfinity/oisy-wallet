387fdc036b1a4c7c76d32189bcaed2fc1e58a37c
David Dal Busco <david.dalbusco@dfinity.org>
2024-01-08 17:16:11 +0100

    feat: extract review network component (#584)
    * feat: extract review network component
    
    * chore: format

---
+++ src/frontend/src/eth/components/send/SendReviewNetwork.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Network } from '$lib/types/network';
+	import { nonNullish } from '@dfinity/utils';
+	import Value from '$lib/components/ui/Value.svelte';
+	import { isNetworkICP } from '$lib/utils/network.utils';
+
+	export let network: Network | undefined = undefined;
+</script>
+
+{#if nonNullish(network)}
+	<Value ref="network" element="div">
+		<svelte:fragment slot="label">Network</svelte:fragment>
+		{#if isNetworkICP(network)}
+			Convert to native ICP
+		{:else}
+			Ethereum
+		{/if}
+	</Value>
+{/if}
