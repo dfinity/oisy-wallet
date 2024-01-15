4e0ee09b08ae6ddc5eed811e3b05824523cba8cc
David Dal Busco <david.dalbusco@dfinity.org>
2024-01-15 09:08:58 +0100

    feat: metadata with query and update (#592)

---
+++ src/frontend/src/lib/actors/query.ic.ts
@@ -0,0 +1,67 @@
+import type { OptionIdentity } from '$lib/types/identity';
+
+export type QueryAndUpdateOnResponse<R> = (options: { certified: boolean; response: R }) => void;
+
+export type QueryAndUpdateOnError<E = unknown> = (options: {
+	certified: boolean;
+	error: E;
+	// The identity used for the request
+	identity: OptionIdentity;
+}) => void;
+
+export type QueryAndUpdateStrategy = 'query_and_update' | 'query' | 'update';
+
+export interface QueryAndUpdateRequestParams {
+	certified: boolean;
+	identity: OptionIdentity;
+}
+
+/**
+ * Perform a query and an update for security reasons.
+ * For example, malicious nodes can forge transactions and balance if no update is performed to certify the results.
+ */
+export const queryAndUpdate = async <R, E = unknown>({
+	request,
+	onLoad,
+	onError,
+	strategy = 'query_and_update',
+	identity
+}: {
+	request: (options: QueryAndUpdateRequestParams) => Promise<R>;
+	onLoad: QueryAndUpdateOnResponse<R>;
+	onError?: QueryAndUpdateOnError<E>;
+	strategy?: QueryAndUpdateStrategy;
+	identity: OptionIdentity;
+}): Promise<void> => {
+	let certifiedDone = false;
+
+	const queryOrUpdate = (certified: boolean) =>
+		request({ certified, identity })
+			.then((response) => {
+				if (certifiedDone) {
+					return;
+				}
+
+				!certifiedDone && onLoad({ certified, response });
+			})
+			.catch((error: E) => {
+				if (certifiedDone) {
+					return;
+				}
+
+				onError?.({ certified, error, identity });
+			})
+			.finally(() => (certifiedDone = certifiedDone || certified));
+
+	let requests: Array<Promise<void>>;
+
+	if (strategy === 'query') {
+		requests = [queryOrUpdate(false)];
+	} else if (strategy === 'update') {
+		requests = [queryOrUpdate(true)];
+	} else {
+		requests = [queryOrUpdate(false), queryOrUpdate(true)];
+	}
+
+	await Promise.race(requests);
+};
