b5716a5f007749e9cfa0d0d2678129f838312652
David Dal Busco <david.dalbusco@outlook.com>
2024-01-15 12:02:49 +0100

    feat: icp and icrc load next transactions with query and update

--- src/frontend/src/icp/services/ic-transactions.services.ts
+++ src/frontend/src/icp/services/ic-transactions.services.ts
@@ -1,10 +1,13 @@
 import { getTransactions as getTransactionsIcp } from '$icp/api/icp-index.api';
 import { getTransactions as getTransactionsIcrc } from '$icp/api/icrc-index.api';
+import { icTransactionsStore } from '$icp/stores/ic-transactions.store';
 import type { IcGetTransactions, IcToken } from '$icp/types/ic';
+import { queryAndUpdate } from '$lib/actors/query.ic';
+import { toastsError } from '$lib/stores/toasts.store';
 import type { OptionIdentity } from '$lib/types/identity';
 import { Principal } from '@dfinity/principal';
 
-export const getTransactions = async ({
+const getTransactions = async ({
 	token: { standard, indexCanisterId },
 	...rest
 }: {
@@ -25,3 +28,53 @@ export const getTransactions = async ({
 		...rest
 	});
 };
+
+export const loadNextTransactions = async ({
+	token: { id: tokenId, ...token },
+	identity,
+	signalEnd,
+	...rest
+}: {
+	owner: Principal;
+	identity: OptionIdentity;
+	start?: bigint;
+	maxResults?: bigint;
+	token: IcToken;
+	signalEnd: () => void;
+}): Promise<void> =>
+	queryAndUpdate<IcGetTransactions>({
+		request: (params) =>
+			getTransactions({
+				token: { id: tokenId, ...token },
+				...rest,
+				...params
+			}),
+		onLoad: ({ response: { transactions }, certified }) => {
+			if (transactions.length === 0) {
+				signalEnd();
+				return;
+			}
+
+			icTransactionsStore.append({
+				tokenId,
+				transactions: transactions.map((data) => ({ data, certified }))
+			});
+		},
+		onError: ({ error: err, certified }) => {
+			console.error(err);
+
+			if (!certified) {
+				return;
+			}
+
+			icTransactionsStore.reset(tokenId);
+
+			toastsError({
+				msg: { text: 'Something went wrong while fetching the transactions.' },
+				err
+			});
+
+			signalEnd();
+		},
+		identity
+	});
