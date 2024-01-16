154ccd1b0a2ae2af395eb22f8055f5e068f0f284
David Dal Busco <david.dalbusco@outlook.com>
2024-01-16 14:52:09 +0100

    feat: debounce init check for a bit smoother UI

--- src/frontend/src/icp/components/send/IcSendDestination.svelte
+++ src/frontend/src/icp/components/send/IcSendDestination.svelte
@@ -3,18 +3,25 @@
 	import type { NetworkId } from '$lib/types/network';
 	import { tokenStandard } from '$lib/derived/token.derived';
 	import { isInvalidDestinationIc } from '$icp/utils/ic-send.utils';
+	import { debounce } from '@dfinity/utils';
 
 	export let destination = '';
 	export let networkId: NetworkId | undefined = undefined;
 	export let invalidDestination = false;
 
 	let isInvalidDestination: () => boolean;
-	$: isInvalidDestination = (): boolean =>
-		isInvalidDestinationIc({
-			destination,
-			tokenStandard: $tokenStandard,
-			networkId
-		});
+
+	const init = () =>
+		(isInvalidDestination = (): boolean =>
+			isInvalidDestinationIc({
+				destination,
+				tokenStandard: $tokenStandard,
+				networkId
+			}));
+
+	const debounceValidateInit = debounce(init);
+
+	$: destination, $tokenStandard, networkId, debounceValidateInit();
 </script>
 
 <SendInputDestination
