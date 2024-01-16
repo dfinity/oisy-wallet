f327c0e60244aa7218a1822b54c22eb20495d665
David Dal Busco <david.dalbusco@dfinity.org>
2024-01-16 18:07:24 +0100

    build: deploy ckETH ledger and index with u256 special wasm for 18 decimals (#615)

--- scripts/download.cketh.sh
+++ scripts/download.cketh.sh
@@ -13,10 +13,11 @@ IC_COMMIT="43f31c0a1b0d9f9ecbc4e2e5f142c56c7d9b0c7b"
 curl -sSL https://download.dfinity.systems/ic/$IC_COMMIT/canisters/ic-cketh-minter.wasm.gz -o "$DIR"/cketh_minter.wasm.gz
 gunzip "$DIR"/cketh_minter.wasm.gz
 
-curl -sSL https://download.dfinity.systems/ic/$IC_COMMIT/canisters/ic-icrc1-ledger.wasm.gz -o "$DIR"/cketh_ledger.wasm.gz
+# ckETH requires special ledger and index to handle 18 decimals, therefore the special wasm
+curl -sSL https://download.dfinity.systems/ic/$IC_COMMIT/canisters/ic-icrc1-ledger-u256.wasm.gz -o "$DIR"/cketh_ledger.wasm.gz
 gunzip "$DIR"/cketh_ledger.wasm.gz
 
-curl -sSL https://download.dfinity.systems/ic/$IC_COMMIT/canisters/ic-icrc1-index-ng.wasm.gz -o "$DIR"/cketh_index.wasm.gz
+curl -sSL https://download.dfinity.systems/ic/$IC_COMMIT/canisters/ic-icrc1-index-ng-u256.wasm.gz -o "$DIR"/cketh_index.wasm.gz
 gunzip "$DIR"/cketh_index.wasm.gz
 
 curl -sSL https://raw.githubusercontent.com/dfinity/ic/$IC_COMMIT/rs/ethereum/cketh/minter/cketh_minter.did -o "$DIR"/cketh_minter.did
