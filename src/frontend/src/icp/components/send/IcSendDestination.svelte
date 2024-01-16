6bd50fbad2d71a328f4c0d853e9c9888dd95da19
David Dal Busco <david.dalbusco@dfinity.org>
2024-01-16 13:42:16 +0100

    feat: invalid address check on review step (#608)

--- src/frontend/src/icp/components/send/IcSendDestination.svelte
+++ src/frontend/src/icp/components/send/IcSendDestination.svelte
@@ -2,39 +2,20 @@
 	import { ICP_NETWORK } from '$lib/constants/networks.constants';
 	import SendDestination from '$lib/components/send/SendDestination.svelte';
 	import { debounce } from '@dfinity/utils';
-	import { isNullishOrEmpty } from '$lib/utils/input.utils';
 	import type { NetworkId } from '$lib/types/network';
-	import { invalidBtcAddress, isNetworkIdBTC } from '$icp/utils/send.utils';
-	import { BTC_NETWORK } from '$icp/constants/ckbtc.constants';
 	import { tokenStandard } from '$lib/derived/token.derived';
-	import { invalidIcrcAddress } from '$icp/utils/icrc-account.utils';
-	import { invalidIcpAddress } from '$icp/utils/icp-account.utils';
+	import { isInvalidDestinationIc } from '$icp/utils/send.utils';
 
 	export let destination = '';
 	export let networkId: NetworkId | undefined = undefined;
 	export let invalidDestination = false;
 
-	const validate = () => {
-		if (isNullishOrEmpty(destination)) {
-			invalidDestination = false;
-			return;
-		}
-
-		if (isNetworkIdBTC(networkId)) {
-			invalidDestination = invalidBtcAddress({
-				address: destination,
-				network: BTC_NETWORK
-			});
-			return;
-		}
-
-		if ($tokenStandard === 'icrc') {
-			invalidDestination = invalidIcrcAddress(destination);
-			return;
-		}
-
-		invalidDestination = invalidIcpAddress(destination) && invalidIcrcAddress(destination);
-	};
+	const validate = () =>
+		(invalidDestination = isInvalidDestinationIc({
+			destination,
+			tokenStandard: $tokenStandard,
+			networkId
+		}));
 
 	const debounceValidate = debounce(validate);
 
--- src/frontend/src/icp/utils/send.utils.ts
+++ src/frontend/src/icp/utils/send.utils.ts
@@ -1,7 +1,11 @@
-import { BTC_NETWORK_ID } from '$icp/constants/ckbtc.constants';
+import { BTC_NETWORK, BTC_NETWORK_ID } from '$icp/constants/ckbtc.constants';
 import { CKBTC_LEDGER_CANISTER_ID } from '$icp/constants/icrc.constants';
 import type { IcToken } from '$icp/types/ic';
+import { invalidIcpAddress } from '$icp/utils/icp-account.utils';
+import { invalidIcrcAddress } from '$icp/utils/icrc-account.utils';
 import type { NetworkId } from '$lib/types/network';
+import type { TokenStandard } from '$lib/types/token';
+import { isNullishOrEmpty } from '$lib/utils/input.utils';
 import { parseBtcAddress, type BtcAddress } from '@dfinity/ckbtc';
 import { isNullish } from '@dfinity/utils';
 
@@ -26,3 +30,30 @@ export const isNetworkUsingCkBtcLedger = ({ ledgerCanisterId }: IcToken): boolea
 
 export const isNetworkIdBTC = (networkId: NetworkId | undefined): boolean =>
 	networkId === BTC_NETWORK_ID;
+
+export const isInvalidDestinationIc = ({
+	destination,
+	networkId,
+	tokenStandard
+}: {
+	destination: string;
+	networkId: NetworkId | undefined;
+	tokenStandard: TokenStandard;
+}): boolean => {
+	if (isNullishOrEmpty(destination)) {
+		return false;
+	}
+
+	if (isNetworkIdBTC(networkId)) {
+		return invalidBtcAddress({
+			address: destination,
+			network: BTC_NETWORK
+		});
+	}
+
+	if (tokenStandard === 'icrc') {
+		return invalidIcrcAddress(destination);
+	}
+
+	return invalidIcpAddress(destination) && invalidIcrcAddress(destination);
+};
