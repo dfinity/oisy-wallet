c93921f2f5d5e2617840ae53ccbba2ae3ca06543
David Dal Busco <david.dalbusco@outlook.com>
2024-01-17 11:50:39 +0100

    feat: eth to ckEth fee

---
+++ src/frontend/src/eth/constants/cketh.constants.ts
@@ -0,0 +1,31 @@
+export const CKETH_ABI = [
+	{
+		inputs: [{ internalType: 'address', name: '_cketh_minter_main_address', type: 'address' }],
+		stateMutability: 'nonpayable',
+		type: 'constructor'
+	},
+	{
+		anonymous: false,
+		inputs: [
+			{ indexed: true, internalType: 'address', name: 'from', type: 'address' },
+			{ indexed: false, internalType: 'uint256', name: 'value', type: 'uint256' },
+			{ indexed: true, internalType: 'bytes32', name: 'principal', type: 'bytes32' }
+		],
+		name: 'ReceivedEth',
+		type: 'event'
+	},
+	{
+		inputs: [{ internalType: 'bytes32', name: '_principal', type: 'bytes32' }],
+		name: 'deposit',
+		outputs: [],
+		stateMutability: 'payable',
+		type: 'function'
+	},
+	{
+		inputs: [],
+		name: 'getMinterAddress',
+		outputs: [{ internalType: 'address', name: '', type: 'address' }],
+		stateMutability: 'view',
+		type: 'function'
+	}
+];
--- src/frontend/src/eth/services/fee.services.ts
+++ src/frontend/src/eth/services/fee.services.ts
@@ -1,24 +1,64 @@
 import { ERC20_FALLBACK_FEE } from '$eth/constants/erc20.constants';
+import { ETH_BASE_FEE } from '$eth/constants/eth.constants';
+import { getFeeData as getCkEthFeeData } from '$eth/providers/infura-cketh.providers';
 import { getFeeData as getBurnFeeData } from '$eth/providers/infura-erc20-icp.providers';
-import { getFeeData } from '$eth/providers/infura-erc20.providers';
+import { getFeeData as getErc20FeeDataProvider } from '$eth/providers/infura-erc20.providers';
+import { CKETH_HELPER_CONTRACT } from '$eth/types/cketh';
 import type { Erc20ContractAddress } from '$eth/types/erc20';
 import { ETHEREUM_NETWORK } from '$lib/constants/networks.constants';
 import type { ETH_ADDRESS } from '$lib/types/address';
+import type { OptionIdentity } from '$lib/types/identity';
 import type { Network } from '$lib/types/network';
 import { isNetworkICP } from '$lib/utils/network.utils';
+import { assertNonNullish, decodeBase32, encodeBase32 } from '@dfinity/utils';
 import { BigNumber } from '@ethersproject/bignumber';
+
+export interface GetFeeData {
+	address: ETH_ADDRESS;
+}
+
+export const getEthFeeData = async ({
+	address,
+	identity,
+	ckEthContract
+}: GetFeeData & {
+	identity: OptionIdentity;
+	ckEthContract: Erc20ContractAddress;
+}): Promise<BigNumber> => {
+	console.log(address, address === CKETH_HELPER_CONTRACT);
+
+	if (address === CKETH_HELPER_CONTRACT) {
+		assertNonNullish(identity, 'No identity provided to calculate the fee for its principal.');
+
+		const rawBytes = decodeBase32(identity.getPrincipal().toText().toLowerCase().replace(/-/g, ''));
+		const encoded = encodeBase32(rawBytes);
+
+		console.log(
+			identity.getPrincipal().toText(),
+			decodeBase32(identity.getPrincipal().toText().toLowerCase().replace(/-/g, ''))
+		);
+
+		const tmp = await getCkEthFeeData({
+			address: decodeBase32(identity.getPrincipal().toText().toLowerCase().replace(/-/g, '')),
+			contract: ckEthContract
+		});
+
+		return tmp;
+	}
+
+	return BigNumber.from(ETH_BASE_FEE);
+};
 
 export const getErc20FeeData = async ({
 	network,
 	...rest
-}: {
+}: GetFeeData & {
 	contract: Erc20ContractAddress;
-	address: ETH_ADDRESS;
 	amount: BigNumber;
 	network: Network | undefined;
 }): Promise<BigNumber> => {
 	try {
-		const fn = isNetworkICP(network ?? ETHEREUM_NETWORK) ? getBurnFeeData : getFeeData;
+		const fn = isNetworkICP(network ?? ETHEREUM_NETWORK) ? getBurnFeeData : getErc20FeeDataProvider;
 		return await fn(rest);
 	} catch (err: unknown) {
 		// We silence the error on purpose.
