062689116a4c6be1575a282c0909d1d5f7e73c67
David Dal Busco <david.dalbusco@dfinity.org>
2024-01-17 14:47:42 +0100

    feat: hardcode eth to ckETH gas estimation to 50k (#625)

--- src/frontend/src/eth/services/fee.services.ts
+++ src/frontend/src/eth/services/fee.services.ts
@@ -1,40 +1,23 @@
+import { CKETH_FEE } from '$eth/constants/cketh.constants';
 import { ERC20_FALLBACK_FEE } from '$eth/constants/erc20.constants';
 import { ETH_BASE_FEE } from '$eth/constants/eth.constants';
-import { getFeeData as getCkEthFeeData } from '$eth/providers/infura-cketh.providers';
 import { getFeeData as getBurnFeeData } from '$eth/providers/infura-erc20-icp.providers';
 import { getFeeData as getErc20FeeDataProvider } from '$eth/providers/infura-erc20.providers';
 import { CKETH_HELPER_CONTRACT } from '$eth/types/cketh';
 import type { Erc20ContractAddress } from '$eth/types/erc20';
 import { ETHEREUM_NETWORK } from '$lib/constants/networks.constants';
 import type { ETH_ADDRESS } from '$lib/types/address';
-import type { OptionIdentity } from '$lib/types/identity';
 import type { Network } from '$lib/types/network';
 import { isNetworkICP } from '$lib/utils/network.utils';
-import { encodePrincipalToEthAddress } from '@dfinity/cketh';
-import { assertNonNullish } from '@dfinity/utils';
 import { BigNumber } from '@ethersproject/bignumber';
 
 export interface GetFeeData {
 	address: ETH_ADDRESS;
 }
 
-export const getEthFeeData = async ({
-	address,
-	identity,
-	ckEthContract
-}: GetFeeData & {
-	identity: OptionIdentity;
-	ckEthContract: Erc20ContractAddress;
-}): Promise<BigNumber> => {
+export const getEthFeeData = async ({ address }: GetFeeData): Promise<BigNumber> => {
 	if (address.toLowerCase() === CKETH_HELPER_CONTRACT.toLowerCase()) {
-		assertNonNullish(identity, 'No identity provided to calculate the fee for its principal.');
-
-		const depositEstimateGas = await getCkEthFeeData({
-			address: encodePrincipalToEthAddress(identity.getPrincipal()),
-			contract: ckEthContract
-		});
-
-		return depositEstimateGas.add(ETH_BASE_FEE);
+		return BigNumber.from(CKETH_FEE);
 	}
 
 	return BigNumber.from(ETH_BASE_FEE);
