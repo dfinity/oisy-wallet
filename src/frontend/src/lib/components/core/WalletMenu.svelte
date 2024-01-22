81a04f92260c4092a9568818e3842140d35f472c
David Dal Busco <david.dalbusco@dfinity.org>
2024-01-22 17:50:23 +0100

    feat: user menu wallet address (#646)

--- src/frontend/src/lib/components/core/WalletMenu.svelte
+++ src/frontend/src/lib/components/core/WalletMenu.svelte
@@ -6,12 +6,11 @@
 	import { OISY_REPO_URL } from '$lib/constants/oisy.constants';
 	import IconWallet from '$lib/components/icons/IconWallet.svelte';
 	import IconChevronDown from '$lib/components/icons/IconChevronDown.svelte';
-	import Copy from '$lib/components/ui/Copy.svelte';
-	import { shortenWithMiddleEllipsis } from '$lib/utils/format.utils';
 	import IconExternalLink from '$lib/components/icons/IconExternalLink.svelte';
-	import { networkAddress, networkICP, networkId } from '$lib/derived/network.derived';
+	import { networkICP, networkId } from '$lib/derived/network.derived';
 	import { networkParam } from '$lib/utils/nav.utils';
-	import { nonNullish } from '@dfinity/utils';
+	import EthWalletAddress from '$eth/components/core/EthWalletAddress.svelte';
+	import IcWalletAddress from '$icp/components/core/IcWalletAddress.svelte';
 
 	let visible = false;
 	let button: HTMLButtonElement | undefined;
@@ -20,11 +19,6 @@
 		visible = false;
 		await goto(`/settings?${networkParam($networkId)}`);
 	};
-
-	const ETHERSCAN_URL = import.meta.env.VITE_ETHERSCAN_EXPLORER_URL;
-
-	let explorerUrl: string | undefined;
-	$: explorerUrl = $networkICP ? undefined : `${ETHERSCAN_URL}/address/${$networkAddress ?? ''}`;
 </script>
 
 <button
@@ -38,25 +32,10 @@
 
 <Popover bind:visible anchor={button} direction="rtl">
 	<div class="flex flex-col gap-4">
-		<div>
-			<output class="break-all">{shortenWithMiddleEllipsis($networkAddress ?? '')}</output><Copy
-				inline
-				value={$networkAddress ?? ''}
-				text="Address copied to clipboard."
-			/>
-		</div>
-
-		{#if nonNullish(explorerUrl)}
-			<a
-				href={explorerUrl}
-				rel="external noopener noreferrer"
-				target="_blank"
-				class="flex gap-2 items-center no-underline"
-				aria-label="Open your address on Etherscan"
-			>
-				<span class="ml-1"><IconExternalLink /></span>
-				View on explorer
-			</a>
+		{#if $networkICP}
+			<IcWalletAddress />
+		{:else}
+			<EthWalletAddress />
 		{/if}
 
 		<Hr />
