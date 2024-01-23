2b22fc5ddb6e8cd29ebbd7de4f93646749b22a12
David Dal Busco <david.dalbusco@outlook.com>
2024-01-23 07:19:22 +0100

    feat: add call to action for ETH to ckETH

--- src/frontend/src/lib/components/hero/Actions.svelte
+++ src/frontend/src/lib/components/hero/Actions.svelte
@@ -1,14 +1,28 @@
 <script lang="ts">
 	import Send from '$lib/components/send/Send.svelte';
 	import Receive from '$lib/components/receive/Receive.svelte';
+	import ConvertETHToCkETH from '$eth/components/convert/ConvertETHToCkETH.svelte';
 
 	export let send = false;
+	export let convertEth = false;
+
+	let singleAction = true;
+	$: singleAction = !send && !convertEth;
 </script>
 
-<div role="toolbar" class="flex gap-4 text-deep-violet font-bold py-6">
+<div
+	role="toolbar"
+	class="flex grid-cols-2 gap-4 text-deep-violet font-bold pt-10 pb-7"
+	class:flex={singleAction}
+	class:grid={!singleAction}
+>
 	<Receive />
 
 	{#if send}
 		<Send />
+	{/if}
+
+	{#if convertEth}
+		<ConvertETHToCkETH />
 	{/if}
 </div>
