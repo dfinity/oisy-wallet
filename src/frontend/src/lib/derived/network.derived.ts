9efcc9228b73698880e9f5f22dc447ac5ef15972
David Dal Busco <david.dalbusco@dfinity.org>
2024-01-23 14:23:08 +0100

    feat: remove network address derived (#661)

--- src/frontend/src/lib/derived/network.derived.ts
+++ src/frontend/src/lib/derived/network.derived.ts
@@ -1,9 +1,6 @@
-import { icpAccountIdentifierStore } from '$icp/derived/ic.derived';
 import { ETHEREUM_NETWORK, ETHEREUM_NETWORK_ID, NETWORKS } from '$lib/constants/networks.constants';
-import { address } from '$lib/derived/address.derived';
 import { routeNetwork } from '$lib/derived/nav.derived';
 import { tokens } from '$lib/derived/tokens.derived';
-import type { OptionAddress } from '$lib/types/address';
 import type { Network, NetworkId } from '$lib/types/network';
 import type { Token } from '$lib/types/token';
 import { isNetworkIdICP } from '$lib/utils/network.utils';
@@ -28,12 +25,6 @@ export const networkICP: Readable<boolean> = derived([networkId], ([$networkId])
 export const networkEthereum: Readable<boolean> = derived(
 	[networkId],
 	([$networkId]) => ETHEREUM_NETWORK_ID === $networkId
-);
-
-export const networkAddress: Readable<OptionAddress | string> = derived(
-	[address, icpAccountIdentifierStore, networkICP],
-	([$address, $icpAccountIdentifiedStore, $networkICP]) =>
-		$networkICP ? $icpAccountIdentifiedStore?.toHex() : $address
 );
 
 export const selectedNetwork: Readable<Network> = derived(
