013c1b15e6073d74cd648d5cd34a405933bbafcd
David Dal Busco <david.dalbusco@dfinity.org>
2024-01-24 13:33:00 +0100

    feat: load btc address (#682)

--- src/frontend/src/icp/stores/ckbtc.store.ts
+++ src/frontend/src/icp/stores/ckbtc.store.ts
@@ -3,5 +3,7 @@ import type { CertifiedData } from '$lib/types/store';
 import type { MinterInfo } from '@dfinity/ckbtc';
 
 export type CkBtcMinterInfoData = CertifiedData<MinterInfo>;
-
 export const ckBtcMinterInfoStore = initCertifiedSetterStore<CkBtcMinterInfoData>();
+
+export type BtcAddressData = CertifiedData<string>;
+export const btcAddressStore = initCertifiedSetterStore<BtcAddressData>();
