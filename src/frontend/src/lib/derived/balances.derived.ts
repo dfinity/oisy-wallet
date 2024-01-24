01874a5afb7d59e9e674572d38021d80018f3e83
David Dal Busco <david.dalbusco@dfinity.org>
2024-01-24 11:40:47 +0100

    feat: express reset value with null (#679)

--- src/frontend/src/lib/derived/balances.derived.ts
+++ src/frontend/src/lib/derived/balances.derived.ts
@@ -1,6 +1,6 @@
 import { tokenId } from '$lib/derived/token.derived';
 import { balancesStore } from '$lib/stores/balances.store';
-import { isNullish, nonNullish } from '@dfinity/utils';
+import { nonNullish } from '@dfinity/utils';
 import type { BigNumber } from '@ethersproject/bignumber';
 import { derived, type Readable } from 'svelte/store';
 
@@ -9,18 +9,10 @@ export const balance: Readable<BigNumber | undefined | null> = derived(
 	([$balanceStore, $tokenId]) => $balanceStore?.[$tokenId]?.data
 );
 
-export const balanceEmpty: Readable<boolean> = derived(
-	[balancesStore, tokenId],
-	([$balanceStore, $tokenId]) =>
-		isNullish($balanceStore) ||
-		isNullish($balanceStore?.[$tokenId]) ||
-		$balanceStore[$tokenId].data.isZero()
-);
-
 export const balanceZero: Readable<boolean> = derived(
 	[balancesStore, tokenId],
 	([$balanceStore, $tokenId]) =>
 		nonNullish($balanceStore) &&
 		nonNullish($balanceStore?.[$tokenId]) &&
-		$balanceStore[$tokenId].data.isZero()
+		$balanceStore[$tokenId]?.data.isZero() === true
 );
