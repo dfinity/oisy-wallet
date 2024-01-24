eca2f3df5422187ee113262b9fe8eaeef0215c42
David Dal Busco <david.dalbusco@dfinity.org>
2024-01-24 16:37:43 +0100

    feat: use icrc address as source for ic (#690)

--- src/frontend/src/icp/derived/ic.derived.ts
+++ src/frontend/src/icp/derived/ic.derived.ts
@@ -1,6 +1,5 @@
 import { getAccountIdentifier } from '$icp/utils/icp-account.utils';
 import { getIcrcAccount } from '$icp/utils/icrc-account.utils';
-import { tokenStandard } from '$lib/derived/token.derived';
 import { authStore } from '$lib/stores/auth.store';
 import type { AccountIdentifier } from '@dfinity/ledger-icp';
 import { encodeIcrcAccount, type IcrcAccount } from '@dfinity/ledger-icrc';
@@ -26,10 +25,4 @@ export const icrcAccountIdentifierText: Readable<string | undefined> = derived(
 	icrcAccount,
 	($icrcAccountStore) =>
 		nonNullish($icrcAccountStore) ? encodeIcrcAccount($icrcAccountStore) : undefined
-);
-
-export const icAccountIdentifierText: Readable<string | undefined> = derived(
-	[tokenStandard, icpAccountIdentifier, icrcAccountIdentifierText],
-	([$tokenStandard, $icpAccountIdentifierStore, $icrcAccountIdentifierStore]) =>
-		$tokenStandard === 'icrc' ? $icrcAccountIdentifierStore : $icpAccountIdentifierStore?.toHex()
 );
