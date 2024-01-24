9e3d48035963f908d6206d16a6064c61bd05176b
David Dal Busco <david.dalbusco@dfinity.org>
2024-01-24 11:01:22 +0100

    feat: close info box (#677)
    * feat: close info box
    
    * feat: hide bitcoin info

---
+++ src/frontend/src/icp/utils/ck.utils.ts
@@ -0,0 +1,23 @@
+import { browser } from '$app/environment';
+
+export const saveHideBitcoinInfo = () => {
+	try {
+		localStorage.setItem('hide_bitcoin_info', 'true');
+	} catch (err: unknown) {
+		// We use the local storage for the operational part of the app but, not crucial
+		console.error(err);
+	}
+};
+
+export const shouldHideBitcoinInfo = (): boolean => {
+	try {
+		const { hide_bitcoin_info }: Storage = browser
+			? localStorage
+			: ({ hide_bitcoin_info: 'false' } as unknown as Storage);
+		return hide_bitcoin_info === 'true';
+	} catch (err: unknown) {
+		// We use the local storage for the operational part of the app but, not crucial
+		console.error(err);
+		return false;
+	}
+};
