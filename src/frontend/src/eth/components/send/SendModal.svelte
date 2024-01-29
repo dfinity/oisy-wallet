2913419101586ff40b26918d3f1007cf1d040cf5
David Dal Busco <david.dalbusco@dfinity.org>
2024-01-29 13:14:03 +0100

    fix: use send context for wallet connect as well (#700)

--- src/frontend/src/eth/components/send/SendModal.svelte
+++ src/frontend/src/eth/components/send/SendModal.svelte
@@ -1,20 +1,8 @@
 <script lang="ts">
-	import { initSendContext, SEND_CONTEXT_KEY, type SendContext } from '$eth/stores/send.store';
-	import { setContext } from 'svelte';
-	import SendEthModal from '$eth/components/send/SendEthModal.svelte';
-	import { token } from '$lib/derived/token.derived';
-
-	/**
-	 * Send modal context store
-	 */
-	const { sendToken, ...rest } = initSendContext({ sendPurpose: 'send' });
-
-	setContext<SendContext>(SEND_CONTEXT_KEY, {
-		sendToken,
-		...rest
-	});
-
-	$: sendToken.set($token);
+	import SendTokenModal from '$eth/components/send/SendTokenModal.svelte';
+	import SendTokenContext from '$eth/components/send/SendTokenContext.svelte';
 </script>
 
-<SendEthModal />
+<SendTokenContext>
+	<SendTokenModal />
+</SendTokenContext>
---
+++ src/frontend/src/eth/components/send/SendTokenContext.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { initSendContext, SEND_CONTEXT_KEY, type SendContext } from '$eth/stores/send.store';
+	import { setContext } from 'svelte';
+	import { token } from '$lib/derived/token.derived';
+
+	/**
+	 * Send modal context store
+	 */
+	const { sendToken, ...rest } = initSendContext({ sendPurpose: 'send' });
+
+	setContext<SendContext>(SEND_CONTEXT_KEY, {
+		sendToken,
+		...rest
+	});
+
+	$: sendToken.set($token);
+</script>
+
+<slot />
