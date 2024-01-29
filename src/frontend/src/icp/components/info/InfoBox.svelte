feb6a056b5be0116065c90d1a4bbf1f87d673ad8
David Dal Busco <david.dalbusco@dfinity.org>
2024-01-29 14:21:20 +0100

    feat: ckETH info (#703)

---
+++ src/frontend/src/icp/components/info/InfoBox.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { IconClose } from '@dfinity/gix-components';
+	import { slide } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+
+	export let hideInfo: boolean;
+</script>
+
+{#if !hideInfo}
+	<div
+		class="border-2 border-dust bg-white rounded-lg mb-12 py-4 px-6 relative"
+		transition:slide={{ easing: quintOut, axis: 'y' }}
+	>
+		<button class="text absolute top-2 right-2" on:click><IconClose size="24px" /></button>
+
+		<slot />
+	</div>
+{/if}
--- src/frontend/src/icp/utils/ck.utils.ts
+++ src/frontend/src/icp/utils/ck.utils.ts
@@ -1,20 +1,20 @@
 import { browser } from '$app/environment';
 
-export const saveHideBitcoinInfo = () => {
+export type HideInfoKey = 'oisy_ic_hide_bitcoin_info' | 'oisy_ic_hide_ethereum_info';
+
+export const saveHideInfo = (key: HideInfoKey) => {
 	try {
-		localStorage.setItem('hide_bitcoin_info', 'true');
+		localStorage.setItem(key, 'true');
 	} catch (err: unknown) {
 		// We use the local storage for the operational part of the app but, not crucial
 		console.error(err);
 	}
 };
 
-export const shouldHideBitcoinInfo = (): boolean => {
+export const shouldHideInfo = (key: HideInfoKey): boolean => {
 	try {
-		const { hide_bitcoin_info }: Storage = browser
-			? localStorage
-			: ({ hide_bitcoin_info: 'false' } as unknown as Storage);
-		return hide_bitcoin_info === 'true';
+		const store: Storage = browser ? localStorage : ({ [key]: 'false' } as unknown as Storage);
+		return store[key] === 'true';
 	} catch (err: unknown) {
 		// We use the local storage for the operational part of the app but, not crucial
 		console.error(err);
