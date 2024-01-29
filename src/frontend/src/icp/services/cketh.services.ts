7efb6a027bb21064f8f1db0ca301974e66e32295
David Dal Busco <david.dalbusco@outlook.com>
2024-01-29 15:43:02 +0100

    feat: load eip 1559 price

---
+++ src/frontend/src/icp/services/cketh.services.ts
@@ -0,0 +1,11 @@
+import { eip1559TransactionPrice } from '$icp/api/cketh-minter.api';
+import { loadCkData } from '$icp/services/ck.services';
+import { eip1559TransactionPriceStore } from '$icp/stores/cketh.store';
+import type { IcCkCanisters, IcToken } from '$icp/types/ic';
+
+export const loadEip1559TransactionPrice = async (params: IcToken & Partial<IcCkCanisters>) =>
+	loadCkData({
+		...params,
+		store: eip1559TransactionPriceStore,
+		request: (params) => eip1559TransactionPrice(params)
+	});
