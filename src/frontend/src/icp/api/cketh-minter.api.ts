b8c4fa8e229e48e6918ceab71bc645f6bd8d55ce
David Dal Busco <david.dalbusco@dfinity.org>
2024-01-29 15:44:02 +0100

    feat: load eip 1559 price (#706)

--- src/frontend/src/icp/api/cketh-minter.api.ts
+++ src/frontend/src/icp/api/cketh-minter.api.ts
@@ -1,7 +1,10 @@
 import { ckEthMinterCanister } from '$icp-eth/api/cketh-minter.api';
 import type { CanisterIdText } from '$lib/types/canister';
 import type { OptionIdentity } from '$lib/types/identity';
-import type { RetrieveEthRequest } from '@dfinity/cketh/dist/candid/minter';
+import type {
+	Eip1559TransactionPrice,
+	RetrieveEthRequest
+} from '@dfinity/cketh/dist/candid/minter';
 import { assertNonNullish } from '@dfinity/utils';
 
 export const withdrawEth = async ({
@@ -19,4 +22,18 @@ export const withdrawEth = async ({
 	const { withdrawEth } = await ckEthMinterCanister({ identity, minterCanisterId });
 
 	return withdrawEth(params);
+};
+
+export const eip1559TransactionPrice = async ({
+	identity,
+	minterCanisterId
+}: {
+	identity: OptionIdentity;
+	minterCanisterId: CanisterIdText;
+}): Promise<Eip1559TransactionPrice> => {
+	assertNonNullish(identity, 'No internet identity.');
+
+	const { eip1559TransactionPrice } = await ckEthMinterCanister({ identity, minterCanisterId });
+
+	return eip1559TransactionPrice();
 };
