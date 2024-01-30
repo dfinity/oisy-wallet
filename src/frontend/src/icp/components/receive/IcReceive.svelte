9a7e8a987c4fbca7ca25e6e5aef4dc6cf3a84503
David Dal Busco <david.dalbusco@outlook.com>
2024-01-30 11:58:45 +0100

    feat: receive ETH modal (design & ux todo)

--- src/frontend/src/icp/components/receive/IcReceive.svelte
+++ src/frontend/src/icp/components/receive/IcReceive.svelte
@@ -3,17 +3,22 @@
 	import { modalStore } from '$lib/stores/modal.store';
 	import { isBusy } from '$lib/derived/busy.derived';
 	import { token, tokenStandard } from '$lib/derived/token.derived';
-	import { modalCkBTCReceive, modalIcpReceive, modalReceive } from '$lib/derived/modal.derived';
+	import {
+		modalCkBTCReceive,
+		modalCkETHReceive,
+		modalIcpReceive,
+		modalReceive
+	} from '$lib/derived/modal.derived';
 	import IcReceiveModal from '$icp/components/receive/IcReceiveModal.svelte';
-	import ReceiveModal from '$lib/components/receive/ReceiveModal.svelte';
 	import { isRouteTokens } from '$lib/utils/nav.utils';
 	import { page } from '$app/stores';
-	import { tokenCkBtcLedger } from '$icp/derived/ic-token.derived';
+	import { tokenCkBtcLedger, tokenCkEthLedger } from '$icp/derived/ic-token.derived';
 	import { loadBtcAddress, loadCkBtcMinterInfo } from '$icp/services/ckbtc.services';
 	import type { IcToken } from '$icp/types/ic';
 	import { authStore } from '$lib/stores/auth.store';
 	import IcReceiveInfo from '$icp/components/receive/IcReceiveInfo.svelte';
 	import IcReceiveInfoCkBTC from '$icp/components/receive/IcReceiveInfoCkBTC.svelte';
+	import IcReceiveInfoCkETH from "$icp/components/receive/IcReceiveInfoCkETH.svelte";
 
 	const openReceive = async () => {
 		if ($tokenStandard === 'icp' || isRouteTokens($page)) {
@@ -31,6 +36,11 @@
 			]);
 
 			modalStore.openCkBTCReceive();
+			return;
+		}
+
+		if ($tokenCkEthLedger) {
+			modalStore.openCkETHReceive();
 			return;
 		}
 
@@ -52,6 +62,6 @@
 	<IcReceiveModal infoCmp={IcReceiveInfo} />
 {:else if $modalCkBTCReceive}
 	<IcReceiveModal infoCmp={IcReceiveInfoCkBTC} />
-{:else if $modalReceive}
-	<ReceiveModal />
+{:else if $modalCkETHReceive}
+	<IcReceiveModal infoCmp={IcReceiveInfoCkETH} />
 {/if}
