3541bce1e111517c451ca921bc535623891be053
David Dal Busco <david.dalbusco@outlook.com>
2024-02-01 13:10:51 +0100

    feat: sort icrc tokens

--- src/frontend/src/lib/derived/tokens.derived.ts
+++ src/frontend/src/lib/derived/tokens.derived.ts
@@ -4,7 +4,11 @@ import { ETHEREUM_TOKEN, ICP_TOKEN } from '$lib/constants/tokens.constants';
 import type { Token } from '$lib/types/token';
 import { derived, type Readable } from 'svelte/store';
 
+export const sortedIcrcTokens: Readable<Token[]> = derived([icrcTokens], ([$icrcTokens]) =>
+	$icrcTokens.sort()
+);
+
 export const tokens: Readable<Token[]> = derived(
-	[erc20Tokens, icrcTokens],
+	[erc20Tokens, sortedIcrcTokens],
 	([$erc20Tokens, $icrcTokens]) => [ICP_TOKEN, ...$icrcTokens, ETHEREUM_TOKEN, ...$erc20Tokens]
 );
