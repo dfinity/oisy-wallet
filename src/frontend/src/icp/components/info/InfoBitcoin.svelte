97d82661e63d0374886a7ccbb7693700cb665d5f
David Dal Busco <david.dalbusco@outlook.com>
2024-02-06 11:21:45 +0100

    fix: Receive from Bitcoin Network load modal

--- src/frontend/src/icp/components/info/InfoBitcoin.svelte
+++ src/frontend/src/icp/components/info/InfoBitcoin.svelte
@@ -3,8 +3,9 @@
 	import Logo from '$lib/components/ui/Logo.svelte';
 	import { modalStore } from '$lib/stores/modal.store';
 	import { isBusy } from '$lib/derived/busy.derived';
+	import { emit } from '$lib/utils/events.utils';
 
-	const openReceive = () => modalStore.openCkBTCReceive();
+	const openReceive = () => emit({ message: 'oisyReceiveCkBTC' });
 </script>
 
 <div class="pr-2">
