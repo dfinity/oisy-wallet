e9164c86a1071870fda8272b7dd7e773149d6aa2
David Dal Busco <david.dalbusco@outlook.com>
2024-02-06 12:28:09 +0100

    fix: load ckBTC minter info on convert to BTC instead of send

--- src/frontend/src/icp/components/convert/ConvertToBTC.svelte
+++ src/frontend/src/icp/components/convert/ConvertToBTC.svelte
@@ -5,13 +5,25 @@
 	import { BTC_NETWORK_ID } from '$icp/constants/ckbtc.constants';
 	import { modalStore } from '$lib/stores/modal.store';
 	import IconImportExport from '$lib/components/icons/IconImportExport.svelte';
+	import { tokenCkBtcLedger } from '$icp/derived/ic-token.derived';
+	import { loadCkBtcMinterInfo } from '$icp/services/ckbtc.services';
+	import { token } from '$lib/derived/token.derived';
+	import type { IcToken } from '$icp/types/ic';
+
+	const openSend = async () => {
+		if ($tokenCkBtcLedger) {
+			await loadCkBtcMinterInfo({ params: $token as IcToken });
+		}
+
+		modalStore.openConvertCkBTCToBTC();
+	};
 </script>
 
 <button
 	class="hero col-span-2"
 	disabled={$isBusy}
 	class:opacity-50={$isBusy}
-	on:click={() => modalStore.openConvertCkBTCToBTC()}
+	on:click={async () => await openSend()}
 >
 	<IconImportExport size="28" />
 	Convert to BTC
