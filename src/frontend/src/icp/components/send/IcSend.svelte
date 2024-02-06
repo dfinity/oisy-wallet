20ed8fdfbb66108b31084b617b2ec13a2778dd7c
David Dal Busco <david.dalbusco@dfinity.org>
2024-02-06 12:29:11 +0100

    fix: load ckBTC minter info on convert to BTC instead of send (#763)

--- src/frontend/src/icp/components/send/IcSend.svelte
+++ src/frontend/src/icp/components/send/IcSend.svelte
@@ -4,27 +4,10 @@
 	import { isBusy } from '$lib/derived/busy.derived';
 	import { modalIcSend } from '$lib/derived/modal.derived';
 	import IcSendModal from '$icp/components/send/IcSendModal.svelte';
-	import { loadCkBtcMinterInfo } from '$icp/services/ckbtc.services';
-	import { tokenCkBtcLedger } from '$icp/derived/ic-token.derived';
-	import { token } from '$lib/derived/token.derived';
-	import type { IcToken } from '$icp/types/ic';
 	import { ICP_NETWORK_ID } from '$lib/constants/networks.constants';
-
-	const openSend = async () => {
-		if ($tokenCkBtcLedger) {
-			await loadCkBtcMinterInfo({ params: $token as IcToken });
-		}
-
-		modalStore.openIcSend();
-	};
 </script>
 
-<button
-	class="hero"
-	on:click={async () => await openSend()}
-	disabled={$isBusy}
-	class:opacity-50={$isBusy}
->
+<button class="hero" on:click={modalStore.openIcSend} disabled={$isBusy} class:opacity-50={$isBusy}>
 	<IconSend size="28" />
 	<span>Send</span></button
 >
