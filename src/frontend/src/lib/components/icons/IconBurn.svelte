02498969ef298382773abee35bdd4a7974b504e4
David Dal Busco <david.dalbusco@dfinity.org>
2024-02-06 14:50:11 +0100

    feat: mint icon direction (#770)

---
+++ src/frontend/src/lib/components/icons/IconBurn.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import IconConvert from '$lib/components/icons/IconConvert.svelte';
+
+	export let styleClass: string | undefined = undefined;
+	export let size = '24';
+</script>
+
+<IconConvert {styleClass} {size} />
