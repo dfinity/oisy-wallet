a1d51bc8962428827b211ffe37e7ce1c9cf4c61f
David Dal Busco <david.dalbusco@dfinity.org>
2024-03-05 12:53:35 +0100

    feat: use ckETH minter info for withdrawal min amount (#888)
    * feat: use ckETH minter info for withdrawal min amount
    
    * chore: format

--- src/frontend/src/icp/utils/cketh.utils.ts
+++ src/frontend/src/icp/utils/cketh.utils.ts
@@ -1,16 +1,19 @@
-import { CKETH_MIN_WITHDRAWAL_AMOUNT } from '$icp/constants/cketh.constants';
+import type { CkEthMinterInfoData } from '$icp/stores/cketh.store';
 import { IcAmountAssertionError } from '$icp/types/ic-send';
 import { formatToken } from '$lib/utils/format.utils';
+import { fromNullable, isNullish } from '@dfinity/utils';
 import { BigNumber } from '@ethersproject/bignumber';
 
 export const assertCkETHUserInputAmount = ({
 	amount,
 	tokenDecimals,
-	tokenSymbol
+	tokenSymbol,
+	minterInfo
 }: {
 	amount: BigNumber;
 	tokenDecimals: number;
 	tokenSymbol: string;
+	minterInfo: CkEthMinterInfoData | undefined | null;
 }): IcAmountAssertionError | undefined => {
 	// We skip validation checks here for zero because it makes the UI/UX ungraceful.
 	// e.g. user enters 0. and an error gets displayed.
@@ -18,13 +21,33 @@ export const assertCkETHUserInputAmount = ({
 		return undefined;
 	}
 
-	if ((amount?.toBigInt() ?? 0n) < CKETH_MIN_WITHDRAWAL_AMOUNT) {
+	if (isNullish(minterInfo)) {
+		return new IcAmountAssertionError(
+			'The minimum amount of ckETH required for converting to ETH is unknown.'
+		);
+	}
+
+	const {
+		data: { minimum_withdrawal_amount },
+		certified: infoCertified
+	} = minterInfo;
+
+	// The `minimum_withdrawal_amount` is optional in the minter info because the team decided to make all fields optional for maintainability reasons. That's why we assume that it is most likely set.
+	const minWithdrawalAmount = fromNullable(minimum_withdrawal_amount) ?? 0n;
+
+	if ((amount?.toBigInt() ?? 0n) < minWithdrawalAmount) {
 		return new IcAmountAssertionError(
 			`The amount falls below the minimum withdrawal amount of ${formatToken({
-				value: BigNumber.from(CKETH_MIN_WITHDRAWAL_AMOUNT),
+				value: BigNumber.from(minWithdrawalAmount),
 				unitName: tokenDecimals,
 				displayDecimals: tokenDecimals
 			})} ${tokenSymbol}.`
+		);
+	}
+
+	if (!infoCertified) {
+		return new IcAmountAssertionError(
+			'Please wait until the ckETH parameters have been certified.'
 		);
 	}
 
