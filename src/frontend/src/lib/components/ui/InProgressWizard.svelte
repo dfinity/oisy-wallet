a6977e4f35aa6337fa27352cd6d453415aef4ef7
David Dal Busco <david.dalbusco@outlook.com>
2024-03-05 13:44:21 +0100

    fix: unmounting beforeunload

--- src/frontend/src/lib/components/ui/InProgressWizard.svelte
+++ src/frontend/src/lib/components/ui/InProgressWizard.svelte
@@ -5,12 +5,25 @@
 	import { onDestroy, onMount } from 'svelte';
 	import { confirmToCloseBrowser } from '$lib/utils/before-unload.utils';
 	import Warning from '$lib/components/ui/Warning.svelte';
+	import { modalStore } from '$lib/stores/modal.store';
+	import { nonNullish } from '@dfinity/utils';
 
 	export let progressStep: string = SendStep.INITIALIZATION;
 	export let steps: [ProgressStep, ...ProgressStep[]];
 
 	onMount(() => confirmToCloseBrowser(true));
 	onDestroy(() => confirmToCloseBrowser(false));
+
+	// Workaround: SvelteKit does not consistently call `onDestroy`. Various issues are open regarding this on Svelte side.
+	// This is the simplest, least verbose solution to always disconnect before unload, given that this component is used in `<WizardModal />` only.
+	$: $modalStore,
+		(() => {
+			if (nonNullish($modalStore)) {
+				return;
+			}
+
+			confirmToCloseBrowser(false);
+		})();
 </script>
 
 <Warning>
