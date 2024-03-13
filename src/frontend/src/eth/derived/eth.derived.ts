bb319eeff893c859c3919ece98185d1809c6fe8a
David Dal Busco <david.dalbusco@outlook.com>
2024-03-13 06:43:58 +0100

    chore: use constant

--- src/frontend/src/eth/derived/eth.derived.ts
+++ src/frontend/src/eth/derived/eth.derived.ts
@@ -2,11 +2,12 @@ import { selectedEthereumNetwork } from '$eth/derived/network.derived';
 import { ETHEREUM_TOKENS } from '$icp-eth/constants/tokens.constants';
 import type { Token, TokenId } from '$lib/types/token';
 import { derived, type Readable } from 'svelte/store';
+import {DEFAULT_ETHEREUM_TOKEN} from "$lib/constants/tokens.constants";
 
 export const ethToken: Readable<Required<Token>> = derived(
 	[selectedEthereumNetwork],
 	([{ id }]) =>
-		ETHEREUM_TOKENS.find(({ network: { id: networkId } }) => id === networkId) ?? ETHEREUM_TOKENS[0]
+		ETHEREUM_TOKENS.find(({ network: { id: networkId } }) => id === networkId) ?? DEFAULT_ETHEREUM_TOKEN
 );
 
 export const ethTokenId: Readable<TokenId> = derived([ethToken], ([{ id }]) => id);
