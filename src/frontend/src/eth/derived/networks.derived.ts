d19c487f8d9de665eb0baf266177aabe102ad7b2
David Dal Busco <david.dalbusco@outlook.com>
2024-03-14 09:44:53 +0100

    feat: ck testnets

--- src/frontend/src/eth/derived/networks.derived.ts
+++ src/frontend/src/eth/derived/networks.derived.ts
@@ -1,18 +1,14 @@
 import { ETHEREUM_NETWORK, SEPOLIA_NETWORK } from '$env/networks.env';
 import { ETH_MAINNET_ENABLED } from '$env/networks.eth.env';
 import type { EthereumNetwork } from '$eth/types/network';
-import { LOCAL } from '$lib/constants/app.constants';
-import { testnetsStore } from '$lib/stores/testnets.store';
+import { testnets } from '$lib/derived/testnets.derived';
 import type { NetworkId } from '$lib/types/network';
 import { derived, type Readable } from 'svelte/store';
 
-export const ethereumNetworks: Readable<EthereumNetwork[]> = derived(
-	[testnetsStore],
-	([$testnetsStore]) => [
-		...(ETH_MAINNET_ENABLED ? [ETHEREUM_NETWORK] : []),
-		...($testnetsStore?.enabled ?? LOCAL ? [SEPOLIA_NETWORK] : [])
-	]
-);
+export const ethereumNetworks: Readable<EthereumNetwork[]> = derived([testnets], ([$testnets]) => [
+	...(ETH_MAINNET_ENABLED ? [ETHEREUM_NETWORK] : []),
+	...($testnets ? [SEPOLIA_NETWORK] : [])
+]);
 
 export const ethereumNetworksIds: Readable<NetworkId[]> = derived(
 	[ethereumNetworks],
--- src/frontend/src/lib/derived/tokens.derived.ts
+++ src/frontend/src/lib/derived/tokens.derived.ts
@@ -1,23 +1,9 @@
 import { ICP_TOKEN } from '$env/tokens.env';
 import { erc20Tokens } from '$eth/derived/erc20.derived';
 import { ethereumTokens } from '$eth/derived/tokens.derived';
-import { icrcTokens } from '$icp/derived/icrc.derived';
+import { sortedIcrcTokens } from '$icp/derived/icrc.derived';
 import type { Token } from '$lib/types/token';
 import { derived, type Readable } from 'svelte/store';
-
-export const sortedIcrcTokens: Readable<Token[]> = derived([icrcTokens], ([$icrcTokens]) =>
-	$icrcTokens.sort(
-		(
-			{ name: nameA, position: positionA, exchangeCoinId: exchangeCoinIdA },
-			{ name: nameB, position: positionB, exchangeCoinId: exchangeCoinIdB }
-		) =>
-			positionA === positionB
-				? exchangeCoinIdA === exchangeCoinIdB
-					? nameA.localeCompare(nameB)
-					: exchangeCoinIdA.localeCompare(exchangeCoinIdB)
-				: positionA - positionB
-	)
-);
 
 export const tokens: Readable<Token[]> = derived(
 	[erc20Tokens, sortedIcrcTokens, ethereumTokens],
