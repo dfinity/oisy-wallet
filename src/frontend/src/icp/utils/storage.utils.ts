f906b9ae0cff2f95ed2b56c35758c87283dfe2e4
David Dal Busco <david.dalbusco@outlook.com>
2024-03-14 10:02:06 +0100

    chore: format

--- src/frontend/src/icp/utils/storage.utils.ts
+++ src/frontend/src/icp/utils/storage.utils.ts
@@ -1,5 +1,5 @@
 import { browser } from '$app/environment';
-import {nonNullish} from "@dfinity/utils";
+import { nonNullish } from '@dfinity/utils';
 
 export const set = <T>({ key, value }: { key: string; value: T }) => {
 	try {
