bb301c8017890024b60769fb5c5d1912f76e3330
David Dal Busco <david.dalbusco@dfinity.org>
2024-03-19 09:04:43 +0100

    feat: split ckbtc update balance and statuses workers (#930)

--- src/frontend/src/icp/workers/ckbtc-update-balance.worker.ts
+++ src/frontend/src/icp/workers/ckbtc-update-balance.worker.ts
@@ -1,20 +1,17 @@
-import { BtcStatusesScheduler } from '$icp/schedulers/btc-statuses.scheduler';
+import { CkBTCUpdateBalanceScheduler } from '$icp/schedulers/ckbtc-update-balance.scheduler';
 import type { PostMessage, PostMessageDataRequestIcCk } from '$lib/types/post-message';
 
-const scheduler = new BtcStatusesScheduler();
+const scheduler = new CkBTCUpdateBalanceScheduler();
 
 onmessage = async ({ data: dataMsg }: MessageEvent<PostMessage<PostMessageDataRequestIcCk>>) => {
 	const { msg, data } = dataMsg;
 
 	switch (msg) {
-		case 'stopCkBTCWalletTimer':
+		case 'stopCkBTCUpdateBalanceTimer':
 			scheduler.stop();
 			return;
-		case 'startCkBTCWalletTimer':
+		case 'startCkBTCUpdateBalanceTimer':
 			await scheduler.start(data);
-			return;
-		case 'triggerCkBTCWalletTimer':
-			await scheduler.trigger(data);
 			return;
 	}
 };
