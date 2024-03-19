1372b538019399634e3ab81d05c9c018dbc96ef0
David Dal Busco <david.dalbusco@outlook.com>
2024-03-19 12:54:07 +0100

    fix: disable verify signature for ic-mgmt

--- src/frontend/src/icp/api/ic.api.ts
+++ src/frontend/src/icp/api/ic.api.ts
@@ -1,4 +1,4 @@
-import { getAgent } from '$lib/actors/agents.ic';
+import { createAgent } from '$lib/actors/agents.ic';
 import type { OptionIdentity } from '$lib/types/identity';
 import type { Identity } from '@dfinity/agent';
 import type { BitcoinNetwork, bitcoin_get_utxos_query_result } from '@dfinity/ic-management';
@@ -30,7 +30,10 @@ const mgmtCanister = async ({
 }: {
 	identity: Identity;
 }): Promise<ICManagementCanister> => {
-	const agent = await getAgent({ identity });
+	// We are intentionally creating a new agent because the agent-js library's current implementation is incompatible with querying bitcoin_get_utxos_query when verifyQuerySignatures is set to true.
+	// This is why we must explicitly disable the verification of the queries' signatures.
+	// TODO: The issue with agent-js was communicated to the SDK team on March 19, 2024.
+	const agent = await createAgent({ identity, verifyQuerySignatures: false });
 
 	return ICManagementCanister.create({
 		agent
