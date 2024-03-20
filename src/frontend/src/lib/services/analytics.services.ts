49af251228de1be9f1431bdd37c647042f5a07d4
David Dal Busco <david.dalbusco@outlook.com>
2024-03-20 12:28:41 +0100

    feat: no analytics if no ids provided

--- src/frontend/src/lib/services/analytics.services.ts
+++ src/frontend/src/lib/services/analytics.services.ts
@@ -1,4 +1,5 @@
 import { LOCAL } from '$lib/constants/app.constants';
+import { isNullish } from '@dfinity/utils';
 import { initOrbiter } from '@junobuild/analytics';
 
 export const initAnalytics = async () => {
@@ -8,6 +9,10 @@ export const initAnalytics = async () => {
 
 	const SATELLITE_ID = import.meta.env.VITE_JUNO_SATELLITE_ID;
 	const ORBITER_ID = import.meta.env.VITE_JUNO_ORBITER_ID;
+
+	if (isNullish(SATELLITE_ID) || isNullish(ORBITER_ID)) {
+		return;
+	}
 
 	await initOrbiter({
 		satelliteId: SATELLITE_ID,
