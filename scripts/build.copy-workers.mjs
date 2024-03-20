8c676a6e1390736e1c37468648afedb9e6b884b8
David Dal Busco <david.dalbusco@outlook.com>
2024-03-20 12:38:46 +0100

    chore: format

--- scripts/build.copy-workers.mjs
+++ scripts/build.copy-workers.mjs
@@ -1,18 +1,18 @@
-import { cp } from "node:fs";
-import { extname } from "node:path";
+import { cp } from 'node:fs';
+import { extname } from 'node:path';
 
 await cp(
-  "node_modules/@junobuild/analytics/dist/workers/",
-  "./build/workers",
-  {
-    recursive: true,
-    filter: (source, destination) => extname(source) !== ".map",
-  },
-  (err) => {
-    if (err === null) {
-      return;
-    }
+	'node_modules/@junobuild/analytics/dist/workers/',
+	'./build/workers',
+	{
+		recursive: true,
+		filter: (source, destination) => extname(source) !== '.map'
+	},
+	(err) => {
+		if (err === null) {
+			return;
+		}
 
-    console.error("Copy workers error:", err);
-  },
+		console.error('Copy workers error:', err);
+	}
 );
