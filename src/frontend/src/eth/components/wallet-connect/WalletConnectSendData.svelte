c110cb70e1807a74aa57a0d5e47f1a03ef817da0
David Dal Busco <david.dalbusco@dfinity.org>
2024-03-20 13:27:16 +0100

    feat: review data wording (#949)

--- src/frontend/src/eth/components/wallet-connect/WalletConnectSendData.svelte
+++ src/frontend/src/eth/components/wallet-connect/WalletConnectSendData.svelte
@@ -7,8 +7,11 @@
 </script>
 
 {#if nonNullish(data)}
-	<label for="data" class="font-bold px-4.5">Data:</label>
+	<label for="data" class="font-bold px-4.5">HEX Data:</label>
 	<div id="data" class="font-normal mb-4 px-4.5 flex items-center gap-1">
-		{shortenWithMiddleEllipsis(data)}<Copy value={data} text="Data copied to clipboard." />
+		{shortenWithMiddleEllipsis(data)}<Copy
+			value={data}
+			text="Raw transaction data copied to clipboard."
+		/>
 	</div>
 {/if}
