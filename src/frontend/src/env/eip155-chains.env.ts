66853e5a1a92203ed9a0cb629a347ce9881b1899
David Dal Busco <david.dalbusco@dfinity.org>
2024-03-20 08:27:25 +0100

    feat: eip155 chains support for eth mainnet enabled (#935)

--- src/frontend/src/env/eip155-chains.env.ts
+++ src/frontend/src/env/eip155-chains.env.ts
@@ -4,12 +4,15 @@ import {
 	SEPOLIA_NETWORK,
 	SEPOLIA_NETWORK_CHAIN_ID
 } from '$env/networks.env';
+import { ETH_MAINNET_ENABLED } from '$env/networks.eth.env';
 
 export const EIP155_CHAINS: Record<string, { chainId: number; name: string }> = {
-	[`eip155:${ETHEREUM_NETWORK_CHAIN_ID}`]: {
-		chainId: Number(ETHEREUM_NETWORK_CHAIN_ID),
-		name: ETHEREUM_NETWORK.name
-	},
+	...(ETH_MAINNET_ENABLED && {
+		[`eip155:${ETHEREUM_NETWORK_CHAIN_ID}`]: {
+			chainId: Number(ETHEREUM_NETWORK_CHAIN_ID),
+			name: ETHEREUM_NETWORK.name
+		}
+	}),
 	[`eip155:${SEPOLIA_NETWORK_CHAIN_ID}`]: {
 		chainId: Number(SEPOLIA_NETWORK_CHAIN_ID),
 		name: SEPOLIA_NETWORK.name
