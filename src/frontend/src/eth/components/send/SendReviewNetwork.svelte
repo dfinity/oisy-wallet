a9c4266db5d37ec27791d4ff4d18f91e26f1b3a2
David Dal Busco <david.dalbusco@outlook.com>
2024-03-25 10:51:17 +0100

    feat: display logo for erc20 icp network too

--- src/frontend/src/eth/components/send/SendReviewNetwork.svelte
+++ src/frontend/src/eth/components/send/SendReviewNetwork.svelte
@@ -6,6 +6,7 @@
 	import type { EthereumNetwork } from '$eth/types/network';
 	import eth from '$icp-eth/assets/eth.svg';
 	import Logo from '$lib/components/ui/Logo.svelte';
+	import icpDark from '$eth/assets/icp_dark.svg';
 
 	export let sourceNetwork: EthereumNetwork;
 	export let targetNetwork: Network | undefined = undefined;
@@ -15,7 +16,7 @@
 	<svelte:fragment slot="label">Network</svelte:fragment>
 	<span class="flex gap-1">
 		{#if nonNullish(targetNetwork) && isNetworkICP(targetNetwork)}
-			Convert to native ICP
+			Convert to native ICP <Logo src={icpDark} size="20px" alt={`Internet computer logo`} />
 		{:else}
 			{sourceNetwork.name}
 			<Logo src={sourceNetwork.icon ?? eth} size="20px" alt={`${sourceNetwork.name} logo`} />
