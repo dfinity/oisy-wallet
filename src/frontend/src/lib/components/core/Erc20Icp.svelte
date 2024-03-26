9e6cbbd16289af564fbfdfaace37bbc5b7cb9b20
David Dal Busco <david.dalbusco@dfinity.org>
2024-03-26 08:47:33 +0100

    feat: i18n hero and core (#967)
    * feat: i18n hero
    
    * feat: i18n core

--- src/frontend/src/lib/components/core/Erc20Icp.svelte
+++ src/frontend/src/lib/components/core/Erc20Icp.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { ERC20_ICP_REPO_URL } from '$eth/constants/erc20-icp.constants';
 	import { IconInfo } from '@dfinity/gix-components';
+	import { i18n } from '$lib/stores/i18n.store';
 </script>
 
 <a
@@ -10,7 +11,7 @@
 	class="no-underline inline-flex items-center gap-2 text-center pt-1 pb-3 font-bold text-xs md:text-base"
 >
 	<IconInfo />
-	<span class="pl-1">Learn more about ERC20 ICP on Ethereum.</span>
+	<span class="pl-1">{$i18n.hero.learn_more_about_erc20_icp}</span>
 </a>
 
 <style lang="scss">
