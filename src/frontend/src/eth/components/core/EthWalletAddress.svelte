a214286ba9d9fc8e66dbc8421c402e23f7c50107
David Dal Busco <david.dalbusco@dfinity.org>
2024-03-26 10:06:38 +0100

    feat: menu i18n (#970)

--- src/frontend/src/eth/components/core/EthWalletAddress.svelte
+++ src/frontend/src/eth/components/core/EthWalletAddress.svelte
@@ -6,6 +6,7 @@
 	import { nonNullish } from '@dfinity/utils';
 	import { notEmptyString } from '@dfinity/utils';
 	import { explorerUrl as explorerUrlStore } from '$eth/derived/network.derived';
+	import { i18n } from '$lib/stores/i18n.store';
 
 	let explorerUrl: string | undefined = notEmptyString($address)
 		? `${$explorerUrlStore}/address/${$address}`
@@ -13,15 +14,15 @@
 </script>
 
 <div>
-	<label class="block text-sm font-bold" for="eth-wallet-address">Wallet address:</label>
+	<label class="block text-sm font-bold" for="eth-wallet-address">{$i18n.wallet.address}:</label>
 
 	<output class="break-all" id="eth-wallet-address"
 		>{shortenWithMiddleEllipsis($address ?? '')}</output
-	><Copy inline color="inherit" value={$address ?? ''} text="Address copied to clipboard." />
+	><Copy inline color="inherit" value={$address ?? ''} text={$i18n.wallet.address_copied} />
 </div>
 
 {#if nonNullish(explorerUrl)}
-	<ExternalLink href={explorerUrl} ariaLabel="Open your address on Etherscan">
-		View on explorer
+	<ExternalLink href={explorerUrl} ariaLabel={$i18n.wallet.alt_open_etherscan}>
+		{$i18n.links.view_on_explorer}
 	</ExternalLink>
 {/if}
--- src/frontend/src/icp/components/core/IcWalletAddress.svelte
+++ src/frontend/src/icp/components/core/IcWalletAddress.svelte
@@ -2,10 +2,11 @@
 	import { shortenWithMiddleEllipsis } from '$lib/utils/format.utils';
 	import Copy from '$lib/components/ui/Copy.svelte';
 	import { icrcAccountIdentifierText } from '$icp/derived/ic.derived';
+	import { i18n } from '$lib/stores/i18n.store';
 </script>
 
 <div>
-	<label class="block text-sm font-bold" for="ic-wallet-address">Wallet address:</label>
+	<label class="block text-sm font-bold" for="ic-wallet-address">{$i18n.wallet.address}:</label>
 
 	<output id="ic-wallet-address" class="break-all"
 		>{shortenWithMiddleEllipsis($icrcAccountIdentifierText ?? '')}</output
@@ -13,10 +14,10 @@
 		color="inherit"
 		inline
 		value={$icrcAccountIdentifierText ?? ''}
-		text="Address copied to clipboard."
+		text={$i18n.wallet.address_copied}
 	/>
 
 	<p class="pt-2 text-misty-rose break-normal">
-		Note: For ICP deposits, use the 'Receive' button in your ICP account to get the right address.
+		{$i18n.wallet.icp_deposits}
 	</p>
 </div>
