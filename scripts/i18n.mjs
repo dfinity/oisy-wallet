98f23a63189aee40bd2897316c9aa3d235815b5b
David Dal Busco <david.dalbusco@dfinity.org>
2024-03-26 12:55:29 +0100

    feat: recursive i18n (#972)

--- scripts/i18n.mjs
+++ scripts/i18n.mjs
@@ -9,26 +9,33 @@ const PATH_TO_OUTPUT = join(PATH_FROM_ROOT, 'lib', 'types', 'i18n.d.ts');
 
 /**
  * Generates TypeScript interfaces from the English translation file.
- *
- * Note: Supports only a one-child depth in the data structure.
  */
 const generateTypes = async () => {
-	const en = await import(PATH_TO_EN_JSON, { assert: { type: 'json' } });
+	const { default: en } = await import(PATH_TO_EN_JSON, { assert: { type: 'json' } });
 
-	const data = Object.keys(en.default).map((key) => {
-		const properties = Object.keys(en.default[key]).map((prop) => `${prop}: string;`);
+	const mapValues = (values) =>
+		Object.entries(values).reduce(
+			(acc, [key, value]) => [
+				...acc,
+				`${key}: ${typeof value === 'object' ? `{${mapValues(value).join('')}}` : 'string'};`
+			],
+			[]
+		);
 
+	const data = Object.entries(en).map(([key, values]) => {
 		return {
 			key,
 			name: `I18n${key.charAt(0).toUpperCase()}${key.slice(1)}`,
-			properties
+			values: mapValues(values)
 		};
 	});
 
 	const lang = `lang: Languages;`;
 
-	const main = `\n\ninterface I18n {${lang}${data.map((i) => `${i.key}: ${i.name};`).join('')}}`;
-	const interfaces = data.map((i) => `\n\ninterface ${i.name} {${i.properties.join('')}}`).join('');
+	const main = `\n\ninterface I18n {${lang}${data.map(({ key, name }) => `${key}: ${name};`).join('')}}`;
+	const interfaces = data
+		.map(({ name, values }) => `\n\ninterface ${name} {${values.join('')}}`)
+		.join('');
 
 	const comment = `/**\n* Auto-generated definitions file ("npm run i18n")\n*/`;
 
