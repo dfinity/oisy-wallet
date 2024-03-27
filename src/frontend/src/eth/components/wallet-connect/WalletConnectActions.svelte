5d8e1d945ca8f01af5420f16039ddb81faa2cad8
David Dal Busco <david.dalbusco@dfinity.org>
2024-03-27 13:28:16 +0100

    feat: wallet connection i18n (#986)

--- src/frontend/src/eth/components/wallet-connect/WalletConnectActions.svelte
+++ src/frontend/src/eth/components/wallet-connect/WalletConnectActions.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
 	import { isBusy } from '$lib/derived/busy.derived';
+	import { i18n } from '$lib/stores/i18n.store';
 
 	const dispatch = createEventDispatcher();
 
@@ -8,11 +9,13 @@
 </script>
 
 <div class="flex justify-end gap-1 mt-8">
-	<button class="secondary" on:click={() => dispatch('icReject')} disabled={$isBusy}>Reject</button>
+	<button class="secondary" on:click={() => dispatch('icReject')} disabled={$isBusy}
+		>{$i18n.wallet_connect.text.reject}</button
+	>
 
 	{#if approve}
 		<button class="primary" on:click={() => dispatch('icApprove')} disabled={$isBusy}>
-			Approve
+			{$i18n.wallet_connect.text.approve}
 		</button>
 	{/if}
 </div>
--- src/frontend/src/eth/components/wallet-connect/WalletConnectModalTitle.svelte
+++ src/frontend/src/eth/components/wallet-connect/WalletConnectModalTitle.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import IconWalletConnectBlue from '$lib/components/icons/IconWalletConnectBlue.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
 </script>
 
 <div class="flex items-center gap-1.5">
-	<div class="flex"><IconWalletConnectBlue /><span class="hidden">WalletConnect</span></div>
+	<div class="flex">
+		<IconWalletConnectBlue /><span class="hidden">{$i18n.wallet_connect.text.name}</span>
+	</div>
 	<slot />
 </div>
