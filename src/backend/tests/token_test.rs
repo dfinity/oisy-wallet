401181e654202ed422d58493052ab3c44cc53f88
David Dal Busco <david.dalbusco@outlook.com>
2024-04-02 09:26:43 +0200

    test: add user token

--- src/backend/tests/token_test.rs
+++ src/backend/tests/token_test.rs
@@ -5,6 +5,24 @@ use shared::types::Token;
 use utils::{setup, update_call, CALLER};
 
 #[test]
+fn test_add_user_token() {
+    let pic_setup = setup();
+
+    let caller = Principal::from_text(CALLER.to_string()).unwrap();
+
+    let token: Token = Token {
+        chain_id: 11155111,
+        contract_address: "0x7439E9Bb6D8a84dd3A23fe621A30F95403F87fB9".to_string(),
+        decimals: Some(18),
+        symbol: Some("Weenus".to_string()),
+    };
+
+    let result = update_call::<()>(pic_setup, caller, "add_user_token", token);
+
+    assert!(result.is_ok());
+}
+
+#[test]
 fn test_add_user_token_symbol_max_length() {
     let pic_setup = setup();
 
