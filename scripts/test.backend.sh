fbc5dca37e73f7ba613417356b1363171cb05409
David Dal Busco <david.dalbusco@dfinity.org>
2024-04-02 13:28:53 +0200

    build: automated CI backend tests (#1008)
    * build: automated CI backend tests
    
    * build: run on push for now
    
    * build: run cargo test
    
    * test: debug path
    
    * fix: build borrow issue
    
    * chore: debug current directory
    
    * chore: debug print out
    
    * chore: ignore backend.wasm.gz
    
    * build: try cache
    
    * fix: typo in path to wasm
    
    * chore: remove not a good idea
    
    * chore: remove logs
    
    * test: CI should fail
    
    * chore: remove debug info
    
    * chore: redo correct test
    
    * build: run only if backend or shared are modified

--- scripts/test.backend.sh
+++ scripts/test.backend.sh
@@ -2,6 +2,21 @@
 
 POCKET_IC_SERVER_VERSION=3.0.1
 
+# If a backend wasm file exists at the root, it will be used for the tests.
+
+if [ -f "./backend.wasm.gz" ]; then
+    # Setting the environment variable will be used in the test to load that particular file. Relative to where the test is.
+    echo "Use existing backend.wasm.gz canister."
+    export BACKEND_WASM_PATH="../../backend.wasm.gz"
+else
+    echo "Building backend canister."
+    cargo build --locked --target wasm32-unknown-unknown --release -p backend
+fi
+
+# Download PocketIC server
+
+POCKET_IC_SERVER_PATH=target/pocket-ic
+
 if [ ! -d "target" ]
 then
      mkdir "target"
@@ -18,17 +33,18 @@ else
     exit 1
 fi
 
-if [ ! -f "$POCKET_IC_PATH" ]; then
+if [ ! -f "$POCKET_IC_SERVER_PATH" ]; then
   echo "Downloading PocketIC."
-  curl -sSL https://github.com/dfinity/pocketic/releases/download/${POCKET_IC_SERVER_VERSION}/pocket-ic-x86_64-${PLATFORM}.gz -o target/pocket-ic.gz
-  gunzip target/pocket-ic.gz
-  chmod +x target/pocket-ic
+  curl -sSL https://github.com/dfinity/pocketic/releases/download/${POCKET_IC_SERVER_VERSION}/pocket-ic-x86_64-${PLATFORM}.gz -o ${POCKET_IC_SERVER_PATH}.gz
+  gunzip ${POCKET_IC_SERVER_PATH}.gz
+  chmod +x ${POCKET_IC_SERVER_PATH}
 else
-    echo "PocketIC already exists, skipping download."
+    echo "PocketIC server already exists, skipping download."
 fi
 
-echo "Building backend canister."
-cargo build --locked --target wasm32-unknown-unknown --release -p backend
+export POCKET_IC_BIN="../../${POCKET_IC_SERVER_PATH}"
+
+# Run tests
 
 echo "Running backend integration tests."
-POCKET_IC_BIN=../../target/pocket-ic cargo test -p backend
+cargo test -p backend
