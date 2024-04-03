2c6cf3a0664bec4333d018b500b11e291ad67ebb
David Dal Busco <david.dalbusco@dfinity.org>
2024-04-03 13:10:50 +0200

    test: upgrade backend (#1015)

--- src/backend/tests/utils.rs
+++ src/backend/tests/utils.rs
@@ -1,5 +1,5 @@
 use candid::{decode_one, encode_one, CandidType, Principal};
-use pocket_ic::{PocketIc, WasmResult};
+use pocket_ic::{CallError, PocketIc, WasmResult};
 use serde::Deserialize;
 use shared::types::{Arg, InitArg};
 use std::env;
@@ -15,10 +15,7 @@ const BACKEND_WASM: &str = "../../target/wasm32-unknown-unknown/release/backend.
 const SUBNET_ID: &str = "fscpm-uiaaa-aaaaa-aaaap-yai";
 
 pub fn setup() -> (PocketIc, Principal) {
-    let pic = PocketIc::new();
-    let canister_id =
-        pic.create_canister_on_subnet(None, None, Principal::from_text(SUBNET_ID).unwrap());
-    pic.add_cycles(canister_id, 2_000_000_000_000);
+    let (pic, canister_id) = init();
 
     let backend_wasm_path =
         env::var("BACKEND_WASM_PATH").unwrap_or_else(|_| BACKEND_WASM.to_string());
@@ -28,14 +25,67 @@ pub fn setup() -> (PocketIc, Principal) {
         backend_wasm_path
     ));
 
-    let arg: Arg = Arg::Init(InitArg {
-        ecdsa_key_name: format!("master_ecdsa_public_key_{}", SUBNET_ID).to_string(),
-        allowed_callers: vec![],
-    });
+    let arg = init_arg();
 
     pic.install_canister(canister_id, wasm_bytes, encode_one(&arg).unwrap(), None);
 
     (pic, canister_id)
+}
+
+pub fn setup_with_custom_wasm(wasm_path: &str) -> (PocketIc, Principal) {
+    let (pic, canister_id) = init();
+
+    let wasm_bytes = read(wasm_path).expect(&format!("Could not find the wasm: {}", wasm_path));
+
+    let arg = init_arg();
+
+    pic.install_canister(canister_id, wasm_bytes, encode_one(&arg).unwrap(), None);
+
+    (pic, canister_id)
+}
+
+pub fn upgrade((pic, canister_id): &(PocketIc, Principal)) -> Result<(), String> {
+    let backend_wasm_path =
+        env::var("BACKEND_WASM_PATH").unwrap_or_else(|_| BACKEND_WASM.to_string());
+
+    let wasm_bytes = read(backend_wasm_path.clone()).expect(&format!(
+        "Could not find the backend wasm: {}",
+        backend_wasm_path
+    ));
+
+    let arg = init_arg();
+
+    pic.upgrade_canister(
+        canister_id.clone(),
+        wasm_bytes,
+        encode_one(&arg).unwrap(),
+        None,
+    )
+    .map_err(|e| match e {
+        CallError::Reject(e) => e,
+        CallError::UserError(e) => {
+            format!(
+                "Upgrade canister error. RejectionCode: {:?}, Error: {}",
+                e.code, e.description
+            )
+        }
+    })
+}
+
+fn init() -> (PocketIc, Principal) {
+    let pic = PocketIc::new();
+    let canister_id =
+        pic.create_canister_on_subnet(None, None, Principal::from_text(SUBNET_ID).unwrap());
+    pic.add_cycles(canister_id, 2_000_000_000_000);
+
+    (pic, canister_id)
+}
+
+fn init_arg() -> Arg {
+    Arg::Init(InitArg {
+        ecdsa_key_name: format!("master_ecdsa_public_key_{}", SUBNET_ID).to_string(),
+        allowed_callers: vec![],
+    })
 }
 
 pub fn update_call<T>(
