339f079b4f5fcfd1b21f4d5851accbd158f69b93
David Dal Busco <david.dalbusco@outlook.com>
2024-04-08 13:29:37 +0200

    test: anonymous

--- src/backend/tests/it/custom_token.rs
+++ src/backend/tests/it/custom_token.rs
@@ -3,7 +3,7 @@ use crate::utils::mock::CALLER;
 use crate::utils::pocketic::{query_call, setup, update_call};
 use candid::Principal;
 use lazy_static::lazy_static;
-use shared::types::{IcrcToken, UserToken};
+use shared::types::{IcrcToken, Token, UserToken};
 
 lazy_static! {
     static ref ICRC_TOKEN: IcrcToken = IcrcToken {
@@ -118,4 +118,85 @@ fn test_list_user_custom_tokens() {
     assert!(results.is_ok());
 
     assert_custom_tokens_eq(results.unwrap(), expected_tokens);
+}
+
+#[test]
+fn test_anonymous_cannot_add_user_token() {
+    let pic_setup = setup();
+
+    let result = update_call::<()>(
+        &pic_setup,
+        Principal::anonymous(),
+        "add_user_custom_token",
+        USER_TOKEN.clone(),
+    );
+
+    assert!(result.is_err());
+    assert_eq!(
+        result.unwrap_err(),
+        "Anonymous caller not authorized.".to_string()
+    );
+}
+
+#[test]
+fn test_anonymous_cannot_remove_user_token() {
+    let pic_setup = setup();
+
+    let result = update_call::<()>(
+        &pic_setup,
+        Principal::anonymous(),
+        "remove_user_custom_token",
+        USER_TOKEN.clone(),
+    );
+
+    assert!(result.is_err());
+    assert_eq!(
+        result.unwrap_err(),
+        "Anonymous caller not authorized.".to_string()
+    );
+}
+
+#[test]
+fn test_anonymous_cannot_list_user_tokens() {
+    let pic_setup = setup();
+
+    let result = query_call::<()>(
+        &pic_setup,
+        Principal::anonymous(),
+        "list_user_custom_tokens",
+        USER_TOKEN.clone(),
+    );
+
+    assert!(result.is_err());
+    assert_eq!(
+        result.unwrap_err(),
+        "Anonymous caller not authorized.".to_string()
+    );
+}
+
+#[test]
+fn test_user_cannot_list_another_user_tokens() {
+    let pic_setup = setup();
+
+    let caller = Principal::from_text(CALLER.to_string()).unwrap();
+
+    let _ = update_call::<()>(
+        &pic_setup,
+        caller,
+        "add_user_custom_token",
+        USER_TOKEN.clone(),
+    );
+
+    let another_caller =
+        Principal::from_text("yaa3n-twfur-6xz6e-3z7ep-xln56-222kz-w2b2m-y5wqz-vu6kk-s3fdg-lqe")
+            .unwrap();
+
+    let results =
+        query_call::<Vec<Token>>(&pic_setup, another_caller, "list_user_custom_tokens", ());
+
+    assert!(results.is_ok());
+
+    let results_tokens = results.unwrap();
+
+    assert_eq!(results_tokens.len(), 0);
 }
