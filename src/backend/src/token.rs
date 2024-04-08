26bd05a2146fe49291e2644b3a82b21e59e5bb64
David Dal Busco <david.dalbusco@outlook.com>
2024-04-08 12:10:10 +0200

    refactor(backend): extract add and remove user token

---
+++ src/backend/src/token.rs
@@ -0,0 +1,50 @@
+use crate::{Candid, StoredPrincipal, VMem};
+use candid::{CandidType, Deserialize};
+use ic_stable_structures::StableBTreeMap;
+
+const MAX_TOKEN_LIST_LENGTH: usize = 100;
+
+pub fn add_to_user_token<T>(
+    stored_principal: StoredPrincipal,
+    user_token: &mut StableBTreeMap<StoredPrincipal, Candid<Vec<T>>, VMem>,
+    token: &T,
+    find: &dyn Fn(&T) -> bool,
+) where
+    T: for<'a> Deserialize<'a> + CandidType + Clone,
+{
+    let Candid(mut tokens) = user_token.get(&stored_principal).unwrap_or_default();
+
+    match tokens.iter().position(find) {
+        Some(p) => {
+            tokens[p] = token.clone();
+        }
+        None => {
+            if tokens.len() == MAX_TOKEN_LIST_LENGTH {
+                ic_cdk::trap(&format!(
+                    "Token list length should not exceed {MAX_TOKEN_LIST_LENGTH}"
+                ));
+            }
+            tokens.push(token.clone());
+        }
+    }
+
+    user_token.insert(stored_principal, Candid(tokens));
+}
+
+pub fn remove_from_user_token<T>(
+    stored_principal: StoredPrincipal,
+    user_token: &mut StableBTreeMap<StoredPrincipal, Candid<Vec<T>>, VMem>,
+    find: &dyn Fn(&T) -> bool,
+) where
+    T: for<'a> Deserialize<'a> + CandidType,
+{
+    match user_token.get(&stored_principal) {
+        None => (),
+        Some(Candid(mut tokens)) => {
+            if let Some(p) = tokens.iter().position(find) {
+                tokens.swap_remove(p);
+                user_token.insert(stored_principal, Candid(tokens));
+            }
+        }
+    }
+}
