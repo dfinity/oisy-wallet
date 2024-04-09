7c6d9110faf97ed2849abbcb18640b3aaf25cf5e
David Dal Busco <david.dalbusco@outlook.com>
2024-04-09 14:04:51 +0200

    chore: format

--- src/frontend/src/icp/components/tokens/IcAddTokenModal.svelte
+++ src/frontend/src/icp/components/tokens/IcAddTokenModal.svelte
@@ -4,18 +4,18 @@
 	import { WizardModal, type WizardStep } from '@dfinity/gix-components';
 	import IcAddTokenForm from '$icp/components/tokens/IcAddTokenForm.svelte';
 	import type { IcCanisters } from '$icp/types/ic';
-    import { modalStore } from '$lib/stores/modal.store';
+	import { modalStore } from '$lib/stores/modal.store';
 
 	let saveProgressStep: string = AddTokenStep.INITIALIZATION;
 
 	let currentStep: WizardStep | undefined;
 	let modal: WizardModal;
 
-    const close = () => {
-        modalStore.close();
+	const close = () => {
+		modalStore.close();
 
-        saveProgressStep = AddTokenStep.INITIALIZATION;
-    };
+		saveProgressStep = AddTokenStep.INITIALIZATION;
+	};
 
 	let canisters: Partial<IcCanisters> | undefined;
 </script>
