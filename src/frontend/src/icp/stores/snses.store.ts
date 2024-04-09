ac0fbba14358a4549686074e90f9d5d1aa829502
David Dal Busco <david.dalbusco@outlook.com>
2024-04-09 06:53:35 +0200

    feat: remove we will not need the json of the snses globally

--- src/frontend/src/icp/stores/snses.store.ts
+++
@@ -1,6 +0,0 @@
-import snses from '$icp/data/snses.json';
-import type { CachedSnsDto } from '$icp/types/sns-aggregator';
-import { readonly, writable, type Readable } from 'svelte/store';
-
-const snesListStore = writable<CachedSnsDto[]>(snses);
-export const snsesStore: Readable<CachedSnsDto[]> = readonly(snesListStore);
