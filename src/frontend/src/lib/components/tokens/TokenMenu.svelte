6ab86a671767fc8bec2aa61ced8e52c94fd6f4f1
David Dal Busco <david.dalbusco@dfinity.org>
2024-04-10 10:15:37 +0200

    feat: hero context menu and open eth asset explorer url (#1084)

---
+++ src/frontend/src/lib/components/tokens/TokenMenu.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Popover } from '@dfinity/gix-components';
+	import { i18n } from '$lib/stores/i18n.store';
+	import IconMore from '$lib/components/icons/IconMore.svelte';
+	let visible = false;
+	let button: HTMLButtonElement | undefined;
+</script>
+
+<button
+	class="icon text-white"
+	bind:this={button}
+	on:click={() => (visible = true)}
+	aria-label={$i18n.tokens.alt.context_menu}
+>
+	<IconMore />
+</button>
+
+<Popover bind:visible anchor={button} invisibleBackdrop direction="rtl">
+	<slot />
+</Popover>
