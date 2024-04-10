e2647677f0fa269a3af22ffbe4e20e9c2c9925cd
David Dal Busco <david.dalbusco@dfinity.org>
2024-04-10 07:18:40 +0200

    feat: init tokens menu (#1074)

---
+++ src/frontend/src/lib/components/tokens/TokensMenu.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Popover } from '@dfinity/gix-components';
+	import { i18n } from '$lib/stores/i18n.store';
+	import IconMore from '$lib/components/icons/IconMore.svelte';
+
+	let visible = false;
+	let button: HTMLButtonElement | undefined;
+</script>
+
+<button
+	class="icon"
+	bind:this={button}
+	on:click={() => (visible = true)}
+	aria-label={$i18n.navigation.alt.menu}
+>
+	<IconMore />
+</button>
+
+<Popover bind:visible anchor={button} invisibleBackdrop direction="rtl">Menu</Popover>
