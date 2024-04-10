7a811b2eb5103f13955129fdcd7360f50d9be8c6
David Dal Busco <david.dalbusco@outlook.com>
2024-04-10 10:14:34 +0200

    feat: hero context menu and open eth asset explorer url

---
+++ src/frontend/src/lib/components/hero/ContextMenu.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import TokenMenu from '$eth/components/tokens/TokenMenu.svelte';
+	import { networkICP } from '$lib/derived/network.derived';
+</script>
+
+{#if $networkICP}
+	&ZeroWidthSpace;
+{:else}
+	<TokenMenu />
+{/if}
