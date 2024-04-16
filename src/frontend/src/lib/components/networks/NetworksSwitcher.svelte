e8a9253cce10faa0919791ff1ed0af10105c0cd1
David Dal Busco <david.dalbusco@dfinity.org>
2024-04-16 15:04:45 +0200

    style: enforce rounded logo (#1124)

--- src/frontend/src/lib/components/networks/NetworksSwitcher.svelte
+++ src/frontend/src/lib/components/networks/NetworksSwitcher.svelte
@@ -38,6 +38,7 @@
 				})}
 				width="100%"
 				height="100%"
+				rounded
 			/>
 		{/if}
 	</div>
--- src/frontend/src/lib/components/ui/Img.svelte
+++ src/frontend/src/lib/components/ui/Img.svelte
@@ -5,6 +5,18 @@
 	export let loading: 'eager' | 'lazy' = 'lazy';
 	export let width: string | undefined = undefined;
 	export let height: string | undefined = undefined;
+	export let rounded = false;
 </script>
 
-<img {src} {role} {alt} {loading} {width} {height} decoding="async" on:load on:error />
+<img
+	{src}
+	{role}
+	{alt}
+	{loading}
+	{width}
+	{height}
+	decoding="async"
+	on:load
+	on:error
+	class:rounded-full={rounded}
+/>
