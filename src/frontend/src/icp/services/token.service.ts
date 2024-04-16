5f8cc0fd71cabd23e94f7c52369207909bcf73f7
David Dal Busco <david.dalbusco@dfinity.org>
2024-04-16 10:09:09 +0200

    feat: load custom user tokens (#1108)

--- src/frontend/src/icp/services/token.service.ts
+++ src/frontend/src/icp/services/token.service.ts
@@ -1,3 +1,4 @@
+import { ICP_NETWORK } from '$env/networks.env';
 import snsTokens from '$env/tokens.sns.json';
 import { getTransactions as getTransactionsIcrc } from '$icp/api/icrc-index-ng.api';
 import { metadata } from '$icp/api/icrc-ledger.api';
@@ -6,7 +7,12 @@ import { mapIcrcToken } from '$icp/utils/icrc.utils';
 import { i18n } from '$lib/stores/i18n.store';
 import { toastsError } from '$lib/stores/toasts.store';
 import type { OptionIdentity } from '$lib/types/identity';
-import { knownIcrcToken, knownIcrcTokens, type KnownIcrcTokens } from '$lib/types/known-token';
+import {
+	knownIcrcToken,
+	knownIcrcTokens,
+	type KnownIcrcToken,
+	type KnownIcrcTokenMetadata
+} from '$lib/types/known-token';
 import type { Identity } from '@dfinity/agent';
 import { assertNonNullish, isNullish } from '@dfinity/utils';
 import { get } from 'svelte/store';
@@ -63,6 +69,7 @@ const loadMetadata = async ({
 			exchangeCoinId: 'internet-computer',
 			// Position does not matter here
 			position: Number.MAX_VALUE,
+			category: 'custom',
 			...rest
 		});
 	} catch (err: unknown) {
@@ -101,7 +108,7 @@ const loadBalance = async ({
 
 export const buildKnownIcrcTokens = (): {
 	result: 'success' | 'error';
-	tokens?: KnownIcrcTokens;
+	tokens?: (IcTokenWithoutId & Pick<KnownIcrcTokenMetadata, 'alternativeName'>)[];
 } => {
 	try {
 		const tokens = knownIcrcTokens.parse(
@@ -123,7 +130,27 @@ export const buildKnownIcrcTokens = (): {
 			)
 		);
 
-		return { result: 'success', tokens };
+		const mapKkownIcrcToken = ({
+			ledgerCanisterId,
+			indexCanisterId,
+			metadata: { name, decimals, symbol, fee, alternativeName }
+		}: KnownIcrcToken): IcTokenWithoutId & Pick<KnownIcrcTokenMetadata, 'alternativeName'> => ({
+			ledgerCanisterId,
+			indexCanisterId,
+			network: ICP_NETWORK,
+			name,
+			decimals,
+			symbol,
+			exchangeCoinId: undefined,
+			position: Number.MAX_VALUE,
+			standard: 'icrc',
+			category: 'custom',
+			icon: undefined,
+			fee,
+			alternativeName
+		});
+
+		return { result: 'success', tokens: tokens.map(mapKkownIcrcToken) };
 	} catch (err: unknown) {
 		toastsError({
 			msg: { text: get(i18n).tokens.manage.error.unexpected_build },
