feb365e600f3c3235d2f7664f818777f4894b94c
David Dal Busco <david.dalbusco@outlook.com>
2024-04-16 10:08:11 +0200

    feat: load custom user tokens

--- src/frontend/src/lib/api/backend.api.ts
+++ src/frontend/src/lib/api/backend.api.ts
@@ -1,8 +1,9 @@
-import type { SignRequest, Token, TokenId } from '$declarations/backend/backend.did';
+import type { SignRequest, Token, TokenId, UserToken } from '$declarations/backend/backend.did';
 import { getBackendActor } from '$lib/actors/actors.ic';
 import type { ECDSA_PUBLIC_KEY } from '$lib/types/address';
 import type { OptionIdentity } from '$lib/types/identity';
 import type { Identity } from '@dfinity/agent';
+import type { QueryParams } from '@dfinity/utils';
 
 export const getEthAddress = async (identity: OptionIdentity): Promise<ECDSA_PUBLIC_KEY> => {
 	const { caller_eth_address } = await getBackendActor({ identity });
@@ -65,6 +66,14 @@ export const removeUserToken = async ({
 };
 
 export const listUserTokens = async ({ identity }: { identity: Identity }): Promise<Token[]> => {
-	const { list_user_tokens } = await getBackendActor({ identity, certified: false });
+	const { list_user_tokens } = await getBackendActor({ identity });
 	return list_user_tokens();
+};
+
+export const listUserCustomTokens = async ({
+	identity,
+	certified = true
+}: { identity: OptionIdentity } & QueryParams): Promise<UserToken[]> => {
+	const { list_user_custom_tokens } = await getBackendActor({ identity, certified });
+	return list_user_custom_tokens();
 };
