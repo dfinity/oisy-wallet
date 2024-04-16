3eaf097a6bee3fbc1ab0263065a05f496183c19f
David Dal Busco <david.dalbusco@outlook.com>
2024-04-16 11:57:07 +0200

    test: add_user_token still works after upgrade

--- src/backend/tests/it/upgrade.rs
+++ src/backend/tests/it/upgrade.rs
@@ -60,3 +60,38 @@ fn test_upgrade_allowed_caller_eth_address_of() {
     assert!(post_upgrade_result.is_ok());
     assert_eq!(post_upgrade_result.unwrap(), CALLER_ETH_ADDRESS.to_string());
 }
+
+#[test]
+fn test_upgrade_add_user_token() {
+    // Deploy a released canister
+    let pic_setup = setup_with_custom_wasm(BACKEND_V0_0_13_WASM_PATH);
+
+    pic_setup.0.tick();
+
+    // Upgrade canister with new wasm
+    upgrade(&pic_setup).unwrap_or_else(|e| panic!("Upgrade canister failed with error: {}", e));
+
+    // Add a user token
+    let caller = Principal::from_text(CALLER.to_string()).unwrap();
+
+    let token: UserToken = UserToken {
+        chain_id: 11155111,
+        contract_address: WEENUS_CONTRACT_ADDRESS.to_string(),
+        decimals: Some(WEENUS_DECIMALS),
+        symbol: Some(WEENUS_SYMBOL.to_string()),
+    };
+
+    let result = update_call::<()>(&pic_setup, caller, "add_user_token", token.clone());
+
+    // Add user token still works after upgrade?
+    assert!(result.is_ok());
+
+    // Get the list of token and check that it still contains the one we added before upgrade
+    let results = update_call::<Vec<UserToken>>(&pic_setup, caller, "list_user_tokens", ());
+
+    let expected_tokens: Vec<UserToken> = vec![token.clone()];
+
+    assert!(results.is_ok());
+
+    assert_tokens_eq(results.unwrap(), expected_tokens);
+}
