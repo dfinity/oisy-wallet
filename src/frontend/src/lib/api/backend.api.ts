ccb73e83d32a99e3aac0a1712422a253cc713b3d
David Dal Busco <david.dalbusco@dfinity.org>
2024-04-16 12:28:40 +0200

    feat(frontend): adapt renamed token types and custom token functions (#1114)

--- src/frontend/src/lib/api/backend.api.ts
+++ src/frontend/src/lib/api/backend.api.ts
@@ -1,4 +1,9 @@
-import type { SignRequest, Token, TokenId, UserToken } from '$declarations/backend/backend.did';
+import type {
+	CustomToken,
+	SignRequest,
+	UserToken,
+	UserTokenId
+} from '$declarations/backend/backend.did';
 import { getBackendActor } from '$lib/actors/actors.ic';
 import type { ECDSA_PUBLIC_KEY } from '$lib/types/address';
 import type { OptionIdentity } from '$lib/types/identity';
@@ -47,7 +52,7 @@ export const addUserToken = async ({
 	token,
 	identity
 }: {
-	token: Token;
+	token: UserToken;
 	identity: Identity;
 }): Promise<void> => {
 	const { add_user_token } = await getBackendActor({ identity });
@@ -58,33 +63,37 @@ export const removeUserToken = async ({
 	tokenId,
 	identity
 }: {
-	tokenId: TokenId;
+	tokenId: UserTokenId;
 	identity: Identity;
 }): Promise<void> => {
 	const { remove_user_token } = await getBackendActor({ identity });
 	return remove_user_token(tokenId);
 };
 
-export const listUserTokens = async ({ identity }: { identity: Identity }): Promise<Token[]> => {
+export const listUserTokens = async ({
+	identity
+}: {
+	identity: Identity;
+}): Promise<UserToken[]> => {
 	const { list_user_tokens } = await getBackendActor({ identity });
 	return list_user_tokens();
 };
 
-export const listUserCustomTokens = async ({
+export const listCustomTokens = async ({
 	identity,
 	certified = true
-}: { identity: OptionIdentity } & QueryParams): Promise<UserToken[]> => {
-	const { list_user_custom_tokens } = await getBackendActor({ identity, certified });
-	return list_user_custom_tokens();
+}: { identity: OptionIdentity } & QueryParams): Promise<CustomToken[]> => {
+	const { list_custom_tokens } = await getBackendActor({ identity, certified });
+	return list_custom_tokens();
 };
 
-export const setManyUserCustomTokens = async ({
+export const setUserCustomTokens = async ({
 	tokens,
 	identity
 }: {
-	tokens: UserToken[];
+	tokens: CustomToken[];
 	identity: Identity;
 }): Promise<void> => {
-	const { set_many_user_custom_tokens } = await getBackendActor({ identity });
-	return set_many_user_custom_tokens(tokens);
+	const { set_many_custom_tokens } = await getBackendActor({ identity });
+	return set_many_custom_tokens(tokens);
 };
