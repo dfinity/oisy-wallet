0bdbf0bee2b8889bd28facff0ccbee35f72346ac
David Dal Busco <david.dalbusco@dfinity.org>
2024-04-18 16:36:20 +0200

    feat: token version (#1137)

--- src/backend/tests/it/custom_token.rs
+++ src/backend/tests/it/custom_token.rs
@@ -1,4 +1,4 @@
-use crate::utils::assertion::assert_custom_tokens_eq;
+use crate::utils::assertion::{assert_custom_tokens_eq, assert_some_tokens_version};
 use crate::utils::mock::CALLER;
 use crate::utils::pocketic::{query_call, setup, update_call};
 use candid::Principal;
@@ -12,7 +12,8 @@ lazy_static! {
     };
     static ref USER_TOKEN: CustomToken = CustomToken {
         token: Token::Icrc(ICRC_TOKEN.clone()),
-        enabled: true
+        enabled: true,
+        version: None,
     };
     static ref USER_TOKEN_ID: CustomTokenId = CustomTokenId::Icrc(ICRC_TOKEN.ledger_id.clone());
     static ref ANOTHER_USER_TOKEN: CustomToken = CustomToken {
@@ -21,6 +22,7 @@ lazy_static! {
             index_id: Principal::from_text("ux4b6-7qaaa-aaaaq-aaboa-cai".to_string()).unwrap(),
         }),
         enabled: true,
+        version: None,
     };
 }
 
@@ -30,28 +32,18 @@ fn test_add_custom_token() {
 
     let caller = Principal::from_text(CALLER.to_string()).unwrap();
 
-    let result = update_call::<()>(
-        &pic_setup,
-        caller,
-        "set_custom_token",
-        USER_TOKEN.clone(),
-    );
+    let result = update_call::<()>(&pic_setup, caller, "set_custom_token", USER_TOKEN.clone());
 
     assert!(result.is_ok());
 }
 
 #[test]
-fn test_update_user_token() {
+fn test_update_custom_token() {
     let pic_setup = setup();
 
     let caller = Principal::from_text(CALLER.to_string()).unwrap();
 
-    let result = update_call::<()>(
-        &pic_setup,
-        caller,
-        "set_custom_token",
-        USER_TOKEN.clone(),
-    );
+    let result = update_call::<()>(&pic_setup, caller, "set_custom_token", USER_TOKEN.clone());
 
     assert!(result.is_ok());
 
@@ -61,19 +53,16 @@ fn test_update_user_token() {
 
     assert!(results.is_ok());
 
-    assert_custom_tokens_eq(results.unwrap(), expected_tokens);
+    assert_custom_tokens_eq(results.clone().unwrap(), expected_tokens);
 
     let update_token: CustomToken = CustomToken {
         enabled: false,
         token: USER_TOKEN.token.clone(),
+        version: results.unwrap().get(0).unwrap().version,
     };
 
-    let update_result = update_call::<()>(
-        &pic_setup,
-        caller,
-        "set_custom_token",
-        update_token.clone(),
-    );
+    let update_result =
+        update_call::<()>(&pic_setup, caller, "set_custom_token", update_token.clone());
 
     assert!(update_result.is_ok());
 
@@ -84,7 +73,10 @@ fn test_update_user_token() {
 
     assert!(updated_results.is_ok());
 
-    assert_custom_tokens_eq(updated_results.unwrap(), expected_updated_tokens);
+    let updated_tokens = updated_results.unwrap();
+
+    assert_custom_tokens_eq(updated_tokens.clone(), expected_updated_tokens);
+    assert_some_tokens_version(&updated_tokens);
 }
 
 #[test]
@@ -101,19 +93,14 @@ fn test_add_many_custom_tokens() {
 }
 
 #[test]
-fn test_update_many_user_tokens() {
+fn test_update_many_custom_tokens() {
     let pic_setup = setup();
 
     let caller = Principal::from_text(CALLER.to_string()).unwrap();
 
     let tokens: Vec<CustomToken> = vec![USER_TOKEN.clone(), ANOTHER_USER_TOKEN.clone()];
 
-    let result = update_call::<()>(
-        &pic_setup,
-        caller,
-        "set_many_custom_tokens",
-        tokens.clone(),
-    );
+    let result = update_call::<()>(&pic_setup, caller, "set_many_custom_tokens", tokens.clone());
 
     assert!(result.is_ok());
 
@@ -121,16 +108,18 @@ fn test_update_many_user_tokens() {
 
     assert!(results.is_ok());
 
-    assert_custom_tokens_eq(results.unwrap(), tokens.clone());
+    assert_custom_tokens_eq(results.clone().unwrap(), tokens.clone());
 
     let update_token: CustomToken = CustomToken {
         enabled: false,
         token: USER_TOKEN.token.clone(),
+        version: results.clone().unwrap().get(0).unwrap().version,
     };
 
     let update_another_token: CustomToken = CustomToken {
         enabled: false,
         token: ANOTHER_USER_TOKEN.token.clone(),
+        version: results.unwrap().get(1).unwrap().version,
     };
 
     let update_tokens: Vec<CustomToken> = vec![update_token.clone(), update_another_token.clone()];
@@ -149,7 +138,10 @@ fn test_update_many_user_tokens() {
 
     assert!(updated_results.is_ok());
 
-    assert_custom_tokens_eq(updated_results.unwrap(), update_tokens);
+    let updated_tokens = updated_results.unwrap();
+
+    assert_custom_tokens_eq(updated_tokens.clone(), update_tokens);
+    assert_some_tokens_version(&updated_tokens);
 }
 
 #[test]
@@ -158,12 +150,7 @@ fn test_list_custom_tokens() {
 
     let caller = Principal::from_text(CALLER.to_string()).unwrap();
 
-    let _ = update_call::<()>(
-        &pic_setup,
-        caller,
-        "set_custom_token",
-        USER_TOKEN.clone(),
-    );
+    let _ = update_call::<()>(&pic_setup, caller, "set_custom_token", USER_TOKEN.clone());
 
     let _ = update_call::<()>(
         &pic_setup,
@@ -178,11 +165,64 @@ fn test_list_custom_tokens() {
 
     assert!(results.is_ok());
 
-    assert_custom_tokens_eq(results.unwrap(), expected_tokens);
+    let list_tokens = results.unwrap();
+
+    assert_custom_tokens_eq(list_tokens.clone(), expected_tokens);
+    assert_some_tokens_version(&list_tokens);
 }
 
 #[test]
-fn test_anonymous_cannot_add_user_token() {
+fn test_cannot_update_custom_token_without_version() {
+    let pic_setup = setup();
+
+    let caller = Principal::from_text(CALLER.to_string()).unwrap();
+
+    let result = update_call::<()>(&pic_setup, caller, "set_custom_token", USER_TOKEN.clone());
+
+    assert!(result.is_ok());
+
+    let update_token: CustomToken = CustomToken {
+        enabled: false,
+        token: USER_TOKEN.token.clone(),
+        version: None,
+    };
+
+    let update_result =
+        update_call::<()>(&pic_setup, caller, "set_custom_token", update_token.clone());
+
+    assert!(update_result.is_err());
+    assert!(update_result
+        .unwrap_err()
+        .contains("Version mismatch, token update not allowed"));
+}
+
+#[test]
+fn test_cannot_update_custom_token_with_invalid_version() {
+    let pic_setup = setup();
+
+    let caller = Principal::from_text(CALLER.to_string()).unwrap();
+
+    let result = update_call::<()>(&pic_setup, caller, "set_custom_token", USER_TOKEN.clone());
+
+    assert!(result.is_ok());
+
+    let update_token: CustomToken = CustomToken {
+        enabled: false,
+        token: USER_TOKEN.token.clone(),
+        version: Some(123456789),
+    };
+
+    let update_result =
+        update_call::<()>(&pic_setup, caller, "set_custom_token", update_token.clone());
+
+    assert!(update_result.is_err());
+    assert!(update_result
+        .unwrap_err()
+        .contains("Version mismatch, token update not allowed"));
+}
+
+#[test]
+fn test_anonymous_cannot_add_custom_token() {
     let pic_setup = setup();
 
     let result = update_call::<()>(
@@ -200,7 +240,7 @@ fn test_anonymous_cannot_add_user_token() {
 }
 
 #[test]
-fn test_anonymous_cannot_list_user_tokens() {
+fn test_anonymous_cannot_list_custom_tokens() {
     let pic_setup = setup();
 
     let result = query_call::<()>(
@@ -218,17 +258,12 @@ fn test_anonymous_cannot_list_user_tokens() {
 }
 
 #[test]
-fn test_user_cannot_list_another_user_tokens() {
+fn test_user_cannot_list_another_custom_tokens() {
     let pic_setup = setup();
 
     let caller = Principal::from_text(CALLER.to_string()).unwrap();
 
-    let _ = update_call::<()>(
-        &pic_setup,
-        caller,
-        "set_custom_token",
-        USER_TOKEN.clone(),
-    );
+    let _ = update_call::<()>(&pic_setup, caller, "set_custom_token", USER_TOKEN.clone());
 
     let another_caller =
         Principal::from_text("yaa3n-twfur-6xz6e-3z7ep-xln56-222kz-w2b2m-y5wqz-vu6kk-s3fdg-lqe")
--- src/shared/src/impls.rs
+++ src/shared/src/impls.rs
@@ -1,9 +1,35 @@
-use crate::types::custom_token::{CustomTokenId, Token};
+use crate::types::custom_token::{CustomToken, CustomTokenId, Token};
+use crate::types::token::UserToken;
+use crate::types::{TokenVersion, Version};
 
 impl From<&Token> for CustomTokenId {
     fn from(token: &Token) -> Self {
         match token {
             Token::Icrc(token) => CustomTokenId::Icrc(token.ledger_id),
         }
+    }
+}
+
+impl TokenVersion for UserToken {
+    fn get_version(&self) -> Option<Version> {
+        self.version
+    }
+
+    fn clone_with_incremented_version(&self) -> Self {
+        let mut cloned = self.clone();
+        cloned.version = Some(cloned.version.unwrap_or_default() + 1);
+        cloned
+    }
+}
+
+impl TokenVersion for CustomToken {
+    fn get_version(&self) -> Option<Version> {
+        self.version
+    }
+
+    fn clone_with_incremented_version(&self) -> Self {
+        let mut cloned = self.clone();
+        cloned.version = Some(cloned.version.unwrap_or_default() + 1);
+        cloned
     }
 }
