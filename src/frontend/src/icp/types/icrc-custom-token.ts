4e3884563f9670ec1df1e728f57f3eb71774c941
David Dal Busco <david.dalbusco@dfinity.org>
2024-04-18 16:46:47 +0200

    feat(frontend): incorporate token version state (#1139)

--- src/frontend/src/icp/types/icrc-custom-token.ts
+++ src/frontend/src/icp/types/icrc-custom-token.ts
@@ -1,6 +1,16 @@
-import type { IcTokenWithoutId } from '$icp/types/ic';
+import type { CustomToken } from '$declarations/backend/backend.did';
+import type { IcToken, IcTokenWithoutId } from '$icp/types/ic';
 import type { IcrcEnvTokenMetadata } from '$icp/types/icrc-env-token';
 
-export type IcrcCustomTokenConfig = IcrcCustomToken & { enabled: boolean };
+export type IcrcCustomTokenConfig = IcTokenWithoutIdExtended & { enabled: boolean };
 
-export type IcrcCustomToken = IcTokenWithoutId & Pick<IcrcEnvTokenMetadata, 'alternativeName'>;
+export type IcTokenWithoutIdExtended = IcTokenWithoutId &
+	Pick<IcrcEnvTokenMetadata, 'alternativeName'>;
+
+export type IcCustomTokenState = Omit<CustomToken, 'token' | 'version'> & {
+	version?: bigint;
+};
+
+export type IcrcCustomTokenWithoutId = IcCustomTokenState & IcTokenWithoutId;
+
+export type IcrcCustomToken = IcCustomTokenState & IcToken;
