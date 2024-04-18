f63a1bd0e142f8ad6cdfd2f6a233ab06bf47c26b
David Dal Busco <david.dalbusco@outlook.com>
2024-04-18 16:29:14 +0200

    feat: replace timestamp with version

--- src/frontend/src/icp/types/icrc-custom-token.ts
+++ src/frontend/src/icp/types/icrc-custom-token.ts
@@ -7,8 +7,8 @@ export type IcrcCustomTokenConfig = IcTokenWithoutIdExtended & { enabled: boolea
 export type IcTokenWithoutIdExtended = IcTokenWithoutId &
 	Pick<IcrcEnvTokenMetadata, 'alternativeName'>;
 
-export type IcCustomTokenState = Omit<CustomToken, 'token' | 'timestamp'> & {
-	timestamp?: bigint;
+export type IcCustomTokenState = Omit<CustomToken, 'token' | 'version'> & {
+	version?: bigint;
 };
 
 export type IcrcCustomTokenWithoutId = IcCustomTokenState & IcTokenWithoutId;
