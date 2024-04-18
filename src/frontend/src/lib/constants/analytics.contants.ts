d3491aa3825826ae84e45f015759126698ef7162
David Dal Busco <david.dalbusco@outlook.com>
2024-04-18 18:09:40 +0200

    feat: track few anonymous events about auth in analytics

---
+++ src/frontend/src/lib/constants/analytics.contants.ts
@@ -0,0 +1,6 @@
+export const TRACK_COUNT_SIGN_IN_SUCCESS = 'sign_in_success_count';
+export const TRACK_SIGN_IN_CANCELLED_COUNT = 'sign_in_cancelled_count';
+export const TRACK_SIGN_IN_ERROR_COUNT = 'sign_in_error_count';
+export const TRACK_SYNC_AUTH_AUTHENTICATED_COUNT = 'sync_auth_authenticated_count';
+export const TRACK_SYNC_AUTH_NOT_AUTHENTICATED_COUNT = 'sync_auth_not_authenticated_count';
+export const TRACK_SYNC_AUTH_ERROR_COUNT = 'sync_auth_error_count';
--- src/frontend/src/lib/services/auth.services.ts
+++ src/frontend/src/lib/services/auth.services.ts
@@ -1,4 +1,10 @@
 import { deleteIdbEthAddress } from '$lib/api/idb.api';
+import {
+	TRACK_COUNT_SIGN_IN_SUCCESS,
+	TRACK_SIGN_IN_CANCELLED_COUNT,
+	TRACK_SIGN_IN_ERROR_COUNT
+} from '$lib/constants/analytics.contants';
+import { trackEvent } from '$lib/services/analytics.services';
 import { authStore, type AuthSignInParams } from '$lib/stores/auth.store';
 import { busy } from '$lib/stores/busy.store';
 import { testnetsStore } from '$lib/stores/settings.store';
@@ -17,15 +23,27 @@ export const signIn = async (
 	try {
 		await authStore.signIn(params);
 
+		await trackEvent({
+			name: TRACK_COUNT_SIGN_IN_SUCCESS
+		});
+
 		// We clean previous messages in case user was signed out automatically before sign-in again.
 		toastsClean();
 
 		return { success: 'ok' };
 	} catch (err: unknown) {
 		if (err === 'UserInterrupt') {
+			await trackEvent({
+				name: TRACK_SIGN_IN_CANCELLED_COUNT
+			});
+
 			// We do not display an error if user explicitly cancelled the process of sign-in
 			return { success: 'cancelled' };
 		}
+
+		await trackEvent({
+			name: TRACK_SIGN_IN_ERROR_COUNT
+		});
 
 		toastsError({
 			msg: { text: `Something went wrong while sign-in.` },
