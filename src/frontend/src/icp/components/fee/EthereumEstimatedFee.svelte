e9c86d4026b391755bfb151babe59bdb4330c3f3
David Dal Busco <david.dalbusco@outlook.com>
2024-04-22 14:10:31 +0200

    feat: ckErc20 to Erc20 fees (for now)

--- src/frontend/src/icp/components/fee/EthereumEstimatedFee.svelte
+++ src/frontend/src/icp/components/fee/EthereumEstimatedFee.svelte
@@ -2,7 +2,7 @@
 	import { slide, fade } from 'svelte/transition';
 	import { token, tokenId } from '$lib/derived/token.derived';
 	import { nonNullish } from '@dfinity/utils';
-	import { isTokenCkEthLedger } from '$icp/utils/ic-send.utils';
+	import { isTokenCkErc20Ledger, isTokenCkEthLedger } from '$icp/utils/ic-send.utils';
 	import type { IcToken } from '$icp/types/ic';
 	import type { NetworkId } from '$lib/types/network';
 	import Value from '$lib/components/ui/Value.svelte';
@@ -13,24 +13,33 @@
 	import { onDestroy } from 'svelte';
 	import { EIGHT_DECIMALS } from '$lib/constants/app.constants';
 	import { isNetworkIdEthereum } from '$lib/utils/network.utils';
-	import { ckEthereumTwinToken } from '$icp-eth/derived/cketh.derived';
+	import { ckEthereumNativeToken } from '$icp-eth/derived/cketh.derived';
 	import { i18n } from '$lib/stores/i18n.store';
+	import { CKERC20_TO_ERC20_MAX_TRANSACTION_FEE } from '$icp/constants/cketh.constants';
 
 	export let networkId: NetworkId | undefined = undefined;
 
 	let ckETH = false;
 	$: ckETH = isTokenCkEthLedger($token as IcToken);
 
+	let ckEr20 = false;
+	$: ckEr20 = isTokenCkErc20Ledger($token as IcToken);
+
 	let ethNetwork = false;
 	$: ethNetwork = isNetworkIdEthereum(networkId);
 
+	let maxTransactionFeeEth: bigint | undefined = undefined;
+	$: maxTransactionFeeEth = $eip1559TransactionPriceStore?.[$tokenId]?.data.max_transaction_fee;
+
 	let maxTransactionFee: bigint | undefined = undefined;
-	$: maxTransactionFee = $eip1559TransactionPriceStore?.[$tokenId]?.data.max_transaction_fee;
+	$: maxTransactionFee = nonNullish(maxTransactionFeeEth)
+		? maxTransactionFeeEth + CKERC20_TO_ERC20_MAX_TRANSACTION_FEE
+		: undefined;
 
 	const loadFee = async () => {
 		clearTimer();
 
-		if (!ckETH || !ethNetwork) {
+		if ((!ckETH && !ckEr20) || !ethNetwork) {
 			return;
 		}
 
@@ -50,7 +59,7 @@
 	onDestroy(clearTimer);
 </script>
 
-{#if ckETH && ethNetwork}
+{#if (ckETH || ckEr20) && ethNetwork}
 	<div transition:slide={{ duration: 250 }}>
 		<Value ref="kyt-fee">
 			<svelte:fragment slot="label">{$i18n.fee.text.estimated_eth}</svelte:fragment>
@@ -63,7 +72,7 @@
 							value: BigNumber.from(maxTransactionFee),
 							displayDecimals: EIGHT_DECIMALS
 						})}
-						{$ckEthereumTwinToken.symbol}
+						{$ckEthereumNativeToken.symbol}
 					</span>
 				{/if}
 			</div>
