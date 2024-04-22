1a6e947fe5ca2460eb66e820786af3de5c3d45a2
David Dal Busco <david.dalbusco@outlook.com>
2024-04-22 14:45:45 +0200

    feat: convert ckUSDC to USDC

--- src/frontend/src/icp/components/send/IcSendProgress.svelte
+++ src/frontend/src/icp/components/send/IcSendProgress.svelte
@@ -5,6 +5,7 @@
 	import type { NetworkId } from '$lib/types/network';
 	import { isNetworkIdBTC, isNetworkIdETH } from '$icp/utils/ic-send.utils';
 	import { i18n } from '$lib/stores/i18n.store';
+	import { tokenCkErc20Ledger } from '$icp/derived/ic-token.derived';
 
 	export let sendProgressStep: string = SendIcStep.INITIALIZATION;
 	export let networkId: NetworkId | undefined = undefined;
@@ -16,10 +17,19 @@
 			text: $i18n.send.text.initializing_transaction,
 			state: 'in_progress'
 		} as ProgressStep,
+		...($tokenCkErc20Ledger
+			? [
+					{
+						step: SendIcStep.APPROVE_FEES,
+						text: $i18n.send.text.approving_fees,
+						state: 'next'
+					} as ProgressStep
+				]
+			: []),
 		...(isNetworkIdBTC(networkId) || isNetworkIdETH(networkId)
 			? [
 					{
-						step: SendIcStep.APPROVE,
+						step: SendIcStep.APPROVE_TRANSFER,
 						text: $i18n.send.text.approving,
 						state: 'next'
 					} as ProgressStep
