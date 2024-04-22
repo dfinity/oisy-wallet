25fa16909e41f2dd050c895eae9d7aef1f4b3dcd
David Dal Busco <david.dalbusco@dfinity.org>
2024-04-22 17:19:40 +0200

    feat: withdraw erc20 (#1210)
    * feat: withdraw erc20
    
    * docs: comment

--- src/frontend/src/icp/services/ck.services.ts
+++ src/frontend/src/icp/services/ck.services.ts
@@ -4,7 +4,7 @@ import {
 	STAGING_CKETH_LEDGER_CANISTER_ID
 } from '$env/networks.ircrc.env';
 import { retrieveBtc } from '$icp/api/ckbtc-minter.api';
-import { withdrawEth } from '$icp/api/cketh-minter.api';
+import { withdrawErc20, withdrawEth } from '$icp/api/cketh-minter.api';
 import { approve } from '$icp/api/icrc-ledger.api';
 import { CKERC20_TO_ERC20_MAX_TRANSACTION_FEE } from '$icp/constants/cketh.constants';
 import type { IcCanisters, IcCkMetadata, IcCkToken } from '$icp/types/ic';
@@ -49,31 +49,38 @@ export const convertCkBTCToBtc = async ({
 	});
 };
 
+/**
+ * Withdrawal ckErc20 to Erc20.
+ *
+ * {@link https://github.com/dfinity/ic/blob/master/rs/ethereum/cketh/docs/ckerc20.adoc#withdrawal-ckerc20-to-erc20}
+ */
 export const convertCkErc20ToErc20 = async ({
 	token,
 	progress,
 	identity,
 	to,
-	...rest
+	amount: amountBigNumber
 }: IcTransferParams & {
 	token: IcCkToken;
 }): Promise<void> => {
-	const { minterCanisterId } = token;
+	const { minterCanisterId, ledgerCanisterId } = token;
 
 	assertNonNullish(minterCanisterId, get(i18n).init.error.minter_ckerc20_erc20);
 
 	// TODO: this is relatively ugly. We should find a way to derive those information cleany.
 
-	const ledgerCanisterId = LOCAL
+	const ckEthledgerCanisterId = LOCAL
 		? LOCAL_CKETH_LEDGER_CANISTER_ID
 		: STAGING
 			? STAGING_CKETH_LEDGER_CANISTER_ID
 			: IC_CKETH_LEDGER_CANISTER_ID;
 
-	assertNonNullish(ledgerCanisterId, get(i18n).init.error.ledger_cketh_eth);
+	assertNonNullish(ckEthledgerCanisterId, get(i18n).init.error.ledger_cketh_eth);
+
+	// 1. Approve fees on ckETH Ledger for minter
 
 	await approveTransfer({
-		canisters: { ledgerCanisterId, minterCanisterId },
+		canisters: { ledgerCanisterId: ckEthledgerCanisterId, minterCanisterId },
 		identity,
 		progress,
 		progressStep: SendIcStep.APPROVE_FEES,
@@ -81,12 +88,28 @@ export const convertCkErc20ToErc20 = async ({
 		to
 	});
 
-	await convertCkETHToEth({
-		token,
-		progress,
+	// 2. Approve amount on ckErc20 Ledger for minter
+
+	const amount = amountBigNumber.toBigInt();
+
+	await approveTransfer({
+		canisters: { ledgerCanisterId, minterCanisterId },
 		identity,
-		to,
-		...rest
+		progress,
+		amount,
+		to
+	});
+
+	// 3. Withdraw Erc20
+
+	progress(SendIcStep.SEND);
+
+	await withdrawErc20({
+		identity,
+		minterCanisterId,
+		ledgerCanisterId,
+		address: to,
+		amount
 	});
 };
 
