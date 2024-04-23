3d1abd2867edd1738f233ee8eed366e70892f8e2
David Dal Busco <david.dalbusco@dfinity.org>
2024-04-23 14:25:43 +0200

    feat: avoid input glitch (#1218)
    * feat: avoid input glitch
    
    * chore: lint

--- src/frontend/src/icp/components/send/IcSendAmount.svelte
+++ src/frontend/src/icp/components/send/IcSendAmount.svelte
@@ -12,13 +12,12 @@
 	import { assertCkBTCUserInputAmount } from '$icp/utils/ckbtc.utils';
 	import { IcAmountAssertionError } from '$icp/types/ic-send';
 	import { ckBtcMinterInfoStore } from '$icp/stores/ckbtc.store';
-	import { assertCkETHMinWithdrawalAmount } from '$icp/utils/cketh.utils';
+	import { assertCkETHMinFee, assertCkETHMinWithdrawalAmount } from '$icp/utils/cketh.utils';
 	import { isNetworkIdEthereum } from '$lib/utils/network.utils';
 	import { isNetworkIdBTC } from '$icp/utils/ic-send.utils';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { ckEthMinterInfoStore } from '$icp-eth/stores/cketh.store';
 	import { tokenCkEthLedger } from '$icp/derived/ic-token.derived';
-	import { replacePlaceholders } from '$lib/utils/i18n.utils';
 
 	export let amount: number | undefined = undefined;
 	export let amountError: IcAmountAssertionError | undefined;
@@ -70,12 +69,13 @@
 			}
 		}
 
-		if (isNetworkIdEthereum(networkId) && BigNumber.from(fee).gt(value)) {
-			amountError = new IcAmountAssertionError(
-				replacePlaceholders($i18n.send.assertion.minimum_ledger_fees, {
-					$symbol: $tokenSymbol
-				})
-			);
+		if (isNetworkIdEthereum(networkId)) {
+			amountError = assertCkETHMinFee({
+				amount: value,
+				tokenSymbol: $tokenSymbol,
+				fee,
+				i18n: $i18n
+			});
 			return;
 		}
 
