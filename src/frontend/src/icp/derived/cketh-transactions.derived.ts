49aad278bd6de055e1067fbf86fa2e2d163f422e
David Dal Busco <david.dalbusco@dfinity.org>
2024-04-23 19:16:24 +0200

    feat: derive pending transactions ckErc20 (#1225)

--- src/frontend/src/icp/derived/cketh-transactions.derived.ts
+++ src/frontend/src/icp/derived/cketh-transactions.derived.ts
@@ -2,14 +2,14 @@ import {
 	icPendingTransactionsStore,
 	type IcPendingTransactionsData
 } from '$icp/stores/ic-pending-transactions.store';
-import { isTokenCkEthLedger } from '$icp/utils/ic-send.utils';
+import {isTokenCkErc20Ledger, isTokenCkEthLedger} from '$icp/utils/ic-send.utils';
 import { token } from '$lib/derived/token.derived';
 import { derived, type Readable } from 'svelte/store';
 
 export const ckEthPendingTransactions: Readable<IcPendingTransactionsData> = derived(
 	[token, icPendingTransactionsStore],
 	([$token, $convertEthToCkEthPendingStore]) => {
-		if (!isTokenCkEthLedger($token)) {
+		if (!isTokenCkEthLedger($token) && !isTokenCkErc20Ledger($token)) {
 			return [];
 		}
 
