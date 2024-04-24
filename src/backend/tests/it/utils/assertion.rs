7aff56f8035e4bef9442ee776abc2ec60804b2b7
David Dal Busco <david.dalbusco@dfinity.org>
2024-04-24 14:21:14 +0200

    test: improve assertions version and debug (#1150)
    * test: debug print token if assertion fails
    
    * test: version for usertoken more accuratly
    
    * test: version for customtoken more accuratly
    
    * test: remove assertions
    
    * test: remove unused
    
    * chore: cleanup

--- src/backend/tests/it/utils/assertion.rs
+++ src/backend/tests/it/utils/assertion.rs
@@ -1,15 +1,19 @@
 use shared::types::custom_token::CustomToken;
 use shared::types::token::UserToken;
-use shared::types::TokenVersion;
 
-pub fn assert_tokens_eq(results_tokens: &[UserToken], expected_tokens: &[UserToken]) {
-    assert_eq!(results_tokens.len(), expected_tokens.len());
+pub fn assert_tokens_data_eq(results_tokens: &[UserToken], expected_tokens: &[UserToken]) {
+    assert_eq!(
+        results_tokens.len(),
+        expected_tokens.len(),
+        "The number of tokens differ."
+    );
 
     for (token, expected) in results_tokens.iter().zip(expected_tokens.iter()) {
-        assert_eq!(token.contract_address, expected.contract_address);
-        assert_eq!(token.chain_id, expected.chain_id);
-        assert_eq!(token.symbol, expected.symbol);
-        assert_eq!(token.decimals, expected.decimals);
+        assert_eq!(
+            token, expected,
+            "Result token differs from expected token: {:?} vs {:?}",
+            token, expected
+        );
     }
 }
 
@@ -17,28 +21,17 @@ pub fn assert_custom_tokens_eq(
     results_tokens: Vec<CustomToken>,
     expected_tokens: Vec<CustomToken>,
 ) {
-    assert_eq!(results_tokens.len(), expected_tokens.len());
+    assert_eq!(
+        results_tokens.len(),
+        expected_tokens.len(),
+        "The number of custom tokens differ."
+    );
 
     for (token, expected) in results_tokens.iter().zip(expected_tokens.iter()) {
-        assert!(expected.token == token.token);
-        assert_eq!(expected.enabled, token.enabled);
-    }
-}
-
-pub fn assert_some_tokens_version<T>(tokens: &[T])
-where
-    T: TokenVersion,
-{
-    for token in tokens.iter() {
-        assert!(token.get_version().is_some());
-    }
-}
-
-pub fn assert_none_tokens_version<T>(tokens: Vec<T>)
-where
-    T: TokenVersion,
-{
-    for token in tokens.iter() {
-        assert!(token.get_version().is_none());
+        assert_eq!(
+            token, expected,
+            "Result custom token differs from expected custom token: {:?} vs {:?}",
+            token, expected
+        );
     }
 }
