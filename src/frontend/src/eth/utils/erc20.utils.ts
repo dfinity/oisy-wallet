d9a647657d9c13e7e931d5b4edf19a62d9764b67
David Dal Busco <david.dalbusco@dfinity.org>
2024-04-24 11:53:57 +0200

    feat: redo usdc logo (#1233)

--- src/frontend/src/eth/utils/erc20.utils.ts
+++ src/frontend/src/eth/utils/erc20.utils.ts
@@ -2,6 +2,7 @@ import oneInch from '$eth/assets/1inch.svg';
 import dai from '$eth/assets/dai.svg';
 import icpDark from '$eth/assets/icp_dark.svg';
 import uniswap from '$eth/assets/uniswap.svg';
+import usdc from '$eth/assets/usdc.svg';
 import usdt from '$eth/assets/usdt.svg';
 import type { Erc20Contract, Erc20Metadata, Erc20Token } from '$eth/types/erc20';
 import type { EthereumNetwork } from '$eth/types/network';
@@ -27,6 +28,9 @@ const mapErc20Icon = (symbol: string): string | undefined => {
 	switch (symbol.toLowerCase()) {
 		case 'uni':
 			return uniswap;
+		case 'usdc':
+			// TODO: to be removed when USDC with twin token is proposed on mainnet
+			return usdc;
 		case 'usdt':
 			return usdt;
 		case 'dai':
