015e0abab701e683ddc7a6b761f1b8c30edd046b
David Dal Busco <david.dalbusco@outlook.com>
2024-04-24 13:28:46 +0200

    feat: decode approve data to display value

--- src/frontend/src/icp-eth/utils/cketh-transactions.utils.ts
+++ src/frontend/src/icp-eth/utils/cketh-transactions.utils.ts
@@ -1,25 +1,52 @@
 import type { EthereumNetwork } from '$eth/types/network';
-import { decodeErc20AbiDataValue, isErc20TransactionApprove } from '$eth/utils/transactions.utils';
 import type { IcCkTwinToken, IcToken, IcTransactionUi } from '$icp/types/ic';
 import { i18n } from '$lib/stores/i18n.store';
 import { replacePlaceholders } from '$lib/utils/i18n.utils';
+import { nonNullish } from '@dfinity/utils';
+import type { BigNumber } from '@ethersproject/bignumber';
 import type { Transaction } from '@ethersproject/transactions';
+import { ethers } from 'ethers';
 import { get } from 'svelte/store';
 
-export const mapCkEthereumPendingTransaction = ({
-	transaction: { hash, from, to, value, data },
-	twinToken,
-	token
-}: {
+export type MapCkEthereumPendingTransactionParams = {
 	transaction: Transaction;
 	token: IcToken;
+} & IcCkTwinToken;
+
+export const mapCkEthPendingTransaction = ({
+	transaction: { value, ...transaction },
+	...rest
+}: MapCkEthereumPendingTransactionParams): IcTransactionUi =>
+	mapPendingTransaction({
+		transaction,
+		value,
+		...rest
+	});
+
+export const mapCkErc20PendingTransaction = ({
+	transaction: { data, value, ...transaction },
+	...rest
+}: MapCkEthereumPendingTransactionParams): IcTransactionUi =>
+	mapPendingTransaction({
+		transaction,
+		value: nonNullish(data) ? decodeCkErc20DepositAbiDataValue(data) : value,
+		...rest
+	});
+
+const mapPendingTransaction = ({
+	transaction: { hash, from, to },
+	twinToken,
+	token,
+	value
+}: {
+	transaction: Omit<Transaction, 'value' | 'data'>;
+	token: IcToken;
+	value: BigNumber;
 } & IcCkTwinToken): IcTransactionUi => {
 	const explorerUrl = (twinToken.network as EthereumNetwork).explorerUrl;
 
 	const { symbol: twinTokenSymbol } = twinToken;
 	const { symbol } = token;
-
-	const erc20Approve = isErc20TransactionApprove(data);
 
 	const {
 		transaction: {
@@ -38,9 +65,19 @@ export const mapCkEthereumPendingTransaction = ({
 			$token: twinTokenSymbol,
 			$ckToken: symbol
 		}),
-		value: (erc20Approve ? decodeErc20AbiDataValue(data) : value).toBigInt(),
+		value: value.toBigInt(),
 		fromExplorerUrl: `${explorerUrl}/address/${from}`,
 		toExplorerUrl: `${explorerUrl}/address/${to}`,
 		txExplorerUrl: `${explorerUrl}/tx/${hash}`
 	};
+};
+
+const decodeCkErc20DepositAbiDataValue = (data: string): BigNumber => {
+	// Types are equals to the internalTypes of the CKERC20_ABI for the deposit
+	const [_to, value] = ethers.utils.defaultAbiCoder.decode(
+		['address', 'uint256', 'bytes32'],
+		ethers.utils.hexDataSlice(data, 4)
+	);
+
+	return value;
 };
