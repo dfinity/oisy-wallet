c4ed92bd7829a8540f5826bf8a0b86001ff59831
David Dal Busco <david.dalbusco@dfinity.org>
2024-04-24 13:31:51 +0200

    feat: decode erc20 approve data to display pending ckErc20 value (#1236)
    * feat: decode erc20 approve data to display pending ckErc20 value
    
    * chore: redo
    
    * chore: redo
    
    * feat: decode approve data to display value

--- src/frontend/src/icp-eth/utils/cketh-transactions.utils.ts
+++ src/frontend/src/icp-eth/utils/cketh-transactions.utils.ts
@@ -1,17 +1,47 @@
 import type { EthereumNetwork } from '$eth/types/network';
 import type { IcCkTwinToken, IcToken, IcTransactionUi } from '$icp/types/ic';
 import { i18n } from '$lib/stores/i18n.store';
-import type { Transaction } from '$lib/types/transaction';
 import { replacePlaceholders } from '$lib/utils/i18n.utils';
+import { nonNullish } from '@dfinity/utils';
+import type { BigNumber } from '@ethersproject/bignumber';
+import type { Transaction } from '@ethersproject/transactions';
+import { ethers } from 'ethers';
 import { get } from 'svelte/store';
 
-export const mapCkEthereumPendingTransaction = ({
-	transaction: { hash, from, to, value },
-	twinToken,
-	token
-}: {
+export type MapCkEthereumPendingTransactionParams = {
 	transaction: Transaction;
 	token: IcToken;
+} & IcCkTwinToken;
+
+export const mapCkEthPendingTransaction = ({
+	transaction: { value, ...transaction },
+	...rest
+}: MapCkEthereumPendingTransactionParams): IcTransactionUi =>
+	mapPendingTransaction({
+		transaction,
+		value,
+		...rest
+	});
+
+export const mapCkErc20PendingTransaction = ({
+	transaction: { data, value, ...transaction },
+	...rest
+}: MapCkEthereumPendingTransactionParams): IcTransactionUi =>
+	mapPendingTransaction({
+		transaction,
+		value: nonNullish(data) ? decodeCkErc20DepositAbiDataValue(data) : value,
+		...rest
+	});
+
+const mapPendingTransaction = ({
+	transaction: { hash, from, to },
+	twinToken,
+	token,
+	value
+}: {
+	transaction: Omit<Transaction, 'value' | 'data'>;
+	token: IcToken;
+	value: BigNumber;
 } & IcCkTwinToken): IcTransactionUi => {
 	const explorerUrl = (twinToken.network as EthereumNetwork).explorerUrl;
 
@@ -40,4 +70,14 @@ export const mapCkEthereumPendingTransaction = ({
 		toExplorerUrl: `${explorerUrl}/address/${to}`,
 		txExplorerUrl: `${explorerUrl}/tx/${hash}`
 	};
+};
+
+const decodeCkErc20DepositAbiDataValue = (data: string): BigNumber => {
+	// Types are equals to the internalTypes of the CKERC20_ABI for the deposit
+	const [_to, value] = ethers.utils.defaultAbiCoder.decode(
+		['address', 'uint256', 'bytes32'],
+		ethers.utils.hexDataSlice(data, 4)
+	);
+
+	return value;
 };
