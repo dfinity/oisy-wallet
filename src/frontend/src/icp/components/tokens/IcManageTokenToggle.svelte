48d0453cc3ae6f4aab0fa10d7ada8d81cdc3933d
David Dal Busco <david.dalbusco@dfinity.org>
2024-04-29 07:58:09 +0200

    feat: show toast on disabled toggle (#1245)

--- src/frontend/src/icp/components/tokens/IcManageTokenToggle.svelte
+++ src/frontend/src/icp/components/tokens/IcManageTokenToggle.svelte
@@ -8,8 +8,11 @@
 
 	export let token: IcrcCustomToken;
 
+	let outdated = false;
+	$: outdated = token.indexCanisterVersion === 'outdated';
+
 	let disabled = false;
-	$: disabled = token.category === 'default';
+	$: disabled = token.category === 'default' || outdated;
 
 	let checked: boolean;
 	$: checked = token.enabled;
@@ -23,31 +26,34 @@
 
 		checked = !checked;
 
-		if (token.indexCanisterVersion === 'outdated') {
-			setTimeout(() => {
-				checked = !checked;
-
-				toastsShow({
-					text: replacePlaceholders($i18n.tokens.manage.info.outdated_index_canister, {
-						$token: token.name
-					}),
-					level: 'info',
-					duration: 5000
-				});
-			}, 500);
-			return;
-		}
-
 		dispatch('icToken', {
 			...token,
 			enabled: checked
 		});
 	};
+
+	const onClick = () => {
+		if (!outdated) {
+			return;
+		}
+
+		toastsShow({
+			text: replacePlaceholders($i18n.tokens.manage.info.outdated_index_canister, {
+				$token: token.name
+			}),
+			level: 'info',
+			duration: 5000
+		});
+	};
 </script>
 
-<Toggle
-	ariaLabel={$i18n.tokens.text.hide_zero_balances}
-	{disabled}
-	bind:checked
-	on:nnsToggle={toggle}
-/>
+<!-- svelte-ignore a11y-interactive-supports-focus -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class:outdated role="button" on:click={onClick}>
+	<Toggle
+		ariaLabel={$i18n.tokens.text.hide_zero_balances}
+		{disabled}
+		bind:checked
+		on:nnsToggle={toggle}
+	/>
+</div>
