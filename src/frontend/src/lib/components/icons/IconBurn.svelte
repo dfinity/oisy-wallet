531ce7105a65d7c6bb9ca738a4179603ea954aa9
David Dal Busco <david.dalbusco@dfinity.org>
2024-05-08 14:10:26 +0200

    feat: review icons (#1283)
    * feat: review icons
    
    * feat: review icons
    
    * chore: format
    
    * chore: tmp
    
    * chore: redo
    
    * feat: review receive icon

--- src/frontend/src/lib/components/icons/IconBurn.svelte
+++ src/frontend/src/lib/components/icons/IconBurn.svelte
@@ -7,10 +7,10 @@
 <svg
 	width={size}
 	height={size}
+	class={styleClass}
 	viewBox="0 0 24 24"
 	fill="none"
 	xmlns="http://www.w3.org/2000/svg"
-	class={styleClass}
 >
 	<path
 		d="M17 17V7M17 7H7M17 7L7 17"
