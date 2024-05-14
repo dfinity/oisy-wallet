14812c122ed17b4ecf2c61d5eeb0bbcf1f3ea571
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-05-14 15:08:12 +0200

    feat: created custom component SendInputAmount that has an optional Max button (#1293)

---
+++ src/frontend/src/lib/components/common/MaxButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { i18n } from '$lib/stores/i18n.store';
+
+	export let disabled = false;
+</script>
+
+<button
+	data-tid="max-button"
+	on:click|preventDefault
+	class="text-blue hover:text-dark-blue active:text-dark-blue font-medium"
+	{disabled}
+>
+	{$i18n.core.text.max}
+</button>
---
+++ src/frontend/src/lib/components/send/SendInputAmount.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import { Input } from '@dfinity/gix-components';
+	import { debounce, nonNullish } from '@dfinity/utils';
+	import { parseToken } from '$lib/utils/parse.utils';
+	import { slide } from 'svelte/transition';
+	import { i18n } from '$lib/stores/i18n.store';
+	import type { BigNumber } from '@ethersproject/bignumber';
+	import { invalidAmount } from '$lib/utils/input.utils';
+	import MaxButton from '$lib/components/common/MaxButton.svelte';
+
+	export let amount: number | undefined = undefined;
+	export let tokenDecimals: number | undefined = undefined;
+	export let placeholder: string = $i18n.core.text.amount;
+	export let customValidate: (userAmount: BigNumber) => Error | undefined = () => undefined;
+	export let calculateMax: (() => number | undefined) | undefined = undefined;
+
+	let error: Error | undefined;
+
+	let onMax = () => {
+		amount = calculateMax?.();
+	};
+
+	const validate = () => {
+		if (invalidAmount(amount)) {
+			error = undefined;
+			return;
+		}
+
+		const parsedValue = parseToken({
+			value: `${amount}`,
+			unitName: tokenDecimals
+		});
+
+		error = customValidate(parsedValue);
+	};
+
+	const debounceValidate = debounce(validate, 300);
+
+	$: amount, tokenDecimals, debounceValidate();
+</script>
+
+<label for="amount" class="font-bold px-4.5">{$i18n.core.text.amount}</label>
+<Input
+	name="amount"
+	inputType="currency"
+	required
+	bind:value={amount}
+	decimals={tokenDecimals}
+	{placeholder}
+	spellcheck={false}
+	testId="amount-input"
+>
+	<svelte:fragment slot="inner-end">
+		{#if nonNullish(calculateMax)}
+			<MaxButton on:click={onMax} />
+		{/if}
+	</svelte:fragment>
+</Input>
+
+{#if nonNullish(error)}
+	<p transition:slide={{ duration: 250 }} class="text-cyclamen pb-3">{error.message}</p>
+{/if}
