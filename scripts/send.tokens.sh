aede0c572113f068a74ffa0396f8712f24ad0c9d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-05-15 09:22:59 +0200

    refactor: changed send.tokens script to accept the principal as input

--- scripts/send.tokens.sh
+++ scripts/send.tokens.sh
@@ -3,7 +3,12 @@
 # Utility Script: Send Tokens to a Local User
 # Particularly handy after installing the canisters for the first time or being forced by dfx to clean the local state.
 
-PRINCIPAL=x4w27-so7wg-cudsa-yy7fh-wcpy5-njul4-q54tv-euzzi-tdnzz-ill46-zqe
+if [ -z "$1" ]; then
+  read -r -p "Enter the PRINCIPAL: " PRINCIPAL
+else
+  PRINCIPAL=$1
+fi
+
 DFX_NETWORK=local
 
 dfx canister call ckbtc_ledger --network "$DFX_NETWORK" icrc1_transfer "(record {from=null; to=record { owner= principal \"$PRINCIPAL\";}; amount=1000000000; fee=null; memo=null; created_at_time=null;})"
