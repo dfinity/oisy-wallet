effd9f18424e41eadab20e54f72bba5dee2fcc71
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-05-21 20:49:45 +0200

    feat: adapted component IcSendAmout to use the new common component SendInputAmount (#1317)

--- src/frontend/src/icp/components/send/IcSendAmount.svelte
+++ src/frontend/src/icp/components/send/IcSendAmount.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-	import { Input } from '@dfinity/gix-components';
-	import { slide } from 'svelte/transition';
-	import { debounce, isNullish, nonNullish } from '@dfinity/utils';
-	import { invalidAmount } from '$lib/utils/input.utils';
-	import { token, tokenDecimals, tokenId, tokenSymbol } from '$lib/derived/token.derived';
+	import { isNullish, nonNullish } from '@dfinity/utils';
+	import {
+		token,
+		tokenDecimals,
+		tokenId,
+		tokenSymbol,
+		tokenStandard
+	} from '$lib/derived/token.derived';
 	import type { IcToken } from '$icp/types/ic';
-	import { parseToken } from '$lib/utils/parse.utils';
 	import { balance } from '$lib/derived/balances.derived';
 	import { BigNumber } from '@ethersproject/bignumber';
 	import type { NetworkId } from '$lib/types/network';
@@ -19,6 +21,8 @@
 	import { ckEthMinterInfoStore } from '$icp-eth/stores/cketh.store';
 	import { tokenCkEthLedger } from '$icp/derived/ic-token.derived';
 	import { ckEthereumNativeTokenId } from '$icp-eth/derived/cketh.derived';
+	import SendInputAmount from '$lib/components/send/SendInputAmount.svelte';
+	import { getMaxTransactionAmount } from '$lib/utils/token.utils';
 
 	export let amount: number | undefined = undefined;
 	export let amountError: IcAmountAssertionError | undefined;
@@ -27,85 +31,70 @@
 	let fee: bigint | undefined;
 	$: fee = ($token as IcToken).fee;
 
-	const validate = () => {
-		if (invalidAmount(amount)) {
-			amountError = undefined;
-			return;
-		}
-
+	$: customValidate = (userAmount: BigNumber): Error | undefined => {
 		if (isNullish(fee)) {
-			amountError = undefined;
 			return;
 		}
-
-		const value = parseToken({
-			value: `${amount}`,
-			unitName: $tokenDecimals
-		});
 
 		if (isNetworkIdBTC(networkId)) {
-			amountError = assertCkBTCUserInputAmount({
-				amount: value,
+			let error = assertCkBTCUserInputAmount({
+				amount: userAmount,
 				minterInfo: $ckBtcMinterInfoStore?.[$tokenId],
 				tokenDecimals: $tokenDecimals,
 				i18n: $i18n
 			});
 
-			if (nonNullish(amountError)) {
-				return;
+			if (nonNullish(error)) {
+				return error;
 			}
 		}
 
 		if (isNetworkIdEthereum(networkId) && $tokenCkEthLedger) {
-			amountError = assertCkETHMinWithdrawalAmount({
-				amount: value,
+			let error = assertCkETHMinWithdrawalAmount({
+				amount: userAmount,
 				tokenDecimals: $tokenDecimals,
 				tokenSymbol: $tokenSymbol,
 				minterInfo: $ckEthMinterInfoStore?.[$ckEthereumNativeTokenId],
 				i18n: $i18n
 			});
 
-			if (nonNullish(amountError)) {
-				return;
+			if (nonNullish(error)) {
+				return error;
 			}
 		}
 
 		if (isNetworkIdEthereum(networkId)) {
-			amountError = assertCkETHMinFee({
-				amount: value,
+			return assertCkETHMinFee({
+				amount: userAmount,
 				tokenSymbol: $tokenSymbol,
 				fee,
 				i18n: $i18n
 			});
-			return;
 		}
 
-		const total = value.add(fee);
+		const total = userAmount.add(fee);
 
 		if (total.gt($balance ?? BigNumber.from(0n))) {
-			amountError = new IcAmountAssertionError($i18n.send.assertion.insufficient_funds);
-			return;
+			return new IcAmountAssertionError($i18n.send.assertion.insufficient_funds);
 		}
 
-		amountError = undefined;
+		return;
 	};
 
-	const debounceValidate = debounce(validate);
-
-	$: amount, fee, $ckBtcMinterInfoStore, $ckEthMinterInfoStore, debounceValidate();
+	$: calculateMax = (): number => {
+		return getMaxTransactionAmount({
+			balance: $balance?.toBigInt(),
+			fee: fee,
+			tokenDecimals: $tokenDecimals,
+			tokenStandard: $tokenStandard
+		});
+	};
 </script>
 
-<label for="amount" class="font-bold px-4.5">{$i18n.core.text.amount}:</label>
-<Input
-	name="amount"
-	inputType="currency"
-	required
-	bind:value={amount}
-	decimals={$tokenDecimals}
-	placeholder={$i18n.core.text.amount}
-	spellcheck={false}
+<SendInputAmount
+	bind:amount
+	tokenDecimals={$tokenDecimals}
+	{customValidate}
+	{calculateMax}
+	bind:error={amountError}
 />
-
-{#if nonNullish(amountError)}
-	<p transition:slide={{ duration: 250 }} class="text-cyclamen pb-3">{amountError.message}</p>
-{/if}
