4c785ef896b173e1ea517fa763d7c9b9b2e70467
David Dal Busco <david.dalbusco@outlook.com>
2024-05-21 13:42:16 +0200

    feat: generate tokens.ckerc20.json

--- scripts/build.tokens.ckerc20.mjs
+++ scripts/build.tokens.ckerc20.mjs
@@ -3,7 +3,9 @@
 import { AnonymousIdentity } from '@dfinity/agent';
 import { CkETHOrchestratorCanister } from '@dfinity/cketh';
 import { Principal } from '@dfinity/principal';
-import { createAgent, fromNullable, isNullish } from '@dfinity/utils';
+import { createAgent, fromNullable, isNullish, jsonReplacer } from '@dfinity/utils';
+import { writeFileSync } from 'node:fs';
+import { join } from 'node:path';
 
 const orchestratorInfo = async ({ orchestratorId: canisterId }) => {
 	const agent = await createAgent({
@@ -22,7 +24,7 @@ const orchestratorInfo = async ({ orchestratorId: canisterId }) => {
 const buildOrchestratorInfo = async (orchestratorId) => {
 	const { managed_canisters } = await orchestratorInfo({ orchestratorId });
 
-	const mapManagedCanisters = ({ ledger, index, ckerc20_token_symbol }) => {
+	const mapManagedCanisters = (acc, { ledger, index, ckerc20_token_symbol }) => {
 		const ledgerCanister = fromNullable(ledger);
 		const indexCanister = fromNullable(index);
 
@@ -36,24 +38,52 @@ const buildOrchestratorInfo = async (orchestratorId) => {
 			'Created' in indexCanister ? indexCanister.Created : indexCanister.Installed;
 
 		return {
-			ledgerCanisterId: ledgerCanisterId.toText(),
-			indexCanisterId: indexCanisterId.toText(),
-			ckerc20_token_symbol
+			...acc,
+			[ckerc20_token_symbol]: [
+				...(acc[ckerc20_token_symbol] ?? []),
+				{
+					ledgerCanisterId: ledgerCanisterId.toText(),
+					indexCanisterId: indexCanisterId.toText()
+				}
+			]
 		};
 	};
 
-	return managed_canisters.map(mapManagedCanisters);
+	const tokens = managed_canisters.reduce(mapManagedCanisters, {});
+
+	const assertUniqueTokenSymbol = Object.values(tokens).find((value) => value.length > 1);
+
+	if (assertUniqueTokenSymbol !== undefined) {
+		throw new Error(
+			`More than one pair of ledger and index canisters were used for the token symbol ${assertUniqueTokenSymbol}.`
+		);
+	}
+
+	return Object.entries(tokens).reduce(
+		(acc, [key, value]) => ({
+			...acc,
+			[key]: value[0]
+		}),
+		{}
+	);
 };
 
 const ORCHESTRATOR_STAGING_ID = Principal.fromText('2s5qh-7aaaa-aaaar-qadya-cai');
 const ORCHESTRATOR_PRODUCTION_ID = Principal.fromText('vxkom-oyaaa-aaaar-qafda-cai');
+
+const DATA_FOLDER = join(process.cwd(), 'src', 'frontend', 'src', 'env');
 
 const findCkErc20 = async () => {
 	const [staging, production] = await Promise.all(
 		[ORCHESTRATOR_STAGING_ID, ORCHESTRATOR_PRODUCTION_ID].map(buildOrchestratorInfo)
 	);
 
-	console.log(staging, production);
+	const tokens = {
+		production,
+		staging
+	};
+
+	writeFileSync(join(DATA_FOLDER, 'tokens.ckerc20.json'), JSON.stringify(tokens, jsonReplacer));
 };
 
 try {
