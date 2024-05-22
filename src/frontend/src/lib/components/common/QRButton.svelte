72efc679cd34af1b109234b5284ba116ec5828d4
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-05-22 12:45:44 +0200

    fix: improved aria-label as per review

--- src/frontend/src/lib/components/common/QRButton.svelte
+++ src/frontend/src/lib/components/common/QRButton.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import { IconQRCodeScanner } from '@dfinity/gix-components';
+	import { i18n } from '$lib/stores/i18n.store';
 </script>
 
 <button
 	type="button"
 	data-tid="qr-code-scanner-button"
 	on:click|preventDefault
-	aria-label="Scan QR code"
+	aria-label={$i18n.send.text.open_qr_modal}
 >
 	<IconQRCodeScanner />
 </button>
