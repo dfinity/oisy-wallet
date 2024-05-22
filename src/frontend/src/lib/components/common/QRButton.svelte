bb12ea0af6510c0c6cf5df2667722ba94c058463
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-05-22 08:18:03 +0200

    feat: created common QR button component

---
+++ src/frontend/src/lib/components/common/QRButton.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { IconQRCodeScanner } from '@dfinity/gix-components';
+</script>
+
+<button type="button" data-tid="qr-code-scanner-button" class="icon-only" on:click|preventDefault>
+	<IconQRCodeScanner />
+</button>
