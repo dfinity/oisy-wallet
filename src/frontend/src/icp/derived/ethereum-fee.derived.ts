e49c14f6d54d563168697c35f958cd6525508fbe
David Dal Busco <david.dalbusco@outlook.com>
2024-05-23 06:37:08 +0200

    feat: clean up tokenCkEth by deriving it globally

---
+++ src/frontend/src/icp/derived/ethereum-fee.derived.ts
@@ -0,0 +1,18 @@
+import { icrcTokens } from '$icp/derived/icrc.derived';
+import type { IcCkToken, IcToken } from '$icp/types/ic';
+import { token } from '$lib/derived/token.derived';
+import { nonNullish } from '@dfinity/utils';
+import { derived, type Readable } from 'svelte/store';
+
+/**
+ * Ethereum fee for converting ckErc20 in ckEth are paid in ckEth.
+ */
+export const ethereumFeeTokenCkEth: Readable<IcToken | undefined> = derived(
+	[token, icrcTokens],
+	([$token, $icrcTokens]) => {
+		const feeLedgerCanisterId = ($token as IcCkToken).feeLedgerCanisterId;
+		return nonNullish(feeLedgerCanisterId)
+			? $icrcTokens.find(({ ledgerCanisterId }) => ledgerCanisterId === feeLedgerCanisterId)
+			: undefined;
+	}
+);
