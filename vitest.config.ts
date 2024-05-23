814f67a0e07eb78fb541461ab11c7c54a3bae442
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-05-23 13:01:04 +0200

    test: created env variables useful for running tests with token stores

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { svelteTesting } from '@testing-library/svelte/vite';
+import { readFileSync } from 'node:fs';
+import { fileURLToPath } from 'node:url';
 import { resolve } from 'path';
 import type { UserConfig } from 'vite';
 import { defineConfig } from 'vitest/config';
+
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const { version } = JSON.parse(json);
+
+const network = process.env.DFX_NETWORK ?? 'local';
 
 export default defineConfig(
 	(): UserConfig => ({
@@ -38,6 +46,10 @@ export default defineConfig(
 					replacement: resolve(__dirname, 'src/frontend/src/env')
 				}
 			]
+		},
+		define: {
+			VITE_APP_VERSION: JSON.stringify(version),
+			VITE_DFX_NETWORK: JSON.stringify(network)
 		},
 		test: {
 			environment: 'jsdom',
