e84a7962c25dbffcdff3ad91f6e6982f7388084c
David Dal Busco <david.dalbusco@outlook.com>
2024-06-05 07:17:24 +0200

    feat: compare token with ID

--- src/frontend/src/eth/utils/qr-code.utils.ts
+++ src/frontend/src/eth/utils/qr-code.utils.ts
@@ -91,11 +91,7 @@ export const decodeQrCode = ({
 		return { status: 'token_incompatible' };
 	}
 
-	if (
-		token.symbol.toLowerCase() !== expectedToken.symbol.toLowerCase() &&
-		(token.network as EthereumNetwork).chainId.toString() !==
-			(expectedToken.network as EthereumNetwork).chainId.toString()
-	) {
+	if (token.id !== expectedToken.id) {
 		return { status: 'token_incompatible' };
 	}
 
