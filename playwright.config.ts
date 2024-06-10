8dee00d299a53d83395ee7d11a2899c4cb7f4c6e
David Dal Busco <david.dalbusco@outlook.com>
2024-06-10 08:22:42 +0200

    chore: init Playwright

---
+++ playwright.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+	webServer: {
+		command: 'npm run build && npm run preview',
+		port: 4173
+	},
+	testDir: 'e2e',
+	testMatch: ['**/*.e2e.ts', '**/*.spec.ts'],
+	use: {
+		testIdAttribute: 'data-tid',
+		trace: 'on'
+	},
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] }
+		},
+
+		{
+			name: 'firefox',
+			use: { ...devices['Desktop Firefox'] }
+		},
+
+		{
+			name: 'webkit',
+			use: { ...devices['Desktop Safari'] }
+		}
+	]
+});
