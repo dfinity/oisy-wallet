22a034e5839d5beef738528b41c4528f5a63e72a
David Dal Busco <david.dalbusco@dfinity.org>
2024-06-10 10:57:36 +0200

    chore: init Playwright (#1397)
    * chore: init Playwright
    
    * feat: playwright dev cmd single browser

---
+++ playwright.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig, devices } from '@playwright/test';
+
+const DEV = (process.env.NODE_ENV ?? 'production') === 'development';
+
+export default defineConfig({
+	webServer: {
+		command: 'npm run build && npm run preview',
+		port: 4173
+	},
+	testDir: 'e2e',
+	testMatch: ['**/*.e2e.ts', '**/*.spec.ts'],
+	use: {
+		testIdAttribute: 'data-tid',
+		trace: 'on'
+	},
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] }
+		},
+		...(!DEV
+			? [
+					{
+						name: 'firefox',
+						use: { ...devices['Desktop Firefox'] }
+					},
+
+					{
+						name: 'webkit',
+						use: { ...devices['Desktop Safari'] }
+					}
+				]
+			: [])
+	]
+});
