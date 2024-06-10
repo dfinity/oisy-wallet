30bc5d9257f69d5adf465ca5ee8683b39896cd99
David Dal Busco <david.dalbusco@outlook.com>
2024-06-10 11:05:55 +0200

    feat: lower case for consistency reason

--- e2e/homepage.spec.ts
+++ e2e/homepage.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from "@playwright/test";
 
 const testUrl = "/";
 
-test("Should display not logged in homepage", async ({ page }) => {
+test("should display not logged in homepage", async ({ page }) => {
     await page.goto(testUrl);
 
     await expect(page).toHaveScreenshot();
