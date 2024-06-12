ff52af11057ebb2088802c48a18281f18741082c
David Dal Busco <david.dalbusco@outlook.com>
2024-06-12 16:17:08 +0200

    feat: twinToken derive

--- src/frontend/src/icp/components/receive/IcReceiveCkEthereum.svelte
+++ src/frontend/src/icp/components/receive/IcReceiveCkEthereum.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
 	import { modalCkETHReceive } from '$lib/derived/modal.derived';
-	import {setContext} from 'svelte';
+	import { getContext, setContext } from 'svelte';
 	import { initSendContext, SEND_CONTEXT_KEY, type SendContext } from '$icp-eth/stores/send.store';
 	import { modalStore } from '$lib/stores/modal.store';
 	import IcReceiveCkEthereumModal from '$icp/components/receive/IcReceiveCkEthereumModal.svelte';
-	import ReceiveButton from '$lib/components/receive/ReceiveButton.svelte';
-	import { ckEthereumTwinToken } from '$icp-eth/derived/cketh.derived';
+	import {
+		RECEIVE_TOKEN_CONTEXT_KEY,
+		type ReceiveTokenContext
+	} from '$icp/stores/receive-token.store';
+	import type { Token } from '$lib/types/token';
+	import type { IcCkToken } from '$icp/types/ic';
+	import { ETHEREUM_TOKEN } from '$env/tokens.env';
 
 	export let compact = false;
 
 	const modalId = Symbol();
+
+	const { token } = getContext<ReceiveTokenContext>(RECEIVE_TOKEN_CONTEXT_KEY);
+
+	let twinToken: Token;
+	$: twinToken = ($token as IcCkToken)?.twinToken ?? ETHEREUM_TOKEN;
 
 	/**
 	 * Send modal context store
@@ -17,14 +27,14 @@
 
 	const { sendToken, ...rest } = initSendContext({
 		sendPurpose: 'convert-eth-to-cketh',
-		token: $ckEthereumTwinToken
+		token: twinToken
 	});
 	setContext<SendContext>(SEND_CONTEXT_KEY, {
 		sendToken,
 		...rest
 	});
 
-	$: sendToken.set($ckEthereumTwinToken);
+	$: sendToken.set(twinToken);
 </script>
 
 <ReceiveButton {compact} on:click={() => modalStore.openCkETHReceive(modalId)} />
