c27723e2fdafb13a2ba64bd1ba9aa9a02c47b95e
David Dal Busco <david.dalbusco@outlook.com>
2024-06-12 08:49:34 +0200

    chore: env variable to disable exchange

--- src/frontend/src/lib/components/exchange/ExchangeWorker.svelte
+++ src/frontend/src/lib/components/exchange/ExchangeWorker.svelte
@@ -3,12 +3,13 @@
 	import type { ExchangeWorker } from '$lib/services/worker.exchange.services';
 	import { initExchangeWorker } from '$lib/services/worker.exchange.services';
 	import { erc20TokensAddresses } from '$eth/derived/erc20.derived';
-	import { LOCAL } from '$lib/constants/app.constants';
 
 	let worker: ExchangeWorker | undefined;
 
+	const EXCHANGE_DISABLED = JSON.parse(import.meta.env.VITE_EXCHANGE_DISABLED ?? false) === true;
+
 	onMount(async () => {
-		if (LOCAL) {
+		if (EXCHANGE_DISABLED) {
 			// Using the exchange API during development if not necessary and given its limitation of calls per minute is annoying at it often throws errors on server reload.
 			return;
 		}
