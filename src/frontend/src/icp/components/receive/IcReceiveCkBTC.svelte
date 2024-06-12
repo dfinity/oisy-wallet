9800d5612c5635e16102351dd38a512bf4a549d0
David Dal Busco <david.dalbusco@outlook.com>
2024-06-12 12:59:44 +0200

    feat: load ckBTC minter info when opening Receive modal

--- src/frontend/src/icp/components/receive/IcReceiveCkBTC.svelte
+++ src/frontend/src/icp/components/receive/IcReceiveCkBTC.svelte
@@ -12,10 +12,23 @@
 		RECEIVE_TOKEN_CONTEXT_KEY,
 		type ReceiveTokenContext
 	} from '$icp/stores/receive-token.store';
+	import { initCkBTCMinterInfoWorker } from '$icp/services/worker.ck-minter-info.services';
+	import IcCkListener from '$icp/components/core/IcCkListener.svelte';
+	import { nonNullish } from '@dfinity/utils';
+	import { ckBtcMinterInfoStore } from '$icp/stores/ckbtc.store';
 
-	const { token } = getContext<ReceiveTokenContext>(RECEIVE_TOKEN_CONTEXT_KEY);
+	const { token, tokenId, twinToken } = getContext<ReceiveTokenContext>(RECEIVE_TOKEN_CONTEXT_KEY);
+
+	let minterInfoLoaded = false;
 
 	const openReceive = async () => {
+		// If the minter info has not been loaded yet, we attach the web worker to load that information.
+		// Imperatively loading that information has the downside that the user might have to wait a few seconds (busy screen) to access the information.
+		// There might also be a race condition when the minter information is called twice if the user clicks really, really quickly on the "Receive" button on a screen where the minter information is already loaded globally.
+		// However, this approach is advantageous in terms of performance, as we can assume most users won't access the "Receive" modal for ckBTC that often, potentially sparing many calls.
+		// Reusing the web worker is also beneficial if loading the ckBTC minter information ever becomes scheduled again.
+		minterInfoLoaded = nonNullish($ckBtcMinterInfoStore?.[$tokenId]);
+
 		await loadAllCkBtcInfo({
 			...($token as IcToken),
 			identity: $authStore.identity
@@ -29,6 +42,10 @@
 <svelte:window on:oisyReceiveCkBTC={openReceive} />
 
 <IcReceiveButton on:click={async () => await openReceive()} />
+
+{#if !minterInfoLoaded && nonNullish($twinToken)}
+	<IcCkListener initFn={initCkBTCMinterInfoWorker} token={$token} twinToken={$twinToken} />
+{/if}
 
 {#if $modalCkBTCReceive}
 	<IcReceiveModal infoCmp={IcReceiveInfoCkBTC} />
--- src/frontend/src/icp/stores/receive-token.store.ts
+++ src/frontend/src/icp/stores/receive-token.store.ts
@@ -1,4 +1,4 @@
-import type { IcToken } from '$icp/types/ic';
+import type {IcCkToken, IcToken} from '$icp/types/ic';
 import type { Token, TokenId, TokenStandard } from '$lib/types/token';
 import { derived, writable, type Readable } from 'svelte/store';
 
@@ -24,14 +24,16 @@ export const initReceiveTokenContext = (token: Token): ReceiveTokenContext => {
 	const tokenStore = initReceiveTokenStore(token as IcToken);
 	const tokenId = derived(tokenStore, ({ id }) => id);
 	const tokenStandard = derived(tokenStore, ({ standard }) => standard);
+	const twinToken = derived(tokenStore,token => (token as IcCkToken).twinToken)
 
-	return { token: tokenStore, tokenId, tokenStandard };
+	return { token: tokenStore, tokenId, tokenStandard, twinToken };
 };
 
 export interface ReceiveTokenContext {
 	token: ReceiveTokenStore;
 	tokenId: Readable<TokenId>;
 	tokenStandard: Readable<TokenStandard>;
+	twinToken: Readable<Token | undefined>;
 }
 
 export const RECEIVE_TOKEN_CONTEXT_KEY = Symbol('receive-token');
