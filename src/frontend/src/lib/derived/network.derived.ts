d39a7df73dd6c5835bc96077c0ef037903d5bbe5
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-06-12 09:27:14 -0300

    refactor: according to review, created utils isNetworkIdChainFusion

--- src/frontend/src/lib/derived/network.derived.ts
+++ src/frontend/src/lib/derived/network.derived.ts
@@ -1,4 +1,3 @@
-import { CHAIN_FUSION_NETWORKS_IDS } from '$env/networks.env';
 import { icrcAccountIdentifierText } from '$icp/derived/ic.derived';
 import { DEFAULT_NETWORK, DEFAULT_NETWORK_ID } from '$lib/constants/networks.constants';
 import { address } from '$lib/derived/address.derived';
@@ -8,7 +7,11 @@ import { tokens } from '$lib/derived/tokens.derived';
 import type { OptionAddress } from '$lib/types/address';
 import type { Network, NetworkId } from '$lib/types/network';
 import type { Token } from '$lib/types/token';
-import { isNetworkIdEthereum, isNetworkIdICP } from '$lib/utils/network.utils';
+import {
+	isNetworkIdChainFusion,
+	isNetworkIdEthereum,
+	isNetworkIdICP
+} from '$lib/utils/network.utils';
 import { nonNullish } from '@dfinity/utils';
 import { derived, type Readable } from 'svelte/store';
 
@@ -27,12 +30,12 @@ export const selectedNetwork: Readable<Network> = derived(
 
 export const networkTokens: Readable<Token[]> = derived(
 	[tokens, selectedNetwork],
-	([$tokens, $selectedNetwork]) => {
-		if (CHAIN_FUSION_NETWORKS_IDS.includes($selectedNetwork.id)) {
-			return $tokens.filter(({ network: { env } }) => env === $selectedNetwork.env);
-		}
-		return $tokens.filter(({ network: { id } }) => id === $selectedNetwork.id);
-	}
+	([$tokens, $selectedNetwork]) =>
+		$tokens.filter(
+			({ network: { id, env } }) =>
+				(isNetworkIdChainFusion($selectedNetwork.id) && env === $selectedNetwork.env) ||
+				id === $selectedNetwork.id
+		)
 );
 
 export const networkICP: Readable<boolean> = derived([networkId], ([$networkId]) =>
