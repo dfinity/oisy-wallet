d4832188e650351d5c6e1e8f893a605e684cc9da
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-06-13 05:52:20 -0300

    Revert "style: create optional sub-logo for network in Logo component"
    This reverts commit e81429abfa4f02e67f83bf00c9caa6aeea8bef3b.

--- src/frontend/src/lib/components/ui/Logo.svelte
+++ src/frontend/src/lib/components/ui/Logo.svelte
@@ -2,14 +2,11 @@
 	import Img from '$lib/components/ui/Img.svelte';
 	import { isNullish, nonNullish } from '@dfinity/utils';
 	import IconRandom from '$lib/components/icons/IconRandom.svelte';
-	import Logo from '$lib/components/ui/Logo.svelte';
 
 	export let src: string | undefined;
 	export let alt = '';
 	export let size: string;
 	export let color: 'dust' | 'off-white' | 'white' = 'dust';
-	export let cornerLogoSrc: string | undefined = undefined;
-	export let cornerLogoAlt = '';
 
 	let loaded = false;
 
@@ -27,7 +24,7 @@
 </script>
 
 <div
-	class="relative flex items-center justify-center rounded-full"
+	class="flex items-center justify-center rounded-full overflow-hidden"
 	class:bg-dust={color === 'dust'}
 	class:bg-off-white={color === 'off-white'}
 	class:bg-white={color === 'white'}
@@ -48,15 +45,5 @@
 		/>
 	{:else}
 		<IconRandom {size} text={alt} />
-	{/if}
-	{#if nonNullish(cornerLogoSrc)}
-		<div class="absolute bottom-0 right-0">
-			<Logo
-				src={cornerLogoSrc}
-				alt={cornerLogoAlt}
-				size={`calc(${size} * 20 / 52)`}
-				color="white"
-			/>
-		</div>
 	{/if}
 </div>
