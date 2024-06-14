f978099aba520758d18966226bcb99d1062b9e0a
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-06-14 08:42:03 -0300

    feat: include list of BTC tokens in tokens store, mirroring ETH structure (#1391)
    * feat: include list of BTC tokens in tokens store, mirroring ETH structure
    
    * temp: created env variable to enable/disable BTC features
    
    * Update tokens.derived.ts
    
    * refactor: renamed flag env variable to NETWORK_BITCOIN_ENABLED
    
    * chore: include new env variable VITE_BITCOIN_MAINNET in .env files

--- src/frontend/src/env/networks.btc.env.ts
+++ src/frontend/src/env/networks.btc.env.ts
@@ -14,3 +14,10 @@ export const BITCOIN_CANISTER_IDS: Record<MinterCanisterIdText, CanisterIdText> 
 		[IC_CKBTC_MINTER_CANISTER_ID]: 'ghsi2-tqaaa-aaaan-aaaca-cai'
 	})
 };
+
+export const BTC_MAINNET_ENABLED =
+	JSON.parse(import.meta.env.VITE_BITCOIN_MAINNET ?? false) === true;
+
+// TODO: to be removed when we have finished implementing all the Bitcoin features
+export const NETWORK_BITCOIN_ENABLED =
+	JSON.parse(import.meta.env.VITE_NETWORK_BITCOIN_ENABLED ?? false) === true;
