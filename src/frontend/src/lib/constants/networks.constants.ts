4b33e460b1f69b2516ead604f0a875afcb2223f6
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-06-14 08:30:44 -0300

    chore: changed DEFAULT_NETWORK to use Chain Fusion networks

--- src/frontend/src/lib/constants/networks.constants.ts
+++ src/frontend/src/lib/constants/networks.constants.ts
@@ -1,4 +1,5 @@
-import { SUPPORTED_ETHEREUM_NETWORKS } from '$env/networks.env';
+import { CHAIN_FUSION_NETWORKS, SUPPORTED_ETHEREUM_NETWORKS } from '$env/networks.env';
 
-export const [DEFAULT_NETWORK, _rest] = SUPPORTED_ETHEREUM_NETWORKS;
+// TODO: remove SUPPORTED_ETHEREUM_NETWORKS fallback when we have finished implementing the single-page view with Chain Fusion
+export const [DEFAULT_NETWORK] = [...CHAIN_FUSION_NETWORKS, ...SUPPORTED_ETHEREUM_NETWORKS];
 export const { id: DEFAULT_NETWORK_ID } = DEFAULT_NETWORK;
