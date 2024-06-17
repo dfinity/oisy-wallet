078b21d0bd7e3b65b383fb3068aa527b660a0efd
David Dal Busco <david.dalbusco@outlook.com>
2024-06-17 06:51:23 +0200

    feat: remove Chain Fusion network

--- src/frontend/src/lib/utils/network.utils.ts
+++ src/frontend/src/lib/utils/network.utils.ts
@@ -1,6 +1,5 @@
 import {
 	BITCOIN_NETWORKS_IDS,
-	CHAIN_FUSION_NETWORKS_IDS,
 	ICP_NETWORK_ID,
 	SUPPORTED_ETHEREUM_NETWORKS_IDS
 } from '$env/networks.env';
@@ -9,13 +8,11 @@ import { nonNullish } from '@dfinity/utils';
 
 export const isNetworkICP = ({ id }: Network): boolean => isNetworkIdICP(id);
 
-export const isNetworkIdICP = (id: NetworkId): boolean => ICP_NETWORK_ID === id;
+export const isNetworkIdICP = (id: NetworkId | undefined): boolean =>
+	nonNullish(id) && ICP_NETWORK_ID === id;
 
 export const isNetworkIdEthereum = (id: NetworkId | undefined): boolean =>
 	nonNullish(id) && SUPPORTED_ETHEREUM_NETWORKS_IDS.includes(id);
 
 export const isNetworkIdBitcoin = (id: NetworkId | undefined): boolean =>
 	nonNullish(id) && BITCOIN_NETWORKS_IDS.includes(id);
-
-export const isNetworkIdChainFusion = (id: NetworkId | undefined): boolean =>
-	nonNullish(id) && CHAIN_FUSION_NETWORKS_IDS.includes(id);
