ff9dd2d98578f7b945c9ebdfb54ab908a012da21
David Dal Busco <david.dalbusco@outlook.com>
2024-06-17 16:30:14 +0200

    chore: format

--- src/frontend/src/lib/derived/page-token.derived.ts
+++ src/frontend/src/lib/derived/page-token.derived.ts
@@ -1,27 +1,27 @@
-import {derived, type Readable} from "svelte/store";
-import type {OptionToken} from "$lib/types/token";
-import {routeToken} from "$lib/derived/nav.derived";
-import {erc20Tokens} from "$eth/derived/erc20.derived";
-import {icrcTokens} from "$icp/derived/icrc.derived";
-import {isNullish} from "@dfinity/utils";
-import {ETHEREUM_TOKEN, ICP_TOKEN, SEPOLIA_TOKEN} from "$env/tokens.env";
+import { ETHEREUM_TOKEN, ICP_TOKEN, SEPOLIA_TOKEN } from '$env/tokens.env';
+import { erc20Tokens } from '$eth/derived/erc20.derived';
+import { icrcTokens } from '$icp/derived/icrc.derived';
+import { routeToken } from '$lib/derived/nav.derived';
+import type { OptionToken } from '$lib/types/token';
+import { isNullish } from '@dfinity/utils';
+import { derived, type Readable } from 'svelte/store';
 
 /**
  * A token derived from the route URL - i.e. if the URL contains a query parameters "token", then this store tries to derive the object from it.
  */
 export const pageToken: Readable<OptionToken> = derived(
-    [routeToken, erc20Tokens, icrcTokens],
-    ([$routeToken, $erc20Tokens, $icrcTokens]) => {
-        if (isNullish($routeToken)) {
-            return undefined;
-        }
+	[routeToken, erc20Tokens, icrcTokens],
+	([$routeToken, $erc20Tokens, $icrcTokens]) => {
+		if (isNullish($routeToken)) {
+			return undefined;
+		}
 
-        if ($routeToken === ICP_TOKEN.name) {
-            return ICP_TOKEN;
-        }
+		if ($routeToken === ICP_TOKEN.name) {
+			return ICP_TOKEN;
+		}
 
-        return [...$erc20Tokens, ...$icrcTokens, ETHEREUM_TOKEN, SEPOLIA_TOKEN].find(
-            ({ name }) => name === $routeToken
-        );
-    }
+		return [...$erc20Tokens, ...$icrcTokens, ETHEREUM_TOKEN, SEPOLIA_TOKEN].find(
+			({ name }) => name === $routeToken
+		);
+	}
 );
