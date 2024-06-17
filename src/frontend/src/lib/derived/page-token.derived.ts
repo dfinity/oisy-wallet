8f3a218908ed3322a3292d84be05feda50ac4a2a
David Dal Busco <david.dalbusco@outlook.com>
2024-06-17 16:19:21 +0200

    feat: writable token store

---
+++ src/frontend/src/lib/derived/page-token.derived.ts
@@ -0,0 +1,27 @@
+import {derived, type Readable} from "svelte/store";
+import type {OptionToken} from "$lib/types/token";
+import {routeToken} from "$lib/derived/nav.derived";
+import {erc20Tokens} from "$eth/derived/erc20.derived";
+import {icrcTokens} from "$icp/derived/icrc.derived";
+import {isNullish} from "@dfinity/utils";
+import {ETHEREUM_TOKEN, ICP_TOKEN, SEPOLIA_TOKEN} from "$env/tokens.env";
+
+/**
+ * A token derived from the route URL - i.e. if the URL contains a query parameters "token", then this store tries to derive the object from it.
+ */
+export const pageToken: Readable<OptionToken> = derived(
+    [routeToken, erc20Tokens, icrcTokens],
+    ([$routeToken, $erc20Tokens, $icrcTokens]) => {
+        if (isNullish($routeToken)) {
+            return undefined;
+        }
+
+        if ($routeToken === ICP_TOKEN.name) {
+            return ICP_TOKEN;
+        }
+
+        return [...$erc20Tokens, ...$icrcTokens, ETHEREUM_TOKEN, SEPOLIA_TOKEN].find(
+            ({ name }) => name === $routeToken
+        );
+    }
+);
