aaf7dfee2027bdcaf632774589b94c15f91c07b0
David Dal Busco <david.dalbusco@dfinity.org>
2024-06-17 10:15:30 +0200

    feat: remove Chain Fusion network (#1441)
    * feat: remove Chain Fusion network
    
    * feat: filter testnet

--- src/frontend/src/lib/derived/network-tokens.derived.ts
+++ src/frontend/src/lib/derived/network-tokens.derived.ts
@@ -2,7 +2,7 @@ import { isTokenIcrcTestnet } from '$icp/utils/icrc-ledger.utils';
 import { selectedNetwork } from '$lib/derived/network.derived';
 import { tokens } from '$lib/derived/tokens.derived';
 import type { Token } from '$lib/types/token';
-import { isNetworkIdChainFusion } from '$lib/utils/network.utils';
+import { isNullish } from '@dfinity/utils';
 import { derived, type Readable } from 'svelte/store';
 
 export const networkTokens: Readable<Token[]> = derived(
@@ -10,15 +10,14 @@ export const networkTokens: Readable<Token[]> = derived(
 	([$tokens, $selectedNetwork]) =>
 		$tokens.filter((token) => {
 			const {
-				network: { id, env }
+				network: { id: networkId }
 			} = token;
 
-			// TODO: extract Chain Fusion logic to a utility maybe?
 			return (
-				(isNetworkIdChainFusion($selectedNetwork.id) &&
-					env === $selectedNetwork.env &&
-					!isTokenIcrcTestnet(token)) ||
-				id === $selectedNetwork.id
+				(isNullish($selectedNetwork) &&
+					!isTokenIcrcTestnet(token) &&
+					token.network.env !== 'testnet') ||
+				$selectedNetwork?.id === networkId
 			);
 		})
 );
