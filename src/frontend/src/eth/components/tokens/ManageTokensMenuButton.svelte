18a467f68dae113ce29e4477b42047ed28ba8465
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-06-17 09:36:43 -0300

    refactor: extract specific network button from TokensMenu (#1448)
    * refactor: extract specific network button from TokensMenu
    
    * refactor: renamed new components

---
+++ src/frontend/src/eth/components/tokens/ManageTokensMenuButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { i18n } from '$lib/stores/i18n.store';
+	import { modalStore } from '$lib/stores/modal.store';
+</script>
+
+<div class="content">
+	<button
+		class="flex gap-2 items-center no-underline hover:text-blue active:text-blue"
+		aria-label={$i18n.tokens.import.text.title}
+		on:click={modalStore.openAddToken}
+	>
+		{`+ ${$i18n.tokens.import.text.title}`}
+	</button>
+</div>
