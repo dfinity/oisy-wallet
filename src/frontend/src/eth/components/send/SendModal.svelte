0c8e13e5445238042e35dfa31757865297c1be72
David Dal Busco <david.dalbusco@outlook.com>
2024-06-17 15:37:51 +0200

    chore: remove empty line

--- src/frontend/src/eth/components/send/SendModal.svelte
+++ src/frontend/src/eth/components/send/SendModal.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import SendTokenModal from '$eth/components/send/SendTokenModal.svelte';
 	import SendTokenContext from '$eth/components/send/SendTokenContext.svelte';
-
 	import { token } from '$lib/stores/token.store';
 </script>
 
