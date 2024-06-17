78e244db286996ecf292a6c9db533548a2d02a43
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-06-17 12:59:22 -0300

    Refactor/extract button menu (#1451)
    * feat: create ButtonMenu component
    
    * refactor: use ButtonMenu instead of button component
    
    * fix: remove onClick prop according to review
    
    * refactor: as per review, substituted label prop with slot

--- src/frontend/src/eth/components/tokens/ManageTokensMenuButton.svelte
+++ src/frontend/src/eth/components/tokens/ManageTokensMenuButton.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
 	import { i18n } from '$lib/stores/i18n.store';
 	import { modalStore } from '$lib/stores/modal.store';
+	import ButtonMenu from '$lib/components/ui/ButtonMenu.svelte';
 </script>
 
 <div class="content">
-	<button
-		class="flex gap-2 items-center no-underline hover:text-blue active:text-blue"
-		aria-label={$i18n.tokens.import.text.title}
-		on:click={modalStore.openAddToken}
-	>
+	<ButtonMenu ariaLabel={$i18n.tokens.import.text.title} on:click={modalStore.openAddToken}>
 		{`+ ${$i18n.tokens.import.text.title}`}
-	</button>
+	</ButtonMenu>
 </div>
--- src/frontend/src/icp/components/tokens/IcManageTokensMenuButton.svelte
+++ src/frontend/src/icp/components/tokens/IcManageTokensMenuButton.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
 	import { i18n } from '$lib/stores/i18n.store';
 	import { modalStore } from '$lib/stores/modal.store';
+	import ButtonMenu from '$lib/components/ui/ButtonMenu.svelte';
 </script>
 
 <div class="content">
-	<button
-		class="flex gap-2 items-center no-underline hover:text-blue active:text-blue"
-		aria-label={$i18n.tokens.manage.text.title}
-		on:click={modalStore.openIcManageTokens}
-	>
+	<ButtonMenu ariaLabel={$i18n.tokens.manage.text.title} on:click={modalStore.openIcManageTokens}>
 		{$i18n.tokens.manage.text.title}
-	</button>
+	</ButtonMenu>
 </div>
