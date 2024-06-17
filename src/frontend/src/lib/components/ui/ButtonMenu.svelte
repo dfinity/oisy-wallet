1b802cf43c5af5e30e8a3ed19cbe272b55095bc9
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-06-17 09:41:05 -0300

    feat: create ButtonMenu component

---
+++ src/frontend/src/lib/components/ui/ButtonMenu.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let label: string;
+	export let onClick: () => void;
+	export let ariaLabel: string | undefined = undefined;
+</script>
+
+<button
+	class={`flex gap-2 items-center no-underline hover:text-blue active:text-blue`}
+	aria-label={ariaLabel ?? label}
+	on:click={onClick}
+>
+	<slot name="before-label"></slot>
+	{label}
+	<slot name="after-label"></slot>
+</button>
