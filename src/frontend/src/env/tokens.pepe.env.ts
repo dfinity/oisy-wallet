1336f31fe11f59278f357167cb4b51ee10804fa4
David Dal Busco <david.dalbusco@outlook.com>
2024-06-17 09:12:07 +0200

    chore: format

--- src/frontend/src/env/tokens.pepe.env.ts
+++ src/frontend/src/env/tokens.pepe.env.ts
@@ -9,15 +9,15 @@ export const SEPOLIA_PEPE_SYMBOL = 'SepoliaPEPE';
 export const SEPOLIA_PEPE_TOKEN_ID: unique symbol = Symbol(SEPOLIA_PEPE_SYMBOL);
 
 export const SEPOLIA_PEPE_TOKEN: RequiredErc20Token = {
-    id: SEPOLIA_PEPE_TOKEN_ID,
-    network: SEPOLIA_NETWORK,
-    standard: 'erc20',
-    category: 'default',
-    name: 'Pepe',
-    symbol: 'PEPE',
-    decimals: PEPE_DECIMALS,
-    icon: pepe,
-    address: '0x560eF9F39E4B08f9693987cad307f6FBfd97B2F6',
-    exchange: 'erc20',
-    twinTokenSymbol: 'ckSepoliaPEPE'
+	id: SEPOLIA_PEPE_TOKEN_ID,
+	network: SEPOLIA_NETWORK,
+	standard: 'erc20',
+	category: 'default',
+	name: 'Pepe',
+	symbol: 'PEPE',
+	decimals: PEPE_DECIMALS,
+	icon: pepe,
+	address: '0x560eF9F39E4B08f9693987cad307f6FBfd97B2F6',
+	exchange: 'erc20',
+	twinTokenSymbol: 'ckSepoliaPEPE'
 };
