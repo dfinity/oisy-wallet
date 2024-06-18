6b9257ba5dbe23a509e1f0324019588ad76ae880
David Dal Busco <david.dalbusco@dfinity.org>
2024-06-18 13:49:55 +0200

    feat: extract dual mode button (#1466)
    * feat: card transaction pending component
    
    * feat: add receive actions within tokens list
    
    * feat: receive token
    
    * feat: receive eth and ic
    
    * feat: modalId
    
    * feat: twinToken derive
    
    * fix: missing import
    
    * feat: twin token
    
    * feat: deprecated
    
    * feat: derive ck eth
    
    * chore: format
    
    * feat: load token and open receive modal
    
    * feat: close receive modal
    
    * feat: remove console log
    
    * feat: revert deprecations
    
    * chore: format
    
    * feat: remove twin token refactor
    
    * refactor: twin token receive context
    
    * feat: extract dual mode button

---
+++ src/frontend/src/lib/components/ui/ButtonDualMode.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let compact = false;
+	export let disabled = false;
+	export let alt: string;
+</script>
+
+<button
+	class:flex-1={!compact}
+	class:hero={!compact}
+	class:text-blue={compact}
+	class:hover:text-dark-blue={compact}
+	class:active:text-dark-blue={compact}
+	{disabled}
+	class:opacity-50={disabled}
+	on:click
+	aria-label={alt}
+>
+	{#if compact}
+		<slot name="icon-only" />
+	{:else}
+		<slot name="text-icon" />
+		<span><slot /></span>{/if}</button
+>
