1b89d695a2e32915579fe580e24acfa5ac34061f
David Dal Busco <david.dalbusco@outlook.com>
2024-06-18 11:45:11 +0200

    feat: send button with dual mode

--- src/frontend/src/icp/components/send/IcSend.svelte
+++ src/frontend/src/icp/components/send/IcSend.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
 	import { modalStore } from '$lib/stores/modal.store';
-	import IconSend from '$lib/components/icons/IconSend.svelte';
-	import { isBusy } from '$lib/derived/busy.derived';
 	import { modalIcSend } from '$lib/derived/modal.derived';
 	import IcSendModal from '$icp/components/send/IcSendModal.svelte';
 	import { ICP_NETWORK_ID } from '$env/networks.env';
-	import { i18n } from '$lib/stores/i18n.store';
+	import SendButton from '$lib/components/send/SendButton.svelte';
 </script>
 
-<button class="hero" on:click={modalStore.openIcSend} disabled={$isBusy} class:opacity-50={$isBusy}>
-	<IconSend size="28" />
-	<span>{$i18n.send.text.send}</span></button
->
+<SendButton on:click={modalStore.openIcSend} />
 
 {#if $modalIcSend}
 	<IcSendModal networkId={ICP_NETWORK_ID} />
