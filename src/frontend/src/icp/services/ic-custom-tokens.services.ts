1377d89d8fca8f1ecdfbbf2f5216f48c14dc1264
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-06-20 14:35:56 -0300

    refactor: extract save function from component ManageTokensModal

--- src/frontend/src/icp/services/ic-custom-tokens.services.ts
+++ src/frontend/src/icp/services/ic-custom-tokens.services.ts
@@ -3,9 +3,14 @@ import { icrcTokensStore } from '$icp/stores/icrc.store';
 import type { IcrcCustomToken } from '$icp/types/icrc-custom-token';
 import { setManyCustomTokens } from '$lib/api/backend.api';
 import { ProgressStepsAddToken } from '$lib/enums/progress-steps';
+import { nullishSignOut } from '$lib/services/auth.services';
+import { authStore } from '$lib/stores/auth.store';
+import { i18n } from '$lib/stores/i18n.store';
+import { toastsError } from '$lib/stores/toasts.store';
 import type { Identity } from '@dfinity/agent';
 import { Principal } from '@dfinity/principal';
-import { toNullable } from '@dfinity/utils';
+import { isNullish, toNullable } from '@dfinity/utils';
+import { get } from 'svelte/store';
 
 export const saveCustomTokens = async ({
 	progress,
@@ -40,4 +45,54 @@ export const saveCustomTokens = async ({
 
 	// Reload all custom tokens for simplicity reason.
 	await loadUserTokens({ identity });
+};
+
+export const saveIcrcCustomToken = async ({
+	tokens,
+	updateSaveProgressStep,
+	modalNext,
+	onSuccess,
+	onError
+}: {
+	tokens: Pick<IcrcCustomToken, 'enabled' | 'version' | 'ledgerCanisterId' | 'indexCanisterId'>[];
+	updateSaveProgressStep: (step: ProgressStepsAddToken) => void;
+	modalNext: () => void;
+	onSuccess: () => void;
+	onError: () => void;
+}) => {
+	const $i18n = get(i18n);
+	const $authStore = get(authStore);
+
+	if (isNullish($authStore.identity)) {
+		await nullishSignOut();
+		return;
+	}
+
+	if (tokens.length === 0) {
+		toastsError({
+			msg: { text: $i18n.tokens.manage.error.empty }
+		});
+		return;
+	}
+
+	modalNext();
+
+	try {
+		await saveCustomTokens({
+			identity: $authStore.identity,
+			tokens,
+			progress: updateSaveProgressStep
+		});
+
+		updateSaveProgressStep(ProgressStepsAddToken.DONE);
+
+		setTimeout(() => onSuccess(), 750);
+	} catch (err: unknown) {
+		toastsError({
+			msg: { text: $i18n.tokens.error.unexpected },
+			err
+		});
+
+		onError();
+	}
 };
