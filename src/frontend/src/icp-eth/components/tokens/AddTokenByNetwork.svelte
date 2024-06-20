e6728699e75a01e03c748607d1cb0e5a8e862ed0
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-06-20 10:48:22 -0300

    refactor: as per review, the new components will have the same logic as SendNetwork

--- src/frontend/src/icp-eth/components/tokens/AddTokenByNetwork.svelte
+++ src/frontend/src/icp-eth/components/tokens/AddTokenByNetwork.svelte
@@ -2,13 +2,21 @@
 	import { i18n } from '$lib/stores/i18n.store';
 	import { Dropdown, DropdownItem } from '@dfinity/gix-components';
 	import { networksMainnets } from '$lib/derived/networks.derived';
-	import { ETHEREUM_NETWORK_SYMBOL, ICP_NETWORK_SYMBOL } from '$env/networks.env';
+	import { ETHEREUM_NETWORK_ID, ICP_NETWORK_ID } from '$env/networks.env';
 	import IcAddTokenForm from '$icp/components/tokens/IcAddTokenForm.svelte';
 	import AddTokenForm from '$eth/components/tokens/AddTokenForm.svelte';
 	import { fade } from 'svelte/transition';
+	import type { Network } from '$lib/types/network';
+	import { nonNullish } from '@dfinity/utils';
 
-	export let dropdownNetwork: string | undefined;
+	export let network: Network | undefined;
 	export let tokenData: Record<string, string>;
+
+	let networkName: string | undefined = network?.name;
+	$: networkName,
+		(network = nonNullish(networkName)
+			? $networksMainnets.find(({ name }) => name === networkName)
+			: undefined);
 
 	let ledgerCanisterId: string;
 	let indexCanisterId: string;
@@ -22,24 +30,34 @@
 </script>
 
 <div class="stretch pt-8">
-	<label for="selectedNetwork" class="font-bold px-4.5"
-		>{$i18n.tokens.manage.text.select_network}:</label
-	>
-	<Dropdown name="selectedNetwork" bind:selectedValue={dropdownNetwork}>
-		{#each $networksMainnets as network}
-			<DropdownItem value={network.id.description ?? ''}>{network.name}</DropdownItem>
-		{/each}
-	</Dropdown>
+	<label for="network" class="font-bold px-4.5">{$i18n.tokens.manage.text.network}:</label>
+
+	<div id="network" class="mb-4 mt-1 pt-0.5">
+		<Dropdown name="network" bind:selectedValue={networkName}>
+			<option disabled selected value={undefined} class="hidden"
+				><span class="description">{$i18n.tokens.manage.placeholder.select_network}</span></option
+			>
+			{#each $networksMainnets as network}
+				<DropdownItem value={network.name}>{network.name}</DropdownItem>
+			{/each}
+		</Dropdown>
+	</div>
 
 	<div class="mt-8">
-		{#if dropdownNetwork === ICP_NETWORK_SYMBOL}
+		{#if network?.id === ICP_NETWORK_ID}
 			<div in:fade>
 				<IcAddTokenForm on:icBack on:icNext bind:ledgerCanisterId bind:indexCanisterId />
 			</div>
-		{:else if dropdownNetwork === ETHEREUM_NETWORK_SYMBOL}
+		{:else if network?.id === ETHEREUM_NETWORK_ID}
 			<div in:fade>
 				<AddTokenForm on:icBack on:icNext bind:contractAddress />
 			</div>
 		{/if}
 	</div>
 </div>
+
+<style lang="scss">
+	.hidden {
+		display: none;
+	}
+</style>
