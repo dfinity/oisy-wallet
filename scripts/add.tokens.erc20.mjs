357e366c444c804de6e5f1049d1e3107c7201ce8
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-06-21 15:18:52 -0300

    feat: changed script to create variables independently of having both envs

--- scripts/add.tokens.erc20.mjs
+++ scripts/add.tokens.erc20.mjs
@@ -1,8 +1,7 @@
 import { isNullish, nonNullish } from '@dfinity/utils';
-import { execSync } from 'child_process';
 import dotenv from 'dotenv';
 import { ethers } from 'ethers';
-import fs from 'fs/promises';
+import { execSync } from 'node:child_process';
 import { readFileSync, writeFileSync } from 'node:fs';
 import { resolve } from 'node:path';
 import path from 'path';
@@ -27,101 +26,207 @@ if (isNullish(ETHERSCAN_API_KEY)) {
 const DATA_DIR = 'src/frontend/src/env';
 const DATA_DIR_PATH = resolve(process.cwd(), DATA_DIR);
 
-const fetchTokenDetails = async (contractAddress) => {
-	const provider = new ethers.providers.EtherscanProvider('homestead', ETHERSCAN_API_KEY);
+const escapeRegExp = (string) => string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+const unescapeRegExp = (string) => string.replace(/\\([.*+?^${}()|/[\]\\])/g, '$1');
+
+const addElementListInContent = ({ content, regex, element, addAtStartIfNotFound = false }) => {
+	const match = content.match(regex);
+	if (match) {
+		const list = match[1].trim();
+		if (!list.includes(element)) {
+			const updatedList = list ? `${list}, ${element}` : element;
+			return {
+				content: content.replace(regex, match[0].replace(match[1], updatedList)),
+				hasChanged: true
+			};
+		}
+		return { content, hasChanged: false };
+	}
+	const newLine = unescapeRegExp(regex.source.replace('([\\s\\S]*?)', element));
+	return {
+		content: addAtStartIfNotFound ? `${newLine}\n${content}` : `${content}\n${newLine}`,
+		hasChanged: true
+	};
+};
+
+const updateListInContent = ({ content, regex, elements, addAtStartIfNotFound = false }) =>
+	elements.reduce(
+		(acc, element) => {
+			const { content: newContent, hasChanged } = addElementListInContent({
+				content: acc.content,
+				regex,
+				element,
+				addAtStartIfNotFound
+			});
+			return {
+				content: newContent,
+				hasChanged: acc.hasChanged || hasChanged
+			};
+		},
+		{ content, hasChanged: false }
+	);
+
+const updateImportsInContent = ({ content, imports, module }) =>
+	updateListInContent({
+		content,
+		regex: new RegExp(`import \\{([\\s\\S]*?)} from '${escapeRegExp(module)}';`),
+		elements: imports,
+		addAtStartIfNotFound: true
+	});
+
+const fetchTokenDetails = async ({ contractAddress, isTestnet }) => {
+	const provider = new ethers.providers.EtherscanProvider(
+		isTestnet ? 'sepolia' : 'homestead',
+		ETHERSCAN_API_KEY
+	);
 	const contract = new ethers.Contract(
 		contractAddress,
-		[
-			'function name() view returns (string)',
-			'function symbol() view returns (string)',
-			'function decimals() view returns (uint8)'
-		],
+		['function name() view returns (string)', 'function decimals() view returns (uint8)'],
 		provider
 	);
 
-	const [name, symbol, decimals] = await Promise.all([
-		contract.name(),
-		contract.symbol(),
-		contract.decimals()
-	]).catch((err) => {
-		console.error(`Error fetching token details:\n${err}`);
-		process.exit(1);
-	});
+	const [name, decimals] = await Promise.all([contract.name(), contract.decimals()]).catch(
+		(err) => {
+			console.error(`Error fetching token details:\n${err}`);
+			process.exit(1);
+		}
+	);
 
-	return { name, symbol, decimals };
+	return { name, decimals };
 };
 
-let newFilesCreated = false;
-
-const createEnvFile = async (token) => {
-	const { tokenName, contractAddress, testnetContractAddress } = token;
-
-	const tokenDetails = await fetchTokenDetails(contractAddress);
-
-	const newFileName = `tokens.${tokenName.toLowerCase()}.env.ts`;
-	const newFilePath = path.join(DATA_DIR_PATH, newFileName);
-
+const loadFileContentOrEmpty = (filePath) => {
 	try {
-		await fs.access(newFilePath);
-		console.log(`File ${newFilePath} already exists.`);
-		return;
+		return readFileSync(filePath, 'utf8');
 	} catch (err) {
-		// File does not exist, proceed with creation
+		console.log(`File ${filePath} does not exist, it will be created.`);
+		return '';
+	}
+};
+
+let filesCreatedOrModified = false;
+
+const manageEnvFile = async ({ mainSymbol: symbol, contractAddress, testnetContractAddress }) => {
+	const fileName = `tokens.${symbol.toLowerCase()}.env.ts`;
+	const filePath = path.join(DATA_DIR_PATH, fileName);
+
+	const existingFileContent = loadFileContentOrEmpty(filePath);
+
+	const mainnetToken = nonNullish(contractAddress) ? `${symbol}_TOKEN` : undefined;
+	const testnetToken = nonNullish(testnetContractAddress) ? `SEPOLIA_${symbol}_TOKEN` : undefined;
+
+	const mainnetTokenCreated = existingFileContent.split(/\W+/).includes(mainnetToken ?? '');
+	const testnetTokenCreated = existingFileContent.split(/\W+/).includes(testnetToken ?? '');
+
+	if (mainnetTokenCreated && testnetTokenCreated) {
+		console.log(`Variables for token ${symbol} already exists in the environment file ${fileName}`);
+		return { fileName, mainnetToken: undefined, testnetToken: undefined };
 	}
 
-	const mainnetToken = `${tokenDetails.symbol}_TOKEN`;
-	const testnetToken = `SEPOLIA_${tokenDetails.symbol}_TOKEN`;
+	const { name, decimals } = await fetchTokenDetails({
+		contractAddress: nonNullish(contractAddress) ? contractAddress : testnetContractAddress,
+		isTestnet: isNullish(contractAddress)
+	});
 
-	const newContent = `import { ETHEREUM_NETWORK, SEPOLIA_NETWORK } from '$env/networks.env';
-import type { RequiredErc20Token } from '$eth/types/erc20';
-import ${tokenName.toLowerCase()} from '$icp-eth/assets/${tokenName.toLowerCase()}.svg';
+	const icon = symbol.toLowerCase();
 
-export const ${tokenDetails.symbol}_DECIMALS = ${tokenDetails.decimals};
+	const fileContentFirstPart =
+		existingFileContent !== ''
+			? existingFileContent
+			: `import type { RequiredErc20Token } from '$eth/types/erc20';
+import ${icon} from '$icp-eth/assets/${icon}.svg';
+`;
 
-export const ${tokenDetails.symbol}_SYMBOL = '${tokenDetails.symbol}';
+	const { content: fileContentFirstPartWithImports } = updateImportsInContent({
+		content: fileContentFirstPart,
+		imports: [
+			...(!mainnetTokenCreated ? ['ETHEREUM_NETWORK'] : []),
+			...(!testnetTokenCreated ? ['SEPOLIA_NETWORK'] : [])
+		],
+		module: '$env/networks.env'
+	});
 
-export const ${tokenDetails.symbol}_TOKEN_ID: unique symbol = Symbol(${tokenDetails.symbol}_SYMBOL);
+	const decimalsConst = `export const ${symbol}_DECIMALS = ${decimals};`;
+	const fileContentDecimalsConst = !existingFileContent.includes(decimalsConst)
+		? `\n${decimalsConst}\n`
+		: '';
+
+	const newFileContentMainnet =
+		nonNullish(mainnetToken) && !mainnetTokenCreated
+			? `
+export const ${symbol}_SYMBOL = '${symbol}';
+
+export const ${symbol}_TOKEN_ID: unique symbol = Symbol(${symbol}_SYMBOL);
 
 export const ${mainnetToken}: RequiredErc20Token = {
-	id: ${tokenDetails.symbol}_TOKEN_ID,
+	id: ${symbol}_TOKEN_ID,
 	network: ETHEREUM_NETWORK,
 	standard: 'erc20',
 	category: 'default',
-	name: '${tokenDetails.name}',
-	symbol: '${tokenDetails.symbol}',
-	decimals: ${tokenDetails.symbol}_DECIMALS,
-	icon: ${tokenName.toLowerCase()},
+	name: '${name}',
+	symbol: '${symbol}',
+	decimals: ${symbol}_DECIMALS,
+	icon: ${icon},
 	address: '${contractAddress}',
 	exchange: 'erc20',
-	twinTokenSymbol: 'ck${tokenDetails.symbol}'
+	twinTokenSymbol: 'ck${symbol}'
 };
+`
+			: '';
 
-export const SEPOLIA_${tokenDetails.symbol}_SYMBOL = 'Sepolia${tokenDetails.symbol}';
+	const newFileContentTestnet =
+		nonNullish(testnetToken) && !testnetTokenCreated
+			? `
+export const SEPOLIA_${symbol}_SYMBOL = 'Sepolia${symbol}';
 
-export const SEPOLIA_${tokenDetails.symbol}_TOKEN_ID: unique symbol = Symbol(SEPOLIA_${tokenDetails.symbol}_SYMBOL);
+export const SEPOLIA_${symbol}_TOKEN_ID: unique symbol = Symbol(SEPOLIA_${symbol}_SYMBOL);
 
 export const ${testnetToken}: RequiredErc20Token = {
-	id: SEPOLIA_${tokenDetails.symbol}_TOKEN_ID,
+	id: SEPOLIA_${symbol}_TOKEN_ID,
 	network: SEPOLIA_NETWORK,
 	standard: 'erc20',
 	category: 'default',
-	name: '${tokenDetails.name}',
-	symbol: '${tokenDetails.symbol}',
-	decimals: ${tokenDetails.symbol}_DECIMALS,
-	icon: ${tokenName.toLowerCase()},
+	name: '${name}',
+	symbol: '${symbol}',
+	decimals: ${symbol}_DECIMALS,
+	icon: ${icon},
 	address: '${testnetContractAddress}',
 	exchange: 'erc20',
-	twinTokenSymbol: 'ckSepolia${tokenDetails.symbol}'
+	twinTokenSymbol: 'ckSepolia${symbol}'
 };
-`;
+`
+			: '';
 
-	writeFileSync(newFilePath, newContent);
-	console.log(`Created ${newFilePath}`);
+	if (newFileContentMainnet + newFileContentTestnet === '') {
+		console.log(`No new variables to create for token ${symbol}`);
+		return { fileName, mainnetToken: undefined, testnetToken: undefined };
+	}
 
-	return { newFileName, mainnetToken, testnetToken };
+	const newFileContent =
+		fileContentFirstPartWithImports +
+		fileContentDecimalsConst +
+		newFileContentMainnet +
+		newFileContentTestnet;
+	writeFileSync(filePath, newFileContent);
+	filesCreatedOrModified = true;
+
+	if (newFileContentMainnet !== '') {
+		console.log(`Created new mainnet variables for token ${symbol} in file ${filePath}`);
+	}
+
+	if (newFileContentTestnet !== '') {
+		console.log(`Created new testnet variables for token ${symbol} in file ${filePath}`);
+	}
+
+	return {
+		fileName,
+		mainnetToken: newFileContentMainnet !== '' ? mainnetToken : undefined,
+		testnetToken: newFileContentTestnet !== '' ? testnetToken : undefined
+	};
 };
 
-const updateTokensErc20Env = (newFileName, mainnetToken, testnetToken) => {
+const updateTokensErc20Env = ({ fileName, mainnetToken, testnetToken }) => {
 	const filePath = path.join(DATA_DIR_PATH, 'tokens.erc20.env.ts');
 	let content = readFileSync(filePath, 'utf8');
 
@@ -136,24 +241,34 @@ const updateTokensErc20Env = (newFileName, mainnetToken, testnetToken) => {
 		}
 	];
 
-	regexList.forEach(({ regex, token }) => {
-		const match = content.match(regex);
-		if (match) {
-			const tokensList = match[1].trim();
-			const updatedTokensList = tokensList ? `${tokensList}, ${token}` : token;
-			content = content.replace(
+	regexList
+		.filter(({ token }) => nonNullish(token))
+		.forEach(({ regex, token }) => {
+			const { content: newContent, hasChanged } = addElementListInContent({
+				content,
 				regex,
-				`${match[0].split(':')[0]}: RequiredErc20Token[] = [${updatedTokensList}];`
-			);
-		}
-	});
+				element: token
+			});
+			if (hasChanged) {
+				content = newContent;
+				console.log(`Included ${token} in token list in ${filePath}`);
+			}
+		});
 
-	const importStatement = `import { ${mainnetToken}, ${testnetToken} } from '$env/${newFileName.replace('.ts', '')}';\n`;
-	content = importStatement + content;
+	if (nonNullish(mainnetToken) || nonNullish(testnetToken)) {
+		const imports = [mainnetToken, testnetToken].filter(Boolean);
+		const { content: newContent, hasChanged } = updateImportsInContent({
+			content,
+			imports,
+			module: `$env/${fileName.replace('.ts', '')}`
+		});
+		if (hasChanged) {
+			content = newContent;
+			console.log(`Added import statement for ${imports.join(', ')} in ${filePath}`);
+		}
+	}
 
 	writeFileSync(filePath, content);
-	newFilesCreated = true;
-	console.log(`Updated ${filePath}`);
 };
 
 const flattenData = (data) => {
@@ -175,96 +290,82 @@ const flattenEnvironmentData = (data) => {
 
 const readSupportedTokens = () => {
 	const jsonPath = path.resolve(DATA_DIR_PATH, 'tokens.ckerc20.json');
-	return flattenEnvironmentData(JSON.parse(readFileSync(jsonPath, 'utf-8')));
+	const { production: prodTokens, staging: testnetTokens } = flattenEnvironmentData(
+		JSON.parse(readFileSync(jsonPath, 'utf-8'))
+	);
+	return [...prodTokens, ...testnetTokens];
 };
 
-const filterTokens = async (prodTokens, testnetTokens) => {
-	const testnetTokenMap = new Map(
-		testnetTokens.map(({ symbol, erc20ContractAddress }) => [
-			symbol.replace('ckSepolia', 'ck'),
-			erc20ContractAddress
-		])
-	);
+const parseTokens = (tokens) =>
+	Object.values(
+		tokens.reduce((acc, { symbol, erc20ContractAddress }) => {
+			const mainSymbol = symbol.replace('Sepolia', '').slice(2);
+			const contractAddress = !symbol.includes('ckSepolia') ? erc20ContractAddress : undefined;
+			const testnetContractAddress = symbol.includes('ckSepolia')
+				? erc20ContractAddress
+				: undefined;
 
-	const results = await Promise.all(
-		prodTokens
-			.filter(({ symbol }) => nonNullish(testnetTokenMap.get(symbol)))
-			.map(async (token) => {
-				const tokenName = token.symbol.slice(2);
-				const tokenFilePath = path.join(DATA_DIR_PATH, `tokens.${tokenName.toLowerCase()}.env.ts`);
-				try {
-					await fs.access(tokenFilePath);
-					return null; // File already exists, filter it out
-				} catch (err) {
-					if (err.code === 'ENOENT') {
-						return token; // File doesn't exist, process this token
-					}
-					throw err;
-				}
-			})
-	);
+			acc[mainSymbol] = {
+				mainSymbol,
+				contractAddress: acc[mainSymbol]?.contractAddress || contractAddress,
+				testnetContractAddress: acc[mainSymbol]?.testnetContractAddress || testnetContractAddress
+			};
 
-	return results.filter(nonNullish).map(({ symbol, erc20ContractAddress }) => {
-		return {
-			tokenName: symbol.slice(2),
-			symbol: symbol,
-			contractAddress: erc20ContractAddress,
-			testnetContractAddress: testnetTokenMap.get(symbol)
-		};
-	});
-};
+			return acc;
+		}, {})
+	);
 
 const main = async () => {
-	const { production: prodTokens, staging: testnetTokens } = await readSupportedTokens();
+	const tokens = readSupportedTokens();
 
-	const tokensToProcess = await filterTokens(prodTokens, testnetTokens);
+	const tokensToProcess = parseTokens(tokens);
 
 	if (tokensToProcess.length === 0) {
 		console.log('No new token found to process.');
-		console.log(
-			'Be aware that the script only processes tokens that are not already in the env directory and that have a testnet counterpart.'
-		);
 		return;
 	}
 
+	console.log(
+		`Found ${tokensToProcess.length} tokens to check: ${tokensToProcess.map(({ mainSymbol }) => mainSymbol).join(', ')}`
+	);
+
 	for (const token of tokensToProcess) {
-		const { tokenName } = token;
+		const { mainSymbol } = token;
 
 		console.log('--------------------------------');
-		console.log(`Creating file for token ${tokenName}`);
+		console.log(`Checking if token ${mainSymbol} already exists in the environment files.`);
 
-		const { newFileName, mainnetToken, testnetToken } = await createEnvFile(token);
+		const { fileName, mainnetToken, testnetToken } = await manageEnvFile(token);
 
-		updateTokensErc20Env(newFileName, mainnetToken, testnetToken);
-
-		console.log(`Finished for token ${tokenName}`);
+		updateTokensErc20Env({ fileName, mainnetToken, testnetToken });
 	}
 
-	console.log('--------------------------------');
-	console.log(
-		'Final Step: To complete the integration of the new tokens, you need to create SVG icon files for each token and place them in the correct directory.'
-	);
-	console.log(`Navigate to the following directory: ${path.join(DATA_DIR)}`);
-
-	tokensToProcess.forEach(({ tokenName }) => {
-		console.log(
-			`- For token ${tokenName}, create an SVG file named '${tokenName.toLowerCase()}.svg'`
-		);
-	});
-
-	console.log(
-		'Ensure that the SVG files are valid SVG format and represent the tokens, typically the logo or symbol of each token.'
-	);
-
-	tokensToProcess.forEach(({ tokenName }) => {
-		console.log(
-			`Example: For token ${tokenName}, the SVG file should be: ${DATA_DIR}/${tokenName.toLowerCase()}.svg`
-		);
-	});
-
-	if (newFilesCreated) {
+	if (filesCreatedOrModified) {
+		console.log('--------------------------------');
 		console.log('Running npm run format && npm run lint');
 		execSync('npm run format && npm run lint', { stdio: 'inherit' });
+
+		console.log('--------------------------------');
+		console.log(
+			'Final Step: To complete the integration of the new tokens, you need to create SVG icon files for each token and place them in the correct directory.'
+		);
+		console.log(`Navigate to the following directory: ${path.join(DATA_DIR)}`);
+
+		tokensToProcess.forEach(({ mainSymbol }) => {
+			console.log(
+				`- For token ${mainSymbol}, create an SVG file named '${mainSymbol.toLowerCase()}.svg'`
+			);
+		});
+
+		console.log(
+			'Ensure that the SVG files are valid SVG format and represent the tokens, typically the logo or symbol of each token.'
+		);
+
+		tokensToProcess.forEach(({ mainSymbol }) => {
+			console.log(
+				`Example: For token ${mainSymbol}, the SVG file should be: ${DATA_DIR}/${mainSymbol.toLowerCase()}.svg`
+			);
+		});
 	}
 };
 
