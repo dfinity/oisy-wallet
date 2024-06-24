94f481b99db7e589a0846feec167d39957e4d3e2
David Dal Busco <david.dalbusco@dfinity.org>
2024-06-24 11:57:33 +0200

    test: authentication flow with internet-identity-playwright (#1403)
    * test: authentication flow
    
    * build: for the time being use local plugin
    
    * test: wait sign-in button rendered before taking screenshot
    
    * build: release playwright plugin
    
    * chore: remove temporary gz
    
    * chore: format

--- src/frontend/src/lib/components/ui/ButtonIC.svelte
+++ src/frontend/src/lib/components/ui/ButtonIC.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import IconIcLogo from '$lib/components/icons/IconIcLogo.svelte';
+
+	export let testId: string | undefined = undefined;
 </script>
 
-<button on:click class="ic">
+<button on:click class="ic" data-tid={testId}>
 	<IconIcLogo />
 	<span class="text-left">
 		<span><slot name="action" /></span>
