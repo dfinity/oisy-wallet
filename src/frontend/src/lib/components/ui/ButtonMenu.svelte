9eaaf481722ada2123cbd0f29700ed74dcae352d
David Dal Busco <david.dalbusco@dfinity.org>
2024-06-24 08:32:18 +0200

    feat: button menu actionable on the full width (#1537)

--- src/frontend/src/lib/components/ui/ButtonMenu.svelte
+++ src/frontend/src/lib/components/ui/ButtonMenu.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <button
-	class={`flex gap-2 items-center no-underline hover:text-blue active:text-blue`}
+	class="flex gap-2 items-center no-underline hover:text-blue active:text-blue w-full"
 	aria-label={ariaLabel}
 	on:click
 >
