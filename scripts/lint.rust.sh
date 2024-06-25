10354041321c5e41b7ad490281b6263f5e73e0db
David Dal Busco <david.dalbusco@outlook.com>
2024-06-25 19:03:23 +0200

    chore: backend clippy allow deprecated

--- scripts/lint.rust.sh
+++ scripts/lint.rust.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 # Lint the rust code
-cargo clippy --locked --target wasm32-unknown-unknown --all-features -- -D warnings -W clippy::pedantic -A clippy::module-name-repetitions -A clippy::struct-field-names
+cargo clippy --locked --target wasm32-unknown-unknown --all-features -- -D warnings -W clippy::pedantic -A clippy::module-name-repetitions -A clippy::struct-field-names -A deprecated
