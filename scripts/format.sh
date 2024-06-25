34ac39d3e842c784a5aa869d70f6c172e959d5c2
Max <bitdivine@users.noreply.github.com>
2024-06-25 11:07:58 +0200

    feat (ci): Add Rust formatting command (#1552)
    # Motivation
    CI has no rust formatter.  As a result we have had PRs that contain changes that are just rust formatting.
    
    # Changes
    - Define rust and cargo formatting commands
    - Format
    
    # Tests
    
    <!-- Please provide any information or screenshots about the tests that have been done -->

---
+++ scripts/format.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+cd "$(dirname "$(realpath "$0")")/.."
+
+set -x
+time xargs -P8 -I{} bash -c "{}" <<EOF
+./scripts/format.cargo.sh
+./scripts/format.rust.sh
+EOF
