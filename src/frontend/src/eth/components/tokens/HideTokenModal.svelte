6965a5779317a99669633491e8bc9a8f1e5397ac
David Dal Busco <david.dalbusco@dfinity.org>
2024-06-26 09:29:09 +0200

    feat: disable user token instead of remove (#1566)

--- src/frontend/src/eth/components/tokens/HideTokenModal.svelte
+++ src/frontend/src/eth/components/tokens/HideTokenModal.svelte
@@ -2,21 +2,21 @@
 	import { i18n } from '$lib/stores/i18n.store';
 	import { isNullishOrEmpty } from '$lib/utils/input.utils';
 	import { toastsError } from '$lib/stores/toasts.store';
-	import { removeUserToken } from '$lib/api/backend.api';
-	import { selectedChainId } from '$eth/derived/network.derived';
-	import { erc20TokensStore } from '$eth/stores/erc20.store';
-	import type { OptionErc20Token } from '$eth/types/erc20';
+	import { setUserToken } from '$lib/api/backend.api';
 	import HideTokenModal from '$lib/components/tokens/HideTokenModal.svelte';
 	import type { Identity } from '@dfinity/agent';
 	import { ETHEREUM_NETWORK_ID } from '$env/networks.env';
 	import { onMount } from 'svelte';
-	import { assertNonNullish } from '@dfinity/utils';
+	import { assertNonNullish, toNullable } from '@dfinity/utils';
 	import { token } from '$lib/stores/token.store';
+	import type { OptionErc20UserToken } from '$eth/types/erc20-user-token';
+	import type { EthereumNetwork } from '$eth/types/network';
+	import { loadUserTokens } from '$eth/services/erc20.services';
 
-	let selectedToken: OptionErc20Token;
+	let selectedToken: OptionErc20UserToken;
 
 	// We must clone the reference to avoid the UI to rerender once we remove the token from the store.
-	onMount(() => (selectedToken = $token as OptionErc20Token));
+	onMount(() => (selectedToken = $token as OptionErc20UserToken));
 
 	const assertHide = (): { valid: boolean } => {
 		const contractAddress = selectedToken?.address;
@@ -32,24 +32,25 @@
 	};
 
 	const hideToken = async (params: { identity: Identity }) => {
-		const contractAddress = ($token as OptionErc20Token)?.address;
+		assertNonNullish(selectedToken);
 
-		assertNonNullish(contractAddress);
+		const { version, symbol, network, address, decimals } = selectedToken;
+		const { chainId } = network as EthereumNetwork;
 
-		await removeUserToken({
+		await setUserToken({
 			...params,
-			tokenId: {
-				chain_id: $selectedChainId,
-				contract_address: contractAddress
+			token: {
+				chain_id: chainId,
+				decimals: toNullable(decimals),
+				contract_address: address,
+				symbol: toNullable(symbol),
+				version: toNullable(version),
+				enabled: toNullable(false)
 			}
 		});
 	};
 
-	const updateUi = async () => {
-		assertNonNullish(selectedToken);
-
-		erc20TokensStore.remove(selectedToken.id);
-	};
+	const updateUi = (params: { identity: Identity }): Promise<void> => loadUserTokens(params);
 </script>
 
 <HideTokenModal backToNetworkId={ETHEREUM_NETWORK_ID} {assertHide} {hideToken} {updateUi} />
