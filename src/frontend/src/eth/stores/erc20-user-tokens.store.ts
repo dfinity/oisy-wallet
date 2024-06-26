414f72f1c049991ad4ef37c9731878d10c406db6
David Dal Busco <david.dalbusco@outlook.com>
2024-06-26 06:28:18 +0200

    fix: spread order

--- src/frontend/src/eth/stores/erc20-user-tokens.store.ts
+++ src/frontend/src/eth/stores/erc20-user-tokens.store.ts
@@ -21,12 +21,12 @@ export const initCertifiedErc20UserTokensStore = (): CertifiedErc20UserTokensSto
 				{
 					certified,
 					data: {
+						...data,
 						// We are using Symbols as key IDs for the ETH and ICP tokens, which is ideal for our use case due to their uniqueness. This ensures that even if two coins fetched dynamically have the same symbol or name, they will be used correctly.
 						// However, this approach presents a challenge with ERC20 tokens, which need to be loaded twice - once with a query and once with an update. When they are loaded the second time, the existing Symbol should be reused to ensure they are identified as the same token.
 						id:
 							(state ?? []).find(({ data: { address } }) => address === data.address)?.data.id ??
 							Symbol(data.symbol),
-						...data
 					}
 				}
 			]),
