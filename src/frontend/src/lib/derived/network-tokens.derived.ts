90a4e662e6191ee0d0d18a64ada5af0a296a22d7
David Dal Busco <david.dalbusco@outlook.com>
2024-06-26 10:20:37 +0200

    refactor: rename constant

--- src/frontend/src/lib/derived/network-tokens.derived.ts
+++ src/frontend/src/lib/derived/network-tokens.derived.ts
@@ -1,4 +1,4 @@
-import { ICRC_SELECTED_LEDGER_CANISTER_IDS } from '$env/networks.icrc.env';
+import { ICRC_CHAIN_FUSION_DEFAULT_LEDGER_CANISTER_IDS } from '$env/networks.icrc.env';
 import { ETHEREUM_TOKEN_ID, ICP_TOKEN_ID } from '$env/tokens.env';
 import { isTokenIcrcTestnet } from '$icp/utils/icrc-ledger.utils';
 import {
@@ -37,7 +37,7 @@ export const filteredNetworkTokens: Readable<Token[]> = derived(
 					(token) =>
 						[ICP_TOKEN_ID, ETHEREUM_TOKEN_ID].includes(token.id) ||
 						('ledgerCanisterId' in token &&
-							ICRC_SELECTED_LEDGER_CANISTER_IDS.includes(
+							ICRC_CHAIN_FUSION_DEFAULT_LEDGER_CANISTER_IDS.includes(
 								(token as { ledgerCanisterId: CanisterIdText }).ledgerCanisterId
 							)) ||
 						('enabled' in token && token.enabled)
