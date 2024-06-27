10492178a1d1c0e7486c4c0766994218546ea64e
David Dal Busco <david.dalbusco@dfinity.org>
2024-06-27 16:16:12 +0200

    feat: integrate ckPEPE (#1601)

--- src/frontend/src/env/tokens.pepe.env.ts
+++ src/frontend/src/env/tokens.pepe.env.ts
@@ -1,8 +1,26 @@
-import { SEPOLIA_NETWORK } from '$env/networks.env';
+import { ETHEREUM_NETWORK, SEPOLIA_NETWORK } from '$env/networks.env';
 import type { RequiredErc20Token } from '$eth/types/erc20';
 import pepe from '$icp-eth/assets/pepe.svg';
 
 export const PEPE_DECIMALS = 18;
+
+export const PEPE_SYMBOL = 'LINK';
+
+export const PEPE_TOKEN_ID: unique symbol = Symbol(PEPE_SYMBOL);
+
+export const PEPE_TOKEN: RequiredErc20Token = {
+	id: PEPE_TOKEN_ID,
+	network: ETHEREUM_NETWORK,
+	standard: 'erc20',
+	category: 'default',
+	name: 'Pepe',
+	symbol: 'PEPE',
+	decimals: PEPE_DECIMALS,
+	icon: pepe,
+	address: '0x6982508145454Ce325dDbE47a25d4ec3d2311933',
+	exchange: 'erc20',
+	twinTokenSymbol: 'ckPEPE'
+};
 
 export const SEPOLIA_PEPE_SYMBOL = 'SepoliaPEPE';
 
