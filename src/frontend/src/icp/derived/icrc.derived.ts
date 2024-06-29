21eac65e7351e5430e1659d12659cd0201dd9539
David Dal Busco <david.dalbusco@dfinity.org>
2024-06-29 13:01:12 +0200

    feat: load default and custom icrc tokens with duplicate (#1610)
    * feat: allow user to enable or disable default Icrc tokens
    
    * feat: icrc default toggleable
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: use mapper
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: more derived stores
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: use global token user types
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: all icrc tokens
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: enabled and all icrc tokens
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: use all tokens
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * chore: format
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: use all icrc tokens
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    ---------
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/icp/derived/icrc.derived.ts
+++ src/frontend/src/icp/derived/icrc.derived.ts
@@ -1,12 +1,17 @@
 import { icrcCustomTokensStore } from '$icp/stores/icrc-custom-tokens.store';
 import { icrcDefaultTokensStore } from '$icp/stores/icrc-default-tokens.store';
 import type { IcToken } from '$icp/types/ic';
+import type { IcTokenToggleable } from '$icp/types/ic-token-toggleable';
 import type { IcrcCustomToken } from '$icp/types/icrc-custom-token';
 import { isTokenIcrcTestnet } from '$icp/utils/icrc-ledger.utils';
 import { sortIcTokens } from '$icp/utils/icrc.utils';
 import { testnets } from '$lib/derived/testnets.derived';
+import { mapDefaultTokenToToggleable } from '$lib/utils/token.utils';
 import { derived, type Readable } from 'svelte/store';
 
+/**
+ * The list of Icrc default tokens - i.e. the statically configured Icrc tokens of Oisy + their metadata, unique ids etc. fetched at runtime.
+ */
 export const icrcDefaultTokens: Readable<IcToken[]> = derived(
 	[icrcDefaultTokensStore, testnets],
 	([$icrcTokensStore, $testnets]) =>
@@ -15,25 +20,94 @@ export const icrcDefaultTokens: Readable<IcToken[]> = derived(
 		)
 );
 
+/**
+ * A flatten list of the default Icrc Ledger and Index canister Ids.
+ */
+const icrcDefaultTokensCanisterIds: Readable<string[]> = derived(
+	[icrcDefaultTokens],
+	([$icrcDefaultTokens]) =>
+		$icrcDefaultTokens.map(
+			({ ledgerCanisterId, indexCanisterId }) => `${ledgerCanisterId}:${indexCanisterId}`
+		)
+);
+
+/**
+ * The list of Icrc tokens the user has added, enabled or disabled. Can contains default tokens for example if user has disabled a default tokens.
+ * i.e. default tokens are configured on the client side. If user disable or enable a default tokens, this token is added as a "custom token" in the backend.
+ */
 export const icrcCustomTokens: Readable<IcrcCustomToken[]> = derived(
 	[icrcCustomTokensStore],
 	([$icrcCustomTokensStore]) => $icrcCustomTokensStore?.map(({ data: token }) => token) ?? []
 );
 
-const icrcCustomTokensEnabled: Readable<IcrcCustomToken[]> = derived(
+const icrcDefaultTokensToggleable: Readable<IcTokenToggleable[]> = derived(
+	[icrcDefaultTokens, icrcCustomTokens],
+	([$icrcDefaultTokens, $icrcUserTokens]) =>
+		$icrcDefaultTokens.map(({ ledgerCanisterId, indexCanisterId, ...rest }) => {
+			const userToken = $icrcUserTokens.find(
+				({ ledgerCanisterId: userLedgerCanisterId, indexCanisterId: userIndexCanisterId }) =>
+					userLedgerCanisterId === ledgerCanisterId && userIndexCanisterId === indexCanisterId
+			);
+
+			return mapDefaultTokenToToggleable({
+				defaultToken: {
+					ledgerCanisterId,
+					indexCanisterId,
+					...rest
+				},
+				userToken
+			});
+		})
+);
+
+/**
+ * The list of default tokens that are enabled - i.e. the list of default Icrc tokens minus those disabled by the user.
+ */
+const enabledIcrcDefaultTokens: Readable<IcToken[]> = derived(
+	[icrcDefaultTokensToggleable],
+	([$icrcDefaultTokensToggleable]) => $icrcDefaultTokensToggleable.filter(({ enabled }) => enabled)
+);
+
+/**
+ * The list of Icrc tokens enabled by the user - i.e. saved in the backend canister as enabled - minus those that duplicate default tokens.
+ * We do so because the default statically configured are those to be used for various feature. This is notably useful for ERC20 <> ckERC20 conversion given that tokens on both sides (ETH an IC) should know about each others ("Twin Token" links).
+ */
+const icrcCustomTokensToggleable: Readable<IcrcCustomToken[]> = derived(
+	[icrcCustomTokens, icrcDefaultTokensCanisterIds],
+	([$icrcCustomTokens, $icrcDefaultTokensCanisterIds]) =>
+		$icrcCustomTokens.filter(
+			({ ledgerCanisterId, indexCanisterId }) =>
+				!$icrcDefaultTokensCanisterIds.includes(`${ledgerCanisterId}:${indexCanisterId}`)
+		)
+);
+
+const enabledIcrcCustomTokens: Readable<IcrcCustomToken[]> = derived(
 	[icrcCustomTokens],
 	([$icrcCustomTokens]) => $icrcCustomTokens.filter(({ enabled }) => enabled)
 );
 
-export const enabledIcrcTokens: Readable<IcToken[]> = derived(
-	[icrcDefaultTokens, icrcCustomTokensEnabled],
-	([$icrcDefaultTokens, $icrcCustomTokensEnabled]) => [
-		...$icrcDefaultTokens,
-		...$icrcCustomTokensEnabled
+/**
+ * The list of all Icrc tokens.
+ */
+export const icrcTokens: Readable<IcToken[]> = derived(
+	[icrcDefaultTokensToggleable, icrcCustomTokensToggleable],
+	([$icrcDefaultTokensToggleable, $icrcCustomTokensToggleable]) => [
+		...$icrcDefaultTokensToggleable,
+		...$icrcCustomTokensToggleable
 	]
 );
 
-export const sortedEnabledIcrcTokens: Readable<IcToken[]> = derived(
-	[enabledIcrcTokens],
-	([$enabledIcrcTokens]) => $enabledIcrcTokens.sort(sortIcTokens)
+export const sortedIcrcTokens: Readable<IcToken[]> = derived([icrcTokens], ([$icrcTokens]) =>
+	$icrcTokens.sort(sortIcTokens)
+);
+
+/**
+ * The list of Icrc tokens that are either enabled by default (static config) or enabled by the users regardless if they are custom or default.
+ */
+export const enabledIcrcTokens: Readable<IcToken[]> = derived(
+	[enabledIcrcDefaultTokens, enabledIcrcCustomTokens],
+	([$enabledIcrcDefaultTokens, $enabledIcrcCustomTokens]) => [
+		...$enabledIcrcDefaultTokens,
+		...$enabledIcrcCustomTokens
+	]
 );
---
+++ src/frontend/src/icp/types/ic-token-toggleable.ts
@@ -0,0 +1,4 @@
+import type { IcToken } from '$icp/types/ic';
+import type { TokenToggleable } from '$lib/types/token-toggleable';
+
+export type IcTokenToggleable = TokenToggleable<IcToken>;
