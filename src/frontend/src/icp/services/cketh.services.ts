f247c186dbd89b563bb75ad666a1c9d2a11b16a6
David Dal Busco <david.dalbusco@dfinity.org>
2024-07-01 17:43:53 +0200

    feat(frontend): pass ckErc20 ledger canister id to get eip1559TransactionPrice (#1644)
    * feat: pass ckErc20 ledger canister id to get eip1559TransactionPrice
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: type
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * chore: format
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    ---------
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/icp/services/cketh.services.ts
+++ src/frontend/src/icp/services/cketh.services.ts
@@ -1,19 +1,30 @@
 import { eip1559TransactionPrice } from '$icp/api/cketh-minter.api';
 import { eip1559TransactionPriceStore } from '$icp/stores/cketh.store';
 import type { IcCkToken } from '$icp/types/ic';
+import { isTokenCkErc20Ledger } from '$icp/utils/ic-send.utils';
 import { queryAndUpdate } from '$lib/actors/query.ic';
 import { i18n } from '$lib/stores/i18n.store';
 import { toastsError } from '$lib/stores/toasts.store';
 import { AnonymousIdentity } from '@dfinity/agent';
 import type { Eip1559TransactionPrice } from '@dfinity/cketh';
+import { Principal } from '@dfinity/principal';
 import { assertNonNullish } from '@dfinity/utils';
 import { get } from 'svelte/store';
 
-export const loadEip1559TransactionPrice = async ({ id: tokenId, minterCanisterId }: IcCkToken) => {
+export const loadEip1559TransactionPrice = async (token: IcCkToken) => {
+	const { id: tokenId, minterCanisterId, ledgerCanisterId } = token;
+
 	assertNonNullish(minterCanisterId, get(i18n).init.error.minter_cketh_info);
 
 	await queryAndUpdate<Eip1559TransactionPrice>({
-		request: (params) => eip1559TransactionPrice({ minterCanisterId, ...params }),
+		request: (params) =>
+			eip1559TransactionPrice({
+				minterCanisterId,
+				...(isTokenCkErc20Ledger(token) && {
+					ckErc20LedgerId: Principal.fromText(ledgerCanisterId)
+				}),
+				...params
+			}),
 		onLoad: ({ certified, response: price }) =>
 			eip1559TransactionPriceStore.set({
 				tokenId,
