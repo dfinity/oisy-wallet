17c3ab31331a77a1ca3cde82ccccde5b7d3a4577
David Dal Busco <david.dalbusco@outlook.com>
2024-07-01 16:22:18 +0200

    feat: oct logo as in ledger
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/env/tokens.oct.env.ts
+++ src/frontend/src/env/tokens.oct.env.ts
@@ -1,6 +1,6 @@
 import { ETHEREUM_NETWORK } from '$env/networks.env';
 import type { RequiredErc20Token } from '$eth/types/erc20';
-import chainlink from '$icp-eth/assets/chainlink.svg';
+import oct from '$icp-eth/assets/oct.svg';
 
 export const OCT_DECIMALS = 18;
 
@@ -16,7 +16,7 @@ export const OCT_TOKEN: RequiredErc20Token = {
 	name: 'Octopus Network Token',
 	symbol: OCT_SYMBOL,
 	decimals: OCT_DECIMALS,
-	icon: chainlink,
+	icon: oct,
 	address: '0xF5cFBC74057C610c8EF151A439252680AC68c6DC',
 	exchange: 'erc20',
 	twinTokenSymbol: 'ckOCT'
