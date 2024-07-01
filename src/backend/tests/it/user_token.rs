f0c81d2236eee3dc7a4ebe02b0caf67db1b39762
David Dal Busco <david.dalbusco@dfinity.org>
2024-07-01 16:01:15 +0200

    chore: remove unused to_string() (#1639)
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/backend/tests/it/user_token.rs
+++ src/backend/tests/it/user_token.rs
@@ -35,7 +35,7 @@ lazy_static! {
 fn test_add_user_token() {
     let pic_setup = setup();
 
-    let caller = Principal::from_text(CALLER.to_string()).unwrap();
+    let caller = Principal::from_text(CALLER).unwrap();
 
     let result = update_call::<()>(&pic_setup, caller, "set_user_token", MOCK_TOKEN.clone());
 
@@ -46,7 +46,7 @@ fn test_add_user_token() {
 fn test_add_many_custom_tokens() {
     let pic_setup = setup();
 
-    let caller = Principal::from_text(CALLER.to_string()).unwrap();
+    let caller = Principal::from_text(CALLER).unwrap();
 
     let tokens: Vec<UserToken> = vec![MOCK_TOKEN.clone(), ANOTHER_TOKEN.clone()];
 
@@ -59,7 +59,7 @@ fn test_add_many_custom_tokens() {
 fn test_update_user_token() {
     let pic_setup = setup();
 
-    let caller = Principal::from_text(CALLER.to_string()).unwrap();
+    let caller = Principal::from_text(CALLER).unwrap();
 
     let result = update_call::<()>(&pic_setup, caller, "set_user_token", MOCK_TOKEN.clone());
 
@@ -95,7 +95,7 @@ fn test_update_user_token() {
 fn test_update_many_user_tokens() {
     let pic_setup = setup();
 
-    let caller = Principal::from_text(CALLER.to_string()).unwrap();
+    let caller = Principal::from_text(CALLER).unwrap();
 
     let tokens: Vec<UserToken> = vec![MOCK_TOKEN.clone(), ANOTHER_TOKEN.clone()];
 
@@ -156,7 +156,7 @@ fn test_update_many_user_tokens() {
 fn test_disable_user_token() {
     let pic_setup = setup();
 
-    let caller = Principal::from_text(CALLER.to_string()).unwrap();
+    let caller = Principal::from_text(CALLER).unwrap();
 
     let result = update_call::<()>(&pic_setup, caller, "set_user_token", MOCK_TOKEN.clone());
 
@@ -192,7 +192,7 @@ fn test_disable_user_token() {
 fn test_list_user_tokens() {
     let pic_setup = setup();
 
-    let caller = Principal::from_text(CALLER.to_string()).unwrap();
+    let caller = Principal::from_text(CALLER).unwrap();
 
     let _ = update_call::<()>(&pic_setup, caller, "set_user_token", MOCK_TOKEN.clone());
 
@@ -216,7 +216,7 @@ fn test_list_user_tokens() {
 fn test_cannot_update_user_token_without_version() {
     let pic_setup = setup();
 
-    let caller = Principal::from_text(CALLER.to_string()).unwrap();
+    let caller = Principal::from_text(CALLER).unwrap();
 
     let result = update_call::<()>(&pic_setup, caller, "set_user_token", MOCK_TOKEN.clone());
 
@@ -241,7 +241,7 @@ fn test_cannot_update_user_token_without_version() {
 fn test_cannot_update_user_token_with_invalid_version() {
     let pic_setup = setup();
 
-    let caller = Principal::from_text(CALLER.to_string()).unwrap();
+    let caller = Principal::from_text(CALLER).unwrap();
 
     let result = update_call::<()>(&pic_setup, caller, "set_user_token", MOCK_TOKEN.clone());
 
@@ -266,7 +266,7 @@ fn test_cannot_update_user_token_with_invalid_version() {
 fn test_set_user_token_enabled_none() {
     let pic_setup = setup();
 
-    let caller = Principal::from_text(CALLER.to_string()).unwrap();
+    let caller = Principal::from_text(CALLER).unwrap();
 
     let token: UserToken = UserToken {
         enabled: None,
@@ -285,7 +285,7 @@ fn test_set_user_token_enabled_none() {
 fn test_set_many_user_tokens_enabled_none() {
     let pic_setup = setup();
 
-    let caller = Principal::from_text(CALLER.to_string()).unwrap();
+    let caller = Principal::from_text(CALLER).unwrap();
 
     let token: UserToken = UserToken {
         enabled: None,
@@ -306,7 +306,7 @@ fn test_set_many_user_tokens_enabled_none() {
 fn test_set_user_token_symbol_max_length() {
     let pic_setup = setup();
 
-    let caller = Principal::from_text(CALLER.to_string()).unwrap();
+    let caller = Principal::from_text(CALLER).unwrap();
 
     let token: UserToken = UserToken {
         chain_id: SEPOLIA_CHAIN_ID,
@@ -329,7 +329,7 @@ fn test_set_user_token_symbol_max_length() {
 fn test_set_user_many_tokens_symbol_max_length() {
     let pic_setup = setup();
 
-    let caller = Principal::from_text(CALLER.to_string()).unwrap();
+    let caller = Principal::from_text(CALLER).unwrap();
 
     let token: UserToken = UserToken {
         chain_id: SEPOLIA_CHAIN_ID,
@@ -390,7 +390,7 @@ fn test_anonymous_cannot_list_user_tokens() {
 fn test_user_cannot_list_another_user_tokens() {
     let pic_setup = setup();
 
-    let caller = Principal::from_text(CALLER.to_string()).unwrap();
+    let caller = Principal::from_text(CALLER).unwrap();
 
     let _ = update_call::<()>(&pic_setup, caller, "set_user_token", MOCK_TOKEN.clone());
 
