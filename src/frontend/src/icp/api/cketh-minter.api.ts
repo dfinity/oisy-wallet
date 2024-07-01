e4b7b1fde0bb62fc5183a6580761eb159e594bb9
David Dal Busco <david.dalbusco@outlook.com>
2024-07-01 17:42:47 +0200

    chore: format
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/icp/api/cketh-minter.api.ts
+++ src/frontend/src/icp/api/cketh-minter.api.ts
@@ -1,9 +1,12 @@
 import { ckEthMinterCanister } from '$icp-eth/api/cketh-minter.api';
 import type { CanisterIdText } from '$lib/types/canister';
 import type { OptionIdentity } from '$lib/types/identity';
-import type { Eip1559TransactionPrice, RetrieveEthRequest } from '@dfinity/cketh';
+import type {
+	Eip1559TransactionPrice,
+	Eip1559TransactionPriceParams,
+	RetrieveEthRequest
+} from '@dfinity/cketh';
 import type { RetrieveErc20Request } from '@dfinity/cketh/dist/candid/minter';
-import type { Eip1559TransactionPriceParams } from '@dfinity/cketh';
 import { Principal } from '@dfinity/principal';
 import { assertNonNullish } from '@dfinity/utils';
 
