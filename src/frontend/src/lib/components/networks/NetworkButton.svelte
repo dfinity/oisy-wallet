188831038476f988edff3370496d00f5c0182e4f
David Dal Busco <david.dalbusco@outlook.com>
2024-07-01 07:58:59 +0200

    feat: component to display text and logo
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/components/networks/NetworkButton.svelte
+++ src/frontend/src/lib/components/networks/NetworkButton.svelte
@@ -7,8 +7,7 @@
 	import { networkId } from '$lib/derived/network.derived';
 	import { back, isRouteTransactions, switchNetwork } from '$lib/utils/nav.utils';
 	import { page } from '$app/stores';
-	import { replacePlaceholders } from '$lib/utils/i18n.utils';
-	import { i18n } from '$lib/stores/i18n.store';
+	import TextWithLogo from '$lib/components/ui/TextWithLogo.svelte';
 
 	export let id: NetworkId | undefined;
 	export let name: string;
@@ -29,15 +28,7 @@
 </script>
 
 <button class="w-full flex justify-between items-center" on:click={onClick}>
-	<div class="flex gap-2 items-center">
-		<Logo
-			src={icon}
-			alt={replacePlaceholders($i18n.core.alt.logo, {
-				$name: name
-			})}
-		/>
-		<span>{name}</span>
-	</div>
+	<TextWithLogo {name} {icon} logo="start" />
 
 	{#if id === $networkId}
 		<span in:fade><IconCheck size="20px" /></span>
