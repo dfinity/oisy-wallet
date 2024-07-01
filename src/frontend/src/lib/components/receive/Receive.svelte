5e793e4b8bc0be8a17b38fa91f722cfd53db6f0f
David Dal Busco <david.dalbusco@outlook.com>
2024-07-01 13:22:31 +0200

    feat: receive information on chain fusion
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

---
+++ src/frontend/src/lib/components/receive/Receive.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { modalStore } from '$lib/stores/modal.store';
+	import ReceiveAddressModal from '$lib/components/receive/ReceiveAddressModal.svelte';
+	import IcReceiveInfoIcrc from '$icp/components/receive/IcReceiveInfoIcrc.svelte';
+	import ReceiveButton from '$lib/components/receive/ReceiveButton.svelte';
+	import { modalIcrcReceive } from '$lib/derived/modal.derived';
+
+	const modalId = Symbol();
+
+	const openReceive = async () => {
+		modalStore.openIcrcReceive(modalId);
+	};
+</script>
+
+<ReceiveButton on:click={openReceive} />
+
+{#if $modalIcrcReceive && $modalStore?.data === modalId}
+	<ReceiveAddressModal infoCmp={IcReceiveInfoIcrc} on:nnsClose={close} />
+{/if}
