7424db2bd9dbb9e66575b3094ebdf0bd6f39f6da
David Dal Busco <david.dalbusco@dfinity.org>
2024-07-03 07:06:22 +0200

    feat(frontend): load less erc20 exchanges (#1667)
    * feat: load less erc20 exchanges
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: type for address
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * chore: lint
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    ---------
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/eth/types/address.ts
+++ src/frontend/src/eth/types/address.ts
@@ -1,3 +1,5 @@
 import type { BaseContract } from 'ethers';
 
 export type ContractAddress = Pick<BaseContract, 'address'>;
+
+export type ContractAddressText = typeof BaseContract.prototype.address;
