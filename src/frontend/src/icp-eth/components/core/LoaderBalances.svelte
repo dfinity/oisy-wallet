660bf72340ad0093df57ead928f2318789ddcb6c
David Dal Busco <david.dalbusco@outlook.com>
2024-07-05 13:54:52 +0200

    fix(frontend): load Erc20 balances
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/icp-eth/components/core/LoaderBalances.svelte
+++ src/frontend/src/icp-eth/components/core/LoaderBalances.svelte
@@ -1,37 +1,21 @@
 <script lang="ts">
 	import { loadBalances, loadErc20Balances } from '$eth/services/balance.services';
 	import { address } from '$lib/derived/address.derived';
-	import { networkEthereum } from '$lib/derived/network.derived';
+	import { selectedNetwork } from '$lib/derived/network.derived';
 	import { enabledErc20Tokens } from '$eth/derived/erc20.derived';
-	import { debounce } from '@dfinity/utils';
-	import { ERC20_TWIN_TOKENS } from '$env/tokens.erc20.env';
-	import { ckEthereumNativeToken, erc20ToCkErc20Enabled } from '$icp-eth/derived/cketh.derived';
-	import { tokenWithFallback } from '$lib/derived/token.derived';
+	import { debounce, nonNullish } from '@dfinity/utils';
 
 	const load = async () => {
 		await Promise.allSettled([
 			// We might require Ethereum balance on IC network as well given that one can convert ckETH to ETH.
 			loadBalances(),
 			...[
-				$networkEthereum
+				nonNullish($selectedNetwork?.id)
 					? [
 							loadErc20Balances({
 								address: $address,
 								erc20Tokens: $enabledErc20Tokens,
-								networkId: $tokenWithFallback.network.id
-							})
-						]
-					: []
-			],
-			...[
-				$erc20ToCkErc20Enabled
-					? [
-							// We might require Erc20 balance on IC network as well given that one can convert ckErc20 to Erc20.
-							// e.g. USDC <> ckUSDC
-							loadErc20Balances({
-								address: $address,
-								erc20Tokens: ERC20_TWIN_TOKENS,
-								networkId: $ckEthereumNativeToken.network.id
+								networkId: $selectedNetwork.id
 							})
 						]
 					: []
@@ -41,7 +25,7 @@
 
 	const debounceLoad = debounce(load);
 
-	$: $address, $enabledErc20Tokens, $tokenWithFallback, $erc20ToCkErc20Enabled, debounceLoad();
+	$: $address, $enabledErc20Tokens, $selectedNetwork, debounceLoad();
 </script>
 
 <slot />
