48e9b9ab86657ed5d86d6d9886aefdbee1e2214a
David Dal Busco <david.dalbusco@dfinity.org>
2024-07-05 14:11:06 +0200

    fix(frontend): load erc20 tokens balance on "Tokens" view (#1688)
    * fix(frontend): load Erc20 balances
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: filter enabled tokens
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: load only erc20
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    ---------
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/icp-eth/components/core/LoaderBalances.svelte
+++ src/frontend/src/icp-eth/components/core/LoaderBalances.svelte
@@ -1,47 +1,23 @@
 <script lang="ts">
 	import { loadBalances, loadErc20Balances } from '$eth/services/balance.services';
 	import { address } from '$lib/derived/address.derived';
-	import { networkEthereum } from '$lib/derived/network.derived';
-	import { enabledErc20Tokens } from '$eth/derived/erc20.derived';
 	import { debounce } from '@dfinity/utils';
-	import { ERC20_TWIN_TOKENS } from '$env/tokens.erc20.env';
-	import { ckEthereumNativeToken, erc20ToCkErc20Enabled } from '$icp-eth/derived/cketh.derived';
-	import { tokenWithFallback } from '$lib/derived/token.derived';
+	import { enabledErc20NetworkTokens } from '$lib/derived/network-tokens.derived';
 
 	const load = async () => {
 		await Promise.allSettled([
 			// We might require Ethereum balance on IC network as well given that one can convert ckETH to ETH.
 			loadBalances(),
-			...[
-				$networkEthereum
-					? [
-							loadErc20Balances({
-								address: $address,
-								erc20Tokens: $enabledErc20Tokens,
-								networkId: $tokenWithFallback.network.id
-							})
-						]
-					: []
-			],
-			...[
-				$erc20ToCkErc20Enabled
-					? [
-							// We might require Erc20 balance on IC network as well given that one can convert ckErc20 to Erc20.
-							// e.g. USDC <> ckUSDC
-							loadErc20Balances({
-								address: $address,
-								erc20Tokens: ERC20_TWIN_TOKENS,
-								networkId: $ckEthereumNativeToken.network.id
-							})
-						]
-					: []
-			]
+			loadErc20Balances({
+				address: $address,
+				erc20Tokens: $enabledErc20NetworkTokens
+			})
 		]);
 	};
 
 	const debounceLoad = debounce(load);
 
-	$: $address, $enabledErc20Tokens, $tokenWithFallback, $erc20ToCkErc20Enabled, debounceLoad();
+	$: $address, $enabledErc20NetworkTokens, debounceLoad();
 </script>
 
 <slot />
