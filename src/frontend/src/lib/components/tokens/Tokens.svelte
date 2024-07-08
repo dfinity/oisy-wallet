36d0f9409f64dae700d6b60b487a5d0303e24bf9
David Dal Busco <david.dalbusco@outlook.com>
2024-07-08 13:31:31 +0200

    feat: tokens signedin or out
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/components/tokens/Tokens.svelte
+++ src/frontend/src/lib/components/tokens/Tokens.svelte
@@ -1,59 +1,20 @@
 <script lang="ts">
-	import { BigNumber } from '@ethersproject/bignumber';
-	import { balancesStore } from '$lib/stores/balances.store';
-	import Listener from '$lib/components/core/Listener.svelte';
-	import TokensSkeletons from '$lib/components/tokens/TokensSkeletons.svelte';
-	import { enabledNetworkTokens } from '$lib/derived/network-tokens.derived';
-	import { i18n } from '$lib/stores/i18n.store';
-	import type { Token } from '$lib/types/token';
-	import { hideZeroBalancesStore } from '$lib/stores/settings.store';
-	import { fade } from 'svelte/transition';
-	import { modalManageTokens } from '$lib/derived/modal.derived';
-	import ManageTokensModal from '$icp-eth/components/tokens/ManageTokensModal.svelte';
-	import TokenCard from '$lib/components/tokens/TokenCard.svelte';
-	import { formatToken } from '$lib/utils/format.utils';
-	import CardAmount from '$lib/components/ui/CardAmount.svelte';
-	import ExchangeTokenValue from '$lib/components/exchange/ExchangeTokenValue.svelte';
-	import TokenReceiveSend from '$lib/components/tokens/TokenReceiveSend.svelte';
-
-	let displayZeroBalance: boolean;
-	$: displayZeroBalance = $hideZeroBalancesStore?.enabled !== true;
-
-	let tokens: Token[];
-	$: tokens = $enabledNetworkTokens.filter(
-		({ id: tokenId }) =>
-			($balancesStore?.[tokenId]?.data ?? BigNumber.from(0n)).gt(0n) || displayZeroBalance
-	);
+	import { i18n } from '$lib/stores/i18n.store.js';
+	import { authSignedIn } from '$lib/derived/auth.derived.js';
+	import TokensMenu from '$lib/components/tokens/TokensMenu.svelte';
+	import Header from '$lib/components/ui/Header.svelte';
+	import TokensSignedIn from '$lib/components/tokens/TokensSignedIn.svelte';
+	import TokensNotSignedIn from '$lib/components/tokens/TokensNotSignedIn.svelte';
 </script>
 
-<TokensSkeletons>
-	{#each tokens as token (token.id)}
-		<Listener {token}>
-			<div in:fade>
-				<TokenCard {token}>
-					<output class="break-all" slot="description">
-						{formatToken({
-							value: $balancesStore?.[token.id]?.data ?? BigNumber.from(0n),
-							unitName: token.decimals
-						})}
-						{token.symbol}
-					</output>
+<Header>
+	{$i18n.tokens.text.title}
 
-					<CardAmount slot="exchange">
-						<ExchangeTokenValue {token} />
-					</CardAmount>
+	<TokensMenu slot="end" />
+</Header>
 
-					<TokenReceiveSend {token} slot="actions" />
-				</TokenCard>
-			</div>
-		</Listener>
-	{/each}
-
-	{#if tokens.length === 0}
-		<p class="mt-4 text-dark opacity-50">{$i18n.tokens.text.all_tokens_with_zero_hidden}</p>
-	{/if}
-
-	{#if $modalManageTokens}
-		<ManageTokensModal />
-	{/if}
-</TokensSkeletons>
+{#if $authSignedIn}
+	<TokensSignedIn />
+{:else}
+	<TokensNotSignedIn />
+{/if}
--- src/frontend/src/routes/(app)/+page.svelte
+++ src/frontend/src/routes/(app)/+page.svelte
@@ -1,20 +1,5 @@
 <script lang="ts">
 	import Tokens from '$lib/components/tokens/Tokens.svelte';
-	import { i18n } from '$lib/stores/i18n.store';
-	import Header from '$lib/components/ui/Header.svelte';
-	import TokensMenu from '$lib/components/tokens/TokensMenu.svelte';
-	import { authSignedIn } from '$lib/derived/auth.derived';
-	import TokensShowcase from '$lib/components/tokens/TokensShowcase.svelte';
 </script>
 
-<Header>
-	{$i18n.tokens.text.title}
-
-	<TokensMenu slot="end" />
-</Header>
-
-{#if $authSignedIn}
-	<Tokens />
-{:else}
-	<TokensShowcase />
-{/if}
+<Tokens />
