3603f0832dcf58996b540ef5132a2a9f238fd704
David Dal Busco <david.dalbusco@dfinity.org>
2024-07-08 12:05:15 +0200

    feat(frontend): header sign-in (#1691)
    * feat(frontend): header sign-in
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: network switcher only if signed in
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * chore: revert
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    ---------
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

---
+++ src/frontend/src/lib/components/hero/SignIn.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { i18n } from '$lib/stores/i18n.store.js';
+	import { signIn } from '$lib/services/auth.services';
+	import icpLight from '$icp/assets/icp_light.svg';
+	import ButtonHero from '$lib/components/ui/ButtonHero.svelte';
+</script>
+
+<ButtonHero
+	on:click={async () => await signIn({})}
+	ariaLabel={$i18n.auth.alt.sign_in}
+	imgSrc={icpLight}
+	imgAlt={$i18n.networks.chain_fusion}>{$i18n.auth.text.connect}</ButtonHero
+>
