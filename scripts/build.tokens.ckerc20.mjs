6251a0c2fd6ea597229c4af3c3ef481a99a2aa6e
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-07-08 15:47:05 +0200

    feat(frontend): save ckErc20 logos when running the update

--- scripts/build.tokens.ckerc20.mjs
+++ scripts/build.tokens.ckerc20.mjs
@@ -2,9 +2,10 @@
 
 import { AnonymousIdentity } from '@dfinity/agent';
 import { CkETHOrchestratorCanister } from '@dfinity/cketh';
+import { IcrcLedgerCanister } from '@dfinity/ledger-icrc';
 import { Principal } from '@dfinity/principal';
 import { createAgent, fromNullable, isNullish, jsonReplacer } from '@dfinity/utils';
-import { writeFileSync } from 'node:fs';
+import { existsSync, writeFileSync } from 'node:fs';
 import { join } from 'node:path';
 
 const orchestratorInfo = async ({ orchestratorId: canisterId }) => {
@@ -78,6 +79,37 @@ const ORCHESTRATOR_PRODUCTION_ID = Principal.fromText('vxkom-oyaaa-aaaar-qafda-c
 
 const DATA_FOLDER = join(process.cwd(), 'src', 'frontend', 'src', 'env');
 
+const LOGO_FOLDER = join(process.cwd(), 'src', 'frontend', 'src', 'icp-eth', 'assets');
+
+const saveTokenLogo = async (canisterId, name) => {
+	const logoName = name.toLowerCase().replace('ck', '').replace('sepolia', '');
+	const file = join(LOGO_FOLDER, `${logoName}.svg`);
+
+	if (existsSync(file)) {
+		return;
+	}
+
+	const agent = await createAgent({
+		identity: new AnonymousIdentity(),
+		host: 'https://icp-api.io'
+	});
+
+	const { metadata } = IcrcLedgerCanister.create({
+		agent,
+		canisterId
+	});
+
+	const data = await metadata({});
+
+	const logoData = data.find((item) => item[0] === 'icrc1:logo')[1].Text;
+
+	const [encoding, encodedStr] = logoData.split(';')[1].split(',');
+
+	const svgContent = Buffer.from(encodedStr, encoding).toString('utf-8');
+
+	writeFileSync(file, svgContent, 'utf-8');
+};
+
 const findCkErc20 = async () => {
 	const [staging, production] = await Promise.all(
 		[ORCHESTRATOR_STAGING_ID, ORCHESTRATOR_PRODUCTION_ID].map(buildOrchestratorInfo)
@@ -89,6 +121,13 @@ const findCkErc20 = async () => {
 	};
 
 	writeFileSync(join(DATA_FOLDER, 'tokens.ckerc20.json'), JSON.stringify(tokens, jsonReplacer, 8));
+
+	for (const [name, { ledgerCanisterId }] of Object.entries({
+		...tokens.production,
+		...tokens.staging
+	})) {
+		await saveTokenLogo(ledgerCanisterId, name);
+	}
 };
 
 try {
