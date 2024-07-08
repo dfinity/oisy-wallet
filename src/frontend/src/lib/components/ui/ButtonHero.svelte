daa48bc5edac0ecd3c092009af6ca0de624336c2
David Dal Busco <david.dalbusco@outlook.com>
2024-07-08 08:07:51 +0200

    feat(frontend): button hero
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

---
+++ src/frontend/src/lib/components/ui/ButtonHero.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Img from '$lib/components/ui/Img.svelte';
+
+	export let disabled = false;
+	export let button: HTMLButtonElement | undefined = undefined;
+	export let ariaLabel: string;
+	export let imgSrc: string;
+	export let imgAlt: string;
+</script>
+
+<button
+	class="token icon desktop-wide"
+	bind:this={button}
+	on:click
+	aria-label={ariaLabel}
+	{disabled}
+	class:opacity-50={disabled}
+>
+	<span class="block w-[22px] h-[22px]">
+		<Img src={imgSrc} alt={imgAlt} width="100%" height="100%" rounded />
+	</span>
+	<span class="text-black font-bold"><slot /></span>
+</button>
