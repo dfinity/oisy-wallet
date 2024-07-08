ee1ff76ba3ec9687f53aa021eba3265cd560d200
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-07-08 16:30:32 +0200

    fix: fixed issue with public_key_to_p2pkh_address bad checksum

--- src/backend/src/bitcoin_utils.rs
+++ src/backend/src/bitcoin_utils.rs
@@ -3,50 +3,69 @@ use ic_cdk::api::management_canister::bitcoin::{BitcoinNetwork};
 use k256::sha2;
 use k256::sha2::Digest;
 
-fn parse_derivation_path(path: &str) -> Vec<Vec<u8>> {
-    let re = regex::Regex::new(r"^m/44'(/\d+'?){2}/[01]/\d+$").unwrap();
-    assert!(re.is_match(path), "Invalid BIP-44 derivation path format");
+struct DerivationPath {
+    account: u32,
+    change: u32,
+    address_index: u32,
+}
 
-    path.split('/')
-        .skip(1) // Skip the leading 'm'
-        .map(|s| {
-            if let Some(stripped) = s.strip_suffix('\'') {
-                // Handle hardened derivation (apostrophe indicates hardened)
-                // Value 0x80000000 is added to represent a hardened key.
-                // It is the hexadecimal of the smallest 32-bit value with the high-order bit set
-                // More details: https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki#Extended_keys
-                let index = u32::from_str(stripped).unwrap() + 0x8000_0000;
-                index.to_be_bytes().to_vec()
-            } else {
-                let index = u32::from_str(s).unwrap();
-                index.to_be_bytes().to_vec()
-            }
+impl FromStr for DerivationPath {
+    type Err = String;
+
+    fn from_str(path: &str) -> Result<Self, Self::Err> {
+        let re = regex::Regex::new(r"^m/44'(/\d+'?){2}/[01]/\d+$").unwrap();
+        if !re.is_match(path) {
+            return Err("Invalid BIP-44 derivation path format".into());
+        }
+
+        let segments: Vec<&str> = path.split('/').skip(1).collect();
+        let account = parse_segment(segments[1])?;
+        let change = parse_segment(segments[2])?;
+        let address_index = segments[3].parse::<u32>().map_err(|e| e.to_string())?;
+
+        Ok(DerivationPath {
+            account,
+            change,
+            address_index,
         })
-        .collect()
+    }
+}
+
+impl Into<Vec<Vec<u8>>> for DerivationPath {
+    fn into(self) -> Vec<Vec<u8>> {
+        vec![
+            self.account.to_be_bytes().to_vec(),
+            self.change.to_be_bytes().to_vec(),
+            self.address_index.to_be_bytes().to_vec(),
+        ]
+    }
+}
+
+fn parse_segment(segment: &str) -> Result<u32, String> {
+    if let Some(stripped) = segment.strip_suffix('\'') {
+        let index = stripped.parse::<u32>().map_err(|e| e.to_string())? + 0x8000_0000;
+        Ok(index)
+    } else {
+        segment.parse::<u32>().map_err(|e| e.to_string())
+    }
 }
 
 pub fn network_to_derivation_path(network: BitcoinNetwork) -> Vec<Vec<u8>> {
-    // This is a simplified version of the complete flow to create a derivation path.
-    // The documentation on the complete flow can be found at https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki
-    // For the current implementation and to spare computation workload, we are using the default account 0, change 0, and address index 0.
-    // For the Regtest network, we are using the Testnet derivation path, even if it is not necessary to conform to BIP-44, since it is a local node.
-    // However, we do that because we would like to have the same behavior of the Testnet network.
     let coin_type = match network {
         BitcoinNetwork::Mainnet => "0",
         BitcoinNetwork::Testnet | BitcoinNetwork::Regtest => "1",
     };
     let path = format!("m/44'/{coin_type}'/0'/0/0");
-    parse_derivation_path(&path)
+    let derivation_path: DerivationPath = path.parse().expect("Invalid derivation path");
+    derivation_path.into()
 }
-
-
-
 
 fn sha256(data: &[u8]) -> Vec<u8> {
     let mut hasher = sha2::Sha256::new();
     hasher.update(data);
     hasher.finalize().to_vec()
 }
+
 fn ripemd160(data: &[u8]) -> Vec<u8> {
     let mut hasher = ripemd::Ripemd160::new();
     hasher.update(data);
@@ -62,16 +81,17 @@ pub fn public_key_to_p2pkh_address(public_key: &[u8], network: BitcoinNetwork) -
         BitcoinNetwork::Testnet | BitcoinNetwork::Regtest => 0x6f,
         BitcoinNetwork::Mainnet => 0x00,
     };
+
     let checksum = {
-          let mut hasher = sha2::Sha256::new();
-          hasher.update(&[prefix]);
-          hasher.update(&result);
-          sha256(hasher.finalize().to_vec()).truncate(4)
+        let mut hasher = sha2::Sha256::new();
+        hasher.update(&[prefix]);
+        hasher.update(&result);
+        let hash_result = hasher.finalize().to_vec();
+        sha2::Sha256::digest(&hash_result)[..4].to_vec()
     };
 
-    let checksum = &sha256(&sha256(&data_with_prefix.clone()))[..4];
-
-    let mut full_address = data_with_prefix;
+    let mut full_address = vec![prefix];
+    full_address.extend(&result);
     full_address.extend(checksum);
 
     bs58::encode(full_address).into_string()
