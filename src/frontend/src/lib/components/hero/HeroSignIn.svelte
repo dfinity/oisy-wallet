2add4465fd9d857c1fb4585e83c10e3f1a30ec8a
David Dal Busco <david.dalbusco@outlook.com>
2024-07-08 08:37:48 +0200

    feat(frontend): hero sign-in
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

---
+++ src/frontend/src/lib/components/hero/HeroSignIn.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { i18n } from '$lib/stores/i18n.store.js';
+	import { signIn } from '$lib/services/auth.services';
+	import ButtonIC from '$lib/components/ui/ButtonIC.svelte';
+</script>
+
+<h1 class="text-off-white text-6xl">
+	{$i18n.auth.text.connect_to_oisy}
+</h1>
+
+<span class="text-off-white block my-4">{$i18n.auth.text.title}</span>
+
+<ButtonIC on:click={async () => await signIn({})} testId="login-button">
+	<svelte:fragment slot="action">{$i18n.auth.text.connect_with}</svelte:fragment>
+	Internet Identity
+</ButtonIC>
