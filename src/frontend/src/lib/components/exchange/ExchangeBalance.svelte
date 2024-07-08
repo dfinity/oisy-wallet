f73fdeb44925b6de11a60f848fec2c58586009a7
David Dal Busco <david.dalbusco@dfinity.org>
2024-07-08 11:10:44 +0200

    style(frontend): use tailwind class instead of inline style (#1694)
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/components/exchange/ExchangeBalance.svelte
+++ src/frontend/src/lib/components/exchange/ExchangeBalance.svelte
@@ -20,8 +20,7 @@
 
 <span class="text-off-white block">
 	<output
-		class={`break-all font-bold ${totalUsd === 0 ? 'opacity-50' : 'opacity-100'} inline-block mt-8`}
-		style="font-size: calc(2 * var(--font-size-h1)); line-height: 0.95;"
+		class={`break-all text-6xl font-bold ${totalUsd === 0 ? 'opacity-50' : 'opacity-100'} inline-block mt-8`}
 	>
 		{#if $exchangeInitialized}
 			{formatUSD(totalUsd, { notation: 'compact' })}
