cffa40b2a75d153f2477d01a5697c7f41639f0bb
David Dal Busco <david.dalbusco@dfinity.org>
2024-07-08 13:58:32 +0200

    feat(frontend): display tokens if not signed-in (#1704)
    * feat(frontend): header sign-in
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat(frontend): hero sign-in
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat(frontend): in-app sign-in
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat(frontend): deprecate sign-in page
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * refactor(frontend): move header hero
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: network switcher only if signed in
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * chore: revert
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: spacing
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * chore: lint
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat(frontend): display tokens if not signed-in
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * chore: no dev
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat(frontend): display empty information
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * chore: lint
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: tokens signedin or out
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * chore: revert
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: rename
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    ---------
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/components/tokens/Tokens.svelte
+++ src/frontend/src/lib/components/tokens/Tokens.svelte
@@ -1,33 +1,10 @@
 <script lang="ts">
-	import Card from '$lib/components/ui/Card.svelte';
-	import { formatToken } from '$lib/utils/format.utils';
-	import { BigNumber } from '@ethersproject/bignumber';
-	import { balancesStore } from '$lib/stores/balances.store';
-	import { transactionsUrl } from '$lib/utils/nav.utils';
-	import Listener from '$lib/components/core/Listener.svelte';
-	import TokensSkeletons from '$lib/components/tokens/TokensSkeletons.svelte';
-	import ExchangeTokenValue from '$lib/components/exchange/ExchangeTokenValue.svelte';
-	import { enabledNetworkTokens } from '$lib/derived/network-tokens.derived';
-	import { i18n } from '$lib/stores/i18n.store';
-	import Header from '$lib/components/ui/Header.svelte';
+	import { i18n } from '$lib/stores/i18n.store.js';
+	import { authSignedIn } from '$lib/derived/auth.derived.js';
 	import TokensMenu from '$lib/components/tokens/TokensMenu.svelte';
-	import type { Token } from '$lib/types/token';
-	import { hideZeroBalancesStore } from '$lib/stores/settings.store';
-	import { fade } from 'svelte/transition';
-	import { modalManageTokens } from '$lib/derived/modal.derived';
-	import ManageTokensModal from '$icp-eth/components/tokens/ManageTokensModal.svelte';
-	import TokenLogo from '$lib/components/tokens/TokenLogo.svelte';
-	import TokenReceiveSend from '$lib/components/tokens/TokenReceiveSend.svelte';
-	import CardAmount from '$lib/components/ui/CardAmount.svelte';
-
-	let displayZeroBalance: boolean;
-	$: displayZeroBalance = $hideZeroBalancesStore?.enabled !== true;
-
-	let tokens: Token[];
-	$: tokens = $enabledNetworkTokens.filter(
-		({ id: tokenId }) =>
-			($balancesStore?.[tokenId]?.data ?? BigNumber.from(0n)).gt(0n) || displayZeroBalance
-	);
+	import Header from '$lib/components/ui/Header.svelte';
+	import TokensSignedIn from '$lib/components/tokens/TokensSignedIn.svelte';
+	import TokensSignedOut from '$lib/components/tokens/TokensSignedOut.svelte';
 </script>
 
 <Header>
@@ -36,47 +13,8 @@
 	<TokensMenu slot="end" />
 </Header>
 
-<TokensSkeletons>
-	{#each tokens as token (token.id)}
-		{@const url = transactionsUrl({ token })}
-
-		<Listener {token}>
-			<div class="flex gap-3 sm:gap-8 mb-6">
-				<a
-					class="no-underline flex-1"
-					href={url}
-					aria-label={`Open the list of ${token.symbol} transactions`}
-					in:fade
-				>
-					<Card noMargin>
-						{token.name}
-
-						<TokenLogo {token} slot="icon" color="white" />
-
-						<output class="break-all" slot="description">
-							{formatToken({
-								value: $balancesStore?.[token.id]?.data ?? BigNumber.from(0n),
-								unitName: token.decimals
-							})}
-							{token.symbol}
-						</output>
-
-						<CardAmount slot="action">
-							<ExchangeTokenValue {token} />
-						</CardAmount>
-					</Card>
-				</a>
-
-				<TokenReceiveSend {token} />
-			</div>
-		</Listener>
-	{/each}
-
-	{#if tokens.length === 0}
-		<p class="mt-4 text-dark opacity-50">{$i18n.tokens.text.all_tokens_with_zero_hidden}</p>
-	{/if}
-
-	{#if $modalManageTokens}
-		<ManageTokensModal />
-	{/if}
-</TokensSkeletons>
+{#if $authSignedIn}
+	<TokensSignedIn />
+{:else}
+	<TokensSignedOut />
+{/if}
---
+++ src/frontend/src/lib/components/tokens/TokensSignedOut.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { enabledNetworkTokens } from '$lib/derived/network-tokens.derived.js';
+	import TokenCard from '$lib/components/tokens/TokenCard.svelte';
+</script>
+
+{#each $enabledNetworkTokens as token (token.id)}
+	<TokenCard {token}>
+		<span class="break-all" slot="description">
+			-/- {token.symbol}
+		</span>
+
+		<span slot="actions" class="mr-[3px] font-bold">-/-</span>
+	</TokenCard>
+{/each}
