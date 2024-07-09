e0287e90d2298895c4290b9fbb364e97a7fac17e
David Dal Busco <david.dalbusco@dfinity.org>
2024-07-09 09:26:55 +0200

    feat(frontend): about Oisy (#1721)
    * feat(frontend): about Oisy
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    * feat: about modal
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>
    
    ---------
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

---
+++ src/frontend/src/lib/components/hero/About.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { modalStore } from '$lib/stores/modal.store.js';
+	import { i18n } from '$lib/stores/i18n.store.js';
+	import { IconInfo } from '@dfinity/gix-components';
+	import { modalAbout } from '$lib/derived/modal.derived';
+	import AboutModal from '$lib/components/hero/AboutModal.svelte';
+</script>
+
+<button
+	class="block text-left text-off-white my-4 max-w-72 sm:max-w-[100%]"
+	on:click={modalStore.openAbout}
+>
+	<span>{$i18n.auth.text.title}</span>
+	<span class="align-middle inline-block"><IconInfo /></span></button
+>
+
+{#if $modalAbout}
+	<AboutModal />
+{/if}
