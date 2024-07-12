034b723b4c2a2b9c8b717891acb81f5f4e30572d
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-07-12 18:08:13 +0200

    style(frontend): include logos for new ERC20 tokens (#1742)
    * style(frontend): include logos for new ERC20 tokens
    
    * fix: reduce size of dmail and weeth

--- src/frontend/src/eth/utils/erc20.utils.ts
+++ src/frontend/src/eth/utils/erc20.utils.ts
@@ -1,8 +1,17 @@
 import oneInch from '$eth/assets/1inch.svg';
 import dai from '$eth/assets/dai.svg';
+import dmail from '$eth/assets/dmail.svg';
+import fet from '$eth/assets/fet.svg';
+import floki from '$eth/assets/floki.svg';
 import icpDark from '$eth/assets/icp_dark.svg';
+import jasmy from '$eth/assets/jasmy.svg';
+import matic from '$eth/assets/matic.svg';
+import rndr from '$eth/assets/rndr.svg';
 import uniswap from '$eth/assets/uniswap.svg';
 import usdt from '$eth/assets/usdt.svg';
+import wbtc from '$eth/assets/wbtc.svg';
+import weeth from '$eth/assets/weeth.svg';
+import wsteth from '$eth/assets/wsteth.svg';
 import type { Erc20Contract, Erc20Metadata, Erc20Token } from '$eth/types/erc20';
 import type { Erc20UserToken, EthereumUserToken } from '$eth/types/erc20-user-token';
 import type { EthereumNetwork } from '$eth/types/network';
@@ -40,12 +49,30 @@ export const mapErc20UserToken = ({
 
 const mapErc20Icon = (symbol: string): string | undefined => {
 	switch (symbol.toLowerCase()) {
+		case 'dai':
+			return dai;
+		case 'dmail':
+			return dmail;
+		case 'fet':
+			return fet;
+		case 'floki':
+			return floki;
+		case 'jasmy':
+			return jasmy;
+		case 'matic':
+			return matic;
+		case 'rndr':
+			return rndr;
 		case 'uni':
 			return uniswap;
 		case 'usdt':
 			return usdt;
-		case 'dai':
-			return dai;
+		case 'wbtc':
+			return wbtc;
+		case 'weeth':
+			return weeth;
+		case 'wsteth':
+			return wsteth;
 		case '1inch':
 			return oneInch;
 		// ICP in production. ckICP was used on staging because the definitive name and symbol had not been decided.
