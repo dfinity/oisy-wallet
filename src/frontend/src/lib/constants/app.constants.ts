0c5e5f24d80eaa246324544a4a61ddf49b29e17e
Llorenç <llorenc.muntaner@dfinity.org>
2024-07-12 08:59:38 +0200

    CR changes

--- src/frontend/src/lib/constants/app.constants.ts
+++ src/frontend/src/lib/constants/app.constants.ts
@@ -7,15 +7,15 @@ export const LOCAL = MODE === 'local';
 export const STAGING = MODE === 'staging';
 export const PROD = MODE === 'ic';
 
+const MAINNET_DOMAIN = 'ic0.app';
+
 export const INTERNET_IDENTITY_CANISTER_ID = LOCAL
 	? import.meta.env.VITE_LOCAL_INTERNET_IDENTITY_CANISTER_ID
 	: undefined;
 
 export const INTERNET_IDENTITY_ORIGIN = LOCAL
 	? `http://${INTERNET_IDENTITY_CANISTER_ID}.localhost:4943`
-	: STAGING
-		? `https://${INTERNET_IDENTITY_CANISTER_ID}.ic0.app`
-		: 'https://identity.ic0.app';
+	: 'https://identity.ic0.app';
 
 export const POH_ISSUER_CANISTER_ID = LOCAL
 	? import.meta.env.VITE_LOCAL_POH_ISSUER_CANISTER_ID
@@ -28,7 +28,7 @@ export const POH_ISSUER_CANISTER_ID = LOCAL
 export const POH_ISSUER_ORIGIN = nonNullish(POH_ISSUER_CANISTER_ID)
 	? LOCAL
 		? `http://${POH_ISSUER_CANISTER_ID}.localhost:4943`
-		: `https://${POH_ISSUER_CANISTER_ID}.ic0.app`
+		: `https://${POH_ISSUER_CANISTER_ID}.${MAINNET_DOMAIN}`
 	: undefined;
 
 export const BACKEND_CANISTER_ID = LOCAL
--- src/frontend/src/lib/services/request-poh-credential.services.ts
+++ src/frontend/src/lib/services/request-poh-credential.services.ts
@@ -17,21 +17,48 @@ import { get } from 'svelte/store';
 // This credential type is defined by the issuer Decide AI
 const POH_CREDENTIAL_TYPE = 'ProofOfUniqueness';
 
+const handleSuccess = async (
+	response: VerifiablePresentationResponse
+): Promise<{ success: boolean }> => {
+	const { auth: authI18n } = get(i18n);
+	if ('Ok' in response) {
+		// TODO: GIX-2646 Add credential to backend and load user profile
+		const fakeTemporaryCredentialSummary = {
+			credential_type: POH_CREDENTIAL_TYPE,
+			verified_date_timestamp: BigInt(Date.now()),
+			expire_date_timestamp: BigInt(Date.now() + 1000 * 60 * 60 * 24 * 365)
+		};
+		const fakeUserProfile: UserProfile = {
+			credentials: {
+				[POH_CREDENTIAL_TYPE]: fakeTemporaryCredentialSummary
+			},
+			created_timestamp: BigInt(Date.now()),
+			updated_timestamp: BigInt(Date.now())
+		};
+		userProfileStore.set({ key: 'user-profile', value: fakeUserProfile });
+		return { success: true };
+	}
+	toastsError({
+		msg: { text: authI18n.error.no_poh_credential }
+	});
+	return { success: false };
+};
+
 export const requestPohCredential = async ({
 	credentialSubject
 }: {
 	credentialSubject: Principal;
-}): Promise<boolean> => {
-	const i18Copy = get(i18n);
+}): Promise<{ success: boolean }> => {
+	const { auth: authI18n } = get(i18n);
 	return new Promise((resolve, reject) => {
 		const issuerCanisterId = nonNullish(POH_ISSUER_CANISTER_ID)
 			? Principal.fromText(POH_ISSUER_CANISTER_ID)
 			: undefined;
 		if (isNullish(POH_ISSUER_ORIGIN) || isNullish(issuerCanisterId)) {
 			toastsError({
-				msg: { text: i18Copy.auth.error.missing_poh_issuer_origin }
+				msg: { text: authI18n.error.missing_poh_issuer_origin }
 			});
-			resolve(false);
+			resolve({ success: false });
 			return;
 		}
 		requestVerifiablePresentation({
@@ -49,33 +76,12 @@ export const requestPohCredential = async ({
 			},
 			onError() {
 				toastsError({
-					msg: { text: i18Copy.auth.error.error_requesting_poh_credential }
+					msg: { text: authI18n.error.error_requesting_poh_credential }
 				});
 				reject();
 			},
-			onSuccess(response: VerifiablePresentationResponse) {
-				if ('Ok' in response) {
-					// TODO: GIX-2646 Add credential to backend and load user profile
-					const fakeCredentialSummary = {
-						credential_type: POH_CREDENTIAL_TYPE,
-						verified_date_timestamp: BigInt(Date.now()),
-						expire_date_timestamp: BigInt(Date.now() + 1000 * 60 * 60 * 24 * 365)
-					};
-					const fakeUserProfile: UserProfile = {
-						credentials: {
-							[POH_CREDENTIAL_TYPE]: fakeCredentialSummary
-						},
-						created_timestamp: BigInt(Date.now()),
-						updated_timestamp: BigInt(Date.now())
-					};
-					userProfileStore.set({ key: 'user-profile', value: fakeUserProfile });
-					resolve(true);
-					return;
-				}
-				toastsError({
-					msg: { text: i18Copy.auth.error.no_poh_credential }
-				});
-				resolve(false);
+			onSuccess: async (response: VerifiablePresentationResponse) => {
+				resolve(await handleSuccess(response));
 			}
 		});
 	});
