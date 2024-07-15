81b19ef977bd0cc5ce8e1747c060d47514a8271a
David Dal Busco <david.dalbusco@dfinity.org>
2024-07-15 11:57:40 +0200

    feat(frontend): env exchange erc20 (#1758)
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/env/tokens.erc20.env.ts
+++ src/frontend/src/env/tokens.erc20.env.ts
@@ -15,86 +15,86 @@ import type { TokenId } from '$lib/types/token';
 const ERC20_CONTRACT_ADDRESS_1INCH: Erc20Contract = {
 	// 1INCH
 	address: '0x111111111117dc0aa78b770fa6a738034120c302',
-	exchange: 'ethereum'
+	exchange: 'erc20'
 };
 
 const ERC20_CONTRACT_ADDRESS_WBTC: Erc20Contract = {
 	// Wrapped Bitcoin
 	address: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
-	exchange: 'ethereum'
+	exchange: 'erc20'
 };
 
 const ERC20_CONTRACT_ADDRESS_WSTETH: Erc20Contract = {
 	// Lido Finance (wstETH)
 	address: '0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0',
-	exchange: 'ethereum'
+	exchange: 'erc20'
 };
 
 const ERC20_CONTRACT_ADDRESS_USDT: Erc20Contract = {
 	// Tether
 	address: '0xdac17f958d2ee523a2206206994597c13d831ec7',
-	exchange: 'ethereum'
+	exchange: 'erc20'
 };
 
 const ERC20_CONTRACT_ADDRESS_DMAIL: Erc20Contract = {
 	// Dmail Network
 	address: '0xcC6f1e1B87cfCbe9221808d2d85C501aab0B5192',
-	exchange: 'ethereum'
+	exchange: 'erc20'
 };
 
 const ERC20_CONTRACT_ADDRESS_UNISWAP: Erc20Contract = {
 	// Uniswap
 	address: '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984',
-	exchange: 'ethereum'
+	exchange: 'erc20'
 };
 
 const ERC20_CONTRACT_ADDRESS_MATIC: Erc20Contract = {
 	// Polygon
 	address: '0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0',
-	exchange: 'ethereum'
+	exchange: 'erc20'
 };
 
 const ERC20_CONTRACT_ADDRESS_JASMY: Erc20Contract = {
 	// Jasmy
 	address: '0x7420B4b9a0110cdC71fB720908340C03F9Bc03EC',
-	exchange: 'ethereum'
+	exchange: 'erc20'
 };
 
 const ERC20_CONTRACT_ADDRESS_DAI: Erc20Contract = {
 	// Multi-Collateral DAI
 	address: '0x6b175474e89094c44da98b954eedeac495271d0f',
-	exchange: 'ethereum'
+	exchange: 'erc20'
 };
 
 const ERC20_CONTRACT_ADDRESS_FLOKI: Erc20Contract = {
 	// Floki Inu
 	address: '0xcf0c122c6b73ff809c693db761e7baebe62b6a2e',
-	exchange: 'ethereum'
+	exchange: 'erc20'
 };
 
 const ERC20_CONTRACT_ADDRESS_RNDR: Erc20Contract = {
 	// Render
 	address: '0x6de037ef9ad2725eb40118bb1702ebb27e4aeb24',
-	exchange: 'ethereum'
+	exchange: 'erc20'
 };
 
 const ERC20_CONTRACT_ADDRESS_FET: Erc20Contract = {
 	// Fetch
 	address: '0xaea46A60368A7bD060eec7DF8CBa43b7EF41Ad85',
-	exchange: 'ethereum'
+	exchange: 'erc20'
 };
 
 const ERC20_CONTRACT_ADDRESS_WEEETH: Erc20Contract = {
 	// Wrapped Ether (weETH)
 	address: '0xCd5fE23C85820F7B72D0926FC9b05b43E359b7ee',
-	exchange: 'ethereum'
+	exchange: 'erc20'
 };
 
 const ERC20_CONTRACTS_SEPOLIA: Erc20Contract[] = [
 	{
 		// Weenus
 		address: '0x7439E9Bb6D8a84dd3A23fe621A30F95403F87fB9',
-		exchange: 'ethereum'
+		exchange: 'erc20'
 	},
 	ERC20_CONTRACT_ADDRESS_UNISWAP
 ];
@@ -110,7 +110,7 @@ const _ERC20_CONTRACTS_GOERLI: Erc20Contract[] = [
 	{
 		// Weenus
 		address: '0xaFF4481D10270F50f203E0763e2597776068CBc5',
-		exchange: 'ethereum'
+		exchange: 'erc20'
 	},
 	ERC20_CONTRACT_ADDRESS_UNISWAP
 ];
