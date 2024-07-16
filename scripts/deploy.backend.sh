5e973856c433ad3cd6104bd768462514c17692a8
Llorenç <llorenc.muntaner@dfinity.org>
2024-07-16 10:09:45 +0200

    Add SupportedCredential

--- scripts/deploy.backend.sh
+++ scripts/deploy.backend.sh
@@ -16,7 +16,15 @@ case $ENV in
     ;;
 esac
 
-echo $WALLET
+II_CANISTER_ID="$(dfx canister id internet_identity --network "$ENV")"
+POUH_ISSUER_CANISTER_ID="$(dfx canister id pouh_issuer --network "$ENV")"
+# URL used by II-issuer in the id_alias-verifiable credentials (hard-coded in II)
+# Represents more an ID than a URL
+II_VC_URL="https://identity.ic0.app"
+# URL used by issuer in the issued verifiable credentials (tipically hard-coded)
+# Represents more an ID than a URL
+# TODO: To be confirmed by DECIDE AI team
+POUH_ISSUER_VC_URL="https://www.elna.ai"
 
 # At the time of writing dfx outputs incorrect JSON with dfx ping (commas between object
 # entries are missing).
@@ -35,7 +43,15 @@ if [ -n "${ENV+1}" ]; then
     Init = record {
          ecdsa_key_name = \"$ECDSA_KEY_NAME\";
          allowed_callers = vec {};
-         supported_credentials = null;
+         supported_credentials = opt vec {
+            record {
+              credential_type = \"ProofOfUniqueness\";
+              ii_origin = \"$II_VC_URL\";
+              ii_canister_id = \"$II_CANISTER_ID\";
+              issuer_origin = \"$POUH_ISSUER_VC_URL\";
+              issuer_canister_id = \"$POUH_ISSUER_CANISTER_ID\";
+            }
+         };
          ic_root_key_der = opt vec $rootkey_did;
      }
   })" --network "$ENV" # add --wallet $WALLET when I figure out the problem
@@ -44,7 +60,15 @@ else
     Init = record {
          ecdsa_key_name = \"$ECDSA_KEY_NAME\";
          allowed_callers = vec {};
-         supported_credentials = null;
+         supported_credentials = opt vec {
+            record {
+              credential_type = \"ProofOfUniqueness\";
+              ii_origin = \"$II_VC_URL\";
+              ii_canister_id = \"$II_CANISTER_ID\";
+              issuer_origin = \"$POUH_ISSUER_VC_URL\";
+              issuer_canister_id = \"$POUH_ISSUER_CANISTER_ID\";
+            }
+         };
          ic_root_key_der = opt vec $rootkey_did;
      }
   })" --specified-id tdxud-2yaaa-aaaad-aadiq-cai
