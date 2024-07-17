3fa74be8ecf2e8dd6f6238cabcd9e03898099fc7
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-07-17 14:22:43 +0200

    style(frontend): NetworksSwitcher is moved from the header to the tokens list (#1791)
    * style(frontend): NetworksSwitcher is moved from the header to the tokens list
    
    * chore: remove reduntant argument

--- src/frontend/src/lib/components/tokens/Tokens.svelte
+++ src/frontend/src/lib/components/tokens/Tokens.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-	import { i18n } from '$lib/stores/i18n.store';
-	import { authSignedIn } from '$lib/derived/auth.derived.js';
+	import { authNotSignedIn, authSignedIn } from '$lib/derived/auth.derived.js';
 	import TokensMenu from '$lib/components/tokens/TokensMenu.svelte';
 	import Header from '$lib/components/ui/Header.svelte';
 	import TokensSignedIn from '$lib/components/tokens/TokensSignedIn.svelte';
 	import TokensSignedOut from '$lib/components/tokens/TokensSignedOut.svelte';
 	import ManageTokensButton from '$lib/components/tokens/ManageTokensButton.svelte';
+	import NetworksSwitcher from '$lib/components/networks/NetworksSwitcher.svelte';
 </script>
 
 <Header>
-	{$i18n.tokens.text.title}
+	<NetworksSwitcher disabled={$authNotSignedIn} />
 
 	<TokensMenu slot="end" />
 </Header>
