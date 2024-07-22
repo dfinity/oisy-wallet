125a16f2d96ad1461fb56b1ffaa26f41c3b5d111
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-07-22 11:58:07 +0200

    style(frontend): add crypto logos to the header when signed out

--- src/frontend/src/lib/components/hero/HeroSignIn.svelte
+++ src/frontend/src/lib/components/hero/HeroSignIn.svelte
@@ -2,9 +2,20 @@
 	import { i18n } from '$lib/stores/i18n.store';
 	import { signIn } from '$lib/services/auth.services';
 	import ButtonIC from '$lib/components/ui/ButtonIC.svelte';
+	import IconHeaderICP from '$lib/components/icons/IconHeaderICP.svelte';
+	import IconHeaderBTC from '$lib/components/icons/IconHeaderBTC.svelte';
+	import IconHeaderETH from '$lib/components/icons/IconHeaderETH.svelte';
+	import IconHeaderUSDC from '$lib/components/icons/IconHeaderUSDC.svelte';
 </script>
 
-<div class="mt-6 xl:mt-12 mb-10 pt-2">
+<div class="flex -space-x-2.5 mt-6 xl:mt-12">
+	<div class="z-0"><IconHeaderICP /></div>
+	<div class="z-10"><IconHeaderBTC /></div>
+	<div class="z-20"><IconHeaderETH /></div>
+	<div class="z-30"><IconHeaderUSDC /></div>
+</div>
+
+<div class="mt-5 mb-10 pt-2">
 	<h1 class="text-off-white text-4xl max-w-[22rem] text-center">
 		{$i18n.auth.text.title}
 	</h1>
