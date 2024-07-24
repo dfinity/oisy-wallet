136ad3d7d0478d76545149c222438f12e89f089a
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-07-24 11:02:45 +0200

    refactor: simplify loop to be more perfomant

--- scripts/check.unused.mjs
+++ scripts/check.unused.mjs
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import { readFileSync, readdirSync } from 'node:fs';
-import { basename, resolve } from 'node:path';
+import { readFileSync } from 'node:fs';
+import { basename, dirname, resolve } from 'node:path';
 import { findFiles } from './utils.mjs';
 
 const RED = '\x1b[31m';
@@ -11,33 +11,19 @@ const NC = '\x1b[0m'; // No Colour
 const DATA_DIR = 'src/frontend/src';
 const DATA_DIR_PATH = resolve(process.cwd(), DATA_DIR);
 
-const findSvelteFiles = (dir) =>
-	findFiles({ dir, extensions: ['.svelte'], ignoreDirs: ['routes'] });
-
-const searchForFileName = ({ filename, dir }) =>
-	readdirSync(dir, { withFileTypes: true }).some((file) => {
-		const res = resolve(dir, file.name);
-		return file.isDirectory()
-			? searchForFileName({ filename, dir: res })
-			: file.isFile() && readFileSync(res, 'utf-8').includes(filename);
-	});
-
-const checkFileUsage = ({ filename, dir }) => searchForFileName({ filename, dir });
-
-const logResult = (isUsed) => process.stdout.write(isUsed ? `${GREEN}.${NC}` : `${RED}x${NC}`);
+const findSvelteFiles = (dir) => findFiles({ dir, extensions: ['.svelte'] });
 
 const main = async () => {
 	console.log(`${NC}Scanning ${DATA_DIR} folder to find all .svelte files`);
-	console.log(`  ${GREEN}.${NC} means the .svelte is imported in another file`);
-	console.log(`  ${RED}x${NC} means the .svelte is not imported and should likely be removed`);
 
-	const svelteFiles = findSvelteFiles(DATA_DIR_PATH);
-	const unusedFiles = svelteFiles.filter((file) => {
-		const filename = basename(file);
-		const isUsed = checkFileUsage({ filename, dir: DATA_DIR });
-		logResult(isUsed);
-		return !isUsed;
-	});
+	const allSvelteFiles = findSvelteFiles(DATA_DIR_PATH);
+
+	const potentialUnusedFiles = allSvelteFiles.filter((file) => !dirname(file).includes('routes'));
+
+	const unusedFiles = allSvelteFiles.reduce((acc, file) => {
+		const content = readFileSync(file, 'utf-8');
+		return acc.filter((f) => !content.includes(basename(f)));
+	}, potentialUnusedFiles);
 
 	console.log('\n');
 
@@ -45,6 +31,7 @@ const main = async () => {
 		console.log(`${GREEN}No unused components found.${NC}`);
 		process.exit(1);
 	} else {
+		console.log(`${RED}Found ${unusedFiles.length} unused component(s).${NC}`);
 		unusedFiles.forEach((file) => console.log(`${RED}Unused Svelte file: ${file}${NC}`));
 	}
 };
