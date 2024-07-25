50cfa49766ebff55bfa4dd853153eaecb78f923d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-07-25 12:45:10 +0200

    refactor: adapt modal store

--- src/frontend/src/eth/components/send/Send.svelte
+++ src/frontend/src/eth/components/send/Send.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { modalStore } from '$lib/stores/modal.store';
 	import { addressNotLoaded } from '$lib/derived/address.derived';
-	import { modalSend } from '$lib/derived/modal.derived';
+	import { modalEthSend } from '$lib/derived/modal.derived';
 	import EthSendModal from '$eth/components/send/EthSendModal.svelte';
 	import { waitWalletReady } from '$lib/services/actions.services';
 	import SendButton from '$lib/components/send/SendButton.svelte';
@@ -24,13 +24,13 @@
 			}
 		}
 
-		const callback = async () => modalStore.openSend(modalId);
+		const callback = async () => modalStore.openEthSend(modalId);
 		await loadTokenAndRun({ token, callback });
 	};
 </script>
 
 <SendButton on:click={async () => await openSend()} {compact} />
 
-{#if $modalSend && $modalStore?.data === modalId}
+{#if $modalEthSend && $modalStore?.data === modalId}
 	<EthSendModal on:nnsClose />
 {/if}
