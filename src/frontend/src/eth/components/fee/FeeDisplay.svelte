6364ee1ef6cc9d475879e70b6aadfdb65fb74650
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-07-26 12:29:19 +0200

    feat(frontend): validation error when ETH not enough for fees

--- src/frontend/src/eth/components/fee/FeeDisplay.svelte
+++ src/frontend/src/eth/components/fee/FeeDisplay.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { BigNumber } from '@ethersproject/bignumber';
+	import { BigNumber } from '@ethersproject/bignumber';
 	import { isNullish, nonNullish } from '@dfinity/utils';
 	import { fade } from 'svelte/transition';
 	import { getContext, onDestroy } from 'svelte';
@@ -7,8 +7,12 @@
 	import type { FeeContext } from '$eth/stores/fee.store';
 	import { FEE_CONTEXT_KEY } from '$eth/stores/fee.store';
 	import { EIGHT_DECIMALS } from '$lib/constants/app.constants';
+	import { balancesStore } from '$lib/stores/balances.store';
+	import { i18n } from '$lib/stores/i18n.store';
+	import { slide } from 'svelte/transition';
 
-	const { maxGasFee, feeSymbolStore }: FeeContext = getContext<FeeContext>(FEE_CONTEXT_KEY);
+	const { maxGasFee, feeSymbolStore, feeIdStore }: FeeContext =
+		getContext<FeeContext>(FEE_CONTEXT_KEY);
 
 	let fee: BigNumber | undefined | null = undefined;
 
@@ -41,12 +45,19 @@
 
 		clearTimeout(timer);
 	});
+
+	const balance = nonNullish($feeIdStore)
+		? $balancesStore?.[$feeIdStore]?.data ?? BigNumber.from(0n)
+		: BigNumber.from(0n);
+
+	let insufficientFeeFunds = false;
+	$: insufficientFeeFunds = nonNullish(fee) && balance.lt(fee);
 </script>
 
 <label for="balance" class="font-bold px-4.5"
 	>Max fee <small>(likely in &lt; 30 seconds)</small>:</label
 >
-<div id="balance" class="font-normal px-4.5 mb-4 break-all" style="min-height: 24px">
+<div id="balance" class="font-normal px-4.5 mb-4 break-all min-h-10">
 	{#if nonNullish(fee)}
 		<div transition:fade>
 			{formatToken({
@@ -54,6 +65,11 @@
 				displayDecimals: EIGHT_DECIMALS
 			})}
 			{$feeSymbolStore ?? ''}
+			{#if insufficientFeeFunds}
+				<p transition:slide={{ duration: 250 }} class="text-cyclamen text-xs">
+					{$i18n.send.assertion.insufficient_ethereum_funds_to_cover_the_fees}
+				</p>
+			{/if}
 		</div>
 	{/if}
 </div>
