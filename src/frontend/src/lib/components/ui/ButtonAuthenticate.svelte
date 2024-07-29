469eec5ade3fffc2ad02b7d304f8c23101c5c53a
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-07-29 18:27:42 +0200

    fix: background color was static

--- src/frontend/src/lib/components/ui/ButtonAuthenticate.svelte
+++ src/frontend/src/lib/components/ui/ButtonAuthenticate.svelte
@@ -4,7 +4,7 @@
 
 <button
 	on:click
-	class="h-16 px-8 py-2 rounded-full gap-4 font-bold text-lg bg-brandeis-blue"
+	class="h-16 px-8 py-2 rounded-full gap-4 font-bold text-lg"
 	data-tid="login-button"
 >
 	<slot />
@@ -14,18 +14,19 @@
 <style lang="scss">
 	button {
 		padding: 1.5em 2em;
-		background-size: 600% 100%;
+		background: var(--color-brandeis-blue);
 		background: linear-gradient(
-				90deg,
-				#0975ff 0%,
-				#0975ff 24.86%,
-				#1bb5ff 30.32%,
-				#03e2ff 50.17%,
-				#1bb5ff 70.25%,
-				#0874ff 75.12%,
-				#0874ff 100%
-			)
-			0 0;
+			90deg,
+			#0975ff 0%,
+			#0975ff 24.86%,
+			#1bb5ff 30.32%,
+			#03e2ff 50.17%,
+			#1bb5ff 70.25%,
+			#0874ff 75.12%,
+			#0874ff 100%
+		);
+		background-size: 600% 100%;
+		background-position: 0 0;
 		transition:
 			background-position 0.7s ease-in-out,
 			box-shadow 0.5s ease-in-out;
