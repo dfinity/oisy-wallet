247fa956f88447261a0d6f8c2213d00b0955540f
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-07-29 18:44:22 +0200

    feat(frontend): pin tokens with balance at top (#1779)
    # Motivation
    
    As final requirement for sorting, we pin the tokens with balance on top,
    per descending USD amount and after per token amount (if USD value is
    not provided).
    
    # Changes
    
    - Create sorting function to pin tokens with balance on top.
    - Apply sorting to the `sortedTokens` derived.
    
    # Tests
    
    Created test for function `pinTokensWithBalanceAtTop`.

--- src/frontend/src/lib/derived/tokens.derived.ts
+++ src/frontend/src/lib/derived/tokens.derived.ts
@@ -4,8 +4,9 @@ import { erc20Tokens } from '$eth/derived/erc20.derived';
 import { enabledEthereumTokens } from '$eth/derived/tokens.derived';
 import { icrcChainFusionDefaultTokens, sortedIcrcTokens } from '$icp/derived/icrc.derived';
 import { exchanges } from '$lib/derived/exchange.derived';
+import { balancesStore } from '$lib/stores/balances.store';
 import type { Token, TokenToPin } from '$lib/types/token';
-import { pinTokensAtTop, sortTokens } from '$lib/utils/tokens.utils';
+import { pinTokensAtTop, pinTokensWithBalanceAtTop, sortTokens } from '$lib/utils/tokens.utils';
 import { derived, type Readable } from 'svelte/store';
 import { enabledBitcoinTokens } from '../../btc/derived/tokens.derived';
 
@@ -31,7 +32,10 @@ export const tokensToPin: Readable<TokenToPin[]> = derived(
 );
 
 export const sortedTokens: Readable<Token[]> = derived(
-	[tokens, tokensToPin, exchanges],
-	([$tokens, $tokensToPin, $exchanges]) =>
-		pinTokensAtTop({ $tokens: sortTokens({ $tokens, $exchanges }), $tokensToPin })
+	[tokens, tokensToPin, exchanges, balancesStore],
+	([$tokens, $tokensToPin, $exchanges, $balancesStore]) =>
+		pinTokensWithBalanceAtTop({
+			$tokens: pinTokensAtTop({ $tokens: sortTokens({ $tokens, $exchanges }), $tokensToPin }),
+			$balancesStore: $balancesStore
+		})
 );
