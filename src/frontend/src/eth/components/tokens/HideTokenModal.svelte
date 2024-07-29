270d3c5cee9d9d379218b78f3ad080ff06fc6893
David Dal Busco <david.dalbusco@dfinity.org>
2024-07-29 16:23:59 +0200

    feat(frontend): auto load erc20 user token if not enabled (#1855)
    # Motivation
    
    When a user converts an ERC20 token to a ckERC20 twin token, the UI
    needs information about the counterpart token (ERC20). For example, the
    user might have a CK token (e.g., ckUSDC) enabled, but its token
    counterpart (e.g., USDC) disabled.
    
    As a result, the UI might lack necessary information. Notably, the
    balance displayed on the "How to convert" modal wizard might remain zero
    even if the user has effectively transferred tokens to Oisy (this is a
    bug that was reported).
    
    Consequently, the user waits indefinitely and cannot proceed with the
    conversion unless they understand what's happening and finally figure
    out they should manually add the ERC20 token.
    
    That's why this PR aims to automatically enable ERC20 tokens if
    required.
    
    # Source
    
    Reported per PM and shared on
    [Slack](https://dfinity.slack.com/archives/C05M7P123HR/p1721277306898159).
    
    # Changes
    
    - New service function `autoLoadUserToken`.
    - On `openCkETHReceive` proceed by calling the function first and if
    required enable automatically the token.
    - Refactor `setUserToken` which is now called manually and
    automatically.
    
    # Screenshot
    
    "Wait for ..." remains zero for ever:
    
    
    ![image](https://github.com/user-attachments/assets/83770bae-c40a-450a-8752-64869ce822b4)
    
    ---------
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/eth/components/tokens/HideTokenModal.svelte
+++ src/frontend/src/eth/components/tokens/HideTokenModal.svelte
@@ -2,15 +2,14 @@
 	import { i18n } from '$lib/stores/i18n.store';
 	import { isNullishOrEmpty } from '$lib/utils/input.utils';
 	import { toastsError } from '$lib/stores/toasts.store';
-	import { setUserToken } from '$lib/api/backend.api';
 	import HideTokenModal from '$lib/components/tokens/HideTokenModal.svelte';
 	import type { Identity } from '@dfinity/agent';
 	import { onMount } from 'svelte';
-	import { assertNonNullish, toNullable } from '@dfinity/utils';
+	import { assertNonNullish } from '@dfinity/utils';
 	import { token } from '$lib/stores/token.store';
 	import type { OptionErc20UserToken } from '$eth/types/erc20-user-token';
-	import type { EthereumNetwork } from '$eth/types/network';
 	import { loadUserTokens } from '$eth/services/erc20.services';
+	import { setUserToken } from '$icp-eth/services/user-token.services';
 
 	let selectedToken: OptionErc20UserToken;
 
@@ -33,20 +32,7 @@
 	const hideToken = async (params: { identity: Identity }) => {
 		assertNonNullish(selectedToken);
 
-		const { version, symbol, network, address, decimals } = selectedToken;
-		const { chainId } = network as EthereumNetwork;
-
-		await setUserToken({
-			...params,
-			token: {
-				chain_id: chainId,
-				decimals: toNullable(decimals),
-				contract_address: address,
-				symbol: toNullable(symbol),
-				version: toNullable(version),
-				enabled: toNullable(false)
-			}
-		});
+		await setUserToken({ ...params, token: selectedToken, enabled: false });
 	};
 
 	const updateUi = (params: { identity: Identity }): Promise<void> => loadUserTokens(params);
--- src/frontend/src/eth/derived/erc20.derived.ts
+++ src/frontend/src/eth/derived/erc20.derived.ts
@@ -34,7 +34,7 @@ const erc20DefaultTokensAddresses: Readable<string[]> = derived(
  * The list of ERC20 tokens the user has added, enabled or disabled. Can contains default tokens for example if user has disabled a default tokens.
  * i.e. default tokens are configured on the client side. If user disable or enable a default tokens, this token is added as a "user token" in the backend.
  */
-const erc20UserTokens: Readable<Erc20UserToken[]> = derived(
+export const erc20UserTokens: Readable<Erc20UserToken[]> = derived(
 	[erc20UserTokensStore],
 	([$erc20UserTokensStore]) => $erc20UserTokensStore?.map(({ data: token }) => token) ?? []
 );
