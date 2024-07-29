89cc9a8f16ab1cdba841b38629887519ab9e34e0
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-07-29 14:11:08 +0200

    chore: remove fade transition

--- src/frontend/src/lib/components/send/SendTokensList.svelte
+++ src/frontend/src/lib/components/send/SendTokensList.svelte
@@ -3,7 +3,6 @@
 	import TokensSkeletons from '$lib/components/tokens/TokensSkeletons.svelte';
 	import { enabledNetworkTokensUiNonZeroBalance } from '$lib/derived/network-tokens.derived';
 	import type { TokenUi } from '$lib/types/token';
-	import { fade } from 'svelte/transition';
 	import { formatToken } from '$lib/utils/format.utils';
 	import CardAmount from '$lib/components/ui/CardAmount.svelte';
 	import ExchangeTokenValue from '$lib/components/exchange/ExchangeTokenValue.svelte';
@@ -19,20 +18,18 @@
 
 <TokensSkeletons>
 	{#each tokens as token (token.id)}
-		<div in:fade>
-			<TokenCardWithOnClick {token} on:click={() => dispatch('icSendToken', token)}>
-				<output class="break-all" slot="description">
-					{formatToken({
-						value: $balancesStore?.[token.id]?.data ?? BigNumber.from(0n),
-						unitName: token.decimals
-					})}
-					{token.symbol}
-				</output>
+		<TokenCardWithOnClick {token} on:click={() => dispatch('icSendToken', token)}>
+			<output class="break-all" slot="description">
+				{formatToken({
+					value: $balancesStore?.[token.id]?.data ?? BigNumber.from(0n),
+					unitName: token.decimals
+				})}
+				{token.symbol}
+			</output>
 
-				<CardAmount slot="exchange">
-					<ExchangeTokenValue {token} />
-				</CardAmount>
-			</TokenCardWithOnClick>
-		</div>
+			<CardAmount slot="exchange">
+				<ExchangeTokenValue {token} />
+			</CardAmount>
+		</TokenCardWithOnClick>
 	{/each}
 </TokensSkeletons>
