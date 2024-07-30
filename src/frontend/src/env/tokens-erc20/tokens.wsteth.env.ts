ee63ddd2d4c5768a3ab89f0fba268b2420db13aa
David Dal Busco <david.dalbusco@dfinity.org>
2024-07-30 13:02:55 +0200

    feat(frontend): support for ckWSTETH (#1926)
    # Motivation
    
    We want to support the new token `ckWSTETH` that has been executed on
    mainnet.
    
    # Changes
    
    - Use `add.tokens.erc20.sh` to generate token.
    - Setup token in `network.icrc` to define the ckERC20 token.
    - Remove existing duplicate contract address.
    
    ---------
    
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

---
+++ src/frontend/src/env/tokens-erc20/tokens.wsteth.env.ts
@@ -0,0 +1,23 @@
+import { ETHEREUM_NETWORK } from '$env/networks.env';
+import type { RequiredErc20Token } from '$eth/types/erc20';
+import wsteth from '$icp-eth/assets/wsteth.svg';
+
+export const WSTETH_DECIMALS = 18;
+
+export const WSTETH_SYMBOL = 'wstETH';
+
+export const WSTETH_TOKEN_ID: unique symbol = Symbol(WSTETH_SYMBOL);
+
+export const WSTETH_TOKEN: RequiredErc20Token = {
+	id: WSTETH_TOKEN_ID,
+	network: ETHEREUM_NETWORK,
+	standard: 'erc20',
+	category: 'default',
+	name: 'Wrapped liquid staked Ether 2.0',
+	symbol: 'wstETH',
+	decimals: WSTETH_DECIMALS,
+	icon: wsteth,
+	address: '0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0',
+	exchange: 'erc20',
+	twinTokenSymbol: 'ckWSTETH'
+};
