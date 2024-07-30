1d24a1e9bae5a14da36d428a23892f8735870bce
David Dal Busco <david.dalbusco@outlook.com>
2024-07-30 17:32:55 +0200

    fix(frontend): rollback pin tokens with balance at top PR #1779
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/frontend/src/lib/derived/tokens.derived.ts
+++ src/frontend/src/lib/derived/tokens.derived.ts
@@ -4,9 +4,8 @@ import { erc20Tokens } from '$eth/derived/erc20.derived';
 import { enabledEthereumTokens } from '$eth/derived/tokens.derived';
 import { icrcChainFusionDefaultTokens, sortedIcrcTokens } from '$icp/derived/icrc.derived';
 import { exchanges } from '$lib/derived/exchange.derived';
-import { balancesStore } from '$lib/stores/balances.store';
 import type { Token, TokenToPin } from '$lib/types/token';
-import { pinTokensAtTop, pinTokensWithBalanceAtTop, sortTokens } from '$lib/utils/tokens.utils';
+import { pinTokensAtTop, sortTokens } from '$lib/utils/tokens.utils';
 import { derived, type Readable } from 'svelte/store';
 import { enabledBitcoinTokens } from '../../btc/derived/tokens.derived';
 
@@ -32,10 +31,7 @@ export const tokensToPin: Readable<TokenToPin[]> = derived(
 );
 
 export const sortedTokens: Readable<Token[]> = derived(
-	[tokens, tokensToPin, exchanges, balancesStore],
-	([$tokens, $tokensToPin, $exchanges, $balancesStore]) =>
-		pinTokensWithBalanceAtTop({
-			$tokens: pinTokensAtTop({ $tokens: sortTokens({ $tokens, $exchanges }), $tokensToPin }),
-			$balancesStore: $balancesStore
-		})
+	[tokens, tokensToPin, exchanges],
+	([$tokens, $tokensToPin, $exchanges]) =>
+		pinTokensAtTop({ $tokens: sortTokens({ $tokens, $exchanges }), $tokensToPin })
 );
