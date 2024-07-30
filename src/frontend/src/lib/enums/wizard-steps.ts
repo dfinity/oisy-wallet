73f9d9820984518919282045b186ca9a574a4c25
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-07-30 11:52:56 +0200

    refactor: remove SendMOdal that was added in another PR

--- src/frontend/src/lib/enums/wizard-steps.ts
+++ src/frontend/src/lib/enums/wizard-steps.ts
@@ -2,6 +2,5 @@ export enum WizardStepsSend {
 	SEND = 'Send',
 	REVIEW = 'Review',
 	SENDING = 'Sending',
-	QR_CODE_SCAN = 'QR Code Scan',
-	TOKENS_LIST = 'Tokens List'
+	QR_CODE_SCAN = 'QR Code Scan'
 }
