d49e5e2b00bf086e20cd9326f0ae5652b15d94c3
Max Murphy <max@winning.black>
2024-07-31 18:33:25 +0200

    Get remote .did files

--- scripts/did.sh
+++ scripts/did.sh
@@ -1,20 +1,34 @@
 #!/usr/bin/env bash
 
+# Gets all .did files listed in dfx.json as http URLs.
+#
+# .did files are placed in the local .dfx directory, where the .did files are expected by `dfx generate` and other commands.
+function get_remote_did_files() {
+  jq -r '.canisters | to_entries | .[] | select("\(.value.candid)" | startswith("http")) | "\(.key) \(.value.candid)"' dfx.json |
+    while read -r line; do
+      IFS=', ' read -r -a array <<<"$line"
+      mkdir -p ".dfx/local/canisters/${array[0]}"
+      curl -sSL "${array[1]}" >".dfx/local/canisters/${array[0]}/${array[0]}.did"
+    done
+}
+
+# Extracts .did files from Wasm metadata.
 function generate_did() {
   local canister=$1
   canister_root="src/$canister"
 
   cargo build --manifest-path="$canister_root/Cargo.toml" \
-      --target wasm32-unknown-unknown \
-      --release --package "$canister" \
+    --target wasm32-unknown-unknown \
+    --release --package "$canister"
 
   # cargo install candid-extractor
-  candid-extractor "target/wasm32-unknown-unknown/release/$canister.wasm" > "$canister_root/$canister.did"
+  candid-extractor "target/wasm32-unknown-unknown/release/$canister.wasm" >"$canister_root/$canister.did"
 }
 
 CANISTERS=backend
 
-for canister in $(echo $CANISTERS | sed "s/,/ /g")
-do
-    generate_did "$canister"
+for canister in $(echo $CANISTERS | sed "s/,/ /g"); do
+  generate_did "$canister"
 done
+
+get_remote_did_files
