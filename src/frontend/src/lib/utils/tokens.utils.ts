26a3260bf9ce3009904ddb4ac4edf95f8462ab4e
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-07-31 14:15:22 +0200

    refactor: improve pinTokensWithBalanceAtTop to do just one loop

--- src/frontend/src/lib/utils/tokens.utils.ts
+++ src/frontend/src/lib/utils/tokens.utils.ts
@@ -1,7 +1,7 @@
 import type { BalancesData } from '$lib/stores/balances.store';
 import type { CertifiedStoreData } from '$lib/stores/certified.store';
 import type { ExchangesData } from '$lib/types/exchange';
-import type { Token, TokenToPin, TokenUi } from '$lib/types/token';
+import type { Token, TokenToPin, TokenUi, TokenWithBalance } from '$lib/types/token';
 import { formatToken } from '$lib/utils/format.utils';
 import { nonNullish } from '@dfinity/utils';
 import { BigNumber } from '@ethersproject/bignumber';
@@ -73,37 +73,35 @@ export const pinTokensWithBalanceAtTop = ({
 	$tokens: TokenUi[];
 	$balancesStore: CertifiedStoreData<BalancesData>;
 }): TokenUi[] => {
-	const tokensWithBalanceToPin: TokenToPin[] = $tokens
-		.reduce(
-			(acc, token) => {
-				const balance = Number(
-					formatToken({
-						value: $balancesStore?.[token.id]?.data ?? BigNumber.from(0),
-						unitName: token.decimals,
-						displayDecimals: token.decimals
-					})
-				);
-
-				const usdBalance = token.usdBalance ?? 0;
-
-				if (usdBalance > 0 || balance > 0) {
-					return [...acc, { ...token, balance }];
-				}
-
-				return acc;
-			},
-			[] as (TokenUi & { balance: number })[]
+	const tokensWithBalance: TokenWithBalance[] = $tokens.map((token) => ({
+		...token,
+		balance: Number(
+			formatToken({
+				value: $balancesStore?.[token.id]?.data ?? BigNumber.from(0),
+				unitName: token.decimals,
+				displayDecimals: token.decimals
+			})
 		)
-		.sort(
+	}));
+
+	const [positiveBalances, nonPositiveBalances] = tokensWithBalance.reduce<
+		[TokenWithBalance[], TokenWithBalance[]]
+	>(
+		(acc, token) => (
+			(token.usdBalance ?? 0) > 0 || token.balance > 0 ? acc[0].push(token) : acc[1].push(token),
+			acc
+		),
+		[[], []]
+	);
+
+	return [
+		...positiveBalances.sort(
 			(a, b) =>
 				(b.usdBalance ?? 0) - (a.usdBalance ?? 0) ||
 				b.balance - a.balance ||
 				a.name.localeCompare(b.name) ||
 				a.network.name.localeCompare(b.network.name)
-		);
-
-	return pinTokensAtTop({
-		$tokens,
-		$tokensToPin: tokensWithBalanceToPin
-	});
+		),
+		...nonPositiveBalances
+	].map(({ balance: _, ...rest }) => rest);
 };
