07b314d8d398c8e3a69b030759d3f71ee5725dfe
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-07-31 10:02:39 +0200

    refactor: change zod to safeParse

--- src/frontend/src/lib/constants/oisy.constants.ts
+++ src/frontend/src/lib/constants/oisy.constants.ts
@@ -1,20 +1,18 @@
 import metadata from '$env/oisy.metadata.json';
-import { z } from 'zod';
+import { oisyMetadata } from '$env/types/env-oisy-metadata';
 
-const MetadataSchema = z.object({
-	OISY_NAME: z.string(),
-	OISY_ONELINER: z.string(),
-	OISY_DESCRIPTION: z.string(),
-	OISY_REPO_URL: z.string().url(),
-	OISY_ALPHA_WARNING_URL: z.string().url()
-});
+const parsedMetadata = oisyMetadata.safeParse(metadata);
 
-const parsedMetadata = MetadataSchema.parse(metadata);
+export const { OISY_NAME, OISY_ONELINER, OISY_DESCRIPTION, OISY_REPO_URL, OISY_ALPHA_WARNING_URL } =
+	parsedMetadata.success
+		? parsedMetadata.data
+		: {
+				OISY_NAME: '',
+				OISY_ONELINER: '',
+				OISY_DESCRIPTION: '',
+				OISY_REPO_URL: '',
+				OISY_ALPHA_WARNING_URL: ''
+			};
 
-export const OISY_NAME = parsedMetadata.OISY_NAME;
-export const OISY_ONELINER = parsedMetadata.OISY_ONELINER;
-export const OISY_DESCRIPTION = parsedMetadata.OISY_DESCRIPTION;
 export const OISY_URL = import.meta.env.VITE_OISY_URL;
 export const OISY_ICON = `${OISY_URL}/favicons/icon-512x512.png`;
-export const OISY_REPO_URL = parsedMetadata.OISY_REPO_URL;
-export const OISY_ALPHA_WARNING_URL = parsedMetadata.OISY_ALPHA_WARNING_URL;
