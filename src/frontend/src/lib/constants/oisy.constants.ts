7379f6de07b7c56b583a0300d50e0d936a6b0c31
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-07-31 09:01:10 +0200

    refactor(frontend): move metadata from .env file to JSON file

--- src/frontend/src/lib/constants/oisy.constants.ts
+++ src/frontend/src/lib/constants/oisy.constants.ts
@@ -1,7 +1,9 @@
-export const OISY_NAME = import.meta.env.VITE_OISY_NAME;
-export const OISY_ONELINER = import.meta.env.VITE_OISY_ONELINER;
-export const OISY_DESCRIPTION = import.meta.env.VITE_OISY_DESCRIPTION;
+import metadata from '../../../static/metadata.json';
+
+export const OISY_NAME = metadata.OISY_NAME;
+export const OISY_ONELINER = metadata.OISY_ONELINER;
+export const OISY_DESCRIPTION = metadata.OISY_DESCRIPTION;
 export const OISY_URL = import.meta.env.VITE_OISY_URL;
-export const OISY_ICON = import.meta.env.VITE_OISY_ICON;
-export const OISY_REPO_URL = import.meta.env.VITE_OISY_REPO_URL;
-export const OISY_ALPHA_WARNING_URL = import.meta.env.VITE_OISY_ALPHA_WARNING_URL;
+export const OISY_ICON = OISY_URL + '/favicons/icon-512x512.png';
+export const OISY_REPO_URL = metadata.OISY_REPO_URL;
+export const OISY_ALPHA_WARNING_URL = metadata.OISY_ALPHA_WARNING_URL;
