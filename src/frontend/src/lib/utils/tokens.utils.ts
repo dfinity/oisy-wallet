5c728f5e79a2ac26ba4e2e1ae12bc3572521c493
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-07-31 16:07:37 +0200

    feat(frontend): pin tokens with balance at top (#1941)
    Revisiting PR #1779 (that was rolled back with PR #1938)
    
    # Motivation
    
    As final requirement for sorting, we pin the tokens with balance on top,
    per descending USD amount and after per token amount (if USD value is
    not provided).
    
    # Changes
    
    - Create sorting function to pin tokens with balance on top.
    - Create global derived store `sortedNetworkTokensUi` that pins tokens
    with balance on top (and sorts them).
    - Adapt usage in `TokensSignedIn`.
    
    # Tests
    
    Created test for function `pinTokensWithBalanceAtTop`.

--- src/frontend/src/lib/utils/tokens.utils.ts
+++ src/frontend/src/lib/utils/tokens.utils.ts
@@ -1,6 +1,10 @@
+import type { BalancesData } from '$lib/stores/balances.store';
+import type { CertifiedStoreData } from '$lib/stores/certified.store';
 import type { ExchangesData } from '$lib/types/exchange';
-import type { Token, TokenToPin } from '$lib/types/token';
+import type { Token, TokenToPin, TokenUi, TokenWithBalance } from '$lib/types/token';
+import { formatToken } from '$lib/utils/format.utils';
 import { nonNullish } from '@dfinity/utils';
+import { BigNumber } from '@ethersproject/bignumber';
 
 export const pinTokensAtTop = ({
 	$tokens,
@@ -27,6 +31,12 @@ export const pinTokensAtTop = ({
 	return [...pinnedTokens, ...otherTokens];
 };
 
+/**
+ * Sorts tokens by market cap, name and network name.
+ *
+ * @param $tokens - The list of tokens to sort.
+ * @param $exchanges - The exchange rates for the tokens.
+ */
 export const sortTokens = ({
 	$tokens,
 	$exchanges
@@ -40,3 +50,58 @@ export const sortTokens = ({
 			a.name.localeCompare(b.name) ||
 			a.network.name.localeCompare(b.network.name)
 	);
+
+/**
+ * Pins tokens by USD value, balance and name.
+ *
+ * The function pins on top of the list the tokens that have a balance and/or an exchange rate.
+ * It sorts first the ones that have an exchange rate and balance non-zero, according to their usd value (descending).
+ * Then, it sorts the ones that have only a balance non-zero and no exchange rate, according to their balance (descending).
+ * Finally, it leaves the rest of the tokens untouched.
+ * In case of a tie, it sorts by token name and network name.
+ *
+ * @param $tokens - The list of tokens to sort.
+ * @param $exchanges - The exchange rates for the tokens.
+ * @param $balancesStore - The balances for the tokens.
+ * @returns The sorted list of tokens.
+ *
+ */
+export const pinTokensWithBalanceAtTop = ({
+	$tokens,
+	$balancesStore
+}: {
+	$tokens: TokenUi[];
+	$balancesStore: CertifiedStoreData<BalancesData>;
+}): TokenUi[] => {
+	const tokensWithBalance: TokenWithBalance[] = $tokens.map((token) => ({
+		...token,
+		balance: Number(
+			formatToken({
+				value: $balancesStore?.[token.id]?.data ?? BigNumber.from(0),
+				unitName: token.decimals,
+				displayDecimals: token.decimals
+			})
+		)
+	}));
+
+	const [positiveBalances, nonPositiveBalances] = tokensWithBalance.reduce<
+		[TokenWithBalance[], TokenWithBalance[]]
+	>(
+		(acc, token) => (
+			(token.usdBalance ?? 0) > 0 || token.balance > 0 ? acc[0].push(token) : acc[1].push(token),
+			acc
+		),
+		[[], []]
+	);
+
+	return [
+		...positiveBalances.sort(
+			(a, b) =>
+				(b.usdBalance ?? 0) - (a.usdBalance ?? 0) ||
+				b.balance - a.balance ||
+				a.name.localeCompare(b.name) ||
+				a.network.name.localeCompare(b.network.name)
+		),
+		...nonPositiveBalances
+	].map(({ balance: _, ...rest }) => rest);
+};
