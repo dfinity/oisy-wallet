7f288dccc0b012dc35d536829946e61d4459393c
Max Murphy <max@winning.black>
2024-08-01 03:35:35 +0200

    Get all candid files needed to generate bindings

--- scripts/did.sh
+++ scripts/did.sh
@@ -1,24 +1,5 @@
 #!/usr/bin/env bash
 
-# Gets all .did files listed in dfx.json as http URLs.
-#
-# .did files are placed in the local .dfx directory, where the .did files are expected by `dfx generate` and other commands.
-function get_remote_did_files() {
-	jq -r '.canisters | to_entries | .[] | select(.value.candid != null) | "\(.key) \(.value.candid)"' dfx.json |
-    while read -r line; do
-      IFS=', ' read -r -a array <<<"$line"
-      canister_name="${array[0]}"
-      source="${array[1]}"
-      destination=".dfx/local/canisters/${array[0]}/${array[0]}.did"
-      mkdir -p "$(dirname "$destination")"
-      case "$source" in
-	      http*) curl -sSL "$source" >"$destination" ;;
-	      *) if test -e "$source" ; then cp "$source" "$destination" ; else echo "WARNING: $canister_name did file not found at $source" ; fi ;;
-      esac
-    done
-}
-
-# Extracts .did files from Wasm metadata.
 function generate_did() {
   local canister=$1
   canister_root="src/$canister"
@@ -36,5 +17,3 @@ CANISTERS=backend
 for canister in $(echo $CANISTERS | sed "s/,/ /g"); do
   generate_did "$canister"
 done
-
-get_remote_did_files
---
+++ scripts/generate.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+[[ "${1:-}" != "--help" ]] || {
+	cat <<-EOF
+	Gets .did files and generates canister bindings from them.
+	EOF
+	exit 0
+}
+
+# Gets all .did files listed in dfx.json.
+#
+# .did files are placed in the local .dfx directory, where the .did files are expected by `dfx generate` and other commands.
+function install_did_files() {
+	jq -r '.canisters | to_entries | .[] | select(.value.candid != null) | "\(.key) \(.value.candid)"' dfx.json |
+    while read -r line; do
+      IFS=', ' read -r -a array <<<"$line"
+      canister_name="${array[0]}"
+      source="${array[1]}"
+      destination=".dfx/local/canisters/${array[0]}/${array[0]}.did"
+      mkdir -p "$(dirname "$destination")"
+      case "$source" in
+	      http*) curl -sSL "$source" >"$destination" ;;
+	      *) if test -e "$source" ; then cp "$source" "$destination" ; else echo "WARNING: $canister_name did file not found at $source" ; fi ;;
+      esac
+    done
+}
+
+# Create local .did files
+./scripts/did.sh
+# Download .did files without an explicit url in dfx.json
+./scripts/download.icp.sh
+./scripts/download.ckbtc.sh
+./scripts/download.cketh.sh
+# Generate bindings
+install_did_files
+dfx generate
+node scripts/did.update.types.mjs
+node scripts/did.delete.types.mjs
+npm run format
