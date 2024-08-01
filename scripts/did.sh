d11057858f3a2fe4a4347d0250186a919236267d
Max Murphy <max@winning.black>
2024-08-01 04:24:01 +0200

    faster

--- scripts/did.sh
+++ scripts/did.sh
@@ -4,6 +4,7 @@ function generate_did() {
   local canister=$1
   canister_root="src/$canister"
 
+  test -e "target/wasm32-unknown-unknown/release/$canister.wasm" || \
   cargo build --manifest-path="$canister_root/Cargo.toml" \
     --target wasm32-unknown-unknown \
     --release --package "$canister"
