697d8206363de659ccdd811e5c70c2404f3b605e
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-08-05 17:04:25 +0200

    lint: fix format

--- src/frontend/src/lib/components/tokens/TokenCard.svelte
+++ src/frontend/src/lib/components/tokens/TokenCard.svelte
@@ -2,7 +2,6 @@
 	import Card from '$lib/components/ui/Card.svelte';
 	import TokenLogo from '$lib/components/tokens/TokenLogo.svelte';
 	import type { Token } from '$lib/types/token';
-	import { transactionsUrl } from '$lib/utils/nav.utils';
 	import Tag from '$lib/components/ui/Tag.svelte';
 	import { isTokenCkLedger } from '$icp/utils/ic-send.utils';
 
@@ -11,11 +10,11 @@
 
 <Card noMargin>
 	{#if isTokenCkLedger(token)}
-				<Tag>ck</Tag>
-				{token.twinToken?.name ?? token.name.replace(/^ck/, '')}
-			{:else}
-				{token.name}
-			{/if}
+		<Tag>ck</Tag>
+		{token.twinToken?.name ?? token.name.replace(/^ck/, '')}
+	{:else}
+		{token.name}
+	{/if}
 
 	<TokenLogo {token} slot="icon" color="white" />
 
