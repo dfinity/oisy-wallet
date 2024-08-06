e3ff8e247a12084a60a6c420a5a2c79090c8dddb
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-08-06 10:01:40 +0200

    refactor: make ariaHidden variable direct

--- src/frontend/src/lib/components/ui/Tag.svelte
+++ src/frontend/src/lib/components/ui/Tag.svelte
@@ -2,9 +2,6 @@
 	export let ariaHidden = false;
 </script>
 
-<span
-	aria-hidden={ariaHidden ? 'true' : 'false'}
-	class="rounded-sm border border-grey px-0.5 text-sm"
->
+<span aria-hidden={ariaHidden} class="rounded-sm border border-grey px-0.5 text-sm">
 	<slot />
 </span>
