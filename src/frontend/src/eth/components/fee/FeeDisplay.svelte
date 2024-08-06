f6f33bfa6b190a43b2b9595fab80fb0d306cba08
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-08-06 10:12:35 +0200

    refactor(frontend): extract component that shows the fee amount (#1930)
    # Motivation
    
    We extract the common component that shows the fee amount for ETH/ERC20,
    with optional validation error.
    The transition animation is delayed between the fee and the possible
    validation error.
    
    
    
    
    https://github.com/user-attachments/assets/badaaa0c-8492-436f-84ef-1ed07b510412

--- src/frontend/src/eth/components/fee/FeeDisplay.svelte
+++ src/frontend/src/eth/components/fee/FeeDisplay.svelte
@@ -1,16 +1,10 @@
 <script lang="ts">
 	import { BigNumber } from '@ethersproject/bignumber';
 	import { isNullish, nonNullish } from '@dfinity/utils';
-	import { fade } from 'svelte/transition';
 	import { getContext, onDestroy } from 'svelte';
-	import { formatToken } from '$lib/utils/format.utils';
 	import type { FeeContext } from '$eth/stores/fee.store';
 	import { FEE_CONTEXT_KEY } from '$eth/stores/fee.store';
-	import { EIGHT_DECIMALS } from '$lib/constants/app.constants';
-	import { balancesStore } from '$lib/stores/balances.store';
-	import { i18n } from '$lib/stores/i18n.store';
-	import { slide } from 'svelte/transition';
-	import { replacePlaceholders } from '$lib/utils/i18n.utils';
+	import FeeAmountDisplay from '$icp-eth/components/fee/FeeAmountDisplay.svelte';
 
 	const { maxGasFee, feeSymbolStore, feeTokenIdStore }: FeeContext =
 		getContext<FeeContext>(FEE_CONTEXT_KEY);
@@ -46,38 +40,13 @@
 
 		clearTimeout(timer);
 	});
-
-	const balance =
-		nonNullish($feeTokenIdStore) && nonNullish($balancesStore)
-			? $balancesStore[$feeTokenIdStore]?.data ?? BigNumber.from(0n)
-			: undefined;
-
-	let insufficientFeeFunds = false;
-	$: insufficientFeeFunds = nonNullish(fee) && nonNullish(balance) && balance.lt(fee);
 </script>
 
 <label for="balance" class="font-bold px-4.5"
 	>Max fee <small>(likely in &lt; 30 seconds)</small>:</label
 >
-<div id="balance" class="font-normal px-4.5 mb-4 break-all min-h-10">
-	{#if nonNullish(fee)}
-		<div transition:fade>
-			{formatToken({
-				value: fee,
-				displayDecimals: EIGHT_DECIMALS
-			})}
-			{$feeSymbolStore ?? ''}
-			{#if insufficientFeeFunds && nonNullish(balance)}
-				<p transition:slide={{ duration: 250 }} class="text-cyclamen text-xs">
-					{replacePlaceholders($i18n.send.assertion.not_enough_tokens_for_gas, {
-						$balance: formatToken({
-							value: balance,
-							displayDecimals: EIGHT_DECIMALS
-						}),
-						$symbol: $feeSymbolStore ?? ''
-					})}
-				</p>
-			{/if}
-		</div>
+<div id="balance" class="font-normal px-4.5 mb-4 break-all min-h-6">
+	{#if nonNullish(fee) && nonNullish($feeSymbolStore) && nonNullish($feeTokenIdStore)}
+		<FeeAmountDisplay {fee} feeSymbol={$feeSymbolStore} feeTokenId={$feeTokenIdStore} />
 	{/if}
 </div>
---
+++ src/frontend/src/icp-eth/components/fee/FeeAmountDisplay.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { debounce, nonNullish } from '@dfinity/utils';
+	import { fade, slide } from 'svelte/transition';
+	import { formatToken } from '$lib/utils/format.utils';
+	import { EIGHT_DECIMALS } from '$lib/constants/app.constants';
+	import { i18n } from '$lib/stores/i18n.store';
+	import { replacePlaceholders } from '$lib/utils/i18n.utils';
+	import { balancesStore } from '$lib/stores/balances.store.js';
+	import { BigNumber } from '@ethersproject/bignumber';
+	import type { TokenId } from '$lib/types/token';
+
+	export let fee: BigNumber;
+	export let feeSymbol: string;
+	export let feeTokenId: TokenId;
+
+	let balance: BigNumber | undefined;
+	$: balance = nonNullish($balancesStore)
+		? $balancesStore[feeTokenId]?.data ?? BigNumber.from(0n)
+		: undefined;
+
+	let insufficientFeeFunds = false;
+
+	const debounceCheckFeeFunds = debounce(
+		() => (insufficientFeeFunds = nonNullish(balance) && balance.lt(fee))
+	);
+
+	$: balance, fee, debounceCheckFeeFunds();
+</script>
+
+<div transition:fade>
+	{formatToken({
+		value: fee,
+		displayDecimals: EIGHT_DECIMALS
+	})}
+	{feeSymbol}
+</div>
+{#if insufficientFeeFunds && nonNullish(balance)}
+	<p in:slide={{ duration: 250 }} class="text-cyclamen">
+		{replacePlaceholders($i18n.send.assertion.not_enough_tokens_for_gas, {
+			$balance: formatToken({
+				value: balance,
+				displayDecimals: EIGHT_DECIMALS
+			}),
+			$symbol: feeSymbol ?? ''
+		})}
+	</p>
+{/if}
