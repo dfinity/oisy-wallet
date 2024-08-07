70cf971f1c790b90196c713e02b5aa20e7ebee7f
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-08-07 10:25:31 +0200

    refactor(frontend): create component SendButtonWithModal

---
+++ src/frontend/src/lib/components/send/SendButtonWithModal.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { modalStore } from '$lib/stores/modal.store';
+	import SendButton from '$lib/components/send/SendButton.svelte';
+
+	export let openModal: (modalId: symbol) => void;
+	export let isOpen: boolean;
+
+	const modalId = Symbol();
+</script>
+
+<SendButton on:click={() => openModal(modalId)} />
+
+{#if isOpen && $modalStore?.data === modalId}
+	<slot name="modal" />
+{/if}
