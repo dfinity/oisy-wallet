e747bc7a219355abe6c9c2e9428429567a39e774
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-08-08 09:31:30 +0200

    feat: create tests

--- src/backend/tests/it/utils/mock.rs
+++ src/backend/tests/it/utils/mock.rs
@@ -1,5 +1,6 @@
 pub const CALLER: &str = "xzg7k-thc6c-idntg-knmtz-2fbhh-utt3e-snqw6-5xph3-54pbp-7axl5-tae";
 pub const CALLER_ETH_ADDRESS: &str = "0xdd7fec4C49CD2Dd4eaa884D22D92503EabA5A791";
+pub const CALLER_BTC_ADDRESS: &str = "msx83kFfWPib2YbnUk6xTpBn85vefcaVX8";
 
 pub const CONTROLLER: &str = "l3lfs-gak7g-xrbil-j4v4h-aztjn-4jyki-wprso-m27h3-ibcl3-2cwuz-oqe";
 
