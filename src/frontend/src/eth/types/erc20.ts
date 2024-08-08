4573154aeb9ea8e54abd6db0bbfabe42d33657cf
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-08-08 10:37:17 +0200

    refactor(frontend): create RequiredExcept function for types (#1985)
    # Motivation
    
    Create utility function `RequiredExcept` to be used for required tokens
    that still retain the state of specific props..

--- src/frontend/src/eth/types/erc20.ts
+++ src/frontend/src/eth/types/erc20.ts
@@ -1,11 +1,11 @@
 import type { ContractAddress } from '$eth/types/address';
 import type { Exchange } from '$lib/types/exchange';
 import type { Token, TokenAppearance, TokenMetadata } from '$lib/types/token';
+import type { RequiredExcept } from '$lib/types/utils';
 
 export type Erc20Token = Erc20Contract & Token;
 
-export type RequiredErc20Token = Required<Omit<Erc20Token, keyof TokenAppearance>> &
-	TokenAppearance;
+export type RequiredErc20Token = RequiredExcept<Erc20Token, keyof TokenAppearance>;
 
 export type Erc20ContractAddress = ContractAddress;
 export type Erc20Contract = Erc20ContractAddress & { exchange: Exchange; twinTokenSymbol?: string };
