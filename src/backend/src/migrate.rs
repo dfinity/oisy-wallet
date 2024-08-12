a0bfaf51d8765afd4c87995bc49d58eed8bc01cc
Max Murphy <max@winning.black>
2024-08-12 19:28:03 +0200

    ++

---
+++ src/backend/src/migrate.rs
@@ -0,0 +1,176 @@
+use std::ops::Bound;
+
+use candid::{decode_one, encode_one, CandidType, Principal};
+use ic_cdk_timers::clear_timer;
+use serde::Deserialize;
+use shared::{backend_api::Service, types::{token::UserToken, ApiEnabled, Guards, MigrationProgress, Timestamp}};
+
+use crate::{modify_state_config, mutate_state, read_state, types::{Candid, StoredPrincipal}};
+
+/// A chunk of data to be migrated.
+/// 
+/// Note: Given that the migration moves data types that may be private, data is transferred with candid type Vec<u8>
+/// rather than littering the .did file with private types.
+#[derive(CandidType, Deserialize, Clone, Eq, PartialEq, Debug)]
+pub enum MigrationChunk {
+    UserToken(Vec<(Principal, Vec<UserToken>)>),
+    UserProfileUpdated(Vec<(Principal, Timestamp)>),
+}
+
+/// Bulk uploads data to this canister.
+pub fn bulk_up(data: &[u8]) {
+    let parsed: MigrationChunk = decode_one(&data).expect("failed to parse the data");
+    match parsed {
+        MigrationChunk::UserToken(tokens) => {
+            mutate_state(|state| {
+                for (principal, token) in tokens {
+                    state
+                        .user_token
+                        .insert(StoredPrincipal(principal), Candid(token))
+                        .expect("failed to insert user token");
+                }
+            });
+        }
+        MigrationChunk::UserProfileUpdated(users) => {
+            mutate_state(|state| {
+                for (principal, timestamp) in users {
+                    state
+                        .user_profile_updated
+                        .insert(StoredPrincipal(principal), timestamp)
+                        .expect("failed to insert user profile update");
+                }
+            });
+        }
+    }
+}
+
+
+pub async fn step_migration() {
+    fn proceed_to_next_stage() {
+        mutate_state(|state| {
+            state.migration.iter_mut().for_each(|migration| {
+                migration.progress.advance();
+            });
+        });
+    }
+    let migration = read_state(|s| s.migration.clone());
+    match migration {
+        Some(mut migration) => {
+            match migration.progress {
+                MigrationProgress::Pending => {
+                    // Lock the local canister APIs.
+                    mutate_state(|state| {
+                        modify_state_config(state, |config| {
+                            config.api = Some(Guards {
+                                threshold_key: ApiEnabled::ReadOnly,
+                                user_data: ApiEnabled::ReadOnly,
+                            })
+                        });
+                    });
+                    proceed_to_next_stage();
+                }
+                MigrationProgress::Locked => {
+                    // Lock the target canister APIs.
+                    let lock_target = Service(migration.to)
+                        .set_guards(Guards {
+                            threshold_key: ApiEnabled::ReadOnly,
+                            user_data: ApiEnabled::ReadOnly,
+                        })
+                        .await;
+                    assert!(lock_target.is_ok()); // TODO: Handle errors
+                    proceed_to_next_stage();
+                }
+                MigrationProgress::TargetLocked => {
+                    // Check that the target canister is empty.
+                    let stats = Service(migration.to).stats().await;
+                    let stats = stats
+                        .expect("failed to get stats from the target canister")
+                        .0; // TODO: Handle errors
+                    assert_eq!(stats.user_profile_count, 0); // TODO: Handle errors
+                    proceed_to_next_stage();
+                }
+                MigrationProgress::TargetPreCheckOk => {
+                    // Start migrating user tokens.
+                    proceed_to_next_stage();
+                }
+                MigrationProgress::MigratedUserTokensUpTo(token_maybe) => {
+                    // Migrate user tokens
+                    let chunk_size = 5;
+                    let range = token_maybe
+                        .map(|token| (Bound::Excluded(StoredPrincipal(token)), Bound::Unbounded))
+                        .unwrap_or((Bound::Unbounded, Bound::Unbounded));
+                    let tokens = read_state(|state| {
+                        state
+                            .user_token
+                            .range(range)
+                            .take(chunk_size)
+                            .map(|(stored_principal, token)| (stored_principal.0, token.0))
+                            .collect::<Vec<_>>()
+                    });
+                    let last = tokens.last().map(|(k, _)| k);
+                    let next_state = last
+                        .map(|last| MigrationProgress::MigratedUserTokensUpTo(Some(last.clone())))
+                        .unwrap_or_else(|| migration.progress.next());
+                    let migration_data = MigrationChunk::UserToken(tokens);
+                    let migration_bytes =
+                        encode_one(migration_data).expect("failed to encode migration data");
+                    Service(migration.to)
+                        .bulk_up(migration_bytes)
+                        .await
+                        .expect("failed to bulk up"); // TODO: Handle errors
+
+                    mutate_state(|state| {
+                        migration.progress = next_state;
+                        state.migration = Some(migration);
+                    });
+                }
+                MigrationProgress::MigratedUserTimestampsUpTo(user_maybe) => {
+                    // Migrate user timestamps
+                    let chunk_size = 5;
+                    let range = user_maybe
+                        .map(|user| (Bound::Excluded(StoredPrincipal(user)), Bound::Unbounded))
+                        .unwrap_or((Bound::Unbounded, Bound::Unbounded));
+                    let users = read_state(|state| {
+                        state
+                            .user_profile_updated
+                            .range(range)
+                            .take(chunk_size)
+                            .map(|(stored_principal, timestamp)| (stored_principal.0, timestamp))
+                            .collect::<Vec<_>>()
+                    });
+                    let last = users.last().map(|(k, _)| k);
+                    let next_state = last
+                        .map(|last| MigrationProgress::MigratedUserTokensUpTo(Some(last.clone())))
+                        .unwrap_or_else(|| migration.progress.next());
+                    let migration_data = MigrationChunk::UserProfileUpdated(users);
+                    let migration_bytes =
+                        encode_one(migration_data).expect("failed to encode migration data");
+                    Service(migration.to)
+                        .bulk_up(migration_bytes)
+                        .await
+                        .expect("failed to bulk up"); // TODO: Handle errors
+
+                    mutate_state(|state| {
+                        migration.progress = next_state;
+                        state.migration = Some(migration);
+                    });
+                }
+                MigrationProgress::MigratedUserProfilesUpTo(_) => todo!(),
+                MigrationProgress::MigratedCustomTokensUpTo(_) => {
+                    // TODO: Migrate custom tokens
+                    proceed_to_next_stage();
+                }
+                MigrationProgress::CheckingTargetCanister => {
+                    // TODO: Check that the target canister has all the data.
+                    proceed_to_next_stage();
+                }
+                MigrationProgress::Completed => {
+                    clear_timer(migration.timer_id);
+                }
+            }
+        }
+        None => {
+            ic_cdk::trap("migration is not in progress");
+        }
+    }
+}
