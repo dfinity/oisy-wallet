031e66c98a121078a60ff5b72a780ad5695da0fe
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-08-13 14:46:43 +0200

    feat: create new Address type

--- src/frontend/src/lib/stores/address.store.ts
+++ src/frontend/src/lib/stores/address.store.ts
@@ -1,7 +1,7 @@
 import { initCertifiedSetterStore } from '$lib/stores/certified-setter.store';
-import type { EthAddress } from '$lib/types/address';
+import type { Address } from '$lib/types/address';
 import type { CertifiedData } from '$lib/types/store';
 
-export type AddressData = CertifiedData<EthAddress>;
+export type AddressData = CertifiedData<Address>;
 
 export const addressStore = initCertifiedSetterStore<AddressData>();
