13de13f58c06fcdc182efa72b48a3a73551767e5
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-08-13 09:15:42 +0200

    revert: change IdbAddress to interface

--- src/frontend/src/lib/types/idb.ts
+++ src/frontend/src/lib/types/idb.ts
@@ -2,11 +2,11 @@ import type { Address } from '$lib/types/address';
 import type { Principal } from '@dfinity/principal';
 import type { UseStore } from 'idb-keyval';
 
-export type IdbAddress = {
+export interface IdbAddress {
 	address: Address;
 	createdAtTimestamp: number;
 	lastUsedTimestamp: number;
-};
+}
 
 export interface IdbParams {
 	principal: Principal;
