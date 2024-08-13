ad7a82cada6ddbfea256c8c7b16bab2293143306
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-08-13 16:46:12 +0200

    refactor: change addressCertified derived

--- src/frontend/src/lib/derived/address.derived.ts
+++ src/frontend/src/lib/derived/address.derived.ts
@@ -1,7 +1,7 @@
 import { ETHEREUM_TOKEN_ID } from '$env/tokens.env';
-import { addressStore, type AddressData } from '$lib/stores/address.store';
+import { addressStore } from '$lib/stores/address.store';
 import type { OptionEthAddress } from '$lib/types/address';
-import { isNullish, nonNullish } from '@dfinity/utils';
+import { isNullish } from '@dfinity/utils';
 import { derived, type Readable } from 'svelte/store';
 
 export const addressNotLoaded: Readable<boolean> = derived([addressStore], ([$addressStore]) =>
@@ -14,11 +14,7 @@ export const ethAddress: Readable<OptionEthAddress> = derived([addressStore], ([
 
 export const addressCertified: Readable<boolean> = derived(
 	[addressStore],
-	([$addressStore]) =>
-		nonNullish($addressStore) &&
-		Object.values($addressStore).every(
-			(value) => value !== null && (value as AddressData).certified
-		)
+	([$addressStore]) => $addressStore?.[ETHEREUM_TOKEN_ID]?.certified === true
 );
 
 export const addressNotCertified: Readable<boolean> = derived(
