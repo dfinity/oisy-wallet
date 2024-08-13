3259882a2a2ef64c5a271dcedd46ef756f990aca
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-08-13 10:14:12 +0200

    revert: make the PR to change only two functions

--- src/frontend/src/lib/types/idb.ts
+++ src/frontend/src/lib/types/idb.ts
@@ -1,14 +1,7 @@
 import type { Address } from '$lib/types/address';
-import type { Principal } from '@dfinity/principal';
-import type { UseStore } from 'idb-keyval';
 
 export interface IdbAddress {
 	address: Address;
 	createdAtTimestamp: number;
 	lastUsedTimestamp: number;
-}
-
-export interface IdbParams {
-	principal: Principal;
-	store: UseStore;
 }
