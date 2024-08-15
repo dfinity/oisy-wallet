6576c7d411ba55c9f6c07cc1fb6b8557eac3a4d1
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2024-08-15 11:54:13 +0200

    ci(docker): fix e2e docker image backend deployment

--- e2e/homepage.spec.ts
+++ e2e/homepage.spec.ts
@@ -26,13 +26,38 @@ test.describe('logged out user', () => {
 });
 
 testWithII.describe('logged in user', () => {
-	testWithII.beforeEach(async ({ page, iiPage }) => {
+	testWithII.beforeEach(async ({ page, iiPage, context }) => {
+		const url = 'http://127.0.0.1:4943';
+		const canisterId = 'bkyz2-fmaaa-aaaaa-qaaaq-cai';
+		const timeout = 180000;
+		await iiPage.waitReady({ url, canisterId, timeout });
+
 		await page.goto(testUrl);
 
-		await iiPage.signInWithNewIdentity();
+		// test if adding timeout solves the issue
+		const iiPageCustomPromise = context.waitForEvent('page', { timeout });
+		await page.getByTestId('login-button').click();
 
-		await page.getByTestId('exchange-balance-output').waitFor();
-		await page.getByTestId('tokens-skeletons-initialized').waitFor();
+		const iiPageCustom = await iiPageCustomPromise;
+		await expect(iiPageCustom).toHaveTitle('Internet Identity', { timeout });
+
+		await iiPageCustom.locator('#registerButton').click({ timeout });
+		await iiPageCustom.locator('[data-action=construct-identity]').click({ timeout });
+
+		await iiPageCustom.locator('input#captchaInput').fill('a');
+		await iiPageCustom.locator('#confirmRegisterButton').click({ timeout });
+
+		const identity = await iiPageCustom.locator('#userNumber').textContent({ timeout });
+		expect(identity).not.toBeNull();
+
+		await iiPageCustom.locator('#displayUserContinue').click();
+		await iiPageCustom.waitForEvent('close', { timeout });
+		expect(iiPageCustom.isClosed()).toBe(true);
+
+		// await iiPage.signInWithNewIdentity();
+
+		await page.getByTestId('exchange-balance-output').waitFor({ timeout });
+		await page.getByTestId('tokens-skeletons-initialized').waitFor({ timeout });
 	});
 
 	testWithII('should display logged in hero and tokens', async ({ page }) => {
