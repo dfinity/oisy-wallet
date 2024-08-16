19c88a8f0c42d647f1f5ddaf2e056f82b5744568
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-08-16 15:46:44 +0200

    build(deploy): update deploy script with missing dfx commands (#2035)
    # Motivation
    
    Added 2 missing dfx command so the deploy.sh script works locally and in
    e2e tests CI.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+dfx canister create internet_identity --specified-id rdmx6-jaaaa-aaaaa-aaadq-cai
+dfx canister create pouh_issuer --specified-id qbw6f-caaaa-aaaah-qdcwa-cai
+
 ./scripts/deploy.backend.sh
 
 mkdir -p ./target/ic
