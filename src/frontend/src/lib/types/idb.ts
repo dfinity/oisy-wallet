7802ee84d085e6b82a7a820a2de554ce39dc052d
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-08-16 19:30:12 +0200

    feat(frontend): IDB functions for Bitcoin address (#2034)
    # Motivation
    
    We replicate the IDB functions for Bitcoin Mainnet address, as they are
    used for Ethereum.

--- src/frontend/src/lib/types/idb.ts
+++ src/frontend/src/lib/types/idb.ts
@@ -1,4 +1,10 @@
-import type { EthAddress } from '$lib/types/address';
+import type { BtcAddress, EthAddress } from '$lib/types/address';
+
+export interface IdbBtcAddress {
+	address: BtcAddress;
+	createdAtTimestamp: number;
+	lastUsedTimestamp: number;
+}
 
 export interface IdbEthAddress {
 	address: EthAddress;
