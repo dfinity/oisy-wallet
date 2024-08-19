d5225d7b1caadf528e65540115d82fee6455c0be
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2024-08-19 09:39:10 +0200

    test(homepage): update homepage tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
 		port: DEV ? 5173 : 4173
 	},
 	testDir: 'e2e',
-	timeout: 120000,
+	timeout: 180000,
 	testMatch: ['**/*.e2e.ts', '**/*.spec.ts'],
 	use: {
 		testIdAttribute: 'data-tid',
--- src/frontend/src/lib/components/exchange/ExchangeBalance.svelte
+++ src/frontend/src/lib/components/exchange/ExchangeBalance.svelte
@@ -2,7 +2,6 @@
 	import { formatUSD } from '$lib/utils/format.utils';
 	import { exchangeInitialized } from '$lib/derived/exchange.derived';
 	import { combinedDerivedEnabledNetworkTokensUi } from '$lib/derived/network-tokens.derived';
-	import { EXCHANGE_BALANCE_OUTPUT } from '$lib/constants/test-ids.constant';
 
 	let totalUsd: number;
 	$: totalUsd = $combinedDerivedEnabledNetworkTokensUi.reduce(
@@ -14,7 +13,6 @@
 <span class="text-off-white block">
 	<output
 		class={`break-all text-5xl font-bold ${totalUsd === 0 ? 'opacity-50' : 'opacity-100'} inline-block mt-8`}
-		data-tid={EXCHANGE_BALANCE_OUTPUT}
 	>
 		{#if $exchangeInitialized}
 			{formatUSD(totalUsd)}
