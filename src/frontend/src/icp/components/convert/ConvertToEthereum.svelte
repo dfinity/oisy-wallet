cfb1fcae9a6427244632ff7a358141a9ff8b5053
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-08-20 14:53:29 +0200

    style(frontend): change conversion icon (#2068)
    # Motivation
    
    Updated conversion icon.
    
    <img width="517" alt="Screenshot 2024-08-20 at 12 09 00"
    src="https://github.com/user-attachments/assets/a62e05bc-3a4c-44bb-94da-c2b40dcfe7c7">

--- src/frontend/src/icp/components/convert/ConvertToEthereum.svelte
+++ src/frontend/src/icp/components/convert/ConvertToEthereum.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import ConvertETH from '$icp-eth/components/send/ConvertETH.svelte';
-	import IconBurn from '$lib/components/icons/IconBurn.svelte';
 	import { modalConvertToTwinTokenEth } from '$lib/derived/modal.derived';
 	import IcSendModal from '$icp/components/send/IcSendModal.svelte';
 	import { ethAddress } from '$lib/derived/address.derived';
@@ -12,6 +11,7 @@
 	} from '$icp-eth/derived/cketh.derived';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { replacePlaceholders } from '$lib/utils/i18n.utils';
+	import IconConvert from '$lib/components/icons/IconConvert.svelte';
 </script>
 
 <ConvertETH
@@ -21,7 +21,7 @@
 		$token: $ckEthereumTwinToken.symbol
 	})}
 >
-	<IconBurn size="28" slot="icon" />
+	<IconConvert slot="icon" />
 	<span
 		>{replacePlaceholders($i18n.convert.text.convert_to_token, {
 			$token: $ckEthereumTwinToken.symbol
