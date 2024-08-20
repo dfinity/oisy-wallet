4cbb5bd6ec9ee2f73de920cf8080c732b2315792
Max <bitdivine@users.noreply.github.com>
2024-08-20 11:35:18 +0200

    feat(backend): Step through migration states (#2058)
    # Motivation
    User data migration to another canister is triggered by an API call.
    This PR introduces a skeleton API call that, ghostlike, steps through
    the motions of the migration without doing anything.
    
    # Changes
    - Add an update method that starts a user data migration.
    - Implement a skeleton migration that steps through the stages of the
    migration.
    
    # Tests
    - A test is included that verifies that the migration state does indeed
    advance as expected.
    
    ---------
    
    Co-authored-by: Frederik Rothenberger <frederik.rothenberger@dfinity.org>

--- src/backend/src/migrate.rs
+++ src/backend/src/migrate.rs
@@ -1,11 +1,13 @@
 use crate::{
-    mutate_state,
+    mutate_state, read_state,
     types::{Candid, StoredPrincipal},
 };
 use candid::{decode_one, CandidType, Principal};
+use ic_cdk_timers::clear_timer;
 use serde::Deserialize;
 use shared::types::{
-    custom_token::CustomToken, token::UserToken, user_profile::StoredUserProfile, Timestamp,
+    custom_token::CustomToken, token::UserToken, user_profile::StoredUserProfile,
+    MigrationProgress, Timestamp,
 };
 
 /// A chunk of data to be migrated.
@@ -65,6 +67,68 @@ pub fn bulk_up(data: &[u8]) {
                         .expect("failed to insert user profile update");
                 }
             });
+        }
+    }
+}
+
+#[allow(clippy::unused_async)] // TODO: remove this once we make real function calls
+pub async fn step_migration() {
+    fn set_progress(progress: MigrationProgress) {
+        mutate_state(|state| {
+            state.migration.iter_mut().for_each(|migration| {
+                migration.progress = progress;
+            });
+        });
+    }
+    let migration = read_state(|s| s.migration.clone());
+    match migration {
+        Some(migration) => {
+            match migration.progress {
+                MigrationProgress::Pending => {
+                    // TODO: Lock the local canister APIs.
+                    set_progress(migration.progress.next());
+                }
+                MigrationProgress::Locked => {
+                    // TODO: Lock the target canister APIs.
+                    set_progress(migration.progress.next());
+                }
+                MigrationProgress::TargetLocked => {
+                    // TODO: Check that the target canister is empty.
+                    set_progress(migration.progress.next());
+                }
+                MigrationProgress::TargetPreCheckOk => {
+                    // TODO: Start migrating user tokens.
+                    set_progress(migration.progress.next());
+                }
+                MigrationProgress::MigratedUserTokensUpTo(_last) => {
+                    // TODO: Migrate user tokens, then move on to the next stage.
+                    set_progress(migration.progress.next());
+                }
+                MigrationProgress::MigratedCustomTokensUpTo(_last_custom_token) => {
+                    // TODO: Migrate custom tokens, then move on to the next stage.
+                    set_progress(migration.progress.next());
+                }
+                MigrationProgress::MigratedUserTimestampsUpTo(_user_maybe) => {
+                    // TODO: Migrate user timestamps, then move on to the next stage.
+                    set_progress(migration.progress.next());
+                }
+                MigrationProgress::MigratedUserProfilesUpTo(_last_user_profile) => {
+                    // TODO: Migrate user profiles, then move on to the next stage.
+                    set_progress(migration.progress.next());
+                }
+                MigrationProgress::CheckingTargetCanister => {
+                    // TODO: Check that the target canister has all the data.
+                    set_progress(migration.progress.next());
+                }
+                // TODO: Add steps to unlock APIs.
+                MigrationProgress::Completed => {
+                    // Migration is complete.
+                    clear_timer(migration.timer_id);
+                }
+            }
+        }
+        None => {
+            ic_cdk::trap("migration is not in progress");
         }
     }
 }
