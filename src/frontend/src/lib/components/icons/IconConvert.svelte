329fc6da603d593b8faefd20bade9af6a18c2c1b
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-08-20 14:53:29 +0200

    style(frontend): change conversion icon (#2068)
    # Motivation
    
    Updated conversion icon.
    
    <img width="517" alt="Screenshot 2024-08-20 at 12 09 00"
    src="https://github.com/user-attachments/assets/a62e05bc-3a4c-44bb-94da-c2b40dcfe7c7">

---
+++ src/frontend/src/lib/components/icons/IconConvert.svelte
@@ -0,0 +1,10 @@
+<!-- source: DFINITY foundation -->
+<svg width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path
+		d="M16 11L20 7M20 7L16 3M20 7L4 7M8 13L4 17M4 17L8 21M4 17L20 17"
+		stroke="currentColor"
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+</svg>
