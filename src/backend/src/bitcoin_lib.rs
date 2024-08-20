c9db970306627126bcf04073f5a7e8e254f57d90
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-08-20 18:07:23 +0200

    lint: adjust link in docstring

--- src/backend/src/bitcoin_lib.rs
+++ src/backend/src/bitcoin_lib.rs
@@ -5,7 +5,7 @@ use ic_cdk::api::management_canister::bitcoin::{
 /// Returns the balance of the given bitcoin address.
 ///
 /// Relies on the `bitcoin_get_balance` endpoint.
-/// See https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-bitcoin_get_balance
+/// See [IC Bitcoin Documentation](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-bitcoin_get_balance)
 pub async fn get_balance(network: BitcoinNetwork, address: String) -> u64 {
     let balance_res = bitcoin_get_balance(GetBalanceRequest {
         address,
