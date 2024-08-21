856175df9ebeb1d8c678974a8bcb57d762525e5a
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-08-21 17:13:48 +0200

    lint: remove unused imports

--- src/frontend/src/lib/components/exchange/ExchangeBalance.svelte
+++ src/frontend/src/lib/components/exchange/ExchangeBalance.svelte
@@ -2,11 +2,6 @@
 	import { formatUSD } from '$lib/utils/format.utils';
 	import { exchangeInitialized } from '$lib/derived/exchange.derived';
 	import { combinedDerivedEnabledNetworkTokensUi } from '$lib/derived/network-tokens.derived';
-	import { isNullish, nonNullish } from '@dfinity/utils';
-	import { balance, balanceZero } from '$lib/derived/balances.derived';
-	import { erc20UserTokensInitialized } from '$eth/derived/erc20.derived';
-	import { tokenSymbol } from '$lib/derived/token.derived';
-	import Amount from '$lib/components/ui/Amount.svelte';
 
 	let totalUsd: number;
 	$: totalUsd = $combinedDerivedEnabledNetworkTokensUi.reduce(
