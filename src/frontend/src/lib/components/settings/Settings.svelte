4490641c11bceb4da4f5a5ffa2b9699753294519
David Dal Busco <david.dalbusco@outlook.com>
2024-08-21 13:13:50 +0200

    refactor(frontend): rename pages to settings

---
+++ src/frontend/src/lib/components/settings/Settings.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { authSignedIn } from '$lib/derived/auth.derived';
+	import SettingsSignedIn from '$lib/components/settings/SettingsSignedIn.svelte';
+	import SettingsSignedOut from '$lib/components/settings/SettingsSignedOut.svelte';
+</script>
+
+{#if $authSignedIn}
+	<SettingsSignedIn />
+{:else}
+	<SettingsSignedOut />
+{/if}
