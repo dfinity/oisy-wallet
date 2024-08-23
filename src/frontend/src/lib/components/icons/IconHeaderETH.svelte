bcf185cc5fc398cc940b12c3963052541818ccca
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-08-23 11:40:36 +0200

    style(frontend): update blockchain icons in header (#2106)
    # Motivation
    
    Updating blockchain icons in header when logged out.
    
    <img width="1027" alt="Screenshot 2024-08-23 at 11 24 36"
    src="https://github.com/user-attachments/assets/02b64126-33eb-4469-8aee-f8c2b710f8f4">

--- src/frontend/src/lib/components/icons/IconHeaderETH.svelte
+++ src/frontend/src/lib/components/icons/IconHeaderETH.svelte
@@ -1,29 +1,29 @@
 <!-- source: DFINITY foundation -->
-<svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-	<circle cx="18" cy="18" r="18" fill="url(#paint0_linear_2033_44174)" />
+<svg width="36" height="36" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<circle cx="19.7143" cy="19.7143" r="19.7143" fill="url(#paint0_radial_3833_14248)" />
 	<path
-		d="M17.9509 7.19995L17.8077 7.68634V21.7988L17.9509 21.9417L24.5017 18.0695L17.9509 7.19995Z"
+		d="M19.6607 7.88477L19.5039 8.41747V23.874L19.6607 24.0305L26.8354 19.7895L19.6607 7.88477Z"
 		fill="#DDDEFE"
 	/>
-	<path d="M17.9509 7.19995L11.4 18.0695L17.9509 21.9417V15.0919V7.19995Z" fill="white" />
+	<path d="M19.6606 7.88477L12.4858 19.7895L19.6606 24.0305V16.5283V7.88477Z" fill="white" />
 	<path
-		d="M17.9509 23.182L17.8702 23.2804V28.3075L17.9509 28.5431L24.5057 19.3118L17.9509 23.182Z"
+		d="M19.6607 25.3884L19.5723 25.4962V31.0021L19.6607 31.2601L26.8397 21.1497L19.6607 25.3884Z"
 		fill="#DDDEFE"
 	/>
-	<path d="M17.9509 28.5431V23.182L11.4 19.3118L17.9509 28.5431Z" fill="white" />
-	<path d="M17.9509 21.9416L24.5017 18.0694L17.9509 15.0918V21.9416Z" fill="#A4A8E8" />
-	<path d="M11.4 18.0694L17.9509 21.9416V15.0918L11.4 18.0694Z" fill="#C4C7F6" />
+	<path d="M19.6606 31.2601V25.3884L12.4858 21.1497L19.6606 31.2601Z" fill="white" />
+	<path d="M19.6606 24.0312L26.8353 19.7903L19.6606 16.5291V24.0312Z" fill="#A4A8E8" />
+	<path d="M12.4858 19.7903L19.6606 24.0312V16.5291L12.4858 19.7903Z" fill="#C4C7F6" />
 	<defs>
-		<linearGradient
-			id="paint0_linear_2033_44174"
-			x1="0"
-			y1="18"
-			x2="36"
-			y2="18"
+		<radialGradient
+			id="paint0_radial_3833_14248"
+			cx="0"
+			cy="0"
+			r="1"
 			gradientUnits="userSpaceOnUse"
+			gradientTransform="translate(33.9287 20) rotate(-180) scale(46 78.7091)"
 		>
-			<stop stop-color="#666ACC" />
-			<stop offset="1" stop-color="#242B95" />
-		</linearGradient>
+			<stop stop-color="#030C7F" />
+			<stop offset="1" stop-color="#1B54CA" />
+		</radialGradient>
 	</defs>
 </svg>
