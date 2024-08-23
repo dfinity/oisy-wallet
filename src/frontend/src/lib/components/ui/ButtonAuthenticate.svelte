d10a4b655bdccebc3e0ddddc92aecb7ce23f635b
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-08-23 11:57:06 +0200

    style(frontend): update authenticate button style (#2105)
    # Motivation
    
    New style for the `ButtonAuthenticate`.
    
    
    
    https://github.com/user-attachments/assets/4f309ae0-8bfb-4fc3-b208-0c8976de9b69
    
    
    
    https://github.com/user-attachments/assets/0cb88f97-9554-4e36-84b6-b6fa30bd3234

--- src/frontend/src/lib/components/ui/ButtonAuthenticate.svelte
+++ src/frontend/src/lib/components/ui/ButtonAuthenticate.svelte
@@ -2,37 +2,25 @@
 	import IconArrowAstronaut from '$lib/components/icons/IconArrowAstronaut.svelte';
 </script>
 
-<button
-	on:click
-	class="h-16 px-10 py-2 rounded-full gap-4 font-bold text-lg bg-brandeis-blue"
-	data-tid="login-button"
->
-	<slot />
-	<IconArrowAstronaut />
-</button>
+<!-- This overlay is needed, otherwise the border opacity would show the gradient instead of the background. -->
+<div class="rounded-full border-overlay">
+	<button
+		on:click
+		class="authenticate h-[72px] px-[74px] py-2 rounded-full border-0 gap-4 font-bold text-lg leading-6 bg-brandeis-blue"
+		data-tid="login-button"
+	>
+		<slot />
+		<IconArrowAstronaut />
+	</button>
+</div>
 
 <style lang="scss">
-	button {
-		background: linear-gradient(
-			90deg,
-			#0975ff 0%,
-			#0975ff 24.86%,
-			#1bb5ff 30.32%,
-			#03e2ff 50.17%,
-			#1bb5ff 70.25%,
-			#0874ff 75.12%,
-			#0874ff 100%
-		);
-		background-size: 600% 100%;
-		transition:
-			background-position 0.7s ease-in-out,
-			box-shadow 0.5s ease-in-out;
+	.border-overlay {
+		border: solid 4px rgba(7, 22, 137, 0.15);
+		transition: border-color 0.5s ease-in-out;
 	}
 
-	button:hover {
-		background-position: 100% 0;
-		box-shadow: 0 0 50px var(--color-spiro-disco-ball);
-		border-width: 1px;
-		border-color: rgba(255, 255, 255, 0.2);
+	.border-overlay:hover {
+		border-color: rgba(7, 22, 137, 0.25);
 	}
 </style>
