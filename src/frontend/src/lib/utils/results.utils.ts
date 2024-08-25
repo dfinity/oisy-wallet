3997d9554824e197bca876899b98e868dd34fd45
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-08-25 22:07:42 +0200

    refactor: make input non empty

--- src/frontend/src/lib/utils/results.utils.ts
+++ src/frontend/src/lib/utils/results.utils.ts
@@ -1,7 +1,9 @@
 import type { ResultSuccess, ResultSuccessReduced } from '$lib/types/utils';
 import { nonNullish } from '@dfinity/utils';
 
-export const reduceResults = <T = unknown>(results: ResultSuccess<T>[]): ResultSuccessReduced<T> =>
+export const reduceResults = <T = unknown>(
+	results: [ResultSuccess<T>, ...ResultSuccess<T>[]]
+): ResultSuccessReduced<T> =>
 	results.reduce(
 		(acc, { success: s, err: e }) => ({
 			success: acc.success && s,
--- src/frontend/src/tests/lib/utils/results.utils.spec.ts
+++ src/frontend/src/tests/lib/utils/results.utils.spec.ts
@@ -3,7 +3,10 @@ import { reduceResults } from '$lib/utils/results.utils';
 
 describe('reduceResults', () => {
 	it('should return a successful result when all results are successful', () => {
-		const results: ResultSuccess<string>[] = [{ success: true }, { success: true }];
+		const results: [ResultSuccess<string>, ...ResultSuccess<string>[]] = [
+			{ success: true },
+			{ success: true }
+		];
 
 		const reduced = reduceResults(results);
 
@@ -11,7 +14,7 @@ describe('reduceResults', () => {
 	});
 
 	it('should return a failed result with errors when any result is unsuccessful', () => {
-		const results: ResultSuccess<string>[] = [
+		const results: [ResultSuccess<string>, ...ResultSuccess<string>[]] = [
 			{ success: true, err: undefined },
 			{ success: false, err: 'Error 1' },
 			{ success: true },
@@ -23,19 +26,11 @@ describe('reduceResults', () => {
 		expect(reduced).toEqual({ success: false, err: ['Error 1', 'Error 2'] });
 	});
 
-	it('should handle an empty array of results', () => {
-		const results: ResultSuccess[] = [];
-
-		const reduced = reduceResults(results);
-
-		expect(reduced).toEqual({ success: true, err: [] });
-	});
-
 	it('should ignore undefined errors', () => {
-		const results: ResultSuccess<string | undefined>[] = [
+		const results: [ResultSuccess<string>, ...ResultSuccess<string>[]] = [
 			{ success: true, err: undefined },
 			{ success: false, err: 'Error 1' },
-			{ success: true, err: undefined },
+			{ success: true },
 			{ success: false, err: undefined }
 		];
 
