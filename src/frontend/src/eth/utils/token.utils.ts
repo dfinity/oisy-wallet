b9737ff7c51acafb114cee680a9ebdea8d9d42ee
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-08-26 09:00:23 +0200

    refactor: using hexZeroPad

--- src/frontend/src/eth/utils/token.utils.ts
+++ src/frontend/src/eth/utils/token.utils.ts
@@ -1,6 +1,7 @@
 import { ERC20_TWIN_TOKENS_IDS } from '$env/tokens.erc20.env';
 import { ERC20_ICP_SYMBOL } from '$eth/constants/erc20-icp.constants';
 import type { OptionToken, TokenId } from '$lib/types/token';
+import { hexZeroPad } from 'ethers/lib/utils';
 
 export const isErc20Icp = (token: OptionToken): boolean =>
 	token?.symbol === ERC20_ICP_SYMBOL && token?.standard === 'erc20';
@@ -11,5 +12,4 @@ export const isSupportedErc20TwinTokenId = (tokenId: TokenId): boolean =>
 export const isNotSupportedErc20TwinTokenId = (tokenId: TokenId): boolean =>
 	!isSupportedErc20TwinTokenId(tokenId);
 
-export const tokenAddressToHex = (address: string): string =>
-	'0x' + address.toLowerCase().substring(2).padStart(64, '0');
+export const tokenAddressToHex = (address: string): string => hexZeroPad(address.toLowerCase(), 64);
