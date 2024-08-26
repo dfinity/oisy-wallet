cdba1aaa615587071c7378134cf91f90f82f5484
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-08-26 09:01:30 +0200

    refactor: use directly etheres

--- src/frontend/src/eth/utils/token.utils.ts
+++ src/frontend/src/eth/utils/token.utils.ts
@@ -1,7 +1,7 @@
 import { ERC20_TWIN_TOKENS_IDS } from '$env/tokens.erc20.env';
 import { ERC20_ICP_SYMBOL } from '$eth/constants/erc20-icp.constants';
 import type { OptionToken, TokenId } from '$lib/types/token';
-import { hexZeroPad } from 'ethers/lib/utils';
+import { ethers } from 'ethers';
 
 export const isErc20Icp = (token: OptionToken): boolean =>
 	token?.symbol === ERC20_ICP_SYMBOL && token?.standard === 'erc20';
@@ -12,4 +12,5 @@ export const isSupportedErc20TwinTokenId = (tokenId: TokenId): boolean =>
 export const isNotSupportedErc20TwinTokenId = (tokenId: TokenId): boolean =>
 	!isSupportedErc20TwinTokenId(tokenId);
 
-export const tokenAddressToHex = (address: string): string => hexZeroPad(address.toLowerCase(), 32);
+export const tokenAddressToHex = (address: string): string =>
+	ethers.utils.hexZeroPad(address.toLowerCase(), 32);
