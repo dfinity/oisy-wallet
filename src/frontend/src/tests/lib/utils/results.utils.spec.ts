b71c968986ab7995c7e6c3cc92f3f8e5c7801c76
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-08-26 09:04:57 +0200

    refactor(frontend): create function to reduce ResultSuccess (#2120)
    # Motivation
    
    We create a function to reduce results given by type `ResultSuccess`.
    
    # Changes
    
    - Create new type `ResultSuccessReduced`.
    - Create function `reduceResults`.
    - Create test.
    
    # Tests
    
    Test worked correctly.

---
+++ src/frontend/src/tests/lib/utils/results.utils.spec.ts
@@ -0,0 +1,41 @@
+import type { ResultSuccess } from '$lib/types/utils';
+import { reduceResults } from '$lib/utils/results.utils';
+
+describe('reduceResults', () => {
+	it('should return a successful result when all results are successful', () => {
+		const results: [ResultSuccess<string>, ...ResultSuccess<string>[]] = [
+			{ success: true },
+			{ success: true }
+		];
+
+		const reduced = reduceResults(results);
+
+		expect(reduced).toEqual({ success: true, err: undefined });
+	});
+
+	it('should return a failed result with errors when any result is unsuccessful', () => {
+		const results: [ResultSuccess<string>, ...ResultSuccess<string>[]] = [
+			{ success: true, err: undefined },
+			{ success: false, err: 'Error 1' },
+			{ success: true },
+			{ success: false, err: 'Error 2' }
+		];
+
+		const reduced = reduceResults(results);
+
+		expect(reduced).toEqual({ success: false, err: ['Error 1', 'Error 2'] });
+	});
+
+	it('should ignore undefined errors', () => {
+		const results: [ResultSuccess<string>, ...ResultSuccess<string>[]] = [
+			{ success: true, err: undefined },
+			{ success: false, err: 'Error 1' },
+			{ success: true },
+			{ success: false, err: undefined }
+		];
+
+		const reduced = reduceResults(results);
+
+		expect(reduced).toEqual({ success: false, err: ['Error 1'] });
+	});
+});
