7ec828fd755c0ee6bc4d2fd9c242b17aa35d3493
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-08-27 11:05:37 +0200

    refactor(frontend): derived store for ETH address data

--- src/frontend/src/lib/derived/address.derived.ts
+++ src/frontend/src/lib/derived/address.derived.ts
@@ -1,12 +1,17 @@
 import { BTC_MAINNET_TOKEN_ID } from '$env/tokens.btc.env';
 import { ETHEREUM_TOKEN_ID } from '$env/tokens.env';
-import { addressStore } from '$lib/stores/address.store';
+import { addressStore, type AddressData } from '$lib/stores/address.store';
 import type { OptionBtcAddress, OptionEthAddress } from '$lib/types/address';
 import { isNullish } from '@dfinity/utils';
 import { derived, type Readable } from 'svelte/store';
 
 export const addressNotLoaded: Readable<boolean> = derived([addressStore], ([$addressStore]) =>
 	isNullish($addressStore)
+);
+
+const ethAddressData: Readable<AddressData | null | undefined> = derived(
+	[addressStore],
+	([$addressStore]) => $addressStore?.[ETHEREUM_TOKEN_ID]
 );
 
 export const btcAddressMainnet: Readable<OptionBtcAddress> = derived(
@@ -17,13 +22,14 @@ export const btcAddressMainnet: Readable<OptionBtcAddress> = derived(
 			: $addressStore?.[BTC_MAINNET_TOKEN_ID]?.data
 );
 
-export const ethAddress: Readable<OptionEthAddress> = derived([addressStore], ([$addressStore]) =>
-	$addressStore?.[ETHEREUM_TOKEN_ID] === null ? null : $addressStore?.[ETHEREUM_TOKEN_ID]?.data
+export const ethAddress: Readable<OptionEthAddress> = derived(
+	[ethAddressData],
+	([$ethAddressData]) => ($ethAddressData === null ? null : $ethAddressData?.data)
 );
 
 export const ethAddressCertified: Readable<boolean> = derived(
-	[addressStore],
-	([$addressStore]) => $addressStore?.[ETHEREUM_TOKEN_ID]?.certified === true
+	[ethAddressData],
+	([$ethAddressData]) => $ethAddressData?.certified === true
 );
 
 export const ethAddressNotCertified: Readable<boolean> = derived(
