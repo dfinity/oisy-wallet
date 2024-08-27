05a940f87ea29174dc4195745bf0928da6c54df7
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-08-27 11:54:19 +0200

    style(frontend): disable token sorting when user interacts with the list (#1796)
    # Motivation
    
    We would like for the sorting of the token list to stop when the user
    interacts with the component. To do that, we create a new list that
    updates all data of the token list except the order when there is an
    interaction event.
    
    
    
    https://github.com/user-attachments/assets/29e36e04-9c45-4674-b1cc-a8725292e7a1

--- src/frontend/src/lib/utils/events.utils.ts
+++ src/frontend/src/lib/utils/events.utils.ts
@@ -1,4 +1,38 @@
+import { pointerEventStore } from '$lib/stores/events.store';
+
 export const emit = <T>({ message, detail }: { message: string; detail?: T | undefined }) => {
 	const $event: CustomEvent<T> = new CustomEvent<T>(message, { detail, bubbles: true });
 	document.dispatchEvent($event);
+};
+
+const pointerEventOn = () => pointerEventStore.set(true);
+
+const pointerEventOff = () => pointerEventStore.set(false);
+
+const pointerEventsHandlers: { [key: string]: EventListener } = {
+	pointerenter: pointerEventOn,
+	pointerover: pointerEventOn,
+	pointerleave: pointerEventOff,
+	focus: pointerEventOn,
+	focusin: pointerEventOn,
+	focusout: pointerEventOff,
+	touchstart: pointerEventOn,
+	touchmove: pointerEventOn,
+	touchend: pointerEventOff
+};
+
+export const pointerEventsHandler = (node: HTMLElement) => {
+	const eventNames = Object.keys(pointerEventsHandlers);
+
+	eventNames.forEach((event) => {
+		node.addEventListener(event, pointerEventsHandlers[event], { passive: true });
+	});
+
+	return {
+		destroy() {
+			eventNames.forEach((event) => {
+				node.removeEventListener(event, pointerEventsHandlers[event]);
+			});
+		}
+	};
 };
