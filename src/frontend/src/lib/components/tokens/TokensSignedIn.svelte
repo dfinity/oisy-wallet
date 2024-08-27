49c653ed18b23c48ddd4dcb9c5b583ec01d4c43e
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-08-27 10:47:09 +0200

    feat: create separate component to handle events

--- src/frontend/src/lib/components/tokens/TokensSignedIn.svelte
+++ src/frontend/src/lib/components/tokens/TokensSignedIn.svelte
@@ -1,68 +1,19 @@
 <script lang="ts">
 	import Listener from '$lib/components/core/Listener.svelte';
 	import TokensSkeletons from '$lib/components/tokens/TokensSkeletons.svelte';
-	import { combinedDerivedSortedNetworkTokensUi } from '$lib/derived/network-tokens.derived';
 	import { i18n } from '$lib/stores/i18n.store';
-	import type { Token, TokenUi } from '$lib/types/token';
-	import { hideZeroBalancesStore } from '$lib/stores/settings.store';
+	import type { TokenUi } from '$lib/types/token';
 	import { fade } from 'svelte/transition';
 	import { modalManageTokens } from '$lib/derived/modal.derived';
 	import ManageTokensModal from '$icp-eth/components/tokens/ManageTokensModal.svelte';
 	import { flip } from 'svelte/animate';
 	import TokenCardWithUrl from '$lib/components/tokens/TokenCardWithUrl.svelte';
 	import TokenCardContent from '$lib/components/tokens/TokenCardContent.svelte';
-	import { balancesStore } from '$lib/stores/balances.store';
-	import { BigNumber } from '@ethersproject/bignumber';
-	import { pointerEventStore } from '$lib/stores/events.store';
-	import { pointerEventsHandler } from '$lib/utils/events.utils';
 	import { debounce, isNullish } from '@dfinity/utils';
 	import { erc20UserTokensNotInitialized } from '$eth/derived/erc20.derived';
+	import TokensDisplayHandler from '$lib/components/tokens/TokensDisplayHandler.svelte';
 
-	let displayZeroBalance: boolean;
-	$: displayZeroBalance = $hideZeroBalancesStore?.enabled !== true;
-
-	let tokens: TokenUi[];
-	$: tokens = $combinedDerivedSortedNetworkTokensUi.filter(
-		({ id: tokenId }) =>
-			($balancesStore?.[tokenId]?.data ?? BigNumber.from(0n)).gt(0n) || displayZeroBalance
-	);
-
-	// We start it as undefined to avoid showing an empty list before the first update.
-	let tokensToDisplay: Token[] | undefined = undefined;
-
-	const parseTokenKey = (token: Token) => `${token.id.description}-${token.network.id.description}`;
-
-	let tokenKeys: string;
-	$: tokenKeys = tokens.map(parseTokenKey).join(',');
-
-	let tokensToDisplayKeys: string;
-	$: tokensToDisplayKeys = tokensToDisplay?.map(parseTokenKey).join(',') ?? '';
-
-	const defineTokensToDisplay = (): Token[] | undefined => {
-		if (!$pointerEventStore) {
-			// No pointer events, so we are not worried about possible glitches on user's interaction.
-			return tokens;
-		}
-
-		// If there are pointer events, we need to avoid visually re-sorting the tokens, to prevent glitches on user's interaction.
-
-		if (tokenKeys === tokensToDisplayKeys) {
-			// The order is the same, so there will be no re-sorting and no possible glitches on user's interaction.
-			// However, we need to update the tokensToDisplay to make sure the balances are up-to-date.
-			return tokens;
-		}
-
-		// The order is not the same, so the list is the same as the one currently showed, but the balances are updated.
-		const tokenMap = new Map(tokens.map((token) => [parseTokenKey(token), token]));
-
-		return tokensToDisplay?.map((token) => tokenMap.get(parseTokenKey(token)) ?? token) ?? [];
-	};
-
-	const updateTokensToDisplay = () => (tokensToDisplay = defineTokensToDisplay());
-
-	const debounceUpdateTokensToDisplay = debounce(updateTokensToDisplay, 500);
-
-	$: $pointerEventStore, tokens, debounceUpdateTokensToDisplay();
+	let tokens: TokenUi[] | undefined;
 
 	let animating = false;
 
@@ -83,12 +34,12 @@
 	}, 250);
 
 	let loading: boolean;
-	$: loading = $erc20UserTokensNotInitialized;
+	$: loading = $erc20UserTokensNotInitialized || isNullish(tokens);
 </script>
 
 <TokensSkeletons {loading}>
-	<div use:pointerEventsHandler>
-		{#each tokensToDisplay ?? [] as token (token.id)}
+	<TokensDisplayHandler bind:tokens>
+		{#each tokens ?? [] as token (token.id)}
 			<div
 				transition:fade
 				animate:flip={{ duration: 250 }}
@@ -103,9 +54,9 @@
 				</Listener>
 			</div>
 		{/each}
-	</div>
+	</TokensDisplayHandler>
 
-	{#if tokensToDisplay?.length === 0}
+	{#if tokens?.length === 0}
 		<p class="mt-4 text-dark opacity-50">{$i18n.tokens.text.all_tokens_with_zero_hidden}</p>
 	{/if}
 
