39ccd3baca1ba71d3e07986910a55e5eec362402
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-08-28 14:00:34 +0200

    feat(frontend): create store for tokens keys

--- src/frontend/src/lib/components/tokens/TokensDisplayHandler.svelte
+++ src/frontend/src/lib/components/tokens/TokensDisplayHandler.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	import type { Token, TokenUi } from '$lib/types/token';
+	import type { Token, TokenIndexKey, TokenUi } from '$lib/types/token';
 	import { pointerEventStore } from '$lib/stores/events.store';
 	import { pointerEventsHandler } from '$lib/utils/events.utils';
 	import { debounce } from '@dfinity/utils';
 	import { hideZeroBalancesStore } from '$lib/stores/settings.store';
 	import { combinedDerivedSortedNetworkTokensUi } from '$lib/derived/network-tokens.derived';
+	import { tokensUiStore } from '$lib/stores/tokens-ui.store';
 
 	// We start it as undefined to avoid showing an empty list before the first update.
 	export let tokens: Token[] | undefined = undefined;
@@ -19,22 +20,23 @@
 
 	const parseTokenKey = (token: Token) => `${token.id.description}-${token.network.id.description}`;
 
-	let tokenKeys: string;
-	$: tokenKeys = tokens?.map(parseTokenKey).join(',') ?? '';
-
-	let sortedTokensKeys: string;
-	$: sortedTokensKeys = sortedTokens?.map(parseTokenKey).join(',') ?? '';
+	let sortedTokensKeys: TokenIndexKey[];
+	$: sortedTokensKeys = sortedTokens?.map(parseTokenKey);
 
 	const updateTokensToDisplay = () => {
 		if (!$pointerEventStore) {
 			// No pointer events, so we are not worried about possible glitches on user's interaction.
+			tokensUiStore.set({ tokensKeys: sortedTokensKeys });
 			tokens = sortedTokens;
 			return;
 		}
 
 		// If there are pointer events, we need to avoid visually re-sorting the tokens, to prevent glitches on user's interaction.
 
-		if (tokenKeys === sortedTokensKeys) {
+		// We are imperatively getting the current tokens keys, as it should not trigger any reactivity.
+		const currentTokensKeys: TokenIndexKey[] = $tokensUiStore.tokensKeys;
+
+		if (currentTokensKeys.join(',') === sortedTokensKeys.join(',')) {
 			// The order is the same, so there will be no re-sorting and no possible glitches on user's interaction.
 			// However, we need to update the tokensToDisplay to make sure the balances are up-to-date.
 			tokens = sortedTokens;
