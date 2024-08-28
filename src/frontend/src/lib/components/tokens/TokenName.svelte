ba685bcf861dbfb2fc8c15470be820a3eecf0dc2
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-08-28 07:34:58 +0200

    feat(frontend): add prefix tag to tokens in ManageModal (#2147)
    # Motivation
    
    To be consistent, we add the `ck` tag in the token list of the
    `ManageModal`.
    
    # Changes
    
    - Separate component specific for the `TokenName`.
    - Use such component in `ManageModal`.
    
    # Tests
    
    ### Manage modal
    
    <img width="517" alt="Screenshot 2024-08-27 at 20 16 03"
    src="https://github.com/user-attachments/assets/309d7fd1-e6ec-4c39-9a8b-c98ba7373b1f">
    
    ### Token list (unchanged)
    
    <img width="669" alt="Screenshot 2024-08-27 at 20 16 09"
    src="https://github.com/user-attachments/assets/af4ba3b2-5ee6-410a-b21d-d8c13cc17020">

---
+++ src/frontend/src/lib/components/tokens/TokenName.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { Token } from '$lib/types/token';
+	import { nonNullish } from '@dfinity/utils';
+	import Tag from '$lib/components/ui/Tag.svelte';
+
+	export let token: Token;
+
+	const ariaLabel = nonNullish(token.oisyName)
+		? `${token.oisyName.prefix ?? ''}${token.oisyName.oisyName}`
+		: token.name;
+</script>
+
+<span aria-label={ariaLabel}>
+	{#if nonNullish(token.oisyName?.prefix)}
+		<Tag ariaHidden>{token.oisyName.prefix}</Tag>
+	{/if}
+	<span aria-hidden="true">{token.oisyName?.oisyName ?? token.name}</span>
+</span>
