e956b85aba16241feacbc72a3e52629b4d8001d9
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-08-30 15:06:52 +0200

    revert: delete unrelated components

--- src/frontend/src/lib/stores/tokens-ui.store.ts
+++
@@ -1,8 +0,0 @@
-// import type { TokenIndexKey } from '$lib/types/token';
-// import { writable } from 'svelte/store';
-//
-// export interface TokensUiData {
-// 	tokensKeys: TokenIndexKey[];
-// }
-//
-// export const tokensUiStore = writable<TokensUiData>({ tokensKeys: [] });
