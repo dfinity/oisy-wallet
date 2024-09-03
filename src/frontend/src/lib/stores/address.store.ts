2b6ab9ee3c4d31a229a8b66257a8adf2ad9bebb6
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-03 11:54:47 +0200

    revert: change the name of the new type back

--- src/frontend/src/lib/stores/address.store.ts
+++ src/frontend/src/lib/stores/address.store.ts
@@ -1,8 +1,11 @@
+import type { StorageStoreData } from '$lib/stores/storage.store';
 import type { Address, BtcAddress, EthAddress } from '$lib/types/address';
 import type { CertifiedData } from '$lib/types/store';
 import { writable, type Readable } from 'svelte/store';
 
-type AddressData<T extends Address> = CertifiedData<T>;
+type AddressData<T extends Address = Address> = CertifiedData<T>;
+
+export type OptionAddressData = StorageStoreData<AddressData>;
 
 export type OptionCertifiedAddressData<T extends Address> = AddressData<T> | undefined | null;
 
--- src/frontend/src/lib/utils/address.utils.ts
+++ src/frontend/src/lib/utils/address.utils.ts
@@ -1,6 +1,5 @@
-import type { OptionCertifiedAddressData } from '$lib/stores/address.store';
+import type { OptionAddressData } from '$lib/stores/address.store';
 import type { Address, OptionAddress } from '$lib/types/address';
 
-export const mapAddress = <T extends Address>(
-	$addressStore: OptionCertifiedAddressData<T>
-): OptionAddress<T> => ($addressStore === null ? null : $addressStore?.data);
+export const mapAddress = ($addressStore: OptionAddressData): OptionAddress<Address> =>
+	$addressStore === null ? null : $addressStore?.data;
