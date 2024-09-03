fc6734596c35408c917fa6e74b7e4b70daaf7b7a
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-03 16:06:35 +0200

    refactor(frontend): remove redundant address data type

--- src/frontend/src/lib/stores/address.store.ts
+++ src/frontend/src/lib/stores/address.store.ts
@@ -3,19 +3,17 @@ import type { Address, BtcAddress, EthAddress } from '$lib/types/address';
 import type { CertifiedData } from '$lib/types/store';
 import { writable, type Readable } from 'svelte/store';
 
-type AddressData<T extends Address = Address> = CertifiedData<T>;
+type AddressData<T extends Address> = CertifiedData<T>;
 
-export type OptionAddressData = StorageStoreData<AddressData>;
+export type OptionAddressData<T extends Address> = StorageStoreData<AddressData<T>>;
 
-export type OptionCertifiedAddressData<T extends Address> = AddressData<T> | undefined | null;
-
-export interface AddressStore<T extends Address> extends Readable<OptionCertifiedAddressData<T>> {
+export interface AddressStore<T extends Address> extends Readable<OptionAddressData<T>> {
 	set: (data: AddressData<T>) => void;
 	reset: () => void;
 }
 
 const initAddressStore = <T extends Address>(): AddressStore<T> => {
-	const { subscribe, set } = writable<OptionCertifiedAddressData<T>>(undefined);
+	const { subscribe, set } = writable<OptionAddressData<T>>(undefined);
 
 	return {
 		set: (data: AddressData<T>) => set(data),
