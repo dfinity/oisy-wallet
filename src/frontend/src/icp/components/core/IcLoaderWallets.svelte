ac91812c9a37fe469d3628ea9cbe04e6e0941c0b
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-03 13:38:13 +0200

    refactor: separate util functions

--- src/frontend/src/icp/components/core/IcLoaderWallets.svelte
+++ src/frontend/src/icp/components/core/IcLoaderWallets.svelte
@@ -1,52 +1,19 @@
 <script lang="ts">
 	import { onDestroy } from 'svelte';
-	import { initIcrcWalletWorker } from '$icp/services/worker.icrc-wallet.services';
-	import { initIcpWalletWorker } from '$icp/services/worker.icp-wallet.services';
 	import type { WalletWorker } from '$icp/types/ic-listener';
 	import { enabledIcNetworkTokens } from '$lib/derived/network-tokens.derived';
 	import type { TokenId } from '$lib/types/token';
-	import { debounce, nonNullish } from '@dfinity/utils';
-	import { toastsError } from '$lib/stores/toasts.store';
-	import { i18n } from '$lib/stores/i18n.store';
-	import { replacePlaceholders } from '$lib/utils/i18n.utils';
+	import { debounce } from '@dfinity/utils';
+	import { cleanWorkers, loadWorker } from '$icp/utils/ic-wallet.utils';
+	import type { IcToken } from '$icp/types/ic';
 
 	const workers: Map<TokenId, WalletWorker> = new Map<TokenId, WalletWorker>();
 
 	const manageWorkers = async () => {
-		const unusedWorkers: TokenId[] = Array.from(workers.keys()).filter(
-			(tokenId) => !$enabledIcNetworkTokens.some(({ id: icTokenId }) => icTokenId === tokenId)
-		);
-
-		unusedWorkers.forEach((tokenId) => {
-			workers.get(tokenId)?.stop();
-			workers.delete(tokenId);
-		});
+		cleanWorkers({ workers, tokens: $enabledIcNetworkTokens });
 
 		await Promise.allSettled(
-			$enabledIcNetworkTokens.map(async (token) => {
-				if (!workers.has(token.id)) {
-					try {
-						const worker = await (token.standard === 'icrc'
-							? initIcrcWalletWorker(token)
-							: initIcpWalletWorker());
-
-						worker.stop();
-						worker.start();
-
-						workers.set(token.id, worker);
-					} catch (err: unknown) {
-						// We show the error to the user, but we keep loading the rest of the workers
-						toastsError({
-							msg: {
-								text: replacePlaceholders($i18n.tokens.error.loading_ic_wallet, {
-									$symbol: token.symbol
-								})
-							},
-							err
-						});
-					}
-				}
-			})
+			$enabledIcNetworkTokens.map(async (token: IcToken) => await loadWorker({ workers, token }))
 		);
 	};
 
--- src/frontend/src/icp/utils/ic-wallet.utils.ts
+++ src/frontend/src/icp/utils/ic-wallet.utils.ts
@@ -1,6 +1,15 @@
 import { INDEX_RELOAD_DELAY } from '$icp/constants/ic.constants';
+import { initIcpWalletWorker } from '$icp/services/worker.icp-wallet.services';
+import { initIcrcWalletWorker } from '$icp/services/worker.icrc-wallet.services';
+import type { IcToken } from '$icp/types/ic';
+import type { WalletWorker } from '$icp/types/ic-listener';
+import { i18n } from '$lib/stores/i18n.store';
+import { toastsError } from '$lib/stores/toasts.store';
+import type { TokenId } from '$lib/types/token';
 import { emit } from '$lib/utils/events.utils';
+import { replacePlaceholders } from '$lib/utils/i18n.utils';
 import { waitForMilliseconds } from '$lib/utils/timeout.utils';
+import { get } from 'svelte/store';
 
 /**
  * Wait few seconds and trigger the wallet to fetch optimistically new transactions twice.
@@ -13,4 +22,61 @@ export const waitAndTriggerWallet = async () => {
 
 	// In case the best case scenario was not met, we optimistically try to retrieve the transactions on more time given that we generally retrieve transactions every WALLET_TIMER_INTERVAL_MILLIS seconds without blocking the UI.
 	waitForMilliseconds(INDEX_RELOAD_DELAY).then(() => emit({ message: 'oisyTriggerWallet' }));
+};
+
+/**
+ * Clean the workers that are not used anymore based on a given list of tokens.
+ *
+ * @param workers The map of workers defined as `Map<TokenId, WalletWorker>`.
+ * @param tokens The list of tokens to check against the workers.
+ */
+export const cleanWorkers = ({
+	workers,
+	tokens
+}: {
+	workers: Map<TokenId, WalletWorker>;
+	tokens: IcToken[];
+}) =>
+	Array.from(workers.keys())
+		.filter((tokenId) => !tokens.some(({ id: icTokenId }) => icTokenId === tokenId))
+		.forEach((tokenId) => {
+			workers.get(tokenId)?.stop();
+			workers.delete(tokenId);
+		});
+
+/**
+ * Load the worker for the token if it is not already loaded.
+ *
+ * @param workers The map of workers defined as `Map<TokenId, WalletWorker>`.
+ * @param token The IC token to load the worker for.
+ */
+export const loadWorker = async ({
+	workers,
+	token
+}: {
+	workers: Map<TokenId, WalletWorker>;
+	token: IcToken;
+}) => {
+	if (!workers.has(token.id)) {
+		try {
+			const worker = await (token.standard === 'icrc'
+				? initIcrcWalletWorker(token)
+				: initIcpWalletWorker());
+
+			worker.stop();
+			worker.start();
+
+			workers.set(token.id, worker);
+		} catch (err: unknown) {
+			// We show the error to the user, but we keep loading the rest of the workers
+			toastsError({
+				msg: {
+					text: replacePlaceholders(get(i18n).tokens.error.loading_ic_wallet, {
+						$symbol: token.symbol
+					})
+				},
+				err
+			});
+		}
+	}
 };
