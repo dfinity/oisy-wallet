98df216054b88bf82c42d8027b58d51fd1013af8
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-03 21:37:30 +0200

    refactor(frontend): extract validateAddress util

--- src/frontend/src/lib/components/guard/AddressGuard.svelte
+++ src/frontend/src/lib/components/guard/AddressGuard.svelte
@@ -1,31 +1,16 @@
 <script lang="ts">
 	import { ethAddressStore } from '$lib/stores/address.store';
-	import { isNullish } from '@dfinity/utils';
 	import { certifyEthAddress } from '$lib/services/address.services';
-	import { warnSignOut } from '$lib/services/auth.services';
+	import { validateAddress } from '$lib/utils/address.utils';
+	import type { EthAddress } from '$lib/types/address';
 
-	const validateAddress = async () => {
-		if (isNullish($ethAddressStore)) {
-			// No address is loaded, we don't have to verify it
-			return;
-		}
+	const validateEthAddress = async () =>
+		await validateAddress<EthAddress>({
+			$addressStore: $ethAddressStore,
+			certifyAddress: certifyEthAddress
+		});
 
-		if ($ethAddressStore.certified === true) {
-			// The address is certified, all good
-			return;
-		}
-
-		const { success, err } = await certifyEthAddress($ethAddressStore.data);
-
-		if (success) {
-			// The address is valid
-			return;
-		}
-
-		await warnSignOut(err ?? 'Error while certifying your address');
-	};
-
-	$: $ethAddressStore, (async () => await validateAddress())();
+	$: $ethAddressStore, validateEthAddress();
 </script>
 
 <slot />
