fd51a92519b27bffd521a8d88638e2ac908f1e00
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-09-03 16:01:36 +0200

    refactor(frontend): use split address stores (#2209)
    # Motivation
    
    As discussed in PR #2067 and after PR #2207, we are going to use the
    split address stores for BTC mainnet and ETH .
    
    # Changes
    
    - Change derived store to use the split stores directly:
    `ethAddressStore` and `btcAddressMainnetStore`.
    - In `AddressGuard`, use the `ethAddressStore` directly.
    - Adjust address services to receive the specific store to set for each
    token.
    - Remove deprecated types and derived stores.
    
    # Tests
    
    No change in local replica.
    
    ---------
    
    Co-authored-by: David Dal Busco <david.dalbusco@dfinity.org>

--- src/frontend/src/lib/utils/address.utils.ts
+++ src/frontend/src/lib/utils/address.utils.ts
@@ -1,5 +1,5 @@
 import type { OptionAddressData } from '$lib/stores/address.store';
 import type { Address, OptionAddress } from '$lib/types/address';
 
-export const mapAddress = ($addressData: OptionAddressData): OptionAddress<Address> =>
-	$addressData === null ? null : $addressData?.data;
+export const mapAddress = ($addressStore: OptionAddressData): OptionAddress<Address> =>
+	$addressStore === null ? null : $addressStore?.data;
