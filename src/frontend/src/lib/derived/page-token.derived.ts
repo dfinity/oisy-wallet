30be990ef240e123c302ab4fdbfbe0c24a05c8fc
Llorenç Muntaner <llorenc.muntaner@gmail.com>
2024-09-04 09:09:08 +0200

    feat(frontend): Add missing bitcoin in stores (#2214)
    # Motivation
    
    The transactions page for Bitcoin is not showing balance, nor logo, nor
    nice background.
    
    One error is that it doesn't find any token in the pageToken derived
    store and it's rendering the default ETH transactions component in
    `TransactionsSignedIn`.
    
    In this PR, I add a new derived store to be used in
    `TransactionsSignedIn` and fix the pageToken derived store to include
    bitcoin tokens.
    
    # Changes
    
    * New derived store `networkBitcoin`.
    * Add bitcoin tokens to `pageToken` derived store.
    
    # Tests
    
    Tested that the logo appears in the transactions page.

--- src/frontend/src/lib/derived/page-token.derived.ts
+++ src/frontend/src/lib/derived/page-token.derived.ts
@@ -1,3 +1,4 @@
+import { enabledBitcoinTokens } from '$btc/derived/tokens.derived';
 import { ETHEREUM_TOKEN, ICP_TOKEN, SEPOLIA_TOKEN } from '$env/tokens.env';
 import { enabledErc20Tokens } from '$eth/derived/erc20.derived';
 import { enabledIcrcTokens } from '$icp/derived/icrc.derived';
@@ -10,8 +11,8 @@ import { derived, type Readable } from 'svelte/store';
  * A token derived from the route URL - i.e. if the URL contains a query parameters "token", then this store tries to derive the object from it.
  */
 export const pageToken: Readable<OptionToken> = derived(
-	[routeToken, enabledErc20Tokens, enabledIcrcTokens],
-	([$routeToken, $erc20Tokens, $icrcTokens]) => {
+	[routeToken, enabledBitcoinTokens, enabledErc20Tokens, enabledIcrcTokens],
+	([$routeToken, $enabledBitcoinTokens, $erc20Tokens, $icrcTokens]) => {
 		if (isNullish($routeToken)) {
 			return undefined;
 		}
@@ -20,8 +21,12 @@ export const pageToken: Readable<OptionToken> = derived(
 			return ICP_TOKEN;
 		}
 
-		return [...$erc20Tokens, ...$icrcTokens, ETHEREUM_TOKEN, SEPOLIA_TOKEN].find(
-			({ name }) => name === $routeToken
-		);
+		return [
+			...$enabledBitcoinTokens,
+			...$erc20Tokens,
+			...$icrcTokens,
+			ETHEREUM_TOKEN,
+			SEPOLIA_TOKEN
+		].find(({ name }) => name === $routeToken);
 	}
 );
