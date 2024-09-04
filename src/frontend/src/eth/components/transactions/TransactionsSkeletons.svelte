dfc2a7174dc12402156874b3d063cdd123ace1c5
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-04 19:39:26 +0200

    lint: fix rules

--- src/frontend/src/eth/components/transactions/TransactionsSkeletons.svelte
+++ src/frontend/src/eth/components/transactions/TransactionsSkeletons.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { fade } from 'svelte/transition';
-	import SkeletonCards from '$lib/components/ui/SkeletonCards.svelte';
-	import { transactionsNotInitialized } from '$eth/derived/transactions.derived';
 	import { erc20UserTokensNotInitialized } from '$eth/derived/erc20.derived';
 	import { tokenNotInitialized } from '$eth/derived/nav.derived';
+	import { transactionsNotInitialized } from '$eth/derived/transactions.derived';
+	import SkeletonCards from '$lib/components/ui/SkeletonCards.svelte';
 </script>
 
 {#if ($erc20UserTokensNotInitialized && $tokenNotInitialized) || $transactionsNotInitialized}
