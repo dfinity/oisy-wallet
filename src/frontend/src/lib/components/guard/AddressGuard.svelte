87f61b9dad3a3e458fec94cdfb74148a6e17a3fe
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-09-04 18:41:35 +0200

    feat(frontend): add BTC verification to AddressGuard (#2226)
    # Motivation
    
    We include the BTC address verification in the `AddressGuard` component.

--- src/frontend/src/lib/components/guard/AddressGuard.svelte
+++ src/frontend/src/lib/components/guard/AddressGuard.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
-	import { ethAddressStore } from '$lib/stores/address.store';
-	import { validateEthAddress } from '$lib/services/address.services';
+	import { btcAddressMainnetStore, ethAddressStore } from '$lib/stores/address.store';
+	import { validateBtcAddressMainnet, validateEthAddress } from '$lib/services/address.services';
+	import { NETWORK_BITCOIN_ENABLED } from '$env/networks.btc.env';
+
+	$: $btcAddressMainnetStore,
+		(async () =>
+			NETWORK_BITCOIN_ENABLED
+				? await validateBtcAddressMainnet($btcAddressMainnetStore)
+				: await Promise.resolve())();
 
 	$: $ethAddressStore, (async () => await validateEthAddress($ethAddressStore))();
 </script>
