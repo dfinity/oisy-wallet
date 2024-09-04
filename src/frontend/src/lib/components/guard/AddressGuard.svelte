20fd24ce93444291b58519b81f8e8704b4d2a337
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-04 14:10:58 +0200

    feat(frontend): add BTC verification to AddressGuard

--- src/frontend/src/lib/components/guard/AddressGuard.svelte
+++ src/frontend/src/lib/components/guard/AddressGuard.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-	import { ethAddressStore } from '$lib/stores/address.store';
-	import { validateEthAddress } from '$lib/services/address.services';
+	import { btcAddressMainnetStore, ethAddressStore } from '$lib/stores/address.store';
+	import { validateBtcAddressMainnet, validateEthAddress } from '$lib/services/address.services';
+
+	$: $btcAddressMainnetStore, (async () => await validateBtcAddressMainnet($btcAddressMainnetStore))();
 
 	$: $ethAddressStore, (async () => await validateEthAddress($ethAddressStore))();
 </script>
