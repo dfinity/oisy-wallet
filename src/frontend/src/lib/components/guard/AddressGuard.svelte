b172cf1b19d8c30f69812723049fd643023ae80e
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-04 15:59:03 +0200

    fix: include BTC flag

--- src/frontend/src/lib/components/guard/AddressGuard.svelte
+++ src/frontend/src/lib/components/guard/AddressGuard.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import { btcAddressMainnetStore, ethAddressStore } from '$lib/stores/address.store';
 	import { validateBtcAddressMainnet, validateEthAddress } from '$lib/services/address.services';
+	import { NETWORK_BITCOIN_ENABLED } from '$env/networks.btc.env';
 
 	$: $btcAddressMainnetStore,
-		(async () => await validateBtcAddressMainnet($btcAddressMainnetStore))();
+		(async () =>
+			NETWORK_BITCOIN_ENABLED
+				? await validateBtcAddressMainnet($btcAddressMainnetStore)
+				: await Promise.resolve())();
 
 	$: $ethAddressStore, (async () => await validateEthAddress($ethAddressStore))();
 </script>
