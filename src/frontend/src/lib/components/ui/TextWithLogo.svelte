2c2bfcb19ec8a31433edbf182ae2408907f1489b
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-09-04 12:06:59 +0200

    feat(frontend): UI for chain selector with USD values (#2219)
    # Motivation
    
    Step 2 of updating the chain selector with aggregate USD balance values:
    implement UI component to display text, description and logo according
    to the provided design. Next step is to calculate and pass balance
    values to that component for all non-test chains.
    
    Note: potentially, we could extend `TextWithLogo` to accept
    `description`, but I decided against it in order to avoid even more
    conditional things inside that component. Please let me know if you
    think otherwise.
    
    Browser:
    <img width="305" alt="web"
    src="https://github.com/user-attachments/assets/0f93eec3-2e19-4368-841f-e0982d84620d">
    
    Figma:
    <img width="425" alt="Figma"
    src="https://github.com/user-attachments/assets/251a3674-54f1-4ef8-a012-b79f34994185">

--- src/frontend/src/lib/components/ui/TextWithLogo.svelte
+++ src/frontend/src/lib/components/ui/TextWithLogo.svelte
@@ -2,18 +2,26 @@
 	import { replacePlaceholders } from '$lib/utils/i18n.utils';
 	import { i18n } from '$lib/stores/i18n.store';
 	import Logo from '$lib/components/ui/Logo.svelte';
+	import { nonNullish } from '@dfinity/utils';
 
 	export let name: string;
 	export let icon: string | undefined;
 	export let logo: 'start' | 'end' = 'end';
+	export let description: string | undefined = undefined;
 </script>
 
 <span
-	class="flex items-center"
+	class="flex"
+	class:items-center={!description}
 	class:gap-2={logo === 'start'}
 	class:gap-1={logo === 'end'}
 	class:flex-row-reverse={logo === 'start'}
 >
-	<span>{name}</span>
+	<span class="flex flex-col gap-0.5">
+		<span class="leading-5">{name}</span>
+		{#if nonNullish(description)}
+			<span class="leading-none text-xs text-misty-rose text-left">{description}</span>
+		{/if}
+	</span>
 	<Logo src={icon} alt={replacePlaceholders($i18n.core.alt.logo, { $name: name })} />
 </span>
