4c2d85dc346f1ee771a0cdcbbe41a889302d0069
Llorenç <llorenc.muntaner@dfinity.org>
2024-09-04 14:27:26 +0200

    Improve dfx script and add section to hacking

--- scripts/dfx.start-with-bitcoin.sh
+++ scripts/dfx.start-with-bitcoin.sh
@@ -3,5 +3,25 @@
 # This script assumes a local Regtest Bitcoin Core node is running.
 # You can run one by executing the `setup.bitcoin-node.sh` script.
 
-# TODO: Remove `--clean`
-dfx start --enable-bitcoin --bitcoin-node "127.0.0.1:18444" --clean
+# Initialize a flag for the --reset option
+clean_flag=false
+
+# Loop through the arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --clean)
+            clean_flag=true  # Set the reset flag to true if --reset is provided
+            shift            # Move to the next argument
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+if [ "$clean_flag" = true ]; then
+    dfx start --enable-bitcoin --bitcoin-node "127.0.0.1:18444" --clean
+else
+    dfx start --enable-bitcoin --bitcoin-node "127.0.0.1:18444"
+fi
--- scripts/reset.bitcoin-node.sh
+++
@@ -1,5 +0,0 @@
-BITCOIN_DIR="bitcoin-core"
-DATA_DIR="$BITCOIN_DIR/data"
-
-rm -r $DATA_DIR
-mkdir -p $DATA_DIR
