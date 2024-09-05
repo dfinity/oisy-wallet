853e3a533f1cd5eef400489b80d033a1241946fa
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-09-05 12:50:59 +0200

    feat(frontend): update network-tokens to enabled tokens balance (#2233)
    # Motivation
    
    Step 3 of updating the chain selector with aggregate USD balance values:
    adjust `network-tokens.derived` to load enabled tokens balance
    regardless if a network selected by user or not. We discussed with
    @AntonioVentilii-DFINITY the performance aspect of this change, and
    since it will be basically the amount of data loaded (and hence time
    needed to load it) as if we were in Chain Fusion, it should not affect
    the user experience and performance.

--- src/frontend/src/lib/derived/network-tokens.derived.ts
+++ src/frontend/src/lib/derived/network-tokens.derived.ts
@@ -1,8 +1,6 @@
-import type { Erc20Token } from '$eth/types/erc20';
-import type { IcToken } from '$icp/types/ic';
 import { exchanges } from '$lib/derived/exchange.derived';
 import { pseudoNetworkChainFusion, selectedNetwork } from '$lib/derived/network.derived';
-import { tokens, tokensToPin } from '$lib/derived/tokens.derived';
+import { enabledTokens, tokensToPin } from '$lib/derived/tokens.derived';
 import { balancesStore } from '$lib/stores/balances.store';
 import type { Token, TokenUi } from '$lib/types/token';
 import { filterTokensForSelectedNetwork } from '$lib/utils/network.utils';
@@ -10,37 +8,11 @@ import { pinTokensWithBalanceAtTop, sortTokens } from '$lib/utils/tokens.utils';
 import { derived, type Readable } from 'svelte/store';
 
 /**
- * All tokens matching the selected network or chain fusion, regardless if they are enabled by the user or not.
+ * All user-enabled tokens matching the selected network or chain fusion.
  */
-const networkTokens: Readable<Token[]> = derived(
-	[tokens, selectedNetwork, pseudoNetworkChainFusion],
+const enabledNetworkTokens: Readable<Token[]> = derived(
+	[enabledTokens, selectedNetwork, pseudoNetworkChainFusion],
 	filterTokensForSelectedNetwork
-);
-
-const enabledNetworkTokens: Readable<Token[]> = derived([networkTokens], ([$networkTokens]) =>
-	$networkTokens.filter((token) => ('enabled' in token ? token.enabled : true))
-);
-
-/**
- * It isn't performant to post filter again the Erc20 tokens that are enabled for the specific selected network or no network selected but, it's code wise convenient to avoid duplication of logic.
- */
-export const enabledErc20NetworkTokens: Readable<Erc20Token[]> = derived(
-	[enabledNetworkTokens],
-	([$enabledNetworkTokens]) =>
-		$enabledNetworkTokens.filter(({ standard }) => standard === 'erc20') as Erc20Token[]
-);
-
-/**
- * The following store is use as reference for the list of WalletWorkers that are started/stopped in the main token page.
- */
-// TODO: The several dependencies of enabledIcNetworkTokens are not strictly only IC tokens, but other tokens too.
-//  We should find a better way to handle this, improving the store.
-export const enabledIcNetworkTokens: Readable<IcToken[]> = derived(
-	[enabledNetworkTokens],
-	([$enabledNetworkTokens]) =>
-		$enabledNetworkTokens.filter(
-			({ standard }) => standard === 'icp' || standard === 'icrc'
-		) as IcToken[]
 );
 
 /**
