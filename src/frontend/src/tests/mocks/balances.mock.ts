e4aa5370027aa494e428a91ce35988ae440a4a71
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-06 11:55:44 +0200

    refactor(frontend): move test mock values to specific folders

---
+++ src/frontend/src/tests/mocks/balances.mock.ts
@@ -0,0 +1,17 @@
+import { BTC_MAINNET_TOKEN } from '$env/tokens.btc.env';
+import { ETHEREUM_TOKEN, ICP_TOKEN } from '$env/tokens.env';
+import type { BalancesData } from '$lib/stores/balances.store';
+import type { CertifiedStoreData } from '$lib/stores/certified.store';
+import { BigNumber } from 'alchemy-sdk';
+
+export const certified = true;
+
+export const bn1 = BigNumber.from(1n);
+export const bn2 = BigNumber.from(2n);
+export const bn3 = BigNumber.from(3n);
+
+export const $balances: CertifiedStoreData<BalancesData> = {
+	[ICP_TOKEN.id]: { data: bn1, certified },
+	[BTC_MAINNET_TOKEN.id]: { data: bn2, certified },
+	[ETHEREUM_TOKEN.id]: { data: bn3, certified }
+};
