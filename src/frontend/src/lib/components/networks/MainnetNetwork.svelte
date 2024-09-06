43f2779ba2e20639c4c6c9503243e4459ad67f85
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2024-09-06 16:14:08 +0200

    feat(tokens): display mainnet tokens total USD

---
+++ src/frontend/src/lib/components/networks/MainnetNetwork.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import NetworkComponent from '$lib/components/networks/Network.svelte';
+	import { enabledMainnetTokensUsdBalancesPerNetwork } from '$lib/derived/tokens.derived';
+	import type { Network } from '$lib/types/network';
+
+	export let network: Network;
+
+	let totalUsd: number;
+	$: totalUsd = $enabledMainnetTokensUsdBalancesPerNetwork[network.id];
+</script>
+
+<NetworkComponent {network} {totalUsd} on:icSelected />
