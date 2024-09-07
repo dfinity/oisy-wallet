0623bacab4e7364d54802ca808846e255038524a
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-07 08:04:24 +0200

    feat: store for show zero balances

--- src/frontend/src/lib/components/tokens/TokensDisplayHandler.svelte
+++ src/frontend/src/lib/components/tokens/TokensDisplayHandler.svelte
@@ -5,17 +5,14 @@
 	import { debounce } from '@dfinity/utils';
 	import { combinedDerivedSortedNetworkTokensUi } from '$lib/derived/network-tokens.derived';
 	import { defineTokensToDisplay } from '$lib/utils/tokens-ui.utils';
-	import { hideZeroBalances } from '$lib/derived/settings.derived';
+	import { showZeroBalances } from '$lib/derived/settings.derived';
 
 	// We start it as undefined to avoid showing an empty list before the first update.
 	export let tokens: Token[] | undefined = undefined;
 
-	let displayZeroBalance: boolean;
-	$: displayZeroBalance = !$hideZeroBalances;
-
 	let sortedTokens: TokenUi[];
 	$: sortedTokens = $combinedDerivedSortedNetworkTokensUi.filter(
-		({ balance, usdBalance }) => Number(balance ?? 0n) || (usdBalance ?? 0) || displayZeroBalance
+		({ balance, usdBalance }) => Number(balance ?? 0n) || (usdBalance ?? 0) || $showZeroBalances
 	);
 
 	const updateTokensToDisplay = () =>
--- src/frontend/src/lib/derived/settings.derived.ts
+++ src/frontend/src/lib/derived/settings.derived.ts
@@ -10,3 +10,8 @@ export const hideZeroBalances: Readable<boolean> = derived(
 	[hideZeroBalancesStore],
 	([$hideZeroBalancesStore]) => $hideZeroBalancesStore?.enabled ?? false
 );
+
+export const showZeroBalances: Readable<boolean> = derived(
+	[hideZeroBalances],
+	([$hideZeroBalances]) => !$hideZeroBalances
+);
