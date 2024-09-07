fd1cdae34d439495c1bb8a5193e7ad0877bff113
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-09-07 08:05:53 +0200

    feat(frontend): derived stores for settings (#2276)
    # Motivation
    
    To avoid repetition of code, we create derived stores for the cached
    settings.

--- src/frontend/src/lib/components/tokens/TokensDisplayHandler.svelte
+++ src/frontend/src/lib/components/tokens/TokensDisplayHandler.svelte
@@ -3,19 +3,16 @@
 	import { pointerEventStore } from '$lib/stores/events.store';
 	import { pointerEventsHandler } from '$lib/utils/events.utils';
 	import { debounce } from '@dfinity/utils';
-	import { hideZeroBalancesStore } from '$lib/stores/settings.store';
 	import { combinedDerivedSortedNetworkTokensUi } from '$lib/derived/network-tokens.derived';
 	import { defineTokensToDisplay } from '$lib/utils/tokens-ui.utils';
+	import { showZeroBalances } from '$lib/derived/settings.derived';
 
 	// We start it as undefined to avoid showing an empty list before the first update.
 	export let tokens: Token[] | undefined = undefined;
 
-	let displayZeroBalance: boolean;
-	$: displayZeroBalance = $hideZeroBalancesStore?.enabled !== true;
-
 	let sortedTokens: TokenUi[];
 	$: sortedTokens = $combinedDerivedSortedNetworkTokensUi.filter(
-		({ balance, usdBalance }) => Number(balance ?? 0n) || (usdBalance ?? 0) || displayZeroBalance
+		({ balance, usdBalance }) => Number(balance ?? 0n) || (usdBalance ?? 0) || $showZeroBalances
 	);
 
 	const updateTokensToDisplay = () =>
