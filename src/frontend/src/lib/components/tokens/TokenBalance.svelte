38471866961b3c182fed22f2ebe6124395fd4250
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-09 13:36:23 +0200

    feat(frontend): show skeleton when starting balance loader

--- src/frontend/src/lib/components/tokens/TokenBalance.svelte
+++ src/frontend/src/lib/components/tokens/TokenBalance.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import SkeletonTokenBalance from '$lib/components/tokens/SkeletonTokenBalance.svelte';
 	import { ZERO } from '$lib/constants/app.constants';
 	import type { TokenUi } from '$lib/types/token';
 	import { formatToken } from '$lib/utils/format.utils';
@@ -6,10 +7,12 @@
 	export let token: TokenUi;
 </script>
 
-<output class="break-all">
-	{formatToken({
-		value: token.balance ?? ZERO,
-		unitName: token.decimals
-	})}
-	{token.symbol}
-</output>
+<SkeletonTokenBalance {token}>
+	<output class="break-all">
+		{formatToken({
+			value: token.balance ?? ZERO,
+			unitName: token.decimals
+		})}
+		{token.symbol}
+	</output>
+</SkeletonTokenBalance>
