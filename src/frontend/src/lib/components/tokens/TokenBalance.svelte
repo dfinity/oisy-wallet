feb84792b083dd61455dd170610f92797b94410b
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-09-10 08:42:32 +0200

    feat(frontend): show skeleton when starting balance loader (#2290)
    # Motivation
    
    Since the `undefined` balance was introduced with PR #2281 for tokens
    that still don't have the balance loader working, we can show such
    information to the user visually: we provide a skeleton text until the
    balance is either some data or `null`, but, either way, until the
    balance loader exists.
    
    # Changes
    
    - Create component for token balance skeleton.
    - Add it to the Token card component.
    
    # Tests
    
    ### Normal speed
    
    
    https://github.com/user-attachments/assets/11b817af-76e1-41c4-b9bc-7d17ecb2032e
    
    ### Balance loader initiator is slowed down
    
    
    https://github.com/user-attachments/assets/527b0b3f-2b14-43de-a4c6-5166fbbd1a00

--- src/frontend/src/lib/components/tokens/TokenBalance.svelte
+++ src/frontend/src/lib/components/tokens/TokenBalance.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import TokenBalanceSkeleton from '$lib/components/tokens/TokenBalanceSkeleton.svelte';
 	import { ZERO } from '$lib/constants/app.constants';
 	import type { TokenUi } from '$lib/types/token';
 	import { formatToken } from '$lib/utils/format.utils';
@@ -6,10 +7,12 @@
 	export let token: TokenUi;
 </script>
 
-<output class="break-all">
-	{formatToken({
-		value: token.balance ?? ZERO,
-		unitName: token.decimals
-	})}
-	{token.symbol}
-</output>
+<TokenBalanceSkeleton {token}>
+	<output class="break-all">
+		{formatToken({
+			value: token.balance ?? ZERO,
+			unitName: token.decimals
+		})}
+		{token.symbol}
+	</output>
+</TokenBalanceSkeleton>
---
+++ src/frontend/src/lib/components/tokens/TokenBalanceSkeleton.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import SkeletonText from '$lib/components/ui/SkeletonText.svelte';
+	import type { TokenUi } from '$lib/types/token';
+
+	export let token: TokenUi;
+</script>
+
+{#if token.balance === undefined}
+	<span class="w-full max-w-[100px] mt-2"><SkeletonText /></span>
+{:else}
+	<slot />
+{/if}
