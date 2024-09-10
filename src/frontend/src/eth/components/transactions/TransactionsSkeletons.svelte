40303ac36a6a727a77ce3c2bdbae86ef6b7a6ba0
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-11 00:53:12 +0200

    feat(frontend): transactions awaits ck Ethereum minter info to initialize

--- src/frontend/src/eth/components/transactions/TransactionsSkeletons.svelte
+++ src/frontend/src/eth/components/transactions/TransactionsSkeletons.svelte
@@ -3,10 +3,11 @@
 	import { erc20UserTokensNotInitialized } from '$eth/derived/erc20.derived';
 	import { tokenNotInitialized } from '$eth/derived/nav.derived';
 	import { transactionsNotInitialized } from '$eth/derived/transactions.derived';
+	import { ckEthMinterInfoNotInitialized } from '$icp-eth/derived/cketh.derived';
 	import SkeletonCards from '$lib/components/ui/SkeletonCards.svelte';
 </script>
 
-{#if ($erc20UserTokensNotInitialized && $tokenNotInitialized) || $transactionsNotInitialized}
+{#if ($erc20UserTokensNotInitialized && $tokenNotInitialized) || $transactionsNotInitialized || $ckEthMinterInfoNotInitialized}
 	<SkeletonCards rows={5} />
 {:else}
 	<div in:fade>
