a1c788932b8b9961676bd0bdddf46404f9b851a9
David Dal Busco <david.dalbusco@outlook.com>
2024-09-10 06:41:34 +0200

    docs: review comment

--- src/frontend/src/routes/(sign)/sign/+page.ts
+++ src/frontend/src/routes/(sign)/sign/+page.ts
@@ -2,7 +2,7 @@ import type { RouteParams } from '$lib/utils/nav.utils';
 import type { LoadEvent } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-// We reset
+// We reset the data because the "sign" route operates without a network or token selected.
 export const load: PageLoad = (_$event: LoadEvent): RouteParams => ({
 	token: null,
 	network: null,
