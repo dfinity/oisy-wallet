468f583c8f6f7ee0e2f3ee5ebb97c7dc82a3e798
David Dal Busco <david.dalbusco@outlook.com>
2024-09-10 07:27:45 +0200

    feat: awaiting connection

---
+++ src/frontend/src/lib/components/signer/SignerIdle.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import IconInfinite from '$lib/components/icons/IconInfinite.svelte';
+</script>
+
+<h2 class="text-center mb-6">Awaiting connection...</h2>
+
+<div class="flex justify-center py-6">
+	<span class="relative flex w-16 aspect-square">
+		<span
+			class="animate-ping absolute inline-flex h-full w-full rounded-full bg-dodger-blue opacity-75"
+		></span>
+		<span
+			class="relative inline-flex items-center justify-center rounded-full h-16 w-16 bg-dodger-blue"
+			><IconInfinite /></span
+		>
+	</span>
+</div>
--- src/frontend/src/routes/(sign)/sign/+page.svelte
+++ src/frontend/src/routes/(sign)/sign/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import { authNotSignedIn } from '$lib/derived/auth.derived';
 	import SignerSignIn from '$lib/components/signer/SignerSignIn.svelte';
+	import SignerIdle from '$lib/components/signer/SignerIdle.svelte';
 </script>
 
 <article class="bg-white rounded-lg px-5 py-6">
 	{#if $authNotSignedIn}
 		<SignerSignIn />
 	{:else}
-		TODO: Awaiting connection...
+		<SignerIdle />
 	{/if}
 </article>
