f8d7dc43fb11eafaa1cb40f2b83b65ae9c22b0f1
David Dal Busco <david.dalbusco@outlook.com>
2024-09-10 08:38:30 +0200

    feat: connection

--- src/frontend/src/lib/components/signer/SignerIdle.svelte
+++ src/frontend/src/lib/components/signer/SignerIdle.svelte
@@ -2,7 +2,7 @@
 	import IconInfinite from '$lib/components/icons/IconInfinite.svelte';
 </script>
 
-<h2 class="text-center mb-6">Awaiting connection...</h2>
+<h2 class="text-center mb-6">Awaiting communication...</h2>
 
 <div class="flex justify-center py-6">
 	<span class="relative flex w-16 aspect-square">
--- src/frontend/src/routes/(sign)/sign/+page.svelte
+++ src/frontend/src/routes/(sign)/sign/+page.svelte
@@ -2,12 +2,47 @@
 	import { authNotSignedIn } from '$lib/derived/auth.derived';
 	import SignerSignIn from '$lib/components/signer/SignerSignIn.svelte';
 	import SignerIdle from '$lib/components/signer/SignerIdle.svelte';
+	import { Signer } from '@dfinity/oisy-wallet-signer/signer';
+	import { authStore } from '$lib/stores/auth.store';
+	import { isNullish } from '@dfinity/utils';
+	import { onDestroy } from 'svelte';
+	import { REPLICA_HOST } from '$lib/constants/app.constants';
+	import { fade } from 'svelte/transition';
+
+	let signer: Signer | undefined | null;
+
+	const reset = () => {
+		signer?.disconnect();
+		signer = null;
+	};
+
+	const init = async () => {
+		if (isNullish($authStore.identity)) {
+			reset();
+			return;
+		}
+
+		signer = Signer.init({
+			owner: $authStore.identity,
+			host: REPLICA_HOST
+		});
+	};
+
+	onDestroy(reset);
+
+	$: $authStore, (async () => init())();
+
+	let idle = true;
 </script>
 
 <article class="bg-white rounded-lg px-5 py-6">
 	{#if $authNotSignedIn}
 		<SignerSignIn />
+	{:else if idle}
+		<div in:fade>
+			<SignerIdle />
+		</div>
 	{:else}
-		<SignerIdle />
+		Doing something
 	{/if}
 </article>
