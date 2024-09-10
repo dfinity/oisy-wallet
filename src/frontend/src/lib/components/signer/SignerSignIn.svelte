4ec1216824aa27bd233af42bebe48b627fc310a5
David Dal Busco <david.dalbusco@outlook.com>
2024-09-10 06:51:58 +0200

    feat: sign in cmp

---
+++ src/frontend/src/lib/components/signer/SignerSignIn.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { IconWallet } from '@dfinity/gix-components';
+	import IconArrowAstronaut from '$lib/components/icons/IconArrowAstronaut.svelte';
+	import { signIn } from '$lib/services/auth.services';
+	import { i18n } from '$lib/stores/i18n.store';
+</script>
+
+<h2 class="text-center mb-6">Authenticate to connect</h2>
+
+<div class="bg-light-blue p-6 mb-6 rounded-lg">
+	<p class="break-normal font-bold">By connecting, the dApp will:</p>
+
+	<p class="break-normal pt-2.5 flex items-center gap-2">
+		<IconWallet /> Learn your Oisy wallet address
+	</p>
+
+	<p class="break-normal pt-2.5 flex items-center gap-2">
+		<IconWallet /> Initiate transaction to approve in Oisy
+	</p>
+</div>
+
+<button class="primary full center text-center" on:click={async () => await signIn({})}
+	>{$i18n.auth.text.authenticate} <IconArrowAstronaut /></button
+>
--- src/frontend/src/routes/(sign)/sign/+page.svelte
+++ src/frontend/src/routes/(sign)/sign/+page.svelte
@@ -1,26 +1,12 @@
 <script lang="ts">
-	import { IconWallet } from '@dfinity/gix-components';
-	import IconArrowAstronaut from '$lib/components/icons/IconArrowAstronaut.svelte';
-	import { signIn } from '$lib/services/auth.services';
-	import { i18n } from '$lib/stores/i18n.store';
+	import { authNotSignedIn } from '$lib/derived/auth.derived';
+	import SignerSignIn from '$lib/components/signer/SignerSignIn.svelte';
 </script>
 
 <article class="bg-white rounded-lg px-5 py-6">
-	<h2 class="text-center mb-6">Authenticate to connect</h2>
-
-	<div class="bg-light-blue p-6 mb-6 rounded-lg">
-		<p class="break-normal font-bold">By connecting, the dApp will:</p>
-
-		<p class="break-normal pt-2.5 flex items-center gap-2">
-			<IconWallet /> Learn your Oisy wallet address
-		</p>
-
-		<p class="break-normal pt-2.5 flex items-center gap-2">
-			<IconWallet /> Initiate transaction to approve in Oisy
-		</p>
-	</div>
-
-	<button class="primary full center text-center" on:click={async () => await signIn({})}
-		>{$i18n.auth.text.authenticate} <IconArrowAstronaut /></button
-	>
+	{#if $authNotSignedIn}
+		<SignerSignIn />
+	{:else}
+		TODO: Awaiting connection...
+	{/if}
 </article>
