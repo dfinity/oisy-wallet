5c5b60797c052c3c4c88342a51120fe47dfb9f55
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-11 23:39:56 +0200

    fix: use correct new type

--- src/frontend/src/eth/derived/wallet-connect.derived.ts
+++ src/frontend/src/eth/derived/wallet-connect.derived.ts
@@ -1,13 +1,11 @@
 import { page } from '$app/stores';
+import type { OptionalNullableString } from '$lib/types/string';
 import { derived, type Readable } from 'svelte/store';
 
-export const walletConnectUri: Readable<OptionalNullableCanisterIdText> = derived(
-	[page],
-	([$page]) => {
-		const {
-			data: { uri }
-		} = $page;
+export const walletConnectUri: Readable<OptionalNullableString> = derived([page], ([$page]) => {
+	const {
+		data: { uri }
+	} = $page;
 
-		return uri;
-	}
-);
+	return uri;
+});
