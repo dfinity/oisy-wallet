12ef37d0d86ee6bd0db7a1272314e5235edac4fd
David Dal Busco <david.dalbusco@outlook.com>
2024-09-11 07:12:02 +0200

    feat: list permissions review

--- src/frontend/src/lib/components/signer/SignerPermissions.svelte
+++ src/frontend/src/lib/components/signer/SignerPermissions.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
-	import { type IcrcScope, type PermissionsConfirmation } from '@dfinity/oisy-wallet-signer';
+	import {
+		type IcrcScope,
+		type IcrcScopedMethod,
+		type PermissionsConfirmation
+	} from '@dfinity/oisy-wallet-signer';
 	import { nonNullish } from '@dfinity/utils';
-	import { getContext } from 'svelte';
+	import { type ComponentType, getContext } from 'svelte';
 	import { fade } from 'svelte/transition';
 	import ButtonGroup from '$lib/components/ui/ButtonGroup.svelte';
 	import { SIGNER_CONTEXT_KEY, type SignerContext } from '$lib/stores/signer.store';
+	import { IconWallet } from '@dfinity/gix-components';
+	import IconShield from '$lib/components/icons/IconShield.svelte';
 
 	const {
 		permissionsPrompt: { payload, reset: resetPrompt }
@@ -29,18 +35,39 @@
 
 		resetPrompt();
 	};
+
+	const listItems: Record<
+		IcrcScopedMethod,
+		{
+			icon: ComponentType;
+			label: string;
+		}
+	> = {
+		icrc27_accounts: {
+			icon: IconWallet,
+			label: 'Learn your Oisy wallet address'
+		},
+		icrc49_call_canister: {
+			icon: IconShield,
+			label: 'Initiate transactions to approve in Oisy'
+		}
+	};
 </script>
 
 {#if nonNullish(scopes)}
 	<form in:fade on:submit|preventDefault={onApprove} method="POST">
-		<h2 class="text-center mb-6">Review permissions</h2>
+		<h2 class="text-center mb-6">Connect your wallet</h2>
 
 		<div class="bg-light-blue p-6 mb-6 rounded-lg">
-			<p class="break-normal font-bold">The dApp is requesting following permissions:</p>
+			<p class="break-normal font-bold">By connecting, ORIGIN will:</p>
 
-			<ul class="flex flex-col gap-1 list-disc mt-2 mx-4">
+			<ul class="flex flex-col gap-1 list-none mt-2.5">
 				{#each scopes as scope}
-					<li>{scope.scope.method}</li>
+					{@const {icon, label} = listItems[scope.scope.method]}
+
+					<li class="break-normal pb-1.5 flex items-center gap-2">
+						<svelte:component this={icon} /> {label}
+					</li>
 				{/each}
 			</ul>
 		</div>
