a68dc2aab440bbd186bb24898a59cd3f9d81fd53
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-11 14:39:50 +0200

    revert: make function await

--- src/frontend/src/icp/services/ic-send.services.ts
+++ src/frontend/src/icp/services/ic-send.services.ts
@@ -48,7 +48,7 @@ export const sendIc = async ({
 	// No need to do it for ETH as it is always enabled by default.
 	const { token, targetNetworkId, identity, ckErc20ToErc20TwinToken } = rest;
 	if (isConvertCkErc20ToErc20({ token, networkId: targetNetworkId })) {
-		enableTwinTokenInUserTokens({
+		await enableTwinTokenInUserTokens({
 			identity,
 			twinToken: ckErc20ToErc20TwinToken
 		});
