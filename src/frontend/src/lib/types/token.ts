51c196389738f5f5e513c7712a73160c22a14710
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-09-12 09:02:22 +0200

    refactor(frontend): common type for balance as BigNumber (#2329)
    # Motivation
    
    To avoid repetition of code, we create a common type for balance when is
    defined as `BigNumber`.

--- src/frontend/src/lib/types/token.ts
+++ src/frontend/src/lib/types/token.ts
@@ -1,6 +1,6 @@
+import type { OptionBalance } from '$lib/types/balance';
 import type { Network } from '$lib/types/network';
 import type { RequiredExcept } from '$lib/types/utils';
-import type { BigNumber } from '@ethersproject/bignumber';
 
 export type TokenId = symbol;
 
@@ -41,7 +41,7 @@ export type OptionTokenStandard = TokenStandard | undefined | null;
 export type TokenToPin = Pick<Token, 'id'> & { network: Pick<Token['network'], 'id'> };
 
 interface TokenFinancialData {
-	balance?: BigNumber | null;
+	balance?: Exclude<OptionBalance, undefined>;
 	usdBalance?: number;
 }
 
