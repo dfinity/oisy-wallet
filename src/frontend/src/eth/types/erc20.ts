a2bf53a97f7ef6f9984186c8d6c718e6f8bee376
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-13 13:12:28 +0200

    refactor(frontend): use common type for RequiredToken

--- src/frontend/src/eth/types/erc20.ts
+++ src/frontend/src/eth/types/erc20.ts
@@ -1,11 +1,11 @@
 import type { ContractAddress } from '$eth/types/address';
 import type { Exchange } from '$lib/types/exchange';
-import type { Token, TokenAppearance, TokenMetadata } from '$lib/types/token';
-import type { Option, RequiredExcept } from '$lib/types/utils';
+import type { RequiredToken, Token, TokenMetadata } from '$lib/types/token';
+import type { Option } from '$lib/types/utils';
 
 export type Erc20Token = Erc20Contract & Token;
 
-export type RequiredErc20Token = RequiredExcept<Erc20Token, keyof TokenAppearance>;
+export type RequiredErc20Token = RequiredToken<Erc20Token>;
 
 export type Erc20ContractAddress = ContractAddress;
 export type Erc20Contract = Erc20ContractAddress & { exchange: Exchange; twinTokenSymbol?: string };
