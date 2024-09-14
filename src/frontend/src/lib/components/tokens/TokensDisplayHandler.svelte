56056a4c08b04d76f1d688272ce28fa3a461b625
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-09-14 10:50:57 +0200

    feat(frontend): remove sorting dependency on pointer events [GIX-2944] (#2354)
    # Motivation
    
    The token list was giving issues due to wrong consistency between
    pointer events and re-sorting. We remove then the logic of re-sorting
    ONLY when there is no pointer event, and we leave the pointer events
    disabled when animating.
    
    # Changes
    
    - Removed logic on resorting dependent on pointer events from
    `TokenDisplayHandler`.
    - Removed all deprecated utils.
    
    # Test
    
    On local replica and in staging, now there is the correct behavior.

--- src/frontend/src/lib/components/tokens/TokensDisplayHandler.svelte
+++ src/frontend/src/lib/components/tokens/TokensDisplayHandler.svelte
@@ -2,10 +2,7 @@
 	import { debounce } from '@dfinity/utils';
 	import { combinedDerivedSortedNetworkTokensUi } from '$lib/derived/network-tokens.derived';
 	import { showZeroBalances } from '$lib/derived/settings.derived';
-	import { pointerEventStore } from '$lib/stores/events.store';
 	import type { Token, TokenUi } from '$lib/types/token';
-	import { pointerEventsHandler } from '$lib/utils/events.utils';
-	import { defineTokensToDisplay } from '$lib/utils/tokens-ui.utils';
 
 	// We start it as undefined to avoid showing an empty list before the first update.
 	export let tokens: Token[] | undefined = undefined;
@@ -15,18 +12,11 @@
 		({ balance, usdBalance }) => Number(balance ?? 0n) || (usdBalance ?? 0) || $showZeroBalances
 	);
 
-	const updateTokensToDisplay = () =>
-		(tokens = defineTokensToDisplay({
-			currentTokens: tokens,
-			sortedTokens,
-			$pointerEventStore: $pointerEventStore
-		}));
+	const updateTokensToDisplay = () => (tokens = [...sortedTokens]);
 
 	const debounceUpdateTokensToDisplay = debounce(updateTokensToDisplay, 500);
 
-	$: $pointerEventStore, sortedTokens, debounceUpdateTokensToDisplay();
+	$: sortedTokens, debounceUpdateTokensToDisplay();
 </script>
 
-<div use:pointerEventsHandler>
-	<slot />
-</div>
+<slot />
