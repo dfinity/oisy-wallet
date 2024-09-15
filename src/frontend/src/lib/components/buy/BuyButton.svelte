d45d27f2c7f7f07860c4eb660b60c949b2e9c6fd
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-16 01:08:07 +0200

    feat: add icon

--- src/frontend/src/lib/components/buy/BuyButton.svelte
+++ src/frontend/src/lib/components/buy/BuyButton.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import IconSend from '$lib/components/icons/IconSend.svelte';
+	import IconBuy from '$lib/components/icons/IconBuy.svelte';
 	import ButtonHero from '$lib/components/ui/ButtonHero.svelte';
 	import { isBusy } from '$lib/derived/busy.derived';
 	import { i18n } from '$lib/stores/i18n.store';
 </script>
 
 <ButtonHero on:click disabled={$isBusy} ariaLabel={$i18n.send.text.send}>
-	<IconSend size="28" slot="icon" />
+	<IconBuy size="28" slot="icon" />
 	{$i18n.buy.text.buy}
 </ButtonHero>
