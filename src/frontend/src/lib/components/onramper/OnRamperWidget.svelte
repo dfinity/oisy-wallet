648d465a150a3da1ceb8e1840179968e9514a4cf
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-16 01:16:10 +0200

    feat: add derived

--- src/frontend/src/lib/components/onramper/OnRamperWidget.svelte
+++ src/frontend/src/lib/components/onramper/OnRamperWidget.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import { buildOnRamperLink } from '$lib/utils/onramper.utils';
+	import { pageToken } from '$lib/derived/page-token.derived';
+	import { tokens } from '$lib/derived/tokens.derived';
+	import { networks } from '$lib/derived/networks.derived';
 
 	let src: string;
 	$: src = buildOnRamperLink({
 		mode: 'buy',
 		defaultFiat: 'USD',
-		onlyCryptos: ['BTC', 'ETH', 'USDT', 'USDC', 'DAI'],
-		onlyCryptoNetworks: ['bitcoin', 'ethereum', 'ethereum', 'ethereum', 'ethereum'],
+		defaultCrypto: $pageToken?.symbol,
+		onlyCryptos: $tokens.map((token) => token.symbol),
+		onlyCryptoNetworks: $networks.map((network) => network.name),
 		wallets: [],
 		supportRecurringPayments: true,
 		enableCountrySelector: true
