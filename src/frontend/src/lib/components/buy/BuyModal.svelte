d0d25528e2339d4c15d88ecc6b3ee76adc6b8f4f
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-16 09:55:43 +0200

    feat(frontend): add Buy button

---
+++ src/frontend/src/lib/components/buy/BuyModal.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Modal } from '@dfinity/gix-components';
+	import { i18n } from '$lib/stores/i18n.store';
+	import { modalStore } from '$lib/stores/modal.store';
+</script>
+
+<Modal on:nnsClose={modalStore.close}>
+	<svelte:fragment slot="title"
+		><p class="text-xl">
+			{$i18n.buy.text.buy}
+		</p></svelte:fragment
+	>
+</Modal>
