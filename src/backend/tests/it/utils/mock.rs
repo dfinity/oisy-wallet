e029214defbf7b679f1a720039e7b0578498c7a8
Max Murphy <max@winning.black>
2024-09-16 14:41:14 +0200

    Update bindings

--- src/backend/tests/it/utils/mock.rs
+++ src/backend/tests/it/utils/mock.rs
@@ -1,5 +1,4 @@
 pub const CALLER: &str = "xzg7k-thc6c-idntg-knmtz-2fbhh-utt3e-snqw6-5xph3-54pbp-7axl5-tae";
-pub const CALLER_ETH_ADDRESS: &str = "0xdd7fec4C49CD2Dd4eaa884D22D92503EabA5A791";
 
 /// An admin user.  Typically, controls the backend canister.
 pub const CONTROLLER: &str = "l3lfs-gak7g-xrbil-j4v4h-aztjn-4jyki-wprso-m27h3-ibcl3-2cwuz-oqe";
