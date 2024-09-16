1b6b789d18145f90a3b5f5bcd55bce5101d3330b
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-09-16 11:58:40 +0200

    feat(frontend): create env flag for OnRamper features (#2367)
    # Motivation
    
    We will start adding features to buy from OnRamper. So, until it is
    finished, we will use the env flag `VITE_ONRAMPER_ENABLED` to show/hide
    such feature, for development scopes.

---
+++ src/frontend/src/env/onramper.env.ts
@@ -0,0 +1,2 @@
+// TODO: to be removed when we have finished implementing all the OnRamper features
+export const ONRAMPER_ENABLED = JSON.parse(import.meta.env.VITE_ONRAMPER_ENABLED ?? false) === true;
