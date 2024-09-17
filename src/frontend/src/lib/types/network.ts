105ab4fc7c313afaacc005aeabf113bce5cfce85
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-17 11:19:26 +0200

    feat(frontend): add OnRamper ID to networks

--- src/frontend/src/lib/types/network.ts
+++ src/frontend/src/lib/types/network.ts
@@ -1,10 +1,16 @@
+import type { OnRamperNetworkId } from '$lib/types/onramper';
+
 export type NetworkId = symbol;
 
 export type NetworkEnvironment = 'mainnet' | 'testnet';
 
-export interface Network {
+export type Network = {
 	id: NetworkId;
 	env: NetworkEnvironment;
 	name: string;
 	icon?: string;
+} & NetworkLinkedData;
+
+export interface NetworkLinkedData {
+	onRamperId?: OnRamperNetworkId;
 }
