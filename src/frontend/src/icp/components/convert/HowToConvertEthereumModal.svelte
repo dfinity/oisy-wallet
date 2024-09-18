a5291bfcb0561511e6ab0c0bafa957eba3d6f0ed
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-18 09:47:34 +0200

    chore(lint): use new prettier format

--- src/frontend/src/icp/components/convert/HowToConvertEthereumModal.svelte
+++ src/frontend/src/icp/components/convert/HowToConvertEthereumModal.svelte
@@ -28,11 +28,11 @@
 	let destination = '';
 	$: destination =
 		$ckEthereumTwinTokenStandard === 'erc20'
-			? toCkErc20HelperContractAddress($ckEthMinterInfoStore?.[$ckEthereumNativeTokenId]) ?? ''
-			: toCkEthHelperContractAddress(
+			? (toCkErc20HelperContractAddress($ckEthMinterInfoStore?.[$ckEthereumNativeTokenId]) ?? '')
+			: (toCkEthHelperContractAddress(
 					$ckEthMinterInfoStore?.[$ckEthereumNativeTokenId],
 					$ckEthereumNativeToken.network.id
-				) ?? '';
+				) ?? '');
 
 	let targetNetwork: Network | undefined = ICP_NETWORK;
 
