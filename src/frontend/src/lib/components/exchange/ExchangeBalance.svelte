dfinity/oisy-wallet

commit c410904ca80ea45fa113f87e7638a7b335c2a21a

feat(lint): custom rule to prefer object parameters (#2416)
# Motivation

We would like to introduce a new custom lint rule that enforces the
usage of an object when there are two or more parameters as function
input.

### Incorrect
```typescript
const fun = (param1: string, param2: number) => {...
```

### Correct
```typescript
const fun2 = ({ param1, param2 }: { param1: string; param2: number }) => {...
```

There are some exceptions:
- loop methods (`map`, `reduce`, `filter`, `forEach`, `sort`,
`replace`);
- new `Promise` definitions;
- `Array.from`;
- `JSON.stringify`;
- class constructors.

Furthermore, in this PR, we will not solve all the new issues raised by
the new rule, otherwise it would become too big: we add the exceptions
to the rule with a `TODO`, for all the functions that we need to fix in
other PRs.

# Changes

- Create new local rule excluding the nodes mentioned in the list of
exceptions.
- Add specific exceptions to custom functions that serve the same
purpose as loop methods (mapping functions, sorting functions).
- Fix a few rules.
- Add temporary exceptions to the rule with a `TODO` associated.

# Tests

The rule worked well: it raises new issues that we could fix.

src/frontend/src/lib/utils/format.utils.ts
@@ -34,6 +34,8 @@ export const formatToken = ({
  * @param splitLength An optional length for the split. e.g. 12345678 becomes, if splitLength = 2, 12...78
  * @returns text
  */
+// TODO: Remove ESLint exception and use object params
+// eslint-disable-next-line local-rules/prefer-object-params
 export const shortenWithMiddleEllipsis = (text: string, splitLength = 7): string => {
 	// Original min length was 16 to extract 7 split
 	const minLength = splitLength * 2 + 2;
@@ -61,6 +63,8 @@ export const formatNanosecondsToDate = (nanoseconds: bigint): string => {
 	return date.toLocaleDateString('en', DATE_TIME_FORMAT_OPTIONS);
 };
 
+// TODO: Remove ESLint exception and use object params
+// eslint-disable-next-line local-rules/prefer-object-params
 export const formatUSD = (
 	value: number,
 	options?: {

.eslintrc.cjs
@@ -57,9 +57,10 @@ module.exports = {
 			}
 		],
 		'@typescript-eslint/prefer-nullish-coalescing': 'error',
-    '@typescript-eslint/prefer-reduce-type-parameter': 'error',
+		'@typescript-eslint/prefer-reduce-type-parameter': 'error',
 		'arrow-body-style': ['warn', 'as-needed'],
 		curly: 'error',
+		'local-rules/prefer-object-params': 'warn',
 		'local-rules/no-svelte-store-in-api': 'error',
 		'local-rules/use-option-type-wrapper': 'warn',
 		'import/no-duplicates': ['error', { 'prefer-inline': true }],

eslint-local-rules.cjs
@@ -1,3 +1,4 @@
+const { isNullish, nonNullish } = require('@dfinity/utils');
 module.exports = {
 	'no-svelte-store-in-api': {
 		meta: {
@@ -79,6 +80,95 @@ module.exports = {
 				},
 				TSTypeAliasDeclaration(node) {
 					checkForOptionType(node);
+				}
+			};
+		}
+	},
+	'prefer-object-params': {
+		meta: {
+			type: 'suggestion',
+			docs: {
+				description:
+					'Enforce passing parameters as an object when a function has more than one parameter',
+				category: 'Best Practices',
+				recommended: true
+			},
+			schema: []
+		},
+		create(context) {
+			const checkForMoreThanOneParameter = (node) => {
+				const parent = node.parent;
+
+				// Check if it is a callback for looping methods
+				if (
+					nonNullish(parent) &&
+					parent.type === 'CallExpression' &&
+					parent.callee.type === 'MemberExpression' &&
+					['map', 'reduce', 'forEach', 'filter', 'sort', 'replace'].includes(
+						parent.callee.property.name
+					)
+				) {
+					return;
+				}
+
+				// Check if it is a callback for Array.from
+				if (
+					nonNullish(parent) &&
+					parent.type === 'CallExpression' &&
+					parent.callee.type === 'MemberExpression' &&
+					parent.callee.object.name === 'Array' &&
+					parent.callee.property.name === 'from'
+				) {
+					return;
+				}
+
+				// Check if it is a callback in a Promise constructor
+				if (
+					nonNullish(parent) &&
+					parent.type === 'NewExpression' &&
+					parent.callee.name === 'Promise'
+				) {
+					return;
+				}
+
+				// Check if it is a callback in JSON.stringify
+				if (
+					nonNullish(parent) &&
+					parent.type === 'CallExpression' &&
+					parent.callee.type === 'MemberExpression' &&
+					parent.callee.object.name === 'JSON' &&
+					parent.callee.property.name === 'stringify'
+				) {
+					return;
+				}
+
+				// Check if it is inside a class constructor
+				if (
+					nonNullish(parent) &&
+					parent.type === 'MethodDefinition' &&
+					parent.kind === 'constructor'
+				) {
+					return;
+				}
+
+				if (node.params.length > 1) {
+					context.report({
+						node,
+						message:
+							'Functions with more than one parameter should accept an object and use destructuring.'
+					});
+				}
+			};
+
+			return {
+				FunctionDeclaration(node) {
+					checkForMoreThanOneParameter(node);
+				},
+				FunctionExpression(node) {
+					checkForMoreThanOneParameter(node);
+				},
+				ArrowFunctionExpression(node) {
+					checkForMoreThanOneParameter(node);
 				}
 			};
 		}

scripts/build.tokens.ckerc20.mjs
@@ -39,6 +39,7 @@ const orchestratorInfo = async ({ orchestratorId: canisterId }) => {
 const buildOrchestratorInfo = async (orchestratorId) => {
 	const { managed_canisters } = await orchestratorInfo({ orchestratorId });
 
+	// eslint-disable-next-line local-rules/prefer-object-params -- This is a destructuring assignment
 	const mapManagedCanisters = (
 		acc,
 		{ ledger, index, ckerc20_token_symbol, erc20_contract: { address: erc20ContractAddress } }
@@ -95,6 +96,8 @@ const DATA_FOLDER = join(process.cwd(), 'src', 'frontend', 'src', 'env');
 
 const LOGO_FOLDER = join(process.cwd(), 'src', 'frontend', 'src', 'icp-eth', 'assets');
 
+// TODO: Remove ESLint exception and use object params
+// eslint-disable-next-line local-rules/prefer-object-params
 const saveTokenLogo = async (canisterId, name) => {
 	const logoName = name.toLowerCase().replace('ck', '').replace('sepolia', '');
 	const file = join(LOGO_FOLDER, `${logoName}.svg`);

scripts/check.i18n.mjs
@@ -8,6 +8,8 @@ const PATH_FROM_ROOT = join(process.cwd(), 'src', 'frontend', 'src');
 const PATH_TO_EN_JSON = join(PATH_FROM_ROOT, 'lib', 'i18n', 'en.json');
 const PATH_TO_CODEBASE = join(PATH_FROM_ROOT);
 
+// TODO: Remove ESLint exception and use object params
+// eslint-disable-next-line local-rules/prefer-object-params
 const extractKeys = (obj, prefix = '') =>
 	Object.keys(obj).reduce((res, el) => {
 		if (typeof obj[el] === 'object') {

scripts/check.tokens.erc20.ts
@@ -15,11 +15,22 @@ const twinTokensMainnetAddresses = getAddresses(ERC20_TWIN_TOKENS_MAINNET);
 const sepoliaAddresses = getAddresses(ERC20_CONTRACTS_SEPOLIA);
 const twinTokensSepoliaAddresses = getAddresses(ERC20_TWIN_TOKENS_SEPOLIA);
 
-const filterDuplicates = (addresses1: string[], addresses2: string[]): string[] =>
-	addresses1.filter((address) => addresses2.includes(address));
+const filterDuplicates = ({
+	addresses1,
+	addresses2
+}: {
+	addresses1: string[];
+	addresses2: string[];
+}): string[] => addresses1.filter((address) => addresses2.includes(address));
 
-const mainnetDuplicates = filterDuplicates(productionAddresses, twinTokensMainnetAddresses);
-const sepoliaDuplicates = filterDuplicates(sepoliaAddresses, twinTokensSepoliaAddresses);
+const mainnetDuplicates = filterDuplicates({
+	addresses1: productionAddresses,
+	addresses2: twinTokensMainnetAddresses
+});
+const sepoliaDuplicates = filterDuplicates({
+	addresses1: sepoliaAddresses,
+	addresses2: twinTokensSepoliaAddresses
+});
 
 if (mainnetDuplicates.length > 0 || sepoliaDuplicates.length > 0) {
 	if (mainnetDuplicates.length > 0) {

src/frontend/src/eth/providers/alchemy-erc20.providers.ts
@@ -35,6 +35,8 @@ export class AlchemyErc20Provider {
 	}): WebSocketListener => {
 		const erc20Contract = new ethers.Contract(contract.address, ERC20_ABI, this.provider);
 
+		// TODO: Remove ESLint exception and use object params
+		// eslint-disable-next-line local-rules/prefer-object-params
 		const filterListener = async (
 			_from: string,
 			_address: string,

src/frontend/src/icp-eth/services/custom-token.services.ts
@@ -19,6 +19,8 @@ const assertErc20SendTokenData = (sendToken: Erc20Token): AutoLoadTokenResult | 
 	}
 };
 
+// TODO: Remove ESLint exception and use object params
+// eslint-disable-next-line local-rules/prefer-object-params
 const findCustomToken = (
 	tokens: IcrcCustomToken[],
 	sendToken: Erc20Token

commit 8a820761ce73e5555e8801e0ebd352c8c2d97215

refactor(frontend): use object param for formatUSD (#2428)
# Motivation

We remove one of the "temporary" exceptions to the new custom ES lint
rule introduced by PR #2416 , and adapt the code. Specifically the one
concerning util `formatUSD`.

src/frontend/src/lib/components/exchange/ExchangeBalance.svelte
@@ -13,9 +13,9 @@
 		class={`break-all text-5xl font-bold ${totalUsd === 0 ? 'opacity-50' : 'opacity-100'} mt-8 inline-block`}
 	>
 		{#if $exchangeInitialized}
-			{formatUSD(totalUsd)}
+			{formatUSD({ value: totalUsd })}
 		{:else}
-			<span class="animate-pulse">{formatUSD(0)}</span>
+			<span class="animate-pulse">{formatUSD({ value: 0 })}</span>
 		{/if}
 	</output>
 </span>

src/frontend/src/lib/components/exchange/ExchangeTokenValue.svelte
@@ -10,9 +10,9 @@
 <TokenExchangeValueSkeleton {token}>
 	<output class="break-all">
 		{#if nonNullish(token.balance) && nonNullish(token.usdBalance)}
-			{formatUSD(token.usdBalance)}
+			{formatUSD({ value: token.usdBalance })}
 		{:else}
-			{formatUSD(0, { minFraction: 0, maxFraction: 0 }).replace('0', '-')}
+			{formatUSD({ value: 0, options: { minFraction: 0, maxFraction: 0 } }).replace('0', '-')}
 		{/if}
 	</output>
 </TokenExchangeValueSkeleton>

src/frontend/src/lib/components/networks/NetworkButton.svelte
@@ -34,7 +34,7 @@
 		{name}
 		{icon}
 		logo="start"
-		description={nonNullish(usdBalance) ? formatUSD(usdBalance) : undefined}
+		description={nonNullish(usdBalance) ? formatUSD({ value: usdBalance }) : undefined}
 	/>
 
 	{#if id === $networkId}

src/frontend/src/lib/utils/format.utils.ts
@@ -67,17 +67,18 @@ export const formatNanosecondsToDate = (nanoseconds: bigint): string => {
 	return date.toLocaleDateString('en', DATE_TIME_FORMAT_OPTIONS);
 };
 
-// TODO: Remove ESLint exception and use object params
-// eslint-disable-next-line local-rules/prefer-object-params
-export const formatUSD = (
-	value: number,
+export const formatUSD = ({
+	value,
+	options
+}: {
+	value: number;
 	options?: {
 		minFraction?: number;
 		maxFraction?: number;
 		maximumSignificantDigits?: number;
 		symbol?: boolean;
-	}
-): string => {
+	};
+}): string => {
 	const {
 		minFraction = 2,
 		maxFraction = 2,