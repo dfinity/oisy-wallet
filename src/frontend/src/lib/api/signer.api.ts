fd4a85503dc309f8011584dd1f6451d1bef966a1
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2024-09-19 13:39:12 +0200

    feat(frontend): use signer API via canister

--- src/frontend/src/lib/api/signer.api.ts
+++ src/frontend/src/lib/api/signer.api.ts
@@ -1,12 +1,13 @@
 import type { BitcoinNetwork, SignRequest } from '$declarations/signer/signer.did';
-import { getSignerActor } from '$lib/actors/actors.ic';
 import { getAgent } from '$lib/actors/agents.ic';
 import { SignerCanister } from '$lib/canisters/signer.canister';
 import { SIGNER_CANISTER_ID } from '$lib/constants/app.constants';
-import type { EthAddress } from '$lib/types/address';
+import type { BtcAddress, EthAddress } from '$lib/types/address';
 import type { OptionIdentity } from '$lib/types/identity';
-import type { Identity } from '@dfinity/agent';
 import { Principal } from '@dfinity/principal';
+import { assertNonNullish, isNullish } from '@dfinity/utils';
+
+let canister: SignerCanister | undefined = undefined;
 
 export const getBtcAddress = async ({
 	identity,
@@ -14,14 +15,29 @@ export const getBtcAddress = async ({
 }: {
 	identity: OptionIdentity;
 	network: BitcoinNetwork;
-}): Promise<EthAddress> => {
-	const { caller_btc_address } = await getSignerActor({ identity });
-	return caller_btc_address(network);
+}): Promise<BtcAddress> => {
+	const { getBtcAddress } = await signerCanister({ identity });
+
+	return getBtcAddress({ network });
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const updateBtcBalance = async ({
+	identity,
+	network
+}: {
+	identity: OptionIdentity;
+	network: BitcoinNetwork;
+}): Promise<bigint> => {
+	const { updateBtcBalance } = await signerCanister({ identity });
+
+	return updateBtcBalance({ network });
 };
 
 export const getEthAddress = async (identity: OptionIdentity): Promise<EthAddress> => {
-	const { caller_eth_address } = await getSignerActor({ identity });
-	return caller_eth_address();
+	const { getEthAddress } = await signerCanister({ identity });
+
+	return getEthAddress({});
 };
 
 export const signTransaction = async ({
@@ -31,8 +47,9 @@ export const signTransaction = async ({
 	transaction: SignRequest;
 	identity: OptionIdentity;
 }): Promise<string> => {
-	const { sign_transaction } = await getSignerActor({ identity });
-	return sign_transaction(transaction);
+	const { signTransaction } = await signerCanister({ identity });
+
+	return signTransaction({ transaction });
 };
 
 export const signMessage = async ({
@@ -42,8 +59,9 @@ export const signMessage = async ({
 	message: string;
 	identity: OptionIdentity;
 }): Promise<string> => {
-	const { personal_sign } = await getSignerActor({ identity });
-	return personal_sign(message);
+	const { personalSign } = await signerCanister({ identity });
+
+	return personalSign({ message });
 };
 
 export const signPrehash = async ({
@@ -53,17 +71,27 @@ export const signPrehash = async ({
 	hash: string;
 	identity: OptionIdentity;
 }): Promise<string> => {
-	const { sign_prehash } = await getSignerActor({ identity });
-	return sign_prehash(hash);
+	const { signPrehash } = await signerCanister({ identity });
+
+	return signPrehash({ hash });
 };
 
-// TODO: implement updateBtcBalance method
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const signerCanister = async ({ identity }: { identity: Identity }): Promise<SignerCanister> => {
-	const agent = await getAgent({ identity });
+const signerCanister = async ({
+	identity
+}: {
+	identity: OptionIdentity;
+}): Promise<SignerCanister> => {
+	// TODO: Delete this dependency once the signer has been stripped down.
+	assertNonNullish(identity);
 
-	return SignerCanister.create({
-		agent,
-		canisterId: Principal.fromText(SIGNER_CANISTER_ID)
-	});
+	if (isNullish(canister)) {
+		const agent = await getAgent({ identity });
+
+		canister = SignerCanister.create({
+			agent,
+			canisterId: Principal.fromText(SIGNER_CANISTER_ID)
+		});
+	}
+
+	return canister;
 };
--- src/frontend/src/lib/canisters/signer.canister.ts
+++ src/frontend/src/lib/canisters/signer.canister.ts
@@ -1,12 +1,21 @@
-import type { BitcoinNetwork, _SERVICE as SignerService } from '$declarations/signer/signer.did';
+import type {
+	BitcoinNetwork,
+	_SERVICE as SignerService,
+	SignRequest
+} from '$declarations/signer/signer.did';
 import { idlFactory as idlCertifiedFactorySigner } from '$declarations/signer/signer.factory.certified.did';
 import { idlFactory as idlFactorySigner } from '$declarations/signer/signer.factory.did';
+import type { BtcAddress, EthAddress } from '$lib/types/address';
 import { Principal } from '@dfinity/principal';
 import { Canister, createServices, type CanisterOptions } from '@dfinity/utils';
 
 interface SignerCanisterOptions<T> extends Omit<CanisterOptions<T>, 'canisterId'> {
 	canisterId: Principal;
 }
+
+type SignerCanisterFunctionParams<T = Record<string, never>> = T & {
+	certified?: boolean;
+};
 
 export class SignerCanister extends Canister<SignerService> {
 	static create(options: SignerCanisterOptions<SignerService>) {
@@ -19,11 +28,76 @@ export class SignerCanister extends Canister<SignerService> {
 		return new SignerCanister(canisterId, service, certifiedService);
 	}
 
-	updateBtcBalance = ({ network }: { network: BitcoinNetwork }): Promise<bigint> => {
+	getBtcAddress = ({
+		network,
+		certified = true
+	}: SignerCanisterFunctionParams<{
+		network: BitcoinNetwork;
+	}>): Promise<BtcAddress> => {
+		const { caller_btc_address } = this.caller({
+			certified
+		});
+
+		return caller_btc_address(network);
+	};
+
+	updateBtcBalance = ({
+		network,
+		certified = true
+	}: SignerCanisterFunctionParams<{
+		network: BitcoinNetwork;
+	}>): Promise<bigint> => {
 		const { caller_btc_balance } = this.caller({
-			certified: true
+			certified
 		});
 
 		return caller_btc_balance(network);
+	};
+
+	getEthAddress = ({ certified = true }: SignerCanisterFunctionParams): Promise<EthAddress> => {
+		const { caller_eth_address } = this.caller({
+			certified
+		});
+
+		return caller_eth_address();
+	};
+
+	signTransaction = ({
+		transaction,
+		certified = true
+	}: SignerCanisterFunctionParams<{
+		transaction: SignRequest;
+	}>): Promise<string> => {
+		const { sign_transaction } = this.caller({
+			certified
+		});
+
+		return sign_transaction(transaction);
+	};
+
+	personalSign = ({
+		message,
+		certified = true
+	}: SignerCanisterFunctionParams<{
+		message: string;
+	}>): Promise<string> => {
+		const { personal_sign } = this.caller({
+			certified
+		});
+
+		return personal_sign(message);
+	};
+
+	signPrehash = ({
+		hash,
+		certified = true
+	}: SignerCanisterFunctionParams<{
+		hash: string;
+	}>): Promise<string> => {
+		const { sign_prehash } = this.caller({
+			certified
+		});
+
+		return sign_prehash(hash);
 	};
 }
