282e44be654f6c48b0c0540d60270c482dde138b
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-20 15:01:02 +0200

    refactor: simplify css

--- src/frontend/src/lib/components/ui/HeroButtonGroup.svelte
+++ src/frontend/src/lib/components/ui/HeroButtonGroup.svelte
@@ -1,12 +1,6 @@
 <div
 	role="toolbar"
-	class="toolbar w-full px-4 py-6 text-black inline-flex justify-between rounded-[32px] bg-[var(--color-primary)]"
+	class="w-full px-4 py-6 text-black inline-flex justify-between rounded-[32px] bg-[var(--color-primary)]"
 >
 	<slot />
 </div>
-
-<style lang="scss">
-	.toolbar > :global(:last-child) {
-		border: none;
-	}
-</style>
