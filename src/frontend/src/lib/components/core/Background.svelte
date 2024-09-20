e39d09e033351f20563dd94f54f02be3d222afcf
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-20 07:32:51 +0200

    fix: adjust exact pixel to change

--- src/frontend/src/lib/components/core/Background.svelte
+++ src/frontend/src/lib/components/core/Background.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import background from '$lib/assets/background.svg';
-	import noBackground from '$lib/assets/no-background.svg';
 	import Img from '$lib/components/ui/Img.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { replaceOisyPlaceholders } from '$lib/utils/i18n.utils';
@@ -11,7 +10,7 @@
 		class="absolute left-1/2 top-0 min-w-[1280px] -translate-x-1/2 transform"
 		aria-label={replaceOisyPlaceholders($i18n.core.alt.background)}
 	>
-		<source srcset={background} media="(min-width: 640px)" />
-		<Img src={noBackground} />
+		<source srcset={background} media="(min-width: 641px)" />
+		<Img src="" />
 	</picture>
 </div>
