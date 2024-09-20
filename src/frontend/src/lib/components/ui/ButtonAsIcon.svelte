667618b6edb1f01c03ff26ee810c31570746d0e3
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-20 09:59:53 +0200

    refactor(frontend): separate button style for icons only

---
+++ src/frontend/src/lib/components/ui/ButtonAsIcon.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let button: HTMLButtonElement | undefined = undefined;
+	export let testId: string | undefined = undefined;
+	export let ariaLabel: string;
+</script>
+
+<button
+	class="w-16 flex flex-col text-primary text-xs font-normal text-center"
+	bind:this={button}
+	on:click
+	aria-label={ariaLabel}
+	data-tid={testId}
+>
+	<div class="rounded-full bg-white p-2 border border-primary">
+		<slot />
+	</div>
+</button>
