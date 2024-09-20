3c68592ba67fef6287660b9c180426fbe9684f51
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-20 07:29:16 +0200

    fix: missing image

--- src/frontend/src/lib/components/core/Background.svelte
+++ src/frontend/src/lib/components/core/Background.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
 	import background from '$lib/assets/background.svg';
+	import noBackground from '$lib/assets/no-background.svg';
+	import Img from '$lib/components/ui/Img.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { replaceOisyPlaceholders } from '$lib/utils/i18n.utils';
 </script>
 
 <div class="fixed inset-0 z-[-1] overflow-hidden bg-white">
-	<img
-		src=""
-		srcset="{background} 1280w"
-		sizes="(min-width: 640px) 100vw, 0vw"
-		alt={replaceOisyPlaceholders($i18n.core.alt.background)}
-		class="absolute left-1/2 top-0 hidden min-w-[1280px] -translate-x-1/2 transform sm:block"
-	/>
+	<picture
+		class="absolute left-1/2 top-0 min-w-[1280px] -translate-x-1/2 transform"
+		aria-label={replaceOisyPlaceholders($i18n.core.alt.background)}
+	>
+		<source srcset={background} media="(min-width: 640px)" />
+		<Img src={noBackground} />
+	</picture>
 </div>
