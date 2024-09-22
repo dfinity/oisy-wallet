7d23778abfac7a16226c7fbca3586c74d09af71e
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-09-22 17:06:40 +0200

    fix(frontend): reactive string for Alpha warning position (#2449)
    # Motivation
    
    Some browsers do not evaluate correctly the class string at runtime:
    `${mw}` is treated as a string interpolation rather than a logic-based
    evaluation. So, when `mw` changes, the template doesn't correctly update
    the classes. Sometimes the dynamic classes are even ignored.
    
    To correct it, we put the condition directly into the class string using
    the store `authSignedIn` directly.
    
    The alternative would be to transform `mw` into a derived of
    `authSignedIn`:
    
    `const mw: Readable<'sm' | 'xl'> = derived(authSignedIn, ($signedIn) =>
    ($signedIn ? 'sm' : 'xl'));`
    
    But then we would have more calls to a readable, since we would use 5
    times `$mw` instead of using just twice `$authSignedIn`.

--- src/frontend/src/lib/components/hero/Header.svelte
+++ src/frontend/src/lib/components/hero/Header.svelte
@@ -7,17 +7,16 @@
 	import AboutMenu from '$lib/components/hero/about/AboutMenu.svelte';
 	import AboutWhatModal from '$lib/components/hero/about/AboutWhatModal.svelte';
 	import OisyWalletLogo from '$lib/components/icons/OisyWalletLogo.svelte';
-	import { authSignedIn } from '$lib/derived/auth.derived';
+	import { authNotSignedIn, authSignedIn } from '$lib/derived/auth.derived';
 	import { modalAboutHow, modalAboutWhat } from '$lib/derived/modal.derived';
 
 	export let back = false;
-
-	let mw: 'sm' | 'xl';
-	$: mw = $authSignedIn ? 'sm' : 'xl';
 </script>
 
 <header
-	class={`grid grid-cols-2 ${mw}:grid-cols-[1fr_auto_1fr] items-center md:px-4 relative z-10 pointer-events-none`}
+	class="grid grid-cols-2 items-center md:px-4 relative z-10 pointer-events-none"
+	class:sm:grid-cols-[1fr_auto_1fr]={$authSignedIn}
+	class:xl:grid-cols-[1fr_auto_1fr]={$authNotSignedIn}
 	style="min-height: 78px"
 >
 	{#if back}
@@ -29,7 +28,15 @@
 	{/if}
 
 	<div
-		class={`col-span-3 col-start-1 row-start-2 ${mw}:col-span-1 ${mw}:col-start-2 ${mw}:row-start-1 ${mw}:w-fit flex px-4`}
+		class="col-span-3 col-start-1 row-start-2 flex px-4"
+		class:sm:col-span-1={$authSignedIn}
+		class:xl:col-span-1={$authNotSignedIn}
+		class:sm:col-start-2={$authSignedIn}
+		class:xl:col-start-2={$authNotSignedIn}
+		class:sm:row-start-1={$authSignedIn}
+		class:xl:row-start-1={$authNotSignedIn}
+		class:sm:w-fit={$authSignedIn}
+		class:xl:w-fit={$authNotSignedIn}
 	>
 		<Alpha />
 	</div>
