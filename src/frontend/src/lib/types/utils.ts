aee115823186f09bf81887e77239b0981a9be015
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-09-23 19:57:34 +0200

    feat(frontend): add OnRamper ID to networks [GIX-2854] (#2386)
    # Motivation
    
    We add the OnRamper Network ID to all the networks that we would like to
    have supported with it. The list is
    [here](https://docs.onramper.com/docs/network-support).

--- src/frontend/src/lib/types/utils.ts
+++ src/frontend/src/lib/types/utils.ts
@@ -7,3 +7,6 @@ export type ResultSuccessReduced<T = unknown> = ResultSuccess<T[]>;
 // We disable the eslint rule here because this is the utility type that we use to define such rule.
 // eslint-disable-next-line local-rules/use-option-type-wrapper
 export type Option<T> = T | null | undefined;
+
+export type AtLeastOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<keyof T, Keys>> &
+	{ [K in Keys]-?: Required<Pick<T, K>> & Partial<Omit<T, K>> }[Keys];
