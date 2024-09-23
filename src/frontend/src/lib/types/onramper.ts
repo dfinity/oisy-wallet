c3e69976bdf892e914e85b9ea088f64bd3af751a
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-23 21:50:42 +0200

    feat(frontend): add Onramper ID for tokens

--- src/frontend/src/lib/types/onramper.ts
+++ src/frontend/src/lib/types/onramper.ts
@@ -1,3 +1,7 @@
 // The list of networks that are supported by OnRamper can be found here:
 // https://docs.onramper.com/docs/network-support
 export type OnRamperNetworkId = 'icp' | 'bitcoin' | 'ethereum';
+
+// The list of cryptocurrencies that are supported by Onramper can be found here:
+// https://docs.onramper.com/docs/crypto-asset-support
+export type OnramperCryptoId = string;
