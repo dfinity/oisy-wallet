756fc992edbd6490972ab0e91e38aaf53fbcb8c3
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-24 00:20:28 +0200

    feat: create mock for page store

--- src/frontend/src/tests/lib/stores/token.store.spec.ts
+++ src/frontend/src/tests/lib/stores/token.store.spec.ts
@@ -1,7 +1,10 @@
 import { ICP_TOKEN } from '$env/tokens.env';
 import { token } from '$lib/stores/token.store';
 import { describe, it } from 'vitest';
+import { mockPageStore } from '../../mocks/page.store.mock';
 import { testDerivedUpdates } from '../../utils/derived.utils';
+
+mockPageStore();
 
 describe('token store', () => {
 	it('should ensure derived stores update at most once when the store changes', () => {
