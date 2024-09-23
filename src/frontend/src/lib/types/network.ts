c1432cf5260715d20382fd81a326c1aabcdeb5e0
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-09-23 19:57:34 +0200

    feat(frontend): add OnRamper ID to networks [GIX-2854] (#2386)
    # Motivation
    
    We add the OnRamper Network ID to all the networks that we would like to
    have supported with it. The list is
    [here](https://docs.onramper.com/docs/network-support).

--- src/frontend/src/lib/types/network.ts
+++ src/frontend/src/lib/types/network.ts
@@ -1,10 +1,18 @@
+import type { OnRamperNetworkId } from '$lib/types/onramper';
+import type { AtLeastOne } from '$lib/types/utils';
+
 export type NetworkId = symbol;
 
 export type NetworkEnvironment = 'mainnet' | 'testnet';
 
-export interface Network {
+export type Network = {
 	id: NetworkId;
 	env: NetworkEnvironment;
 	name: string;
 	icon?: string;
-}
+	buy?: AtLeastOne<NetworkBuy>;
+};
+
+export type NetworkBuy = {
+	onRamperId?: OnRamperNetworkId;
+};
