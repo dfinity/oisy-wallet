dbe29f74a8f07c6b18974baa11227dce317b6297
David Dal Busco <david.dalbusco@outlook.com>
2024-09-24 18:26:08 +0200

    refactor: rename

--- src/frontend/src/lib/components/signer/SignerSignIn.svelte
+++ src/frontend/src/lib/components/signer/SignerSignIn.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import SignerAnimatedIconInfinite from '$lib/components/signer/SignerAnimatedIconInfinite.svelte';
+	import SignerAnimatedAstronaut from '$lib/components/signer/SignerAnimatedAstronaut.svelte';
 	import ButtonAuthenticate from '$lib/components/ui/ButtonAuthenticate.svelte';
 	import { signIn } from '$lib/services/auth.services';
 	import { i18n } from '$lib/stores/i18n.store';
 </script>
 
 <div class="flex justify-center pt-12">
-	<SignerAnimatedIconInfinite />
+	<SignerAnimatedAstronaut />
 </div>
 
 <h2 class="text-center mt-12 mb-16">{$i18n.signer.sign_in.text.authenticate_to_connect}</h2>
