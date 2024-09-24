cac43207d249c9c6017296cba53f020dedad7e54
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2024-09-24 18:47:48 +0200

    feat(frontend): review adjustments

--- src/frontend/src/lib/derived/exchange.derived.ts
+++ src/frontend/src/lib/derived/exchange.derived.ts
@@ -1,8 +1,4 @@
-import {
-	BTC_MAINNET_TOKEN_ID,
-	BTC_REGTEST_TOKEN_ID,
-	BTC_TESTNET_TOKEN_ID
-} from '$env/tokens.btc.env';
+import { BTC_MAINNET_TOKEN_ID, BTC_REGTEST_TOKEN_ID } from '$env/tokens.btc.env';
 import { ETHEREUM_TOKEN_ID, ICP_TOKEN_ID, SEPOLIA_TOKEN_ID } from '$env/tokens.env';
 import { enabledErc20Tokens } from '$eth/derived/erc20.derived';
 import type { Erc20Token } from '$eth/types/erc20';
@@ -26,7 +22,6 @@ export const exchanges: Readable<ExchangesData> = derived(
 
 		return {
 			[BTC_MAINNET_TOKEN_ID]: btcPrice,
-			[BTC_TESTNET_TOKEN_ID]: btcPrice,
 			[BTC_REGTEST_TOKEN_ID]: btcPrice,
 			[ETHEREUM_TOKEN_ID]: ethPrice,
 			[SEPOLIA_TOKEN_ID]: ethPrice,
