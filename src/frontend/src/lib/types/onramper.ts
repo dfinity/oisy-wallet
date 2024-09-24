3fae665cf8737cc32614e7105bb85e90c69a8ff6
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-24 16:44:05 +0200

    feat(frontend): Onramper util to create the widget source link

--- src/frontend/src/lib/types/onramper.ts
+++ src/frontend/src/lib/types/onramper.ts
@@ -1,3 +1,5 @@
+import type { BtcAddress, EthAddress } from '$lib/types/address';
+
 // The list of networks that are supported by Onramper can be found here:
 // https://docs.onramper.com/docs/network-support
 export type OnramperNetworkId = 'icp' | 'bitcoin' | 'ethereum';
@@ -5,3 +7,14 @@ export type OnramperNetworkId = 'icp' | 'bitcoin' | 'ethereum';
 // The list of cryptocurrencies that are supported by Onramper can be found here:
 // https://docs.onramper.com/docs/crypto-asset-support
 export type OnramperId = string;
+
+// The list of fiat currencies that are supported by Onramper can be found here:
+// https://docs.onramper.com/docs/fiat-currency-support
+export type OnramperFiatId = 'usd' | 'eur' | 'gbp' | 'chf';
+
+export type OnramperMode = 'buy';
+
+export interface OnramperCryptoWallet {
+	cryptoId: OnramperId;
+	wallet: BtcAddress | EthAddress;
+}
---
+++ src/frontend/src/lib/utils/onramper.utils.ts
@@ -0,0 +1,50 @@
+import { ONRAMPER_API_KEY, ONRAMPER_BASE_URL } from '$env/onramper.env';
+import type {
+	OnramperCryptoWallet,
+	OnramperFiatId,
+	OnramperId,
+	OnramperMode,
+	OnramperNetworkId
+} from '$lib/types/onramper';
+
+export interface BuildOnramperLinkParams {
+	mode: OnramperMode;
+	defaultFiat: OnramperFiatId;
+	defaultCrypto?: OnramperId;
+	onlyCryptos: OnramperId[];
+	onlyCryptoNetworks: OnramperNetworkId[];
+	wallets: OnramperCryptoWallet[];
+	supportRecurringPayments: boolean;
+	enableCountrySelector: boolean;
+}
+
+const arrayToParam = (array: OnramperId[] | OnramperNetworkId[]) => array.join(',');
+
+const walletToParam = ({ cryptoId, wallet }: OnramperCryptoWallet) => `${cryptoId}:${wallet}`;
+
+const walletsToParam = (wallets: OnramperCryptoWallet[]) =>
+	arrayToParam(wallets.map(walletToParam));
+
+const toQueryString = (params: Omit<BuildOnramperLinkParams, 'wallets'>) =>
+	Object.entries(params)
+		.map(([key, value]) => `${key}=${Array.isArray(value) ? arrayToParam(value) : value}`)
+		.join('&');
+
+/**
+ * Build a source link for the Onramper widget, given a set of parameters.
+ * The documentation for the Onramper widget's parameters can be found here:
+ * https://docs.onramper.com/docs/supported-widget-parameters
+ *
+ * @param params - The parameters to build the link with.
+ * @param params.mode - The mode of the widget (buy or sell).
+ * @param params.defaultFiat - The default fiat currency.
+ * @param params.defaultCrypto - The optional default cryptocurrency.
+ * @param params.onlyCryptos - The list of allowed cryptocurrencies.
+ * @param params.onlyCryptoNetworks - The list of allowed cryptocurrency networks.
+ * @param params.wallets - The list of combination of cryptocurrency and wallet addresses.
+ * @param params.supportRecurringPayments - Whether to support recurring payments.
+ * @param params.enableCountrySelector - Whether to enable the country selector.
+ * @returns The Onramper source link.
+ */
+export const buildOnramperLink = ({ wallets, ...params }: BuildOnramperLinkParams) =>
+	`${ONRAMPER_BASE_URL}?apiKey=${ONRAMPER_API_KEY}&${toQueryString(params)}&wallets=${walletsToParam(wallets)}`;
