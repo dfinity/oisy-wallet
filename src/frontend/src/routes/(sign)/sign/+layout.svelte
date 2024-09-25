7d71f04c608442a82731745345ca11debc1e237d
David Dal Busco <david.dalbusco@outlook.com>
2024-09-25 07:14:42 +0200

    feat: warning

--- src/frontend/src/routes/(sign)/sign/+layout.svelte
+++ src/frontend/src/routes/(sign)/sign/+layout.svelte
@@ -3,12 +3,18 @@
 	import LoadersGuard from '$lib/components/core/LoadersGuard.svelte';
 	import Modals from '$lib/components/core/Modals.svelte';
 	import OisyWalletLogoLink from '$lib/components/core/OisyWalletLogoLink.svelte';
+	import WarningBanner from '$lib/components/ui/WarningBanner.svelte';
 </script>
 
 <Background />
 
 <main class="pt-10 flex flex-col gap-y-8">
 	<div class="flex justify-center"><OisyWalletLogoLink /></div>
+
+	<WarningBanner>
+		This feature is unreviewed. DO NOT USE until it has undergone proper code and security
+		assessment!
+	</WarningBanner>
 
 	<LoadersGuard>
 		<slot />
