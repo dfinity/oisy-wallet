5aeffb9af6573d92b2381b8efa5766e099f5f2c9
David Dal Busco <david.dalbusco@outlook.com>
2024-09-25 11:49:19 +0200

    feat: redo css global

--- src/frontend/src/routes/(sign)/sign/+layout.svelte
+++ src/frontend/src/routes/(sign)/sign/+layout.svelte
@@ -1,18 +1,8 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import LoadersGuard from '$lib/components/core/LoadersGuard.svelte';
 	import Modals from '$lib/components/core/Modals.svelte';
 	import OisyWalletLogoLink from '$lib/components/core/OisyWalletLogoLink.svelte';
 	import WarningBanner from '$lib/components/ui/WarningBanner.svelte';
-	import { LOCAL } from '$lib/constants/app.constants';
-
-	onMount(async () => {
-		if (!LOCAL) {
-			return;
-		}
-
-		await import('./+oisy.page.css');
-	});
 </script>
 
 <main class="pt-10 flex flex-col gap-y-8">
@@ -29,3 +19,7 @@
 </main>
 
 <Modals />
+
+<style global lang="scss">
+	@import './+oisy.page.css';
+</style>
