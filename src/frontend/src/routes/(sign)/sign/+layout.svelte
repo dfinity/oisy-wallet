3da8082aceafcf22f806cbab2754a7cfdc4a80db
David Dal Busco <david.dalbusco@outlook.com>
2024-09-25 09:05:09 +0200

    feat: integrate background in html pages

--- src/frontend/src/routes/(sign)/sign/+layout.svelte
+++ src/frontend/src/routes/(sign)/sign/+layout.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
-	import Background from '$lib/components/core/Background.svelte';
 	import LoadersGuard from '$lib/components/core/LoadersGuard.svelte';
 	import Modals from '$lib/components/core/Modals.svelte';
 	import OisyWalletLogoLink from '$lib/components/core/OisyWalletLogoLink.svelte';
 	import WarningBanner from '$lib/components/ui/WarningBanner.svelte';
 </script>
-
-<Background />
 
 <main class="pt-10 flex flex-col gap-y-8">
 	<div class="flex justify-center"><OisyWalletLogoLink /></div>
