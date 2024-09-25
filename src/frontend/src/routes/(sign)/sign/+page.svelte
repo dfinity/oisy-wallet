b5f00a8ef77e5ca12eacda60c51e0ef05f6e8541
David Dal Busco <david.dalbusco@outlook.com>
2024-09-25 12:05:19 +0200

    style: bottom spacing

--- src/frontend/src/routes/(sign)/sign/+page.svelte
+++ src/frontend/src/routes/(sign)/sign/+page.svelte
@@ -39,7 +39,7 @@
 	$: $authIdentity, init();
 </script>
 
-<article class="border border-water bg-white rounded-lg px-5 py-6">
+<article class="border border-water bg-white rounded-lg px-5 py-6 mb-10">
 	{#if $authNotSignedIn}
 		<SignerSignIn />
 	{:else if $idle}
