2a765da2c54f314568f938e406b88aa4e4b9cb5a
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-26 09:39:31 +0200

    refactor: rename prop

--- src/frontend/src/lib/components/ui/Button.svelte
+++ src/frontend/src/lib/components/ui/Button.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	export let color: 'primary' | 'secondary' = 'primary';
+	export let style: 'primary' | 'secondary' = 'primary';
 	export let type: 'submit' | 'reset' | 'button' = 'submit';
 	export let disabled = false;
 </script>
 
-<button class={`${color} block flex-1`} {type} {disabled} on:click>
+<button class={`${style} block flex-1`} {type} {disabled} on:click>
 	<slot />
 </button>
