7ab16b3af86f9207287fcda8be9f63fcd717ea32
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-09-26 19:33:12 +0200

    style(frontend): update background colors for primary button in each state [GIX-3015] (#2539)
    # Motivation
    
    We change the background colors of the `primary` button including the
    `hover` and the `active` states.
    
    <img width="526" alt="Screenshot 2024-09-26 at 15 47 45"
    src="https://github.com/user-attachments/assets/3a374f2f-eabf-4e17-bb9c-bff972e1bf91">
    <img width="609" alt="Screenshot 2024-09-26 at 15 47 51"
    src="https://github.com/user-attachments/assets/383cb07c-6e9a-447e-bb9d-245b24c77ea6">
    
    
    https://github.com/user-attachments/assets/8f4d79ee-7dbc-449b-a35b-d05ec50cd540

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,7 +32,9 @@ export default {
 			'alice-blue': '#ecf3fb',
 			'american-orange': '#ff8a00',
 			'crayola-yellow': '#ffe57f',
-			cornsilk: '#fff7d8'
+			cornsilk: '#fff7d8',
+			cobalt: '#004abe',
+			'resolution-blue': '#012f80'
 		},
 		extend: {
 			minWidth: {
