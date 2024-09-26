4ef7fde020befa31a01860e09c2da7d45880dfe8
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-09-26 10:39:38 +0200

    feat(frontend): implement service for communicating with Blockchain API (#2507)
    # Motivation
    
    The goal is to implement service for communicating with Blockchain API.
    Initially, it will be used for fetching uncertified BTC transactions.

---
+++ src/frontend/src/lib/rest/blockchain.rest.ts
@@ -0,0 +1,27 @@
+import type { BitcoinAddressData, BlockchainBtcAddressDataParams } from '$lib/types/blockchain';
+
+const API_URL = import.meta.env.VITE_BLOCKCHAIN_API_URL;
+
+/**
+ * Get BTC address data (including transactions).
+ *
+ * Documentation:
+ * - https://www.blockchain.com/explorer/api/blockchain_api
+ *
+ */
+export const btcAddressData = async ({
+	btcAddress
+}: BlockchainBtcAddressDataParams): Promise<BitcoinAddressData> =>
+	fetchBlockchainApi<BitcoinAddressData>({
+		endpointPath: `/rawaddr/${btcAddress}`
+	});
+
+const fetchBlockchainApi = async <T>({ endpointPath }: { endpointPath: string }): Promise<T> => {
+	const response = await fetch(`${API_URL}/${endpointPath}`);
+
+	if (!response.ok) {
+		throw new Error('Blockchain API response not ok.');
+	}
+
+	return response.json();
+};
