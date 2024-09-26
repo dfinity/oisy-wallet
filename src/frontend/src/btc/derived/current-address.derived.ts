beabb3a40ab2878e9353bd21260d7ef30795c001
Llorenç <llorenc.muntaner@dfinity.org>
2024-09-26 14:39:36 +0200

    Use networkId instead of token

--- src/frontend/src/btc/derived/current-address.derived.ts
+++ src/frontend/src/btc/derived/current-address.derived.ts
@@ -8,21 +8,21 @@ import {
 	btcAddressRegtest,
 	btcAddressTestnet
 } from '$lib/derived/address.derived';
-import { token } from '$lib/stores/token.store';
+import { networkId } from '$lib/derived/network.derived';
 import type { OptionBtcAddress } from '$lib/types/address';
 import { nonNullish } from '@dfinity/utils';
 import { derived, type Readable } from 'svelte/store';
 
 export const currentBtcAddress: Readable<OptionBtcAddress | undefined> = derived(
-	[token, btcAddressMainnet, btcAddressTestnet, btcAddressRegtest],
-	([$token, $btcAddressMainnet, $btcAddressTestnet, $btcAddressRegtest]) => {
+	[networkId, btcAddressMainnet, btcAddressTestnet, btcAddressRegtest],
+	([$networkId, $btcAddressMainnet, $btcAddressTestnet, $btcAddressRegtest]) => {
 		const mapper: Record<symbol, OptionBtcAddress> = {
 			[BTC_MAINNET_TOKEN_ID]: $btcAddressMainnet,
 			[BTC_TESTNET_TOKEN_ID]: $btcAddressTestnet,
 			[BTC_REGTEST_TOKEN_ID]: $btcAddressRegtest
 		};
-		if (nonNullish($token)) {
-			return mapper[$token?.id];
+		if (nonNullish($networkId)) {
+			return mapper[$networkId];
 		}
 	}
 );
