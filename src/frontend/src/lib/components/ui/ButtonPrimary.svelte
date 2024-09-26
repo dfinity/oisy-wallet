bb3efa1bb1004ba1b5c4769b206195900c1da696
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-09-26 07:03:43 +0200

    refactor: add prop type

--- src/frontend/src/lib/components/ui/ButtonPrimary.svelte
+++ src/frontend/src/lib/components/ui/ButtonPrimary.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+	export let type: 'submit' | 'reset' | 'button' = 'submit';
 	export let disabled = false;
 </script>
 
-<button class="primary block flex-1" {disabled} on:click>
+<button class="primary block flex-1" {type} {disabled} on:click>
 	<slot />
 </button>
