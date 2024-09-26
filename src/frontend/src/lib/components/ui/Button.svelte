f123c374d13196f5e88883a2fc3e689db4255f50
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-09-26 15:22:34 +0200

    refactor(frontend): extract secondary buttons like Back and Cancel (#2527)
    # Motivation
    
    Another step to "concentrate" all button style in less places: we create
    two new components ButtonBack and ButtonCancel, that are secondary
    buttons re-used several times in the code.
    
    Furthermore, to avoid any possible conflict with built-in props of
    `<button>` we rename property `style` to `colorStyle`.
    
    # Changes
    
    - Change property `style` to `colorStyle`.
    - Create new button components.
    - Adapt usage in the code.
    - Use secondary buttons in other needed instances.
    
    # Tests
    
    No changes in local replica.

--- src/frontend/src/lib/components/ui/Button.svelte
+++ src/frontend/src/lib/components/ui/Button.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	export let style: 'primary' | 'secondary' = 'primary';
+	export let colorStyle: 'primary' | 'secondary' = 'primary';
 	export let type: 'submit' | 'reset' | 'button' = 'submit';
 	export let disabled = false;
 </script>
 
-<button class={`${style} block flex-1`} {type} {disabled} on:click>
+<button class={`${colorStyle} block flex-1`} {type} {disabled} on:click>
 	<slot />
 </button>
