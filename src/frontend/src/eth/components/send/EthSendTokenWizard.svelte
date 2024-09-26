7e6324be02fa4a243149dd0d4ff7a815fd4fe7d5
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-09-26 19:31:29 +0200

    refactor(frontend): default type of Cancel and Back buttons is not submit (#2544)
    # Motivation
    
    As mentioned by @peterpeterparker , it doesn't make sense that the
    `ButtonCancel` and the `ButtonBack` components have `submit` type as
    default. Moreover, the few instances where they were used in a form,
    they were set as type `button`.
    
    So, we default both of them as type `button`.

--- src/frontend/src/eth/components/send/EthSendTokenWizard.svelte
+++ src/frontend/src/eth/components/send/EthSendTokenWizard.svelte
@@ -291,9 +291,9 @@
 		>
 			<svelte:fragment slot="cancel">
 				{#if formCancelAction === 'back'}
-					<ButtonBack type="button" on:click={back} />
+					<ButtonBack on:click={back} />
 				{:else}
-					<ButtonCancel type="button" on:click={close} />
+					<ButtonCancel on:click={close} />
 				{/if}
 			</svelte:fragment>
 		</SendForm>
