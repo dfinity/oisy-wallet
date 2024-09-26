6b9d8b6f1efb355a40f0570540fa4d35d065ebcf
Llorenç <llorenc.muntaner@dfinity.org>
2024-09-26 13:24:21 +0200

    feat(frontend): Add regtest to IcSendBtcNetwork

--- src/frontend/src/icp/components/send/IcSendBtcNetwork.svelte
+++ src/frontend/src/icp/components/send/IcSendBtcNetwork.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { BTC_MAINNET_NETWORK_ID } from '$env/networks.env';
-	import { BTC_MAINNET_TOKEN, BTC_TESTNET_TOKEN } from '$env/tokens.btc.env';
+	import { BTC_MAINNET_NETWORK_ID, BTC_TESTNET_NETWORK_ID } from '$env/networks.env';
+	import { BTC_MAINNET_TOKEN, BTC_REGTEST_TOKEN, BTC_TESTNET_TOKEN } from '$env/tokens.btc.env';
 	import type { NetworkId } from '$lib/types/network';
 
 	export let networkId: NetworkId;
@@ -8,6 +8,8 @@
 
 {#if BTC_MAINNET_NETWORK_ID === networkId}
 	{BTC_MAINNET_TOKEN.name}
-{:else}
+{:else if BTC_TESTNET_NETWORK_ID === networkId}
 	{BTC_TESTNET_TOKEN.name}
+{:else}
+	{BTC_REGTEST_TOKEN.name}
 {/if}
