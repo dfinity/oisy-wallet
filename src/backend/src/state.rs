f18002b79379c1d5f737c721d8f366844335703a
Max Murphy <max@winning.black>
2024-10-01 17:39:16 +0200

    format

--- src/backend/src/state.rs
+++ src/backend/src/state.rs
@@ -4,7 +4,6 @@ use lazy_static::lazy_static;
 const MAINNET_CYCLES_LEDGER_CANISTER_ID: &str = "um5iw-rqaaa-aaaaq-qaaba-cai";
 const MAINNET_SIGNER_CANISTER_ID: &str = "grghe-syaaa-aaaar-qabyq-cai";
 
-
 lazy_static! {
     pub static ref CYCLES_LEDGER: Principal = Principal::from_text(option_env!("CANISTER_ID_CYCLES_LEDGER").unwrap_or(MAINNET_CYCLES_LEDGER_CANISTER_ID))
         .unwrap_or_else(|e| unreachable!("The cycles canister ID from DFX and mainnet are valid and should have been parsed.  Is this being compiled in some strange way? {e}"));
