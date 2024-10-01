4842908aa4dd33671efc59f4340877fa920c8671
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-10-01 17:08:26 +0200

    feat(frontend): BTC transactions [GIX-2795] (#2547)
    # Motivation
    
    This is the base branch for BTC transactions feature.

---
+++ src/frontend/src/btc/types/btc.ts
@@ -0,0 +1,20 @@
+import type { TransactionType } from '$lib/types/transaction';
+
+export type BtcTransactionStatus = 'confirmed' | 'pending';
+
+export interface BtcTransactionUi {
+	id: string;
+	timestamp: number;
+	value?: bigint;
+	type: TransactionType;
+	status: BtcTransactionStatus;
+	from: string;
+	to?: string;
+	// The block number available only if a transaction has been confirmed
+	blockNumber?: number;
+
+	/* TODO: add one more field "confirmations", a number that represents the acceptance of a new block by the blockchain network.
+	 1. Use https://blockchain.info/latestblock to get info about the latest block height.
+	 2. Calculate confirmations: confirmations = currentBlockHeight - transactionBlockHeight + 1
+	 */
+}
