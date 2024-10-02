ea4166f01387d41f2a6b3cb7fa776f81ce1410c5
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-10-02 16:47:32 +0200

    style(frontend): change authenticate button label [GIX-3044] (#2618)
    # Motivation
    
    New label for `ButtonAuthenticate`.
    
    <img width="542" alt="Screenshot 2024-10-02 at 16 28 30"
    src="https://github.com/user-attachments/assets/0219ff3e-c3b1-4515-a83f-e5b0aeeae21c">
    <img width="420" alt="Screenshot 2024-10-02 at 16 28 39"
    src="https://github.com/user-attachments/assets/e672627b-4c3d-4b5a-b853-83fe6db4e41e">

--- src/frontend/src/lib/types/i18n.d.ts
+++ src/frontend/src/lib/types/i18n.d.ts
@@ -19,8 +19,6 @@ interface I18nCore {
 		more: string;
 		reject: string;
 		approve: string;
-		open: string;
-		create: string;
 	};
 	info: { test_banner: string };
 	alt: { logo: string; go_to_home: string };
@@ -38,7 +36,7 @@ interface I18nNavigation {
 }
 
 interface I18nAuth {
-	text: { title_part_1: string; title_part_2: string; logout: string };
+	text: { title_part_1: string; title_part_2: string; logout: string; authenticate: string };
 	alt: { sign_in: string };
 	warning: { not_signed_in: string; session_expired: string };
 	error: {
