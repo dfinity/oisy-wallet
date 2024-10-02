f60d89dd1ecee8a69495fefc4e153678edb156da
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2024-10-02 18:39:12 +0200

    fix(frontend): prefix btc-transactions derived stores

--- src/frontend/src/btc/components/transactions/BtcTransactions.svelte
+++ src/frontend/src/btc/components/transactions/BtcTransactions.svelte
@@ -2,21 +2,21 @@
 	import { slide } from 'svelte/transition';
 	import BtcTransaction from '$btc/components/transactions/BtcTransaction.svelte';
 	import {
-		sortedTransactions,
-		transactionsNotInitialized
+		sortedBtcTransactions,
+		btcTransactionsNotInitialized
 	} from '$btc/derived/btc-transactions.derived';
 	import TokensSkeletons from '$lib/components/tokens/TokensSkeletons.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 </script>
 
-<TokensSkeletons loading={$transactionsNotInitialized}>
-	{#each $sortedTransactions as transaction (transaction.data.id)}
+<TokensSkeletons loading={$btcTransactionsNotInitialized}>
+	{#each $sortedBtcTransactions as transaction (transaction.data.id)}
 		<div transition:slide={{ duration: 250 }}>
 			<BtcTransaction transaction={transaction.data} />
 		</div>
 	{/each}
 
-	{#if $sortedTransactions.length === 0}
+	{#if $sortedBtcTransactions.length === 0}
 		<p class="text-secondary mt-4 opacity-50">{$i18n.transactions.text.no_transactions}</p>
 	{/if}
 </TokensSkeletons>
