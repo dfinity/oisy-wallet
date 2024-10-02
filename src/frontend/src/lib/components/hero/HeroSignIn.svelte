5b0f13bb2b219b7a213c42919c1740c425ab8145
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-10-02 16:11:57 +0200

    style(frontend): remove crypto logos from the header when signed out [GIX-3044] (#2616)
    # Motivation
    
    No more crypto logos in the header when signed out.
    
    <img width="1329" alt="Screenshot 2024-10-02 at 15 52 51"
    src="https://github.com/user-attachments/assets/5e18c393-0a34-4e31-887c-f751efaec5ec">
    <img width="431" alt="Screenshot 2024-10-02 at 15 52 58"
    src="https://github.com/user-attachments/assets/97cae368-d704-4b31-81eb-5441d7e6ba6e">

--- src/frontend/src/lib/components/hero/HeroSignIn.svelte
+++ src/frontend/src/lib/components/hero/HeroSignIn.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
-	import IconHeaderTokens from '$lib/components/icons/IconHeaderTokens.svelte';
 	import ButtonAuthenticate from '$lib/components/ui/ButtonAuthenticate.svelte';
 	import { signIn } from '$lib/services/auth.services';
 	import { i18n } from '$lib/stores/i18n.store';
 </script>
-
-<div class="mt-6 flex xl:mt-12">
-	<IconHeaderTokens />
-</div>
 
 <div class="mb-7 mt-5 pt-2">
 	<h1 class="text-center text-4xl">
