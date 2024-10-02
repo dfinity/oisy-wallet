626ab59af605174d6a7ad3c8dc6473fedadf0a1b
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-02 16:37:02 +0200

    feat(frontend): add Authenticate icon button in header when sigend out

--- src/frontend/src/lib/components/hero/Header.svelte
+++ src/frontend/src/lib/components/hero/Header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import WalletConnect from '$eth/components/wallet-connect/WalletConnect.svelte';
+	import SignIn from '$lib/components/auth/SignIn.svelte';
 	import Alpha from '$lib/components/core/Alpha.svelte';
 	import Back from '$lib/components/core/Back.svelte';
 	import Menu from '$lib/components/core/Menu.svelte';
@@ -47,6 +48,8 @@
 			<Menu />
 		{:else}
 			<AboutMenu />
+
+			<SignIn />
 		{/if}
 	</div>
 </header>
