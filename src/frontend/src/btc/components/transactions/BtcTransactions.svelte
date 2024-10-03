690fd87a42b87859eb0367917f46028e337d7985
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-10-03 09:25:39 +0200

    fix(frontend): BTC transaction modal (#2613)
    # Motivation
    
    The goal is to have the BTC modal implemented with basic info (e.g. it's
    missing confirmations number, or a proper component for displaying a tx
    status). Also, I added a common `TransactionModal` which will be later
    for IC and ETH cases too.
    
    The further data will be added in separate PRs.
    
    
    <img width="480" alt="Screenshot 2024-10-02 at 13 21 35"
    src="https://github.com/user-attachments/assets/b42f8921-1aa4-47db-a1da-a96e9468240a">

--- src/frontend/src/btc/components/transactions/BtcTransactions.svelte
+++ src/frontend/src/btc/components/transactions/BtcTransactions.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
+	import { nonNullish } from '@dfinity/utils';
 	import { slide } from 'svelte/transition';
 	import BtcTransaction from '$btc/components/transactions/BtcTransaction.svelte';
+	import BtcTransactionModal from '$btc/components/transactions/BtcTransactionModal.svelte';
 	import {
 		sortedBtcTransactions,
 		btcTransactionsNotInitialized
 	} from '$btc/derived/btc-transactions.derived';
+	import type { BtcTransactionUi } from '$btc/types/btc';
 	import TokensSkeletons from '$lib/components/tokens/TokensSkeletons.svelte';
+	import { modalBtcTransaction } from '$lib/derived/modal.derived';
 	import { i18n } from '$lib/stores/i18n.store';
+	import { modalStore } from '$lib/stores/modal.store';
+
+	let selectedTransaction: BtcTransactionUi | undefined;
+	$: selectedTransaction = $modalBtcTransaction
+		? ($modalStore?.data as BtcTransactionUi | undefined)
+		: undefined;
 </script>
 
 <TokensSkeletons loading={$btcTransactionsNotInitialized}>
@@ -20,3 +30,7 @@
 		<p class="text-secondary mt-4 opacity-50">{$i18n.transactions.text.no_transactions}</p>
 	{/if}
 </TokensSkeletons>
+
+{#if $modalBtcTransaction && nonNullish(selectedTransaction)}
+	<BtcTransactionModal transaction={selectedTransaction} />
+{/if}
