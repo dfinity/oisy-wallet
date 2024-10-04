cdc2a87145a7a8ddb2818b404f0201918d74e569
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-04 13:03:10 +0200

    feat: remove onramper feature flag

--- src/frontend/src/env/onramper.env.ts
+++ src/frontend/src/env/onramper.env.ts
@@ -1,8 +1,5 @@
 import { LOCAL, STAGING } from '$lib/constants/app.constants';
 
-// TODO: to be removed when we have finished implementing all the OnRamper features
-export const ONRAMPER_ENABLED = JSON.parse(import.meta.env.VITE_ONRAMPER_ENABLED ?? false) === true;
-
 const ONRAMPER_ENV: 'dev' | 'prod' = LOCAL || STAGING ? 'dev' : 'prod';
 
 export const isOnRamperDev = ONRAMPER_ENV === 'dev';
