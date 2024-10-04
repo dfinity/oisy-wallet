c59a61b664a1e0e239392f149e27fbd5f0a3cbc2
David Dal Busco <david.dalbusco@outlook.com>
2024-10-04 07:24:34 +0200

    feat(frontend): handle allow signing errors

--- src/frontend/src/lib/canisters/backend.errors.ts
+++ src/frontend/src/lib/canisters/backend.errors.ts
@@ -1,8 +1,10 @@
 import type {
+	AllowSigningError,
 	BtcAddPendingTransactionError,
 	SelectedUtxosFeeError
 } from '$declarations/backend/backend.did';
 import { CanisterInternalError } from '$lib/canisters/errors';
+import { mapIcrc2ApproveError } from '@dfinity/ledger-icp';
 
 export const mapBtcPendingTransactionError = (response: BtcAddPendingTransactionError) => {
 	if ('InternalError' in response) {
@@ -24,4 +26,20 @@ export const mapBtcSelectUserUtxosFeeError = (response: SelectedUtxosFeeError) =
 	}
 
 	return new CanisterInternalError('Unknown BtcSelectUserUtxosFeeError');
+};
+
+export const mapAllowSigningError = (err: AllowSigningError) => {
+	if ('ApproveError' in err) {
+		return mapIcrc2ApproveError(err.ApproveError);
+	}
+
+	if ('FailedToContactCyclesLedger' in err) {
+		return new CanisterInternalError('The Cycles Ledger cannot be contacted.');
+	}
+
+	if ('Other' in err) {
+		return new CanisterInternalError(err.Other);
+	}
+
+	return new CanisterInternalError('Unknown AllowSigningError');
 };
