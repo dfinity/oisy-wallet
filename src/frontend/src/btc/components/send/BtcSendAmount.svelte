3c412ba2531946b9148667c5f64d9f9494e00529
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-10-04 11:03:20 +0200

    feat(frontend): implement BtcSendAmount component (#2653)
    # Motivation
    
    As a part of BTC send UX flow, we need to implement `BtcSendAmount`.

---
+++ src/frontend/src/btc/components/send/BtcSendAmount.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { BtcAmountAssertionError } from '$btc/types/btc-send';
+	import SendInputAmount from '$lib/components/send/SendInputAmount.svelte';
+	import { tokenDecimals } from '$lib/derived/token.derived';
+
+	export let amount: number | undefined = undefined;
+	export let amountError: BtcAmountAssertionError | undefined;
+</script>
+
+<SendInputAmount bind:amount tokenDecimals={$tokenDecimals} bind:error={amountError} />
