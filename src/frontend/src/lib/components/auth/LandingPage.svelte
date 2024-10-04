5f7c8c5a4f6fa48fc4ef2346d3e0872a0c88ef5e
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-04 19:11:29 +0200

    style: cleanup landing page

--- src/frontend/src/lib/components/auth/LandingPage.svelte
+++ src/frontend/src/lib/components/auth/LandingPage.svelte
@@ -11,15 +11,15 @@
 </script>
 
 <div
-	class="m-auto flex h-full w-full max-w-screen-2.5xl flex-col items-start gap-12 overflow-hidden px-5 md:grid md:grid-cols-2 md:grid-rows-1 md:gap-8 md:overflow-visible"
+	class="m-auto h-full w-full max-w-screen-2.5xl items-start gap-12 px-5 md:grid md:grid-cols-2 md:grid-rows-1 md:gap-8 md:overflow-visible"
 >
-	<div class="h-full w-full content-center md:flex-1 md:pl-8 md:pt-12">
+	<div class="w-full content-center md:h-full md:flex-1 md:pl-8 md:pt-12">
 		<HeroSignIn />
 	</div>
 
 	<!-- TODO: determine if this value is specific/permanent or can be changed -->
 	<div
-		class="ml-auto min-w-[1127px] md:m-0 md:flex md:h-full md:content-center md:items-center md:pt-12"
+		class="ml-auto min-w-[1127px] pt-12 md:m-0 md:flex md:h-full md:content-center md:items-center"
 	>
 		<picture aria-label={ariaLabel} class="w-full md:mt-auto" role="presentation">
 			<source srcset={previewSmall} media="(max-width: 767px)" />
