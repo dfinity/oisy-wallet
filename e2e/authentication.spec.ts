e8ceed11894982bc0fadc7f9bb7f8241a6a7e6af
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-05 15:52:48 +0200

    chore: adjust test title

--- e2e/authentication.spec.ts
+++ e2e/authentication.spec.ts
@@ -9,7 +9,7 @@ testWithII('should sign-in', async ({ page, iiPage }) => {
 	await homepageLoggedIn.waitForLoggedInIndicator();
 });
 
-testWithII('should stay logged in after an interval', async ({ page, iiPage }) => {
+testWithII('should stay signed in after an interval', async ({ page, iiPage }) => {
 	const homepageLoggedIn = new HomepageLoggedIn({ page, iiPage });
 
 	await homepageLoggedIn.waitForAuthentication();
