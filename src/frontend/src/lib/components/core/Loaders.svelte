064537cf9f6dc6e6831040e131fca8823c0d0fd1
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-07 01:21:59 +0200

    Adjust wallets.

--- src/frontend/src/lib/components/core/Loaders.svelte
+++ src/frontend/src/lib/components/core/Loaders.svelte
@@ -1,22 +1,19 @@
 <script lang="ts">
-	import BtcLoaderWallets from '$btc/components/core/BtcLoaderWallets.svelte';
-	import IcLoaderWallets from '$icp/components/core/IcLoaderWallets.svelte';
 	import LoaderBalances from '$icp-eth/components/core/LoaderBalances.svelte';
 	import Loader from '$lib/components/core/Loader.svelte';
 	import LoaderMetamask from '$lib/components/core/LoaderMetamask.svelte';
+	import LoaderWallets from '$lib/components/core/LoaderWallets.svelte';
 	import AddressGuard from '$lib/components/guard/AddressGuard.svelte';
 </script>
 
 <AddressGuard>
 	<Loader>
 		<LoaderBalances>
-			<IcLoaderWallets>
-				<BtcLoaderWallets>
-					<LoaderMetamask>
-						<slot />
-					</LoaderMetamask>
-				</BtcLoaderWallets>
-			</IcLoaderWallets>
+			<LoaderWallets>
+				<LoaderMetamask>
+					<slot />
+				</LoaderMetamask>
+			</LoaderWallets>
 		</LoaderBalances>
 	</Loader>
 </AddressGuard>
