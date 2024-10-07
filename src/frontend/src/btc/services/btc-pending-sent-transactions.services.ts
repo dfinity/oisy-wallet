c093f5e20d019eaff5226658dd3bf5ee129ddacf
Llorenç <llorenc.muntaner@dfinity.org>
2024-10-07 16:14:42 +0200

    return result success

--- src/frontend/src/btc/services/btc-pending-sent-transactions.services.ts
+++ src/frontend/src/btc/services/btc-pending-sent-transactions.services.ts
@@ -1,8 +1,10 @@
 import { btcPendingSentTransactionsStore } from '$btc/stores/btc-pending-sent-transactions.store';
 import { getPendingBtcTransactions } from '$lib/api/backend.api';
 import type { NetworkId } from '$lib/types/network';
+import type { ResultSuccess } from '$lib/types/utils';
 import { mapNetworkIdToBitcoinNetwork, mapToSignerBitcoinNetwork } from '$lib/utils/network.utils';
 import type { Identity } from '@dfinity/agent';
+import { isNullish } from '@dfinity/utils';
 
 export const loadBtcPendingSentTransactions = async ({
 	address,
@@ -12,10 +14,10 @@ export const loadBtcPendingSentTransactions = async ({
 	address: string;
 	identity: Identity;
 	networkId: NetworkId;
-}) => {
+}): Promise<ResultSuccess> => {
 	try {
 		const network = mapNetworkIdToBitcoinNetwork(networkId);
-		if (!network) {
+		if (isNullish(network)) {
 			throw new Error(`Invalid networkId: ${networkId.toString}`);
 		}
 		const pendingTransactions = await getPendingBtcTransactions({
@@ -27,8 +29,10 @@ export const loadBtcPendingSentTransactions = async ({
 			address,
 			pendingTransactions
 		});
+		return { success: true };
 	} catch (err: unknown) {
 		console.error('Error loading pending transactions', err);
 		btcPendingSentTransactionsStore.setPendingTransactionsError({ address });
+		return { success: false, err };
 	}
 };
