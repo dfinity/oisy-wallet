2356708e4c1683bca24aa81e7e8e61c9e03e9f19
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-07 20:41:48 +0200

    style(frontend): change network switcher menu style

--- src/frontend/src/lib/components/ui/ButtonSwitcher.svelte
+++ src/frontend/src/lib/components/ui/ButtonSwitcher.svelte
@@ -1,16 +1,11 @@
 <script lang="ts">
+	import DropdownButton from '$lib/components/ui/DropdownButton.svelte';
+
 	export let disabled = false;
 	export let button: HTMLButtonElement | undefined = undefined;
 	export let ariaLabel: string;
 </script>
 
-<button
-	class="text-primary w-full text-left font-bold no-underline"
-	bind:this={button}
-	on:click
-	aria-label={ariaLabel}
-	{disabled}
-	class:opacity-50={disabled}
->
+<DropdownButton bind:button on:click {ariaLabel} {disabled}>
 	<slot />
-</button>
+</DropdownButton>
---
+++ src/frontend/src/lib/components/ui/DropdownButton.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { IconExpandMore } from '@dfinity/gix-components';
+
+	export let disabled = false;
+	export let button: HTMLButtonElement | undefined = undefined;
+	export let ariaLabel: string;
+	export let opened = false;
+</script>
+
+<button
+	class="text-secondary min-w-72 justify-between gap-2 rounded-xl border border-light-grey bg-white px-4 py-3 text-left font-medium leading-5 text-inherit hover:border-blue-ribbon"
+	bind:this={button}
+	on:click
+	aria-label={ariaLabel}
+	{disabled}
+	class:opacity-50={disabled}
+	class:border={opened}
+	class:border-blue-ribbon={opened}
+>
+	<slot />
+	<div
+		class="transform transition-transform duration-300 ease-in-out"
+		class:scale-y-[-1]={opened}
+		class:scale-y-[1]={!opened}
+	>
+		<IconExpandMore size="24" />
+	</div>
+</button>
