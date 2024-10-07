cad186df3ad3697ca5cd248d17f4e4aa19c0155a
David Dal Busco <david.dalbusco@outlook.com>
2024-10-07 06:33:25 +0200

    chore: lint

--- src/frontend/src/lib/components/guard/AddressGuard.svelte
+++ src/frontend/src/lib/components/guard/AddressGuard.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { NETWORK_BITCOIN_ENABLED } from '$env/networks.btc.env';
 	import { validateBtcAddressMainnet, validateEthAddress } from '$lib/services/address.services';
-	import { signOut } from '$lib/services/auth.services';
 	import { initSignerAllowance } from '$lib/services/loader.services';
 	import { btcAddressMainnetStore, ethAddressStore } from '$lib/stores/address.store';
 
