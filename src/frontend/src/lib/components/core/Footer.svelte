0f8571e4c5d138b7d45b2a34a60b2c1236ecf99b
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-10-07 12:31:26 +0200

    feat(frontend): add footer in landing page [GIX-3049] (#2651)
    # Motivation
    
    Unfortunately a big PR...
    
    We include a new footer in Oisy: it will contain mainly hyperlinks to
    social pages and DFINITY. The real big job is the main layout. The idea
    is:
    
    - It will be always at the bottom, never overlapping
    - For larger screens, it should decrease in width and stay on the left
    IF it is below the preview image of the landing page.
    - For smaller screens, it will always stay at the bottom.
    
    
    # Additional Note
    
    The Oisy twitter link is actually DFINITY for now.
    
    # Changes
    
    - Create DFINITY and X (Twitter) icons.
    - Add strings in `i18n`.
    - Create `Footer` component containing:
      - Github hyperlink
      - X (Twitter) hyperlink
      - A string redirecting to DFINITY homepage
    - Create a new Tailwind media threshold for height, set to be the
    preview image height plus a buffer.
    - Adjust `Footer` style to have specific behaviors, using the above
    threshold.
    - To add the `Footer` in `+layout` we need to adjust the other
    components:
    - Wrap `HeroSignIn` in a container that will auto-adjust the the height
    using flex.
    - Adjust `LandingPage` to expand and occupy the entire height of its
    parent.
    - Adjust the picture inside `LandingPage` to have different spacing
    based on screen height.
    - Finally, wrap `+layout` in a container with min height and and column
    display for larger screens, and add `Footer`.
    
    # Test
    
    To make it easier (for me), I recorded a series of screens (desktop,
    mobile and responsive), both logged out and logged in.
    
    ### Logged out
    
    <img width="2370" alt="Screenshot 2024-10-04 at 20 14 47"
    src="https://github.com/user-attachments/assets/ba63b201-d875-4323-8918-5bd182b7a1fc">
    <img width="1029" alt="Screenshot 2024-10-04 at 20 14 53"
    src="https://github.com/user-attachments/assets/cf60880a-aad5-4e52-a6f8-127b09a2b7ac">
    
    
    https://github.com/user-attachments/assets/8ef57b9a-c676-4797-ba6a-c4e980d3b706
    
    
    https://github.com/user-attachments/assets/aad54009-1b1e-456d-aa37-9d12633e66df
    
    ### Logged in
    
    <img width="2367" alt="Screenshot 2024-10-04 at 20 19 26"
    src="https://github.com/user-attachments/assets/ad594cb0-49a4-4a05-87c7-f563b16b65f7">
    <img width="1030" alt="Screenshot 2024-10-04 at 20 19 36"
    src="https://github.com/user-attachments/assets/fa0b60ab-1bc4-499f-940b-4e65c8beec87">
    
    
    https://github.com/user-attachments/assets/171e1bc0-fb92-4b46-9ce2-bda2dce15dd8
    
    
    https://github.com/user-attachments/assets/83832ff1-32b5-46f5-a47f-401bc0313491

---
+++ src/frontend/src/lib/components/core/Footer.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import { IconGitHub } from '@dfinity/gix-components';
+	import IconDfinity from '$lib/components/icons/IconDfinity.svelte';
+	import IconTwitter from '$lib/components/icons/IconTwitter.svelte';
+	import ExternalLink from '$lib/components/ui/ExternalLink.svelte';
+	import { OISY_REPO_URL } from '$lib/constants/oisy.constants';
+	import { authNotSignedIn } from '$lib/derived/auth.derived';
+	import { i18n } from '$lib/stores/i18n.store';
+	import { replaceOisyPlaceholders } from '$lib/utils/i18n.utils';
+
+	// TODO: set up a correct twitter account for Oisy and move this value to the oisy.contants module.
+	const OISY_TWITTER_URL = 'https://x.com/dfinity';
+</script>
+
+<footer
+	class="z-1 mx-auto flex w-full max-w-screen-2.5xl flex-1 flex-col items-center justify-between px-4 pt-6 md:flex-1 md:flex-grow md:flex-row md:px-8"
+	class:md:sticky={$authNotSignedIn}
+	class:md:h-md:mx-0={$authNotSignedIn}
+	class:md:h-md:w-[50%]={$authNotSignedIn}
+	class:md:h-md:pr-0={$authNotSignedIn}
+>
+	<div class="flex flex-row items-center gap-4">
+		<a
+			href={OISY_REPO_URL}
+			rel="external noopener noreferrer"
+			target="_blank"
+			class="rounded-full bg-white p-2"
+			aria-label={$i18n.navigation.text.source_code_on_github}
+		>
+			<IconGitHub />
+		</a>
+
+		<a
+			href={OISY_TWITTER_URL}
+			rel="external noopener noreferrer"
+			target="_blank"
+			class="rounded-full bg-white p-2"
+			aria-label={$i18n.navigation.text.source_code_on_github}
+		>
+			<IconTwitter />
+		</a>
+	</div>
+
+	<div class="flex flex-row items-center gap-2 text-sm">
+		<IconDfinity />
+		{$i18n.footer.text.copyright}
+		<span>
+			{replaceOisyPlaceholders($i18n.footer.text.developed_with)}
+			<ExternalLink
+				href="https://dfinity.org"
+				ariaLabel={replaceOisyPlaceholders($i18n.footer.alt.dfinity)}
+				color="blue"
+				iconVisible={false}
+			>
+				{replaceOisyPlaceholders($i18n.footer.text.dfinity)}
+			</ExternalLink>
+		</span>
+	</div>
+</footer>
