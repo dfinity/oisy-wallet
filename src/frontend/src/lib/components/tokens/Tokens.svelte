b3efe14a4f63461178f42bce3ca9943c1ab55f57
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-08 18:43:39 +0200

    refactor(frontend): move the Zero Balance trigger in the token list

--- src/frontend/src/lib/components/tokens/Tokens.svelte
+++ src/frontend/src/lib/components/tokens/Tokens.svelte
@@ -2,9 +2,9 @@
 	import { fade } from 'svelte/transition';
 	import NetworksSwitcher from '$lib/components/networks/NetworksSwitcher.svelte';
 	import ManageTokensButton from '$lib/components/tokens/ManageTokensButton.svelte';
-	import TokensMenu from '$lib/components/tokens/TokensMenu.svelte';
 	import TokensSignedIn from '$lib/components/tokens/TokensSignedIn.svelte';
 	import TokensSignedOut from '$lib/components/tokens/TokensSignedOut.svelte';
+	import TokensZeroBalanceCheckbox from '$lib/components/tokens/TokensZeroBalanceCheckbox.svelte';
 	import Header from '$lib/components/ui/Header.svelte';
 	import { authNotSignedIn, authSignedIn } from '$lib/derived/auth.derived';
 </script>
@@ -13,7 +13,7 @@
 	<Header>
 		<NetworksSwitcher disabled={$authNotSignedIn} />
 
-		<TokensMenu slot="end" />
+		<TokensZeroBalanceCheckbox slot="end" />
 	</Header>
 
 	{#if $authSignedIn}
