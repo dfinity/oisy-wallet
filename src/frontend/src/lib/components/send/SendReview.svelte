942db7f59cefb928ed476558b701f30e4ca2b3cf
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2024-10-08 10:55:42 +0200

    feat(frontend): use send store

--- src/frontend/src/lib/components/send/SendReview.svelte
+++ src/frontend/src/lib/components/send/SendReview.svelte
@@ -1,28 +1,27 @@
 <script lang="ts">
 	import { nonNullish } from '@dfinity/utils';
-	import { createEventDispatcher } from 'svelte';
+	import { createEventDispatcher, getContext } from 'svelte';
+	import { SEND_CONTEXT_KEY, type SendContext } from '$icp-eth/stores/send.store';
 	import SendData from '$lib/components/send/SendData.svelte';
 	import Button from '$lib/components/ui/Button.svelte';
 	import ButtonBack from '$lib/components/ui/ButtonBack.svelte';
 	import ButtonGroup from '$lib/components/ui/ButtonGroup.svelte';
 	import ContentWithToolbar from '$lib/components/ui/ContentWithToolbar.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
-	import type { OptionBalance } from '$lib/types/balance';
-	import type { OptionToken } from '$lib/types/token';
 
 	export let destination = '';
 	export let amount: number | undefined = undefined;
 	export let source: string;
-	export let token: OptionToken;
-	export let balance: OptionBalance;
 	export let disabled: boolean | undefined = false;
 
 	const dispatch = createEventDispatcher();
+
+	const { sendToken, sendBalance } = getContext<SendContext>(SEND_CONTEXT_KEY);
 </script>
 
 <ContentWithToolbar>
-	{#if nonNullish(token)}
-		<SendData {amount} {destination} {token} {balance} {source}>
+	{#if nonNullish($sendToken)}
+		<SendData {amount} {destination} token={$sendToken} balance={$sendBalance} {source}>
 			<slot name="fee" slot="fee" />
 
 			<slot name="network" slot="network" />
