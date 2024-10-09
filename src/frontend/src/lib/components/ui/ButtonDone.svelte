1863adb8d2bf5d3c5815cb098a5447fd266e152a
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-09 19:19:29 +0200

    chore: remove redundant prop

--- src/frontend/src/lib/components/ui/ButtonDone.svelte
+++ src/frontend/src/lib/components/ui/ButtonDone.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
 	import Button from '$lib/components/ui/Button.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
-
-	export let type: 'submit' | 'reset' | 'button' = 'submit';
 </script>
 
-<Button {type} fullWidth on:click>
+<Button fullWidth type="button" on:click>
 	{$i18n.core.text.done}
 </Button>
