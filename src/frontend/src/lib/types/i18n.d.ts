265eafd5e5b8da6ff4f87a5a1e6ecaa7dc694ad1
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-09 09:50:47 +0200

    feat(frontend): Remove Alpha warning

--- src/frontend/src/lib/types/i18n.d.ts
+++ src/frontend/src/lib/types/i18n.d.ts
@@ -127,7 +127,7 @@ interface I18nInit {
 }
 
 interface I18nHero {
-	text: { use_with_caution: string; learn_more_about_erc20_icp: string };
+	text: { learn_more_about_erc20_icp: string };
 }
 
 interface I18nSettings {
