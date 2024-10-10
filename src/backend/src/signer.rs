8c5266526729ef00869f9fe9c2fcb99ffe3ece9a
Llorenç <llorenc.muntaner@dfinity.org>
2024-10-10 14:51:29 +0200

    Fix backend test

--- src/backend/src/signer.rs
+++ src/backend/src/signer.rs
@@ -128,9 +128,7 @@ pub async fn btc_principal_to_p2wpkh_address(
     network: BitcoinNetwork,
     principal: &Principal,
 ) -> Result<String, String> {
-    let ecdsa_pubkey = cfs_ecdsa_pubkey_of(principal)
-        .await
-        .map_err(|_| "Error getting ECDSA public key".to_string())?;
+    let ecdsa_pubkey = cfs_ecdsa_pubkey_of(principal).await?;
     if let Ok(compressed_public_key) = CompressedPublicKey::from_slice(&ecdsa_pubkey) {
         Ok(Address::p2wpkh(&compressed_public_key, transform_network(network)).to_string())
     } else {
