e48376021e617197b00c52803f16127e2695b20e
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-10-10 09:37:11 +0200

    feat(frontend): calculate max BTC amount on send (#2764)
    # Motivation
    
    Calculate max BTC amount on send, for now without fees.

--- src/frontend/src/btc/components/send/BtcSendAmount.svelte
+++ src/frontend/src/btc/components/send/BtcSendAmount.svelte
@@ -1,10 +1,31 @@
 <script lang="ts">
+	import { isNullish } from '@dfinity/utils';
+	import { getContext } from 'svelte';
 	import type { BtcAmountAssertionError } from '$btc/types/btc-send';
+	import { SEND_CONTEXT_KEY, type SendContext } from '$icp-eth/stores/send.store';
 	import SendInputAmount from '$lib/components/send/SendInputAmount.svelte';
 	import { tokenDecimals } from '$lib/derived/token.derived';
+	import { getMaxTransactionAmount } from '$lib/utils/token.utils';
 
 	export let amount: number | undefined = undefined;
 	export let amountError: BtcAmountAssertionError | undefined;
+
+	const { sendToken, sendBalance } = getContext<SendContext>(SEND_CONTEXT_KEY);
+
+	$: calculateMax = (): number | undefined =>
+		isNullish($sendToken)
+			? undefined
+			: // TODO: Add fee to the calculation
+				getMaxTransactionAmount({
+					balance: $sendBalance ?? undefined,
+					tokenDecimals: $sendToken.decimals,
+					tokenStandard: $sendToken.standard
+				});
 </script>
 
-<SendInputAmount bind:amount tokenDecimals={$tokenDecimals} bind:error={amountError} />
+<SendInputAmount
+	bind:amount
+	tokenDecimals={$tokenDecimals}
+	bind:error={amountError}
+	{calculateMax}
+/>
