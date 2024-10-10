73d6a2318a634e045507146cf987e49cfebf9fa1
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-10 18:41:01 +0200

    style(frontend): Use Iconly library for icons in actions

--- src/frontend/src/lib/components/send/SendButton.svelte
+++ src/frontend/src/lib/components/send/SendButton.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import IconSend from '$lib/components/icons/IconSend.svelte';
+	import IconlySend from '$lib/components/icons/iconly/IconlySend.svelte';
 	import ButtonHero from '$lib/components/ui/ButtonHero.svelte';
 	import { isBusy } from '$lib/derived/busy.derived';
 	import { i18n } from '$lib/stores/i18n.store';
 </script>
 
 <ButtonHero on:click disabled={$isBusy} ariaLabel={$i18n.send.text.send}>
-	<IconSend size="28" slot="icon" />
+	<IconlySend size="28" slot="icon" />
 	{$i18n.send.text.send}
 </ButtonHero>
