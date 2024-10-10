c31233a8bf6957cb63683e59fc46f3e5544a432f
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-10 17:11:27 +0200

    refactor: move it inside div

--- src/frontend/src/lib/components/tokens/Tokens.svelte
+++ src/frontend/src/lib/components/tokens/Tokens.svelte
@@ -2,9 +2,9 @@
 	import { fade } from 'svelte/transition';
 	import NetworksSwitcher from '$lib/components/networks/NetworksSwitcher.svelte';
 	import ManageTokensButton from '$lib/components/tokens/ManageTokensButton.svelte';
+	import TokensMenu from '$lib/components/tokens/TokensMenu.svelte';
 	import TokensSignedIn from '$lib/components/tokens/TokensSignedIn.svelte';
 	import TokensSignedOut from '$lib/components/tokens/TokensSignedOut.svelte';
-	import TokensZeroBalanceCheckbox from '$lib/components/tokens/TokensZeroBalanceCheckbox.svelte';
 	import Header from '$lib/components/ui/Header.svelte';
 	import { authNotSignedIn, authSignedIn } from '$lib/derived/auth.derived';
 </script>
@@ -13,13 +13,13 @@
 	<Header>
 		<NetworksSwitcher disabled={$authNotSignedIn} />
 
-		<TokensZeroBalanceCheckbox slot="end" />
+		<TokensMenu slot="end" />
 	</Header>
 
 	{#if $authSignedIn}
 		<TokensSignedIn />
 
-		<div transition:fade class="mb-4 mt-12 flex w-full justify-center sm:w-auto">
+		<div transition:fade class="mb-4 mt-12 hidden w-full justify-center sm:flex sm:w-auto">
 			<ManageTokensButton />
 		</div>
 	{:else}
--- src/frontend/src/lib/components/tokens/TokensMenu.svelte
+++ src/frontend/src/lib/components/tokens/TokensMenu.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-	import { IconDots, Popover } from '@dfinity/gix-components';
+	import { Popover } from '@dfinity/gix-components';
 	import { erc20UserTokensNotInitialized } from '$eth/derived/erc20.derived';
+	import IconManage from '$lib/components/icons/IconManage.svelte';
 	import ManageTokensMenuButton from '$lib/components/manage/ManageTokensMenuButton.svelte';
 	import TokensZeroBalance from '$lib/components/tokens/TokensZeroBalance.svelte';
+	import TokensZeroBalanceCheckbox from '$lib/components/tokens/TokensZeroBalanceCheckbox.svelte';
+	import ButtonIcon from '$lib/components/ui/ButtonIcon.svelte';
 	import Hr from '$lib/components/ui/Hr.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 
@@ -10,25 +13,30 @@
 	let button: HTMLButtonElement | undefined;
 </script>
 
-<button
-	class="icon"
-	bind:this={button}
-	on:click={() => (visible = true)}
-	aria-label={$i18n.navigation.alt.menu}
-	disabled={$erc20UserTokensNotInitialized}
-	class:opacity-10={$erc20UserTokensNotInitialized}
->
-	<IconDots size="24" />
-</button>
+<div class="hidden sm:block">
+	<TokensZeroBalanceCheckbox />
+</div>
 
-<Popover bind:visible anchor={button} invisibleBackdrop direction="rtl">
-	<div class="flex flex-col gap-3">
-		<TokensZeroBalance />
+<div class="block sm:hidden">
+	<ButtonIcon
+		bind:button
+		on:click={() => (visible = true)}
+		ariaLabel={$i18n.navigation.alt.menu}
+		disabled={$erc20UserTokensNotInitialized}
+		link={false}
+	>
+		<IconManage slot="icon" />
+	</ButtonIcon>
 
-		<div class="my">
-			<Hr />
+	<Popover bind:visible anchor={button} invisibleBackdrop direction="rtl">
+		<div class="flex flex-col gap-3">
+			<TokensZeroBalance />
+
+			<div class="my">
+				<Hr />
+			</div>
+
+			<ManageTokensMenuButton on:icCloseMenu={() => (visible = false)} />
 		</div>
-
-		<ManageTokensMenuButton on:icCloseMenu={() => (visible = false)} />
-	</div>
-</Popover>
+	</Popover>
+</div>
