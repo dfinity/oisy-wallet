19feae083d521ba92493fbcebf3b8717d59ed0b6
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-10-10 14:22:47 +0200

    style(frontend): New style for Manage button [GIX-3046] (#2778)
    # Motivation
    
    We update the style for the Manage button in the main page.
    
    ### Before
    
    <img width="1279" alt="Screenshot 2024-10-10 at 11 06 19"
    src="https://github.com/user-attachments/assets/6133152e-79bf-404b-ae44-8f332dfeddcd">
    <img width="376" alt="Screenshot 2024-10-10 at 11 06 13"
    src="https://github.com/user-attachments/assets/b57066be-9943-4dee-bf3b-880095f58314">
    
    
    ### After
    
    <img width="1278" alt="Screenshot 2024-10-10 at 11 05 13"
    src="https://github.com/user-attachments/assets/c5ab982f-fcc0-4e8f-8573-60838813f487">
    <img width="375" alt="Screenshot 2024-10-10 at 11 05 24"
    src="https://github.com/user-attachments/assets/6f29688b-cdf2-477e-a64c-4d9416f247e8">

--- src/frontend/src/lib/types/i18n.d.ts
+++ src/frontend/src/lib/types/i18n.d.ts
@@ -361,7 +361,7 @@ interface I18nTokens {
 	manage: {
 		text: {
 			title: string;
-			your_tokens: string;
+			manage_list: string;
 			do_not_see_import: string;
 			clear_filter: string;
 			manage_for_network: string;
