1b3f11461c1cf41aa96f2a2874cccead8ab63c1a
Llorenç <llorenc.muntaner@dfinity.org>
2024-10-10 12:49:32 +0200

    CR changes

---
+++ src/frontend/src/btc/components/send/BtcSendHasPendingTransactions.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import WarningBanner from '$lib/components/ui/WarningBanner.svelte';
+	import { fade } from 'svelte/transition';
+	import { i18n } from '$lib/stores/i18n.store';
+
+	export let pendingTransactionsStatus: boolean | 'loading' | 'error';
+</script>
+
+<div in:fade>
+	{#if pendingTransactionsStatus === true || true}
+		<WarningBanner>{$i18n.send.info.pending_bitcoin_transaction}</WarningBanner>
+	{:else if pendingTransactionsStatus === 'error'}
+		<WarningBanner>{$i18n.send.error.no_pending_bitcoin_transaction}</WarningBanner>
+	{/if}
+</div>
--- src/frontend/src/btc/components/send/BtcSendReview.svelte
+++ src/frontend/src/btc/components/send/BtcSendReview.svelte
@@ -2,11 +2,10 @@
 	import type { Readable } from 'svelte/store';
 	import { initHasPendingSentTransactions } from '$btc/derived/has-pending-sent-transactions.derived';
 	import SendReview from '$lib/components/send/SendReview.svelte';
-	import WarningBanner from '$lib/components/ui/WarningBanner.svelte';
-	import { i18n } from '$lib/stores/i18n.store';
 	import type { NetworkId } from '$lib/types/network';
 	import { invalidAmount } from '$lib/utils/input.utils';
 	import { isInvalidDestinationBtc } from '$lib/utils/send.utils';
+	import BtcSendHasPendingTransactions from './BtcSendHasPendingTransactions.svelte';
 
 	export let destination = '';
 	export let amount: number | undefined = undefined;
@@ -30,11 +29,8 @@
 </script>
 
 <SendReview on:icBack on:icSend {source} {amount} {destination} disabled={disableSend}>
-	<svelte:fragment slot="info">
-		{#if $hasPendingTransactionsStore === true}
-			<WarningBanner>{$i18n.send.info.pending_bitcoin_transaction}</WarningBanner>
-		{:else if $hasPendingTransactionsStore === 'error'}
-			<WarningBanner>{$i18n.send.error.no_pending_bitcoin_transaction}</WarningBanner>
-		{/if}
-	</svelte:fragment>
+	<BtcSendHasPendingTransactions
+		slot="info"
+		pendingTransactionsStatus={$hasPendingTransactionsStore}
+	/>
 </SendReview>
