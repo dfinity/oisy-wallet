aae001cd2c15cb8ef23182c1b941a36c161f3eb5
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-10 19:04:06 +0200

    feat(frontend): Merge testnet tokens with mainnet in token list

--- src/frontend/src/lib/derived/network-tokens.derived.ts
+++ src/frontend/src/lib/derived/network-tokens.derived.ts
@@ -1,25 +1,15 @@
 import { exchanges } from '$lib/derived/exchange.derived';
-import { pseudoNetworkChainFusion, selectedNetwork } from '$lib/derived/network.derived';
 import { enabledTokens, tokensToPin } from '$lib/derived/tokens.derived';
 import { balancesStore } from '$lib/stores/balances.store';
 import type { Token, TokenUi } from '$lib/types/token';
-import { filterTokensForSelectedNetwork } from '$lib/utils/network.utils';
 import { pinTokensWithBalanceAtTop, sortTokens } from '$lib/utils/tokens.utils';
 import { derived, type Readable } from 'svelte/store';
-
-/**
- * All user-enabled tokens matching the selected network or chain fusion.
- */
-const enabledNetworkTokens: Readable<Token[]> = derived(
-	[enabledTokens, selectedNetwork, pseudoNetworkChainFusion],
-	filterTokensForSelectedNetwork
-);
 
 /**
  * Network tokens sorted by market cap, with the ones to pin at the top of the list.
  */
 export const combinedDerivedSortedNetworkTokens: Readable<Token[]> = derived(
-	[enabledNetworkTokens, tokensToPin, exchanges],
+	[enabledTokens, tokensToPin, exchanges],
 	([$tokens, $tokensToPin, $exchanges]) => sortTokens({ $tokens, $exchanges, $tokensToPin })
 );
 
