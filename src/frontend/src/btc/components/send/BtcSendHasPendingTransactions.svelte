68301ec78b1a16d12f192d7d6304670e07151384
Llorenç <llorenc.muntaner@dfinity.org>
2024-10-10 13:49:25 +0200

    refactor(frontend): Refactor initHasPendingSentTransactions return type

--- src/frontend/src/btc/components/send/BtcSendHasPendingTransactions.svelte
+++ src/frontend/src/btc/components/send/BtcSendHasPendingTransactions.svelte
@@ -2,15 +2,16 @@
 	import { fade } from 'svelte/transition';
 	import WarningBanner from '$lib/components/ui/WarningBanner.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
+	import { BtcPendingSentTransactionsStatus } from '$btc/derived/btc-pending-sent-transactions-status.derived';
 
-	export let pendingTransactionsStatus: boolean | 'loading' | 'error';
+	export let pendingTransactionsStatus: BtcPendingSentTransactionsStatus;
 </script>
 
-{#if pendingTransactionsStatus === true}
+{#if pendingTransactionsStatus === BtcPendingSentTransactionsStatus.PRESENT}
 	<div in:fade>
 		<WarningBanner>{$i18n.send.info.pending_bitcoin_transaction}</WarningBanner>
 	</div>
-{:else if pendingTransactionsStatus === 'error'}
+{:else if pendingTransactionsStatus === BtcPendingSentTransactionsStatus.ERROR}
 	<div in:fade>
 		<WarningBanner>{$i18n.send.error.no_pending_bitcoin_transaction}</WarningBanner>
 	</div>
