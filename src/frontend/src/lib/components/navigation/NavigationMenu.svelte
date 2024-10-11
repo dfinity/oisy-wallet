31c68b9dbef158154b2521ac501ce1ab8740ef61
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-11 14:01:31 +0200

    lint: format

--- src/frontend/src/lib/components/navigation/NavigationMenu.svelte
+++ src/frontend/src/lib/components/navigation/NavigationMenu.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
+	import IconSettings from '$lib/components/icons/IconSettings.svelte';
+	import IconWallet from '$lib/components/icons/IconWallet.svelte';
 	import NavigationItem from '$lib/components/navigation/NavigationItem.svelte';
 	import { networkId } from '$lib/derived/network.derived';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { isRouteSettings, isRouteTransactions, networkParam } from '$lib/utils/nav.utils.js';
-	import IconWallet from '$lib/components/icons/IconWallet.svelte';
-	import IconSettings from '$lib/components/icons/IconSettings.svelte';
 
 	let route: 'transactions' | 'tokens' | 'settings' = 'tokens';
 	$: route = isRouteSettings($page)
@@ -28,16 +28,18 @@
 	<NavigationItem
 		ariaLabel={$i18n.navigation.text.homepage}
 		selected={route === 'tokens' || route === 'transactions'}
-		on:click={gotoAssets}>
+		on:click={gotoAssets}
+	>
 		<IconWallet />
 		{$i18n.navigation.text.homepage}
-	</NavigationItem	>
+	</NavigationItem>
 
 	<NavigationItem
 		ariaLabel={$i18n.navigation.text.settings}
 		selected={route === 'settings'}
-		on:click={gotoSettings}>
+		on:click={gotoSettings}
+	>
 		<IconSettings />
 		{$i18n.navigation.text.settings}
-	</NavigationItem	>
+	</NavigationItem>
 </div>
