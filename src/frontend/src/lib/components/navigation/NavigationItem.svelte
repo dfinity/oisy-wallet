90ba4ba02e1faeb32eb4f5249d1f207a44ec2954
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-11 14:23:44 +0200

    style: more contrast when selected

--- src/frontend/src/lib/components/navigation/NavigationItem.svelte
+++ src/frontend/src/lib/components/navigation/NavigationItem.svelte
@@ -7,7 +7,7 @@
 	class="w-full rounded-xl p-3 text-left hover:text-blue-ribbon"
 	class:text-blue-ribbon={selected}
 	class:bg-white={selected}
-	class:hover:bg-zumthor={selected}
+	class:hover:bg-onahau={selected}
 	aria-label={ariaLabel}
 	on:click
 >
