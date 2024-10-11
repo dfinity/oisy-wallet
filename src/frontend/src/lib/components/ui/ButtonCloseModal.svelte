19d58f1884faa99e235800867dfbb9a4e0740ad1
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-11 17:23:08 +0200

    refactor: simpliify usage

--- src/frontend/src/lib/components/ui/ButtonCloseModal.svelte
+++ src/frontend/src/lib/components/ui/ButtonCloseModal.svelte
@@ -5,17 +5,12 @@
 	import type { ButtonColorStyle } from '$lib/types/style';
 
 	export let colorStyle: ButtonColorStyle = 'secondary';
-	export let disabled = false;
-	export let fullWidth = false;
-	export let additionalClasses = '';
 </script>
 
 <Button
 	{colorStyle}
 	type="button"
-	{disabled}
-	{fullWidth}
-	{additionalClasses}
+	fullWidth
 	on:click={modalStore.close}
 >
 	{$i18n.core.text.close}
