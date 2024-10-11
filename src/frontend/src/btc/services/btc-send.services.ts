8d0a0a76b137481061430803df6d808b9b8c2b7b
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-10-11 09:26:50 +0200

    feat(frontend): btc-send.service - sendBtc (#2799)
    # Motivation
    
    One more service required to send BTC.

--- src/frontend/src/btc/services/btc-send.services.ts
+++ src/frontend/src/btc/services/btc-send.services.ts
@@ -1,21 +1,35 @@
 import type { UtxosFee } from '$btc/types/btc-send';
 import { convertNumberToSatoshis } from '$btc/utils/btc-send.utils';
+import type { SendBtcResponse } from '$declarations/signer/signer.did';
 import { selectUserUtxosFee } from '$lib/api/backend.api';
+import { sendBtc as sendBtcApi } from '$lib/api/signer.api';
+import { ProgressStepsSendBtc } from '$lib/enums/progress-steps';
+import type { BtcAddress } from '$lib/types/address';
 import { mapToSignerBitcoinNetwork } from '$lib/utils/network.utils';
+import { waitAndTriggerWallet } from '$lib/utils/wallet.utils';
 import type { Identity } from '@dfinity/agent';
 import type { BitcoinNetwork } from '@dfinity/ckbtc';
+import { toNullable } from '@dfinity/utils';
 
 const DEFAULT_MIN_CONFIRMATIONS = 6;
+
+interface BtcSendServiceParams {
+	identity: Identity;
+	network: BitcoinNetwork;
+	amount: number;
+	progress: (step: ProgressStepsSendBtc) => void;
+}
+
+type SendBtcParams = BtcSendServiceParams & {
+	destination: BtcAddress;
+	utxosFee: UtxosFee;
+};
 
 export const selectUtxosFee = async ({
 	identity,
 	network,
 	amount
-}: {
-	identity: Identity;
-	network: BitcoinNetwork;
-	amount: number;
-}): Promise<UtxosFee> => {
+}: BtcSendServiceParams): Promise<UtxosFee> => {
 	const satoshisAmount = convertNumberToSatoshis({ amount });
 	const signerBitcoinNetwork = mapToSignerBitcoinNetwork({ network });
 
@@ -30,4 +44,35 @@ export const selectUtxosFee = async ({
 		feeSatoshis: fee_satoshis,
 		utxos
 	};
+};
+
+export const sendBtc = async ({ progress, ...rest }: SendBtcParams): Promise<void> => {
+	// TODO: use txid returned by this method to register it as a pending transaction in BE
+	await send({ progress, ...rest });
+
+	progress(ProgressStepsSendBtc.RELOAD);
+
+	await waitAndTriggerWallet();
+};
+
+const send = async ({
+	identity,
+	destination,
+	network,
+	amount,
+	utxosFee,
+	progress
+}: SendBtcParams): Promise<SendBtcResponse> => {
+	const satoshisAmount = convertNumberToSatoshis({ amount });
+	const signerBitcoinNetwork = mapToSignerBitcoinNetwork({ network });
+
+	progress(ProgressStepsSendBtc.SEND);
+
+	return sendBtcApi({
+		identity,
+		network: signerBitcoinNetwork,
+		feeSatoshis: toNullable(utxosFee.feeSatoshis),
+		utxosToSpend: utxosFee.utxos,
+		outputs: [{ destination_address: destination, sent_satoshis: satoshisAmount }]
+	});
 };
