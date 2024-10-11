c568e75d342a77c1fa500f8e71391ad8307d7e2c
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-11 18:45:48 +0200

    lint:format

--- src/frontend/src/lib/components/navigation/NavigationItem.svelte
+++ src/frontend/src/lib/components/navigation/NavigationItem.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <button
-	class="w-full rounded-xl p-3 text-left hover:text-blue-ribbon transition-colors duration-700"
+	class="w-full rounded-xl p-3 text-left transition-colors duration-700 hover:text-blue-ribbon"
 	class:text-blue-ribbon={selected}
 	class:bg-white={selected}
 	class:hover:bg-onahau={selected}
