049ced28c7bd217da9ebc226afd23ec775816bdb
Robert Schlittler <robert.schlittler@inacta.ch>
2024-10-14 11:05:35 +0200

    feat(frontend): implement grid

--- src/frontend/src/lib/components/dapps/Card.svelte
+++ src/frontend/src/lib/components/dapps/Card.svelte
@@ -2,10 +2,9 @@
 </script>
 
 <div
-	class="relative rounded-3xl bg-white pt-12 p-4 hover:shadow hover:cursor-pointer transition-shadow ${$$restProps.class}">
+	class="relative rounded-3xl bg-white pt-12 p-4 flex-1 hover:shadow transition-shadow {$$restProps.class}">
 	<div class="flex flex-col justify-between gap-y-2 h-full">
 		<slot name="content"></slot>
-
 		<slot name="footer"></slot>
 	</div>
 </div>
--- src/frontend/src/lib/components/dapps/DAppCardGrid.svelte
+++ src/frontend/src/lib/components/dapps/DAppCardGrid.svelte
@@ -6,6 +6,7 @@
 	import DAppModal from '$lib/components/dapps/DAppModal.svelte';
 	import { nonNullish } from '@dfinity/utils';
 	import DAppCard from '$lib/components/dapps/DAppCard.svelte';
+	import Card from '$lib/components/dapps/Card.svelte';
 
 	export let dApps: DApp[];
 
@@ -17,26 +18,29 @@
 		: undefined;
 
 	export let selectedFilter;
-	$: filteredDApps = selectedFilter === "All"
+	$: filteredDApps = selectedFilter === 'All'
 		? dApps
 		: dApps.filter(dApp => dApp.categories.includes(selectedFilter));
 </script>
 
 <div class="container mx-auto py-8">
-	<div class="grid grid-cols-1 sm:grid-cols-2 gap-y-12 gap-x-3">
+	<div class="grid grid-cols-1 md:grid-cols-2 gap-y-12 gap-x-3">
 		{#each filteredDApps as dApp (dApp.name)}
 			<button on:click={() => modalStore.openDAppDetails(dApp)} class="contents">
 				<DAppCard dApp={dApp} />
 			</button>
 		{/each}
 
-		<div
-			class="rounded-lg shadow-md bg-blue-500 p-6 flex items-center justify-center text-white cursor-pointer hover:bg-blue-600 transition-colors">
-			<a href="/submit-dapp" class="text-center">
-				<span class="block text-2xl font-semibold">+</span>
-				<p class="mt-2">Submit your DApp</p>
-			</a>
-		</div>
+		<a href="/submit-dapp" class="no-underline flex">
+			<Card class="!bg-blue-ribbon">
+				<div slot="content">
+					<p class="m-0 text-lg text-white font-bold">+</p>
+				</div>
+				<div slot="footer">
+					<p class="m-0 text-lg text-white font-bold">Submit your DApp</p>
+				</div>
+			</Card>
+		</a>
 	</div>
 </div>
 
