93be31f01bdfeedee8dc08fbf1e0a4172dc6d5c7
David Dal Busco <david.dalbusco@outlook.com>
2024-10-14 07:26:43 +0200

    chore: format

--- src/frontend/src/lib/components/ui/ButtonIcon.svelte
+++ src/frontend/src/lib/components/ui/ButtonIcon.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <button
-	class={`${colorStyle} icon flex flex-col text-center text-xs font-normal w-10 h-10`}
+	class={`${colorStyle} icon flex h-10 w-10 flex-col text-center text-xs font-normal`}
 	class:link
 	bind:this={button}
 	on:click
