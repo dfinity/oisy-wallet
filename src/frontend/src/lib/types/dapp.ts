6871a98d4c9e6705e6743c0a960fb6d58fa24792
Robert Schlittler <robert.schlittler@inacta.ch>
2024-10-14 16:36:37 +0200

    feat(frontend): add type for dapp

---
+++ src/frontend/src/lib/types/dapp.ts
@@ -0,0 +1,14 @@
+export interface DApp {
+	name: string;
+	featured?: boolean;
+	oneLiner: string;
+	description: string;
+	categories: string[];
+	logoUrl: string;
+	imageUrl: string;
+	url: string;
+	channels?: {
+		github?: string;
+		twitter?: string;
+	};
+}
