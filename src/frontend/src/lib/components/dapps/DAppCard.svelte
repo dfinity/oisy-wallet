3cf24fbf3186acf9e998f51bb80edd9ac0e51650
Robert Schlittler <robert.schlittler@inacta.ch>
2024-10-14 10:46:15 +0200

    feat(frontend): polish DAppCard

--- src/frontend/src/lib/components/dapps/DAppCard.svelte
+++ src/frontend/src/lib/components/dapps/DAppCard.svelte
@@ -4,17 +4,22 @@
 	export let dApp: DApp;
 </script>
 
-<div class="rounded-lg shadow-md bg-white p-6 hover:shadow-lg transition-shadow">
-	<img
+<div class="relative rounded-3xl bg-white p-4 pt-14 hover:shadow hover:cursor-pointer transition-shadow">
+	{#if dApp.imageUrl} <img
 		src={dApp.imageUrl || '/default-dapp-icon.png'}
 		alt={dApp.name}
-		class="w-16 h-16 rounded-full mx-auto mb-4"
+		class="absolute -top-9 left-4 w-20 h-20 rounded-full"
 	/>
-	<h2 class="text-center text-lg font-semibold">{dApp.name}</h2>
-	<p class="text-center text-gray-600 mt-2">{dApp.description}</p>
-	<div class="flex justify-center mt-4 space-x-2">
-		{#each dApp.tags as tag}
-			<span class="bg-blue-100 text-blue-800 text-sm font-medium px-3 py-1 rounded">{tag}</span>
-		{/each}
+	{/if}
+	<div class="flex flex-col justify-between gap-y-2 h-full">
+		<div>
+			<h2 class="text-start text-lg font-semibold">{dApp.name}</h2>
+			<p class="text-start text-misty-rose m-0">{dApp.description}</p>
+		</div>
+		<div class="flex space-x-2">
+			{#each dApp.tags as tag}
+				<span class="bg-zumthor text-primary text-xs px-3 py-1.5 rounded-full">{tag}</span>
+			{/each}
+		</div>
 	</div>
 </div>
