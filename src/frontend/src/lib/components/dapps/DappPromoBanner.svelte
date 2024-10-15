f9ca9a6200686e7add8b23f647e53e43526cf239
Robert Schlittler <robert.schlittler@inacta.ch>
2024-10-15 10:54:39 +0200

    feat(frontend): fix btn wrapped with a

--- src/frontend/src/lib/components/dapps/DappPromoBanner.svelte
+++ src/frontend/src/lib/components/dapps/DappPromoBanner.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import Button from '$lib/components/ui/Button.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 	import type { DApp } from '$lib/types/dapp';
 	import Img from '$lib/components/ui/Img.svelte';
+	import { replacePlaceholders } from '$lib/utils/i18n.utils';
 
 	export let dApp: DApp;
 </script>
@@ -12,23 +12,25 @@
 >
 	{#if dApp.imageUrl}
 		<div class="absolute">
-			<Img src={dApp.imageUrl} alt={dApp.name}/>
+			<Img src={dApp.imageUrl} alt={dApp.name} />
 		</div>
 	{/if}
 	<div class="flex-1 px-4 py-4 backdrop-blur-sm">
 		<div class="flex items-center gap-x-2">
 			<div class="h-12 w-12 rounded-full">
-				<Img  src={dApp.logoUrl} alt={dApp.name} />
+				<Img src={dApp.logoUrl} alt={dApp.name} />
 			</div>
 			<div class="flex-1">
-				<h6 class="text-sm font-bold text-info">{$i18n.dapps.text.featured}</h6>
+				<h6 class="text-sm font-bold">{$i18n.dapps.text.featured}</h6>
 				<h4 class="text-white">{dApp.name}</h4>
 			</div>
-			<a href={dApp.url} target="_blank" class="no-underline">
-				<Button size="sm" colorStyle="secondary" styleClass="flex-grow-0"
-				>{$i18n.core.text.view}</Button
-				>
+
+			<a rel="external noopener noreferrer"
+				 aria-label={replacePlaceholders($i18n.dapps.alt.open_dapp, {'$dAppname': dApp.name})} href={dApp.url}
+				 target="_blank"
+				 class="as-button secondary no-underline">{$i18n.core.text.view}
 			</a>
+
 		</div>
 	</div>
 </article>
