c4100c2dd135376496485939a89424bfe02ef8bf
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-10-15 10:24:58 +0200

    style(frontend): adjust layout to have sticky header footer and menu [GIX-3046] (#2876)
    # Motivation
    
    We aim to leave the Header, the Footer and the Navigational Menu sticky
    for medium and larger screens. However, the main content should be able
    to freely scroll in the space occupied by the Header and the Footer,
    when it not overlaps (basically larger screens). Meaning that the class
    `sticky` is not apt to be used in that case.
    
    So, we use `fixed` for all three, but being careful that it should
    applied only when signed in (the landing page is ok as it is).
    
    # Results
    
    
    https://github.com/user-attachments/assets/88c2375c-6d27-4a91-8725-70aed20181ab
    
    # Landing page unchanged
    
    
    
    https://github.com/user-attachments/assets/a098e72d-c7c0-43fe-9572-298dd5734afa

--- src/frontend/src/lib/components/core/Footer.svelte
+++ src/frontend/src/lib/components/core/Footer.svelte
@@ -14,8 +14,8 @@
 </script>
 
 <footer
-	class="z-1 mx-auto flex w-full max-w-screen-2.5xl flex-1 flex-col items-center justify-end px-4 pb-5 pt-6 md:flex-1 md:flex-grow md:flex-row md:items-end md:justify-between md:px-8"
-	class:md:sticky={$authNotSignedIn}
+	class="z-1 mx-auto flex w-full max-w-screen-2.5xl flex-1 flex-col items-center justify-end px-4 pb-5 pt-6 sm:flex-1 sm:flex-grow sm:flex-row sm:items-end sm:justify-between sm:px-8 lg:fixed lg:inset-x-0 lg:bottom-0"
+	class:sm:sticky={$authNotSignedIn}
 	class:md:h-md:grid={$authNotSignedIn}
 	class:md:h-md:grid-cols-4={$authNotSignedIn}
 	class:md:h-md:pr-0={$authNotSignedIn}
@@ -33,7 +33,10 @@
 		</ExternalLinkIcon>
 	</div>
 
-	<div class="item flex flex-row items-center justify-end gap-2 text-sm">
+	<div
+		class="item flex flex-row items-center justify-end gap-2 text-sm lg:max-w-48 xl:max-w-none"
+		class:sm:max-w-none={$authNotSignedIn}
+	>
 		<IconDfinity />
 		<span>
 			{replaceOisyPlaceholders($i18n.footer.text.developed_with)}
