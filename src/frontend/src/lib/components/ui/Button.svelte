25da79ab3a30034bcb0ad1276dc085bcd2f35f76
Robert Schlittler <robert.schlittler@inacta.ch>
2024-10-15 10:22:13 +0200

    feat(frontend): PR feedback, revert button changes for later PR

--- src/frontend/src/lib/components/ui/Button.svelte
+++ src/frontend/src/lib/components/ui/Button.svelte
@@ -3,25 +3,18 @@
 
 	export let colorStyle: ButtonColorStyle = 'primary';
 	export let type: 'submit' | 'reset' | 'button' = 'submit';
-	export let size: 'xs' | 'sm' | 'md' | 'lg' | 'xl' = 'md';
 	export let disabled = false;
 	export let fullWidth = false;
 	export let link = false;
+	export let paddingSmall = false;
 	export let testId: string | undefined = undefined;
 	export let ariaLabel: string | undefined = undefined;
 	export let styleClass = '';
-
-	const buttonClasses = {
-		xs: 'p-1 text-xs rounded-lg',
-		sm: 'px-3 py-2 text-xs rounded-xl',
-		md: 'px-4 py-3 text-xs rounded-2xl',
-		lg: 'px-5 py-4 text-xs rounded-2xl',
-		xl: 'p-5 text-base rounded-2xl'
-	};
 </script>
 
 <button
-	class={`${colorStyle} flex h-fit flex-1 text-center ${styleClass} ${buttonClasses[size]}`}
+	class={`${colorStyle} flex flex-1 text-center ${styleClass}`}
+	class:padding-sm={paddingSmall}
 	class:w-full={fullWidth}
 	class:link
 	{type}
