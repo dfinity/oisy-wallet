2a5f4af5c0d57034eaade4d694f53f22a89627ff
Robert Schlittler <robert.schlittler@inacta.ch>
2024-10-15 07:31:33 +0200

    feat(frontend): implement button sizes according to figma design. - replace paddingSmall with more versatile usage of a size property. Moving padding from scss to tailwind - adjust only usage of paddingSmall (the design from https://www.figma.com/design/duPCw1leqer7ES0sBb6Uua/7.-Oisy-Redesign?node-id=142-50384&node-type=instance&t=6owMa8M3f2OQzxtz-0 use the 'md' button.

--- src/frontend/src/lib/components/ui/ButtonHero.svelte
+++ src/frontend/src/lib/components/ui/ButtonHero.svelte
@@ -6,7 +6,7 @@
 	export let ariaLabel: string;
 </script>
 
-<Button on:click {ariaLabel} {disabled} {testId} colorStyle="tertiary" link paddingSmall>
+<Button on:click {ariaLabel} {disabled} {testId} colorStyle="tertiary" link>
 	<div class="flex flex-col items-center justify-center gap-2 md:flex-row">
 		<slot name="icon" />
 		<div class="min-w-12 max-w-[72px] text-xs md:text-base">
