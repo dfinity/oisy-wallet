e05067cb6bf27a704dc25626400c93428a7e58a1
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-10-15 16:42:16 +0200

    feat(frontend): Add info menu at bottom of navigational menu (#2899)
    # Motivation
    
    We want to add some informative elements at the bottom of the
    navigational Menu.
    
    # Changes
    
    - Adjust lateral menu column to extend until bottom of the viewport.
    - Split the menu into two elements to put distance automatically between
    them: the top part is the navigation menu, the bottom part will be
    mostly informative and hyperlinks.
    - Use the About Modal buttons as elements to put in the "info" part.
    - Adjust the buttons to have a shorter string there and update the icon
    (using Lucide as per design).
    
    # Tests
    
    
    
    https://github.com/user-attachments/assets/73e2c0df-cffe-46e7-a2c7-4b4ba9137868

---
+++ src/frontend/src/lib/components/navigation/InfoMenu.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import AboutHow from '$lib/components/hero/about/AboutHow.svelte';
+	import AboutWhat from '$lib/components/hero/about/AboutWhat.svelte';
+</script>
+
+<div class="box-content flex w-full flex-col gap-5 py-3">
+	<AboutHow asMenuItem />
+
+	<AboutWhat asMenuItem />
+</div>
