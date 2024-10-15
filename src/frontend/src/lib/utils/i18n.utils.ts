8a945bc32f4b1c6e3c24fb16b93b106b16dd98e8
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-15 15:08:09 +0200

    feat(frontend): Add short OISY name

--- src/frontend/src/lib/utils/i18n.utils.ts
+++ src/frontend/src/lib/utils/i18n.utils.ts
@@ -1,4 +1,5 @@
 import {
+	OISY,
 	OISY_ALPHA_WARNING_URL,
 	OISY_DESCRIPTION,
 	OISY_NAME,
@@ -32,6 +33,7 @@ export const replacePlaceholders = (text: string, substitutions: I18nSubstitutio
 
 export const replaceOisyPlaceholders = (text: string): string =>
 	replacePlaceholders(text, {
+		$oisy: OISY,
 		$oisy_name: OISY_NAME,
 		$oisy_oneliner: OISY_ONELINER,
 		$oisy_description: OISY_DESCRIPTION,
