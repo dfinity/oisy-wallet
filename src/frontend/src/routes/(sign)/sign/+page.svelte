72f845e8232d79d3431bcffd520e6563ec6a609f
David Dal Busco <david.dalbusco@outlook.com>
2024-10-16 13:41:59 +0200

    feat(frontend): initiate new /sign route

---
+++ src/frontend/src/routes/(sign)/sign/+page.svelte
@@ -0,0 +1,3 @@
+<article class="mb-10 flex min-h-96 flex-col rounded-lg border border-water bg-white px-5 py-6">
+	<p>Work in progress. Click the OISY Wallet logo above to go back to wallet.</p>
+</article>
