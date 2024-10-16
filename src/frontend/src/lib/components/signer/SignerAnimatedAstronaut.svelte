d429a7a14098886fec60920ac5e1f27c8b78f040
David Dal Busco <david.dalbusco@outlook.com>
2024-10-16 17:01:17 +0200

    feat: icon inherit current color

--- src/frontend/src/lib/components/signer/SignerAnimatedAstronaut.svelte
+++ src/frontend/src/lib/components/signer/SignerAnimatedAstronaut.svelte
@@ -7,7 +7,7 @@
 		class="absolute inline-flex h-full w-full animate-ping rounded-full bg-[var(--color-primary)] opacity-75"
 	></span>
 	<span
-		class="relative inline-flex h-20 w-20 items-center justify-center rounded-full bg-[var(--color-primary)]"
+		class="relative inline-flex h-20 w-20 items-center justify-center rounded-full bg-[var(--color-primary)] text-[var(--color-primary)]"
 		><IconAstronautWithEyes /></span
 	>
 </span>
