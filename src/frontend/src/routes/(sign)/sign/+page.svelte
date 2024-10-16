451a0dd0f9f683bf5b7a972d3c43538dfabc110a
David Dal Busco <david.dalbusco@outlook.com>
2024-10-16 17:42:56 +0200

    feat(frontend): signer accounts

--- src/frontend/src/routes/(sign)/sign/+page.svelte
+++ src/frontend/src/routes/(sign)/sign/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import SignerAccounts from "$lib/components/signer/SignerAccounts.svelte";
 	import SignerSignIn from '$lib/components/signer/SignerSignIn.svelte';
 	import { authNotSignedIn } from '$lib/derived/auth.derived';
 </script>
@@ -7,6 +8,8 @@
 	{#if $authNotSignedIn}
 		<SignerSignIn />
 	{:else}
-		<p>Work in progress. Click the OISY Wallet logo above to go back to wallet.</p>
+		<SignerAccounts>
+			<p>Work in progress. Click the OISY Wallet logo above to go back to wallet.</p>
+		</SignerAccounts>
 	{/if}
 </article>
