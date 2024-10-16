b2f0adfd15dd66fc5de3d64a5a4924deebe14776
David Dal Busco <david.dalbusco@dfinity.org>
2024-10-16 18:15:35 +0200

    feat(frontend): signer loading (#2947)
    # Motivation
    
    When the signer is doing some work, like loading the consent message or
    effectivelly processing the call to a canister, we want to display a
    spinner with a message.
    
    # Changes
    
    - Add a component to present a loading msg.
    
    # Screenshot
    
    <img width="688" alt="Capture d’écran 2024-10-16 à 17 58 45"
    src="https://github.com/user-attachments/assets/1c2a5182-4075-4478-961d-6e7d6ffa320d">

---
+++ src/frontend/src/lib/components/signer/SignerLoading.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { Spinner } from '@dfinity/gix-components';
+</script>
+
+<div class="flex min-h-full flex-1 flex-col items-center justify-center gap-3">
+	<div class="text-[var(--color-primary)]">
+		<Spinner inline />
+	</div>
+	<span><slot /></span>
+</div>
