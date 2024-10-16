362491de988eb0598b958a79b7fb52c8bd3a5f84
David Dal Busco <david.dalbusco@outlook.com>
2024-10-16 19:01:43 +0200

    feat(frontend): signer "alert" symbol

---
+++ src/frontend/src/lib/components/signer/SignerAlert.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { ComponentType } from 'svelte';
+	import IconCheck from '$lib/components/icons/IconCheck.svelte';
+	import IconClose from '$lib/components/icons/IconClose.svelte';
+
+	export let type: 'ok' | 'error';
+
+	let icon: ComponentType;
+	$: icon = type === 'error' ? IconClose : IconCheck;
+</script>
+
+<div class="flex justify-center pb-5 pt-8">
+	<div
+		class="flex h-20 w-20 items-center justify-center rounded-full"
+		class:bg-cyclamen={type === 'error'}
+		class:bg-[var(--color-primary)]={type === 'ok'}
+	>
+		<svelte:component this={icon} />
+	</div>
+</div>
