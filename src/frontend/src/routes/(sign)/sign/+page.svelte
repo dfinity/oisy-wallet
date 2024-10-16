abebcd9fbfe7f3137c7ff19b7690209cfe1532fd
David Dal Busco <david.dalbusco@outlook.com>
2024-10-16 18:31:00 +0200

    feat(frontend): signer permissions

--- src/frontend/src/routes/(sign)/sign/+page.svelte
+++ src/frontend/src/routes/(sign)/sign/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import SignerAccounts from '$lib/components/signer/SignerAccounts.svelte';
+	import SignerPermissions from '$lib/components/signer/SignerPermissions.svelte';
 	import SignerSignIn from '$lib/components/signer/SignerSignIn.svelte';
 	import { authNotSignedIn } from '$lib/derived/auth.derived';
+	import { initSignerContext } from '$lib/stores/signer.store';
+
+	const { idle } = initSignerContext();
 </script>
 
 <article class="mb-10 flex min-h-96 flex-col rounded-lg border border-water bg-white px-5 py-6">
@@ -9,7 +13,11 @@
 		<SignerSignIn />
 	{:else}
 		<SignerAccounts>
-			<p>Work in progress. Click the OISY Wallet logo above to go back to wallet.</p>
+			{#if $idle}
+				<p>Work in progress. Click the OISY Wallet logo above to go back to wallet.</p>
+			{:else}
+				<SignerPermissions />
+			{/if}
 		</SignerAccounts>
 	{/if}
 </article>
