a62075109ecdfdad8576f908b45fa7bcce1282b3
David Dal Busco <david.dalbusco@outlook.com>
2024-10-16 14:22:56 +0200

    feat: background is now the same on signer route

--- src/frontend/src/routes/(sign)/sign/+layout.svelte
+++ src/frontend/src/routes/(sign)/sign/+layout.svelte
@@ -1,17 +1,7 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import Modals from '$lib/components/core/Modals.svelte';
 	import OisyWalletLogoLink from '$lib/components/core/OisyWalletLogoLink.svelte';
 	import WarningBanner from '$lib/components/ui/WarningBanner.svelte';
-	import { LOCAL } from '$lib/constants/app.constants';
-
-	onMount(async () => {
-		if (!LOCAL) {
-			return;
-		}
-
-		await import('./+oisy.page.css');
-	});
 </script>
 
 <main class="mx-auto flex flex-col gap-y-6 px-5 pt-10 sm:w-sm">
