c895c592de6d907820f8d1219e6e80ec9ee513c4
David Dal Busco <david.dalbusco@dfinity.org>
2024-10-16 17:35:32 +0200

    feat(frontend): signer sign-in (#2940)
    # Motivation
    
    The `/sign` route has a particular authentication.
    
    # Changes
    
    - Create signer sign-in component that uses UI components and the
    standard `ButtonAuthenticate`
    - Consumes the component in the route and present if if the user is not
    signed-in
    
    # Screenshtos
    
    <img width="1452" alt="Capture d’écran 2024-10-16 à 16 26 48"
    src="https://github.com/user-attachments/assets/d10833ae-5392-4e82-83f1-66638f31675b">
    <img width="1470" alt="Capture d’écran 2024-10-16 à 16 26 42"
    src="https://github.com/user-attachments/assets/7ab5acf3-a327-436f-a91c-aef0421df1e5">
    <img width="688" alt="Capture d’écran 2024-10-16 à 16 26 38"
    src="https://github.com/user-attachments/assets/8f6824a9-9378-4856-a0d0-b8b8cacc7ca0">

--- src/frontend/src/routes/(sign)/sign/+page.svelte
+++ src/frontend/src/routes/(sign)/sign/+page.svelte
@@ -1,3 +1,12 @@
+<script lang="ts">
+	import SignerSignIn from '$lib/components/signer/SignerSignIn.svelte';
+	import { authNotSignedIn } from '$lib/derived/auth.derived';
+</script>
+
 <article class="mb-10 flex min-h-96 flex-col rounded-lg border border-water bg-white px-5 py-6">
-	<p>Work in progress. Click the OISY Wallet logo above to go back to wallet.</p>
+	{#if $authNotSignedIn}
+		<SignerSignIn />
+	{:else}
+		<p>Work in progress. Click the OISY Wallet logo above to go back to wallet.</p>
+	{/if}
 </article>
