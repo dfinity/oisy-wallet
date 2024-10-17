03d8398b4fa5a93f913b34de0b24919280d09c76
Dario Lötscher <dario.loetscher@inacta.ch>
2024-10-17 11:06:14 +0200

    feat(frontend): add token group card

---
+++ src/frontend/src/lib/components/tokens/TokenGroupCard.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import Listener from '$lib/components/core/Listener.svelte';
+	import TokenCard from '$lib/components/tokens/TokenCard.svelte';
+	import TokenCardWithUrl from '$lib/components/tokens/TokenCardWithUrl.svelte';
+	import TokenLogo from '$lib/components/tokens/TokenLogo.svelte';
+	import TokenName from '$lib/components/tokens/TokenName.svelte';
+	import TokenSymbol from '$lib/components/tokens/TokenSymbol.svelte';
+	import Card from '$lib/components/ui/Card.svelte';
+	import { TOKEN_GROUP } from '$lib/constants/test-ids.constants';
+	import type { TokenGroupUi, TokenUi } from '$lib/types/token';
+
+	export let tokenGroup: TokenGroupUi;
+
+	const totalBalance = tokenGroup.tokens.reduce((sum, token: TokenUi) => sum + (token.balance ?? 0), 0);
+
+	const totalBalanceUsd = tokenGroup.tokens.reduce((sum, token: TokenUi) => sum + (token.usdBalance ?? 0), 0);
+</script>
+
+<Card noMargin testId={`${TOKEN_GROUP}-${tokenGroup.header.symbol}`}>
+	<TokenSymbol token={{symbol:tokenGroup.header.name, network: tokenGroup.native}} />
+
+	<TokenName token={{name: tokenGroup.header.symbol}} slot="description" />
+
+	<TokenLogo token={{name: tokenGroup.header.name, icon: tokenGroup.header.icon, network: tokenGroup.native}} showNetworkIcon={false} slot="icon" color="white" />
+
+	<span slot="balance">{totalBalance}</span>
+	<span slot="amountDescription">{totalBalanceUsd}</span>
+</Card>
+
+<div class="bg-grey">
+	{#each tokenGroup.tokens as token}
+		<Listener {token}>
+			<TokenCardWithUrl {token}>
+				<TokenCard {token} />
+			</TokenCardWithUrl>
+		</Listener>
+	{/each}
+</div>
