eef436151eef707040a0f374661fc94cb5b761fa
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-17 16:49:02 +0200

    feat(frontend): Add USD balance in Hero for token page

--- src/frontend/src/lib/components/hero/Balance.svelte
+++ src/frontend/src/lib/components/hero/Balance.svelte
@@ -1,23 +1,45 @@
 <script lang="ts">
 	import { isNullish, nonNullish } from '@dfinity/utils';
-	import { erc20UserTokensInitialized } from '$eth/derived/erc20.derived';
 	import Amount from '$lib/components/ui/Amount.svelte';
 	import { balance, balanceZero } from '$lib/derived/balances.derived';
+	import { exchanges } from '$lib/derived/exchange.derived';
 	import { tokenSymbol } from '$lib/derived/token.derived';
+	import { balancesStore } from '$lib/stores/balances.store';
+	import { token } from '$lib/stores/token.store';
+	import { formatUSD } from '$lib/utils/format.utils';
+	import { calculateTokenUsdBalance } from '$lib/utils/token.utils';
+
+	let usdBalance: number | undefined;
+	$: usdBalance = nonNullish($token)
+		? calculateTokenUsdBalance({
+				token: $token,
+				$balances: $balancesStore,
+				$exchanges: $exchanges
+			})
+		: undefined;
 </script>
 
-<span>
+<span class="flex flex-col gap-2">
 	<output
-		class={`break-all ${($balance?.toBigInt() ?? 0n) === 0n ? 'opacity-50' : 'opacity-100'} flex flex-col sm:block`}
+		class={`break-words ${($balance?.toBigInt() ?? 0n) === 0n ? 'opacity-50' : 'opacity-100'} inline-flex w-full flex-row justify-center gap-3 text-4xl font-bold lg:text-5xl`}
 	>
 		{#if nonNullish($balance) && !$balanceZero}
-			<span class="text-5xl font-bold"><Amount amount={$balance} /></span>
+			<span><Amount amount={$balance} /> {$tokenSymbol}</span>
 		{:else}
-			<span class="text-5xl font-bold" class:animate-pulse={isNullish($balance)}>0.00</span>
-		{/if}
-
-		{#if $erc20UserTokensInitialized && nonNullish(tokenSymbol)}
-			<span class="opacity-100">{$tokenSymbol}</span>
+			<span class:animate-pulse={isNullish($balance)}>0.00 {$tokenSymbol}</span>
 		{/if}
 	</output>
+
+	<span class="text-xl font-bold" class:opacity-50={usdBalance ?? 0 === 0}>
+		{#if nonNullish(usdBalance)}
+			{formatUSD({ value: usdBalance })}
+		{:else}
+			<span class:animate-pulse={isNullish(usdBalance)}
+				>{formatUSD({
+					value: 0,
+					options: { minFraction: 0, maxFraction: 0 }
+				}).replace('0', '-')}</span
+			>
+		{/if}
+	</span>
 </span>
