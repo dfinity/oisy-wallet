38cd6981f7a959c5ba8aa0cd7770ff8f9c845a52
David Dal Busco <david.dalbusco@dfinity.org>
2024-10-17 09:31:19 +0200

    feat(frontend): integrate signer idle (#2964)
    # Motivation
    
    Not sure if I did not integrated it in a previous PR or if it was
    discarded in a merge conflicts but, the idle animation for the signer
    should be integrated in the `/sign` route.
    
    # Changes
    
    - User `SignerIdle` in `+page` route `/sign`

--- src/frontend/src/routes/(sign)/sign/+page.svelte
+++ src/frontend/src/routes/(sign)/sign/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import { isNullish } from '@dfinity/utils';
 	import { onDestroy, setContext } from 'svelte';
+	import { fade, type FadeParams } from 'svelte/transition';
 	import SignerAccounts from '$lib/components/signer/SignerAccounts.svelte';
 	import SignerConsentMessage from '$lib/components/signer/SignerConsentMessage.svelte';
+	import SignerIdle from '$lib/components/signer/SignerIdle.svelte';
 	import SignerPermissions from '$lib/components/signer/SignerPermissions.svelte';
 	import SignerSignIn from '$lib/components/signer/SignerSignIn.svelte';
 	import { authNotSignedIn, authIdentity } from '$lib/derived/auth.derived';
@@ -27,6 +29,11 @@
 	onDestroy(reset);
 
 	$: $authIdentity, init();
+
+	// We use specific fade parameters for the idle state due to the asynchronous communication between the relying party and the wallet.
+	// Because the idle state might be displayed when a client starts communication with the wallet, we add a small delay to prevent a minor glitch where the idle animation is briefly shown before the actual action is rendered.
+	// Technically, from a specification standpoint, we don't have a way to fully prevent this.
+	const fadeParams: FadeParams = { delay: 150, duration: 250 };
 </script>
 
 <article class="mb-10 flex min-h-96 flex-col rounded-lg border border-water bg-white px-5 py-6">
@@ -35,11 +42,15 @@
 	{:else}
 		<SignerAccounts>
 			{#if $idle}
-				<p>Work in progress. Click the OISY Wallet logo above to go back to wallet.</p>
+				<div in:fade={fadeParams}>
+					<SignerIdle />
+				</div>
 			{:else}
 				<SignerPermissions />
 
 				<SignerConsentMessage />
+
+				<p>Work in progress. Click the OISY Wallet logo above to go back to wallet.</p>
 			{/if}
 		</SignerAccounts>
 	{/if}
