dac066e79ac8c89dfb20ec493272759838b14782
Robert Schlittler <robert.schlittler@inacta.ch>
2024-10-17 11:19:53 +0200

    feat(frontend): style

--- src/frontend/src/lib/components/dapps/DappCard.svelte
+++ src/frontend/src/lib/components/dapps/DappCard.svelte
@@ -12,7 +12,7 @@
 <button
 	aria-label={replacePlaceholders($i18n.dapps.alt.learn_more, { $dAppname: name })}
 	on:click
-	class="relative rounded-3xl bg-white p-4 pt-12"
+	class="relative rounded-3xl bg-white p-4 pt-12 h-44 md:h-60"
 >
 	<span class="absolute -top-5 left-4">
 		<Img
@@ -23,12 +23,12 @@
 			alt={replacePlaceholders($i18n.dapps.alt.website, { $dAppname: name })}
 		/>
 	</span>
-	<article class="flex h-full flex-col justify-between gap-y-2">
+	<article class="flex h-full flex-col justify-between gap-y-4 md:gap-y-2">
 		<section>
 			<p class="m-0 text-start text-lg font-semibold">{name}</p>
 			<p
 				title={oneLiner}
-				class="m-0 line-clamp-2 md:line-clamp-4 text-ellipsis text-start text-xs text-misty-rose"
+				class="m-0 mt-2 line-clamp-2 md:line-clamp-4 text-ellipsis text-start text-xs text-misty-rose"
 			>
 				{oneLiner}
 			</p>
