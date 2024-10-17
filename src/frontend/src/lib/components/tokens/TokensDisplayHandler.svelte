583e141671d3b2bf52191c6e04508e436c136e0c
Dario Lötscher <dario.loetscher@inacta.ch>
2024-10-17 13:17:08 +0200

    feat(frontend): map token uis to group uis

--- src/frontend/src/lib/components/tokens/TokensDisplayHandler.svelte
+++ src/frontend/src/lib/components/tokens/TokensDisplayHandler.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
 	import { debounce } from '@dfinity/utils';
-	import { combinedDerivedFilteredNetworkTokensUi } from '$lib/derived/network-tokens.derived';
-	import type { TokenUi } from '$lib/types/token';
+	import {
+		combinedDerivedGroupedTokensUi
+	} from '$lib/derived/network-tokens.derived';
+	import type { TokenUiOrGroupUi } from '$lib/types/token';
 
 	// We start it as undefined to avoid showing an empty list before the first update.
-	export let tokens: TokenUi[] | undefined = undefined;
+	export let tokens: TokenUiOrGroupUi[] | undefined = undefined;
 
-	let sortedTokens: TokenUi[];
-	$: sortedTokens = $combinedDerivedFilteredNetworkTokensUi;
+	let sortedTokens: TokenUiOrGroupUi[];
+	$: sortedTokens = $combinedDerivedGroupedTokensUi;
 
 	const updateTokensToDisplay = () => (tokens = [...sortedTokens]);
 
