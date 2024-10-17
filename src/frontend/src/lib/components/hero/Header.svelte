3065f099a68b1d5ee09d54a3305825750defdbb8
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-10-17 10:01:07 +0200

    fix(frontend): Clickable header and footer when signed in (#2959)
    # Motivation
    
    When signed in and in large screens, a part of the token list is
    overlapped by the Header and the Footer. That means that those part are
    un-clickable (for example Alpha and Manage list).
    
    To solve it, we make the entire Header and Footer with disabled pointer
    events, enabling it ONLY for the elements inside it.

--- src/frontend/src/lib/components/hero/Header.svelte
+++ src/frontend/src/lib/components/hero/Header.svelte
@@ -15,28 +15,31 @@
 </script>
 
 <header
-	class="z-1 relative grid w-full max-w-screen-2.5xl grid-cols-2 items-center gap-y-5 px-4 pt-6 sm:px-8"
+	class="z-1 pointer-events-none relative flex w-full max-w-screen-2.5xl items-center justify-between gap-y-5 px-4 pt-6 sm:px-8"
 	class:lg:fixed={$authSignedIn}
 	class:lg:top-0={$authSignedIn}
 	class:lg:inset-x-0={$authSignedIn}
 	class:lg:z-10={$authSignedIn}
+	class:xl:grid={$authNotSignedIn}
 	class:xl:grid-cols-[1fr_auto_1fr]={$authNotSignedIn}
 >
-	{#if back}
-		<Back />
-	{:else}
-		<OisyWalletLogoLink />
-	{/if}
+	<div class="pointer-events-auto">
+		{#if back}
+			<Back />
+		{:else}
+			<OisyWalletLogoLink />
+		{/if}
+	</div>
 
 	{#if $authNotSignedIn}
 		<div
-			class="col-span-3 col-start-1 row-start-2 flex xl:col-span-1 xl:col-start-2 xl:row-start-1 xl:w-fit"
+			class="pointer-events-auto col-span-3 col-start-1 row-start-2 flex xl:col-span-1 xl:col-start-2 xl:row-start-1 xl:w-fit"
 		>
 			<Alpha />
 		</div>
 	{/if}
 
-	<div class="flex justify-end gap-4">
+	<div class="pointer-events-auto flex justify-end gap-4">
 		{#if $authSignedIn}
 			<WalletConnect />
 		{/if}
