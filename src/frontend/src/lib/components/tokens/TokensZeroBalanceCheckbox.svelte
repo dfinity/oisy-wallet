774fdc169800fda7f5c11c0fd9e448f2e3a006f4
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-18 16:32:49 +0200

    refactor(frontend): Create specific Checkbox component to be re-used

--- src/frontend/src/lib/components/tokens/TokensZeroBalanceCheckbox.svelte
+++ src/frontend/src/lib/components/tokens/TokensZeroBalanceCheckbox.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Checkbox } from '@dfinity/gix-components';
+	import Checkbox from '$lib/components/ui/Checkbox.svelte';
 	import { hideZeroBalances } from '$lib/derived/settings.derived';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { hideZeroBalancesStore } from '$lib/stores/settings.store';
@@ -11,8 +11,6 @@
 		hideZeroBalancesStore.set({ key: 'hide-zero-balances', value: { enabled: !checked } });
 </script>
 
-<div style="--checkbox-label-order: 1">
-	<Checkbox inputId="checkbox-zero-balance" {checked} on:nnsChange={toggleHide}>
-		{$i18n.tokens.text.hide_zeros}
-	</Checkbox>
-</div>
+<Checkbox bind:checked on:click={toggleHide}>
+	{$i18n.tokens.text.hide_zeros}
+</Checkbox>
---
+++ src/frontend/src/lib/components/ui/Checkbox.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { draw } from 'svelte/transition';
+
+	export let checked: boolean;
+	export let testId: string | undefined = undefined;
+</script>
+
+<label class="flex cursor-pointer items-center gap-3">
+	<div class="relative flex size-fit items-center">
+		<input
+			type="checkbox"
+			class="peer relative size-5 appearance-none rounded border border-light-grey bg-white checked:border-0 checked:bg-blue-ribbon hover:border-blue-ribbon checked:hover:bg-cobalt focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-ribbon focus-visible:ring-offset-2"
+			bind:checked
+			on:click
+			data-tid={testId}
+		/>
+		{#if checked}
+			<svg
+				class="pointer-events-none absolute inset-0 m-0.5 hidden size-4 stroke-white outline-none peer-checked:block"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="3"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			>
+				<polyline in:draw={{ duration: 800 }} points="4 12 9 17 19 7" />
+			</svg>
+		{/if}
+	</div>
+	<slot />
+</label>
