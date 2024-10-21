42df4c85207ec7e01b9a0b2f9638b36d73c82c3b
Robert Schlittler <robert.schlittler@inacta.ch>
2024-10-21 14:26:09 +0200

    feat(frontend): format

--- src/frontend/src/routes/(app)/explore/+page.svelte
+++ src/frontend/src/routes/(app)/explore/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-import DappExplorer from '$lib/components/dapps/DappExplorer.svelte';
+	import DappExplorer from '$lib/components/dapps/DappExplorer.svelte';
 </script>
 
 <DappExplorer />
