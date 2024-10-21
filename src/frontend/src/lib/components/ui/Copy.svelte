954e7e565063755fdfaf920fde516fc8cba64c61
David Dal Busco <david.dalbusco@dfinity.org>
2024-10-21 16:58:12 +0200

    refactor(frontend): extract copy to clipboard to a module (#3014)
    # Motivation
    
    In #3013 we will need a specific copy button icon. I'm not sure if it
    will be use elsewhere, that's why instead of enhancing the existing
    `Copy` button, I rather like to create a specific "Receive related
    button". Also why it's handy to extract the "copy to clipboard" function
    to a module for reusability purposes.
    
    # Changes
    
    - Move existing copy to clipboard function to a utils.
    
    # Tests
    
    No particular tests.

--- src/frontend/src/lib/components/ui/Copy.svelte
+++ src/frontend/src/lib/components/ui/Copy.svelte
@@ -1,27 +1,16 @@
 <script lang="ts">
 	import IconCopy from '$lib/components/icons/IconCopy.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
-	import { toastsShow } from '$lib/stores/toasts.store';
-	import { copyText } from '$lib/utils/share.utils';
+	import { copyToClipboard } from '$lib/utils/clipboard.utils';
 
 	export let value: string;
 	export let text: string;
 	export let inline = false;
 	export let color: 'blue' | 'inherit' = 'blue';
-
-	const copyToClipboard = async () => {
-		await copyText(value);
-
-		toastsShow({
-			text,
-			level: 'success',
-			duration: 2000
-		});
-	};
 </script>
 
 <button
-	on:click|preventDefault|stopPropagation={copyToClipboard}
+	on:click|preventDefault|stopPropagation={async () => await copyToClipboard({ value, text })}
 	aria-label={`${$i18n.core.text.copy}: ${value}`}
 	class="pl-0.5"
 	class:py-2={!inline}
