1c23f7cb21b79dcd27fca64a5cf5d6e8a8109000
Robert Schlittler <robert.schlittler@inacta.ch>
2024-10-21 13:08:39 +0200

    feat(frontend): add styleClass to TokenCardWithOnClick.svelte

--- src/frontend/src/lib/components/tokens/TokenCardWithOnClick.svelte
+++ src/frontend/src/lib/components/tokens/TokenCardWithOnClick.svelte
@@ -1,4 +1,9 @@
-<div class="mb-6 flex gap-3 sm:gap-8">
+<script lang="ts">
+
+	export let styleClass: string | undefined;
+</script>
+
+<div class="mb-6 flex gap-3 sm:gap-8 {styleClass}">
 	<button class="flex-1" on:click>
 		<div class="w-full">
 			<slot />
