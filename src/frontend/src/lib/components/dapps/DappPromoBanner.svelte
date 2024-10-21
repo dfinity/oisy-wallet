a88b4fa5c4d00b6cebba4635db27b128f613013d
Robert Schlittler <robert.schlittler@inacta.ch>
2024-10-21 16:47:23 +0200

    feat(frontend): uncomment the btn in the promobanner

--- src/frontend/src/lib/components/dapps/DappPromoBanner.svelte
+++ src/frontend/src/lib/components/dapps/DappPromoBanner.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Button from '$lib/components/ui/Button.svelte';
 	import Img from '$lib/components/ui/Img.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 	import type { FeaturedDappDescription } from '$lib/types/dappDescription';
@@ -29,10 +30,9 @@
 				<h4 class="text-white">{dAppDescription.name}</h4>
 			</div>
 
-			<!--	TODO add button as soon as modalOpen feature is implemented		-->
-			<!--			<Button styleClass="flex-grow-0" colorStyle="secondary" on:click>-->
-			<!--				{$i18n.core.text.view}-->
-			<!--			</Button>-->
+			<Button paddingSmall styleClass="flex-grow-0 text-sm" colorStyle="secondary" on:click>
+				{$i18n.core.text.view}
+			</Button>
 		</div>
 	</div>
 </article>
