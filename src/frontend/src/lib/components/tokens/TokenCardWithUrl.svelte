2c38cd3f320f6f06143c00ba76a7854e3e6eccd3
Robert Schlittler <robert.schlittler@inacta.ch>
2024-10-21 13:43:49 +0200

    feat(frontend): style TokenCardWithUrl.svelte according to figma

--- src/frontend/src/lib/components/tokens/TokenCardWithUrl.svelte
+++ src/frontend/src/lib/components/tokens/TokenCardWithUrl.svelte
@@ -11,9 +11,9 @@
 	$: url = transactionsUrl({ token });
 </script>
 
-<div class="mb-6 flex gap-3 sm:gap-8">
+<div class="flex gap-3 sm:gap-8 rounded-xl hover:bg-white">
 	<a
-		class="flex-1 no-underline"
+		class="flex-1 no-underline py-2 px-3"
 		href={url}
 		aria-label={replacePlaceholders($i18n.transactions.text.open_transactions, {
 			token: token.symbol
