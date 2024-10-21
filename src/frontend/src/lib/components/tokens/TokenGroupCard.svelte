f4abd8dce09339d7e1093e268cf4564d93cf6219
Robert Schlittler <robert.schlittler@inacta.ch>
2024-10-21 13:44:01 +0200

    feat(frontend): implement TokenGroupCard.svelte

--- src/frontend/src/lib/components/tokens/TokenGroupCard.svelte
+++ src/frontend/src/lib/components/tokens/TokenGroupCard.svelte
@@ -1,38 +1,63 @@
 <script lang="ts">
 	import Listener from '$lib/components/core/Listener.svelte';
-	import TokenCard from '$lib/components/tokens/TokenCard.svelte';
 	import TokenCardWithUrl from '$lib/components/tokens/TokenCardWithUrl.svelte';
 	import TokenLogo from '$lib/components/tokens/TokenLogo.svelte';
 	import TokenName from '$lib/components/tokens/TokenName.svelte';
-	import TokenSymbol from '$lib/components/tokens/TokenSymbol.svelte';
 	import Card from '$lib/components/ui/Card.svelte';
 	import { TOKEN_GROUP } from '$lib/constants/test-ids.constants';
 	import type { TokenGroupUi, TokenUi } from '$lib/types/token';
+	import TokenCardWithOnClick from '$lib/components/tokens/TokenCardWithOnClick.svelte';
+	import TokenCardContent from '$lib/components/tokens/TokenCardContent.svelte';
+	import ExchangeTokenValue from '$lib/components/exchange/ExchangeTokenValue.svelte';
+	import TokenBalance from '$lib/components/tokens/TokenBalance.svelte';
+	import { BigNumber } from '@ethersproject/bignumber';
+	import { LogoSize } from '$lib/types/logo-size';
 
 	export let tokenGroup: TokenGroupUi;
+	let isOpened: boolean = false;
 
-	const totalBalance = tokenGroup.tokens.reduce((sum, token: TokenUi) => sum + (token.balance ?? 0), 0);
-
-	const totalBalanceUsd = tokenGroup.tokens.reduce((sum, token: TokenUi) => sum + (token.usdBalance ?? 0), 0);
+	$: totalBalance = tokenGroup.tokens.reduce((sum, token: TokenUi) => sum.add(token.balance ?? BigNumber.from(0)), BigNumber.from(0));
+	$: totalUsdBalance = tokenGroup.tokens.reduce((sum, token: TokenUi) => sum + (token.usdBalance ?? 0), 0);
+	$: tokenGroupBalance = { balance: totalBalance, symbol: tokenGroup.header.symbol, decimals: tokenGroup.header.decimals, usdBalance: totalUsdBalance };
 </script>
 
-<Card noMargin testId={`${TOKEN_GROUP}-${tokenGroup.header.symbol}`}>
-	<TokenSymbol token={{symbol:tokenGroup.header.name, network: tokenGroup.nativeNetwork}} />
+<TokenCardWithOnClick on:click={()=> isOpened = !isOpened}
+											styleClass="!m-0 py-2 px-3 w-full rounded-xl hover:text-blue-ribbon hover:bg-white {isOpened ? 'bg-white rounded-b-none' : ''}">
 
-	<TokenName token={{name: tokenGroup.header.symbol}} slot="description" />
+	<Card noMargin testId={`${TOKEN_GROUP}-${tokenGroup.header.symbol}`}>
 
-	<TokenLogo token={{name: tokenGroup.header.name, icon: tokenGroup.header.icon, network: tokenGroup.nativeNetwork}} showNetworkIcon={false} slot="icon" color="white" />
+		<div class="flex">
+			{tokenGroup.header.name}
+		</div>
+		<TokenName token={{name: tokenGroup.header.symbol}} slot="description" />
 
-	<span slot="balance">{totalBalance}</span>
-	<span slot="amountDescription">{totalBalanceUsd}</span>
-</Card>
+		<div class="relative" slot="icon">
+			<TokenLogo token={{name: tokenGroup.header.name, icon: tokenGroup.header.icon, network: tokenGroup.nativeNetwork}}
+								 showNetworkIcon={false} color="white" />
+			<span
+				class="token-count border border-light-grey h-6 w-6 font-semibold absolute bottom-0 flex items-center justify-center -right-2.5 rounded-full bg-white">{tokenGroup.tokens.length}</span>
+		</div>
 
-<div class="bg-grey">
-	{#each tokenGroup.tokens as token}
-		<Listener {token}>
-			<TokenCardWithUrl {token}>
-				<TokenCard {token} />
-			</TokenCardWithUrl>
-		</Listener>
-	{/each}
-</div>
+		<TokenBalance slot="amount" token={tokenGroupBalance} />
+		<ExchangeTokenValue slot="amountDescription" token={tokenGroupBalance} />
+
+	</Card>
+</TokenCardWithOnClick>
+
+{#if isOpened}
+	<div class="bg-white/40 rounded-b-xl pt-2">
+		{#each tokenGroup.tokens as token}
+			<Listener {token}>
+				<TokenCardWithUrl {token}>
+					<TokenCardContent logoSize={LogoSize.SM} {token} />
+				</TokenCardWithUrl>
+			</Listener>
+		{/each}
+	</div>
+{/if}
+
+<style lang="scss">
+    .token-count {
+					color: var(--color-secondary);
+			}
+</style>
