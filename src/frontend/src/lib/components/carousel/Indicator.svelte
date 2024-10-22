7d3e41e6f6676c3378bb170baf889986baa41876
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-10-22 15:38:16 +0200

    feat(frontend): Carousel Indicator components (#3044)
    # Motivation
    
    This PR includes Carousel Indicator components. 
    
    
    https://github.com/user-attachments/assets/5545fc0c-b7ca-4368-a1cc-c4fffb3b3718

---
+++ src/frontend/src/lib/components/carousel/Indicator.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { i18n } from '$lib/stores/i18n.store';
+	import { replacePlaceholders } from '$lib/utils/i18n.utils';
+
+	export let currentSlide: number;
+	export let index: number;
+
+	let isActive: boolean;
+	$: isActive = currentSlide === index;
+</script>
+
+<button
+	on:click
+	aria-label={replacePlaceholders($i18n.carousel.text.indicator, { $index: `${index + 1}` })}
+	class="{`${isActive ? 'w-7 bg-black' : 'w-4 bg-dust'} mr-1 h-1.5 transition-all duration-300 ease-linear last:mr-0`}}"
+></button>
