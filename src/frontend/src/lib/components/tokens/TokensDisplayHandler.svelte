c00f5fabba27842c8b9e2b787b6e8ab9c1a2882c
Dario Lötscher <dario.loetscher@inacta.ch>
2024-10-22 11:10:40 +0200

    feat(frontend): fix type issue

--- src/frontend/src/lib/components/tokens/TokensDisplayHandler.svelte
+++ src/frontend/src/lib/components/tokens/TokensDisplayHandler.svelte
@@ -2,13 +2,13 @@
 	import { debounce } from '@dfinity/utils';
 	import { combinedDerivedSortedNetworkTokensUi } from '$lib/derived/network-tokens.derived';
 	import { showZeroBalances } from '$lib/derived/settings.derived';
-	import type { TokenUi, TokenUiOrGroupUi } from '$lib/types/token';
+	import type { TokenUiOrGroupUi } from '$lib/types/token';
 	import { groupTokensByTwin } from '$lib/utils/token.utils.js';
 
 	// We start it as undefined to avoid showing an empty list before the first update.
 	export let tokens: TokenUiOrGroupUi[] | undefined = undefined;
 
-	let sortedTokens: TokenUi[];
+	let sortedTokens: TokenUiOrGroupUi[];
 	$: sortedTokens = groupTokensByTwin(
 		$combinedDerivedSortedNetworkTokensUi.filter(
 			({ balance, usdBalance }) => Number(balance ?? 0n) || (usdBalance ?? 0) || $showZeroBalances
