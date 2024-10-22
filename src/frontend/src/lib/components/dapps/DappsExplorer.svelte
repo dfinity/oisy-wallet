ab9c988f7a38eef53fbc6b1646251bcd3a0936cc
Robert Schlittler <43857623+loki344@users.noreply.github.com>
2024-10-22 10:01:02 +0200

    feat(frontend): dapp route content (#3019)
    # Motivation
    
    Render the dapp explorer component under /explore
    
    # Changes
    
    - Implement DappExplorer with a Promobanner, a row to filter the dapps
    by the tags and a card grid.
    
    
    # Screenshots
    <img width="715" alt="image"
    src="https://github.com/user-attachments/assets/fc0e5985-cbe8-41d9-9fae-ced764b80cfd">
    
    <img width="595" alt="image"
    src="https://github.com/user-attachments/assets/1f8ca3be-636f-49ca-8611-9822d4e06000">

---
+++ src/frontend/src/lib/components/dapps/DappsExplorer.svelte
@@ -0,0 +1,11 @@
+<script>
+	import DappsExplorerSignedIn from '$lib/components/dapps/DappsExplorerSignedIn.svelte';
+	import DappsExplorerSignedOut from '$lib/components/dapps/DappsExplorerSignedOut.svelte';
+	import { authSignedIn } from '$lib/derived/auth.derived';
+</script>
+
+{#if $authSignedIn}
+	<DappsExplorerSignedIn />
+{:else}
+	<DappsExplorerSignedOut />
+{/if}
---
+++ src/frontend/src/lib/components/dapps/DappsExplorerSignedIn.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+	import { isNullish, nonNullish } from '@dfinity/utils';
+	import { fade } from 'svelte/transition';
+
+	import DappCard from '$lib/components/dapps/DappCard.svelte';
+	import DappModal from '$lib/components/dapps/DappModal.svelte';
+	import DappPromoBanner from '$lib/components/dapps/DappPromoBanner.svelte';
+	import Button from '$lib/components/ui/Button.svelte';
+	import { modalDAppDetails } from '$lib/derived/modal.derived';
+	import { i18n } from '$lib/stores/i18n.store';
+	import { modalStore } from '$lib/stores/modal.store';
+	import {
+		type DappDescription,
+		dAppDescriptions,
+		type FeaturedDappDescription
+	} from '$lib/types/dappDescription';
+	import { replacePlaceholders } from '$lib/utils/i18n.utils';
+
+	// For the moment only the first featured dapp is highlighted
+	const selectFirstFeaturedDapp = (): FeaturedDappDescription | undefined =>
+		dAppDescriptions.find(
+			({ featured, screenshots }) => featured && nonNullish(screenshots) && screenshots.length
+		) as FeaturedDappDescription | undefined;
+
+	const featuredDapp = selectFirstFeaturedDapp();
+
+	let selectedTag: string | undefined = undefined;
+	const uniqueTags = new Set(
+		dAppDescriptions.flatMap((dapp) => dapp.tags).sort((tagA, tagB) => tagA.localeCompare(tagB))
+	);
+
+	$: filteredDapps = dAppDescriptions.filter(
+		({ tags }) => isNullish(selectedTag) || tags.includes(selectedTag)
+	);
+
+	let selectedDapp: DappDescription | undefined = undefined;
+	$: selectedDapp = $modalDAppDetails
+		? ($modalStore?.data as DappDescription | undefined)
+		: undefined;
+</script>
+
+<h1 class="mb-5 mt-6">{$i18n.dapps.text.title}</h1>
+
+{#if nonNullish(featuredDapp) && nonNullish(featuredDapp.screenshots)}
+	<div class="mb-10">
+		<DappPromoBanner
+			on:click={() => modalStore.openDappDetails(featuredDapp)}
+			dAppDescription={featuredDapp}
+		/>
+	</div>
+{/if}
+
+<div class="flex flex-wrap gap-4">
+	<Button
+		paddingSmall
+		ariaLabel={$i18n.dapps.alt.show_all}
+		on:click={() => (selectedTag = undefined)}
+		styleClass="text-nowrap max-w-fit text-sm"
+		colorStyle={selectedTag === undefined ? 'primary' : 'tertiary'}
+		>{$i18n.dapps.text.all_dapps}</Button
+	>
+	{#each uniqueTags as tag}
+		<Button
+			paddingSmall
+			ariaLabel={replacePlaceholders($i18n.dapps.alt.show_tag, { $tag: tag })}
+			on:click={() => (selectedTag = tag)}
+			styleClass="text-nowrap max-w-fit text-sm"
+			colorStyle={selectedTag === tag ? 'primary' : 'tertiary'}>{tag}</Button
+		>
+	{/each}
+</div>
+
+<ul class="mt-8 grid list-none grid-cols-1 gap-x-4 gap-y-7 md:grid-cols-3">
+	{#each filteredDapps as dApp}
+		<li class="flex" in:fade>
+			<DappCard
+				on:click={() => {
+					modalStore.openDappDetails(dApp);
+				}}
+				dAppDescription={dApp}
+			/>
+		</li>
+	{/each}
+</ul>
+
+{#if $modalDAppDetails && nonNullish(selectedDapp)}
+	<DappModal dAppDescription={selectedDapp} />
+{/if}
