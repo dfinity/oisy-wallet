0210590e1b5ccd25edf238a379f35cff4d95818e
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-22 11:58:20 +0200

    refactor: break grouping and sorting

--- src/frontend/src/lib/components/tokens/TokensDisplayHandler.svelte
+++ src/frontend/src/lib/components/tokens/TokensDisplayHandler.svelte
@@ -2,20 +2,21 @@
 	import { debounce } from '@dfinity/utils';
 	import { combinedDerivedSortedNetworkTokensUi } from '$lib/derived/network-tokens.derived';
 	import { showZeroBalances } from '$lib/derived/settings.derived';
-	import type { TokenUiOrGroupUi } from '$lib/types/token';
+	import type { TokenUi, TokenUiOrGroupUi } from '$lib/types/token';
 	import { groupTokensByTwin } from '$lib/utils/token.utils.js';
 
 	// We start it as undefined to avoid showing an empty list before the first update.
 	export let tokens: TokenUiOrGroupUi[] | undefined = undefined;
 
-	let sortedTokens: TokenUiOrGroupUi[];
-	$: sortedTokens = groupTokensByTwin(
-		$combinedDerivedSortedNetworkTokensUi.filter(
-			({ balance, usdBalance }) => Number(balance ?? 0n) || (usdBalance ?? 0) || $showZeroBalances
-		)
+	let sortedTokens: TokenUi[];
+	$: sortedTokens = $combinedDerivedSortedNetworkTokensUi.filter(
+		({ balance, usdBalance }) => Number(balance ?? 0n) || (usdBalance ?? 0) || $showZeroBalances
 	);
 
-	const updateTokensToDisplay = () => (tokens = [...sortedTokens]);
+	let groupedTokens: TokenUiOrGroupUi[];
+	$: groupedTokens = groupTokensByTwin(sortedTokens);
+
+	const updateTokensToDisplay = () => (tokens = [...groupedTokens]);
 
 	const debounceUpdateTokensToDisplay = debounce(updateTokensToDisplay, 1000);
 
