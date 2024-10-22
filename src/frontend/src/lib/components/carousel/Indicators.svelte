295707b98e5a90020285655790b87e0063a1908b
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-10-22 15:38:16 +0200

    feat(frontend): Carousel Indicator components (#3044)
    # Motivation
    
    This PR includes Carousel Indicator components. 
    
    
    https://github.com/user-attachments/assets/5545fc0c-b7ca-4368-a1cc-c4fffb3b3718

---
+++ src/frontend/src/lib/components/carousel/Indicators.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Indicator from '$lib/components/carousel/Indicator.svelte';
+
+	export let currentSlide: number;
+	export let totalSlides: number;
+
+	export let onIndicatorClick: (index: number) => void;
+</script>
+
+<div class="flex w-[75%] items-center justify-center">
+	{#each { length: totalSlides } as _, index}
+		<Indicator on:click={() => onIndicatorClick(index)} {index} {currentSlide} />
+	{/each}
+</div>
