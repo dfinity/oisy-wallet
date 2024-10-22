257dfd05c153d898a63c7824dabf63302f4f3c38
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-22 09:27:30 +0200

    style: pulsating loads

--- src/frontend/src/lib/components/tokens/TokenExchangeBalance.svelte
+++ src/frontend/src/lib/components/tokens/TokenExchangeBalance.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { nonNullish } from '@dfinity/utils';
+	import { isNullish, nonNullish } from '@dfinity/utils';
 	import type { TokenFinancialData } from '$lib/types/token';
 	import { formatUSD } from '$lib/utils/format.utils';
 
@@ -11,6 +11,11 @@
 	{#if nonNullish(balance) && nonNullish(usdBalance)}
 		{formatUSD({ value: usdBalance })}
 	{:else}
-		{formatUSD({ value: 0, options: { minFraction: 0, maxFraction: 0 } }).replace('0', '-')}
+		<span class:animate-pulse={isNullish(balance)}
+			>{formatUSD({
+				value: 0,
+				options: { minFraction: 0, maxFraction: 0 }
+			}).replace('0', '-')}
+		</span>
 	{/if}
 </output>
