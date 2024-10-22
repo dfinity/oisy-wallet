8574a002ef6066ca4a7d97cea580071c718d378d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-22 14:42:44 +0200

    feat(frontend): Provide token count in Token logo

--- src/frontend/src/lib/components/tokens/TokenLogo.svelte
+++ src/frontend/src/lib/components/tokens/TokenLogo.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { nonNullish } from '@dfinity/utils';
 	import Logo from '$lib/components/ui/Logo.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 	import type { Token } from '$lib/types/token';
@@ -6,6 +7,7 @@
 
 	export let token: Token;
 	export let color: 'dust' | 'off-white' | 'white' = 'dust';
+	export let tokenCount: number | undefined = undefined;
 	export let showNetworkIcon = true;
 	export let networkIconBlackAndWhite = false;
 	export let ring = false;
@@ -25,7 +27,13 @@
 		{color}
 		{ring}
 	/>
-	{#if showNetworkIcon}
+	{#if nonNullish(tokenCount) && tokenCount > 0}
+		<span
+			class="token-count absolute -right-2.5 bottom-0 flex h-6 w-6 items-center justify-center rounded-full border-[0.5px] border-light-grey bg-white text-sm font-semibold"
+		>
+			{tokenCount}
+		</span>
+	{:else if showNetworkIcon}
 		<div class="absolute -bottom-1 -right-1">
 			<Logo
 				src={networkIconBlackAndWhite ? networkIconBW : networkIcon}
@@ -35,3 +43,9 @@
 		</div>
 	{/if}
 </div>
+
+<style lang="scss">
+	.token-count {
+		color: var(--color-secondary);
+	}
+</style>
