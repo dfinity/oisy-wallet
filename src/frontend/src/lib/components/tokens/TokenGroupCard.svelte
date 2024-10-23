e08bfea1c306d6c9917b729f473ae91c9532102b
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-23 17:09:10 +0200

    fix: correct merge

--- src/frontend/src/lib/components/tokens/TokenGroupCard.svelte
+++ src/frontend/src/lib/components/tokens/TokenGroupCard.svelte
@@ -6,16 +6,22 @@
 	import TokenCardWithUrl from '$lib/components/tokens/TokenCardWithUrl.svelte';
 	import { TOKEN_GROUP } from '$lib/constants/test-ids.constants';
 	import { SLIDE_PARAMS } from '$lib/constants/transition.constants';
-	import type { TokenGroupUi, TokenUi } from '$lib/types/token';
+	import type { TokenUiGroup } from '$lib/types/token';
+	import type { CardData } from '$lib/types/token-card';
 
-	export let tokenGroup: TokenGroupUi;
+	export let tokenGroup: TokenUiGroup;
 
 	let isOpened = false;
 
-	let tokenGroupHeader: TokenUi;
-	$: tokenGroupHeader = {
-		...tokenGroup.nativeToken,
-		oisySymbol: { oisySymbol: tokenGroup.nativeToken.name },
+	let { nativeToken } = tokenGroup;
+
+	let headerData: CardData;
+	$: headerData = {
+		symbol: nativeToken.symbol,
+		name: nativeToken.name,
+		decimals: nativeToken.decimals,
+		network: nativeToken.network,
+		oisySymbol: { oisySymbol: nativeToken.name },
 		oisyName: { oisyName: tokenGroup.header.name },
 		balance: tokenGroup.balance,
 		usdBalance: tokenGroup.usdBalance
@@ -30,18 +36,14 @@
 			? 'bg-white rounded-b-none'
 			: ''}"
 	>
-		<TokenCard
-			token={tokenGroupHeader}
-			testIdPrefix={TOKEN_GROUP}
-			tokenCount={tokenGroup.tokens.length}
-		/>
+		<TokenCard data={headerData} testIdPrefix={TOKEN_GROUP} tokenCount={tokenGroup.tokens.length} />
 	</TokenCardWithOnClick>
 
 	{#if isOpened}
 		<div class="flex flex-col gap-3 rounded-b-xl bg-white/40 pt-2" transition:slide={SLIDE_PARAMS}>
 			{#each tokenGroup.tokens as token (token.id)}
 				<TokenCardWithUrl {token}>
-					<TokenCardContent logoSize="md" {token} />
+					<TokenCardContent logoSize="md" data={token} />
 				</TokenCardWithUrl>
 			{/each}
 		</div>
