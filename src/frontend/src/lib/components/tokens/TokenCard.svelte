b0e951ea48a28653bb644a4be23f12292704251a
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-10-23 09:11:07 +0200

    test(frontend): Add test ID for token group in TokenCard (#3052)
    # Motivation
    
    The component `TokenCard` will be re-used for token groups too. So we
    create a test ID based on its usage: either as single-token card or as
    header token card for a group.

--- src/frontend/src/lib/components/tokens/TokenCard.svelte
+++ src/frontend/src/lib/components/tokens/TokenCard.svelte
@@ -3,13 +3,14 @@
 	import TokenName from '$lib/components/tokens/TokenName.svelte';
 	import TokenSymbol from '$lib/components/tokens/TokenSymbol.svelte';
 	import Card from '$lib/components/ui/Card.svelte';
-	import { TOKEN_CARD } from '$lib/constants/test-ids.constants';
+	import { TOKEN_CARD, TOKEN_GROUP } from '$lib/constants/test-ids.constants';
 	import type { Token } from '$lib/types/token';
 
 	export let token: Token;
+	export let testIdPrefix: typeof TOKEN_CARD | typeof TOKEN_GROUP = TOKEN_CARD;
 </script>
 
-<Card noMargin testId={`${TOKEN_CARD}-${token.symbol}`}>
+<Card noMargin testId={`${testIdPrefix}-${token.symbol}`}>
 	<TokenSymbol {token} />
 
 	<TokenName {token} slot="description" />
