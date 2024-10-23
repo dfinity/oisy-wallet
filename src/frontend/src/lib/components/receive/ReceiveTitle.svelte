d9a18e69796aff6c6967e2e93eac2178da8d89ba
David Dal Busco <david.dalbusco@dfinity.org>
2024-10-23 11:56:54 +0200

    feat(frontend): receive modal title with symbol (#3036)
    # Motivation
    
    The designers want to display the token symbol within the "Receive"
    modal title.
    
    # Changes
    
    - Since `addressToken` is per definition potentially `undefined` either
    just display "Receive" or "Receive $token"
    - Create a component to extract the logic for the title (could have been
    a function but felt cleaner that way. no strong opinion here)
    - Consume the title component in receive modals
    
    # Screenshots
    
    <img width="1536" alt="Capture d’écran 2024-10-22 à 13 45 03"
    src="https://github.com/user-attachments/assets/4747471d-e323-4e04-9f81-394b38aa320f">
    <img width="1536" alt="Capture d’écran 2024-10-22 à 13 44 55"
    src="https://github.com/user-attachments/assets/077fa373-4585-453b-b5b9-9670c12542e2">
    <img width="1536" alt="Capture d’écran 2024-10-22 à 13 44 50"
    src="https://github.com/user-attachments/assets/ba52e90e-a920-455a-b991-b657f9164392">
    <img width="1536" alt="Capture d’écran 2024-10-22 à 13 44 45"
    src="https://github.com/user-attachments/assets/a0de824f-bdfd-49f4-a841-de070ec2a6c7">
    <img width="1536" alt="Capture d’écran 2024-10-22 à 13 44 38"
    src="https://github.com/user-attachments/assets/fc3a0354-bc76-486f-9f9c-6357c4c98777">
    <img width="1536" alt="Capture d’écran 2024-10-22 à 13 44 30"
    src="https://github.com/user-attachments/assets/b051193a-9073-43c9-85db-14b9402e0601">
    <img width="1536" alt="Capture d’écran 2024-10-22 à 13 44 26"
    src="https://github.com/user-attachments/assets/024d344d-7edc-4082-97a2-e323337e4e48">

---
+++ src/frontend/src/lib/components/receive/ReceiveTitle.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { isNullish } from '@dfinity/utils';
+	import { i18n } from '$lib/stores/i18n.store';
+	import type { Token } from '$lib/types/token';
+	import { replacePlaceholders } from '$lib/utils/i18n.utils.js';
+
+	export let addressToken: Token | undefined;
+
+	let symbol: string | undefined;
+	$: symbol = addressToken?.symbol;
+</script>
+
+{isNullish(symbol)
+	? $i18n.receive.text.receive
+	: replacePlaceholders($i18n.receive.text.receive_token, {
+			$token: symbol
+		})}
