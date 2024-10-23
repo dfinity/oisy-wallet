ec24bfbadea9f37373ed4b3e00911c471cc37c41
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-10-23 16:53:59 +0200

    feat(frontend): Carousel utils (#3067)
    # Motivation
    
    This PR includes utils needed for the Carousel component. The idea is to
    take provided to `Carousel` as children slides (can be any HTML
    elements, components, etc.) and to create a slider frame - a div that
    has `slidesNumber * slideWidth` width (only active slide is visible) and
    includes all slides with equal (percentage) size. The other 3 functions
    are small helpers to enable/disable (we need to disable animation when
    we do last-to-first and first-to-last switches) animation and to change
    a current slide by applying `transform` to the frame.
    
    Please let me know if you need more context to review this PR (e.g. you
    also want to include the UI component itself here).
    
    
    
    https://github.com/user-attachments/assets/0cf5618b-1e26-45d2-8b9f-b2d1c3ce771c

---
+++ src/frontend/src/lib/utils/carousel.utils.ts
@@ -0,0 +1,106 @@
+import { isNullish } from '@dfinity/utils';
+
+interface CommonParams {
+	sliderFrame: HTMLElement | undefined;
+}
+
+/**
+ * A util for building carousel slider frame item.
+ */
+export const buildCarouselSliderFrameItem = ({
+	slide,
+	totalSlides
+}: {
+	slide: Node;
+	totalSlides: number;
+}): HTMLDivElement => {
+	// Create slider frame item and apply styling
+	// TODO: check if slide is an HTMLElement and if so, only set width w/o creating a new div
+	const frameItem = document.createElement('div');
+
+	if (totalSlides > 0) {
+		frameItem.style.width = `${100 / totalSlides}%`;
+	}
+
+	frameItem.appendChild(slide);
+
+	return frameItem;
+};
+
+/**
+ * A util for extending provided sliderFrame element with carousel items and width style..
+ */
+export const extendCarouselSliderFrame = ({
+	sliderFrame,
+	slides,
+	slideWidth
+}: {
+	slides: Node[];
+	slideWidth: number;
+} & CommonParams) => {
+	if (isNullish(sliderFrame)) {
+		return;
+	}
+
+	// TODO: use Svelte 5 and/or pure CSS in order to avoid the hacky solution
+	// We add the last slide as the first element to properly animate last-to-first slide transition.
+	// The same applies to the last element - we use the first slide as the last item of the array.
+	const extendedSlides = [
+		slides[slides.length - 1].cloneNode(true),
+		...slides,
+		slides[0].cloneNode(true)
+	];
+	const totalSlides = extendedSlides.length;
+
+	// Calculate and set width for CSS transform to work correctly
+	sliderFrame.style.width = `${slideWidth * totalSlides}px`;
+
+	sliderFrame.append(
+		...extendedSlides.map((slide) =>
+			buildCarouselSliderFrameItem({
+				slide,
+				totalSlides
+			})
+		)
+	);
+};
+
+/**
+ * Disable transition for the provided slider frame.
+ */
+export const disableTransition = ({ sliderFrame }: CommonParams) => {
+	if (isNullish(sliderFrame)) {
+		return;
+	}
+
+	sliderFrame.style.transition = 'none';
+};
+
+/**
+ * Enable transition for the provided slider frame.
+ */
+export const enableTransition = ({
+	sliderFrame,
+	duration,
+	easing
+}: { duration: number; easing: string } & CommonParams) => {
+	if (isNullish(sliderFrame)) {
+		return;
+	}
+
+	sliderFrame.style.transition = `all ${duration}ms ${easing}`;
+};
+
+/**
+ * Update visible part of the provided slider frame.
+ */
+export const moveSlider = ({
+	animateTo,
+	sliderFrame
+}: { animateTo: number } & CommonParams): void => {
+	if (isNullish(sliderFrame)) {
+		return;
+	}
+
+	sliderFrame.style.transform = `translate3d(${animateTo}px, 0, 0)`;
+};
