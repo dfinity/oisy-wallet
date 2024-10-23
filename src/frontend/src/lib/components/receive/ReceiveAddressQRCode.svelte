c77771693c3819aed29bb22e5822321245c907b3
David Dal Busco <david.dalbusco@outlook.com>
2024-10-23 08:29:57 +0200

    chore: fmt

--- src/frontend/src/lib/components/receive/ReceiveAddressQRCode.svelte
+++ src/frontend/src/lib/components/receive/ReceiveAddressQRCode.svelte
@@ -3,9 +3,9 @@
 	import ReceiveAddressQRCodeContent from '$lib/components/receive/ReceiveAddressQRCodeContent.svelte';
 	import Button from '$lib/components/ui/Button.svelte';
 	import ContentWithToolbar from '$lib/components/ui/ContentWithToolbar.svelte';
+	import { RECEIVE_TOKENS_MODAL_QR_CODE_OUTPUT } from '$lib/constants/test-ids.constants';
 	import { i18n } from '$lib/stores/i18n.store';
 	import type { Token } from '$lib/types/token';
-	import { RECEIVE_TOKENS_MODAL_QR_CODE_OUTPUT } from '$lib/constants/test-ids.constants';
 
 	export let address: undefined | string;
 	export let addressLabel: string | undefined = undefined;
