937f39cc6ce964ef7d35be97a2e0f63a37a32bdc
Robert Schlittler <robert.schlittler@inacta.ch>
2024-10-23 11:24:24 +0200

    feat(frontend): format

--- src/frontend/src/lib/components/ui/ImgBanner.svelte
+++ src/frontend/src/lib/components/ui/ImgBanner.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <div class="relative block" class:h-56={size === 'big'} class:h-40={size === 'small'}>
-	<div class="absolute w-full h-full">
+	<div class="absolute h-full w-full">
 		<Img {src} {alt} {width} {height} {fitHeight} />
 	</div>
 </div>
