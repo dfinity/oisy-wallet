ece8d142a587c2f661f0f958a19c6328352d9e7c
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-24 10:51:21 +0200

    fix: add writable

--- src/frontend/src/lib/stores/certified-setter.store.ts
+++ src/frontend/src/lib/stores/certified-setter.store.ts
@@ -1,8 +1,12 @@
 import type { CertifiedStoreBaseData } from '$lib/stores/certified.store';
-import { initSetterStore, type SetterStoreStore } from '$lib/stores/setter.store';
+import {
+	initSetterStore,
+	type SetterStoreStore,
+	type WritableUpdateStore
+} from '$lib/stores/setter.store';
 
 export type CertifiedSetterStoreStore<T extends CertifiedStoreBaseData> = SetterStoreStore<T>;
 
 export const initCertifiedSetterStore = <
 	T extends CertifiedStoreBaseData
->(): CertifiedSetterStoreStore<T> => initSetterStore<T>();
+>(): CertifiedSetterStoreStore<T> & WritableUpdateStore<T> => initSetterStore<T>();
