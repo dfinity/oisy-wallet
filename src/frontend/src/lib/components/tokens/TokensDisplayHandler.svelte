6cdac0cd8ef52d173791ec0e7895d7e4830bdfae
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-10-24 19:43:32 +0200

    feat(frontend): Implement token groups in tokens list (#3099)
    # Motivation
    
    We want to start grouping tokens that refers to the same underlying. For
    example `BTC` and `ckBTC`, or `ETH` and `ckETH`. Basically twin tokens,
    where the "native token" definition falls on the most mainstream one of
    the group (BTC, ETH, etc)
    
    To do that we first group the sorted tokens and then we show
    single-tokens differently from the token groups (we use the
    `TokenCardGroup` component introduced recently).
    
    
    # Changes
    
    - Modify function `isTokenUiGroup` to recognize the correct group type
    instead of the token type.
    - Add an ID for the token group: this will be the same of the native
    token so that the animation can recognize it as the same element.
    - Group tokens with function `groupTokensByTwin` in
    `TokenDisplayHandler`.
    - In the token list, differentiate how we show a group from how we show
    a single-token card.
    
    # Tests
    
    I deployed the current branch in a test environment (otherwise there
    were no tokens to be grouped...):
    
    
    
    https://github.com/user-attachments/assets/46f4abbc-e661-4e1b-aadf-18e973da1d97
    
    ---------
    
    Co-authored-by: Dario Lötscher <dario.loetscher@inacta.ch>
    Co-authored-by: darioloetsc <76442731+darioloetsc@users.noreply.github.com>

--- src/frontend/src/lib/components/tokens/TokensDisplayHandler.svelte
+++ src/frontend/src/lib/components/tokens/TokensDisplayHandler.svelte
@@ -2,17 +2,21 @@
 	import { debounce } from '@dfinity/utils';
 	import { combinedDerivedSortedNetworkTokensUi } from '$lib/derived/network-tokens.derived';
 	import { showZeroBalances } from '$lib/derived/settings.derived';
-	import type { Token, TokenUi } from '$lib/types/token';
+	import type { TokenUi, TokenUiOrGroupUi } from '$lib/types/token';
+	import { groupTokensByTwin } from '$lib/utils/token.utils';
 
 	// We start it as undefined to avoid showing an empty list before the first update.
-	export let tokens: Token[] | undefined = undefined;
+	export let tokens: TokenUiOrGroupUi[] | undefined = undefined;
 
 	let sortedTokens: TokenUi[];
 	$: sortedTokens = $combinedDerivedSortedNetworkTokensUi.filter(
 		({ balance, usdBalance }) => Number(balance ?? 0n) || (usdBalance ?? 0) || $showZeroBalances
 	);
 
-	const updateTokensToDisplay = () => (tokens = [...sortedTokens]);
+	let groupedTokens: TokenUiOrGroupUi[];
+	$: groupedTokens = groupTokensByTwin(sortedTokens);
+
+	const updateTokensToDisplay = () => (tokens = [...groupedTokens]);
 
 	const debounceUpdateTokensToDisplay = debounce(updateTokensToDisplay, 500);
 
