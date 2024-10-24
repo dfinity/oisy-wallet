2e474544c051e0b9a0920173ec63d825b68d1646
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-24 17:00:52 +0200

    feat(frontend): Page for Terms and Conditions

---
+++ src/frontend/src/lib/components/terms-and-conditions/TermsAndConditions.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+import { replaceOisyPlaceholders } from '$lib/utils/i18n.utils.js';
+import CoverHow from '$lib/assets/cover-how-it-works.png';
+import ImgBanner from '$lib/components/ui/ImgBanner.svelte';
+import IconIdCard from '$lib/components/icons/IconIdCard.svelte';
+import IconKey from '$lib/components/icons/IconKey.svelte';
+import { Html } from '@dfinity/gix-components';
+import IconWorld from '$lib/components/icons/IconWorld.svelte';
+import IconIcLogoPlain from '$lib/components/icons/IconIcLogoPlain.svelte';
+import IconGitHub from '$lib/components/icons/IconGitHub.svelte';
+import { i18n } from '$lib/stores/i18n.store.js';
+
+
+
+
+</script>
+
+<h1 class="text-xl">{replaceOisyPlaceholders($i18n.about.how.text.title)}</h1>
+
+<div class="stretch pt-4">
+	<ImgBanner size="small" src={CoverHow} alt={$i18n.about.how.text.title} />
+
+	<p class="mt-12">
+		<IconKey />
+		<Html text={replaceOisyPlaceholders($i18n.about.how.text.self_custody)} />
+	</p>
+
+	<p class="mt-6">
+		<IconIcLogoPlain />
+		<Html text={replaceOisyPlaceholders($i18n.about.how.text.fully_on_chain)} />
+	</p>
+
+	<p class="mt-6">
+		<IconWorld />
+		<Html text={replaceOisyPlaceholders($i18n.about.how.text.cross_device)} />
+	</p>
+
+	<p class="mt-6">
+		<IconIdCard />
+		<Html text={replaceOisyPlaceholders($i18n.about.how.text.verifiable_credentials)} />
+	</p>
+
+	<p class="my-6">
+		<IconGitHub />
+		<Html text={replaceOisyPlaceholders($i18n.about.how.text.open_source)} />
+	</p>
+</div>
---
+++ src/frontend/src/routes/(terms-and-conditions)/terms-and-conditions/+layout.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import AuthGuard from '$lib/components/auth/AuthGuard.svelte';
+	import Footer from '$lib/components/core/Footer.svelte';
+	import LoadersGuard from '$lib/components/core/LoadersGuard.svelte';
+	import Modals from '$lib/components/core/Modals.svelte';
+	import Header from '$lib/components/hero/Header.svelte';
+	import Hero from '$lib/components/hero/Hero.svelte';
+	import NavigationMenu from '$lib/components/navigation/NavigationMenu.svelte';
+	import SplitPane from '$lib/components/ui/SplitPane.svelte';
+	import { authNotSignedIn, authSignedIn } from '$lib/derived/auth.derived';
+	import { pageToken } from '$lib/derived/page-token.derived';
+	import { token } from '$lib/stores/token.store';
+	import { isRouteDappExplorer, isRouteSettings, isRouteTransactions } from '$lib/utils/nav.utils';
+	import OisyWalletLogoLink from '$lib/components/core/OisyWalletLogoLink.svelte';
+
+	// TODO: We should consider adding a description for the pages, as this block of code is now appearing in two places.
+	// Other areas, like the Menu, are also somewhat disorganized, with navigation logic spread across multiple locations.
+	let route: 'transactions' | 'tokens' | 'settings' | 'explore' = 'tokens';
+	$: route = isRouteSettings($page)
+		? 'settings'
+		: isRouteDappExplorer($page)
+			? 'explore'
+			: isRouteTransactions($page)
+				? 'transactions'
+				: 'tokens';
+
+	$: token.set($pageToken);
+</script>
+
+<main class="mx-auto flex flex-col gap-y-6 px-5 pt-10 sm:w-sm">
+	<div class="absolute top-0"><OisyWalletLogoLink /></div>
+
+	<slot />
+</main>
