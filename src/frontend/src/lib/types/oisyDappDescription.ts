c034e32c81a157cb28387a42f02399ce3a7948ae
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-10-24 12:23:16 +0200

    feat(frontend): extend dApps description schema with data for carousel (#3093)
    # Motivation
    
    We need to extend dApps description schema with data required for
    Carousel slides. Some fields (e.g. logo) is already present in the JSON,
    therefore I added only the missing ones. The fields are optional in case
    in the future not all dApps need to be showcased in the carousel.
    
    <img width="312" alt="Screenshot 2024-10-24 at 11 08 54"
    src="https://github.com/user-attachments/assets/186119ed-d28e-4a05-959c-22b1b1a3301d">

--- src/frontend/src/lib/types/oisyDappDescription.ts
+++ src/frontend/src/lib/types/oisyDappDescription.ts
@@ -30,16 +30,24 @@ const dAppDescriptionSchema = z.object({
 	submittableId: z.string().optional()
 });
 
+const carouselDappDescriptionSchema = z.object({
+	// TODO: check if text is not too long using logic from https://github.com/dfinity/portal/blob/34a0328ed4792f5a7f3943be73f13f5abaefb4b8/plugins/validate-showcase.js#L179
+	text: z.string(),
+	callToAction: z.string()
+});
+
 const oisyDappDescriptionSchema = dAppDescriptionSchema.extend({
 	featured: z.boolean().optional(),
 	callToAction: z.string().optional(),
 	telegram: z.string().url().optional(),
-	openChat: z.string().url().optional()
+	openChat: z.string().url().optional(),
+	carousel: carouselDappDescriptionSchema.optional()
 });
 
 export type OisyDappDescription = z.infer<typeof oisyDappDescriptionSchema>;
 export type FeaturedOisyDappDescription = Omit<OisyDappDescription, 'screenshots'> &
 	Required<Pick<OisyDappDescription, 'screenshots'>>;
-
+export type CarouselSlideOisyDappDescription = Omit<OisyDappDescription, 'carousel'> &
+	Required<Pick<OisyDappDescription, 'carousel'>>;
 const parseResult = z.array(oisyDappDescriptionSchema).safeParse(dAppDescriptionsJson);
 export const dAppDescriptions: OisyDappDescription[] = parseResult.success ? parseResult.data : [];
