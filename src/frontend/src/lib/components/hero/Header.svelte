a8fcd1d34b56dc3a7845b38e285552cd5f510f93
Robert Schlittler <robert.schlittler@inacta.ch>
2024-10-24 09:12:58 +0200

    feat(frontend): remove login button from landing page

--- src/frontend/src/lib/components/hero/Header.svelte
+++ src/frontend/src/lib/components/hero/Header.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import WalletConnect from '$eth/components/wallet-connect/WalletConnect.svelte';
-	import SignIn from '$lib/components/auth/SignIn.svelte';
 	import Alpha from '$lib/components/core/Alpha.svelte';
 	import Menu from '$lib/components/core/Menu.svelte';
 	import OisyWalletLogoLink from '$lib/components/core/OisyWalletLogoLink.svelte';
@@ -44,7 +43,6 @@
 		{:else}
 			<AboutMenu />
 
-			<SignIn />
 		{/if}
 	</div>
 </header>
