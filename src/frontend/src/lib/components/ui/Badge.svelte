204943bfa87fcd145f47a8cd421c43c767493916
Robert Schlittler <robert.schlittler@inacta.ch>
2024-10-24 15:30:49 +0200

    feat(frontend): implement badge according to figma

---
+++ src/frontend/src/lib/components/ui/Badge.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+	export let variant: 'default' | 'info' | 'error' | 'warning' | 'success' | 'outline' = 'default';
+
+	const variantClassNames = {
+		default: 'border border-light-grey bg-dust/30',
+		info: 'bg-light-blue text-blue-ribbon',
+		error: 'bg-rusty-red/20 text-rusty-red',
+		warning: 'bg-american-orange/20 text-american-orange',
+		success: 'bg-green-crayola/20 text-green-crayola',
+		outline: 'border border-light-grey bg-off-white'
+	};
+
+</script>
+
+
+<div class="inline-flex px-2 py-0.5 gap-1 items-center rounded  text-xs font-semibold {variantClassNames[variant]}">
+	<slot />
+</div>
