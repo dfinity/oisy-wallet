0848d27bf89b5c1a930021e63d0db0ddc71467a8
Robert Schlittler <robert.schlittler@inacta.ch>
2024-10-24 11:57:08 +0200

    feat(frontend): rename test resources

--- e2e/about-why-oisy-modal.spec.ts
+++ e2e/about-why-oisy-modal.spec.ts
@@ -6,15 +6,15 @@ import {
 import { MODALS_VIEWPORT_WIDTH } from './utils/constants/e2e.constants';
 import { HomepageLoggedOut } from './utils/pages/homepage.page';
 
-const ABOUT_HOW_MODAL_VIEWPORT_HEIGHT = 1600;
+const ABOUT_WHY_OISY_MODAL_VIEWPORT_HEIGHT = 1600;
 
-test('should display about-how modal', async ({ page, isMobile }) => {
+test('should display about-why-oisy modal', async ({ page, isMobile }) => {
 	const homepageLoggedOut = new HomepageLoggedOut({
 		page,
 		viewportSize: !isMobile
 			? {
 					width: MODALS_VIEWPORT_WIDTH,
-					height: ABOUT_HOW_MODAL_VIEWPORT_HEIGHT
+					height: ABOUT_WHY_OISY_MODAL_VIEWPORT_HEIGHT
 				}
 			: undefined
 	});
