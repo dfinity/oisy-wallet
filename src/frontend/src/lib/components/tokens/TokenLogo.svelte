86a966ae8b4a977d55a9b94ac0fde9ac0f748967
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-24 08:48:26 +0200

    refactor(frontend): Rename subLogo to badge

--- src/frontend/src/lib/components/tokens/TokenLogo.svelte
+++ src/frontend/src/lib/components/tokens/TokenLogo.svelte
@@ -6,7 +6,7 @@
 
 	export let data: CardData;
 	export let color: 'dust' | 'off-white' | 'white' = 'dust';
-	export let subLogo:
+	export let badge:
 		| { type: 'network'; blackAndWhite?: boolean }
 		| { type: 'tokenCount'; count: number }
 		| undefined = undefined;
@@ -27,17 +27,17 @@
 		{color}
 		{ring}
 	/>
-	{#if subLogo?.type === 'tokenCount' && subLogo.count > 0}
+	{#if badge?.type === 'tokenCount' && badge.count > 0}
 		<span
 			class="absolute -right-2.5 bottom-0 flex h-6 w-6 items-center justify-center rounded-full border-[0.5px] border-light-grey bg-white text-sm font-semibold text-[var(--color-secondary)]"
 			aria-label={replacePlaceholders($i18n.tokens.alt.token_group_number, { $token: data.name })}
 		>
-			{subLogo.count}
+			{badge.count}
 		</span>
-	{:else if subLogo?.type === 'network'}
+	{:else if badge?.type === 'network'}
 		<div class="absolute -bottom-1 -right-1">
 			<Logo
-				src={subLogo.blackAndWhite ? networkIconBW : networkIcon}
+				src={badge.blackAndWhite ? networkIconBW : networkIcon}
 				alt={replacePlaceholders($i18n.core.alt.logo, { $name: networkName })}
 				{color}
 			/>
