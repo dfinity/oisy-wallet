73f9696d45c5344c0c9a64a40f612789ba863c7e
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-10-24 16:12:35 +0200

    feat(frontend): Create listener for multiple tokens (#3040)
    # Motivation
    
    The `TokenGroupCard` component created in PR #3054 is still lacking the
    listeners for the tokens.
    
    Basically, some tokens requires the `Listener` component to update the
    balance. We cannot put it in the single-token cards that are
    sub-elements of the group card, because they will not be mounted if the
    group is not expanded.
    
    So, to have the correct `Listener` objects, we will not wrap the cards
    with the `Listener`, but we wrap the entire Token Group card with a new
    component `MultipleListeners`.
    
    This component is simply a stack of single-token `Listeners`, but only
    the ones that are required.
    
    # Changes
    
    - Create util to map a Listener to a specific token: `BTC` -->
    `BtcListener`, for example.
    - Create component MultipleListener to stack the required `Listeners`
    given a list of tokens.
    - Wrap the token group card with the new `Listener` component.
    
    # Test
    
    Created some tests for the util function.

---
+++ src/frontend/src/tests/lib/utils/listener.utils.spec.ts
@@ -0,0 +1,41 @@
+import BitcoinListener from '$btc/components/core/BitcoinListener.svelte';
+import { BTC_MAINNET_TOKEN } from '$env/tokens.btc.env';
+import { ICP_TOKEN, SEPOLIA_TOKEN } from '$env/tokens.env';
+import EthListener from '$eth/components/core/EthListener.svelte';
+import type { OptionToken } from '$lib/types/token';
+import { mapListeners } from '$lib/utils/listener.utils';
+
+describe('mapListeners', () => {
+	it('should return an empty array if all tokens are nullish', () => {
+		const tokens: OptionToken[] = [null, undefined, null];
+
+		expect(mapListeners(tokens)).toEqual([]);
+	});
+
+	it('should map a Bitcoin token to BitcoinListener', () => {
+		const tokens: OptionToken[] = [null, undefined, BTC_MAINNET_TOKEN];
+
+		expect(mapListeners(tokens)).toEqual([{ token: BTC_MAINNET_TOKEN, listener: BitcoinListener }]);
+	});
+
+	it('should map an Ethereum token to EthListener', () => {
+		const tokens: OptionToken[] = [null, undefined, SEPOLIA_TOKEN];
+
+		expect(mapListeners(tokens)).toEqual([{ token: SEPOLIA_TOKEN, listener: EthListener }]);
+	});
+
+	it('should map other tokens with no listener', () => {
+		const tokens: OptionToken[] = [null, undefined, ICP_TOKEN];
+
+		expect(mapListeners(tokens)).toEqual([]);
+	});
+
+	it('should handle multiple tokens correctly', () => {
+		const tokens: OptionToken[] = [null, undefined, ICP_TOKEN, SEPOLIA_TOKEN, BTC_MAINNET_TOKEN];
+
+		expect(mapListeners(tokens)).toEqual([
+			{ token: SEPOLIA_TOKEN, listener: EthListener },
+			{ token: BTC_MAINNET_TOKEN, listener: BitcoinListener }
+		]);
+	});
+});
