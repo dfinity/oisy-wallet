70ddddec39b8bc351b635c44bdc443f254bdbd6a
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-24 15:46:07 +0200

    refactor: only required

--- src/frontend/src/lib/components/core/MultipleListeners.svelte
+++ src/frontend/src/lib/components/core/MultipleListeners.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
-	import Listener from '$lib/components/core/Listener.svelte';
-	import type { OptionToken } from '$lib/types/token.js';
+	import { authSignedIn } from '$lib/derived/auth.derived';
+	import type { OptionToken, Token } from '$lib/types/token';
+	import { mapListeners } from '$lib/utils/listener.utils';
+	import type { TokenToListener } from '$lib/types/listener';
 
 	export let tokens: OptionToken[];
+
+	let listeners: TokenToListener[];
+	$: listeners = $authSignedIn ? mapListeners(tokens) : [];
 </script>
 
-{#each tokens as token}
-	<Listener {token} />
+{#each listeners as { token, listener }}
+	<svelte:component this={listener} {token} />
 {/each}
 
 <slot />
