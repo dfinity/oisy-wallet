f7721f4362ff7055ee7fc684813bb6c6e11a5b23
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-10-25 10:39:48 +0200

    feat(frontend): DappsCarousel components (#3109)
    # Motivation
    
    This PR includes DappsCarousel components.
    
    
    
    https://github.com/user-attachments/assets/3a474efe-4a3b-45c7-895f-02591836f3ed

---
+++ src/frontend/src/lib/components/dapps/DappsCarousel.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { nonNullish } from '@dfinity/utils';
+	import Carousel from '$lib/components/carousel/Carousel.svelte';
+	import DappsCarouselSlide from '$lib/components/dapps/DappsCarouselSlide.svelte';
+	import {
+		dAppDescriptions,
+		type CarouselSlideOisyDappDescription
+	} from '$lib/types/oisyDappDescription';
+
+	export let styleClass: string | undefined = undefined;
+
+	const dappsCarouselSlides = dAppDescriptions.filter(({ carousel }) =>
+		nonNullish(carousel)
+	) as CarouselSlideOisyDappDescription[];
+</script>
+
+{#if nonNullish(dappsCarouselSlides)}
+	<Carousel {styleClass}>
+		{#each dappsCarouselSlides as dappsCarouselSlide}
+			<DappsCarouselSlide {dappsCarouselSlide} />
+		{/each}
+	</Carousel>
+{/if}
