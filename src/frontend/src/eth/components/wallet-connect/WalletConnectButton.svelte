0767acded8fc31fafdba8597dd83899e49bce991
Robert Schlittler <robert.schlittler@inacta.ch>
2024-10-25 16:12:51 +0200

    feat(frontend): refactor WalletConnectButton to handle empty default slots

--- src/frontend/src/eth/components/wallet-connect/WalletConnectButton.svelte
+++ src/frontend/src/eth/components/wallet-connect/WalletConnectButton.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
 	import IconWalletConnect from '$lib/components/icons/IconWalletConnect.svelte';
 	import { ethAddressNotLoaded } from '$lib/derived/address.derived';
+
+	export let ariaLabel: string | undefined = undefined;
 </script>
 
-<button on:click class="tertiary text-primary h-10" disabled={$ethAddressNotLoaded}>
+<button
+	aria-label={ariaLabel}
+	on:click
+	class="tertiary text-primary h-10"
+	class:icon={!$$slots.default}
+	disabled={$ethAddressNotLoaded}
+>
 	<IconWalletConnect size="24" />
 	<slot />
 </button>
