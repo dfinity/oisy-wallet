ca7a0d494eb4663ec847992b776e272d483d4f63
Robert Schlittler <43857623+loki344@users.noreply.github.com>
2024-10-25 09:11:28 +0200

    feat(frontend): add badge component (#3104)
    # Motivation
    
    Implement the reusable component 'Badge' from figma.
    
    
    ![image](https://github.com/user-attachments/assets/c07642a2-5cd8-4b0f-8ea4-d05d7663fa9f)
    
    # Changes
    
    - Implement Badge
    - Replace current concrete 'implementations' in DappTags with abstracted
    component
    
    
    # Tests
    
    Looks same same:
    
    new: 
    
    ![image](https://github.com/user-attachments/assets/2a9a9145-b327-4203-a0f6-36c6631258fc)
    
    ![image](https://github.com/user-attachments/assets/f954b736-ea77-4f3a-8695-46d1c3228104)

---
+++ src/frontend/src/lib/components/ui/Badge.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let styleClass: string | undefined = undefined;
+
+	const variantClassNames = {
+		default: 'border border-light-grey bg-dust/30',
+		info: 'bg-light-blue text-blue-ribbon',
+		error: 'bg-rusty-red/20 text-rusty-red',
+		warning: 'bg-american-orange/20 text-american-orange',
+		success: 'bg-green-crayola/20 text-green-crayola',
+		outline: 'border border-light-grey bg-off-white'
+	};
+
+	export let variant: keyof typeof variantClassNames = 'default';
+</script>
+
+<span
+	class="inline-flex items-center gap-1 rounded px-2 py-0.5 text-xs font-semibold {variantClassNames[
+		variant
+	]} {styleClass ?? ''}"
+>
+	<slot />
+</span>
