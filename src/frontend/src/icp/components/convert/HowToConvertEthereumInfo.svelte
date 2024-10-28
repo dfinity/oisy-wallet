693992fef9c06abefa05730950342b9e10f9a70d
David Dal Busco <david.dalbusco@dfinity.org>
2024-10-28 09:22:02 +0100

    feat(frontend): how to convert back btn full width (#3154)
    # Motivation
    
    The "Back" button on the "How to convert..." modal does no inherits the
    secondary style of back button and is not fullwidth.
    
    # Changes
    
    - Add `fullWidth` to `ButtonBack`
    - Correct slot, style and width for the back button on the modal
    
    # Screenshots
    
    Before:
    
    <img width="1532" alt="Capture d’écran 2024-10-28 à 09 13 05"
    src="https://github.com/user-attachments/assets/b7e46298-9252-4ada-8883-08bff2bcec53">
    
    After:
    
    <img width="1532" alt="Capture d’écran 2024-10-28 à 09 13 41"
    src="https://github.com/user-attachments/assets/2cb184b0-6c00-4f60-93fe-7758e7b98d1f">

--- src/frontend/src/icp/components/convert/HowToConvertEthereumInfo.svelte
+++ src/frontend/src/icp/components/convert/HowToConvertEthereumInfo.svelte
@@ -9,6 +9,7 @@
 	} from '$icp-eth/derived/cketh.derived';
 	import ReceiveAddress from '$lib/components/receive/ReceiveAddress.svelte';
 	import Button from '$lib/components/ui/Button.svelte';
+	import ButtonBack from '$lib/components/ui/ButtonBack.svelte';
 	import ButtonDone from '$lib/components/ui/ButtonDone.svelte';
 	import ContentWithToolbar from '$lib/components/ui/ContentWithToolbar.svelte';
 	import Value from '$lib/components/ui/Value.svelte';
@@ -151,13 +152,11 @@
 		</div>
 	</div>
 
-	<div slot="toolbar">
+	<svelte:fragment slot="toolbar">
 		{#if formCancelAction === 'back'}
-			<Button fullWidth type="button" on:click={() => dispatch('icBack')}
-				>{$i18n.core.text.back}</Button
-			>
+			<ButtonBack fullWidth on:click={() => dispatch('icBack')} />
 		{:else}
 			<ButtonDone on:click={modalStore.close} />
 		{/if}
-	</div>
+	</svelte:fragment>
 </ContentWithToolbar>
