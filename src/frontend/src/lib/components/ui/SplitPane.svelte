887dc49b20894259aee2986af83938fdebca6cae
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-10-28 16:50:03 +0100

    style(frontend): Reduce padding of Hero (#3177)
    # Motivation
    
    To make it less dispersive, we reduce the padding on top of Hero, for
    the different brakpoints.
    
    
    
    https://github.com/user-attachments/assets/5da80529-c68c-49f5-9c57-c8b57b53cc1e
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/ui/SplitPane.svelte
+++ src/frontend/src/lib/components/ui/SplitPane.svelte
@@ -1,6 +1,4 @@
-<main
-	class="relative mx-auto max-w-screen-2.5xl pt-8 md:flex md:w-full md:flex-row lg:w-auto lg:pt-0"
->
+<main class="relative mx-auto max-w-screen-2.5xl pt-6 md:flex md:w-full md:flex-row lg:w-auto">
 	<div
 		class="invisible absolute -translate-x-full transition-all duration-200 ease-in-out md:visible md:fixed md:inset-y-0 md:my-24 md:block md:min-w-36 md:translate-x-0 md:transition-none lg:min-w-44 xl:w-80 1.5xl:w-[22rem]"
 	>
