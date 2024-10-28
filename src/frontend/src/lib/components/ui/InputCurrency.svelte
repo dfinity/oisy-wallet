7a9fffb89289688822d28aef0716a456aa6fe9b9
David Dal Busco <david.dalbusco@dfinity.org>
2024-10-28 08:26:42 +0100

    feat(frontend): extract common input currency component (#3144)
    # Motivation
    
    Similarly to #3141, this PR extract a common input component for input
    of type currency.
    
    # Changes
    
    - Create component and consume it.

---
+++ src/frontend/src/lib/components/ui/InputCurrency.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { Input } from '@dfinity/gix-components';
+
+	export let value: number | undefined = undefined;
+	export let name: string;
+	export let placeholder: string;
+	export let required = true;
+	export let decimals: number | undefined = undefined;
+	export let testId: string | undefined = undefined;
+</script>
+
+<Input
+	{name}
+	inputType="currency"
+	{required}
+	bind:value
+	{decimals}
+	{placeholder}
+	spellcheck={false}
+	{testId}
+	on:nnsInput
+>
+	<slot name="inner-end" slot="inner-end" />
+</Input>
