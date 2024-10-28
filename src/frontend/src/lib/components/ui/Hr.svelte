d34bb52cafdeb6d91b6a00782e36a4f8b2ce4943
David Dal Busco <david.dalbusco@outlook.com>
2024-10-28 17:44:42 +0100

    feat(frontend): hr spacing

--- src/frontend/src/lib/components/ui/Hr.svelte
+++ src/frontend/src/lib/components/ui/Hr.svelte
@@ -1,1 +1,9 @@
-<hr class="bg-dark-blue opacity-10" style="width: 100%; border: 0.05rem solid" />
+<script lang="ts">
+	export let spacing: 'none' | 'lg' = 'none';
+</script>
+
+<hr
+	class="bg-dark-blue opacity-10"
+	class:my-6={spacing === 'lg'}
+	style="width: 100%; border: 0.05rem solid"
+/>
