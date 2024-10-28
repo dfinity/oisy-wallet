fd747b8a05a152a11bbe7b84dedda1c7da95fd91
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-10-28 14:32:55 +0100

    feat(frontend): remove BTC flag from the rest of the code (#3166)
    # Motivation
    
    Remove BTC flag from the docs, utils and GH workflows.
    
    Note: all related PRs need to be merged first, this goes as the last
    one.

--- src/frontend/src/env/networks.btc.env.ts
+++ src/frontend/src/env/networks.btc.env.ts
@@ -17,7 +17,3 @@ export const BITCOIN_CANISTER_IDS: Record<MinterCanisterIdText, CanisterIdText> 
 
 export const BTC_MAINNET_ENABLED =
 	JSON.parse(import.meta.env.VITE_BITCOIN_MAINNET_DISABLED ?? false) === false;
-
-// TODO: to be removed when we have finished implementing all the Bitcoin features
-export const NETWORK_BITCOIN_ENABLED =
-	JSON.parse(import.meta.env.VITE_NETWORK_BITCOIN_ENABLED ?? false) === true;
