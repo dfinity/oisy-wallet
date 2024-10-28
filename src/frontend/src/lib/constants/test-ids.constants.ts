7e3aad9416b2f781bb03194b62af36516ba9bd99
darioloetsc <76442731+darioloetsc@users.noreply.github.com>
2024-10-28 14:20:07 +0100

    feat(frontend): e2e test ids (#3146)
    # Motivation
    
    Current e2e tests are read only, this pr requests to add test-ids mostly
    on buttons and forms. Preparing for the integration of a e2e send test
    case.
    
    # Changes
    
    - Add multiple test ids
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/constants/test-ids.constants.ts
+++ src/frontend/src/lib/constants/test-ids.constants.ts
@@ -3,6 +3,7 @@ export const NAVIGATION_MENU = 'navigation-menu';
 
 export const LOGOUT_BUTTON = 'logout-button';
 export const LOGIN_BUTTON = 'login-button';
+export const MAX_BUTTON = 'max-button';
 
 export const LOADER_MODAL = 'loader-modal';
 
@@ -14,6 +15,7 @@ export const ABOUT_WHY_OISY_BUTTON = 'about-why-oisy-modal-open-button';
 export const ABOUT_WHY_OISY_MODAL = 'about-why-oisy-modal';
 
 export const RECEIVE_TOKENS_MODAL_OPEN_BUTTON = 'receive-tokens-modal-open-button';
+export const RECEIVE_TOKENS_MODAL_DONE_BUTTON = 'receive-tokens-modal-done-button';
 export const RECEIVE_TOKENS_MODAL = 'receive-tokens-modal';
 
 export const RECEIVE_TOKENS_MODAL_ICRC_SECTION = 'receive-tokens-modal-icrc-section';
@@ -23,3 +25,12 @@ export const RECEIVE_TOKENS_MODAL_ETH_SECTION = 'receive-tokens-modal-eth-sectio
 export const RECEIVE_TOKENS_MODAL_ADDRESS_LABEL = 'receive-tokens-modal-address-label';
 export const RECEIVE_TOKENS_MODAL_QR_CODE_BUTTON = 'receive-tokens-modal-qr-code-button';
 export const RECEIVE_TOKENS_MODAL_QR_CODE_OUTPUT = 'receive-tokens-modal-qr-code-output';
+export const RECEIVE_TOKENS_MODAL_COPY_ADDRESS_BUTTON = 'receive-tokens-modal-copy-address-button';
+export const RECEIVE_TOKENS_MODAL_COPY_ICP_ADDRESS_BUTTON =
+	'receive-tokens-modal-copy-icp-address-button';
+export const RECEIVE_TOKENS_MODAL_COPY_ICP_ACCOUNT_ID_BUTTON =
+	'receive-tokens-modal-copy-icp-account-id-button';
+
+export const SEND_TOKENS_MODAL_OPEN_BUTTON = 'send-tokens-modal-open-button';
+export const SEND_FORM_NEXT_BUTTON = 'send-form-next-button';
+export const REVIEW_FORM_SEND_BUTTON = 'review-form-send-button';
