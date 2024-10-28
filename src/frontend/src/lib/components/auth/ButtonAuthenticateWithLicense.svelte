4e0118ec79d8fa607af001b6711c0d7aa258c47a
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-28 19:03:58 +0100

    feat(frontend): Add License URL in menu

--- src/frontend/src/lib/components/auth/ButtonAuthenticateWithLicense.svelte
+++ src/frontend/src/lib/components/auth/ButtonAuthenticateWithLicense.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
+	import LicenseLink from '$lib/components/license-agreement/LicenseLink.svelte';
 	import ButtonAuthenticate from '$lib/components/ui/ButtonAuthenticate.svelte';
 	import { signIn } from '$lib/services/auth.services';
 	import { i18n } from '$lib/stores/i18n.store';
-	import { replaceOisyPlaceholders } from '$lib/utils/i18n.utils';
 
 	export let fullWidth = false;
 	export let licenseAlignment: 'inherit' | 'center' = 'inherit';
 </script>
 
-<div class="ali flex w-full flex-col">
+<div class="flex w-full flex-col">
 	<ButtonAuthenticate on:click={async () => await signIn({})} {fullWidth} />
 
 	<span
@@ -16,11 +16,6 @@
 	>
 		{$i18n.license_agreement.text.accept_terms}
 
-		<a
-			href="/license-agreement"
-			aria-label={replaceOisyPlaceholders($i18n.license_agreement.alt.license_agreement)}
-		>
-			{$i18n.license_agreement.text.accept_terms_link}
-		</a>
+		<LicenseLink />
 	</span>
 </div>
