13c62c3d0006c1d927b96a978dacde03d27a5abe
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-10-29 08:51:25 +0100

    feat(frontend): Add License URL in menu (#3184)
    # Motivation
    
    We include the License agreement link in the menu as per design. To do
    that, we extract the License Agreement URL in a separate link.
    
    ![Screenshot 2024-10-28 at 19 04
    03](https://github.com/user-attachments/assets/9177fd2b-0987-4805-afeb-2b69f41b6904)

---
+++ src/frontend/src/lib/components/license-agreement/LicenseLink.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { i18n } from '$lib/stores/i18n.store';
+	import { replaceOisyPlaceholders } from '$lib/utils/i18n.utils.js';
+
+	export let noUnderline = false;
+</script>
+
+<a
+	href="/license-agreement"
+	aria-label={replaceOisyPlaceholders($i18n.license_agreement.alt.license_agreement)}
+	class:no-underline={noUnderline}
+>
+	{$i18n.license_agreement.text.accept_terms_link}
+</a>
