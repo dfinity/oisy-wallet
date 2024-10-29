a4f8190a9877c820b343ae102dde1d2d830049c3
David Dal Busco <david.dalbusco@outlook.com>
2024-10-29 15:00:01 +0100

    feat: load address

--- src/frontend/src/lib/components/receive/ReceiveAddress.svelte
+++ src/frontend/src/lib/components/receive/ReceiveAddress.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { nonNullish } from '@dfinity/utils';
+	import { isNullish, nonNullish } from '@dfinity/utils';
 	import ReceiveActions from '$lib/components/receive/ReceiveActions.svelte';
 	import Logo from '$lib/components/ui/Logo.svelte';
 	import Value from '$lib/components/ui/Value.svelte';
@@ -7,9 +7,12 @@
 	import type { Network } from '$lib/types/network';
 	import type { ReceiveQRCodeAction } from '$lib/types/receive';
 	import { replacePlaceholders } from '$lib/utils/i18n.utils';
+	import type { OptionString } from '$lib/types/string';
+	import SkeletonText from '$lib/components/ui/SkeletonText.svelte';
+	import { fade } from 'svelte/transition';
 
 	export let labelRef: string;
-	export let address: string;
+	export let address: OptionString;
 	export let network: Network;
 	export let qrCodeAction: ReceiveQRCodeAction;
 	export let copyAriaLabel: string;
@@ -44,9 +47,21 @@
 				/>
 			</div>
 
-			<output id="ic-wallet-address" class="break-all text-sm" data-tid={testId}>{address}</output>
+			{#if isNullish(address)}
+				<span class="w-full"><SkeletonText /></span>
+			{:else}
+				<output id="ic-wallet-address" class="break-all text-sm" data-tid={testId} in:fade
+					>{address}</output
+				>
+			{/if}
 
-			<ReceiveActions on:click {address} {copyAriaLabel} {qrCodeAction} {copyButtonTestId} />
+			{#if nonNullish(address)}
+				<div in:fade>
+					<ReceiveActions on:click {address} {copyAriaLabel} {qrCodeAction} {copyButtonTestId} />
+				</div>
+			{:else}
+				<div class="min-w-20">&ZeroWidthSpace;</div>
+			{/if}
 		</div>
 
 		<slot />
