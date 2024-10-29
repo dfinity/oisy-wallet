0f2c4a8b553fbc0f436f85ca5d055fa623af1769
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-10-29 11:09:46 +0100

    fix(e2e): Missing placholder in BTC addresses (#3190)
    # Motivation
    
    The E2E tests are failing because the BTC addresses are being changed
    every run (this is consistent with the local replica). What is missing
    is the placeholder for the addresses like we do for ETH and IC. So we
    re-use the same method.
    
    # Changes
    
    - Create util to find multiple mocking selectors.
    - Include test ID for each Bitcoin address.
    - Include placholders for Bitcoin addresses.
    
    # Tests
    
    The E2E CI will run in this PR.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- e2e/receive-tokens-modal.spec.ts
+++ e2e/receive-tokens-modal.spec.ts
@@ -1,5 +1,8 @@
 import {
 	RECEIVE_TOKENS_MODAL,
+	RECEIVE_TOKENS_MODAL_BTC_MAINNET_SECTION,
+	RECEIVE_TOKENS_MODAL_BTC_REGTEST_SECTION,
+	RECEIVE_TOKENS_MODAL_BTC_TESTNET_SECTION,
 	RECEIVE_TOKENS_MODAL_ETH_SECTION,
 	RECEIVE_TOKENS_MODAL_ICP_SECTION,
 	RECEIVE_TOKENS_MODAL_ICRC_SECTION,
@@ -8,7 +11,7 @@ import {
 import { testWithII } from '@dfinity/internet-identity-playwright';
 import { MODALS_VIEWPORT_WIDTH } from './utils/constants/e2e.constants';
 import { HomepageLoggedIn } from './utils/pages/homepage.page';
-import { getReceiveTokensModalAddressLabelSelector } from './utils/selectors.utils';
+import { getReceiveTokensModalAddressLabelSelectors } from './utils/selectors.utils';
 
 const RECEIVE_TOKENS_MODAL_VIEWPORT_HEIGHT = 900;
 let homepageLoggedIn: HomepageLoggedIn;
@@ -32,17 +35,14 @@ testWithII('should display receive-tokens modal', async () => {
 	await homepageLoggedIn.testModalSnapshot({
 		modalOpenButtonTestId: RECEIVE_TOKENS_MODAL_OPEN_BUTTON,
 		modalTestId: RECEIVE_TOKENS_MODAL,
-		selectorsToMock: [
-			getReceiveTokensModalAddressLabelSelector({
-				sectionSelector: RECEIVE_TOKENS_MODAL_ICRC_SECTION
-			}),
-			getReceiveTokensModalAddressLabelSelector({
-				sectionSelector: RECEIVE_TOKENS_MODAL_ICP_SECTION
-			}),
-			getReceiveTokensModalAddressLabelSelector({
-				sectionSelector: RECEIVE_TOKENS_MODAL_ETH_SECTION
-			})
-		]
+		selectorsToMock: getReceiveTokensModalAddressLabelSelectors([
+			RECEIVE_TOKENS_MODAL_ICRC_SECTION,
+			RECEIVE_TOKENS_MODAL_ICP_SECTION,
+			RECEIVE_TOKENS_MODAL_ETH_SECTION,
+			RECEIVE_TOKENS_MODAL_BTC_MAINNET_SECTION,
+			RECEIVE_TOKENS_MODAL_BTC_TESTNET_SECTION,
+			RECEIVE_TOKENS_MODAL_BTC_REGTEST_SECTION
+		])
 	});
 });
 
