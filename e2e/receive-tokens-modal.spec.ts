6bbabe0f3b8e9b970adcc36f3b328bb1b0ea3399
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-29 09:58:38 +0100

    fix: missing parenthesis

--- e2e/receive-tokens-modal.spec.ts
+++ e2e/receive-tokens-modal.spec.ts
@@ -35,15 +35,16 @@ testWithII('should display receive-tokens modal', async () => {
 	await homepageLoggedIn.testModalSnapshot({
 		modalOpenButtonTestId: RECEIVE_TOKENS_MODAL_OPEN_BUTTON,
 		modalTestId: RECEIVE_TOKENS_MODAL,
-		selectorsToMock: getReceiveTokensModalAddressLabelSelectors( [
-				RECEIVE_TOKENS_MODAL_ICRC_SECTION,
-				RECEIVE_TOKENS_MODAL_ICP_SECTION,
-				RECEIVE_TOKENS_MODAL_ETH_SECTION,
-				RECEIVE_TOKENS_MODAL_BTC_MAINNET_SECTION,
-				RECEIVE_TOKENS_MODAL_BTC_TESTNET_SECTION,
-				RECEIVE_TOKENS_MODAL_BTC_REGTEST_SECTION
-			])
+		selectorsToMock: getReceiveTokensModalAddressLabelSelectors([
+			RECEIVE_TOKENS_MODAL_ICRC_SECTION,
+			RECEIVE_TOKENS_MODAL_ICP_SECTION,
+			RECEIVE_TOKENS_MODAL_ETH_SECTION,
+			RECEIVE_TOKENS_MODAL_BTC_MAINNET_SECTION,
+			RECEIVE_TOKENS_MODAL_BTC_TESTNET_SECTION,
+			RECEIVE_TOKENS_MODAL_BTC_REGTEST_SECTION
+		])
 	});
+});
 
 testWithII('should display correct QR codes for receiving ICP tokens', async () => {
 	await homepageLoggedIn.testReceiveModalQrCode({
