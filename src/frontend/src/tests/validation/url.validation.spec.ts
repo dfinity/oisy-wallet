5fc1a24042cef44830303c0dd1f05acfb5869d19
David Dal Busco <david.dalbusco@dfinity.org>
2024-10-30 10:59:34 +0100

    feat(frontend): dapp description https url validation (#3231)
    # Motivation
    
    URL for the dapps must be on httpS.
    
    # Changes
    
    - Extend Zod validation for url with https

---
+++ src/frontend/src/tests/validation/url.validation.spec.ts
@@ -0,0 +1,58 @@
+import { UrlSchema } from '$lib/validation/url.validation';
+
+describe('UrlSchema', () => {
+	it('should accept valid https URLs', () => {
+		const validUrl = 'https://example.com';
+		expect(() => UrlSchema.parse(validUrl)).not.toThrow();
+	});
+
+	it('should accept valid https URLs with port', () => {
+		const validUrl = 'https://example.com:666';
+		expect(() => UrlSchema.parse(validUrl)).not.toThrow();
+	});
+
+	it('should accept valid https URLs with sub domain', () => {
+		const validUrl = 'https://staging.oisy.com';
+		expect(() => UrlSchema.parse(validUrl)).not.toThrow();
+	});
+
+	it('should accept valid https URLs with route', () => {
+		const validUrl = 'https://staging.oisy.com/sign';
+		expect(() => UrlSchema.parse(validUrl)).not.toThrow();
+	});
+
+	it('should reject localhost with http', () => {
+		const invalidUrl = 'http://localhost:3000';
+		expect(() => UrlSchema.parse(invalidUrl)).toThrow('Invalid URL.');
+	});
+
+	it('should reject 127.0.0.1 with http', () => {
+		const invalidUrl = 'http://127.0.0.1:3000';
+		expect(() => UrlSchema.parse(invalidUrl)).toThrow('Invalid URL.');
+	});
+
+	it('should reject localhost with an invalid protocol', () => {
+		const invalidUrl = 'ftp://localhost:3000';
+		expect(() => UrlSchema.parse(invalidUrl)).toThrow('Invalid URL.');
+	});
+
+	it('should reject non-localhost with an invalid protocol', () => {
+		const invalidUrl = 'ftp://example.com:3000';
+		expect(() => UrlSchema.parse(invalidUrl)).toThrow('Invalid URL.');
+	});
+
+	it('should reject non-localhost URLs without https', () => {
+		const invalidUrl = 'http://example.com';
+		expect(() => UrlSchema.parse(invalidUrl)).toThrow('Invalid URL.');
+	});
+
+	it('should reject an invalid URL', () => {
+		const invalidUrl = 'invalid-url';
+		expect(() => UrlSchema.parse(invalidUrl)).toThrow('Invalid URL.');
+	});
+
+	it('should reject an empty string', () => {
+		const invalidUrl = '';
+		expect(() => UrlSchema.parse(invalidUrl)).toThrow('Invalid URL.');
+	});
+});
