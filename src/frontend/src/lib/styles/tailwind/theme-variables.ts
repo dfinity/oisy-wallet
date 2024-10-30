bb9e5ab823593973c7b7ecfcdfc4545e0066b065
Robert Schlittler <robert.schlittler@inacta.ch>
2024-10-30 15:53:47 +0100

    style(frontend): implement basic theming

---
+++ src/frontend/src/lib/styles/tailwind/theme-variables.ts
@@ -0,0 +1,17 @@
+export const themeVariables = {
+	background: {
+		primary: 'var(--color-background-primary)',
+		secondary: 'var(--color-background-secondary)',
+		tertiary: 'var(--color-background-tertiary)',
+	},
+	border: {
+		primary: 'var(--color-border-primary)',
+		secondary: 'var(--color-border-secondary)',
+		tertiary: 'var(--color-border-tertiary)',
+	},
+	foreground: {
+		primary: 'var(--color-foreground-primary)',
+		secondary: 'var(--color-foreground-secondary)',
+		tertiary: 'var(--color-foreground-tertiary)',
+	}
+}
