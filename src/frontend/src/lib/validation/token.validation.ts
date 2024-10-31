ec3caa43185a0853a2f2d10a9169ce856cb42d50
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-31 12:00:14 +0100

    add token schema

--- src/frontend/src/lib/validation/token.validation.ts
+++ src/frontend/src/lib/validation/token.validation.ts
@@ -1,4 +1,6 @@
-import { TokenIdSchema, type TokenId } from '$lib/types/token';
+import { TokenIdSchema, type Token, type TokenId } from '$lib/types/token';
 
 export const parseTokenId = (tokenIdString: string): TokenId =>
 	TokenIdSchema.parse(Symbol(tokenIdString));
+
+export const safeParseToken = (token: object): Token => TokenSchema.safeParse(token);
