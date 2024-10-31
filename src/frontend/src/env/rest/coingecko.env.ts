232e45a80411f5755299ba3cf415732ac7f7bae1
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-01 00:04:50 +0100

    chore: remove deprecation

--- src/frontend/src/env/rest/coingecko.env.ts
+++ src/frontend/src/env/rest/coingecko.env.ts
@@ -1,3 +1,5 @@
-export const COINGECKO_API_URL = import.meta.env.VITE_COINGECKO_API_URL;
+import { UrlSchema } from '$lib/validation/url.validation';
+
+export const COINGECKO_API_URL = UrlSchema.parse('https://pro-api.coingecko.com/api/v3/');
 
 export const COINGECKO_API_KEY = import.meta.env.VITE_COINGECKO_API_KEY;
