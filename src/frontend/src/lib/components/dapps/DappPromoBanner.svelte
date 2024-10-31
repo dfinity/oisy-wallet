a74285115aeacbaa267b0877ad2cbbaf3eed9118
Robert Schlittler <robert.schlittler@inacta.ch>
2024-10-31 09:31:44 +0100

    feat(frontend): use new ImgBanner

--- src/frontend/src/lib/components/dapps/DappPromoBanner.svelte
+++ src/frontend/src/lib/components/dapps/DappPromoBanner.svelte
@@ -9,34 +9,30 @@
 	export let dAppDescription: FeaturedOisyDappDescription;
 </script>
 
-<article class="relative flex h-64 items-end overflow-hidden rounded-2xl">
+<article class="relative flex items-end overflow-hidden rounded-2xl">
 	{#if dAppDescription.screenshots.length > 0}
-		<div class="absolute inset-0 bg-onahau">
-			<ImgBanner
-				fitHeight={true}
-				height="100%"
-				width="100%"
-				src={dAppDescription.screenshots[0]}
-				alt={replacePlaceholders($i18n.dapps.alt.website, { $dAppName: dAppDescription.name })}
-			/>
+		<ImgBanner
+			styleClass="bg-onahau h-64 max-h-64"
+			src={dAppDescription.screenshots[0]}
+			alt={replacePlaceholders($i18n.dapps.alt.website, { $dAppName: dAppDescription.name })}
+		/>
+		<div class="absolute start-0 w-full flex-1 bg-black/30 px-4 py-4 backdrop-blur-sm">
+			<div class="flex items-center gap-x-2">
+				<div class="h-12 w-12 rounded-full">
+					<Img
+						src={dAppDescription.logo}
+						alt={replacePlaceholders($i18n.dapps.alt.logo, { $dAppName: dAppDescription.name })}
+					/>
+				</div>
+				<div class="flex-1">
+					<h6 class="text-sm font-bold text-info-blue">{$i18n.dapps.text.featured}</h6>
+					<h4 class="text-white">{dAppDescription.name}</h4>
+				</div>
+
+				<Button paddingSmall styleClass="flex-grow-0 text-sm" colorStyle="secondary" on:click>
+					{$i18n.core.text.view}
+				</Button>
+			</div>
 		</div>
 	{/if}
-	<div class="z-10 flex-1 bg-black/30 px-4 py-4 backdrop-blur-sm">
-		<div class="flex items-center gap-x-2">
-			<div class="h-12 w-12 rounded-full">
-				<Img
-					src={dAppDescription.logo}
-					alt={replacePlaceholders($i18n.dapps.alt.logo, { $dAppName: dAppDescription.name })}
-				/>
-			</div>
-			<div class="flex-1">
-				<h6 class="text-sm font-bold text-info-blue">{$i18n.dapps.text.featured}</h6>
-				<h4 class="text-white">{dAppDescription.name}</h4>
-			</div>
-
-			<Button paddingSmall styleClass="flex-grow-0 text-sm" colorStyle="secondary" on:click>
-				{$i18n.core.text.view}
-			</Button>
-		</div>
-	</div>
 </article>
