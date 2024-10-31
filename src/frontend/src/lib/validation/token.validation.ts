55d89e32c2b0016445b08366cef373d05cc1661b
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-10-31 17:51:38 +0100

    fix: validation

--- src/frontend/src/lib/validation/token.validation.ts
+++ src/frontend/src/lib/validation/token.validation.ts
@@ -1,6 +1,8 @@
 import { TokenIdSchema, TokenSchema, type Token, type TokenId } from '$lib/types/token';
+import { type SafeParseReturnType } from 'zod';
 
 export const parseTokenId = (tokenIdString: string): TokenId =>
 	TokenIdSchema.parse(Symbol(tokenIdString));
 
-export const safeParseToken = (token: object): Token => TokenSchema.safeParse(token);
+export const safeParseToken = (token: object): SafeParseReturnType<object, Token> =>
+	TokenSchema.safeParse(token);
