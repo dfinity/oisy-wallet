56547d006207ae52bb15ab281cc36a9f6a2c7476
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-10-31 10:17:16 +0100

    refactor(frontend): Include Zod in Network types (#3169)
    # Motivation
    
    We adding Zod to other `Network` sub-types, to ultimately arrive to the
    `Token` type.

--- src/frontend/src/lib/types/network.ts
+++ src/frontend/src/lib/types/network.ts
@@ -1,12 +1,24 @@
 import type { OnramperNetworkId } from '$lib/types/onramper';
 import type { AtLeastOne } from '$lib/types/utils';
+import { UrlSchema } from '$lib/validation/url.validation';
 import { z } from 'zod';
 
 export const NetworkIdSchema = z.symbol().brand<'NetworkId'>();
 
+const NetworkEnvironmentSchema = z.enum(['mainnet', 'testnet']);
+
+// TODO: use Zod to validate the OnramperNetworkId
+const NetworkBuySchema = z.object({
+	onramperId: z.custom<OnramperNetworkId>().optional()
+});
+
+const NetworkAppMetadataSchema = z.object({
+	explorerUrl: UrlSchema
+});
+
 export type NetworkId = z.infer<typeof NetworkIdSchema>;
 
-export type NetworkEnvironment = 'mainnet' | 'testnet';
+export type NetworkEnvironment = z.infer<typeof NetworkEnvironmentSchema>;
 
 export interface Network {
 	id: NetworkId;
@@ -17,10 +29,6 @@ export interface Network {
 	buy?: AtLeastOne<NetworkBuy>;
 }
 
-export interface NetworkBuy {
-	onramperId?: OnramperNetworkId;
-}
+export type NetworkBuy = z.infer<typeof NetworkBuySchema>;
 
-export interface NetworkAppMetadata {
-	explorerUrl: string;
-}
+export type NetworkAppMetadata = z.infer<typeof NetworkAppMetadataSchema>;
