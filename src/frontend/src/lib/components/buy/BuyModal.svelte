7759124a2df253ecf6bddd06f3a0eff504db1ac5
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-01 14:56:26 +0100

    set better padding

--- src/frontend/src/lib/components/buy/BuyModal.svelte
+++ src/frontend/src/lib/components/buy/BuyModal.svelte
@@ -11,9 +11,15 @@
 		>{isOnRamperDev ? $i18n.buy.text.buy_dev : $i18n.buy.text.buy}</svelte:fragment
 	>
 
-	<div class="stretch flex overflow-hidden" style="padding-bottom: var(--padding-3x);">
+	<div class="stretch flex overflow-hidden">
 		<div class="w-full overflow-auto">
 			<OnramperWidget />
 		</div>
 	</div>
 </Modal>
+
+<style lang="scss">
+	.stretch {
+		--stretch-padding-bottom: var(--padding-3x);
+	}
+</style>
