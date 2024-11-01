e8f40c84c18367c08e029b52433207905839912b
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-01 22:14:50 +0100

    use safeParse

--- src/frontend/src/env/rest/blockstream.env.ts
+++ src/frontend/src/env/rest/blockstream.env.ts
@@ -1,3 +1,7 @@
 import { UrlSchema } from '$lib/validation/url.validation';
+import { safeParse } from '$lib/validation/utils.validation';
 
-export const BLOCKSTREAM_API_URL = UrlSchema.parse('https://blockstream.info/api');
+export const BLOCKSTREAM_API_URL = safeParse({
+	schema: UrlSchema,
+	value: 'https://blockstream.info/api'
+});
