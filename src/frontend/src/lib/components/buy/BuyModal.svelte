d33283eaf4254d55697b8d2eb24933a3f84ee8e0
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-11-01 16:01:10 +0100

    fix(frontend): Onramper widget is scrollable now (#3273)
    # Motivation
    
    The Onramper widget was not scrollable for smaller screens, impeding the
    user to click the Buy button.
    
    
    
    https://github.com/user-attachments/assets/ef40b4c9-cae5-4305-9dbb-239bf824c72c

--- src/frontend/src/lib/components/buy/BuyModal.svelte
+++ src/frontend/src/lib/components/buy/BuyModal.svelte
@@ -11,7 +11,15 @@
 		>{isOnRamperDev ? $i18n.buy.text.buy_dev : $i18n.buy.text.buy}</svelte:fragment
 	>
 
-	<div class="stretch overflow-hidden">
-		<OnramperWidget />
+	<div class="stretch flex overflow-hidden">
+		<div class="w-full overflow-auto">
+			<OnramperWidget />
+		</div>
 	</div>
 </Modal>
+
+<style lang="scss">
+	.stretch {
+		--stretch-padding-bottom: var(--padding-3x);
+	}
+</style>
