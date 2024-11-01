48264bc26d4ff56c545a38ce10f0d4f0a6fa0f3f
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-01 16:46:28 +0100

    re-fancy

--- src/frontend/src/env/rest/coingecko.env.ts
+++ src/frontend/src/env/rest/coingecko.env.ts
@@ -1,9 +1,7 @@
 import { UrlSchema } from '$lib/validation/url.validation';
 
-const parsedCoingeckoApiUrl = UrlSchema.safeParse('https://pro-api.coingecko.com/api/v3/');
+const { success, data } = UrlSchema.safeParse('https://pro-api.coingecko.com/api/v3/');
 
-export const COINGECKO_API_URL = parsedCoingeckoApiUrl.success
-	? parsedCoingeckoApiUrl.data
-	: undefined;
+export const COINGECKO_API_URL = success ? data : undefined;
 
 export const COINGECKO_API_KEY = import.meta.env.VITE_COINGECKO_API_KEY;
