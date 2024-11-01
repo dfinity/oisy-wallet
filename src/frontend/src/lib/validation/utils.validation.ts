85c1f569c4affaad1e677da442168db17759897c
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-01 17:30:18 +0100

    use safeParse more

--- src/frontend/src/lib/validation/utils.validation.ts
+++ src/frontend/src/lib/validation/utils.validation.ts
@@ -1,15 +1,15 @@
 import { ZodType, type ZodTypeDef } from 'zod';
 
-export const safeParse = <T>({
+export const safeParse = <T, Fallback extends T | undefined = undefined>({
 	schema,
 	value,
 	fallback = undefined
 }: {
 	schema: ZodType<T, ZodTypeDef, unknown>;
 	value: unknown;
-	fallback?: T | undefined;
-}): T | undefined => {
+	fallback?: Fallback;
+}): Fallback extends undefined ? T | undefined : T => {
 	const { success, data } = schema.safeParse(value);
 
-	return success ? data : fallback;
+	return success ? data : (fallback as Fallback extends undefined ? T | undefined : T);
 };
