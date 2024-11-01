9bb1195e9186a06cc55cc3c3cce6419863b77d87
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-01 22:13:28 +0100

    use safeParse

--- src/frontend/src/env/rest/blockchain.env.ts
+++ src/frontend/src/env/rest/blockchain.env.ts
@@ -1,3 +1,7 @@
 import { UrlSchema } from '$lib/validation/url.validation';
+import { safeParse } from '$lib/validation/utils.validation';
 
-export const BLOCKCHAIN_API_URL = UrlSchema.parse('https://blockchain.info');
+export const BLOCKCHAIN_API_URL = safeParse({
+	schema: UrlSchema,
+	value: 'https://blockchain.info'
+});
