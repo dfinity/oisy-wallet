7c05bed708736790162b25c87ed20176785882e2
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-01 13:51:31 +0100

    feat: use SafeParse

--- src/frontend/src/env/rest/coingecko.env.ts
+++ src/frontend/src/env/rest/coingecko.env.ts
@@ -1,5 +1,7 @@
 import { UrlSchema } from '$lib/validation/url.validation';
 
-export const COINGECKO_API_URL = UrlSchema.parse('https://pro-api.coingecko.com/api/v3/');
+const parsedCoingeckoApiUrl = UrlSchema.safeParse('https://pro-api.coingecko.com/api/v3/');
+
+export const COINGECKO_API_URL = parsedCoingeckoApiUrl.success ? parsedCoingeckoApiUrl.data : '';
 
 export const COINGECKO_API_KEY = import.meta.env.VITE_COINGECKO_API_KEY;
