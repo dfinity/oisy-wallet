80dfa3bb2f9d7c6ddd0a64fcfd7d357c1f5061e9
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-01 15:01:31 +0100

    fix: fallback

--- src/frontend/src/env/rest/coingecko.env.ts
+++ src/frontend/src/env/rest/coingecko.env.ts
@@ -2,6 +2,8 @@ import { UrlSchema } from '$lib/validation/url.validation';
 
 const parsedCoingeckoApiUrl = UrlSchema.safeParse('https://pro-api.coingecko.com/api/v3/');
 
-export const COINGECKO_API_URL = parsedCoingeckoApiUrl.success ? parsedCoingeckoApiUrl.data : '';
+export const COINGECKO_API_URL = parsedCoingeckoApiUrl.success
+	? parsedCoingeckoApiUrl.data
+	: undefined;
 
 export const COINGECKO_API_KEY = import.meta.env.VITE_COINGECKO_API_KEY;
