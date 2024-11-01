aa8d19837e051b86c2b5048874e36be0c6215d56
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-11-01 22:08:43 +0100

    feat(frontend): util to safe parse schemas (#3277)
    # Motivation
    
    Small util to have an out-of-the-box `safeParse` function.
    
    UNRELATED: the `validation` folder for tests was outside `lib`, so we
    moved it.

--- src/frontend/src/env/rest/coingecko.env.ts
+++ src/frontend/src/env/rest/coingecko.env.ts
@@ -1,7 +1,9 @@
 import { UrlSchema } from '$lib/validation/url.validation';
+import { safeParse } from '$lib/validation/utils.validation';
 
-const { success, data } = UrlSchema.safeParse('https://pro-api.coingecko.com/api/v3/');
-
-export const COINGECKO_API_URL = success ? data : undefined;
+export const COINGECKO_API_URL = safeParse({
+	schema: UrlSchema,
+	value: 'https://pro-api.coingecko.com/api/v3/'
+});
 
 export const COINGECKO_API_KEY = import.meta.env.VITE_COINGECKO_API_KEY;
--- src/frontend/src/lib/constants/oisy.constants.ts
+++ src/frontend/src/lib/constants/oisy.constants.ts
@@ -1,7 +1,6 @@
 import metadata from '$env/oisy.metadata.json';
 import { oisyMetadata } from '$env/types/env-oisy-metadata';
-
-const parsedMetadata = oisyMetadata.safeParse(metadata);
+import { safeParse } from '$lib/validation/utils.validation';
 
 export const {
 	OISY_SHORT,
@@ -11,17 +10,19 @@ export const {
 	OISY_REPO_URL,
 	OISY_STATUS_URL,
 	OISY_TWITTER_URL
-} = parsedMetadata.success
-	? parsedMetadata.data
-	: {
-			OISY_SHORT: '',
-			OISY_NAME: '',
-			OISY_ONELINER: '',
-			OISY_DESCRIPTION: '',
-			OISY_REPO_URL: '',
-			OISY_STATUS_URL: '',
-			OISY_TWITTER_URL: ''
-		};
+} = safeParse({
+	schema: oisyMetadata,
+	value: metadata,
+	fallback: {
+		OISY_SHORT: '',
+		OISY_NAME: '',
+		OISY_ONELINER: '',
+		OISY_DESCRIPTION: '',
+		OISY_REPO_URL: '',
+		OISY_STATUS_URL: '',
+		OISY_TWITTER_URL: ''
+	}
+});
 
 export const OISY_URL = import.meta.env.VITE_OISY_URL;
 export const OISY_ICON = `${OISY_URL}/favicons/icon-512x512.png`;
