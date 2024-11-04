1b277e073ac47e1f4bd73dfaecf04d28f166447f
David Dal Busco <david.dalbusco@outlook.com>
2024-11-04 17:37:18 +0100

    test(frontend): icTransactions

--- src/frontend/src/tests/icp/derived/ic-transactions.derived.spec.ts
+++ src/frontend/src/tests/icp/derived/ic-transactions.derived.spec.ts
@@ -1,4 +1,14 @@
+import {
+	IC_CKETH_INDEX_CANISTER_ID,
+	IC_CKETH_LEDGER_CANISTER_ID,
+	IC_CKETH_MINTER_CANISTER_ID
+} from '$env/networks.icrc.env';
 import { icTransactions } from '$icp/derived/ic-transactions.derived';
+import { icPendingTransactionsStore } from '$icp/stores/ic-pending-transactions.store';
+import type { IcCkToken, IcTransactionUi } from '$icp/types/ic';
+import { token } from '$lib/stores/token.store';
+import type { TokenId } from '$lib/types/token';
+import { parseTokenId } from '$lib/validation/token.validation';
 import { mockCkBtcPendingUtxoTransaction } from '$tests/mocks/ckbtc.mock';
 import { setupCkBTCStores } from '$tests/utils/ckbtc-stores.test-utils';
 import { get } from 'svelte/store';
@@ -9,10 +19,8 @@ describe('ic-transactions.derived', () => {
 		expect(result).toEqual([]);
 	});
 
-	describe('with data', () => {
-		beforeEach(() => {
-			setupCkBTCStores();
-		});
+	describe('with ckBtc pending data', () => {
+		beforeEach(setupCkBTCStores);
 
 		it('should derive ic transactions', () => {
 			const result = get(icTransactions);
@@ -20,6 +28,54 @@ describe('ic-transactions.derived', () => {
 				{
 					data: mockCkBtcPendingUtxoTransaction,
 					certified: false
+				}
+			]);
+		});
+	});
+
+	describe('with ckEth pending data', () => {
+		const mockPendingCkEth: IcTransactionUi = {
+			id: 'tx1',
+			value: 100n,
+			type: 'send',
+			status: 'pending'
+		};
+
+		beforeEach(() => {
+			const tokenId: TokenId = parseTokenId('ckTest');
+
+			const mockToken: IcCkToken = {
+				id: tokenId,
+				standard: 'icrc',
+				ledgerCanisterId: IC_CKETH_LEDGER_CANISTER_ID,
+				indexCanisterId: IC_CKETH_INDEX_CANISTER_ID,
+				minterCanisterId: IC_CKETH_MINTER_CANISTER_ID
+			} as unknown as IcCkToken;
+
+			token.set(mockToken);
+
+			icPendingTransactionsStore.set({
+				tokenId: mockToken.id,
+				data: [
+					{
+						data: mockPendingCkEth,
+						certified: true
+					}
+				]
+			});
+		});
+
+		it('should derive ic transactions', () => {
+			const result = get(icTransactions);
+			expect(result).toEqual([
+				{
+					data: {
+						id: `${mockPendingCkEth.id}`,
+						status: mockPendingCkEth.status,
+						type: mockPendingCkEth.type,
+						value: mockPendingCkEth.value
+					},
+					certified: true
 				}
 			]);
 		});
