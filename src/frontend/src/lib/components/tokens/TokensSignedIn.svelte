67cd197f8c6e4381b4628babdbaf6eb17b6d2694
Robert Schlittler <robert.schlittler@inacta.ch>
2024-11-04 13:49:15 +0100

    feat(frontend): rm import

--- src/frontend/src/lib/components/tokens/TokensSignedIn.svelte
+++ src/frontend/src/lib/components/tokens/TokensSignedIn.svelte
@@ -12,7 +12,6 @@
 	import TokensDisplayHandler from '$lib/components/tokens/TokensDisplayHandler.svelte';
 	import TokensSkeletons from '$lib/components/tokens/TokensSkeletons.svelte';
 	import { modalManageTokens } from '$lib/derived/modal.derived';
-	import { i18n } from '$lib/stores/i18n.store';
 	import type { TokenUiOrGroupUi } from '$lib/types/token-group';
 	import { isTokenUiGroup } from '$lib/utils/token-group.utils';
 
