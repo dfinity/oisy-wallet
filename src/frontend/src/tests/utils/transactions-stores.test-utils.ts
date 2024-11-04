cffd4898560aaef637515d5d1aaebfbc44301718
David Dal Busco <david.dalbusco@dfinity.org>
2024-11-04 18:46:44 +0100

    test(frontend): icTransactions store (#3313)
    # Motivation
    
    Some bare minimum tests for `icTransactions`.
    
    # Changes
    
    - Refactor some existing mocks and utils for test to avoid duplicating
    code.
    - Create bare minimum tests.

---
+++ src/frontend/src/tests/utils/transactions-stores.test-utils.ts
@@ -0,0 +1,18 @@
+import type { IcTransactionUi } from '$icp/types/ic';
+import { nowInBigIntNanoSeconds } from '$icp/utils/date.utils';
+import type { CertifiedData } from '$lib/types/store';
+
+const createIcTransactionUiMock = (id: string): IcTransactionUi => ({
+	id,
+	timestamp: nowInBigIntNanoSeconds(),
+	type: 'send',
+	value: BigInt(100),
+	from: 'sender',
+	to: 'receiver',
+	status: 'pending'
+});
+
+export const createCertifiedIcTransactionUiMock = (id: string): CertifiedData<IcTransactionUi> => ({
+	certified: true,
+	data: createIcTransactionUiMock(id)
+});
