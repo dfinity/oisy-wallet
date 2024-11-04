ff3c8948107654e2992fd74d4afab09d1ac38c22
Robert Schlittler <robert.schlittler@inacta.ch>
2024-11-04 10:33:48 +0100

    style(frontend): fix build

--- src/frontend/src/lib/components/transactions/TransactionsPlaceholder.svelte
+++ src/frontend/src/lib/components/transactions/TransactionsPlaceholder.svelte
@@ -32,6 +32,6 @@
 
 <style lang="postcss">
 	.transaction-action-icon {
-		@apply flex items-center justify-center rounded-full bg-background-brand-subtle-alt p-3.5 ring-2 ring-border-brand-subtle;
+		@apply bg-brand-subtle-alt ring-brand-subtle flex items-center justify-center rounded-full p-3.5 ring-2;
 	}
 </style>
