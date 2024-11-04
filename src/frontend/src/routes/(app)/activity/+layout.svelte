a0a8b17e28f2dec7c7baa970007745c03ff3bba1
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-04 19:57:05 +0100

    feat(frontend): New Activity page

---
+++ src/frontend/src/routes/(app)/activity/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import MultipleListeners from '$lib/components/core/MultipleListeners.svelte';
+	import { enabledNetworkTokens } from '$lib/derived/network-tokens.derived';
+</script>
+
+<MultipleListeners tokens={$enabledNetworkTokens}>
+	<slot />
+</MultipleListeners>
---
+++ src/frontend/src/routes/(app)/activity/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { isNullish } from '@dfinity/utils';
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	import Transactions from '$lib/components/transactions/Transactions.svelte';
+	import { routeNetwork } from '$lib/derived/nav.derived';
+	import { networks } from '$lib/derived/networks.derived';
+
+	onMount(async () => {
+		// We need to know the network on which the transactions should be loaded.
+		// While we can guess the network for ICP, we cannot do the same for ICRC tokens as they are loaded asynchronously.
+		// Therefore, we cannot automatically select the network if it is not provided when the component mounts, and we cannot wait indefinitely.
+		// That's why, if no network is provided, we route to the root.
+		if (isNullish($routeNetwork)) {
+			await goto('/');
+		}
+
+		const unknownNetwork =
+			$networks.find(({ id }) => id.description === $routeNetwork) === undefined;
+
+		if (unknownNetwork) {
+			await goto('/');
+		}
+	});
+</script>
+
+<!--TODO: Add unified transaction component here.-->
+Working on it...
+<slot />
