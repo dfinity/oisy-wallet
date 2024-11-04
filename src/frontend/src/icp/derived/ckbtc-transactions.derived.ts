509325c3e8296fa331deb76afdd9e95e3be07fa4
David Dal Busco <david.dalbusco@outlook.com>
2024-11-04 13:20:41 +0100

    feat(frontend): nullify transactions store

--- src/frontend/src/icp/derived/ckbtc-transactions.derived.ts
+++ src/frontend/src/icp/derived/ckbtc-transactions.derived.ts
@@ -9,7 +9,7 @@ import { isNullish } from '@dfinity/utils';
 import { derived, type Readable } from 'svelte/store';
 
 // I would rather like to move mapping those pending utxos to a web worker but the dependency on the minter info is conceptually annoying as it would lead to developping even more custom code for ckBTC which already required way to many mumbo jumbo.
-export const ckBtcPendingUtxoTransactions: Readable<IcTransactionsData> = derived(
+export const ckBtcPendingUtxoTransactions: Readable<NonNullable<IcTransactionsData>> = derived(
 	[tokenWithFallback, ckBtcMinterInfoStore, ckBtcPendingUtxosStore],
 	([$token, $ckBtcMinterInfoStore, $ckBtcPendingUtxosStore]) => {
 		if (!isTokenCkBtcLedger($token)) {
