a3fcd3065fbc1b061eceffa44c66d959e050f0fd
David Dal Busco <david.dalbusco@dfinity.org>
2024-11-04 20:00:23 +0100

    feat(frontend): nullify transactions store (#3301)
    # Motivation
    
    We want to add support for tokens without Index canister which means
    tokens won't have transactions. As a result we decided to extend
    transactions store with a `nullify` option.
    
    # Changes
    
    - Set content of the transactions store to potentially be `null`.
    - Add function `nullify`.
    - Mark derived store as returning `NonNullable` answer.
    - Add a type in BTC store for consistency.

--- src/frontend/src/btc/stores/btc-transactions.store.ts
+++ src/frontend/src/btc/stores/btc-transactions.store.ts
@@ -1,4 +1,6 @@
 import type { BtcTransactionUi } from '$btc/types/btc';
-import { initTransactionsStore } from '$lib/stores/transactions.store';
+import { initTransactionsStore, type TransactionsData } from '$lib/stores/transactions.store';
+
+export type BtcTransactionsData = TransactionsData<BtcTransactionUi>;
 
 export const btcTransactionsStore = initTransactionsStore<BtcTransactionUi>();
