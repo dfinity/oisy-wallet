7c543c598b9563de28dd4ee81c0d89f6de257133
Antonio Ventilii <169057656+AntonioVentilii-DFINITY@users.noreply.github.com>
2024-11-05 09:16:38 +0100

    refactor(frontend): remove deprecated logged out status in Settings (#3320)
    # Motivation
    
    The route that loads the `Settings` component has currently an
    Authorization guard, so we will never have access to it with the user
    signed out.
    
    # Changes
    
    - Remove all auth-related conditions in `Settings` component.
    - Delete deprecated component `SettingsSignedOut`
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/settings/Settings.svelte
+++ src/frontend/src/lib/components/settings/Settings.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
 	import SettingsSignedIn from '$lib/components/settings/SettingsSignedIn.svelte';
-	import SettingsSignedOut from '$lib/components/settings/SettingsSignedOut.svelte';
 	import SettingsVersion from '$lib/components/settings/SettingsVersion.svelte';
-	import { authSignedIn } from '$lib/derived/auth.derived';
 </script>
 
-{#if $authSignedIn}
-	<SettingsSignedIn />
-{:else}
-	<SettingsSignedOut />
-{/if}
+<SettingsSignedIn />
 
 <SettingsVersion />
