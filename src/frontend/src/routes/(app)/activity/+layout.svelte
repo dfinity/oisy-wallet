a9b306579d899796feb93509376d48d0cb8172e1
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-05 13:03:37 +0100

    refactor: move MultipleListeners

--- src/frontend/src/routes/(app)/activity/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import MultipleListeners from '$lib/components/core/MultipleListeners.svelte';
-	import { enabledNetworkTokens } from '$lib/derived/network-tokens.derived';
-</script>
-
-<MultipleListeners tokens={$enabledNetworkTokens}>
-	<slot />
-</MultipleListeners>
--- src/frontend/src/routes/(app)/activity/+page.svelte
+++ src/frontend/src/routes/(app)/activity/+page.svelte
@@ -1,2 +1,10 @@
-<!--TODO: Add unified transaction component here.-->
-Working on it...
+<script lang="ts">
+	import MultipleListeners from '$lib/components/core/MultipleListeners.svelte';
+	import { enabledNetworkTokens } from '$lib/derived/network-tokens.derived';
+</script>
+
+<MultipleListeners tokens={$enabledNetworkTokens}>
+	<!--TODO: Add unified transaction component here.-->
+	Working on it...
+</MultipleListeners>
+
