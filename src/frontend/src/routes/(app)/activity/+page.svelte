411a1f64033caf34696d9b5040c51ae0da3fe326
Antonio Ventilii <32648800+AntonioVentilii@users.noreply.github.com>
2024-11-05 16:34:41 +0100

    feat(frontend): New Activity page (#3352)
    # Note
    
    Duplicate of #3316 , because I changed account.
    
    # Motivation
    
    We want to create a new page with the list of the user activities,
    mainly the unified transactions. As first step we create the route
    similar to the the main page route.
    
    In the layout we set a `MultipleListener` for the entire list of tokens
    enabled for the current network, so that the transactions are updated.
    This reflects what is being done with the transaction page specific to a
    token, where there is a single `Listener`.
    
    
    ![Screenshot 2024-11-04 at 20 01
    53](https://github.com/user-attachments/assets/d8e46371-27cf-45ec-91b7-18c5036485cc)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/frontend/src/routes/(app)/activity/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import MultipleListeners from '$lib/components/core/MultipleListeners.svelte';
+	import { enabledNetworkTokens } from '$lib/derived/network-tokens.derived';
+</script>
+
+<MultipleListeners tokens={$enabledNetworkTokens}>
+	<!--TODO: Add unified transaction component here.-->
+	Working on it...
+</MultipleListeners>
