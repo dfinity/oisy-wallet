3ec6579cc95a2c9150959b98bddb2e967c55c800
Antonio Ventilii <32648800+AntonioVentilii@users.noreply.github.com>
2024-11-05 12:30:50 +0100

    refactor(frontend): use enum for app paths (#3343)
    # Motivation
    
    This is a copy of #3324 after I changed my account.

---
+++ src/frontend/src/lib/constants/routes.constants.ts
@@ -0,0 +1,12 @@
+export enum AppPath {
+	Explore = '/explore',
+	Settings = '/settings',
+	Transactions = '/transactions'
+}
+
+// SvelteKit uses the group defined in src/routes/(app)/ as part of the routeId. It also prefixes it with /.
+export const ROUTE_ID_GROUP_APP = '/(app)';
+
+export const TOKEN_PARAM = 'token';
+export const NETWORK_PARAM = 'network';
+export const URI_PARAM = 'uri';
