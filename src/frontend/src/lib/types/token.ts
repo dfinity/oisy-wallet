86843cb1e4af2ef8670460e24f571f27ce227e5f
David Dal Busco <david.dalbusco@dfinity.org>
2024-11-05 11:51:59 +0100

    feat(frontend): move and test token validation (#3340)
    # Motivation
    
    Extract the token zod schemas and add test.
    
    # Changes
    
    - Extract the `Token` zod from `types` to `validation`
    - Add tests
    - Comment about explicit type to avoid recursive import
    - Add parameter validation in `parseTokenId`

--- src/frontend/src/lib/types/token.ts
+++ src/frontend/src/lib/types/token.ts
@@ -1,55 +1,16 @@
 import type { OptionBalance } from '$lib/types/balance';
-import { NetworkSchema } from '$lib/types/network';
-import type { OnramperId } from '$lib/types/onramper';
-import type { AtLeastOne, Option, RequiredExcept } from '$lib/types/utils';
+import type { Option, RequiredExcept } from '$lib/types/utils';
+import {
+	TokenAppearanceSchema,
+	TokenBuySchema,
+	TokenBuyableSchema,
+	TokenCategorySchema,
+	TokenMetadataSchema,
+	TokenSchema,
+	TokenStandardSchema,
+	type TokenIdSchema
+} from '$lib/validation/token.validation';
 import { z } from 'zod';
-
-export const TokenIdSchema = z.symbol().brand<'TokenId'>();
-
-const TokenStandardSchema = z.enum(['ethereum', 'erc20', 'icp', 'icrc', 'bitcoin']);
-
-const TokenCategorySchema = z.enum(['default', 'custom']);
-
-const TokenMetadataSchema = z.object({
-	name: z.string(),
-	symbol: z.string(),
-	decimals: z.number(),
-	icon: z.string().optional()
-});
-
-const TokenOisySymbolSchema = z.object({
-	oisySymbol: z.string()
-});
-
-const TokenOisyNameSchema = z.object({
-	prefix: z.string().optional(),
-	oisyName: z.string()
-});
-
-const TokenAppearanceSchema = z.object({
-	oisySymbol: TokenOisySymbolSchema.optional(),
-	oisyName: TokenOisyNameSchema.optional()
-});
-
-// TODO: use Zod to validate the OnramperId
-const TokenBuySchema = z.object({
-	onramperId: z.custom<OnramperId>().optional()
-});
-
-const TokenBuyableSchema = z.object({
-	buy: z.custom<AtLeastOne<TokenBuy>>().optional()
-});
-
-const TokenSchema = z
-	.object({
-		id: TokenIdSchema,
-		network: NetworkSchema,
-		standard: TokenStandardSchema,
-		category: TokenCategorySchema
-	})
-	.merge(TokenMetadataSchema)
-	.merge(TokenAppearanceSchema)
-	.merge(TokenBuyableSchema);
 
 export type TokenId = z.infer<typeof TokenIdSchema>;
 
