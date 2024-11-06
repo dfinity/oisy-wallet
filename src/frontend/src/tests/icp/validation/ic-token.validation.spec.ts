670b1d661e274a7bc635366011fe31efe7292be2
David Dal Busco <david.dalbusco@dfinity.org>
2024-11-06 09:31:41 +0100

    test(frontend): cleanup mocks prefix (#3372)
    # Motivation
    
    It's cleaner to prefix all mock values with `mock` to warn the developer
    that a value imported might comes from the tests.

--- src/frontend/src/tests/icp/validation/ic-token.validation.spec.ts
+++ src/frontend/src/tests/icp/validation/ic-token.validation.spec.ts
@@ -5,34 +5,34 @@ import {
 	isNotIcToken,
 	isNotIcTokenCanistersStrict
 } from '$icp/validation/ic-token.validation';
-import { validIcToken } from '$tests/mocks/ic-tokens.mock';
-import { validToken } from '$tests/mocks/tokens.mock';
+import { mockValidIcToken } from '$tests/mocks/ic-tokens.mock';
+import { mockValidToken } from '$tests/mocks/tokens.mock';
 import { describe, expect, it } from 'vitest';
 
 describe('ic-token.validation', () => {
 	describe('isIcToken', () => {
 		it('should return true for a valid IcToken', () => {
-			expect(isIcToken(validIcToken)).toBe(true);
+			expect(isIcToken(mockValidIcToken)).toBe(true);
 		});
 
 		it('should return false for an invalid IcToken', () => {
-			expect(isIcToken(validToken)).toBe(false);
+			expect(isIcToken(mockValidToken)).toBe(false);
 		});
 	});
 
 	describe('isNotIcToken', () => {
 		it('should return false for a valid IcToken', () => {
-			expect(isNotIcToken(validIcToken)).toBe(false);
+			expect(isNotIcToken(mockValidIcToken)).toBe(false);
 		});
 
 		it('should return true for an invalid IcToken', () => {
-			expect(isNotIcToken(validToken)).toBe(true);
+			expect(isNotIcToken(mockValidToken)).toBe(true);
 		});
 	});
 
 	describe('isIcTokenCanistersStrict', () => {
 		it('should return true for a valid IcToken with IcCanistersStrict', () => {
-			expect(isIcTokenCanistersStrict(validIcToken)).toBe(true);
+			expect(isIcTokenCanistersStrict(mockValidIcToken)).toBe(true);
 		});
 
 		// TODO: test missing indexCanisterId when it becomes optional
@@ -41,13 +41,13 @@ describe('ic-token.validation', () => {
 		// });
 
 		it('should return false for a token type casted to IcToken', () => {
-			expect(isIcTokenCanistersStrict(validToken as IcToken)).toBe(false);
+			expect(isIcTokenCanistersStrict(mockValidToken as IcToken)).toBe(false);
 		});
 	});
 
 	describe('isNotIcTokenCanistersStrict', () => {
 		it('should return false for a valid IcToken with IcCanistersStrict', () => {
-			expect(isNotIcTokenCanistersStrict(validIcToken)).toBe(false);
+			expect(isNotIcTokenCanistersStrict(mockValidIcToken)).toBe(false);
 		});
 
 		// TODO: test missing indexCanisterId when it becomes optional
@@ -56,7 +56,7 @@ describe('ic-token.validation', () => {
 		// });
 
 		it('should return true for a token type casted to IcToken', () => {
-			expect(isNotIcTokenCanistersStrict(validToken as IcToken)).toBe(true);
+			expect(isNotIcTokenCanistersStrict(mockValidToken as IcToken)).toBe(true);
 		});
 	});
 });
