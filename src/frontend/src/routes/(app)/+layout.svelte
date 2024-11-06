3295ac86b22901b913cbb7fbb65738382596a775
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-06 18:27:39 +0100

    refactor(frontend): Simplify props of Hero (#3347)
    # Motivation
    
    The props given to `Hero` are somehow a bit confused since derived from
    routing. They were ok before, since the `Hero` was used in all routes.
    With the latest changes, the scope of the `Hero` was reduced to only the
    main page (tokens) and the transaction page.
    
    To clean-up a bit we refactor the Hero to be a bit more obvious. In
    another PR, we will probably merge `Hero` and `HeroContent`.
    
    # Changes
    
    - Use navigations utils directly in `HeroContent` and `+layout`
    - Remove deprecated props from Hero.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/routes/(app)/+layout.svelte
+++ src/frontend/src/routes/(app)/+layout.svelte
@@ -15,18 +15,16 @@
 	import { authNotSignedIn, authSignedIn } from '$lib/derived/auth.derived';
 	import { pageToken } from '$lib/derived/page-token.derived';
 	import { token } from '$lib/stores/token.store';
-	import { isRouteDappExplorer, isRouteSettings, isRouteTransactions } from '$lib/utils/nav.utils';
+	import { isRouteTokens, isRouteTransactions } from '$lib/utils/nav.utils';
 
-	// TODO: We should consider adding a description for the pages, as this block of code is now appearing in two places.
-	// Other areas, like the Menu, are also somewhat disorganized, with navigation logic spread across multiple locations.
-	let route: 'transactions' | 'tokens' | 'settings' | 'explore' = 'tokens';
-	$: route = isRouteSettings($page)
-		? 'settings'
-		: isRouteDappExplorer($page)
-			? 'explore'
-			: isRouteTransactions($page)
-				? 'transactions'
-				: 'tokens';
+	let tokensRoute: boolean;
+	$: tokensRoute = isRouteTokens($page);
+
+	let transactionsRoute: boolean;
+	$: transactionsRoute = isRouteTransactions($page);
+
+	let showHero: boolean;
+	$: showHero = tokensRoute || transactionsRoute;
 
 	$: token.set($pageToken);
 
@@ -60,15 +58,15 @@
 	<AuthGuard>
 		<SplitPane>
 			<NavigationMenu slot="menu">
-				{#if route === 'tokens'}
+				{#if tokensRoute}
 					<div transition:fade class="hidden xl:block">
 						<DappsCarousel />
 					</div>
 				{/if}
 			</NavigationMenu>
 
-			{#if route !== 'settings' && route !== 'explore'}
-				<Hero usdTotal={route === 'tokens'} summary={route === 'transactions'} />
+			{#if showHero}
+				<Hero />
 			{/if}
 
 			<LoadersGuard>
