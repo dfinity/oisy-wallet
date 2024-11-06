6ec2e2dab5c97d2ace2c69d1c48b4beaa0a148e8
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2024-11-06 14:53:08 +0100

    updates tokenToggleable function

--- src/frontend/src/lib/derived/token.derived.ts
+++ src/frontend/src/lib/derived/token.derived.ts
@@ -1,3 +1,9 @@
+import { isBitcoinToken } from '$btc/utils/token.utils';
+import { ICRC_CHAIN_FUSION_DEFAULT_LEDGER_CANISTER_IDS } from '$env/networks.icrc.env';
+import type { EthereumUserToken } from '$eth/types/erc20-user-token';
+import { icTokenEthereumUserToken } from '$eth/utils/erc20.utils';
+import type { IcrcCustomToken } from '$icp/types/icrc-custom-token';
+import { icTokenIcrcCustomToken } from '$icp/utils/icrc.utils';
 import { DEFAULT_ETHEREUM_TOKEN } from '$lib/constants/tokens.constants';
 import { token } from '$lib/stores/token.store';
 import type { OptionTokenId, OptionTokenStandard, Token } from '$lib/types/token';
@@ -29,7 +35,38 @@ export const tokenDecimals: Readable<number | undefined> = derived(
 	([$token]) => $token?.decimals
 );
 
-export const tokenToggleable: Readable<boolean> = derived(
-	[token],
-	([$token]) => nonNullish($token) && 'enabled' in $token
-);
+export const tokenToggleable: Readable<boolean> = derived([token], ([$token]) => {
+	if (nonNullish($token)) {
+		let toggleable = false;
+
+		if (icTokenIcrcCustomToken($token)) {
+			toggleable = !isIcTokenIcrcCustomTokenDisabled($token);
+		} else if (icTokenEthereumUserToken($token)) {
+			toggleable = !isIcTokenEthereumUserTokenDisabled($token);
+		} else if (isBitcoinToken($token)) {
+			toggleable = false;
+		}
+
+		console.log($token);
+		console.log(toggleable);
+		console.log('enabled' in $token);
+
+		return toggleable;
+	} else {
+		return false;
+	}
+});
+
+function isIcTokenIcrcCustomTokenDisabled(token: IcrcCustomToken): boolean {
+	let outdated: boolean;
+	outdated = token.indexCanisterVersion === 'outdated';
+	return (
+		(token.category === 'default' && token.standard === 'icp') ||
+		ICRC_CHAIN_FUSION_DEFAULT_LEDGER_CANISTER_IDS.includes(token.ledgerCanisterId) ||
+		outdated
+	);
+}
+
+function isIcTokenEthereumUserTokenDisabled(token: EthereumUserToken): boolean {
+	return token.category === 'default' && token.standard === 'ethereum';
+}
