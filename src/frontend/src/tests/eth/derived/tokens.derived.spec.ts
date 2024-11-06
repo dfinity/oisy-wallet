e3c30443f81b6d53b5357cac7f26dbbaeb97fa87
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-06 12:21:34 +0100

    test(frontend): Test derived enabledEthereumTokens (#3375)
    Some tests for derived `enabledEthereumTokens`.

---
+++ src/frontend/src/tests/eth/derived/tokens.derived.spec.ts
@@ -0,0 +1,50 @@
+import * as ethEnv from '$env/networks.eth.env';
+import { ETHEREUM_TOKEN, SEPOLIA_TOKEN } from '$env/tokens.env';
+import { enabledEthereumTokens } from '$eth/derived/tokens.derived';
+import * as appContants from '$lib/constants/app.constants';
+import { testnetsStore } from '$lib/stores/settings.store';
+import { get } from 'svelte/store';
+import { expect } from 'vitest';
+
+describe('tokens.derived', () => {
+	describe('enabledEthereumTokens', () => {
+		beforeEach(() => {
+			vi.resetAllMocks();
+
+			testnetsStore.reset({ key: 'testnets' });
+
+			vi.spyOn(ethEnv, 'ETH_MAINNET_ENABLED', 'get').mockImplementation(() => true);
+
+			vi.spyOn(appContants, 'LOCAL', 'get').mockImplementation(() => false);
+		});
+
+		it('should return only ETH by default', () => {
+			expect(get(enabledEthereumTokens)).toEqual([ETHEREUM_TOKEN]);
+		});
+
+		it('should return an empty array when mainnet is disabled', () => {
+			vi.spyOn(ethEnv, 'ETH_MAINNET_ENABLED', 'get').mockImplementationOnce(() => false);
+
+			expect(get(enabledEthereumTokens)).toEqual([]);
+		});
+
+		it('should return only Sepolia ETH when testnets are enabled and mainnet disabled', () => {
+			vi.spyOn(ethEnv, 'ETH_MAINNET_ENABLED', 'get').mockImplementationOnce(() => false);
+			testnetsStore.set({ key: 'testnets', value: { enabled: true } });
+
+			expect(get(enabledEthereumTokens)).toEqual([SEPOLIA_TOKEN]);
+		});
+
+		it('should return ETH and Sepolia ETH when testnets are enabled', () => {
+			testnetsStore.set({ key: 'testnets', value: { enabled: true } });
+
+			expect(get(enabledEthereumTokens)).toEqual([ETHEREUM_TOKEN, SEPOLIA_TOKEN]);
+		});
+
+		it('should return ETH and Sepolia ETH when in local env', () => {
+			vi.spyOn(appContants, 'LOCAL', 'get').mockImplementationOnce(() => true);
+
+			expect(get(enabledEthereumTokens)).toEqual([ETHEREUM_TOKEN, SEPOLIA_TOKEN]);
+		});
+	});
+});
