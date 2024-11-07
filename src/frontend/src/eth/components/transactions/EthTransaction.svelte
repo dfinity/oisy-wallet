5cc4f000d90f6b763a21a8aa2e873b0fe2c84177
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-07 13:58:08 +0100

    feat(frontend): use single component for BTC and ETH transactions (#3401)
    # Motivation
    
    The component `Transaction` could attend both needs of `BtcTransaction`
    and `EthTransaction` with a few tweaks. Mainly, expanding the type of
    accepted transactions types and adjust the icon and the label.
    
    # Changes
    
    - Increase the logic of `icon` selection in `Transaction` to include
    other types (for example conversions).
    - Change `label` to be a `slot` instead of an internal variable.
    - Adapt component `BtcTransaction`: this means only providing the
    `label` as `slot`.
    - Replace the `Card` in `EthTransaction` with `Transaction`.
    
    # Tests
    
    No changes in local replica.

--- src/frontend/src/eth/components/transactions/EthTransaction.svelte
+++ src/frontend/src/eth/components/transactions/EthTransaction.svelte
@@ -1,26 +1,15 @@
 <script lang="ts">
-	import { nonNullish } from '@dfinity/utils';
 	import { BigNumber } from '@ethersproject/bignumber';
-	import type { ComponentType } from 'svelte';
 	import type { Erc20Token } from '$eth/types/erc20';
 	import type { EthTransactionType, EthTransactionUi } from '$eth/types/eth-transaction';
 	import { isSupportedEthToken } from '$eth/utils/eth.utils';
 	import { isTransactionPending } from '$eth/utils/transactions.utils';
-	import IconConvert from '$lib/components/icons/IconConvert.svelte';
-	import IconConvertFrom from '$lib/components/icons/IconConvertFrom.svelte';
-	import IconConvertTo from '$lib/components/icons/IconConvertTo.svelte';
-	import IconReceive from '$lib/components/icons/IconReceive.svelte';
-	import IconSend from '$lib/components/icons/IconSend.svelte';
-	import TransactionStatusComponent from '$lib/components/transactions/TransactionStatus.svelte';
-	import Amount from '$lib/components/ui/Amount.svelte';
-	import Card from '$lib/components/ui/Card.svelte';
-	import RoundedIcon from '$lib/components/ui/RoundedIcon.svelte';
+	import Transaction from '$lib/components/transactions/Transaction.svelte';
 	import { tokenSymbol } from '$lib/derived/token.derived';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { modalStore } from '$lib/stores/modal.store';
 	import { token } from '$lib/stores/token.store';
 	import type { TransactionStatus } from '$lib/types/transaction';
-	import { formatSecondsToDate } from '$lib/utils/format.utils';
 	import { replacePlaceholders } from '$lib/utils/i18n.utils';
 
 	export let transaction: EthTransactionUi;
@@ -70,18 +59,6 @@
 					? $i18n.send.text.send
 					: $i18n.receive.text.receive;
 
-	let icon: ComponentType;
-	$: icon =
-		(type === 'withdraw' || type === 'deposit') && pending
-			? IconConvert
-			: type === 'withdraw'
-				? IconConvertFrom
-				: type === 'deposit'
-					? IconConvertTo
-					: type === 'send'
-						? IconSend
-						: IconReceive;
-
 	let amount: BigNumber;
 	$: amount = type === 'send' || type === 'deposit' ? value.mul(BigNumber.from(-1)) : value;
 
@@ -89,20 +66,12 @@
 	$: transactionDate = timestamp ?? displayTimestamp;
 </script>
 
-<button on:click={() => modalStore.openTransaction(transaction)} class="contents">
-	<Card>
-		{label}
-
-		<RoundedIcon slot="icon" {icon} iconStyleClass={pending ? 'opacity-10' : ''} />
-
-		<Amount {amount} slot="amount" />
-
-		<svelte:fragment slot="description">
-			{#if nonNullish(transactionDate)}
-				{formatSecondsToDate(transactionDate)}
-			{/if}
-
-			<TransactionStatusComponent {status} />
-		</svelte:fragment>
-	</Card>
-</button>
+<Transaction
+	on:click={() => modalStore.openTransaction(transaction)}
+	{amount}
+	{type}
+	timestamp={transactionDate}
+	{status}
+>
+	{label}
+</Transaction>
