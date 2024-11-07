4ca67f80d67ff9fadf6aa724d5cc00e6d8669981
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-07 22:14:01 +0100

    feat(frontend): create list of global transactions

--- src/frontend/src/lib/types/transaction.ts
+++ src/frontend/src/lib/types/transaction.ts
@@ -1,7 +1,11 @@
+import type { BtcTransactionUi } from '$btc/types/btc';
+import type { EthTransactionUi } from '$eth/types/eth-transaction';
+import type { IcTransactionUi } from '$icp/types/ic-transaction';
 import type { TransactionResponse } from '@ethersproject/abstract-provider';
 import type { BigNumber } from '@ethersproject/bignumber';
 import type { FeeData } from '@ethersproject/providers';
 import type { Transaction as EthTransaction } from '@ethersproject/transactions';
+import type { ComponentType } from 'svelte';
 
 export type Transaction = Omit<EthTransaction, 'data'> &
 	Pick<TransactionResponse, 'blockNumber' | 'from' | 'to' | 'timestamp'> & {
@@ -22,4 +26,9 @@ export type TransactionUiCommon = Pick<Transaction, 'blockNumber' | 'from' | 'to
 	txExplorerUrl?: string;
 	toExplorerUrl?: string;
 	fromExplorerUrl?: string;
+};
+
+export type TransactionUi = (BtcTransactionUi | EthTransactionUi | IcTransactionUi) & {
+	id: string;
+	transactionComponent: ComponentType;
 };
