0664fc6737606d5695e805b55fe2dbbf91739046
David Dal Busco <david.dalbusco@outlook.com>
2024-11-07 07:33:57 +0100

    feat: add todos

--- src/frontend/src/tests/icp-eth/services/custom-token.services.spec.ts
+++ src/frontend/src/tests/icp-eth/services/custom-token.services.spec.ts
@@ -1,3 +1,4 @@
+import { IC_CKBTC_INDEX_CANISTER_ID } from '$env/networks.icrc.env';
 import { autoLoadCustomToken } from '$icp-eth/services/custom-token.services';
 import { icrcCustomTokensStore } from '$icp/stores/icrc-custom-tokens.store';
 import type { IcrcCustomToken } from '$icp/types/icrc-custom-token';
@@ -12,6 +13,7 @@ import { mockValidToken } from '$tests/mocks/tokens.mock';
 import type { HttpAgent } from '@dfinity/agent';
 import { IcrcLedgerCanister } from '@dfinity/ledger-icrc';
 import { Principal } from '@dfinity/principal';
+import { isNullish } from '@dfinity/utils';
 import { get } from 'svelte/store';
 import { expect, type MockInstance } from 'vitest';
 import { mock } from 'vitest-mock-extended';
@@ -58,10 +60,12 @@ describe('custom-token.services', () => {
 		describe('success', () => {
 			const assertSetCustomToken = async ({
 				customTokens,
-				expectedVersion
+				expectedVersion,
+				indexCanisterId
 			}: {
 				customTokens: IcrcCustomToken[];
 				expectedVersion: [] | [bigint];
+				indexCanisterId: string | undefined;
 			}) => {
 				const spySetCustomToken = backendCanisterMock.setCustomToken.mockResolvedValue(undefined);
 				const spyListCustomTokens = backendCanisterMock.listCustomTokens.mockResolvedValue([]);
@@ -72,8 +76,11 @@ describe('custom-token.services', () => {
 					standard: 'erc20' as const
 				};
 
+				const [first, ...rest] = customTokens;
+				const icrcCustomTokens = [{ ...first, indexCanisterId }, ...rest];
+
 				const { result } = await autoLoadCustomToken({
-					icrcCustomTokens: customTokens,
+					icrcCustomTokens,
 					sendToken: mockSendToken,
 					identity: mockIdentity
 				});
@@ -86,7 +93,7 @@ describe('custom-token.services', () => {
 						version: expectedVersion,
 						token: {
 							Icrc: {
-								index_id: [Principal.fromText(mockSendToken.indexCanisterId)],
+								index_id: isNullish(indexCanisterId) ? [] : [Principal.fromText(indexCanisterId)],
 								ledger_id: Principal.fromText(mockSendToken.ledgerCanisterId)
 							}
 						}
@@ -96,77 +103,92 @@ describe('custom-token.services', () => {
 				expect(spyListCustomTokens).toHaveBeenCalledWith({ certified: true });
 			};
 
-			it('should call setCustomToken with a new custom token', async () => {
-				await assertSetCustomToken({ customTokens: mockIcrcCustomTokens, expectedVersion: [] });
-			});
+			it.each([undefined, IC_CKBTC_INDEX_CANISTER_ID])(
+				'should call setCustomToken with a new custom token with index %s',
+				async (indexCanisterId) => {
+					await assertSetCustomToken({
+						customTokens: mockIcrcCustomTokens,
+						expectedVersion: [],
+						indexCanisterId
+					});
+				}
+			);
 
-			it('should call setCustomToken to update a custom token', async () => {
-				const customTokens: IcrcCustomToken[] = [
-					{
-						...mockIcrcCustomTokens[0],
-						version: 1n
-					},
-					mockIcrcCustomTokens[1]
-				];
-
-				await assertSetCustomToken({
-					customTokens,
-					expectedVersion: [customTokens[0].version ?? 0n]
-				});
-			});
-
-			it('should load tokens after set custom token', async () => {
-				backendCanisterMock.setCustomToken.mockResolvedValue(undefined);
-				const spyListCustomTokens = backendCanisterMock.listCustomTokens.mockResolvedValue([
-					{
-						token: {
-							Icrc: {
-								index_id: [Principal.fromText(mockValidSendToken.indexCanisterId)],
-								ledger_id: Principal.fromText(mockValidSendToken.ledgerCanisterId)
-							}
-						},
-						version: [1n],
-						enabled: true
-					}
-				]);
-
-				const spyMetadata = ledgerCanisterMock.metadata.mockResolvedValue([
-					['icrc1:name', { Text: mockValidSendToken.name }],
-					['icrc1:symbol', { Text: mockValidSendToken.symbol }],
-					['icrc1:decimals', { Nat: BigInt(mockValidSendToken.decimals) }],
-					['icrc1:fee', { Nat: mockValidSendToken.fee }]
-				]);
-
-				const { result } = await autoLoadCustomToken({
-					icrcCustomTokens: mockIcrcCustomTokens,
-					sendToken: mockValidSendToken,
-					identity: mockIdentity
-				});
-
-				expect(result).toBe('loaded');
-
-				expect(spyListCustomTokens).toHaveBeenCalledWith({ certified: true });
-
-				expect(spyMetadata).toHaveBeenCalledWith({ certified: true });
-
-				const store = get(icrcCustomTokensStore);
-
-				expect(store).toHaveLength(1);
-				expect(store).toEqual([
-					{
-						certified: true,
-						data: expect.objectContaining({
-							...mockValidIcToken,
-							id: expect.any(Symbol),
-							category: 'custom',
-							position: 4,
-							enabled: true,
-							standard: 'icrc',
+			it.each([undefined, IC_CKBTC_INDEX_CANISTER_ID])(
+				'should call setCustomToken to update a custom token with index %s',
+				async (indexCanisterId) => {
+					const customTokens: IcrcCustomToken[] = [
+						{
+							...mockIcrcCustomTokens[0],
 							version: 1n
-						})
-					}
-				]);
-			});
+						},
+						mockIcrcCustomTokens[1]
+					];
+
+					await assertSetCustomToken({
+						customTokens,
+						expectedVersion: [customTokens[0].version ?? 0n],
+						indexCanisterId
+					});
+				}
+			);
+
+			// TODO(OISY-296): introduce test for undefined Index canister ID
+			it.each([IC_CKBTC_INDEX_CANISTER_ID])(
+				'should load tokens after set custom token with index ID %s',
+				async (indexCanisterId) => {
+					backendCanisterMock.setCustomToken.mockResolvedValue(undefined);
+					const spyListCustomTokens = backendCanisterMock.listCustomTokens.mockResolvedValue([
+						{
+							token: {
+								Icrc: {
+									index_id: isNullish(indexCanisterId) ? [] : [Principal.fromText(indexCanisterId)],
+									ledger_id: Principal.fromText(mockValidSendToken.ledgerCanisterId)
+								}
+							},
+							version: [1n],
+							enabled: true
+						}
+					]);
+
+					const spyMetadata = ledgerCanisterMock.metadata.mockResolvedValue([
+						['icrc1:name', { Text: mockValidSendToken.name }],
+						['icrc1:symbol', { Text: mockValidSendToken.symbol }],
+						['icrc1:decimals', { Nat: BigInt(mockValidSendToken.decimals) }],
+						['icrc1:fee', { Nat: mockValidSendToken.fee }]
+					]);
+
+					const { result } = await autoLoadCustomToken({
+						icrcCustomTokens: mockIcrcCustomTokens,
+						sendToken: mockValidSendToken,
+						identity: mockIdentity
+					});
+
+					expect(result).toBe('loaded');
+
+					expect(spyListCustomTokens).toHaveBeenCalledWith({ certified: true });
+
+					expect(spyMetadata).toHaveBeenCalledWith({ certified: true });
+
+					const store = get(icrcCustomTokensStore);
+
+					expect(store).toHaveLength(1);
+					expect(store).toEqual([
+						{
+							certified: true,
+							data: expect.objectContaining({
+								...mockValidIcToken,
+								id: expect.any(Symbol),
+								category: 'custom',
+								position: 4,
+								enabled: true,
+								standard: 'icrc',
+								version: 1n
+							})
+						}
+					]);
+				}
+			);
 		});
 
 		describe('error', () => {
@@ -210,38 +232,42 @@ describe('custom-token.services', () => {
 				});
 			});
 
-			it('should result with loaded but toastError if metadata fails', async () => {
-				backendCanisterMock.setCustomToken.mockResolvedValue(undefined);
+			// TODO(OISY-296): introduce test for undefined Index canister ID
+			it.each([IC_CKBTC_INDEX_CANISTER_ID])(
+				'should result with loaded but toastError if metadata fails with index ID %s',
+				async (indexCanisterId) => {
+					backendCanisterMock.setCustomToken.mockResolvedValue(undefined);
 
-				backendCanisterMock.listCustomTokens.mockResolvedValue([
-					{
-						token: {
-							Icrc: {
-								index_id: [Principal.fromText(mockValidSendToken.indexCanisterId)],
-								ledger_id: Principal.fromText(mockValidSendToken.ledgerCanisterId)
-							}
-						},
-						version: [1n],
-						enabled: true
-					}
-				]);
+					backendCanisterMock.listCustomTokens.mockResolvedValue([
+						{
+							token: {
+								Icrc: {
+									index_id: isNullish(indexCanisterId) ? [] : [Principal.fromText(indexCanisterId)],
+									ledger_id: Principal.fromText(mockValidSendToken.ledgerCanisterId)
+								}
+							},
+							version: [1n],
+							enabled: true
+						}
+					]);
 
-				const err = new Error('test');
-				ledgerCanisterMock.metadata.mockRejectedValue(err);
+					const err = new Error('test');
+					ledgerCanisterMock.metadata.mockRejectedValue(err);
 
-				const { result } = await autoLoadCustomToken({
-					icrcCustomTokens: mockIcrcCustomTokens,
-					sendToken: mockValidSendToken,
-					identity: mockIdentity
-				});
+					const { result } = await autoLoadCustomToken({
+						icrcCustomTokens: mockIcrcCustomTokens,
+						sendToken: mockValidSendToken,
+						identity: mockIdentity
+					});
 
-				expect(result).toBe('loaded');
+					expect(result).toBe('loaded');
 
-				expect(spyToastsError).toHaveBeenNthCalledWith(1, {
-					msg: { text: get(i18n).init.error.icrc_canisters },
-					err
-				});
-			});
+					expect(spyToastsError).toHaveBeenNthCalledWith(1, {
+						msg: { text: get(i18n).init.error.icrc_canisters },
+						err
+					});
+				}
+			);
 		});
 	});
 });
