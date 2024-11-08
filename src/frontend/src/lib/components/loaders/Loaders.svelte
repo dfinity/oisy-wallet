e246700520cf967ad99d2e0672b7d2e37c6df05b
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-08 08:48:41 +0100

    refactor(frontend): move all loaders components in specific folder (#3410)
    # Motivation
    
    Moving all loaders component to a single folder, to keep it tidy.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/loaders/Loaders.svelte
+++ src/frontend/src/lib/components/loaders/Loaders.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import LoaderBalances from '$icp-eth/components/core/LoaderBalances.svelte';
-	import Loader from '$lib/components/core/Loader.svelte';
-	import LoaderMetamask from '$lib/components/core/LoaderMetamask.svelte';
-	import LoaderWallets from '$lib/components/core/LoaderWallets.svelte';
 	import ExchangeWorker from '$lib/components/exchange/ExchangeWorker.svelte';
 	import AddressGuard from '$lib/components/guard/AddressGuard.svelte';
+	import Loader from '$lib/components/loaders/Loader.svelte';
+	import LoaderMetamask from '$lib/components/loaders/LoaderMetamask.svelte';
+	import LoaderWallets from '$lib/components/loaders/LoaderWallets.svelte';
 </script>
 
 <AddressGuard>
