4349127b82954fa54c784384d323304e68bab4fb
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-08 12:03:50 +0100

    test(frontend): create global mock for page store (#3415)
    # Motivation
    
    Inspired by [NNS Dapp
    codebase](https://github.com/dfinity/nns-dapp/blob/95d58213187190f606cffe7b43e6fc857d50528d/frontend/__mocks__/%24app/stores.ts#L40),
    we create a mock for the `page` store at the base of the vitest configs.
    
    # Changes
    
    - Create `page` store mock similar to NNS Dapp.
    - Include it in `vitest.setup`.
    - Adapt the tests. 
    
    # Tests
    
    All tests still works.

--- src/frontend/src/tests/lib/stores/balances.store.spec.ts
+++ src/frontend/src/tests/lib/stores/balances.store.spec.ts
@@ -1,12 +1,14 @@
 import { ICP_TOKEN } from '$env/tokens.env';
 import { balancesStore } from '$lib/stores/balances.store';
-import { mockPageStore } from '$tests/mocks/page.store.mock';
+import { mockPage } from '$tests/mocks/page.store.mock';
 import { testDerivedUpdates } from '$tests/utils/derived.utils';
 import { BigNumber } from 'alchemy-sdk';
 
-mockPageStore();
-
 describe('balancesStore', () => {
+	beforeEach(() => {
+		mockPage.reset();
+	});
+
 	it('should ensure derived stores update at most once when the store changes', async () => {
 		await testDerivedUpdates(() =>
 			balancesStore.set({
--- src/frontend/src/tests/lib/stores/token.store.spec.ts
+++ src/frontend/src/tests/lib/stores/token.store.spec.ts
@@ -1,11 +1,13 @@
 import { ICP_TOKEN } from '$env/tokens.env';
 import { token } from '$lib/stores/token.store';
-import { mockPageStore } from '$tests/mocks/page.store.mock';
+import { mockPage } from '$tests/mocks/page.store.mock';
 import { testDerivedUpdates } from '$tests/utils/derived.utils';
 
-mockPageStore();
-
 describe('token store', () => {
+	beforeEach(() => {
+		mockPage.reset();
+	});
+
 	it('should ensure derived stores update at most once when the store changes', async () => {
 		await testDerivedUpdates(() => token.set(ICP_TOKEN));
 	});
