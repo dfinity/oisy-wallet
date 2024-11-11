806a6ba7340be293b88ba1e3246122c8eb1176f4
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-11-11 13:47:37 +0100

    feat(frontend): new MessageBox level (#3433)
    # Motivation
    
    Add new level to the MessageBox component.
    
    <img width="496" alt="Screenshot 2024-11-11 at 12 28 52"
    src="https://github.com/user-attachments/assets/d69dde7a-3840-437d-b569-ccd727f548e0">

--- src/frontend/src/lib/components/ui/MessageBox.svelte
+++ src/frontend/src/lib/components/ui/MessageBox.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
 	import IconInfo from '$lib/components/icons/lucide/IconInfo.svelte';
 
-	export let level: 'info' | 'light-warning' = 'info';
+	export let level: 'info' | 'light-warning' | 'error' = 'info';
 </script>
 
 <div
 	class="mb-4 flex items-start gap-4 rounded-xl px-4 py-3 text-sm font-medium sm:text-base"
 	class:bg-brand-subtle-alt={level === 'info'}
 	class:bg-warning-subtle={level === 'light-warning'}
+	class:bg-error-subtle-alt={level === 'error'}
 >
 	<div
 		class="min-w-5 py-0 sm:py-0.5"
 		class:text-brand-primary={level === 'info'}
 		class:text-warning={level === 'light-warning'}
+		class:text-error={level === 'error'}
 	>
 		<IconInfo />
 	</div>
