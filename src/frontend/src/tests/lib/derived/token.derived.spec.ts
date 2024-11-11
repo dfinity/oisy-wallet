da3af09275fc7541e7345d6e8468bbd53dd95c30
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2024-11-11 14:43:57 +0100

    implements some tests for token.derived

---
+++ src/frontend/src/tests/lib/derived/token.derived.spec.ts
@@ -0,0 +1,63 @@
+import type { Erc20UserToken } from '$eth/types/erc20-user-token';
+import type { IcrcCustomToken } from '$icp/types/icrc-custom-token';
+import { tokenToggleable } from '$lib/derived/token.derived';
+import { token } from '$lib/stores/token.store';
+import { parseTokenId } from '$lib/validation/token.validation';
+import { mockValidErc20Token } from '$tests/mocks/erc20-tokens.mock';
+import { mockValidIcToken } from '$tests/mocks/ic-tokens.mock';
+import { get } from 'svelte/store';
+import { expect } from 'vitest';
+
+describe('token.derived', () => {
+	const mockEr20UserToken: Erc20UserToken = {
+		...mockValidErc20Token,
+		id: parseTokenId('Erc20UserTokenId'),
+		symbol: 'EUTK',
+		address: `${mockValidErc20Token.address}2`,
+		version: undefined,
+		enabled: true
+	};
+
+	const mockIcrcCustomToken: IcrcCustomToken = {
+		...mockValidIcToken,
+		ledgerCanisterId: `${mockValidIcToken.ledgerCanisterId}2`,
+		version: 1n,
+		enabled: true
+	};
+
+	describe('tokenToggleable', () => {
+		it('should return false for nullish token', () => {
+			expect(get(tokenToggleable)).toBe(false);
+		});
+
+		it('should return if default erc20 user token is toggleable', () => {
+			token.set(mockEr20UserToken);
+			expect(get(tokenToggleable)).toBe(true);
+		});
+
+		it('should return if custom erc20 user token is toggleable', () => {
+			token.set({ ...mockEr20UserToken, category: 'custom' });
+			expect(get(tokenToggleable)).toBe(true);
+		});
+
+		it('should return if default ethereum user token is toggleable', () => {
+			token.set({ ...mockEr20UserToken, standard: 'ethereum' });
+			expect(get(tokenToggleable)).toBe(false);
+		});
+
+		it('should return if custom ethereum user token is toggleable', () => {
+			token.set({ ...mockEr20UserToken, category: 'custom', standard: 'ethereum' });
+			expect(get(tokenToggleable)).toBe(true);
+		});
+
+		it('should return if icrc default token is toggleable', () => {
+			token.set(mockIcrcCustomToken);
+			expect(get(tokenToggleable)).toBe(false);
+		});
+
+		it('should return if icrc custom token is toggleable', () => {
+			token.set({ ...mockIcrcCustomToken, category: 'custom' });
+			expect(get(tokenToggleable)).toBe(true);
+		});
+	});
+});
