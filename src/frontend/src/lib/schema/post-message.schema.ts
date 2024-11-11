05fe5fcc702aa58320256f0e0a17062f5d4552a3
David Dal Busco <david.dalbusco@outlook.com>
2024-11-11 07:50:25 +0100

    feat(frontend): post-message type zod schema

---
+++ src/frontend/src/lib/schema/post-message.schema.ts
@@ -0,0 +1,145 @@
+import type { BitcoinNetwork as SignerBitcoinNetwork } from '$declarations/signer/signer.did';
+import type { Erc20ContractAddress } from '$eth/types/erc20';
+import { IcCanistersSchema, IcCkMetadataSchema } from '$icp/schema/ic-token.schema';
+import type { BtcAddressData } from '$icp/stores/btc.store';
+import type { JsonText } from '$icp/types/btc.post-message';
+import { NetworkSchema } from '$lib/schema/network.schema';
+import { SyncStateSchema } from '$lib/schema/sync.schema';
+import type { BtcAddress } from '$lib/types/address';
+import { CanisterIdTextSchema } from '$lib/types/canister';
+import type {
+	CoingeckoSimplePriceResponse,
+	CoingeckoSimpleTokenPriceResponse
+} from '$lib/types/coingecko';
+import type { CertifiedData } from '$lib/types/store';
+import type { BitcoinNetwork } from '@dfinity/ckbtc';
+import { z } from 'zod';
+
+export const PostMessageRequestSchema = z.enum([
+	'startIdleTimer',
+	'stopIdleTimer',
+	'startCodeTimer',
+	'stopCodeTimer',
+	'startExchangeTimer',
+	'stopExchangeTimer',
+	'stopIcpWalletTimer',
+	'startIcpWalletTimer',
+	'triggerIcpWalletTimer',
+	'stopIcrcWalletTimer',
+	'startIcrcWalletTimer',
+	'triggerIcrcWalletTimer',
+	'stopBtcWalletTimer',
+	'startBtcWalletTimer',
+	'triggerBtcWalletTimer',
+	'stopBtcStatusesTimer',
+	'startBtcStatusesTimer',
+	'triggerBtcStatusesTimer',
+	'stopCkBTCUpdateBalanceTimer',
+	'startCkBTCUpdateBalanceTimer',
+	'stopCkMinterInfoTimer',
+	'startCkMinterInfoTimer',
+	'triggerCkMinterInfoTimer'
+]);
+
+export const PostMessageDataRequestSchema = z.never();
+export const PostMessageDataResponseSchema = z.object({});
+
+export const PostMessageDataRequestExchangeTimerSchema = z.object({
+	erc20Addresses: z.array(z.custom<Erc20ContractAddress>()),
+	icrcCanisterIds: z.array(CanisterIdTextSchema)
+});
+
+export const PostMessageDataRequestIcrcSchema = IcCanistersSchema.merge(
+	NetworkSchema.pick({ env: true })
+);
+
+export const PostMessageDataRequestIcCkSchema = IcCkMetadataSchema.pick({
+	minterCanisterId: true
+}).partial();
+
+export const PostMessageDataRequestIcCkBTCUpdateBalanceSchema =
+	PostMessageDataRequestIcCkSchema.extend({
+		btcAddress: z.string().optional(),
+		bitcoinNetwork: z.custom<BitcoinNetwork>()
+	});
+
+export const PostMessageDataRequestBtcSchema = z.object({
+	btcAddress: z.custom<CertifiedData<BtcAddress>>(),
+	shouldFetchTransactions: z.boolean(),
+	bitcoinNetwork: z.custom<SignerBitcoinNetwork>()
+});
+
+export const PostMessageResponseStatusSchema = z.enum([
+	'syncIcWalletStatus',
+	'syncBtcWalletStatus',
+	'syncBtcStatusesStatus',
+	'syncCkMinterInfoStatus',
+	'syncCkBTCUpdateBalanceStatus'
+]);
+
+export const PostMessageResponseSchema = z.enum([
+	'signOutIdleTimer',
+	'delegationRemainingTime',
+	'syncExchange',
+	'syncExchangeError',
+	'syncIcpWallet',
+	'syncIcrcWallet',
+	'syncBtcWallet',
+	'syncIcpWalletError',
+	'syncIcrcWalletError',
+	'syncIcpWalletCleanUp',
+	'syncIcrcWalletCleanUp',
+	'syncBtcStatuses',
+	'syncBtcStatusesError',
+	'syncCkMinterInfo',
+	'syncCkMinterInfoError',
+	'syncBtcPendingUtxos',
+	'syncCkBTCUpdateOk',
+	'syncBtcAddress',
+	...PostMessageResponseStatusSchema.options
+]);
+
+export const PostMessageDataResponseAuthSchema = PostMessageDataResponseSchema.extend({
+	authRemainingTime: z.number()
+});
+
+export const PostMessageDataResponseExchangeSchema = PostMessageDataResponseSchema.extend({
+	currentEthPrice: z.custom<CoingeckoSimplePriceResponse>(),
+	currentBtcPrice: z.custom<CoingeckoSimplePriceResponse>(),
+	currentErc20Prices: z.custom<CoingeckoSimpleTokenPriceResponse>(),
+	currentIcpPrice: z.custom<CoingeckoSimplePriceResponse>(),
+	currentIcrcPrices: z.custom<CoingeckoSimpleTokenPriceResponse>()
+});
+
+export const PostMessageDataResponseExchangeErrorSchema = PostMessageDataResponseSchema.extend({
+	err: z.string().optional()
+});
+
+// Transactions & {certified: boolean}
+export const JsonTransactionsTextSchema = z.string();
+
+export const PostMessageDataResponseErrorSchema = PostMessageDataResponseSchema.extend({
+	error: z.unknown()
+});
+
+export const PostMessageDataResponseWalletCleanUpSchema = PostMessageDataResponseSchema.extend({
+	transactionIds: z.array(z.string())
+});
+
+export const PostMessageJsonDataResponseSchema = PostMessageDataResponseSchema.extend({
+	json: z.custom<JsonText>()
+});
+
+export const PostMessageSyncStateSchema = PostMessageDataResponseSchema.extend({
+	state: SyncStateSchema
+});
+
+export const PostMessageDataResponseBTCAddressSchema = PostMessageDataResponseSchema.extend({
+	address: z.custom<BtcAddressData>()
+});
+
+export const PostMessageSchema = <T extends z.ZodTypeAny>(dataSchema: T) =>
+	z.object({
+		msg: z.union([PostMessageRequestSchema, PostMessageResponseSchema]),
+		data: dataSchema.optional()
+	});
