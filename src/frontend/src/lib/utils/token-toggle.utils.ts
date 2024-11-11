9eb4005f8e95acfcbdccef5ede3d941a2f4f27bb
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2024-11-11 11:09:36 +0100

    writes some test for token-toggle.utils.ts

--- src/frontend/src/lib/utils/token-toggle.utils.ts
+++ src/frontend/src/lib/utils/token-toggle.utils.ts
@@ -3,7 +3,7 @@ import type { EthereumUserToken } from '$eth/types/erc20-user-token';
 import type { IcrcCustomToken } from '$icp/types/icrc-custom-token';
 import { nonNullish } from '@dfinity/utils';
 
-// TODO: check why this functionality is used instead of eth.utils.ts -> isSupportedEthToken.
+// TODO: Check why this functionality is used instead of eth.utils.ts -> isSupportedEthToken.
 export function isEthereumUserTokenDisabled(token: EthereumUserToken): boolean {
 	return nonNullish(token) && token.category === 'default' && token.standard === 'ethereum';
 }
@@ -12,7 +12,7 @@ export function isEthereumUserTokenEnabled(token: EthereumUserToken): boolean {
 	return !isEthereumUserTokenDisabled(token);
 }
 
-// TODO: like above - check why this functionality is used.
+// TODO: Like above - check why this functionality is used.
 export function isIcrcCustomTokenDisabled(token: IcrcCustomToken): boolean {
 	return nonNullish(token)
 		? token.indexCanisterVersion === 'outdated' ||
---
+++ src/frontend/src/tests/lib/utils/token-toggle.utils.spec.ts
@@ -0,0 +1,105 @@
+import type { EthereumUserToken } from '$eth/types/erc20-user-token';
+import { ETHEREUM_TOKEN, ETHEREUM_TOKEN_ID, ICP_TOKEN, ICP_TOKEN_ID, SEPOLIA_TOKEN } from '$env/tokens.env';
+import { ETHEREUM_NETWORK, ICP_NETWORK } from '$env/networks.env';
+import { isEthereumUserTokenDisabled, isIcrcCustomTokenDisabled } from '$lib/utils/token-toggle.utils';
+import type { IcrcCustomToken } from '$icp/types/icrc-custom-token';
+import { describe, expect } from 'vitest';
+import {
+	IC_CKBTC_INDEX_CANISTER_ID,
+	IC_CKBTC_LEDGER_CANISTER_ID,
+} from '$env/networks.icrc.env';
+import { ICP_TRANSACTION_FEE_E8S } from '$icp/constants/icp.constants';
+
+describe('isEthereumUserTokenDisabled', () => {
+	it('should check if default ethereum user token is disabled for token toggle', () => {
+		const token: EthereumUserToken = {
+			id: ETHEREUM_TOKEN_ID,
+			name: ETHEREUM_TOKEN.name,
+			decimals: ETHEREUM_TOKEN.decimals,
+			network: ETHEREUM_NETWORK,
+			symbol: ETHEREUM_NETWORK.name,
+			standard: 'ethereum',
+			enabled: false,
+			category: 'default'
+		};
+
+		expect(isEthereumUserTokenDisabled(token)).toBe(true);
+	});
+
+	it('should check if custom ethereum user token is disabled for token toggle', () => {
+		const token: EthereumUserToken = {
+			id: ETHEREUM_TOKEN_ID,
+			name: ETHEREUM_TOKEN.name,
+			decimals: ETHEREUM_TOKEN.decimals,
+			network: ETHEREUM_NETWORK,
+			symbol: ETHEREUM_NETWORK.name,
+			standard: 'ethereum',
+			enabled: false,
+			category: 'custom'
+		};
+
+		expect(isEthereumUserTokenDisabled(token)).toBe(false);
+	});
+});
+
+describe('isIcrcCustomTokenDisabled', () => {
+	it('should check if icrc custom token is disabled for token toggle', () => {
+		const token: IcrcCustomToken = {
+			id: ICP_TOKEN_ID,
+			name: ICP_TOKEN.name,
+			decimals: ICP_TOKEN.decimals,
+			network: ICP_NETWORK,
+			symbol: ICP_NETWORK.name,
+			ledgerCanisterId: IC_CKBTC_LEDGER_CANISTER_ID,
+			indexCanisterId: IC_CKBTC_INDEX_CANISTER_ID,
+			indexCanisterVersion: 'up-to-date',
+			standard: 'icp',
+			category: 'default',
+			position: 0,
+			enabled: false,
+			fee: ICP_TRANSACTION_FEE_E8S,
+		}
+
+		expect(isIcrcCustomTokenDisabled(token)).toBe(true);
+	});
+
+	it('should check if custom icrc custom token is disabled for token toggle', () => {
+		const token: IcrcCustomToken = {
+			id: ICP_TOKEN_ID,
+			name: ICP_TOKEN.name,
+			decimals: ICP_TOKEN.decimals,
+			network: ICP_NETWORK,
+			symbol: ICP_NETWORK.name,
+			ledgerCanisterId: 'mxzaz-hqaaa-aaaar-qaadu-cai',
+			indexCanisterId: IC_CKBTC_INDEX_CANISTER_ID,
+			indexCanisterVersion: 'up-to-date',
+			standard: 'icp',
+			category: 'custom',
+			position: 0,
+			enabled: false,
+			fee: ICP_TRANSACTION_FEE_E8S,
+		}
+
+		expect(isIcrcCustomTokenDisabled(token)).toBe(false);
+	});
+
+	it('should check if outdated custom icrc custom token is disabled for token toggle', () => {
+		const token: IcrcCustomToken = {
+			id: ICP_TOKEN_ID,
+			name: ICP_TOKEN.name,
+			decimals: ICP_TOKEN.decimals,
+			network: ICP_NETWORK,
+			symbol: ICP_NETWORK.name,
+			ledgerCanisterId: 'mxzaz-hqaaa-aaaar-qaadu-cai',
+			indexCanisterId: IC_CKBTC_INDEX_CANISTER_ID,
+			indexCanisterVersion: 'outdated',
+			standard: 'icp',
+			category: 'custom',
+			position: 0,
+			enabled: false,
+			fee: ICP_TRANSACTION_FEE_E8S,
+		}
+
+		expect(isIcrcCustomTokenDisabled(token)).toBe(true);
+	});
+});
