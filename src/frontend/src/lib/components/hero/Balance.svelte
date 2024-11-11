9b2acd73741db817badd8ad87b07326c84daa42d
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2024-11-11 14:57:09 +0100

    removes opacity definition of balance component

--- src/frontend/src/lib/components/hero/Balance.svelte
+++ src/frontend/src/lib/components/hero/Balance.svelte
@@ -13,7 +13,7 @@
 
 <span class="flex flex-col gap-2">
 	<output
-		class={`break-words ${(token?.balance?.toBigInt() ?? 0n) === 0n ? 'opacity-50' : 'opacity-100'} inline-flex w-full flex-row justify-center gap-3 text-4xl font-bold lg:text-5xl`}
+		class={`break-words inline-flex w-full flex-row justify-center gap-3 text-4xl font-bold lg:text-5xl`}
 	>
 		{#if nonNullish(token?.balance) && nonNullish(token?.symbol) && !token.balance.isZero()}
 			<span><Amount amount={token.balance} /> {token.symbol}</span>
