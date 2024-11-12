36a5fde7ac5c1b66fde812eae6581b80e60f95ce
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2024-11-12 15:07:15 +0100

    feat(frontend): update border

--- src/frontend/src/lib/components/convert/ConvertInputsContainer.svelte
+++ src/frontend/src/lib/components/convert/ConvertInputsContainer.svelte
@@ -1,7 +1,7 @@
 <div class="flex flex-col items-start justify-between md:flex-row">
 	<!-- TODO: Update colors with values from theme-variables when they are available there -->
 	<div
-		class="mb-3 flex h-14 w-full items-center rounded-lg border-[1px] border-bright-gray bg-ghost-white px-3 md:mb-0 md:w-[35%]"
+		class="mb-3 flex h-14 w-full items-center rounded-lg border border-bright-gray bg-ghost-white px-3 md:mb-0 md:w-[35%]"
 	>
 		<slot name="token-info" />
 	</div>
