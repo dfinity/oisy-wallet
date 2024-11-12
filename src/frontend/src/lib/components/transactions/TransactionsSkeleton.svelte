6b15c3144ff3738e2b960697cfd6c7531b589b7b
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-12 11:11:30 +0100

    feat(frontend): common component for transactions skeleton

---
+++ src/frontend/src/lib/components/transactions/TransactionsSkeleton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+	import SkeletonCards from '$lib/components/ui/SkeletonCards.svelte';
+
+	export let loading: boolean;
+</script>
+
+{#if loading}
+	<SkeletonCards rows={5} />
+{:else}
+	<div in:fade>
+		<slot />
+	</div>
+{/if}
