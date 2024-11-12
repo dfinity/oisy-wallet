47b82a5a81142dac8865e0fd2db40958070217b9
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-12 09:20:39 +0100

    feat: use new component

--- src/frontend/src/routes/(app)/activity/+page.svelte
+++ src/frontend/src/routes/(app)/activity/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+	import LoaderMultipleEthTransactions from '$eth/components/loaders/LoaderMultipleEthTransactions.svelte';
 	import MultipleListeners from '$lib/components/core/MultipleListeners.svelte';
 	import Activity from '$lib/components/transactions/Activity.svelte';
 	import { enabledNetworkTokens } from '$lib/derived/network-tokens.derived';
 </script>
 
 <MultipleListeners tokens={$enabledNetworkTokens}>
-	<Activity />
+	<LoaderMultipleEthTransactions>
+		<Activity />
+	</LoaderMultipleEthTransactions>
 </MultipleListeners>
