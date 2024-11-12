413065d1574d686b5e3768bb821bf63f7cfcbe64
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-11-12 15:09:18 +0100

    feat(frontend): ConvertInputsContainer component (#3452)
    # Motivation
    
    Component that will be re-used for Source and Destination tokens inputs.
    
    
    <img width="406" alt="Screenshot 2024-11-11 at 21 28 25"
    src="https://github.com/user-attachments/assets/d607b5f5-cc2f-4529-8c74-14ea0d10f283">
    <img width="310" alt="Screenshot 2024-11-11 at 21 28 44"
    src="https://github.com/user-attachments/assets/17e0b748-18ef-464d-9b63-0231edd6e27b">

---
+++ src/frontend/src/lib/components/convert/ConvertInputsContainer.svelte
@@ -0,0 +1,18 @@
+<div class="flex flex-col items-start justify-between md:flex-row">
+	<!-- TODO: Update colors with values from theme-variables when they are available there -->
+	<div
+		class="mb-3 flex h-14 w-full items-center rounded-lg border border-bright-gray bg-ghost-white px-3 md:mb-0 md:w-[35%]"
+	>
+		<slot name="token-info" />
+	</div>
+	<div class="w-full md:w-[62%]">
+		<slot name="amount" />
+
+		<div class="mt-2 flex items-center justify-between">
+			<div class="text-aurometalsaurus"><slot name="amount-info" /></div>
+			<div class="text-sm font-semibold">
+				<slot name="balance" />
+			</div>
+		</div>
+	</div>
+</div>
