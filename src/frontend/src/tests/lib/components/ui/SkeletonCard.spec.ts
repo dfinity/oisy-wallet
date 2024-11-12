797c8662b9f98f9c85e2d114c58122f6cbbc1336
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-12 16:27:50 +0100

    test(frontend): tests for skeleton cards (#3462)
    # Motivation
    
    Small useful tests for Skeleton cards components.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/frontend/src/tests/lib/components/ui/SkeletonCard.spec.ts
@@ -0,0 +1,11 @@
+import SkeletonCard from '$lib/components/ui/SkeletonCard.svelte';
+import { render } from '@testing-library/svelte';
+
+describe('SkeletonCard', () => {
+	it('renders a Card component', () => {
+		const { getByTestId } = render(SkeletonCard, { props: { testId: 'skeleton-card' } });
+
+		const skeleton = getByTestId('skeleton-card');
+		expect(skeleton).toBeInTheDocument();
+	});
+});
