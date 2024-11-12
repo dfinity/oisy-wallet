31ab86423de1bd618c09dd261eb37189e6b0ddd8
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-12 15:19:06 +0100

    fix: correct test

--- src/frontend/src/tests/lib/components/ui/SkeletonCard.spec.ts
+++ src/frontend/src/tests/lib/components/ui/SkeletonCard.spec.ts
@@ -3,7 +3,7 @@ import { render } from '@testing-library/svelte';
 
 describe('SkeletonCard', () => {
 	it('renders a Card component', () => {
-		const { getByTestId } = render(SkeletonCard);
+		const { getByTestId } = render(SkeletonCard, { props: { testId: 'skeleton-card' } });
 
 		const skeleton = getByTestId('skeleton-card');
 		expect(skeleton).toBeInTheDocument();
--- src/frontend/src/tests/lib/components/ui/SkeletonCards.spec.ts
+++ src/frontend/src/tests/lib/components/ui/SkeletonCards.spec.ts
@@ -3,7 +3,9 @@ import { render } from '@testing-library/svelte';
 
 describe('SkeletonCards', () => {
 	it('renders a number of SkeletonCard components', () => {
-		const { getByTestId } = render(SkeletonCards, { props: { rows: 5 } });
+		const { getByTestId } = render(SkeletonCards, {
+			props: { rows: 5, testIdPrefix: 'skeleton-card' }
+		});
 
 		Array.from({ length: 5 }).forEach((_, i) => {
 			const skeleton = getByTestId(`skeleton-card-${i}`);
