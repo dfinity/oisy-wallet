a3ca1ac47185a7da1c6076ce8ef5ac68519e4b77
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-12 10:11:13 +0100

    refactor(frontend): rename Activity to AllTransactions (#3456)
    # Motivation
    
    For consistency, we rename component `Activity` to `AllTransactions`.

--- src/frontend/src/tests/lib/components/transactions/AllTransactions.spec.ts
+++ src/frontend/src/tests/lib/components/transactions/AllTransactions.spec.ts
@@ -1,11 +1,11 @@
-import Activity from '$lib/components/transactions/Activity.svelte';
+import AllTransactions from '$lib/components/transactions/AllTransactions.svelte';
 import en from '$tests/mocks/i18n.mock';
 import { assertNonNullish } from '@dfinity/utils';
 import { render } from '@testing-library/svelte';
 
 describe('Activity', () => {
 	it('renders the title', () => {
-		const { container } = render(Activity);
+		const { container } = render(AllTransactions);
 
 		const title: HTMLHeadingElement | null = container.querySelector('h1');
 
@@ -16,7 +16,7 @@ describe('Activity', () => {
 	});
 
 	it('renders the transactions list', () => {
-		const { getByText } = render(Activity);
+		const { getByText } = render(AllTransactions);
 
 		expect(getByText(en.transactions.text.transaction_history)).toBeInTheDocument();
 	});
