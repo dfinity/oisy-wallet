0b6d05bca21eae4a9ef61a6aa8bde339f2f5d9ad
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-12 10:51:39 +0100

    feat(frontend): util to map unified transactions list

--- src/frontend/src/lib/components/transactions/AllTransactionsList.svelte
+++ src/frontend/src/lib/components/transactions/AllTransactionsList.svelte
@@ -1,37 +1,17 @@
 <script lang="ts">
 	import { slide } from 'svelte/transition';
+	import { btcTransactionsStore } from '$btc/stores/btc-transactions.store';
 	import TransactionsPlaceholder from '$lib/components/transactions/TransactionsPlaceholder.svelte';
 	import { SLIDE_DURATION } from '$lib/constants/transition.constants';
 	import { enabledTokens } from '$lib/derived/tokens.derived';
 	import type { AllTransactionsUi } from '$lib/types/transaction';
-	import {
-		isNetworkIdBTCMainnet,
-		isNetworkIdEthereum,
-		isNetworkIdICP
-	} from '$lib/utils/network.utils';
+	import { mapAllTransactionsUi } from '$lib/utils/transactions.utils';
 
 	let transactions: AllTransactionsUi;
-	// TODO: extract the function to a separate util
-	$: transactions = $enabledTokens.reduce<AllTransactionsUi>(
-		(acc, { network: { id: networkId } }) => {
-			if (isNetworkIdBTCMainnet(networkId)) {
-				// TODO: Implement BTC transactions
-			}
-
-			if (isNetworkIdICP(networkId)) {
-				// TODO: Implement ICP transactions
-				return acc;
-			}
-
-			if (isNetworkIdEthereum(networkId)) {
-				// TODO: Implement Ethereum transactions
-				return acc;
-			}
-
-			return acc;
-		},
-		[]
-	);
+	$: transactions = mapAllTransactionsUi({
+		tokens: $enabledTokens,
+		$btcTransactions: $btcTransactionsStore
+	});
 </script>
 
 <!--TODO: include skeleton for loading transactions-->
@@ -39,7 +19,7 @@
 {#if transactions.length > 0}
 	{#each transactions as transaction, index (`${transaction.id}-${index}`)}
 		<li in:slide={SLIDE_DURATION}>
-			<svelte:component this={transaction.transactionComponent} {transaction} />
+			<svelte:component this={transaction.component} {transaction} />
 		</li>
 	{/each}
 {/if}
--- src/frontend/src/tests/lib/components/transactions/AllTransactionsList.spec.ts
+++ src/frontend/src/tests/lib/components/transactions/AllTransactionsList.spec.ts
@@ -1,28 +1,70 @@
-import { btcTransactionsStore } from '$btc/stores/btc-transactions.store';
-import {
-	BTC_MAINNET_TOKEN_ID,
-	BTC_REGTEST_TOKEN_ID,
-	BTC_TESTNET_TOKEN_ID
-} from '$env/tokens.btc.env';
-import { ethTransactionsStore } from '$eth/stores/eth-transactions.store';
+import BtcTransaction from '$btc/components/transactions/BtcTransaction.svelte';
 import AllTransactionsList from '$lib/components/transactions/AllTransactionsList.svelte';
+import { mapAllTransactionsUi } from '$lib/utils/transactions.utils';
+import { createMockBtcTransactionsUi } from '$tests/mocks/btc.mock';
 import en from '$tests/mocks/i18n.mock';
 import { render } from '@testing-library/svelte';
+import type { MockedFunction } from 'vitest';
+
+vi.mock('$lib/utils/transactions.utils', () => ({
+	mapAllTransactionsUi: vi.fn()
+}));
 
 describe('Activity', () => {
+	const mockedMapAllTransactionsUi = mapAllTransactionsUi as MockedFunction<
+		typeof mapAllTransactionsUi
+	>;
+
 	beforeEach(() => {
 		vi.clearAllMocks();
-
-		[BTC_MAINNET_TOKEN_ID, BTC_TESTNET_TOKEN_ID, BTC_REGTEST_TOKEN_ID].forEach((tokenId) => {
-			btcTransactionsStore.reset(tokenId);
-		});
-
-		ethTransactionsStore.reset();
 	});
 
-	it('renders the placeholder when the transactions stores are empty', () => {
-		const { getByText } = render(AllTransactionsList);
+	it('should call the function to map the transactions list', () => {
+		mockedMapAllTransactionsUi.mockReturnValue([]);
 
-		expect(getByText(en.transactions.text.transaction_history)).toBeInTheDocument();
+		render(AllTransactionsList);
+
+		expect(mapAllTransactionsUi).toHaveBeenCalled();
+	});
+
+	describe('when the transactions list is empty', () => {
+		beforeEach(() => {
+			mockedMapAllTransactionsUi.mockReturnValue([]);
+		});
+
+		it('should render the placeholder', () => {
+			mockedMapAllTransactionsUi.mockReturnValue([]);
+
+			const { getByText } = render(AllTransactionsList);
+
+			expect(getByText(en.transactions.text.transaction_history)).toBeInTheDocument();
+		});
+	});
+
+	describe('when the transactions list is not empty', () => {
+		const btcTransactionsNumber = 5;
+
+		beforeEach(() => {
+			mockedMapAllTransactionsUi.mockReturnValue(
+				createMockBtcTransactionsUi(btcTransactionsNumber).map((transaction) => ({
+					...transaction,
+					component: BtcTransaction
+				}))
+			);
+		});
+
+		it('should not render the placeholder', () => {
+			const { queryByText } = render(AllTransactionsList);
+
+			expect(queryByText(en.transactions.text.transaction_history)).not.toBeInTheDocument();
+		});
+
+		it('should render the transactions list', () => {
+			const { container } = render(AllTransactionsList);
+
+			const listItems = container.querySelectorAll('li');
+
+			expect(listItems).toHaveLength(btcTransactionsNumber);
+		});
 	});
 });
