4cafd4a4d27cf00ae2198f3ea7dbe25b00273e6a
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-11-13 10:27:34 +0100

    feat(frontend): ConvertToken component (#3476)
    # Motivation
    
    `xs`:
    <img width="75" alt="Screenshot 2024-11-12 at 18 29 00"
    src="https://github.com/user-attachments/assets/9725f466-3f85-4814-9c06-b3c9a06f5528">
    
    `md`:
    <img width="129" alt="Screenshot 2024-11-12 at 18 31 06"
    src="https://github.com/user-attachments/assets/99a786dc-ebd2-4d5a-9ad2-f70065e5701e">

---
+++ src/frontend/src/lib/components/convert/ConvertToken.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import TokenLogo from '$lib/components/tokens/TokenLogo.svelte';
+	import type { LogoSize } from '$lib/types/components';
+	import type { Token } from '$lib/types/token';
+
+	export let token: Token;
+	export let size: Extract<LogoSize, 'md' | 'xs'> = 'xs';
+</script>
+
+<div class="flex items-center">
+	<TokenLogo data={token} logoSize={size} />
+
+	<!-- TODO: Update colors with values from theme-variables when they are available there -->
+	<span class={`${size === 'md' ? 'text-3xl font-bold' : 'text-aurometalsaurus'} ml-2 `}
+		>{token.symbol}</span
+	>
+</div>
