36a8fc8ef96b5a40c2ad8ff13716c6a53efbf4bb
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2024-11-13 14:51:19 +0100

    feat(frontend): review adjustments

--- src/frontend/src/tests/lib/stores/convert.store.spec.ts
+++ src/frontend/src/tests/lib/stores/convert.store.spec.ts
@@ -1,12 +1,25 @@
 import { ETHEREUM_TOKEN, ICP_TOKEN } from '$env/tokens.env';
+import * as exchanges from '$lib/derived/exchange.derived';
+import { balancesStore } from '$lib/stores/balances.store';
 import { initConvertContext } from '$lib/stores/convert.store';
 import { mockPage } from '$tests/mocks/page.store.mock';
 import { testDerivedUpdates } from '$tests/utils/derived.utils';
-import { expect } from 'vitest';
+import { BigNumber } from 'alchemy-sdk';
+import { get, readable } from 'svelte/store';
+
+const ethExchangeValue = 1;
+const icpExchangeValue = 2;
 
 describe('convertStore', () => {
 	beforeEach(() => {
 		mockPage.reset();
+
+		vi.spyOn(exchanges, 'exchanges', 'get').mockImplementation(() =>
+			readable({
+				[ETHEREUM_TOKEN.id]: { usd: ethExchangeValue },
+				[ICP_TOKEN.id]: { usd: icpExchangeValue }
+			})
+		);
 	});
 
 	it('should ensure derived stores update at most once when the store changes', async () => {
@@ -18,17 +31,37 @@ describe('convertStore', () => {
 		);
 	});
 
-	it('should have all expected properties', () => {
-		const store = initConvertContext({
+	it('should have all expected values', () => {
+		const {
+			sourceToken,
+			sourceTokenBalance,
+			sourceTokenExchangeRate,
+			destinationTokenExchangeRate,
+			destinationTokenBalance,
+			destinationToken
+		} = initConvertContext({
 			destinationToken: ETHEREUM_TOKEN,
 			sourceToken: ICP_TOKEN
 		});
+		const ethBalance = BigNumber.from(1n);
+		const icpBalance = BigNumber.from(2n);
 
-		expect(store).toHaveProperty('sourceToken');
-		expect(store).toHaveProperty('destinationToken');
-		expect(store).toHaveProperty('sourceTokenBalance');
-		expect(store).toHaveProperty('destinationTokenBalance');
-		expect(store).toHaveProperty('sourceTokenExchangeRate');
-		expect(store).toHaveProperty('destinationTokenExchangeRate');
+		balancesStore.set({
+			tokenId: ETHEREUM_TOKEN.id,
+			data: { data: ethBalance, certified: true }
+		});
+		balancesStore.set({
+			tokenId: ICP_TOKEN.id,
+			data: { data: icpBalance, certified: true }
+		});
+
+		expect(get(sourceToken)).toBe(ICP_TOKEN);
+		expect(get(destinationToken)).toBe(ETHEREUM_TOKEN);
+
+		expect(get(sourceTokenBalance)).toStrictEqual(icpBalance);
+		expect(get(destinationTokenBalance)).toStrictEqual(ethBalance);
+
+		expect(get(sourceTokenExchangeRate)).toStrictEqual(icpExchangeValue);
+		expect(get(destinationTokenExchangeRate)).toStrictEqual(ethExchangeValue);
 	});
 });
