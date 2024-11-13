10a3fb28cd6c5b9d271da96b89d620e3e1828ae5
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2024-11-13 10:03:55 +0100

    feat(frontend): review adjustments

--- src/frontend/src/lib/components/convert/ConvertToken.svelte
+++ src/frontend/src/lib/components/convert/ConvertToken.svelte
@@ -1,20 +1,14 @@
 <script lang="ts">
-	import Logo from '$lib/components/ui/Logo.svelte';
-	import { i18n } from '$lib/stores/i18n.store.js';
+	import TokenLogo from '$lib/components/tokens/TokenLogo.svelte';
 	import type { LogoSize } from '$lib/types/components';
 	import type { Token } from '$lib/types/token';
-	import { replacePlaceholders } from '$lib/utils/i18n.utils.js';
 
 	export let token: Token;
 	export let size: Extract<LogoSize, 'md' | 'xs'> = 'xs';
 </script>
 
 <div class="flex items-center">
-	<Logo
-		src={token.icon}
-		alt={replacePlaceholders($i18n.core.alt.logo, { $name: token.name })}
-		{size}
-	/>
+	<TokenLogo data={token} logoSize={size} />
 
 	<!-- TODO: Update colors with values from theme-variables when they are available there -->
 	<span class={`${size === 'md' ? 'text-3xl font-bold' : 'text-aurometalsaurus'} ml-2 `}
