de85087df27d0ce3267cacfad8740046da7065d2
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-11-13 12:02:12 +0100

    feat(frontend): findTwinToken util (#3463)
    # Motivation
    
    The goal is to create a util for finding a twin token that can be reused
    in token-grouping.util as well as in the upcoming `convert.store`.

---
+++ src/frontend/src/lib/components/convert/ConvertAmountExchange.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { nonNullish } from '@dfinity/utils';
+	import { fade } from 'svelte/transition';
+	import SkeletonText from '$lib/components/ui/SkeletonText.svelte';
+	import { formatUSD } from '$lib/utils/format.utils';
+	export let amount: number | undefined = undefined;
+	export let exchangeRate: number | undefined = undefined;
+	let usdValue: string | undefined;
+	$: usdValue =
+		nonNullish(amount) && nonNullish(exchangeRate)
+			? formatUSD({
+					value: amount * exchangeRate
+				})
+			: undefined;
+</script>
+
+{#if nonNullish(usdValue)}
+	<div in:fade>
+		{`${nonNullish(amount) && amount === 0 ? '' : '~'}`}{usdValue}
+	</div>
+{:else}
+	<div class="w-10 sm:w-8">
+		<SkeletonText />
+	</div>
+{/if}
