656e843b5cf520f55ab405c6283fbb7ef517e2fb
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2024-11-13 13:04:24 +0100

    feat(frontend): review adjustments

--- src/frontend/src/tests/lib/components/convert/ConvertAmountExchange.spec.ts
+++ src/frontend/src/tests/lib/components/convert/ConvertAmountExchange.spec.ts
@@ -1,6 +1,4 @@
 import ConvertAmountExchange from '$lib/components/convert/ConvertAmountExchange.svelte';
-import { formatUSD } from '$lib/utils/format.utils';
-import { assertNonNullish } from '@dfinity/utils';
 import { render } from '@testing-library/svelte';
 
 describe('ConvertAmountExchange', () => {
@@ -12,56 +10,34 @@ describe('ConvertAmountExchange', () => {
 		exchangeRate
 	};
 
-	const divSelector = 'div[data-tid="convert-amount-exchange"]';
-	const skeletonSelector = 'div[data-tid="convert-amount-exchange-skeleton"]';
+	const divTestId = 'convert-amount-exchange';
+	const skeletonTestId = 'convert-amount-exchange-skeleton';
 
-	it('should display value with tilde sign if amount is provided', () => {
-		const { container, getByText } = render(ConvertAmountExchange, { props });
+	it('should display correct value if amount is provided', () => {
+		const { getByTestId } = render(ConvertAmountExchange, { props });
 
-		const div: HTMLDivElement | null = container.querySelector(divSelector);
-		assertNonNullish(div, 'Div not found');
-
-		expect(
-			getByText(
-				`~${formatUSD({
-					value: amount * exchangeRate
-				})}`
-			)
-		).toBeInTheDocument();
+		expect(getByTestId(divTestId)).toHaveTextContent('~$0.20');
 	});
 
-	it('should display value without tilde sign if amount is zero', () => {
-		const { container, getByText } = render(ConvertAmountExchange, {
+	it('should display correct value if amount is zero', () => {
+		const { getByTestId } = render(ConvertAmountExchange, {
 			props: { ...props, amount: 0 }
 		});
 
-		const div: HTMLDivElement | null = container.querySelector(divSelector);
-		assertNonNullish(div, 'Div not found');
-
-		expect(
-			getByText(
-				formatUSD({
-					value: 0
-				})
-			)
-		).toBeInTheDocument();
+		expect(getByTestId(divTestId)).toHaveTextContent('$0.00');
 	});
 
 	it('should display skeleton if amount is not provided', () => {
 		const { amount: _, ...newProps } = props;
-		const { container } = render(ConvertAmountExchange, { props: newProps });
+		const { getByTestId } = render(ConvertAmountExchange, { props: newProps });
 
-		const skeleton: HTMLDivElement | null = container.querySelector(skeletonSelector);
-
-		expect(skeleton).toBeInTheDocument();
+		expect(getByTestId(skeletonTestId)).toBeInTheDocument();
 	});
 
 	it('should display skeleton if exchange rate is not provided', () => {
 		const { exchangeRate: _, ...newProps } = props;
-		const { container } = render(ConvertAmountExchange, { props: newProps });
+		const { getByTestId } = render(ConvertAmountExchange, { props: newProps });
 
-		const skeleton: HTMLDivElement | null = container.querySelector(skeletonSelector);
-
-		expect(skeleton).toBeInTheDocument();
+		expect(getByTestId(skeletonTestId)).toBeInTheDocument();
 	});
 });
