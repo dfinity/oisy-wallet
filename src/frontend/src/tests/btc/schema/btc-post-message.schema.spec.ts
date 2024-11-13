a0c636e20db5c1ba53ec5bb0174fd9945f96e0c1
David Dal Busco <david.dalbusco@dfinity.org>
2024-11-13 17:29:42 +0100

    feat(frontend): btc post message strict (#3521)
    # Motivation
    
    BTC was using the same interfaces for post message as the ICP/ICRC
    tokens. Given that the index becomes optional for the later, it would
    introduce a potential nullability for BTC "newTransactions" which
    actually is incorrecty, given that we always fetch transactions for BTC.
    
    Long story short, we need a separate post message strict types for BTC.
    
    # Changes
    
    - Create schema and types for strict btc post message
    - Use those
    - Add todos for some existing types which should be reviewed

---
+++ src/frontend/src/tests/btc/schema/btc-post-message.schema.spec.ts
@@ -0,0 +1,51 @@
+import { BtcPostMessageDataResponseWalletSchema } from '$btc/schema/btc-post-message.schema';
+import type { CertifiedData } from '$lib/types/store';
+
+describe('btc-post-message.schema', () => {
+	describe('BtcPostMessageDataResponseWalletSchema', () => {
+		const mockValidBalance: CertifiedData<bigint> = {
+			data: 1000n,
+			certified: true
+		};
+		const mockValidTransactions = JSON.stringify([{ id: 'tx1', amount: 500 }]);
+
+		it('should validate with a valid balance and newTransactions', () => {
+			const validData = {
+				wallet: {
+					balance: mockValidBalance,
+					newTransactions: mockValidTransactions
+				}
+			};
+			expect(BtcPostMessageDataResponseWalletSchema.parse(validData)).toEqual(validData);
+		});
+
+		it('should fail if newTransactions is missing', () => {
+			const invalidData = {
+				wallet: {
+					balance: mockValidBalance
+				}
+			};
+			expect(() => BtcPostMessageDataResponseWalletSchema.parse(invalidData)).toThrow();
+		});
+
+		it('should validate if balance is not a bigint because of zod custom', () => {
+			const validData = {
+				wallet: {
+					balance: 'not_a_bigint',
+					newTransactions: mockValidTransactions
+				}
+			};
+			expect(BtcPostMessageDataResponseWalletSchema.parse(validData)).toEqual(validData);
+		});
+
+		it('should validate if newTransactions is not valid JSON because it accepts a string', () => {
+			const validData = {
+				wallet: {
+					balance: mockValidBalance,
+					newTransactions: 'invalid_json'
+				}
+			};
+			expect(BtcPostMessageDataResponseWalletSchema.parse(validData)).toEqual(validData);
+		});
+	});
+});
