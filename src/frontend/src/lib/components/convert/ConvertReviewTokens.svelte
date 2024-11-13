dab85eb3a9b1b3a7478f465256e61f2307a5df20
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2024-11-13 18:09:00 +0100

    feat(frontend): ConvertReviewTokens component

---
+++ src/frontend/src/lib/components/convert/ConvertReviewTokens.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import ConvertToken from '$lib/components/convert/ConvertToken.svelte';
+	import IconMoveRight from '$lib/components/icons/lucide/IconMoveRight.svelte';
+	import { CONVERT_CONTEXT_KEY, type ConvertContext } from '$lib/stores/convert.store';
+	import { i18n } from '$lib/stores/i18n.store.js';
+
+	const { sourceToken, destinationToken } = getContext<ConvertContext>(CONVERT_CONTEXT_KEY);
+</script>
+
+<div class="flex w-full flex-col items-center justify-center">
+	<p class="font-bold">
+		{$i18n.convert.text.review_tokens_info_title}
+	</p>
+	<div class="flex items-center">
+		<ConvertToken token={$sourceToken} size="md" />
+		<span class="mx-3"><IconMoveRight /></span>
+		<ConvertToken token={$destinationToken} size="md" />
+	</div>
+</div>
