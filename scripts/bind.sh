f3b43fb251c317b5e4a9ebb97e0dbdde608f4e2b
Max Murphy <max@winning.black>
2024-11-13 13:30:30 +0100

    Update text

--- scripts/bind.sh
+++ scripts/bind.sh
@@ -5,6 +5,8 @@
 	Generates candid files and bindings.
 
 	Prerequisites:
+	- Git is clean.  This is to ensure that the automated code changes are easy to audit.
+	  - Note: It is recommended also to have the code formatted before generating bindings.
 	- Deploy all canisters to the 'local' network.  Or otherwise ensure that:
 	  - Wasm for each canister is at: '.dfx/local/canisters/$CANISTER/$CANISTER.wasm.gz'
 	    Note: You may need to set '"gzip": true' for canisters in 'dfx.json'.
@@ -14,6 +16,11 @@
   exit 0
 }
 
+# Ensure that git is clean
+if git status --porcelain --untracked-files=no | grep -q .
+then echo "ERROR: Git is not clean.  Please commit all changes before automated code generation."
+	exit 1
+fi
 # Generate candid for the backend
 scripts/did.sh # TODO: Use local Wasm as input.
 # Generate rust bindings
