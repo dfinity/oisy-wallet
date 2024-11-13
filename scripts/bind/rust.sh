6004240e77ad525e220d8c09f2849954c3d6bab0
Max Murphy <max@winning.black>
2024-11-13 16:26:35 +0100

    ++

--- scripts/bind/rust.sh
+++ scripts/bind/rust.sh
@@ -26,8 +26,11 @@ else
 fi
 
 for canister in "${canisters[@]}"; do
+  # Paths:
   canister_binding_config="./scripts/bind/rust/${canister}.client.toml"
-  candid_file="".dfx/local/canisters/$canister/${canister}.did""
+  candid_file=".dfx/local/canisters/$canister/${canister}.did"
+  generated_client="src/$canister/client/src/lib.rs"
+  # Generate:
   if test -f "$canister_binding_config"; then
     echo "INFO: Creating rust bindings for $canister..."
     test -f "$candid_file" || {
@@ -36,7 +39,7 @@ for canister in "${canisters[@]}"; do
       exit 1
     } >&2
     mkdir -p "src/backend/src/bind"
-    didc bind -t rs ".dfx/local/canisters/$canister/$canister.did" --config "$canister_binding_config" >"src/$canister/client/src/lib.rs"
+    didc bind -t rs "$candid_file" --config "$canister_binding_config" >"$generated_client"
   else
     echo "INFO: No rust binding script for $canister at $canister_binding_config"
   fi
