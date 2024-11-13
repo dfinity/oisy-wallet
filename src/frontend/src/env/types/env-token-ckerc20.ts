726cf4749d43d760e8538025f910fd52b30194b7
David Dal Busco <david.dalbusco@outlook.com>
2024-11-13 13:56:45 +0100

    refactor(frontend): ckErc20 env token schema

--- src/frontend/src/env/types/env-token-ckerc20.ts
+++ src/frontend/src/env/types/env-token-ckerc20.ts
@@ -1,19 +1,4 @@
-import { EnvIcTokenSchema } from '$env/schema/env-icrc-token.schema';
-import { envTokenSymbol } from '$env/types/env-token-common';
-import { isEthAddress } from '$lib/utils/account.utils';
+import { EnvCkErc20TokensSchema } from '$env/schema/env-token-ckerc20.schema';
 import { z } from 'zod';
 
-const envErc20ContractAddress = z.custom<string>(isEthAddress, 'Invalid ERC20 Contract Address');
-
-const envCkErc20TokenData = EnvIcTokenSchema.extend({
-	erc20ContractAddress: envErc20ContractAddress
-});
-
-const envCkErc20Tokens = z.record(envTokenSymbol, z.union([z.undefined(), envCkErc20TokenData]));
-
-export type EnvCkErc20Tokens = z.infer<typeof envCkErc20Tokens>;
-
-export const envTokensCkErc20 = z.object({
-	production: envCkErc20Tokens,
-	staging: envCkErc20Tokens
-});
+export type EnvCkErc20Tokens = z.infer<typeof EnvCkErc20TokensSchema>;
