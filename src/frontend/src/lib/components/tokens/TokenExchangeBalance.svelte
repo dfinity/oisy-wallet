922e314055468108e4222b85fd565bb6e9fdfcb7
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2024-11-13 08:34:36 +0100

    removes used default value of variable

--- src/frontend/src/lib/components/tokens/TokenExchangeBalance.svelte
+++ src/frontend/src/lib/components/tokens/TokenExchangeBalance.svelte
@@ -6,14 +6,14 @@
 
 	export let balance: TokenFinancialData['balance'];
 	export let usdBalance: TokenFinancialData['usdBalance'];
-	export let nullishBalanceMessage = '-';
+	export let nullishBalanceMessage: string | undefined = undefined;
 </script>
 
 <output class="break-all">
 	{#if nonNullish(balance) && nonNullish(usdBalance)}
 		{formatUSD({ value: usdBalance })}
 	{:else if isNullish(balance)}
-		<span class:animate-pulse={isNullish(balance)}>{nullishBalanceMessage}</span>
+		<span class:animate-pulse={isNullish(balance)}>{nullishBalanceMessage ?? '-'}</span>
 	{:else}
 		<span class:animate-pulse={isNullish(balance)}>{$i18n.tokens.balance.error.not_applicable}</span
 		>
