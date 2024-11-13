7ca523722bacfb31afc4741a5ae4a1805d5bd4d6
David Dal Busco <david.dalbusco@dfinity.org>
2024-11-13 13:40:55 +0100

    refactor(frontend): rename icrc env tokens specific for sns (#3514)
    # Motivation
    
    On one hand, we will extend the environment with some "additional icrc
    tokens" #3506, on the other, existing "sns env tokens" are not solely
    "icrc tokens" because they contains addition information such as the
    root canister.
    
    To clean up a bit the naming and data inconsistency, this PR move and
    rename the sns related env types to a particular module and also add the
    "Schema" suffix we have started to use for our zod schema.
    
    # Tests
    
    Skipped.

--- src/frontend/src/icp/types/icrc-custom-token.ts
+++ src/frontend/src/icp/types/icrc-custom-token.ts
@@ -1,10 +1,11 @@
-import type { EnvIcrcToken, EnvIcrcTokenMetadata } from '$env/types/env-icrc-token';
+import type { EnvIcrcTokenMetadata } from '$env/types/env-icrc-token';
+import type { EnvSnsToken } from '$env/types/env-sns-token';
 import type { IcToken, IcTokenWithoutId } from '$icp/types/ic-token';
 import type { TokenToggleable, UserTokenState } from '$lib/types/token-toggleable';
 import type { Option } from '$lib/types/utils';
 
 export type IcrcCustomTokenExtra = Pick<EnvIcrcTokenMetadata, 'alternativeName'> &
-	Partial<Pick<EnvIcrcToken, 'indexCanisterVersion'>>;
+	Partial<Pick<EnvSnsToken, 'indexCanisterVersion'>>;
 
 export type IcTokenWithoutIdExtended = IcTokenWithoutId & IcrcCustomTokenExtra;
 
