67026df20bc6b03afe44c8fc13c7647aee1c6cb0
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-11-13 13:08:50 +0100

    feat(frontend): ConvertAmountExchange component (#3478)
    # Motivation
    
    `ConvertAmountExchange` component in action (when some of the values is
    undefined, just a regular skeleton will be shown):
    
    <img width="78" alt="Screenshot 2024-11-12 at 18 40 11"
    src="https://github.com/user-attachments/assets/574afa51-f5da-4bd4-9e85-d3131829c1bd">

--- src/frontend/src/lib/components/convert/ConvertAmountExchange.svelte
+++ src/frontend/src/lib/components/convert/ConvertAmountExchange.svelte
@@ -3,8 +3,10 @@
 	import { fade } from 'svelte/transition';
 	import SkeletonText from '$lib/components/ui/SkeletonText.svelte';
 	import { formatUSD } from '$lib/utils/format.utils';
+
 	export let amount: number | undefined = undefined;
 	export let exchangeRate: number | undefined = undefined;
+
 	let usdValue: string | undefined;
 	$: usdValue =
 		nonNullish(amount) && nonNullish(exchangeRate)
@@ -15,11 +17,11 @@
 </script>
 
 {#if nonNullish(usdValue)}
-	<div in:fade>
+	<div in:fade data-tid="convert-amount-exchange">
 		{`${nonNullish(amount) && amount === 0 ? '' : '~'}`}{usdValue}
 	</div>
 {:else}
-	<div class="w-10 sm:w-8">
+	<div class="w-10 sm:w-8" data-tid="convert-amount-exchange-skeleton">
 		<SkeletonText />
 	</div>
 {/if}
