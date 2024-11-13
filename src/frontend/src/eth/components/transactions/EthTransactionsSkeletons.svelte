f6854453445ef78e6e59991438a303501fc8a95d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-13 07:40:15 +0100

    feat(frontend): common component for transactions skeleton (#3459)
    # Motivation
    
    We create a common component to show the skeleton of transactions and we
    use in the code.
    
    Note that the component is identical to `TokensSkeleton`, but I can
    create a generic Skeleton guard in another PR if needed, since in this
    one I just want to normalize the usage in all network transactions.
    
    # Changes
    
    - Extract component for skeleton from `IcTransactionsSkeletons` and
    `EthTransactionsSkeletons`.
    - Invert login of loading for `IcTransactionsSkeletons` to provide the
    correct prop to the new component.
    - Use the new component in `BtcTransactions` too.
    
    # Tests
    
    Some component tests.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/eth/components/transactions/EthTransactionsSkeletons.svelte
+++ src/frontend/src/eth/components/transactions/EthTransactionsSkeletons.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
-	import { fade } from 'svelte/transition';
 	import { erc20UserTokensNotInitialized } from '$eth/derived/erc20.derived';
 	import { ethTransactionsNotInitialized } from '$eth/derived/eth-transactions.derived';
 	import { tokenNotInitialized } from '$eth/derived/nav.derived';
-	import SkeletonCards from '$lib/components/ui/SkeletonCards.svelte';
+	import TransactionsSkeletons from '$lib/components/transactions/TransactionsSkeletons.svelte';
+
+	let loading: boolean;
+	$: loading =
+		($erc20UserTokensNotInitialized && $tokenNotInitialized) || $ethTransactionsNotInitialized;
 </script>
 
-{#if ($erc20UserTokensNotInitialized && $tokenNotInitialized) || $ethTransactionsNotInitialized}
-	<SkeletonCards rows={5} />
-{:else}
-	<div in:fade>
-		<slot />
-	</div>
-{/if}
+<TransactionsSkeletons {loading}>
+	<slot />
+</TransactionsSkeletons>
--- src/frontend/src/icp/components/transactions/IcTransactionsSkeletons.svelte
+++ src/frontend/src/icp/components/transactions/IcTransactionsSkeletons.svelte
@@ -1,18 +1,13 @@
 <script lang="ts">
-	import { nonNullish } from '@dfinity/utils';
-	import { fade } from 'svelte/transition';
+	import { isNullish } from '@dfinity/utils';
 	import { icTransactionsStore } from '$icp/stores/ic-transactions.store';
-	import SkeletonCards from '$lib/components/ui/SkeletonCards.svelte';
+	import TransactionsSkeletons from '$lib/components/transactions/TransactionsSkeletons.svelte';
 	import { token } from '$lib/stores/token.store';
 
-	let transactionsInitialized: boolean;
-	$: transactionsInitialized = nonNullish($token) && nonNullish($icTransactionsStore?.[$token.id]);
+	let loading: boolean;
+	$: loading = isNullish($token) || isNullish($icTransactionsStore?.[$token.id]);
 </script>
 
-{#if !transactionsInitialized}
-	<SkeletonCards rows={5} />
-{:else}
-	<div in:fade>
-		<slot />
-	</div>
-{/if}
+<TransactionsSkeletons {loading}>
+	<slot />
+</TransactionsSkeletons>
