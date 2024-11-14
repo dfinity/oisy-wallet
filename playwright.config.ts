128591b36c8b681c72acce473e47ac538e453525
Nico Mächler <135843237+inc-man@users.noreply.github.com>
2024-11-14 14:32:58 +0100

    adjust timeout for testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ dotenv.populate(
 const DEV = (process.env.NODE_ENV ?? 'production') === 'development';
 
 export default defineConfig({
-	timeout: 60 * 1000,
+	timeout: 300 * 1000,
 	workers: 2,
 	webServer: {
 		command: DEV ? 'npm run dev' : 'npm run build && npm run preview',
@@ -31,8 +31,8 @@ export default defineConfig({
 	use: {
 		testIdAttribute: 'data-tid',
 		trace: 'on',
-		actionTimeout: 60 * 1000,
-		navigationTimeout: 60 * 1000,
+		actionTimeout: 300 * 1000,
+		navigationTimeout: 300 * 1000,
 		...(DEV && { headless: false })
 	},
 	projects: [
