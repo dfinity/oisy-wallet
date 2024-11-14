f2aa9ded854069bf74af61667c51b8e5a0499fd8
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-11-14 14:20:09 +0100

    feat(frontend): ConvertReviewTokens component (#3554)
    # Motivation
    
    Desktop:
    <img width="510" alt="Screenshot 2024-11-14 at 12 13 00"
    src="https://github.com/user-attachments/assets/ed9bcbf0-80c0-4233-951f-a01636e03f2f">
    
    Mobile:
    <img width="252" alt="Screenshot 2024-11-14 at 12 16 13"
    src="https://github.com/user-attachments/assets/5cccc6b7-640e-4121-915a-85498c1f3861">

---
+++ src/frontend/src/lib/components/convert/ConvertReviewTokens.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import ConvertToken from '$lib/components/convert/ConvertToken.svelte';
+	import IconMoveDown from '$lib/components/icons/lucide/IconMoveDown.svelte';
+	import IconMoveRight from '$lib/components/icons/lucide/IconMoveRight.svelte';
+	import { CONVERT_CONTEXT_KEY, type ConvertContext } from '$lib/stores/convert.store';
+	import { i18n } from '$lib/stores/i18n.store.js';
+
+	const { sourceToken, destinationToken } = getContext<ConvertContext>(CONVERT_CONTEXT_KEY);
+</script>
+
+<div class="flex w-full flex-col items-center justify-center">
+	<p class="font-bold">
+		{$i18n.convert.text.review_tokens_info_title}
+	</p>
+	<div class="flex flex-col items-center sm:flex-row">
+		<ConvertToken token={$sourceToken} size="md" />
+
+		<span class="mx-3 hidden sm:block"><IconMoveRight /></span>
+		<span class="my-3 block sm:hidden"><IconMoveDown /></span>
+
+		<ConvertToken token={$destinationToken} size="md" />
+	</div>
+</div>
