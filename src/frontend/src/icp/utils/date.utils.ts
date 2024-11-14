9faf4016dd62dbba0844ff54d4cf42cb249737a1
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-14 17:07:45 +0100

    feat(frontend): util to normalize a timestamp to seconds (#3562)
    # Motivation
    
    Since we are going to re-use the logic, we create a small util that
    normalize any milliseconds or nanoseconds timestamp to seconds.

--- src/frontend/src/icp/utils/date.utils.ts
+++ src/frontend/src/icp/utils/date.utils.ts
@@ -1,1 +1,13 @@
+import { MILLISECONDS_IN_SECOND, NANO_SECONDS_IN_SECOND } from '$lib/constants/app.constants';
+
 export const nowInBigIntNanoSeconds = (): bigint => BigInt(Date.now()) * BigInt(1e6);
+
+export const normalizeTimestampToSeconds = (timestamp: number | bigint): number => {
+	const ts = Number(timestamp);
+
+	return ts > 1e15
+		? ts / Number(NANO_SECONDS_IN_SECOND)
+		: ts > 1e12
+			? ts / Number(MILLISECONDS_IN_SECOND)
+			: ts;
+};
