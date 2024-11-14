5436c708d9a29ff52c5ed9c8438d562e7a3a7529
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-14 09:18:10 +0100

    feat(frontend): util to map unified transactions list (#3458)
    # Motivation
    
    Following up PR #3449 , we implement the util to create the list of
    unified transactions, starting with BTC transactions.
    
    # Changes
    
    - Move the util from inside the `AllTransactionsList` component to a
    specific module.
    - Use the existing transaction store for BTC and map their component.
    
    # Tests
    
    Created some additional tests for both the util and the component. Here
    are the result loading a BTC address that has a transaction. Please note
    that the UI will be modified later.
    
    
    ![Screenshot 2024-11-12 at 15 24
    56](https://github.com/user-attachments/assets/bc7cb738-cb2e-4def-93e7-b82f7fd7c663)
    ![Screenshot 2024-11-12 at 15 25
    03](https://github.com/user-attachments/assets/b396054b-5b90-4201-8a1f-523acd716169)

--- src/frontend/src/tests/lib/components/transactions/AllTransactionsList.spec.ts
+++ src/frontend/src/tests/lib/components/transactions/AllTransactionsList.spec.ts
@@ -1,28 +1,62 @@
 import { btcTransactionsStore } from '$btc/stores/btc-transactions.store';
-import {
-	BTC_MAINNET_TOKEN_ID,
-	BTC_REGTEST_TOKEN_ID,
-	BTC_TESTNET_TOKEN_ID
-} from '$env/tokens.btc.env';
-import { ethTransactionsStore } from '$eth/stores/eth-transactions.store';
+import * as btcEnv from '$env/networks.btc.env';
+import { BTC_MAINNET_TOKEN_ID } from '$env/tokens.btc.env';
 import AllTransactionsList from '$lib/components/transactions/AllTransactionsList.svelte';
+import * as transactionsUtils from '$lib/utils/transactions.utils';
+import { createMockBtcTransactionsUi } from '$tests/mocks/btc.mock';
 import en from '$tests/mocks/i18n.mock';
 import { render } from '@testing-library/svelte';
 
-describe('Activity', () => {
-	beforeEach(() => {
-		vi.clearAllMocks();
+describe('AllTransactionsList', () => {
+	it('should call the function to map the transactions list', () => {
+		const spyMapAllTransactionsUi = vi.spyOn(transactionsUtils, 'mapAllTransactionsUi');
 
-		[BTC_MAINNET_TOKEN_ID, BTC_TESTNET_TOKEN_ID, BTC_REGTEST_TOKEN_ID].forEach((tokenId) => {
-			btcTransactionsStore.reset(tokenId);
-		});
+		render(AllTransactionsList);
 
-		ethTransactionsStore.reset();
+		expect(spyMapAllTransactionsUi).toHaveBeenCalled();
+
+		spyMapAllTransactionsUi.mockRestore();
 	});
 
-	it('renders the placeholder when the transactions stores are empty', () => {
-		const { getByText } = render(AllTransactionsList);
+	describe('when the transactions list is empty', () => {
+		it('should render the placeholder', () => {
+			const { getByText } = render(AllTransactionsList);
 
-		expect(getByText(en.transactions.text.transaction_history)).toBeInTheDocument();
+			expect(getByText(en.transactions.text.transaction_history)).toBeInTheDocument();
+		});
+	});
+
+	describe('when the transactions list is not empty', () => {
+		const btcTransactionsNumber = 5;
+
+		beforeEach(() => {
+			vi.resetAllMocks();
+
+			vi.spyOn(btcEnv, 'BTC_MAINNET_ENABLED', 'get').mockImplementation(() => true);
+		});
+
+		btcTransactionsStore.append({
+			tokenId: BTC_MAINNET_TOKEN_ID,
+			transactions: createMockBtcTransactionsUi(btcTransactionsNumber).map((transaction) => ({
+				data: transaction,
+				certified: false
+			}))
+		});
+
+		it('should not render the placeholder', () => {
+			const { queryByText } = render(AllTransactionsList);
+
+			expect(queryByText(en.transactions.text.transaction_history)).not.toBeInTheDocument();
+		});
+
+		it('should render the transactions list', () => {
+			const { container } = render(AllTransactionsList);
+
+			const transactionComponents = Array.from(container.querySelectorAll('div')).filter(
+				(el) => el.parentElement === container
+			);
+
+			expect(transactionComponents).toHaveLength(btcTransactionsNumber);
+		});
 	});
 });
