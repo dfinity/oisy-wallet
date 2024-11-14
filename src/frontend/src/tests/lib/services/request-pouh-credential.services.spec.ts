e7f45aecfcb99324689840cca570942791253468
Llorenç <llorenc.muntaner@dfinity.org>
2024-11-14 16:36:06 +0100

    Stub global

--- src/frontend/src/tests/lib/services/request-pouh-credential.services.spec.ts
+++ src/frontend/src/tests/lib/services/request-pouh-credential.services.spec.ts
@@ -45,9 +45,13 @@ describe('request-pouh-credential.services', () => {
 		beforeEach(() => {
 			vi.clearAllMocks();
 			vi.resetAllMocks();
-			// vi.resetModules();
-			// vi.unstubAllEnvs();
-			vi.stubEnv('VITE_LOCAL_POUH_ISSUER_CANISTER_ID', 'qbw6f-caaaa-aaaah-qdcwa-cai');
+			vi.resetModules();
+			vi.unstubAllEnvs();
+			// vi.stubEnv('VITE_LOCAL_POUH_ISSUER_CANISTER_ID', 'qbw6f-caaaa-aaaah-qdcwa-cai');
+			vi.stubGlobal(
+				'import.meta.env.VITE_LOCAL_POUH_ISSUER_CANISTER_ID',
+				'qbw6f-caaaa-aaaah-qdcwa-cai'
+			);
 			toastsStore.reset();
 			userProfileStore.reset();
 			userProfileStore.set({ certified: true, profile: initialUserProfile });
