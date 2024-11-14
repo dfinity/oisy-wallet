52833626252a3587f4f7477e97fea441e196a680
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-14 17:24:17 +0100

    feat(frontend): add Ethereum transactions to unified list (#3547)
    # Motivation
    
    Following PR #3458 , we include Ethereum transactions in the unified
    list. It simply uses the same logic of the BTC transactions.
    
    # Note
    
    A few notes:
    - For now, we include Sepolia, because it is easy for testing; I
    included a TODO to filter after the feature is complete.
    - We do not test `mapEthTransactionUi` (it is tested elsewhere), so we
    provide undefined props for it. This function is used mostly for
    defining the ID and the transaction type, that is not really relevant
    for the tests of this PR.
    - We still don't treat sorting and grouping in this PR, we are just
    focusing on unifing.
    
    # Changes
    
    - Create util to verify Sepolia networks.
    - Add ETH transactions store in mapping.
    
    # Tests
    
    I included more tests, both for the util and the component.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/utils/transactions.utils.ts
+++ src/frontend/src/lib/utils/transactions.utils.ts
@@ -1,13 +1,22 @@
 import BtcTransaction from '$btc/components/transactions/BtcTransaction.svelte';
 import type { BtcTransactionUi } from '$btc/types/btc';
+import { ETHEREUM_NETWORK_ID, SEPOLIA_NETWORK_ID } from '$env/networks.env';
+import { ETHEREUM_TOKEN_ID, SEPOLIA_TOKEN_ID } from '$env/tokens.env';
+import EthTransaction from '$eth/components/transactions/EthTransaction.svelte';
+import type { EthTransactionsData } from '$eth/stores/eth-transactions.store';
+import { mapEthTransactionUi } from '$eth/utils/transactions.utils';
+import type { CkEthMinterInfoData } from '$icp-eth/stores/cketh.store';
+import { toCkMinterInfoAddresses } from '$icp-eth/utils/cketh.utils';
 import type { CertifiedStoreData } from '$lib/stores/certified.store';
 import type { TransactionsData } from '$lib/stores/transactions.store';
+import type { OptionEthAddress } from '$lib/types/address';
 import type { Token } from '$lib/types/token';
 import type { AllTransactionsUi } from '$lib/types/transaction';
 import {
 	isNetworkIdBTCMainnet,
 	isNetworkIdEthereum,
-	isNetworkIdICP
+	isNetworkIdICP,
+	isNetworkIdSepolia
 } from '$lib/utils/network.utils';
 import { isNullish } from '@dfinity/utils';
 
@@ -15,16 +24,35 @@ import { isNullish } from '@dfinity/utils';
  * Maps the transactions stores to a unified list of transactions with their respective components.
  *
  * @param tokens - The tokens to map the transactions for.
- * @param $btcTransactions - The BTC transactions store.
+ * @param $btcTransactions - The BTC transactions store data.
+ * @param $ethTransactions - The ETH transactions store data.
+ * @param $ckEthMinterInfo - The CK Ethereum minter info store data.
+ * @param $ethAddress - The ETH address of the user.
  */
 export const mapAllTransactionsUi = ({
 	tokens,
-	$btcTransactions
+	$btcTransactions,
+	$ethTransactions,
+	$ckEthMinterInfo,
+	$ethAddress
 }: {
 	tokens: Token[];
 	$btcTransactions: CertifiedStoreData<TransactionsData<BtcTransactionUi>>;
-}): AllTransactionsUi =>
-	tokens.reduce<AllTransactionsUi>((acc, { id: tokenId, network: { id: networkId } }) => {
+	$ethTransactions: EthTransactionsData;
+	$ckEthMinterInfo: CertifiedStoreData<CkEthMinterInfoData>;
+	$ethAddress: OptionEthAddress;
+}): AllTransactionsUi => {
+	const ckEthMinterInfoAddressesMainnet = toCkMinterInfoAddresses({
+		minterInfo: $ckEthMinterInfo?.[ETHEREUM_TOKEN_ID],
+		networkId: ETHEREUM_NETWORK_ID
+	});
+
+	const ckEthMinterInfoAddressesSepolia = toCkMinterInfoAddresses({
+		minterInfo: $ckEthMinterInfo?.[SEPOLIA_TOKEN_ID],
+		networkId: SEPOLIA_NETWORK_ID
+	});
+
+	return tokens.reduce<AllTransactionsUi>((acc, { id: tokenId, network: { id: networkId } }) => {
 		if (isNetworkIdBTCMainnet(networkId)) {
 			if (isNullish($btcTransactions)) {
 				return acc;
@@ -40,8 +68,22 @@ export const mapAllTransactionsUi = ({
 		}
 
 		if (isNetworkIdEthereum(networkId)) {
-			// TODO: Implement Ethereum transactions
-			return acc;
+			// TODO: remove Sepolia transactions when the feature is complete; for now we use it for testing
+			const isSepoliaNetwork = isNetworkIdSepolia(networkId);
+
+			return [
+				...acc,
+				...($ethTransactions[tokenId] ?? []).map((transaction) => ({
+					...mapEthTransactionUi({
+						transaction,
+						ckMinterInfoAddresses: isSepoliaNetwork
+							? ckEthMinterInfoAddressesSepolia
+							: ckEthMinterInfoAddressesMainnet,
+						$ethAddress: $ethAddress
+					}),
+					component: EthTransaction
+				}))
+			];
 		}
 
 		if (isNetworkIdICP(networkId)) {
@@ -51,3 +93,4 @@ export const mapAllTransactionsUi = ({
 
 		return acc;
 	}, []);
+};
