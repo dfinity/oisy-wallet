82da98c8386a48169d64e94e8320f0d1187790af
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-14 11:02:13 +0100

    feat(frontend): add info box in activity page

--- src/frontend/src/lib/components/transactions/AllTransactions.svelte
+++ src/frontend/src/lib/components/transactions/AllTransactions.svelte
@@ -2,8 +2,15 @@
 	import AllTransactionsList from '$lib/components/transactions/AllTransactionsList.svelte';
 	import PageTitle from '$lib/components/ui/PageTitle.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
+	import MessageBox from '$lib/components/ui/MessageBox.svelte';
 </script>
 
+<div class="flex flex-col gap-5">
 <PageTitle>{$i18n.activity.text.title}</PageTitle>
 
+<MessageBox level="plain">
+	{$i18n.activity.info.btc_transactions}
+</MessageBox>
+
 <AllTransactionsList />
+</div>
