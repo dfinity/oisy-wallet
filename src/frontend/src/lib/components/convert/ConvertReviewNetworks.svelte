532cef4d3d3defbda78b9da064b0b17e893e2c10
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-11-14 14:22:09 +0100

    feat(frontend): ConvertReviewNetworks component (#3555)
    # Motivation
    
    Desktop:
    
    <img width="541" alt="Screenshot 2024-11-14 at 12 23 46"
    src="https://github.com/user-attachments/assets/b6dbb05e-7628-478d-a7fd-c723122ecb16">
    
    Mobile:
    <img width="385" alt="Screenshot 2024-11-14 at 12 23 53"
    src="https://github.com/user-attachments/assets/022347e0-8621-48aa-acd3-39ab1403d114">

---
+++ src/frontend/src/lib/components/convert/ConvertReviewNetworks.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import ConvertNetwork from '$lib/components/convert/ConvertNetwork.svelte';
+	import { CONVERT_CONTEXT_KEY, type ConvertContext } from '$lib/stores/convert.store';
+	import { i18n } from '$lib/stores/i18n.store.js';
+
+	const { sourceToken, destinationToken } = getContext<ConvertContext>(CONVERT_CONTEXT_KEY);
+</script>
+
+<ConvertNetwork token={$sourceToken}>
+	<svelte:fragment slot="label">{$i18n.convert.text.source_network}</svelte:fragment
+	></ConvertNetwork
+>
+
+<ConvertNetwork token={$destinationToken}>
+	<svelte:fragment slot="label">{$i18n.convert.text.destination_network}</svelte:fragment
+	></ConvertNetwork
+>
