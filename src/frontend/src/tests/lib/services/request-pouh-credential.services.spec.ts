dd5fa57b36070ee1d38548e1cde1081ab8297e51
Llorenç <llorenc.muntaner@dfinity.org>
2024-11-14 16:48:37 +0100

    change stub name

--- src/frontend/src/tests/lib/services/request-pouh-credential.services.spec.ts
+++ src/frontend/src/tests/lib/services/request-pouh-credential.services.spec.ts
@@ -1,7 +1,7 @@
 import type { UserProfile } from '$declarations/backend/backend.did';
 import { nowInBigIntNanoSeconds } from '$icp/utils/date.utils';
 import * as api from '$lib/api/backend.api';
-// import { POUH_ISSUER_CANISTER_ID } from '$lib/constants/app.constants';
+import { POUH_ISSUER_CANISTER_ID } from '$lib/constants/app.constants';
 import { POUH_CREDENTIAL_TYPE } from '$lib/constants/credentials.constants';
 import { requestPouhCredential } from '$lib/services/request-pouh-credential.services';
 import { i18n } from '$lib/stores/i18n.store';
@@ -47,7 +47,7 @@ describe('request-pouh-credential.services', () => {
 			vi.resetAllMocks();
 			vi.resetModules();
 			vi.unstubAllEnvs();
-			vi.stubEnv('VITE_LOCAL_POUH_ISSUER_CANISTER_ID', 'qbw6f-caaaa-aaaah-qdcwa-cai');
+			vi.stubEnv('import.env.VITE_LOCAL_POUH_ISSUER_CANISTER_ID', 'qbw6f-caaaa-aaaah-qdcwa-cai');
 			toastsStore.reset();
 			userProfileStore.reset();
 			userProfileStore.set({ certified: true, profile: initialUserProfile });
@@ -82,7 +82,7 @@ describe('request-pouh-credential.services', () => {
 					credential_type: POUH_CREDENTIAL_TYPE,
 					arguments: []
 				},
-				issuerCanisterId: Principal.fromText('aaaaa-aa'),
+				issuerCanisterId: Principal.fromText(POUH_ISSUER_CANISTER_ID),
 				currentUserVersion: undefined,
 				nullishIdentityErrorMessage: get(i18n).auth.error.no_internet_identity
 			});
@@ -107,7 +107,7 @@ describe('request-pouh-credential.services', () => {
 					credential_type: POUH_CREDENTIAL_TYPE,
 					arguments: []
 				},
-				issuerCanisterId: Principal.fromText('aaaaa-aa'),
+				issuerCanisterId: Principal.fromText(POUH_ISSUER_CANISTER_ID),
 				currentUserVersion: undefined,
 				nullishIdentityErrorMessage: get(i18n).auth.error.no_internet_identity
 			});
