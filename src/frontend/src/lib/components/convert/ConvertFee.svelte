81176c8f6080c674c8f1fb06abaccc31d386759b
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2024-11-14 14:44:41 +0100

    feat(frontend): ConvertFee component

---
+++ src/frontend/src/lib/components/convert/ConvertFee.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { nonNullish } from '@dfinity/utils';
+	import { BigNumber } from '@ethersproject/bignumber';
+	import ConvertAmountDisplay from '$lib/components/convert/ConvertAmountDisplay.svelte';
+	import { formatToken } from '$lib/utils/format.utils';
+
+	export let feeAmount: bigint | undefined = undefined;
+	export let symbol: string;
+	export let decimals: number;
+	export let exchangeRate: number | undefined = undefined;
+	export let zeroAmountLabel: string | undefined = undefined;
+
+	let formattedFeeAmount: number | undefined;
+	$: formattedFeeAmount = nonNullish(feeAmount)
+		? Number(
+				formatToken({
+					value: BigNumber.from(feeAmount),
+					unitName: decimals,
+					displayDecimals: decimals
+				})
+			)
+		: undefined;
+</script>
+
+<ConvertAmountDisplay amount={formattedFeeAmount} {exchangeRate} {symbol} {zeroAmountLabel}
+	><slot slot="label" name="label" />
+</ConvertAmountDisplay>
