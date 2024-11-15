925a5d1ab168829d4cdf24e82b5033c124fb1907
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-16 00:15:45 +0100

    lint: remove imports

--- src/frontend/src/lib/components/tokens/TokenSymbol.svelte
+++ src/frontend/src/lib/components/tokens/TokenSymbol.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
 	import { nonNullish } from '@dfinity/utils';
 	import NetworkLogo from '$lib/components/networks/NetworkLogo.svelte';
-	import Logo from '$lib/components/ui/Logo.svelte';
-	import { i18n } from '$lib/stores/i18n.store';
 	import type { CardData } from '$lib/types/token-card';
-	import { replacePlaceholders } from '$lib/utils/i18n.utils';
 
 	export let data: CardData;
 	export let hideNetworkLogo = false;
