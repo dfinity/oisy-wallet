d2227870b796fa0a1e072f076985bbbac45ee723
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-15 13:18:47 +0100

    use random crypto id

--- src/frontend/src/tests/mocks/ic-transactions.mock.ts
+++ src/frontend/src/tests/mocks/ic-transactions.mock.ts
@@ -2,7 +2,7 @@ import type { IcTransactionUi } from '$icp/types/ic-transaction';
 
 export const createMockIcTransactionsUi = (n: number): IcTransactionUi[] =>
 	Array.from({ length: n }, () => ({
-		id: Math.random().toString(36).substring(7),
+		id: crypto.randomUUID(),
 		type: 'send',
 		status: 'executed'
 	}));
