9a984ed7dc917e86b1289c6dcda99d334a3ac800
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2024-11-15 08:37:57 +0100

    updates styling

--- src/frontend/src/lib/components/tokens/TokenExchangeBalance.svelte
+++ src/frontend/src/lib/components/tokens/TokenExchangeBalance.svelte
@@ -13,7 +13,7 @@
 	{#if nonNullish(balance) && nonNullish(usdBalance)}
 		{formatUSD({ value: usdBalance })}
 	{:else if isNullish(balance)}
-		<span class:animate-pulse={isNullish(balance)}>{nullishBalanceMessage ?? '-'}</span>
+		<span class="animate-pulse">{nullishBalanceMessage ?? '-'}</span>
 	{:else}
 		<span class:animate-pulse={isNullish(balance)}>{$i18n.tokens.balance.error.not_applicable}</span
 		>
