ea8491c13cf26fff57826088cd0c5c3721a8b236
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-15 15:56:38 +0100

    fix(frontend): remove circular reference in tokens env file (#3590)
    # Motivation
    
    We move `SUPPORTED_BITCOIN_TOKENS` since it was giving circular
    reference on initiation.

--- src/frontend/src/lib/constants/tokens.constants.ts
+++ src/frontend/src/lib/constants/tokens.constants.ts
@@ -1,6 +1,16 @@
-import { SUPPORTED_BITCOIN_TOKENS } from '$env/tokens.btc.env';
+import { BTC_MAINNET_ENABLED } from '$env/networks.btc.env';
+import { BTC_MAINNET_TOKEN, BTC_REGTEST_TOKEN, BTC_TESTNET_TOKEN } from '$env/tokens.btc.env';
 import { SUPPORTED_ETHEREUM_TOKENS } from '$env/tokens.env';
+import type { Token } from '$lib/types/token';
 
 export const [DEFAULT_ETHEREUM_TOKEN, _rest] = SUPPORTED_ETHEREUM_TOKENS;
+
+// The following tokens are used as fallback for any Bitcoin token defined in the token store.
+// That means that the order of the tokens in the array is important, to have a correct fallback chain.
+export const SUPPORTED_BITCOIN_TOKENS: [...Token[], Token] = [
+	...(BTC_MAINNET_ENABLED ? [BTC_MAINNET_TOKEN] : []),
+	BTC_TESTNET_TOKEN,
+	BTC_REGTEST_TOKEN
+];
 
 export const [DEFAULT_BITCOIN_TOKEN, _] = SUPPORTED_BITCOIN_TOKENS;
