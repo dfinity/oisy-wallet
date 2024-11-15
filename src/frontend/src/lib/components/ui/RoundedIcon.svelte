eada061d69e62d67f618c3ef4438bdfce860b135
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-16 00:37:58 +0100

    refactor(frontend): remove custom class prop from RoundedIcon

--- src/frontend/src/lib/components/ui/RoundedIcon.svelte
+++ src/frontend/src/lib/components/ui/RoundedIcon.svelte
@@ -2,11 +2,13 @@
 	import type { ComponentType } from 'svelte';
 
 	export let icon: ComponentType;
-	export let iconStyleClass = '';
+	export let withOpacity = false;
 </script>
 
 <div
 	class="relative flex items-center justify-center rounded-full bg-primary p-3 ring-2 ring-brand-subtle"
 >
-	<svelte:component this={icon} styleClass={iconStyleClass} />
+	<div class:opacity-10={withOpacity}>
+		<svelte:component this={icon}  />
+	</div>
 </div>
