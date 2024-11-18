bea399309ba75437047af71846ef881cce64de3e
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-18 17:21:39 +0100

    chore: docstring

--- src/frontend/src/lib/services/rest.services.ts
+++ src/frontend/src/lib/services/rest.services.ts
@@ -7,7 +7,14 @@ export interface RestRequestParams<Response, Error = unknown, Success = void> {
 }
 
 /**
+ * A utility function to make a REST request with possible retries.
  *
+ * @param request - The request function to call.
+ * @param onSuccess - The function to call when the request is successful.
+ * @param onError - The optional function to call when the request fails and the max retries are reached.
+ * @param onRetry - The optional function to call when the request fails and a retry is needed.
+ * @param maxRetries - The maximum number of retries to attempt.
+ * @returns The result of the onSuccess/onError function or undefined if the max retries are reached.
  */
 export const restRequest = async <Response, Error, Success>({
 	request,
