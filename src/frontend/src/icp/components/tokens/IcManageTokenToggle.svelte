a70aafe4f401b88ab9b0d73cdeb6f39d15039c0b
David Dal Busco <david.dalbusco@outlook.com>
2024-11-18 07:18:15 +0100

    feat(frontend): remove outdated

--- src/frontend/src/icp/components/tokens/IcManageTokenToggle.svelte
+++ src/frontend/src/icp/components/tokens/IcManageTokenToggle.svelte
@@ -3,8 +3,6 @@
 	import { createEventDispatcher } from 'svelte';
 	import { type IcrcCustomToken } from '$icp/types/icrc-custom-token';
 	import { i18n } from '$lib/stores/i18n.store';
-	import { toastsShow } from '$lib/stores/toasts.store';
-	import { replaceOisyPlaceholders, replacePlaceholders } from '$lib/utils/i18n.utils';
 	import { isIcrcTokenToggleDisabled } from '$lib/utils/token-toggle.utils';
 
 	export let token: IcrcCustomToken;
@@ -29,28 +27,11 @@
 			enabled: checked
 		});
 	};
-
-	const onClick = () => {
-		toastsShow({
-			text: replacePlaceholders(
-				replaceOisyPlaceholders($i18n.tokens.manage.info.outdated_index_canister),
-				{
-					$token: token.name
-				}
-			),
-			level: 'info',
-			duration: 5000
-		});
-	};
 </script>
 
-<!-- svelte-ignore a11y-interactive-supports-focus -->
-<!-- svelte-ignore a11y-click-events-have-key-events -->
-<div role="button" on:click={onClick}>
-	<Toggle
-		ariaLabel={checked ? $i18n.tokens.text.hide_token : $i18n.tokens.text.show_token}
-		{disabled}
-		bind:checked
-		on:nnsToggle={toggle}
-	/>
-</div>
+<Toggle
+	ariaLabel={checked ? $i18n.tokens.text.hide_token : $i18n.tokens.text.show_token}
+	{disabled}
+	bind:checked
+	on:nnsToggle={toggle}
+/>
