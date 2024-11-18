9c75854d1c424994b01ac4f94f6fb7ac302c9672
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-18 17:17:42 +0100

    refactor(frontend): remove staging ICRC

--- src/frontend/src/env/schema/env-additional-icrc-token.schema.ts
+++ src/frontend/src/env/schema/env-additional-icrc-token.schema.ts
@@ -3,12 +3,7 @@ import { EnvTokenSymbolSchema } from '$env/schema/env-token-common.schema';
 import { z } from 'zod';
 
 // TODO, extract the union into it's own schema
-export const EnvAdditionalIcrcTokensSchema = z.record(
+export const EnvTokensAdditionalIcrcSchema = z.record(
 	EnvTokenSymbolSchema,
 	z.union([z.undefined(), EnvIcTokenSchema])
 );
-
-export const EnvTokensAdditionalIcrcSchema = z.object({
-	production: EnvAdditionalIcrcTokensSchema,
-	staging: EnvAdditionalIcrcTokensSchema
-});
