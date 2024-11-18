2ac6d597e3cd8b2f80b80899a30d7132e0f462f9
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-18 13:42:33 +0100

    simplify

--- src/frontend/src/lib/services/rest.services.ts
+++ src/frontend/src/lib/services/rest.services.ts
@@ -1,44 +1,36 @@
-export type RestRequestOnSuccess<R> = (response: R) => void;
-
-export type RestRequestOnError<E = unknown> = (error: E) => void;
-
-export type RestRequestOnRetry<E = unknown> = (options: { error: E; retryCount: number }) => void;
-
-export interface RestRequestParams<R, E = unknown> {
+export interface RestRequestParams<R, E = unknown, S = void> {
 	request: () => Promise<R>;
-	onSuccess: RestRequestOnSuccess<R>;
-	onError?: RestRequestOnError<E>;
-	onRetry?: RestRequestOnRetry<E>;
+	onSuccess: (response: R) => S | undefined;
+	onError?: (error: E) => S | undefined;
+	onRetry?: (options: { error: E; retryCount: number }) => Promise<void>;
 	maxRetries?: number;
 }
 
 /**
  *
  */
-export const restRequest = async <R, E = unknown>({
+export const restRequest = async <R, E, S>({
 	request,
 	onSuccess,
 	onError,
 	onRetry,
-	maxRetries = 5
-}: RestRequestParams<R, E>): Promise<void> => {
+	maxRetries = 10
+}: RestRequestParams<R, E, S>): Promise<S | undefined> => {
 	let retryCount = 0;
 
 	while (retryCount <= maxRetries) {
 		try {
 			const response = await request();
-			onSuccess(response);
-			return;
+			return onSuccess(response);
 		} catch (error) {
 			retryCount++;
 
 			if (retryCount > maxRetries) {
-				onError?.(error as E);
 				console.error(`Max retries reached. Error:`, error);
-				return;
+				return onError?.(error as E);
 			}
 
-			onRetry?.({ error: error as E, retryCount });
+			await onRetry?.({ error: error as E, retryCount });
 			console.warn(`Request attempt ${retryCount} failed. Retrying...`);
 		}
 	}
