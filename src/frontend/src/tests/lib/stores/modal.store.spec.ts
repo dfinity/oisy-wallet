2ed25680dcd686af201c06e09d0fb31b2eeeae19
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-18 10:53:13 +0100

    refactor(frontend): make modalId a prop (#3608)
    # Motivation
    
    We would like to pass both `data` and `modalId` to the modal store. But
    it seems fair first that passing modalId is differentiated from passing
    any data.
    
    So, we add the prop modalId in the modal store and adapt the code where
    needed.
    
    # Changes
    
    - Add optional prop `modalId` to modal store type.
    - Create sub-functions to define the type of modals:
      - no input
      - `modalId` input
      - `data` input
    - Adapt the list of modals functions to use the sub-functions.
    - Adapt usage of modal store when inquired on `modalId`.
    
    # Tests
    
    Provided some tests.

---
+++ src/frontend/src/tests/lib/stores/modal.store.spec.ts
@@ -0,0 +1,52 @@
+import { initModalStore, type ModalStore } from '$lib/stores/modal.store';
+import { get } from 'svelte/store';
+
+describe('modal.store', () => {
+	interface TestData {
+		value: number;
+	}
+
+	let modalStore: ModalStore<TestData>;
+
+	beforeEach(() => {
+		modalStore = initModalStore<TestData>();
+	});
+
+	it('should initialise with undefined', () => {
+		expect(get(modalStore)).toBeUndefined();
+	});
+
+	it('should open eth-receive modal with modalId', () => {
+		const id = Symbol('modalId');
+		modalStore.openEthReceive(id);
+
+		expect(get(modalStore)).toEqual({ type: 'eth-receive', id });
+	});
+
+	it('should open icp-receive modal with modalId', () => {
+		const id = Symbol('modalId');
+		modalStore.openIcpReceive(id);
+
+		expect(get(modalStore)).toEqual({ type: 'icp-receive', id });
+	});
+
+	it('should open wallet-connect-sign modal with data', () => {
+		const data = { value: 12345 };
+		modalStore.openWalletConnectSign(data);
+
+		expect(get(modalStore)).toEqual({ type: 'wallet-connect-sign', data });
+	});
+
+	it('should open convert-ckbtc-btc modal without modalId', () => {
+		modalStore.openConvertCkBTCToBTC();
+
+		expect(get(modalStore)).toEqual({ type: 'convert-ckbtc-btc' });
+	});
+
+	it('should close the modal and reset the store', () => {
+		modalStore.openToken();
+		modalStore.close();
+
+		expect(get(modalStore)).toBeNull();
+	});
+});
