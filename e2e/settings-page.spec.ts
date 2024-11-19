9e243689bee5476263c3e2ca314f2cec3468a314
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-19 22:16:06 +0100

    chore: change string

--- e2e/settings-page.spec.ts
+++ e2e/settings-page.spec.ts
@@ -2,7 +2,7 @@ import { testWithII } from '@dfinity/internet-identity-playwright';
 import { expect } from '@playwright/test';
 import { SettingsPage } from './utils/pages/settings.page';
 
-testWithII('should display activity page', async ({ page, iiPage }) => {
+testWithII('should display settings page', async ({ page, iiPage }) => {
 	const settingsPage = new SettingsPage({ page, iiPage });
 
 	await settingsPage.waitForReady();
