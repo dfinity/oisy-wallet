7b90dc357441ecc7d7e7fe7720ab9349c2a837ac
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-19 17:33:25 +0100

    lint: remove unused imports

--- src/frontend/src/eth/components/loaders/LoaderMultipleEthTransactions.svelte
+++ src/frontend/src/eth/components/loaders/LoaderMultipleEthTransactions.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
 	import { debounce, isNullish } from '@dfinity/utils';
-	import { get } from 'svelte/store';
 	import { ETHERSCAN_MAX_CALLS_PER_SECOND } from '$env/rest/etherscan.env';
-	import { erc20UserTokensNotInitialized } from '$eth/derived/erc20.derived';
 	import { enabledEthereumTokens } from '$eth/derived/tokens.derived';
 	import { loadEthereumTransactions } from '$eth/services/eth-transactions.services';
-	import { ethTransactionsStore } from '$eth/stores/eth-transactions.store';
 	import { enabledErc20Tokens } from '$lib/derived/tokens.derived';
 	import { batch } from '$lib/services/batch.services';
-	import type { Token, TokenId } from '$lib/types/token';
+	import type { TokenId } from '$lib/types/token';
 	import type { ResultSuccess } from '$lib/types/utils';
 
 	// TODO: make it more functional
