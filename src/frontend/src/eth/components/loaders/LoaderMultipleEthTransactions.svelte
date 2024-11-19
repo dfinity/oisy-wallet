98421a058d1ab5740fcb2ee96a7d712456fe58d4
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-19 17:27:37 +0100

    feat(frontend): divide LoaderMultipleEthTransactions into buckets of promises

--- src/frontend/src/eth/components/loaders/LoaderMultipleEthTransactions.svelte
+++ src/frontend/src/eth/components/loaders/LoaderMultipleEthTransactions.svelte
@@ -1,31 +1,59 @@
 <script lang="ts">
+	import { debounce, isNullish } from '@dfinity/utils';
+	import { get } from 'svelte/store';
+	import { ETHERSCAN_MAX_CALLS_PER_SECOND } from '$env/rest/etherscan.env';
 	import { erc20UserTokensNotInitialized } from '$eth/derived/erc20.derived';
 	import { enabledEthereumTokens } from '$eth/derived/tokens.derived';
 	import { loadEthereumTransactions } from '$eth/services/eth-transactions.services';
+	import { ethTransactionsStore } from '$eth/stores/eth-transactions.store';
 	import { enabledErc20Tokens } from '$lib/derived/tokens.derived';
-	import type { TokenId } from '$lib/types/token';
+	import { batch } from '$lib/services/batch.services';
+	import type { Token, TokenId } from '$lib/types/token';
+	import type { ResultSuccess } from '$lib/types/utils';
 
 	// TODO: make it more functional
 	let tokensLoaded: TokenId[] = [];
 
 	const load = async () => {
-		if ($erc20UserTokensNotInitialized) {
+		if (isNullish($enabledEthereumTokens) || isNullish($enabledErc20Tokens)) {
 			return;
 		}
 
-		await Promise.allSettled(
-			[...$enabledEthereumTokens, ...$enabledErc20Tokens].map(
-				async ({ network: { id: networkId }, id: tokenId }) => {
-					if (!tokensLoaded.includes(tokenId)) {
-						await loadEthereumTransactions({ tokenId, networkId });
-						tokensLoaded.push(tokenId);
+		const promises = [...$enabledEthereumTokens, ...$enabledErc20Tokens].reduce<
+			Promise<ResultSuccess & { tokenId: TokenId }>[]
+		>((acc, { network: { id: networkId }, id: tokenId }) => {
+			if (!tokensLoaded.includes(tokenId)) {
+				const promise = (async (): Promise<ResultSuccess & { tokenId: TokenId }> => {
+					const result = await loadEthereumTransactions({ tokenId, networkId });
+					return { ...result, tokenId };
+				})();
+
+				return [...acc, promise];
+			}
+			return acc;
+		}, []);
+
+		const loader = batch<ResultSuccess & { tokenId: TokenId }>({
+			promises,
+			batchSize: ETHERSCAN_MAX_CALLS_PER_SECOND
+		});
+
+		for await (const results of loader) {
+			tokensLoaded = results.reduce<TokenId[]>(
+				(acc, result) => {
+					if (result.status === 'fulfilled' && result.value.success) {
+						return [...acc, result.value.tokenId];
 					}
-				}
-			)
-		);
+					return acc;
+				},
+				[...tokensLoaded]
+			);
+		}
 	};
 
-	$: $enabledEthereumTokens, $enabledErc20Tokens, $erc20UserTokensNotInitialized, load();
+	const debounceLoad = debounce(load, 1000);
+
+	$: $enabledEthereumTokens, $enabledErc20Tokens, debounceLoad();
 </script>
 
 <slot />
