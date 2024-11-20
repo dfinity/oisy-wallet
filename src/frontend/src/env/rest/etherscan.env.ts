2b103d647aee44bfc436157ab08b6e87c1ae0889
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-20 13:01:30 +0100

    feat(frontend): divide LoaderMultipleEthTransactions into buckets of promises (#3662)
    # Motivation
    
    The Etherscan API has a maximum [calls/second
    limit](https://docs.etherscan.io/support/rate-limits) and with the
    unified transaction we are always breaking it.
    
    There are a few ways to handle/solve this issue, but one of the possible
    help would be to break the component `LoaderMultipleEthTransactions`
    into smaller buckets of promises (limited by the API key calls/second
    limit).
    
    Using the service created in PR #3651 , we use `batch` for the
    component.
    
    # Changes
    
    - Change `batch` so that it may return the `Promise` values.
    - Create list of promises inside `LoaderMultipleEthTransactions`.
    - Create the `AsyncGenerator` with the limitations of Etherscan
    calls/second.
    - Update the tokens already loaded using the generator.
    
    # Tests
    
    Adjusted tests.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/env/rest/etherscan.env.ts
+++ src/frontend/src/env/rest/etherscan.env.ts
@@ -1,1 +1,5 @@
+import { BETA, PROD } from '$lib/constants/app.constants';
+
 export const ETHERSCAN_API_KEY = import.meta.env.VITE_ETHERSCAN_API_KEY;
+
+export const ETHERSCAN_MAX_CALLS_PER_SECOND = BETA || PROD ? 10 : 5;
