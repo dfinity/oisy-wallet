86c5172b0d76078b0092f6a333f159dcc2b0cb39
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-20 12:19:46 +0100

    add tests

--- src/frontend/src/tests/eth/services/eth-transactions-batch.services.spec.ts
+++ src/frontend/src/tests/eth/services/eth-transactions-batch.services.spec.ts
@@ -3,6 +3,7 @@ import { USDC_TOKEN } from '$env/tokens-erc20/tokens.usdc.env';
 import { ETHEREUM_TOKEN, SEPOLIA_TOKEN } from '$env/tokens.env';
 import {
 	batchLoadTransactions,
+	batchResultsToTokenId,
 	type ResultByToken
 } from '$eth/services/eth-transactions-batch.services';
 import { loadEthereumTransactions } from '$eth/services/eth-transactions.services';
@@ -115,6 +116,67 @@ describe('eth-transactions-batch.services', () => {
 				promises: expect.any(Array),
 				batchSize: ETHERSCAN_MAX_CALLS_PER_SECOND
 			});
+		});
+	});
+
+	describe('batchResultsToTokenId', () => {
+		const mockTokenId1 = Symbol('token1');
+		const mockTokenId2 = Symbol('token2');
+
+		const mockResults = [
+			{
+				status: 'fulfilled',
+				value: { success: true, tokenId: mockTokenId1 }
+			},
+			{
+				status: 'fulfilled',
+				value: { success: true, tokenId: mockTokenId2 }
+			}
+		] as PromiseSettledResult<ResultByToken>[];
+
+		it('should return an empty array when all promises are rejected', () => {
+			const results = [
+				{ status: 'rejected', reason: new Error('Error 1') },
+				{ status: 'rejected', reason: new Error('Error 2') }
+			] as PromiseSettledResult<ResultByToken>[];
+
+			expect(batchResultsToTokenId(results)).toEqual([]);
+		});
+
+		it('should return an array of tokenIds for successful results', () => {
+			expect(batchResultsToTokenId(mockResults)).toEqual([mockTokenId1, mockTokenId2]);
+		});
+
+		it('should skip results that are fulfilled but not successful', () => {
+			const results = [
+				{
+					status: 'fulfilled',
+					value: { success: false, tokenId: mockTokenId1 }
+				},
+				{
+					status: 'fulfilled',
+					value: { success: true, tokenId: mockTokenId2 }
+				}
+			] as PromiseSettledResult<ResultByToken>[];
+
+			expect(batchResultsToTokenId(results)).toEqual([mockTokenId2]);
+		});
+
+		it('should skip both rejected results and unsuccessful fulfilled results', () => {
+			const results = [
+				{ status: 'rejected', reason: new Error('Error 1') },
+				{
+					status: 'fulfilled',
+					value: { success: false, tokenId: mockTokenId1 }
+				},
+				{
+					status: 'fulfilled',
+					value: { success: true, tokenId: mockTokenId2 }
+				}
+			] as PromiseSettledResult<ResultByToken>[];
+			const result = batchResultsToTokenId(results);
+
+			expect(result).toEqual([mockTokenId2]);
 		});
 	});
 });
