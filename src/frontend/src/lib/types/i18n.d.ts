52d89f902a0527dd146726153811ecea0cfd8951
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2024-11-20 08:11:12 +0100

    style(frontend): changes footer copyright text (#3588)
    # Motivation
    
    Instead of the text `© 2024 Developed with ❤️ at DFINITY` the text
    `Incubated with ❤️ by DFINITY Foundation © 2024` should be displayed as
    copyright text in the footer.
    
    # Changes
    
    - changes copyright text
    - improves behaviour
    
    # Tests
    
    **before:**
    
    https://github.com/user-attachments/assets/4c48d02f-6de4-4f7e-8127-80dde42491e4
    
    **after:**
    
    https://github.com/user-attachments/assets/0c48e243-6505-4795-b875-9b42c86c98d2
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/types/i18n.d.ts
+++ src/frontend/src/lib/types/i18n.d.ts
@@ -103,7 +103,7 @@ interface I18nDapps {
 }
 
 interface I18nFooter {
-	text: { developed_with: string };
+	text: { incubated_with: string; dfinity_foundation: string; copyright: string };
 	alt: { dfinity: string; status: string };
 }
 
