62af14feb30155c5feaae3f2014b2c0dda4de716
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2024-11-20 09:56:20 +0100

    displays warning to display which tokens do not have an index canister

--- src/frontend/src/lib/components/transactions/AllTransactions.svelte
+++ src/frontend/src/lib/components/transactions/AllTransactions.svelte
@@ -3,14 +3,31 @@
 	import MessageBox from '$lib/components/ui/MessageBox.svelte';
 	import PageTitle from '$lib/components/ui/PageTitle.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
+	import { icTransactionsStore } from '$icp/stores/ic-transactions.store';
+	import type { TokenUi } from '$lib/types/token';
+	import { combinedDerivedSortedNetworkTokensUi } from '$lib/derived/network-tokens.derived';
+	import { showZeroBalances } from '$lib/derived/settings.derived';
+	import { sortedBtcTransactions } from '$btc/derived/btc-transactions.derived';
+	import { sortedEthTransactions } from '$eth/derived/eth-transactions.derived';
+	import { replacePlaceholders } from '$lib/utils/i18n.utils.js';
+
+	let tokensWithoutCanister: TokenUi[];
+	$: tokensWithoutCanister = $combinedDerivedSortedNetworkTokensUi.filter(
+		({ balance, usdBalance }) => Number(balance ?? 0n) || (usdBalance ?? 0) || $showZeroBalances
+	).filter(token => $sortedEthTransactions?.[token.id] === null || $sortedBtcTransactions?.[token.id] === null || $icTransactionsStore?.[token.id] === null)
+
+	let tokenList: string;
+	$: tokenList = tokensWithoutCanister.map(token => `$${token.symbol}`).join(', ');
 </script>
 
 <div class="flex flex-col gap-5">
 	<PageTitle>{$i18n.activity.text.title}</PageTitle>
 
-	<MessageBox level="light-warning" closable>
-		{$i18n.activity.warning.incomplete_transaction_list}
-	</MessageBox>
+	{#if tokenList}
+		<MessageBox level="light-warning" closable>
+			{replacePlaceholders($i18n.activity.warning.incomplete_transaction_list, {'$token_list': tokenList})}
+		</MessageBox>
+	{/if}
 
 	<MessageBox level="plain" closable>
 		{$i18n.activity.info.btc_transactions}
