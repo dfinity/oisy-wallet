3001353b06d98438c3269d0e5812aae19aa38eca
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-21 16:43:38 +0100

    feat(frontend): add network selector in Activity page (#3720)
    # Motivation
    
    We would like to include the logic of the NetworkSwitcher in the
    Activity page.
    
    Basically:
    
    1. It will not appear if testnet is not enabled and show Chain Fusion
    tokens transactions.
    2. It will appear when testnet is enabled and allows to filter
    transactions per network.
    
    NOTE: By default all networks (Chain Fusion) shows everything except
    transactions of testnet tokens.
    
    # Changes
    
    - Add `NetworksSwitcher` in unified transaction component.
    - Add route parameters breakdown in `+page` of Activity.
    - Change tokens store in unified transaction list to be
    `enabledNetworkTokens`.
    
    # Tests
    
    
    
    https://github.com/user-attachments/assets/cf54b381-b2cc-4103-9311-959e602ed967
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/transactions/AllTransactions.svelte
+++ src/frontend/src/lib/components/transactions/AllTransactions.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
+	import NetworksSwitcher from '$lib/components/networks/NetworksSwitcher.svelte';
 	import AllTransactionsList from '$lib/components/transactions/AllTransactionsList.svelte';
 	import MessageBox from '$lib/components/ui/MessageBox.svelte';
 	import PageTitle from '$lib/components/ui/PageTitle.svelte';
+	import { testnetsEnabled } from '$lib/derived/settings.derived';
 	import { i18n } from '$lib/stores/i18n.store';
 </script>
 
 <div class="flex flex-col gap-5">
 	<PageTitle>{$i18n.activity.text.title}</PageTitle>
+
+	{#if $testnetsEnabled}
+		<div>
+			<NetworksSwitcher />
+		</div>
+	{/if}
 
 	<MessageBox level="plain" closableKey="oisy_ic_hide_bitcoin_activity">
 		{$i18n.activity.info.btc_transactions}
