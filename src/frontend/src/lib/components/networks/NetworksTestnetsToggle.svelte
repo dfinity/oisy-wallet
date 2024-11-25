39f4230ee681fc7bcfd8f3d2bd60d6dbc78e6d7c
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-11-25 11:55:50 +0100

    feat(frontend): reset network param when turning testnets off

--- src/frontend/src/lib/components/networks/NetworksTestnetsToggle.svelte
+++ src/frontend/src/lib/components/networks/NetworksTestnetsToggle.svelte
@@ -3,11 +3,25 @@
 	import { testnetsEnabled } from '$lib/derived/settings.derived';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { testnetsStore } from '$lib/stores/settings.store';
+	import { NETWORK_PARAM } from '$lib/constants/routes.constants';
+	import { goto } from '$app/navigation';
+
+	// TODO: create tests for this component once we have testId from GIX-CMP
+	// PR: https://github.com/dfinity/gix-components/pull/531
+
 
 	let checked: boolean;
 	$: checked = $testnetsEnabled;
 
-	const toggleTestnets = () => testnetsStore.set({ key: 'testnets', value: { enabled: !checked } });
+	const toggleTestnets = async () => {
+		testnetsStore.set({ key: 'testnets', value: { enabled: !checked } });
+		// Reset network param, since the network is selectable only when testnets are enabled
+		if (checked) {
+			const url = new URL(window.location.href);
+			url.searchParams.delete(NETWORK_PARAM);
+			await goto(url, { replaceState: true, noScroll: true });
+		}
+	};
 </script>
 
 <Toggle ariaLabel={$i18n.settings.alt.testnets_toggle} bind:checked on:nnsToggle={toggleTestnets} />
