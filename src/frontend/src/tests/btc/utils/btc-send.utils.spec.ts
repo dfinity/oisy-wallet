de25aff866096d162dd8e784119136abf8445f25
Denys Karmazyn <denys.karmazyn@dfinity.org>
2024-11-25 09:20:16 +0100

    fix(frontend): issue with converting very low numbers to satoshis (#3724)
    # Motivation
    
    I noticed that for some numbers the `convertNumberToSatoshis` was
    throwing `cannot be converted to a BigInt because it is not an integer`
    error. This PR fixes it as well as adds some explanations to the code.

--- src/frontend/src/tests/btc/utils/btc-send.utils.spec.ts
+++ src/frontend/src/tests/btc/utils/btc-send.utils.spec.ts
@@ -6,5 +6,8 @@ describe('convertNumberToSatoshis', () => {
 		expect(convertNumberToSatoshis({ amount: 0.00005 })).toEqual(5000n);
 		expect(convertNumberToSatoshis({ amount: 0.25 })).toEqual(25000000n);
 		expect(convertNumberToSatoshis({ amount: 0 })).toEqual(0n);
+		expect(convertNumberToSatoshis({ amount: 0.00004 })).toEqual(4000n);
+		expect(convertNumberToSatoshis({ amount: 0.00000001 })).toEqual(1n);
+		expect(convertNumberToSatoshis({ amount: 0.0000399999999999999 })).toEqual(4000n);
 	});
 });
