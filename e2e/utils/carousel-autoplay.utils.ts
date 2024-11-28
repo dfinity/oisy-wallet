8b20ecd8e1ab6872515a2294d87330b91ac42f3f
Nico Mächler <135843237+inc-man@users.noreply.github.com>
2024-11-28 16:22:23 +0100

    another try

--- e2e/utils/carousel-autoplay.utils.ts
+++ e2e/utils/carousel-autoplay.utils.ts
@@ -3,8 +3,10 @@ import { type Page } from '@playwright/test';
 export async function disableCarouselAutoplay(page: Page) {
 	await page.addInitScript(() => {
 		const originalSetInterval = setInterval
+    // eslint-disable-line local-rules/prefer-object-params
 		const mockSetInterval = (handler: TimerHandler, timeout?: number, ...args: unknown[]): number => {
-				console.warn('setInterval has been disabled to prevent carousel autoplay.');
+        console.log(`Received call for setTimeout with handler: ${handler}, timeout: ${timeout}, args: ${args}`)
+				console.log('setInterval has been disabled to prevent carousel autoplay.');
 				return 0;
 		};
 		
