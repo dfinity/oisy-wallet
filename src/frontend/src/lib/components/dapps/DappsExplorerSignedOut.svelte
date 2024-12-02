85e35cdb7e9e1c35d5d1e15411b2ac6bd9384f66
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-12-02 15:37:55 +0100

    refactor(frontend): replace text-secondary with black

--- src/frontend/src/lib/components/dapps/DappsExplorerSignedOut.svelte
+++ src/frontend/src/lib/components/dapps/DappsExplorerSignedOut.svelte
@@ -3,4 +3,4 @@
 	import { replaceOisyPlaceholders } from '$lib/utils/i18n.utils';
 </script>
 
-<p class="text-secondary mt-4 opacity-50">{replaceOisyPlaceholders($i18n.dapps.text.sign_in)}</p>
+<p class="text-black mt-4 opacity-50">{replaceOisyPlaceholders($i18n.dapps.text.sign_in)}</p>
