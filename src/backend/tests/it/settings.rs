d7e14a23b79b849e26e3689f093e0bd24e4c8a6d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-12-03 19:01:12 +0100

    add test for length

--- src/backend/tests/it/settings.rs
+++ src/backend/tests/it/settings.rs
@@ -288,3 +288,53 @@ fn test_add_user_hidden_dapp_id_does_not_add_duplicate_dapp_id() {
         vec!["test_dapp_id".to_string()]
     );
 }
+
+
+
+
+#[test]
+fn test_add_user_hidden_dapp_id_does_not_allow_long_ids() {
+    let pic_setup = setup();
+
+    let caller = Principal::from_text(CALLER).unwrap();
+
+    let create_profile_response =
+        pic_setup.update::<UserProfile>(caller, "create_user_profile", ());
+
+    let profile = create_profile_response.expect("Create failed");
+    assert_eq!(profile.settings.dapp.dapp_carousel.hidden_dapp_ids.len(), 0);
+
+    let add_hidden_dapp_id_arg = AddHiddenDappIdRequest {
+        dapp_id: "test_dapp_id".repeat(100),
+        current_user_version: profile.version,
+    };
+
+    let add_hidden_dapp_id_response = pic_setup.update::<Result<(), AddDappSettingsError>>(
+        caller,
+        "add_user_hidden_dapp_id",
+        add_hidden_dapp_id_arg,
+    );
+
+    assert_eq!(
+        add_hidden_dapp_id_response,
+        Ok(Err(AddDappSettingsError::DappIdTooLong))
+    );
+
+    let get_profile_response = pic_setup.update::<Result<UserProfile, GetUserProfileError>>(
+        caller,
+        "get_user_profile",
+        (),
+    );
+
+    assert_eq!(
+        get_profile_response
+            .expect("Call to get profile failed")
+            .expect("Get profile failed")
+            .settings
+            .dapp
+            .dapp_carousel
+            .hidden_dapp_ids
+            .len(),
+        0
+    );
+}
