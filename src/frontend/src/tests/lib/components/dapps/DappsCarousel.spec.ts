883b88139c84348ef82e9070275548eb2a60eab6
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-12-03 20:46:17 +0100

    test: add test for component

--- src/frontend/src/tests/lib/components/dapps/DappsCarousel.spec.ts
+++ src/frontend/src/tests/lib/components/dapps/DappsCarousel.spec.ts
@@ -1,13 +1,18 @@
+import type { UserProfile } from '$declarations/backend/backend.did';
 import DappsCarousel from '$lib/components/dapps/DappsCarousel.svelte';
 import { CAROUSEL_CONTAINER } from '$lib/constants/test-ids.constants';
+import { userProfileStore } from '$lib/stores/user-profile.store';
 import * as dapps from '$lib/types/dapp-description';
 import { mockDappsDescriptions } from '$tests/mocks/dapps.mock';
+import { mockUserProfile } from '$tests/mocks/user-profile.mock';
 import { render } from '@testing-library/svelte';
 import { describe, expect, it } from 'vitest';
 
 describe('DappsCarousel', () => {
 	beforeEach(() => {
 		vi.clearAllMocks();
+
+		userProfileStore.set({ profile: mockUserProfile, certified: false });
 	});
 
 	it('should render nothing if there is no dApps', () => {
@@ -21,6 +26,27 @@ describe('DappsCarousel', () => {
 		vi.spyOn(dapps, 'dAppDescriptions', 'get').mockReturnValueOnce(
 			mockDappsDescriptions.map((dapp) => ({ ...dapp, carousel: undefined }))
 		);
+
+		const { container } = render(DappsCarousel);
+		expect(container.innerHTML).toBe('');
+	});
+
+	it('should render nothing if all dApps were hidden', () => {
+		const userProfile: UserProfile = {
+			...mockUserProfile,
+			settings: {
+				...mockUserProfile.settings,
+				dapp: {
+					...mockUserProfile.settings.dapp,
+					dapp_carousel: {
+						...mockUserProfile.settings.dapp.dapp_carousel,
+						hidden_dapp_ids: mockDappsDescriptions.map(({ id }) => id)
+					}
+				}
+			}
+		};
+
+		userProfileStore.set({ profile: userProfile, certified: false });
 
 		const { container } = render(DappsCarousel);
 		expect(container.innerHTML).toBe('');
