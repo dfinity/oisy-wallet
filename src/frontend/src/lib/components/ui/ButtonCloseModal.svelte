c9796c18d5e9050377b6efccefcf03eab7d51a5a
Robert Schlittler <43857623+loki344@users.noreply.github.com>
2024-12-04 13:11:41 +0100

    style(frontend): change colorStyle of close btn to secondary (#3826)
    # Motivation
    
    Uniform layout of modals. Close buttons are secondary colored.
    
    # Changes
    
    - rm primary color for close button in IcTransactionModal
    - rm primary color for close button in ethTransactionModal
    - rm primary color for close btn in AboutWhyOisyModal
    
    # Tests
    
    before:
    
    ![image](https://github.com/user-attachments/assets/eb57b745-13b8-42d9-b67f-ff62f7529115)
    
    
    after: 
    
    ![image](https://github.com/user-attachments/assets/bb47943c-7bae-4c2f-8f7f-16492f012c73)

--- src/frontend/src/lib/components/ui/ButtonCloseModal.svelte
+++ src/frontend/src/lib/components/ui/ButtonCloseModal.svelte
@@ -2,11 +2,8 @@
 	import Button from '$lib/components/ui/Button.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { modalStore } from '$lib/stores/modal.store';
-	import type { ButtonColorStyle } from '$lib/types/style';
-
-	export let colorStyle: ButtonColorStyle = 'secondary';
 </script>
 
-<Button paddingSmall {colorStyle} type="button" fullWidth on:click={modalStore.close}>
+<Button paddingSmall colorStyle="secondary" type="button" fullWidth on:click={modalStore.close}>
 	{$i18n.core.text.close}
 </Button>
