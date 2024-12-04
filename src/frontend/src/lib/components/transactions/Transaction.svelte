8f50335ac74511923bb57845ed0c22621ad92ada
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2024-12-04 15:39:15 +0100

    style(frontend): improves transaction lists (#3739)
    # Motivation
    
    In the transaction lists the token ticket should also be displayed (e.g.
    0.00123 -> 0.00123 ETH). In addition the ck-Token symbols should always
    start lowercase.
    
    # Changes
    
    - adds token ticket to the transactions in the transaction lists
    - capitalizes the first letter of the token label only on fallback
    
    # Tests
    
    **before:**
    activity:
    <img width="461" alt="image"
    src="https://github.com/user-attachments/assets/d548d509-852b-45ee-86cd-9fb15bec76f9">
    
    token view:
    <img width="461" alt="image"
    src="https://github.com/user-attachments/assets/6ae1f9a2-895f-4483-91a6-07d346e33177">
    
    
    
    **after:**
    activity:
    <img width="457" alt="image"
    src="https://github.com/user-attachments/assets/c0dbd0a5-ed44-41f7-8d0f-aa651f9950e7">
    
    token view:
    <img width="461" alt="image"
    src="https://github.com/user-attachments/assets/54ce6e7e-d522-4c50-ab66-7f04fbcc36e6">

--- src/frontend/src/lib/components/transactions/Transaction.svelte
+++ src/frontend/src/lib/components/transactions/Transaction.svelte
@@ -29,7 +29,7 @@
 
 <button class={`contents ${styleClass ?? ''}`} on:click>
 	<Card>
-		<span class="inline-block first-letter:capitalize"><slot /></span>
+		<span class="inline-block"><slot /></span>
 
 		<div slot="icon">
 			{#if iconType === 'token'}
@@ -41,7 +41,7 @@
 
 		<svelte:fragment slot="amount">
 			{#if nonNullish(amount)}
-				<Amount {amount} decimals={token.decimals} formatPositiveAmount />
+				<Amount {amount} decimals={token.decimals} symbol={token.symbol} formatPositiveAmount />
 			{/if}
 		</svelte:fragment>
 
