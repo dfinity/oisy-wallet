b2a79e9831436a7d379a465079122d65ad9e09f2
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2024-12-04 15:07:58 +0100

    implements test cases for warning message boxes

--- src/frontend/src/tests/lib/components/transactions/AllTransactions.spec.ts
+++ src/frontend/src/tests/lib/components/transactions/AllTransactions.spec.ts
@@ -2,8 +2,20 @@ import AllTransactions from '$lib/components/transactions/AllTransactions.svelte
 import en from '$tests/mocks/i18n.mock';
 import { assertNonNullish } from '@dfinity/utils';
 import { render } from '@testing-library/svelte';
+import { mockValidIcToken } from '$tests/mocks/ic-tokens.mock';
+import type { IcrcCustomToken } from '$icp/types/icrc-custom-token';
+import { icrcCustomTokensStore } from '$icp/stores/icrc-custom-tokens.store';
+import { icTransactionsStore } from '$icp/stores/ic-transactions.store';
+import { get } from 'svelte/store';
+import { replacePlaceholders } from '$lib/utils/i18n.utils';
 
 describe('Activity', () => {
+	const customIcrcToken: IcrcCustomToken = {
+		...mockValidIcToken,
+		version: 1n,
+		enabled: true,
+	};
+
 	it('renders the title', () => {
 		const { container } = render(AllTransactions);
 
@@ -13,6 +25,49 @@ describe('Activity', () => {
 		assertNonNullish(title, 'Title not found');
 		expect(title).toBeInTheDocument();
 		expect(title.textContent).toBe(en.activity.text.title);
+	});
+
+	it('renders the broken Index canister warning box', () => {
+		const tokenWithBrokenIndexCanister: IcrcCustomToken = {
+			...customIcrcToken,
+			symbol: 'UTC',
+			indexCanisterId: 'mxzaz-hqaaa-aaaar-qaada-cai'
+		};
+
+		icrcCustomTokensStore.set(({ data: tokenWithBrokenIndexCanister, certified: true }));
+
+		const store = get(icrcCustomTokensStore);
+		const tokenId = store!.at(0)!.data!.id!;
+		icTransactionsStore.nullify(tokenId);
+
+		const { getByText } = render(AllTransactions);
+
+		const exceptedText = replacePlaceholders(en.activity.warning.broken_index_canister, {
+			$token_list: '$UTC'
+		});
+
+		expect(getByText(exceptedText)).toBeInTheDocument();
+	});
+
+	it('renders the no Index canister warning box', () => {
+		const tokenWithoutIndexCanister: IcrcCustomToken = {
+			...customIcrcToken,
+			symbol: 'UWT'
+		}
+
+		icrcCustomTokensStore.set(({ data: tokenWithoutIndexCanister, certified: true }));
+
+		const store = get(icrcCustomTokensStore);
+		const tokenId = store!.at(0)!.data!.id!;
+		icTransactionsStore.nullify(tokenId);
+
+		const { getByText } = render(AllTransactions);
+
+		const exceptedText = replacePlaceholders(en.activity.warning.no_index_canister, {
+			$token_list: '$UWT'
+		});
+
+		expect(getByText(exceptedText)).toBeInTheDocument();
 	});
 
 	it('renders the info box list', () => {
