13629b59dbe6837ff21ad6b6791b46556e04ad6d
Robert Schlittler <robert.schlittler@inacta.ch>
2024-12-04 09:49:28 +0100

    Revert "style(frontend): rm _modal scss as its no longer used"
    This reverts commit 78b859470fa1635cbea18a42846735a0d97538fc.

--- src/frontend/src/lib/components/dapps/DappModal.svelte
+++ src/frontend/src/lib/components/dapps/DappModal.svelte
@@ -144,8 +144,10 @@
 </Modal>
 
 <style lang="scss">
+	@use '../../styles/mixins/modal';
 
 	article {
+		@include modal.content;
 
 		padding: var(--padding-3x) var(--padding-2_5x);
 		margin: 0 0 var(--padding-3x);
