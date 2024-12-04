3c726b7c71b38a077636eee42abb2e4d0712c0f5
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-12-04 15:33:04 +0100

    refactor: check login

--- src/frontend/src/lib/components/loaders/LoaderUserProfile.svelte
+++ src/frontend/src/lib/components/loaders/LoaderUserProfile.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { authIdentity } from '$lib/derived/auth.derived';
+	import { authIdentity, authNotSignedIn } from '$lib/derived/auth.derived';
 	import { loadUserProfile } from '$lib/services/load-user-profile.services';
-	import type { OptionIdentity } from '$lib/types/identity';
+	import { userProfileStore } from '$lib/stores/user-profile.store';
 
-	let identity: OptionIdentity;
-	$: identity = $authIdentity;
+	const load = ({ reload = false }: { reload?: boolean }) => {
+		if ($authNotSignedIn) {
+			userProfileStore.reset();
+			return;
+		}
+
+		loadUserProfile({ identity: $authIdentity, reload });
+	};
 
 	onMount(() => {
-		loadUserProfile({ identity });
+		load({});
 	});
 
 	const reload = () => {
-		loadUserProfile({ identity, reload: true });
+		load({ reload: true });
 	};
 </script>
 
