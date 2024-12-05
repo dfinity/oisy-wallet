8e1118896691fadfe73b7a65ebb19dae35a74eb6
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-12-05 13:44:29 -0300

    refactor: use settings derived store

--- src/frontend/src/lib/components/dapps/DappsCarousel.svelte
+++ src/frontend/src/lib/components/dapps/DappsCarousel.svelte
@@ -2,24 +2,24 @@
 	import { nonNullish } from '@dfinity/utils';
 	import Carousel from '$lib/components/carousel/Carousel.svelte';
 	import DappsCarouselSlide from '$lib/components/dapps/DappsCarouselSlide.svelte';
-	import { userProfileStore } from '$lib/stores/user-profile.store';
 	import {
 		type CarouselSlideOisyDappDescription,
 		dAppDescriptions,
 		type OisyDappDescription
 	} from '$lib/types/dapp-description';
 	import { filterCarouselDapps } from '$lib/utils/dapps.utils';
+	import { userSettings } from '$lib/derived/user-profile.derived';
 
 	export let styleClass: string | undefined = undefined;
 
 	let hiddenDappsIds: OisyDappDescription['id'][];
-	$: hiddenDappsIds = $userProfileStore?.profile.settings.dapp.dapp_carousel.hidden_dapp_ids ?? [];
+	$: hiddenDappsIds = $userSettings?.dapp.dapp_carousel.hidden_dapp_ids ?? [];
 
 	let dappsCarouselSlides: CarouselSlideOisyDappDescription[];
 	$: dappsCarouselSlides = filterCarouselDapps({ dAppDescriptions, hiddenDappsIds });
 </script>
 
-{#if nonNullish($userProfileStore) && nonNullish(dappsCarouselSlides) && dappsCarouselSlides.length > 0}
+{#if nonNullish($userSettings) && nonNullish(dappsCarouselSlides) && dappsCarouselSlides.length > 0}
 	<!-- To align controls section with slide text - 100% - logo width (4rem) - margin logo-text (1rem) -->
 	<Carousel controlsWidthStyleClass="w-[calc(100%-5rem)]" styleClass={`w-full ${styleClass ?? ''}`}>
 		{#each dappsCarouselSlides as dappsCarouselSlide}
--- src/frontend/src/tests/lib/components/dapps/DappsCarousel.spec.ts
+++ src/frontend/src/tests/lib/components/dapps/DappsCarousel.spec.ts
@@ -1,10 +1,11 @@
-import type { UserProfile } from '$declarations/backend/backend.did';
+import type { Settings, UserProfile } from '$declarations/backend/backend.did';
 import DappsCarousel from '$lib/components/dapps/DappsCarousel.svelte';
 import { CAROUSEL_CONTAINER } from '$lib/constants/test-ids.constants';
 import { userProfileStore } from '$lib/stores/user-profile.store';
 import * as dapps from '$lib/types/dapp-description';
 import { mockDappsDescriptions } from '$tests/mocks/dapps.mock';
-import { mockUserProfile } from '$tests/mocks/user-profile.mock';
+import { mockUserProfile, mockUserSettings } from '$tests/mocks/user-profile.mock';
+import { toNullable } from '@dfinity/utils';
 import { render } from '@testing-library/svelte';
 import { describe, expect, it } from 'vitest';
 
@@ -32,18 +33,20 @@ describe('DappsCarousel', () => {
 	});
 
 	it('should render nothing if all dApps were hidden', () => {
-		const userProfile: UserProfile = {
-			...mockUserProfile,
-			settings: {
-				...mockUserProfile.settings,
-				dapp: {
-					...mockUserProfile.settings.dapp,
-					dapp_carousel: {
-						...mockUserProfile.settings.dapp.dapp_carousel,
-						hidden_dapp_ids: mockDappsDescriptions.map(({ id }) => id)
-					}
+		const settings: Settings = {
+			...mockUserSettings,
+			dapp: {
+				...mockUserSettings.dapp,
+				dapp_carousel: {
+					...mockUserSettings.dapp.dapp_carousel,
+					hidden_dapp_ids: mockDappsDescriptions.map(({ id }) => id)
 				}
 			}
+		};
+
+		const userProfile: UserProfile = {
+			...mockUserProfile,
+			settings: toNullable(settings)
 		};
 
 		userProfileStore.set({ profile: userProfile, certified: false });
@@ -54,6 +57,18 @@ describe('DappsCarousel', () => {
 
 	it('should render nothing if the user profile is nullish', () => {
 		userProfileStore.reset();
+
+		const { container } = render(DappsCarousel);
+		expect(container.innerHTML).toBe('');
+	});
+
+	it('should render nothing if the user settings are nullish', () => {
+		const userProfile: UserProfile = {
+			...mockUserProfile,
+			settings: []
+		};
+
+		userProfileStore.set({ profile: userProfile, certified: false });
 
 		const { container } = render(DappsCarousel);
 		expect(container.innerHTML).toBe('');
