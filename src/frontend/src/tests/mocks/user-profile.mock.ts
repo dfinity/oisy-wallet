4fba7b33af8423826119a644a813afaba2ea9e49
Antonio Ventilii <antonioventilii@gmail.com>
2024-12-05 17:40:16 +0100

    feat(frontend): derived store for user settings (#3865)
    # Motivation
    
    We create a derived store for the user settings, so that we can handle
    eventual `nullable` values.

--- src/frontend/src/tests/mocks/user-profile.mock.ts
+++ src/frontend/src/tests/mocks/user-profile.mock.ts
@@ -1,10 +1,12 @@
 import type { UserProfile } from '$declarations/backend/backend.did';
 import { toNullable } from '@dfinity/utils';
 
+export const mockUserSettings = { dapp: { dapp_carousel: { hidden_dapp_ids: [] } } };
+
 export const mockUserProfile: UserProfile = {
 	credentials: [],
 	version: [],
-	settings: toNullable({ dapp: { dapp_carousel: { hidden_dapp_ids: [] } } }),
+	settings: toNullable(mockUserSettings),
 	created_timestamp: 1234n,
 	updated_timestamp: 1234n
 };
