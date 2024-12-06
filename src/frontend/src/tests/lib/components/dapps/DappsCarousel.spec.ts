ab390e31dcba188be9d0e3f8872ee3d34782b3ea
Antonio Ventilii <antonioventilii@gmail.com>
2024-12-06 08:26:19 +0100

    feat(frontend): filter carousel dApps using user profile (#3811)
    # Motivation
    
    With PR #3770 , we introduces a list of hidden dApps in the carousel,
    saved in the user profile.
    
    Now we filter the daps accordingly.
    
    # Changes
    
    - Extract dapps filter function.
    - Add filter by hidden dApps from the User Profile. 
    
    # Tests
    
    Tests provided.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/tests/lib/components/dapps/DappsCarousel.spec.ts
+++ src/frontend/src/tests/lib/components/dapps/DappsCarousel.spec.ts
@@ -1,13 +1,19 @@
+import type { Settings, UserProfile } from '$declarations/backend/backend.did';
 import DappsCarousel from '$lib/components/dapps/DappsCarousel.svelte';
 import { CAROUSEL_CONTAINER } from '$lib/constants/test-ids.constants';
+import { userProfileStore } from '$lib/stores/user-profile.store';
 import * as dapps from '$lib/types/dapp-description';
 import { mockDappsDescriptions } from '$tests/mocks/dapps.mock';
+import { mockUserProfile, mockUserSettings } from '$tests/mocks/user-profile.mock';
+import { toNullable } from '@dfinity/utils';
 import { render } from '@testing-library/svelte';
 import { describe, expect, it } from 'vitest';
 
 describe('DappsCarousel', () => {
 	beforeEach(() => {
 		vi.clearAllMocks();
+
+		userProfileStore.set({ profile: mockUserProfile, certified: false });
 	});
 
 	it('should render nothing if there is no dApps', () => {
@@ -21,6 +27,48 @@ describe('DappsCarousel', () => {
 		vi.spyOn(dapps, 'dAppDescriptions', 'get').mockReturnValueOnce(
 			mockDappsDescriptions.map((dapp) => ({ ...dapp, carousel: undefined }))
 		);
+
+		const { container } = render(DappsCarousel);
+		expect(container.innerHTML).toBe('');
+	});
+
+	it('should render nothing if all dApps were hidden', () => {
+		const settings: Settings = {
+			...mockUserSettings,
+			dapp: {
+				...mockUserSettings.dapp,
+				dapp_carousel: {
+					...mockUserSettings.dapp.dapp_carousel,
+					hidden_dapp_ids: mockDappsDescriptions.map(({ id }) => id)
+				}
+			}
+		};
+
+		const userProfile: UserProfile = {
+			...mockUserProfile,
+			settings: toNullable(settings)
+		};
+
+		userProfileStore.set({ profile: userProfile, certified: false });
+
+		const { container } = render(DappsCarousel);
+		expect(container.innerHTML).toBe('');
+	});
+
+	it('should render nothing if the user profile is nullish', () => {
+		userProfileStore.reset();
+
+		const { container } = render(DappsCarousel);
+		expect(container.innerHTML).toBe('');
+	});
+
+	it('should render nothing if the user settings are nullish', () => {
+		const userProfile: UserProfile = {
+			...mockUserProfile,
+			settings: []
+		};
+
+		userProfileStore.set({ profile: userProfile, certified: false });
 
 		const { container } = render(DappsCarousel);
 		expect(container.innerHTML).toBe('');
