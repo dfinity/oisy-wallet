45f99688f30f747184d5882f8ac576da166b2139
Antonio Ventilii <antonioventilii@gmail.com>
2024-12-06 08:26:19 +0100

    feat(frontend): filter carousel dApps using user profile (#3811)
    # Motivation
    
    With PR #3770 , we introduces a list of hidden dApps in the carousel,
    saved in the user profile.
    
    Now we filter the daps accordingly.
    
    # Changes
    
    - Extract dapps filter function.
    - Add filter by hidden dApps from the User Profile. 
    
    # Tests
    
    Tests provided.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/dapps/DappsCarousel.svelte
+++ src/frontend/src/lib/components/dapps/DappsCarousel.svelte
@@ -2,19 +2,24 @@
 	import { nonNullish } from '@dfinity/utils';
 	import Carousel from '$lib/components/carousel/Carousel.svelte';
 	import DappsCarouselSlide from '$lib/components/dapps/DappsCarouselSlide.svelte';
+	import { userSettings } from '$lib/derived/user-profile.derived';
 	import {
 		type CarouselSlideOisyDappDescription,
-		dAppDescriptions
+		dAppDescriptions,
+		type OisyDappDescription
 	} from '$lib/types/dapp-description';
 	import { filterCarouselDapps } from '$lib/utils/dapps.utils';
 
 	export let styleClass: string | undefined = undefined;
 
+	let hiddenDappsIds: OisyDappDescription['id'][];
+	$: hiddenDappsIds = $userSettings?.dapp.dapp_carousel.hidden_dapp_ids ?? [];
+
 	let dappsCarouselSlides: CarouselSlideOisyDappDescription[];
-	$: dappsCarouselSlides = filterCarouselDapps(dAppDescriptions);
+	$: dappsCarouselSlides = filterCarouselDapps({ dAppDescriptions, hiddenDappsIds });
 </script>
 
-{#if nonNullish(dappsCarouselSlides) && dappsCarouselSlides.length > 0}
+{#if nonNullish($userSettings) && nonNullish(dappsCarouselSlides) && dappsCarouselSlides.length > 0}
 	<!-- To align controls section with slide text - 100% - logo width (4rem) - margin logo-text (1rem) -->
 	<Carousel controlsWidthStyleClass="w-[calc(100%-5rem)]" styleClass={`w-full ${styleClass ?? ''}`}>
 		{#each dappsCarouselSlides as dappsCarouselSlide}
