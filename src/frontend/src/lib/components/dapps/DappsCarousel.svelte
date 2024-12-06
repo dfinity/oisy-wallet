9a99fb278eaee5ea924cf04e584856f9e9ca0cb4
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-12-06 05:42:31 -0300

    introduce refresh

--- src/frontend/src/lib/components/dapps/DappsCarousel.svelte
+++ src/frontend/src/lib/components/dapps/DappsCarousel.svelte
@@ -18,16 +18,26 @@
 	let dappsCarouselSlides: CarouselSlideOisyDappDescription[];
 	$: dappsCarouselSlides = filterCarouselDapps({ dAppDescriptions, hiddenDappsIds });
 
+	let carousel: Carousel;
+
 	const closeSlide = async ({
 		detail: dappId
 	}: CustomEvent<CarouselSlideOisyDappDescription['id']>) => {
-		hiddenDappsIds = [...hiddenDappsIds, dappId];
+		const idx = dappsCarouselSlides.findIndex(({ id }) => id === dappId);
+
+		if (idx !== -1) {
+			carousel.removeSlide(idx);
+		}
 	};
 </script>
 
 {#if nonNullish($userSettings) && nonNullish(dappsCarouselSlides) && dappsCarouselSlides.length > 0}
 	<!-- To align controls section with slide text - 100% - logo width (4rem) - margin logo-text (1rem) -->
-	<Carousel controlsWidthStyleClass="w-[calc(100%-5rem)]" styleClass={`w-full ${styleClass ?? ''}`}>
+	<Carousel
+		bind:this={carousel}
+		controlsWidthStyleClass="w-[calc(100%-5rem)]"
+		styleClass={`w-full ${styleClass ?? ''}`}
+	>
 		{#each dappsCarouselSlides as dappsCarouselSlide}
 			<DappsCarouselSlide {dappsCarouselSlide} on:icCloseCarouselSlide={closeSlide} />
 		{/each}
