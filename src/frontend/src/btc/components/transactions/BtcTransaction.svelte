389bb63821f5036ec14f54dc3f412640922a01b3
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2024-12-06 11:38:13 +0100

    style(frontend): calculates amount for btc transactions correctly (#3877)
    # Motivation
    
    On the transactions page the BTC send transactions are displayed as if
    the user is receiving tokens instead of sending them. The amount of a
    `send` transaction should be displayed with a leading `-` (minus) and
    the text should be black.
    
    # Changes
    
    - fixes calculation
    
    # Tests
    
    **before:**
    
    ![image](https://github.com/user-attachments/assets/8b56855f-f69c-4f8b-b1f6-32aacd8fd8a1)
    
    
    **after:**
    
    ![image](https://github.com/user-attachments/assets/44671b83-b7b1-45fc-8fc9-774689a02782)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/btc/components/transactions/BtcTransaction.svelte
+++ src/frontend/src/btc/components/transactions/BtcTransaction.svelte
@@ -21,11 +21,14 @@
 
 	let label: string;
 	$: label = type === 'send' ? $i18n.send.text.send : $i18n.receive.text.receive;
+
+	let amount: bigint | undefined;
+	$: amount = nonNullish(value) ? (type === 'send' ? value * BigInt(-1) : value) : undefined;
 </script>
 
 <Transaction
 	on:click={() => modalStore.openBtcTransaction({ transaction, token })}
-	amount={nonNullish(value) ? BigNumber.from(value) : undefined}
+	amount={nonNullish(amount) ? BigNumber.from(amount) : undefined}
 	{type}
 	timestamp={Number(timestamp)}
 	{status}
