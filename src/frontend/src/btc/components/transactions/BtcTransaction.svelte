3dd7fab39c5186803191d47490f5bcd5e1f51adc
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2024-12-06 09:28:35 +0100

    fixes lint issues

--- src/frontend/src/btc/components/transactions/BtcTransaction.svelte
+++ src/frontend/src/btc/components/transactions/BtcTransaction.svelte
@@ -22,7 +22,7 @@
 	let label: string;
 	$: label = type === 'send' ? $i18n.send.text.send : $i18n.receive.text.receive;
 
-	let amount: BigNumber;
+	let amount: BigInt | undefined;
 	$: amount = nonNullish(value) ? (type === 'send' ? value * BigInt(-1) : value) : undefined;
 </script>
 
