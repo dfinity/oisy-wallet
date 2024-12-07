d07405f25b3a92c7ded7d0633b5db4c6548b4403
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-12-06 22:58:16 -0300

    fix(frontend): load Carousel when missing settings in user profile

--- src/frontend/src/lib/derived/user-profile.derived.ts
+++ src/frontend/src/lib/derived/user-profile.derived.ts
@@ -1,10 +1,14 @@
 import type { Settings } from '$declarations/backend/backend.did';
 import { userProfileStore } from '$lib/stores/user-profile.store';
+import type { Option } from '$lib/types/utils';
 import { fromNullable, nonNullish } from '@dfinity/utils';
 import { derived, type Readable } from 'svelte/store';
 
-export const userSettings: Readable<Settings | undefined> = derived(
+export const userSettings: Readable<Option<Settings>> = derived(
 	[userProfileStore],
 	([$userProfile]) =>
-		nonNullish($userProfile) ? fromNullable($userProfile.profile.settings) : undefined
+		// The user profile may or may not have the settings field.
+		// To distinguish between user profile not loaded and missing settings,
+		// we use `undefined` for the former and `null` for the latter.
+		nonNullish($userProfile) ? (fromNullable($userProfile.profile.settings) ?? null) : undefined
 );
