b152bb7cb27038599612ca2473118391cea5f10d
Robert Schlittler <43857623+loki344@users.noreply.github.com>
2024-12-10 15:48:26 +0100

    style(frontend): use content with toolbar in AddTokenForms (#3916)
    # Motivation
    
    Uniform display of modals
    
    # Changes
    
    use ContentWitToolbar
    
    # Tests
    
    before:
    
    <img width="586" alt="image"
    src="https://github.com/user-attachments/assets/ff167894-a3cc-42d5-a81b-fda56c4b24ca">
    
    <img width="580" alt="image"
    src="https://github.com/user-attachments/assets/cde7b9da-d014-4802-8bc9-4a79382b727e">
    
    
    
    after:
    
    <img width="633" alt="image"
    src="https://github.com/user-attachments/assets/2098bbb3-7342-40a4-8a81-2c0eea08a1bd">
    
    <img width="600" alt="image"
    src="https://github.com/user-attachments/assets/665209e0-df86-40d4-8234-892b9ab72b9e">

--- src/frontend/src/icp/components/tokens/IcAddTokenForm.svelte
+++ src/frontend/src/icp/components/tokens/IcAddTokenForm.svelte
@@ -7,27 +7,25 @@
 	export let indexCanisterId = '';
 </script>
 
-<div class="stretch">
-	<p class="mb-6">{$i18n.tokens.import.text.info}</p>
+<p class="mb-6">{$i18n.tokens.import.text.info}</p>
 
-	<label for="ledgerCanisterId" class="px-4.5 font-bold"
-		>{$i18n.tokens.import.text.ledger_canister_id}: <span class="text-blue-ribbon">*</span></label
-	>
-	<InputText
-		name="ledgerCanisterId"
-		bind:value={ledgerCanisterId}
-		placeholder="_____-_____-_____-_____-cai"
-	/>
+<label for="ledgerCanisterId" class="px-4.5 font-bold"
+	>{$i18n.tokens.import.text.ledger_canister_id}: <span class="text-blue-ribbon">*</span></label
+>
+<InputText
+	name="ledgerCanisterId"
+	bind:value={ledgerCanisterId}
+	placeholder="_____-_____-_____-_____-cai"
+/>
 
-	<label for="indexCanisterId" class="px-4.5 mt-6 block font-bold"
-		>{$i18n.tokens.import.text.index_canister_id}:</label
-	>
-	<InputText
-		name="indexCanisterId"
-		bind:value={indexCanisterId}
-		placeholder="_____-_____-_____-_____-cai"
-		required={false}
-	/>
+<label for="indexCanisterId" class="px-4.5 mt-6 block font-bold"
+	>{$i18n.tokens.import.text.index_canister_id}:</label
+>
+<InputText
+	name="indexCanisterId"
+	bind:value={indexCanisterId}
+	placeholder="_____-_____-_____-_____-cai"
+	required={false}
+/>
 
-	<p class="mb-6">{replaceOisyPlaceholders($i18n.tokens.import.text.info_index)}</p>
-</div>
+<p class="mb-6">{replaceOisyPlaceholders($i18n.tokens.import.text.info_index)}</p>
