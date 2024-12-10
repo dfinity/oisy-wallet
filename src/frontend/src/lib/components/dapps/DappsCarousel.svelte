43ac325e14d71f625fc888271db5563e65bcfcaa
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-12-10 11:53:17 -0300

    revert: hidden array

--- src/frontend/src/lib/components/dapps/DappsCarousel.svelte
+++ src/frontend/src/lib/components/dapps/DappsCarousel.svelte
@@ -39,6 +39,7 @@
 		const idx = dappsCarouselSlides.findIndex(({ id }) => id === dappId);
 
 		temporaryHiddenDappsIds = [...temporaryHiddenDappsIds, dappId];
+		hiddenDappsIds = [...hiddenDappsIds, dappId]
 
 		dappsCarouselSlides = filterCarouselDapps({ dAppDescriptions, hiddenDappsIds });
 
