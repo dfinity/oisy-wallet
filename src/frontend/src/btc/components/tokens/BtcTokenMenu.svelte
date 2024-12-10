58a0aeb343b77c0cbb4186b97a7d1edae9aeaf80
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2024-12-10 15:44:11 +0100

    clean up code

--- src/frontend/src/btc/components/tokens/BtcTokenMenu.svelte
+++ src/frontend/src/btc/components/tokens/BtcTokenMenu.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
-	import { nonNullish, notEmptyString } from '@dfinity/utils';
+	import { nonNullish } from '@dfinity/utils';
 	import { fade } from 'svelte/transition';
-	import { explorerUrl as explorerUrlStore } from '$btc/derived/network.derived';
 	import TokenMenu from '$lib/components/tokens/TokenMenu.svelte';
 	import ExternalLink from '$lib/components/ui/ExternalLink.svelte';
-	import { btcAddressMainnet } from '$lib/derived/address.derived';
 	import { i18n } from '$lib/stores/i18n.store';
+	import { token } from '$lib/stores/token.store';
 
 	let explorerUrl: string | undefined;
-	$: explorerUrl = notEmptyString($btcAddressMainnet)
-		? `${$explorerUrlStore}/address/${$btcAddressMainnet}`
-		: undefined;
+	$: explorerUrl = $token?.network.explorerUrl ?? undefined;
 </script>
 
 <TokenMenu testId="btc-token-menu">
--- src/frontend/src/btc/derived/network.derived.ts
+++
@@ -1,4 +0,0 @@
-import { BTC_MAINNET_NETWORK } from '$env/networks.env';
-import { derived, type Readable } from 'svelte/store';
-
-export const explorerUrl: Readable<string> = derived([], () => BTC_MAINNET_NETWORK.explorerUrl!);
