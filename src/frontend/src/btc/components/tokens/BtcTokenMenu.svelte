efdbc62a0aa9759ba40332add7f21b621ca7de2e
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2024-12-11 10:45:10 +0100

    fixes lint issue

--- src/frontend/src/btc/components/tokens/BtcTokenMenu.svelte
+++ src/frontend/src/btc/components/tokens/BtcTokenMenu.svelte
@@ -11,11 +11,12 @@
 	} from '$lib/stores/address.store';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { token } from '$lib/stores/token.store';
-	import type { BtcAddress, OptionBtcAddress } from '$lib/types/address';
+	import type { BtcAddress } from '$lib/types/address';
+	import type { Option } from '$lib/types/utils';
 	import { mapAddress } from '$lib/utils/address.utils';
 	import { isNetworkIdBTCMainnet, isNetworkIdBTCTestnet } from '$lib/utils/network.utils';
 
-	let btcAddress: OptionBtcAddress<BtcAddress>;
+	let btcAddress: Option<string>;
 	$: btcAddress = isNetworkIdBTCMainnet($networkId)
 		? mapAddress<BtcAddress>($btcAddressMainnetStore)
 		: isNetworkIdBTCTestnet($networkId)
