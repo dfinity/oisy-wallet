4e8e99afc8c9a56fde6ba7a7e06e522089c82603
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-12-11 04:22:02 -0300

    rename

--- src/frontend/src/env/networks.sol.env.ts
+++ src/frontend/src/env/networks.sol.env.ts
@@ -9,9 +9,9 @@ import { parseNetworkId } from '$lib/validation/network.validation';
  * SOL
  */
 
-export const SOLANA_NETWORK_SYMBOL = 'SOL';
+export const SOLANA_MAINNET_NETWORK_SYMBOL = 'SOL';
 
-export const SOLANA_MAINNET_NETWORK_ID: NetworkId = parseNetworkId(SOLANA_NETWORK_SYMBOL);
+export const SOLANA_MAINNET_NETWORK_ID: NetworkId = parseNetworkId(SOLANA_MAINNET_NETWORK_SYMBOL);
 
 export const SOLANA_MAINNET_NETWORK: Network = {
 	id: SOLANA_MAINNET_NETWORK_ID,
