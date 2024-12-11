0c93c930ae17ccafa2875d2eed4eaf54e41b78d7
Robert Schlittler <robert.schlittler@inacta.ch>
2024-12-11 08:55:15 +0100

    style(frontend): MR feedback

--- src/frontend/src/lib/components/ui/ContentWithToolbar.svelte
+++ src/frontend/src/lib/components/ui/ContentWithToolbar.svelte
@@ -8,6 +8,6 @@
 
 <slot name="outer-content" />
 
-<div class="toolbar">
+<div class="sticky bottom-0 left-0 w-full px-3 md:px-6 pb-4 md:pb-6 pt-4 md:pt-5 bg-white border-t border-secondary">
 	<slot name="toolbar" />
 </div>
