8950720e07d574a9497fe533e8ab7b211059d327
David Dal Busco <david.dalbusco@dfinity.org>
2024-12-11 10:18:41 +0100

    feat(frontend): listener templating (#3937)
    # Motivation
    
    Migration to Svelte v5 (#3929) involves replacing the ComponentType.
    While updating the Listener, a minor TypeScript issue was identified. We
    use a `svelte:component` that maps a token property, which may or may
    not be present, and can sometimes be set or unset. This approach works
    because the component is assigned to a state variable, but it is not
    fully TypeScript-compliant. To address this, we can leverage the
    template to correctly mount the listener.

--- src/frontend/src/lib/components/core/Listener.svelte
+++ src/frontend/src/lib/components/core/Listener.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { isNullish } from '@dfinity/utils';
-	import type { ComponentType } from 'svelte';
 	import BitcoinListener from '$btc/components/core/BitcoinListener.svelte';
 	import EthListener from '$eth/components/core/EthListener.svelte';
 	import NoListener from '$lib/components/core/NoListener.svelte';
@@ -9,18 +8,22 @@
 	import { isNetworkIdBitcoin, isNetworkIdICP } from '$lib/utils/network.utils';
 
 	export let token: OptionToken;
-
-	let cmp: ComponentType;
-	$: cmp =
-		isNullish(token) || $authNotSignedIn
-			? NoListener
-			: isNetworkIdICP(token.network.id)
-				? NoListener
-				: isNetworkIdBitcoin(token.network.id)
-					? BitcoinListener
-					: EthListener;
 </script>
 
-<svelte:component this={cmp} {token}>
-	<slot />
-</svelte:component>
+{#if isNullish(token) || $authNotSignedIn}
+	<NoListener>
+		<slot />
+	</NoListener>
+{:else if isNetworkIdICP(token.network.id)}
+	<NoListener>
+		<slot />
+	</NoListener>
+{:else if isNetworkIdBitcoin(token.network.id)}
+	<BitcoinListener>
+		<slot />
+	</BitcoinListener>
+{:else}
+	<EthListener {token}>
+		<slot />
+	</EthListener>
+{/if}
--- src/frontend/src/lib/components/core/NoListener.svelte
+++ src/frontend/src/lib/components/core/NoListener.svelte
@@ -1,7 +1,1 @@
-<script lang="ts">
-	import type { OptionToken } from '$lib/types/token';
-
-	export const token: OptionToken = undefined;
-</script>
-
 <slot />
