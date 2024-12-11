9c13c8734541fb24b9961ba77b63dad0c3bb2226
David Dal Busco <david.dalbusco@outlook.com>
2024-12-11 10:19:53 +0100

    feat(frontend): remove no listener

--- src/frontend/src/lib/components/core/Listener.svelte
+++ src/frontend/src/lib/components/core/Listener.svelte
@@ -2,7 +2,6 @@
 	import { isNullish } from '@dfinity/utils';
 	import BitcoinListener from '$btc/components/core/BitcoinListener.svelte';
 	import EthListener from '$eth/components/core/EthListener.svelte';
-	import NoListener from '$lib/components/core/NoListener.svelte';
 	import { authNotSignedIn } from '$lib/derived/auth.derived';
 	import type { OptionToken } from '$lib/types/token';
 	import { isNetworkIdBitcoin, isNetworkIdICP } from '$lib/utils/network.utils';
@@ -11,13 +10,9 @@
 </script>
 
 {#if isNullish(token) || $authNotSignedIn}
-	<NoListener>
-		<slot />
-	</NoListener>
+	<slot />
 {:else if isNetworkIdICP(token.network.id)}
-	<NoListener>
-		<slot />
-	</NoListener>
+	<slot />
 {:else if isNetworkIdBitcoin(token.network.id)}
 	<BitcoinListener>
 		<slot />
