088c22b42eb618bed7d85635af64ca1c3b68e652
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-12-11 09:39:53 -0300

    feat(frontend): add Solana networks (#3921)
    # Motivation
    
    We include the Solana networks:
    
    - Mainnet
    - Testnet
    - Devnet
    - Local

--- src/frontend/src/env/networks.sol.env.ts
+++
@@ -1,3 +0,0 @@
-// TODO: to be removed when the feature is fully implemented
-export const SOLANA_NETWORK_ENABLED =
-	JSON.parse(import.meta.env.VITE_SOLANA_NETWORK_ENABLED ?? false) === true;
---
+++ src/frontend/src/env/networks/networks.sol.env.ts
@@ -0,0 +1,63 @@
+import solDevnetIconBW from '$lib/assets/networks/sol-devnet-bw.svg';
+import solLocalnetIconBW from '$lib/assets/networks/sol-localnet-bw.svg';
+import solMainnetIconBW from '$lib/assets/networks/sol-mainnet-bw.svg';
+import solTestnetIconBW from '$lib/assets/networks/sol-testnet-bw.svg';
+import sol from '$lib/assets/networks/sol.svg';
+import type { Network, NetworkId } from '$lib/types/network';
+import { parseNetworkId } from '$lib/validation/network.validation';
+
+/**
+ * SOL
+ */
+
+export const SOLANA_MAINNET_NETWORK_SYMBOL = 'SOL';
+
+export const SOLANA_MAINNET_NETWORK_ID: NetworkId = parseNetworkId(SOLANA_MAINNET_NETWORK_SYMBOL);
+
+export const SOLANA_MAINNET_NETWORK: Network = {
+	id: SOLANA_MAINNET_NETWORK_ID,
+	env: 'mainnet',
+	name: 'Solana Mainnet Beta',
+	icon: sol,
+	iconBW: solMainnetIconBW
+};
+
+export const SOLANA_TESTNET_NETWORK_SYMBOL = 'SOL (Testnet)';
+
+export const SOLANA_TESTNET_NETWORK_ID: NetworkId = parseNetworkId(SOLANA_TESTNET_NETWORK_SYMBOL);
+
+export const SOLANA_TESTNET_NETWORK: Network = {
+	id: SOLANA_TESTNET_NETWORK_ID,
+	env: 'testnet',
+	name: 'Solana Testnet',
+	icon: sol,
+	iconBW: solTestnetIconBW
+};
+
+export const SOLANA_DEVNET_NETWORK_SYMBOL = 'SOL (Devnet)';
+
+export const SOLANA_DEVNET_NETWORK_ID: NetworkId = parseNetworkId(SOLANA_DEVNET_NETWORK_SYMBOL);
+
+export const SOLANA_DEVNET_NETWORK: Network = {
+	id: SOLANA_DEVNET_NETWORK_ID,
+	env: 'testnet',
+	name: 'Solana Devnet',
+	icon: sol,
+	iconBW: solDevnetIconBW
+};
+
+export const SOLANA_LOCAL_NETWORK_SYMBOL = 'SOL (Local)';
+
+export const SOLANA_LOCAL_NETWORK_ID: NetworkId = parseNetworkId(SOLANA_LOCAL_NETWORK_SYMBOL);
+
+export const SOLANA_LOCAL_NETWORK: Network = {
+	id: SOLANA_LOCAL_NETWORK_ID,
+	env: 'testnet',
+	name: 'Solana Local',
+	icon: sol,
+	iconBW: solLocalnetIconBW
+};
+
+// TODO: to be removed when the feature is fully implemented
+export const SOLANA_NETWORK_ENABLED =
+	JSON.parse(import.meta.env.VITE_SOLANA_NETWORK_ENABLED ?? false) === true;
