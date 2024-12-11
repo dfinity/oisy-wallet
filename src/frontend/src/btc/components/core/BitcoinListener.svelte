1cfaf95ee72cf52e5eb324f6013f57c76e59e107
David Dal Busco <david.dalbusco@outlook.com>
2024-12-11 10:42:34 +0100

    feat: use const

--- src/frontend/src/btc/components/core/BitcoinListener.svelte
+++ src/frontend/src/btc/components/core/BitcoinListener.svelte
@@ -1,9 +1,8 @@
 <!-- TODO: Implement https://dfinity.atlassian.net/browse/GIX-2884 -->
 <script lang="ts">
-	import type { Token } from '$lib/types/token';
+	import type { OptionToken } from '$lib/types/token';
 
-	// eslint-disable-next-line svelte/valid-compile
-	export let token: Token;
+	export const token: OptionToken = undefined;
 </script>
 
 <slot />
