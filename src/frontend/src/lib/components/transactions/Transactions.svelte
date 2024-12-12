8da99d9e8857b89f2398a0e3614742d295662d8f
Robert Schlittler <robert.schlittler@inacta.ch>
2024-12-12 15:19:54 +0100

    feat(frontend): refactor

--- src/frontend/src/lib/components/transactions/Transactions.svelte
+++ src/frontend/src/lib/components/transactions/Transactions.svelte
@@ -1,54 +1,48 @@
 <script lang="ts">
+	import type { Identity } from '@dfinity/agent';
 	import { isNullish, nonNullish } from '@dfinity/utils';
+	import TransactionsSkeletons from './TransactionsSkeletons.svelte';
 	import BtcTransactions from '$btc/components/transactions/BtcTransactions.svelte';
 	import EthTransactions from '$eth/components/transactions/EthTransactions.svelte';
+	import { loadUserTokens } from '$eth/services/erc20.services';
+	import { icTokenErc20UserToken } from '$eth/utils/erc20.utils';
 	import IcTransactions from '$icp/components/transactions/IcTransactions.svelte';
+	import { loadCustomTokens } from '$icp/services/icrc.services';
+	import { icTokenIcrcCustomToken } from '$icp/utils/icrc.utils';
+	import { setCustomToken } from '$icp-eth/services/custom-token.services';
+	import { setUserToken } from '$icp-eth/services/user-token.services';
+	import Header from '$lib/components/ui/Header.svelte';
+	import { allTokens } from '$lib/derived/all-tokens.derived';
+	import { authIdentity } from '$lib/derived/auth.derived';
 	import { routeNetwork, routeToken } from '$lib/derived/nav.derived';
 	import { networkBitcoin, networkICP } from '$lib/derived/network.derived';
 	import { pageToken } from '$lib/derived/page-token.derived';
-	import { allTokens } from '$lib/derived/all-tokens.derived';
-	import { icTokenErc20UserToken } from '$eth/utils/erc20.utils';
-	import { authIdentity } from '$lib/derived/auth.derived';
-	import { setUserToken } from '$icp-eth/services/user-token.services';
 	import { i18n } from '$lib/stores/i18n.store';
-	import { loadUserTokens } from '$eth/services/erc20.services';
-	import type { OptionToken, Token } from '$lib/types/token';
-	import { icTokenIcrcCustomToken } from '$icp/utils/icrc.utils';
-	import { setCustomToken } from '$icp-eth/services/custom-token.services';
-	import { loadCustomTokens } from '$icp/services/icrc.services';
-	import { onMount } from 'svelte';
-	import Header from '../ui/Header.svelte';
-	import TransactionsSkeletons from './TransactionsSkeletons.svelte';
 	import { toastsError } from '$lib/stores/toasts.store';
-	import type { Identity } from '@dfinity/agent';
+	import type { OptionToken, Token } from '$lib/types/token';
 
-	let availableTokenIds: string[] = [];
-	$: availableTokenIds = $allTokens.map(token => token.name);
 	let autoEnabledToken = false;
 	let isLoading = false;
 	let token: OptionToken;
 	$: token = $allTokens.find(token => token.name === $routeToken);
 
-	const isValidVersion = (token: { version?: bigint }): boolean => {
-		return nonNullish(token.version) && token.version >= 0n;
-	};
+	const isValidVersion = (token: { version?: bigint }): boolean => nonNullish(token.version) && token.version >= 0n;
 
 	const tryEnableToken = async (token: Token) => {
-		if (autoEnabledToken) return;
+		if (autoEnabledToken) {return;}
 
 		isLoading = true;
-		console.log(`Enabling token: ${token.name}`);
 
 		try {
 			const identity = $authIdentity;
-			if (!identity) throw new Error('No identity found');
+			if (!identity) {throw new Error('No identity found');}
 
 			if (icTokenErc20UserToken(token) || icTokenIcrcCustomToken(token)) {
 				if (!isValidVersion(token)) {
 					return;
 				}
 
-				await enableToken(token, identity);
+				await enableToken({ token, identity });
 			} else {
 				console.error(`Unknown token type: ${token.name}`);
 				throw new Error(`Unknown token type ${token.name}`);
@@ -61,7 +55,7 @@
 		}
 	};
 
-	const enableToken = async (token: Token, identity: Identity) => {
+	const enableToken = async ({ token, identity }: { token: Token, identity: Identity }) => {
 		if (icTokenErc20UserToken(token)) {
 			autoEnabledToken = true;
 			await setUserToken({ identity, token, enabled: true });
