9eec997ac6ce0baf0aba5f8af170d2f15d748af4
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-12-12 12:10:52 -0300

    feat: send components for Solana

---
+++ src/frontend/src/sol/components/send/SolSendDestination.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import SendInputDestination from '$lib/components/send/SendInputDestination.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
+	import type { NetworkId } from '$lib/types/network';
+	import { isNetworkIdSolana } from '$lib/utils/network.utils';
+	import { isInvalidDestinationBtc } from '$lib/utils/send.utils';
+
+	export let destination = '';
+	export let networkId: NetworkId | undefined = undefined;
+	export let invalidDestination = false;
+
+	const dispatch = createEventDispatcher();
+
+	let isInvalidDestination: () => boolean;
+	// TODO: create check for invalid destination
+	$: isInvalidDestination = (): boolean => isNetworkIdSolana(networkId);
+</script>
+
+<SendInputDestination
+	bind:destination
+	bind:invalidDestination
+	{isInvalidDestination}
+	inputPlaceholder={$i18n.send.placeholder.enter_recipient_address}
+	on:icQRCodeScan
+	onQRButtonClick={() => dispatch('icQRCodeScan')}
+/>
