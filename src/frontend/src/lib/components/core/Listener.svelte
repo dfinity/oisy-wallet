dc4d43b1bb1bfaa64cab970c6070bfafb06bfe47
Antonio Ventilii <antonio.ventilii@dfinity.org>
2024-12-13 02:45:04 -0300

    feat(frontend): no Listener if no known network (#3960)
    # Motivation
    
    We are integrating more networks. To be on the safe side and avoiding
    raising possible mismatches, we make the listener fallback to nothing,
    if the network is not known.

--- src/frontend/src/lib/components/core/Listener.svelte
+++ src/frontend/src/lib/components/core/Listener.svelte
@@ -4,7 +4,11 @@
 	import EthListener from '$eth/components/core/EthListener.svelte';
 	import { authNotSignedIn } from '$lib/derived/auth.derived';
 	import type { OptionToken } from '$lib/types/token';
-	import { isNetworkIdBitcoin, isNetworkIdICP } from '$lib/utils/network.utils';
+	import {
+		isNetworkIdBitcoin,
+		isNetworkIdEthereum,
+		isNetworkIdICP
+	} from '$lib/utils/network.utils';
 
 	export let token: OptionToken;
 </script>
@@ -17,8 +21,10 @@
 	<BitcoinListener>
 		<slot />
 	</BitcoinListener>
-{:else}
+{:else if isNetworkIdEthereum(token.network.id)}
 	<EthListener {token}>
 		<slot />
 	</EthListener>
+{:else}
+	<slot />
 {/if}
