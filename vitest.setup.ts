f697a2ce6f66331abfd7040e253e8f7834185dc3
David Dal Busco <david.dalbusco@outlook.com>
2024-12-16 06:48:00 +0100

    chore: more resize observer for tests

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -4,6 +4,20 @@ import 'fake-indexeddb/auto';
 import { vi } from 'vitest';
 import { mockPage } from './src/frontend/src/tests/mocks/page.store.mock';
 
+// We mock ResizeObserver and element.animate because neither JSDOM nor Happy DOM supports them, while Svelte v5 requires them.
+// Interesting related thread: https://github.com/testing-library/svelte-testing-library/issues/284
+global.ResizeObserver = class ResizeObserver {
+	observe() {
+		// do nothing
+	}
+	unobserve() {
+		// do nothing
+	}
+	disconnect() {
+		// do nothing
+	}
+};
+
 vi.mock('$app/stores', () => ({
 	page: mockPage
 }));
