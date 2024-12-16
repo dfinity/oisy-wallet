1d7f8c1d3513708cc17ce63e2d2265a7c28f787a
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2024-12-16 14:58:43 +0100

    resolves lint issue

--- src/frontend/src/lib/components/core/Menu.svelte
+++ src/frontend/src/lib/components/core/Menu.svelte
@@ -128,7 +128,7 @@
 		{/if}
 
 		{#if isVip}
-			<ButtonMenu ariaLabel={$i18n.navigation.alt.vip_qr_code} on:click={openVipQrCode}>
+			<ButtonMenu ariaLabel={$i18n.navigation.alt.vip_qr_code} on:click={() => console.log("openModal")}>
 				<IconVipQr size="20" />
 				{$i18n.navigation.text.vip_qr_code}
 			</ButtonMenu>
