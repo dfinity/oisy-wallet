8d46390f0ffc6b6a01361c36ece8b87f0c6f4857
Robert Schlittler <43857623+loki344@users.noreply.github.com>
2024-12-17 11:05:27 +0100

    feat(frontend): add sol to idb.api (#3995)
    # Motivation
    
    Add Solana address in the IDB functions. Use it in the auth.service.
    
    # Changes
    
    - Add solana
    - Add tests for all networks
    - Add clean up in auth.service for sol
    
    # Tests
    
    Unit testing added
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/api/idb.api.ts
+++ src/frontend/src/lib/api/idb.api.ts
@@ -1,8 +1,19 @@
 import { browser } from '$app/environment';
 import { ETHEREUM_NETWORK_SYMBOL } from '$env/networks/networks.env';
+import {
+	SOLANA_DEVNET_NETWORK_SYMBOL,
+	SOLANA_LOCAL_NETWORK_SYMBOL,
+	SOLANA_MAINNET_NETWORK_SYMBOL,
+	SOLANA_TESTNET_NETWORK_SYMBOL
+} from '$env/networks/networks.sol.env';
 import { BTC_MAINNET_SYMBOL, BTC_TESTNET_SYMBOL } from '$env/tokens/tokens.btc.env';
-import type { BtcAddress, EthAddress } from '$lib/types/address';
-import type { IdbBtcAddress, IdbEthAddress, SetIdbAddressParams } from '$lib/types/idb';
+import type { BtcAddress, EthAddress, SolAddress } from '$lib/types/address';
+import type {
+	IdbBtcAddress,
+	IdbEthAddress,
+	IdbSolAddress,
+	SetIdbAddressParams
+} from '$lib/types/idb';
 import type { Principal } from '@dfinity/principal';
 import { isNullish } from '@dfinity/utils';
 import { createStore, del, get, set, update, type UseStore } from 'idb-keyval';
@@ -15,6 +26,11 @@ const idbBtcAddressesStoreMainnet = idbAddressesStore(BTC_MAINNET_SYMBOL.toLower
 const idbBtcAddressesStoreTestnet = idbAddressesStore(BTC_TESTNET_SYMBOL.toLowerCase());
 
 const idbEthAddressesStore = idbAddressesStore(ETHEREUM_NETWORK_SYMBOL.toLowerCase());
+
+const idbSolAddressesStoreMainnet = idbAddressesStore(SOLANA_MAINNET_NETWORK_SYMBOL.toLowerCase());
+const idbSolAddressesStoreTestnet = idbAddressesStore(SOLANA_TESTNET_NETWORK_SYMBOL.toLowerCase());
+const idbSolAddressesStoreDevnet = idbAddressesStore(SOLANA_DEVNET_NETWORK_SYMBOL.toLowerCase());
+const idbSolAddressesStoreLocal = idbAddressesStore(SOLANA_LOCAL_NETWORK_SYMBOL.toLowerCase());
 
 export const setIdbBtcAddressMainnet = ({
 	address,
@@ -33,6 +49,30 @@ export const setIdbEthAddress = ({
 	principal
 }: SetIdbAddressParams<EthAddress>): Promise<void> =>
 	set(principal.toText(), address, idbEthAddressesStore);
+
+export const setIdbSolAddressMainnet = ({
+	address,
+	principal
+}: SetIdbAddressParams<SolAddress>): Promise<void> =>
+	set(principal.toText(), address, idbSolAddressesStoreMainnet);
+
+export const setIdbSolAddressTestnet = ({
+	address,
+	principal
+}: SetIdbAddressParams<SolAddress>): Promise<void> =>
+	set(principal.toText(), address, idbSolAddressesStoreTestnet);
+
+export const setIdbSolAddressDevnet = ({
+	address,
+	principal
+}: SetIdbAddressParams<SolAddress>): Promise<void> =>
+	set(principal.toText(), address, idbSolAddressesStoreDevnet);
+
+export const setIdbSolAddressLocal = ({
+	address,
+	principal
+}: SetIdbAddressParams<SolAddress>): Promise<void> =>
+	set(principal.toText(), address, idbSolAddressesStoreLocal);
 
 const updateIdbAddressLastUsage = ({
 	principal,
@@ -62,14 +102,23 @@ export const updateIdbBtcAddressMainnetLastUsage = (principal: Principal): Promi
 export const updateIdbEthAddressLastUsage = (principal: Principal): Promise<void> =>
 	updateIdbAddressLastUsage({ principal, idbAddressesStore: idbEthAddressesStore });
 
+export const updateIdbSolAddressMainnetLastUsage = (principal: Principal): Promise<void> =>
+	updateIdbAddressLastUsage({ principal, idbAddressesStore: idbSolAddressesStoreMainnet });
+
 export const getIdbBtcAddressMainnet = (principal: Principal): Promise<IdbBtcAddress | undefined> =>
 	get(principal.toText(), idbBtcAddressesStoreMainnet);
 
 export const getIdbEthAddress = (principal: Principal): Promise<IdbEthAddress | undefined> =>
 	get(principal.toText(), idbEthAddressesStore);
 
+export const getIdbSolAddressMainnet = (principal: Principal): Promise<IdbSolAddress | undefined> =>
+	get(principal.toText(), idbSolAddressesStoreMainnet);
+
 export const deleteIdbBtcAddressMainnet = (principal: Principal): Promise<void> =>
 	del(principal.toText(), idbBtcAddressesStoreMainnet);
 
 export const deleteIdbEthAddress = (principal: Principal): Promise<void> =>
 	del(principal.toText(), idbEthAddressesStore);
+
+export const deleteIdbSolAddressMainnet = (principal: Principal): Promise<void> =>
+	del(principal.toText(), idbSolAddressesStoreMainnet);
