0db17fe20a17bdaf6da64bf74b8502a2e3d95628
Robert Schlittler <robert.schlittler@inacta.ch>
2024-12-18 21:40:16 +0100

    feat(frontend): implement SolLoaderWallets.svelte and add it to  LoaderWallets

---
+++ src/frontend/src/sol/components/core/SolLoaderWallets.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import WalletWorkers from '$lib/components/core/WalletWorkers.svelte';
+	import { enabledSplTokens } from '$lib/derived/tokens.derived';
+	import type { InitWalletWorkerFn } from '$lib/types/listener';
+	import { initSolWalletWorker } from '$sol/services/worker.sol-wallet.services';
+
+
+	const initWalletWorker: InitWalletWorkerFn = ({ token }) =>
+		initSolWalletWorker({ token });
+</script>
+
+<WalletWorkers tokens={$enabledSplTokens} {initWalletWorker}>
+	<slot />
+</WalletWorkers> 
