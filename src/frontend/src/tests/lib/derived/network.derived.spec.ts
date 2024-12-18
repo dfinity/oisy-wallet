badc308a0844cc5f95aefcf865b62a6ee121ad54
Robert Schlittler <43857623+loki344@users.noreply.github.com>
2024-12-18 09:04:29 +0100

    feat(frontend): add tests for networkAddress (#4005)
    # Motivation
    
    network.derived.ts derived networkAddress is currently misleading as it
    only takes into account icp and eth as a network. Implementing SOL as a
    further network I want to consolidate the logic spread in the different
    receive modal components to evaluate the current address for the
    network.
    
    As a first step, we add unit tests to ensure the current functionality.
    
    # Changes
    
    - Add unit tests for networkAddress
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/frontend/src/tests/lib/derived/network.derived.spec.ts
@@ -0,0 +1,44 @@
+import { getIcrcAccount } from '$icp/utils/icrc-account.utils';
+import { networkAddress } from '$lib/derived/network.derived';
+import { ethAddressStore } from '$lib/stores/address.store';
+import { authStore } from '$lib/stores/auth.store';
+import { mockEthAddress } from '$tests/mocks/eth.mocks';
+import { mockIdentity } from '$tests/mocks/identity.mock';
+import { mockPage } from '$tests/mocks/page.store.mock';
+import { encodeIcrcAccount } from '@dfinity/ledger-icrc';
+import { get } from 'svelte/store';
+import { beforeEach, describe, expect, test, vi } from 'vitest';
+
+describe('network.derived', () => {
+	beforeEach(() => {
+		vi.clearAllMocks();
+		ethAddressStore.reset();
+		mockPage.reset();
+		authStore.setForTesting(mockIdentity);
+		ethAddressStore.set(mockEthAddressWithCertified);
+	});
+
+	const mockEthAddressWithCertified = {
+		data: mockEthAddress,
+		certified: true
+	};
+
+	const expectedIcrcAddress = encodeIcrcAccount(getIcrcAccount(mockIdentity.getPrincipal()));
+
+	test.each([
+		{
+			network: 'ICP',
+			expectedAddress: expectedIcrcAddress
+		},
+		{
+			network: 'ETH',
+			expectedAddress: mockEthAddress
+		}
+	])('returns $network address when network is $network', ({ network, expectedAddress }) => {
+		mockPage.mock({ network });
+
+		const actualAddress = get(networkAddress);
+
+		expect(actualAddress).toBe(expectedAddress);
+	});
+});
