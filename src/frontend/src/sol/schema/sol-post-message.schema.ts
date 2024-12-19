687e50709e816aad7911a2204dde5a8b4f3ee79b
Robert Schlittler <robert.schlittler@inacta.ch>
2024-12-19 14:04:04 +0100

    feat(frontend): change balance from bigint to bignumber

--- src/frontend/src/sol/schema/sol-post-message.schema.ts
+++ src/frontend/src/sol/schema/sol-post-message.schema.ts
@@ -4,9 +4,10 @@ import {
 } from '$lib/schema/post-message.schema';
 import type { CertifiedData } from '$lib/types/store';
 import { z } from 'zod';
+import type { BigNumber } from '@ethersproject/bignumber';
 
 const SolPostMessageWalletDataSchema = z.object({
-	balance: z.custom<CertifiedData<bigint | null>>(),
+	balance: z.custom<CertifiedData<BigNumber | null>>(),
 	newTransactions: JsonTransactionsTextSchema
 });
 
