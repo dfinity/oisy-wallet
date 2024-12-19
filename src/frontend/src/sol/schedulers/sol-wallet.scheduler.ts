b4e4caaad6a026b60f87c351c0456292d6da4b69
Robert Schlittler <robert.schlittler@inacta.ch>
2024-12-19 14:04:19 +0100

    feat(frontend): implement sol wallet worker

---
+++ src/frontend/src/sol/schedulers/sol-wallet.scheduler.ts
@@ -0,0 +1,114 @@
+import { WALLET_TIMER_INTERVAL_MILLIS } from '$lib/constants/app.constants';
+import { type Scheduler, type SchedulerJobData, SchedulerTimer } from '$lib/schedulers/scheduler';
+import type { SolAddress } from '$lib/types/address';
+import type { Balance } from '$lib/types/balance';
+import type {
+	PostMessageDataRequestSol,
+	PostMessageDataResponseError
+} from '$lib/types/post-message';
+import type { CertifiedData } from '$lib/types/store';
+import type { SolPostMessageDataResponseWallet } from '$sol/types/sol-post-message';
+import { assertNonNullish, isNullish } from '@dfinity/utils';
+import { loadSolBalance } from '$sol/api/solana.api';
+import type { NetworkId } from '$lib/types/network';
+
+interface LoadSolWalletParams {
+	networkId: NetworkId;
+	address: SolAddress;
+}
+
+interface SolWalletStore {
+	balance: CertifiedData<Balance | null> | undefined;
+}
+
+interface SolWalletData {
+	balance: CertifiedData<Balance | null>;
+}
+
+export class SolWalletScheduler implements Scheduler<PostMessageDataRequestSol> {
+	private timer = new SchedulerTimer('syncSolWalletStatus');
+
+	private store: SolWalletStore = {
+		balance: undefined
+	};
+
+	stop() {
+		this.timer.stop();
+	}
+
+	async start(data: PostMessageDataRequestSol | undefined) {
+		await this.timer.start<PostMessageDataRequestSol>({
+			interval: WALLET_TIMER_INTERVAL_MILLIS,
+			job: this.syncWallet,
+			data
+		});
+	}
+
+	async trigger(data: PostMessageDataRequestSol | undefined) {
+		await this.timer.trigger<PostMessageDataRequestSol>({
+			job: this.syncWallet,
+			data
+		});
+	}
+
+	private loadBalance = async (
+		params: LoadSolWalletParams
+	): Promise<CertifiedData<Balance | null>> => ({
+		data: await loadSolBalance(params),
+		certified: true
+	});
+
+	private syncWallet = async ({ data }: SchedulerJobData<PostMessageDataRequestSol>) => {
+		assertNonNullish(data, 'No data provided to get Solana balance.');
+
+		try {
+			const balance = await this.loadBalance({
+				address: data.address.data,
+				networkId: data.networkId
+			});
+
+			this.syncWalletData({ response: { balance } });
+		} catch (error: unknown) {
+			this.postMessageWalletError({ error });
+		}
+	};
+
+	private syncWalletData = ({ response: { balance } }: { response: SolWalletData }) => {
+		const newBalance =
+			isNullish(this.store.balance) ||
+			this.store.balance.data !== balance.data ||
+			(!this.store.balance.certified && balance.certified);
+
+		this.store = {
+			...this.store,
+			...(newBalance && { balance })
+		};
+
+		if (!newBalance) {
+			return;
+		}
+
+		this.postMessageWallet({
+			wallet: {
+				balance,
+				newTransactions: ''
+			}
+		});
+	};
+
+	private postMessageWallet(data: SolPostMessageDataResponseWallet) {
+		this.timer.postMsg<SolPostMessageDataResponseWallet>({
+			msg: 'syncSolWallet',
+			data
+		});
+	}
+
+	protected postMessageWalletError({ error }: { error: unknown }) {
+		this.timer.postMsg<PostMessageDataResponseError>({
+			msg: 'syncSolWalletError',
+			data: {
+				error
+			}
+		});
+	}
+}
