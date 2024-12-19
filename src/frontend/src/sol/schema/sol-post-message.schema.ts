d3d265747729e3dffae02e3013c388bd4cb78a4e
Robert Schlittler <robert.schlittler@inacta.ch>
2024-12-19 17:35:00 +0100

    feat(frontend): Bignumber => bigint

--- src/frontend/src/sol/schema/sol-post-message.schema.ts
+++ src/frontend/src/sol/schema/sol-post-message.schema.ts
@@ -1,13 +1,9 @@
-import {
-	JsonTransactionsTextSchema,
-	PostMessageDataResponseSchema
-} from '$lib/schema/post-message.schema';
+import { JsonTransactionsTextSchema, PostMessageDataResponseSchema } from '$lib/schema/post-message.schema';
 import type { CertifiedData } from '$lib/types/store';
 import { z } from 'zod';
-import type { BigNumber } from '@ethersproject/bignumber';
 
 const SolPostMessageWalletDataSchema = z.object({
-	balance: z.custom<CertifiedData<BigNumber | null>>(),
+	balance: z.custom<CertifiedData<bigint | null>>(),
 	newTransactions: JsonTransactionsTextSchema
 });
 
