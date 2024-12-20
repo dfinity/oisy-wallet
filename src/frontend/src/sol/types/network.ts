dd3b2bf0d22b6648dfe83e01921a2e2b66312ce6
Robert Schlittler <43857623+loki344@users.noreply.github.com>
2024-12-20 11:01:18 +0100

    feat(frontend): adjust SOL typing (#4033)
    # Motivation
    
    adjust sol typing to conform with continous implementation.
    
    # Changes
    
    - separate rpc urls from token network
    - introduce mapping function to map from networkId to network
    - improve enum typing with zod
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/sol/types/network.ts
+++ src/frontend/src/sol/types/network.ts
@@ -1,11 +1,10 @@
-import type { SolNetworkSchema } from '$sol/schema/network.schema';
+import { SolRpcConnectionConfigSchema } from '$sol/schema/network.schema';
 import { z } from 'zod';
 
-export type SolNetwork = z.infer<typeof SolNetworkSchema>;
+export type SolRpcConnectionConfig = z.infer<typeof SolRpcConnectionConfigSchema>;
 
-export enum SolanaNetworks {
-	MAINNET = 'mainnet',
-	TESTNET = 'testnet',
-	DEVNET = 'devnet',
-	LOCAL = 'local'
-}
+export const SolanaNetworkSchema = z.enum(['mainnet', 'testnet', 'devnet', 'local']);
+
+export type SolanaNetworkType = z.infer<typeof SolanaNetworkSchema>;
+
+export const SolanaNetworks = SolanaNetworkSchema.enum;
