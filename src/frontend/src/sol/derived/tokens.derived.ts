341f9d534d6c51ce11716742c1e598f45e3feab8
Robert Schlittler <43857623+loki344@users.noreply.github.com>
2024-12-30 07:00:53 +0100

    feat(frontend): make solana native tokens requiredtokens (#4040)
    # Motivation
    
    To align with the other implementations, let's make Solana native tokens
    of type RequiredToken.
    
    # Changes
    
    adjust type
    
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

--- src/frontend/src/sol/derived/tokens.derived.ts
+++ src/frontend/src/sol/derived/tokens.derived.ts
@@ -7,10 +7,10 @@ import {
 } from '$env/tokens/tokens.sol.env';
 import { LOCAL } from '$lib/constants/app.constants';
 import { testnets } from '$lib/derived/testnets.derived';
-import type { Token } from '$lib/types/token';
+import type { RequiredToken } from '$lib/types/token';
 import { derived, type Readable } from 'svelte/store';
 
-export const enabledSolanaTokens: Readable<Token[]> = derived([testnets], ([$testnets]) =>
+export const enabledSolanaTokens: Readable<RequiredToken[]> = derived([testnets], ([$testnets]) =>
 	SOLANA_NETWORK_ENABLED
 		? [
 				SOLANA_TOKEN,
