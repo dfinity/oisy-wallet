304bb05c2d0e165116ea1cce471affdca2cf4d40
David Dal Busco <david.dalbusco@outlook.com>
2025-01-06 12:56:36 +0100

    fix: redo test

--- src/frontend/src/lib/components/signer/SignerConsentMessageWarning.svelte
+++ src/frontend/src/lib/components/signer/SignerConsentMessageWarning.svelte
@@ -2,7 +2,7 @@
 	import type { ResultConsentInfo } from '@dfinity/oisy-wallet-signer';
 	import { nonNullish } from '@dfinity/utils';
 	import MessageBox from '$lib/components/ui/MessageBox.svelte';
-	import {i18n} from "$lib/stores/i18n.store";
+	import { i18n } from '$lib/stores/i18n.store';
 
 	export let consentInfo: ResultConsentInfo | undefined;
 
@@ -12,7 +12,7 @@
 	let displayInfo: boolean | undefined;
 	$: displayInfo = nonNullish(consentInfo) && 'Warn' in consentInfo;
 </script>
-<MessageBox level="info">{$i18n.signer.consent_message.warning.details_generated}</MessageBox>
-{#if displayInfo}
 
+{#if displayInfo}
+	<MessageBox level="info">{$i18n.signer.consent_message.warning.details_generated}</MessageBox>
 {/if}
