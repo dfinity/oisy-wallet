cc774e9f32280c876f87769e0ef70dc6f49f3d14
David Dal Busco <david.dalbusco@outlook.com>
2025-01-06 11:39:55 +0100

    feat: support ledgers that do not implement ICRC-21

---
+++ src/frontend/src/lib/components/signer/SignerConsentMessageWarning.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { ResultConsentInfo } from '@dfinity/oisy-wallet-signer';
+	import { nonNullish } from '@dfinity/utils';
+	import MessageBox from '$lib/components/ui/MessageBox.svelte';
+
+	export let consentInfo: ResultConsentInfo | undefined;
+
+	// The ICRC-49 standard specifies that a user should be warned when a consent message is interpreted on the frontend side instead of being retrieved through a canister.
+	// See https://github.com/dfinity/wg-identity-authentication/blob/main/topics/icrc_49_call_canister.md#message-processing
+	// The OISY product decision is to display this warning in the UI as an information box.
+	let displayInfo: boolean | undefined;
+	$: displayInfo = nonNullish(consentInfo) && 'Warn' in consentInfo;
+</script>
+
+{#if displayInfo}
+	<MessageBox level="info">TODO</MessageBox>
+{/if}
