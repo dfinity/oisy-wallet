07659c0fd90dcf2381cd4b62ae4746e8c02dd50a
David Dal Busco <david.dalbusco@dfinity.org>
2025-01-07 09:49:12 +0100

    feat(frontend): review reward-code services "code style" (#4087)
    # Motivation
    
    I reviewed the `reward-code.services` after merging #3996 and noticed a
    few areas that could be better aligned with the codebase's style and
    patterns for consistency.
    
    # Changes for Consistency
    
    - Add JSDoc comments for exported functions.  
    - Use specific error classes instead of the generic `Error` when the
    error type is known.
    - Bubble the `err` in the `ResultSuccess` return to potentially inform
    the consumer about which error occurred.
    - Align the pattern of `updateVipReward` with the pattern of other
    internal functions in the service, `updateReward`, which throws errors
    instead of returning values.
    - Define `err` as `err: unknown`.
    
    # Opinionated Changes
    
    - Destructure parameters.  
    
    # TODO
    
    - Add a TODO for `getNewReward` to use a `ResultSuccess` return type as
    well.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/tests/lib/services/reward-code.services.spec.ts
+++ src/frontend/src/tests/lib/services/reward-code.services.spec.ts
@@ -7,14 +7,18 @@ import * as rewardApi from '$lib/api/reward.api';
 import { claimVipReward, getNewReward, isVipUser } from '$lib/services/reward-code.services';
 import { i18n } from '$lib/stores/i18n.store';
 import * as toastsStore from '$lib/stores/toasts.store';
+import { AlreadyClaimedError, InvalidCodeError } from '$lib/types/errors';
 import en from '$tests/mocks/i18n.mock';
 import { mockIdentity } from '$tests/mocks/identity.mock';
 import { get } from 'svelte/store';
-import { vi } from 'vitest';
 
 const nullishIdentityErrorMessage = en.auth.error.no_internet_identity;
 
 describe('reward-code', () => {
+	beforeEach(() => {
+		vi.spyOn(console, 'error').mockImplementation(() => {});
+	});
+
 	describe('isVip', () => {
 		const mockedUserData: UserData = {
 			is_vip: [true],
@@ -122,7 +126,9 @@ describe('reward-code', () => {
 				vipReward: { code: '1234567890' },
 				nullishIdentityErrorMessage
 			});
-			expect(result).toEqual({ success: false });
+			expect(result.success).toBeFalsy();
+			expect(result.err).not.toBeUndefined();
+			expect(result.err).toBeInstanceOf(InvalidCodeError);
 		});
 
 		it('should return false if an already used vip reward code is used', async () => {
@@ -138,7 +144,9 @@ describe('reward-code', () => {
 				vipReward: { code: '1234567890' },
 				nullishIdentityErrorMessage
 			});
-			expect(result).toEqual({ success: false });
+			expect(result.success).toBeFalsy();
+			expect(result.err).not.toBeUndefined();
+			expect(result.err).toBeInstanceOf(AlreadyClaimedError);
 		});
 	});
 });
