5cb27e52876a32b480aabdfa9ec9b2d09f7278be
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-01-07 17:17:54 +0100

    fix(scripts): issue with generating certified candid (#4112)
    Currently, the certified candid generation script does not handle
    correctly the case when `['query'] parameter is placed on the new line.
    It wasn't noticed before since such a case only present in the frontend
    candid for which we do not generate a certified file.
    
    This code
    ```
    get_txs: IDL.Func(
    	[IDL.Opt(IDL.Nat64), IDL.Opt(IDL.Nat64), IDL.Opt(IDL.Nat32), IDL.Opt(IDL.Nat16)],
    	[TxsResult],
    	['query']
    ),
    ```
    is parsed into
    ```
    'get_txs' : IDL.Func(
            [
              IDL.Opt(IDL.Nat64),
              IDL.Opt(IDL.Nat64),
              IDL.Opt(IDL.Nat32),
              IDL.Opt(IDL.Nat16),
            ],
            [TxsResult],
            ,
     ),
    ```
    
    With this fix, that code will be correctly compiled into:
    ```
    get_txs: IDL.Func(
    	[IDL.Opt(IDL.Nat64), IDL.Opt(IDL.Nat64), IDL.Opt(IDL.Nat32), IDL.Opt(IDL.Nat16)],
    	[TxsResult]
    ),
    ```

--- scripts/did.update.types.mjs
+++ scripts/did.update.types.mjs
@@ -105,7 +105,7 @@ const copyCertifiedFactory = async ({ dest = `./src/declarations` }) => {
 
 						const certifiedFactoryPath = join(dest, dir, `${dir}.factory.certified.did.js`);
 
-						await writeFile(certifiedFactoryPath, content.toString().replace(/\['query']/g, ''));
+						await writeFile(certifiedFactoryPath, content.toString().replace(/\['query'],?/g, ''));
 
 						resolve();
 					};
