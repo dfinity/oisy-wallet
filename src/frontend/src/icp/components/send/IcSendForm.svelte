2ea94ab569e8e15b0175c1ed8ea35c04ccd5fa92
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-07 16:27:44 +0100

    refactor(frontend): use SendForm in IcSendForm

--- src/frontend/src/icp/components/send/IcSendForm.svelte
+++ src/frontend/src/icp/components/send/IcSendForm.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
 	import { isNullish, nonNullish } from '@dfinity/utils';
-	import { createEventDispatcher, getContext } from 'svelte';
+	import { getContext } from 'svelte';
 	import IcFeeDisplay from '$icp/components/send/IcFeeDisplay.svelte';
 	import IcSendAmount from '$icp/components/send/IcSendAmount.svelte';
 	import IcSendDestination from '$icp/components/send/IcSendDestination.svelte';
 	import type { IcAmountAssertionError } from '$icp/types/ic-send';
-	import SendSource from '$lib/components/send/SendSource.svelte';
-	import ButtonGroup from '$lib/components/ui/ButtonGroup.svelte';
-	import ButtonNext from '$lib/components/ui/ButtonNext.svelte';
-	import ContentWithToolbar from '$lib/components/ui/ContentWithToolbar.svelte';
+	import SendForm from '$lib/components/send/SendForm.svelte';
 	import { balance } from '$lib/derived/balances.derived';
 	import { SEND_CONTEXT_KEY, type SendContext } from '$lib/stores/send.store';
 	import type { NetworkId } from '$lib/types/network';
@@ -32,25 +29,23 @@
 		nonNullish(amountError) ||
 		isNullishOrEmpty(destination) ||
 		isNullish(amount);
-
-	const dispatch = createEventDispatcher();
 </script>
 
-<form on:submit={() => dispatch('icNext')} method="POST">
-	<ContentWithToolbar>
-		{#if !simplifiedForm}
-			<IcSendDestination bind:destination bind:invalidDestination {networkId} on:icQRCodeScan />
-		{/if}
 
-		<IcSendAmount bind:amount bind:amountError {networkId} />
+<SendForm on:icNext {source} token={$sendToken} balance={$balance} disabled={invalid}>
+	{#if !simplifiedForm}
+		<IcSendDestination
+			slot="destination"
+			bind:destination
+			bind:invalidDestination
+			{networkId}
+			on:icQRCodeScan
+		/>
+	{/if}
 
-		<SendSource token={$sendToken} balance={$balance} {source} hideSource={simplifiedForm} />
+	<IcSendAmount slot="amount" bind:amount bind:amountError {networkId} />
 
-		<IcFeeDisplay {networkId} />
+	<IcFeeDisplay slot="fee" {networkId} />
 
-		<ButtonGroup slot="toolbar" testId="toolbar">
-			<slot name="cancel" />
-			<ButtonNext disabled={invalid} />
-		</ButtonGroup>
-	</ContentWithToolbar>
-</form>
+	<slot name="cancel" slot="cancel" />
+</SendForm>
