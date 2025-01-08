014e2ab3d0f67371d0d2d4aee01ba27257b981e4
Max Murphy <max@winning.black>
2025-01-08 16:58:18 +0100

    Build the backend

--- scripts/build.backend.sh
+++ scripts/build.backend.sh
@@ -3,3 +3,9 @@ set -euxo pipefail
 
 scripts/build.backend.args.sh
 
+WASM_FILE="$(jq -r .canisters.backend.wasm dfx.json)"
+BUILD_DIR="target/wasm32-unknown-unknown/release"
+mkdir -p "$(dirname "$WASM_FILE")"
+
+cargo build --locked --target wasm32-unknown-unknown --release -p backend
+cp "$BUILD_DIR/backend.wasm" "$WASM_FILE"
