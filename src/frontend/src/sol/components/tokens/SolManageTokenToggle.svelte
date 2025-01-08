ec48e96b9a388c5e874efb9be8320871ceb500ba
Robert Schlittler <43857623+loki344@users.noreply.github.com>
2025-01-08 09:48:00 +0100

    feat(frontend): sol manage tokens (#4054)
    # Motivation
    After adding SOL native tokens to all-tokens in
    https://github.com/dfinity/oisy-wallet/pull/4038.
    Add toggle for solana native token in manage token. SOL native tokens
    cannot be disabled.
    
    # Changes
    
    Add ManageTokenstoggle for SOL.
    
    # Tests
    
    <img width="571" alt="image"
    src="https://github.com/user-attachments/assets/f7b93b3e-c70e-4fb0-8250-9ed02cffdf41"
    />
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/frontend/src/sol/components/tokens/SolManageTokenToggle.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { Toggle } from '@dfinity/gix-components';
+	import { i18n } from '$lib/stores/i18n.store';
+</script>
+
+<!-- For now, we just handle native SOL tokens, they are not manageable. -->
+<Toggle disabled ariaLabel={$i18n.tokens.text.hide_token} checked />
