68539f393b85a45b6c55360798f5798416ecf5db
Robert Schlittler <robert.schlittler@inacta.ch>
2025-01-08 14:22:23 +0100

    feat(frontend): add sol txn skeleton

---
+++ src/frontend/src/sol/components/transactions/SolTransactionsSkeletons.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { isNullish } from '@dfinity/utils';
+	import TransactionsSkeletons from '$lib/components/transactions/TransactionsSkeletons.svelte';
+	import { token } from '$lib/stores/token.store';
+	import { solTransactionsNotInitialized } from '$sol/derived/sol-transactions.derived';
+
+	let loading: boolean;
+	$: loading = isNullish($token) || $solTransactionsNotInitialized;
+</script>
+
+<TransactionsSkeletons testIdPrefix="sol-txn" {loading}>
+	<slot />
+</TransactionsSkeletons>
