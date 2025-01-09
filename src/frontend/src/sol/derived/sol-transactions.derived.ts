62fcda6fd492a07c99280d6f829477ea23589e05
Robert Schlittler <43857623+loki344@users.noreply.github.com>
2025-01-09 11:30:44 +0100

    feat(frontend): add sol txn initialized derived (#4130)
    # Motivation
    
    Add derived for indicating initialised txns for SOL.
    
    # Changes
    
    Add derived
    
    # Tests
    
    Unit tests added
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

--- src/frontend/src/sol/derived/sol-transactions.derived.ts
+++ src/frontend/src/sol/derived/sol-transactions.derived.ts
@@ -1,10 +1,21 @@
 import { tokenWithFallback } from '$lib/derived/token.derived';
 import { solTransactionsStore } from '$sol/stores/sol-transactions.store';
 import type { SolTransactionUi } from '$sol/types/sol-transaction';
+import { nonNullish } from '@dfinity/utils';
 import { derived, type Readable } from 'svelte/store';
 
 export const solTransactions: Readable<SolTransactionUi[]> = derived(
 	[tokenWithFallback, solTransactionsStore],
 	([$token, $solTransactionsStore]) =>
 		($solTransactionsStore?.[$token.id] ?? []).map(({ data: transaction }) => transaction)
+);
+
+export const solTransactionsInitialized: Readable<boolean> = derived(
+	[solTransactionsStore, tokenWithFallback],
+	([$solTransactionsStore, { id: $tokenId }]) => nonNullish($solTransactionsStore?.[$tokenId])
+);
+
+export const solTransactionsNotInitialized: Readable<boolean> = derived(
+	[solTransactionsInitialized],
+	([$solTransactionsInitialized]) => !$solTransactionsInitialized
 );
--- src/frontend/src/tests/sol/derived/sol-transactions.derived.spec.ts
+++ src/frontend/src/tests/sol/derived/sol-transactions.derived.spec.ts
@@ -1,6 +1,10 @@
 import { SOLANA_TOKEN, SOLANA_TOKEN_ID } from '$env/tokens/tokens.sol.env';
 import { token } from '$lib/stores/token.store';
-import { solTransactions } from '$sol/derived/sol-transactions.derived';
+import {
+	solTransactions,
+	solTransactionsInitialized,
+	solTransactionsNotInitialized
+} from '$sol/derived/sol-transactions.derived';
 import { solTransactionsStore } from '$sol/stores/sol-transactions.store';
 import { createMockSolTransactionUi } from '$tests/mocks/sol-transactions.mock';
 import { get } from 'svelte/store';
@@ -21,31 +25,93 @@ describe('sol-transactions.derived', () => {
 		token.set(SOLANA_TOKEN);
 	});
 
-	it('should return an empty array when transactions store is empty', () => {
-		const result = get(solTransactions);
-		expect(result).toEqual([]);
-	});
-
-	it('should return empty array when transactions is nullish', () => {
-		solTransactionsStore.append({
-			tokenId: SOLANA_TOKEN_ID,
-			transactions
+	describe('solTransactions', () => {
+		it('should return an empty array when transactions store is empty', () => {
+			const result = get(solTransactions);
+			expect(result).toEqual([]);
 		});
 
-		solTransactionsStore.nullify(SOLANA_TOKEN_ID);
+		it('should return empty array when transactions is nullish', () => {
+			solTransactionsStore.append({
+				tokenId: SOLANA_TOKEN_ID,
+				transactions
+			});
 
-		const result = get(solTransactions);
-		expect(result).toHaveLength(0);
-		expect(result).toEqual([]);
-	});
+			solTransactionsStore.nullify(SOLANA_TOKEN_ID);
 
-	it('should return transactions for the current token', () => {
-		solTransactionsStore.append({
-			tokenId: SOLANA_TOKEN_ID,
-			transactions
+			const result = get(solTransactions);
+			expect(result).toHaveLength(0);
+			expect(result).toEqual([]);
 		});
 
-		const result = get(solTransactions);
-		expect(result).toEqual(transactions.map(({ data }) => data));
+		it('should return transactions for the current token', () => {
+			solTransactionsStore.append({
+				tokenId: SOLANA_TOKEN_ID,
+				transactions
+			});
+
+			const result = get(solTransactions);
+			expect(result).toEqual(transactions.map(({ data }) => data));
+		});
+	});
+
+	describe('solTransactionsInitialized', () => {
+		it('should return false when transactions store is empty', () => {
+			solTransactionsStore.reset(SOLANA_TOKEN_ID);
+			const result = get(solTransactionsInitialized);
+			expect(result).toBe(false);
+		});
+
+		it('should return false when transactions are nullish', () => {
+			solTransactionsStore.append({
+				tokenId: SOLANA_TOKEN_ID,
+				transactions
+			});
+
+			solTransactionsStore.nullify(SOLANA_TOKEN_ID);
+
+			const result = get(solTransactionsInitialized);
+			expect(result).toBe(false);
+		});
+
+		it('should return true when transactions are initialized', () => {
+			solTransactionsStore.append({
+				tokenId: SOLANA_TOKEN_ID,
+				transactions
+			});
+
+			const result = get(solTransactionsInitialized);
+			expect(result).toBe(true);
+		});
+	});
+
+	describe('solTransactionsNotInitialized', () => {
+		it('should return true when transactions store is empty', () => {
+			solTransactionsStore.reset(SOLANA_TOKEN_ID);
+			const result = get(solTransactionsNotInitialized);
+			expect(result).toBe(true);
+		});
+
+		it('should return true when transactions are nullish', () => {
+			solTransactionsStore.append({
+				tokenId: SOLANA_TOKEN_ID,
+				transactions
+			});
+
+			solTransactionsStore.nullify(SOLANA_TOKEN_ID);
+
+			const result = get(solTransactionsNotInitialized);
+			expect(result).toBe(true);
+		});
+
+		it('should return false when transactions are initialized', () => {
+			solTransactionsStore.append({
+				tokenId: SOLANA_TOKEN_ID,
+				transactions
+			});
+
+			const result = get(solTransactionsNotInitialized);
+			expect(result).toBe(false);
+		});
 	});
 });
