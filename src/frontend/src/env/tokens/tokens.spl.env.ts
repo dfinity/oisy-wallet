f6981fb5d235334a1bd4c37ce37a8aca066e9234
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-10 17:30:19 +0100

    add flag

--- src/frontend/src/env/tokens/tokens.spl.env.ts
+++ src/frontend/src/env/tokens/tokens.spl.env.ts
@@ -1,7 +1,8 @@
+import { SOLANA_NETWORK_ENABLED } from '$env/networks/networks.sol.env';
 import { DEVNET_EURC_TOKEN } from '$env/tokens/tokens-spl/tokens.eurc.env';
 import { DEVNET_USDC_TOKEN } from '$env/tokens/tokens-spl/tokens.usdc.env';
 import type { RequiredSplToken } from '$sol/types/spl';
 
 export const SPL_TOKENS_DEVNET: RequiredSplToken[] = [DEVNET_USDC_TOKEN, DEVNET_EURC_TOKEN];
 
-export const SPL_TOKENS: RequiredSplToken[] = [...SPL_TOKENS_DEVNET];
+export const SPL_TOKENS: RequiredSplToken[] = SOLANA_NETWORK_ENABLED ? [...SPL_TOKENS_DEVNET] : [];
