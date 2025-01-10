4471aaf2f0b66b2ec696402e560b94e153fa07cb
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-01-10 11:36:51 +0100

    feat(frontend): icons update (#4160)
    # Motivation
    
    In this PR, 2 new icons have been added, as well as an existing got an
    additional `size` prop.

--- src/frontend/src/lib/components/icons/lucide/IconArrowUpDown.svelte
+++ src/frontend/src/lib/components/icons/lucide/IconArrowUpDown.svelte
@@ -13,10 +13,8 @@
 	stroke-width="2"
 	stroke-linecap="round"
 	stroke-linejoin="round"
-	class="lucide lucide-wallet"
+	class="lucide lucide-arrow-up-down"
+	><path d="m21 16-4 4-4-4" /><path d="M17 20V4" /><path d="m3 8 4-4 4 4" /><path
+		d="M7 4v16"
+	/></svg
 >
-	<path
-		d="M19 7V4a1 1 0 0 0-1-1H5a2 2 0 0 0 0 4h15a1 1 0 0 1 1 1v4h-3a2 2 0 0 0 0 4h3a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1"
-	/>
-	<path d="M3 5v14a2 2 0 0 0 2 2h15a1 1 0 0 0 1-1v-4" />
-</svg>
