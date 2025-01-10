c5162d6af7763c050e8eebabc9fc2dcdd276c04f
Robert Schlittler <43857623+loki344@users.noreply.github.com>
2025-01-10 13:43:30 +0100

    feat(frontend): sol txn listener and worker (#4135)
    # Note
    first: https://github.com/dfinity/oisy-wallet/pull/4125
    
    # Motivation
    
    After adding the api for fetching SOL transactions, we want to use it in
    the SOL wallet scheduler.
    
    # Changes
    
    Implement transaction handling in sol wallet scheduler.
    
    # Tests
    
    Extended existing unit tests.
    
    ---------
    
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/sol/schema/sol-post-message.schema.ts
+++ src/frontend/src/sol/schema/sol-post-message.schema.ts
@@ -1,10 +1,14 @@
-import { PostMessageDataResponseSchema } from '$lib/schema/post-message.schema';
+import {
+	JsonTransactionsTextSchema,
+	PostMessageDataResponseSchema
+} from '$lib/schema/post-message.schema';
 import type { CertifiedData } from '$lib/types/store';
 import type { Lamports } from '@solana/rpc-types';
 import { z } from 'zod';
 
 const SolPostMessageWalletDataSchema = z.object({
-	balance: z.custom<CertifiedData<Lamports | null>>()
+	balance: z.custom<CertifiedData<Lamports | null>>(),
+	newTransactions: JsonTransactionsTextSchema
 });
 
 export const SolPostMessageDataResponseWalletSchema = PostMessageDataResponseSchema.extend({
