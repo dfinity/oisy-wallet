2e5351462d7f9b07ae905fd28738a776572af5c0
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-01-10 13:20:22 +0100

    fix(frontend): updates the reward modal texts (#4134)
    # Motivation
    
    The texts of the reward modals needed some changes to be more clear.
    
    # Changes
    
    - modal texts
    
    # Tests
    
    
    
    **success:**
    
    before:
    <img width="427" alt="image"
    src="https://github.com/user-attachments/assets/fc7fc9c7-8763-41d6-aa73-b83dcc85faec"
    />
    
    after:
    <img width="433" alt="image"
    src="https://github.com/user-attachments/assets/de7b86eb-131a-4905-8de8-17ac800f369b"
    />
    
    **failed:**
    
    before:
    <img width="428" alt="image"
    src="https://github.com/user-attachments/assets/107dcd5c-ab75-43cd-b2d5-015c584460df"
    />
    
    
    after:
    <img width="429" alt="image"
    src="https://github.com/user-attachments/assets/faf59fe3-e360-4998-911c-1f7d5772bc90"
    />
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/qr/SuccessfulRewardModal.svelte
+++ src/frontend/src/lib/components/qr/SuccessfulRewardModal.svelte
@@ -6,6 +6,7 @@
 	import ImgBanner from '$lib/components/ui/ImgBanner.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { modalStore } from '$lib/stores/modal.store';
+	import { replaceOisyPlaceholders } from '$lib/utils/i18n.utils';
 </script>
 
 <Modal on:nnsClose={modalStore.close}>
@@ -16,7 +17,9 @@
 	<ContentWithToolbar>
 		<ImgBanner src={successfulReward} styleClass="aspect-auto" />
 
-		<h3 class="my-3 text-center">{$i18n.vip.reward.text.reward_received}</h3>
+		<h3 class="my-3 text-center"
+			>{replaceOisyPlaceholders($i18n.vip.reward.text.reward_received)}</h3
+		>
 		<span class="block w-full text-center">{$i18n.vip.reward.text.reward_received_description}</span
 		>
 
--- src/frontend/src/tests/lib/components/qr/SuccessfulRewardModal.spec.ts
+++ src/frontend/src/tests/lib/components/qr/SuccessfulRewardModal.spec.ts
@@ -1,5 +1,6 @@
 import SuccessfulRewardModal from '$lib/components/qr/SuccessfulRewardModal.svelte';
 import { i18n } from '$lib/stores/i18n.store';
+import { replaceOisyPlaceholders } from '$lib/utils/i18n.utils';
 import { render } from '@testing-library/svelte';
 import { get } from 'svelte/store';
 
@@ -8,7 +9,9 @@ describe('SuccessfulRewardModal', () => {
 		const { getByText } = render(SuccessfulRewardModal);
 
 		expect(getByText(get(i18n).vip.reward.text.title_successful)).toBeInTheDocument();
-		expect(getByText(get(i18n).vip.reward.text.reward_received)).toBeInTheDocument();
+		expect(
+			getByText(replaceOisyPlaceholders(get(i18n).vip.reward.text.reward_received))
+		).toBeInTheDocument();
 		expect(getByText(get(i18n).vip.reward.text.reward_received_description)).toBeInTheDocument();
 		expect(getByText(get(i18n).vip.reward.text.open_wallet)).toBeInTheDocument();
 	});
