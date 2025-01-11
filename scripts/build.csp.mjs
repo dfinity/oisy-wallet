507e2657b2da72f1d76d345b3b20127bcc9888cd
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-11 11:25:50 +0100

    add websocket

--- scripts/build.csp.mjs
+++ scripts/build.csp.mjs
@@ -157,7 +157,7 @@ const updateCSP = (indexHtml) => {
 	const solanaRpcApiConnectSrc =
 		'https://api.mainnet-beta.solana.com wss://api.mainnet-beta.solana.com https://api.testnet.solana.com wss://api.testnet.solana.com https://api.devnet.solana.com wss://api.devnet.solana.com';
 	const solanaAlchemyApiConnectSrc =
-		'https://solana-mainnet.g.alchemy.com https://solana-testnet.g.alchemy.com https://solana-devnet.g.alchemy.com';
+		'https://solana-mainnet.g.alchemy.com wss://solana-mainnet.g.alchemy.com https://solana-testnet.g.alchemy.com wss://solana-testnet.g.alchemy.com https://solana-devnet.g.alchemy.com wss://solana-devnet.g.alchemy.com';
 
 	const csp = `<meta
         http-equiv="Content-Security-Policy"
