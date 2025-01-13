0de67bc704e48ee88bfe695d2ce6b1569845647b
David Dal Busco <david.dalbusco@outlook.com>
2025-01-13 08:51:57 +0100

    feat(frontend): make zod tree-shakable

--- src/frontend/src/sol/schema/sol-post-message.schema.ts
+++ src/frontend/src/sol/schema/sol-post-message.schema.ts
@@ -4,7 +4,7 @@ import {
 } from '$lib/schema/post-message.schema';
 import type { CertifiedData } from '$lib/types/store';
 import type { Lamports } from '@solana/rpc-types';
-import { z } from 'zod';
+import * as z from 'zod';
 
 const SolPostMessageWalletDataSchema = z.object({
 	balance: z.custom<CertifiedData<Lamports | null>>(),
