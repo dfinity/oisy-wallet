ae6cc3a4785607c02d975338717a0a1c26feb857
Stefan Berger <stefan.berger@inacta.ch>
2025-01-13 17:31:21 +0100

    feat(frontend): add SPL token RAY (#4214)
    Adding the SPL token RAY
    
    ---------
    
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/env/tokens/tokens.spl.env.ts
+++ src/frontend/src/env/tokens/tokens.spl.env.ts
@@ -3,10 +3,11 @@ import { DEVNET_EURC_TOKEN } from '$env/tokens/tokens-spl/tokens.eurc.env';
 import { JUP_TOKEN } from '$env/tokens/tokens-spl/tokens.jup.env';
 import { ORCA_TOKEN } from '$env/tokens/tokens-spl/tokens.orca.env';
 import { POPCAT_TOKEN } from '$env/tokens/tokens-spl/tokens.popcat.env';
+import { RAY_TOKEN } from '$env/tokens/tokens-spl/tokens.ray.env';
 import { DEVNET_USDC_TOKEN } from '$env/tokens/tokens-spl/tokens.usdc.env';
 import type { RequiredSplToken } from '$sol/types/spl';
 
-const SPL_TOKENS_MAINNET: RequiredSplToken[] = [JUP_TOKEN, ORCA_TOKEN, POPCAT_TOKEN];
+const SPL_TOKENS_MAINNET: RequiredSplToken[] = [JUP_TOKEN, ORCA_TOKEN, POPCAT_TOKEN, RAY_TOKEN];
 
 const SPL_TOKENS_DEVNET: RequiredSplToken[] = [DEVNET_USDC_TOKEN, DEVNET_EURC_TOKEN];
 
