3150269bf570b9f271f0221454d90303b5694c1e
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-13 11:53:04 -0300

    feat(frontend): add service to send SPL tokens (#4191)
    # Motivation
    
    To send SPL tokens, we need to add the function that creates the message
    instructions correct (that is provided by the library
    [`@solana-program/token`](https://www.npmjs.com/package/@solana-program/token)).
    Furthermore we will use the token accounts associated to `source` and
    `destination`.
    
    # Changes
    
    - Install
    [`@solana-program/token`](https://www.npmjs.com/package/@solana-program/token).
    - Add Token Program Address constant.
    - Create `createSplTokenTransactionMessage` that will return the correct
    instruction message for SPL tokens.
    
    # Tests
    
    Included unitest.

--- src/frontend/src/tests/sol/services/sol-send.services.spec.ts
+++ src/frontend/src/tests/sol/services/sol-send.services.spec.ts
@@ -1,16 +1,18 @@
+import { DEVNET_USDC_TOKEN } from '$env/tokens/tokens-spl/tokens.usdc.env';
 import { SOLANA_TOKEN } from '$env/tokens/tokens.sol.env';
 import { signWithSchnorr } from '$lib/api/signer.api';
 import { replacePlaceholders } from '$lib/utils/i18n.utils';
 import { solanaHttpRpc, solanaWebSocketRpc } from '$sol/providers/sol-rpc.providers';
 import { sendSol } from '$sol/services/sol-send.services';
-import { mapNetworkIdToNetwork } from '$sol/utils/network.utils';
+import * as networkUtils from '$sol/utils/network.utils';
 import en from '$tests/mocks/i18n.mock';
 import { mockIdentity } from '$tests/mocks/identity.mock';
-import { mockSolAddress, mockSolAddress2 } from '$tests/mocks/sol.mock';
+import { mockSolAddress, mockSolAddress2, mockSplAddress } from '$tests/mocks/sol.mock';
 import { BigNumber } from '@ethersproject/bignumber';
 import type { Rpc, SolanaRpcApi } from '@solana/rpc';
 import type { RpcSubscriptions, SolanaRpcSubscriptionsApi } from '@solana/rpc-subscriptions';
 import { sendAndConfirmTransactionFactory } from '@solana/web3.js';
+import type { MockInstance } from 'vitest';
 
 vi.mock('@solana/functional', () => ({
 	pipe: vi.fn()
@@ -46,10 +48,6 @@ vi.mock('$lib/api/signer.api', () => ({
 	signWithSchnorr: vi.fn()
 }));
 
-vi.mock('$sol/utils/network.utils', () => ({
-	mapNetworkIdToNetwork: vi.fn()
-}));
-
 describe('sol-send.services', () => {
 	// TODO: add more practical tests deploying the Solana local node
 	describe('sendSol', () => {
@@ -59,9 +57,14 @@ describe('sol-send.services', () => {
 		const mockRpc = {
 			getLatestBlockhash: vi.fn(() => ({
 				send: vi.fn(() => Promise.resolve({ value: { blockhash: 'test-blockhash' } }))
+			})),
+			getTokenAccountsByOwner: vi.fn(() => ({
+				send: vi.fn(() => Promise.resolve({ value: [{ pubkey: mockSplAddress }] }))
 			}))
 		} as unknown as Rpc<SolanaRpcApi>;
 		const mockRpcSubscriptions = {} as RpcSubscriptions<SolanaRpcSubscriptionsApi>;
+
+		let spyMapNetworkIdToNetwork: MockInstance;
 
 		beforeEach(() => {
 			vi.clearAllMocks();
@@ -69,8 +72,9 @@ describe('sol-send.services', () => {
 			vi.mocked(solanaHttpRpc).mockReturnValue(mockRpc);
 			vi.mocked(solanaWebSocketRpc).mockReturnValue(mockRpcSubscriptions);
 			vi.mocked(signWithSchnorr).mockResolvedValue(new Uint8Array([0, 1, 2, 3]));
-			vi.mocked(mapNetworkIdToNetwork).mockReturnValue('mainnet');
 			vi.mocked(sendAndConfirmTransactionFactory).mockReturnValue(() => Promise.resolve());
+
+			spyMapNetworkIdToNetwork = vi.spyOn(networkUtils, 'mapNetworkIdToNetwork');
 		});
 
 		it('should send SOL successfully', async () => {
@@ -85,12 +89,28 @@ describe('sol-send.services', () => {
 				})
 			).resolves.not.toThrow();
 
-			expect(mapNetworkIdToNetwork).toHaveBeenCalledWith(SOLANA_TOKEN.network.id);
+			expect(spyMapNetworkIdToNetwork).toHaveBeenCalledWith(SOLANA_TOKEN.network.id);
+			expect(mockRpc.getLatestBlockhash).toHaveBeenCalled();
+		});
+
+		it('should send SPL tokens successfully', async () => {
+			await expect(
+				sendSol({
+					identity: mockIdentity,
+					token: DEVNET_USDC_TOKEN,
+					amount: mockAmount,
+					destination: mockDestination,
+					source: mockSource,
+					onProgress: vi.fn()
+				})
+			).resolves.not.toThrow();
+
+			expect(spyMapNetworkIdToNetwork).toHaveBeenCalledWith(DEVNET_USDC_TOKEN.network.id);
 			expect(mockRpc.getLatestBlockhash).toHaveBeenCalled();
 		});
 
 		it('should throw an error if network is invalid', async () => {
-			vi.mocked(mapNetworkIdToNetwork).mockReturnValueOnce(undefined);
+			spyMapNetworkIdToNetwork.mockReturnValueOnce(undefined);
 
 			await expect(
 				sendSol({
@@ -105,6 +125,28 @@ describe('sol-send.services', () => {
 				replacePlaceholders(en.init.error.no_solana_network, {
 					$network: SOLANA_TOKEN.network.id.description ?? ''
 				})
+			);
+		});
+
+		it('should throw an error if no token accounts are found', async () => {
+			vi.mocked(solanaHttpRpc).mockReturnValue({
+				...mockRpc,
+				getTokenAccountsByOwner: vi.fn(() => ({
+					send: vi.fn(() => Promise.resolve({ value: [] }))
+				}))
+			} as unknown as Rpc<SolanaRpcApi>);
+
+			await expect(
+				sendSol({
+					identity: mockIdentity,
+					token: DEVNET_USDC_TOKEN,
+					amount: mockAmount,
+					destination: mockDestination,
+					source: mockSource,
+					onProgress: vi.fn()
+				})
+			).rejects.toThrowError(
+				`Token account not found for wallet ${mockSource} and token ${DEVNET_USDC_TOKEN.address} on devnet network`
 			);
 		});
 	});
