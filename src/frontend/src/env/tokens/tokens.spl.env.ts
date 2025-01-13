bb5aad8010c30aac4c3a9d92eae89182a9157a4c
Stefan Berger <stefan.berger@inacta.ch>
2025-01-13 13:45:18 +0100

    Include RAY to token list

--- src/frontend/src/env/tokens/tokens.spl.env.ts
+++ src/frontend/src/env/tokens/tokens.spl.env.ts
@@ -1,10 +1,11 @@
 import { DEVNET_EURC_TOKEN } from '$env/tokens/tokens-spl/tokens.eurc.env';
 import { JUP_TOKEN } from '$env/tokens/tokens-spl/tokens.jup.env';
 import { POPCAT_TOKEN } from '$env/tokens/tokens-spl/tokens.popcat.env';
+import { RAY_TOKEN } from '$env/tokens/tokens-spl/tokens.ray.env';
 import { DEVNET_USDC_TOKEN } from '$env/tokens/tokens-spl/tokens.usdc.env';
 import type { RequiredSplToken } from '$sol/types/spl';
 
-export const SPL_TOKEN_MAINNET: RequiredSplToken[] = [JUP_TOKEN, POPCAT_TOKEN];
+export const SPL_TOKEN_MAINNET: RequiredSplToken[] = [JUP_TOKEN, POPCAT_TOKEN, RAY_TOKEN];
 
 export const SPL_TOKENS_DEVNET: RequiredSplToken[] = [DEVNET_USDC_TOKEN, DEVNET_EURC_TOKEN];
 
