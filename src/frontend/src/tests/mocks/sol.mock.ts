dc46f844e8c43a7781e2ae39edf63fc774d83dcc
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-13 06:45:02 -0300

    feat(frontend): add Solana API call to load token account (#4190)
    # Motivation
    
    When transferring SPL tokens we deal with the specific token account
    associated to an address. So we create the call to fetch such a value,
    given the wallet address, the token address and the network.

--- src/frontend/src/tests/mocks/sol.mock.ts
+++ src/frontend/src/tests/mocks/sol.mock.ts
@@ -3,3 +3,5 @@ import type { SolAddress } from '$lib/types/address';
 export const mockSolAddress: SolAddress = 'AeZDiNSSTvGxxD92vao15YRV7f4c4Q1GfcKnwezXucZa';
 
 export const mockSolAddress2: SolAddress = '4GsmSut5stEKJHsGfiTeP5c6VbPyt4KmtCQ9TAM56JR8';
+
+export const mockSplAddress: SolAddress = 'CcExVbJ17Pec4SyiAsxXQZKCDSqdv3r9cNTLNrFRrbta';
