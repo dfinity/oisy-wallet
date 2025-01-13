b962ef6b24f9e3aa374e5878bbb992f8f145dd86
Stefan Berger <stefan.berger@inacta.ch>
2025-01-13 12:09:19 +0100

    Use token ticker instead of name

--- src/frontend/src/env/tokens/tokens-spl/tokens.jupiter.env.ts
+++ src/frontend/src/env/tokens/tokens-spl/tokens.jupiter.env.ts
@@ -1,23 +1,23 @@
 import { SOLANA_MAINNET_NETWORK } from '$env/networks/networks.sol.env';
 import type { TokenId } from '$lib/types/token';
 import { parseTokenId } from '$lib/validation/token.validation';
-import jupiter from '$sol/assets/jupiter.svg';
+import jup from '$sol/assets/jup.svg';
 import type { RequiredSplToken } from '$sol/types/spl';
 
-export const JUPITER_DECIMALS = 6;
+export const KUP_DECIMALS = 6;
 
-export const JUPITER_SYMBOL = 'JUPITER';
+export const JUP_SYMBOL = 'JUP';
 
-export const JUPITER_TOKEN_ID: TokenId = parseTokenId(JUPITER_SYMBOL);
+export const JUP_TOKEN_ID: TokenId = parseTokenId(JUP_SYMBOL);
 
-export const JUPITER_TOKEN: RequiredSplToken = {
-	id: JUPITER_TOKEN_ID,
+export const JUP_TOKEN: RequiredSplToken = {
+	id: JUP_TOKEN_ID,
 	network: SOLANA_MAINNET_NETWORK,
 	standard: 'spl',
 	category: 'default',
 	name: 'Jupiter',
-	symbol: JUPITER_SYMBOL,
-	decimals: JUPITER_DECIMALS,
-	icon: jupiter,
+	symbol: JUP_SYMBOL,
+	decimals: JUP_DECIMALS,
+	icon: jup,
 	address: 'JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN'
 };
