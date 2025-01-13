6f394461987d52b685734780db8a533b00c62268
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-13 12:38:26 -0300

    test(frontend): add tests for SolSendForm (#4207)
    # Motivation
    
    Small unittest for `SolSendForm`, similar to other similar components
    for other networks.

---
+++ src/frontend/src/tests/sol/components/send/SolSendForm.spec.ts
@@ -0,0 +1,54 @@
+import { SOLANA_TOKEN } from '$env/tokens/tokens.sol.env';
+import { SEND_CONTEXT_KEY, initSendContext } from '$lib/stores/send.store';
+import SolSendForm from '$sol/components/send/SolSendForm.svelte';
+import { mockSolAddress, mockSolAddress2 } from '$tests/mocks/sol.mock';
+import { render } from '@testing-library/svelte';
+
+describe('SolSendForm', () => {
+	const mockContext = new Map([]);
+	mockContext.set(
+		SEND_CONTEXT_KEY,
+		initSendContext({
+			sendPurpose: 'send',
+			token: SOLANA_TOKEN
+		})
+	);
+
+	const props = {
+		destination: mockSolAddress2,
+		amount: 22_000_000,
+		source: mockSolAddress
+	};
+
+	const destinationSelector = 'input[data-tid="destination-input"]';
+	const amountSelector = 'input[data-tid="amount-input"]';
+	const sourceSelector = 'div[id="source"]';
+	const balanceSelector = 'div[id="balance"]';
+	const feeSelector = 'p[id="fee"]';
+	const toolbarSelector = 'div[data-tid="toolbar"]';
+
+	it('should render all fields', () => {
+		const { container } = render(SolSendForm, {
+			props,
+			context: mockContext
+		});
+
+		const destination: HTMLInputElement | null = container.querySelector(destinationSelector);
+		expect(destination).not.toBeNull();
+
+		const amount: HTMLInputElement | null = container.querySelector(amountSelector);
+		expect(amount).not.toBeNull();
+
+		const source: HTMLDivElement | null = container.querySelector(sourceSelector);
+		expect(source).not.toBeNull();
+
+		const balance: HTMLDivElement | null = container.querySelector(balanceSelector);
+		expect(balance).not.toBeNull();
+
+		const fee: HTMLParagraphElement | null = container.querySelector(feeSelector);
+		expect(fee).not.toBeNull();
+
+		const toolbar: HTMLDivElement | null = container.querySelector(toolbarSelector);
+		expect(toolbar).not.toBeNull();
+	});
+});
