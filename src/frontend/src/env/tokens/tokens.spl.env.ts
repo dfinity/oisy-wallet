3b682fa2163d22a2593a983bfdcc2d0b21d32a8e
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-13 17:28:30 +0100

    add to list

--- src/frontend/src/env/tokens/tokens.spl.env.ts
+++ src/frontend/src/env/tokens/tokens.spl.env.ts
@@ -1,4 +1,5 @@
 import { SOLANA_NETWORK_ENABLED } from '$env/networks/networks.sol.env';
+import { BONK_TOKEN } from '$env/tokens/tokens-spl/tokens.bonk.env';
 import { DEVNET_EURC_TOKEN } from '$env/tokens/tokens-spl/tokens.eurc.env';
 import { JUP_TOKEN } from '$env/tokens/tokens-spl/tokens.jup.env';
 import { ORCA_TOKEN } from '$env/tokens/tokens-spl/tokens.orca.env';
@@ -6,7 +7,7 @@ import { POPCAT_TOKEN } from '$env/tokens/tokens-spl/tokens.popcat.env';
 import { DEVNET_USDC_TOKEN } from '$env/tokens/tokens-spl/tokens.usdc.env';
 import type { RequiredSplToken } from '$sol/types/spl';
 
-const SPL_TOKENS_MAINNET: RequiredSplToken[] = [JUP_TOKEN, ORCA_TOKEN, POPCAT_TOKEN];
+const SPL_TOKENS_MAINNET: RequiredSplToken[] = [BONK_TOKEN, JUP_TOKEN, ORCA_TOKEN, POPCAT_TOKEN];
 
 const SPL_TOKENS_DEVNET: RequiredSplToken[] = [DEVNET_USDC_TOKEN, DEVNET_EURC_TOKEN];
 
