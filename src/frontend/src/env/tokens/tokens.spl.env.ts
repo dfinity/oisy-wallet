3ef3961de79144a8702f6c8cc28b04b9e872cb5c
Stefan Berger <stefan.berger@inacta.ch>
2025-01-13 13:38:00 +0100

    feat(frontend): add SPL token JUPITER (#4199)
    We add SPL token **JUPITER**
    ([Coinmarketcap](https://coinmarketcap.com/currencies/jupiter-ag/)).
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/env/tokens/tokens.spl.env.ts
+++ src/frontend/src/env/tokens/tokens.spl.env.ts
@@ -1,9 +1,10 @@
 import { DEVNET_EURC_TOKEN } from '$env/tokens/tokens-spl/tokens.eurc.env';
+import { JUP_TOKEN } from '$env/tokens/tokens-spl/tokens.jup.env';
 import { POPCAT_TOKEN } from '$env/tokens/tokens-spl/tokens.popcat.env';
 import { DEVNET_USDC_TOKEN } from '$env/tokens/tokens-spl/tokens.usdc.env';
 import type { RequiredSplToken } from '$sol/types/spl';
 
-export const SPL_TOKEN_MAINNET: RequiredSplToken[] = [POPCAT_TOKEN];
+export const SPL_TOKEN_MAINNET: RequiredSplToken[] = [JUP_TOKEN, POPCAT_TOKEN];
 
 export const SPL_TOKENS_DEVNET: RequiredSplToken[] = [DEVNET_USDC_TOKEN, DEVNET_EURC_TOKEN];
 
