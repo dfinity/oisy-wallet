9d3d370fb334eda091f8cede4f523fc0733ab45c
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-01-13 13:05:24 +0100

    resolves lint issue

--- src/frontend/src/lib/components/tokens/TokensDisplayHandler.svelte
+++ src/frontend/src/lib/components/tokens/TokensDisplayHandler.svelte
@@ -3,7 +3,7 @@
 	import { combinedDerivedSortedNetworkTokensUi } from '$lib/derived/network-tokens.derived';
 	import { showZeroBalances } from '$lib/derived/settings.derived';
 	import type { TokenUi } from '$lib/types/token';
-	import type { TokenUiGroup, TokenUiOrGroupUi } from '$lib/types/token-group';
+	import type { TokenUiOrGroupUi } from '$lib/types/token-group';
 	import { groupTokensByTwin } from '$lib/utils/token-group.utils';
 
 	// We start it as undefined to avoid showing an empty list before the first update.
@@ -17,7 +17,7 @@
 		const hasBalance = (token: TokenUiOrGroupUi) =>
 			Number(token.balance ?? 0n) ?? token.usdBalance ?? $showZeroBalances;
 
-		sortedTokensOrGroups = groupedTokens.filter((t: TokenUiGroup) =>
+		sortedTokensOrGroups = groupedTokens.filter((t: TokenUiOrGroupUi) =>
 			nonNullish(t.tokens) ? t.tokens.some((tok: TokenUi) => hasBalance(tok)) : hasBalance(t)
 		);
 	}
