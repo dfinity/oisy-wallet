8a08bae0caccb2609ef7c25642d7ea86a31a60e5
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-13 13:13:51 -0300

    feat(frontend): add Max button for Solana (#4213)
    # Motivation
    
    Similar to the other networks, we enable the Max button using the
    `calculateMax` function.
    
    
    ![Screenshot 2025-01-13 at 16 59
    59](https://github.com/user-attachments/assets/30fb373c-e3d8-421e-a445-9e83d2b9e33f)
    ![Screenshot 2025-01-13 at 17 02
    10](https://github.com/user-attachments/assets/1575b510-dc35-45e6-a67f-64886b594ee0)

--- src/frontend/src/sol/components/send/SolSendAmount.svelte
+++ src/frontend/src/sol/components/send/SolSendAmount.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
-	import { nonNullish } from '@dfinity/utils';
-	import type { BigNumber } from 'alchemy-sdk';
+	import { isNullish, nonNullish } from '@dfinity/utils';
+	import { BigNumber } from 'alchemy-sdk';
 	import { getContext } from 'svelte';
 	import SendInputAmount from '$lib/components/send/SendInputAmount.svelte';
+	import { ZERO } from '$lib/constants/app.constants';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { SEND_CONTEXT_KEY, type SendContext } from '$lib/stores/send.store';
 	import type { OptionAmount } from '$lib/types/send';
 	import { invalidAmount } from '$lib/utils/input.utils';
+	import { getMaxTransactionAmount } from '$lib/utils/token.utils';
+	import { SOLANA_TRANSACTION_FEE_IN_LAMPORTS } from '$sol/constants/sol.constants';
 	import { SolAmountAssertionError } from '$sol/types/sol-send';
 
 	export let amount: OptionAmount = undefined;
 	export let amountError: SolAmountAssertionError | undefined;
 
-	const { sendBalance, sendTokenDecimals } = getContext<SendContext>(SEND_CONTEXT_KEY);
+	const { sendToken, sendBalance, sendTokenDecimals, sendTokenStandard } =
+		getContext<SendContext>(SEND_CONTEXT_KEY);
 
 	$: customValidate = (userAmount: BigNumber): Error | undefined => {
 		if (invalidAmount(userAmount.toNumber()) || userAmount.isZero()) {
@@ -26,12 +30,21 @@
 		// TODO: add check for fee, when we will calculate the fees
 	};
 
-	// TODO: Enable Max button by passing the `calculateMax` prop
+	$: calculateMax = (): number | undefined =>
+		isNullish($sendToken)
+			? undefined
+			: getMaxTransactionAmount({
+					balance: $sendBalance ?? ZERO,
+					fee: BigNumber.from(SOLANA_TRANSACTION_FEE_IN_LAMPORTS),
+					tokenDecimals: $sendTokenDecimals,
+					tokenStandard: $sendTokenStandard
+				});
 </script>
 
 <SendInputAmount
 	bind:amount
 	tokenDecimals={$sendTokenDecimals}
 	{customValidate}
+	{calculateMax}
 	bind:error={amountError}
 />
