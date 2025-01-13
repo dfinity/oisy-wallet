2e4762cfd70058b28d6c8e2893ae3584695af477
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-13 09:42:09 -0300

    feat(frontend): add SPL token POPCAT (#4194)
    # Motivation
    
    We add SPL token [POPCAT](https://www.popcatsolana.xyz/)
    ([Coinmarketcap](https://coinmarketcap.com/currencies/popcat-sol/)).
    
    Co-authored-by: Stefan Berger <stefan.berger@inacta.ch>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/env/tokens/tokens.spl.env.ts
+++ src/frontend/src/env/tokens/tokens.spl.env.ts
@@ -1,10 +1,11 @@
 import { DEVNET_EURC_TOKEN } from '$env/tokens/tokens-spl/tokens.eurc.env';
+import { JUP_TOKEN } from '$env/tokens/tokens-spl/tokens.jup.env';
 import { ORCA_TOKEN } from '$env/tokens/tokens-spl/tokens.orca.env';
 import { POPCAT_TOKEN } from '$env/tokens/tokens-spl/tokens.popcat.env';
 import { DEVNET_USDC_TOKEN } from '$env/tokens/tokens-spl/tokens.usdc.env';
 import type { RequiredSplToken } from '$sol/types/spl';
 
-export const SPL_TOKEN_MAINNET: RequiredSplToken[] = [ORCA_TOKEN, POPCAT_TOKEN];
+export const SPL_TOKEN_MAINNET: RequiredSplToken[] = [JUP_TOKEN, ORCA_TOKEN, POPCAT_TOKEN];
 
 export const SPL_TOKENS_DEVNET: RequiredSplToken[] = [DEVNET_USDC_TOKEN, DEVNET_EURC_TOKEN];
 
