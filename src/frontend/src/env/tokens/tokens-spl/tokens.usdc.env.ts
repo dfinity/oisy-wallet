16d10107c5d3ba6f6e8570a6013a401f2ff97ae8
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-14 07:44:41 -0300

    feat(frontend): add mainnet Solana USDC (#4227)
    # Motivation
    
    Including USDC mainnet for Solana.

--- src/frontend/src/env/tokens/tokens-spl/tokens.usdc.env.ts
+++ src/frontend/src/env/tokens/tokens-spl/tokens.usdc.env.ts
@@ -1,4 +1,4 @@
-import { SOLANA_DEVNET_NETWORK } from '$env/networks/networks.sol.env';
+import { SOLANA_DEVNET_NETWORK, SOLANA_MAINNET_NETWORK } from '$env/networks/networks.sol.env';
 import usdc from '$eth/assets/usdc.svg';
 import type { TokenId } from '$lib/types/token';
 import { parseTokenId } from '$lib/validation/token.validation';
@@ -7,6 +7,20 @@ import type { RequiredSplToken } from '$sol/types/spl';
 export const USDC_DECIMALS = 6;
 
 export const USDC_SYMBOL = 'USDC';
+
+export const USDC_TOKEN_ID: TokenId = parseTokenId(USDC_SYMBOL);
+
+export const USDC_TOKEN: RequiredSplToken = {
+	id: USDC_TOKEN_ID,
+	network: SOLANA_MAINNET_NETWORK,
+	standard: 'spl',
+	category: 'default',
+	name: 'USD Coin',
+	symbol: USDC_SYMBOL,
+	decimals: USDC_DECIMALS,
+	icon: usdc,
+	address: 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'
+};
 
 export const DEVNET_USDC_SYMBOL = 'DevnetUSDC';
 
@@ -18,7 +32,7 @@ export const DEVNET_USDC_TOKEN: RequiredSplToken = {
 	standard: 'spl',
 	category: 'default',
 	name: 'USDC (Devnet)',
-	symbol: USDC_SYMBOL,
+	symbol: DEVNET_USDC_SYMBOL,
 	decimals: USDC_DECIMALS,
 	icon: usdc,
 	address: '4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU'
