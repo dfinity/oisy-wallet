4d2d8ede9de3365c02327e92b17530a606f9d85d
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-01-14 16:34:41 +0100

    feat(frontend): SwapValueDifference component (#4262)
    # Motivation
    
    SwapValueDifference component:
    <img width="523" alt="Screenshot 2025-01-14 at 16 12 49"
    src="https://github.com/user-attachments/assets/8172c6ea-e5fe-4816-a01b-1b00be98e767"
    />

---
+++ src/frontend/src/lib/components/swap/SwapValueDifference.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { nonNullish } from '@dfinity/utils';
+	import { getContext } from 'svelte';
+	import {
+		SWAP_VALUE_DIFFERENCE_WARNING_VALUE,
+		SWAP_VALUE_DIFFERENCE_ERROR_VALUE
+	} from '$lib/constants/swap.constants';
+	import { SWAP_CONTEXT_KEY, type SwapContext } from '$lib/stores/swap.store';
+	import type { OptionAmount } from '$lib/types/send';
+
+	export let swapAmount: OptionAmount;
+	export let receiveAmount: number | undefined;
+
+	const { sourceTokenExchangeRate, destinationTokenExchangeRate } =
+		getContext<SwapContext>(SWAP_CONTEXT_KEY);
+
+	let paidValue: number | undefined;
+	$: paidValue =
+		nonNullish(swapAmount) && nonNullish($sourceTokenExchangeRate)
+			? Number(swapAmount) * $sourceTokenExchangeRate
+			: undefined;
+
+	let receivedValue: number | undefined;
+	$: receivedValue =
+		nonNullish(receiveAmount) && nonNullish($destinationTokenExchangeRate)
+			? receiveAmount * $destinationTokenExchangeRate
+			: undefined;
+
+	let valueDifference: number | undefined;
+	$: valueDifference =
+		nonNullish(paidValue) && nonNullish(receivedValue) && paidValue !== 0
+			? ((receivedValue - paidValue) / paidValue) * 100
+			: undefined;
+</script>
+
+{#if nonNullish(valueDifference)}
+	<span
+		class="underline"
+		class:text-success={valueDifference > SWAP_VALUE_DIFFERENCE_WARNING_VALUE}
+		class:text-warning={valueDifference <= SWAP_VALUE_DIFFERENCE_WARNING_VALUE &&
+			valueDifference > SWAP_VALUE_DIFFERENCE_ERROR_VALUE}
+		class:text-error={valueDifference <= SWAP_VALUE_DIFFERENCE_ERROR_VALUE}
+	>
+		{`${valueDifference > 0 ? '+' : ''}${valueDifference.toFixed(2)}`}%
+	</span>
+{/if}
