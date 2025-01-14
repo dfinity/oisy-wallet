09f9e6217e7cd7657449ab5a32b897679e213800
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-14 07:48:18 -0300

    feat(frontend): add mainnet Solana EURC (#4226)
    # Motivation
    
    Including EURC mainnet for Solana.

--- src/frontend/src/env/tokens/tokens-spl/tokens.eurc.env.ts
+++ src/frontend/src/env/tokens/tokens-spl/tokens.eurc.env.ts
@@ -1,5 +1,5 @@
-import { SOLANA_DEVNET_NETWORK } from '$env/networks/networks.sol.env';
-import usdc from '$eth/assets/usdc.svg';
+import { SOLANA_DEVNET_NETWORK, SOLANA_MAINNET_NETWORK } from '$env/networks/networks.sol.env';
+import eurc from '$eth/assets/eurc.svg';
 import type { TokenId } from '$lib/types/token';
 import { parseTokenId } from '$lib/validation/token.validation';
 import type { RequiredSplToken } from '$sol/types/spl';
@@ -7,6 +7,20 @@ import type { RequiredSplToken } from '$sol/types/spl';
 export const EURC_DECIMALS = 6;
 
 export const EURC_SYMBOL = 'EURC';
+
+export const EURC_TOKEN_ID: TokenId = parseTokenId(EURC_SYMBOL);
+
+export const EURC_TOKEN: RequiredSplToken = {
+	id: EURC_TOKEN_ID,
+	network: SOLANA_MAINNET_NETWORK,
+	standard: 'spl',
+	category: 'default',
+	name: 'Euro Coin',
+	symbol: EURC_SYMBOL,
+	decimals: EURC_DECIMALS,
+	icon: eurc,
+	address: 'HzwqbKZw8HxMN6bF2yFZNrht3c2iXXzpKcFu7uBEDKtr'
+};
 
 export const DEVNET_EURC_SYMBOL = 'DevnetEURC';
 
@@ -18,8 +32,8 @@ export const DEVNET_EURC_TOKEN: RequiredSplToken = {
 	standard: 'spl',
 	category: 'default',
 	name: 'EURC (Devnet)',
-	symbol: EURC_SYMBOL,
+	symbol: DEVNET_EURC_SYMBOL,
 	decimals: EURC_DECIMALS,
-	icon: usdc,
+	icon: eurc,
 	address: 'HzwqbKZw8HxMN6bF2yFZNrht3c2iXXzpKcFu7uBEDKtr'
 };
