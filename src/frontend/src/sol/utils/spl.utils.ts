43013799c943c44ac42025d473d8f6cdd2664943
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-14 13:37:25 -0300

    feat(frontend): enable manageability of SPL tokens (#4267)
    # Motivation
    
    We make the SPL tokens manageable. However, since the backend part is
    still not developed, the enabled/disabled tokens will be persisted only
    during the session (excluding possible refreshes of page).
    
    
    
    
    https://github.com/user-attachments/assets/3ea5e604-b804-44a8-8f29-2ab7909ed694
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/sol/utils/spl.utils.ts
+++ src/frontend/src/sol/utils/spl.utils.ts
@@ -1,4 +1,8 @@
 import type { Token } from '$lib/types/token';
 import type { SplToken } from '$sol/types/spl';
+import type { SplTokenToggleable } from '$sol/types/spl-token-toggleable';
 
 export const isTokenSpl = (token: Token): token is SplToken => token.standard === 'spl';
+
+export const isTokenSplToggleable = (token: Token): token is SplTokenToggleable =>
+	isTokenSpl(token) && 'enabled' in token;
