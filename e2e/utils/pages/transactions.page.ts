4c3f70d3d15e63d319225c81d191f60558fdfe09
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-15 11:16:26 -0300

    fix(e2e): adjust the tests for Token Cards (#4270)
    # Motivation
    
    In PR https://github.com/dfinity/oisy-wallet/pull/4201 I forgot to
    update the E2E tests for the new test ID of `TokenCard`.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- e2e/utils/pages/transactions.page.ts
+++ e2e/utils/pages/transactions.page.ts
@@ -3,19 +3,22 @@ import { HomepageLoggedIn, type HomepageLoggedInParams } from './homepage.page';
 
 export type TransactionsPageParams = {
 	tokenSymbol: string;
+	networkId: string;
 } & HomepageLoggedInParams;
 
 export class TransactionsPage extends HomepageLoggedIn {
 	readonly #tokenSymbol: string;
+	readonly #networkId: string;
 
-	constructor({ page, iiPage, viewportSize, tokenSymbol }: TransactionsPageParams) {
+	constructor({ page, iiPage, viewportSize, tokenSymbol, networkId }: TransactionsPageParams) {
 		super({ page, iiPage, viewportSize });
 
 		this.#tokenSymbol = tokenSymbol;
+		this.#networkId = networkId;
 	}
 
 	override async extendWaitForReady(): Promise<void> {
-		await this.clickByTestId(`${TOKEN_CARD}-${this.#tokenSymbol}`);
+		await this.clickByTestId(`${TOKEN_CARD}-${this.#tokenSymbol}-${this.#networkId}`);
 		await this.waitForLoadState();
 	}
 }
