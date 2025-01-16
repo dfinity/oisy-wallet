4e6ede867c8ae582beb34e116f8ac3fd46c1216b
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-16 14:53:06 +0100

    temp commit

--- src/frontend/src/lib/components/wallet-connect/WalletConnectSession.svelte
+++ src/frontend/src/lib/components/wallet-connect/WalletConnectSession.svelte
@@ -34,6 +34,8 @@
 		SESSION_REQUEST_SOL_SIGN_AND_SEND_TRANSACTION,
 		SESSION_REQUEST_SOL_SIGN_TRANSACTION
 	} from '$sol/constants/wallet-connect.constants';
+	import { getCompiledTransactionMessageDecoder } from '@solana/web3.js';
+	import type { TransactionMessageBytes } from '@solana/transactions';
 
 	export let listener: OptionWalletConnectListener;
 
@@ -228,13 +230,21 @@
 				id,
 				topic,
 				params: {
-					request: { method }
+					request: { method, params:{transaction} }
 				}
 			} = sessionRequest;
 
 			switch (method) {
 				case SESSION_REQUEST_SOL_SIGN_TRANSACTION: {
 					console.log(sessionRequest)
+
+					const decoder = getCompiledTransactionMessageDecoder();
+					const rawTransaction = Buffer.from(
+						transaction,
+						'base64'
+					) as unknown as TransactionMessageBytes;
+
+					console.log(123, decoder.decode(rawTransaction));
 					return;
 				}
 				case SESSION_REQUEST_ETH_SIGN_V4:
