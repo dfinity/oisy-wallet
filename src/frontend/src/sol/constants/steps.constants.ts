3d268c38d237cf829ef6c34e03bee91a9992cc7a
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-20 14:51:23 -0300

    feat(frontend): modal to sign Solana requests from WalletConnect (#4330)
    # Motivation
    
    Similar to Ethereum, we create a modal that will be used in case the
    WalletConnect listener receives a Solana sign request.
    
    ![Screenshot 2025-01-20 at 11 32
    45](https://github.com/user-attachments/assets/34d7d83c-bbc2-4a5e-b9d8-a440b000ad76)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/sol/constants/steps.constants.ts
+++ src/frontend/src/sol/constants/steps.constants.ts
@@ -1,4 +1,4 @@
-import { ProgressStepsSendSol } from '$lib/enums/progress-steps';
+import { ProgressStepsSendSol, ProgressStepsSign } from '$lib/enums/progress-steps';
 import type { ProgressSteps } from '$lib/types/progress-steps';
 
 export const sendSteps = (i18n: I18n): ProgressSteps => [
@@ -15,6 +15,39 @@ export const sendSteps = (i18n: I18n): ProgressSteps => [
 	{
 		step: ProgressStepsSendSol.RELOAD,
 		text: i18n.send.text.refreshing_ui,
+		state: 'next'
+	}
+];
+
+export const walletConnectSignSteps = ({
+	i18n,
+	signWithSending
+}: {
+	i18n: I18n;
+	signWithSending: boolean;
+}): ProgressSteps => [
+	{
+		step: ProgressStepsSign.INITIALIZATION,
+		text: i18n.send.text.initializing,
+		state: 'in_progress'
+	},
+	{
+		step: ProgressStepsSign.SIGN,
+		text: i18n.send.text.signing_message,
+		state: 'next'
+	},
+	...(signWithSending
+		? ([
+				{
+					step: ProgressStepsSendSol.SEND,
+					text: i18n.send.text.sending,
+					state: 'next'
+				}
+			] as ProgressSteps)
+		: []),
+	{
+		step: ProgressStepsSign.APPROVE_WALLET_CONNECT,
+		text: i18n.send.text.approving,
 		state: 'next'
 	}
 ];
