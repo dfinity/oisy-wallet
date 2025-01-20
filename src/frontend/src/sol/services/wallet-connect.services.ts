31e4d7dd553dab6bf46ec15f344aecdf3ff2c07f
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-20 14:20:34 -0300

    feat(frontend): create decode service for Solana WalletConnect (#4341)
    # Motivation
    
    Service to help decoding the base-64 transaction that arrives through
    Solana WalletConnect.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/frontend/src/sol/services/wallet-connect.services.ts
@@ -0,0 +1,37 @@
+import { i18n } from '$lib/stores/i18n.store';
+import type { NetworkId } from '$lib/types/network';
+import { replacePlaceholders } from '$lib/utils/i18n.utils';
+import { solanaHttpRpc } from '$sol/providers/sol-rpc.providers';
+import { mapNetworkIdToNetwork } from '$sol/utils/network.utils';
+import {
+	mapSolTransactionMessage,
+	parseSolBase64TransactionMessage
+} from '$sol/utils/sol-transactions.utils';
+import { assertNonNullish } from '@dfinity/utils';
+import { get } from 'svelte/store';
+
+interface WalletConnectDecodeTransactionParams {
+	base64EncodedTransactionMessage: string;
+	networkId: NetworkId;
+}
+
+export const decode = async ({
+	base64EncodedTransactionMessage,
+	networkId
+}: WalletConnectDecodeTransactionParams) => {
+	const solNetwork = mapNetworkIdToNetwork(networkId);
+
+	assertNonNullish(
+		solNetwork,
+		replacePlaceholders(get(i18n).init.error.no_solana_network, {
+			$network: networkId.description ?? ''
+		})
+	);
+
+	const parsedTransactionMessage = await parseSolBase64TransactionMessage({
+		transactionMessage: base64EncodedTransactionMessage,
+		rpc: solanaHttpRpc(solNetwork)
+	});
+
+	return mapSolTransactionMessage(parsedTransactionMessage);
+};
