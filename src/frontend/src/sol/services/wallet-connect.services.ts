e9041fc04cf4fc3004192348db02a59a0973f82b
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-20 17:53:28 +0100

    remove unused

--- src/frontend/src/sol/services/wallet-connect.services.ts
+++ src/frontend/src/sol/services/wallet-connect.services.ts
@@ -1,11 +1,5 @@
-import { ProgressStepsSendSol, ProgressStepsSign } from '$lib/enums/progress-steps';
-import { type WalletConnectExecuteParams } from '$lib/services/wallet-connect.services';
 import { i18n } from '$lib/stores/i18n.store';
-import type { OptionSolAddress } from '$lib/types/address';
-import type { OptionIdentity } from '$lib/types/identity';
 import type { NetworkId } from '$lib/types/network';
-import type { Token } from '$lib/types/token';
-import type { OptionWalletConnectListener } from '$lib/types/wallet-connect';
 import { replacePlaceholders } from '$lib/utils/i18n.utils';
 import { solanaHttpRpc } from '$sol/providers/sol-rpc.providers';
 import { mapNetworkIdToNetwork } from '$sol/utils/network.utils';
@@ -20,15 +14,6 @@ interface WalletConnectDecodeTransactionParams {
 	base64EncodedTransactionMessage: string;
 	networkId: NetworkId;
 }
-
-type WalletConnectSignTransactionParams = WalletConnectExecuteParams & {
-	listener: OptionWalletConnectListener;
-	address: OptionSolAddress;
-	modalNext: () => void;
-	progress: (step: ProgressStepsSign | ProgressStepsSendSol.SEND) => void;
-	token: Token;
-	identity: OptionIdentity;
-};
 
 export const decode = async ({
 	base64EncodedTransactionMessage,
