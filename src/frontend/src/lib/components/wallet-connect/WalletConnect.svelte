a44e75f27dca95c72593364150e4deacb81a2329
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-20 17:21:08 +0100

    feat(frontend): add Solana requests to WalletConnectSign

--- src/frontend/src/lib/components/wallet-connect/WalletConnect.svelte
+++ src/frontend/src/lib/components/wallet-connect/WalletConnect.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import WalletConnectSend from '$eth/components/wallet-connect/WalletConnectSend.svelte';
-	import WalletConnectSign from '$eth/components/wallet-connect/WalletConnectSign.svelte';
+	import WalletConnectSign from '$lib/components/wallet-connect/WalletConnectSign.svelte';
 	import WalletConnectSession from '$lib/components/wallet-connect/WalletConnectSession.svelte';
 	import type { OptionWalletConnectListener } from '$lib/types/wallet-connect';
 
