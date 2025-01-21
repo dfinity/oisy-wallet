797640492f4dde459e6251fccf04a53ee6d1b894
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-21 18:33:52 +0100

    test: add

--- src/frontend/src/tests/lib/stores/user-tokens.store.spec.ts
+++ src/frontend/src/tests/lib/stores/user-tokens.store.spec.ts
@@ -1,3 +1,7 @@
+import { PEPE_TOKEN } from '$env/tokens/tokens-erc20/tokens.pepe.env';
+import { USDC_TOKEN } from '$env/tokens/tokens-erc20/tokens.usdc.env';
+import { BONK_TOKEN } from '$env/tokens/tokens-spl/tokens.bonk.env';
+import { TRUMP_TOKEN } from '$env/tokens/tokens-spl/tokens.trump.env';
 import { BTC_MAINNET_TOKEN } from '$env/tokens/tokens.btc.env';
 import { ETHEREUM_TOKEN } from '$env/tokens/tokens.eth.env';
 import { ICP_TOKEN } from '$env/tokens/tokens.icp.env';
@@ -31,7 +35,6 @@ describe('user-token.store', () => {
 					data: { ...token, enabled },
 					certified
 				}));
-
 				mockStore.setAll(mockTokens);
 
 				expect(get(mockStore)).toEqual(mockTokens);
@@ -43,7 +46,6 @@ describe('user-token.store', () => {
 					data: { ...token, enabled },
 					certified
 				}));
-
 				mockStore.setAll(mockTokens);
 
 				const otherTokens = [SOLANA_TOKEN];
@@ -51,7 +53,6 @@ describe('user-token.store', () => {
 					data: { ...token, enabled: false },
 					certified
 				}));
-
 				mockStore.setAll(otherMockTokens);
 
 				const expectedResults = [...mockTokens, ...otherMockTokens];
@@ -65,7 +66,6 @@ describe('user-token.store', () => {
 					data: { ...token, enabled },
 					certified
 				}));
-
 				mockStore.setAll(mockTokens);
 
 				const otherTokens = [BTC_MAINNET_TOKEN];
@@ -73,7 +73,6 @@ describe('user-token.store', () => {
 					data: { ...token, enabled: false },
 					certified
 				}));
-
 				mockStore.setAll(otherMockTokens);
 
 				const expectedResults = [...mockTokens.slice(1), ...otherMockTokens];
@@ -87,7 +86,6 @@ describe('user-token.store', () => {
 					data: { ...token, enabled },
 					certified
 				}));
-
 				mockStore.setAll(mockTokens);
 
 				const otherTokens = [BTC_MAINNET_TOKEN, SOLANA_TOKEN];
@@ -95,12 +93,47 @@ describe('user-token.store', () => {
 					data: { ...token, enabled: false },
 					certified
 				}));
-
 				mockStore.setAll(otherMockTokens);
 
 				const expectedResults = [...mockTokens.slice(1), ...otherMockTokens];
 
 				expect(get(mockStore)).toEqual(expectedResults);
+			});
+
+			it('should use the token address as identifier for ERC20 tokens', () => {
+				mockStore.setAll([{ data: { ...USDC_TOKEN, enabled }, certified }]);
+				const erc20Token2 = { ...PEPE_TOKEN, address: USDC_TOKEN.address };
+				mockStore.setAll([{ data: { ...erc20Token2, enabled }, certified }]);
+
+				const expectedResults = [
+					{ data: { ...erc20Token2, enabled, id: USDC_TOKEN.id }, certified }
+				];
+
+				expect(get(mockStore)).toEqual(expectedResults);
+			});
+
+			it('should use the token address as identifier for SPL tokens', () => {
+				mockStore.setAll([{ data: { ...BONK_TOKEN, enabled }, certified }]);
+				const erc20Token2 = { ...TRUMP_TOKEN, address: BONK_TOKEN.address };
+				mockStore.setAll([{ data: { ...erc20Token2, enabled }, certified }]);
+
+				const expectedResults = [
+					{ data: { ...erc20Token2, enabled, id: BONK_TOKEN.id }, certified }
+				];
+
+				expect(get(mockStore)).toEqual(expectedResults);
+			});
+
+			// TODO: solve this test
+			it('should duplicate a token if it is already given as duplicate', () => {
+				const tokens = [BTC_MAINNET_TOKEN, BTC_MAINNET_TOKEN];
+				const mockTokens = tokens.map((token) => ({
+					data: { ...token, enabled },
+					certified
+				}));
+				mockStore.setAll(mockTokens);
+
+				expect(get(mockStore)).toEqual(mockTokens);
 			});
 		});
 
