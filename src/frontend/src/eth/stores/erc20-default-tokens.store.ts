64dc3e45bed3583f14bd058e70830503da03f9a0
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-21 20:39:15 +0100

    fix: type

--- src/frontend/src/eth/stores/erc20-default-tokens.store.ts
+++ src/frontend/src/eth/stores/erc20-default-tokens.store.ts
@@ -1,3 +1,4 @@
+import type { Erc20Token } from '$eth/types/erc20';
 import { initDefaultTokensStore } from '$lib/stores/default-tokens.store';
 
-export const erc20DefaultTokensStore = initDefaultTokensStore();
+export const erc20DefaultTokensStore = initDefaultTokensStore<Erc20Token>();
