c4e6ff9d55250646f5e40804feacf2bd323f3a93
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-21 21:03:04 +0100

    feat(frontend): load SPL user tokens

---
+++ src/frontend/src/sol/stores/spl-user-tokens.store.ts
@@ -0,0 +1,8 @@
+import type { SplToken, SplTokenAddress } from '$sol/types/spl';
+import type { PrincipalText } from '@dfinity/zod-schemas';
+
+export const SPL_USER_TOKENS_KEY = 'spl-user-tokens';
+
+export type SplAddressMap = Record<PrincipalText, SplTokenAddress[]>;
+
+export const splUserTokensStore = initCertifiedUserTokensStore<SplToken>();
