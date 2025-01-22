46b4b16f46d81c9272229149ba3416921ee4a35f
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-22 19:48:44 +0100

    temp

--- src/frontend/src/env/networks/networks.sol.env.ts
+++ src/frontend/src/env/networks/networks.sol.env.ts
@@ -24,7 +24,7 @@ export const SOLANA_RPC_HTTP_URL_TESTNET = 'https://api.testnet.solana.com';
 export const SOLANA_RPC_HTTP_URL_DEVNET = `https://solana-devnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`;
 export const SOLANA_RPC_HTTP_URL_LOCAL = 'http://localhost:8899';
 
-export const SOLANA_RPC_WS_URL_MAINNET = 'ws://api.mainnet-beta.solana.com/';
+export const SOLANA_RPC_WS_URL_MAINNET = `wss://solana-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`;
 export const SOLANA_RPC_WS_URL_TESTNET = 'wss://api.testnet.solana.com/';
 export const SOLANA_RPC_WS_URL_DEVNET = 'wss://api.devnet.solana.com/';
 export const SOLANA_RPC_WS_URL_LOCAL = 'ws://localhost:8900';
