4c07a2d535c704ecbcbc7538d70914f73c66ffb3
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-22 04:38:20 -0300

    refactor(frontend): extract function to init an user token store (#4378)
    # Motivation
    
    We will need to use the user token store for other networks (like
    Solana). So, we move it to `lib` and adapt the code.
    
    # Changes
    
    - Create util to identify an ERC20 token based on the standard.
    - Move `initCertifiedErc20UserTokensStore` (and relative types) to a
    `lib` folder.
    - Rename `initCertifiedErc20UserTokensStore` to
    `initCertifiedUserTokensStore`.
    - Create sub-function to select the correct identifier: token address
    for ERC20 and SPL, and ID for all the others.
    - Adapt the rest of the code: functions and types.
    
    # Tests
    
    Tests provided.

--- src/frontend/src/eth/stores/erc20-user-tokens.store.ts
+++ src/frontend/src/eth/stores/erc20-user-tokens.store.ts
@@ -1,44 +1,4 @@
-import type { Erc20UserToken } from '$eth/types/erc20-user-token';
-import type { CertifiedData } from '$lib/types/store';
-import type { TokenId } from '$lib/types/token';
-import type { Option } from '$lib/types/utils';
-import { writable, type Readable } from 'svelte/store';
+import type { Erc20Token } from '$eth/types/erc20';
+import { initCertifiedUserTokensStore } from '$lib/stores/user-tokens.store';
 
-export type CertifiedErc20UserTokensData = Option<CertifiedData<Erc20UserToken>[]>;
-
-export interface CertifiedErc20UserTokensStore extends Readable<CertifiedErc20UserTokensData> {
-	setAll: (tokens: CertifiedData<Erc20UserToken>[]) => void;
-	reset: (tokenId: TokenId) => void;
-	resetAll: () => void;
-}
-
-export const initCertifiedErc20UserTokensStore = (): CertifiedErc20UserTokensStore => {
-	const { subscribe, update, set } = writable<CertifiedErc20UserTokensData>(undefined);
-
-	return {
-		setAll: (tokens: CertifiedData<Erc20UserToken>[]) =>
-			update((state) => [
-				...(state ?? []).filter(
-					({ data: { address } }) =>
-						!tokens.map(({ data: { address } }) => address).includes(address)
-				),
-				...tokens.map(({ data, certified }) => ({
-					certified,
-					data: {
-						...data,
-						// We are using Symbols as key IDs for the ETH and ICP tokens, which is ideal for our use case due to their uniqueness. This ensures that even if two coins fetched dynamically have the same symbol or name, they will be used correctly.
-						// However, this approach presents a challenge with ERC20 tokens, which need to be loaded twice - once with a query and once with an update. When they are loaded the second time, the existing Symbol should be reused to ensure they are identified as the same token.
-						id:
-							(state ?? []).find(({ data: { address } }) => address === data.address)?.data.id ??
-							data.id
-					}
-				}))
-			]),
-		reset: (tokenId: TokenId) =>
-			update((state) => [...(state ?? []).filter(({ data: { id } }) => id !== tokenId)]),
-		resetAll: () => set(null),
-		subscribe
-	};
-};
-
-export const erc20UserTokensStore = initCertifiedErc20UserTokensStore();
+export const erc20UserTokensStore = initCertifiedUserTokensStore<Erc20Token>();
