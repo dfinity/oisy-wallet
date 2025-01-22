66e41ca5d0c21e92d56222cbcafef20c11eceac5
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-22 04:57:17 -0300

    refactor(frontend): extract function to init a default tokens store (#4379)
    # Motivation
    
    We will need to use the deafult-tokens store for other networks (like
    Solana). So, we move it to `lib` and adapt the code.
    
    # Changes
    
    - Create util to identify an ERC20 token based on the standard.
    - Move `initEcr20DefaultTokensStore` (and relative types) to a `lib`
    folder.
    - Rename `initEcr20DefaultTokensStore` to `initDefaultTokensStore`.
    - Create sub-function to select the correct identifier: token address
    for ERC20 and SPL, and ID for all the others.
    - Adapt the rest of the code: functions and types.
    
    # Tests
    
    Tests provided.

---
+++ src/frontend/src/lib/stores/default-tokens.store.ts
@@ -0,0 +1,45 @@
+import type { Erc20ContractAddress } from '$eth/types/erc20';
+import { isTokenErc20 } from '$eth/utils/erc20.utils';
+import type { Token, TokenId } from '$lib/types/token';
+import type { SplTokenAddress } from '$sol/types/spl';
+import { isTokenSpl } from '$sol/utils/spl.utils';
+import { writable, type Readable } from 'svelte/store';
+
+type DefaultTokensData<T extends Token> = T[] | undefined;
+
+export interface DefaultTokensStore<T extends Token> extends Readable<DefaultTokensData<T>> {
+	set: (tokens: DefaultTokensData<T>) => void;
+	add: (token: T) => void;
+	remove: (tokenId: TokenId) => void;
+	reset: () => void;
+}
+
+export const initDefaultTokensStore = <T extends Token>(): DefaultTokensStore<T> => {
+	const INITIAL: DefaultTokensData<T> = undefined;
+
+	const { subscribe, set, update } = writable<DefaultTokensData<T>>(INITIAL);
+
+	const getIdentifier = <T extends Token>(
+		token: T
+	): string | Erc20ContractAddress['address'] | SplTokenAddress =>
+		isTokenSpl(token)
+			? token.address
+			: isTokenErc20(token)
+				? token.address
+				: (token.id.description ?? '');
+
+	return {
+		set,
+		add: (token: T) =>
+			update((state) => [
+				...(state ?? []).filter(
+					(data) => getIdentifier(data).toLowerCase() !== getIdentifier(token).toLowerCase()
+				),
+				token
+			]),
+		remove: (tokenId: TokenId) =>
+			update((state) => [...(state ?? []).filter(({ id }) => id !== tokenId)]),
+		reset: () => set(INITIAL),
+		subscribe
+	};
+};
