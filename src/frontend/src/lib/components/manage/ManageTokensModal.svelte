9e061210dfb8d91cd8643ed4bd0faa06929bd974
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-22 06:40:18 -0300

    feat(frontend): use saveSplUserTokens in ManageTokensModal (#4394)
    # Motivation
    
    We can now use the `saveSplUserTokens` service in `ManageTokensModal`.

--- src/frontend/src/lib/components/manage/ManageTokensModal.svelte
+++ src/frontend/src/lib/components/manage/ManageTokensModal.svelte
@@ -28,7 +28,7 @@
 	import type { Network } from '$lib/types/network';
 	import { isNullishOrEmpty } from '$lib/utils/input.utils';
 	import { isNetworkIdEthereum, isNetworkIdICP } from '$lib/utils/network.utils';
-	import { splDefaultTokensStore } from '$sol/stores/spl-default-tokens.store';
+	import { saveSplUserTokens } from '$sol/services/manage-tokens.services';
 	import type { SplTokenToggleable } from '$sol/types/spl-token-toggleable';
 
 	const steps: WizardSteps = [
@@ -145,12 +145,14 @@
 
 	// TODO: implement this function in the backend
 	const saveSpl = (tokens: SplTokenToggleable[]): void => {
-		modal.set(3);
-		progress(ProgressStepsAddToken.SAVE);
-		splDefaultTokensStore.set(tokens);
-		progress(ProgressStepsAddToken.UPDATE_UI);
-		progress(ProgressStepsAddToken.DONE);
-		setTimeout(() => close(), 750);
+		saveSplUserTokens({
+			tokens,
+			progress,
+			modalNext: () => modal.set(3),
+			onSuccess: close,
+			onError: () => modal.set(0),
+			identity: $authIdentity
+		});
 	};
 
 	const close = () => {
