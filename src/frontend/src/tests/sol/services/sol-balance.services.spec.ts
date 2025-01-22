d3f4860db05f5edefbf7221f5009d4e52b40d495
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-22 10:17:02 +0100

    fix(test): correct test to load Sol balance

--- src/frontend/src/tests/sol/services/sol-balance.services.spec.ts
+++ src/frontend/src/tests/sol/services/sol-balance.services.spec.ts
@@ -1,22 +1,52 @@
-import { SOLANA_TESTNET_TOKEN } from '$env/tokens/tokens.sol.env';
+import {
+	SOLANA_DEVNET_TOKEN,
+	SOLANA_LOCAL_TOKEN,
+	SOLANA_TESTNET_TOKEN,
+	SOLANA_TOKEN
+} from '$env/tokens/tokens.sol.env';
 import { balancesStore } from '$lib/stores/balances.store';
 import type { Token } from '$lib/types/token';
+import * as solRpcProviders from '$sol/providers/sol-rpc.providers';
 import { loadSolBalance } from '$sol/services/sol-balance.services';
 import { mockSolAddress } from '$tests/mocks/sol.mock';
+import { lamports } from '@solana/rpc-types';
 import { get } from 'svelte/store';
+import type { MockInstance } from 'vitest';
+
+vi.mock('$sol/providers/sol-rpc.providers');
 
 describe('sol-balance.services', () => {
-	// TODO: change DEVNET to use the normal RPC and not alchemy, and add it to this tests
-	const solanaTokens: Token[] = [SOLANA_TESTNET_TOKEN];
-
 	describe('loadSolBalance', () => {
+		let mockGetBalance: MockInstance;
+
+		const mockBalance = 500000n;
+		const solanaTokens: Token[] = [
+			SOLANA_TOKEN,
+			SOLANA_TESTNET_TOKEN,
+			SOLANA_DEVNET_TOKEN,
+			SOLANA_LOCAL_TOKEN
+		];
+
+		beforeEach(() => {
+			vi.clearAllMocks();
+
+			mockGetBalance = vi
+				.fn()
+				.mockReturnValue({ send: () => Promise.resolve({ value: lamports(mockBalance) }) });
+
+			const mockSolanaHttpRpc = vi.fn().mockReturnValue({
+				getBalance: mockGetBalance
+			});
+			vi.mocked(solRpcProviders.solanaHttpRpc).mockImplementation(mockSolanaHttpRpc);
+		});
+
 		it.each(solanaTokens)(
 			'should return the balance in SOL of the $name native token for the address',
 			async (token) => {
 				const result = await loadSolBalance({ address: mockSolAddress, token });
 
 				expect(result).toEqual({ success: true });
-				expect(get(balancesStore)?.[token.id]?.data.toNumber()).toBeGreaterThanOrEqual(0);
+				expect(get(balancesStore)?.[token.id]?.data.toBigInt()).toBe(mockBalance);
 			}
 		);
 	}, 60000);
