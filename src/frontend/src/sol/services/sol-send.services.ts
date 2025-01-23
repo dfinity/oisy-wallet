869f0802b43ff0accf06d034f5718645bf72e43e
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-23 08:38:54 +0100

    temp

--- src/frontend/src/sol/services/sol-send.services.ts
+++ src/frontend/src/sol/services/sol-send.services.ts
@@ -181,17 +181,11 @@ const createSplTokenTransactionMessage = async ({
 		calculatedDestinationTokenAccountAddress
 	);
 
-	return pipe(
-		await createDefaultTransaction({
-			rpc,
-			feePayer: signer,
-			version: mustCreateDestinationTokenAccount ? 0 : 'legacy'
-		}),
-		(tx) =>
-			appendTransactionMessageInstructions(
-				[...(mustCreateDestinationTokenAccount ? [ataInstruction] : []), transferInstruction],
-				tx
-			)
+	return pipe(await createDefaultTransaction({ rpc, feePayer: signer }), (tx) =>
+		appendTransactionMessageInstructions(
+			[...(mustCreateDestinationTokenAccount ? [ataInstruction] : []), transferInstruction],
+			tx
+		)
 	);
 };
 
