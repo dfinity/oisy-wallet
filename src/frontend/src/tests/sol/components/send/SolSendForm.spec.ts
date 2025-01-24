01042af810603d6b74939b68e5ab46d6ca978bdd
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-24 04:22:39 -0300

    test(frontend): test for ATA creation fee (#4393)
    # Motivation
    
    For completion we had some tests in `SolSendFOrm`, to check the
    rendering of the ATA fee.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Max <bitdivine@users.noreply.github.com>

--- src/frontend/src/tests/sol/components/send/SolSendForm.spec.ts
+++ src/frontend/src/tests/sol/components/send/SolSendForm.spec.ts
@@ -1,7 +1,10 @@
+import { TRUMP_TOKEN } from '$env/tokens/tokens-spl/tokens.trump.env';
 import { SOLANA_TOKEN } from '$env/tokens/tokens.sol.env';
 import { SEND_CONTEXT_KEY, initSendContext } from '$lib/stores/send.store';
+import * as solanaApi from '$sol/api/solana.api';
 import SolSendForm from '$sol/components/send/SolSendForm.svelte';
 import { mockSolAddress, mockSolAddress2 } from '$tests/mocks/sol.mock';
+import { lamports } from '@solana/rpc-types';
 import { render } from '@testing-library/svelte';
 
 describe('SolSendForm', () => {
@@ -9,7 +12,7 @@ describe('SolSendForm', () => {
 
 	const props = {
 		destination: mockSolAddress2,
-		amount: 22_000_000,
+		amount: 22_000,
 		source: mockSolAddress
 	};
 
@@ -18,6 +21,7 @@ describe('SolSendForm', () => {
 	const sourceSelector = 'div[id="source"]';
 	const balanceSelector = 'div[id="balance"]';
 	const feeSelector = 'p[id="fee"]';
+	const ataFeeSelector = 'p[id="ataFee"]';
 	const toolbarSelector = 'div[data-tid="toolbar"]';
 
 	it('should render all fields', () => {
@@ -51,5 +55,60 @@ describe('SolSendForm', () => {
 
 		const toolbar: HTMLDivElement | null = container.querySelector(toolbarSelector);
 		expect(toolbar).not.toBeNull();
+	});
+
+	describe('with SPL token', () => {
+		beforeEach(() => {
+			vi.resetAllMocks();
+
+			mockContext.set(
+				SEND_CONTEXT_KEY,
+				initSendContext({
+					sendPurpose: 'send',
+					token: TRUMP_TOKEN
+				})
+			);
+		});
+
+		it('should not render ATA creation fee if the destination is empty', () => {
+			const { container } = render(SolSendForm, {
+				props: { ...props, destination: '' },
+				context: mockContext
+			});
+
+			const ataFee: HTMLParagraphElement | null = container.querySelector(ataFeeSelector);
+			expect(ataFee).toBeNull();
+		});
+
+		it('should render ATA creation fee if there is no ATA for the destination', async () => {
+			vi.spyOn(solanaApi, 'loadTokenAccount').mockResolvedValueOnce(undefined);
+			vi.spyOn(solanaApi, 'getSolCreateAccountFee').mockResolvedValueOnce(lamports(123n));
+
+			const { container } = render(SolSendForm, {
+				props,
+				context: mockContext
+			});
+
+			// Wait for the fee to be loaded
+			await new Promise((resolve) => setTimeout(resolve, 100));
+
+			const ataFee: HTMLParagraphElement | null = container.querySelector(ataFeeSelector);
+			expect(ataFee).not.toBeNull();
+		});
+
+		it('should not render ATA creation fee if there is an ATA for the destination', async () => {
+			vi.spyOn(solanaApi, 'loadTokenAccount').mockResolvedValueOnce('mock-ata');
+
+			const { container } = render(SolSendForm, {
+				props,
+				context: mockContext
+			});
+
+			// Wait for the fee to be loaded
+			await new Promise((resolve) => setTimeout(resolve, 100));
+
+			const ataFee: HTMLParagraphElement | null = container.querySelector(ataFeeSelector);
+			expect(ataFee).toBeNull();
+		});
 	});
 });
