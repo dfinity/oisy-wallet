45d4e68dc37787c299d0e6581d261ff8562f3185
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-27 11:15:00 +0100

    test(frontend): fail when tests logs to console

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,12 +1,15 @@
+import { mockPage } from '$tests/mocks/page.store.mock';
+import { failTestsThatLogToConsole } from '$tests/utils/console.test-utils';
 import '@testing-library/jest-dom';
 import { configure } from '@testing-library/svelte';
 import 'fake-indexeddb/auto';
 import { vi } from 'vitest';
-import { mockPage } from './src/frontend/src/tests/mocks/page.store.mock';
 
 vi.mock('$app/stores', () => ({
 	page: mockPage
 }));
+
+failTestsThatLogToConsole();
 
 configure({
 	testIdAttribute: 'data-tid'
