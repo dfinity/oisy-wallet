1310bb88d316b2c1c00680a55abb25bd893f0108
Max Murphy <max@winning.black>
2025-01-27 16:56:00 +0100

    Revert "Simplify by removing the Serde code"
    This reverts commit 3fe988b0a8235966f7ece081788dbc0944cc044f.

--- src/shared/src/types/tests.rs
+++ src/shared/src/types/tests.rs
@@ -25,4 +25,15 @@ mod custom_token {
             assert_eq!(*expected, result.is_ok());
         }
     }
+    #[ignore] // This does NOT currently work for candid
+    #[test]
+    fn spl_token_validation_works_for_candid() {
+        for (input, expected) in SPL_TEST_VECTORS.iter() {
+            let spl_token_id = SplTokenId(input.to_string());
+
+            let candid = Encode!(&spl_token_id).unwrap();
+            let result: Result<SplTokenId, _> = Decode!(&candid, SplTokenId);
+            assert_eq!(*expected, result.is_ok());
+        }
+    }
 }
