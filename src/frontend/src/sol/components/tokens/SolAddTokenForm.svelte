a15071af0db1d2005ffc452a854c3b2e048e55b1
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-27 21:36:23 +0100

    feat(frontend): component SolAddTokenForm to add an SPL token

---
+++ src/frontend/src/sol/components/tokens/SolAddTokenForm.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import InputText from '$lib/components/ui/InputText.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
+
+	export let tokenAddress = '';
+</script>
+
+<label for="destination" class="px-4.5 font-bold">{$i18n.tokens.text.token_address}:</label>
+<InputText
+	name="tokenAddress"
+	bind:value={tokenAddress}
+	placeholder={$i18n.tokens.placeholder.enter_token_address}
+/>
