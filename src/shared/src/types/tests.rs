083bbac4d3d7894961612e3f477170d51a522f10
Max Murphy <max@winning.black>
2025-01-28 07:34:13 +0100

    Test ICRC1 validation

--- src/shared/src/types/tests.rs
+++ src/shared/src/types/tests.rs
@@ -39,4 +39,123 @@ mod custom_token {
             }
         }
     }
+
+    mod icrc {
+        //! Tests for the icrc module.
+        use candid::Principal;
+
+        use super::*;
+        struct TestVector {
+            input: IcrcToken,
+            expected: bool,
+            description: &'static str,
+        }
+        fn test_vectors() -> Vec<TestVector> {
+            // Principals to use in the test vectors
+            let canister_id1 = Principal::from_text("um5iw-rqaaa-aaaaq-qaaba-cai").unwrap();
+            let canister_id2 = Principal::from_text("rdmx6-jaaaa-aaaaa-aaadq-cai").unwrap();
+            let user_id = Principal::from_text(
+                "tdb26-jop6k-aogll-7ltgs-eruif-6kk7m-qpktf-gdiqx-mxtrf-vb5e6-eqe",
+            )
+            .unwrap();
+
+            vec![
+                TestVector {
+                    input: IcrcToken {
+                        ledger_id: canister_id1,
+                        index_id: None,
+                    },
+                    expected: true,
+                    description: "IcrcToken with valid ledger_id",
+                },
+                TestVector {
+                    input: IcrcToken {
+                        ledger_id: canister_id2,
+                        index_id: Some(canister_id1),
+                    },
+                    expected: true,
+                    description: "IcrcToken with valid ledger_id and index",
+                },
+                TestVector {
+                    input: IcrcToken {
+                        ledger_id: Principal::anonymous(),
+                        index_id: None,
+                    },
+                    expected: false,
+                    description: "IcrcToken with anonymous ledger_id",
+                },
+                TestVector {
+                    input: IcrcToken {
+                        ledger_id: Principal::anonymous(),
+                        index_id: Some(Principal::anonymous()),
+                    },
+                    expected: false,
+                    description: "IcrcToken with anonymous ledger_id and index",
+                },
+                TestVector {
+                    input: IcrcToken {
+                        ledger_id: canister_id1,
+                        index_id: Some(Principal::anonymous()),
+                    },
+                    expected: false,
+                    description: "IcrcToken with valid ledger_id and anonymous index",
+                },
+                TestVector {
+                    input: IcrcToken {
+                        ledger_id: Principal::management_canister(),
+                        index_id: Some(canister_id2),
+                    },
+                    expected: false,
+                    description: "IcrcToken with the management canister as ledger_id",
+                },
+                TestVector {
+                    input: IcrcToken {
+                        ledger_id: Principal::management_canister(),
+                        index_id: Some(Principal::anonymous()),
+                    },
+                    expected: false,
+                    description:
+                        "IcrcToken with the management canister as ledger_id and anonymous index",
+                },
+                TestVector {
+                    input: IcrcToken {
+                        ledger_id: Principal::management_canister(),
+                        index_id: None,
+                    },
+                    expected: false,
+                    description: "IcrcToken with the management canister as ledger_id and no index",
+                },
+                TestVector {
+                    input: IcrcToken {
+                        ledger_id: Principal::anonymous(),
+                        index_id: Some(Principal::management_canister()),
+                    },
+                    expected: false,
+                    description:
+                        "IcrcToken with anonymous ledger_id and the management canister as index",
+                },
+                TestVector {
+                    input: IcrcToken {
+                        ledger_id: user_id,
+                        index_id: None,
+                    },
+                    expected: false,
+                    description: "IcrcToken with user or network principal as ledger_id",
+                },
+            ]
+        }
+
+        #[test]
+        fn validate_icrc_token() {
+            for TestVector {
+                input,
+                expected,
+                description,
+            } in test_vectors()
+            {
+                let result = input.validate();
+                assert_eq!(expected, result.is_ok(), "{}", description);
+            }
+        }
+    }
 }
