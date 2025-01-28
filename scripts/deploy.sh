7c0a616144308d759006fb22d4030e7b37d0066f
Max <bitdivine@users.noreply.github.com>
2025-01-28 08:48:17 +0100

    fix(ci): Clean up deployment (#4473)
    # Motivation
    `update-snapshots.yaml` is failing. Reading through the code there are
    quite a few places that are in need of tidying up. Simplifying the code
    is quite effective at exposing where the bug is.
    
    # Changes
    - Specify canister IDs in dfx.json rather than with flags in deploy.sh.
    (Note: There are a few more places where this still needs to be done).
    - When getting cached Wasms, put them where dfx expects them, so that no
    additional flags are needed.
    - Delete duplicate software installation.
    - When using `dfx deploy backend`, support build strategies that just
    use the existing artefacts. This is just for speed.
    
    # Tests
    - See CI. update-snapshots is triggered by this PR. Formerly the
    canister installation failed. Now it succeeds.
    - But updated snapshots are added to the PR, which then runs again and
    creates different snapshots, so it runs again ad nauseam. I'll remove
    the `run-e2e-snapshots` label.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
+set -euxo pipefail
 
-dfx canister create internet_identity --specified-id rdmx6-jaaaa-aaaaa-aaadq-cai
-dfx canister create pouh_issuer --specified-id qbw6f-caaaa-aaaah-qdcwa-cai
-
+dfx canister create --all
+dfx deploy backend
 ./scripts/deploy.signer.sh
-./scripts/deploy.backend.sh
 
 mkdir -p ./target/ic
 
