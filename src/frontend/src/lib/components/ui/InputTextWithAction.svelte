7e359ff862836b7a813d58e38d94f88fbfd2dfc0
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-01-29 10:04:22 +0100

    adds auto select code

--- src/frontend/src/lib/components/ui/InputTextWithAction.svelte
+++ src/frontend/src/lib/components/ui/InputTextWithAction.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
 	import { Input } from '@dfinity/gix-components';
+	import { nonNullish } from '@dfinity/utils';
+	import { onMount } from 'svelte';
 
 	export let value = '';
 	export let name: string;
 	export let placeholder: string;
 	export let required = true;
 	export let testId: string | undefined = undefined;
+	export let autofocus = false;
+
+	onMount(() => {
+		if (autofocus) {
+			let component = document.querySelector<HTMLInputElement>(`input[name="${name}"]`);
+			if (nonNullish(component)) {
+				component.focus();
+			}
+		}
+	});
 </script>
 
 <Input
