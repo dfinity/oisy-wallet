15c4ea3396d1461a1f900a0e3523a9acdb8d3fbe
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-01-31 09:54:51 +0100

    test(frontend): fail when tests logs to console (#4455)
    # Motivation
    
    Under @peterpeterparker 's suggestion in
    https://github.com/dfinity/oisy-wallet/pull/4453#pullrequestreview-2574990676:
    taking inspiration from [NNS
    Dapp](https://github.com/dfinity/nns-dapp/blob/ac40ed19f67ce583f6ec0094aafbe3bc15d0ca0b/frontend/vitest.setup.ts#L134),
    we want the tests to fail in case they log to console. That is done to
    simplify our readability and the consistent writing of unittests.
    
    However, we allow the developer to avoid such restriction when
    debugging, if necessary.
    
    # Changes
    
    - Copy utils from NNS dApp to throw errors when loggin, and to allow
    logging in debugging.
    - Adapt vitest setup to use such functions.
    - Create script `test:debug` that allows logging, via an env variable.
    
    # Tests
    
    The CI ran several times on this PR when there were still errors being
    logged to console, and it failed, as expected. Furthermore, I injected
    an error to run the `test:debug` script locally and it worked.

---
+++ src/frontend/src/tests/utils/console.test-utils.ts
@@ -0,0 +1,47 @@
+import { runResolvedPromises } from '$tests/utils/timers.test-utils';
+
+type LogType = 'log' | 'debug' | 'warn' | 'error';
+
+const logTypes: LogType[] = ['log', 'debug', 'warn', 'error'];
+
+for (const logType of logTypes) {
+	replaceRealLogger(logType);
+}
+
+let gotLogs = false;
+let isLoggingAllowed = false;
+
+function replaceRealLogger(logType: LogType) {
+	// eslint-disable-next-line no-console
+	const realLogger = console[logType];
+	// eslint-disable-next-line no-console
+	console[logType] = function (...args) {
+		gotLogs = true;
+		realLogger(...args);
+	};
+}
+
+export const failTestsThatLogToConsole = () => {
+	beforeEach(() => {
+		gotLogs = false;
+	});
+
+	afterEach(async () => {
+		await runResolvedPromises();
+		if (!isLoggingAllowed && gotLogs) {
+			throw new Error(
+				'Your test produced console logs, which is not allowed.\n' +
+					'If you need console output, mock and expect it in your test.\n' +
+					'This failure only happens after your test finishes so if your test had other failures, you can still see them and just ignore this.\n' +
+					'If this is only for debugging, call allowLoggingInOneTest' +
+					'ForDebugging() from $tests/utils/console.test-utils in your test.'
+			);
+		}
+		isLoggingAllowed = false;
+	});
+};
+
+// Use this only when debugging.
+export const allowLoggingForDebugging = () => {
+	isLoggingAllowed = true;
+};
