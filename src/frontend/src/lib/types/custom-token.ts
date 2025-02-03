7d128b9ef0e952e5759edb6c82ac2b0c773e399c
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-02-03 13:24:50 +0100

    feat(frontend): add SPL parser to Custom Token parser

--- src/frontend/src/lib/types/custom-token.ts
+++ src/frontend/src/lib/types/custom-token.ts
@@ -1,13 +1,22 @@
 import type { IcrcCustomToken } from '$icp/types/icrc-custom-token';
 import type { UserTokenState } from '$lib/types/token-toggleable';
+import type { SplToken } from '$sol/types/spl';
 
-export type CustomTokenNetworkKeys = 'Icrc';
+export type CustomTokenNetworkKeys = 'Icrc' | 'SplMainnet' | 'SplDevnet';
 
 export type IcrcSaveCustomToken = Pick<IcrcCustomToken, 'ledgerCanisterId' | 'indexCanisterId'>;
 
-export type SaveCustomToken = UserTokenState & IcrcSaveCustomToken;
+export type SplSaveCustomToken = Pick<SplToken, 'address' | 'decimals' | 'symbol'>;
+
+export type SaveCustomToken = UserTokenState & (IcrcSaveCustomToken | SplSaveCustomToken);
+
+type IcrcSaveCustomTokenWithKey = IcrcSaveCustomToken & {
+	networkKey: Extract<CustomTokenNetworkKeys, 'Icrc'>;
+};
+
+type SplSaveCustomTokenWithKey = SplSaveCustomToken & {
+	networkKey: Extract<CustomTokenNetworkKeys, 'SplMainnet' | 'SplDevnet'>;
+};
 
 export type SaveCustomTokenWithKey = UserTokenState &
-	(IcrcSaveCustomToken & {
-		networkKey: Extract<CustomTokenNetworkKeys, 'Icrc'>;
-	});
+	(IcrcSaveCustomTokenWithKey | SplSaveCustomTokenWithKey);
