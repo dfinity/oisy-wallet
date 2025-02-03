d88539b667ec71ec4f2da09103395cce6bf59395
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-02-03 16:59:56 +0100

    clean

--- src/frontend/src/lib/actors/agents.ic.ts
+++ src/frontend/src/lib/actors/agents.ic.ts
@@ -1,7 +1,4 @@
 import { LOCAL, REPLICA_HOST } from '$lib/constants/app.constants';
-import type { HttpAgent, Identity } from '@dfinity/agent';
 import { AgentManager } from '@dfinity/utils';
 
 export const agents = AgentManager.create({ fetchRootKey: LOCAL, host: REPLICA_HOST });
-
-export const getAgent = async ({ identity }: { identity: Identity }): Promise<HttpAgent> => {};
