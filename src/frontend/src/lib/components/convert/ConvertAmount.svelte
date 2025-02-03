00e63210c52447a4a4cd8915ef945f4755da0571
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2025-02-03 15:55:58 +0100

    feat(frontend): update BtcConvertForm component

--- src/frontend/src/lib/components/convert/ConvertAmount.svelte
+++ src/frontend/src/lib/components/convert/ConvertAmount.svelte
@@ -3,6 +3,7 @@
 	import ConvertAmountSource from '$lib/components/convert/ConvertAmountSource.svelte';
 	import IconMoveDown from '$lib/components/icons/lucide/IconMoveDown.svelte';
 	import type { OptionAmount } from '$lib/types/send';
+	import type {DisplayUnit} from "$lib/types/swap";
 
 	export let sendAmount: OptionAmount;
 	export let receiveAmount: number | undefined;
