4c90ad90a9927697c404b11134a4b9b7d4351d4a
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-02-04 16:18:19 +0100

    implements airdrop state modal

--- src/frontend/src/lib/components/airdrops/AirdropStateModal.svelte
+++ src/frontend/src/lib/components/airdrops/AirdropStateModal.svelte
@@ -3,12 +3,28 @@
 	import ButtonCloseModal from '$lib/components/ui/ButtonCloseModal.svelte';
 	import ContentWithToolbar from '$lib/components/ui/ContentWithToolbar.svelte';
 	import { modalStore } from '$lib/stores/modal.store';
+	import successfulReward from '$lib/assets/successful-reward.svg';
+	import ImgBanner from '$lib/components/ui/ImgBanner.svelte';
+	import ButtonGroup from '$lib/components/ui/ButtonGroup.svelte';
+	import Button from '$lib/components/ui/Button.svelte';
 </script>
 
 <Modal on:nnsClose={modalStore.close}>
-	<ContentWithToolbar>
-		asdas
+	<svelte:fragment slot="title">
+		<span class="text-xl">You received an airdrop</span>
+	</svelte:fragment>
 
-		<ButtonCloseModal slot="toolbar" />
+	<ContentWithToolbar>
+		<ImgBanner src={successfulReward} styleClass="aspect-auto" />
+
+		<h3 class="my-3 text-center">Welcome to OISY</h3>
+		<span class="block w-full text-center">You will receive your welcome gift any moment.</span>
+
+		<ButtonGroup slot="toolbar">
+			<ButtonCloseModal />
+			<Button on:click={() => console.log("Hmm")}>
+				see airdrops
+			</Button>
+		</ButtonGroup>
 	</ContentWithToolbar>
 </Modal>
