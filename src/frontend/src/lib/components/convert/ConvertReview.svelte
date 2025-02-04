11301916481bef085eac8277b100c530d122ea0a
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-02-04 11:45:54 +0100

    feat(frontend): update ConvertReview component (#4568)
    # Motivation
    
    The last step of aligning Swap and Conversion flow is to update
    ConvertReview component.
    
    
    <img width="602" alt="Screenshot 2025-02-04 at 11 16 51"
    src="https://github.com/user-attachments/assets/8f00ea24-2aeb-413c-956a-dfab0ee2a753"
    />

--- src/frontend/src/lib/components/convert/ConvertReview.svelte
+++ src/frontend/src/lib/components/convert/ConvertReview.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
-	import ConvertReviewAmount from '$lib/components/convert/ConvertReviewAmount.svelte';
+	import { createEventDispatcher, getContext } from 'svelte';
 	import ConvertReviewNetworks from '$lib/components/convert/ConvertReviewNetworks.svelte';
-	import ConvertReviewTokens from '$lib/components/convert/ConvertReviewTokens.svelte';
+	import TokensReview from '$lib/components/tokens/TokensReview.svelte';
 	import Button from '$lib/components/ui/Button.svelte';
 	import ButtonGroup from '$lib/components/ui/ButtonGroup.svelte';
 	import ContentWithToolbar from '$lib/components/ui/ContentWithToolbar.svelte';
+	import { CONVERT_CONTEXT_KEY, type ConvertContext } from '$lib/stores/convert.store';
 	import { i18n } from '$lib/stores/i18n.store';
 	import type { OptionAmount } from '$lib/types/send';
 
@@ -13,15 +13,23 @@
 	export let receiveAmount: number | undefined = undefined;
 
 	const dispatch = createEventDispatcher();
+
+	const { sourceToken, destinationToken, sourceTokenExchangeRate, destinationTokenExchangeRate } =
+		getContext<ConvertContext>(CONVERT_CONTEXT_KEY);
 </script>
 
-<ContentWithToolbar styleClass="flex flex-col">
-	<ConvertReviewTokens />
+<ContentWithToolbar>
+	<div slot="outer-content" class="mx-6 mb-8">
+		<TokensReview
+			{sendAmount}
+			{receiveAmount}
+			sourceToken={$sourceToken}
+			destinationToken={$destinationToken}
+			sourceTokenExchangeRate={$sourceTokenExchangeRate}
+			destinationTokenExchangeRate={$destinationTokenExchangeRate}
+		/>
 
-	<div slot="outer-content" class="mx-6 my-4 flex-1">
 		<ConvertReviewNetworks />
-
-		<ConvertReviewAmount {sendAmount} {receiveAmount} />
 
 		<slot name="fee" />
 
