9f2a218c839c10078f66e5f71a0313c35703f9b1
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-02-06 08:55:39 +0100

    refactor(frontend): re-use existing Env ICRC types for SNS script (#4608)
    # Motivation
    
    In script `build.tokens.sns` there are a few redundancies with the
    types: we can use some that already exists in folder $`env`, or a
    derived version of them.
    
    # Changes
    
    - Create type `EnvIcrcTokenIcon` that is just the optional `icon` prop
    used in the script.
    - Create type `EnvIcrcTokenMetadataWithIcon` as union of
    `EnvIcrcTokenMetadata` and `EnvIcrcTokenIcon`.
    - Replace `SnsMetadata` with `EnvIcrcTokenMetadataWithIcon`, and
    `OptionalSnsMetadata` with a `Partial` version of it.
    - Created type `EnvSnsTokenWithIcon` that add the `icon` prop to
    `metadata` in `EnvSnsToken`.
    - Replace `SnsToken` with `EnvSnsTokenWithIcon`.
    - Adjusted `SnsTokenWithOptionalMetadata` to use be
    `EnvSnsTokenWithIcon` but with optional `metadata`.
    - Remove unused `EnvSnsTokens` type.
    
    # Tests
    
    I injected a missing SNS token in this PR and ran the script, and it
    worked correctly.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/env/types/env-icrc-token.ts
+++ src/frontend/src/env/types/env-icrc-token.ts
@@ -1,4 +1,11 @@
-import { EnvIcrcTokenMetadataSchema } from '$env/schema/env-icrc-token.schema';
+import {
+	EnvIcrcTokenIconSchema,
+	EnvIcrcTokenMetadataSchema
+} from '$env/schema/env-icrc-token.schema';
 import * as z from 'zod';
 
 export type EnvIcrcTokenMetadata = z.infer<typeof EnvIcrcTokenMetadataSchema>;
+
+export type EnvIcrcTokenIcon = z.infer<typeof EnvIcrcTokenIconSchema>;
+
+export type EnvIcrcTokenMetadataWithIcon = EnvIcrcTokenMetadata & EnvIcrcTokenIcon;
