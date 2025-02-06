b475b919581c0e0e951354aaba55b03b82d7f14e
inc-man <nicolas.maechler@dfinity.org>
2025-02-06 14:39:29 +0100

    new send and receive test

---
+++ e2e/send-and-receive-flow.spec.ts
@@ -0,0 +1,11 @@
+import { testWithII } from '@dfinity/internet-identity-playwright';
+import { FlowPage } from './utils/pages/send-and-receive-flow.page';
+
+testWithII('receive and send icp', async ({ page, context, iiPage }) => {
+	const flowPage = new FlowPage({ page, context, iiPage });
+
+	await flowPage.waitForReady();
+	await flowPage.receiveTokens();
+	await flowPage.sendTokens();
+	await flowPage.takeScreenshot();
+});
