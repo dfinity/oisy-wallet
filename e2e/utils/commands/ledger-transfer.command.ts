54e3c668add41fb0f52bd3f8e88f026265753480
Nico Mächler <135843237+inc-man@users.noreply.github.com>
2025-02-06 21:47:30 +0100

    test(e2e): added command runner for upcoming send receive test (#4529)
    # Motivation
    
    The new end-to-end tests for sending and receiving require a command
    runner. This command runner allows us to execute scripts that top up a
    fake balance during testing, enabling us to simulate and verify the
    associated flows. This pull request introduces that functionality.
    
    # Changes
    
    Created new command runner.
    
    # Tests
    
    Tested on PR: https://github.com/dfinity/oisy-wallet/pull/3760
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

---
+++ e2e/utils/commands/ledger-transfer.command.ts
@@ -0,0 +1,23 @@
+import { type Command } from './runner';
+
+interface LedgerTransferCommandParams {
+	amount: string;
+	recipient: string | undefined;
+	memo?: number;
+}
+
+export class LedgerTransferCommand implements Command {
+	readonly #amount: string;
+	readonly #recipient: string | undefined;
+	readonly #memo: number;
+
+	constructor({ amount, recipient, memo = 0 }: LedgerTransferCommandParams) {
+		this.#amount = amount;
+		this.#recipient = recipient;
+		this.#memo = memo;
+	}
+
+	toString(): string {
+		return `dfx ledger transfer --memo ${this.#memo} --amount ${this.#amount} ${this.#recipient}`;
+	}
+}
