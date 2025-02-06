8875f61783bdd93eed33c1e3b147c23e31c99e9d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-02-06 01:01:45 +0100

    refactor(frontend): extract list of deprecated SNS tokens and use it

---
+++ src/frontend/src/env/tokens/tokens.sns.deprecated.env.ts
@@ -0,0 +1,11 @@
+import type { EnvIcrcTokenMetadataWithIcon } from '$env/types/env-icrc-token';
+
+export const DEPRECATED_SNES: Record<string, Partial<EnvIcrcTokenMetadataWithIcon>> = {
+	['ibahq-taaaa-aaaaq-aadna-cai']: {
+		name: '---- (formerly CYCLES-TRANSFER-STATION)',
+		symbol: '--- (CTS)',
+		alternativeName: undefined,
+		url: undefined,
+		icon: undefined
+	}
+};
