e16b435be12268ae6e84de4eafaed5a41971f530
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-02-08 08:54:16 +0100

    refactor(frontend): self-sent transactions after its chain one

--- src/frontend/src/sol/services/sol-transactions.services.ts
+++ src/frontend/src/sol/services/sol-transactions.services.ts
@@ -99,7 +99,6 @@ export const fetchSolTransactionsForSignature = async ({
 
 				return [
 					...(await acc),
-					newTransaction,
 					...(from === to
 						? [
 								{
@@ -108,7 +107,8 @@ export const fetchSolTransactionsForSignature = async ({
 									type: newTransaction.type === 'send' ? 'receive' : 'send'
 								} as SolTransactionUi
 							]
-						: [])
+						: []),
+					newTransaction
 				];
 			}
 
