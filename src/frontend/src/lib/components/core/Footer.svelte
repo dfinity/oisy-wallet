c90600767afa6aab59ed8879dfec5a048353599b
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-02-10 21:50:52 +0100

    revert custom max-w screen theme

--- src/frontend/src/lib/components/core/Footer.svelte
+++ src/frontend/src/lib/components/core/Footer.svelte
@@ -17,7 +17,7 @@
 </script>
 
 <footer
-	class="z-1 pointer-events-none mx-auto flex w-full max-w-[theme(screens.2.5xl)] flex-1 flex-col items-center justify-end px-4 pb-5 pt-12 sm:flex-1 sm:grow sm:flex-row sm:items-end sm:justify-between sm:px-8 lg:fixed lg:inset-x-0 lg:bottom-0"
+	class="z-1 pointer-events-none mx-auto flex w-full max-w-screen-2.5xl flex-1 flex-col items-center justify-end px-4 pb-5 pt-12 sm:flex-1 sm:grow sm:flex-row sm:items-end sm:justify-between sm:px-8 lg:fixed lg:inset-x-0 lg:bottom-0"
 	class:sm:sticky={$authNotSignedIn}
 	class:md:h-md:grid={$authNotSignedIn}
 	class:md:h-md:grid-cols-2={$authNotSignedIn}
