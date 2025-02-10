3b49879a2efe8406780b666624758d07176d7551
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-02-10 22:05:34 +0100

    Removed deprecated utilities

--- src/frontend/src/lib/components/dapps/DappPromoBanner.svelte
+++ src/frontend/src/lib/components/dapps/DappPromoBanner.svelte
@@ -31,7 +31,7 @@
 				<h4 class="text-white">{dAppDescription.name}</h4>
 			</div>
 
-			<Button paddingSmall styleClass="flex-grow-0 text-sm" colorStyle="secondary" on:click>
+			<Button paddingSmall styleClass="grow-0 text-sm" colorStyle="secondary" on:click>
 				{$i18n.core.text.view}
 			</Button>
 		</div>
