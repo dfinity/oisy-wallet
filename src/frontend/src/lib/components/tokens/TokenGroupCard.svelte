2be33c410c56f79e0d6a1aa881926d1c4694b699
daviddecentage <david.farina@dfinity.org>
2025-02-11 09:53:15 +0100

    theming

--- src/frontend/src/lib/components/tokens/TokenGroupCard.svelte
+++ src/frontend/src/lib/components/tokens/TokenGroupCard.svelte
@@ -27,8 +27,8 @@
 	<MultipleListeners tokens={tokenGroup.tokens}>
 		<TokenCardWithOnClick
 			on:click={() => toggleIsExpanded(!isExpanded)}
-			styleClass="rounded-xl px-3 py-2 hover:bg-primary active:bg-primary {isExpanded
-				? 'bg-white rounded-b-none'
+			styleClass="rounded-xl px-3 py-2 hover:bg-brand-subtle-10 {isExpanded
+				? 'bg-primary rounded-b-none'
 				: ''}"
 		>
 			<TokenCardContent data={headerData} hideNetworkLogo testIdPrefix={TOKEN_GROUP} />
@@ -36,7 +36,7 @@
 	</MultipleListeners>
 
 	{#if isExpanded}
-		<div class="flex flex-col gap-3 rounded-b-xl bg-white/40 pt-2" transition:slide={SLIDE_PARAMS}>
+		<div class="flex flex-col gap-3 rounded-b-xl bg-secondary pt-2" transition:slide={SLIDE_PARAMS}>
 			{#each tokenGroup.tokens as token (token.id)}
 				<TokenCardWithUrl {token}>
 					<TokenCardContent logoSize="md" data={token} />
--- src/frontend/src/lib/components/ui/Badge.svelte
+++ src/frontend/src/lib/components/ui/Badge.svelte
@@ -3,10 +3,10 @@
 
 	const variantClassNames = {
 		default: 'border border-tertiary bg-dust/30',
-		info: 'bg-brand-subtle text-brand-primary',
-		error: 'bg-error-subtle text-error',
-		warning: 'bg-warning-subtle text-warning',
-		success: 'bg-success-subtle text-success',
+		info: 'bg-brand-subtle-20 text-brand-primary',
+		error: 'bg-error-subtle-20 text-error-primary',
+		warning: 'bg-warning-subtle-20 text-warning-primary',
+		success: 'bg-success-subtle-20 text-success-primary',
 		outline: 'border border-tertiary bg-off-white'
 	};
 
