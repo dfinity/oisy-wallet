3e77755362ae50b0c9e0f0a3d06ca06b9551f718
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-02-12 01:23:33 +0100

    test(frontend): add Solana to mockTokens

--- src/frontend/src/tests/mocks/tokens.mock.ts
+++ src/frontend/src/tests/mocks/tokens.mock.ts
@@ -2,10 +2,11 @@ import { ICP_NETWORK } from '$env/networks/networks.env';
 import { BTC_MAINNET_TOKEN } from '$env/tokens/tokens.btc.env';
 import { ETHEREUM_TOKEN } from '$env/tokens/tokens.eth.env';
 import { ICP_TOKEN } from '$env/tokens/tokens.icp.env';
+import { SOLANA_TOKEN } from '$env/tokens/tokens.sol.env';
 import type { Token } from '$lib/types/token';
 import { parseTokenId } from '$lib/validation/token.validation';
 
-export const mockTokens: Token[] = [ICP_TOKEN, BTC_MAINNET_TOKEN, ETHEREUM_TOKEN];
+export const mockTokens: Token[] = [ICP_TOKEN, BTC_MAINNET_TOKEN, ETHEREUM_TOKEN, SOLANA_TOKEN];
 
 export const mockValidToken: Token = {
 	id: parseTokenId('TokenId'),
