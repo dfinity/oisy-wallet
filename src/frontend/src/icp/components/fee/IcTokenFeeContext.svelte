af6e64b43597b974a18588c02bc3484ec980620f
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-02-12 14:49:04 +0100

    feat(frontend): IcTokenFee context and store (#4672)
    # Motivation
    
    In this PR, IcTokenFee context and store are created. They will be used
    by the Swap components in the follow-up PR.
    
    # Changes
    
    1. IcTokenFee store created.
    2. IcTokenFee context created.
    
    # Tests
    
    Context and store tests are added.

---
+++ src/frontend/src/icp/components/fee/IcTokenFeeContext.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { debounce, isNullish, nonNullish } from '@dfinity/utils';
+	import { getContext } from 'svelte';
+	import { transactionFee } from '$icp/api/icrc-ledger.api';
+	import { IC_TOKEN_FEE_CONTEXT_KEY, type IcTokenFeeContext } from '$icp/stores/ic-token-fee.store';
+	import type { IcToken } from '$icp/types/ic-token';
+	import { authIdentity } from '$lib/derived/auth.derived';
+	import { nullishSignOut } from '$lib/services/auth.services';
+
+	export let token: IcToken | undefined;
+
+	const { store } = getContext<IcTokenFeeContext>(IC_TOKEN_FEE_CONTEXT_KEY);
+
+	const loadIcTokenFee = async () => {
+		if (isNullish($authIdentity)) {
+			store.reset();
+			await nullishSignOut();
+			return;
+		}
+
+		if (isNullish(token) || nonNullish($store?.[token.symbol])) {
+			return;
+		}
+
+		try {
+			store.setIcTokenFee({
+				tokenSymbol: token.symbol,
+				fee: await transactionFee({
+					identity: $authIdentity,
+					ledgerCanisterId: token.ledgerCanisterId
+				})
+			});
+		} catch (e: unknown) {
+			// as a fallback, we use the icToken fee prop
+			store.setIcTokenFee({
+				tokenSymbol: token.symbol,
+				fee: token.fee
+			});
+		}
+	};
+	const debounceLoadIcTokenFee = debounce(loadIcTokenFee);
+
+	$: token, debounceLoadIcTokenFee();
+</script>
+
+<slot />
---
+++ src/frontend/src/tests/icp/components/fee/IcTokenFeeContext.spec.ts
@@ -0,0 +1,127 @@
+import { ICP_TOKEN } from '$env/tokens/tokens.icp.env';
+import * as icrcLedgerApi from '$icp/api/icrc-ledger.api';
+import IcTokenFeeContext from '$icp/components/fee/IcTokenFeeContext.svelte';
+import {
+	IC_TOKEN_FEE_CONTEXT_KEY,
+	icTokenFeeStore,
+	type IcTokenFeeStore
+} from '$icp/stores/ic-token-fee.store';
+import * as authStore from '$lib/derived/auth.derived';
+import * as authServices from '$lib/services/auth.services';
+import { mockIdentity } from '$tests/mocks/identity.mock';
+import type { Identity } from '@dfinity/agent';
+import { render, waitFor } from '@testing-library/svelte';
+import { readable } from 'svelte/store';
+
+describe('IcTokenFeeContext', () => {
+	const mockFee = 10000n;
+	const mockContext = (store: IcTokenFeeStore) => new Map([[IC_TOKEN_FEE_CONTEXT_KEY, { store }]]);
+	const mockTransactionFee = () =>
+		vi.spyOn(icrcLedgerApi, 'transactionFee').mockResolvedValue(mockFee);
+	const mockAuthStore = (value: Identity | null = mockIdentity) =>
+		vi.spyOn(authStore, 'authIdentity', 'get').mockImplementation(() => readable(value));
+
+	const props = {
+		token: ICP_TOKEN
+	};
+
+	beforeEach(() => {
+		icTokenFeeStore.reset();
+	});
+
+	it('should call setIcTokenFee with proper params', async () => {
+		const setIcTokenFeeSpy = vi.spyOn(icTokenFeeStore, 'setIcTokenFee');
+		const transactionFeeSpy = mockTransactionFee();
+
+		mockAuthStore();
+
+		render(IcTokenFeeContext, {
+			props,
+			context: mockContext(icTokenFeeStore)
+		});
+
+		await waitFor(() => {
+			expect(transactionFeeSpy).toHaveBeenCalledOnce();
+			expect(transactionFeeSpy).toBeCalledWith({
+				ledgerCanisterId: ICP_TOKEN.ledgerCanisterId,
+				identity: mockIdentity
+			});
+			expect(setIcTokenFeeSpy).toHaveBeenCalledOnce();
+			expect(setIcTokenFeeSpy).toHaveBeenCalledWith({
+				tokenSymbol: ICP_TOKEN.symbol,
+				fee: mockFee
+			});
+		});
+	});
+
+	it('should not call transactionFee if no authIdentity available', async () => {
+		const nullishSignOutSpy = vi.spyOn(authServices, 'nullishSignOut').mockResolvedValue();
+
+		mockAuthStore(null);
+
+		render(IcTokenFeeContext, {
+			props,
+			context: mockContext(icTokenFeeStore)
+		});
+
+		await waitFor(() => {
+			expect(nullishSignOutSpy).toHaveBeenCalledOnce();
+		});
+	});
+
+	it('should not call transactionFee if no token provided', async () => {
+		const transactionFeeSpy = mockTransactionFee();
+
+		mockAuthStore();
+
+		render(IcTokenFeeContext, {
+			props: { token: undefined },
+			context: mockContext(icTokenFeeStore)
+		});
+
+		await waitFor(() => {
+			expect(transactionFeeSpy).not.toHaveBeenCalled();
+		});
+	});
+
+	it('should not call transactionFee if token fee is already known', async () => {
+		const transactionFeeSpy = mockTransactionFee();
+
+		mockAuthStore();
+
+		icTokenFeeStore.setIcTokenFee({
+			tokenSymbol: ICP_TOKEN.symbol,
+			fee: mockFee
+		});
+		render(IcTokenFeeContext, {
+			props,
+			context: mockContext(icTokenFeeStore)
+		});
+
+		await waitFor(() => {
+			expect(transactionFeeSpy).not.toHaveBeenCalled();
+		});
+	});
+
+	it('should set a fallback token fee if transactionFee fails', async () => {
+		vi.spyOn(icrcLedgerApi, 'transactionFee').mockImplementation(async () => {
+			await Promise.resolve();
+			throw new Error('error');
+		});
+		const setIcTokenFeeSpy = vi.spyOn(icTokenFeeStore, 'setIcTokenFee');
+
+		mockAuthStore();
+		render(IcTokenFeeContext, {
+			props,
+			context: mockContext(icTokenFeeStore)
+		});
+
+		await waitFor(() => {
+			expect(setIcTokenFeeSpy).toHaveBeenCalledOnce();
+			expect(setIcTokenFeeSpy).toHaveBeenCalledWith({
+				tokenSymbol: ICP_TOKEN.symbol,
+				fee: ICP_TOKEN.fee
+			});
+		});
+	});
+});
