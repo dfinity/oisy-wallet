50a7404a6b883f9f8caf130599cc2cdd8fe8babc
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-02-13 10:57:46 +0100

    some code cleanups

---
+++ src/frontend/src/lib/components/ui/DateBadge.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Badge from '$lib/components/ui/Badge.svelte';
+
+    export let date: Date;
+    export let prefix: string | undefined = undefined;
+    export let suffix: string | undefined = undefined;
+    export let testId: string | undefined = undefined;
+
+    let formattedDate = `${date.toLocaleDateString('en-US', { month: 'long' })} ${date.getDate()}`;
+</script>
+
+<ul class="gap-2 flex list-none flex-wrap" data-tid={testId}>
+    <li class="flex">
+        <Badge>
+            {prefix ?? ''}
+            {formattedDate}
+            {suffix ?? ''}
+        </Badge>
+    </li>
+</ul>
