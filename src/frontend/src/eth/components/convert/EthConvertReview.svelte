1f258ae107f82d5bbc124f403e22077c542ef1cb
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-02-13 21:58:59 +0100

    feat(frontend): EthConvertReview component (#4696)
    # Motivation
    
    In this PR, a new EthConvertReview component is implemented.
    
    # Tests
    
    No extra tests needed.
    
    
    <img width="581" alt="Screenshot 2025-02-13 at 12 49 50"
    src="https://github.com/user-attachments/assets/5d121d87-ac78-4c82-9b0e-dba855923a30"
    />

---
+++ src/frontend/src/eth/components/convert/EthConvertReview.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import EthFeeDisplay from '$eth/components/fee/EthFeeDisplay.svelte';
+	import ConvertReview from '$lib/components/convert/ConvertReview.svelte';
+	import MessageBox from '$lib/components/ui/MessageBox.svelte';
+	import { CONVERT_CONTEXT_KEY, type ConvertContext } from '$lib/stores/convert.store';
+	import { i18n } from '$lib/stores/i18n.store';
+	import type { OptionAmount } from '$lib/types/send';
+
+	export let sendAmount: OptionAmount;
+	export let receiveAmount: number | undefined;
+
+	const { sourceTokenExchangeRate } = getContext<ConvertContext>(CONVERT_CONTEXT_KEY);
+</script>
+
+<ConvertReview on:icConvert on:icBack {sendAmount} {receiveAmount}>
+	<EthFeeDisplay exchangeRate={$sourceTokenExchangeRate} slot="fee" />
+
+	<div slot="info-message" class="mt-4">
+		<MessageBox>{$i18n.convert.text.cketh_conversions_may_take}</MessageBox>
+	</div>
+
+	<slot name="cancel" slot="cancel" />
+</ConvertReview>
