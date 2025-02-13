4519498f133b9d3c87aee0af4cbda751f33800ae
daviddecentage <david.farina@dfinity.org>
2025-02-13 15:52:25 +0100

    theme dependant images

--- src/frontend/src/lib/components/auth/LandingPage.svelte
+++ src/frontend/src/lib/components/auth/LandingPage.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import preview from '$lib/assets/preview.webp';
 	import HeroSignIn from '$lib/components/hero/HeroSignIn.svelte';
 	import Img from '$lib/components/ui/Img.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { replaceOisyPlaceholders } from '$lib/utils/i18n.utils';
+	import { themeStore } from '@dfinity/gix-components';
 
 	let ariaLabel: string;
 	$: ariaLabel = replaceOisyPlaceholders($i18n.auth.alt.preview);
@@ -21,7 +21,9 @@
 		class="pt-12 md:m-0 md:flex md:h-full md:content-center md:items-center ml-auto min-w-[1127px]"
 	>
 		<div class="md:h-md:mt-auto w-full">
-			<Img src={preview} alt={ariaLabel} />
+			{#await import(`$lib/assets/main_image-${$themeStore}.webp`) then { default: src }}
+				<Img src={src} alt={ariaLabel} />
+			{/await}
 		</div>
 	</div>
 </div>
--- src/frontend/src/lib/components/ui/ImgBanner.svelte
+++ src/frontend/src/lib/components/ui/ImgBanner.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Img from '$lib/components/ui/Img.svelte';
+	import { themeStore } from '@dfinity/gix-components';
 
 	export let src: string;
 	export let alt = '';
