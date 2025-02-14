d1f1816e1873ee5b33cd028d628aa3f3942fc7d5
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-02-14 14:55:43 +0100

    implements AirdropsGroup.svelte

---
+++ src/frontend/src/lib/components/airdrops/AllAirdropsList.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+    import { airdropCampaigns } from '$env/airdrop-campaigns.env';
+    import type { AirdropDescription } from '$env/types/env-airdrop';
+    import airdropBanner from '$lib/assets/airdrops-banner.svg';
+    import AirdropsGroups from '$lib/components/airdrops/AirdropsGroup.svelte';
+    import ImgBanner from '$lib/components/ui/ImgBanner.svelte';
+    import {
+        AIRDROPS_ACTIVE_CAMPAIGNS_CONTAINER,
+        AIRDROPS_BANNER,
+        AIRDROPS_UPCOMING_CAMPAIGNS_CONTAINER
+    } from '$lib/constants/test-ids.constants';
+    import { i18n } from '$lib/stores/i18n.store';
+    import { modalStore } from '$lib/stores/modal.store';
+
+    let selectedAirdrop: AirdropDescription;
+    $: selectedAirdrop = $modalStore?.data as AirdropDescription;
+
+    const isOngoingEvent = ({ startDate, endDate }: { startDate: Date; endDate: Date }) => {
+        const currentDate = new Date(Date.now());
+        let startDiff = startDate.getTime() - currentDate.getTime();
+        let endDiff = endDate.getTime() - currentDate.getTime();
+        return startDiff <= 0 && endDiff > 0;
+    };
+
+    const isUpcomingEvent = (startDate: Date) => {
+        const currentDate = new Date(Date.now());
+        let startDiff = startDate.getTime() - currentDate.getTime();
+        return startDiff > 0;
+    };
+
+    let ongoingEvents: AirdropDescription[];
+    $: ongoingEvents = airdropCampaigns.filter(({ startDate, endDate }) =>
+        isOngoingEvent({ startDate, endDate })
+    );
+
+    let upcomingEvents: AirdropDescription[];
+    $: upcomingEvents = airdropCampaigns.filter(({ startDate }) => isUpcomingEvent(startDate));
+</script>
+
+<div class="mb-6 rounded-2xl md:mb-10 relative flex items-end overflow-hidden">
+    <div class="max-h-64">
+        <ImgBanner src={airdropBanner} testId={AIRDROPS_BANNER} />
+    </div>
+</div>
+
+<AirdropsGroups
+        title={$i18n.airdrops.text.active_campaigns}
+        airdrops={ongoingEvents}
+        testId={AIRDROPS_ACTIVE_CAMPAIGNS_CONTAINER}
+/>
+
+<AirdropsGroups
+        title={$i18n.airdrops.text.upcoming_campaigns}
+        airdrops={upcomingEvents}
+        altText={$i18n.airdrops.alt.upcoming_campaigns}
+        testId={AIRDROPS_UPCOMING_CAMPAIGNS_CONTAINER}
+/>
