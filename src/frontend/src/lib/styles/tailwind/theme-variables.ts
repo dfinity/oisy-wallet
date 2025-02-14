da19b3d4b1338dd03b68d10325db15b62c5393e6
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-02-14 14:37:20 +0100

    fix names

--- src/frontend/src/lib/styles/tailwind/theme-variables.ts
+++ src/frontend/src/lib/styles/tailwind/theme-variables.ts
@@ -1,108 +1,108 @@
 export const themeVariables = {
 	background: {
-		page: 'var(--colors-background-page)',
-		surface: 'var(--colors-background-surface)',
-		primary: 'var(--colors-background-primary)',
-		'primary-inverted': 'var(--colors-background-primary-inverted)',
-		secondary: 'var(--colors-background-secondary)',
-		'secondary-inverted': 'var(--colors-background-secondary-inverted)',
-		tertiary: 'var(--colors-background-tertiary)',
-		'tertiary-inverted': 'var(--colors-background-tertiary-inverted)',
-		disabled: 'var(--colors-background-disabled)',
+		page: 'var(--color-background-page)',
+		surface: 'var(--color-background-surface)',
+		primary: 'var(--color-background-primary)',
+		'primary-inverted': 'var(--color-background-primary-inverted)',
+		secondary: 'var(--color-background-secondary)',
+		'secondary-inverted': 'var(--color-background-secondary-inverted)',
+		tertiary: 'var(--color-background-tertiary)',
+		'tertiary-inverted': 'var(--color-background-tertiary-inverted)',
+		disabled: 'var(--color-background-disabled)',
 		brand: {
-			'subtle-10': 'var(--colors-background-brand-subtle-10)',
-			'subtle-20': 'var(--colors-background-brand-subtle-20)',
-			'subtle-30': 'var(--colors-background-brand-subtle-30)',
-			primary: 'var(--colors-background-brand-primary)',
-			secondary: 'var(--colors-background-brand-secondary)',
-			tertiary: 'var(--colors-background-brand-tertiary)',
-			disabled: 'var(--colors-background-brand-disabled)'
+			'subtle-10': 'var(--color-background-brand-subtle-10)',
+			'subtle-20': 'var(--color-background-brand-subtle-20)',
+			'subtle-30': 'var(--color-background-brand-subtle-30)',
+			primary: 'var(--color-background-brand-primary)',
+			secondary: 'var(--color-background-brand-secondary)',
+			tertiary: 'var(--color-background-brand-tertiary)',
+			disabled: 'var(--color-background-brand-disabled)'
 		},
 		success: {
-			'subtle-10': 'var(--colors-background-success-subtle-10)',
-			'subtle-20': 'var(--colors-background-success-subtle-20)',
-			'subtle-30': 'var(--colors-background-success-subtle-30)',
-			solid: 'var(--colors-background-success-solid)',
-			'solid-alt': 'var(--colors-background-success-solid-alt)',
-			'solid-alt2': 'var(--colors-background-success-solid-alt2)'
+			'subtle-10': 'var(--color-background-success-subtle-10)',
+			'subtle-20': 'var(--color-background-success-subtle-20)',
+			'subtle-30': 'var(--color-background-success-subtle-30)',
+			solid: 'var(--color-background-success-solid)',
+			'solid-alt': 'var(--color-background-success-solid-alt)',
+			'solid-alt2': 'var(--color-background-success-solid-alt2)'
 		},
 		warning: {
-			'subtle-10': 'var(--colors-background-warning-subtle-10)',
-			'subtle-20': 'var(--colors-background-warning-subtle-20)',
-			'subtle-30': 'var(--colors-background-warning-subtle-30)',
-			solid: 'var(--colors-background-warning-solid)',
-			'solid-alt': 'var(--colors-background-warning-solid-alt)',
-			'solid-alt2': 'var(--colors-background-warning-solid-alt2)'
+			'subtle-10': 'var(--color-background-warning-subtle-10)',
+			'subtle-20': 'var(--color-background-warning-subtle-20)',
+			'subtle-30': 'var(--color-background-warning-subtle-30)',
+			solid: 'var(--color-background-warning-solid)',
+			'solid-alt': 'var(--color-background-warning-solid-alt)',
+			'solid-alt2': 'var(--color-background-warning-solid-alt2)'
 		},
 		error: {
-			'subtle-10': 'var(--colors-background-error-subtle-10)',
-			'subtle-20': 'var(--colors-background-error-subtle-20)',
-			'subtle-30': 'var(--colors-background-error-subtle-30)',
-			solid: 'var(--colors-background-error-solid)',
-			'solid-alt': 'var(--colors-background-error-solid-alt)',
-			'solid-alt2': 'var(--colors-background-error-solid-alt2)'
+			'subtle-10': 'var(--color-background-error-subtle-10)',
+			'subtle-20': 'var(--color-background-error-subtle-20)',
+			'subtle-30': 'var(--color-background-error-subtle-30)',
+			solid: 'var(--color-background-error-solid)',
+			'solid-alt': 'var(--color-background-error-solid-alt)',
+			'solid-alt2': 'var(--color-background-error-solid-alt2)'
 		}
 	},
 	border: {
-		basic: 'var(--colors-border-basic)',
-		primary: 'var(--colors-border-primary)',
-		'primary-inverted': 'var(--colors-border-primary-inverted)',
-		secondary: 'var(--colors-border-secondary)',
-		'secondary-inverted': 'var(--colors-border-secondary-inverted)',
-		tertiary: 'var(--colors-border-tertiary)',
-		'tertiary-inverted': 'var(--colors-border-tertiary-inverted)',
-		disabled: 'var(--colors-border-disabled)',
+		basic: 'var(--color-border-basic)',
+		primary: 'var(--color-border-primary)',
+		'primary-inverted': 'var(--color-border-primary-inverted)',
+		secondary: 'var(--color-border-secondary)',
+		'secondary-inverted': 'var(--color-border-secondary-inverted)',
+		tertiary: 'var(--color-border-tertiary)',
+		'tertiary-inverted': 'var(--color-border-tertiary-inverted)',
+		disabled: 'var(--color-border-disabled)',
 		brand: {
-			'subtle-10': 'var(--colors-border-brand-subtle-10)',
-			'subtle-20': 'var(--colors-border-brand-subtle-20)',
-			'subtle-30': 'var(--colors-border-brand-subtle-30)',
-			primary: 'var(--colors-border-brand-primary)',
-			secondary: 'var(--colors-border-brand-secondary)',
-			tertiary: 'var(--colors-border-brand-tertiary)',
-			disabled: 'var(--colors-border-brand-disabled)'
+			'subtle-10': 'var(--color-border-brand-subtle-10)',
+			'subtle-20': 'var(--color-border-brand-subtle-20)',
+			'subtle-30': 'var(--color-border-brand-subtle-30)',
+			primary: 'var(--color-border-brand-primary)',
+			secondary: 'var(--color-border-brand-secondary)',
+			tertiary: 'var(--color-border-brand-tertiary)',
+			disabled: 'var(--color-border-brand-disabled)'
 		},
 		success: {
-			'subtle-10': 'var(--colors-border-success-subtle-10)',
-			'subtle-20': 'var(--colors-border-success-subtle-20)',
-			'subtle-30': 'var(--colors-border-success-subtle-30)',
-			solid: 'var(--colors-border-success-solid)',
-			'solid-alt': 'var(--colors-border-success-solid-alt)'
+			'subtle-10': 'var(--color-border-success-subtle-10)',
+			'subtle-20': 'var(--color-border-success-subtle-20)',
+			'subtle-30': 'var(--color-border-success-subtle-30)',
+			solid: 'var(--color-border-success-solid)',
+			'solid-alt': 'var(--color-border-success-solid-alt)'
 		},
 		warning: {
-			'subtle-10': 'var(--colors-border-warning-subtle-10)',
-			'subtle-20': 'var(--colors-border-warning-subtle-20)',
-			'subtle-30': 'var(--colors-border-warning-subtle-30)',
-			solid: 'var(--colors-border-warning-solid)',
-			'solid-alt': 'var(--colors-border-warning-solid-alt)'
+			'subtle-10': 'var(--color-border-warning-subtle-10)',
+			'subtle-20': 'var(--color-border-warning-subtle-20)',
+			'subtle-30': 'var(--color-border-warning-subtle-30)',
+			solid: 'var(--color-border-warning-solid)',
+			'solid-alt': 'var(--color-border-warning-solid-alt)'
 		},
 		error: {
-			'subtle-10': 'var(--colors-border-error-subtle-10)',
-			'subtle-20': 'var(--colors-border-error-subtle-20)',
-			'subtle-30': 'var(--colors-border-error-subtle-30)',
-			solid: 'var(--colors-border-error-solid)',
-			'solid-alt': 'var(--colors-border-error-solid-alt)'
+			'subtle-10': 'var(--color-border-error-subtle-10)',
+			'subtle-20': 'var(--color-border-error-subtle-20)',
+			'subtle-30': 'var(--color-border-error-subtle-30)',
+			solid: 'var(--color-border-error-solid)',
+			'solid-alt': 'var(--color-border-error-solid-alt)'
 		}
 	},
 	foreground: {
-		primary: 'var(--colors-foreground-primary)',
-		'primary-inverted': 'var(--colors-foreground-primary-inverted)',
-		tertiary: 'var(--colors-foreground-tertiary)',
-		'tertiary-inverted': 'var(--colors-foreground-tertiary-inverted)',
-		disabled: 'var(--colors-foreground-disabled)',
+		primary: 'var(--color-foreground-primary)',
+		'primary-inverted': 'var(--color-foreground-primary-inverted)',
+		tertiary: 'var(--color-foreground-tertiary)',
+		'tertiary-inverted': 'var(--color-foreground-tertiary-inverted)',
+		disabled: 'var(--color-foreground-disabled)',
 		brand: {
-			primary: 'var(--colors-foreground-brand-primary)',
-			'primary-alt': 'var(--colors-foreground-brand-primary-alt)',
-			secondary: 'var(--colors-foreground-brand-secondary)',
-			'secondary-alt': 'var(--colors-foreground-brand-secondary-alt)',
-			tertiary: 'var(--colors-foreground-brand-tertiary)',
-			'tertiary-alt': 'var(--colors-foreground-brand-tertiary-alt)',
-			disabled: 'var(--colors-foreground-brand-disabled)'
+			primary: 'var(--color-foreground-brand-primary)',
+			'primary-alt': 'var(--color-foreground-brand-primary-alt)',
+			secondary: 'var(--color-foreground-brand-secondary)',
+			'secondary-alt': 'var(--color-foreground-brand-secondary-alt)',
+			tertiary: 'var(--color-foreground-brand-tertiary)',
+			'tertiary-alt': 'var(--color-foreground-brand-tertiary-alt)',
+			disabled: 'var(--color-foreground-brand-disabled)'
 		},
-		'success-primary': 'var(--colors-foreground-success-primary)',
-		'success-alt': 'var(--colors-foreground-success-alt)',
-		'warning-primary': 'var(--colors-foreground-warning-primary)',
-		'warning-alt': 'var(--colors-foreground-warning-alt)',
-		'error-primary': 'var(--colors-foreground-error-primary)',
-		'error-alt': 'var(--colors-foreground-error-alt)'
+		'success-primary': 'var(--color-foreground-success-primary)',
+		'success-alt': 'var(--color-foreground-success-alt)',
+		'warning-primary': 'var(--color-foreground-warning-primary)',
+		'warning-alt': 'var(--color-foreground-warning-alt)',
+		'error-primary': 'var(--color-foreground-error-primary)',
+		'error-alt': 'var(--color-foreground-error-alt)'
 	}
 };
