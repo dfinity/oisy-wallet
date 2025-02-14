0faeefb1f7a8a027e78fab214ebd8ad2c7524b35
inc-man <nicolas.maechler@dfinity.org>
2025-02-14 13:24:59 +0100

    adjusted new test

--- e2e/enable-testnets.spec.ts
+++ e2e/enable-testnets.spec.ts
@@ -1,68 +1,11 @@
 import { testWithII } from '@dfinity/internet-identity-playwright';
-import { TestnetsPage } from './utils/pages/testnets.page';
+import { TestnetsPage, TestnetCases } from './utils/pages/testnets.page';
 
-testWithII('enable BTC (Testnet)', async ({ page, iiPage }) => {
-	const testnetsPage = new TestnetsPage({ page, iiPage });
-
-	await testnetsPage.waitForReady();
-	await testnetsPage.enableTestnets({
-		networkSymbol: 'BTC (Testnet)',
-		tokenSymbol: 'BTC (Testnet)'
+TestnetCases.forEach(({ name, networkSymbol, tokenSymbol }) => {
+	testWithII(name, async ({ page, iiPage }) => {
+		const testnetsPage = new TestnetsPage({ page, iiPage });
+		await testnetsPage.waitForReady();
+		await testnetsPage.enableTestnets({ networkSymbol, tokenSymbol });
+		await testnetsPage.takeScreenshot();
 	});
-	await testnetsPage.takeScreenshot();
-});
-
-testWithII('enable BTC (Regtest)', async ({ page, iiPage }) => {
-	const testnetsPage = new TestnetsPage({ page, iiPage });
-
-	await testnetsPage.waitForReady();
-	await testnetsPage.enableTestnets({
-		networkSymbol: 'BTC (Regtest)',
-		tokenSymbol: 'BTC (Regtest)'
-	});
-	await testnetsPage.takeScreenshot();
-});
-
-testWithII('enable SepoliaETH', async ({ page, iiPage }) => {
-	const testnetsPage = new TestnetsPage({ page, iiPage });
-
-	await testnetsPage.waitForReady();
-	await testnetsPage.enableTestnets({
-		networkSymbol: 'SepoliaETH',
-		tokenSymbol: 'SepoliaETH'
-	});
-	await testnetsPage.takeScreenshot();
-});
-
-testWithII('enable SOL (Testnet)', async ({ page, iiPage }) => {
-	const testnetsPage = new TestnetsPage({ page, iiPage });
-
-	await testnetsPage.waitForReady();
-	await testnetsPage.enableTestnets({
-		networkSymbol: 'SOL (Testnet)',
-		tokenSymbol: 'SOL (Testnet)'
-	});
-	await testnetsPage.takeScreenshot();
-});
-
-testWithII('enable SOL (Devnet)', async ({ page, iiPage }) => {
-	const testnetsPage = new TestnetsPage({ page, iiPage });
-
-	await testnetsPage.waitForReady();
-	await testnetsPage.enableTestnets({
-		networkSymbol: 'SOL (Devnet)',
-		tokenSymbol: 'SOL (Devnet)'
-	});
-	await testnetsPage.takeScreenshot();
-});
-
-testWithII('enable SOL (Local)', async ({ page, iiPage }) => {
-	const testnetsPage = new TestnetsPage({ page, iiPage });
-
-	await testnetsPage.waitForReady();
-	await testnetsPage.enableTestnets({
-		networkSymbol: 'SOL (Local)',
-		tokenSymbol: 'SOL (Local)'
-	});
-	await testnetsPage.takeScreenshot();
 });
--- e2e/utils/pages/testnets.page.ts
+++ e2e/utils/pages/testnets.page.ts
@@ -3,6 +3,44 @@ import { HomepageLoggedIn, type HomepageLoggedInParams } from './homepage.page';
 
 export type TestnetsPageParams = HomepageLoggedInParams;
 
+export interface TestnetConfig {
+  name: string;
+  networkSymbol: string;
+  tokenSymbol: string;
+}
+
+export const TestnetCases: TestnetConfig[] = [
+  {
+    name: 'enable BTC (Testnet)',
+    networkSymbol: 'BTC (Testnet)',
+    tokenSymbol: 'BTC (Testnet)',
+  },
+  {
+    name: 'enable BTC (Regtest)',
+    networkSymbol: 'BTC (Regtest)',
+    tokenSymbol: 'BTC (Regtest)',
+  },
+  {
+    name: 'enable SepoliaETH',
+    networkSymbol: 'SepoliaETH',
+    tokenSymbol: 'SepoliaETH',
+  },
+  {
+    name: 'enable SOL (Testnet)',
+    networkSymbol: 'SOL (Testnet)',
+    tokenSymbol: 'SOL (Testnet)',
+  },
+  {
+    name: 'enable SOL (Devnet)',
+    networkSymbol: 'SOL (Devnet)',
+    tokenSymbol: 'SOL (Devnet)',
+  },
+  {
+    name: 'enable SOL (Local)',
+    networkSymbol: 'SOL (Local)',
+    tokenSymbol: 'SOL (Local)',
+  },
+];
 export class TestnetsPage extends HomepageLoggedIn {
 	constructor({ page, iiPage }: TestnetsPageParams) {
 		super({ page, iiPage });
