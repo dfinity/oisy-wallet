94e6e8637896be22bf369bbd23b7086b001358e4
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-02-14 12:34:31 +0100

    refactor(frontend): rename tailwind border color classes (#4717)
    # Motivation
    As part of the new theme refactoring
    https://github.com/dfinity/oisy-wallet/pull/4686 , we rename a few
    bordr-* and ring-* (because ring classes reference border colors) colors
    that were hardcoded colors and map them to theme variables.

--- src/frontend/src/lib/components/ui/RoundedIcon.svelte
+++ src/frontend/src/lib/components/ui/RoundedIcon.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div
-	class="p-3 relative flex items-center justify-center rounded-full bg-primary ring-2 ring-brand-subtle"
+	class="p-3 relative flex items-center justify-center rounded-full bg-primary ring-2 ring-brand-subtle-10"
 >
 	<svelte:component this={icon} styleClass={opacity ? 'opacity-10' : ''} />
 </div>
