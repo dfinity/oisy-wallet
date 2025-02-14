09b83c87cc17bf88b398af8aea2e554bf6143b18
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-02-14 14:00:41 +0100

    refactor(frontend): move dApp descriptions to env folder (#4705)
    # Motivation
    
    For consistency, we move the dApp descriptions to the $env folder.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/swap/SwapProvider.svelte
+++ src/frontend/src/lib/components/swap/SwapProvider.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import { nonNullish } from '@dfinity/utils';
+	import { dAppDescriptions } from '$env/dapp-descriptions.env';
 	import Logo from '$lib/components/ui/Logo.svelte';
 	import ModalValue from '$lib/components/ui/ModalValue.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
-	import { dAppDescriptions, type OisyDappDescription } from '$lib/types/dapp-description';
+	import { type OisyDappDescription } from '$lib/types/dapp-description';
 	import type { OptionString } from '$lib/types/string';
 	import type { Option } from '$lib/types/utils';
 	import { replacePlaceholders } from '$lib/utils/i18n.utils';
