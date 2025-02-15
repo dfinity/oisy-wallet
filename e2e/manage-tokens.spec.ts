d0d4042e2eb285708546304b1fe0f0c57e06091a
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-02-15 22:10:22 +0100

    revert

--- e2e/manage-tokens.spec.ts
+++ e2e/manage-tokens.spec.ts
@@ -2,26 +2,20 @@ import { testWithII } from '@dfinity/internet-identity-playwright';
 import { MODALS_VIEWPORT_WIDTH, MODAL_VIEWPORT_HEIGHT } from './utils/constants/e2e.constants';
 import { ManageTokensCases, ManageTokensPage } from './utils/pages/manage-tokens.page';
 
-let manageTokensPage: ManageTokensPage;
-
-testWithII.beforeEach(async ({ page, iiPage, isMobile }) => {
-	manageTokensPage = new ManageTokensPage({
-		page,
-		iiPage,
-		// TODO: check a better way to make the select network visible in the network switcher dropdown, otherwise the test will fail, since the network cannot be clicked
-		viewportSize: !isMobile
-			? {
-					width: MODALS_VIEWPORT_WIDTH,
-					height: MODAL_VIEWPORT_HEIGHT
-				}
-			: undefined
-	});
-
-	await manageTokensPage.waitForReady();
-});
-
 ManageTokensCases.forEach(({ type, tokenSymbol, networkSymbol }) => {
-	testWithII(`should enable and disable ${type} token`, async () => {
+	testWithII(`should enable and disable ${type} token`, async ({ page, iiPage, isMobile }) => {
+		const manageTokensPage = new ManageTokensPage({
+			page,
+			iiPage,
+			// TODO: check a better way to make the select network visible in the network switcher dropdown, otherwise the test will fail, since the network cannot be clicked
+			viewportSize: !isMobile
+				? {
+						width: MODALS_VIEWPORT_WIDTH,
+						height: MODAL_VIEWPORT_HEIGHT
+					}
+				: undefined
+		});
+		await manageTokensPage.waitForReady();
 		await manageTokensPage.enableAndDisableToken({ tokenSymbol, networkSymbol });
 	});
 });
