c835fad763275e4fc1b21b19b921f7811e38cd1d
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-02-17 09:51:52 +0100

    resolves lint issues

--- src/frontend/src/lib/utils/airdrops.utils.ts
+++ src/frontend/src/lib/utils/airdrops.utils.ts
@@ -26,15 +26,15 @@ export const loadAirdropResult = async (identity: Identity): Promise<AirdropResu
 
 export const isOngoingCampaign = ({ startDate, endDate }: { startDate: Date; endDate: Date }) => {
 	const currentDate = new Date(Date.now());
-	let startDiff = startDate.getTime() - currentDate.getTime();
-	let endDiff = endDate.getTime() - currentDate.getTime();
+	const startDiff = startDate.getTime() - currentDate.getTime();
+	const endDiff = endDate.getTime() - currentDate.getTime();
 
 	return startDiff <= 0 && endDiff > 0;
 };
 
 export const isUpcomingCampaign = (startDate: Date) => {
 	const currentDate = new Date(Date.now());
-	let startDiff = startDate.getTime() - currentDate.getTime();
+	const startDiff = startDate.getTime() - currentDate.getTime();
 
 	return startDiff > 0;
 };
