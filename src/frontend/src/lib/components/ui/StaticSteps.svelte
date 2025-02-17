8a71a44d6ac1b17209dce41b2a257b8f87f8477c
daviddecentage <david.farina@dfinity.org>
2025-02-17 09:43:01 +0100

    initial commit

--- src/frontend/src/lib/components/ui/StaticSteps.svelte
+++ src/frontend/src/lib/components/ui/StaticSteps.svelte
@@ -28,118 +28,115 @@
 {/each}
 
 <style lang="scss">
-	.step {
-		display: grid;
-		grid-template-columns: max-content auto;
-		grid-template-rows: repeat(2, auto);
+  .step {
+    display: grid;
+    grid-template-columns: max-content auto;
+    grid-template-rows: repeat(2, auto);
 
-		align-items: center;
+    align-items: center;
 
-		column-gap: var(--padding-2x);
-		row-gap: var(--padding);
+    column-gap: var(--padding-2x);
+    row-gap: var(--padding);
 
-		padding: 0 0 var(--padding);
+    padding: 0 0 var(--padding);
 
-		--icon-check-circle-background: var(--positive-emphasis);
-		--icon-check-circle-color: white;
+    color: var(--color-foreground-primary);
+    transition: color var(--animation-time-normal) ease-out;
+  }
 
-		color: var(--value-color);
-		transition: color var(--animation-time-normal) ease-out;
-	}
+  .line,
+  .state {
+    align-self: flex-start;
+  }
 
-	.line,
-	.state {
-		align-self: flex-start;
-	}
+  .in_progress,
+  .next {
+    .line {
+      --line-color: var(--color-background-brand-primary);
+    }
+  }
 
-	.in_progress,
-	.next {
-		.line {
-			--line-color: var(--progress-color);
-		}
-	}
+  .in_progress {
+    color: var(--color-foreground-primary-inverted);
 
-	.in_progress {
-		color: var(--progress-color);
+    --icon-check-circle-background: var(--color-background-brand-primary);
+    --icon-check-circle-color: var(--color-background-brand-primary-alt);
 
-		--icon-check-circle-background: var(--progress-color);
-		--icon-check-circle-color: var(--progress-color-contrast);
+    .state {
+      color: var(--color-foreground-primary-inverted);
+      background: var(--color-background-brand-subtle-20);
+    }
+  }
 
-		.state {
-			color: var(--progress-color);
-			background: rgba(var(--progress-color-rgb), 0.3);
-		}
-	}
+  .next {
+    color: var(--color-foreground-primary);
 
-	.next {
-		color: var(--tertiary);
+    --icon-check-circle-background: transparent;
+    --icon-check-circle-color: var(--color-foreground-primary);
+    --icon-check-circle-border-color: var(--color-foreground-primary);
+  }
 
-		--icon-check-circle-background: transparent;
-		--icon-check-circle-color: var(--tertiary);
-		--icon-check-circle-border-color: var(--tertiary);
-	}
+  .state {
+    display: inline-flex;
+    gap: var(--padding-0_5x);
 
-	.state {
-		display: inline-flex;
-		gap: var(--padding-0_5x);
+    font-size: var(--font-size-small);
+    line-height: var(--line-height-small);
 
-		font-size: var(--font-size-small);
-		line-height: var(--line-height-small);
+    color: var(--color-foreground-primary-inverted);
+    background: var(--color-background-success-subtle-20);
 
-		color: var(--positive-emphasis);
-		background: rgba(var(--positive-emphasis-rgb), 0.3);
+    width: fit-content;
 
-		width: fit-content;
+    padding: var(--padding-0_5x) var(--padding);
+    border-radius: var(--border-radius-0_5x);
 
-		padding: var(--padding-0_5x) var(--padding);
-		border-radius: var(--border-radius-0_5x);
+    margin-bottom: var(--padding-4x);
 
-		margin-bottom: var(--padding-4x);
+    div {
+      position: relative;
+    }
+  }
 
-		div {
-			position: relative;
-		}
-	}
+  .line {
+    height: 100%;
+    min-height: calc(4 * var(--padding));
+    --line-color: var(--color-background-success-primary);
+    background: var(--color-background-brand-primary);
+  }
 
-	.line {
-		height: 100%;
-		min-height: calc(4 * var(--padding));
-		--line-color: var(--positive-emphasis);
-		background: linear-gradient(var(--line-color), var(--line-color)) no-repeat center/1.5px 100%;
-	}
+  .checkmark {
+    font-size: var(--font-size-small);
+    line-height: var(--line-height-small);
 
-	.checkmark {
-		font-size: var(--font-size-small);
-		line-height: var(--line-height-small);
+    color: var(--color-foreground-primary);
+    --checkmark-color: var(--color-background-brand-primary);
+  }
 
-		color: var(--progress-color);
-		--checkmark-color: var(--progress-color);
-	}
+  .spinner {
+    width: 24px;
+    height: 24px;
 
-	.spinner {
-		width: 24px;
-		height: 24px;
+    position: relative;
 
-		position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
+  .round {
+    width: 22px;
+    height: 22px;
 
-	.round {
-		width: 22px;
-		height: 22px;
+    border-radius: 50%;
 
-		border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid var(--color-background-brand-primary);
+  }
 
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		border: 1px solid var(--checkmark-color);
-	}
-
-	.completed {
-		color: var(--positive-emphasis);
-	}
+  .completed {
+    color: var(--color-background-success-primary);
+  }
 </style>
