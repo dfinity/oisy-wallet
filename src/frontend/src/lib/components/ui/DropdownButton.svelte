2a399e8f172293bbe0f57ba41cea7e15d64aa912
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-02-17 16:08:33 +0100

    refactor(frontend): adjust dropdowns according to new button styling (#4734)
    # Motivation
    As part of the new theme refactoring
    https://github.com/dfinity/oisy-wallet/pull/4686 , we adjust dropdown
    buttons and items styling to match the new button styles
    
    ---------
    
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/ui/DropdownButton.svelte
+++ src/frontend/src/lib/components/ui/DropdownButton.svelte
@@ -9,14 +9,14 @@
 </script>
 
 <button
-	class="min-w-72 gap-2 rounded-xl px-4 py-3 leading-5 font-medium justify-between border border-tertiary bg-primary text-left text-inherit text-primary hover:border-brand-primary"
+	class="dropdown-button"
 	bind:this={button}
 	on:click
 	aria-label={ariaLabel}
 	data-tid={testId}
 	{disabled}
-	class:opacity-50={disabled}
-	class:border-brand-primary={opened}
+	class:disabled
+	class:opened
 >
 	<slot />
 	<div class="ease-in-out transform transition-transform duration-300" class:-scale-y-100={opened}>
