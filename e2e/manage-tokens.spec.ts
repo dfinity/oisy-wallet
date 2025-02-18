de0e3dd036a6449f16cabf58e8c752a7c17a4cb1
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-02-19 00:49:31 +0100

    adapt

--- e2e/manage-tokens.spec.ts
+++ e2e/manage-tokens.spec.ts
@@ -2,30 +2,6 @@ import { testWithII } from '@dfinity/internet-identity-playwright';
 import { MODALS_VIEWPORT_WIDTH, MODAL_VIEWPORT_HEIGHT } from './utils/constants/e2e.constants';
 import { ManageTokensCases, ManageTokensPage } from './utils/pages/manage-tokens.page';
 
-
-testWithII('should enable and disable SepoliaERC20 token', async () => {
-	await enableAndDisableToken({
-		page: homepageLoggedIn,
-		tokenSymbol: 'USDC',
-		networkSymbol: 'SepoliaETH'
-	});
-});
-
-testWithII('should enable and disable SPL token', async () => {
-	await enableAndDisableToken({
-		page: homepageLoggedIn,
-		tokenSymbol: 'EURC',
-		networkSymbol: 'SOL'
-	});
-});
-
-testWithII('should enable and disable DevnetSPL token', async () => {
-	await enableAndDisableToken({
-		page: homepageLoggedIn,
-		tokenSymbol: 'DevnetEURC',
-		networkSymbol: 'SOL (Devnet)'
-
-    
 ManageTokensCases.forEach(({ type, tokenSymbol, networkSymbol }) => {
 	testWithII(`should enable and disable ${type} token`, async ({ page, iiPage, isMobile }) => {
 		const manageTokensPage = new ManageTokensPage({
