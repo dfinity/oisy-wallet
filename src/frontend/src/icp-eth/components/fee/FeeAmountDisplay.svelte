01467ea0cc288ede66011bce6631fd8adbd27d36
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-02-19 15:54:25 +0100

    updates displayed fees

--- src/frontend/src/icp-eth/components/fee/FeeAmountDisplay.svelte
+++ src/frontend/src/icp-eth/components/fee/FeeAmountDisplay.svelte
@@ -8,8 +8,10 @@
 	import { i18n } from '$lib/stores/i18n.store';
 	import type { OptionBalance } from '$lib/types/balance';
 	import type { TokenId } from '$lib/types/token';
-	import { formatToken } from '$lib/utils/format.utils';
+	import {formatToken, formatUSD} from '$lib/utils/format.utils';
 	import { replacePlaceholders } from '$lib/utils/i18n.utils';
+	import {SWAP_TOTAL_FEE_THRESHOLD} from "$lib/constants/swap.constants";
+	import {Utils} from "alchemy-sdk";
 
 	export let fee: BigNumber;
 	export let feeSymbol: string;
@@ -18,6 +20,9 @@
 
 	let balance: Exclude<OptionBalance, null>;
 	$: balance = nonNullish($balancesStore) ? ($balancesStore[feeTokenId]?.data ?? ZERO) : undefined;
+
+	let feeAsNumber: number;
+	$: feeAsNumber = Number(Utils.formatUnits(fee, feeDecimals));
 
 	let insufficientFeeFunds = false;
 
@@ -28,12 +33,22 @@
 	$: balance, fee, debounceCheckFeeFunds();
 </script>
 
-<div transition:fade>
+<div transition:fade class="flex gap-4">
 	{formatToken({
 		value: fee,
 		displayDecimals: EIGHT_DECIMALS
 	})}
 	{feeSymbol}
+
+	<div class="text-tertiary">
+		{#if feeAsNumber < SWAP_TOTAL_FEE_THRESHOLD}
+			{`( < ${formatUSD({
+				value: SWAP_TOTAL_FEE_THRESHOLD
+			})} )`}
+		{:else}
+			{`( ${formatUSD({ value: feeAsNumber })} )`}
+		{/if}
+	</div>
 </div>
 {#if insufficientFeeFunds && nonNullish(balance)}
 	<p in:slide={SLIDE_DURATION} class="text-cyclamen">
