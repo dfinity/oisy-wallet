bc4db304043662c3a31ccc99c06baee716a20b06
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-02-20 16:12:51 +0100

    cleans code

--- src/frontend/src/icp/components/fee/BitcoinKYTFee.svelte
+++ src/frontend/src/icp/components/fee/BitcoinKYTFee.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	import {debounce, nonNullish} from '@dfinity/utils';
+	import {nonNullish} from '@dfinity/utils';
 	import { BigNumber } from '@ethersproject/bignumber';
 	import { getContext } from 'svelte';
 	import { slide } from 'svelte/transition';
-	import { BTC_DECIMALS } from '$env/tokens/tokens.btc.env';
 	import { tokenWithFallbackAsIcToken } from '$icp/derived/ic-token.derived';
 	import { ckBtcMinterInfoStore } from '$icp/stores/ckbtc.store';
 	import { isTokenCkBtcLedger } from '$icp/utils/ic-send.utils';
@@ -12,15 +11,12 @@
 	import { i18n } from '$lib/stores/i18n.store';
 	import { SEND_CONTEXT_KEY, type SendContext } from '$lib/stores/send.store';
 	import type { NetworkId } from '$lib/types/network';
-	import {formatToken, formatUSD} from '$lib/utils/format.utils';
 	import { isNetworkIdBitcoin } from '$lib/utils/network.utils';
-	import {usdValue} from "$lib/utils/exchange.utils";
-	import {SWAP_TOTAL_FEE_THRESHOLD} from "$lib/constants/swap.constants";
-	import {replacePlaceholders} from "$lib/utils/i18n.utils";
+	import FeeAmountDisplay from "$icp-eth/components/fee/FeeAmountDisplay.svelte";
 
 	export let networkId: NetworkId | undefined = undefined;
 
-	const { sendTokenId, sendToken, sendTokenExchangeRate, sendBalance, sendTokenDecimals } = getContext<SendContext>(SEND_CONTEXT_KEY);
+	const { sendTokenId, sendTokenDecimals, sendTokenSymbol } = getContext<SendContext>(SEND_CONTEXT_KEY);
 
 	let ckBTC = false;
 	$: ckBTC = isTokenCkBtcLedger($tokenWithFallbackAsIcToken);
@@ -32,20 +28,6 @@
 	$: kytFee = nonNullish($sendTokenId)
 		? $ckBtcMinterInfoStore?.[$sendTokenId]?.data.kyt_fee
 		: undefined;
-
-	let usdFee: number;
-	$: usdFee = nonNullish($sendToken) && nonNullish(kytFee) && nonNullish($sendTokenExchangeRate)
-			? usdValue({token: $sendToken, balance: kytFee, exchangeRate: $sendTokenExchangeRate})
-			: 0;
-
-	let insufficientFeeFunds = false;
-
-	const debounceCheckFeeFunds = debounce(
-			() =>
-					(insufficientFeeFunds = nonNullish($sendBalance) && nonNullish(kytFee) && $sendBalance.lt(kytFee))
-	);
-
-	$: $sendBalance, kytFee, debounceCheckFeeFunds();
 </script>
 
 {#if ckBTC && btcNetwork && nonNullish(kytFee)}
@@ -53,36 +35,12 @@
 		<Value ref="kyt-fee">
 			<svelte:fragment slot="label">{$i18n.fee.text.estimated_inter_network}</svelte:fragment>
 
-			<div class="flex gap-4">
-				{formatToken({
-					value: BigNumber.from(kytFee),
-					unitName: BTC_DECIMALS,
-					displayDecimals: BTC_DECIMALS
-				})}
-				BTC
-
-				<div class="text-tertiary">
-					{#if usdFee < SWAP_TOTAL_FEE_THRESHOLD}
-						{`( < ${formatUSD({
-							value: SWAP_TOTAL_FEE_THRESHOLD
-						})} )`}
-					{:else}
-						{`( ${formatUSD({ value: usdFee })} )`}
-					{/if}
-				</div>
-			</div>
-
-			{#if insufficientFeeFunds && nonNullish($sendBalance)}
-				<p in:slide={SLIDE_DURATION} class="text-cyclamen">
-					{replacePlaceholders($i18n.send.assertion.not_enough_tokens_for_gas, {
-						$balance: formatToken({
-							value: $sendBalance,
-							displayDecimals: $sendTokenDecimals
-						}),
-						$symbol: $sendToken.symbol ?? ''
-					})}
-				</p>
-			{/if}
+			<FeeAmountDisplay
+				fee={BigNumber.from(kytFee)}
+				feeSymbol={$sendTokenSymbol}
+				feeTokenId={$sendTokenId}
+				feeDecimals={$sendTokenDecimals}
+			/>
 		</Value>
 	</div>
 {/if}
