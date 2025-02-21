93898728ff27b5e8d4ab1409156423b41536df75
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-02-21 16:17:47 +0100

    feat(frontend): improves fee display (#4836)
    # Motivation
    
    Currently, only the token fee is displayed on the send form, but the USD
    value is not shown. To improve user-friendliness, the USD value should
    also be displayed.
    
    # Changes
    
    - displays fee in usd
    
    # Tests
    
    **IC Send:**
    before
    
    ![image](https://github.com/user-attachments/assets/3ea7679a-bc6f-4057-ba11-c4c64a6791b0)
    
    
    after
    
    ![image](https://github.com/user-attachments/assets/98cc931c-cf7f-4a44-a015-0c69df3978ee)
    
    
    
    **ETH Send:**
    before
    
    ![image](https://github.com/user-attachments/assets/80b49d2f-11bc-48b5-888e-3b7a16e8e747)
    
    
    after
    
    ![image](https://github.com/user-attachments/assets/8a94d191-8360-4840-9ef5-20b0a49abe42)
    
    
    
    **SOL Send:**
    before
    
    ![image](https://github.com/user-attachments/assets/6123a1ef-23a4-4436-abbd-9eb50b701277)
    
    
    after
    
    ![image](https://github.com/user-attachments/assets/d9deb770-5310-4e56-99ee-24260409f68e)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/icp/components/fee/BitcoinKYTFee.svelte
+++ src/frontend/src/icp/components/fee/BitcoinKYTFee.svelte
@@ -3,21 +3,21 @@
 	import { BigNumber } from '@ethersproject/bignumber';
 	import { getContext } from 'svelte';
 	import { slide } from 'svelte/transition';
-	import { BTC_DECIMALS } from '$env/tokens/tokens.btc.env';
 	import { tokenWithFallbackAsIcToken } from '$icp/derived/ic-token.derived';
 	import { ckBtcMinterInfoStore } from '$icp/stores/ckbtc.store';
 	import { isTokenCkBtcLedger } from '$icp/utils/ic-send.utils';
+	import ExchangeAmountDisplay from '$lib/components/exchange/ExchangeAmountDisplay.svelte';
 	import Value from '$lib/components/ui/Value.svelte';
 	import { SLIDE_DURATION } from '$lib/constants/transition.constants';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { SEND_CONTEXT_KEY, type SendContext } from '$lib/stores/send.store';
 	import type { NetworkId } from '$lib/types/network';
-	import { formatToken } from '$lib/utils/format.utils';
 	import { isNetworkIdBitcoin } from '$lib/utils/network.utils';
 
 	export let networkId: NetworkId | undefined = undefined;
 
-	const { sendTokenId } = getContext<SendContext>(SEND_CONTEXT_KEY);
+	const { sendTokenId, sendTokenDecimals, sendTokenSymbol, sendTokenExchangeRate } =
+		getContext<SendContext>(SEND_CONTEXT_KEY);
 
 	let ckBTC = false;
 	$: ckBTC = isTokenCkBtcLedger($tokenWithFallbackAsIcToken);
@@ -36,12 +36,12 @@
 		<Value ref="kyt-fee">
 			<svelte:fragment slot="label">{$i18n.fee.text.estimated_inter_network}</svelte:fragment>
 
-			{formatToken({
-				value: BigNumber.from(kytFee),
-				unitName: BTC_DECIMALS,
-				displayDecimals: BTC_DECIMALS
-			})}
-			BTC
+			<ExchangeAmountDisplay
+				amount={BigNumber.from(kytFee)}
+				decimals={$sendTokenDecimals}
+				symbol={$sendTokenSymbol}
+				exchangeRate={$sendTokenExchangeRate}
+			/>
 		</Value>
 	</div>
 {/if}
--- src/frontend/src/icp/components/fee/IcTokenFee.svelte
+++ src/frontend/src/icp/components/fee/IcTokenFee.svelte
@@ -3,18 +3,13 @@
 	import { BigNumber } from '@ethersproject/bignumber';
 	import { getContext } from 'svelte';
 	import type { OptionIcToken } from '$icp/types/ic-token';
+	import ExchangeAmountDisplay from '$lib/components/exchange/ExchangeAmountDisplay.svelte';
 	import Value from '$lib/components/ui/Value.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { SEND_CONTEXT_KEY, type SendContext } from '$lib/stores/send.store';
-	import { formatToken } from '$lib/utils/format.utils';
 
-	const { sendToken, sendTokenDecimals } = getContext<SendContext>(SEND_CONTEXT_KEY);
-
-	let decimals: number | undefined;
-	$: decimals = $sendToken?.decimals;
-
-	let symbol: string | undefined;
-	$: symbol = $sendToken?.symbol;
+	const { sendToken, sendTokenDecimals, sendTokenSymbol, sendTokenExchangeRate } =
+		getContext<SendContext>(SEND_CONTEXT_KEY);
 
 	let fee: bigint | undefined;
 	$: fee = ($sendToken as OptionIcToken)?.fee;
@@ -23,12 +18,12 @@
 <Value ref="fee">
 	<svelte:fragment slot="label">{$i18n.fee.text.fee}</svelte:fragment>
 
-	{#if nonNullish(fee) && nonNullish(decimals) && nonNullish(symbol)}
-		{formatToken({
-			value: BigNumber.from(fee),
-			unitName: decimals,
-			displayDecimals: $sendTokenDecimals
-		})}
-		{symbol}
+	{#if nonNullish(fee)}
+		<ExchangeAmountDisplay
+			amount={BigNumber.from(fee)}
+			decimals={$sendTokenDecimals}
+			symbol={$sendTokenSymbol}
+			exchangeRate={$sendTokenExchangeRate}
+		/>
 	{/if}
 </Value>
