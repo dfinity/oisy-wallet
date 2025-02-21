20cd3c77a0a60389aae72ff72f1d9a0ee3002249
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-02-21 15:44:24 +0100

    feat(frontend): Added dark and light mode button to top right icon list (#4829)
    # Motivation
    
    According to new Figma design we add a simplified theme switcher to the
    top right icon list to toggle between light and dark mode.
    
    # Changes
    
    - Added sun and moon icons
    - Added theme switcher button component
    - Adjusted icon button styles for top right icon button list according
    to new Figma design
    - Added theme switcher button to top right icon button list when logged
    in and on logged out landing page
    - Fixed a typo for the aria-label text for the "select light/dark mode"
    button
    
    # Tests
    
    
    ![image](https://github.com/user-attachments/assets/0672c799-0bd0-4913-820d-b49665860773)
    
    ![image](https://github.com/user-attachments/assets/b6e0fa31-a22a-4397-9045-10e934d27c8c)
    
    ![image](https://github.com/user-attachments/assets/3d499590-4f02-4802-a9de-09662b43da22)
    
    ![image](https://github.com/user-attachments/assets/cd9a88cd-a179-4f7c-8f3b-2a09c8b7fdab)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/hero/Header.svelte
+++ src/frontend/src/lib/components/hero/Header.svelte
@@ -3,6 +3,7 @@
 	import AboutWhyOisyModal from '$lib/components/about/AboutWhyOisyModal.svelte';
 	import Menu from '$lib/components/core/Menu.svelte';
 	import OisyWalletLogoLink from '$lib/components/core/OisyWalletLogoLink.svelte';
+	import ThemeSwitchButton from '$lib/components/ui/ThemeSwitchButton.svelte';
 	import WalletConnect from '$lib/components/wallet-connect/WalletConnect.svelte';
 	import { authNotSignedIn, authSignedIn } from '$lib/derived/auth.derived';
 	import { modalAboutWhyOisy } from '$lib/derived/modal.derived';
@@ -23,14 +24,16 @@
 
 	<div class="pointer-events-auto flex justify-end gap-4">
 		{#if $authSignedIn}
+			<ThemeSwitchButton />
 			<WalletConnect />
 		{/if}
 
 		{#if $authSignedIn}
 			<Menu />
 		{:else}
-			<div class="mr-2 md:mr-0">
+			<div class="mr-2 flex justify-end gap-4 md:mr-0">
 				<AboutWhyOisy />
+				<ThemeSwitchButton />
 			</div>
 		{/if}
 	</div>
