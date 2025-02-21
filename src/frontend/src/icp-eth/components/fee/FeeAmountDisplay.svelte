173b2b117fbc4d6adba6ffad76b87d453fc6a95a
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-02-21 16:17:47 +0100

    feat(frontend): improves fee display (#4836)
    # Motivation
    
    Currently, only the token fee is displayed on the send form, but the USD
    value is not shown. To improve user-friendliness, the USD value should
    also be displayed.
    
    # Changes
    
    - displays fee in usd
    
    # Tests
    
    **IC Send:**
    before
    
    ![image](https://github.com/user-attachments/assets/3ea7679a-bc6f-4057-ba11-c4c64a6791b0)
    
    
    after
    
    ![image](https://github.com/user-attachments/assets/98cc931c-cf7f-4a44-a015-0c69df3978ee)
    
    
    
    **ETH Send:**
    before
    
    ![image](https://github.com/user-attachments/assets/80b49d2f-11bc-48b5-888e-3b7a16e8e747)
    
    
    after
    
    ![image](https://github.com/user-attachments/assets/8a94d191-8360-4840-9ef5-20b0a49abe42)
    
    
    
    **SOL Send:**
    before
    
    ![image](https://github.com/user-attachments/assets/6123a1ef-23a4-4436-abbd-9eb50b701277)
    
    
    after
    
    ![image](https://github.com/user-attachments/assets/d9deb770-5310-4e56-99ee-24260409f68e)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/icp-eth/components/fee/FeeAmountDisplay.svelte
+++ src/frontend/src/icp-eth/components/fee/FeeAmountDisplay.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import { debounce, nonNullish } from '@dfinity/utils';
 	import { BigNumber } from '@ethersproject/bignumber';
-	import { fade, slide } from 'svelte/transition';
-	import { EIGHT_DECIMALS, ZERO } from '$lib/constants/app.constants';
+	import { getContext } from 'svelte';
+	import { slide } from 'svelte/transition';
+	import ExchangeAmountDisplay from '$lib/components/exchange/ExchangeAmountDisplay.svelte';
+	import { ZERO } from '$lib/constants/app.constants';
 	import { SLIDE_DURATION } from '$lib/constants/transition.constants';
 	import { balancesStore } from '$lib/stores/balances.store';
 	import { i18n } from '$lib/stores/i18n.store';
+	import { SEND_CONTEXT_KEY, type SendContext } from '$lib/stores/send.store';
 	import type { OptionBalance } from '$lib/types/balance';
 	import type { TokenId } from '$lib/types/token';
 	import { formatToken } from '$lib/utils/format.utils';
@@ -15,6 +18,8 @@
 	export let feeSymbol: string;
 	export let feeTokenId: TokenId;
 	export let feeDecimals: number;
+
+	const { sendTokenExchangeRate } = getContext<SendContext>(SEND_CONTEXT_KEY);
 
 	let balance: Exclude<OptionBalance, null>;
 	$: balance = nonNullish($balancesStore) ? ($balancesStore[feeTokenId]?.data ?? ZERO) : undefined;
@@ -28,13 +33,12 @@
 	$: balance, fee, debounceCheckFeeFunds();
 </script>
 
-<div transition:fade>
-	{formatToken({
-		value: fee,
-		displayDecimals: EIGHT_DECIMALS
-	})}
-	{feeSymbol}
-</div>
+<ExchangeAmountDisplay
+	amount={fee}
+	decimals={feeDecimals}
+	symbol={feeSymbol}
+	exchangeRate={$sendTokenExchangeRate}
+/>
 {#if insufficientFeeFunds && nonNullish(balance)}
 	<p in:slide={SLIDE_DURATION} class="text-cyclamen">
 		{replacePlaceholders($i18n.send.assertion.not_enough_tokens_for_gas, {
