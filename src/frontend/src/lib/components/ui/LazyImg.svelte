940176d9d0d622f8a41b1a008f75c5203a0ba8db
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-02-23 22:38:14 +0100

    refactor(frontend): component to lazy-load images (#4784)
    # Motivation
    
    We have the snippet to lazy-load images used a few times already, so it
    makes sense to export it in a separate component.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/frontend/src/lib/components/ui/LazyImg.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { ComponentType } from 'svelte';
+	import Img from '$lib/components/ui/Img.svelte';
+	import ImgBanner from '$lib/components/ui/ImgBanner.svelte';
+
+	export let src: string;
+	export let alt = '';
+	export let styleClass: string | undefined = undefined;
+	export let testId: string | undefined = undefined;
+	export let asBanner = false;
+
+	let cmp: ComponentType;
+	$: cmp = asBanner ? ImgBanner : Img;
+</script>
+
+{#await import(src) then { default: src }}
+	<svelte:component this={cmp} {src} {alt} {styleClass} {testId} />
+{/await}
