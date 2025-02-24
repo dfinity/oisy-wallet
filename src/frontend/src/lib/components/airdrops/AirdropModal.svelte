f707a14a6dbfa56d7b2934801789482cbca4c918
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-02-24 09:48:19 +0100

    adds learn more buttons to rewards page and rewards modal

--- src/frontend/src/lib/components/airdrops/AirdropModal.svelte
+++ src/frontend/src/lib/components/airdrops/AirdropModal.svelte
@@ -8,6 +8,7 @@
 	import Share from '$lib/components/ui/Share.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { modalStore } from '$lib/stores/modal.store';
+	import ExternalLink from "$lib/components/ui/ExternalLink.svelte";
 
 	export let airdrop: AirdropDescription;
 </script>
@@ -20,6 +21,16 @@
 
 		<span class="text-lg font-semibold">{$i18n.airdrops.text.participate_title}</span>
 		<p class="mb-0 mt-2">{airdrop.description}</p>
+
+		<ExternalLink
+				href={airdrop.learnMoreHref}
+				ariaLabel={$i18n.airdrops.text.learn_more}
+				iconVisible={false}
+				asButton
+				styleClass={`rounded-xl px-3 py-2 secondary-light`}
+		>
+			{$i18n.airdrops.text.learn_more}
+		</ExternalLink>
 
 		<Share text={$i18n.airdrops.text.share} href={airdrop.campaignHref} styleClass="mt-2" />
 
--- src/frontend/src/lib/components/airdrops/Airdrops.svelte
+++ src/frontend/src/lib/components/airdrops/Airdrops.svelte
@@ -2,8 +2,21 @@
 	import AllAirdropsList from '$lib/components/airdrops/AllAirdropsList.svelte';
 	import PageTitle from '$lib/components/ui/PageTitle.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
+    import ExternalLink from "$lib/components/ui/ExternalLink.svelte";
 </script>
 
-<PageTitle>{$i18n.airdrops.text.title}</PageTitle>
+<div class="flex flex-row items-center">
+    <PageTitle>{$i18n.airdrops.text.title}</PageTitle>
+
+    <ExternalLink
+            href="https://docs.oisy.com/rewards"
+            ariaLabel={$i18n.airdrops.text.learn_more}
+            iconVisible={false}
+            color="blue"
+            styleClass={`ml-auto font-semibold`}
+    >
+        {$i18n.airdrops.text.learn_more}
+    </ExternalLink>
+</div>
 
 <AllAirdropsList />
