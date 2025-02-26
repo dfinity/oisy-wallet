0f4373007451a33fd808e5822bce7b013b98b41d
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-02-26 14:04:37 +0100

    feat(frontend): updates eth send form (#4902)
    # Motivation
    
    The `ETH send form` should be updated and match the updated design.
    
    # Changes
    
    - updates form
    
    # Tests
    
    **before:**
    <img width="433" alt="image"
    src="https://github.com/user-attachments/assets/d1e839d5-d31f-4efe-a4e6-66047942bd96"
    />
    
    
    **after:**
    <img width="432" alt="image"
    src="https://github.com/user-attachments/assets/f8955600-46bc-4ff2-9fe6-a474e3d69532"
    />
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/eth/components/send/EthSendForm.svelte
+++ src/frontend/src/eth/components/send/EthSendForm.svelte
@@ -8,11 +8,9 @@
 	import SendNetworkICP from '$eth/components/send/SendNetworkICP.svelte';
 	import type { EthereumNetwork } from '$eth/types/network';
 	import NetworkInfo from '$lib/components/networks/NetworkInfo.svelte';
-	import SendSource from '$lib/components/send/SendSource.svelte';
 	import ButtonGroup from '$lib/components/ui/ButtonGroup.svelte';
 	import ButtonNext from '$lib/components/ui/ButtonNext.svelte';
 	import ContentWithToolbar from '$lib/components/ui/ContentWithToolbar.svelte';
-	import { ethAddress } from '$lib/derived/address.derived';
 	import { SEND_CONTEXT_KEY, type SendContext } from '$lib/stores/send.store';
 	import type { Network } from '$lib/types/network';
 	import type { OptionAmount } from '$lib/types/send';
@@ -36,11 +34,13 @@
 
 	const dispatch = createEventDispatcher();
 
-	const { sendToken, sendBalance } = getContext<SendContext>(SEND_CONTEXT_KEY);
+	const { sendToken } = getContext<SendContext>(SEND_CONTEXT_KEY);
 </script>
 
 <form on:submit={() => dispatch('icNext')} method="POST">
 	<ContentWithToolbar>
+		<EthSendAmount {nativeEthereumToken} bind:amount bind:insufficientFunds />
+
 		{#if destinationEditable}
 			<EthSendDestination
 				token={$sendToken}
@@ -52,10 +52,6 @@
 
 			<SendNetworkICP {destination} {sourceNetwork} bind:network />
 		{/if}
-
-		<EthSendAmount {nativeEthereumToken} bind:amount bind:insufficientFunds />
-
-		<SendSource token={$sendToken} balance={$sendBalance} source={$ethAddress ?? ''} />
 
 		<NetworkInfo network={sourceNetwork} />
 
