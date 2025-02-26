726490a39414a50ced6b7a5b35ce69a96c4fb0c1
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-02-26 09:12:39 +0100

    updates sol send form

--- src/frontend/src/sol/components/send/SolSendForm.svelte
+++ src/frontend/src/sol/components/send/SolSendForm.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
 	import { isNullish, nonNullish } from '@dfinity/utils';
 	import SendForm from '$lib/components/send/SendForm.svelte';
-	import { balance } from '$lib/derived/balances.derived';
-	import { token } from '$lib/stores/token.store';
 	import type { OptionAmount } from '$lib/types/send';
 	import { isNullishOrEmpty } from '$lib/utils/input.utils';
 	import SolFeeDisplay from '$sol/components/fee/SolFeeDisplay.svelte';
 	import SolSendAmount from '$sol/components/send/SolSendAmount.svelte';
 	import SolSendDestination from '$sol/components/send/SolSendDestination.svelte';
 	import type { SolAmountAssertionError } from '$sol/types/sol-send';
+	import {SEND_CONTEXT_KEY, type SendContext} from "$lib/stores/send.store";
+	import {getContext} from "svelte";
 
 	export let amount: OptionAmount = undefined;
 	export let destination = '';
 	export let source: string;
+
+	const { sendToken, sendBalance } = getContext<SendContext>(SEND_CONTEXT_KEY);
 
 	let amountError: SolAmountAssertionError | undefined;
 	let invalidDestination: boolean;
@@ -25,10 +27,10 @@
 		isNullish(amount);
 </script>
 
-<SendForm on:icNext {source} token={$token} balance={$balance} disabled={invalid}>
-	<SolSendDestination slot="destination" bind:destination bind:invalidDestination on:icQRCodeScan />
-
+<SendForm on:icNext {source} token={$sendToken} balance={$sendBalance} disabled={invalid} hideSource>
 	<SolSendAmount slot="amount" bind:amount bind:amountError />
+
+	<SolSendDestination slot="destination" bind:destination bind:invalidDestination on:icQRCodeScan />
 
 	<SolFeeDisplay slot="fee" />
 
