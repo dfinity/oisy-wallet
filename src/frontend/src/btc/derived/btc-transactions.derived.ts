8140f232311958961b98359c13a436046290aa41
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-01 02:11:45 +0100

    revert

--- src/frontend/src/btc/derived/btc-transactions.derived.ts
+++ src/frontend/src/btc/derived/btc-transactions.derived.ts
@@ -1,8 +1,7 @@
-import { enabledBitcoinTokens } from '$btc/derived/tokens.derived';
 import { btcTransactionsStore, type BtcTransactionsData } from '$btc/stores/btc-transactions.store';
 import { sortBtcTransactions } from '$btc/utils/btc-transactions.utils';
 import { tokenWithFallback } from '$lib/derived/token.derived';
-import { isNullish, nonNullish } from '@dfinity/utils';
+import { nonNullish } from '@dfinity/utils';
 import { derived, type Readable } from 'svelte/store';
 
 export const sortedBtcTransactions: Readable<NonNullable<BtcTransactionsData>> = derived(
@@ -21,11 +20,4 @@ export const btcTransactionsInitialized: Readable<boolean> = derived(
 export const btcTransactionsNotInitialized: Readable<boolean> = derived(
 	[btcTransactionsInitialized],
 	([$btcTransactionsInitialized]) => !$btcTransactionsInitialized
-);
-
-export const btcTransactionsLoading: Readable<boolean> = derived(
-	[btcTransactionsStore, enabledBitcoinTokens],
-	([$btcTransactionsStore, $enabledBitcoinTokens]) =>
-		isNullish($btcTransactionsStore) ||
-		Object.getOwnPropertySymbols($btcTransactionsStore).length !== $enabledBitcoinTokens.length
 );
