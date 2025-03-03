d8f694be36011b3d1fb44982753996deb43f3e26
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-03 08:26:55 +0100

    more tests

--- src/frontend/src/tests/lib/components/transactions/AllTransactionsSkeletons.spec.ts
+++ src/frontend/src/tests/lib/components/transactions/AllTransactionsSkeletons.spec.ts
@@ -1,6 +1,7 @@
 import { btcTransactionsStore } from '$btc/stores/btc-transactions.store';
 import * as btcEnv from '$env/networks/networks.btc.env';
 import * as ethEnv from '$env/networks/networks.eth.env';
+import { JUP_TOKEN } from '$env/tokens/tokens-spl/tokens.jup.env';
 import { BTC_MAINNET_TOKEN_ID } from '$env/tokens/tokens.btc.env';
 import { ETHEREUM_TOKEN_ID } from '$env/tokens/tokens.eth.env';
 import { ICP_TOKEN_ID } from '$env/tokens/tokens.icp.env';
@@ -8,6 +9,7 @@ import { SOLANA_TOKEN_ID } from '$env/tokens/tokens.sol.env';
 import { ethTransactionsStore } from '$eth/stores/eth-transactions.store';
 import { icTransactionsStore } from '$icp/stores/ic-transactions.store';
 import AllTransactionsSkeletons from '$lib/components/transactions/AllTransactionsSkeletons.svelte';
+import { enabledSplTokens } from '$sol/derived/spl.derived';
 import { solTransactionsStore } from '$sol/stores/sol-transactions.store';
 import { render } from '@testing-library/svelte';
 import { tick } from 'svelte';
@@ -66,6 +68,52 @@ describe('AllTransactionsSkeletons', () => {
 					testIdPrefix
 				}
 			});
+
+			Array.from({ length: 5 }).forEach((_, i) => {
+				const skeleton: HTMLParagraphElement | null = container.querySelector(
+					`div[data-tid="${testIdPrefix}-${i}"]`
+				);
+				expect(skeleton).toBeDefined();
+			});
+
+			solTransactionsStore.append({ tokenId: SOLANA_TOKEN_ID, transactions: [] });
+
+			await tick();
+
+			Array.from({ length: 5 }).forEach((_, i) => {
+				const skeleton: HTMLParagraphElement | null = container.querySelector(
+					`div[data-tid="${testIdPrefix}-${i}"]`
+				);
+				expect(skeleton).toBeNull();
+			});
+		});
+
+		it('should not render the skeleton when at least one store has not all the tokens initialized', async () => {
+			vi.spyOn(enabledSplTokens, 'subscribe').mockImplementation((fn) => {
+				fn([{ ...JUP_TOKEN, enabled: true }]);
+				return () => {};
+			});
+
+			const { container } = render(AllTransactionsSkeletons, {
+				props: {
+					testIdPrefix
+				}
+			});
+
+			solTransactionsStore.append({ tokenId: SOLANA_TOKEN_ID, transactions: [] });
+
+			await tick();
+
+			Array.from({ length: 5 }).forEach((_, i) => {
+				const skeleton: HTMLParagraphElement | null = container.querySelector(
+					`div[data-tid="${testIdPrefix}-${i}"]`
+				);
+				expect(skeleton).toBeNull();
+			});
+
+			solTransactionsStore.nullify(SOLANA_TOKEN_ID);
+
+			await tick();
 
 			Array.from({ length: 5 }).forEach((_, i) => {
 				const skeleton: HTMLParagraphElement | null = container.querySelector(
