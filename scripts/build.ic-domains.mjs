bcfa8b102548a2a26b4d0f2c11f7a4f6a9473c5b
Max Murphy <max@winning.black>
2025-03-03 18:48:23 +0100

    Set ic-domains from DFX_NETWORK

--- scripts/build.ic-domains.mjs
+++ scripts/build.ic-domains.mjs
@@ -3,13 +3,26 @@ import { readFileSync, writeFileSync } from 'node:fs';
 import { join } from 'node:path';
 import { ENV, replaceEnv } from './build.utils.mjs';
 
-config({ path: `.env.${ENV}` });
+const domain_for_dfx_network = (dfx_network) => {
+  return dfx_network === 'ic'? 'https://oisy.com'
+       : dfx_network === 'test_fe_1'? 'https://fe1.oisy.com'
+       : dfx_network === 'test_fe_2'? 'https://fe2.oisy.com'
+       : dfx_network === 'test_fe_3'? 'https://fe3.oisy.com'
+       : dfx_network === 'test_fe_4'? 'https://fe4.oisy.com'
+       : dfx_network === 'staging'? 'https://staging.oisy.com'
+       : dfx_network === 'beta'? 'https://beta.oisy.com'
+       : dfx_network === 'audit'? 'https://audit.oisy.com'
+       : `https://${dfx_network}.oisy.com`;
+};
 
 const generateDomain = (targetFile) => {
 	let content = readFileSync(targetFile, 'utf8');
 
+	const dfx_network = process.env.DFX_NETWORK;
+	const domain = domain_for_dfx_network(dfx_network);
+
 	// For information such as custom domains, only the domain without protocol is required
-	const { host: value } = new URL(process.env['VITE_OISY_URL'] ?? 'https://oisy.com');
+	const { host: value } = new URL(domain);
 	content = replaceEnv({
 		content,
 		pattern: `{{VITE_OISY_DOMAIN}}`,
