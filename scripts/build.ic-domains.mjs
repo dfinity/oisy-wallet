e376fbd920a2120fa1d8aa839b2560f763cf60ff
Max Murphy <max@winning.black>
2025-03-04 15:42:40 +0100

    Move the mapping into a JSON file

--- scripts/build.ic-domains.mjs
+++ scripts/build.ic-domains.mjs
@@ -1,26 +1,11 @@
 import { readFileSync, writeFileSync } from 'node:fs';
 import { join } from 'node:path';
-import { replaceEnv } from './build.utils.mjs';
-
-const DFX_NETWORK_DOMAINS = {
-	ic: 'https://oisy.com',
-	test_fe_1: 'https://fe1.oisy.com',
-	test_fe_2: 'https://fe2.oisy.com',
-	test_fe_3: 'https://fe3.oisy.com',
-	test_fe_4: 'https://fe4.oisy.com',
-	staging: 'https://staging.oisy.com',
-	beta: 'https://beta.oisy.com',
-	audit: 'https://audit.oisy.com'
-};
-
-const domain_for_dfx_network = (dfx_network) =>
-	DFX_NETWORK_DOMAINS[dfx_network] || `https://${dfx_network}.oisy.com`;
+import { OISY_IC_DOMAIN, replaceEnv } from './build.utils.mjs';
 
 const generateDomain = (targetFile) => {
 	let content = readFileSync(targetFile, 'utf8');
 
-	const dfx_network = process.env.DFX_NETWORK;
-	const domain = domain_for_dfx_network(dfx_network);
+	const domain = OISY_IC_DOMAIN;
 
 	// For information such as custom domains, only the domain without protocol is required
 	const { host: value } = new URL(domain);
--- scripts/build.utils.mjs
+++ scripts/build.utils.mjs
@@ -15,6 +15,17 @@ export const ENV =
 				? REQUESTED_ENV
 				: 'development';
 
+const OISY_DOMAINS_FILE = 'domains.json';
+export const OISY_DOMAINS = () => {
+	const jsonPath = resolve(OISY_DOMAINS_FILE);
+	return JSON.parse(readFileSync(jsonPath, 'utf-8'));
+};
+const domain_for_dfx_network = (dfx_network) =>
+	OISY_DOMAINS_FILE.frontend[dfx_network] || `https://${dfx_network}.oisy.com`;
+
+// The domain name, as in the browser location bar and in the web assets under .well-known/ic-domain
+export const OISY_IC_DOMAIN = domain_for_dfx_network(process.env.DFX_NETWORK);
+
 export const replaceEnv = ({ content, pattern, value }) => {
 	const regex = new RegExp(pattern, 'g');
 	return content.replace(regex, value);
