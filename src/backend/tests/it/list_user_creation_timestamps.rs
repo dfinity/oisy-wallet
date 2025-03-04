85a00ba100ca918a4cba5935ba8bb02fb4778e4d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-04 09:16:41 +0100

    more tests

--- src/backend/tests/it/list_user_creation_timestamps.rs
+++ src/backend/tests/it/list_user_creation_timestamps.rs
@@ -5,7 +5,7 @@ use ic_verifiable_credentials::issuer_api::CredentialSpec;
 use shared::types::{
     user_profile::{
         AddUserCredentialError, AddUserCredentialRequest, ListUserCreationTimestampsResponse,
-        ListUsersRequest, OisyUser, UserProfile,
+        ListUsersRequest, UserProfile,
     },
     Timestamp,
 };
@@ -36,10 +36,10 @@ fn test_list_user_creation_timestamps_cannot_be_called_if_not_allowed() {
 fn test_list_user_creation_timestamps_returns_timestamps() {
     let pic_setup = setup();
 
-    let expected_users: Vec<OisyUser> = pic_setup.create_users(1..=5);
-    let expected_timestamps: Vec<Timestamp> = expected_users
+    let expected_user_profiles: Vec<UserProfile> = pic_setup.create_user_profiles(1..=5);
+    let expected_timestamps: Vec<Timestamp> = expected_user_profiles
         .iter()
-        .map(|user| user.updated_timestamp)
+        .map(|user| user.created_timestamp)
         .collect();
 
     let caller = Principal::from_text(CALLER).unwrap();
@@ -64,7 +64,7 @@ fn test_list_user_creation_timestamps_returns_filtered_timestamps_by_updated() {
 
     // Add 15 users
     let users_count_initial = 15;
-    pic_setup.create_users(1..=users_count_initial);
+    pic_setup.create_user_profiles(1..=users_count_initial);
 
     // Add one user that will be updated after the desired timestamp
     let vc_holder = Principal::from_text(VC_HOLDER).expect("VC Holder principal is invalid");
@@ -84,8 +84,8 @@ fn test_list_user_creation_timestamps_returns_filtered_timestamps_by_updated() {
 
     // Add 10 more users
     let users_count_after_timestamp = 10;
-    let mut expected_users: Vec<OisyUser> = pic_setup
-        .create_users(users_count_initial + 1..=users_count_initial + users_count_after_timestamp);
+    let mut expected_user_profiles: Vec<UserProfile> = pic_setup
+        .create_user_profiles(users_count_initial + 1..=users_count_initial + users_count_after_timestamp);
 
     // Advance time before updating one of the users
     pic_setup.pic().advance_time(Duration::new(10, 0));
@@ -126,16 +126,12 @@ fn test_list_user_creation_timestamps_returns_filtered_timestamps_by_updated() {
         .expect("Call failed")
         .creation_timestamps;
 
-    let vc_holder_expected_user = OisyUser {
-        principal: vc_holder,
-        updated_timestamp: timestamp_nanos_2 as u64,
-        pouh_verified: true,
-    };
-    expected_users.push(vc_holder_expected_user);
+    let vc_holder_expected_user_profile = initial_profile.clone();
+    expected_user_profiles.push(vc_holder_expected_user_profile);
 
-    let expected_timestamps: Vec<Timestamp> = expected_users
+    let expected_timestamps: Vec<Timestamp> = expected_user_profiles
         .iter()
-        .map(|user| user.updated_timestamp)
+        .map(|user| user.created_timestamp)
         .collect();
 
     assert_user_creation_timestamps_eq(results_timestamps, expected_timestamps);
@@ -146,7 +142,7 @@ fn test_list_user_creation_timestamps_returns_requested_users_count() {
     let pic_setup = setup();
 
     let users_count_initial = 20;
-    pic_setup.create_users(1..=users_count_initial);
+    pic_setup.create_user_profiles(1..=users_count_initial);
 
     let caller = Principal::from_text(CALLER).unwrap();
 
@@ -162,17 +158,17 @@ fn test_list_user_creation_timestamps_returns_requested_users_count() {
     // Add 15 more users
     let users_count_after_timestamp = 15;
     let users_after_expected_timestamp = pic_setup
-        .create_users(users_count_initial + 1..=users_count_initial + users_count_after_timestamp);
+        .create_user_profiles(users_count_initial + 1..=users_count_initial + users_count_after_timestamp);
 
     let requested_count: usize = 10;
     let arg = ListUsersRequest {
         matches_max_length: Some(requested_count as u64),
         updated_after_timestamp: Some(timestamp_nanos as u64),
     };
-    let expected_users = &users_after_expected_timestamp[0..requested_count];
-    let expected_timestamps: Vec<Timestamp> = expected_users
+    let expected_user_profiles = &users_after_expected_timestamp[0..requested_count];
+    let expected_timestamps: Vec<Timestamp> = expected_user_profiles
         .iter()
-        .map(|user| user.updated_timestamp)
+        .map(|user| user.created_timestamp)
         .collect();
     let list_user_creation_timestamps_response = pic_setup
         .query::<ListUserCreationTimestampsResponse>(caller, "list_user_creation_timestamps", arg);
@@ -189,7 +185,7 @@ fn test_list_user_creation_timestamps_returns_less_than_requested_users_count() 
     let pic_setup = setup();
 
     let users_count = 20;
-    let created_users = pic_setup.create_users(1..=users_count);
+    let created_users = pic_setup.create_user_profiles(1..=users_count);
 
     let caller = Principal::from_text(CALLER).unwrap();
 
@@ -204,81 +200,12 @@ fn test_list_user_creation_timestamps_returns_less_than_requested_users_count() 
     let results_timestamps = list_user_creation_timestamps_response
         .expect("Call failed")
         .creation_timestamps;
-    let expected_users = &created_users[0..requested_count];
-    let expected_timestamps: Vec<Timestamp> = expected_users
+    let expected_user_profiles = &created_users[0..requested_count];
+    let expected_timestamps: Vec<Timestamp> = expected_user_profiles
         .iter()
-        .map(|user| user.updated_timestamp)
+        .map(|user| user.created_timestamp)
         .collect();
 
     assert_user_creation_timestamps_eq(results_timestamps, expected_timestamps);
 }
 
-#[test]
-fn test_list_user_creation_timestamps_returns_pouh_credential() {
-    let pic_setup = setup();
-
-    // Add one user that will be updated after the desired timestamp
-    let vc_holder = Principal::from_text(VC_HOLDER).expect("VC Holder principal is invalid");
-
-    let create_profile_response =
-        pic_setup.update::<UserProfile>(vc_holder, "create_user_profile", ());
-    let initial_profile = create_profile_response.expect("Create failed");
-
-    // Add 10 more users
-    let users_count = 10;
-    let mut expected_users = pic_setup.create_users(1..=users_count);
-
-    // Advance time before adding credentials
-    pic_setup.pic().advance_time(Duration::new(10, 0));
-
-    // Update the first user
-    let add_user_cred_arg = AddUserCredentialRequest {
-        credential_jwt: VP_JWT.to_string(),
-        current_user_version: initial_profile.version,
-        credential_spec: CredentialSpec {
-            credential_type: "ProofOfUniqueness".to_string(),
-            arguments: None,
-        },
-        issuer_canister_id: Principal::from_text(ISSUER_CANISTER_ID)
-            .expect("VC Holder principal is invalid"),
-    };
-
-    let _ = pic_setup.update::<Result<(), AddUserCredentialError>>(
-        vc_holder,
-        "add_user_credential",
-        add_user_cred_arg.clone(),
-    );
-    let timestamp_nanos = pic_setup
-        .pic()
-        .get_time()
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards")
-        .as_nanos();
-
-    let caller = Principal::from_text(CALLER).unwrap();
-
-    let arg = ListUsersRequest {
-        matches_max_length: None,
-        updated_after_timestamp: None,
-    };
-    let list_user_creation_timestamps_response = pic_setup
-        .query::<ListUserCreationTimestampsResponse>(caller, "list_user_creation_timestamps", arg);
-
-    let results_timestamps = list_user_creation_timestamps_response
-        .expect("Call failed")
-        .creation_timestamps;
-
-    let expected_vc_holder_user = OisyUser {
-        principal: vc_holder,
-        updated_timestamp: timestamp_nanos as u64,
-        pouh_verified: true,
-    };
-    expected_users.push(expected_vc_holder_user);
-
-    let expected_timestamps: Vec<Timestamp> = expected_users
-        .iter()
-        .map(|user| user.updated_timestamp)
-        .collect();
-
-    assert_user_creation_timestamps_eq(results_timestamps, expected_timestamps);
-}
