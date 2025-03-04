1da2deab80d6620d3c3b7a0832ca0a2fadb8fca8
Max Murphy <max@winning.black>
2025-03-04 15:53:22 +0100

    Fix import

--- scripts/build.utils.mjs
+++ scripts/build.utils.mjs
@@ -1,5 +1,6 @@
 import { join } from 'node:path';
 import { findFiles } from './utils.mjs';
+import OISY_DOMAINS from "./domains.json" with { type: "json" };
 
 export const findHtmlFiles = (dir = join(process.cwd(), 'build')) =>
 	findFiles({ dir, extensions: ['.html'] });
@@ -15,13 +16,8 @@ export const ENV =
 				? REQUESTED_ENV
 				: 'development';
 
-const OISY_DOMAINS_FILE = 'domains.json';
-export const OISY_DOMAINS = () => {
-	const jsonPath = resolve(OISY_DOMAINS_FILE);
-	return JSON.parse(readFileSync(jsonPath, 'utf-8'));
-};
 const domain_for_dfx_network = (dfx_network) =>
-	OISY_DOMAINS_FILE.frontend[dfx_network] || `https://${dfx_network}.oisy.com`;
+	OISY_DOMAINS.frontend[dfx_network] || `https://${dfx_network}.oisy.com`;
 
 // The domain name, as in the browser location bar and in the web assets under .well-known/ic-domain
 export const OISY_IC_DOMAIN = domain_for_dfx_network(process.env.DFX_NETWORK);
