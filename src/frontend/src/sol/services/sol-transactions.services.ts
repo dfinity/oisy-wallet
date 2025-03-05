ed7ffb2956041d5814fb266e42ac82e866f6001d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-05 22:17:39 +0100

    feat(frontend): reverse Solana transactions only after mapping

--- src/frontend/src/sol/services/sol-transactions.services.ts
+++ src/frontend/src/sol/services/sol-transactions.services.ts
@@ -71,69 +71,71 @@ export const fetchSolTransactionsForSignature = async ({
 				})
 			: [undefined];
 
-	// The instructions are received in the order they were executed, meaning the first instruction
-	// in the list was executed first, and the last instruction was executed last.
-	// However, since they all share the same timestamp, we want to display them in reverse
-	// order—from the last executed instruction to the first. This ensures that when shown,
-	// the most recently executed instruction appears first, maintaining a more intuitive,
-	// backward-looking view of execution history.
-	return await allInstructions.reverse().reduce(
-		async (acc, instruction, idx) => {
-			const innerInstructionsRaw =
-				putativeInnerInstructions.find(({ index }) => index === idx)?.instructions ?? [];
+	return (
+		(
+			await allInstructions.reduce<Promise<SolTransactionUi[]>>(async (acc, instruction, idx) => {
+				const innerInstructionsRaw =
+					putativeInnerInstructions.find(({ index }) => index === idx)?.instructions ?? [];
 
-			const innerInstructions: SolRpcInstruction[] = innerInstructionsRaw.map(
-				(innerInstruction) => ({
-					...innerInstruction,
-					programAddress: innerInstruction.programId
-				})
-			);
+				const innerInstructions: SolRpcInstruction[] = innerInstructionsRaw.map(
+					(innerInstruction) => ({
+						...innerInstruction,
+						programAddress: innerInstruction.programId
+					})
+				);
 
-			const mappedTransaction = await mapSolParsedInstruction({
-				instruction: {
-					...instruction,
-					programAddress: instruction.programId
-				},
-				innerInstructions,
-				network
-			});
+				const mappedTransaction = await mapSolParsedInstruction({
+					instruction: {
+						...instruction,
+						programAddress: instruction.programId
+					},
+					innerInstructions,
+					network
+				});
 
-			if (nonNullish(mappedTransaction) && mappedTransaction.tokenAddress === tokenAddress) {
-				const { value, from, to } = mappedTransaction;
+				if (nonNullish(mappedTransaction) && mappedTransaction.tokenAddress === tokenAddress) {
+					const { value, from, to } = mappedTransaction;
 
-				if (from !== address && to !== address && from !== ataAddress && to !== ataAddress) {
-					return acc;
+					if (from !== address && to !== address && from !== ataAddress && to !== ataAddress) {
+						return acc;
+					}
+
+					const newTransaction: SolTransactionUi = {
+						id: `${signature.signature}-${instruction.programId}`,
+						signature: signature.signature,
+						timestamp: blockTime ?? 0n,
+						value,
+						type: address === from ? 'send' : 'receive',
+						from,
+						to,
+						status
+					};
+
+					return [
+						...(await acc),
+						...(from === to
+							? [
+									{
+										...newTransaction,
+										id: `${newTransaction.id}-self`,
+										type: newTransaction.type === 'send' ? 'receive' : 'send'
+									} as SolTransactionUi
+								]
+							: []),
+						newTransaction
+					];
 				}
 
-				const newTransaction: SolTransactionUi = {
-					id: `${signature.signature}-${instruction.programId}`,
-					signature: signature.signature,
-					timestamp: blockTime ?? 0n,
-					value,
-					type: address === from ? 'send' : 'receive',
-					from,
-					to,
-					status
-				};
-
-				return [
-					...(await acc),
-					...(from === to
-						? [
-								{
-									...newTransaction,
-									id: `${newTransaction.id}-self`,
-									type: newTransaction.type === 'send' ? 'receive' : 'send'
-								} as SolTransactionUi
-							]
-						: []),
-					newTransaction
-				];
-			}
-
-			return acc;
-		},
-		Promise.resolve([] as SolTransactionUi[])
+				return acc;
+			}, Promise.resolve([]))
+		)
+			// The instructions are received in the order they were executed, meaning the first instruction
+			// in the list was executed first, and the last instruction was executed last.
+			// However, since they all share the same timestamp, we want to display them in reverse
+			// order—from the last executed instruction to the first. This ensures that when shown,
+			// the most recently executed instruction appears first, maintaining a more intuitive,
+			// backward-looking view of execution history.
+			.reverse()
 	);
 };
 
