f0e36f066e5842ae28efa34c992d691114afa83d
daviddecentage <david.farina@dfinity.org>
2025-03-05 19:19:55 +0100

    fix

--- src/frontend/src/lib/components/airdrops/AirdropModal.svelte
+++ src/frontend/src/lib/components/airdrops/AirdropModal.svelte
@@ -12,13 +12,60 @@
 	import { getUserInfo } from '$lib/api/reward.api';
 	import { authIdentity } from '$lib/derived/auth.derived';
 	import { onMount } from 'svelte';
+	import type { RewardInfo } from '$declarations/rewards/rewards.did';
+	import {
+		LOCAL_CKUSDC_LEDGER_CANISTER_ID,
+		CKBTC_LEDGER_CANISTER_IDS,
+		STAGING_CKBTC_LEDGER_CANISTER_ID,
+		CKBTC_LEDGER_CANISTER_TESTNET_IDS,
+		LOCAL_CKBTC_LEDGER_CANISTER_ID,
+		CKERC20_LEDGER_CANISTER_TESTNET_IDS
+	} from '$env/networks/networks.icrc.env';
+	import { ICP_LEDGER_CANISTER_ID } from '$env/networks/networks.icp.env';
+	import { BigNumber } from '@ethersproject/bignumber';
+	import { formatToken } from '$lib/utils/format.utils';
+	import { EIGHT_DECIMALS } from '$lib/constants/app.constants';
 
 	export let airdrop: AirdropDescription;
 
+	let ckBtcReward: BigNumber;
+	$: ckBtcReward = BigNumber.from(0);
+	let ckUsdcReward: BigNumber;
+	$: ckUsdcReward = BigNumber.from(0);
+	let icpReward: BigNumber;
+	$: icpReward = BigNumber.from(0);
+
 	const loadRewardsUserInfo = async () => {
 		const data = await getUserInfo({ identity: $authIdentity });
-		console.log('Rewards user info', data);
+
+		console.log(data);
+
+		data.usage_awards[0]?.forEach((aw, i) => {
+			const canisterId = aw.ledger.toText();
+			console.log('canisterId ' + i, canisterId);
+			if (
+				CKBTC_LEDGER_CANISTER_IDS.includes(canisterId) ||
+				STAGING_CKBTC_LEDGER_CANISTER_ID === canisterId ||
+				CKBTC_LEDGER_CANISTER_TESTNET_IDS.includes(canisterId) ||
+				LOCAL_CKBTC_LEDGER_CANISTER_ID === canisterId
+			) {
+				ckBtcReward = BigNumber.from(ckBtcReward).add(aw.amount);
+			} else if (ICP_LEDGER_CANISTER_ID.includes(canisterId)) {
+				icpReward = BigNumber.from(ckBtcReward).add(aw.amount);
+			} else if (
+				LOCAL_CKUSDC_LEDGER_CANISTER_ID === canisterId ||
+				CKERC20_LEDGER_CANISTER_TESTNET_IDS.includes(canisterId)
+			) {
+				ckUsdcReward = BigNumber.from(ckBtcReward).add(aw.amount);
+			} else {
+				console.warn('Ledger canister mapping not found for: ' + canisterId);
+			}
+		});
 	};
+
+	$: console.log('ckBtcReward', ckBtcReward);
+	$: console.log('ckUsdcReward', ckUsdcReward);
+	$: console.log('icpReward', icpReward);
 
 	onMount(loadRewardsUserInfo);
 </script>
@@ -28,6 +75,33 @@
 
 	<ContentWithToolbar>
 		<AirdropBanner />
+
+		<div class="flex w-full gap-2">
+			<div class="w-1/3 rounded-xl bg-success-primary p-5 font-bold text-primary-inverted">
+				{formatToken({
+					value: ckBtcReward,
+					unitName: 8,
+					displayDecimals: EIGHT_DECIMALS,
+					showPlusSign: true
+				})} ckBTC
+			</div>
+			<div class="w-1/3 rounded-xl bg-success-primary p-5 font-bold text-primary-inverted">
+				{formatToken({
+					value: ckUsdcReward,
+					unitName: 8,
+					displayDecimals: EIGHT_DECIMALS,
+					showPlusSign: true
+				})} ckUSDC
+			</div>
+			<div class="w-1/3 rounded-xl bg-success-primary p-5 font-bold text-primary-inverted">
+				{formatToken({
+					value: icpReward,
+					unitName: 8,
+					displayDecimals: EIGHT_DECIMALS,
+					showPlusSign: true
+				})} ICP
+			</div>
+		</div>
 
 		<span class="text-lg font-semibold">{$i18n.airdrops.text.participate_title}</span>
 		<p class="mb-0 mt-2">{airdrop.description}</p>
