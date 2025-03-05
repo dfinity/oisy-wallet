c77e97643c70c0a77b5a84449ffffcd95d37d267
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-05 22:47:08 +0100

    test

--- src/frontend/src/lib/components/navigation/NavigationMenu.svelte
+++ src/frontend/src/lib/components/navigation/NavigationMenu.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="flex h-full w-full flex-col justify-between py-3 pl-4 md:pl-8">
-	<div class="flex flex-col gap-3">
+	<div class="flex flex-col gap-3 hidden md:block">
 		<NavigationMenuMainItems />
 	</div>
 
