6d12e337da98f905bc7c663ecb670fb677786d11
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-03-06 14:02:31 +0100

    feat(frontend): assert-amount utils (#5052)
    # Motivation
    
    The idea is to create utils for validating user amount that can be
    re-used in both convert and send flow. Some of the utils take into
    account tokens-specific conditions. The whole list of added functions:
    * `assertAmount` - common for BTC and ETH
    * `assertCkBtcAmount` - specific for ckBTC
    * `assertCkErc20Amount` - specific for ckERC20 tokens
    * `assertCkEthAmount` - specific for ckETH
    * `assertErc20Amount` - specific for ERC20 tokens

---
+++ src/frontend/src/tests/lib/utils/assert-amount.utils.spec.ts
@@ -0,0 +1,261 @@
+import {
+	assertAmount,
+	assertCkBtcAmount,
+	assertCkErc20Amount,
+	assertCkEthAmount,
+	assertErc20Amount
+} from '$lib/utils/assert-amount.utils';
+import { mockCkBtcMinterInfo } from '$tests/mocks/ckbtc.mock';
+import type { MinterInfo as CkBtcMinterInfo } from '@dfinity/ckbtc';
+import type { MinterInfo as CkEthMinterInfo } from '@dfinity/cketh';
+import { BigNumber } from 'alchemy-sdk';
+
+describe('asserts-amount.utils', () => {
+	describe('assertAmount', () => {
+		const params = {
+			userAmount: BigNumber.from(1000n),
+			balance: BigNumber.from(2000n),
+			fee: 100n
+		};
+
+		it('should return balance error', () => {
+			expect(
+				assertAmount({
+					...params,
+					balance: BigNumber.from(900n)
+				})
+			).toBe('insufficient-funds');
+		});
+
+		it('should return funds for fee error', () => {
+			expect(
+				assertAmount({
+					...params,
+					balance: BigNumber.from(1050n)
+				})
+			).toBe('insufficient-funds-for-fee');
+		});
+
+		it('should return undefined', () => {
+			expect(assertAmount(params)).toBeUndefined();
+		});
+	});
+
+	describe('assertErc20Amount', () => {
+		const params = {
+			userAmount: BigNumber.from(1000n),
+			balance: BigNumber.from(2000n),
+			balanceForFee: BigNumber.from(1000n),
+			fee: 100n
+		};
+
+		it('should return balance error', () => {
+			expect(
+				assertErc20Amount({
+					...params,
+					balance: BigNumber.from(900n)
+				})
+			).toBe('insufficient-funds');
+		});
+
+		it('should return funds for fee error', () => {
+			expect(
+				assertErc20Amount({
+					...params,
+					balanceForFee: BigNumber.from(50n)
+				})
+			).toBe('insufficient-funds-for-fee');
+		});
+
+		it('should return undefined', () => {
+			expect(assertErc20Amount(params)).toBeUndefined();
+		});
+	});
+
+	describe('assertCkBtcAmount', () => {
+		const params = {
+			userAmount: BigNumber.from(1000n),
+			balance: BigNumber.from(2000n),
+			fee: 100n,
+			minterInfo: { data: mockCkBtcMinterInfo, certified: true }
+		};
+
+		it('should return balance error', () => {
+			expect(
+				assertCkBtcAmount({
+					...params,
+					balance: BigNumber.from(900n)
+				})
+			).toBe('insufficient-funds');
+		});
+
+		it('should return funds for fee error', () => {
+			expect(
+				assertCkBtcAmount({
+					...params,
+					balance: BigNumber.from(1050n)
+				})
+			).toBe('insufficient-funds-for-fee');
+		});
+
+		it('should return unknown minimum amount error', () => {
+			expect(
+				assertCkBtcAmount({
+					...params,
+					minterInfo: undefined
+				})
+			).toBe('unknown-minimum-amount');
+		});
+
+		it('should return minimum amount not reached error', () => {
+			expect(
+				assertCkBtcAmount({
+					...params,
+					minterInfo: {
+						...params.minterInfo,
+						data: {
+							...params.minterInfo.data,
+							retrieve_btc_min_amount: 5000n
+						} as CkBtcMinterInfo
+					}
+				})
+			).toBe('minimum-amount-not-reached');
+		});
+
+		it('should return minter info not certified error', () => {
+			expect(
+				assertCkBtcAmount({
+					...params,
+					minterInfo: {
+						...params.minterInfo,
+						certified: false
+					}
+				})
+			).toBe('minter-info-not-certified');
+		});
+
+		it('should return undefined', () => {
+			expect(assertCkBtcAmount(params)).toBeUndefined();
+		});
+	});
+
+	describe('assertCkEthAmount', () => {
+		const params = {
+			userAmount: BigNumber.from(1000n),
+			balance: BigNumber.from(2000n),
+			fee: 100n,
+			minterInfo: {
+				data: { minimum_withdrawal_amount: [500n] } as CkEthMinterInfo,
+				certified: true
+			}
+		};
+
+		it('should return balance error', () => {
+			expect(
+				assertCkEthAmount({
+					...params,
+					balance: BigNumber.from(900n)
+				})
+			).toBe('insufficient-funds');
+		});
+
+		it('should return funds for fee error', () => {
+			expect(
+				assertCkEthAmount({
+					...params,
+					balance: BigNumber.from(1050n)
+				})
+			).toBe('insufficient-funds-for-fee');
+		});
+
+		it('should return user amount less than fee error', () => {
+			expect(
+				assertCkEthAmount({
+					...params,
+					userAmount: BigNumber.from(50n)
+				})
+			).toBe('minimum-amount-not-reached');
+		});
+
+		it('should return unknown minimum amount error', () => {
+			expect(
+				assertCkEthAmount({
+					...params,
+					minterInfo: undefined
+				})
+			).toBe('unknown-minimum-amount');
+		});
+
+		it('should return minimum amount not reached error', () => {
+			expect(
+				assertCkEthAmount({
+					...params,
+					minterInfo: {
+						...params.minterInfo,
+						data: {
+							...params.minterInfo.data,
+							minimum_withdrawal_amount: [5000n]
+						} as CkEthMinterInfo
+					}
+				})
+			).toBe('minimum-amount-not-reached');
+		});
+
+		it('should return minter info not certified error', () => {
+			expect(
+				assertCkBtcAmount({
+					...params,
+					minterInfo: {
+						...params.minterInfo,
+						certified: false
+					}
+				})
+			).toBe('minter-info-not-certified');
+		});
+
+		it('should return undefined', () => {
+			expect(assertCkBtcAmount(params)).toBeUndefined();
+		});
+	});
+
+	describe('assertCkErc20Amount', () => {
+		const params = {
+			userAmount: BigNumber.from(1000n),
+			balance: BigNumber.from(2000n),
+			balanceForFee: BigNumber.from(1000n),
+			ethereumEstimateFee: 500n,
+			fee: 100n
+		};
+
+		it('should return balance error', () => {
+			expect(
+				assertCkErc20Amount({
+					...params,
+					balance: BigNumber.from(900n)
+				})
+			).toBe('insufficient-funds');
+		});
+
+		it('should return funds for fee error if balance cannot cover IC token fee', () => {
+			expect(
+				assertCkErc20Amount({
+					...params,
+					balance: BigNumber.from(1050n)
+				})
+			).toBe('insufficient-funds-for-fee');
+		});
+
+		it('should return funds for fee error if balanceForFee cannot cover ethereumEstimateFee', () => {
+			expect(
+				assertCkErc20Amount({
+					...params,
+					ethereumEstimateFee: 4000n
+				})
+			).toBe('insufficient-funds-for-fee');
+		});
+
+		it('should return undefined', () => {
+			expect(assertCkErc20Amount(params)).toBeUndefined();
+		});
+	});
+});
