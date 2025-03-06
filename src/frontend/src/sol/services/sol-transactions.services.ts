9515a32d5fa2795d6f2b4639dbf2487840af87c2
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-06 23:49:15 +0100

    refactor(frontend): make object for internal reduce of fetchSolTransactionsForSignature (#5059)
    # Motivation
    
    We refactor the `reduce` function inside
    `fetchSolTransactionsForSignature` to return an object, so that we can
    prepare for further calculation: we will calculate the cumulative
    balance in another PR, to pass it to the mapping and calculate the
    `closeAccount` value (from PR
    https://github.com/dfinity/oisy-wallet/pull/5056).
    
    # Changes
    
    Instead of returning an array of parsed transactions `SolTransactionUi`,
    the reduce function returns an object that is `{ parsedTransactions:
    SolTransactionUi[] }`.
    
    Nothing changed in the implementation of the functions.
    
    # Tests
    
    Current tests are enough.

--- src/frontend/src/sol/services/sol-transactions.services.ts
+++ src/frontend/src/sol/services/sol-transactions.services.ts
@@ -74,68 +74,79 @@ export const fetchSolTransactionsForSignature = async ({
 				})
 			: [undefined];
 
-	const parsedTransactions: SolTransactionUi[] = await allInstructions.reduce<
-		Promise<SolTransactionUi[]>
-	>(async (acc, instruction, idx) => {
-		const innerInstructionsRaw =
-			putativeInnerInstructions.find(({ index }) => index === idx)?.instructions ?? [];
+	const { parsedTransactions } = await allInstructions.reduce<
+		Promise<{
+			parsedTransactions: SolTransactionUi[];
+		}>
+	>(
+		async (acc, instruction, idx) => {
+			const innerInstructionsRaw =
+				putativeInnerInstructions.find(({ index }) => index === idx)?.instructions ?? [];
 
-		const innerInstructions: SolRpcInstruction[] = innerInstructionsRaw.map((innerInstruction) => ({
-			...innerInstruction,
-			programAddress: innerInstruction.programId
-		}));
+			const innerInstructions: SolRpcInstruction[] = innerInstructionsRaw.map(
+				(innerInstruction) => ({
+					...innerInstruction,
+					programAddress: innerInstruction.programId
+				})
+			);
 
-		const mappedTransaction = await mapSolParsedInstruction({
-			instruction: {
-				...instruction,
-				programAddress: instruction.programId
-			},
-			innerInstructions,
-			network
-		});
+			const { parsedTransactions } = await acc;
 
-		if (isNullish(mappedTransaction)) {
-			return acc;
-		}
+			const mappedTransaction = await mapSolParsedInstruction({
+				instruction: {
+					...instruction,
+					programAddress: instruction.programId
+				},
+				innerInstructions,
+				network
+			});
 
-		const { value, from, to, tokenAddress: mappedTokenAddress } = mappedTransaction;
+			if (isNullish(mappedTransaction)) {
+				return acc;
+			}
 
-		// Ignoring the instruction if the transaction is not related to the address or its associated token account.
-		if (from !== address && to !== address && from !== ataAddress && to !== ataAddress) {
-			return acc;
-		}
+			const { value, from, to, tokenAddress: mappedTokenAddress } = mappedTransaction;
 
-		// If the token address is not the one we are looking for, we can skip this instruction.
-		// In case of Solana native tokens, the token address is undefined.
-		if (mappedTokenAddress !== tokenAddress) {
-			return acc;
-		}
+			// Ignoring the instruction if the transaction is not related to the address or its associated token account.
+			if (from !== address && to !== address && from !== ataAddress && to !== ataAddress) {
+				return acc;
+			}
 
-		const newTransaction: SolTransactionUi = {
-			id: `${signature.signature}-${instruction.programId}`,
-			signature: signature.signature,
-			timestamp: blockTime ?? 0n,
-			value,
-			type: address === from ? 'send' : 'receive',
-			from,
-			to,
-			status
-		};
+			// If the token address is not the one we are looking for, we can skip this instruction.
+			// In case of Solana native tokens, the token address is undefined.
+			if (mappedTokenAddress !== tokenAddress) {
+				return acc;
+			}
 
-		return [
-			...(await acc),
-			newTransaction,
-			...(from === to
-				? [
-						{
-							...newTransaction,
-							id: `${newTransaction.id}-self`,
-							type: newTransaction.type === 'send' ? 'receive' : 'send'
-						} as SolTransactionUi
-					]
-				: [])
-		];
-	}, Promise.resolve([]));
+			const newTransaction: SolTransactionUi = {
+				id: `${signature.signature}-${instruction.programId}`,
+				signature: signature.signature,
+				timestamp: blockTime ?? 0n,
+				value,
+				type: address === from ? 'send' : 'receive',
+				from,
+				to,
+				status
+			};
+
+			return {
+				parsedTransactions: [
+					...parsedTransactions,
+					newTransaction,
+					...(from === to
+						? [
+								{
+									...newTransaction,
+									id: `${newTransaction.id}-self`,
+									type: newTransaction.type === 'send' ? 'receive' : 'send'
+								} as SolTransactionUi
+							]
+						: [])
+				]
+			};
+		},
+		Promise.resolve({ parsedTransactions: [] })
+	);
 
 	// The instructions are received in the order they were executed, meaning the first instruction
 	// in the list was executed first, and the last instruction was executed last.
