3c49d96b23cfdcef6adc29fb59f70e522c9f2952
daviddecentage <david.farina@dfinity.org>
2025-03-07 16:18:06 +0100

    refactoring

--- src/frontend/src/lib/components/airdrops/AirdropEarnings.svelte
+++ src/frontend/src/lib/components/airdrops/AirdropEarnings.svelte
@@ -7,57 +7,56 @@
 	import { BTC_MAINNET_TOKEN } from '$env/tokens/tokens.btc.env';
 	import { ICP_TOKEN } from '$env/tokens/tokens.icp.env';
 	import type { IcToken } from '$icp/types/ic-token';
-	import { getUserInfo } from '$lib/api/reward.api';
+	import { getUserRewardsTokenAmounts } from '$lib/api/reward.api';
 	import AirdropEarningsCard from '$lib/components/airdrops/AirdropEarningsCard.svelte';
 	import Hr from '$lib/components/ui/Hr.svelte';
 	import { ZERO } from '$lib/constants/app.constants';
 	import { AppPath } from '$lib/constants/routes.constants';
 	import { SLIDE_DURATION } from '$lib/constants/transition.constants';
-	import { authIdentity } from '$lib/derived/auth.derived';
-	import { exchanges } from '$lib/derived/exchange.derived';
 	import { networkId } from '$lib/derived/network.derived';
 	import { tokens } from '$lib/derived/tokens.derived';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { isMobile } from '$lib/utils/device.utils';
-	import { usdValue } from '$lib/utils/exchange.utils';
 	import { formatUSD } from '$lib/utils/format.utils';
 	import { replacePlaceholders } from '$lib/utils/i18n.utils';
 	import { networkUrl } from '$lib/utils/nav.utils';
-	import { findTwinToken } from '$lib/utils/token.utils';
+	import { calculateTokenUsdAmount, findTwinToken } from '$lib/utils/token.utils';
 	import Button from '$lib/components/ui/Button.svelte';
+	import { exchanges } from '$lib/derived/exchange.derived';
+	import { authIdentity } from '$lib/derived/auth.derived';
+
+	import type { Token } from '$lib/types/token';
 
 	export let isEligible = false;
-
-	const getUsdAmount = ({ amount, token }: { amount: BigNumber; token?: IcToken }) => {
-		if (isNullish(amount) || isNullish(token)) {
-			return 0;
-		}
-		const exchangeRate = $exchanges?.[token.id]?.usd;
-		return nonNullish(exchangeRate)
-			? usdValue({ decimals: token.decimals, balance: amount, exchangeRate })
-			: 0;
-	};
 
 	let ckBtcToken: IcToken | undefined;
 	$: ckBtcToken = findTwinToken({ tokenToPair: BTC_MAINNET_TOKEN, tokens: $tokens });
 	let ckBtcReward: BigNumber;
 	$: ckBtcReward = ZERO;
 	let ckBtcRewardUsd: number;
-	$: ckBtcRewardUsd = getUsdAmount({ amount: ckBtcReward, token: ckBtcToken });
+	$: ckBtcRewardUsd = nonNullish(ckBtcToken)
+		? (calculateTokenUsdAmount({ amount: ckBtcReward, token: ckBtcToken as Token, $exchanges }) ??
+			0)
+		: 0;
 
 	let ckUsdcToken: IcToken | undefined;
 	$: ckUsdcToken = findTwinToken({ tokenToPair: USDC_TOKEN, tokens: $tokens });
 	let ckUsdcReward: BigNumber;
 	$: ckUsdcReward = ZERO;
 	let ckUsdcRewardUsd: number;
-	$: ckUsdcRewardUsd = getUsdAmount({ amount: ckUsdcReward, token: ckUsdcToken });
+	$: ckUsdcRewardUsd = nonNullish(ckBtcToken)
+		? (calculateTokenUsdAmount({ amount: ckUsdcReward, token: ckUsdcToken as Token, $exchanges }) ??
+			0)
+		: 0;
 
 	let icpToken: IcToken | undefined;
 	$: icpToken = ICP_TOKEN;
 	let icpReward: BigNumber;
 	$: icpReward = ZERO;
 	let icpRewardUsd: number;
-	$: icpRewardUsd = getUsdAmount({ amount: icpReward, token: icpToken });
+	$: icpRewardUsd = nonNullish(ckBtcToken)
+		? (calculateTokenUsdAmount({ amount: icpReward, token: icpToken as Token, $exchanges }) ?? 0)
+		: 0;
 
 	let totalRewardUsd: number;
 	$: totalRewardUsd = ckBtcRewardUsd + ckUsdcRewardUsd + icpRewardUsd;
@@ -65,7 +64,7 @@
 	let loading: boolean;
 	$: loading = true;
 
-	const loadUserRewardsInfo = async ({
+	const loadRewards = async ({
 		ckBtcToken,
 		ckUsdcToken,
 		icpToken
@@ -74,38 +73,22 @@
 		ckUsdcToken: IcToken | undefined;
 		icpToken: IcToken | undefined;
 	}) => {
-		if (isNullish(ckBtcToken) || isNullish(ckUsdcToken) || isNullish(icpToken)) {
-			return;
-		}
-		const data = await getUserInfo({ identity: $authIdentity });
+		if (isNullish(ckBtcToken) || isNullish(ckUsdcToken) || isNullish(icpToken)) return;
 
-		let _ckBtcReward: BigNumber = ZERO;
-		let _ckUsdcReward: BigNumber = ZERO;
-		let _icpReward: BigNumber = ZERO;
+		const result = await getUserRewardsTokenAmounts({
+			ckBtcToken,
+			ckUsdcToken,
+			icpToken,
+			$authIdentity
+		});
 
-		for (let i = 0; i < (data.usage_awards[0] ?? []).length; i++) {
-			const aw = data.usage_awards[0]?.[i];
-			if (nonNullish(aw)) {
-				const canisterId = aw.ledger.toText();
-				if (ckBtcToken.ledgerCanisterId === canisterId) {
-					_ckBtcReward = BigNumber.from(_ckBtcReward).add(aw.amount);
-				} else if (icpToken.ledgerCanisterId === canisterId) {
-					_icpReward = BigNumber.from(_icpReward).add(aw.amount);
-				} else if (ckUsdcToken.ledgerCanisterId === canisterId) {
-					_ckUsdcReward = BigNumber.from(_ckUsdcReward).add(aw.amount);
-				} else {
-					console.warn(`Ledger canister mapping not found for: ${canisterId}`);
-				}
-			}
-		}
-
-		ckBtcReward = _ckBtcReward;
-		ckUsdcReward = _ckUsdcReward;
-		icpReward = _icpReward;
+		ckBtcReward = result.ckBtcReward;
+		ckUsdcReward = result.ckUsdcReward;
+		icpReward = result.icpReward;
 		loading = false;
 	};
 
-	$: loadUserRewardsInfo({ ckBtcToken, ckUsdcToken, icpToken });
+	$: loadRewards({ ckBtcToken, ckUsdcToken, icpToken });
 
 	const gotoActivity = async () => {
 		await goto(
