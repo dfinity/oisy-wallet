4272df2fd4aff490fe6e856102b6c8373a8e156a
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-03-07 10:16:42 +0100

    feat(frontend): extend convert store (#5072)
    # Motivation
    
    In this PR, I'm extending the Convert store with some data needed for
    ckERC/ckBTC/ckETH conversions.
    
    # Changes
    
    1. Add `balanceForFee` to the store.
    2. Add `minterInfo` to the store.
    
    # Tests
    
    Tests are updated.
    
    Co-authored-by: Max <bitdivine@users.noreply.github.com>

--- src/frontend/src/lib/stores/convert.store.ts
+++ src/frontend/src/lib/stores/convert.store.ts
@@ -1,6 +1,18 @@
+import { ethereumToken } from '$eth/derived/token.derived';
+import { isTokenErc20 } from '$eth/utils/erc20.utils';
+import { ckEthereumNativeToken } from '$icp-eth/derived/cketh.derived';
+import { ckEthMinterInfoStore, type CkEthMinterInfoData } from '$icp-eth/stores/cketh.store';
+import { ethereumFeeTokenCkEth } from '$icp/derived/ethereum-fee.derived';
+import { ckBtcMinterInfoStore, type CkBtcMinterInfoData } from '$icp/stores/ckbtc.store';
+import {
+	isTokenCkBtcLedger,
+	isTokenCkErc20Ledger,
+	isTokenCkEthLedger
+} from '$icp/utils/ic-send.utils';
 import { exchanges } from '$lib/derived/exchange.derived';
 import { balancesStore } from '$lib/stores/balances.store';
 import type { Token } from '$lib/types/token';
+import type { Option } from '$lib/types/utils';
 import { BigNumber } from '@ethersproject/bignumber';
 import { derived, writable, type Readable } from 'svelte/store';
 
@@ -33,13 +45,41 @@ export const initConvertContext = (convertData: ConvertData): ConvertContext => 
 		([$exchanges, $destinationToken]) => $exchanges?.[$destinationToken.id]?.usd
 	);
 
+	const balanceForFee = derived(
+		[sourceToken, balancesStore, ethereumToken, ethereumFeeTokenCkEth, ckEthereumNativeToken],
+		([
+			$sourceToken,
+			$balancesStore,
+			$ethereumToken,
+			$ethereumFeeTokenCkEth,
+			$ckEthereumNativeToken
+		]) =>
+			isTokenErc20($sourceToken)
+				? $balancesStore?.[$ethereumToken.id]?.data
+				: isTokenCkErc20Ledger($sourceToken)
+					? $balancesStore?.[($ethereumFeeTokenCkEth ?? $ckEthereumNativeToken).id]?.data
+					: undefined
+	);
+
+	const minterInfo = derived(
+		[sourceToken, ckEthMinterInfoStore, ckBtcMinterInfoStore, ckEthereumNativeToken],
+		([$sourceToken, $ckEthMinterInfoStore, $ckBtcMinterInfoStore, $ckEthereumNativeToken]) =>
+			isTokenCkEthLedger($sourceToken)
+				? $ckEthMinterInfoStore?.[$ckEthereumNativeToken.id]
+				: isTokenCkBtcLedger($sourceToken)
+					? $ckBtcMinterInfoStore?.[$sourceToken.id]
+					: undefined
+	);
+
 	return {
 		sourceToken,
 		destinationToken,
 		sourceTokenBalance,
 		destinationTokenBalance,
 		sourceTokenExchangeRate,
-		destinationTokenExchangeRate
+		destinationTokenExchangeRate,
+		balanceForFee,
+		minterInfo
 	};
 };
 
@@ -48,8 +88,10 @@ export interface ConvertContext {
 	destinationToken: Readable<Token>;
 	sourceTokenBalance: Readable<BigNumber | undefined>;
 	destinationTokenBalance: Readable<BigNumber | undefined>;
+	balanceForFee: Readable<BigNumber | undefined>;
 	sourceTokenExchangeRate: Readable<number | undefined>;
 	destinationTokenExchangeRate: Readable<number | undefined>;
+	minterInfo: Readable<Option<CkBtcMinterInfoData | CkEthMinterInfoData>>;
 }
 
 export const CONVERT_CONTEXT_KEY = Symbol('convert');
