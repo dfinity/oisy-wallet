a2316187562ce63db27b05745ca0aa0e35f09b0f
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-10 22:36:12 +0100

    feat(frontend): pass last signature to Sol wallet worker

--- src/frontend/src/sol/services/worker.sol-wallet.services.ts
+++ src/frontend/src/sol/services/worker.sol-wallet.services.ts
@@ -11,6 +11,7 @@ import type {
 	PostMessageDataResponseError
 } from '$lib/types/post-message';
 import type { Token } from '$lib/types/token';
+import { last } from '$lib/utils/array.utils';
 import {
 	isNetworkIdSOLDevnet,
 	isNetworkIdSOLLocal,
@@ -18,6 +19,7 @@ import {
 	isNetworkIdSOLTestnet
 } from '$lib/utils/network.utils';
 import { syncWallet, syncWalletError } from '$sol/services/sol-listener.services';
+import { solTransactionsStore } from '$sol/stores/sol-transactions.store';
 import type { SolPostMessageDataResponseWallet } from '$sol/types/sol-post-message';
 import { mapNetworkIdToNetwork } from '$sol/utils/network.utils';
 import { isTokenSpl } from '$sol/utils/spl.utils';
@@ -75,6 +77,9 @@ export const initSolWalletWorker = async ({ token }: { token: Token }): Promise<
 	const network = mapNetworkIdToNetwork(token.network.id);
 	assertNonNullish(network, 'No Solana network provided to start Solana wallet worker.');
 
+	// To spare calls to the Solana RPC we get the last signature fetched by the wallet
+	const beforeSignature = last(get(solTransactionsStore)?.[tokenId] ?? [])?.data.signature;
+
 	// If the token is an SPL token, we need to pass the token address and the owner address to the worker.
 	// Otherwise, we pass undefined, which will be considered as the native SOLANA token.
 	const { address: tokenAddress, owner: tokenOwnerAddress } = isTokenSpl(token)
@@ -85,7 +90,8 @@ export const initSolWalletWorker = async ({ token }: { token: Token }): Promise<
 		address,
 		solanaNetwork: network,
 		tokenAddress,
-		tokenOwnerAddress
+		tokenOwnerAddress,
+		beforeSignature
 	};
 
 	return {
