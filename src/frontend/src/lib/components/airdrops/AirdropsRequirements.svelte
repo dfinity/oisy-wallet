da3234d5a111f8fc9c4a632a1d8c3f27fd8a343c
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-03-10 11:06:56 +0100

    writes some test cases

--- src/frontend/src/lib/components/airdrops/AirdropsRequirements.svelte
+++ src/frontend/src/lib/components/airdrops/AirdropsRequirements.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-	import { IconCheckCircleFill } from '@dfinity/gix-components';
+	import {IconCheckCircleFill, Spinner} from '@dfinity/gix-components';
 	import type { AirdropDescription } from '$env/types/env-airdrop';
 	import Badge from '$lib/components/ui/Badge.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 
 	export let loading = true;
 	export let airdrop: AirdropDescription;
-	export let badgeOnly = false;
 	export let isEligible = false;
 	export let requirementsFulfilled: boolean[];
 
@@ -14,39 +13,43 @@
 </script>
 
 {#if airdrop.requirements.length > 0}
-	{#if badgeOnly}
-		{#if isEligible}<span class="inline-flex pl-3"
-				><Badge variant="success">{$i18n.airdrops.text.youre_eligible}</Badge></span
-			>{/if}
-	{:else}
-		<span class="text-md font-semibold"
-			>{$i18n.airdrops.text.requirements_title}
-		</span>{#if isEligible}<span class="inline-flex pl-3"
-				><Badge variant="success">{$i18n.airdrops.text.youre_eligible}</Badge></span
-			>{/if}
-		<ul class="list-none">
-			{#each airdrop.requirements as requirement, i}
-				<li class="flex gap-2 pt-1">
-					<span
-						class="flex w-full flex-row"
-						class:transition={!isRequirementFulfilled(i) && loading}
-						class:duration-500={!isRequirementFulfilled(i) && loading}
-						class:ease-in-out={!isRequirementFulfilled(i) && loading}
-						class:animate-pulse={!isRequirementFulfilled(i) && loading}
-					>
+	<span class="text-md font-semibold"
+		>{$i18n.airdrops.text.requirements_title}
+	</span>{#if isEligible}<span class="inline-flex pl-3"
+			><Badge variant="success">{$i18n.airdrops.text.youre_eligible}</Badge></span
+		>{/if}
+	<ul class="list-none">
+		{#each airdrop.requirements as requirement, i}
+			<li class="flex gap-2 pt-1">
+				<span
+					class="flex w-full flex-row"
+					class:transition={!isRequirementFulfilled(i) && loading}
+					class:duration-500={!isRequirementFulfilled(i) && loading}
+					class:ease-in-out={!isRequirementFulfilled(i) && loading}
+					class:animate-pulse={!isRequirementFulfilled(i) && loading}
+				>
+					{#if airdrop.requirements.length === requirementsFulfilled.length}
 						<span
+							data-tid={`airdrop-requirement-status-${i}`}
 							class="-mt-0.5 mr-2"
 							class:text-success-primary={isRequirementFulfilled(i)}
 							class:text-disabled={!isRequirementFulfilled(i)}
 						>
 							<IconCheckCircleFill size={32} />
 						</span>
-						<span>
-							{requirement}
+					{:else}
+						<span
+							data-tid={`airdrop-requirement-status-spinner-${i}`}
+							class="-mt-0.5 mr-2"
+						>
+							<Spinner inline size="small" />
 						</span>
+					{/if}
+					<span>
+						{requirement}
 					</span>
-				</li>
-			{/each}
-		</ul>
-	{/if}
+				</span>
+			</li>
+		{/each}
+	</ul>
 {/if}
---
+++ src/frontend/src/tests/lib/components/airdrops/AirdropsRequirements.spec.ts
@@ -0,0 +1,113 @@
+import type {AirdropDescription} from "$env/types/env-airdrop";
+import {mockAirdropCampaigns} from "$tests/mocks/airdrop-campaigns.mock";
+import {assertNonNullish} from "@dfinity/utils";
+import {render} from "@testing-library/svelte";
+import AirdropsRequirements from "$lib/components/airdrops/AirdropsRequirements.svelte";
+import {get} from "svelte/store";
+import {i18n} from "$lib/stores/i18n.store";
+
+describe('AirdropsRequirements', () => {
+    const mockAirdropCampaign: AirdropDescription | undefined = mockAirdropCampaigns.find(
+        ({ id }) => id === 'OISY Airdrop #1'
+    );
+    assertNonNullish(mockAirdropCampaign);
+
+    describe('IsEligible', () => {
+        it('should not render badge if not eligible', () => {
+            const { queryByText } = render(AirdropsRequirements, {
+                props: {
+                    loading: false,
+                    airdrop: mockAirdropCampaign,
+                    isEligible: false,
+                    requirementsFulfilled: [true, true, true]
+                }
+            });
+
+            expect(queryByText(get(i18n).airdrops.text.youre_eligible)).not.toBeInTheDocument();
+        });
+
+        it('should render badge if eligible', () => {
+            const { queryByText } = render(AirdropsRequirements, {
+                props: {
+                    loading: false,
+                    airdrop: mockAirdropCampaign,
+                    isEligible: true,
+                    requirementsFulfilled: [true, true, true]
+                }
+            });
+
+            expect(queryByText(get(i18n).airdrops.text.youre_eligible)).toBeInTheDocument();
+        });
+    });
+
+    describe('RequirementsFulfilled', () => {
+        const requirementStatusSelector = (index: number) => `span[data-tid="airdrop-requirement-status-${index}"]`;
+        const requirementStatusSpinnerSelector = (index: number) => `span[data-tid="airdrop-requirement-status-spinner-${index}"]`;
+
+        it('should render all requirements as fulfilled', () => {
+            const { container } = render(AirdropsRequirements, {
+                props: {
+                    loading: false,
+                    airdrop: mockAirdropCampaign,
+                    isEligible: false,
+                    requirementsFulfilled: [true, true, true]
+                }
+            });
+
+            mockAirdropCampaign.requirements.forEach((requirement, index) => {
+                const requirementStatus: HTMLSpanElement | null = container.querySelector(requirementStatusSelector(index));
+                expect(requirementStatus).toBeInTheDocument();
+                expect(requirementStatus?.className).toContain('text-success-primary');
+            });
+        });
+
+        it('should render all requirements as not fulfilled', () => {
+            const { container } = render(AirdropsRequirements, {
+                props: {
+                    loading: false,
+                    airdrop: mockAirdropCampaign,
+                    isEligible: false,
+                    requirementsFulfilled: [false, false, false]
+                }
+            });
+
+            mockAirdropCampaign.requirements.forEach((requirement, index) => {
+                const requirementStatus: HTMLSpanElement | null = container.querySelector(requirementStatusSelector(index));
+                expect(requirementStatus).toBeInTheDocument();
+                expect(requirementStatus?.className).toContain('text-disabled');
+            });
+        });
+
+        it('should render spinners if wrong amount of requirement states are available', () => {
+            const { container } = render(AirdropsRequirements, {
+                props: {
+                    loading: false,
+                    airdrop: mockAirdropCampaign,
+                    isEligible: false,
+                    requirementsFulfilled: [false, false]
+                }
+            });
+
+            mockAirdropCampaign.requirements.forEach((requirement, index) => {
+                const requirementStatusSpinner: HTMLSpanElement | null = container.querySelector(requirementStatusSpinnerSelector(index));
+                expect(requirementStatusSpinner).toBeInTheDocument();
+            });
+        });
+
+        it('should render spinners if no requirement states are available', () => {
+            const { container } = render(AirdropsRequirements, {
+                props: {
+                    loading: false,
+                    airdrop: mockAirdropCampaign,
+                    isEligible: false,
+                    requirementsFulfilled: []
+                }
+            });
+
+            mockAirdropCampaign.requirements.forEach((requirement, index) => {
+                const requirementStatusSpinner: HTMLSpanElement | null = container.querySelector(requirementStatusSpinnerSelector(index));
+                expect(requirementStatusSpinner).toBeInTheDocument();
+            });
+        });
+    });
+});
