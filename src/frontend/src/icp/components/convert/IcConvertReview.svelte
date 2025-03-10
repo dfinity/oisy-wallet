d54fbe83b9b686125ce741e702defa0e38a93909
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2025-03-10 11:56:31 +0100

    feat(frontend): PR review

--- src/frontend/src/icp/components/convert/IcConvertReview.svelte
+++ src/frontend/src/icp/components/convert/IcConvertReview.svelte
@@ -3,21 +3,20 @@
 	import IcTokenFees from '$icp/components/fee/IcTokenFees.svelte';
 	import ConvertReview from '$lib/components/convert/ConvertReview.svelte';
 	import { CONVERT_CONTEXT_KEY, type ConvertContext } from '$lib/stores/convert.store';
-	import type { NetworkId } from '$lib/types/network';
 	import type { OptionAmount } from '$lib/types/send';
 
 	export let sendAmount: OptionAmount;
 	export let receiveAmount: number | undefined;
-	export let networkId: NetworkId;
 
-	const { sourceToken, sourceTokenExchangeRate } = getContext<ConvertContext>(CONVERT_CONTEXT_KEY);
+	const { sourceToken, sourceTokenExchangeRate, destinationToken } =
+		getContext<ConvertContext>(CONVERT_CONTEXT_KEY);
 </script>
 
 <ConvertReview on:icConvert on:icBack {sendAmount} {receiveAmount}>
 	<IcTokenFees
 		sourceToken={$sourceToken}
 		sourceTokenExchangeRate={$sourceTokenExchangeRate}
-		{networkId}
+		networkId={$destinationToken.network.id}
 		slot="fee"
 	/>
 
