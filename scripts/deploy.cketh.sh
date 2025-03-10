26433a8e7b876261304b74a860280cd9de64a2fa
Max <bitdivine@users.noreply.github.com>
2025-03-10 18:45:59 +0100

    refactor(devops): Remove unnecessary arguments (#5133)
    # Motivation
    The arguments to `dfx deploy cketh_ledger` match the defaults so there
    is no point in providing them.
    
    # Changes
    - Remove `cketh_ledger` arguments.
    
    # Tests
    ### Arguments match the defaults
    See the comments in the PR.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

--- scripts/deploy.cketh.sh
+++ scripts/deploy.cketh.sh
@@ -30,28 +30,7 @@ dfx deploy cketh_minter --specified-id jzenf-aiaaa-aaaar-qaa7q-cai --network "$D
 })"
 
 echo "Step 3: deploy ledger canister..."
-PRINCIPAL="$(dfx identity get-principal)"
-dfx deploy cketh_ledger --specified-id apia6-jaaaa-aaaar-qabma-cai --network "$DFX_NETWORK" --argument "(variant {
-  Init = record {
-     token_symbol = \"ckSepoliaETH\";
-     token_name = \"Chain key local Sepolia Ethereum\";
-     decimals = opt 18;
-     max_memo_length = opt 80;
-     minting_account = record { owner = principal \"$MINTERID\" };
-     transfer_fee = 9_500;
-     metadata = vec {};
-     initial_balances = vec { record { record { owner = principal \"$PRINCIPAL\"; }; 100_000_000_000_000_000_000; }; };
-     archive_options = record {
-         num_blocks_to_archive = 10_000;
-         trigger_threshold = 20_000;
-         controller_id = principal \"$PRINCIPAL\";
-         cycles_for_archive_creation = opt 1_000_000_000_000;
-         max_message_size_bytes = null;
-         node_max_memory_size_bytes = opt 3_221_225_472;
-     };
-     feature_flags  = opt record { icrc2 = true };
- }
-})"
+dfx deploy cketh_ledger --network "$DFX_NETWORK"
 
 echo "Step 4: deploy index canister..."
 dfx deploy cketh_index --specified-id sh5u2-cqaaa-aaaar-qacna-cai --network "$DFX_NETWORK" --argument "(opt variant {
