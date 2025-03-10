b2d8fa360799d2af602b5d50c58e1b0a85e691d3
daviddecentage <david.farina@dfinity.org>
2025-03-10 09:44:29 +0100

    fix import

--- src/frontend/src/lib/components/airdrops/AirdropEarnings.svelte
+++ src/frontend/src/lib/components/airdrops/AirdropEarnings.svelte
@@ -7,15 +7,13 @@
 	import { BTC_MAINNET_TOKEN } from '$env/tokens/tokens.btc.env';
 	import { ICP_TOKEN } from '$env/tokens/tokens.icp.env';
 	import type { IcToken } from '$icp/types/ic-token';
-	import { getUserRewardsTokenAmounts } from '$lib/api/reward.api';
+	import { getUserRewardsTokenAmounts } from '$lib/services/reward-code.services';
 	import AirdropEarningsCard from '$lib/components/airdrops/AirdropEarningsCard.svelte';
 	import Button from '$lib/components/ui/Button.svelte';
 	import Hr from '$lib/components/ui/Hr.svelte';
 	import { ZERO } from '$lib/constants/app.constants';
 	import { AppPath } from '$lib/constants/routes.constants';
 	import { SLIDE_DURATION } from '$lib/constants/transition.constants';
-	import { authIdentity } from '$lib/derived/auth.derived';
-	import { exchanges } from '$lib/derived/exchange.derived';
 	import { networkId } from '$lib/derived/network.derived';
 	import { tokens } from '$lib/derived/tokens.derived';
 	import { i18n } from '$lib/stores/i18n.store';
