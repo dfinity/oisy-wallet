cde7c6bb5e86a9323106c13c37f1873ef02baa78
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-11 23:46:08 +0100

    revert

--- e2e/enable-testnets.spec.ts
+++ e2e/enable-testnets.spec.ts
@@ -1,12 +1,6 @@
 import { testWithII } from '@dfinity/internet-identity-playwright';
 import { TestnetCases, TestnetsPage } from './utils/pages/testnets.page';
 
-testWithII.beforeEach(async ({ page }) => {
-	await page.addStyleTag({
-		content: '* { transition: none !important; animation: none !important; }'
-	});
-});
-
 TestnetCases.forEach(({ networkSymbol, tokenSymbol }) => {
 	testWithII(`should enable ${networkSymbol} network`, async ({ page, iiPage }) => {
 		const testnetsPage = new TestnetsPage({ page, iiPage });
