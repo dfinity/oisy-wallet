232aacf4eaf2b0998878c185221934ece09cd91e
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-11 18:02:31 +0100

    feat(frontend): remove the warning for non-ATA destination address (#5175)
    # Motivation
    
    The PRODSEC review suggested that we warn/inform the user when the
    destination input for Solana is not an ATA address, since we will derive
    it in our code.
    
    This information can happen either in the form or in the confirmation
    review, or both, or in whichever form it is acceptable to have the user
    aware.
    
    However, we are going to revisit how to show this information further
    on. For the current release we temporarily remove it.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/sol/components/send/SolSendDestination.svelte
+++ src/frontend/src/sol/components/send/SolSendDestination.svelte
@@ -1,40 +1,16 @@
 <script lang="ts">
-	import { assertNonNullish, debounce, notEmptyString } from '@dfinity/utils';
-	import { createEventDispatcher, getContext } from 'svelte';
-	import { slide } from 'svelte/transition';
+	import { createEventDispatcher } from 'svelte';
 	import SendInputDestination from '$lib/components/send/SendInputDestination.svelte';
-	import { SLIDE_DURATION } from '$lib/constants/transition.constants';
 	import { i18n } from '$lib/stores/i18n.store';
-	import { SEND_CONTEXT_KEY, type SendContext } from '$lib/stores/send.store';
-	import type { SolanaNetworkType } from '$sol/types/network';
-	import { mapNetworkIdToNetwork } from '$sol/utils/network.utils';
-	import { isAtaAddress } from '$sol/utils/sol-address.utils';
 	import { isInvalidDestinationSol } from '$sol/utils/sol-send.utils';
 
 	export let destination = '';
 	export let invalidDestination = false;
 
-	const { sendTokenNetworkId } = getContext<SendContext>(SEND_CONTEXT_KEY);
-
 	const dispatch = createEventDispatcher();
 
 	let isInvalidDestination: () => boolean;
 	$: isInvalidDestination = (): boolean => isInvalidDestinationSol(destination);
-
-	let network: SolanaNetworkType | undefined;
-	$: network = mapNetworkIdToNetwork($sendTokenNetworkId);
-
-	let isAtaDestination = true;
-
-	const updateIsAtaDestination = async () => {
-		assertNonNullish(network, 'No Solana network provided to start Solana wallet worker.');
-
-		isAtaDestination = await isAtaAddress({ address: destination, network });
-	};
-
-	const debounceUpdateIsAtaDestination = debounce(updateIsAtaDestination);
-
-	$: destination, debounceUpdateIsAtaDestination();
 </script>
 
 <SendInputDestination
@@ -46,8 +22,4 @@
 	onQRButtonClick={() => dispatch('icQRCodeScan')}
 />
 
-{#if notEmptyString(destination) && !invalidDestination && !isAtaDestination}
-	<p transition:slide={SLIDE_DURATION} class="pb-3 text-warning-primary">
-		{$i18n.send.info.ata_will_be_calculated}
-	</p>
-{/if}
+<!--TODO: PRODSEC: add some sort of warning/info when the destination input is not an ATA address, either here or in the confirmation review step-->
