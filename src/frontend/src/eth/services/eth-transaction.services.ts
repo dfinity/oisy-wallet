c197c3c00a18c2a1d1a8e535b99ca37304c18b91
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-12 22:25:16 +0100

    refactor

--- src/frontend/src/eth/services/eth-transaction.services.ts
+++ src/frontend/src/eth/services/eth-transaction.services.ts
@@ -44,9 +44,7 @@ export const processErc20Transaction = async ({
 	type: 'pending' | 'mined';
 }) => {
 	if (type === 'mined') {
-		// await processMinedTransaction({ ...rest });
-		// Reload transactions as a transaction has been mined
-		await loadEthereumTransactions({ tokenId: rest.token.id, networkId: rest.token.network.id });
+		await processMinedTransaction({ ...rest });
 		return;
 	}
 
@@ -62,7 +60,12 @@ const processPendingTransaction = async ({
 	token: Token;
 	value?: BigNumber;
 }) => {
-	const { getTransaction } = alchemyProviders(token.network.id);
+	const {
+		id: tokenId,
+		network: { id: networkId }
+	} = token;
+
+	const { getTransaction } = alchemyProviders(networkId);
 	const transaction = await getTransaction(hash);
 
 	if (isNullish(transaction)) {
@@ -83,7 +86,7 @@ const processPendingTransaction = async ({
 	}
 
 	ethTransactionsStore.add({
-		tokenId: token.id,
+		tokenId,
 		transactions: [
 			{
 				...transaction,
@@ -93,62 +96,21 @@ const processPendingTransaction = async ({
 		]
 	});
 
-	const { wait, hash: transactionHash } = transaction;
+	const { wait } = transaction;
 
 	await wait();
 
-	// Reload transactions as a transaction has been mined
-	await loadEthereumTransactions({ tokenId: token.id, networkId: token.network.id });
-
-	// await processMinedTransaction({ hash: transactionHash, token, value });
+	await processMinedTransaction({ token });
 };
 
-const processMinedTransaction = async ({
-	hash,
-	token,
-	value
-}: {
-	hash: string;
-	token: Token;
-	value?: BigNumber;
-}) => {
-	const { getTransaction } = alchemyProviders(token.network.id);
-	const minedTransaction = await getTransaction(hash);
-
-	if (isNullish(minedTransaction)) {
-		const {
-			transaction: {
-				error: { failed_get_mined_transaction }
-			}
-		} = get(i18n);
-
-		toastsError({
-			msg: {
-				text: replacePlaceholders(failed_get_mined_transaction, {
-					$hash: hash
-				})
-			}
-		});
-		return;
-	}
-
-	// We noticed that the timestamp was not provided when retrieving the transaction at this stage, i.e., after transaction.wait().
-	// Therefore, because the transaction has just been mined and as the UI displays a transaction date in the list of transactions, we display now timestamp if undefined.
-	// This is for simplicity reasons and because it allows us to avoid making an additional call to getTransaction.
-	const { timestamp, ...rest } = minedTransaction;
-
-	ethTransactionsStore.update({
-		tokenId: token.id,
-		transaction: {
-			...rest,
-			timestamp,
-			displayTimestamp: timestamp ?? Date.now() / 1000,
-			...(nonNullish(value) && { value })
-		}
-	});
+const processMinedTransaction = async ({ token }: { token: Token }) => {
+	const {
+		id: tokenId,
+		network: { id: networkId }
+	} = token;
 
 	// Reload transactions as a transaction has been mined
-	await loadEthereumTransactions({ tokenId: token.id, networkId: token.network.id });
+	await loadEthereumTransactions({ tokenId, networkId });
 
 	// Reload balance as a transaction has been mined
 	await reloadEthereumBalance(token);
