a285bfbd0cf41a401e75985bc597a72b58041103
Max <bitdivine@users.noreply.github.com>
2025-03-12 08:42:23 +0100

    refactor(devops): dfx deploy cketh_index (#5177)
    # Motivation
    `dfx deploy X` should work for all canisters, with the default settings.
    This PR implements this for `cketh_index`.
    
    # Changes
    - Create `cketh_index` build commands and configure `dfx` to call them.
    
    # Tests
    ## The `cketh_index` canister ID should be unchanged
    The canister is created with the correct ID by default:
    ```
    $ dfx start --clean --background && dfx canister create cketh_index && dfx canister id cketh_index
    Running dfx start for version 0.23.0
    Using project-specific network 'local' defined in /home/max/dfn/oisy-wallet/branches/dfx-deploy/dfx.json
    Initialized replica.
    Initialized HTTP gateway.
    Replica API running on 127.0.0.1:4943
    Creating canister cketh_index...
    cketh_index canister created with canister id: sh5u2-cqaaa-aaaar-qacna-cai
    sh5u2-cqaaa-aaaar-qacna-cai
    ```
    
    ## The local init arg file should be unchanged.
    The correct arguments are created by default:
    ```
    $ dfx build cketh_index
    ...
    Executing 'scripts/build.cketh_index.sh'
    $ cat target/ic/cketh_index.args.did 
    (opt variant {
      Init = record {
        ledger_id = principal "apia6-jaaaa-aaaar-qabma-cai";
       }
    })
    ```
    The principal above should be the ledger:
    ```
    $ dfx canister id cketh_ledger
    apia6-jaaaa-aaaar-qabma-cai
    ```
    
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

---
+++ scripts/build.cketh_index.args.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+PRINCIPAL="$(dfx identity get-principal)"
+ARG_FILE="$(jq -re .canisters.cketh_index.init_arg_file dfx.json)"
+
+mkdir -p "$(dirname "$ARG_FILE")"
+
+cat <<-EOF >"$ARG_FILE"
+(opt variant {
+  Init = record {
+    ledger_id = principal "$CANISTER_ID_CKETH_LEDGER";
+   }
+})
+EOF
--- scripts/deploy.cketh.sh
+++ scripts/deploy.cketh.sh
@@ -6,9 +6,6 @@ echo "Step 1: create canisters..."
 dfx canister create cketh_ledger --network "$DFX_NETWORK"
 dfx canister create cketh_minter --network "$DFX_NETWORK"
 
-LEDGERID="$(dfx canister id cketh_ledger --network "$DFX_NETWORK")"
-echo "$LEDGERID"
-
 echo "Step 2: deploy minter canister..."
 
 # ckETH minter deployed on using the smart contract address on Sepolia used by testnet.
@@ -20,11 +17,7 @@ echo "Step 3: deploy ledger canister..."
 dfx deploy cketh_ledger --network "$DFX_NETWORK"
 
 echo "Step 4: deploy index canister..."
-dfx deploy cketh_index --specified-id sh5u2-cqaaa-aaaar-qacna-cai --network "$DFX_NETWORK" --argument "(opt variant {
-  Init = record {
-    ledger_id = principal \"$LEDGERID\";
-   }
-})"
+dfx deploy cketh_index --network "$DFX_NETWORK"
 
 echo "Step 5: transfer ckETH to principal..."
 dfx canister call cketh_ledger --network "$DFX_NETWORK" icrc1_transfer "(record {from=null; to=record { owner= principal \"x4w27-so7wg-cudsa-yy7fh-wcpy5-njul4-q54tv-euzzi-tdnzz-ill46-zqe\";}; amount=500_000_000_000_000_000; fee=null; memo=null; created_at_time=null;})"
