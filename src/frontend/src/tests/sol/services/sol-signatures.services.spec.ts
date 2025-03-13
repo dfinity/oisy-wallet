c192980a45b3a023b7f295b4699fb3992f195609
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-13 16:21:26 +0100

    feat(frontend): Solana service to fetch all signatures for address (#5150)
    # Motivation
    
    We are fetching and parsing the Solana signatures for each Solana token
    (native or SPL). That is quite inefficient, since the same signature may
    be fetched twice and consequently parsed twice. It is not an issue,
    since we parse the instructions inside and returns transactions that are
    unique per token. So no duplicates in the transactions that are
    returned.
    
    However we do an unnecessary amount of calls to the Solana RPC for
    unnecessary mapping of the same signatures.
    
    To avoid this, we would ideally fetch all the signature for a specific
    user first, remove the duplicates, and then parse each one.
    
    So, as first step, we create a service that fetches all the transaction
    of a specific user: we provide a list of tokens too, so that we fetch
    the signatures for the respective ATA addresses. Furthermore, the
    function will filter all duplicates signatures.
    
    # Changes
    
    - Add service `getSolSignatures`, as described above.
    
    # Tests
    
    Added tests for the new service.

--- src/frontend/src/tests/sol/services/sol-signatures.services.spec.ts
+++ src/frontend/src/tests/sol/services/sol-signatures.services.spec.ts
@@ -1,20 +1,30 @@
+import { BONK_TOKEN } from '$env/tokens/tokens-spl/tokens.bonk.env';
+import { USDC_TOKEN } from '$env/tokens/tokens-spl/tokens.usdc.env';
 import { SOLANA_TOKEN_ID } from '$env/tokens/tokens.sol.env';
+import { WALLET_PAGINATION } from '$lib/constants/app.constants';
+import type { SolAddress } from '$lib/types/address';
 import * as solanaApi from '$sol/api/solana.api';
 import { TOKEN_PROGRAM_ADDRESS } from '$sol/constants/sol.constants';
-import { getSolTransactions } from '$sol/services/sol-signatures.services';
+import { getSolSignatures, getSolTransactions } from '$sol/services/sol-signatures.services';
 import * as solTransactionsServices from '$sol/services/sol-transactions.services';
 import { solTransactionsStore } from '$sol/stores/sol-transactions.store';
 import { SolanaNetworks } from '$sol/types/network';
 import type { SolSignature, SolTransactionUi } from '$sol/types/sol-transaction';
+import type { RequiredSplToken, SplTokenAddress } from '$sol/types/spl';
 import {
 	mockSolSignature,
 	mockSolSignatureResponse,
 	mockSolSignatureResponses
 } from '$tests/mocks/sol-signatures.mock';
 import { createMockSolTransactionsUi } from '$tests/mocks/sol-transactions.mock';
-import { mockSolAddress, mockSplAddress } from '$tests/mocks/sol.mock';
+import {
+	mockAtaAddress,
+	mockAtaAddress2,
+	mockSolAddress,
+	mockSplAddress
+} from '$tests/mocks/sol.mock';
 import * as solProgramToken from '@solana-program/token';
-import { address } from '@solana/web3.js';
+import { address, type Address } from '@solana/web3.js';
 import { type MockInstance } from 'vitest';
 
 vi.mock('@solana-program/token', () => ({
@@ -28,6 +38,179 @@ describe('sol-transactions.services', () => {
 		vi.restoreAllMocks();
 
 		solTransactionsStore.reset(SOLANA_TOKEN_ID);
+	});
+
+	describe('getSolSignatures', () => {
+		let spyFetchSignatures: MockInstance;
+		let spyFindAssociatedTokenPda: MockInstance;
+
+		const mockNetwork = SolanaNetworks.mainnet;
+		const mockTokensList: RequiredSplToken[] = [BONK_TOKEN, USDC_TOKEN];
+		const mockAtaAddresses: Record<SplTokenAddress, SolAddress> = {
+			[BONK_TOKEN.address]: address(mockAtaAddress),
+			[USDC_TOKEN.address]: address(mockAtaAddress2)
+		};
+		const mockParams = {
+			address: mockSolAddress,
+			network: mockNetwork,
+			tokensList: mockTokensList
+		};
+
+		const mockError = new Error('Mock Error');
+
+		const mockSignaturesSol: SolSignature[] = mockSolSignatureResponses(7);
+		const mockSignaturesAta1: SolSignature[] = mockSolSignatureResponses(3);
+		const mockSignaturesAta2: SolSignature[] = mockSolSignatureResponses(5);
+		const mockSignatures: SolSignature[] = [
+			...mockSignaturesSol,
+			...mockSignaturesAta1,
+			...mockSignaturesAta2
+		];
+
+		beforeEach(() => {
+			spyFetchSignatures = vi.spyOn(solanaApi, 'fetchSignatures');
+			spyFetchSignatures.mockImplementation(({ wallet }: { wallet: Address }) =>
+				wallet.toString() === mockAtaAddress
+					? mockSignaturesAta1
+					: wallet.toString() === mockAtaAddress2
+						? mockSignaturesAta2
+						: mockSignaturesSol
+			);
+
+			spyFindAssociatedTokenPda = vi.spyOn(solProgramToken, 'findAssociatedTokenPda');
+			spyFindAssociatedTokenPda.mockImplementation(({ mint }: { mint: Address }) => [
+				mockAtaAddresses[mint.toString()]
+			]);
+		});
+
+		it('should fetch all signatures successfully', async () => {
+			const signatures = await getSolSignatures(mockParams);
+
+			expect(signatures).toEqual(mockSignatures);
+
+			expect(spyFetchSignatures).toHaveBeenCalledTimes(1 + mockTokensList.length);
+			expect(spyFetchSignatures).toHaveBeenNthCalledWith(1, {
+				network: mockNetwork,
+				wallet: mockSolAddress,
+				limit: Number(WALLET_PAGINATION)
+			});
+			mockTokensList.forEach(({ address }, index) => {
+				expect(spyFetchSignatures).toHaveBeenNthCalledWith(index + 2, {
+					network: mockNetwork,
+					wallet: mockAtaAddresses[address],
+					limit: Number(WALLET_PAGINATION)
+				});
+			});
+		});
+
+		it('should remove duplicates signatures', async () => {
+			spyFetchSignatures.mockImplementation(({ wallet }: { wallet: Address }) =>
+				wallet.toString() === mockAtaAddress
+					? mockSignaturesAta1
+					: wallet.toString() === mockAtaAddress2
+						? mockSignaturesSol
+						: mockSignaturesSol
+			);
+
+			const signatures = await getSolSignatures(mockParams);
+
+			expect(signatures).toEqual([...mockSignaturesSol, ...mockSignaturesAta1]);
+		});
+
+		it('should handle no token list', async () => {
+			const { tokensList: _, ...params } = mockParams;
+
+			const signatures = await getSolSignatures(params);
+
+			expect(signatures).toEqual(mockSignaturesSol);
+
+			expect(spyFetchSignatures).toHaveBeenCalledOnce();
+			expect(spyFetchSignatures).toHaveBeenNthCalledWith(1, {
+				network: mockNetwork,
+				wallet: mockSolAddress,
+				limit: Number(WALLET_PAGINATION)
+			});
+		});
+
+		it('should handle an empty token list', async () => {
+			const signatures = await getSolSignatures({
+				...mockParams,
+				tokensList: []
+			});
+
+			expect(signatures).toEqual(mockSignaturesSol);
+
+			expect(spyFetchSignatures).toHaveBeenCalledOnce();
+			expect(spyFetchSignatures).toHaveBeenNthCalledWith(1, {
+				network: mockNetwork,
+				wallet: mockSolAddress,
+				limit: Number(WALLET_PAGINATION)
+			});
+		});
+
+		it('should handle empty signatures for a token', async () => {
+			spyFetchSignatures.mockImplementation(({ wallet }: { wallet: Address }) =>
+				wallet.toString() === mockAtaAddress
+					? mockSignaturesAta1
+					: wallet.toString() === mockAtaAddress2
+						? []
+						: mockSignaturesSol
+			);
+
+			const signatures = await getSolSignatures(mockParams);
+
+			expect(signatures).toEqual([...mockSignaturesSol, ...mockSignaturesAta1]);
+		});
+
+		it('should handle empty signatures for the native token', async () => {
+			spyFetchSignatures.mockImplementation(({ wallet }: { wallet: Address }) =>
+				wallet.toString() === mockAtaAddress
+					? mockSignaturesAta1
+					: wallet.toString() === mockAtaAddress2
+						? mockSignaturesAta2
+						: []
+			);
+
+			const signatures = await getSolSignatures(mockParams);
+
+			expect(signatures).toEqual([...mockSignaturesAta1, ...mockSignaturesAta2]);
+		});
+
+		it('should handle before parameter', async () => {
+			const signature = mockSolSignature();
+
+			await getSolSignatures({ ...mockParams, before: signature });
+
+			expect(spyFetchSignatures).toHaveBeenCalledWith(
+				expect.objectContaining({
+					before: signature
+				})
+			);
+		});
+
+		it('should handle limit parameter', async () => {
+			await getSolSignatures({ ...mockParams, limit: 5 });
+
+			expect(spyFetchSignatures).toHaveBeenCalledWith(
+				expect.objectContaining({
+					limit: 5
+				})
+			);
+		});
+
+		it('should handle empty signatures response', async () => {
+			spyFetchSignatures.mockReturnValue([]);
+
+			const transactions = await getSolSignatures(mockParams);
+
+			expect(transactions).toEqual([]);
+		});
+
+		it('should handle RPC errors gracefully', async () => {
+			spyFetchSignatures.mockRejectedValue(mockError);
+
+			await expect(getSolSignatures(mockParams)).rejects.toThrow(mockError);
+		});
 	});
 
 	describe('getSolTransactions', () => {
