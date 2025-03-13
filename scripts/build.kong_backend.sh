9239e51d1dba1da935eb236fa45b6ccfc3d4399e
Max <bitdivine@users.noreply.github.com>
2025-03-13 10:53:38 +0100

    refactor(devops): Address shellcehck lints (#5203)
    # Motivation
    There are shellcheck lint warnings.
    
    # Changes
    - Address the shellcheck warnings
    
    # Tests
    See CI
    
    Note: The changes need careful review to ensure that they change
    nothing.

--- scripts/build.kong_backend.sh
+++ scripts/build.kong_backend.sh
@@ -20,7 +20,9 @@ KONG_BUILDENV="$DFX_NETWORK"
 export KONG_BUILDENV
 
 KONG_REPO_URL="https://raw.githubusercontent.com/KongSwap/kong/refs/heads/main/canisters"
+# shellcheck disable=SC2034 # This variable is used - see ${!asset_url} below.
 CANDID_URL="${KONG_REPO_URL}/kong_backend.did"
+# shellcheck disable=SC2034 # This variable is used - see ${!asset_url} below.
 WASM_URL="${KONG_REPO_URL}/kong_backend.wasm.gz"
 
 CANDID_FILE="$(jq -r .canisters.kong_backend.candid dfx.json)"
@@ -28,6 +30,7 @@ WASM_FILE="$(jq -r .canisters.kong_backend.wasm dfx.json)"
 
 download() {
   : 'Downloads a URL to a given file.'
+  # shellcheck disable=SC2016 # The $ in the comment is not meant to be expanded.
   : '* With argument x, the filename is $X_FILE and the URL is $X_URL'
   : '* If the file already exists, the user is prompted whether to overwrite, keeping the existing file by default.'
   local asset asset_url asset_file response
@@ -38,7 +41,7 @@ download() {
   if test -e "${!asset_file}" && read -r -p "Overwrite existing ${!asset_file}? [y/N] " response && [[ "${response,,}" != y* ]]; then
     echo "Using existing kong $asset file."
   else
-    echo Downloading ${!asset_url} "-->" ${!asset_file}
+    echo "Downloading ${!asset_url} --> ${!asset_file}"
     mkdir -p "$(dirname "${!asset_file}")"
     curl -sSL "${!asset_url}" >"${!asset_file}"
   fi
