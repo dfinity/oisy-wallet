a439591382db93357149ba9ecb1d7c924eb6b269
Nico Mächler <135843237+inc-man@users.noreply.github.com>
2025-03-16 23:01:45 +0100

    fix(e2e): change freeze carousel method (#5223)
    # Motivation
    
    The freeze carousel function seems to be flaky and should be fixed.
    
    # Changes
    
    - Changed the way the carousel freezes on the first slide.
    - Freeze the `clock` before taking a snapshot when freezing the
    carousel.
    
    # Tests
    
    CI runs on this PR
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>
    Co-authored-by: daviddecentage <david.farina@dfinity.org>

--- e2e/utils/components/promotion-carousel.component.ts
+++ e2e/utils/components/promotion-carousel.component.ts
@@ -1,4 +1,4 @@
-import { CAROUSEL_SLIDE, CAROUSEL_SLIDE_NAVIGATION } from '$lib/constants/test-ids.constants';
+import { CAROUSEL_SLIDE_NAVIGATION } from '$lib/constants/test-ids.constants';
 import type { Page } from '@playwright/test';
 
 export class PromotionCarousel {
@@ -8,27 +8,20 @@ export class PromotionCarousel {
 		this.#page = page;
 	}
 
-	public async navigateToSlide(slideNumber: number): Promise<void> {
-		const navigation1Selector = `[data-tid="${CAROUSEL_SLIDE_NAVIGATION}${slideNumber}"]:visible`;
-		await this.#page.click(navigation1Selector);
-	}
-
-	public async freezeCarousel(): Promise<void> {
-		// Freeze the carousel by applying the first slide's transform style to all visible slides
-		await this.#page.$$eval(`div[data-tid="${CAROUSEL_SLIDE}"]:visible`, (elements) => {
-			elements.forEach((el) => {
-				const slideStyle = el.style.transform;
-				el.style.transform = slideStyle;
-			});
-		});
-		// Freeze the carousel navigation indicators by fixing their current width and background color styles
-		await this.#page.$$eval(`[data-tid^="${CAROUSEL_SLIDE_NAVIGATION}"]:visible`, (elements) => {
-			elements.forEach((el) => {
-				const indicatorWidth = el.style.width;
-				const indicatorBackgroundColor = el.style.backgroundColor;
-				el.style.width = indicatorWidth;
-				el.style.backgroundColor = indicatorBackgroundColor;
-			});
+	public async freezeCarouselToSlide(slideNumber: number): Promise<void> {
+		const navigationSelector1 = `[data-tid="${CAROUSEL_SLIDE_NAVIGATION}${slideNumber}"]:visible`;
+		await this.#page.click(navigationSelector1);
+		await this.#page.evaluate(() => {
+			const slide = document.querySelector(`div[data-tid="carousel-slide"]`);
+			if (slide) {
+				slide.setAttribute(
+					'style',
+					'width: 1584px; ' +
+						'transform: translate3d(-264px, 0px, 0px) !important; ' +
+						'transition: none !important; ' +
+						'animation: none !important;'
+				);
+			}
 		});
 	}
 }
