11459a68855dc52cd606237aff1117538a88d244
inc-man <nicolas.maechler@dfinity.org>
2025-03-17 13:00:55 +0100

    change viewportAdjuster

--- e2e/utils/pages/homepage.page.ts
+++ e2e/utils/pages/homepage.page.ts
@@ -369,10 +369,10 @@ abstract class Homepage {
 			Math.max(
 				document.body.scrollHeight,
 				document.documentElement.scrollHeight,
-				document.body.offsetHeight,
-				document.documentElement.offsetHeight,
-				document.body.clientHeight,
-				document.documentElement.clientHeight
+				//document.body.offsetHeight,
+				//document.documentElement.offsetHeight,
+				//document.body.clientHeight,
+				//document.documentElement.clientHeight
 			)
 		);
 
