57903f10b3ac146a972d3cdd1641f9ffcfd3d9c5
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-18 21:12:21 +0100

    refactor

--- src/frontend/src/lib/derived/user-profile.derived.ts
+++ src/frontend/src/lib/derived/user-profile.derived.ts
@@ -1,7 +1,7 @@
 import type { Settings, UserProfile } from '$declarations/backend/backend.did';
 import { userProfileStore } from '$lib/stores/user-profile.store';
-import type { Option } from '$lib/types/utils';
-import { fromNullable, nonNullish } from '@dfinity/utils';
+import { nonNullish } from '@dfinity/utils';
+import { fromNullishNullable } from '@dfinity/utils/dist/types/utils/did.utils';
 import { derived, type Readable } from 'svelte/store';
 
 export const userProfileLoaded: Readable<boolean> = derived([userProfileStore], ([$userProfile]) =>
@@ -10,14 +10,15 @@ export const userProfileLoaded: Readable<boolean> = derived([userProfileStore], 
 
 export const userProfile: Readable<UserProfile | undefined> = derived(
 	[userProfileStore],
-	([$userProfile]) => (nonNullish($userProfile) ? $userProfile.profile : undefined)
+	([$userProfile]) => $userProfile?.profile
 );
 
 export const userProfileVersion: Readable<bigint | undefined> = derived(
 	[userProfile],
-	([$userProfile]) => (nonNullish($userProfile) ? fromNullable($userProfile.version) : undefined)
+	([$userProfile]) => fromNullishNullable($userProfile?.version)
 );
 
-export const userSettings: Readable<Option<Settings>> = derived([userProfile], ([$userProfile]) =>
-	nonNullish($userProfile) ? fromNullable($userProfile.settings) : undefined
+export const userSettings: Readable<Settings | undefined> = derived(
+	[userProfile],
+	([$userProfile]) => fromNullishNullable($userProfile?.settings)
 );
