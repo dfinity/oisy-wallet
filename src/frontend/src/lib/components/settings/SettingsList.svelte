2d929a2e19f8ae640881e3323cfa5c3fe6714be1
daviddecentage <david.farina@dfinity.org>
2025-03-18 13:35:32 +0100

    new components

---
+++ src/frontend/src/lib/components/settings/SettingsList.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	const SLOTS = $$props.$$slots;
+</script>
+
+{#if SLOTS.title}
+	<div class="flex w-full flex-row justify-between">
+		<h5 class="mb-4 flex"><slot name="title" /></h5>
+		<div><slot name="title-action" /></div>
+	</div>
+{/if}
+
+<div class="mb-8 flex flex-col px-1">
+	<slot />
+</div>
--- src/frontend/src/lib/components/settings/SettingsModalEnabledNetworks.svelte
+++ src/frontend/src/lib/components/settings/SettingsModalEnabledNetworks.svelte
@@ -12,6 +12,9 @@
 	import ContentWithToolbar from '$lib/components/ui/ContentWithToolbar.svelte';
 	import { modalStore } from '$lib/stores/modal.store';
 	import { userSettings } from '$lib/derived/user-profile.derived';
+	import SettingsList from '$lib/components/settings/SettingsList.svelte';
+	import SettingsListItem from '$lib/components/settings/SettingsListItem.svelte';
+	import Badge from '$lib/components/ui/Badge.svelte';
 
 	let testnetsEnabledChecked: boolean;
 	$: testnetsEnabledChecked = $userSettings?.settings?.testnetsEnabled ?? false;
@@ -35,64 +38,64 @@
 
 		modalStore.close();
 	};
+
+	let selected: number;
+	$: selected = 0;
 </script>
 
 <div class="p-6">
-	<div class="flex w-full flex-row justify-between">
-		<h5 class="mb-4 flex">Mainnets</h5>
-		<div class="font-bold"
-			><Checkbox
-				text="inline"
-				inputId="smth"
-				checked={testnetsEnabledChecked}
-				on:nnsChange={toggleTestnets}
-			>
-				Enable Testnets
-			</Checkbox>
-		</div>
-	</div>
+	<SettingsList>
+		<svelte:fragment slot="title">Mainnets</svelte:fragment>
+		<svelte:fragment slot="title-action"
+			><div class="font-bold"
+				><Checkbox
+					text="inline"
+					inputId="smth"
+					checked={testnetsEnabledChecked}
+					on:nnsChange={toggleTestnets}
+				>
+					Enable Testnets
+				</Checkbox>
+			</div></svelte:fragment
+		>
 
-	<div class="mb-8 flex flex-col gap-1 px-1">
 		{#each $networksMainnets as network}
-			<div class="border-b-1 flex w-full border-brand-subtle-20 py-1">
-				<KeyValuePair>
-					<span slot="key" class="flex w-full flex-row"
+			<SettingsListItem>
+				<svelte:fragment slot="key"
+					><NetworkLogo {network} blackAndWhite size="xxs" />
+					<span class="ml-2 flex">{network.name}</span></svelte:fragment
+				>
+				<svelte:fragment slot="value"
+					><Toggle
+						ariaLabel="Enable/Disable"
+						checked={enabledNetworksData?.[network.id]}
+						on:nnsToggle={() => toggleNetwork(network)}
+					/></svelte:fragment
+				>
+			</SettingsListItem>
+		{/each}
+	</SettingsList>
+
+	{#if $testnetsEnabled}
+		<SettingsList>
+			<svelte:fragment slot="title">Testnets</svelte:fragment>
+
+			{#each $networksTestnets as network}
+				<SettingsListItem>
+					<svelte:fragment slot="key"
 						><NetworkLogo {network} blackAndWhite size="xxs" />
-						<span class="ml-2 flex">{network.name}</span></span
+						<span class="ml-2 flex">{network.name}</span></svelte:fragment
 					>
-					<span slot="value"
+					<svelte:fragment slot="value"
 						><Toggle
 							ariaLabel="Enable/Disable"
 							checked={enabledNetworksData?.[network.id]}
 							on:nnsToggle={() => toggleNetwork(network)}
-						/></span
+						/></svelte:fragment
 					>
-				</KeyValuePair>
-			</div>
-		{/each}
-	</div>
-
-	{#if $testnetsEnabled}
-		<h5 class="mb-4">Testnets</h5>
-		<div class="mb-8 flex flex-col gap-1 px-1">
-			{#each $networksTestnets as network}
-				<div class="border-b-1 flex w-full border-brand-subtle-20 py-1">
-					<KeyValuePair>
-						<span slot="key" class="flex w-full flex-row"
-							><NetworkLogo {network} blackAndWhite size="xxs" />
-							<span class="ml-2 flex">{network.name}</span></span
-						>
-						<span slot="value"
-							><Toggle
-								ariaLabel="Test"
-								checked={enabledNetworksData?.[network.id]}
-								on:nnsToggle={() => toggleNetwork(network)}
-							/></span
-						>
-					</KeyValuePair>
-				</div>
+				</SettingsListItem>
 			{/each}
-		</div>
+		</SettingsList>
 	{/if}
 </div>
 <ContentWithToolbar>
