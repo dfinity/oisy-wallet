c63616e3446bdc80cd3fac5e4421c931ee8dfe7b
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-19 11:57:35 +0100

    feat(frontend): add Sepolia EURC token (#5304)
    # Motivation
    
    We include [Sepolia EURC
    token](https://sepolia.etherscan.io/token/0x08210f9170f89ab7658f0b5e3ff39b0e03c594d4)
    in our list of Sepolia tokens.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/env/tokens/tokens-erc20/tokens.eurc.env.ts
+++ src/frontend/src/env/tokens/tokens-erc20/tokens.eurc.env.ts
@@ -1,4 +1,4 @@
-import { ETHEREUM_NETWORK } from '$env/networks/networks.env';
+import { ETHEREUM_NETWORK, SEPOLIA_NETWORK } from '$env/networks/networks.env';
 import eurc from '$eth/assets/eurc.svg';
 import type { RequiredErc20Token } from '$eth/types/erc20';
 import type { TokenId } from '$lib/types/token';
@@ -22,4 +22,22 @@ export const EURC_TOKEN: RequiredErc20Token = {
 	address: '0x1aBaEA1f7C830bD89Acc67eC4af516284b1bC33c',
 	exchange: 'erc20',
 	twinTokenSymbol: 'ckEURC'
+};
+
+export const SEPOLIA_EURC_SYMBOL = 'SepoliaEURC';
+
+export const SEPOLIA_EURC_TOKEN_ID: TokenId = parseTokenId(SEPOLIA_EURC_SYMBOL);
+
+export const SEPOLIA_EURC_TOKEN: RequiredErc20Token = {
+	id: SEPOLIA_EURC_TOKEN_ID,
+	network: SEPOLIA_NETWORK,
+	standard: 'erc20',
+	category: 'default',
+	name: 'EURC',
+	symbol: EURC_SYMBOL,
+	decimals: EURC_DECIMALS,
+	icon: eurc,
+	address: '0x08210F9170F89Ab7658F0B5E3fF39b0E03C594D4',
+	exchange: 'erc20',
+	twinTokenSymbol: 'ckSepoliaEURC'
 };
