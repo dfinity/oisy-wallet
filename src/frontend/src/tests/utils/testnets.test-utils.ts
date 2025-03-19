7159ebd47fd90dab36d1fe2fb9e7930492cc0536
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-19 13:24:10 +0100

    mock

--- src/frontend/src/tests/utils/testnets.test-utils.ts
+++ src/frontend/src/tests/utils/testnets.test-utils.ts
@@ -1,8 +1,27 @@
-import * as testnetsStore from '$lib/derived/testnets.derived';
-import { readable } from 'svelte/store';
+import { userProfileStore, type UserProfileStoreData } from '$lib/stores/user-profile.store';
+import { mockUserProfile, mockUserSettings } from '$tests/mocks/user-profile.mock';
+import { fromNullable, toNullable } from '@dfinity/utils';
+import { get } from 'svelte/store';
 
 export const setupTestnetsStore = (value: 'enabled' | 'disabled' | 'reset') => {
-	vi.spyOn(testnetsStore, 'testnets', 'get').mockImplementation(() =>
-		readable(value === 'enabled')
-	);
+	const userProfile: UserProfileStoreData = get(userProfileStore) ?? {
+		profile: mockUserProfile,
+		certified: false
+	};
+
+	const settings = fromNullable(userProfile.profile.settings) ?? mockUserSettings;
+
+	userProfileStore.set({
+		...userProfile,
+		profile: {
+			...userProfile.profile,
+			settings: toNullable({
+				...settings,
+				networks: {
+					...settings.networks,
+					testnets: { ...settings.networks.testnets, show_testnets: value === 'enabled' }
+				}
+			})
+		}
+	});
 };
