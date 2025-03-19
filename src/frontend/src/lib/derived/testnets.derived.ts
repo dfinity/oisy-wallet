26c2da36a9961e3dc87d128897401bb8df39e41e
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-19 12:04:25 +0100

    tests

--- src/frontend/src/lib/derived/testnets.derived.ts
+++ src/frontend/src/lib/derived/testnets.derived.ts
@@ -3,5 +3,5 @@ import { derived, type Readable } from 'svelte/store';
 
 export const testnets: Readable<boolean> = derived(
 	[userSettings],
-	([$userSettings]) => $userSettings?.networks?.testnets.show_testnets ?? false
+	([$userSettings]) => $userSettings?.networks.testnets.show_testnets ?? false
 );
