c35713a03e3e2a1ee30ee163cf6288f0e679c8d3
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-20 15:57:14 +0100

    feat(e2e): take mobile screenshot both with and without navigation bar (#5295)
    # Motivation
    
    It is useful that some mobile screenshots are shown both in a normal
    viewport mode (as the users sees it) and as full page (to check all the
    details).
    
    So we create both tests, with and without the navigation bar.
    
    # Changes
    
    - We include a function to scroll the lateral navigation menu up top
    (this is due to a recent change too, that unfortunately MUST enter in
    this PR).
    - We modify the code for the viewport adjuster to verify at intervals if
    the maximum height changed (code by @inc-man )
    - Adapt the `takeScreenshot` method to create the aforementioned tests
    for mobiles.
    
    # Tests
    
    More screenshots were included in this PR since we accumulated a few
    past changes.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- e2e/activity-page.spec.ts
+++ e2e/activity-page.spec.ts
@@ -1,10 +1,10 @@
 import { testWithII } from '@dfinity/internet-identity-playwright';
 import { ActivityPage } from './utils/pages/activity.page';
 
-testWithII('should display activity page', async ({ page, iiPage }) => {
+testWithII('should display activity page', async ({ page, iiPage, isMobile }) => {
 	const activityPage = new ActivityPage({ page, iiPage });
 
 	await activityPage.waitForReady();
 
-	await activityPage.takeScreenshot();
+	await activityPage.takeScreenshot({ isMobile });
 });
--- e2e/send-and-receive-flow.spec.ts
+++ e2e/send-and-receive-flow.spec.ts
@@ -1,11 +1,11 @@
 import { testWithII } from '@dfinity/internet-identity-playwright';
 import { FlowPage } from './utils/pages/send-and-receive-flow.page';
 
-testWithII('should receive and send ICP', async ({ page, iiPage }) => {
+testWithII('should receive and send ICP', async ({ page, iiPage, isMobile }) => {
 	const flowPage = new FlowPage({ page, iiPage });
 
 	await flowPage.waitForReady();
 	await flowPage.receiveTokens();
 	await flowPage.sendTokens();
-	await flowPage.takeScreenshot();
+	await flowPage.takeScreenshot({ isMobile });
 });
