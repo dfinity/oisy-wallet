9827b555427e0548db1b9d5a1d3dea5a30769afd
David Dal Busco <david.dalbusco@outlook.com>
2025-03-24 16:55:04 +0100

    chore: use release gix-cmp

--- update-sveltev5.sh
+++ update-sveltev5.sh
@@ -4,4 +4,4 @@ npm rm @dfinity/gix-components @sveltejs/adapter-static @sveltejs/kit @sveltejs/
 
 npm i @sveltejs/adapter-static @sveltejs/kit @sveltejs/vite-plugin-svelte @testing-library/jest-dom @testing-library/svelte @vitest/coverage-v8 svelte svelte-check vite vite-node vitest vitest-mock-extended -D
 
-npm i dfinity-gix-components-5.1.0.tgz
+npm i @dfinity/gix-components
