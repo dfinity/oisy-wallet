07047253fe2a1540892a24c81c8c4c145d336a44
Max Murphy <max@winning.black>
2025-03-24 16:33:55 +0100

    ++

--- src/shared/src/types.rs
+++ src/shared/src/types.rs
@@ -9,6 +9,7 @@ pub type Timestamp = u64;
 pub mod account;
 pub mod bitcoin;
 pub mod custom_token;
+pub mod dapp;
 pub mod network;
 pub mod number;
 pub mod signer;
@@ -119,50 +120,6 @@ pub trait TokenVersion: Debug {
 
 /// The default maximum length of a token symbol.
 pub const MAX_SYMBOL_LENGTH: usize = 20;
-
-pub mod dapp {
-    use candid::{CandidType, Deserialize};
-
-    use crate::types::Version;
-
-    #[derive(CandidType, Deserialize, Clone, Debug, Eq, PartialEq, Default)]
-    pub struct DappCarouselSettings {
-        pub hidden_dapp_ids: Vec<String>,
-    }
-
-    #[derive(CandidType, Deserialize, Clone, Debug, Eq, PartialEq, Default)]
-    pub struct DappSettings {
-        pub dapp_carousel: DappCarouselSettings,
-    }
-
-    #[derive(CandidType, Deserialize, Clone, Eq, PartialEq, Debug)]
-    pub enum AddDappSettingsError {
-        DappIdTooLong,
-        UserNotFound,
-        VersionMismatch,
-    }
-
-    #[derive(CandidType, Deserialize, Clone, Eq, PartialEq, Debug)]
-    pub struct AddHiddenDappIdRequest {
-        pub dapp_id: String,
-        pub current_user_version: Option<Version>,
-    }
-
-    impl AddHiddenDappIdRequest {
-        /// The maximum supported dApp ID length.
-        pub const MAX_LEN: usize = 32;
-
-        /// Checks whether the request is syntactically valid
-        ///
-        /// # Errors
-        /// - If the dApp ID is too long.
-        pub fn check(&self) -> Result<(), AddDappSettingsError> {
-            (self.dapp_id.len() < Self::MAX_LEN)
-                .then_some(())
-                .ok_or(AddDappSettingsError::DappIdTooLong)
-        }
-    }
-}
 
 pub mod settings {
     use candid::{CandidType, Deserialize};
