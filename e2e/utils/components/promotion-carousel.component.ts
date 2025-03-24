a64a0a38de3212e3d4d786887bb472f343f11820
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-24 09:11:38 +0100

    feat(e2e): completly hide the carousel

--- e2e/utils/components/promotion-carousel.component.ts
+++ e2e/utils/components/promotion-carousel.component.ts
@@ -1,4 +1,4 @@
-import { CAROUSEL_SLIDE_NAVIGATION } from '$lib/constants/test-ids.constants';
+import { CAROUSEL_CONTAINER } from '$lib/constants/test-ids.constants';
 import type { Page } from '@playwright/test';
 
 export class PromotionCarousel {
@@ -9,19 +9,27 @@ export class PromotionCarousel {
 	}
 
 	public async freezeCarouselToSlide(slideNumber: number): Promise<void> {
-		const navigationSelector1 = `[data-tid="${CAROUSEL_SLIDE_NAVIGATION}${slideNumber}"]:visible`;
-		await this.#page.click(navigationSelector1);
-		await this.#page.evaluate(() => {
-			const slide = document.querySelector(`div[data-tid="carousel-slide"]`);
-			if (slide) {
-				slide.setAttribute(
-					'style',
-					// 'width: 1584px; ' +
-					'transform: translate3d(-264px, 0px, 0px) !important; ' +
-						'transition: none !important; ' +
-						'animation: none !important;'
-				);
-			}
-		});
+		// TODO: the carousel is too flaky for the E2E tests, so we need completely hide it and work on freezing it in a permanent state in another PR.
+
+		const selector = `[data-tid="${CAROUSEL_CONTAINER}${slideNumber}"]`;
+		const isVisible = await this.#page.isVisible(selector);
+		if (!isVisible) {
+			await this.#page.locator(selector).evaluate((element) => (element.style.display = 'none'));
+		}
+
+		// const navigationSelector1 = `[data-tid="${CAROUSEL_SLIDE_NAVIGATION}${slideNumber}"]:visible`;
+		// await this.#page.click(navigationSelector1);
+		// await this.#page.evaluate(() => {
+		// 	const slide = document.querySelector(`div[data-tid="carousel-slide"]`);
+		// 	if (slide) {
+		// 		slide.setAttribute(
+		// 			'style',
+		// 			// 'width: 1584px; ' +
+		// 			'transform: translate3d(-264px, 0px, 0px) !important; ' +
+		// 				'transition: none !important; ' +
+		// 				'animation: none !important;'
+		// 		);
+		// 	}
+		// });
 	}
 }
