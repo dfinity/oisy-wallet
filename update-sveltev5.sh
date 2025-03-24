64f6958cbdd7b0d66916b1381d8333c0be74d054
David Dal Busco <david.dalbusco@outlook.com>
2025-03-24 17:07:37 +0100

    chore: remove script

--- update-sveltev5.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-npm rm @dfinity/gix-components @sveltejs/adapter-static @sveltejs/kit @sveltejs/vite-plugin-svelte @testing-library/jest-dom @testing-library/svelte @vitest/coverage-v8 svelte svelte-check vite vite-node vitest vitest-mock-extended prettier-plugin-svelte svelte-confetti
-
-npm i @sveltejs/adapter-static @sveltejs/kit @sveltejs/vite-plugin-svelte @testing-library/jest-dom @testing-library/svelte @vitest/coverage-v8 svelte svelte-check vite vite-node vitest vitest-mock-extended prettier-plugin-svelte -D
-
-npm i @dfinity/gix-components
-
-npm i svelte-confetti
