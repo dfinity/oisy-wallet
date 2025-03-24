d50424dd8b18d9f45120e7ceb97c7ea24dddd0e7
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2025-03-24 20:10:16 +0100

    feat(frontend): implement ModalTokensList component

---
+++ src/frontend/src/lib/components/tokens/ModalTokensListItem.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { nonNullish } from '@dfinity/utils';
+	import ExchangeTokenValue from '$lib/components/exchange/ExchangeTokenValue.svelte';
+	import TokenBalance from '$lib/components/tokens/TokenBalance.svelte';
+	import TokenLogo from '$lib/components/tokens/TokenLogo.svelte';
+	import LogoButton from '$lib/components/ui/LogoButton.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
+	import type { LogoSize } from '$lib/types/components';
+	import type { CardData } from '$lib/types/token-card';
+
+	export let data: CardData;
+	export let logoSize: LogoSize = 'md';
+</script>
+
+<LogoButton on:click dividers={true}>
+	<svelte:fragment slot="title">
+		{nonNullish(data.oisySymbol) ? data.oisySymbol.oisySymbol : data.symbol}
+	</svelte:fragment>
+
+	<svelte:fragment slot="subtitle">
+		{#if nonNullish(data.oisyName?.prefix)}
+			{$i18n.tokens.text.chain_key}
+		{/if}
+
+		{data.oisyName?.oisyName ?? data.name}
+	</svelte:fragment>
+
+	<svelte:fragment slot="description">
+		{data.network.name}
+	</svelte:fragment>
+
+	<TokenLogo
+		{data}
+		slot="logo"
+		color="white"
+		badge={{ type: 'network', blackAndWhite: true }}
+		{logoSize}
+	/>
+
+	<TokenBalance {data} slot="title-end" />
+
+	<ExchangeTokenValue {data} slot="description-end" />
+</LogoButton>
