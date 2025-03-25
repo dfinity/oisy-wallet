e88c206d63283232225050c9ccbcb6701aa28627
daviddecentage <david.farina@dfinity.org>
2025-03-25 21:07:49 +0100

    fix

--- src/frontend/src/lib/components/ui/LogoButton.svelte
+++ src/frontend/src/lib/components/ui/LogoButton.svelte
@@ -29,8 +29,8 @@
 	$: hasActionSlot = nonNullish($$slots['action']);
 </script>
 
-<div class:hover:bg-brand-subtle-10={hover} class:rounded-lg={rounded} data-tid={testId}>
-	<button on:click class="flex w-full border-0 px-2">
+<div class:hover:bg-brand-subtle-10={hover} class:rounded-lg={rounded}>
+	<button on:click class="flex w-full border-0 px-2" data-tid={testId}>
 		<span
 			class="flex w-full flex-row justify-between rounded-none border-l-0 border-r-0 border-t-0 py-3"
 			class:border-brand-subtle-20={dividers}
