3304d3c63cb274f931cdcaf102cea34025c2e3be
daviddecentage <david.farina@dfinity.org>
2025-03-26 15:04:59 +0100

    update

--- src/frontend/src/lib/components/ui/Responsive.svelte
+++ src/frontend/src/lib/components/ui/Responsive.svelte
@@ -1,58 +1,35 @@
 <script lang="ts">
-	import { themeVariables } from '$lib/styles/tailwind/theme-variables';
-
-	type ScreensKeyType = keyof typeof themeVariables.screens;
-
-	const MIN_SCREEN: ScreensKeyType = 'xs';
-	const MAX_SCREEN: ScreensKeyType = '2.5xl';
+	import {
+		type AvailableScreen,
+		filterScreens,
+		getActiveScreen,
+		getAvailableScreens,
+		MAX_SCREEN,
+		MIN_SCREEN,
+		type ScreensKeyType,
+		shouldDisplay
+	} from '$lib/utils/screens.utils';
 
 	export let up: ScreensKeyType = MIN_SCREEN;
 	export let down: ScreensKeyType = MAX_SCREEN;
 
 	$: innerWidth = 0;
 
-	let screens: { screen: ScreensKeyType; width: number }[];
-	$: screens = ((os) => {
-		return Object.entries(os)
-			.filter(([, v]) => typeof v === 'string') // warning is wrong since we have a custom element which is an object
-			.map(([k, v]) => {
-				console.log(v, typeof v);
-				return {
-					screen: k as ScreensKeyType,
-					width: Number((v as string).replaceAll('rem', '')) * 16
-				};
-			})
-			.sort((a, b) => a.width - b.width);
-	})(themeVariables.screens);
-
-	const getActiveScreen = (w: number) => {
-		for (const { width, screen } of screens) {
-			if (w < width) {
-				return screen;
-			}
-		}
-		return MAX_SCREEN;
-	};
+	let screens: AvailableScreen[];
+	$: screens = getAvailableScreens();
 
 	let activeScreen: ScreensKeyType;
-	$: activeScreen = getActiveScreen(innerWidth);
+	$: activeScreen = getActiveScreen({ screenWidth: innerWidth, availableScreens: screens });
 
-	const getBreakpoints = () => {
-		const upIndex = screens.findIndex((screen) => screen.screen === up);
-		const downIndex = screens.findIndex((screen) => screen.screen === down);
-
-		// If either key is invalid, return an empty array
-		if (upIndex === -1 || downIndex === -1) {
-			return [];
-		}
-
-		// Return the slice of screens between `up` and `down` inclusive
-		return screens.slice(upIndex, downIndex + 1).map((screen) => screen.screen);
-	};
+	let display = false;
+	$: display = shouldDisplay({
+		filteredScreens: filterScreens({ availableScreens: screens, up, down }),
+		activeScreen
+	});
 </script>
 
 <svelte:window bind:innerWidth />
 
-{#if getBreakpoints().includes(activeScreen)}
+{#if display}
 	<slot />
 {/if}
---
+++ src/frontend/src/lib/utils/screens.utils.ts
@@ -0,0 +1,65 @@
+import { themeVariables } from '$lib/styles/tailwind/theme-variables';
+
+export type ScreensKeyType = keyof typeof themeVariables.screens;
+export type AvailableScreen = { screen: ScreensKeyType; width: number };
+
+export const MIN_SCREEN: ScreensKeyType = 'xs';
+export const MAX_SCREEN: ScreensKeyType = '2.5xl';
+
+export const getAvailableScreens = () => {
+	return Object.entries(themeVariables.screens)
+		.filter(([, v]) => typeof v === 'string') // warning is wrong since we have a custom element which is an object
+		.map(([k, v]) => {
+			console.log(v, typeof v);
+			return {
+				screen: k as ScreensKeyType,
+				width: Number((v as string).replaceAll('rem', '')) * 16
+			};
+		})
+		.sort((a, b) => a.width - b.width);
+};
+
+export const getActiveScreen = ({
+	screenWidth,
+	availableScreens
+}: {
+	screenWidth: number;
+	availableScreens: AvailableScreen[];
+}) => {
+	for (const { width, screen } of availableScreens) {
+		if (screenWidth < width) {
+			return screen;
+		}
+	}
+	// if nothing matches it must be the largest screen size
+	return MAX_SCREEN;
+};
+
+export const filterScreens = ({
+	availableScreens,
+	up,
+	down
+}: {
+	availableScreens: AvailableScreen[];
+	up: ScreensKeyType;
+	down: ScreensKeyType;
+}) => {
+	const upIndex = availableScreens.findIndex((screen) => screen.screen === up);
+	const downIndex = availableScreens.findIndex((screen) => screen.screen === down);
+
+	// If either key is invalid, return an empty array
+	if (upIndex === -1 || downIndex === -1) {
+		return [];
+	}
+
+	// Return the slice of screens between `up` and `down` inclusive
+	return availableScreens.slice(upIndex, downIndex + 1).map((screen) => screen.screen);
+};
+
+export const shouldDisplay = ({
+	filteredScreens,
+	activeScreen
+}: {
+	filteredScreens: ScreensKeyType[];
+	activeScreen: ScreensKeyType;
+}) => filteredScreens.includes(activeScreen);
