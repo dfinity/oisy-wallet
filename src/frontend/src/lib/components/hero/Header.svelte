22dd03931dcb7150a809137767d4ad0aca5ad62b
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-03-26 15:04:00 +0100

    refactor(frontend): Network switcher relocation (#5413)
    # Motivation
    
    We need to adjust the network switcher slightly in design and position.
    This change has quite a lot of implications regarding layout/responsive
    design which explains the rather many changes.
    
    # Changes
    
    - Moved dropdown to header
    - Removed dropdown from tokens and transacions page
    - Added new viewport size
    - Hide Oisy logo text if screen below new viewport size
    - Adjusted layout slightly to accommodate for new dropdown button on the
    top right
    
    # Tests
    
    
    ![ezgif-5b1807df3e0566](https://github.com/user-attachments/assets/041e6ce5-0609-4e72-b533-406f112d24d0)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

--- src/frontend/src/lib/components/hero/Header.svelte
+++ src/frontend/src/lib/components/hero/Header.svelte
@@ -4,18 +4,20 @@
 	import Menu from '$lib/components/core/Menu.svelte';
 	import OisyWalletLogoLink from '$lib/components/core/OisyWalletLogoLink.svelte';
 	import DocumentationLink from '$lib/components/navigation/DocumentationLink.svelte';
+	import NetworksSwitcher from '$lib/components/networks/NetworksSwitcher.svelte';
 	import ThemeSwitchButton from '$lib/components/ui/ThemeSwitchButton.svelte';
 	import WalletConnect from '$lib/components/wallet-connect/WalletConnect.svelte';
 	import { authNotSignedIn, authSignedIn } from '$lib/derived/auth.derived';
 	import { modalAboutWhyOisy } from '$lib/derived/modal.derived';
 </script>
 
+<!-- todo: revert z-3 back to z-1 when the modal for mobile version dropdowns is using real modals with store etc with responsive component -->
 <header
-	class="z-1 pointer-events-none relative flex w-full max-w-screen-2.5xl items-center justify-between gap-y-5 px-4 pt-6 md:px-8"
-	class:lg:fixed={$authSignedIn}
-	class:lg:top-0={$authSignedIn}
-	class:lg:inset-x-0={$authSignedIn}
-	class:lg:z-10={$authSignedIn}
+	class="z-3 pointer-events-none relative flex w-full max-w-screen-2.5xl items-center justify-between gap-y-5 px-4 pt-6 md:px-8"
+	class:1.5xl:fixed={$authSignedIn}
+	class:1.5xl:top-0={$authSignedIn}
+	class:1.5xl:inset-x-0={$authSignedIn}
+	class:1.5xl:z-10={$authSignedIn}
 	class:pb-10={$authNotSignedIn}
 	class:sm:pb-8={$authNotSignedIn}
 >
@@ -23,8 +25,9 @@
 		<OisyWalletLogoLink />
 	</div>
 
-	<div class="pointer-events-auto flex justify-end gap-5">
+	<div class="pointer-events-auto flex justify-end gap-2 md:gap-5">
 		{#if $authSignedIn}
+			<NetworksSwitcher />
 			<ThemeSwitchButton />
 			<WalletConnect />
 		{/if}
