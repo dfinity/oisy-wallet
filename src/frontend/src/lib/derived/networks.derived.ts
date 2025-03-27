26d2c673cab889bc9fe30af7d94508032155a1fd
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-27 10:12:48 +0100

    feat(frontend): UI network list is linked to user networks

--- src/frontend/src/lib/derived/networks.derived.ts
+++ src/frontend/src/lib/derived/networks.derived.ts
@@ -1,18 +1,29 @@
 import { enabledBitcoinNetworks } from '$btc/derived/networks.derived';
 import { ICP_NETWORK } from '$env/networks/networks.icp.env';
 import { enabledEthereumNetworks } from '$eth/derived/networks.derived';
+import { userNetworks } from '$lib/derived/user-networks.derived';
 import type { Network } from '$lib/types/network';
 import { enabledSolanaNetworks } from '$sol/derived/networks.derived';
 import { derived, type Readable } from 'svelte/store';
 
 export const networks: Readable<Network[]> = derived(
-	[enabledBitcoinNetworks, enabledEthereumNetworks, enabledSolanaNetworks],
-	([$enabledBitcoinNetworks, $enabledEthereumNetworks, $enabledSolanaNetworks]) => [
-		...$enabledBitcoinNetworks,
-		...$enabledEthereumNetworks,
-		ICP_NETWORK,
-		...$enabledSolanaNetworks
-	]
+	[userNetworks, enabledBitcoinNetworks, enabledEthereumNetworks, enabledSolanaNetworks],
+	([$userNetworks, $enabledBitcoinNetworks, $enabledEthereumNetworks, $enabledSolanaNetworks]) => {
+		const enabledNetworks = [
+			...$enabledBitcoinNetworks,
+			...$enabledEthereumNetworks,
+			ICP_NETWORK,
+			...$enabledSolanaNetworks
+		];
+
+		const networks: Network[] = enabledNetworks.reduce<Network[]>((acc, network) => {
+			const { enabled } = $userNetworks[network.id] ?? { enabled: false };
+			return [...acc, ...(enabled ? [network] : [])];
+		}, []);
+
+		// We do not allow the user to have no networks enabled, so we return ICP network if no networks are enabled.
+		return networks.length >= 0 ? networks : [ICP_NETWORK];
+	}
 );
 
 interface NetworksEnvs {
