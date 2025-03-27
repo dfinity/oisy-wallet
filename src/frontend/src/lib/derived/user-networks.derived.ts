bb9a01c89d80f51718727440ef426e8b35b15fcd
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-27 09:57:39 +0100

    feat(frontend): derived store for user networks maps (#5431)
    # Motivation
    
    We are saving the networks settings in the backend, especially the
    enabled/disabled networks. So, we need to have a way to fetch them
    easily. To that scope we create a store derived from the user profile.
    
    # Changes
    
    - Create derived store to extract the networks map from the user
    settings.
    - If nothing is set, by default it will show all mainnets as enabled.
    - Otherwise it will map each network variant to its network ID and the
    settings.
    
    # Tests
    
    Added tests.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/derived/user-networks.derived.ts
+++ src/frontend/src/lib/derived/user-networks.derived.ts
@@ -1,8 +1,82 @@
-import type { NetworksSettings } from '$declarations/backend/backend.did';
+import type { NetworkSettingsFor, NetworksSettings } from '$declarations/backend/backend.did';
+import {
+	BTC_MAINNET_NETWORK_ID,
+	BTC_REGTEST_NETWORK_ID,
+	BTC_TESTNET_NETWORK_ID
+} from '$env/networks/networks.btc.env';
+import { SUPPORTED_MAINNET_NETWORKS_IDS } from '$env/networks/networks.env';
+import { ETHEREUM_NETWORK_ID, SEPOLIA_NETWORK_ID } from '$env/networks/networks.eth.env';
+import { ICP_NETWORK_ID } from '$env/networks/networks.icp.env';
+import {
+	SOLANA_DEVNET_NETWORK_ID,
+	SOLANA_LOCAL_NETWORK_ID,
+	SOLANA_MAINNET_NETWORK_ID,
+	SOLANA_TESTNET_NETWORK_ID
+} from '$env/networks/networks.sol.env';
 import { userSettings } from '$lib/derived/user-profile.derived';
+import type { NetworkId } from '$lib/types/network';
+import type { UserNetworks } from '$lib/types/user-networks';
+import { isNullish } from '@dfinity/utils';
 import { derived, type Readable } from 'svelte/store';
 
 export const userSettingsNetworks: Readable<NetworksSettings | undefined> = derived(
 	[userSettings],
 	([$userSettings]) => $userSettings?.networks
+);
+
+export const userNetworks: Readable<UserNetworks> = derived(
+	[userSettingsNetworks],
+	([$userSettingsNetworks]) => {
+		const userNetworks = $userSettingsNetworks?.networks;
+
+		if (isNullish(userNetworks) || userNetworks.length === 0) {
+			// Returning all mainnets enabled by default
+			return SUPPORTED_MAINNET_NETWORKS_IDS.reduce<UserNetworks>(
+				(acc, id) => ({
+					...acc,
+					[id]: { enabled: true, isTestnet: false }
+				}),
+				{}
+			);
+		}
+
+		const keyToNetworkId = (key: NetworkSettingsFor): NetworkId => {
+			const networkId: NetworkId | undefined =
+				'InternetComputer' in key
+					? ICP_NETWORK_ID
+					: 'EthereumMainnet' in key
+						? ETHEREUM_NETWORK_ID
+						: 'EthereumSepolia' in key
+							? SEPOLIA_NETWORK_ID
+							: 'BitcoinMainnet' in key
+								? BTC_MAINNET_NETWORK_ID
+								: 'BitcoinTestnet' in key
+									? BTC_TESTNET_NETWORK_ID
+									: 'BitcoinRegtest' in key
+										? BTC_REGTEST_NETWORK_ID
+										: 'SolanaMainnet' in key
+											? SOLANA_MAINNET_NETWORK_ID
+											: 'SolanaTestnet' in key
+												? SOLANA_TESTNET_NETWORK_ID
+												: 'SolanaDevnet' in key
+													? SOLANA_DEVNET_NETWORK_ID
+													: 'SolanaLocal' in key
+														? SOLANA_LOCAL_NETWORK_ID
+														: undefined;
+
+			if (isNullish(networkId)) {
+				throw new Error(`Unknown network key: ${key}`);
+			}
+
+			return networkId;
+		};
+
+		return userNetworks.reduce<UserNetworks>((acc, [key, { enabled, is_testnet: isTestnet }]) => {
+			const networkId: NetworkId = keyToNetworkId(key);
+			return {
+				...acc,
+				[networkId]: { enabled, isTestnet }
+			};
+		}, {});
+	}
 );
--- src/frontend/src/tests/lib/derived/user-networks.derived.spec.ts
+++ src/frontend/src/tests/lib/derived/user-networks.derived.spec.ts
@@ -1,12 +1,20 @@
-import { userSettingsNetworks } from '$lib/derived/user-networks.derived';
+import { SUPPORTED_MAINNET_NETWORKS_IDS } from '$env/networks/networks.env';
+import { userNetworks, userSettingsNetworks } from '$lib/derived/user-networks.derived';
 import { userProfileStore } from '$lib/stores/user-profile.store';
-import { mockNetworksSettings, mockUserProfile } from '$tests/mocks/user-profile.mock';
+import type { UserNetworks } from '$lib/types/user-networks';
+import { mockUserNetworks } from '$tests/mocks/user-networks.mock';
+import {
+	mockNetworksSettings,
+	mockUserProfile,
+	mockUserSettings
+} from '$tests/mocks/user-profile.mock';
+import { toNullable } from '@dfinity/utils';
 import { get } from 'svelte/store';
 
 describe('user-networks.derived', () => {
 	const certified = true;
 
-	describe('userNetworksSettings', () => {
+	describe('userSettingsNetworks', () => {
 		it('should return undefined when user profile is not set', () => {
 			userProfileStore.reset();
 			expect(get(userSettingsNetworks)).toBeUndefined();
@@ -15,6 +23,40 @@ describe('user-networks.derived', () => {
 		it('should return user profile if it is not nullish', () => {
 			userProfileStore.set({ certified, profile: mockUserProfile });
 			expect(get(userSettingsNetworks)).toEqual(mockNetworksSettings);
+		});
+	});
+
+	describe('userNetworks', () => {
+		const expectedMainnets: UserNetworks = SUPPORTED_MAINNET_NETWORKS_IDS.reduce<UserNetworks>(
+			(acc, id) => ({
+				...acc,
+				[id]: { enabled: true, isTestnet: false }
+			}),
+			{}
+		);
+
+		it('should return only mainnets when user profile is not set', () => {
+			userProfileStore.reset();
+			expect(get(userNetworks)).toEqual(expectedMainnets);
+		});
+
+		it('should return only mainnets when user networks are nullish', () => {
+			userProfileStore.set({
+				certified,
+				profile: {
+					...mockUserProfile,
+					settings: toNullable({
+						...mockUserSettings,
+						networks: { ...mockNetworksSettings, networks: [] }
+					})
+				}
+			});
+			expect(get(userNetworks)).toEqual(expectedMainnets);
+		});
+
+		it('should return the user networks if they are set', () => {
+			userProfileStore.set({ certified, profile: mockUserProfile });
+			expect(get(userNetworks)).toEqual(mockUserNetworks);
 		});
 	});
 });
