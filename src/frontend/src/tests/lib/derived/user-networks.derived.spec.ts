4fc8611df1083ededb5fcb7044f27e3a02c86b38
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-27 13:26:18 +0100

    feat(frontend): include testnets in default of userNetworks

--- src/frontend/src/tests/lib/derived/user-networks.derived.spec.ts
+++ src/frontend/src/tests/lib/derived/user-networks.derived.spec.ts
@@ -1,4 +1,7 @@
-import { SUPPORTED_MAINNET_NETWORKS_IDS } from '$env/networks/networks.env';
+import {
+	SUPPORTED_MAINNET_NETWORKS_IDS,
+	SUPPORTED_TESTNET_NETWORKS_IDS
+} from '$env/networks/networks.env';
 import { userNetworks, userSettingsNetworks } from '$lib/derived/user-networks.derived';
 import { userProfileStore } from '$lib/stores/user-profile.store';
 import type { UserNetworks } from '$lib/types/user-networks';
@@ -35,6 +38,14 @@ describe('user-networks.derived', () => {
 			{}
 		);
 
+		const expectedTestnets: UserNetworks = SUPPORTED_TESTNET_NETWORKS_IDS.reduce<UserNetworks>(
+			(acc, id) => ({
+				...acc,
+				[id]: { enabled: true, isTestnet: true }
+			}),
+			{}
+		);
+
 		it('should return only mainnets when user profile is not set', () => {
 			userProfileStore.reset();
 			expect(get(userNetworks)).toEqual(expectedMainnets);
@@ -57,6 +68,20 @@ describe('user-networks.derived', () => {
 		it('should return the user networks if they are set', () => {
 			userProfileStore.set({ certified, profile: mockUserProfile });
 			expect(get(userNetworks)).toEqual(mockUserNetworks);
+		});
+
+		it('should return all networks when user networks are nullish but testnets are enabled', () => {
+			userProfileStore.set({
+				certified,
+				profile: {
+					...mockUserProfile,
+					settings: toNullable({
+						...mockUserSettings,
+						networks: { ...mockNetworksSettings, networks: [], testnets: { show_testnets: true } }
+					})
+				}
+			});
+			expect(get(userNetworks)).toEqual({ ...expectedMainnets, ...expectedTestnets });
 		});
 	});
 });
