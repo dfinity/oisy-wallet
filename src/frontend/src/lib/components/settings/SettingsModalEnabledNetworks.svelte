17ac178d6318650d6ccc6b244d95fd4dad5058c1
daviddecentage <david.farina@dfinity.org>
2025-03-27 12:26:49 +0100

    update

--- src/frontend/src/lib/components/settings/SettingsModalEnabledNetworks.svelte
+++ src/frontend/src/lib/components/settings/SettingsModalEnabledNetworks.svelte
@@ -5,30 +5,51 @@
 	import SettingsListItem from '$lib/components/settings/SettingsListItem.svelte';
 	import ButtonCloseModal from '$lib/components/ui/ButtonCloseModal.svelte';
 	import ContentWithToolbar from '$lib/components/ui/ContentWithToolbar.svelte';
-	import { userProfileVersion, userSettings } from '$lib/derived/user-profile.derived';
+	import { userProfileVersion } from '$lib/derived/user-profile.derived';
 	import { i18n } from '$lib/stores/i18n.store';
 	import type { Network } from '$lib/types/network';
-	import { setUserShowTestnets } from '$lib/api/backend.api';
+	import { setUserShowTestnets, updateUserNetworkSettings } from '$lib/api/backend.api';
 	import { authIdentity } from '$lib/derived/auth.derived';
 	import { emit } from '$lib/utils/events.utils';
 	import { modalStore } from '$lib/stores/modal.store';
 	import { SUPPORTED_NETWORKS } from '$env/networks/networks.env';
 	import { testnets } from '$lib/derived/testnets.derived';
+	import { userNetworks } from '$lib/derived/user-networks.derived';
+	import type { UserNetworks } from '$lib/types/user-networks';
+	import ButtonGroup from '$lib/components/ui/ButtonGroup.svelte';
+	import Button from '$lib/components/ui/Button.svelte';
+	import { ICP_NETWORK_ID } from '$env/networks/networks.icp.env';
 
-	let enabledNetworksData: { [id: symbol]: boolean };
-	$: enabledNetworksData = { ...$userSettings?.settings?.enabledNetworks };
+	let enabledNetworks: UserNetworks;
+	$: enabledNetworks = $userNetworks;
+	const enabledNetworksInitial = { ...$userNetworks };
 
 	let enabledTestnet: boolean;
 	$: enabledTestnet = $testnets;
 	const enabledTestnetInitial = $testnets;
 
-	let isModified: boolean;
-	$: isModified = (() => {
+	const checkModified = ({
+		enabledTestnet,
+		enabledNetworks
+	}: {
+		enabledTestnet: boolean;
+		enabledNetworks: UserNetworks;
+	}) => {
 		if (enabledTestnet !== enabledTestnetInitial) {
 			return true;
 		}
+		const symbols = Object.getOwnPropertySymbols(enabledNetworks ?? {});
+		for (const k of symbols) {
+			const value = enabledNetworks[k as keyof typeof enabledNetworks];
+			const initialValue = enabledNetworksInitial[k as keyof typeof enabledNetworks];
+			if (value?.enabled !== initialValue?.enabled) {
+				return true;
+			}
+		}
 		return false;
-	})();
+	};
+	let isModified: boolean;
+	$: isModified = checkModified({ enabledTestnet, enabledNetworks });
 
 	let mainnetsList: Network[];
 	$: mainnetsList = SUPPORTED_NETWORKS.filter((n) => n.env === 'mainnet');
@@ -39,9 +60,14 @@
 	const toggleTestnets = async () => {
 		enabledTestnet = !enabledTestnet;
 	};
+
 	const toggleNetwork = (network: Network) => {
-		// todo: call service to toggle network
+		enabledNetworks[network.id] = {
+			enabled: !enabledNetworks[network.id]?.enabled,
+			isTestnet: network.env === 'testnet'
+		};
 	};
+
 	let saveLoading: boolean;
 	$: saveLoading = false;
 
@@ -52,6 +78,13 @@
 			identity: $authIdentity,
 			currentUserVersion: $userProfileVersion
 		});
+
+		await updateUserNetworkSettings({
+			identity: $authIdentity,
+			networks: enabledNetworks,
+			currentUserVersion: $userProfileVersion
+		});
+
 		emit({ message: 'oisyRefreshUserProfile' });
 		modalStore.close();
 	};
@@ -82,8 +115,9 @@
 				<svelte:fragment slot="value"
 					><Toggle
 						ariaLabel="Enable/Disable"
-						checked={enabledNetworksData?.[network.id]}
+						checked={enabledNetworks[network.id]?.enabled ?? false}
 						on:nnsToggle={() => toggleNetwork(network)}
+						disabled={network.id === ICP_NETWORK_ID}
 					/></svelte:fragment
 				>
 			</SettingsListItem>
@@ -103,7 +137,7 @@
 					<svelte:fragment slot="value"
 						><Toggle
 							ariaLabel="Enable/Disable"
-							checked={enabledNetworksData?.[network.id]}
+							checked={enabledNetworks[network.id]?.enabled ?? false}
 							on:nnsToggle={() => toggleNetwork(network)}
 						/></svelte:fragment
 					>
@@ -112,5 +146,10 @@
 		</SettingsList>
 	{/if}
 
-	<ButtonCloseModal slot="toolbar" />
+	<ButtonGroup slot="toolbar">
+		<ButtonCloseModal />
+		<Button loading={saveLoading} colorStyle="primary" on:click={save} disabled={!isModified}
+			>{$i18n.core.text.save}</Button
+		>
+	</ButtonGroup>
 </ContentWithToolbar>
