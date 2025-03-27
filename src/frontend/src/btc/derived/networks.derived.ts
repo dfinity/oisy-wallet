e71d7c2ba0d9ff3a1c47603b8eed73ddfaf99d6f
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-27 16:29:37 +0100

    feat(frontend): filter enabled networks using user networks

--- src/frontend/src/btc/derived/networks.derived.ts
+++ src/frontend/src/btc/derived/networks.derived.ts
@@ -6,10 +6,18 @@ import {
 } from '$env/networks/networks.btc.env';
 import { LOCAL } from '$lib/constants/app.constants';
 import { testnets } from '$lib/derived/testnets.derived';
+import { userNetworks } from '$lib/derived/user-networks.derived';
 import type { Network } from '$lib/types/network';
+import { isUserNetworkEnabled } from '$lib/utils/user-networks.utils';
 import { derived, type Readable } from 'svelte/store';
 
-export const enabledBitcoinNetworks: Readable<Network[]> = derived([testnets], ([$testnets]) => [
-	...(BTC_MAINNET_ENABLED ? [BTC_MAINNET_NETWORK] : []),
-	...($testnets ? [BTC_TESTNET_NETWORK, ...(LOCAL ? [BTC_REGTEST_NETWORK] : [])] : [])
-]);
+export const enabledBitcoinNetworks: Readable<Network[]> = derived(
+	[testnets, userNetworks],
+	([$testnets, $userNetworks]) =>
+		[
+			...(BTC_MAINNET_ENABLED ? [BTC_MAINNET_NETWORK] : []),
+			...($testnets ? [BTC_TESTNET_NETWORK, ...(LOCAL ? [BTC_REGTEST_NETWORK] : [])] : [])
+		].filter(({ id: networkId }) =>
+			isUserNetworkEnabled({ userNetworks: $userNetworks, networkId })
+		)
+);
