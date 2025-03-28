370ff438f6f236a1ff022f7a365248e6215641d5
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-03-28 14:33:10 +0100

    refactor(frontend): revert changes done to NetworksSwitcher (#5494)
    # Motivation
    
    Since the ModalTokensList component won't be re-using
    `NetworksSwitcher`, the changes made in
    https://github.com/dfinity/oisy-wallet/commit/34e5f7e2805ffb5a08a1b4e9c922ad0bd197d54f
    can be reverted.

--- src/frontend/src/lib/components/networks/NetworksSwitcher.svelte
+++ src/frontend/src/lib/components/networks/NetworksSwitcher.svelte
@@ -1,17 +1,76 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import NetworksDropdown from '$lib/components/networks/NetworksDropdown.svelte';
+	import { nonNullish } from '@dfinity/utils';
+	import { slide } from 'svelte/transition';
+	import chainFusion from '$lib/assets/chain_fusion.svg';
+	import MainnetNetwork from '$lib/components/networks/MainnetNetwork.svelte';
+	import Network from '$lib/components/networks/Network.svelte';
+	import NetworkButton from '$lib/components/networks/NetworkButton.svelte';
+	import NetworkLogo from '$lib/components/networks/NetworkLogo.svelte';
+	import Dropdown from '$lib/components/ui/Dropdown.svelte';
+	import Logo from '$lib/components/ui/Logo.svelte';
+	import { NETWORKS_SWITCHER_DROPDOWN } from '$lib/constants/test-ids.constants';
+	import { SLIDE_EASING } from '$lib/constants/transition.constants';
 	import { selectedNetwork } from '$lib/derived/network.derived';
-	import type { NetworkId } from '$lib/types/network';
-	import { gotoReplaceRoot, isRouteTransactions, switchNetwork } from '$lib/utils/nav.utils';
+	import { networksMainnets, networksTestnets } from '$lib/derived/networks.derived';
+	import { testnetsEnabled } from '$lib/derived/testnets.derived';
+	import { enabledMainnetTokensUsdBalancesPerNetwork } from '$lib/derived/tokens.derived';
+	import { i18n } from '$lib/stores/i18n.store';
 
-	const onNetworkSelect = async ({ detail: networkId }: CustomEvent<NetworkId>) => {
-		await switchNetwork(networkId);
+	export let disabled = false;
 
-		if (isRouteTransactions($page)) {
-			await gotoReplaceRoot();
-		}
-	};
+	let dropdown: Dropdown | undefined;
+
+	let mainnetTokensUsdBalance: number;
+	$: mainnetTokensUsdBalance = $networksMainnets.reduce(
+		(acc, { id }) => acc + ($enabledMainnetTokensUsdBalancesPerNetwork[id] ?? 0),
+		0
+	);
 </script>
 
-<NetworksDropdown selectedNetwork={$selectedNetwork} on:icSelected={onNetworkSelect} />
+<Dropdown
+	bind:this={dropdown}
+	ariaLabel={$i18n.networks.title}
+	testId={NETWORKS_SWITCHER_DROPDOWN}
+	{disabled}
+	asModalOnMobile
+>
+	{#if nonNullish($selectedNetwork)}
+		<NetworkLogo network={$selectedNetwork} size="xs" />
+	{:else}
+		<Logo src={chainFusion} size="xs" />
+	{/if}
+	<span class="hidden md:block">{$selectedNetwork?.name ?? $i18n.networks.chain_fusion}</span>
+
+	<svelte:fragment slot="title">{$i18n.networks.filter}</svelte:fragment>
+	<div slot="items">
+		<NetworkButton
+			id={undefined}
+			name={$i18n.networks.chain_fusion}
+			icon={chainFusion}
+			usdBalance={mainnetTokensUsdBalance}
+			on:icSelected={dropdown.close}
+		/>
+
+		<ul class="flex list-none flex-col">
+			{#each $networksMainnets as network (network.id)}
+				<li transition:slide={SLIDE_EASING}
+					><MainnetNetwork {network} on:icSelected={dropdown.close} /></li
+				>
+			{/each}
+		</ul>
+
+		{#if $testnetsEnabled}
+			<span class="my-5 flex px-3 font-bold" transition:slide={SLIDE_EASING}
+				>{$i18n.networks.test_networks}</span
+			>
+
+			<ul class="flex list-none flex-col" transition:slide={SLIDE_EASING}>
+				{#each $networksTestnets as network (network.id)}
+					<li transition:slide={SLIDE_EASING}
+						><Network {network} on:icSelected={dropdown.close} /></li
+					>
+				{/each}
+			</ul>
+		{/if}
+	</div>
+</Dropdown>
