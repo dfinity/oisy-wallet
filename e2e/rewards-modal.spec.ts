c7c67640b6d2f343fc4cf1e72c6b26013e4c0d56
inc-man <nicolas.maechler@dfinity.org>
2025-03-28 15:54:26 +0100

    adjusted to correct modal test

--- e2e/rewards-modal.spec.ts
+++ e2e/rewards-modal.spec.ts
@@ -1,10 +1,35 @@
+import { NAVIGATION_ITEM_REWARDS, REWARDS_MODAL, REWARDS_MODAL_DATE_BADGE, REWARDS_STATUS_BUTTON } from '$lib/constants/test-ids.constants';
 import { testWithII } from '@dfinity/internet-identity-playwright';
-import { RewardsModalPage } from './utils/pages/rewards-modal.page';
+import { MODALS_VIEWPORT_WIDTH } from './utils/constants/e2e.constants';
+import { HomepageLoggedIn } from './utils/pages/homepage.page';
+import { getReceiveTokensModalAddressLabelSelectors } from './utils/selectors.utils';
 
-testWithII('should display rewards page', async ({ page, iiPage }) => {
-	const rewardsModalPage = new RewardsModalPage({ page, iiPage });
+const RECEIVE_TOKENS_MODAL_VIEWPORT_HEIGHT = 900;
+let homepageLoggedIn: HomepageLoggedIn;
 
-	await rewardsModalPage.waitForReady();
+testWithII.beforeEach(async ({ page, iiPage, isMobile }) => {
+	await page.clock.install();
 
-	await rewardsModalPage.takeScreenshot();
+	homepageLoggedIn = new HomepageLoggedIn({
+		page,
+		iiPage,
+		viewportSize: !isMobile
+			? {
+					width: MODALS_VIEWPORT_WIDTH,
+					height: RECEIVE_TOKENS_MODAL_VIEWPORT_HEIGHT
+				}
+			: undefined
+	});
+	await homepageLoggedIn.waitForReady();
+	await homepageLoggedIn.navigateTo(NAVIGATION_ITEM_REWARDS);
+});
+
+testWithII('should display receive-tokens modal', async () => {
+	await homepageLoggedIn.testModalSnapshot({
+		modalOpenButtonTestId: REWARDS_STATUS_BUTTON,
+		modalTestId: REWARDS_MODAL,
+		selectorsToMock: getReceiveTokensModalAddressLabelSelectors([
+			REWARDS_MODAL_DATE_BADGE
+		])
+	});
 });
