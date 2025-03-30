8761693e2964df5e51f4e3260ae004496d85b3bd
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-30 10:17:32 +0200

    feat(frontend): filter enabled tokens using user networks (#5500)
    # Motivation
    
    We are going to use the enabled/disabled networks from the user profile
    saved in the backend. So, we need to hide the tokens of the disabled
    stores.
    
    To do that we use the same method that we used for the enabled networks
    in PR https://github.com/dfinity/oisy-wallet/pull/5480

--- src/frontend/src/eth/derived/tokens.derived.ts
+++ src/frontend/src/eth/derived/tokens.derived.ts
@@ -1,13 +1,18 @@
 import { ETH_MAINNET_ENABLED } from '$env/networks/networks.eth.env';
 import { ETHEREUM_TOKEN, SEPOLIA_TOKEN } from '$env/tokens/tokens.eth.env';
 import { testnetsEnabled } from '$lib/derived/testnets.derived';
+import { userNetworks } from '$lib/derived/user-networks.derived';
 import type { RequiredTokenWithLinkedData } from '$lib/types/token';
+import { isUserNetworkEnabled } from '$lib/utils/user-networks.utils';
 import { derived, type Readable } from 'svelte/store';
 
 export const enabledEthereumTokens: Readable<RequiredTokenWithLinkedData[]> = derived(
-	[testnetsEnabled],
-	([$testnetsEnabled]) => [
-		...(ETH_MAINNET_ENABLED ? [ETHEREUM_TOKEN] : []),
-		...($testnetsEnabled ? [SEPOLIA_TOKEN] : [])
-	]
+	[testnetsEnabled, userNetworks],
+	([$testnetsEnabled, $userNetworks]) =>
+		[
+			...(ETH_MAINNET_ENABLED ? [ETHEREUM_TOKEN] : []),
+			...($testnetsEnabled ? [SEPOLIA_TOKEN] : [])
+		].filter(({ network: { id: networkId } }) =>
+			isUserNetworkEnabled({ userNetworks: $userNetworks, networkId })
+		)
 );
