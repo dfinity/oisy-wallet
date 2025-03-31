4f6eeb05ea7a5c6ebdfb26982d596909f9fa20f3
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-03-31 08:10:11 +0200

    feat(frontend): remove unnecessary store to check if ICP network is enabled

--- src/frontend/src/lib/derived/networks.derived.ts
+++ src/frontend/src/lib/derived/networks.derived.ts
@@ -5,7 +5,7 @@ import {
 	BTC_TESTNET_NETWORK_ID
 } from '$env/networks/networks.btc.env';
 import { ETHEREUM_NETWORK_ID, SEPOLIA_NETWORK_ID } from '$env/networks/networks.eth.env';
-import { ICP_NETWORK, ICP_NETWORK_ID } from '$env/networks/networks.icp.env';
+import { ICP_NETWORK } from '$env/networks/networks.icp.env';
 import { enabledEthereumNetworks } from '$eth/derived/networks.derived';
 import type { Network } from '$lib/types/network';
 import { enabledSolanaNetworks } from '$sol/derived/networks.derived';
@@ -44,15 +44,6 @@ export const networksMainnets: Readable<Network[]> = derived(
 export const networksTestnets: Readable<Network[]> = derived(
 	[networksEnvs],
 	([{ testnets }]) => testnets
-);
-
-export const networkICPEnabled: Readable<boolean> = derived([networks], ([$networks]) =>
-	$networks.some(({ id }) => id === ICP_NETWORK_ID)
-);
-
-export const networkICPDisabled: Readable<boolean> = derived(
-	[networkICPEnabled],
-	([$networkICPEnabled]) => !$networkICPEnabled
 );
 
 export const networkEthereumEnabled: Readable<boolean> = derived([networks], ([$networks]) =>
