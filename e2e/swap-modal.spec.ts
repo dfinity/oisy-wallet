33314aecef0b62b9bfaff6e2f0026d2ca187f625
inc-man <nicolas.maechler@dfinity.org>
2025-03-31 16:26:18 +0200

    fix configuration issue

--- e2e/swap-modal.spec.ts
+++ e2e/swap-modal.spec.ts
@@ -4,25 +4,28 @@ import {
 } from '$lib/constants/test-ids.constants';
 import { testWithII } from '@dfinity/internet-identity-playwright';
 import { MODALS_VIEWPORT_WIDTH } from './utils/constants/e2e.constants';
-import { RewardsPage } from './utils/pages/rewards-page';
+import { HomepageLoggedIn } from './utils/pages/homepage.page';
 
-const SWAP_MODAL_VIEWPORT_HEIGHT = 900;
+const SWAP_TOKENS_MODAL_VIEWPORT_HEIGHT = 900;
+let homepageLoggedIn: HomepageLoggedIn;
 
-testWithII('should display swap modal', async ({ page, iiPage, isMobile }) => {
-	const rewardsPage = new RewardsPage({
+testWithII.beforeEach(async ({ page, iiPage, isMobile }) => {
+
+	homepageLoggedIn = new HomepageLoggedIn({
 		page,
 		iiPage,
 		viewportSize: !isMobile
 			? {
 					width: MODALS_VIEWPORT_WIDTH,
-					height: SWAP_MODAL_VIEWPORT_HEIGHT
+					height: SWAP_TOKENS_MODAL_VIEWPORT_HEIGHT
 				}
 			: undefined
 	});
+	await homepageLoggedIn.waitForReady();
+});
 
-	await rewardsPage.waitForReady();
-
-	await rewardsPage.testModalSnapshot({
+testWithII('should display swap-tokens modal', async () => {
+	await homepageLoggedIn.testModalSnapshot({
 		modalOpenButtonTestId: SWAP_TOKENS_MODAL_OPEN_BUTTON,
 		modalTestId: SWAP_TOKENS_MODAL
 	});
