3173a84bdc5463aa65c3f3e9b29afb800c2ae257
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-03-31 15:15:18 +0200

    feat(frontend): implements referral service implementation (#5518)
    # Motivation
    
    As a user i want to be able to use the new `referral` endpoints of the
    rewards canister.
    
    # Changes
    
    - updates service implementation
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

--- src/frontend/src/lib/api/reward.api.ts
+++ src/frontend/src/lib/api/reward.api.ts
@@ -1,6 +1,7 @@
 import type {
 	ClaimVipRewardResponse,
 	NewVipRewardResponse,
+	ReferrerInfo,
 	UserData,
 	UserSnapshot,
 	VipReward
@@ -39,6 +40,26 @@ export const claimVipReward = async ({
 	const { claimVipReward } = await rewardCanister({ identity });
 
 	return claimVipReward(vipReward);
+};
+
+export const getReferrerInfo = async ({
+	identity,
+	certified
+}: CanisterApiFunctionParams<QueryParams>): Promise<ReferrerInfo> => {
+	const { getReferrerInfo } = await rewardCanister({ identity });
+
+	return getReferrerInfo({ certified });
+};
+
+export const setReferrer = async ({
+	referrerCode,
+	identity
+}: CanisterApiFunctionParams<{
+	referrerCode: number;
+}>): Promise<void> => {
+	const { setReferrer } = await rewardCanister({ identity });
+
+	return setReferrer(referrerCode);
 };
 
 export const registerAirdropRecipient = async ({
