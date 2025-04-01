4d5ff516720672ed4201fc5acb4626016291c0cc
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-01 12:27:28 +0200

    refactor(frontend): extract service that initalize the loader (#5544)
    # Motivation
    
    For better manageability, we extract the service called on mounting in
    the `Loader` component: this service is responsible for:
    - load user profile;
    - load mainnet addresses from IDB;
    - validate such addresses;
    - load the addresses from the backend in case of failures (initializing
    the signer allowance too).
    
    # Changes
    
    - Move entire internal function to new service `initLoader`.
    - Use it in component `Loader`.
    
    # Tests
    
    Created tests specific for it.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/tests/lib/services/loader.services.spec.ts
+++ src/frontend/src/tests/lib/services/loader.services.spec.ts
@@ -1,10 +1,29 @@
+import { ICP_TOKEN_ID } from '$env/tokens/tokens.icp.env';
+import { SOLANA_TOKEN_ID } from '$env/tokens/tokens.sol.env';
 import * as api from '$lib/api/backend.api';
+import { allowSigning } from '$lib/api/backend.api';
 import { CanisterInternalError } from '$lib/canisters/errors';
+import { loadAddresses, loadIdbAddresses } from '$lib/services/addresses.services';
 import * as authServices from '$lib/services/auth.services';
-import { initSignerAllowance } from '$lib/services/loader.services';
+import { nullishSignOut, signOut } from '$lib/services/auth.services';
+import { loadUserProfile } from '$lib/services/load-user-profile.services';
+import { initLoader, initSignerAllowance } from '$lib/services/loader.services';
 import { authStore } from '$lib/stores/auth.store';
-import { Ed25519KeyIdentity } from '@dfinity/identity';
-import type { MockInstance } from 'vitest';
+import { loading } from '$lib/stores/loader.store';
+import type { LoadIdbAddressError } from '$lib/types/errors';
+import { mockAuthStore } from '$tests/mocks/auth.mock';
+import { mockIdentity } from '$tests/mocks/identity.mock';
+import { get } from 'svelte/store';
+import { type MockInstance } from 'vitest';
+
+vi.mock('$lib/services/load-user-profile.services', () => ({
+	loadUserProfile: vi.fn(() => Promise.resolve({ success: true }))
+}));
+
+vi.mock('$lib/services/addresses.services', () => ({
+	loadIdbAddresses: vi.fn(() => Promise.resolve({ success: true })),
+	loadAddresses: vi.fn(() => Promise.resolve({ success: true }))
+}));
 
 describe('loader.services', () => {
 	describe('initSignerAllowance', () => {
@@ -16,8 +35,7 @@ describe('loader.services', () => {
 
 			apiMock = vi.spyOn(api, 'allowSigning');
 
-			const identity = Ed25519KeyIdentity.generate();
-			authStore.setForTesting(identity);
+			mockAuthStore();
 
 			Object.defineProperty(window, 'location', {
 				writable: true,
@@ -61,6 +79,85 @@ describe('loader.services', () => {
 
 			expect(spySignOut).toHaveBeenCalledOnce();
 			expect(spy).toHaveBeenCalledOnce();
+		});
+	});
+
+	describe('initLoader', () => {
+		const mockValidateAddresses = vi.fn();
+		const mockProgressAndLoad = vi.fn();
+		const mockSetProgressModal = vi.fn();
+
+		const mockParams = {
+			identity: mockIdentity,
+			validateAddresses: mockValidateAddresses,
+			progressAndLoad: mockProgressAndLoad,
+			setProgressModal: mockSetProgressModal
+		};
+
+		beforeEach(() => {
+			vi.clearAllMocks();
+			vi.resetAllMocks();
+
+			vi.spyOn(authServices, 'signOut').mockImplementation(vi.fn());
+			vi.spyOn(authServices, 'nullishSignOut').mockImplementation(vi.fn());
+			vi.spyOn(api, 'allowSigning').mockImplementation(vi.fn());
+		});
+
+		it('should sign out if the identity is nullish', async () => {
+			await initLoader({ ...mockParams, identity: null });
+
+			expect(nullishSignOut).toHaveBeenCalledOnce();
+		});
+
+		it('should load the user profile', async () => {
+			await initLoader(mockParams);
+
+			expect(loadUserProfile).toHaveBeenCalledOnce();
+			expect(loadUserProfile).toHaveBeenNthCalledWith(1, { identity: mockIdentity });
+		});
+
+		it('should sign out if the user profile is not loaded', async () => {
+			vi.mocked(loadUserProfile).mockResolvedValueOnce({ success: false });
+
+			await initLoader(mockParams);
+
+			expect(signOut).toHaveBeenCalledOnce();
+		});
+
+		it('should load addresses from IDB', async () => {
+			await initLoader(mockParams);
+
+			expect(loadIdbAddresses).toHaveBeenCalledOnce();
+
+			expect(mockProgressAndLoad).toHaveBeenCalledOnce();
+			expect(mockValidateAddresses).toHaveBeenCalledOnce();
+
+			expect(get(loading)).toBeFalsy();
+		});
+
+		it('should not load addresses from the backend if the IDB addresses are loaded', async () => {
+			await initLoader(mockParams);
+
+			expect(allowSigning).not.toHaveBeenCalled();
+			expect(loadAddresses).not.toHaveBeenCalledOnce();
+		});
+
+		it('should load addresses from the backend if the IDB addresses are not loaded', async () => {
+			mockAuthStore(mockIdentity);
+			authStore.setForTesting(mockIdentity);
+
+			vi.mocked(loadIdbAddresses).mockResolvedValueOnce({
+				success: false,
+				err: [{ tokenId: ICP_TOKEN_ID }, { tokenId: SOLANA_TOKEN_ID }] as LoadIdbAddressError[]
+			});
+
+			await initLoader(mockParams);
+
+			expect(allowSigning).toHaveBeenCalledOnce();
+			expect(allowSigning).toHaveBeenNthCalledWith(1, { identity: mockIdentity });
+
+			expect(loadAddresses).toHaveBeenCalledOnce();
+			expect(loadAddresses).toHaveBeenNthCalledWith(1, [ICP_TOKEN_ID, SOLANA_TOKEN_ID]);
 		});
 	});
 });
