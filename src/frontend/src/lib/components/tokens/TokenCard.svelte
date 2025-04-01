9aed37b33040399f1706abc2824b001f396a542b
daviddecentage <david.farina@dfinity.org>
2025-04-01 17:37:12 +0200

    init

--- src/frontend/src/lib/components/tokens/TokenCard.svelte
+++ src/frontend/src/lib/components/tokens/TokenCard.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import { nonNullish } from '@dfinity/utils';
+	import { slide } from 'svelte/transition';
+	import { nonNullish, type Token } from '@dfinity/utils';
 	import TokenLogo from '$lib/components/tokens/TokenLogo.svelte';
 	import TokenName from '$lib/components/tokens/TokenName.svelte';
 	import TokenSymbol from '$lib/components/tokens/TokenSymbol.svelte';
@@ -7,13 +8,59 @@
 	import { TOKEN_CARD, TOKEN_GROUP } from '$lib/constants/test-ids.constants';
 	import type { LogoSize } from '$lib/types/components';
 	import type { CardData } from '$lib/types/token-card';
+	import LogoButton from '$lib/components/ui/LogoButton.svelte';
+	import TokenBalance from '$lib/components/tokens/TokenBalance.svelte';
+	import ExchangeTokenValue from '$lib/components/exchange/ExchangeTokenValue.svelte';
+	import { SLIDE_PARAMS } from '$lib/constants/transition.constants';
 
 	export let data: CardData;
 	export let testIdPrefix: typeof TOKEN_CARD | typeof TOKEN_GROUP = TOKEN_CARD;
-	export let logoSize: LogoSize = 'lg';
+	export let cardSize: 'xs' | 'lg' = 'lg';
 	export let hideNetworkLogo = false;
+	export let isExpanded = false;
+	export let hover = false;
+	export let description = '';
 </script>
 
+<div class="flex w-full flex-col">
+	<LogoButton
+		dividers={false}
+		rounded={false}
+		testId={`${testIdPrefix}-${data.symbol}-${data.network.id.description}`}
+		on:click
+		spacings={cardSize === 'xs' ? 'sm' : 'md'}
+		{hover}
+	>
+		<TokenLogo
+			{data}
+			badge={nonNullish(data.tokenCount)
+				? { type: 'tokenCount', count: data.tokenCount }
+				: undefined}
+			slot="logo"
+			color="white"
+			logoSize={cardSize}
+		/>
+
+		<span class:text-sm={cardSize === 'xs'} slot="title">
+			{data.symbol}
+		</span>
+
+		<span class:text-sm={cardSize === 'xs'} slot="subtitle">&nbsp;&middot;&nbsp;{data.name}</span>
+
+		<span class:text-sm={cardSize === 'xs'} class="block min-w-12" slot="title-end">
+			<TokenBalance {data} />
+		</span>
+
+		<span class:text-sm={cardSize === 'xs'} class="block min-w-12" slot="description">
+			{@html description}
+		</span>
+
+		<span class:text-sm={cardSize === 'xs'} class="block min-w-12" slot="description-end">
+			<ExchangeTokenValue {data} />
+		</span>
+	</LogoButton>
+</div>
+<!--
 <Card noMargin testId={`${testIdPrefix}-${data.symbol}-${data.network.id.description}`}>
 	<TokenSymbol {data} {hideNetworkLogo} />
 
@@ -31,3 +78,4 @@
 
 	<slot name="exchange" slot="amountDescription" />
 </Card>
+-->
--- src/frontend/src/lib/components/tokens/TokenGroupCard.svelte
+++ src/frontend/src/lib/components/tokens/TokenGroupCard.svelte
@@ -1,47 +1,91 @@
 <script lang="ts">
 	import { slide } from 'svelte/transition';
 	import MultipleListeners from '$lib/components/core/MultipleListeners.svelte';
-	import TokenCardContent from '$lib/components/tokens/TokenCardContent.svelte';
-	import TokenCardWithOnClick from '$lib/components/tokens/TokenCardWithOnClick.svelte';
-	import TokenCardWithUrl from '$lib/components/tokens/TokenCardWithUrl.svelte';
 	import { TOKEN_GROUP } from '$lib/constants/test-ids.constants';
 	import { SLIDE_PARAMS } from '$lib/constants/transition.constants';
 	import { tokenGroupStore } from '$lib/stores/token-group.store';
 	import type { CardData } from '$lib/types/token-card';
 	import type { TokenUiGroup } from '$lib/types/token-group';
 	import { mapHeaderData } from '$lib/utils/token-card.utils';
+	import TokenCard from '$lib/components/tokens/TokenCard.svelte';
+	import { transactionsUrl } from '$lib/utils/nav.utils';
+	import { goto } from '$app/navigation';
+	import Button from '$lib/components/ui/Button.svelte';
+	import { isNullish, nonNullish } from '@dfinity/utils';
+	import type { TokenUi } from '$lib/types/token';
 
 	export let tokenGroup: TokenUiGroup;
 
 	let isExpanded: boolean;
 	$: isExpanded = ($tokenGroupStore ?? {})[tokenGroup.id]?.isExpanded ?? false;
 
+	let hideZeros: boolean;
+	$: hideZeros = ($tokenGroupStore ?? {})[tokenGroup.id]?.hideZeros ?? true;
+
+	const toggleIsExpanded = (toggle: boolean) =>
+		tokenGroupStore.set({ tokenId: tokenGroup.id, data: { isExpanded: toggle, hideZeros } });
+
+	const toggleHideZeros = (toggle: boolean) =>
+		tokenGroupStore.set({ tokenId: tokenGroup.id, data: { isExpanded, hideZeros: toggle } });
+
 	let headerData: CardData;
 	$: headerData = mapHeaderData(tokenGroup);
 
-	const toggleIsExpanded = (toggle: boolean) =>
-		tokenGroupStore.set({ tokenId: tokenGroup.id, data: { isExpanded: toggle } });
+	const isDefaultToken = (token: TokenUi) => tokenGroup.nativeToken.id === token.id;
+	const isCkToken = (token: TokenUi) => nonNullish(token.oisyName?.prefix);
+
+	$: filteredTokens = tokenGroup.tokens.filter(
+		(token) => isCkToken(token) || isDefaultToken(token) || (token.balance || 0n) > 0 // Always display if balance > 0
+	);
+
+	// Show all if hideZeros = false
+	$: tokensToShow = (hideZeros ? filteredTokens : tokenGroup.tokens).sort((a, b) =>
+		isDefaultToken(a) ? -1 : isCkToken(a) && !isDefaultToken(b) ? -1 : 1
+	);
+
+	// Count tokens that are not displayed
+	$: notDisplayedCount = tokenGroup.tokens.length - tokensToShow.length;
 </script>
 
-<div class="flex flex-col">
+<div class="flex flex-col" class:bg-primary={isExpanded}>
 	<MultipleListeners tokens={tokenGroup.tokens}>
-		<TokenCardWithOnClick
-			on:click={() => toggleIsExpanded(!isExpanded)}
-			styleClass="px-3 py-2 hover:bg-brand-subtle-10 {isExpanded
-				? 'bg-primary rounded-b-none'
-				: ''}"
-		>
-			<TokenCardContent data={headerData} hideNetworkLogo testIdPrefix={TOKEN_GROUP} />
-		</TokenCardWithOnClick>
+		<div class="transition duration-300 hover:bg-primary">
+			<TokenCard
+				data={headerData}
+				hideNetworkLogo
+				testIdPrefix={TOKEN_GROUP}
+				on:click={() => toggleIsExpanded(!isExpanded)}
+				{isExpanded}
+				description={[...new Set(tokenGroup.tokens.map((t) => t.network.name))].join(
+					'&nbsp;&middot;&nbsp;'
+				)}
+			/>
+		</div>
 	</MultipleListeners>
 
 	{#if isExpanded}
-		<div class="flex flex-col gap-0 bg-secondary" transition:slide={SLIDE_PARAMS}>
-			{#each tokenGroup.tokens as token (token.id)}
-				<TokenCardWithUrl styleClass="p-3 hover:bg-brand-subtle-10" {token}>
-					<TokenCardContent logoSize="md" data={token} />
-				</TokenCardWithUrl>
+		<div class="ml-0 flex flex-col gap-1.5 p-2 md:ml-16" transition:slide={SLIDE_PARAMS}>
+			{#each tokensToShow as token (token.id)}
+				<div
+					class="flex overflow-hidden rounded-lg bg-secondary transition duration-300 hover:bg-brand-subtle-10"
+				>
+					<TokenCard
+						data={token}
+						hideNetworkLogo
+						cardSize="xs"
+						on:click={() => goto(transactionsUrl({ token }))}
+					/>
+				</div>
 			{/each}
+
+			{#if notDisplayedCount > 0 || !hideZeros}
+				<Button
+					styleClass="font-normal text-sm justify-start py-2"
+					link
+					on:click={() => toggleHideZeros(!hideZeros)}
+					>{hideZeros ? `Show ${notDisplayedCount} more networks` : `Hide more networks`}</Button
+				>
+			{/if}
 		</div>
 	{/if}
 </div>
