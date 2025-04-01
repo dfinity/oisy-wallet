e85e0e75df6095faf529b7b7404e5ed6d2822549
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-01 22:33:04 +0200

    feat(frontend): sign route opens in brwoser for PWA onc Android

--- src/frontend/src/routes/(sign)/sign/+page.ts
+++ src/frontend/src/routes/(sign)/sign/+page.ts
@@ -3,4 +3,19 @@ import type { LoadEvent } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 // We reset the data because the "sign" route operates without a network or token selected.
-export const load: PageLoad = (_$event: LoadEvent): RouteParams => resetRouteParams();
+export const load: PageLoad = (_$event: LoadEvent): RouteParams => {
+	if (typeof window !== 'undefined') {
+		const isPWA = window.matchMedia('(display-mode: standalone)').matches;
+		const isAndroid = /Android/i.test(navigator.userAgent);
+
+		// The `/sign` route does not work in the PWA for Android users
+		if (isPWA && isAndroid) {
+			// Open the exact same URL in the browser (escaping the PWA shell)
+			const fullUrl = window.location.href;
+			window.open(fullUrl, '_blank');
+			window.close(); // Attempt to close PWA tab (may not always work)
+		}
+	}
+
+	return resetRouteParams();
+};
