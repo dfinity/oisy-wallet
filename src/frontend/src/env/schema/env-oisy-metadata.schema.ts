4f9ffe598adc70a56587633c8bbf03bb3646630f
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-04-01 11:37:34 +0200

    feat(frontend): implements referral modal (#5530)
    # Motivation
    
    As a user i want to have a modal that displays my referral code and
    allows me to share it with friends or other people. It should also show
    how many people have registered using my referral code.
    
    # Changes
    
    - add referral modal
    
    # Tests
    
    
    
    https://github.com/user-attachments/assets/791145d2-696b-43cc-a65c-58693de07395
    
    when clicking on the share button:
    <img width="493" alt="image"
    src="https://github.com/user-attachments/assets/17e1309e-aaeb-4cf1-97c1-576adfe15f92"
    />
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/env/schema/env-oisy-metadata.schema.ts
+++ src/frontend/src/env/schema/env-oisy-metadata.schema.ts
@@ -10,5 +10,6 @@ export const OisyMetadataSchema = z.object({
 	OISY_DOCS_URL: z.string().url(),
 	OISY_SUPPORT_URL: z.string().url(),
 	OISY_REWARDS_URL: z.string().url(),
+	OISY_REFERRAL_URL: z.string().url(),
 	OISY_REFERRAL_TWITTER_URL: z.string().url()
 });
--- src/frontend/src/lib/components/ui/ButtonCloseModal.svelte
+++ src/frontend/src/lib/components/ui/ButtonCloseModal.svelte
@@ -2,11 +2,13 @@
 	import Button from '$lib/components/ui/Button.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { modalStore } from '$lib/stores/modal.store';
+
+	export let isPrimary = false;
 </script>
 
 <Button
 	paddingSmall
-	colorStyle="secondary-light"
+	colorStyle={isPrimary ? 'primary' : 'secondary-light'}
 	type="button"
 	fullWidth
 	on:click={modalStore.close}
