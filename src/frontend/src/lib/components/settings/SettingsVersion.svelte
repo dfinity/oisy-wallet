d54ac2e687893a08775eabdbd7ff10ebf6b53457
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-03 01:22:46 +0200

    format

--- src/frontend/src/lib/components/settings/SettingsVersion.svelte
+++ src/frontend/src/lib/components/settings/SettingsVersion.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
+	import { notEmptyString } from '@dfinity/utils';
 	import ExternalLink from '$lib/components/ui/ExternalLink.svelte';
-	import { APP_VERSION, TEST_FE } from '$lib/constants/app.constants';
+	import { APP_VERSION, GIT_BRANCH_NAME, GIT_COMMIT_HASH, TEST_FE } from '$lib/constants/app.constants';
 	import { OISY_NAME, OISY_REPO_URL } from '$lib/constants/oisy.constants';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { replaceOisyPlaceholders } from '$lib/utils/i18n.utils';
-	import { notEmptyString } from '@dfinity/utils';
 
 	const version = `v${APP_VERSION}`;
 	const releaseUrl = `${OISY_REPO_URL}/releases/tag/${version}`;
 
-	const commit = VITE_GIT_COMMIT_HASH;
-	const branch = VITE_GIT_BRANCH_NAME;
+	const commit = GIT_COMMIT_HASH;
+	const branch = GIT_BRANCH_NAME;
 </script>
 
 <p class="mt-24 text-center text-xs text-primary">
@@ -25,7 +25,7 @@
 {#if TEST_FE && notEmptyString(branch) && notEmptyString(commit)}
 	<p class="mt-24 text-center text-xs text-primary">
 		For development purpose, we show the branch and the commit only in test canisters.<br />
-		<b>Branch:</b> {branch}<br />
-		<b>Commit:</b> {commit}
+		<b>Branch:</b>{branch}<br />
+		<b>Commit:</b>{commit}
 	</p>
 {/if}
