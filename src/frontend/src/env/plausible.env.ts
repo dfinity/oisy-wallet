a75b8513a20214e0852e32619e89b64a9f9307e3
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-04-03 15:29:19 +0200

    feat(frontend): added feature flag plausible (#5608)
    # Motivation
    
    We aim to track analytics using Plausible. The goal is to ensure that
    pageviews and custom events are tracked correctly across the app.
    
    # Changes
    
    Added feature flag to test Plausible on staging.
    
    # Tests
    
    Updated tests.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/env/plausible.env.ts
+++ src/frontend/src/env/plausible.env.ts
@@ -1,1 +1,6 @@
-export const PLAUSIBLE_DOMAIN = 'oisy.com';
+import { PROD } from '$lib/constants/app.constants';
+
+export const PLAUSIBLE_DOMAIN = PROD ? 'oisy.com' : 'staging.oisy.com';
+
+export const PLAUSIBLE_ENABLED =
+	JSON.parse(import.meta.env.VITE_PLAUSIBLE_ENABLED ?? false) === true;
