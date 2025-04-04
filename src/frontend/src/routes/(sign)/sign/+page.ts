0a03717f5355472fe579dd9e35392e154b5b6919
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-04 23:32:14 +0200

    chore

--- src/frontend/src/routes/(sign)/sign/+page.ts
+++ src/frontend/src/routes/(sign)/sign/+page.ts
@@ -11,6 +11,7 @@ export const load: PageLoad = (_$event: LoadEvent): RouteParams => {
 		const fullUrl = new URL(window.location.href);
 		const hasEscaped = fullUrl.searchParams.has('pwa-escape');
 
+		// The `/sign` route does not work in the PWA for Android users
 		if (isPWA && isAndroid && !hasEscaped) {
 			// Open the exact same URL in the browser (escaping the PWA shell)
 			fullUrl.searchParams.set('pwa-escape', '1');
