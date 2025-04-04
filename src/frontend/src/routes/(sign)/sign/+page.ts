7a3d147847296876373843f5d73ddf1d0150273e
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-04 23:25:52 +0200

    refactor

--- src/frontend/src/routes/(sign)/sign/+page.ts
+++ src/frontend/src/routes/(sign)/sign/+page.ts
@@ -8,11 +8,13 @@ export const load: PageLoad = (_$event: LoadEvent): RouteParams => {
 		const isPWA = window.matchMedia('(display-mode: standalone)').matches;
 		const isAndroid = /Android/i.test(navigator.userAgent);
 
-		// The `/sign` route does not work in the PWA for Android users
-		if (isPWA && isAndroid) {
+		const fullUrl = new URL(window.location.href);
+		const hasEscaped = fullUrl.searchParams.has('pwa-escape');
+
+		if (isPWA && isAndroid && !hasEscaped) {
 			// Open the exact same URL in the browser (escaping the PWA shell)
-			const fullUrl = window.location.href;
-			window.open(fullUrl, '_blank');
+			fullUrl.searchParams.set('pwa-escape', '1');
+			window.open(fullUrl.toString(), '_blank');
 		}
 	}
 
