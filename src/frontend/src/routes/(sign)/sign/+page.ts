bed09048e026116b2f6a488e713577c81d27462c
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-04 23:33:02 +0200

    fix

--- src/frontend/src/routes/(sign)/sign/+page.ts
+++ src/frontend/src/routes/(sign)/sign/+page.ts
@@ -3,17 +3,17 @@ import type { LoadEvent } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 // We reset the data because the "sign" route operates without a network or token selected.
-export const load: PageLoad = (_$event: LoadEvent): RouteParams => {
+export const load: PageLoad = ({ url }: LoadEvent): RouteParams => {
 	if (typeof window !== 'undefined') {
 		const isPWA = window.matchMedia('(display-mode: standalone)').matches;
 		const isAndroid = /Android/i.test(navigator.userAgent);
 
-		const fullUrl = new URL(window.location.href);
-		const hasEscaped = fullUrl.searchParams.has('pwa-escape');
+		const hasEscaped = url.searchParams.has('pwa-escape');
 
 		// The `/sign` route does not work in the PWA for Android users
 		if (isPWA && isAndroid && !hasEscaped) {
 			// Open the exact same URL in the browser (escaping the PWA shell)
+			const fullUrl = new URL(window.location.href);
 			fullUrl.searchParams.set('pwa-escape', '1');
 			window.open(fullUrl.toString(), '_blank');
 		}
