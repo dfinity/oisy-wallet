a4f32e3d53551a6721d76946c45dddc624034586
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-04-07 13:36:07 +0200

    Updated tests

--- src/frontend/src/tests/routes/layout.spec.ts
+++ src/frontend/src/tests/routes/layout.spec.ts
@@ -5,49 +5,6 @@ import App from '$routes/+layout.svelte';
 import { render } from '@testing-library/svelte';
 import { beforeAll, describe, expect, it, vi } from 'vitest';
 
-vi.mock('$lib/services/worker.auth.services', () => ({
-	initAuthWorker: vi.fn().mockResolvedValue({
-		syncAuthIdle: vi.fn()
-	})
-}));
-
-vi.mock('$lib/services/analytics.services', async () => {
-	const actual = await vi.importActual<typeof import('$lib/services/analytics.services')>(
-		'$lib/services/analytics.services'
-	);
-	return {
-		...actual,
-		initAnalytics: vi
-			.fn()
-			.mockRejectedValue(
-				new Error('Unexpected issue while syncing the status of your authentication.')
-			),
-		initPlausibleAnalytics: vi.fn()
-	};
-});
-
-vi.mock('$lib/stores/auth.store', async (importOriginal) => {
-	const actual = await importOriginal<typeof import('$lib/stores/auth.store')>();
-	return {
-		...actual,
-		authStore: {
-			...actual.authStore,
-			sync: vi.fn().mockResolvedValue(undefined)
-		}
-	};
-});
-
-vi.mock('$lib/stores/i18n.store', async (importOriginal) => {
-	const actual = await importOriginal<typeof import('$lib/stores/i18n.store')>();
-	return {
-		...actual,
-		i18n: {
-			...actual.i18n,
-			init: vi.fn().mockResolvedValue(undefined)
-		}
-	};
-});
-
 beforeAll(() => {
 	Object.defineProperty(window, 'matchMedia', {
 		writable: true,
@@ -70,24 +27,17 @@ describe('App Layout', () => {
 	});
 
 	it('should initialize all services in parallel', () => {
-		vi.clearAllMocks();
+		const spyAuthSync = vi.spyOn(authStore, 'sync');
+		const spyInitAnalytics = vi.spyOn(analytics, 'initAnalytics');
+		const spyInitPlausible = vi.spyOn(analytics, 'initPlausibleAnalytics');
+		const spyI18n = vi.spyOn(i18n, 'init');
 
 		render(App);
 
-		expect(authStore.sync).toHaveBeenCalledTimes(1);
-		expect(analytics.initAnalytics).toHaveBeenCalledTimes(1);
-		expect(analytics.initPlausibleAnalytics).toHaveBeenCalledTimes(1);
-		expect(i18n.init).toHaveBeenCalledTimes(1);
-	});
-
-	it('should initialize auth store on mount', () => {
-		const spy = vi.spyOn(authStore, 'sync');
-
-		expect(spy).not.toHaveBeenCalled();
-
-		render(App);
-
-		expect(spy).toHaveBeenCalledTimes(1);
+		expect(spyAuthSync).toHaveBeenCalledTimes(1);
+		expect(spyInitAnalytics).toHaveBeenCalledTimes(1);
+		expect(spyInitPlausible).toHaveBeenCalledTimes(1);
+		expect(spyI18n).toHaveBeenCalledTimes(1);
 	});
 
 	it('should initialize analytics tracking on mount', () => {
