beab4b117133db7474a42d94c9eed68d03db5033
daviddecentage <david.farina@dfinity.org>
2025-04-08 14:25:01 +0200

    update

--- src/frontend/src/lib/components/ui/NotificationBlob.svelte
+++ src/frontend/src/lib/components/ui/NotificationBlob.svelte
@@ -5,7 +5,7 @@
 
 <div class="relative">
 	<div
-		class="duration-250 absolute h-2 w-2 rounded-full bg-brand-primary shadow shadow-white transition"
+		class="duration-250 shadow-primary absolute h-2 w-2 rounded-full bg-brand-primary shadow transition"
 		class:-top-1={position.indexOf('top') >= 0}
 		class:-left-1={position.indexOf('left') >= 0}
 		class:-right-1={position.indexOf('right') >= 0}
