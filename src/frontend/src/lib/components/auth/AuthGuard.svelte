ea2d46f1614cdc085a6565af2a7628f63cba2b90
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-04-09 15:58:16 +0200

    feat(frontend): uses auth help modal (#5744)
    # Motivation
    
    When a user starts the login process and then closes it, the new help
    modal should appear.
    
    # Changes
    
    - opens auth help modal
    
    # Tests
    
    
    
    https://github.com/user-attachments/assets/5d6f5788-756a-4e85-990f-171303aed707
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/auth/AuthGuard.svelte
+++ src/frontend/src/lib/components/auth/AuthGuard.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import { nonNullish } from '@dfinity/utils';
 	import { fade } from 'svelte/transition';
+	import AuthHelpModal from '$lib/components/auth/AuthHelpModal.svelte';
 	import LandingPage from '$lib/components/auth/LandingPage.svelte';
 	import { authNotSignedIn } from '$lib/derived/auth.derived';
+	import { modalAuthHelp, modalAuthHelpData } from '$lib/derived/modal.derived';
 </script>
 
 {#if $authNotSignedIn}
@@ -10,4 +13,8 @@
 	<div in:fade class="md:block md:overflow-y-auto">
 		<slot />
 	</div>
+{/if}
+
+{#if $modalAuthHelp && nonNullish($modalAuthHelpData)}
+	<AuthHelpModal usesIdentityHelp={$modalAuthHelpData} />
 {/if}
