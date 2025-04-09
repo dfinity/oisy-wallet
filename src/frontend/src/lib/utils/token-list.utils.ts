8aef0141e0a4de36b0cc6deb5289b4ba74a3f88b
daviddecentage <david.farina@dfinity.org>
2025-04-09 19:01:25 +0200

    update

--- src/frontend/src/lib/utils/token-list.utils.ts
+++ src/frontend/src/lib/utils/token-list.utils.ts
@@ -1,7 +1,6 @@
 import type { TokenUi } from '$lib/types/token';
 import type { TokenUiOrGroupUi } from '$lib/types/token-group';
 import { isTokenUiGroup } from '$lib/utils/token-group.utils';
-import { nonNullish } from '@dfinity/utils';
 
 const getFilterCondition = ({ filter, token }: { filter: string; token: TokenUi }) =>
 	token.name.toLowerCase().indexOf(filter.toLowerCase()) >= 0 ||
@@ -17,20 +16,21 @@ export const getFilteredTokenList: ({
 	list.reduce<TokenUiOrGroupUi[]>((acc, item) => {
 		if (filter === '') {
 			return [...acc, item];
-		}
-		if (!isTokenUiGroup(item)) {
-			const token = item.token;
-
-			if (getFilterCondition({ filter, token })) {
-				return [...acc, item];
-			}
 		} else {
-			const group = item.group;
-			const matchesGroup = group.tokens.some((token) => getFilterCondition({ filter, token }));
+			if (!isTokenUiGroup(item)) {
+				const token = item.token;
 
-			// If any token in the group matches, add the group to the result
-			if (nonNullish(matchesGroup)) {
-				return [...acc, item];
+				if (getFilterCondition({ filter, token })) {
+					return [...acc, item];
+				}
+			} else {
+				const group = item.group;
+				const matchesGroup = group.tokens.some((token) => getFilterCondition({ filter, token }));
+
+				// If any token in the group matches, add the group to the result
+				if (matchesGroup) {
+					return [...acc, item];
+				}
 			}
 		}
 
