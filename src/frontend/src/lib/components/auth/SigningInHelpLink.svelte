9fa9f99447e1f5b3eb4e29311837ebca4bd9b18f
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-04-09 16:20:34 +0200

    feat(frontend): adds need help sign in link (#5745)
    # Motivation
    
    Users should be able to click a `Need help signing in?` button to get
    some information about `Internet Identity` and the possibility to start
    a "legacy" signIn.
    
    # Changes
    
    - adds `Need help signing in?` button
    
    # Tests
    
    
    
    https://github.com/user-attachments/assets/dc59ab2a-cbfb-4417-aa98-395bfff71491
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/frontend/src/lib/components/auth/SigningInHelpLink.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/Button.svelte';
+	import { TRACK_HELP_SIGNING_IN } from '$lib/constants/analytics.contants';
+	import { trackEvent } from '$lib/services/analytics.services';
+	import { i18n } from '$lib/stores/i18n.store';
+	import { modalStore } from '$lib/stores/modal.store';
+
+	export let styleClass = '';
+
+	const onClick = async () => {
+		await trackEvent({
+			name: TRACK_HELP_SIGNING_IN
+		});
+		modalStore.openAuthHelp(true);
+	};
+</script>
+
+<span class={`${styleClass} flex flex-row gap-1`}>
+	{$i18n.auth.help.text.need_help}
+	<Button link inlineLink on:click={onClick}>{$i18n.auth.help.text.sign_in}</Button>
+</span>
