759a813d690cdb8e71660ddc4785304bc0c38038
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-09 22:28:13 +0200

    feat(frontend): switch from ic0 to internetcomputer (#5765)
    # Motivation
    
    This branch was already merged with PR
    https://github.com/dfinity/oisy-wallet/pull/5737 but then reverted with
    PR https://github.com/dfinity/oisy-wallet/pull/5762 because we thought
    it caused an issue in production.
    
    Since the cause was solved elsewhere (PR
    https://github.com/dfinity/oisy-wallet/pull/5764), we re-integrate the
    change.
    
    We want to switch the Internet Identity domain from `ic0` to
    `internetcomputer`.
    
    # Changes
    
    - changes Internet Identity domain
    
    # Tests
    
    I deployed the changes to `fe-1` and signedIn with an "old" Internet
    Identity. It worked and also the ICP Principal did not change.
    
    ---------
    
    Co-authored-by: AlessandroBonomo <bonomo.alessandro@yahoo.de>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Max <bitdivine@users.noreply.github.com>
    Co-authored-by: BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>

--- scripts/build.backend.args.sh
+++ scripts/build.backend.args.sh
@@ -70,7 +70,7 @@ fi
 
 # URL used by II-issuer in the id_alias-verifiable credentials (hard-coded in II)
 # Represents more an ID than a URL
-II_VC_URL="https://identity.ic0.app"
+II_VC_URL="https://identity.internetcomputer.org"
 
 echo "Deploying backend with the following arguments: ${POUH_ISSUER_VC_URL}"
 
