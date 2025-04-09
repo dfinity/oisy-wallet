4c7ad335536d002552af124f48bd95b0e68eb24d
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-04-09 16:55:06 +0200

    refactor(frontend): TextWithLogo accepting slot for icon (#5750)
    # Motivation
    
    For the upcoming Network icon refactoring we need the TextWithLogo to
    accept a slot for the icon.
    
    # Changes
    
    Refactored how the icon is rendered (from slot) and adjusted the
    occurrences.
    
    # Tests
    
    Nothing changed:
    
    ![image](https://github.com/user-attachments/assets/a122f09b-9c0b-4e7b-88a0-ed75dbcecdf6)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/frontend/src/lib/components/networks/NetworkWithLogo.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import NetworkLogo from '$lib/components/networks/NetworkLogo.svelte';
+	import TextWithLogo from '$lib/components/ui/TextWithLogo.svelte';
+	import type { Network } from '$lib/types/network';
+
+	export let network: Network;
+</script>
+
+<TextWithLogo name={network.name}>
+	<NetworkLogo slot="icon" {network} />
+</TextWithLogo>
--- src/frontend/src/lib/components/ui/TextWithLogo.svelte
+++ src/frontend/src/lib/components/ui/TextWithLogo.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
 	import { nonNullish } from '@dfinity/utils';
-	import Logo from '$lib/components/ui/Logo.svelte';
-	import { i18n } from '$lib/stores/i18n.store';
-	import { replacePlaceholders } from '$lib/utils/i18n.utils';
 
 	export let name: string;
-	export let icon: string | undefined;
 	export let logo: 'start' | 'end' = 'end';
 	export let description: string | undefined = undefined;
 </script>
@@ -23,5 +19,5 @@
 			<span class="text-left text-xs leading-none text-tertiary">{description}</span>
 		{/if}
 	</span>
-	<Logo src={icon} alt={replacePlaceholders($i18n.core.alt.logo, { $name: name })} />
+	<slot name="icon" />
 </span>
