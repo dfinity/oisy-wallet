202d24a1210c141e25cf042c98cd44a97a47ea8e
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-04-09 14:44:02 +0200

    resolves lint issues

--- src/frontend/src/lib/components/auth/SigningInHelpLink.svelte
+++ src/frontend/src/lib/components/auth/SigningInHelpLink.svelte
@@ -3,9 +3,8 @@
 	import { trackEvent } from '$lib/services/analytics.services';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { modalStore } from '$lib/stores/modal.store';
-	import { replaceOisyPlaceholders } from '$lib/utils/i18n.utils';
+	import Button from "$lib/components/ui/Button.svelte";
 
-	export let noUnderline = false;
 	export let styleClass = '';
 
 	const onClick = async () => {
@@ -16,13 +15,7 @@
 	};
 </script>
 
-<span class={`${styleClass}`}>
+<span class={`${styleClass} flex flex-row gap-1`}>
 	{$i18n.auth.help.text.need_help}
-	<a
-		onclick={onClick}
-		aria-label={replaceOisyPlaceholders($i18n.auth.help.alt.sign_in)}
-		class:no-underline={noUnderline}
-	>
-		{$i18n.auth.help.text.sign_in}
-	</a>
+	<Button link inlineLink on:click={onClick}>{$i18n.auth.help.text.sign_in}</Button>
 </span>
