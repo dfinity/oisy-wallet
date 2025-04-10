a0aa91afc36404dd2524abae85f2b8297497fd2a
daviddecentage <david.farina@dfinity.org>
2025-04-10 16:03:14 +0200

    update

--- src/frontend/src/lib/utils/token-list.utils.ts
+++ src/frontend/src/lib/utils/token-list.utils.ts
@@ -2,17 +2,17 @@ import type { TokenUi } from '$lib/types/token';
 import type { TokenUiOrGroupUi } from '$lib/types/token-group';
 import { isTokenUiGroup } from '$lib/utils/token-group.utils';
 
-const getFilterCondition = ({ filter, token }: { filter: string; token: TokenUi }) =>
+const getFilterCondition = ({ filter, token }: { filter: string; token: TokenUi }): boolean =>
 	token.name.toLowerCase().indexOf(filter.toLowerCase()) >= 0 ||
 	token.symbol.toLowerCase().indexOf(filter.toLowerCase()) >= 0;
 
-export const getFilteredTokenList: ({
+export const getFilteredTokenList = ({
 	filter,
 	list
 }: {
 	filter: string;
 	list: TokenUiOrGroupUi[];
-}) => TokenUiOrGroupUi[] = ({ filter, list }: { filter: string; list: TokenUiOrGroupUi[] }) =>
+}): TokenUiOrGroupUi[] =>
 	filter === ''
 		? list
 		: list.filter((item) =>
@@ -21,5 +21,10 @@ export const getFilteredTokenList: ({
 					: getFilterCondition({ filter, token: item.token })
 			);
 
-export const getFilteredTokenGroup = ({ filter, list }: { filter: string; list: TokenUi[] }) =>
-	list.filter((item) => getFilterCondition({ filter, token: item }));
+export const getFilteredTokenGroup = ({
+	filter,
+	list
+}: {
+	filter: string;
+	list: TokenUi[];
+}): TokenUi[] => list.filter((item) => getFilterCondition({ filter, token: item }));
