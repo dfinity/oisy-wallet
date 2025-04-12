2a496247581de346db66f2dc3dc761a7478464a0
Max Murphy <max@winning.black>
2025-04-12 13:01:46 +0200

    Less lazy lock

--- src/backend/src/state.rs
+++ src/backend/src/state.rs
@@ -1,5 +1,6 @@
-use candid::Principal;
 use std::sync::LazyLock;
+
+use candid::Principal;
 
 const MAINNET_CYCLES_LEDGER_CANISTER_ID: &str = "um5iw-rqaaa-aaaaq-qaaba-cai";
 const MAINNET_SIGNER_CANISTER_ID: &str = "grghe-syaaa-aaaar-qabyq-cai";
@@ -10,7 +11,11 @@ const MAINNET_SIGNER_CANISTER_ID: &str = "grghe-syaaa-aaaar-qabyq-cai";
 // - If that variable is not set for any reason, e.g. because we are not building with dfx, we
 //   default to the mainnet canister ID, which is also the recommended ID to use in test
 //   environments.
-    pub static CYCLES_LEDGER: LazyLock<Principal> = LazyLock::new(|| Principal::from_text(option_env!("CANISTER_ID_CYCLES_LEDGER").unwrap_or(MAINNET_CYCLES_LEDGER_CANISTER_ID))
-        .unwrap_or_else(|e| unreachable!("The cycles_ledger canister ID from DFX and mainnet are valid and should have been parsed.  Is this being compiled in some strange way? {e}")));
-    pub static SIGNER: LazyLock<Principal> = LazyLock::new(|| Principal::from_text(option_env!("CANISTER_ID_SIGNER").unwrap_or(MAINNET_SIGNER_CANISTER_ID))
-        .unwrap_or_else(|e| unreachable!("The signer canister ID from mainnet or dfx valid and should have been parsed.  Is this being compiled in some strange way? {e}")));
+pub static CYCLES_LEDGER: LazyLock<Principal> = LazyLock::new(|| {
+    Principal::from_text(option_env!("CANISTER_ID_CYCLES_LEDGER").unwrap_or(MAINNET_CYCLES_LEDGER_CANISTER_ID))
+        .unwrap_or_else(|e| unreachable!("The cycles_ledger canister ID from DFX and mainnet are valid and should have been parsed.  Is this being compiled in some strange way? {e}"))
+});
+pub static SIGNER: LazyLock<Principal> = LazyLock::new(|| {
+    Principal::from_text(option_env!("CANISTER_ID_SIGNER").unwrap_or(MAINNET_SIGNER_CANISTER_ID))
+        .unwrap_or_else(|e| unreachable!("The signer canister ID from mainnet or dfx valid and should have been parsed.  Is this being compiled in some strange way? {e}"))
+});
