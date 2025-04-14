59c27b0f625e4f50349a4e81ff818c75a68582aa
daviddecentage <david.farina@dfinity.org>
2025-04-14 10:44:57 +0200

    update

--- src/frontend/src/lib/components/ui/SlidingInput.svelte
+++ src/frontend/src/lib/components/ui/SlidingInput.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { Backdrop } from '@dfinity/gix-components';
 	import { onMount } from 'svelte';
-	import { quintOut } from 'svelte/easing';
 	import { fade, slide } from 'svelte/transition';
 	import { afterNavigate } from '$app/navigation';
 	import IconClose from '$lib/components/icons/lucide/IconClose.svelte';
@@ -9,12 +8,16 @@
 	import InputTextWithAction from '$lib/components/ui/InputTextWithAction.svelte';
 	import { SLIDE_PARAMS } from '$lib/constants/transition.constants.js';
 	import { i18n } from '$lib/stores/i18n.store';
+	import { nonNullish } from '@dfinity/utils';
 
 	export let testIdPrefix: string;
 	export let inputValue: string;
 	export let inputPlaceholder: string | undefined;
 	export let disabled = false;
 	export let ariaLabel: string;
+
+	let hasOverflowableSlot: boolean;
+	$: hasOverflowableSlot = nonNullish($$slots['overflowable-element']);
 
 	let visible = false;
 
@@ -61,52 +64,60 @@
 	});
 </script>
 
-<div class="z-3 absolute right-0 w-full">
-	{#if visible}
-		<div
-			in:slide={{ ...SLIDE_PARAMS, axis: 'x' }}
-			out:fade={{ delay: 0, duration: 250, easing: quintOut }}
-			class="input-field condensed absolute right-0 -mt-[11px] mr-px flex w-full overflow-hidden transition-all duration-300 md:w-[250px]"
+<div class="relative flex w-full">
+	<div class="flex pr-12">
+		<slot name="overflowable-element" />
+	</div>
+	<div class="z-3 absolute right-0 w-full">
+		{#if visible}
+			<div
+				in:slide={{ ...SLIDE_PARAMS, axis: 'x' }}
+				out:fade
+				class="input-field condensed absolute right-0 -mt-[11px] mr-px flex overflow-hidden"
+				class:w-full={hasOverflowableSlot}
+				class:md:w-[250px]={hasOverflowableSlot}
+				class:w-[250px]={!hasOverflowableSlot}
+			>
+				<InputTextWithAction
+					bind:inputElement
+					name="slidingInput"
+					placeholder={inputPlaceholder ?? ''}
+					bind:value={inputValue}
+					testId={`${testIdPrefix}-input`}
+					autofocus
+				/>
+				{#if inputValue !== ''}
+					<div class="top-3.25 absolute right-12 bg-primary" transition:fade>
+						<ButtonIcon
+							ariaLabel={$i18n.core.text.clear_filter}
+							colorStyle="muted"
+							on:click={handleClear}
+							link={false}
+							testId={`${testIdPrefix}-clear-btn`}
+						>
+							<IconClose slot="icon" size="18" />
+						</ButtonIcon>
+					</div>
+				{/if}
+			</div>
+		{/if}
+		<ButtonIcon
+			bind:button
+			on:click={handleOpen}
+			{disabled}
+			link={false}
+			colorStyle="muted"
+			styleClass={`absolute right-[5px] ${visible ? 'active' : ''}`}
+			{ariaLabel}
+			testId={`${testIdPrefix}-open-btn`}
 		>
-			<InputTextWithAction
-				bind:inputElement
-				name="slidingInput"
-				placeholder={inputPlaceholder ?? ''}
-				bind:value={inputValue}
-				testId={`${testIdPrefix}-input`}
-				autofocus
-			/>
-			{#if inputValue !== ''}
-				<div
-					class="transition-bg duration-250 top-3.25 absolute right-12 bg-primary"
-					transition:fade
-				>
-					<ButtonIcon
-						ariaLabel={$i18n.core.text.clear_filter}
-						styleClass="text-tertiary"
-						on:click={handleClear}
-						testId={`${testIdPrefix}-clear-btn`}><IconClose slot="icon" size="18" /></ButtonIcon
-					>
-				</div>
-			{/if}
+			<slot name="icon" slot="icon" />
+		</ButtonIcon>
+	</div>
+
+	{#if visible && inputValue === ''}
+		<div class="z-2 fixed bottom-0 left-0 right-0 top-0">
+			<Backdrop invisible on:nnsClose={handleClose} />
 		</div>
 	{/if}
-	<ButtonIcon
-		bind:button
-		on:click={handleOpen}
-		{disabled}
-		link={false}
-		colorStyle="muted"
-		styleClass={`absolute right-[5px] ${visible ? 'active' : ''}`}
-		{ariaLabel}
-		testId={`${testIdPrefix}-open-btn`}
-	>
-		<slot name="icon" slot="icon" />
-	</ButtonIcon>
 </div>
-
-{#if visible && inputValue === ''}
-	<div class="fixed bottom-0 left-0 right-0 top-0 z-0">
-		<Backdrop invisible on:nnsClose={handleClose} />
-	</div>
-{/if}
--- src/frontend/src/lib/components/ui/SlidingInputBoundary.svelte
+++
@@ -1,9 +0,0 @@
-<!--
-	We need this boundary element to define how far out the SlidingInput can go.
--->
-<div class="relative flex w-full">
-	<div class="flex pr-12">
-		<slot name="content" />
-	</div>
-	<slot name="input" />
-</div>
