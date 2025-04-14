1ab0b26fab84cd5efce76900dbcaff1bd73c4fb2
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-04-14 08:51:00 +0200

    feat(frontend): added beta domain to plausible (#5822)
    # Motivation
    
    This change simplifies the selection of the correct Plausible Analytics
    domain based on the environment and adds support for tracking analytics
    in the beta environment.
    
    # Changes
    
    Added a new condition to dynamically select the correct domain for
    tracking analytics across different environments (production, beta,
    staging, and test).

--- src/frontend/src/env/plausible.env.ts
+++ src/frontend/src/env/plausible.env.ts
@@ -1,6 +1,14 @@
-import { PROD } from '$lib/constants/app.constants';
+import { BETA, PROD, STAGING, TEST_FE } from '$lib/constants/app.constants';
 import { parseBoolEnvVar } from '$lib/utils/env.utils';
 
-export const PLAUSIBLE_DOMAIN = PROD ? 'oisy.com' : 'staging.oisy.com';
+export const PLAUSIBLE_DOMAIN = PROD
+	? 'oisy.com'
+	: BETA
+		? 'beta.oisy.com'
+		: TEST_FE // Currently using 'fe1.oisy.com' for TEST_FE env, but can be adjusted to 'fe#.oisy.com' if needed.
+			? 'fe1.oisy.com'
+			: STAGING
+				? 'staging.oisy.com'
+				: null;
 
 export const PLAUSIBLE_ENABLED = parseBoolEnvVar(import.meta.env.VITE_PLAUSIBLE_ENABLED);
