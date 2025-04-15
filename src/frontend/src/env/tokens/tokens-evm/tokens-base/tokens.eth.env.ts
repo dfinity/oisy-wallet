2608ef37fd4cc3ce7a01df27d820230421092c68
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-15 14:13:36 +0200

    rename

--- src/frontend/src/env/tokens/tokens-evm/tokens-base/tokens.eth.env.ts
+++ src/frontend/src/env/tokens/tokens-evm/tokens-base/tokens.eth.env.ts
@@ -6,37 +6,37 @@ import eth from '$icp-eth/assets/eth.svg';
 import type { RequiredToken, TokenId } from '$lib/types/token';
 import { parseTokenId } from '$lib/validation/token.validation';
 
-export const BASE_ETHEREUM_DEFAULT_DECIMALS = 18;
+export const BASE_ETH_DEFAULT_DECIMALS = 18;
 
-const BASE_ETHEREUM_SYMBOL = 'ETH';
+const BASE_ETH_SYMBOL = 'ETH';
 
-export const BASE_ETHEREUM_TOKEN_ID: TokenId = parseTokenId(BASE_ETHEREUM_SYMBOL);
+export const BASE_ETH_TOKEN_ID: TokenId = parseTokenId(BASE_ETH_SYMBOL);
 
-export const BASE_ETHEREUM_TOKEN: RequiredToken = {
-	id: BASE_ETHEREUM_TOKEN_ID,
+export const BASE_ETH_TOKEN: RequiredToken = {
+	id: BASE_ETH_TOKEN_ID,
 	network: BASE_NETWORK,
 	standard: 'ethereum',
 	category: 'default',
 	name: 'Ethereum',
-	symbol: BASE_ETHEREUM_SYMBOL,
-	decimals: BASE_ETHEREUM_DEFAULT_DECIMALS,
+	symbol: BASE_ETH_SYMBOL,
+	decimals: BASE_ETH_DEFAULT_DECIMALS,
 	icon: eth,
 	buy: {
 		onramperId: 'eth_base'
 	}
 };
 
-export const BASE_SEPOLIA_SYMBOL = 'SepoliaETH';
+export const BASE_SEPOLIA_ETH_SYMBOL = 'SepoliaETH';
 
-export const BASE_SEPOLIA_TOKEN_ID: TokenId = parseTokenId(BASE_SEPOLIA_SYMBOL);
+export const BASE_SEPOLIA_ETH_TOKEN_ID: TokenId = parseTokenId(BASE_SEPOLIA_ETH_SYMBOL);
 
-export const BASE_SEPOLIA_TOKEN: RequiredToken = {
-	id: BASE_SEPOLIA_TOKEN_ID,
+export const BASE_SEPOLIA_ETH_TOKEN: RequiredToken = {
+	id: BASE_SEPOLIA_ETH_TOKEN_ID,
 	network: BASE_SEPOLIA_NETWORK,
 	standard: 'ethereum',
 	category: 'default',
 	name: 'SepoliaETH',
-	symbol: BASE_SEPOLIA_SYMBOL,
-	decimals: BASE_ETHEREUM_DEFAULT_DECIMALS,
+	symbol: BASE_SEPOLIA_ETH_SYMBOL,
+	decimals: BASE_ETH_DEFAULT_DECIMALS,
 	icon: eth
 };
