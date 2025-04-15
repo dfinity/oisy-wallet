4be013a1cebfe2e7ad6e2b8b28001f6357dc26d6
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-15 10:37:08 +0200

    fix

--- src/frontend/src/env/networks/networks-evm/networks.evm.env.ts
+++ src/frontend/src/env/networks/networks-evm/networks.evm.env.ts
@@ -1,6 +1,7 @@
 import { SUPPORTED_BASE_NETWORKS } from '$env/networks/networks-evm/networks.evm.base.env';
 import { SUPPORTED_BSC_NETWORKS } from '$env/networks/networks-evm/networks.evm.bsc.env';
 import type { EthereumNetwork } from '$eth/types/network';
+import { parseBoolEnvVar } from '$lib/utils/env.utils';
 
 export const EVM_NETWORKS_ENABLED = parseBoolEnvVar(import.meta.env.VITE_EVM_NETWORKS_ENABLED);
 
