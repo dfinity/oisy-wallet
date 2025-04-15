b9d7ca5736b72385619b05fd79afc7d2cb00ed93
Stefan Berger <stefan.berger@inacta.ch>
2025-04-15 09:01:13 +0200

    Add VEUR to network config

--- src/frontend/src/env/networks/networks.icrc.env.ts
+++ src/frontend/src/env/networks/networks.icrc.env.ts
@@ -459,6 +459,14 @@ const VCHF_IC_DATA: IcInterface | undefined = nonNullish(ADDITIONAL_ICRC_PRODUCT
 		}
 	: undefined;
 
+const VEUR_IC_DATA: IcInterface | undefined = nonNullish(ADDITIONAL_ICRC_PRODUCTION_DATA?.VEUR)
+	? {
+			...ADDITIONAL_ICRC_PRODUCTION_DATA.VEUR,
+			position: 22
+		}
+	: undefined;
+
+
 export const CKERC20_LEDGER_CANISTER_TESTNET_IDS: CanisterIdText[] = [
 	...(nonNullish(LOCAL_CKUSDC_LEDGER_CANISTER_ID) ? [LOCAL_CKUSDC_LEDGER_CANISTER_ID] : []),
 	...(nonNullish(CKUSDC_STAGING_DATA?.ledgerCanisterId)
@@ -533,7 +541,8 @@ const ADDITIONAL_ICRC_TOKENS: IcInterface[] = [
 	...(nonNullish(vUSD_IC_DATA) ? [vUSD_IC_DATA] : []),
 	...(nonNullish(RUGGY_IC_DATA) ? [RUGGY_IC_DATA] : []),
 	...(nonNullish(NAK_IC_DATA) ? [NAK_IC_DATA] : []),
-	...(nonNullish(VCHF_IC_DATA) ? [VCHF_IC_DATA] : [])
+	...(nonNullish(VCHF_IC_DATA) ? [VCHF_IC_DATA] : []),
+	...(nonNullish(VEUR_IC_DATA) ? [VEUR_IC_DATA] : [])
 ];
 
 export const ICRC_TOKENS: IcInterface[] = [
