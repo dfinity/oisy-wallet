ae0293145bc4211792c0bdb2ad193f8f1722717f
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-04-15 10:07:49 +0200

    feat(frontend): writes some test cases for ButtonAuthenticateWithLicense.svelte (#5858)
    # Motivation
    
    We want to have some test cases for the component
    `ButtonAuthenticateWithLicense`.
    
    # Changes
    
    - adds some test cases
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/auth/SigningInHelpLink.svelte
+++ src/frontend/src/lib/components/auth/SigningInHelpLink.svelte
@@ -6,6 +6,7 @@
 	import { modalStore } from '$lib/stores/modal.store';
 
 	export let styleClass = '';
+	export let testId: string | undefined = undefined;
 
 	const onClick = () => {
 		trackEvent({
@@ -15,7 +16,7 @@
 	};
 </script>
 
-<span class={`${styleClass} flex flex-row justify-center gap-1 md:justify-start`}>
+<span data-tid={testId} class={`${styleClass} flex flex-row justify-center gap-1 md:justify-start`}>
 	{$i18n.auth.help.text.need_help}
 	<Button link inlineLink on:click={onClick}>{$i18n.auth.help.text.sign_in}</Button>
 </span>
