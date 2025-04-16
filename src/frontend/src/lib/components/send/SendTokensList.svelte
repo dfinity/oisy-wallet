3f2688e6885668e08d462e1dd77247367c38aef1
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-04-16 17:00:19 +0200

    refactor(frontend): Modal token list reusability (#5897)
    # Motivation
    
    For the upcoming manage tokens modal refactoring, we adjust the
    `ModalTokenList` to make it reusable for the new use case.
    
    # Changes
    
    - Added slot for no result message
    - Added slot for single token list item
    
    ---------
    
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>
    Co-authored-by: Max <bitdivine@users.noreply.github.com>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/send/SendTokensList.svelte
+++ src/frontend/src/lib/components/send/SendTokensList.svelte
@@ -3,8 +3,10 @@
 	import { createEventDispatcher } from 'svelte';
 	import { erc20UserTokensNotInitialized } from '$eth/derived/erc20.derived';
 	import ModalTokensList from '$lib/components/tokens/ModalTokensList.svelte';
+	import ModalTokensListItem from '$lib/components/tokens/ModalTokensListItem.svelte';
 	import ButtonCloseModal from '$lib/components/ui/ButtonCloseModal.svelte';
 	import { selectedNetwork } from '$lib/derived/network.derived';
+	import { i18n } from '$lib/stores/i18n.store';
 	import type { Token } from '$lib/types/token';
 
 	const dispatch = createEventDispatcher();
@@ -23,6 +25,14 @@
 	on:icTokenButtonClick={onIcTokenButtonClick}
 	networkSelectorViewOnly={nonNullish($selectedNetwork)}
 >
+	{#snippet tokenListItem(token, onClick)}
+		<ModalTokensListItem data={token} on:click={onClick} />
+	{/snippet}
+	{#snippet noResults()}
+		<p class="text-primary">
+			{$i18n.tokens.manage.text.all_tokens_zero_balance}
+		</p>
+	{/snippet}
 	{#snippet toolbar()}
 		<ButtonCloseModal />
 	{/snippet}
