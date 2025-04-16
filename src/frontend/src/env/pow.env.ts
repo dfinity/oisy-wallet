2a34d9e59f5849a38103fca79c833dd72efc6942
DecentAgeCoder <marco.oesch@dfinity.org>
2025-04-16 18:22:26 +0200

    Renamed POW_ENABLED and relocate POW constants

--- src/frontend/src/env/pow.env.ts
+++ src/frontend/src/env/pow.env.ts
@@ -1,4 +1,1 @@
-export const POW_ENABLED = JSON.parse(import.meta.env.VITE_POW_ENABLED ?? false) === true;
-export const POW_CHALLENGE_INTERVAL_MILLIS: number = JSON.parse(
-	import.meta.env.VITE_POW_CHALLENGE_INTERVALL_MILLIS ?? 120000
-);
+export const POW_FEATURE_ENABLED = JSON.parse(import.meta.env.VITE_POW_ENABLED ?? false) === true;
