56d910ee91a3979b3ff34fdc58395dbd11d6f854
Max <bitdivine@users.noreply.github.com>
2025-04-16 18:16:07 +0200

    docs(shared): User token comments (#5904)
    # Motivation
    The user token types are for EVM only. This is not clear from the
    Rustdoc.
    
    # Changes
    - Make it clear in the `UserToken` types that they are for EVM only.
    
    # Tests
    N/A
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/shared/src/types/token.rs
+++ src/shared/src/types/token.rs
@@ -1,11 +1,21 @@
 //! ERC20 specific user defined tokens
+//!
+//! Note: These are legacy types and are likely to be phased out or adapted to fit into a consistent
+//! cross-chain approach.
 use candid::{CandidType, Deserialize};
 use serde::Serialize;
 
 use crate::types::Version;
 
+/// EVM chain ID
+///
+/// IDs may be found on: <https://chainlist.org/>
 pub type ChainId = u64;
 
+/// EVM token information.
+///
+/// Note: This is a legacy type and is likely to be phased out or adapted to fit into a consistent
+/// cross-chain approach.
 #[derive(CandidType, Serialize, Deserialize, Clone, Eq, PartialEq, Debug)]
 pub struct UserToken {
     pub contract_address: String,
@@ -16,6 +26,10 @@ pub struct UserToken {
     pub enabled: Option<bool>,
 }
 
+/// Unique identifier for an EVM token.
+///
+/// Note: This is a legacy type and is likely to be phased out or adapted to fit into a consistent
+/// cross-chain approach.
 #[derive(CandidType, Deserialize, Clone)]
 pub struct UserTokenId {
     pub contract_address: String,
