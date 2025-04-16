310018e833e2178ca512ac3f36049ffa4dcb6ab9
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-16 11:26:49 +0200

    feat(frontend): create list of EVM supported networks (#5806)
    # Motivation
    
    Adding the BASE networks and the BSC networks to the list of supported
    networks.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/env/networks/networks-evm/networks.evm.env.ts
+++ src/frontend/src/env/networks/networks-evm/networks.evm.env.ts
@@ -1,2 +1,10 @@
-export const EVM_NETWORKS_ENABLED =
-	JSON.parse(import.meta.env.VITE_EVM_NETWORKS_ENABLED ?? false) === true;
+import { SUPPORTED_BASE_NETWORKS } from '$env/networks/networks-evm/networks.evm.base.env';
+import { SUPPORTED_BSC_NETWORKS } from '$env/networks/networks-evm/networks.evm.bsc.env';
+import type { EthereumNetwork } from '$eth/types/network';
+import { parseBoolEnvVar } from '$lib/utils/env.utils';
+
+export const EVM_NETWORKS_ENABLED = parseBoolEnvVar(import.meta.env.VITE_EVM_NETWORKS_ENABLED);
+
+export const SUPPORTED_EVM_NETWORKS: EthereumNetwork[] = EVM_NETWORKS_ENABLED
+	? [...SUPPORTED_BASE_NETWORKS, ...SUPPORTED_BSC_NETWORKS]
+	: [];
