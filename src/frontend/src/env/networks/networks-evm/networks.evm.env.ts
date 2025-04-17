7608da3a7379c8ec96b76815a005393c78e7728b
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-18 00:48:30 +0200

    fix

--- src/frontend/src/env/networks/networks-evm/networks.evm.env.ts
+++ src/frontend/src/env/networks/networks-evm/networks.evm.env.ts
@@ -1,6 +1,5 @@
 import { SUPPORTED_BASE_NETWORKS } from '$env/networks/networks-evm/networks.evm.base.env';
 import { SUPPORTED_BSC_NETWORKS } from '$env/networks/networks-evm/networks.evm.bsc.env';
-import { SUPPORTED_NETWORKS } from '$env/networks/networks.env';
 import type { EthereumNetwork } from '$eth/types/network';
 import type { Network, NetworkId } from '$lib/types/network';
 import { parseBoolEnvVar } from '$lib/utils/env.utils';
@@ -11,11 +10,11 @@ export const SUPPORTED_EVM_NETWORKS: EthereumNetwork[] = EVM_NETWORKS_ENABLED
 	? [...SUPPORTED_BASE_NETWORKS, ...SUPPORTED_BSC_NETWORKS]
 	: [];
 
-const SUPPORTED_EVM_MAINNET_NETWORKS: Network[] = SUPPORTED_NETWORKS.filter(
+const SUPPORTED_EVM_MAINNET_NETWORKS: Network[] = SUPPORTED_EVM_NETWORKS.filter(
 	({ env }) => env === 'mainnet'
 );
 
-const SUPPORTED_EVM_TESTNET_NETWORKS: Network[] = SUPPORTED_NETWORKS.filter(
+const SUPPORTED_EVM_TESTNET_NETWORKS: Network[] = SUPPORTED_EVM_NETWORKS.filter(
 	({ env }) => env === 'testnet'
 );
 
