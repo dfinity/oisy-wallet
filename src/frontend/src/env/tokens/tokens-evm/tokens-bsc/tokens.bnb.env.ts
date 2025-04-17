02b329cb36a07fdfbd44502d3fdd4c19a19090cc
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-17 09:39:45 +0200

    feat(frontend): add BNB native tokens for BNB Smart Chain network (#5870)
    # Motivation
    
    Addin BNB (mainnet and testnet) for BNB Smart Chain (BSC).
    
    ---------
    
    Co-authored-by: Max <bitdivine@users.noreply.github.com>

---
+++ src/frontend/src/env/tokens/tokens-evm/tokens-bsc/tokens.bnb.env.ts
@@ -0,0 +1,39 @@
+import { BSC_NETWORK, BSC_TESTNET_NETWORK } from '$env/networks/networks-evm/networks.evm.bsc.env';
+import bnb from '$evm/bsc/assets/bnb.svg';
+import type { RequiredToken, TokenId } from '$lib/types/token';
+import { parseTokenId } from '$lib/validation/token.validation';
+
+const BNB_DECIMALS = 18;
+
+const BNB_MAINNET_SYMBOL = 'BNB';
+
+export const BNB_MAINNET_TOKEN_ID: TokenId = parseTokenId(BNB_MAINNET_SYMBOL);
+
+export const BNB_MAINNET_TOKEN: RequiredToken = {
+	id: BNB_MAINNET_TOKEN_ID,
+	network: BSC_NETWORK,
+	standard: 'ethereum',
+	category: 'default',
+	name: 'BNB',
+	symbol: BNB_MAINNET_SYMBOL,
+	decimals: BNB_DECIMALS,
+	icon: bnb,
+	buy: {
+		onramperId: 'bnb_bsc'
+	}
+};
+
+const BNB_TESTNET_SYMBOL = 'BNB (Testnet)';
+
+export const BNB_TESTNET_TOKEN_ID: TokenId = parseTokenId(BNB_TESTNET_SYMBOL);
+
+export const BNB_TESTNET_TOKEN: RequiredToken = {
+	id: BNB_TESTNET_TOKEN_ID,
+	network: BSC_TESTNET_NETWORK,
+	standard: 'ethereum',
+	category: 'default',
+	name: 'BNB (Testnet)',
+	symbol: BNB_TESTNET_SYMBOL,
+	decimals: BNB_DECIMALS,
+	icon: bnb
+};
