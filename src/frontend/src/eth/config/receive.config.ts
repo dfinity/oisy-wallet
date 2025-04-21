60acf98a671a2f4cbd5177547576c47ec5bd56ca
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-04-21 16:44:33 +0200

    feat(frontend): receive wizard config (#5986)
    # Motivation
    
    Previously, the receive config was defined directly in the component. As
    a part of migration to the convert flow, I'm moving it to a separate
    file.

---
+++ src/frontend/src/eth/config/receive.config.ts
@@ -0,0 +1,20 @@
+import { howToConvertWizardSteps } from '$icp-eth/config/how-to-convert.config';
+import { type ConvertWizardStepsParams } from '$lib/config/convert.config';
+import { WizardStepsReceive } from '$lib/enums/wizard-steps';
+import type { WizardSteps } from '@dfinity/gix-components';
+
+export const receiveWizardSteps = ({
+	i18n,
+	sourceToken,
+	destinationToken
+}: ConvertWizardStepsParams): WizardSteps => [
+	{
+		name: WizardStepsReceive.RECEIVE,
+		title: i18n.receive.text.receive
+	},
+	{
+		name: WizardStepsReceive.QR_CODE,
+		title: i18n.receive.text.address
+	},
+	...howToConvertWizardSteps({ i18n, sourceToken, destinationToken })
+];
