bc55bd722a4dcb6d464ef432861355efceae7ea2
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-22 21:26:16 +0200

    feat(frontend): add EVM tokens in the balance loader (#6028)
    # Motivation
    
    Including EVM tokens in component `LoaderEthBalances`.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/eth/components/loaders/LoaderEthBalances.svelte
+++ src/frontend/src/eth/components/loaders/LoaderEthBalances.svelte
@@ -3,6 +3,7 @@
 	import type { Snippet } from 'svelte';
 	import { enabledEthereumTokens } from '$eth/derived/tokens.derived';
 	import { loadErc20Balances, loadEthBalances } from '$eth/services/eth-balance.services';
+	import { enabledEvmTokens } from '$evm/derived/tokens.derived';
 	import { ethAddress } from '$lib/derived/address.derived';
 	import { enabledErc20Tokens } from '$lib/derived/tokens.derived';
 
@@ -19,7 +20,7 @@
 
 		await Promise.allSettled([
 			// We might require Ethereum balance on IC network as well given that one can convert ckETH to ETH.
-			loadEthBalances($enabledEthereumTokens),
+			loadEthBalances([...$enabledEthereumTokens, ...$enabledEvmTokens]),
 			loadErc20Balances({
 				address: $ethAddress,
 				erc20Tokens: $enabledErc20Tokens
@@ -31,7 +32,7 @@
 
 	$effect(() => {
 		// To trigger the load function when any of the dependencies change.
-		[$ethAddress, $enabledEthereumTokens, $enabledErc20Tokens];
+		[$ethAddress, $enabledEthereumTokens, $enabledEvmTokens, $enabledErc20Tokens];
 		debounceLoad();
 	});
 </script>
