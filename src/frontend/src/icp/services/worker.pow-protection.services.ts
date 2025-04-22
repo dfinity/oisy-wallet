1231c15c5b0189e03759e5b3e9e1972620b809b3
DecentAgeCoder <marco.oesch@dfinity.org>
2025-04-22 14:07:28 +0200

    Using Pow Protector Worker interface

--- src/frontend/src/icp/services/worker.pow-protection.services.ts
+++ src/frontend/src/icp/services/worker.pow-protection.services.ts
@@ -1,11 +1,12 @@
-import type { PowProtectorWorkerInitResult } from '$icp/types/pow-protector-listener';
+import type { PowProtectorWorker, PowProtectorWorkerInitResult } from '$icp/types/pow-protector-listener';
 import type { PostMessage, PostMessageDataResponse } from '$lib/types/post-message';
 
-export const initPowProtectorWorker = async (): Promise<PowProtectorWorkerInitResult> => {
+export const initPowProtectorWorker: PowProtectorWorker = async (): Promise<PowProtectorWorkerInitResult> => {
 	const PowWorker = await import('$lib/workers/workers?worker');
 	const worker: Worker = new PowWorker.default();
 
-	worker.onmessage = ({ data: _data }: MessageEvent<PostMessage<PostMessageDataResponse>>) => {};
+	worker.onmessage = ({ data: _data }: MessageEvent<PostMessage<PostMessageDataResponse>>) => {
+	};
 
 	return {
 		start: () => {
