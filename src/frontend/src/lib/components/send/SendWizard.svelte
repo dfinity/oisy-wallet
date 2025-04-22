b21fd3635c7b876c3b9eb18e5d99c7673ffb708c
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-22 23:09:03 +0200

    factor

--- src/frontend/src/lib/components/send/SendWizard.svelte
+++ src/frontend/src/lib/components/send/SendWizard.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import { type WizardStep } from '@dfinity/gix-components';
+	import { nonNullish } from '@dfinity/utils';
 	import BtcSendTokenWizard from '$btc/components/send/BtcSendTokenWizard.svelte';
 	import EthSendTokenWizard from '$eth/components/send/EthSendTokenWizard.svelte';
 	import { selectedEthereumNetworkWithFallback } from '$eth/derived/network.derived';
 	import { ethereumToken } from '$eth/derived/token.derived';
+	import { selectedEvmNetwork } from '$evm/derived/network.derived';
+	import { evmNativeToken } from '$evm/derived/token.derived';
 	import IcSendTokenWizard from '$icp/components/send/IcSendTokenWizard.svelte';
 	import SendTokenContext from '$lib/components/send/SendTokenContext.svelte';
 	import { token } from '$lib/stores/token.store';
@@ -16,9 +19,6 @@
 		isNetworkIdEvm
 	} from '$lib/utils/network.utils';
 	import SolSendTokenWizard from '$sol/components/send/SolSendTokenWizard.svelte';
-	import { selectedEvmNetwork } from '$evm/derived/network.derived';
-	import { nonNullish } from '@dfinity/utils';
-	import { evmNativeToken } from '$evm/derived/token.derived';
 
 	export let source: string;
 	export let destination: string;
@@ -49,11 +49,12 @@
 			on:icQRCodeBack
 		/>
 	{:else if isNetworkIdEvm($token?.network.id) && nonNullish($selectedEvmNetwork) && nonNullish($evmNativeToken)}
+		<!--			TODO: use store evmNativeToken here when we adapt the fee context to fetch the EVM fees -->
 		<EthSendTokenWizard
 			{currentStep}
 			{formCancelAction}
 			sourceNetwork={$selectedEvmNetwork}
-			nativeEthereumToken={$evmNativeToken}
+			nativeEthereumToken={$ethereumToken}
 			bind:destination
 			bind:targetNetwork
 			bind:amount
