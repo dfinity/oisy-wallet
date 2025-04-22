06054f9802cbe836c1a9fd174981d0dbe1da8c6c
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-22 15:40:24 +0200

    feat(frontend): add SHIB token group data (#6009)
    # Motivation
    
    Splitting PR https://github.com/dfinity/oisy-wallet/pull/5918.
    
    We are going to use `groupData` to group similar ("twin") tokens.
    Currently we are using different props depending on the type of token
    (for example `twinToken` for ICRC, or `twinTokenSymbol` for ERC20). With
    groupData, we aim to have a single center of aggregation for "twin"
    tokens.
    
    In this PR we create the group for token SHIB.
    
    ---------
    
    Co-authored-by: daviddecentage <80264282+daviddecentage@users.noreply.github.com>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/frontend/src/env/tokens/groups/groups.shib.env.ts
@@ -0,0 +1,9 @@
+import shib from '$icp-eth/assets/shib.svg';
+
+const SHIB_TOKEN_GROUP_SYMBOL = 'SHIB';
+
+export const SHIB_TOKEN_GROUP = {
+	icon: shib,
+	name: 'SHIBA INU',
+	symbol: SHIB_TOKEN_GROUP_SYMBOL
+};
