6010f0c0ad0ef302f89c5bb148a923eadc3924bd
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-22 14:04:40 +0200

    feat(frontend): add WSTETH token group data

--- src/frontend/src/env/networks/networks.icrc.env.ts
+++ src/frontend/src/env/networks/networks.icrc.env.ts
@@ -9,6 +9,7 @@ import { EURC_TOKEN_GROUP } from '$env/tokens/groups/groups.eurc.env';
 import { LINK_TOKEN_GROUP } from '$env/tokens/groups/groups.link.env';
 import { OCT_TOKEN_GROUP } from '$env/tokens/groups/groups.oct.env';
 import { USDC_TOKEN_GROUP } from '$env/tokens/groups/groups.usdc.env';
+import { WSTETH_TOKEN_GROUP } from '$env/tokens/groups/groups.wsteth.env';
 import { EURC_TOKEN } from '$env/tokens/tokens-erc20/tokens.eurc.env';
 import { LINK_TOKEN, SEPOLIA_LINK_TOKEN } from '$env/tokens/tokens-erc20/tokens.link.env';
 import { OCT_TOKEN } from '$env/tokens/tokens-erc20/tokens.oct.env';
@@ -368,7 +369,8 @@ const CKWSTETH_IC_DATA: IcCkInterface | undefined = nonNullish(CKERC20_PRODUCTIO
 	? {
 			...CKERC20_PRODUCTION_DATA.ckWSTETH,
 			position: 8,
-			twinToken: WSTETH_TOKEN
+			twinToken: WSTETH_TOKEN,
+			groupData: WSTETH_TOKEN_GROUP
 		}
 	: undefined;
 
