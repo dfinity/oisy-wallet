e3c0f0290b4c36fe78782161f82e1261df4da28b
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-22 16:32:36 +0200

    feat(frontend): add WSTETH token group data (#6013)
    # Motivation
    
    Splitting PR https://github.com/dfinity/oisy-wallet/pull/5918.
    
    We are going to use `groupData` to group similar ("twin") tokens.
    Currently we are using different props depending on the type of token
    (for example `twinToken` for ICRC, or `twinTokenSymbol` for ERC20). With
    groupData, we aim to have a single center of aggregation for "twin"
    tokens.
    
    In this PR we create the group for token wstETH.

---
+++ src/frontend/src/env/tokens/groups/groups.wsteth.env.ts
@@ -0,0 +1,9 @@
+import wsteth from '$icp-eth/assets/wsteth.svg';
+
+const WSTETH_TOKEN_GROUP_SYMBOL = 'wstETH';
+
+export const WSTETH_TOKEN_GROUP = {
+	icon: wsteth,
+	name: 'Wrapped liquid staked Ether 2.0',
+	symbol: WSTETH_TOKEN_GROUP_SYMBOL
+};
