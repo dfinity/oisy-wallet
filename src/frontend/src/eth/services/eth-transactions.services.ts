8bbcedb231f17654c5becf8c9d5cb0b56fffd532
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-04-22 10:35:19 +0200

    feat(frontend): improves eth transaction loading errors (#5607)
    # Motivation
    
    Currently every time the request to load the ETH transactions fails, we
    display an error toast. In some cases this leads to multiple error
    toasts being displayed if the application has been idle. To improve the
    user experience we want to only show the error toast if the request
    fails 3 times in a row. In this way, the user will not be interrupted by
    unnecessary error messages when everything else works as expected.
    
    # Changes
    
    - improves error handling
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/eth/services/eth-transactions.services.ts
+++ src/frontend/src/eth/services/eth-transactions.services.ts
@@ -1,3 +1,4 @@
+import { ETHEREUM_NETWORK_SYMBOL } from '$env/networks/networks.eth.env';
 import { enabledErc20Tokens } from '$eth/derived/erc20.derived';
 import { etherscanProviders } from '$eth/providers/etherscan.providers';
 import { etherscanRests } from '$eth/rest/etherscan.rest';
@@ -18,14 +19,16 @@ import { get } from 'svelte/store';
 export const loadEthereumTransactions = ({
 	networkId,
 	tokenId,
-	updateOnly = false
+	updateOnly = false,
+	silent = false
 }: {
 	tokenId: TokenId;
 	networkId: NetworkId;
 	updateOnly?: boolean;
+	silent?: boolean;
 }): Promise<ResultSuccess> => {
 	if (isSupportedEthTokenId(tokenId)) {
-		return loadEthTransactions({ networkId, tokenId, updateOnly });
+		return loadEthTransactions({ networkId, tokenId, updateOnly, silent });
 	}
 
 	return loadErc20Transactions({ networkId, tokenId, updateOnly });
@@ -36,16 +39,19 @@ export const loadEthereumTransactions = ({
 export const reloadEthereumTransactions = (params: {
 	tokenId: TokenId;
 	networkId: NetworkId;
+	silent?: boolean;
 }): Promise<ResultSuccess> => loadEthereumTransactions({ ...params, updateOnly: true });
 
 const loadEthTransactions = async ({
 	networkId,
 	tokenId,
-	updateOnly = false
+	updateOnly = false,
+	silent = false
 }: {
 	networkId: NetworkId;
 	tokenId: TokenId;
 	updateOnly?: boolean;
+	silent?: boolean;
 }): Promise<ResultSuccess> => {
 	const address = get(addressStore);
 
@@ -75,16 +81,23 @@ const loadEthTransactions = async ({
 	} catch (err: unknown) {
 		ethTransactionsStore.nullify(tokenId);
 
-		const {
-			transactions: {
-				error: { loading_transactions }
-			}
-		} = get(i18n);
+		if (!silent) {
+			const {
+				transactions: {
+					error: { loading_transactions_symbol }
+				}
+			} = get(i18n);
 
-		toastsErrorNoTrace({
-			msg: { text: loading_transactions },
-			err
-		});
+			toastsErrorNoTrace({
+				msg: {
+					text: replacePlaceholders(loading_transactions_symbol, {
+						$symbol: ETHEREUM_NETWORK_SYMBOL
+					})
+				},
+				err
+			});
+		}
+
 		return { success: false };
 	}
 
