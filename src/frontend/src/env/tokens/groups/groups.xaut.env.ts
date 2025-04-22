e393c352014cec62264d15eb6e37e89845295f61
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-22 16:55:50 +0200

    feat(frontend): add XAUT token group data (#6014)
    # Motivation
    
    Splitting PR https://github.com/dfinity/oisy-wallet/pull/5918.
    
    We are going to use `groupData` to group similar ("twin") tokens.
    Currently we are using different props depending on the type of token
    (for example `twinToken` for ICRC, or `twinTokenSymbol` for ERC20). With
    groupData, we aim to have a single center of aggregation for "twin"
    tokens.
    
    In this PR we create the group for token XAUt.

---
+++ src/frontend/src/env/tokens/groups/groups.xaut.env.ts
@@ -0,0 +1,9 @@
+import xaut from '$eth/assets/xaut.svg';
+
+const XAUT_TOKEN_GROUP_SYMBOL = 'XAUt';
+
+export const XAUT_TOKEN_GROUP = {
+	icon: xaut,
+	name: 'Tether Gold',
+	symbol: XAUT_TOKEN_GROUP_SYMBOL
+};
