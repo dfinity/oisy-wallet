921be5862718a2506ada43a89b85cdc86f3bb359
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-22 15:20:10 +0200

    feat(frontend): add USDT token group data (#6008)
    # Motivation
    
    Splitting PR https://github.com/dfinity/oisy-wallet/pull/5918.
    
    We are going to use `groupData` to group similar ("twin") tokens.
    Currently we are using different props depending on the type of token
    (for example `twinToken` for ICRC, or `twinTokenSymbol` for ERC20). With
    groupData, we aim to have a single center of aggregation for "twin"
    tokens.
    
    In this PR we create the group for token USDT.

---
+++ src/frontend/src/env/tokens/groups/groups.usdt.env.ts
@@ -0,0 +1,9 @@
+import usdt from '$eth/assets/usdt.svg';
+
+const USDT_TOKEN_GROUP_SYMBOL = 'USDT';
+
+export const USDT_TOKEN_GROUP = {
+	icon: usdt,
+	name: 'Tether USD',
+	symbol: USDT_TOKEN_GROUP_SYMBOL
+};
