88eff9e40e6a56b5d14a77ced08a080cd5242b4c
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-23 10:26:16 +0200

    refactor(frontend): use `pageToken` store in `Actions` component (#6039)
    # Motivation
    
    Instead of using store `tokenWithFallback` (that we need to deprecated),
    we use the (almost) equivalent `pageToken` store in component `Actions`,
    with an additional check.

--- src/frontend/src/lib/components/hero/Actions.svelte
+++ src/frontend/src/lib/components/hero/Actions.svelte
@@ -29,7 +29,6 @@
 	} from '$lib/derived/network.derived';
 	import { networkBitcoinMainnetEnabled } from '$lib/derived/networks.derived';
 	import { pageToken } from '$lib/derived/page-token.derived';
-	import { tokenWithFallback } from '$lib/derived/token.derived';
 	import { isRouteTransactions } from '$lib/utils/nav.utils';
 	import { isNetworkIdBTCMainnet } from '$lib/utils/network.utils';
 	import SolReceive from '$sol/components/receive/SolReceive.svelte';
@@ -63,16 +62,18 @@
 
 <div role="toolbar" class="flex w-full justify-center pt-10">
 	<HeroButtonGroup>
-		{#if $networkICP}
-			<IcReceive token={$tokenWithFallback} />
-		{:else if $networkEthereum}
-			<EthReceive token={$tokenWithFallback} />
-		{:else if $networkBitcoin}
-			<BtcReceive />
-		{:else if $networkSolana}
-			<SolReceive token={$tokenWithFallback} />
-		{:else if $pseudoNetworkChainFusion}
+		{#if $pseudoNetworkChainFusion}
 			<Receive />
+		{:else if nonNullish($pageToken)}
+			{#if $networkICP}
+				<IcReceive token={$pageToken} />
+			{:else if $networkEthereum}
+				<EthReceive token={$pageToken} />
+			{:else if $networkBitcoin}
+				<BtcReceive />
+			{:else if $networkSolana}
+				<SolReceive token={$pageToken} />
+			{/if}
 		{/if}
 
 		{#if sendAction}
