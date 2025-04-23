abd07e6a84725b1e2f53068a96bfcaed4d1cfd93
Svetoslav Kirov <svetoslav.yuliev@inacta.ch>
2025-04-23 13:48:45 +0300

    Add address type to shared.

--- src/shared/src/types/contact.rs
+++ src/shared/src/types/contact.rs
@@ -1,6 +1,6 @@
-use std::fmt;
-
 use candid::{CandidType, Deserialize};
+
+use super::address_type::AddressType;
 
 #[derive(CandidType, Deserialize, Clone, Debug, Eq, PartialEq)]
 pub struct Contact {
@@ -8,51 +8,6 @@ pub struct Contact {
     pub name: String,
     pub addresses: Vec<ContactAddressData>,
     pub avatar: Option<String>,
-}
-
-#[derive(CandidType, Deserialize, Clone, Debug, Eq, PartialEq)]
-pub enum AddressType {
-    ICP,
-    BTC,
-    ETH,
-    SOL,
-    ADA,
-    DOT,
-    AVAX,
-    BSC,
-    MATIC,
-}
-
-impl fmt::Display for AddressType {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            AddressType::ICP => write!(f, "ICP"),
-            AddressType::BTC => write!(f, "BTC"),
-            AddressType::ETH => write!(f, "ETH"),
-            AddressType::SOL => write!(f, "SOL"),
-            AddressType::ADA => write!(f, "ADA"),
-            AddressType::DOT => write!(f, "DOT"),
-            AddressType::AVAX => write!(f, "AVAX"),
-            AddressType::BSC => write!(f, "BSC"),
-            AddressType::MATIC => write!(f, "MATIC"),
-        }
-    }
-}
-
-impl From<&str> for AddressType {
-    fn from(s: &str) -> Self {
-        match s {
-            "BTC" => AddressType::BTC,
-            "ETH" => AddressType::ETH,
-            "SOL" => AddressType::SOL,
-            "ADA" => AddressType::ADA,
-            "DOT" => AddressType::DOT,
-            "AVAX" => AddressType::AVAX,
-            "BSC" => AddressType::BSC,
-            "MATIC" => AddressType::MATIC,
-            _ => AddressType::ICP,
-        }
-    }
 }
 
 #[derive(CandidType, Deserialize, Clone, Debug, Eq, PartialEq)]
