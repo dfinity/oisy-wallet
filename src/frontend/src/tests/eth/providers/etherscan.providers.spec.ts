46c3ca1417affc201993fa3def93a387ae4904e8
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-24 09:41:26 +0200

    feat(frontend): use Etherscan API v2 (#6021)
    # Motivation
    
    We are adding new EVM networks (for example Base and BNB Smart Chain),
    so we can re-use some providers that works with the difference of
    passing different Chain IDs. For example Etherscan API provides a single
    entry point for all supported EVM networks with [the new API
    v2](https://docs.etherscan.io/etherscan-v2).
    
    This allow us to have less API keys, and re-use part of the code.
    
    # Changes
    
    - Create new Etherscan Plugin with the v2 base URL (see
    [docs](https://docs.ethers.org/v6/api/providers/thirdparty/#EtherscanPlugin)).
    - Change map of Ethereum providers to create instances of Network.
    - Attach custom plugin to the network map.
    - Add param chainId to each request of class `EtherscanProvider`.
    
    # Tests
    
    Added unittests, but tried in env container and there were no changes.

--- src/frontend/src/tests/eth/providers/etherscan.providers.spec.ts
+++ src/frontend/src/tests/eth/providers/etherscan.providers.spec.ts
@@ -1,14 +1,11 @@
-import {
-	ETHEREUM_NETWORK_ID,
-	ETHERSCAN_NETWORK_HOMESTEAD,
-	SEPOLIA_NETWORK_ID
-} from '$env/networks/networks.eth.env';
+import { ETHEREUM_NETWORK, SEPOLIA_NETWORK } from '$env/networks/networks.eth.env';
 import { ICP_NETWORK_ID } from '$env/networks/networks.icp.env';
 import { EtherscanProvider, etherscanProviders } from '$eth/providers/etherscan.providers';
 import type {
 	EtherscanProviderInternalTransaction,
 	EtherscanProviderTransaction
 } from '$eth/types/etherscan-transaction';
+import type { EthereumNetwork } from '$eth/types/network';
 import type { Transaction } from '$lib/types/transaction';
 import { replacePlaceholders } from '$lib/utils/i18n.utils';
 import { mockEthAddress } from '$tests/mocks/eth.mocks';
@@ -17,18 +14,48 @@ import {
 	createMockEtherscanTransactions
 } from '$tests/mocks/etherscan.mock';
 import en from '$tests/mocks/i18n.mock';
-import { EtherscanProvider as EtherscanProviderLib } from 'ethers/providers';
-import type { MockedClass } from 'vitest';
+import {
+	EtherscanPlugin,
+	EtherscanProvider as EtherscanProviderLib,
+	Network
+} from 'ethers/providers';
+import { type MockedClass } from 'vitest';
 
 vi.mock('$env/rest/etherscan.env', () => ({
 	ETHERSCAN_API_KEY: 'test-api-key'
 }));
 
 describe('etherscan.providers', () => {
+	const ETHERSCAN_API_KEY = 'test-api-key';
+
+	const networks: EthereumNetwork[] = [ETHEREUM_NETWORK, SEPOLIA_NETWORK];
+
+	it('should create the correct map of providers', () => {
+		expect(EtherscanProviderLib).toHaveBeenCalledTimes(networks.length);
+
+		networks.forEach(({ name, chainId }, index) => {
+			expect(EtherscanProviderLib).toHaveBeenNthCalledWith(
+				index + 1,
+				new Network(name, chainId),
+				ETHERSCAN_API_KEY
+			);
+		});
+	});
+
+	it('should attach the custom plugin to the providers', () => {
+		const ETHERSCAN_PLUGIN = new EtherscanPlugin('https://api.etherscan.io/v2');
+
+		expect(Network.prototype.attachPlugin).toHaveBeenCalledTimes(networks.length);
+
+		networks.forEach((_, index) => {
+			expect(Network.prototype.attachPlugin).toHaveBeenNthCalledWith(index + 1, ETHERSCAN_PLUGIN);
+		});
+	});
+
 	describe('EtherscanProvider', () => {
-		const network = ETHERSCAN_NETWORK_HOMESTEAD;
+		const network: Network = new Network(ETHEREUM_NETWORK.name, ETHEREUM_NETWORK.chainId);
+		const chainId = ETHEREUM_NETWORK.chainId;
 		const address = mockEthAddress;
-		const ETHERSCAN_API_KEY = 'test-api-key';
 
 		const normalTransactions: EtherscanProviderTransaction[] = createMockEtherscanTransactions(3);
 
@@ -56,8 +83,7 @@ describe('etherscan.providers', () => {
 				gasLimit: BigInt(gas),
 				gasPrice: BigInt(gasPrice),
 				value: BigInt(value),
-				// Chain ID is not delivered by the Etherscan API so, we naively set 0
-				chainId: 0n
+				chainId
 			})
 		);
 
@@ -79,8 +105,7 @@ describe('etherscan.providers', () => {
 				nonce: 0,
 				gasLimit: BigInt(gas),
 				value: BigInt(value),
-				// Chain ID is not delivered by the Etherscan API so, we naively set 0
-				chainId: 0n
+				chainId
 			})
 		);
 
@@ -107,14 +132,14 @@ describe('etherscan.providers', () => {
 		});
 
 		it('should initialise the provider with the correct network and API key', () => {
-			const provider = new EtherscanProvider(network);
+			const provider = new EtherscanProvider(network, chainId);
 
 			expect(provider).toBeDefined();
 			expect(EtherscanProviderLib).toHaveBeenCalledWith(network, ETHERSCAN_API_KEY);
 		});
 
 		it('should call fetch for all history types', async () => {
-			const provider = new EtherscanProvider(network);
+			const provider = new EtherscanProvider(network, chainId);
 
 			const result = await provider.transactions({ address });
 
@@ -126,7 +151,7 @@ describe('etherscan.providers', () => {
 		});
 
 		it('should call fetch with correct parameters for getHistory', async () => {
-			const provider = new EtherscanProvider(network);
+			const provider = new EtherscanProvider(network, chainId);
 
 			await provider.transactions({ address });
 
@@ -134,6 +159,7 @@ describe('etherscan.providers', () => {
 
 			expect(mockFetch).toHaveBeenCalledTimes(2);
 			expect(mockFetch).toHaveBeenNthCalledWith(1, 'account', {
+				chainId,
 				action: 'txlist',
 				address,
 				startblock: 0,
@@ -143,7 +169,7 @@ describe('etherscan.providers', () => {
 		});
 
 		it('should call fetch with correct parameters for getInternalHistory', async () => {
-			const provider = new EtherscanProvider(network);
+			const provider = new EtherscanProvider(network, chainId);
 
 			await provider.transactions({ address });
 
@@ -151,6 +177,7 @@ describe('etherscan.providers', () => {
 
 			expect(mockFetch).toHaveBeenCalledTimes(2);
 			expect(mockFetch).toHaveBeenNthCalledWith(2, 'account', {
+				chainId,
 				action: 'txlistinternal',
 				address,
 				startblock: 0,
@@ -160,7 +187,7 @@ describe('etherscan.providers', () => {
 		});
 
 		it('should handle errors gracefully', async () => {
-			const provider = new EtherscanProvider(network);
+			const provider = new EtherscanProvider(network, chainId);
 			mockFetch.mockRejectedValue(new Error('Network error'));
 
 			await expect(provider.transactions({ address })).rejects.toThrow('Network error');
@@ -168,12 +195,15 @@ describe('etherscan.providers', () => {
 	});
 
 	describe('etherscanProviders', () => {
-		it('should return the correct provider for Ethereum network', () => {
-			expect(etherscanProviders(ETHEREUM_NETWORK_ID)).toBeInstanceOf(EtherscanProvider);
-		});
+		networks.forEach(({ id, name }) => {
+			it(`should return the correct provider for ${name} network`, () => {
+				const provider = etherscanProviders(id);
 
-		it('should return the correct provider for Sepolia network', () => {
-			expect(etherscanProviders(SEPOLIA_NETWORK_ID)).toBeInstanceOf(EtherscanProvider);
+				expect(provider).toBeInstanceOf(EtherscanProvider);
+
+				expect(provider).toHaveProperty('network');
+				expect(provider).toHaveProperty('chainId');
+			});
 		});
 
 		it('should throw an error for an unsupported network ID', () => {
