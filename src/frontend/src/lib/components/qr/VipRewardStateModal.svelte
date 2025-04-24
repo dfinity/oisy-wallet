7fbb5c2e0b3958900bebd3ad4721d08ace98a87f
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-04-24 15:28:41 +0200

    feat(frontend): opens manage tokens modal for gold token if not yet enabled (#6068)
    # Motivation
    
    When a user receives a Gold token via an invitation code, they get a
    notification. As soon as they click the `Take me to the wallet` button,
    the `GLDT` token should get enabled if it is not already.
    Since we cannot guarantee that all data has fully loaded at this point,
    we are unable to enable the token automatically. For this reason we open
    the `Manage Tokens` modal with the `GLDT` token preselected. This allows
    the user to easily enable the token manually.
    In the worst case the modal might be open even if the token is already
    enabled, simply because the data was not fully loaded yet, but this
    behavior is acceptable.
    
    # Changes
    
    - opens `Manage Tokens` modal if `GLDT` token is not yet enabled
    
    # Tests
    
    Claim reward while `GLDT` token is disabled and wait until all tokens
    are loaded
    
    
    https://github.com/user-attachments/assets/b9c25468-0414-4f7b-956e-34117b034b86
    
    
    Claim reward while `GLDT` token is disabled and do not wait until all
    tokens are loaded
    
    
    https://github.com/user-attachments/assets/07d515f1-f496-4d6d-9864-9dd597e4f927
    
    
    Claim reward while `GLDT` token is already enabled
    
    
    https://github.com/user-attachments/assets/02cf1a17-e5d1-4059-8d9a-56a7a042418e
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

--- src/frontend/src/lib/components/qr/VipRewardStateModal.svelte
+++ src/frontend/src/lib/components/qr/VipRewardStateModal.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
 	import { Modal } from '@dfinity/gix-components';
+	import { isNullish, nonNullish } from '@dfinity/utils';
+	import { GLDT_IC_DATA } from '$env/networks/networks.icrc.env';
+	import { additionalIcrcTokens } from '$env/tokens/tokens.icrc.env';
+	import type { EnvIcToken } from '$env/types/env-icrc-token';
+	import type { IcToken } from '$icp/types/ic-token';
 	import failedVipReward from '$lib/assets/failed-vip-reward.svg';
 	import successfulBinanceReward from '$lib/assets/successful-binance-reward.svg';
 	import successfulVipReward from '$lib/assets/successful-vip-reward.svg';
@@ -7,7 +12,8 @@
 	import Button from '$lib/components/ui/Button.svelte';
 	import ContentWithToolbar from '$lib/components/ui/ContentWithToolbar.svelte';
 	import ImgBanner from '$lib/components/ui/ImgBanner.svelte';
-	import { VIP_STATE_IMAGE_BANNER } from '$lib/constants/test-ids.constants';
+	import { VIP_STATE_BUTTON, VIP_STATE_IMAGE_BANNER } from '$lib/constants/test-ids.constants';
+	import { enabledIcTokens } from '$lib/derived/tokens.derived';
 	import { QrCodeType } from '$lib/enums/qr-code-types';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { modalStore } from '$lib/stores/modal.store';
@@ -15,6 +21,31 @@
 
 	export let isSuccessful: boolean;
 	export let codeType: QrCodeType = QrCodeType.VIP;
+
+	let goldTokenSymbol: string | undefined;
+	[goldTokenSymbol] =
+		Object.entries(additionalIcrcTokens).find(
+			([_, value]) => (value as EnvIcToken)?.ledgerCanisterId === GLDT_IC_DATA?.ledgerCanisterId
+		) ?? [];
+
+	let goldToken: IcToken | undefined;
+	$: goldToken = $enabledIcTokens.find(
+		(token) => token.ledgerCanisterId === GLDT_IC_DATA?.ledgerCanisterId
+	);
+
+	const manageTokensId = Symbol();
+
+	const onClick = () => {
+		codeType === QrCodeType.GOLD && isNullish(goldToken) && nonNullish(goldTokenSymbol)
+			? modalStore.openManageTokens({
+					id: manageTokensId,
+					data: {
+						initialSearch: goldTokenSymbol,
+						message: replaceOisyPlaceholders($i18n.tokens.manage.text.default_message)
+					}
+				})
+			: modalStore.close();
+	};
 </script>
 
 {#if isSuccessful}
@@ -57,7 +88,8 @@
 			colorStyle="secondary-light"
 			type="button"
 			fullWidth
-			on:click={modalStore.close}
+			on:click={onClick}
+			testId={VIP_STATE_BUTTON}
 			slot="toolbar"
 		>
 			{isSuccessful ? $i18n.vip.reward.text.open_wallet : $i18n.vip.reward.text.open_wallet}
