3510b12606335f5c77783753c10ba8ce8e2f9869
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-24 14:09:50 +0200

    test(frontend): add tests for util icTokenIcrcCustomToken (#6085)
    # Motivation
    
    Adding tests for util `icTokenIcrcCustomToken`.

--- src/frontend/src/tests/icp/utils/icrc.utils.spec.ts
+++ src/frontend/src/tests/icp/utils/icrc.utils.spec.ts
@@ -1,7 +1,38 @@
 import { DEPRECATED_SNES } from '$env/tokens/tokens.sns.deprecated.env';
-import { isDeprecatedSns, isNotDeprecatedSns } from '$icp/utils/icrc.utils';
+import { icTokenIcrcCustomToken, isDeprecatedSns, isNotDeprecatedSns } from '$icp/utils/icrc.utils';
+import type { TokenStandard } from '$lib/types/token';
+import { mockIcrcCustomToken } from '$tests/mocks/icrc-custom-tokens.mock';
 
 describe('icrc.utils', () => {
+	describe('icTokenIcrcCustomToken', () => {
+		it.each(['icp', 'icrc'])('should return true for valid token standards: %s', (standard) => {
+			expect(
+				icTokenIcrcCustomToken({ ...mockIcrcCustomToken, standard: standard as TokenStandard })
+			).toBeTruthy();
+		});
+
+		it.each(['ethereum', 'erc20', 'bitcoin', 'solana', 'spl'])(
+			'should return false for invalid token standards: %s',
+			(standard) => {
+				expect(
+					icTokenIcrcCustomToken({ ...mockIcrcCustomToken, standard: standard as TokenStandard })
+				).toBeFalsy();
+			}
+		);
+
+		it('should return true is the token has the prop `enabled`', () => {
+			expect(icTokenIcrcCustomToken({ ...mockIcrcCustomToken, enabled: true })).toBeTruthy();
+
+			expect(icTokenIcrcCustomToken({ ...mockIcrcCustomToken, enabled: false })).toBeTruthy();
+		});
+
+		it('should return false is the token has no prop `enabled`', () => {
+			const { enabled: _, ...mockIcrcCustomTokenWithoutEnabled } = mockIcrcCustomToken;
+
+			expect(icTokenIcrcCustomToken(mockIcrcCustomTokenWithoutEnabled)).toBeFalsy();
+		});
+	});
+
 	describe('isDeprecatedSns', () => {
 		it.each(Object.keys(DEPRECATED_SNES))(
 			"should return true for deprecated SNS with ledgerCanisterId '%s'",
