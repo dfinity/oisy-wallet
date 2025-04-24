7f7d6c95b13fb6c05275a3e3f60f58d5593775dc
daviddecentage <david.farina@dfinity.org>
2025-04-24 11:39:33 +0200

    update

--- src/frontend/src/icp/derived/ic-transactions.derived.ts
+++ src/frontend/src/icp/derived/ic-transactions.derived.ts
@@ -5,20 +5,18 @@ import { ckBtcPendingUtxosStore } from '$icp/stores/ckbtc-utxos.store';
 import { ckBtcMinterInfoStore } from '$icp/stores/ckbtc.store';
 import { icPendingTransactionsStore } from '$icp/stores/ic-pending-transactions.store';
 import { icTransactionsStore, type IcTransactionsData } from '$icp/stores/ic-transactions.store';
-import { extendIcTransaction, getAllIcTransactions } from '$icp/utils/ic-transactions.utils';
+import { getAllIcTransactions, getIcExtendedTransactions } from '$icp/utils/ic-transactions.utils';
 import { tokenWithFallback } from '$lib/derived/token.derived';
 import { derived, type Readable } from 'svelte/store';
 
 const icExtendedTransactions: Readable<NonNullable<IcTransactionsData>> = derived(
 	[tokenWithFallback, icTransactionsStore, btcStatusesStore],
 	([$token, $icTransactionsStore, $btcStatusesStore]) =>
-		($icTransactionsStore?.[$token.id] ?? []).map((transaction) =>
-			extendIcTransaction({
-				transaction,
-				token: $token,
-				btcStatuses: $btcStatusesStore?.[$token.id] ?? undefined
-			})
-		)
+		getIcExtendedTransactions({
+			token: $token,
+			icTransactionsStore: $icTransactionsStore,
+			btcStatusesStore: $btcStatusesStore
+		})
 );
 
 export const icTransactions: Readable<NonNullable<IcTransactionsData>> = derived(
