6ccca4a3f91ca4b4d4081c44928f92bec08c3a6d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-25 23:14:18 +0200

    revert

--- src/frontend/src/lib/workers/workers.ts
+++ src/frontend/src/lib/workers/workers.ts
@@ -5,7 +5,6 @@ import { onCkBtcUpdateBalanceMessage } from '$icp/workers/ckbtc-update-balance.w
 import { onCkEthMinterInfoMessage } from '$icp/workers/cketh-minter-info.worker';
 import { onIcpWalletMessage } from '$icp/workers/icp-wallet.worker';
 import { onIcrcWalletMessage } from '$icp/workers/icrc-wallet.worker';
-import { onPowProtectionMessage } from '$icp/workers/pow-protection.worker';
 import type { PostMessage, PostMessageDataRequest } from '$lib/types/post-message';
 import { onAuthMessage } from '$lib/workers/auth.worker';
 import { onExchangeMessage } from '$lib/workers/exchange.worker';
@@ -22,7 +21,6 @@ onmessage = async (msg: MessageEvent<PostMessage<PostMessageDataRequest>>) => {
 		onCkEthMinterInfoMessage(msg),
 		onIcpWalletMessage(msg),
 		onIcrcWalletMessage(msg),
-		onSolWalletMessage(msg),
-		onPowProtectionMessage(msg)
+		onSolWalletMessage(msg)
 	]);
 };
