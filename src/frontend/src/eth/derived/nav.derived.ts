9e7409a1e2f507d69a9a9be69db02b72d60dd7cb
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-25 21:05:57 +0200

    feat(frontend): include EVM tokens among the checks for initialized token (#6117)
    # Motivation
    
    Continuing to integrate EVM networks in the same flows as Ethereum
    networks, we include the EVM networks in the derived store that checks
    if an Ethereum token is initialized.

--- src/frontend/src/eth/derived/nav.derived.ts
+++ src/frontend/src/eth/derived/nav.derived.ts
@@ -1,15 +1,18 @@
-import { ETHEREUM_TOKEN, SEPOLIA_TOKEN } from '$env/tokens/tokens.eth.env';
 import { enabledErc20Tokens } from '$eth/derived/erc20.derived';
+import { enabledEthereumTokens } from '$eth/derived/tokens.derived';
+import { enabledEvmTokens } from '$evm/derived/tokens.derived';
 import { routeToken } from '$lib/derived/nav.derived';
 import { nonNullish } from '@dfinity/utils';
 import { derived, type Readable } from 'svelte/store';
 
 export const tokenInitialized: Readable<boolean> = derived(
-	[routeToken, enabledErc20Tokens],
-	([$routeToken, $erc20Tokens]) =>
-		$routeToken === ETHEREUM_TOKEN.name ||
-		$routeToken === SEPOLIA_TOKEN.name ||
-		nonNullish($erc20Tokens.find(({ name }) => name === $routeToken))
+	[routeToken, enabledEthereumTokens, enabledEvmTokens, enabledErc20Tokens],
+	([$routeToken, $enabledEthereumTokens, $enabledEvmTokens, $erc20Tokens]) =>
+		nonNullish(
+			[...$enabledEthereumTokens, ...$enabledEvmTokens, ...$erc20Tokens].find(
+				({ name }) => name === $routeToken
+			)
+		)
 );
 
 export const tokenNotInitialized: Readable<boolean> = derived(
