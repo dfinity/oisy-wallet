b328ac2aef8415e5fe24db31e997ad41a14f5587
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-25 21:34:57 +0200

    chore(frontend): remove deprecated utils for SNS tokens (#6086)
    # Motivation
    
    Removing deprecated utils to check if a SNS token is deprecated.

--- src/frontend/src/tests/icp/utils/icrc.utils.spec.ts
+++ src/frontend/src/tests/icp/utils/icrc.utils.spec.ts
@@ -1,5 +1,4 @@
-import { DEPRECATED_SNES } from '$env/tokens/tokens.sns.deprecated.env';
-import { icTokenIcrcCustomToken, isDeprecatedSns, isNotDeprecatedSns } from '$icp/utils/icrc.utils';
+import { icTokenIcrcCustomToken } from '$icp/utils/icrc.utils';
 import type { TokenStandard } from '$lib/types/token';
 import { mockIcrcCustomToken } from '$tests/mocks/icrc-custom-tokens.mock';
 
@@ -30,40 +29,6 @@ describe('icrc.utils', () => {
 			const { enabled: _, ...mockIcrcCustomTokenWithoutEnabled } = mockIcrcCustomToken;
 
 			expect(icTokenIcrcCustomToken(mockIcrcCustomTokenWithoutEnabled)).toBeFalsy();
-		});
-	});
-
-	describe('isDeprecatedSns', () => {
-		it.each(Object.keys(DEPRECATED_SNES))(
-			"should return true for deprecated SNS with ledgerCanisterId '%s'",
-			(ledgerCanisterId) => {
-				expect(isDeprecatedSns({ ledgerCanisterId })).toBeTruthy();
-			}
-		);
-
-		it('should return false for non-deprecated SNS', () => {
-			expect(isDeprecatedSns({ ledgerCanisterId: 'zfcdd-tqaaa-aaaaq-aaaga-cai' })).toBeFalsy();
-		});
-
-		it('should return false for empty ledgerCanisterId', () => {
-			expect(isDeprecatedSns({ ledgerCanisterId: '' })).toBeFalsy();
-		});
-	});
-
-	describe('isNotDeprecatedSns', () => {
-		it.each(Object.keys(DEPRECATED_SNES))(
-			"should return false for deprecated SNS with ledgerCanisterId '%s'",
-			(ledgerCanisterId) => {
-				expect(isNotDeprecatedSns({ ledgerCanisterId })).toBeFalsy();
-			}
-		);
-
-		it('should return true for non-deprecated SNS', () => {
-			expect(isNotDeprecatedSns({ ledgerCanisterId: 'zfcdd-tqaaa-aaaaq-aaaga-cai' })).toBeTruthy();
-		});
-
-		it('should return true for empty ledgerCanisterId', () => {
-			expect(isNotDeprecatedSns({ ledgerCanisterId: '' })).toBeTruthy();
 		});
 	});
 });
