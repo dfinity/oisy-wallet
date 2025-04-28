f3c2a728721d8c05cf2a0aa51ce5667f2af3c1a9
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-28 15:10:05 +0200

    feat(frontend): remove Base USDT since (#6151)
    # Motivation
    
    The token USDT for Base network seems to be inexistent (I cannot find
    any more information apart from the address). So we remove it.

--- src/frontend/src/env/tokens/tokens-evm/tokens-base/tokens.erc20.env.ts
+++ src/frontend/src/env/tokens/tokens-evm/tokens-base/tokens.erc20.env.ts
@@ -7,12 +7,11 @@ import {
 	SEPOLIA_USDC_TOKEN,
 	USDC_TOKEN
 } from '$env/tokens/tokens-evm/tokens-base/tokens-erc20/tokens.usdc.env';
-import { USDT_TOKEN } from '$env/tokens/tokens-evm/tokens-base/tokens-erc20/tokens.usdt.env';
 import type { RequiredEvmErc20Token } from '$evm/types/erc20';
 
 const BASE_ERC20_TOKENS_SEPOLIA: RequiredEvmErc20Token[] = [SEPOLIA_USDC_TOKEN, SEPOLIA_EURC_TOKEN];
 
-const BASE_ERC20_TOKENS_MAINNET: RequiredEvmErc20Token[] = [USDC_TOKEN, USDT_TOKEN, EURC_TOKEN];
+const BASE_ERC20_TOKENS_MAINNET: RequiredEvmErc20Token[] = [USDC_TOKEN, EURC_TOKEN];
 
 export const BASE_ERC20_TOKENS: RequiredEvmErc20Token[] = [
 	...(BASE_MAINNET_ENABLED ? BASE_ERC20_TOKENS_MAINNET : []),
