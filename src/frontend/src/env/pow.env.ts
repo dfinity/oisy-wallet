df5928dfa31cc7ea5d5932bb53647cada4cc5b18
DecentAgeCoder <79855464+DecentAgeCoder@users.noreply.github.com>
2025-04-28 15:48:10 +0100

    fix(frontend): disable parsing of pow feature flag (#6156)
    # Motivation
    
    This error apprears in oiwsy-wallet when the variable
    VITE_POW_FEATURE_ENABLED is empty . For the moment we have to disable
    the parsing of the VITE_POW_FEATURE_ENABLED and just set it to false, to
    avoid the issue until a fallback is implemented and the secret
    VITE_POW_FEATURE_ENABLED is set to false.

--- src/frontend/src/env/pow.env.ts
+++ src/frontend/src/env/pow.env.ts
@@ -1,5 +1,4 @@
-import { parseBoolEnvVar } from '$lib/utils/env.utils';
-
-export const POW_FEATURE_ENABLED = parseBoolEnvVar(import.meta.env.VITE_POW_FEATURE_ENABLED);
+// TODO enable this feature flag evaluation logic once VITE_POW_FEATURE_ENABLED is referenced
+// export const POW_FEATURE_ENABLED = parseBoolEnvVar(import.meta.env.VITE_POW_FEATURE_ENABLED);
 
 export const POW_CHALLENGE_INTERVAL_MILLIS = 120000;
