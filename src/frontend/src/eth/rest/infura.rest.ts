5a58c90437d93915b5fd0e29a0fac8aba8db60d4
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-28 17:04:10 +0200

    feat(frontend): add Infura Gas API

---
+++ src/frontend/src/eth/rest/infura.rest.ts
@@ -0,0 +1,38 @@
+import { INFURA_API_KEY, INFURA_GAS_REST_URL } from '$env/rest/infura.env';
+import type { GasFeeEstimate } from '$eth/types/infura';
+import type { EthereumChainId } from '$eth/types/network';
+import { parseToken } from '$lib/utils/parse.utils';
+import { fromNullable } from '@dfinity/utils';
+import type { FeeData } from 'ethers/providers';
+
+export class InfuraGasRest {
+	private readonly apiUrl = INFURA_GAS_REST_URL;
+
+	constructor(private readonly chainId: EthereumChainId) {}
+
+	// https://docs.metamask.io/services/reference/gas-api
+	getSuggestedFeeData = async (): Promise<
+		Pick<FeeData, 'maxFeePerGas' | 'maxPriorityFeePerGas'>
+	> => {
+		const url = new URL(
+			`${this.apiUrl}/${INFURA_API_KEY}/networks/${this.chainId.toString()}/suggestedGasFees`
+		);
+
+		const response = await fetch(url);
+
+		if (!response.ok) {
+			throw new Error(`Fetching gas data with Infura Gas API failed.`);
+		}
+
+		const { result } = await response.json();
+
+		const {
+			medium: { suggestedMaxPriorityFeePerGas, suggestedMaxFeePerGas }
+		} = fromNullable(result) as GasFeeEstimate;
+
+		return {
+			maxFeePerGas: parseToken({ value: suggestedMaxFeePerGas, unitName: 'gwei' }),
+			maxPriorityFeePerGas: parseToken({ value: suggestedMaxPriorityFeePerGas, unitName: 'gwei' })
+		};
+	};
+}
