60d859e5b59c5b42ef44545cf610201d46eff5fe
DecentAgeCoder <marco.oesch@dfinity.org>
2025-04-28 16:19:26 +0200

    Removed the declaration of the POW_FEATURE_ENABLED since it is currently not used anywhere in the code.

--- src/frontend/src/env/pow.env.ts
+++ src/frontend/src/env/pow.env.ts
@@ -1,7 +1,4 @@
-// TODO remove this hardocded assignement once VITE_POW_FEATURE_ENABLED is correctly set in github
-export const POW_FEATURE_ENABLED = false;
-
-// TODO enable this feature flag evaluation logic once VITE_POW_FEATURE_ENABLED is set in github
+// TODO enable this feature flag evaluation logic once VITE_POW_FEATURE_ENABLED is referenced
 // export const POW_FEATURE_ENABLED = parseBoolEnvVar(import.meta.env.VITE_POW_FEATURE_ENABLED);
 
 export const POW_CHALLENGE_INTERVAL_MILLIS = 120000;
