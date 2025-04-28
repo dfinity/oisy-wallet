d4486af72bd096549f1d0ba22da4c658ba24f761
Flavio Pellanda <flavio.pellanda@inacta.ch>
2025-04-28 13:48:26 +0200

    Add edit button to contact header

--- src/frontend/src/lib/components/address-book/ContactHeader.svelte
+++ src/frontend/src/lib/components/address-book/ContactHeader.svelte
@@ -1,11 +1,29 @@
 <script lang="ts">
 	import Avatar from '$lib/components/address-book/Avatar.svelte';
 	import IconContactHeader from '$lib/components/icons/IconContactHeader.svelte';
+	import IconPencil from '$lib/components/icons/lucide/IconPencil.svelte';
+	import Button from '$lib/components/ui/Button.svelte';
+	import { CONTACT_HEADER_EDIT_BUTTON } from '$lib/constants/test-ids.constants';
+	import { i18n } from '$lib/stores/i18n.store';
 
-	let { name, styleClass = '' } = $props();
+	let { name, edit = undefined, styleClass = '' } = $props();
 </script>
 
 <div class={`relative flex flex-col items-center pb-5 ${styleClass}`}>
+	{#if edit}
+		<Button
+			styleClass="absolute top-2.5 right-2.5 bg-black/16 dark:bg-black/10 px-3 py-2 font-xs"
+			ariaLabel={$i18n.core.text.edit}
+			colorStyle="secondary"
+			testId={CONTACT_HEADER_EDIT_BUTTON}
+			on:click={edit}
+		>
+			<span class="flex items-center">
+				<IconPencil />
+			</span>
+			{$i18n.core.text.edit}
+		</Button>
+	{/if}
 	<div class="self-stretch text-[lightgray]">
 		<IconContactHeader></IconContactHeader>
 	</div>
--- src/frontend/src/lib/constants/test-ids.constants.ts
+++ src/frontend/src/lib/constants/test-ids.constants.ts
@@ -184,3 +184,5 @@ export const ADDRESS_BOOK_CONTACT_FORM = 'address-book-contact-form';
 export const ADDRESS_BOOK_CONTACT_NAME_INPUT = 'address-book-contact-name-input';
 export const ADDRESS_BOOK_SAVE_BUTTON = 'address-book-save-button';
 export const ADDRESS_BOOK_CANCEL_BUTTON = 'address-book-cancel-button';
+
+export const CONTACT_HEADER_EDIT_BUTTON = 'contact-header-edit-button';
