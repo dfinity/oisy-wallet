bbcba5737a339b1d9a3ea46a210f8447554f15cb
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-04-29 15:14:36 +0200

    feat(frontend): BSC BEP20 tokens (USDC and USDT) (#6197)
    # Motivation
    
    Adding new BSC BEP20 tokens - USDC and USDT.
    
    ---------
    
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

---
+++ src/frontend/src/evm/types/bep20.ts
@@ -0,0 +1,4 @@
+import type { RequiredErc20Token } from '$eth/types/erc20';
+import type { TokenLinkedData } from '$lib/types/token';
+
+export type RequiredEvmBep20Token = Omit<RequiredErc20Token, keyof TokenLinkedData>;
