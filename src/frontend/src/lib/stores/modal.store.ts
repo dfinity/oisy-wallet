525e8c4e5658fa8e7872cca8352c6b2b4e5eb7ba
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-04-29 14:14:22 +0200

    feat(frontend): removes unused RewardModalDetails (#6190)
    # Motivation
    
    The `RewardModalDetails` component is not used. It only loads the `modal
    store` data which can also be solved with a `modal derived` function.
    
    # Changes
    
    - removes unused `RewardModalDetails`
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/stores/modal.store.ts
+++ src/frontend/src/lib/stores/modal.store.ts
@@ -1,3 +1,4 @@
+import type { RewardDescription } from '$env/types/env-reward';
 import type { QrCodeType } from '$lib/enums/qr-code-types';
 import type { SettingsModalType } from '$lib/enums/settings-modal-types';
 import type { ManageTokensData } from '$lib/types/manage-tokens';
@@ -97,7 +98,7 @@ export interface ModalStore<T> extends Readable<ModalData<T>> {
 	openReferralState: () => void;
 	openDappDetails: <D extends T>(params: SetWithDataParams<D>) => void;
 	openVipRewardState: (params: SetWithDataParams<VipRewardStateData>) => void;
-	openRewardDetails: <D extends T>(params: SetWithDataParams<D>) => void;
+	openRewardDetails: (params: SetWithDataParams<RewardDescription>) => void;
 	openRewardState: <D extends T>(params: SetWithDataParams<D>) => void;
 	// todo: type methods above accordingly, otherwise data will be typed as unknown without making use of generics
 	openSettings: (params: SetWithDataParams<SettingsModalType>) => void;
@@ -160,7 +161,9 @@ const initModalStore = <T>(): ModalStore<T> => {
 		openVipRewardState: <(params: SetWithDataParams<VipRewardStateData>) => void>(
 			setTypeWithData('vip-reward-state')
 		),
-		openRewardDetails: setTypeWithData('reward-details'),
+		openRewardDetails: <(params: SetWithDataParams<RewardDescription>) => void>(
+			setTypeWithData('reward-details')
+		),
 		openRewardState: setTypeWithData('reward-state'),
 		// todo: explicitly define type here as well
 		openSettings: <(params: SetWithDataParams<SettingsModalType>) => void>(
