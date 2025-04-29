fca67f261ad73d83cfa88bf8af4eefc5f590cde8
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-29 12:19:56 +0200

    feat(frontend): check for network in derived store `tokenInitialized` (#6167)
    # Motivation
    
    We should check for network in the derived store `tokenInitialized`
    since now we can have tokens with the same name but on different EVM
    networks.

--- src/frontend/src/eth/derived/nav.derived.ts
+++ src/frontend/src/eth/derived/nav.derived.ts
@@ -1,16 +1,17 @@
 import { enabledErc20Tokens } from '$eth/derived/erc20.derived';
 import { enabledEthereumTokens } from '$eth/derived/tokens.derived';
 import { enabledEvmTokens } from '$evm/derived/tokens.derived';
-import { routeToken } from '$lib/derived/nav.derived';
+import { routeNetwork, routeToken } from '$lib/derived/nav.derived';
 import { nonNullish } from '@dfinity/utils';
 import { derived, type Readable } from 'svelte/store';
 
 export const tokenInitialized: Readable<boolean> = derived(
-	[routeToken, enabledEthereumTokens, enabledEvmTokens, enabledErc20Tokens],
-	([$routeToken, $enabledEthereumTokens, $enabledEvmTokens, $erc20Tokens]) =>
+	[routeToken, routeNetwork, enabledEthereumTokens, enabledEvmTokens, enabledErc20Tokens],
+	([$routeToken, $routeNetwork, $enabledEthereumTokens, $enabledEvmTokens, $erc20Tokens]) =>
 		nonNullish(
 			[...$enabledEthereumTokens, ...$enabledEvmTokens, ...$erc20Tokens].find(
-				({ name }) => name === $routeToken
+				({ name, network: { id: networkId } }) =>
+					name === $routeToken && networkId.description === $routeNetwork
 			)
 		)
 );
