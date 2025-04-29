91fa16b42edf7880aa6bbf7147e118c7e10f3d09
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-29 20:53:57 +0200

    chore(frontend): remove feature flag for EVM networks

--- src/frontend/src/env/networks/networks-evm/networks.evm.env.ts
+++ src/frontend/src/env/networks/networks-evm/networks.evm.env.ts
@@ -2,9 +2,6 @@ import { SUPPORTED_BASE_NETWORKS } from '$env/networks/networks-evm/networks.evm
 import { SUPPORTED_BSC_NETWORKS } from '$env/networks/networks-evm/networks.evm.bsc.env';
 import type { EthereumChainId, EthereumNetwork } from '$eth/types/network';
 import type { Network, NetworkId } from '$lib/types/network';
-import { parseBoolEnvVar } from '$lib/utils/env.utils';
-
-export const EVM_NETWORKS_ENABLED = parseBoolEnvVar(import.meta.env.VITE_EVM_NETWORKS_ENABLED);
 
 export const SUPPORTED_EVM_NETWORKS: EthereumNetwork[] = EVM_NETWORKS_ENABLED
 	? [...SUPPORTED_BASE_NETWORKS, ...SUPPORTED_BSC_NETWORKS]
