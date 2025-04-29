de36bcdf83712fb876cdc972f9fe16502ed85798
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-29 10:58:06 +0200

    chore(frontend): add type to token groups (#6101)
    # Motivation
    
    Better to explicitly define the type of the token groups using interface
    `TokenGroup`.

--- src/frontend/src/env/tokens/groups/groups.eurc.env.ts
+++ src/frontend/src/env/tokens/groups/groups.eurc.env.ts
@@ -1,12 +1,12 @@
 import eurc from '$eth/assets/eurc.svg';
-import type { TokenGroupId } from '$lib/types/token-group';
+import type { TokenGroupData, TokenGroupId } from '$lib/types/token-group';
 import { parseTokenGroupId } from '$lib/validation/token-group.validation';
 
 const EURC_TOKEN_GROUP_SYMBOL = 'EURC';
 
 export const EURC_TOKEN_GROUP_ID: TokenGroupId = parseTokenGroupId(EURC_TOKEN_GROUP_SYMBOL);
 
-export const EURC_TOKEN_GROUP = {
+export const EURC_TOKEN_GROUP: TokenGroupData = {
 	id: EURC_TOKEN_GROUP_ID,
 	icon: eurc,
 	name: 'Euro Coin',
