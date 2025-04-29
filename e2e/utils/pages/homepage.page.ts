6232cd58872ba28ac0096370b1f2cf78572690c0
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-29 10:18:08 +0200

    feat(e2e): remove check for ETH card in homepage

--- e2e/utils/pages/homepage.page.ts
+++ e2e/utils/pages/homepage.page.ts
@@ -253,10 +253,8 @@ abstract class Homepage {
 
 	protected async waitForTokensInitialization(options?: WaitForLocatorOptions): Promise<void> {
 		await this.waitForByTestId({ testId: `${TOKEN_CARD}-ICP-ICP`, options });
-		await this.waitForByTestId({ testId: `${TOKEN_CARD}-ETH-ETH`, options });
 
 		await this.waitForByTestId({ testId: `${TOKEN_BALANCE}-ICP-ICP`, options });
-		await this.waitForByTestId({ testId: `${TOKEN_BALANCE}-ETH-ETH`, options });
 	}
 
 	protected async clickMenuItem({ menuItemTestId }: ClickMenuItemParams): Promise<void> {
