0dae3ca5c46b0fe6d05ce152132b0de1ef371997
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-29 20:23:38 +0200

    tests

--- src/frontend/src/lib/utils/token-card.utils.ts
+++ src/frontend/src/lib/utils/token-card.utils.ts
@@ -1,6 +1,5 @@
 import type { CardData } from '$lib/types/token-card';
 import type { TokenUiGroup } from '$lib/types/token-group';
-import { nonNullish } from '@dfinity/utils';
 
 /** Maps the token group to the card data of the card that will be used as "summary" for the group.
  *
@@ -17,7 +16,7 @@ export const mapHeaderData = ({
 	name,
 	symbol,
 	decimals,
-	icon: nonNullish(tokens[0]?.groupData?.icon) ? tokens[0].groupData.icon : icon, // TODO: this can be directly the prop of TokenUiGroup type
+	icon,
 	network,
 	oisyName: { oisyName: tokens.map((token) => token.symbol).join(', ') },
 	oisySymbol: { oisySymbol: name },
