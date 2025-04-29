ca724b911559e30705dcf6d01dd2fc144e908cde
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-29 09:31:12 +0200

    feat(frontend): use group data for utils to group tokens (#6063)
    # Motivation
    
    We want to use `groupData` field to group tokens in the list, instead of
    custom props for each type of token (for example `twinToken` for ICRC).
    
    Unfortunately there is no easy way of doing that unless we completely
    refactor util `groupTokens`.
    
    The idea is to change its logic to return a group if `groupData` are
    present, otherwise return the token alone.
    
    # Changes
    
    - In the loop of util `groupTokens`, check if there are `groupData`:
    - if there are, proceed as before, mapping a group variant `{ group:
    TokenUiGroup }`
      - it there aren't, proceed returning the variant `{ token: TokenUi }`
    - Use `groupData` ID when mapping a new `TokenUiGroup`.
    - Include `groupData` when mapping a new `TokenUiGroup`.
    - Adapt docstrings.
    - Remove deprecated util `groupMainToken` (doing it in this PR is
    simpler, since the types do not match anymore).
    
    # Next Steps
    
    Util `groupSecondaryToken` shall be renamed since it now simply creates
    a new group or update an existing group.
    
    # Tests
    
    Test adapted. Furthermore, test canister replica works as before:
    
    ![Screenshot 2025-04-24 at 17 50
    23](https://github.com/user-attachments/assets/67e18438-901b-4313-866a-aa2101e128ce)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/types/token-group.ts
+++ src/frontend/src/lib/types/token-group.ts
@@ -1,17 +1,22 @@
-import { type TokenGroupIdSchema, type TokenGroupPropSchema } from '$lib/schema/token-group.schema';
-import type { TokenFinancialData, TokenId, TokenUi } from '$lib/types/token';
+import {
+	type TokenGroupIdSchema,
+	type TokenGroupPropSchema,
+	type TokenGroupSchema
+} from '$lib/schema/token-group.schema';
+import type { TokenFinancialData, TokenUi } from '$lib/types/token';
 import type * as z from 'zod';
 
 export type TokenGroupId = z.infer<typeof TokenGroupIdSchema>;
 
+export type TokenGroupData = z.infer<typeof TokenGroupSchema>;
+
 export type TokenGroup = z.infer<typeof TokenGroupPropSchema>;
 
-//todo: separate typing from token id
-type GroupId = TokenId;
-
 export type TokenUiGroup = {
-	id: GroupId;
+	id: TokenGroupId;
+	// TODO: remove deprecated field when groupData is completely integrated
 	nativeToken: TokenUi;
+	groupData: TokenGroupData;
 	tokens: [TokenUi, ...TokenUi[]];
 } & TokenFinancialData;
 
