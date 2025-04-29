f19d60a823093c061f7ca0069bf223100ca67ea0
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-04-29 15:23:20 +0200

    fix

--- src/frontend/src/lib/canisters/icp_swap.errors.ts
+++ src/frontend/src/lib/canisters/icp_swap.errors.ts
@@ -1,5 +1,4 @@
-import { type Error as SwapPoolError } from '$declarations/icp_swap/icp_swap.did';
 import { CanisterInternalError } from '$lib/canisters/errors';
 
-export const mapIcpSwapCanisterError = (err: SwapPoolError): CanisterInternalError =>
+export const mapIcpSwapCanisterError = (err: any): CanisterInternalError =>
 	new CanisterInternalError(JSON.stringify(err));
