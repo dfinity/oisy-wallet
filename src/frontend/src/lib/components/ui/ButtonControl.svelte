4217120ed4c71018c665fc7bf080a3d62781e601
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-04-30 08:58:01 +0200

    upgrades ButtonControl to svelte 5

--- src/frontend/src/lib/components/ui/ButtonControl.svelte
+++ src/frontend/src/lib/components/ui/ButtonControl.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
-	export let ariaLabel: string;
-	export let styleClass: string | undefined = undefined;
+	import type {Snippet} from "svelte";
+
+	interface Props {
+		children: Snippet;
+		onclick: () => void;
+		ariaLabel: string;
+		styleClass?: string | undefined;
+	}
+
+	let {children, onclick, ariaLabel, styleClass = undefined}: Props = $props();
 </script>
 
 <button
 	aria-label={ariaLabel}
 	class={`${styleClass ?? ''} h-7 w-7 rounded-md bg-primary p-1 transition-colors duration-700 hover:text-brand-primary active:text-brand-primary`}
-	on:click
+	{onclick}
 >
-	<slot />
+	{@render children?.()}
 </button>
