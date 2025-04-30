3e5b17c287379a8a716bce4af46ed3583b222d0f
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-04-30 09:59:52 +0200

    resolves review feedback

--- src/frontend/src/lib/components/ui/ButtonBack.svelte
+++ src/frontend/src/lib/components/ui/ButtonBack.svelte
@@ -9,7 +9,7 @@
 		testId?: string;
 	}
 
-	let { onclick, disabled = false, fullWidth = false, testId = undefined }: Props = $props();
+	let { onclick, disabled = false, fullWidth = false, testId }: Props = $props();
 </script>
 
 <Button
