8d489b0db24ab3245c7873796c30f5dca94499ad
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-04-30 12:12:53 +0200

    refactor(frontend): use directly prop `groupData` of type `TokenUiGroup` (#6213)
    # Motivation
    
    Using prop `groupData` directly to map header data.

--- src/frontend/src/lib/utils/token-card.utils.ts
+++ src/frontend/src/lib/utils/token-card.utils.ts
@@ -1,6 +1,5 @@
 import type { CardData } from '$lib/types/token-card';
 import type { TokenUiGroup } from '$lib/types/token-group';
-import { nonNullish } from '@dfinity/utils';
 
 /** Maps the token group to the card data of the card that will be used as "summary" for the group.
  *
@@ -8,7 +7,8 @@ import { nonNullish } from '@dfinity/utils';
  * @returns {CardData} The card data for the token group
  */
 export const mapHeaderData = ({
-	nativeToken: { name, symbol, decimals, icon, network },
+	groupData: { name, symbol, icon },
+	nativeToken: { decimals, network },
 	tokens,
 	balance,
 	usdBalance
@@ -16,12 +16,11 @@ export const mapHeaderData = ({
 	name,
 	symbol,
 	decimals,
-	icon: nonNullish(tokens[0]?.groupData?.icon) ? tokens[0].groupData.icon : icon, // TODO: this can be directly the prop of TokenUiGroup type
+	icon,
 	network,
 	oisyName: { oisyName: tokens.map((token) => token.symbol).join(', ') },
 	oisySymbol: { oisySymbol: name },
 	balance,
 	usdBalance,
-	tokenCount: tokens.length,
-	groupData: tokens[0]?.groupData // TODO: this can be directly the prop of TokenUiGroup type
+	tokenCount: tokens.length
 });
