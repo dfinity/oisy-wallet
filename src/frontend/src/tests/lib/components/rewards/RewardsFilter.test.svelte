6c148a7ccda2a1c4010c217319b4e1b0a4aadb0d
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-05-02 13:00:00 +0200

    implements feature

---
+++ src/frontend/src/tests/lib/components/rewards/RewardsFilter.test.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import RewardsFilter from '$lib/components/rewards/RewardsFilter.svelte';
+    import type { RewardStates } from '$lib/enums/reward-states';
+
+    interface Props {
+        rewardState: RewardStates;
+        endedCampaignsAmount?: number;
+    }
+
+    let { rewardState = $bindable(), endedCampaignsAmount = 1 }: Props = $props();
+</script>
+
+<RewardsFilter bind:rewardState={$rewardState} {endedCampaignsAmount} />
