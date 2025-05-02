5e91de5b1201070bb58a04cbbb03452d86ea4938
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-05-02 08:39:23 +0200

    implements feature

--- src/frontend/src/lib/components/rewards/RewardsFilter.svelte
+++ src/frontend/src/lib/components/rewards/RewardsFilter.svelte
@@ -3,6 +3,7 @@
 	import { RewardStates } from '$lib/enums/reward-states';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { replacePlaceholders } from '$lib/utils/i18n.utils';
+	import {REWARDS_FILTER} from "$lib/constants/test-ids.constants";
 
 	interface Props {
 		rewardState: RewardStates;
@@ -12,13 +13,14 @@
 	let { rewardState = $bindable(), endedCampaignsAmount = 0 }: Props = $props();
 </script>
 
-<div class="no-scrollbar mb-5 flex gap-4 overflow-x-auto p-1 md:flex-wrap md:p-0">
+<div class="no-scrollbar mb-5 flex gap-4 overflow-x-auto p-1 md:flex-wrap md:p-0" data-tid={REWARDS_FILTER}>
 	<Button
 		paddingSmall
 		ariaLabel={$i18n.rewards.text.ongoing}
 		on:click={() => (rewardState = RewardStates.ONGOING)}
 		styleClass="text-nowrap max-w-28 text-sm"
 		colorStyle={rewardState === RewardStates.ONGOING ? 'primary' : 'tertiary'}
+		testId={`${REWARDS_FILTER}-ongoing-button`}
 	>
 		{$i18n.rewards.text.ongoing}
 	</Button>
@@ -29,6 +31,7 @@
 		on:click={() => (rewardState = RewardStates.ENDED)}
 		styleClass="text-nowrap max-w-28 text-sm"
 		colorStyle={rewardState === RewardStates.ENDED ? 'primary' : 'tertiary'}
+		testId={`${REWARDS_FILTER}-ended-button`}
 	>
 		{replacePlaceholders($i18n.rewards.text.ended, { $amount: endedCampaignsAmount.toString() })}
 	</Button>
