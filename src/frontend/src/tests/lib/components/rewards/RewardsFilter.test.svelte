e6980c171902b7e0cb69efb071ba3e7f5a04d6aa
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-05-02 09:27:44 +0200

    implements feature

---
+++ src/frontend/src/tests/lib/components/rewards/RewardsFilter.test.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import RewardsFilter from "$lib/components/rewards/RewardsFilter.svelte";
+
+    export let rewardState
+</script>
+
+<RewardsFilter bind:rewardState={$rewardState} />
