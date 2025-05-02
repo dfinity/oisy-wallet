812d5f57f9582a3e699a1245e458d811418e27e4
daviddecentage <david.farina@dfinity.org>
2025-05-02 10:33:26 +0200

    update

--- src/frontend/src/lib/components/ui/BottomSheet.svelte
+++ src/frontend/src/lib/components/ui/BottomSheet.svelte
@@ -4,25 +4,42 @@
 	import type { Snippet } from 'svelte';
 	import Responsive from '$lib/components/ui/Responsive.svelte';
 
-	let { content, contentHeader }: { content: Snippet; contentHeader: Snippet } = $props();
+	let {
+		content,
+		contentHeader,
+		bottomSheetFooter
+	}: {
+		content: Snippet;
+		contentHeader: Snippet;
+		bottomSheetFooter: Snippet<[closeFn: () => void]>;
+	} = $props();
 
 	let expanded = $state(false);
 
 	$effect(() => {
 		document.body.classList.toggle('overflow-hidden', expanded);
 	});
+
+	let fullH = $state(false);
 </script>
 
 <Responsive down="md">
 	{#if expanded}
 		<div
-			class="z-199 rounded-t-4xl fixed bottom-0 left-0 right-0 min-h-[60vh] bg-primary p-4 pt-12"
+			class="z-199 rounded-t-4xl fixed bottom-0 left-0 right-0 flex min-h-[40vh] flex-col justify-between bg-primary pt-8"
 			transition:slide={{ axis: 'y', duration: 500 }}
 		>
 			<span
 				class="absolute left-[calc(50%-1.5rem)] top-4 h-[0.4rem] w-[3rem] rounded-full bg-disabled-alt"
 			></span>
-			{@render content()}
+			<div class="flex p-4">
+				{@render content()}
+			</div>
+			<div class="flex border-t border-disabled p-4">
+				{@render bottomSheetFooter(() => {
+					expanded = false;
+				})}
+			</div>
 		</div>
 		<div class="z-198 fixed bottom-0 left-0 right-0 top-0">
 			<Backdrop on:nnsClose={() => (expanded = false)} />
