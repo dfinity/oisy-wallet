2d5b8355a9fe8cfff2ac5ba6a73279aa1d0d5a0f
Flavio Pellanda <flavio.pellanda@inacta.ch>
2025-05-05 09:10:29 +0200

    Add paste button to general input

--- src/frontend/src/lib/types/i18n.d.ts
+++ src/frontend/src/lib/types/i18n.d.ts
@@ -28,6 +28,7 @@ interface I18nCore {
 		new: string;
 		edit: string;
 		no_results: string;
+		paste: string;
 	};
 	info: { test_banner: string };
 	alt: { logo: string; go_to_home: string; back: string };
