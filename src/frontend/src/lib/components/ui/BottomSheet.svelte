327c3328f62d9724cdb4430833b8976fed2e2d27
daviddecentage <david.farina@dfinity.org>
2025-05-05 12:22:36 +0200

    update

--- src/frontend/src/lib/components/ui/BottomSheet.svelte
+++ src/frontend/src/lib/components/ui/BottomSheet.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	import { Collapsible, Backdrop } from '@dfinity/gix-components';
+	import { Collapsible, Backdrop, BottomSheet } from '@dfinity/gix-components';
 	import { nonNullish } from '@dfinity/utils';
 	import type { Snippet } from 'svelte';
 	import Responsive from '$lib/components/ui/Responsive.svelte';
-	import GixBottomSheet from '$lib/components/ui/GixBottomSheet.svelte';
 	import ButtonIcon from '$lib/components/ui/ButtonIcon.svelte';
 	import IconClose from '$lib/components/icons/lucide/IconClose.svelte';
 
@@ -18,16 +17,12 @@
 	} = $props();
 
 	let expanded = $state(false);
-
-	$effect(() => {
-		document.body.classList.toggle('overflow-hidden', expanded);
-	});
 </script>
 
 <Responsive down="md">
 	{#if expanded}
 		<div class="z-14 fixed bottom-0 left-0 right-0 top-0">
-			<GixBottomSheet on:nnsClose={() => (expanded = false)} transition>
+			<BottomSheet on:nnsClose={() => (expanded = false)}>
 				<div slot="header" class="w-full p-4">
 					<ButtonIcon
 						on:click={() => (expanded = false)}
@@ -47,7 +42,7 @@
 						})}
 					{/if}
 				</div>
-			</GixBottomSheet>
+			</BottomSheet>
 			<Backdrop on:nnsClose={() => (expanded = false)} />
 		</div>
 	{/if}
