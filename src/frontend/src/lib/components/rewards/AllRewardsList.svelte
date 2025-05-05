47f604338822489890224a2b69a1b47cf97460a5
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-05-05 11:36:33 +0200

    feat(frontend): adds filter to AllRewardsList (#6314)
    # Motivation
    
    On the `AllRewardsList` we want to use the `RewardsFilter` and only
    display the corresponding rewards.
    
    # Changes
    
    - adds `RewardsFilter`
    
    # Tests
    
    before:
    <img width="953" alt="image"
    src="https://github.com/user-attachments/assets/8c9f241c-cdd2-4c0c-997c-0f125c5bb4d3"
    />
    
    
    after:
    <img width="943" alt="image"
    src="https://github.com/user-attachments/assets/a683b844-34c3-44c8-8132-af7a61cae243"
    />
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/rewards/AllRewardsList.svelte
+++ src/frontend/src/lib/components/rewards/AllRewardsList.svelte
@@ -2,16 +2,21 @@
 	import { rewardCampaigns } from '$env/reward-campaigns.env';
 	import type { RewardDescription } from '$env/types/env-reward';
 	import rewardBanner from '$lib/assets/rewards-banner.svg';
+	import RewardsFilter from '$lib/components/rewards/RewardsFilter.svelte';
 	import RewardsGroup from '$lib/components/rewards/RewardsGroup.svelte';
 	import Img from '$lib/components/ui/Img.svelte';
 	import {
 		REWARDS_ACTIVE_CAMPAIGNS_CONTAINER,
 		REWARDS_BANNER,
+		REWARDS_ENDED_CAMPAIGNS_CONTAINER,
 		REWARDS_UPCOMING_CAMPAIGNS_CONTAINER
 	} from '$lib/constants/test-ids.constants';
+	import { RewardStates } from '$lib/enums/reward-states';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { replaceOisyPlaceholders } from '$lib/utils/i18n.utils';
-	import { isOngoingCampaign, isUpcomingCampaign } from '$lib/utils/rewards.utils';
+	import { isEndedCampaign, isOngoingCampaign, isUpcomingCampaign } from '$lib/utils/rewards.utils';
+
+	let selectedRewardState = $state(RewardStates.ONGOING);
 
 	const ongoingCampaigns: RewardDescription[] = rewardCampaigns.filter(({ startDate, endDate }) =>
 		isOngoingCampaign({ startDate, endDate })
@@ -19,6 +24,10 @@
 
 	const upcomingCampaigns: RewardDescription[] = rewardCampaigns.filter(({ startDate }) =>
 		isUpcomingCampaign(startDate)
+	);
+
+	const endedCampaigns: RewardDescription[] = rewardCampaigns.filter(({ endDate }) =>
+		isEndedCampaign(endDate)
 	);
 </script>
 
@@ -28,15 +37,20 @@
 	</div>
 </div>
 
-<RewardsGroup
-	title={$i18n.rewards.text.active_campaigns}
-	rewards={ongoingCampaigns}
-	testId={REWARDS_ACTIVE_CAMPAIGNS_CONTAINER}
+<RewardsFilter
+	bind:rewardState={selectedRewardState}
+	endedCampaignsAmount={endedCampaigns.length}
 />
 
-<RewardsGroup
-	title={$i18n.rewards.text.upcoming_campaigns}
-	rewards={upcomingCampaigns}
-	altText={replaceOisyPlaceholders($i18n.rewards.alt.upcoming_campaigns)}
-	testId={REWARDS_UPCOMING_CAMPAIGNS_CONTAINER}
-/>
+{#if selectedRewardState === RewardStates.ONGOING}
+	<RewardsGroup rewards={ongoingCampaigns} testId={REWARDS_ACTIVE_CAMPAIGNS_CONTAINER} />
+
+	<RewardsGroup
+		title={$i18n.rewards.text.upcoming_campaigns}
+		rewards={upcomingCampaigns}
+		altText={replaceOisyPlaceholders($i18n.rewards.alt.upcoming_campaigns)}
+		testId={REWARDS_UPCOMING_CAMPAIGNS_CONTAINER}
+	/>
+{:else if selectedRewardState === RewardStates.ENDED}
+	<RewardsGroup rewards={endedCampaigns} testId={REWARDS_ENDED_CAMPAIGNS_CONTAINER} />
+{/if}
