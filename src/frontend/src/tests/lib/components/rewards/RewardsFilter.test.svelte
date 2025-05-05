94ba287ca5c3f71d87ce89ed039b49d5ef720344
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-05-05 09:27:38 +0200

    resolves review feedback

--- src/frontend/src/tests/lib/components/rewards/RewardsFilter.test.svelte
+++ src/frontend/src/tests/lib/components/rewards/RewardsFilter.test.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import RewardsFilter from '$lib/components/rewards/RewardsFilter.svelte';
 	import type { RewardStates } from '$lib/enums/reward-states';
+    import type { Writable } from 'svelte/store';
 
-	interface Props {
-		rewardState: RewardStates;
+    interface Props {
+		rewardState: Writable<RewardStates>;
 		endedCampaignsAmount?: number;
 	}
 
