b6d37ff26ff4a584fea8e6a414f5ea499815f7ec
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-05-05 17:14:51 +0200

    refactor(frontend): upgrades IconShare to svelte 5 (#6335)
    # Motivation
    
    Since we upgraded svelte from V4 to V5 we also want to upgrade the
    components step by step to V5.
    
    
    # Changes
    
    - Upgrades `IconShare`
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/icons/lucide/IconShare.svelte
+++ src/frontend/src/lib/components/icons/lucide/IconShare.svelte
@@ -1,6 +1,10 @@
 <!-- source: ISC Lucide - please visit https://lucide.dev/license -->
 <script lang="ts">
-	export let size = '20';
+	interface Props {
+		size?: string;
+	}
+
+	let { size = '20' }: Props = $props();
 </script>
 
 <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
