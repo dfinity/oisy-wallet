702ffef4b372b53ba98f754d592322f28adfd854
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-05-05 15:19:57 +0200

    displays reward logo as RewardBanner

--- src/frontend/src/lib/components/rewards/RewardBanner.svelte
+++ src/frontend/src/lib/components/rewards/RewardBanner.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
-	import rewardBanner from '$lib/assets/reward-modal-banner.svg';
-	import ImgBanner from '$lib/components/ui/ImgBanner.svelte';
 	import { REWARDS_MODAL_IMAGE_BANNER } from '$lib/constants/test-ids.constants';
+	import {isEndedCampaign} from "$lib/utils/rewards.utils";
+	import Img from "$lib/components/ui/Img.svelte";
+	import type { RewardDescription } from '$env/types/env-reward';
+	import {replacePlaceholders} from "$lib/utils/i18n.utils";
+	import { i18n } from '$lib/stores/i18n.store';
+
+	interface Props {
+		reward: RewardDescription;
+	}
+
+	let { reward }: Props = $props();
+
+	const hasEnded = $derived(isEndedCampaign(reward.endDate));
 </script>
 
-<div class="relative mb-5 flex overflow-hidden rounded-2xl">
-	<div class="max-h-60">
-		<ImgBanner src={rewardBanner} testId={REWARDS_MODAL_IMAGE_BANNER} styleClass="object-cover" />
-	</div>
+<div class="mb-5 max-h-60 overflow-hidden rounded-2xl">
+	<Img src={reward.logo} testId={REWARDS_MODAL_IMAGE_BANNER} grayscale={hasEnded}
+		 alt={replacePlaceholders($i18n.rewards.alt.reward_logo, {
+					$campaignName: reward.cardTitle
+				})} />
 </div>
