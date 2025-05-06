c5ce28e9aa212a82bd2dc4899cc052c4dc7b971a
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-05-06 13:23:41 +0200

    implements feature

--- src/frontend/src/lib/components/receive/ReceiveModal.svelte
+++ src/frontend/src/lib/components/receive/ReceiveModal.svelte
@@ -17,10 +17,13 @@
 
 	export let network: Network;
 	export let copyAriaLabel: string;
+
+	let title: string | undefined;
+	$: title = isNullish($token) ? network.name : addressToken?.symbol;
 </script>
 
 <Modal on:nnsClose={modalStore.close}>
-	<ReceiveTitle slot="title" title={isNullish($token) ? network.name : addressToken.symbol} />
+	<ReceiveTitle slot="title" {title} />
 
 	<ContentWithToolbar>
 		<ReceiveAddressQRCodeContent
