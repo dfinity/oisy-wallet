9d1b7c541e026dea195aa461ea2292c29874dcfb
FlavioInacta <flavio.pellanda@inacta.ch>
2025-05-06 17:27:54 +0200

    feat(frontend): Adds reset button to name input of address form (#6308)
    # Motivation
    
    For easily resetting the name field, the user should see the reset
    button of the common Input component.

--- src/frontend/src/lib/components/ui/InputText.svelte
+++ src/frontend/src/lib/components/ui/InputText.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	import { Input } from '@dfinity/gix-components';
+	import Input from '$lib/components/ui/Input.svelte';
 
 	export let value = '';
 	export let name: string;
 	export let placeholder: string;
+	export let showResetButton = false;
 	export let required = true;
 	export let testId: string | undefined = undefined;
 </script>
@@ -14,6 +15,7 @@
 	{required}
 	bind:value
 	{placeholder}
+	{showResetButton}
 	spellcheck={false}
 	autocomplete="off"
 	{testId}
