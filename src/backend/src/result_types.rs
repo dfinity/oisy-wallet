3f1c07f8c5591066e69b1061cb4549e16de95041
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-05-06 21:52:33 +0200

    variant

--- src/backend/src/result_types.rs
+++ src/backend/src/result_types.rs
@@ -5,14 +5,14 @@ use shared::types::user_profile::AddUserCredentialError;
 #[derive(CandidType, Serialize, Deserialize, Clone, Eq, PartialEq, Debug)]
 pub enum AddUserCredentialResult {
     /// The user's credential was added successfully.
-    Ok(),
+    Ok(()),
     /// The user's credential was not added due to an error.
     Err(AddUserCredentialError),
 }
 impl From<Result<(), AddUserCredentialError>> for AddUserCredentialResult {
     fn from(result: Result<(), AddUserCredentialError>) -> Self {
         match result {
-            Ok(()) => AddUserCredentialResult::Ok(),
+            Ok(()) => AddUserCredentialResult::Ok(()),
             Err(err) => AddUserCredentialResult::Err(err),
         }
     }
