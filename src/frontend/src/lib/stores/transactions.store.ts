15979ac7d66ae60c56cf2f7c242f46158c2b7015
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-05-06 22:20:12 +0200

    refactor(frontend): specify generic type of initTransactionsStore

--- src/frontend/src/lib/stores/transactions.store.ts
+++ src/frontend/src/lib/stores/transactions.store.ts
@@ -1,27 +1,37 @@
 import type { BtcTransactionUi } from '$btc/types/btc';
 import type { IcTransactionUi } from '$icp/types/ic-transaction';
 import { initCertifiedStore, type CertifiedStore } from '$lib/stores/certified.store';
+import type { OptionIdentity } from '$lib/types/identity';
 import type { CertifiedData } from '$lib/types/store';
 import type { TokenId } from '$lib/types/token';
 import type { SolTransactionUi } from '$sol/types/sol-transaction';
 import { nonNullish } from '@dfinity/utils';
 
-export type CertifiedTransaction<T> = CertifiedData<T>;
+type TransactionTypes = IcTransactionUi | BtcTransactionUi | SolTransactionUi;
+
+export type CertifiedTransaction<T extends TransactionTypes> = CertifiedData<T>;
+
+export type TransactionsStoreParams<T extends TransactionTypes> = {
+	tokenId: TokenId;
+	transactions: CertifiedTransaction<T>[];
+	identity: OptionIdentity;
+};
 
 export type NullableCertifiedTransactions = null;
 
-export type TransactionsData<T> = CertifiedTransaction<T>[] | NullableCertifiedTransactions;
+export type TransactionsData<T extends TransactionTypes> =
+	| CertifiedTransaction<T>[]
+	| NullableCertifiedTransactions;
 
-export interface TransactionsStore<T> extends CertifiedStore<TransactionsData<T>> {
-	prepend: (params: { tokenId: TokenId; transactions: CertifiedTransaction<T>[] }) => void;
-	append: (params: { tokenId: TokenId; transactions: CertifiedTransaction<T>[] }) => void;
+export interface TransactionsStore<T extends TransactionTypes>
+	extends CertifiedStore<TransactionsData<T>> {
+	prepend: (params: TransactionsStoreParams<T>) => void;
+	append: (params: TransactionsStoreParams<T>) => void;
 	cleanUp: (params: { tokenId: TokenId; transactionIds: string[] }) => void;
 	nullify: (tokenId: TokenId) => void;
 }
 
-export const initTransactionsStore = <
-	T extends IcTransactionUi | BtcTransactionUi | SolTransactionUi
->(): TransactionsStore<T> => {
+export const initTransactionsStore = <T extends TransactionTypes>(): TransactionsStore<T> => {
 	const { subscribe, update, reset } = initCertifiedStore<TransactionsData<T>>();
 
 	return {
