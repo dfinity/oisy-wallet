6228246fd307596e98019be2ec24cc3b32cf2408
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-05-06 14:21:43 +0200

    ++

---
+++ scripts/build.xtc_ledger.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+print_help() {
+  cat <<-EOF
+	Creates the Kong Backend installation files:
+
+	- The Wasm and Candid files are downloaded.
+
+	The files are installed at at the locations defined for 'xtc_ledger' in 'dfx.json'.
+	EOF
+}
+
+[[ "${1:-}" != "--help" ]] || {
+  print_help
+  exit 0
+}
+
+KONG_BUILDENV="$DFX_NETWORK"
+export KONG_BUILDENV
+
+KONG_REPO_URL="https://raw.githubusercontent.com/Psychedelic/dank/refs/heads/develop/candid"
+# shellcheck disable=SC2034 # This variable is used - see ${!asset_url} below.
+CANDID_URL="${KONG_REPO_URL}/xtc.did"
+# shellcheck disable=SC2034 # This variable is used - see ${!asset_url} below.
+WASM_URL="https://github.com/dfinity/cycles-ledger/releases/download/cycles-ledger-v1.0.1/cycles-ledger.wasm.gz"
+
+CANDID_FILE="$(jq -r .canisters.xtc_ledger.candid dfx.json)"
+WASM_FILE="$(jq -r .canisters.xtc_ledger.wasm dfx.json)"
+
+download() {
+  : 'Downloads a URL to a given file.'
+  # shellcheck disable=SC2016 # The $ in the comment is not meant to be expanded.
+  : '* With argument x, the filename is $X_FILE and the URL is $X_URL'
+  : '* If the file already exists, the user is prompted whether to overwrite, keeping the existing file by default.'
+  local asset asset_url asset_file response
+  asset="$1"
+  asset_url="${asset^^}_URL"
+  asset_file="${asset^^}_FILE"
+  : 'If the asset file already exists, ask the user whether to overwrite it.'
+  if test -e "${!asset_file}" && read -r -p "Overwrite existing ${!asset_file}? [y/N] " response && [[ "${response,,}" != y* ]]; then
+    echo "Using existing kong $asset file."
+  else
+    echo "Downloading ${!asset_url} --> ${!asset_file}"
+    mkdir -p "$(dirname "${!asset_file}")"
+    curl -sSL "${!asset_url}" >"${!asset_file}"
+  fi
+}
+
+####
+# Downloads the candid file, if it does not exist already.
+download candid
+
+####
+# Downloads the Wasm file, if it does not exist already.
+download wasm
+
+####
+# Success
+cat <<EOF
+SUCCESS: The kong_backend installation files have been created:
+kong_backend candid:       $CANDID_FILE
+kong_backend Wasm:         $WASM_FILE
+EOF
