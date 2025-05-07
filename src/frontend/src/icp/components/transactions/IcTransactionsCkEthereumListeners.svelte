628f34f4068c0c9c1ff0a780e7f3ac7e046de246
daviddecentage <david.farina@dfinity.org>
2025-05-07 10:13:12 +0200

    update

--- src/frontend/src/icp/components/transactions/IcTransactionsCkEthereumListeners.svelte
+++ src/frontend/src/icp/components/transactions/IcTransactionsCkEthereumListeners.svelte
@@ -3,20 +3,27 @@
 	import { initCkETHMinterInfoWorker } from '$icp/services/worker.ck-minter-info.services';
 	import type { OptionIcCkToken } from '$icp/types/ic-token';
 	import CkEthereumPendingTransactionsListener from '$icp-eth/components/core/CkEthereumPendingTransactionsListener.svelte';
-	import { ckEthereumNativeToken } from '$icp-eth/derived/cketh.derived';
-	import { token } from '$lib/stores/token.store';
 	import type { CanisterIdText } from '$lib/types/canister';
+	import type { OptionToken, Token } from '$lib/types/token';
+	import type { Snippet } from 'svelte';
 
-	let minterCanisterId: CanisterIdText | undefined = undefined;
-	$: minterCanisterId = ($token as OptionIcCkToken)?.minterCanisterId;
+	interface Props {
+		token: OptionToken;
+		ckEthereumNativeToken: Token;
+		children?: Snippet;
+	}
+
+	let { token, ckEthereumNativeToken, children }: Props = $props();
+
+	let minterCanisterId: CanisterIdText | undefined = $derived(
+		(token as OptionIcCkToken)?.minterCanisterId
+	);
 </script>
 
-<IcCkListener
-	initFn={initCkETHMinterInfoWorker}
-	token={$ckEthereumNativeToken}
-	{minterCanisterId}
-/>
+<IcCkListener initFn={initCkETHMinterInfoWorker} token={ckEthereumNativeToken} {minterCanisterId} />
 
 <CkEthereumPendingTransactionsListener />
 
-<slot />
+{#if children}
+	{@render children()}
+{/if}
