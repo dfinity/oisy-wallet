6ba3027facce17bb4543388f404befeb80ad0149
Flavio Pellanda <flavio.pellanda@inacta.ch>
2025-05-07 15:24:56 +0200

    Make buttons reusable

--- src/frontend/src/lib/components/ui/Input.svelte
+++ src/frontend/src/lib/components/ui/Input.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-	import { Input as GixInput, IconClose } from '@dfinity/gix-components';
+	import { Input as GixInput } from '@dfinity/gix-components';
 	import { nonNullish, notEmptyString } from '@dfinity/utils';
 	import type { ComponentProps, Snippet } from 'svelte';
 	import { fade } from 'svelte/transition';
-	import Button from '$lib/components/ui/Button.svelte';
-	import ButtonIcon from '$lib/components/ui/ButtonIcon.svelte';
-	import { i18n } from '$lib/stores/i18n.store';
+	import ButtonPaste from '$lib/components/ui/ButtonPaste.svelte';
+	import ButtonReset from '$lib/components/ui/ButtonReset.svelte';
 
 	type GixInputProps = ComponentProps<GixInput>;
 	type InputProps = {
@@ -25,56 +24,22 @@
 		innerEnd,
 		...props
 	}: InputProps & GixInputProps = $props();
-
-	const handleReset = () => {
-		value = undefined;
-	};
-
-	const handlePaste = () => {
-		try {
-			navigator.clipboard.readText().then((text) => {
-				value = text;
-			});
-		} catch (e) {
-			console.error('Failed to paste clipboard', e);
-		}
-	};
 </script>
-
-{#snippet resetButton()}
-	<div transition:fade>
-		<ButtonIcon
-			styleClass="border-0 px-0 py-2 text-tertiary"
-			on:click={handleReset}
-			ariaLabel={resetButtonAriaLabel ?? $i18n.convert.text.input_reset_button}
-		>
-			<IconClose slot="icon" size="24" />
-		</ButtonIcon>
-	</div>
-	{#if nonNullish(innerEnd) || showPasteButton}
-		<div class="border-r-1 border-black/20" transition:fade></div>
-	{/if}
-{/snippet}
-
-{#snippet pasteButton()}
-	<Button
-		type="button"
-		styleClass="text-sm px-1 py-2 mx-1"
-		colorStyle="tertiary-alt"
-		ariaLabel={$i18n.core.text.paste}
-		on:click={handlePaste}>{$i18n.core.text.paste}</Button
-	>
-{/snippet}
 
 <div style={`--input-padding-inner-end: calc(var(--padding-2x) + ${endWidth}px)`}>
 	<GixInput {...props} bind:value on:nnsInput>
 		<svelte:fragment slot="inner-end">
-			<div bind:clientWidth={endWidth} class="flex pl-2">
+			<div bind:clientWidth={endWidth} class="flex items-center pl-2">
 				{#if nonNullish(value) && notEmptyString(value.toString()) && showResetButton}
-					{@render resetButton()}
+					<div transition:fade>
+						<ButtonReset onclick={() => (value = undefined)} ariaLabel={resetButtonAriaLabel} />
+					</div>
+					{#if nonNullish(innerEnd) || showPasteButton}
+						<div class="border-r-1 self-stretch border-black/20" transition:fade></div>
+					{/if}
 				{/if}
 				{#if showPasteButton}
-					{@render pasteButton()}
+					<ButtonPaste onpaste={(text) => (value = text)} />
 				{/if}
 				{@render innerEnd?.()}
 			</div>
