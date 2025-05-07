63d600c40f773356c26e2927a2422a34884724b7
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-05-07 22:07:00 +0200

    fix

--- src/frontend/src/lib/api/icp-swap-factory.api.ts
+++ src/frontend/src/lib/api/icp-swap-factory.api.ts
@@ -1,28 +1,25 @@
 import type { PoolData } from '$declarations/icp_swap_factory/icp_swap_factory.did';
 import { ICPSwapFactoryCanister } from '$lib/canisters/icp-swap-factory.canister';
-import { ICP_SWAP_CANISTER_ID } from '$lib/constants/app.constants';
 import type { ICPSwapGetPoolParams } from '$lib/types/api';
 import type { CanisterApiFunctionParams } from '$lib/types/canister';
 import { Principal } from '@dfinity/principal';
-import { assertNonNullish } from '@dfinity/utils';
+import { assertNonNullish, isNullish } from '@dfinity/utils';
 
-// let canister: ICPSwapFactoryCanister | undefined = undefined;
+let canister: ICPSwapFactoryCanister | undefined = undefined;
 
 const icpSwapFactoryCanister = async ({
 	identity,
 	nullishIdentityErrorMessage,
-	canisterId = ICP_SWAP_CANISTER_ID
+	canisterId = '4mmnk-kiaaa-aaaag-qbllq-cai'
 }: CanisterApiFunctionParams): Promise<ICPSwapFactoryCanister> => {
 	assertNonNullish(identity, nullishIdentityErrorMessage);
 
-	console.log('icpSwapFactoryCanister', canisterId);
-
-	const canister = await ICPSwapFactoryCanister.create({
-		identity,
-		canisterId: Principal.fromText(canisterId)
-	});
-
-	console.log('icpSwapFactoryCanister canister', canister);
+	if (isNullish(canister)) {
+		canister = await ICPSwapFactoryCanister.create({
+			identity,
+			canisterId: Principal.fromText(canisterId)
+		});
+	}
 
 	return canister;
 };
@@ -33,8 +30,6 @@ export const getPool = async ({
 	canisterId,
 	...restParams
 }: CanisterApiFunctionParams<ICPSwapGetPoolParams>): Promise<PoolData> => {
-	console.log(`before getPool: ${canisterId}`);
-
 	const { getPool } = await icpSwapFactoryCanister({
 		identity,
 		canisterId,
