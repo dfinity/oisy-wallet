6f5441b8a5ac1c88e62cb568d4794fa0f149773a
daviddecentage <david.farina@dfinity.org>
2025-05-07 10:14:47 +0200

    update

--- src/frontend/src/icp-eth/utils/cketh-transactions.utils.ts
+++ src/frontend/src/icp-eth/utils/cketh-transactions.utils.ts
@@ -59,7 +59,7 @@ const mapPendingTransaction = ({
 
 	return {
 		id: `${hash}`,
-		incoming: true,
+		incoming: true, // we mark this as incoming so we display a positive balance, as all pending mapped ckEth txs are incoming
 		type: 'burn',
 		status: 'pending',
 		from,
