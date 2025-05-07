31ed58761bc43ed1696decf426f77b8d0de8f723
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-05-07 17:20:37 +0200

    feat(frontend): update SendInputDestination component (#6404)
    # Motivation
    
    A few updates on the existing SendInputDestination component:
    
    1. Label is now a part of the component.
    2. There is an additional warning message for an unknown destination.
    3. Tests added.

--- src/frontend/src/lib/components/send/SendInputDestination.svelte
+++ src/frontend/src/lib/components/send/SendInputDestination.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-	import { debounce, nonNullish } from '@dfinity/utils';
+	import { debounce, isNullish, nonNullish, notEmptyString } from '@dfinity/utils';
 	import { slide } from 'svelte/transition';
 	import QrButton from '$lib/components/common/QrButton.svelte';
 	import InputTextWithAction from '$lib/components/ui/InputTextWithAction.svelte';
+	import MessageBox from '$lib/components/ui/MessageBox.svelte';
 	import { DESTINATION_INPUT } from '$lib/constants/test-ids.constants';
 	import { SLIDE_DURATION } from '$lib/constants/transition.constants';
 	import { i18n } from '$lib/stores/i18n.store';
 	import type { NetworkId } from '$lib/types/network';
+	import type { KnownDestinations } from '$lib/types/transactions';
 
 	export let destination = '';
 	export let networkId: NetworkId | undefined = undefined;
@@ -14,6 +16,7 @@
 	export let inputPlaceholder: string;
 	export let isInvalidDestination: (() => boolean) | undefined;
 	export let onQRButtonClick: (() => void) | undefined = undefined;
+	export let knownDestinations: KnownDestinations | undefined = undefined;
 
 	const validate = () => (invalidDestination = isInvalidDestination?.() ?? false);
 
@@ -22,7 +25,9 @@
 	$: destination, networkId, isInvalidDestination, debounceValidate();
 </script>
 
-<slot name="label" />
+<label for="destination" class="font-bold">
+	{$i18n.core.text.to}
+</label>
 
 <div class="mb-4">
 	<InputTextWithAction
@@ -44,4 +49,10 @@
 	<p transition:slide={SLIDE_DURATION} class="pb-3 text-error-primary">
 		{$i18n.send.assertion.invalid_destination_address}
 	</p>
+{:else if notEmptyString(destination) && nonNullish(knownDestinations) && isNullish(knownDestinations[destination])}
+	<div transition:slide={SLIDE_DURATION}>
+		<MessageBox level="warning">
+			{$i18n.send.info.unknown_destination}
+		</MessageBox>
+	</div>
 {/if}
