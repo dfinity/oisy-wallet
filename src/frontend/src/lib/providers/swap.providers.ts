97cd99fa99e9f8b1d204101e1483232d737a549b
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-05-08 13:54:41 +0200

    fix

--- src/frontend/src/lib/providers/swap.providers.ts
+++ src/frontend/src/lib/providers/swap.providers.ts
@@ -5,16 +5,16 @@ import { SwapProvider, type SwapProviderConfig } from '$lib/types/swap';
 import { mapIcpSwapResult, mapKongSwapResult } from '$lib/utils/swap.utils';
 
 export const swapProviders: SwapProviderConfig[] = [
-	// {
-	// 	key: SwapProvider.KONG_SWAP,
-	// 	getQuote: kongSwapAmounts,
-	// 	mapQuoteResult: ({ swap, tokens }) => mapKongSwapResult({ swap, tokens }),
-	// 	isEnabled: true
-	// },
+	{
+		key: SwapProvider.KONG_SWAP,
+		getQuote: kongSwapAmounts,
+		mapQuoteResult: ({ swap, tokens }) => mapKongSwapResult({ swap, tokens }),
+		isEnabled: true
+	},
 	{
 		key: SwapProvider.ICP_SWAP,
 		getQuote: icpSwapAmounts,
 		mapQuoteResult: ({ swap, slippage }) => mapIcpSwapResult({ swap, slippage }),
-		isEnabled: ICP_SWAP_ENABLED
+		isEnabled: true
 	}
 ];
