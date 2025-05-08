54a385c935b8f09bbe9496c6e346d00cd30c9189
FlavioInacta <flavio.pellanda@inacta.ch>
2025-05-08 08:38:21 +0200

    feat(frontend): Adds contact colors to avatar (#6343)
    # Motivation
    
    The avatar should use the colors of the design based on the contact
    name.
    
    # Demo
    
    
    [vokoscreenNG-2025-05-07_13-52-21.webm](https://github.com/user-attachments/assets/46b06210-bbc0-4c01-98c4-3f119deb7adb)

--- src/frontend/src/lib/components/contact/Avatar.svelte
+++ src/frontend/src/lib/components/contact/Avatar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { nonNullish } from '@dfinity/utils';
+	import { isEmptyString, nonNullish } from '@dfinity/utils';
 	import IconAvatar from '$lib/components/icons/IconAvatar.svelte';
 	import type { AvatarVariants } from '$lib/types/style';
 
@@ -9,6 +9,20 @@
 		styleClass?: string;
 	}
 	const { name, variant = 'md', styleClass }: AvatarProps = $props();
+	// This constant is needed because all classes need to be somewhere in the
+	// sourcecode. Otherwise tailwind will not include the classes in the css.
+	// Compare: https://v3.tailwindcss.com/docs/content-configuration#class-detection-in-depth
+	const COLORS = [
+		`bg-contact-1`,
+		`bg-contact-2`,
+		`bg-contact-3`,
+		`bg-contact-4`,
+		`bg-contact-5`,
+		`bg-contact-6`,
+		`bg-contact-7`,
+		`bg-contact-8`,
+		`bg-contact-9`
+	];
 
 	const font = $derived(
 		{
@@ -20,7 +34,23 @@
 		}[variant]
 	);
 	let size = $derived(variant === 'xl' ? 'size-25' : 'size-[2.5em]');
-	let commonClasses = $derived(`${font} ${size} rounded-full`);
+
+	const computeContactColor = (contactName?: string) => {
+		const trimmedName = contactName?.trim?.();
+		if (isEmptyString(trimmedName)) {
+			return '';
+		}
+
+		let hash = 0;
+		for (let i = 0; i < trimmedName.length; i++) {
+			hash = (hash + trimmedName.charCodeAt(i)) % 9;
+		}
+
+		return COLORS[hash];
+	};
+	let bgColor = $derived(computeContactColor(name));
+
+	let commonClasses = $derived(`${font} ${size} ${bgColor} rounded-full`);
 
 	const initials = $derived(
 		(nonNullish(name) ? name : '')
@@ -38,7 +68,7 @@
 	</div>
 {:else}
 	<span
-		class={`${commonClasses} inline-block inline-flex items-center justify-center bg-[lightgray] font-bold text-white ${styleClass}`}
+		class={`${commonClasses} inline-block inline-flex items-center justify-center font-bold text-white transition-colors duration-1000 ${styleClass}`}
 		>{initials}</span
 	>
 {/if}
