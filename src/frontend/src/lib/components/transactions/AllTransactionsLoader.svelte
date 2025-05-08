e1b22efb06732f975ffceda8e0081cd72534e46a
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-05-08 23:50:27 +0200

    tests

--- src/frontend/src/lib/components/transactions/AllTransactionsLoader.svelte
+++ src/frontend/src/lib/components/transactions/AllTransactionsLoader.svelte
@@ -1,34 +1,19 @@
 <script lang="ts">
-	import { isNullish } from '@dfinity/utils';
+	import { isNullish, nonNullish } from '@dfinity/utils';
 	import { onMount, type Snippet } from 'svelte';
 	import { loadNextIcTransactions } from '$icp/services/ic-transactions.services';
 	import { icTransactionsStore } from '$icp/stores/ic-transactions.store';
 	import type { IcTransactionUi } from '$icp/types/ic-transaction';
-	import { isIcToken, isIcTokenCanistersStrict } from '$icp/validation/ic-token.validation';
 	import { WALLET_PAGINATION } from '$lib/constants/app.constants';
-	import {
-		solAddressDevnet,
-		solAddressLocal,
-		solAddressMainnet,
-		solAddressTestnet
-	} from '$lib/derived/address.derived';
 	import { authIdentity } from '$lib/derived/auth.derived';
 	import { enabledNetworkTokens } from '$lib/derived/network-tokens.derived';
 	import { nullishSignOut } from '$lib/services/auth.services';
 	import type { Token, TokenId } from '$lib/types/token';
 	import type { AllTransactionUiWithCmp } from '$lib/types/transaction';
-	import {
-		isNetworkIdICP,
-		isNetworkIdSolana,
-		isNetworkIdSOLDevnet,
-		isNetworkIdSOLLocal,
-		isNetworkIdSOLTestnet
-	} from '$lib/utils/network.utils';
+	import { isNetworkIdICP, isNetworkIdSolana } from '$lib/utils/network.utils';
 	import { loadNextSolTransactions } from '$sol/services/sol-transactions.services.js';
 	import { solTransactionsStore } from '$sol/stores/sol-transactions.store';
 	import type { SolTransactionUi } from '$sol/types/sol-transaction';
-	import { mapNetworkIdToNetwork } from '$sol/utils/network.utils';
-	import { isTokenSpl } from '$sol/utils/spl.utils';
 
 	interface Props {
 		transactions: AllTransactionUiWithCmp[];
@@ -53,12 +38,16 @@
 			...transactions.map(({ transaction: { timestamp } }) => Number(timestamp))
 		);
 
-		const findLastTransaction = <T extends IcTransactionUi | SolTransactionUi>(transactions: T[]) =>
-			transactions.reduce<T>(
-				(min, transaction) =>
-					(transaction.timestamp ?? Infinity) < (min.timestamp ?? Infinity) ? transaction : min,
-				transactions[0]
-			);
+		const findLastTransaction = <T extends IcTransactionUi | SolTransactionUi>(
+			transactions: T[]
+		): T | undefined =>
+			transactions.length >= 0
+				? transactions.reduce<T>(
+						(min, transaction) =>
+							(transaction.timestamp ?? Infinity) < (min.timestamp ?? Infinity) ? transaction : min,
+						transactions[0]
+					)
+				: undefined;
 
 		const loadNextTransactions = async (token: Token) => {
 			const {
@@ -71,7 +60,7 @@
 			}
 
 			// To be type-safe we need to check for valid interfaces too
-			if (isNetworkIdICP(networkId) && isIcToken(token) && isIcTokenCanistersStrict(token)) {
+			if (isNetworkIdICP(networkId)) {
 				const icTransactions = ($icTransactionsStore?.[tokenId] ?? []).map(({ data }) => data);
 
 				if (icTransactions.length === 0) {
@@ -80,30 +69,17 @@
 
 				const lastIcTransaction = findLastTransaction(icTransactions);
 
-				const { timestamp: minIcTimestamp, id: lastIcId } = lastIcTransaction;
+				const { timestamp: minIcTimestamp, id: lastIcId } = lastIcTransaction ?? {};
 
-				if (Number(minIcTimestamp) <= minTimestamp) {
-					return;
-				}
-
-				if (isNullish(lastIcId)) {
-					// No transactions, we do nothing here and wait for the worker to post the first transactions
-					return;
-				}
-
-				try {
-					BigInt(lastIcId.replace('-self', ''));
-				} catch {
-					// Pseudo transactions are displayed at the end of the list. There is not such use case in Oisy.
-					// Additionally, if it would be the case, that would mean that we display pseudo transactions at the end of the list and therefore we could assume all valid transactions have been fetched
+				if (nonNullish(minIcTimestamp) && Number(minIcTimestamp) <= minTimestamp) {
 					return;
 				}
 
 				await loadNextIcTransactions({
+					lastId: lastIcId,
 					owner: $authIdentity.getPrincipal(),
 					identity: $authIdentity,
 					maxResults: WALLET_PAGINATION,
-					start: BigInt(lastIcId.replace('-self', '')),
 					token,
 					signalEnd: () => (disableLoader[tokenId] = true)
 				});
@@ -113,41 +89,17 @@
 			} else if (isNetworkIdSolana(networkId)) {
 				const solTransactions = ($solTransactionsStore?.[tokenId] ?? []).map(({ data }) => data);
 
-				if (solTransactions.length === 0) {
-					return;
-				}
-
 				const lastSolTransaction = findLastTransaction(solTransactions);
 
-				const { timestamp: minSolTimestamp, signature: lastSolSignature } = lastSolTransaction;
+				const { timestamp: minSolTimestamp, signature: lastSolSignature } =
+					lastSolTransaction ?? {};
 
-				if (Number(minSolTimestamp) <= minTimestamp) {
+				if (nonNullish(minSolTimestamp) && Number(minSolTimestamp) <= minTimestamp) {
 					return;
 				}
-
-				const network = mapNetworkIdToNetwork(networkId);
-
-				const address = isNetworkIdSOLTestnet(networkId)
-					? $solAddressTestnet
-					: isNetworkIdSOLDevnet(networkId)
-						? $solAddressDevnet
-						: isNetworkIdSOLLocal(networkId)
-							? $solAddressLocal
-							: $solAddressMainnet;
-
-				if (isNullish(network) || isNullish(address)) {
-					return;
-				}
-
-				const { address: tokenAddress, owner: tokenOwnerAddress } = isTokenSpl(token)
-					? token
-					: { address: undefined, owner: undefined };
 
 				await loadNextSolTransactions({
-					network,
-					address,
-					tokenAddress,
-					tokenOwnerAddress,
+					token,
 					before: lastSolSignature,
 					signalEnd: () => (disableLoader[tokenId] = true)
 				});
