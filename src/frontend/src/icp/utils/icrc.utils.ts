9878f1f83259147078e7dbf975e16b00b54b5ab5
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-05-09 11:35:01 +0200

    feat(frontend): add XTC token

--- src/frontend/src/icp/utils/icrc.utils.ts
+++ src/frontend/src/icp/utils/icrc.utils.ts
@@ -107,6 +107,9 @@ export const isTokenIcp = (token: Partial<IcToken>): token is IcToken => token.s
 
 export const isTokenIcrc = (token: Partial<IcToken>): token is IcToken => token.standard === 'icrc';
 
+export const isTokenDip20 = (token: Partial<IcToken>): token is IcToken =>
+	token.standard === 'dip20';
+
 export const isTokenIc = (token: Partial<IcToken>): token is IcToken =>
 	isTokenIcp(token) || isTokenIcrc(token);
 
