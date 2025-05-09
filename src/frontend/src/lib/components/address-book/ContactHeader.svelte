4b5c33b37e5282fabbcce0381d2bddc4ec5fa3b9
FlavioInacta <flavio.pellanda@inacta.ch>
2025-05-09 22:59:30 +0200

    feat(frontend): Add contact header (#6246)
    # Motivation
    
    Adds the contact header with background and edit button.
    
    Figma:
    https://www.figma.com/design/dUNegIE5geiWu7916IC07c/6.-OISY---Theme-library?node-id=7373-30108&t=O5MsV6XqIDmCJOHu-0
    
    # Screenshot
    
    
    ![Screenshot_2025-05-08_08-55-18](https://github.com/user-attachments/assets/3e458ae1-79ea-4c96-ba08-77e1b77cf9fb)
    
    Mobile:
    
    
    ![Screenshot_2025-05-08_08-55-50](https://github.com/user-attachments/assets/ed7e1b11-b0d0-4f2c-a571-f4a078d2cde6)

---
+++ src/frontend/src/lib/components/address-book/ContactHeader.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import Avatar from '$lib/components/contact/Avatar.svelte';
+	import IconContactHeader from '$lib/components/icons/IconContactHeader.svelte';
+	import IconPencil from '$lib/components/icons/lucide/IconPencil.svelte';
+	import Button from '$lib/components/ui/Button.svelte';
+	import { CONTACT_TEXT_COLORS } from '$lib/constants/contact.constants';
+	import { CONTACT_HEADER_EDIT_BUTTON } from '$lib/constants/test-ids.constants';
+	import { i18n } from '$lib/stores/i18n.store';
+	import { selectColorForName } from '$lib/utils/contact.utils';
+
+	let {
+		name,
+		edit,
+		styleClass = ''
+	}: { name: string; edit: () => void; styleClass?: string } = $props();
+
+	let color = $derived(selectColorForName({ name, colors: CONTACT_TEXT_COLORS }));
+</script>
+
+<div class={`relative flex w-full flex-col items-center pb-5 ${styleClass}`}>
+	<Button
+		styleClass="absolute top-2.5 right-2.5 bg-black/16 dark:bg-black/10 px-3 py-2 font-xs"
+		ariaLabel={$i18n.core.text.edit}
+		colorStyle="secondary"
+		testId={CONTACT_HEADER_EDIT_BUTTON}
+		on:click={edit}
+	>
+		<span class="flex items-center">
+			<IconPencil />
+		</span>
+		{$i18n.core.text.edit}
+	</Button>
+
+	<div class={`self-stretch ${color} transition-colors duration-1000`}>
+		<IconContactHeader />
+	</div>
+	<div>
+		<Avatar variant="xl" {name} styleClass="mt-[-50%] border-3 border-primary-inverted"></Avatar>
+	</div>
+	<div class="pt-5 text-2xl font-bold text-primary md:text-3xl">
+		{name}
+	</div>
+</div>
