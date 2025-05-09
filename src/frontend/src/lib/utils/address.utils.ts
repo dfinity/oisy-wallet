d4ab1a7cfd6804a5bed0f93aabc2b7a70a607440
Flavio Pellanda <flavio.pellanda@inacta.ch>
2025-05-09 15:45:04 +0200

    Add supported networks to address list item

--- src/frontend/src/lib/utils/address.utils.ts
+++ src/frontend/src/lib/utils/address.utils.ts
@@ -1,5 +1,10 @@
+import { SUPPORTED_BITCOIN_NETWORKS } from '$env/networks/networks.btc.env';
+import { SUPPORTED_ETHEREUM_NETWORKS } from '$env/networks/networks.eth.env';
+import { ICP_NETWORK } from '$env/networks/networks.icp.env';
+import { SUPPORTED_SOLANA_NETWORKS } from '$env/networks/networks.sol.env';
 import type { StorageAddressData } from '$lib/stores/address.store';
-import type { Address, OptionAddress } from '$lib/types/address';
+import type { Address, AddressType, OptionAddress } from '$lib/types/address';
+import type { Network } from '$lib/types/network';
 import { mapCertifiedData } from '$lib/utils/certified-store.utils';
 import { parseBtcAddress, type BtcAddress } from '@dfinity/ckbtc';
 import { isNullish } from '@dfinity/utils';
@@ -34,3 +39,13 @@ export const shortenAddress = (address: string) => {
 
 	return `${prefix}...${suffix}`;
 };
+
+const ADDRESS_TYPE_TO_NETWORKS: { [key in AddressType]: Network[] } = {
+	Icrc2: [ICP_NETWORK],
+	Btc: SUPPORTED_BITCOIN_NETWORKS,
+	Eth: SUPPORTED_ETHEREUM_NETWORKS,
+	Sol: SUPPORTED_SOLANA_NETWORKS
+};
+
+export const getNetworksForAddressType = (addressType: AddressType): Network[] =>
+	ADDRESS_TYPE_TO_NETWORKS[addressType];
--- src/frontend/src/tests/lib/components/contact/AddressListItem.spec.ts
+++ src/frontend/src/tests/lib/components/contact/AddressListItem.spec.ts
@@ -51,6 +51,9 @@ describe('AddressListItem', () => {
 		// Check address type name is displayed
 		expect(container).toHaveTextContent(en.address.types.Icrc2);
 
+		// Check network name is displayed
+		expect(container).toHaveTextContent('Internet Computer');
+
 		// Check shortened address is displayed
 		expect(container).toHaveTextContent(shortenAddress(icrcAddress.address));
 
@@ -66,6 +69,9 @@ describe('AddressListItem', () => {
 
 		// Check address type name is displayed
 		expect(container).toHaveTextContent(en.address.types.Btc);
+
+		// Check network name is displayed
+		expect(container).toHaveTextContent('Bitcoin');
 
 		// Check shortened address is displayed
 		expect(container).toHaveTextContent(shortenAddress(btcAddress.address));
