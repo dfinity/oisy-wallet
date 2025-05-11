4cf90c3a917e4c74ab1616a685785706c050a1a4
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-05-11 12:59:26 +0200

    fix

--- src/frontend/src/lib/components/ui/CollapsibleBottomSheet.svelte
+++ src/frontend/src/lib/components/ui/CollapsibleBottomSheet.svelte
@@ -29,7 +29,7 @@
 			on:click={() => (expanded = true)}
 			ariaLabel="expand"
 			colorStyle="muted"
-			styleClass="text-disabled mb-2 items-end w-8"
+			styleClass="text-disabled mb-2 items-end"
 		>
 			<IconInfo slot="icon" />
 		</ButtonIcon>
