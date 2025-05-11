8581181084e2dd42c93e3b0fa65b1bf97e82af6d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-05-11 11:10:55 +0200

    feat(frontend): add Polygon

---
+++ src/frontend/src/env/networks/networks-evm/networks.evm.polygon.env.ts
@@ -0,0 +1,58 @@
+import { POLYGON_AMOY_EXPLORER_URL, POLYGON_EXPLORER_URL } from '$env/explorers.env';
+import type { EthereumNetwork } from '$eth/types/network';
+import baseMainnetIconDark from '$lib/assets/networks/dark/base-mainnet.svg';
+import baseSepoliaIconDark from '$lib/assets/networks/dark/base-sepolia.svg';
+import baseMainnetIconLight from '$lib/assets/networks/light/base-mainnet.svg';
+import baseSepoliaIconLight from '$lib/assets/networks/light/base-sepolia.svg';
+import type { NetworkId } from '$lib/types/network';
+import { defineSupportedNetworks } from '$lib/utils/env.networks.utils';
+import { parseEnabledMainnetBoolEnvVar } from '$lib/utils/env.utils';
+import { parseNetworkId } from '$lib/validation/network.validation';
+
+export const POLYGON_MAINNET_ENABLED = parseEnabledMainnetBoolEnvVar(
+	import.meta.env.VITE_POLYGON_MAINNET_DISABLED
+);
+
+export const POLYGON_NETWORK_SYMBOL = 'POL';
+
+export const POLYGON_NETWORK_ID: NetworkId = parseNetworkId(POLYGON_NETWORK_SYMBOL);
+
+export const POLYGON_NETWORK: EthereumNetwork = {
+	id: POLYGON_NETWORK_ID,
+	env: 'mainnet',
+	name: 'Polygon',
+	chainId: 137n,
+	// TODO: add the correct icon
+	iconLight: baseMainnetIconLight,
+	iconDark: baseMainnetIconDark,
+	explorerUrl: POLYGON_EXPLORER_URL,
+	providers: { infura: 'polygon-mainnet' },
+	exchange: { coingeckoId: 'polygon-pos' },
+	buy: { onramperId: 'polygon' }
+};
+
+export const POLYGON_AMOY_NETWORK_SYMBOL = 'BSC (Amoy Testnet)';
+
+export const POLYGON_AMOY_NETWORK_ID: NetworkId = parseNetworkId(POLYGON_AMOY_NETWORK_SYMBOL);
+
+export const POLYGON_AMOY_NETWORK: EthereumNetwork = {
+	id: POLYGON_AMOY_NETWORK_ID,
+	env: 'testnet',
+	name: 'Polygon (Amoy Testnet)',
+	chainId: 80002n,
+	// TODO: add the correct icon
+	iconLight: baseSepoliaIconLight,
+	iconDark: baseSepoliaIconDark,
+	explorerUrl: POLYGON_AMOY_EXPLORER_URL,
+	providers: { infura: 'polygon-amoy' }
+};
+
+export const SUPPORTED_POLYGON_NETWORKS: EthereumNetwork[] = defineSupportedNetworks({
+	mainnetFlag: POLYGON_MAINNET_ENABLED,
+	mainnetNetworks: [POLYGON_NETWORK],
+	testnetNetworks: [POLYGON_AMOY_NETWORK]
+});
+
+export const SUPPORTED_POLYGON_NETWORK_IDS: NetworkId[] = SUPPORTED_POLYGON_NETWORKS.map(
+	({ id }) => id
+);
