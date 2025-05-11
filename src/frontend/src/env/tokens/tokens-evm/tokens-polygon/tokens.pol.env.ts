d480e26327a0749e5cd5b817232e9ff76b5c4b15
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-05-11 11:22:56 +0200

    chore

--- src/frontend/src/env/tokens/tokens-evm/tokens-polygon/tokens.pol.env.ts
+++ src/frontend/src/env/tokens/tokens-evm/tokens-polygon/tokens.pol.env.ts
@@ -34,6 +34,7 @@ const POL_AMOY_SYMBOL = 'POL (Amoy Testnet)';
 
 export const POL_AMOY_TOKEN_ID: TokenId = parseTokenId(POL_AMOY_SYMBOL);
 
+// TODO: add instructions for faucets
 export const POL_AMOY_TOKEN: RequiredToken = {
 	id: POL_AMOY_TOKEN_ID,
 	network: POLYGON_AMOY_NETWORK,
