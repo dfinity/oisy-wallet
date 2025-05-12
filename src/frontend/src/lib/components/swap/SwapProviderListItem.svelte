8b5375ad8225fa02a70edb16b834a4decad3b2f0
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-05-12 18:48:57 +0200

    feat(frontend): added new swapProviderListItem coponent

---
+++ src/frontend/src/lib/components/swap/SwapProviderListItem.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+	import { isNullish, nonNullish } from '@dfinity/utils';
+	import { safeParse } from '$lib/validation/utils.validation';
+	import { UrlSchema } from '@dfinity/zod-schemas';
+	import type { LogoSize } from '$lib/types/components';
+	import type { OptionAmount } from '$lib/types/send';
+	import LogoButton from '$lib/components/ui/LogoButton.svelte';
+	import { replacePlaceholders } from '$lib/utils/i18n.utils';
+	import SwapBestRateBadge from '$lib/components/swap/SwapBestRateBadge.svelte';
+	import Amount from '$lib/components/ui/Amount.svelte';
+	import Logo from '$lib/components/ui/Logo.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
+	import type { OisyDappDescription } from '$lib/types/dapp-description';
+	import type { IcTokenToggleable } from '$icp/types/ic-token-toggleable';
+
+	const {
+		amount,
+		token,
+		logoSize = 'md',
+		usdBalance,
+		dapp,
+		isBest
+	} = $props<{
+		amount: bigint;
+		token: IcTokenToggleable;
+		logoSize?: LogoSize;
+		usdBalance: OptionAmount;
+		dapp?: OisyDappDescription;
+		isBest: boolean;
+	}>();
+	let websiteURL: URL | null = $state(null);
+	let displayURL: string | null = $state(null);
+
+	$effect(() => {
+		if (isNullish(dapp)) {
+			websiteURL = null;
+			displayURL = null;
+			return;
+		}
+		try {
+			const validated = safeParse({
+				schema: UrlSchema,
+				value: dapp.website
+			});
+			if (nonNullish(validated)) {
+				const url = new URL(validated);
+				websiteURL = url;
+				displayURL = url.hostname.startsWith('www.') ? url.hostname.slice(4) : url.hostname;
+			}
+		} catch {
+			websiteURL = null;
+			displayURL = null;
+		}
+	});
+</script>
+
+// TODO: Migrate to Svelte 5, remove legacy slot usage and use render composition instead
+<LogoButton on:click dividers>
+	<span slot="title">{dapp.name}</span>
+
+	<span slot="description">
+		{#if displayURL}
+			<span class="text-sm text-tertiary">{displayURL}</span>
+		{/if}
+	</span>
+
+	<Logo
+		slot="logo"
+		src={dapp.logo}
+		alt={replacePlaceholders($i18n.dapps.alt.logo, { $dAppName: dapp.name })}
+		size={logoSize}
+	/>
+
+	<Amount {amount} decimals={token.decimals} symbol={token.symbol} slot="title-end" />
+
+	<div class="flex items-center justify-end gap-2" slot="description-end">
+		{#if isBest}
+			<SwapBestRateBadge />
+		{/if}
+		<span class="mt-1">
+			{usdBalance}
+		</span>
+	</div>
+</LogoButton>
