b41ee0962b06fc42deef7ac6f2be6febc1606ee9
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-05-12 11:56:31 +0200

    feat(frontend): added new kongSwap details component

---
+++ src/frontend/src/lib/components/swap/SwapDetailsKongSwap.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import SwapRoute from '$lib/components/swap/SwapRoute.svelte';
+	import SwapNetworkFee from '$lib/components/swap/SwapNetworkFee.svelte';
+	import SwapLiquidityFees from '$lib/components/swap/SwapLiquidityFees.svelte';
+	import type { SwapMappedResult, SwapProvider } from '$lib/types/swap';
+	import { nonNullish } from '@dfinity/utils';
+	export let provider: Extract<SwapMappedResult, { provider: SwapProvider.KONG_SWAP }>;
+</script>
+
+{#if nonNullish(provider.route) && provider.route?.length > 0}
+	<SwapRoute route={provider.route} />
+{/if}
+
+{#if provider.networkFee}
+	<SwapNetworkFee networkFee={provider.networkFee} />
+{/if}
+
+{#if nonNullish(provider.liquidityFees) && provider.liquidityFees?.length > 0}
+	<SwapLiquidityFees liquidityFees={provider.liquidityFees} />
+{/if}
