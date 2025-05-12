d90afa8c712e3aae5a97394ad911d0fbbb4c18a9
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-05-12 13:31:22 +0200

    feat(frontend): worker service for DIP20 tokens (#6468)
    # Motivation
    
    To integrate DIP-20 tokens, like XTC, we need a worker to synchronize
    the wallet. So we basically replicate the one that exists for ICP.

---
+++ src/frontend/src/icp/services/worker.dip20-wallet.services.ts
@@ -0,0 +1,83 @@
+import { syncWallet } from '$icp/services/ic-listener.services';
+import {
+	onLoadTransactionsError,
+	onTransactionsCleanUp
+} from '$icp/services/ic-transactions.services';
+import type { IcToken } from '$icp/types/ic-token';
+import type { WalletWorker } from '$lib/types/listener';
+import type {
+	PostMessage,
+	PostMessageDataResponseError,
+	PostMessageDataResponseWallet,
+	PostMessageDataResponseWalletCleanUp
+} from '$lib/types/post-message';
+
+export const initDip20WalletWorker = async ({
+	ledgerCanisterId,
+	id: tokenId,
+	network: { env }
+}: IcToken): Promise<WalletWorker> => {
+	const WalletWorker = await import('$lib/workers/workers?worker');
+	const worker: Worker = new WalletWorker.default();
+
+	worker.onmessage = ({
+		data
+	}: MessageEvent<
+		PostMessage<
+			| PostMessageDataResponseWallet
+			| PostMessageDataResponseError
+			| PostMessageDataResponseWalletCleanUp
+		>
+	>) => {
+		const { msg } = data;
+
+		switch (msg) {
+			case 'syncDip20Wallet':
+				syncWallet({
+					tokenId,
+					data: data.data as PostMessageDataResponseWallet
+				});
+				return;
+			case 'syncDip20WalletError':
+				onLoadTransactionsError({
+					tokenId,
+					error: (data.data as PostMessageDataResponseError).error,
+					silent: true
+				});
+
+				return;
+			case 'syncDip20WalletCleanUp':
+				onTransactionsCleanUp({
+					tokenId,
+					transactionIds: (data.data as PostMessageDataResponseWalletCleanUp).transactionIds
+				});
+				return;
+		}
+	};
+
+	return {
+		start: () => {
+			worker.postMessage({
+				msg: 'startDip20WalletTimer',
+				data: {
+					ledgerCanisterId,
+					env
+				}
+			});
+		},
+		stop: () => {
+			worker.postMessage({
+				msg: 'stopDip20WalletTimer'
+			});
+		},
+		trigger: () => {
+			worker.postMessage({
+				msg: 'triggerDip20WalletTimer',
+				data: {
+					ledgerCanisterId,
+					env
+				}
+			});
+		}
+	};
+};
