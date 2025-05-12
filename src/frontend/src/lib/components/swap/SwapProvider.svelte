198aa1da85fdc901cdfd4b13d359c789197f2ad8
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-05-12 22:21:44 +0200

    fix

--- src/frontend/src/lib/components/swap/SwapProvider.svelte
+++ src/frontend/src/lib/components/swap/SwapProvider.svelte
@@ -1,49 +1,46 @@
 <script lang="ts">
 	import { nonNullish } from '@dfinity/utils';
-	import { getContext } from 'svelte';
+	import { createEventDispatcher, getContext } from 'svelte';
 	import { dAppDescriptions } from '$env/dapp-descriptions.env';
-	import SwapLiquidityFees from '$lib/components/swap/SwapLiquidityFees.svelte';
-	import SwapNetworkFee from '$lib/components/swap/SwapNetworkFee.svelte';
-	import SwapRoute from '$lib/components/swap/SwapRoute.svelte';
 	import Logo from '$lib/components/ui/Logo.svelte';
-	import ModalExpandableValues from '$lib/components/ui/ModalExpandableValues.svelte';
 	import ModalValue from '$lib/components/ui/ModalValue.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 	import {
 		SWAP_AMOUNTS_CONTEXT_KEY,
 		type SwapAmountsContext
 	} from '$lib/stores/swap-amounts.store';
-	import type { OisyDappDescription } from '$lib/types/dapp-description';
 	import type { OptionString } from '$lib/types/string';
-	import type { ProviderFee } from '$lib/types/swap';
+	import { SwapProvider } from '$lib/types/swap';
 	import type { Option } from '$lib/types/utils';
 	import { replacePlaceholders } from '$lib/utils/i18n.utils';
 	import { UrlSchema } from '$lib/validation/url.validation';
 	import { safeParse } from '$lib/validation/utils.validation';
+	import SwapDetailsKong from '$lib/components/swap/SwapDetailsKongSwap.svelte';
+	import SwapDetailsIcp from '$lib/components/swap/SwapDetailsIcp.svelte';
+	import CollapsibleBottomSheet from '$lib/components/ui/CollapsibleBottomSheet.svelte';
+	import type { OisyDappDescription } from '$lib/types/dapp-description';
+	import Button from '$lib/components/ui/Button.svelte';
+	import SwapBestRateBadge from './SwapBestRateBadge.svelte';
 
 	const { store: swapAmountsStore } = getContext<SwapAmountsContext>(SWAP_AMOUNTS_CONTEXT_KEY);
 
-	let route: string[] | undefined;
-	$: route = $swapAmountsStore?.swapAmounts?.route;
+	let swapDApp: OisyDappDescription | undefined;
 
-	let networkFee: ProviderFee | undefined;
-	$: networkFee = $swapAmountsStore?.swapAmounts?.networkFee;
-
-	let liquidityFees: ProviderFee[] | undefined;
-	$: liquidityFees = $swapAmountsStore?.swapAmounts?.liquidityFees;
-
-	const kongSwapDApp: OisyDappDescription | undefined = dAppDescriptions.find(
-		({ id }) => id === 'kongswap'
+	$: swapDApp = dAppDescriptions.find(
+		({ id }) => id === $swapAmountsStore?.selectedProvider?.provider.toLowerCase()
 	);
 
-	// TODO: this state - websiteURL - isn't one and should become a local variable
+	$: provider = $swapAmountsStore?.selectedProvider;
+
+	$: bestRate = provider?.provider === $swapAmountsStore?.swaps[0]?.provider;
+
 	let websiteURL: Option<URL>;
 	let displayURL: OptionString;
-	$: if (nonNullish(kongSwapDApp)) {
+	$: if (nonNullish(swapDApp)) {
 		try {
 			const validatedWebsiteUrl = safeParse({
 				schema: UrlSchema,
-				value: kongSwapDApp?.website
+				value: swapDApp?.website
 			});
 			if (nonNullish(validatedWebsiteUrl)) {
 				websiteURL = new URL(validatedWebsiteUrl);
@@ -56,41 +53,60 @@
 			displayURL = null;
 		}
 	}
+	const dispatch = createEventDispatcher();
 </script>
 
-{#if nonNullish(kongSwapDApp)}
-	<ModalExpandableValues>
-		<ModalValue slot="list-header">
-			<svelte:fragment slot="label">{$i18n.swap.text.swap_provider}</svelte:fragment>
-
-			<svelte:fragment slot="main-value">
-				<div class="flex gap-2">
-					<div class="mt-1">
-						<Logo
-							src={kongSwapDApp.logo}
-							alt={replacePlaceholders($i18n.dapps.alt.logo, { $dAppName: kongSwapDApp.name })}
-						/>
-					</div>
-					<div class="mr-auto">
-						<div class="text-lg font-bold">{kongSwapDApp.name}</div>
-						{#if nonNullish(displayURL)}
-							<div class="text-sm text-tertiary">{displayURL}</div>
+{#if nonNullish(swapDApp) && nonNullish(provider)}
+	<CollapsibleBottomSheet showContentHeader>
+		{#snippet contentHeader({ isInBottomSheet })}
+			<ModalValue wrapperStyleClass="items-center">
+				<svelte:fragment slot="label">
+					<div class="flex justify-center gap-2">
+						{$i18n.swap.text.swap_provider}
+						{#if nonNullish($swapAmountsStore) && $swapAmountsStore?.swaps.length > 1 && !isInBottomSheet}
+							<Button link on:click={() => dispatch('icShowProviderList')}>Select ></Button>
 						{/if}
 					</div>
-				</div>
-			</svelte:fragment>
-		</ModalValue>
+				</svelte:fragment>
 
-		<svelte:fragment slot="list-items">
-			{#if nonNullish(route) && route.length > 0}
-				<SwapRoute {route} />
+				<svelte:fragment slot="main-value">
+					<div class="flex items-start gap-3">
+						{#if bestRate}
+							<SwapBestRateBadge />
+						{/if}
+						<div class="flex gap-2">
+							<div class="mt-1">
+								<Logo
+									src={swapDApp.logo}
+									alt={replacePlaceholders($i18n.dapps.alt.logo, { $dAppName: swapDApp.name })}
+								/>
+							</div>
+							<div class="mr-auto">
+								<div class="text-lg font-bold">{swapDApp.name}</div>
+							</div>
+						</div>
+					</div>
+				</svelte:fragment>
+			</ModalValue>
+		{/snippet}
+		{#snippet content()}
+			{#if displayURL}
+				<ModalValue>
+					<svelte:fragment slot="label">Website</svelte:fragment>
+
+					<svelte:fragment slot="main-value">
+						<div class="text-sm">{displayURL}</div>
+					</svelte:fragment>
+				</ModalValue>
 			{/if}
-			{#if nonNullish(networkFee)}
-				<SwapNetworkFee {networkFee} />
+			{#if provider.provider === SwapProvider.KONG_SWAP}
+				<SwapDetailsKong {provider} />
+			{:else if provider.provider === SwapProvider.ICP_SWAP}
+				<SwapDetailsIcp {provider} />
 			{/if}
-			{#if nonNullish(liquidityFees) && liquidityFees.length > 0}
-				<SwapLiquidityFees {liquidityFees} />
-			{/if}
-		</svelte:fragment>
-	</ModalExpandableValues>
+		{/snippet}
+		{#snippet contentFooter(closeFn)}
+			<Button fullWidth on:click={closeFn}>Done</Button>
+		{/snippet}
+	</CollapsibleBottomSheet>
 {/if}
