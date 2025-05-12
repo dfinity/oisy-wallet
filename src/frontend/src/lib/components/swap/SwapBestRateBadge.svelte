301077c8d191312268b60038b193dcca56aec151
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-05-12 16:22:16 +0200

    feat(frontend): added new BestRate component

---
+++ src/frontend/src/lib/components/swap/SwapBestRateBadge.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { i18n } from '$lib/stores/i18n.store';
+	import IconStar from '../icons/IconStar.svelte';
+	import Badge from '../ui/Badge.svelte';
+	import Responsive from '../ui/Responsive.svelte';
+</script>
+
+<Badge variant="success" styleClass="mt-1" width="w-fit">
+	<span class="flex items-center gap-1">
+		<IconStar />
+		<Responsive up="md">{$i18n.swap.text.best_rate}</Responsive>
+	</span>
+</Badge>
--- src/frontend/src/lib/types/i18n.d.ts
+++ src/frontend/src/lib/types/i18n.d.ts
@@ -553,6 +553,7 @@ interface I18nSwap {
 		swap_route: string;
 		included_network_fees: string;
 		included_liquidity_fees: string;
+		best_rate: string;
 	};
 	error: {
 		kong_not_available: string;
