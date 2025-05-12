4a0c226f7f8e4c67d1d278c51843d129944dd352
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2025-05-12 16:46:26 +0200

    feat(frontend): update SolSendReview components

--- src/frontend/src/tests/lib/components/send/SendDataDestination.spec.ts
+++ src/frontend/src/tests/lib/components/send/SendDataDestination.spec.ts
@@ -1,4 +1,5 @@
 import SendDataDestination from '$lib/components/send/SendDataDestination.svelte';
+import { shortenWithMiddleEllipsis } from '$lib/utils/format.utils';
 import { mockBtcAddress } from '$tests/mocks/btc.mock';
 import en from '$tests/mocks/i18n.mock';
 import { render } from '@testing-library/svelte';
@@ -16,6 +17,6 @@ describe('SendDataDestination', () => {
 
 		expect(container).toHaveTextContent(en.send.text.destination);
 
-		expect(container).toHaveTextContent(props.destination);
+		expect(container).toHaveTextContent(shortenWithMiddleEllipsis({ text: props.destination }));
 	});
 });
