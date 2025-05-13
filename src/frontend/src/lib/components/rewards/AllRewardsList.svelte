78a78fa400e7bfd06d129fccfe84a43a402d41ea
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-05-13 13:10:27 +0200

    feat(frontend): initialize reward store on AllRewardsList loading (#6562)
    # Motivation
    
    When rendering the `AllRewardsList` component, the campaign eligibility
    data should be loaded and stored in the `reward store`.
    
    # Changes
    
    - initialize `reward store`
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/rewards/AllRewardsList.svelte
+++ src/frontend/src/lib/components/rewards/AllRewardsList.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { isNullish } from '@dfinity/utils';
+	import { onMount, setContext } from 'svelte';
 	import { rewardCampaigns } from '$env/reward-campaigns.env';
 	import type { RewardDescription } from '$env/types/env-reward';
 	import RewardsFilter from '$lib/components/rewards/RewardsFilter.svelte';
@@ -8,10 +10,33 @@
 		REWARDS_ENDED_CAMPAIGNS_CONTAINER,
 		REWARDS_UPCOMING_CAMPAIGNS_CONTAINER
 	} from '$lib/constants/test-ids.constants';
+	import { authIdentity } from '$lib/derived/auth.derived';
 	import { RewardStates } from '$lib/enums/reward-states';
+	import { nullishSignOut } from '$lib/services/auth.services';
 	import { i18n } from '$lib/stores/i18n.store';
+	import {
+		initRewardEligibilityStore,
+		REWARD_ELIGIBILITY_CONTEXT_KEY,
+		type RewardEligibilityContext
+	} from '$lib/stores/reward.store';
 	import { replaceOisyPlaceholders } from '$lib/utils/i18n.utils';
 	import { isEndedCampaign, isOngoingCampaign, isUpcomingCampaign } from '$lib/utils/rewards.utils';
+
+	const { store } = setContext<RewardEligibilityContext>(REWARD_ELIGIBILITY_CONTEXT_KEY, {
+		store: initRewardEligibilityStore()
+	});
+
+	const loadEligibilityReport = async () => {
+		if (isNullish($authIdentity)) {
+			await nullishSignOut();
+			return;
+		}
+
+		// TODO load campaign eligibilities from reward service
+		store.setCampaignEligibilities([]);
+	};
+
+	onMount(loadEligibilityReport);
 
 	let selectedRewardState = $state(RewardStates.ONGOING);
 
