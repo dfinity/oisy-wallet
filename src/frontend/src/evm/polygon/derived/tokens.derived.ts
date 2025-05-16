b2bcf2a54f1ef2d8b8bb7fa4ecc13e996b6705d5
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-05-16 16:46:56 +0200

    feat(frontend): create derived store for enabled Polygon tokens (#6650)
    # Motivation
    
    Similar to the other networks, we create a derived store with all the
    enabled Polygon tokens.

---
+++ src/frontend/src/evm/polygon/derived/tokens.derived.ts
@@ -0,0 +1,22 @@
+import { POLYGON_MAINNET_ENABLED } from '$env/networks/networks-evm/networks.evm.polygon.env';
+import {
+	POL_AMOY_TOKEN,
+	POL_MAINNET_TOKEN
+} from '$env/tokens/tokens-evm/tokens-polygon/tokens.pol.env';
+import { testnetsEnabled } from '$lib/derived/testnets.derived';
+import { userNetworks } from '$lib/derived/user-networks.derived';
+import type { RequiredToken } from '$lib/types/token';
+import { defineEnabledTokens } from '$lib/utils/tokens.utils';
+import { derived, type Readable } from 'svelte/store';
+
+export const enabledPolygonTokens: Readable<RequiredToken[]> = derived(
+	[testnetsEnabled, userNetworks],
+	([$testnetsEnabled, $userNetworks]) =>
+		defineEnabledTokens({
+			$testnetsEnabled,
+			$userNetworks,
+			mainnetFlag: POLYGON_MAINNET_ENABLED,
+			mainnetTokens: [POL_MAINNET_TOKEN],
+			testnetTokens: [POL_AMOY_TOKEN]
+		})
+);
