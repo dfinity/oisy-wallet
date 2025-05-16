121e27363a8134a0025e00ec3d44f8fadff5885b
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-05-16 17:06:43 +0200

    feat(frontend): create derived store for enabled Polygon networks (#6651)
    # Motivation
    
    Similar to the other networks, we create a derived store with all the
    enabled Polygon networks.

--- src/frontend/src/evm/derived/networks.derived.ts
+++ src/frontend/src/evm/derived/networks.derived.ts
@@ -1,12 +1,17 @@
 import type { EthereumNetwork } from '$eth/types/network';
 import { enabledBaseNetworks } from '$evm/base/derived/networks.derived';
 import { enabledBscNetworks } from '$evm/bsc/derived/networks.derived';
+import { enabledPolygonNetworks } from '$evm/polygon/derived/networks.derived';
 import type { NetworkId } from '$lib/types/network';
 import { derived, type Readable } from 'svelte/store';
 
 export const enabledEvmNetworks: Readable<EthereumNetwork[]> = derived(
-	[enabledBaseNetworks, enabledBscNetworks],
-	([$enabledBaseNetworks, $enabledBscNetworks]) => [...$enabledBaseNetworks, ...$enabledBscNetworks]
+	[enabledBaseNetworks, enabledBscNetworks, enabledPolygonNetworks],
+	([$enabledBaseNetworks, $enabledBscNetworks, $enabledPolygonNetworks]) => [
+		...$enabledBaseNetworks,
+		...$enabledBscNetworks,
+		...$enabledPolygonNetworks
+	]
 );
 
 export const enabledEvmNetworksIds: Readable<NetworkId[]> = derived(
