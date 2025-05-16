2d8fa48b1ddd16b8ef116ec46497f2a32df8d34e
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2025-05-16 17:57:32 +0200

    feat(frontend): use provided destination as address picker filter

--- src/frontend/src/tests/lib/components/send/KnownDestinations.spec.ts
+++ src/frontend/src/tests/lib/components/send/KnownDestinations.spec.ts
@@ -1,24 +1,59 @@
 import { BTC_MAINNET_TOKEN } from '$env/tokens/tokens.btc.env';
+import { ETHEREUM_TOKEN } from '$env/tokens/tokens.eth.env';
+import { SOLANA_TOKEN } from '$env/tokens/tokens.sol.env';
 import KnownDestinations from '$lib/components/send/KnownDestinations.svelte';
+import { shortenWithMiddleEllipsis } from '$lib/utils/format.utils';
 import { mockBtcAddress } from '$tests/mocks/btc.mock';
+import { mockEthAddress } from '$tests/mocks/eth.mocks';
 import en from '$tests/mocks/i18n.mock';
+import { mockSolAddress } from '$tests/mocks/sol.mock';
 import { render } from '@testing-library/svelte';
+import { expect } from 'vitest';
 
 describe('KnownDestinations', () => {
+	const knownDestinations = {
+		[mockBtcAddress]: {
+			amounts: [{ value: 10000000n, token: BTC_MAINNET_TOKEN }],
+			timestamp: 1671234567890
+		},
+		[mockEthAddress]: {
+			amounts: [{ value: 10000000n, token: ETHEREUM_TOKEN }],
+			timestamp: 1671234567890
+		},
+		[mockSolAddress]: {
+			amounts: [{ value: 10000000n, token: SOLANA_TOKEN }],
+			timestamp: 1671234567890
+		}
+	};
+
 	it('renders content if data is provided', () => {
 		const { getByText } = render(KnownDestinations, {
 			props: {
-				destination: mockBtcAddress,
-				knownDestinations: {
-					[mockBtcAddress]: {
-						amounts: [{ value: 10000000n, token: BTC_MAINNET_TOKEN }],
-						timestamp: 1671234567890
-					}
-				}
+				destination: '',
+				knownDestinations
 			}
 		});
 
 		expect(getByText(en.send.text.recently_used)).toBeInTheDocument();
+
+		expect(getByText(shortenWithMiddleEllipsis({ text: mockBtcAddress }))).toBeInTheDocument();
+		expect(getByText(shortenWithMiddleEllipsis({ text: mockEthAddress }))).toBeInTheDocument();
+		expect(getByText(shortenWithMiddleEllipsis({ text: mockSolAddress }))).toBeInTheDocument();
+	});
+
+	it('renders filtered content if data is provided', () => {
+		const { getByText } = render(KnownDestinations, {
+			props: {
+				destination: mockBtcAddress,
+				knownDestinations
+			}
+		});
+
+		expect(getByText(en.send.text.recently_used)).toBeInTheDocument();
+
+		expect(getByText(shortenWithMiddleEllipsis({ text: mockBtcAddress }))).toBeInTheDocument();
+		expect(() => getByText(shortenWithMiddleEllipsis({ text: mockEthAddress }))).toThrow();
+		expect(() => getByText(shortenWithMiddleEllipsis({ text: mockSolAddress }))).toThrow();
 	});
 
 	it('does not render content if data is empty', () => {
