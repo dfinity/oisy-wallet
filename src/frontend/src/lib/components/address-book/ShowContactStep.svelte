8add120be086e95a1ae3afd720cb21fd1f0fd3a3
CvetelinaAngelova <110659037+CvetelinaAngelova@users.noreply.github.com>
2025-05-19 13:44:58 +0300

    remove space

--- src/frontend/src/lib/components/address-book/ShowContactStep.svelte
+++ src/frontend/src/lib/components/address-book/ShowContactStep.svelte
@@ -47,7 +47,7 @@
 			{/each}
 		</div>
 	{:else}
-		<div class="my-5 flex flex-col items-center gap-5">
+		<div class="mb-5 flex flex-col items-center gap-5">
 			<div class="text-secondary-inverted">
 				<IconEmptyAddresses />
 			</div>
