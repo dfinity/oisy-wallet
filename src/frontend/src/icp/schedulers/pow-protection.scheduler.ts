91aa58aa95483d2b01ff25a59f6fbe1c40a55ba7
DecentAgeCoder <79855464+DecentAgeCoder@users.noreply.github.com>
2025-05-20 13:49:40 +0100

    feat(frontend): Correctly handle errors in PowProtectionScheduler (#6566)
    # Motivation
    
    The PR mainly improves the error handling introducing
    InternalCanisterError based errors to allow handling specific errors in
    the PowProtectionScheduler after the are thrown.
    
    # Changes
    
    - Introduced new error classes (PowCreateChallengeError and
    PowChallengeError) and enums for improved error handling related to PoW
    challenges.
    - Refactored PoW scheduling code to handle specific errors such as
    ChallengeInProgressError and ExpiredChallengeError with proper internal
    handling and logs.
    
    # Tests
    
    - Existing test suites were updated for compatibility with refactored
    logic and behavior.
    - Added new test cases to ensure errors (e.g., ChallengeInProgress,
    ExpiredChallenge) are handled correctly and gracefully.
    - Verified error mappings align with new error classes and enums.
    - Ensured correct behavior of PoW protections under various scenarios,
    including edge and failure cases.

--- src/frontend/src/icp/schedulers/pow-protection.scheduler.ts
+++ src/frontend/src/icp/schedulers/pow-protection.scheduler.ts
@@ -1,9 +1,10 @@
-import type { CreateChallengeResponse } from '$declarations/backend/backend.did';
 import { solvePowChallenge } from '$icp/services/pow-protector.services';
 import { allowSigning, createPowChallenge } from '$lib/api/backend.api';
+import { CreateChallengeEnum, PowCreateChallengeError } from '$lib/canisters/backend.errors';
 import { POW_CHALLENGE_INTERVAL_MILLIS } from '$lib/constants/pow.constants';
 import { SchedulerTimer, type Scheduler, type SchedulerJobData } from '$lib/schedulers/scheduler';
 import type { PostMessageDataRequest } from '$lib/types/post-message';
+import { isNullish } from '@dfinity/utils';
 
 export class PowProtectionScheduler implements Scheduler<PostMessageDataRequest> {
 	private timer = new SchedulerTimer('syncPowProtectionStatus');
@@ -36,23 +37,51 @@ export class PowProtectionScheduler implements Scheduler<PostMessageDataRequest>
 	 * 3. Uses the solved `nonce` to request signing permission.
 	 *
 	 * @param identity - The user's identity for the operation.
-	 * @throws Errors if any step in the sequence fails.
+	 * @throws Errors if any step with no specific error handling in the sequence fails.
 	 */
 	private requestSignerCycles = async ({ identity }: SchedulerJobData<PostMessageDataRequest>) => {
 		// Step 1: Request creation of the Proof-of-Work (PoW) challenge (throws when unsuccessful).
-		const { start_timestamp_ms: timestamp, difficulty }: CreateChallengeResponse =
-			await createPowChallenge({ identity });
+		// TODO provide functionality to post and receive the a post message to indicate the progress
+		// this.postMessagePowProgress({ progress: 'REQUEST_CHALLENGE' });
+		try {
+			const createChallengeResponse = await createPowChallenge({
+				identity
+			});
 
-		// Step 2: Solve the PoW challenge.
-		const nonce = await solvePowChallenge({
-			timestamp,
-			difficulty
-		});
+			// Make sure we have a valid response before continuing
+			if (isNullish(createChallengeResponse)) {
+				return;
+			}
 
-		// Step 3: Request allowance for signing operations with solved nonce.
-		await allowSigning({
-			identity,
-			request: { nonce }
-		});
+			// Step 2: Solve the PoW challenge.
+			// TODO provide functionality to post and receive the a post message to indicate the progress
+			// this.postMessagePowProgress({ progress: 'SOLVE_CHALLENGE' });
+			const nonce = await solvePowChallenge({
+				timestamp: createChallengeResponse.start_timestamp_ms,
+				difficulty: createChallengeResponse.difficulty
+			});
+
+			// Step 3: Request allowance for signing operations with solved nonce.
+			// TODO provide functionality to post and receive the a post message to indicate the progress
+			// this.postMessagePowProgress({ progress: 'GRANT_CYCLES'});
+
+			await allowSigning({
+				identity,
+				request: { nonce }
+			});
+			// TODO provide functionality to post and receive the a post message to indicate when the next challenge starts
+			// if (_allowSigningResponse?.challenge_completion[0]?.next_allowance_ms !== undefined) {this.postMessagePowNextAllowance({ nextAllowanceMs: allowSigningResponse.challenge_completion[0].next_allowance_ms });
+		} catch (err: unknown) {
+			// We can skip the "Challenge already in progress" since we are already in the middle of a challenge. This
+			// usually happens when:
+			// 1.) The backend setting EXPIRY_DURATION_MS exceeds the frontend setting POW_CHALLENGE_INTERVAL_MILLIS
+			// 2.) The frontend is opened in more tabs/windows (leading to multiple competing schedulers)
+			if (this.isChallengeInProgressError(err)) {
+				return;
+			}
+			throw err;
+		}
 	};
+	private isChallengeInProgressError = (err: unknown): boolean =>
+		err instanceof PowCreateChallengeError && err.code === CreateChallengeEnum.ChallengeInProgress;
 }
