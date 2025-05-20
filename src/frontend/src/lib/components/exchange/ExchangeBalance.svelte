bdd6c1699414fa5e751ee90668108afb28845774
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-05-20 15:18:52 +0200

    feat(frontend): adds privacy mode to exchange balance (#6717)
    # Motivation
    
    We want to be able to hide the balance on the `Hero` component if
    `privacy mode` is on.
    
    # Changes
    
    - adds privacy mode to `ExchangeBalance`
    
    # Tests
    
    **on:**
    <img width="890" alt="image"
    src="https://github.com/user-attachments/assets/34766c6e-334b-4ada-ba28-16580c9f6fa0"
    />
    
    **off:**
    <img width="889" alt="image"
    src="https://github.com/user-attachments/assets/17ee39cd-2c25-49c2-9b1b-b01e850b79b7"
    />
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/exchange/ExchangeBalance.svelte
+++ src/frontend/src/lib/components/exchange/ExchangeBalance.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { getContext } from 'svelte';
+	import IconDots from '$lib/components/icons/IconDots.svelte';
 	import { allBalancesZero } from '$lib/derived/balances.derived';
 	import { combinedDerivedSortedNetworkTokensUi } from '$lib/derived/network-tokens.derived';
 	import { HERO_CONTEXT_KEY, type HeroContext } from '$lib/stores/hero.store';
@@ -7,18 +8,33 @@
 	import { formatUSD } from '$lib/utils/format.utils';
 	import { sumTokensUiUsdBalance } from '$lib/utils/tokens.utils';
 
+	interface Props {
+		hideBalance?: boolean;
+	}
+
+	let { hideBalance }: Props = $props();
+
 	const { loaded } = getContext<HeroContext>(HERO_CONTEXT_KEY);
 
-	let totalUsd: number;
-	$: totalUsd = sumTokensUiUsdBalance($combinedDerivedSortedNetworkTokensUi);
+	const totalUsd = $derived(sumTokensUiUsdBalance($combinedDerivedSortedNetworkTokensUi));
 </script>
 
 <span class="flex flex-col items-center gap-2">
 	<output class="mt-8 inline-block break-all text-5xl font-bold">
 		{#if $loaded}
-			{formatUSD({ value: totalUsd })}
+			{#if hideBalance}
+				<IconDots variant="lg" times={6} styleClass="h-12" />
+			{:else}
+				{formatUSD({ value: totalUsd })}
+			{/if}
 		{:else}
-			<span class="animate-pulse">{formatUSD({ value: 0 })}</span>
+			<span class="animate-pulse">
+				{#if hideBalance}
+					<IconDots variant="lg" times={6} styleClass="h-12" />
+				{:else}
+					{formatUSD({ value: 0 })}
+				{/if}
+			</span>
 		{/if}
 	</output>
 	<span class="max-w-48 text-xl font-medium text-brand-secondary-alt sm:max-w-none">
