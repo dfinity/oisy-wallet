17ae0bf2e3b541672b2323ccaca85f44d4eb1a09
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-05-20 15:56:54 +0200

    implements feature

---
+++ src/frontend/src/tests/lib/components/guard/ShortcutGuard.spec.ts
@@ -0,0 +1,43 @@
+import {render} from "@testing-library/svelte";
+import ShortcutGuard from "$lib/components/guard/ShortcutGuard.svelte";
+import {get} from "svelte/store";
+import {isPrivacyMode} from "$lib/derived/settings.derived";
+import {privacyModeStore} from "$lib/stores/settings.store";
+
+describe('ShortcutGuard', () => {
+    describe('Privacy mode', () => {
+        const keyDownEvent = new KeyboardEvent('keydown', { key: 'p' });
+
+        beforeEach(() => {
+            privacyModeStore.set({ key: 'privacy-mode', value: { enabled: false } });
+        });
+
+        it('should turn on and off privacy mode on keyboard click', async () => {
+            render(ShortcutGuard);
+
+            expect(get(isPrivacyMode)).toBe(false);
+
+            window.dispatchEvent(keyDownEvent);
+
+            expect(get(isPrivacyMode)).toBe(true);
+
+            window.dispatchEvent(keyDownEvent);
+
+            expect(get(isPrivacyMode)).toBe(false);
+        });
+
+        it('should not turn on privacy mode when typing in input field', () => {
+            render(ShortcutGuard);
+
+            const input = document.createElement('input');
+            document.body.appendChild(input);
+            input.focus();
+
+            expect(get(isPrivacyMode)).toBe(false);
+
+            input.dispatchEvent(keyDownEvent);
+
+            expect(get(isPrivacyMode)).toBe(false);
+        });
+    });
+});
