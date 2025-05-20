2f1194d3757d4a69f6b959e71af76b897324d1ad
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-05-20 10:54:15 +0200

    implements feature

--- src/frontend/src/tests/lib/components/core/Menu.spec.ts
+++ src/frontend/src/tests/lib/components/core/Menu.spec.ts
@@ -5,7 +5,7 @@ import Menu from '$lib/components/core/Menu.svelte';
 import {
 	NAVIGATION_MENU_ADDRESS_BOOK_BUTTON,
 	NAVIGATION_MENU_BUTTON,
-	NAVIGATION_MENU_GOLD_BUTTON,
+	NAVIGATION_MENU_GOLD_BUTTON, NAVIGATION_MENU_PRIVACY_MODE_BUTTON,
 	NAVIGATION_MENU_REFERRAL_BUTTON,
 	NAVIGATION_MENU_VIP_BUTTON
 } from '$lib/constants/test-ids.constants';
@@ -15,6 +15,7 @@ import { render, waitFor } from '@testing-library/svelte';
 
 describe('Menu', () => {
 	const menuButtonSelector = `button[data-tid="${NAVIGATION_MENU_BUTTON}"]`;
+	const menuItemPrivacyModeButtonSelector = `button[data-tid="${NAVIGATION_MENU_PRIVACY_MODE_BUTTON}"]`;
 	const menuItemVipButtonSelector = `button[data-tid="${NAVIGATION_MENU_VIP_BUTTON}"]`;
 	const menuItemGoldButtonSelector = `button[data-tid="${NAVIGATION_MENU_GOLD_BUTTON}"]`;
 	const menuItemAddressBookSelector = `button[data-tid="${NAVIGATION_MENU_ADDRESS_BOOK_BUTTON}"]`;
@@ -67,6 +68,11 @@ describe('Menu', () => {
 			}
 			return element;
 		});
+
+	it('renders the privacy mode menu item', async () => {
+		await openMenu();
+		await waitForElement({ selector: menuItemPrivacyModeButtonSelector });
+	});
 
 	it('renders the vip menu item', async () => {
 		vi.spyOn(rewardApi, 'getUserInfo').mockResolvedValue(mockUserData(['vip']));
