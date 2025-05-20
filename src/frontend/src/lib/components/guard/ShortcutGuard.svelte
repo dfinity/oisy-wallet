c26a0935de5d27e47f1607aa8f3a6001eff37db8
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-05-20 12:12:01 +0200

    implements feature

--- src/frontend/src/lib/components/guard/ShortcutGuard.svelte
+++ src/frontend/src/lib/components/guard/ShortcutGuard.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-
     import type {Snippet} from "svelte";
     import {privacyModeStore} from "$lib/stores/settings.store";
     import {isPrivacyMode} from "$lib/derived/settings.derived";
