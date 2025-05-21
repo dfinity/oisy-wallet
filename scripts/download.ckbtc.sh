70e61969ccabef126fcb83809e15ac9a0f85e758
Max <bitdivine@users.noreply.github.com>
2025-05-21 23:07:43 +0200

    refactor(devops): No repeat downloads (#6765)
    # Motivation
    We download a lot of data with curl. In some places we try to make this
    more efficient by not downloading a file if it already exists, however
    that breaks if the URL changes, giving strange results. We have a script
    now that does this properly, so we can roll it out everywhere.
    
    # Changes
    - Use  `download-immutable` in most places where we used to curl.
    
    # Tests
    Existing tests should verify that existing functionality still works.

--- scripts/download.ckbtc.sh
+++ scripts/download.ckbtc.sh
@@ -10,22 +10,22 @@ fi
 
 IC_COMMIT="03dd6ee6de80c2202f66948692c69c61eb6af54d"
 
-curl -sSL https://download.dfinity.systems/ic/$IC_COMMIT/canisters/ic-ckbtc-minter.wasm.gz -o "$DIR"/ckbtc_minter.wasm.gz
-gunzip "$DIR"/ckbtc_minter.wasm.gz
+scripts/download-immutable.sh "https://download.dfinity.systems/ic/$IC_COMMIT/canisters/ic-ckbtc-minter.wasm.gz" /ckbtc_minter.wasm.gz
+gunzip --keep --force "$DIR"/ckbtc_minter.wasm.gz
 
-curl -sSL https://download.dfinity.systems/ic/$IC_COMMIT/canisters/ic-icrc1-ledger.wasm.gz -o "$DIR"/ckbtc_ledger.wasm.gz
-gunzip -k "$DIR"/ckbtc_ledger.wasm.gz
+scripts/download-immutable.sh "https://download.dfinity.systems/ic/$IC_COMMIT/canisters/ic-icrc1-ledger.wasm.gz" "$DIR"/ckbtc_ledger.wasm.gz
+gunzip --keep --force "$DIR"/ckbtc_ledger.wasm.gz
 
-curl -sSL https://download.dfinity.systems/ic/$IC_COMMIT/canisters/ic-icrc1-index-ng.wasm.gz -o "$DIR"/ckbtc_index.wasm.gz
-gunzip -k "$DIR"/ckbtc_index.wasm.gz
+scripts/download-immutable.sh "https://download.dfinity.systems/ic/$IC_COMMIT/canisters/ic-icrc1-index-ng.wasm.gz" "$DIR"/ckbtc_index.wasm.gz
+gunzip --keep --force "$DIR"/ckbtc_index.wasm.gz
 
-curl -sSL https://download.dfinity.systems/ic/$IC_COMMIT/canisters/ic-ckbtc-kyt.wasm.gz -o "$DIR"/ckbtc_kyt.wasm.gz
-gunzip "$DIR"/ckbtc_kyt.wasm.gz
+scripts/download-immutable.sh "https://download.dfinity.systems/ic/$IC_COMMIT/canisters/ic-ckbtc-kyt.wasm.gz" "$DIR"/ckbtc_kyt.wasm.gz
+gunzip --keep --force "$DIR"/ckbtc_kyt.wasm.gz
 
-curl -sSL https://raw.githubusercontent.com/dfinity/ic/$IC_COMMIT/rs/bitcoin/ckbtc/minter/ckbtc_minter.did -o "$DIR"/ckbtc_minter.did
+scripts/download-immutable.sh "https://raw.githubusercontent.com/dfinity/ic/$IC_COMMIT/rs/bitcoin/ckbtc/minter/ckbtc_minter.did" "$DIR"/ckbtc_minter.did
 
-curl -sSL https://raw.githubusercontent.com/dfinity/ic/$IC_COMMIT/rs/ledger_suite/icrc1/ledger/ledger.did -o "$DIR"/ckbtc_ledger.did
+scripts/download-immutable.sh "https://raw.githubusercontent.com/dfinity/ic/$IC_COMMIT/rs/ledger_suite/icrc1/ledger/ledger.did" "$DIR"/ckbtc_ledger.did
 
-curl -sSL https://raw.githubusercontent.com/dfinity/ic/$IC_COMMIT/rs/ledger_suite/icrc1/index-ng/index-ng.did -o "$DIR"/ckbtc_index.did
+scripts/download-immutable.sh "https://raw.githubusercontent.com/dfinity/ic/$IC_COMMIT/rs/ledger_suite/icrc1/index-ng/index-ng.did" "$DIR"/ckbtc_index.did
 
-curl -sSL https://raw.githubusercontent.com/dfinity/ic/$IC_COMMIT/rs/bitcoin/ckbtc/kyt/kyt.did -o "$DIR"/ckbtc_kyt.did
+scripts/download-immutable.sh "https://raw.githubusercontent.com/dfinity/ic/$IC_COMMIT/rs/bitcoin/ckbtc/kyt/kyt.did" "$DIR"/ckbtc_kyt.did
