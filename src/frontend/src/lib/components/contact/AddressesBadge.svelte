1bb5f9e3d0ad40a4e76bcf4f7f034e674ca648f0
FlavioInacta <flavio.pellanda@inacta.ch>
2025-05-21 17:33:05 +0200

    feat(frontend): Adds AddressesBadge to be used in contact card later (#6756)
    # Motivation
    
    Adds a badge component to show either the address type or the amount of
    addresses
    
    # Tests
    
    
    ![Screenshot_2025-05-21_15-28-05](https://github.com/user-attachments/assets/4da8e096-594d-4625-b154-d7552f6bc3b0)
    
    ![Screenshot_2025-05-21_15-30-48](https://github.com/user-attachments/assets/287d6e75-b7e4-41d7-9247-0f9a89333a4b)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/frontend/src/lib/components/contact/AddressesBadge.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import IconAddressType from '$lib/components/address/IconAddressType.svelte';
+	import type { ContactAddressUi } from '$lib/types/contact';
+
+	interface Props {
+		addresses: ContactAddressUi[];
+	}
+
+	let { addresses }: Props = $props();
+</script>
+
+{#if addresses.length > 0}
+	<span class="md:size-5.5 absolute -bottom-1 -right-1 size-5 rounded-full bg-primary">
+		{#if addresses.length == 1}
+			<IconAddressType addressType={addresses[0].addressType} />
+		{:else}
+			<div
+				class="border-1 flex size-full items-center justify-center rounded-full border-secondary text-xs font-bold"
+				>{addresses.length}</div
+			>
+		{/if}
+	</span>
+{/if}
