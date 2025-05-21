de6a40561313c8c051a1438db90acd2ac26f1e04
CvetelinaAngelova <110659037+CvetelinaAngelova@users.noreply.github.com>
2025-05-21 15:25:24 +0300

    restore default setUp

--- src/frontend/src/lib/components/address-book/AddressBookModal.svelte
+++ src/frontend/src/lib/components/address-book/AddressBookModal.svelte
@@ -2,9 +2,7 @@
 	import { WizardModal, type WizardStep, type WizardSteps } from '@dfinity/gix-components';
 	import { nonNullish } from '@dfinity/utils';
 	import AddContactStep from '$lib/components/address-book/AddContactStep.svelte';
-	import AddressBookInfoPage from '$lib/components/address-book/AddressBookInfoPage.svelte';
 	import AddressBookStep from '$lib/components/address-book/AddressBookStep.svelte';
-	import Avatar from '$lib/components/contact/Avatar.svelte';
 	import ShowContactStep from '$lib/components/address-book/ShowContactStep.svelte';
 	import { ADDRESS_BOOK_MODAL } from '$lib/constants/test-ids.constants';
 	import { AddressBookSteps } from '$lib/enums/progress-steps';
@@ -23,10 +21,6 @@
 			title: $i18n.contact.form.add_new_contact
 		},
 		{
-			name: AddressBookSteps.INFO_PAGE,
-			title: 'Contanc Info Pages'
-		},
-		{
 			name: AddressBookSteps.SHOW_CONTACT,
 			title: $i18n.address_book.show_contact.title
 		}
@@ -39,9 +33,8 @@
 	let currentStepName = $derived(currentStep?.name as AddressBookSteps | undefined);
 	let addContactStep = $state<AddContactStep>();
 
-		let contacts: ContactUi[] = $state([]);
-		let currentContact: ContactUi | undefined = $state();
-	// let currentAddress: Address | undefined = $state();
+	let contacts: ContactUi[] = $state([]);
+	let currentContact: ContactUi | undefined = $state();
 
 	const gotoStep = (stepName: AddressBookSteps) => {
 		if (nonNullish(modal)) {
@@ -67,21 +60,11 @@
 	testId={ADDRESS_BOOK_MODAL}
 	on:nnsClose={close}
 >
-	<svelte:fragment slot="title">
-		{#if currentStepName === AddressBookSteps.INFO_PAGE}
-			<div class="flex flex-wrap items-center gap-2">
-				<Avatar variant="xs" styleClass="rounded-full flex items-center justify-center mb-2" />
-				<div class="text-center text-lg font-semibold text-primary">
-					{currentContact?.name}
-				</div>
-			</div>
-		{:else if currentStepName === AddressBookSteps.ADD_CONTACT && nonNullish(addContactStep)}
-			{addContactStep.title}
-		{:else}
-			{currentStep?.title ?? ''}
-		{/if}
-	</svelte:fragment>
-
+	<svelte:fragment slot="title"
+		>{currentStepName === AddressBookSteps.ADD_CONTACT && nonNullish(addContactStep)
+			? addContactStep.title
+			: (currentStep?.title ?? '')}</svelte:fragment
+	>
 	{#if currentStepName === AddressBookSteps.ADDRESS_BOOK}
 		<AddressBookStep
 			{contacts}
@@ -94,11 +77,11 @@
 				gotoStep(AddressBookSteps.ADD_CONTACT);
 			}}
 		></AddressBookStep>
-	<!-- {:else if currentStep?.name === AddressBookSteps.INFO_PAGE}
-		<AddressBookInfoPage
-			address={currentAddress ?? { address: '', address_type: 'Eth' }}
-			close={() => modalStore.close()}
-		/> -->
+	{:else if currentStep?.name === AddressBookSteps.SHOW_CONTACT}
+		<ShowContactStep
+			onClose={() => gotoStep(AddressBookSteps.ADDRESS_BOOK)}
+			contact={currentContact!}
+		></ShowContactStep>
 	{:else if currentStep?.name === AddressBookSteps.ADD_CONTACT}
 		<AddContactStep
 			bind:this={addContactStep}
