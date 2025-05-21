aa17f169e060303d29229162fc611fd4c626a19f
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-05-21 23:09:01 +0200

    refactor(frontend): Move FLOKI token to additional ERC20 tokens

--- src/frontend/src/tests/eth/utils/erc20.utils.spec.ts
+++ src/frontend/src/tests/eth/utils/erc20.utils.spec.ts
@@ -6,7 +6,6 @@ import { SUPPORTED_ETHEREUM_TOKENS } from '$env/tokens/tokens.eth.env';
 import { ICP_TOKEN } from '$env/tokens/tokens.icp.env';
 import { SUPPORTED_SOLANA_TOKENS } from '$env/tokens/tokens.sol.env';
 import { SPL_TOKENS } from '$env/tokens/tokens.spl.env';
-import floki from '$eth/assets/floki.svg';
 import rndr from '$eth/assets/rndr.svg';
 import weeth from '$eth/assets/weeth.svg';
 import weth from '$eth/assets/weth.svg';
@@ -24,7 +23,6 @@ import { mockValidErc20Token } from '$tests/mocks/erc20-tokens.mock';
 
 describe('erc20.utils', () => {
 	const iconCases = [
-		['floki', floki],
 		['rndr', rndr],
 		['weeth', weeth],
 		['weth', weth],
