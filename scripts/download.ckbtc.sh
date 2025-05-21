add356cd7f41c4c4214a5bc49d8a1a6573f8f882
Max Murphy <max@winning.black>
2025-05-21 16:32:08 +0200

    make gunzip workeven if the target already exsts

--- scripts/download.ckbtc.sh
+++ scripts/download.ckbtc.sh
@@ -11,16 +11,16 @@ fi
 IC_COMMIT="03dd6ee6de80c2202f66948692c69c61eb6af54d"
 
 scripts/download-immutable.sh "https://download.dfinity.systems/ic/$IC_COMMIT/canisters/ic-ckbtc-minter.wasm.gz" /ckbtc_minter.wasm.gz
-gunzip "$DIR"/ckbtc_minter.wasm.gz
+gunzip --keep --force "$DIR"/ckbtc_minter.wasm.gz
 
 scripts/download-immutable.sh "https://download.dfinity.systems/ic/$IC_COMMIT/canisters/ic-icrc1-ledger.wasm.gz" "$DIR"/ckbtc_ledger.wasm.gz
-gunzip -k "$DIR"/ckbtc_ledger.wasm.gz
+gunzip --keep --force "$DIR"/ckbtc_ledger.wasm.gz
 
 scripts/download-immutable.sh "https://download.dfinity.systems/ic/$IC_COMMIT/canisters/ic-icrc1-index-ng.wasm.gz" "$DIR"/ckbtc_index.wasm.gz
-gunzip -k "$DIR"/ckbtc_index.wasm.gz
+gunzip --keep --force "$DIR"/ckbtc_index.wasm.gz
 
 scripts/download-immutable.sh "https://download.dfinity.systems/ic/$IC_COMMIT/canisters/ic-ckbtc-kyt.wasm.gz" "$DIR"/ckbtc_kyt.wasm.gz
-gunzip "$DIR"/ckbtc_kyt.wasm.gz
+gunzip --keep --force "$DIR"/ckbtc_kyt.wasm.gz
 
 scripts/download-immutable.sh "https://raw.githubusercontent.com/dfinity/ic/$IC_COMMIT/rs/bitcoin/ckbtc/minter/ckbtc_minter.did" "$DIR"/ckbtc_minter.did
 
--- scripts/download.icp.sh
+++ scripts/download.icp.sh
@@ -11,9 +11,9 @@ fi
 IC_VERSION=6dcfafb491092704d374317d9a72a7ad2475d7c9
 
 scripts/download-immutable.sh "https://download.dfinity.systems/ic/$IC_VERSION/canisters/ic-icp-index-canister.wasm.gz" "$DIR"/icp_index.wasm.gz
-gunzip "$DIR"/icp_index.wasm.gz
+gunzip --force "$DIR"/icp_index.wasm.gz
 scripts/download-immutable.sh "https://raw.githubusercontent.com/dfinity/ic/$IC_VERSION/rs/ledger_suite/icp/index/index.did" "$DIR"/icp_index.did
 
 scripts/download-immutable.sh "https://download.dfinity.systems/ic/$IC_VERSION/canisters/ledger-canister.wasm.gz" "$DIR"/icp_ledger.wasm.gz
-gunzip "$DIR"/icp_ledger.wasm.gz
+gunzip --force "$DIR"/icp_ledger.wasm.gz
 scripts/download-immutable.sh "https://raw.githubusercontent.com/dfinity/ic/$IC_VERSION/rs/ledger_suite/icp/ledger.did" "$DIR"/icp_ledger.did
