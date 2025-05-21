e3c34da5505138282b83d3592c61a4c9fd10b7f1
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-05-21 10:02:34 +0200

    upgrades ButtonIcon to svelte 5

--- src/frontend/src/lib/components/receive/ReceiveActions.svelte
+++ src/frontend/src/lib/components/receive/ReceiveActions.svelte
@@ -3,18 +3,21 @@
 	import ReceiveCopy from '$lib/components/receive/ReceiveCopy.svelte';
 	import ButtonIcon from '$lib/components/ui/ButtonIcon.svelte';
 	import type { ReceiveQRCodeAction } from '$lib/types/receive';
+	import {createEventDispatcher} from "svelte";
 
 	export let address: string;
 	export let qrCodeAction: ReceiveQRCodeAction = { enabled: false };
 	export let copyAriaLabel: string;
 	export let copyButtonTestId: string | undefined = undefined;
+
+	const dispatch = createEventDispatcher();
 </script>
 
 <div class="flex justify-center gap-2">
 	{#if qrCodeAction.enabled}
 		<ButtonIcon
 			ariaLabel={qrCodeAction.ariaLabel}
-			on:click
+			onclick={() => dispatch('click')}
 			testId={qrCodeAction?.testId}
 			link={false}
 		>
