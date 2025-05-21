66dd46dd60edda569fb07ee7de0b63d7fc5d8c54
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-05-21 09:17:39 +0200

    refactor(frontend): upgrades TokenInputCurrency to svelte 5 (#6685)
    # Motivation
    
    Since we upgraded svelte from V4 to V5 we also want to upgrade the
    components step by step to V5.
    
    
    # Changes
    
    - Upgrades `TokenInputCurrency`
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

--- src/frontend/src/lib/components/tokens/TokenInputCurrency.svelte
+++ src/frontend/src/lib/components/tokens/TokenInputCurrency.svelte
@@ -2,16 +2,31 @@
 	import InputCurrency from '$lib/components/ui/InputCurrency.svelte';
 	import type { OptionAmount } from '$lib/types/send';
 
-	export let value: OptionAmount;
-	export let decimals: number;
-	export let name = 'token-input-currency';
-	export let disabled = false;
-	export let placeholder = '0';
-	export let error = false;
-	export let loading = false;
-	export let testId: string | undefined = undefined;
-	export let styleClass = '';
-	export let autofocus = false;
+	interface Props {
+		value: OptionAmount;
+		decimals: number;
+		name?: string;
+		disabled?: boolean;
+		placeholder?: string;
+		error?: boolean;
+		loading?: boolean;
+		testId?: string;
+		styleClass?: string;
+		autofocus?: boolean;
+	}
+
+	let {
+		value = $bindable(),
+		decimals,
+		name = 'token-input-currency',
+		disabled = false,
+		placeholder = '0',
+		error = false,
+		loading = false,
+		testId,
+		styleClass = '',
+		autofocus = false
+	}: Props = $props();
 </script>
 
 <div
