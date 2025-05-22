dfinity/oisy-wallet

commit 7af621516ee548d5f90fb00c4b13630fc014695b

feat(backend): add implementation for get_contacts service (#6744)
# Motivation

This PR aims to introduce a way to manage user contacts, enhancing the
system's functionality for storing and retrieving user-specific contact
data from stable storage.

# Changes

- Added a new contacts module in the backend for retrieving user
contacts (src/backend/src/contacts.rs).
- Integrated the contacts module into the main library
(src/backend/src/lib.rs), including initialization of the `ContactMap`
storage (**Is a separate PR required?**).
- Modified types.rs to define a type for the `ContactMap` used for
contact storage (**Is a separate PR required?**).
- Updated the get_contacts function to fetch data from the new contacts
module rather than using mock data.
- Added a placeholder test file for contacts functionality
(src/backend/tests/it/contacts.rs).
- Removed unused legacy struct `CreateCanisterFromRet` in the
cycles_ledger types library (**Is a separate PR required?**)

# Tests

- TODO: Integration tests for testing the endpoints as soon as the
create_contact has been merged

---------

Co-authored-by: Svetoslav Kirov <svetoslav.yuliev@inacta.ch>
Co-authored-by: DecentAgeCoder <marco.oesch@infinity.org>
Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

src/backend/src/contacts.rs
@@ -1,0 +1,19 @@
+use shared::types::contact::Contact;
+
+use crate::{read_state, types::StoredPrincipal};
+
+// The contact services provide the
+// Make sure to stick to the following rules when implementing a contact service
+// 1. Do not expose the stored model StoredContacts
+// 2. Return the
+
+pub fn get_contacts() -> Vec<Contact> {
+    let stored_principal = StoredPrincipal(ic_cdk::caller());
+    read_state(|s| {
+        s.contact
+            .get(&stored_principal)
+            .map(|stored_contacts| stored_contacts.contacts.clone())
+            // a user is allowed to have no contacts (if no contact has yet been added)
+            .unwrap_or_default()
+    })
+}

src/backend/src/lib.rs
@@ -70,7 +70,7 @@ use crate::{
     assertions::assert_token_enabled_is_some,
     guards::{caller_is_allowed, caller_is_controller, caller_is_not_anonymous},
     token::{add_to_user_token, remove_from_user_token},
-    types::PowChallengeMap,
+    types::{ContactMap, PowChallengeMap},
     user_profile::{add_hidden_dapp_id, set_show_testnets, update_network_settings},
 };
 
@@ -78,6 +78,7 @@ mod assertions;
 mod bitcoin_api;
 mod bitcoin_utils;
 mod config;
+mod contacts;
 mod guards;
 mod heap_state;
 mod impls;
@@ -98,6 +99,7 @@ const USER_CUSTOM_TOKEN_MEMORY_ID: MemoryId = MemoryId::new(2);
 const USER_PROFILE_MEMORY_ID: MemoryId = MemoryId::new(3);
 const USER_PROFILE_UPDATED_MEMORY_ID: MemoryId = MemoryId::new(4);
 const POW_CHALLENGE_MEMORY_ID: MemoryId = MemoryId::new(5);
+const CONTACT_MEMORY_ID: MemoryId = MemoryId::new(6);
 
 thread_local! {
     static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> = RefCell::new(
@@ -113,6 +115,7 @@ thread_local! {
             user_profile: UserProfileMap::init(mm.borrow().get(USER_PROFILE_MEMORY_ID)),
             user_profile_updated: UserProfileUpdatedMap::init(mm.borrow().get(USER_PROFILE_UPDATED_MEMORY_ID)),
             pow_challenge: PowChallengeMap::init(mm.borrow().get(POW_CHALLENGE_MEMORY_ID)),
+            contact: ContactMap::init(mm.borrow().get(CONTACT_MEMORY_ID)),
         })
     );
 }
@@ -150,6 +153,7 @@ pub struct State {
     user_profile: UserProfileMap,
     user_profile_updated: UserProfileUpdatedMap,
     pow_challenge: PowChallengeMap,
+    contact: ContactMap,
 }
 
 fn set_config(arg: InitArg) {
@@ -942,26 +946,15 @@ pub fn get_contact(contact_id: u64) -> Result<Contact, ContactError> {
     })
 }
 
-/// Lists all contacts for the caller.
+/// Returns all contacts for the caller
 ///
-/// # Errors
-/// Errors are enumerated by: `ContactError`.
+/// This query function returns a list of the user's contacts.
+/// # Returns
+/// * `Ok(Vec<Contact>)` - A vector of the user's contacts.
 #[query(guard = "caller_is_not_anonymous")]
 #[must_use]
 pub fn get_contacts() -> GetContactsResult {
-    // TODO replace mock data with the get contacts service
-    let normal_result = Ok(vec![Contact {
-        id: time(),
-        name: "John Doe".to_string(),
-        addresses: vec![ContactAddressData {
-            token_account_id: TokenAccountId::Eth(EthAddress::Public(
-                "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045".to_string(),
-            )),
-            label: Some("ETH Wallet".to_string()),
-        }],
-        update_timestamp_ns: time(),
-    }]);
-    normal_result.into()
+    let result = Ok(contacts::get_contacts());
+    result.into()
 }
-
 export_candid!();

src/backend/tests/it/contacts.rs
@@ -1,0 +1,1 @@
+

src/backend/src/types.rs
@@ -3,8 +3,8 @@ use ic_stable_structures::{
     memory_manager::VirtualMemory, DefaultMemoryImpl, StableBTreeMap, StableCell,
 };
 use shared::types::{
-    backend_config::Config, custom_token::CustomToken, pow::StoredChallenge, token::UserToken,
-    user_profile::StoredUserProfile, Timestamp,
+    backend_config::Config, contact::StoredContacts, custom_token::CustomToken,
+    pow::StoredChallenge, token::UserToken, user_profile::StoredUserProfile, Timestamp,
 };
 
 pub type VMem = VirtualMemory<DefaultMemoryImpl>;
@@ -24,3 +24,5 @@ where
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct StoredPrincipal(pub Principal);
+// Define a new type for the contact storage
+pub type ContactMap = StableBTreeMap<StoredPrincipal, Candid<StoredContacts>, VMem>;

src/shared/src/types/result_types.rs
@@ -57,31 +57,12 @@ impl From<Result<(), AddUserCredentialError>> for AddUserCredentialResult {
 
 #[derive(CandidType, Deserialize, Clone, Eq, PartialEq, Debug)]
 pub enum GetContactResult {
-    /// The contact was created successfully.
+    /// The contacts were retrieved successfully.
     Ok(Contact),
-    /// The contact was not created due to an error.
+    /// The contacts were not retrieved due to an error.
     Err(ContactError),
 }
-impl GetContactResult {
-    #[must_use]
-    pub fn is_err(&self) -> bool {
-        matches!(self, Self::Err(_))
-    }
 
-    /// Returns the contained `ContactError` if the result is an `Err`.
-    ///
-    /// # Panics
-    /// - If the result is `Ok`.
-    #[must_use]
-    pub fn unwrap_err(self) -> ContactError {
-        match self {
-            Self::Err(err) => err,
-            Self::Ok(_) => {
-                panic!("Called `CreateContactResult.unwrap_err()` on an `Ok` value")
-            }
-        }
-    }
-}
 impl From<Result<Contact, ContactError>> for GetContactResult {
     fn from(result: Result<Contact, ContactError>) -> Self {
         match result {

commit a83e02d030044243d3466f4a7657d113174d11e4

feat(backend): add implementation for get_contact service (#6747)
# Motivation

This PR adds new functionality to retrieve a contact by it's id

# Changes

- Added a new function `get_contact` in contacts.rs to retrieve a
contact by its ID using real stored data instead of mocks.
- Updated the get_contact implementation in lib.rs to use the new
`get_contact` function from `contacts.rs`.
- Cleaned up unnecessary imports in lib.rs (**Resolving the clippy error
that got introduced by #6744** ).
- Removed outdated comments and unused imports in `contacts.rs.`
# Tests

TODO: Integration tests for testing the endpoints as soon as the
create_contact has been merged

---------

Co-authored-by: Svetoslav Kirov <svetoslav.yuliev@inacta.ch>
Co-authored-by: DecentAgeCoder <marco.oesch@infinity.org>
Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

src/backend/src/contacts.rs
@@ -1,11 +1,6 @@
-use shared::types::contact::Contact;
+use shared::types::contact::{Contact, ContactError};
 
 use crate::{read_state, types::StoredPrincipal};
-
-// The contact services provide the
-// Make sure to stick to the following rules when implementing a contact service
-// 1. Do not expose the stored model StoredContacts
-// 2. Return the
 
 pub fn get_contacts() -> Vec<Contact> {
     let stored_principal = StoredPrincipal(ic_cdk::caller());
@@ -15,5 +10,32 @@ pub fn get_contacts() -> Vec<Contact> {
             .map(|stored_contacts| stored_contacts.contacts.clone())
             // a user is allowed to have no contacts (if no contact has yet been added)
             .unwrap_or_default()
+    })
+}
+
+/// Retrieves a specific contact by ID for the current user.
+///
+/// # Arguments
+/// * `contact_id` - The unique identifier of the contact to retrieve
+///
+/// # Returns
+/// * `Ok(Contact)` - The requested contact if found
+/// * `Err(ContactError::NotFound)` - If no contact with the given ID exists for the user
+pub fn get_contact(contact_id: u64) -> Result<Contact, ContactError> {
+    let stored_principal = StoredPrincipal(ic_cdk::caller());
+
+    read_state(|s| {
+        // Get the user's contacts storage
+        let contacts = s
+            .contact
+            .get(&stored_principal)
+            .map(|stored_contacts| stored_contacts.contacts.clone())
+            .unwrap_or_default();
+
+        // Find the specific contact by ID
+        contacts
+            .into_iter()
+            .find(|contact| contact.id == contact_id)
+            .ok_or(ContactError::ContactNotFound)
     })
 }

src/backend/src/lib.rs
@@ -21,7 +21,6 @@ use shared::{
     metrics::get_metrics,
     std_canister_status,
     types::{
-        account::{EthAddress, TokenAccountId},
         backend_config::{Arg, Config, InitArg},
         bitcoin::{
             BtcAddPendingTransactionError, BtcAddPendingTransactionRequest,
@@ -29,7 +28,7 @@ use shared::{
             BtcGetPendingTransactionsRequest, PendingTransaction, SelectedUtxosFeeError,
             SelectedUtxosFeeRequest, SelectedUtxosFeeResponse,
         },
-        contact::{Contact, ContactAddressData, CreateContactRequest, UpdateContactRequest},
+        contact::{Contact, CreateContactRequest, UpdateContactRequest},
         custom_token::{CustomToken, CustomTokenId},
         dapp::{AddDappSettingsError, AddHiddenDappIdRequest},
         network::{SaveNetworksSettingsError, SaveNetworksSettingsRequest, SetShowTestnetsRequest},
@@ -927,24 +926,16 @@ pub fn delete_contact(contact_id: u64) -> DeleteContactResult {
 
 /// Gets a contact by ID for the caller.
 ///
+/// # Arguments
+/// * `contact_id` - The unique identifier of the contact to retrieve
+/// # Returns
+/// * `Ok(GetContactResult)` - The requested contact if found
 /// # Errors
-/// Errors are enumerated by: `ContactError`.
+/// * `ContactNotFound` - If no contact for the proivided contact_id could be found
 #[query(guard = "caller_is_not_anonymous")]
 #[must_use]
 pub fn get_contact(contact_id: u64) -> GetContactResult {
-    // TODO replace mock data with the get contact service
-    let result = Ok(Contact {
-        id: contact_id,
-        name: "John Doe".to_string(),
-        addresses: vec![ContactAddressData {
-            token_account_id: TokenAccountId::Eth(EthAddress::Public(
-                "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045".to_string(),
-            )),
-            label: Some("ETH Wallet".to_string()),
-        }],
-        update_timestamp_ns: time(),
-    });
-    result.into()
+    contacts::get_contact(contact_id).into()
 }
 
 /// Returns all contacts for the caller