ca5ef09b5d37bda394afa50ec3b8bc8510ec3f13
daviddecentage <david.farina@dfinity.org>
2025-05-22 14:37:16 +0200

    update

---
+++ src/frontend/src/lib/components/common/ModalHero.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import type { ModalHeroVariant } from '$lib/types/style';
+	import type { Snippet } from 'svelte';
+	import { nonNullish } from '@dfinity/utils';
+	interface Props {
+		logo?: Snippet;
+		title?: Snippet;
+		subtitle?: Snippet;
+		description?: Snippet;
+		variant?: ModalHeroVariant;
+	}
+	const { logo, title, subtitle, description, variant = 'default' }: Props = $props();
+</script>
+
+<div
+	class="mb-6 flex flex-col items-center justify-center rounded-lg border border-solid p-4"
+	class:bg-brand-subtle-10={variant === 'default'}
+	class:border-brand-subtle-20={variant === 'default'}
+	class:bg-success-subtle-20={variant === 'success'}
+	class:border-success-subtle-20={variant === 'success'}
+>
+	{#if nonNullish(logo)}
+		<div class="mb-1.5">
+			{@render logo()}
+		</div>
+	{/if}
+
+	{#if nonNullish(subtitle)}
+		<div class="mb-1 text-base font-bold">
+			{@render subtitle()}
+		</div>
+	{/if}
+
+	{#if nonNullish(title)}
+		<div class="text-2xl font-bold">
+			{@render title()}
+		</div>
+	{/if}
+
+	{#if nonNullish(description)}
+		<div class="text-sm text-tertiary">
+			{@render description()}
+		</div>
+	{/if}
+</div>
