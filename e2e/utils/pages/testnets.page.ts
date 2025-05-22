19d8b2884bec2bb211f5d889c8800a552b2dfad2
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-05-22 16:54:02 +0200

    fix

--- e2e/utils/pages/testnets.page.ts
+++ e2e/utils/pages/testnets.page.ts
@@ -43,7 +43,7 @@ export const TestnetCases: TestnetConfig[] = [
 		tokenSymbol: 'BNB (Testnet)'
 	},
 	{
-		networkSymbol: 'Polygon (Amoy Testnet)',
+		networkSymbol: 'POL (Amoy Testnet)',
 		tokenSymbol: 'POL (Amoy Testnet)'
 	}
 ];
