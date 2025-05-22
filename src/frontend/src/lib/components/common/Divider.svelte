a64008cdaa48c9ced2d29dc52d3ee29206a3d723
daviddecentage <david.farina@dfinity.org>
2025-05-22 16:48:16 +0200

    update

---
+++ src/frontend/src/lib/components/common/Divider.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export interface Props {
+		variant?: 'dot';
+	}
+
+	const { variant = 'dot' }: Props = $props();
+</script>
+
+{#if variant === 'dot'}
+	<svg
+		fill="currentColor"
+		width="8px"
+		height="8px"
+		viewBox="0 0 20 20"
+		xmlns="http://www.w3.org/2000/svg"
+		stroke="currentColor"
+		><g stroke-width="0"></g><g stroke-linecap="round" stroke-linejoin="round"></g><g
+			><path d="M7.8 10a2.2 2.2 0 0 0 4.4 0 2.2 2.2 0 0 0-4.4 0z"></path></g
+		></svg
+	>
+{/if}
---
+++ src/frontend/src/lib/components/common/DividerList.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import Divider from '$lib/components/common/Divider.svelte';
+	import type { Props as DividerProps } from '$lib/components/common/Divider.svelte';
+
+	interface Props {
+		[key: `item${number}`]: Snippet;
+		dividerProps?: DividerProps;
+		dividerColor?: string;
+	}
+
+	const { dividerProps, dividerColor = '', ...snippets }: Props = $props();
+</script>
+
+<span class="flex flex-row items-center">
+	{#each Object.entries(snippets) as [key, snippet], index (key)}
+		{#if index > 0}
+			<span class={dividerColor}><Divider {...dividerProps} /></span>
+		{/if}
+		<span class="mr-1.5 flex" class:ml-1.5={index > 0}>
+			{@render snippet()}
+		</span>
+	{/each}
+</span>
