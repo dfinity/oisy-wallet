429af07ebca3d73679e459a5a2ae9600c08196f8
Stefan Berger <stefan.berger@inacta.ch>
2025-05-23 12:14:55 +0200

    Add XP to network config

--- src/frontend/src/env/networks/networks.icrc.env.ts
+++ src/frontend/src/env/networks/networks.icrc.env.ts
@@ -495,6 +495,13 @@ const VEUR_IC_DATA: IcInterface | undefined = nonNullish(ADDITIONAL_ICRC_PRODUCT
 		}
 	: undefined;
 
+const XP_IC_DATA: IcInterface | undefined = nonNullish(ADDITIONAL_ICRC_PRODUCTION_DATA?.XP)
+	? {
+			...ADDITIONAL_ICRC_PRODUCTION_DATA.XP,
+			position: 24
+		}
+	: undefined;
+
 export const CKERC20_LEDGER_CANISTER_TESTNET_IDS: CanisterIdText[] = [
 	...(nonNullish(LOCAL_CKUSDC_LEDGER_CANISTER_ID) ? [LOCAL_CKUSDC_LEDGER_CANISTER_ID] : []),
 	...(nonNullish(CKUSDC_STAGING_DATA?.ledgerCanisterId)
@@ -570,7 +577,8 @@ const ADDITIONAL_ICRC_TOKENS: IcInterface[] = [
 	...(nonNullish(RUGGY_IC_DATA) ? [RUGGY_IC_DATA] : []),
 	...(nonNullish(NAK_IC_DATA) ? [NAK_IC_DATA] : []),
 	...(nonNullish(VCHF_IC_DATA) ? [VCHF_IC_DATA] : []),
-	...(nonNullish(VEUR_IC_DATA) ? [VEUR_IC_DATA] : [])
+	...(nonNullish(VEUR_IC_DATA) ? [VEUR_IC_DATA] : []),
+	...(nonNullish(XP_IC_DATA) ? [XP_IC_DATA] : [])	
 ];
 
 export const ICRC_TOKENS: IcInterface[] = [
