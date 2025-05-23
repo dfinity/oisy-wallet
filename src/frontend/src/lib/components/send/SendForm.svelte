c06e97850a2b08e0b34fb896b32953d09fc850ac
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-05-23 09:19:13 +0200

    refactor(frontend): remove convert-related code from ETH send components (#6820)
    # Motivation
    
    We want to remove convert-related code from ETH send components.

--- src/frontend/src/lib/components/send/SendForm.svelte
+++ src/frontend/src/lib/components/send/SendForm.svelte
@@ -1,22 +1,14 @@
 <script lang="ts">
-	import { nonNullish } from '@dfinity/utils';
 	import { createEventDispatcher } from 'svelte';
 	import SendDestination from '$lib/components/send/SendDestination.svelte';
-	import SendSource from '$lib/components/send/SendSource.svelte';
 	import ButtonGroup from '$lib/components/ui/ButtonGroup.svelte';
 	import ButtonNext from '$lib/components/ui/ButtonNext.svelte';
 	import ContentWithToolbar from '$lib/components/ui/ContentWithToolbar.svelte';
 	import { SEND_FORM_NEXT_BUTTON } from '$lib/constants/test-ids.constants';
-	import type { OptionBalance } from '$lib/types/balance';
-	import type { OptionToken } from '$lib/types/token';
 
-	export let source: string | undefined = undefined;
 	export let destination = '';
 	export let invalidDestination = false;
 	export let disabled: boolean | undefined = false;
-	export let token: OptionToken = undefined;
-	export let balance: OptionBalance = undefined;
-	export let hideSource = false;
 
 	const dispatch = createEventDispatcher();
 
@@ -28,10 +20,6 @@
 		<slot name="amount" />
 
 		<SendDestination {destination} {invalidDestination} on:icSendDestinationStep={back} />
-
-		{#if !hideSource && nonNullish(source)}
-			<SendSource {token} {balance} {source} />
-		{/if}
 
 		<slot name="fee" />
 
