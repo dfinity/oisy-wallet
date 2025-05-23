3ab8eb0ac2c84cd4fa0b36b3d8d540eadab64288
daviddecentage <david.farina@dfinity.org>
2025-05-23 17:00:39 +0200

    update

---
+++ src/frontend/src/lib/components/contact/AvatarWithBadge.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import AddressesBadge from '$lib/components/contact/AddressesBadge.svelte';
+	import Avatar from '$lib/components/contact/Avatar.svelte';
+	import type { ContactUi } from '$lib/types/contact';
+	import type { AvatarVariants } from '$lib/types/style';
+
+	interface Props {
+		contact: ContactUi;
+		badge?: {
+			address: string;
+		};
+		variant?: AvatarVariants;
+	}
+
+	const { contact, variant }: Props = $props();
+</script>
+
+<div class="relative">
+	<Avatar name={contact.name} {variant} styleClass="md:text-[19.2px]"></Avatar>
+	<AddressesBadge addresses={contact.addresses} />
+</div>
