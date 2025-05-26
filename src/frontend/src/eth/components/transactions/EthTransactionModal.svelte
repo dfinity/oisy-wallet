8aa19694f6d852625d63eeef3a53f14f3a974977
daviddecentage <david.farina@dfinity.org>
2025-05-26 14:25:59 +0200

    update

--- src/frontend/src/eth/components/transactions/EthTransactionModal.svelte
+++ src/frontend/src/eth/components/transactions/EthTransactionModal.svelte
@@ -19,12 +19,17 @@
 	import type { OptionString } from '$lib/types/string';
 	import type { OptionToken } from '$lib/types/token';
 	import {
+		formatNanosecondsToDate,
 		formatSecondsToDate,
 		formatToken,
 		shortenWithMiddleEllipsis
 	} from '$lib/utils/format.utils';
 	import { replacePlaceholders } from '$lib/utils/i18n.utils';
 	import { isNetworkIdSepolia } from '$lib/utils/network.utils';
+	import ModalHero from '$lib/components/common/ModalHero.svelte';
+	import TokenLogo from '$lib/components/tokens/TokenLogo.svelte';
+	import TransactionContactCard from '$lib/components/transactions/TransactionContactCard.svelte';
+	import NetworkWithLogo from '$lib/components/networks/NetworkWithLogo.svelte';
 
 	export let transaction: EthTransactionUi;
 	export let token: OptionToken;
@@ -78,112 +83,169 @@
 	<svelte:fragment slot="title">{$i18n.transaction.text.details}</svelte:fragment>
 
 	<ContentWithToolbar>
-		{#if nonNullish(hash)}
-			<Value ref="hash">
-				{#snippet label()}
-					{$i18n.transaction.text.hash}
-				{/snippet}
-				{#snippet content()}
-					<output>{shortenWithMiddleEllipsis({ text: hash })}</output>
-					<Copy
-						value={hash}
-						text={replacePlaceholders($i18n.transaction.text.hash_copied, {
-							$hash: hash
-						})}
-						inline
-					/>
-					{#if nonNullish(explorerUrl)}
-						<ExternalLink
-							iconSize="18"
-							href={explorerUrl}
-							ariaLabel={$i18n.transaction.alt.open_block_explorer}
-							inline
-							color="blue"
-						/>
-					{/if}
-				{/snippet}
-			</Value>
-		{/if}
-
-		{#if nonNullish(blockNumber)}
-			<Value ref="blockNumber">
-				{#snippet label()}
-					{$i18n.transaction.text.block}
-				{/snippet}
-				{#snippet content()}
-					<output>{blockNumber}</output>
-				{/snippet}
-			</Value>
-
-			<EthTransactionStatus {blockNumber} />
-		{/if}
-
-		{#if nonNullish(timestamp)}
-			<Value ref="timestamp">
-				{#snippet label()}
-					{$i18n.transaction.text.timestamp}
-				{/snippet}
-				{#snippet content()}
-					<output>{formatSecondsToDate(timestamp)}</output>
-				{/snippet}
-			</Value>
-		{/if}
-
-		<Value ref="type">
-			{#snippet label()}
-				{$i18n.transaction.text.type}
-			{/snippet}
-			{#snippet content()}
-				{`${type === 'send' ? $i18n.send.text.send : $i18n.receive.text.receive}`}
-			{/snippet}
-		</Value>
-
-		<Value ref="from">
-			{#snippet label()}
-				{$i18n.transaction.text.from}
-			{/snippet}
-			{#snippet content()}
-				<output>{fromDisplay}</output>
-				<Copy value={from} text={$i18n.transaction.text.from_copied} inline />
-				{#if nonNullish(fromExplorerUrl)}
-					<ExternalLink
-						iconSize="18"
-						href={fromExplorerUrl}
-						ariaLabel={$i18n.transaction.alt.open_from_block_explorer}
-						inline
-						color="blue"
-					/>
+		<ModalHero variant={type === 'receive' ? 'success' : 'default'}>
+			{#snippet logo()}
+				{#if nonNullish(token)}
+					<div class="relative block flex w-[54px]">
+						<TokenLogo logoSize="lg" data={token} badge={{ type: 'network' }} />
+					</div>
 				{/if}
 			{/snippet}
-		</Value>
+			{#snippet subtitle()}
+				<span class="capitalize">{type}</span>
+			{/snippet}
+			{#snippet title()}
+				{#if nonNullish(token) && nonNullish(value)}
+					<output class:text-success-primary={type === 'receive'}>
+						{formatToken({
+							value,
+							unitName: token.decimals,
+							displayDecimals: token.decimals,
+							showPlusSign: type === 'receive'
+						})}
+						{token.symbol}
+					</output>
+				{:else}
+					&ZeroWidthSpace;
+				{/if}
+			{/snippet}
+		</ModalHero>
 
-		{#if nonNullish(to) && nonNullish(toDisplay)}
-			<Value ref="to">
+		{#if nonNullish(to) && nonNullish(from)}
+			<TransactionContactCard
+				type={type === 'receive' ? 'receive' : 'send'}
+				{to}
+				{from}
+				{toExplorerUrl}
+				{fromExplorerUrl}
+			/>
+		{/if}
+
+		<ul class="mt-5">
+			{#if type === 'receive' && nonNullish(to)}
+				<li class="border-b-1 flex flex-row justify-between border-brand-subtle-10 py-1.5">
+					<span>{$i18n.transaction.text.to}</span>
+					<output class="flex max-w-[50%] flex-row">
+						<output class="truncate">{to}</output>
+						<Copy value={to} text={$i18n.transaction.text.to_copied} inline />
+						{#if nonNullish(toExplorerUrl)}
+							<ExternalLink
+								iconSize="18"
+								href={toExplorerUrl}
+								ariaLabel={$i18n.transaction.alt.open_to_block_explorer}
+								inline
+								color="blue"
+							/>
+						{/if}
+					</output>
+				</li>
+			{/if}
+			{#if type === 'send' && nonNullish(from)}
+				<li class="border-b-1 flex flex-row justify-between border-brand-subtle-10 py-1.5">
+					<span>{$i18n.transaction.text.from}</span>
+					<output class="flex max-w-[50%] flex-row">
+						<output class="truncate">{from}</output>
+						<Copy value={from} text={$i18n.transaction.text.from_copied} inline />
+						{#if nonNullish(fromExplorerUrl)}
+							<ExternalLink
+								iconSize="18"
+								href={fromExplorerUrl}
+								ariaLabel={$i18n.transaction.alt.open_from_block_explorer}
+								inline
+								color="blue"
+							/>
+						{/if}
+					</output>
+				</li>
+			{/if}
+
+			{#if nonNullish(hash)}
+				<li class="border-b-1 flex flex-row justify-between border-brand-subtle-10 py-1.5">
+					<span>{$i18n.transaction.text.hash}</span>
+					<span>
+						<output>{shortenWithMiddleEllipsis({ text: hash })}</output>
+						<Copy
+							value={hash}
+							text={replacePlaceholders($i18n.transaction.text.hash_copied, {
+								$hash: hash
+							})}
+							inline
+						/>
+						{#if nonNullish(explorerUrl)}
+							<ExternalLink
+								iconSize="18"
+								href={explorerUrl}
+								ariaLabel={$i18n.transaction.alt.open_block_explorer}
+								inline
+								color="blue"
+							/>
+						{/if}
+					</span>
+				</li>
+			{/if}
+
+			{#if nonNullish(blockNumber)}
+				<li class="border-b-1 flex flex-row justify-between border-brand-subtle-10 py-1.5">
+					<span>{$i18n.transaction.text.block}</span>
+					<span>
+						<output>{blockNumber}</output>
+
+						<EthTransactionStatus {blockNumber} />
+					</span>
+				</li>
+			{/if}
+
+			{#if nonNullish(timestamp)}
+				<li class="border-b-1 flex flex-row justify-between border-brand-subtle-10 py-1.5">
+					<span>{$i18n.transaction.text.timestamp}</span>
+					<output>{formatSecondsToDate(timestamp)}</output>
+				</li>
+			{/if}
+			<!--
+			<Value ref="from">
 				{#snippet label()}
-					{$i18n.transaction.text.interacted_with}
+					{$i18n.transaction.text.from}
 				{/snippet}
 				{#snippet content()}
-					<output>{toDisplay}</output>
-					<Copy value={to} text={$i18n.transaction.text.to_copied} inline />
-					{#if nonNullish(toExplorerUrl)}
+					<output>{fromDisplay}</output>
+					<Copy value={from} text={$i18n.transaction.text.from_copied} inline />
+					{#if nonNullish(fromExplorerUrl)}
 						<ExternalLink
 							iconSize="18"
-							href={toExplorerUrl}
-							ariaLabel={$i18n.transaction.alt.open_to_block_explorer}
+							href={fromExplorerUrl}
+							ariaLabel={$i18n.transaction.alt.open_from_block_explorer}
 							inline
 							color="blue"
 						/>
 					{/if}
 				{/snippet}
 			</Value>
-		{/if}
 
-		{#if nonNullish(token)}
-			<Value ref="amount">
-				{#snippet label()}
-					{$i18n.core.text.amount}
-				{/snippet}
-				{#snippet content()}
+			{#if nonNullish(to) && nonNullish(toDisplay)}
+				<Value ref="to">
+					{#snippet label()}
+						{$i18n.transaction.text.interacted_with}
+					{/snippet}
+					{#snippet content()}
+						<output>{toDisplay}</output>
+						<Copy value={to} text={$i18n.transaction.text.to_copied} inline />
+						{#if nonNullish(toExplorerUrl)}
+							<ExternalLink
+								iconSize="18"
+								href={toExplorerUrl}
+								ariaLabel={$i18n.transaction.alt.open_to_block_explorer}
+								inline
+								color="blue"
+							/>
+						{/if}
+					{/snippet}
+				</Value>
+			{/if}
+			-->
+
+			{#if nonNullish(token)}
+				<li class="border-b-1 flex flex-row justify-between border-brand-subtle-10 py-1.5">
+					<span>{$i18n.core.text.amount}</span>
 					<output>
 						{formatToken({
 							value,
@@ -192,9 +254,9 @@
 						})}
 						{token.symbol}
 					</output>
-				{/snippet}
-			</Value>
-		{/if}
+				</li>
+			{/if}
+		</ul>
 
 		<ButtonCloseModal slot="toolbar" />
 	</ContentWithToolbar>
