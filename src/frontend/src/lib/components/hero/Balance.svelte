e9c2c0352ec9f4607460248198dad0df1053af3c
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-05-26 15:05:10 +0200

    fix(frontend): displays token symbol even balance is ZERO (#6884)
    # Motivation
    
    On the `token view` the symbol of the token should be displayed even if
    the token balance is ZERO.
    
    # Changes
    
    - displays token symbol
    
    # Tests
    
    **before:**
    <img width="442" alt="image"
    src="https://github.com/user-attachments/assets/fcd01c27-1bdb-4158-8907-4d48672b46a6"
    />
    
    **after:**
    <img width="467" alt="image"
    src="https://github.com/user-attachments/assets/cb8026bc-4cdb-4fcf-83fa-da8d9577e4bb"
    />

--- src/frontend/src/lib/components/hero/Balance.svelte
+++ src/frontend/src/lib/components/hero/Balance.svelte
@@ -4,7 +4,6 @@
 	import IconDots from '$lib/components/icons/IconDots.svelte';
 	import TokenExchangeBalance from '$lib/components/tokens/TokenExchangeBalance.svelte';
 	import Amount from '$lib/components/ui/Amount.svelte';
-	import { ZERO } from '$lib/constants/app.constants';
 	import { AMOUNT_DATA } from '$lib/constants/test-ids.constants';
 	import { isPrivacyMode } from '$lib/derived/settings.derived';
 	import { HERO_CONTEXT_KEY, type HeroContext } from '$lib/stores/hero.store';
@@ -22,7 +21,7 @@
 		data-tid={AMOUNT_DATA}
 		class="inline-flex w-full flex-row justify-center gap-3 break-words text-4xl font-bold lg:text-5xl"
 	>
-		{#if nonNullish(token?.balance) && nonNullish(token?.symbol) && !(token.balance === ZERO)}
+		{#if nonNullish(token?.balance) && nonNullish(token?.symbol)}
 			{#if $isPrivacyMode}
 				<IconDots variant="lg" times={6} styleClass="h-12.5 my-4.25" />
 			{:else}
