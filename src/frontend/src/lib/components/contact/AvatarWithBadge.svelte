8cd08348d71152505b343ae4f06ded65f3c3530e
daviddecentage <david.farina@dfinity.org>
2025-05-26 17:57:05 +0200

    update

--- src/frontend/src/lib/components/contact/AvatarWithBadge.svelte
+++ src/frontend/src/lib/components/contact/AvatarWithBadge.svelte
@@ -21,20 +21,17 @@
 	const { contact, badge, variant }: Props = $props();
 </script>
 
-<div class="flex">
-	<div class="relative">
-		{#if nonNullish(contact)}
-			<Avatar name={contact.name} {variant} />
-			{#if nonNullish(badge)}
-				{#if badge.type === 'addressTypeOrCount'}
-					<AddressesBadge addresses={contact.addresses} />
-				{:else if badge.type === 'addressType'}
-					<AddressesBadge addresses={contact.addresses} selectedAddress={badge.address} />
-				{/if}
-			{/if}
-		{:else}
-			<Img src={emptyOisyLogo} width="60px" height="60px" />
-			<!-- Todo: add badge for string address, needs util to get address type for address -->
+<div class="relative flex">
+	{#if nonNullish(contact)}
+		<Avatar name={contact.name} {variant} />
+		{#if nonNullish(badge)}
+			<AddressesBadge
+				addresses={contact.addresses}
+				selectedAddress={badge.type === 'addressTypeOrCount' ? badge.address : undefined}
+			/>
 		{/if}
-	</div>
+	{:else}
+		<Img src={emptyOisyLogo} width="60px" height="60px" />
+		<!-- Todo: add badge for string address, needs util to get address type for address -->
+	{/if}
 </div>
