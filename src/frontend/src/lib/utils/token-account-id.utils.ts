efdad5ef42e7b52c0997ef9151b6005986a3fd9f
FlavioInacta <flavio.pellanda@inacta.ch>
2025-05-27 18:19:00 +0200

    refactor(frontend): Fix merge and use consistent name for getTokenAccountIdAddressString (#6926)
    # Motivation
    
    There was a merge issue. This PR cleans this up by removes duplicate
    code and renames the method to `getTokenAccountIdAddressString` in order
    to match consistent nameing with other `get*AddressString` methods .
    
    # Changes
    
    - Cleanup merge
    - Use consistent name
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

--- src/frontend/src/lib/utils/token-account-id.utils.ts
+++ src/frontend/src/lib/utils/token-account-id.utils.ts
@@ -1,12 +1,11 @@
 import { getBtcAddressString } from '$btc/utils/btc-address.utils';
 import type { TokenAccountId } from '$declarations/backend/backend.did';
 import { TOKEN_ACCOUNT_ID_TO_NETWORKS } from '$lib/constants/token-account-id.constants';
-import type { Address } from '$lib/types/address';
 import type { Network } from '$lib/types/network';
 import type { TokenAccountIdTypes } from '$lib/types/token-account-id';
 import { assertNever } from '$lib/types/utils';
 
-export const tokenAccountIdToAddressString = (tokenAccountId: TokenAccountId): string => {
+export const getTokenAccountIdAddressString = (tokenAccountId: TokenAccountId): string => {
 	if ('Btc' in tokenAccountId) {
 		return getBtcAddressString(tokenAccountId.Btc);
 	}
@@ -38,24 +37,6 @@ export const getDiscriminatorForTokenAccountId = (
 	}
 	if ('Sol' in tokenAccountId) {
 		return 'Sol';
-	}
-
-	return assertNever({ variable: tokenAccountId, typeName: 'TokenAccountId' });
-};
-
-export const getAddressString = (tokenAccountId: TokenAccountId): Address => {
-	if ('Btc' in tokenAccountId) {
-		return getBtcAddressString(tokenAccountId.Btc);
-	}
-	if ('Eth' in tokenAccountId) {
-		return tokenAccountId.Eth.Public;
-	}
-	if ('Icrcv2' in tokenAccountId) {
-		// TODO PR: https://github.com/dfinity/oisy-wallet/pull/6716
-		throw new Error('Not implemented yet');
-	}
-	if ('Sol' in tokenAccountId) {
-		return tokenAccountId.Sol;
 	}
 
 	return assertNever({ variable: tokenAccountId, typeName: 'TokenAccountId' });
