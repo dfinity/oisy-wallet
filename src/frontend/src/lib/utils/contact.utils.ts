b529f4afbad3e39c48977740f27418163b27acdc
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-05-27 18:04:47 +0200

    feat(frontend): mapAddressToContactAddressUi util (#6932)
    # Motivation
    
    We need to create a mapper for Address -> ContactAddressUi.

--- src/frontend/src/lib/utils/contact.utils.ts
+++ src/frontend/src/lib/utils/contact.utils.ts
@@ -1,12 +1,13 @@
 import type { Contact } from '$declarations/backend/backend.did';
 import { TokenAccountIdSchema } from '$lib/schema/token-account-id.schema';
-import type { ContactUi } from '$lib/types/contact';
+import type { Address } from '$lib/types/address';
+import type { ContactAddressUi, ContactUi } from '$lib/types/contact';
 import type { NonEmptyArray } from '$lib/types/utils';
 import {
 	getAddressString,
 	getDiscriminatorForTokenAccountId
 } from '$lib/utils/token-account-id.utils';
-import { fromNullable, isEmptyString, toNullable } from '@dfinity/utils';
+import { fromNullable, isEmptyString, isNullish, toNullable } from '@dfinity/utils';
 
 export const selectColorForName = <T>({
 	colors,
@@ -63,3 +64,19 @@ export const getContactForAddress = ({
 	contactList.find((c) =>
 		c.addresses.find((address) => address.address.toLowerCase() === addressString.toLowerCase())
 	);
+
+export const mapAddressToContactAddressUi = (address: Address): ContactAddressUi | undefined => {
+	const tokenAccountIdParseResult = TokenAccountIdSchema.safeParse(address);
+	const currentAddressType = tokenAccountIdParseResult?.success
+		? getDiscriminatorForTokenAccountId(tokenAccountIdParseResult.data)
+		: undefined;
+
+	if (isNullish(currentAddressType)) {
+		return;
+	}
+
+	return {
+		address,
+		addressType: currentAddressType
+	};
+};
