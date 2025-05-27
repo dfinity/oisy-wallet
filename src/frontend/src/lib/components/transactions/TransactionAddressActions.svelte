a16aad7349f4abbde006059d122e5f0d3b04c286
daviddecentage <david.farina@dfinity.org>
2025-05-27 13:52:47 +0200

    update

---
+++ src/frontend/src/lib/components/transactions/TransactionAddressActions.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Copy from '$lib/components/ui/Copy.svelte';
+	import ExternalLink from '$lib/components/ui/ExternalLink.svelte';
+	import { nonNullish } from '@dfinity/utils';
+
+	interface Props {
+		copyAddress?: string;
+		copyAddressText?: string;
+		explorerUrl?: string;
+		explorerUrlAriaLabel?: string;
+	}
+
+	const { copyAddress, copyAddressText, explorerUrl, explorerUrlAriaLabel }: Props = $props();
+</script>
+
+{#if nonNullish(copyAddress) && nonNullish(copyAddressText)}
+	<Copy value={copyAddress} text={copyAddressText} inline />
+{/if}
+{#if nonNullish(explorerUrl) && nonNullish(explorerUrlAriaLabel)}
+	<ExternalLink
+		iconSize="18"
+		href={explorerUrl}
+		ariaLabel={explorerUrlAriaLabel}
+		inline
+		color="blue"
+	/>
+{/if}
