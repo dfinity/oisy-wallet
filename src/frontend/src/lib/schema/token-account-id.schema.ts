5fe24f59cfa643c932a66da1c63940aa0cc35acf
FlavioInacta <flavio.pellanda@inacta.ch>
2025-05-27 08:10:39 +0200

    feat(frontend): Add IcpAccountSchema (#6716)
    # Motivation
    
    In order to be able to parse ICP adresses, this PR adds a
    `IcpAddressSchema`
    
    # Changes
    
    - Adds `IcpAddressSchema`

--- src/frontend/src/lib/schema/token-account-id.schema.ts
+++ src/frontend/src/lib/schema/token-account-id.schema.ts
@@ -1,9 +1,13 @@
+import {
+	BtcAddressObjectSchema,
+	EthAddressObjectSchema,
+	Icrcv2AccountIdObjectSchema,
+	SolAddressSchema
+} from '$lib/schema/address.schema';
 import { z } from 'zod';
-import { BtcAddressObjectSchema, EthAddressObjectSchema, SolAddressSchema } from './address.schema';
 
 export const TokenAccountIdSchema = z.union([
-	// TODO: PR https://github.com/dfinity/oisy-wallet/pull/6716
-	// Icrcv2AccountIdObjectSchema.transform((data) => ({ Icrcv2: data })),
+	Icrcv2AccountIdObjectSchema.transform((data) => ({ Icrcv2: data })),
 	BtcAddressObjectSchema.transform((data) => ({ Btc: data })),
 	EthAddressObjectSchema.transform((data) => ({ Eth: data })),
 	SolAddressSchema.transform((data) => ({ Sol: data }))
