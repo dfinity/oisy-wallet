5706e371a0e330b7e7bdbedbe115c3652d702e13
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2025-05-27 16:54:01 +0200

    feat(frontend): review

--- src/frontend/src/lib/components/contact/AvatarWithBadge.svelte
+++ src/frontend/src/lib/components/contact/AvatarWithBadge.svelte
@@ -4,11 +4,14 @@
 	import AddressesBadge from '$lib/components/contact/AddressesBadge.svelte';
 	import Avatar from '$lib/components/contact/Avatar.svelte';
 	import Img from '$lib/components/ui/Img.svelte';
+	import { OISY_NAME } from '$lib/constants/oisy.constants';
 	import { AVATAR_WITH_BADGE_FALLBACK_IMAGE } from '$lib/constants/test-ids.constants';
+	import { i18n } from '$lib/stores/i18n.store';
 	import type { Address } from '$lib/types/address';
 	import type { ContactUi } from '$lib/types/contact';
 	import type { AvatarVariants } from '$lib/types/style';
 	import { mapAddressToContactAddressUi } from '$lib/utils/contact.utils';
+	import { replacePlaceholders } from '$lib/utils/i18n.utils';
 
 	interface Props {
 		contact?: ContactUi;
@@ -22,7 +25,7 @@
 		address?: Address;
 	}
 
-	const { contact, badge, variant, address }: Props = $props();
+	const { contact, badge, variant = 'md', address }: Props = $props();
 
 	let emptyOisyLogoSize = $derived(
 		{
@@ -31,7 +34,11 @@
 			md: '48px',
 			sm: '40px',
 			xs: '32px'
-		}[variant ?? 'md']
+		}[variant]
+	);
+
+	let mappedAddress = $derived(
+		nonNullish(address) ? mapAddressToContactAddressUi(address) : undefined
 	);
 </script>
 
@@ -48,13 +55,14 @@
 	{:else}
 		<Img
 			src={emptyOisyLogo}
+			alt={replacePlaceholders($i18n.core.alt.logo, { $name: OISY_NAME })}
 			width={emptyOisyLogoSize}
 			height={emptyOisyLogoSize}
 			testId={AVATAR_WITH_BADGE_FALLBACK_IMAGE}
 		/>
 
-		{#if nonNullish(address)}
-			<AddressesBadge addresses={[mapAddressToContactAddressUi(address)]} />
+		{#if nonNullish(mappedAddress)}
+			<AddressesBadge addresses={[mappedAddress]} />
 		{/if}
 	{/if}
 </div>
