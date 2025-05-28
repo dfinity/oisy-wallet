40c4f607f50636ead30e0dac54e9579778a08831
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-05-28 10:53:23 +0200

    improves implementation

--- playwright.config.ts
+++ playwright.config.ts
@@ -55,24 +55,24 @@ const nonAppleProjects = [
 		name: 'Google Chrome',
 		use: devices['Desktop Chrome']
 	}
-	// {
-	// 	name: 'Firefox',
-	// 	use: devices['Desktop Firefox']
-	// },
-	// {
-	// 	name: 'Pixel 7',
-	// 	use: {
-	// 		...devices['Pixel 7'],
-	// 		screen: { width: 412, height: 915 },
-	// 		viewport: { width: 412, height: 915 }
-	// 	}
-	// }
+	{
+		name: 'Firefox',
+		use: devices['Desktop Firefox']
+	},
+	{
+		name: 'Pixel 7',
+		use: {
+			...devices['Pixel 7'],
+			screen: { width: 412, height: 915 },
+			viewport: { width: 412, height: 915 }
+		}
+	}
 ];
 
 const TIMEOUT = 5 * 60 * 1000;
 
 export default defineConfig({
-	retries: 0,
+	retries: 3,
 	timeout: TIMEOUT,
 	workers: 5,
 	expect: {
