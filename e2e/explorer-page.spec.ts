446a8dd42266ff563ab8b1b9aad3e5033ec1c10f
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-05-28 14:16:24 +0200

    improves implementation

---
+++ e2e/explorer-page.spec.ts
@@ -0,0 +1,10 @@
+import { testWithII } from '@dfinity/internet-identity-playwright';
+import { ExplorerPage } from './utils/pages/explorer.page';
+
+testWithII('should display explorer page', async ({ page, iiPage, isMobile }) => {
+    const explorerPage = new ExplorerPage({ page, iiPage, isMobile });
+
+    await explorerPage.waitForReady();
+
+    await explorerPage.takeScreenshot();
+});
---
+++ e2e/homepage.spec.ts
@@ -0,0 +1,23 @@
+import { testWithII } from '@dfinity/internet-identity-playwright';
+import { test } from '@playwright/test';
+import { HomepageLoggedIn, HomepageLoggedOut } from './utils/pages/homepage.page';
+
+test('should display homepage in logged out state', async ({ page }) => {
+    const homepageLoggedOut = new HomepageLoggedOut({ page });
+
+    await homepageLoggedOut.waitForReady();
+
+    await homepageLoggedOut.takeScreenshot();
+});
+
+testWithII.beforeEach(async ({ page }) => {
+    await page.clock.install();
+});
+
+testWithII('should display homepage in logged in state', async ({ page, iiPage, isMobile }) => {
+    const homepageLoggedIn = new HomepageLoggedIn({ page, iiPage, isMobile });
+
+    await homepageLoggedIn.waitForReady();
+
+    await homepageLoggedIn.takeScreenshot({ freezeCarousel: true });
+});
