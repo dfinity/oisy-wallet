1e1d378a876b6a69fae8dcc9ea078b8472af0d4a
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-05-28 11:42:27 +0200

    feat(frontend): implements contact delete confirmation (#6934)
    # Motivation
    
    When a user wants to delete a contact, he needs to confirm that the
    contact really should be removed.
    
    # Changes
    
    - adds confirmation to delete contact flow
    
    # Tests
    
    
    
    https://github.com/user-attachments/assets/5ab062d0-e491-46c2-896b-367ed2643cc5
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/frontend/src/lib/components/address-book/DeleteContactConfirmBottomSheet.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { Backdrop, BottomSheet } from '@dfinity/gix-components';
+	import DeleteContactConfirmContent from '$lib/components/address-book/DeleteContactConfirmContent.svelte';
+	import IconClose from '$lib/components/icons/lucide/IconClose.svelte';
+	import ButtonIcon from '$lib/components/ui/ButtonIcon.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
+	import type { ContactUi } from '$lib/types/contact';
+	import { replacePlaceholders } from '$lib/utils/i18n.utils.js';
+
+	interface Props {
+		onCancel: () => void;
+		onDelete: () => void;
+		contact: ContactUi;
+	}
+
+	let { onCancel, onDelete, contact }: Props = $props();
+</script>
+
+<div class="fixed inset-0 z-50">
+	<BottomSheet transition>
+		<div class="flex w-full flex-col px-1">
+			<div class="w-full p-4">
+				<ButtonIcon
+					onclick={onCancel}
+					styleClass="text-disabled float-right"
+					ariaLabel={$i18n.core.alt.close_details}
+				>
+					{#snippet icon()}
+						<IconClose size="24" />
+					{/snippet}
+				</ButtonIcon>
+			</div>
+
+			<h3 class="mb-2 mt-4 text-center"
+				>{replacePlaceholders($i18n.contact.delete.title, {
+					$contact: contact.name
+				})}</h3
+			>
+
+			<DeleteContactConfirmContent {onCancel} {onDelete} {contact} />
+		</div>
+	</BottomSheet>
+
+	<Backdrop on:nnsClose={onCancel} />
+</div>
