e15aaa171423baa2a2e1e8f340a60b9b74279fd9
daviddecentage <david.farina@dfinity.org>
2025-05-28 14:33:22 +0200

    update

--- src/frontend/src/lib/components/address/AddressCard.svelte
+++ src/frontend/src/lib/components/address/AddressCard.svelte
@@ -5,16 +5,28 @@
 		logo?: Snippet;
 		content: Snippet;
 		actions?: Snippet;
+		variant?: 'default' | 'info';
 		hasError?: boolean;
 		items?: 'start' | 'center' | 'end';
 	}
 
-	const { logo, content, actions, hasError = false, items = 'start' }: Props = $props();
+	const {
+		logo,
+		content,
+		actions,
+		variant = 'default',
+		hasError = false,
+		items = 'start'
+	}: Props = $props();
 </script>
 
 <div
 	class={`flex rounded-lg border border-disabled bg-secondary px-2 py-3 items-${items}`}
 	class:border-error-solid={hasError}
+	class:bg-secondary={variant === 'default'}
+	class:border-disabled={variant === 'default'}
+	class:bg-brand-subtle-10={variant === 'info'}
+	class:border-brand-subtle-10={variant === 'info'}
 >
 	{@render logo?.()}
 
