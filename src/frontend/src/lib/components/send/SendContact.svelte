3168cf7eda76df8b5f5defb868884002b72462ed
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2025-05-29 14:03:30 +0200

    feat(frontend): SendContacts components

---
+++ src/frontend/src/lib/components/send/SendContact.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import AvatarWithBadge from '$lib/components/contact/AvatarWithBadge.svelte';
+	import LogoButton from '$lib/components/ui/LogoButton.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
+	import type { Address } from '$lib/types/address';
+	import type { ContactUi } from '$lib/types/contact';
+	import { shortenWithMiddleEllipsis } from '$lib/utils/format.utils';
+
+	interface Props {
+		address: Address;
+		contact: ContactUi;
+	}
+
+	let { contact, address }: Props = $props();
+
+	const dispatch = createEventDispatcher();
+</script>
+
+<LogoButton
+	styleClass="group"
+	onClick={() => {
+		dispatch('click');
+	}}
+>
+	{#snippet logo()}
+		<AvatarWithBadge {contact} badge={{ type: 'addressType', address }} variant="sm" />
+	{/snippet}
+
+	{#snippet title()}
+		{contact.name}
+	{/snippet}
+
+	{#snippet description()}
+		{shortenWithMiddleEllipsis({ text: address })}
+	{/snippet}
+
+	{#snippet descriptionEnd()}
+		<div class="hidden text-brand-primary group-hover:block">
+			{$i18n.send.text.send}
+		</div>
+	{/snippet}
+</LogoButton>
