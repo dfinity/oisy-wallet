e0cf9b66b49a3458514e41e89555ef7b3e0fc25f
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-05-30 10:29:39 +0200

    feat(frontend): display network contacts in the send flow (#7038)
    # Motivation
    
    We now can connect the recently added network contacts stores with the
    UI components.
    
    Note: the parent PR (https://github.com/dfinity/oisy-wallet/pull/7046)
    should be merged first.
    
    <img width="596" alt="Screenshot 2025-05-29 at 14 47 12"
    src="https://github.com/user-attachments/assets/d5f493d6-0c24-4cd1-bed2-1b2cc418fc00"
    />

--- src/frontend/src/tests/lib/components/send/SendDestinationTabs.spec.ts
+++ src/frontend/src/tests/lib/components/send/SendDestinationTabs.spec.ts
@@ -1,10 +1,18 @@
 import SendDestinationTabs from '$lib/components/send/SendDestinationTabs.svelte';
+import type { ContactUi } from '$lib/types/contact';
 import { shortenWithMiddleEllipsis } from '$lib/utils/format.utils';
+import { getMockContactsUi, mockContactBtcAddressUi } from '$tests/mocks/contacts.mock';
 import { knownDestinations } from '$tests/mocks/transactions.mock';
 import { render } from '@testing-library/svelte';
 
 describe('SendDestinationTabs', () => {
-	it('renders known destinations tab by default', () => {
+	const [contact] = getMockContactsUi({
+		n: 1,
+		name: 'Multiple Addresses Contact',
+		addresses: [mockContactBtcAddressUi]
+	}) as unknown as ContactUi[];
+
+	it('renders known destinations tab', () => {
 		const { getByText } = render(SendDestinationTabs, {
 			props: {
 				destination: '',
@@ -18,5 +26,21 @@ describe('SendDestinationTabs', () => {
 		});
 	});
 
-	// TODO: add test for contacts tab when it's implemented
+	it('renders contacts tab', () => {
+		const { getByText } = render(SendDestinationTabs, {
+			props: {
+				destination: '',
+				knownDestinations,
+				networkContacts: {
+					[mockContactBtcAddressUi.address]: contact
+				},
+				activeSendDestinationTab: 'contacts'
+			}
+		});
+
+		expect(
+			getByText(shortenWithMiddleEllipsis({ text: mockContactBtcAddressUi.address }))
+		).toBeInTheDocument();
+		expect(getByText(contact.name)).toBeInTheDocument();
+	});
 });
