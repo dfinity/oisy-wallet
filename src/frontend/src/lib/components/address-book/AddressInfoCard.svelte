7328d5399c89f65b95e3efc5669bd011408192fb
CvetelinaAngelova <110659037+CvetelinaAngelova@users.noreply.github.com>
2025-05-30 13:17:36 +0300

    fix: code review comments

--- src/frontend/src/lib/components/address-book/AddressInfoCard.svelte
+++ src/frontend/src/lib/components/address-book/AddressInfoCard.svelte
@@ -15,7 +15,7 @@
 
 <Value ref="address-info" element="div">
 	{#snippet label()}
-		<div class="text-md mb-3 font-bold text-primary">
+		<div class="mb-3 font-bold text-primary">
 			{$i18n.address.types[address.addressType]}
 		</div>
 	{/snippet}
