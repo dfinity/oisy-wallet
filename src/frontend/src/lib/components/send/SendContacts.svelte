52fbc26031114d03666e37f1500acf83827d74ed
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-05-30 17:05:20 +0200

    style(frontend): in effects of the content in SendDestinationTabs (#7066)
    # Motivation
    
    For a smoother UI experience, we should introduce some additional `in`
    effects in SendDestinationTabs components.

--- src/frontend/src/lib/components/send/SendContacts.svelte
+++ src/frontend/src/lib/components/send/SendContacts.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { nonNullish } from '@dfinity/utils';
 	import { createEventDispatcher } from 'svelte';
+	import { fade } from 'svelte/transition';
 	import SendContact from '$lib/components/send/SendContact.svelte';
 	import EmptyState from '$lib/components/ui/EmptyState.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
@@ -43,25 +44,27 @@
 	let filteredNetworkContactsKeys = $derived(Object.keys(filteredNetworkContacts));
 </script>
 
-{#if nonNullish(networkContacts) && filteredNetworkContactsKeys.length > 0}
-	<div class="flex flex-col overflow-y-hidden sm:max-h-[13.5rem]">
-		<ul class="list-none overflow-y-auto overscroll-contain">
-			{#each filteredNetworkContactsKeys as address, index (index)}
-				<SendContact
-					contact={networkContacts[address]}
-					{address}
-					onClick={() => {
-						selectedContact = networkContacts[address];
-						destination = address;
-						dispatch('icNext');
-					}}
-				/>
-			{/each}
-		</ul>
-	</div>
-{:else}
-	<EmptyState
-		title={$i18n.send.text.contacts_empty_state_title}
-		description={$i18n.send.text.contacts_empty_state_description}
-	/>
-{/if}
+<div in:fade>
+	{#if nonNullish(networkContacts) && filteredNetworkContactsKeys.length > 0}
+		<div in:fade class="flex flex-col overflow-y-hidden sm:max-h-[13.5rem]">
+			<ul class="list-none overflow-y-auto overscroll-contain">
+				{#each filteredNetworkContactsKeys as address, index (index)}
+					<SendContact
+						contact={networkContacts[address]}
+						{address}
+						onClick={() => {
+							selectedContact = networkContacts[address];
+							destination = address;
+							dispatch('icNext');
+						}}
+					/>
+				{/each}
+			</ul>
+		</div>
+	{:else}
+		<EmptyState
+			title={$i18n.send.text.contacts_empty_state_title}
+			description={$i18n.send.text.contacts_empty_state_description}
+		/>
+	{/if}
+</div>
