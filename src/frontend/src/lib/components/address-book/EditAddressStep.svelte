03be1d817c09ea0c1a4373d8e898eaa7eecce616
CvetelinaAngelova <110659037+CvetelinaAngelova@users.noreply.github.com>
2025-06-03 16:15:02 +0300

    feat: disable save button if Alias is not changed

--- src/frontend/src/lib/components/address-book/EditAddressStep.svelte
+++ src/frontend/src/lib/components/address-book/EditAddressStep.svelte
@@ -76,6 +76,9 @@
 	);
 
 	let isInvalid = $state(false);
+
+	let originalLabel = $derived(!isNewAddress && nonNullish(address?.label) ? address.label : '');
+	let labelChanged = $derived(isNewAddress ? true : editingAddress.label !== originalLabel);
 </script>
 
 <form onsubmit={handleSubmit} method="POST" class="flex w-full flex-col items-center">
@@ -102,7 +105,7 @@
 			<ButtonCancel {disabled} onclick={onClose} testId={ADDRESS_BOOK_CANCEL_BUTTON}></ButtonCancel>
 			<Button
 				colorStyle="primary"
-				disabled={isInvalid}
+				disabled={isInvalid || (!isNewAddress && !labelChanged)}
 				on:click={handleSave}
 				testId={ADDRESS_BOOK_SAVE_BUTTON}
 				loading={disabled}
