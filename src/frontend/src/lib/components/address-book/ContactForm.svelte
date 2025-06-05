3ee2f68d4372b4cf9dbb8c7e23c0304bf7018f41
CvetelinaAngelova <110659037+CvetelinaAngelova@users.noreply.github.com>
2025-06-05 09:54:14 +0300

    add post to a form

--- src/frontend/src/lib/components/address-book/ContactForm.svelte
+++ src/frontend/src/lib/components/address-book/ContactForm.svelte
@@ -31,7 +31,7 @@
 
 <svelte:window on:keydown={handleKeydown} />
 
-<form class="w-full">
+<form class="w-full" method="POST">
 	<div class="rounded-lg bg-brand-subtle-10 p-4 pb-6 pt-4 text-sm md:p-6 md:text-base md:font-bold">
 		{$i18n.contact.fields.name}
 		<InputText
