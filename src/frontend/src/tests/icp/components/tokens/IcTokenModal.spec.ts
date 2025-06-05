34bbe0d9f6b02e07cb9b4da3f593195a510dbf4f
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-06-05 16:57:51 +0200

    feat(frontend): IcTokenModal update (#7158)
    # Motivation
    
    We now can update the design on IcTokenModal, as well as to switch from
    token to pageToken.
    
    <img width="592" alt="Screenshot 2025-06-05 at 11 48 29"
    src="https://github.com/user-attachments/assets/59374efb-a5b3-4aae-9f27-a703139edb1f"
    />

---
+++ src/frontend/src/tests/icp/components/tokens/IcTokenModal.spec.ts
@@ -0,0 +1,48 @@
+import { BTC_MAINNET_TOKEN } from '$env/tokens/tokens.btc.env';
+import IcTokenModal from '$icp/components/tokens/IcTokenModal.svelte';
+import { enabledIcrcTokens } from '$icp/derived/icrc.derived';
+import type { IcCkToken } from '$icp/types/ic-token';
+import { modalStore } from '$lib/stores/modal.store';
+import { mockValidIcCkToken } from '$tests/mocks/ic-tokens.mock';
+import { mockPage } from '$tests/mocks/page.store.mock';
+import { render } from '@testing-library/svelte';
+
+describe('IcTokenModal', () => {
+	const mockIcCkToken = {
+		...mockValidIcCkToken,
+		name: 'ckBTC',
+		symbol: BTC_MAINNET_TOKEN.twinTokenSymbol,
+		ledgerCanisterId: 'random id',
+		indexCanisterId: 'random id',
+		twinToken: BTC_MAINNET_TOKEN
+	} as IcCkToken;
+
+	beforeEach(() => {
+		mockPage.reset();
+	});
+
+	it('displays all required values', () => {
+		vi.spyOn(enabledIcrcTokens, 'subscribe').mockImplementation((fn) => {
+			fn([mockIcCkToken]);
+			return () => {};
+		});
+
+		mockPage.mock({
+			token: mockIcCkToken.name,
+			network: mockIcCkToken.network.id.description
+		});
+
+		const { container } = render(IcTokenModal);
+
+		modalStore.openBtcToken(mockIcCkToken.id);
+
+		expect(container).toHaveTextContent(mockIcCkToken.network.name);
+		expect(container).toHaveTextContent(mockIcCkToken.name);
+		expect(container).toHaveTextContent(mockIcCkToken.symbol);
+		expect(container).toHaveTextContent(mockIcCkToken.twinToken?.name as string);
+		expect(container).toHaveTextContent(mockIcCkToken.ledgerCanisterId);
+		expect(container).toHaveTextContent(mockIcCkToken.indexCanisterId as string);
+		expect(container).toHaveTextContent(mockIcCkToken.minterCanisterId as string);
+		expect(container).toHaveTextContent(`${mockIcCkToken.decimals}`);
+	});
+});
