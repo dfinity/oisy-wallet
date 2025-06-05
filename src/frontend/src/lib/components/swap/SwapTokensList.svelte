aef903a5d37d7b77dd9a2bf055eadf224dd5e80d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-06-05 14:22:30 +0200

    feat(frontend): Remove event dispatcher from `ModalTokensListItem` component (#7153)
    # Motivation
    
    In Svelte 5 event dispatchers are deprecated. So we can remove it from
    `ModalTokensListItem`.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/swap/SwapTokensList.svelte
+++ src/frontend/src/lib/components/swap/SwapTokensList.svelte
@@ -51,7 +51,7 @@
 	on:icTokenButtonClick={onIcTokenButtonClick}
 >
 	{#snippet tokenListItem(token, onClick)}
-		<ModalTokensListItem {token} on:click={onClick} />
+		<ModalTokensListItem {token} {onClick} />
 	{/snippet}
 	{#snippet noResults()}
 		<p class="text-primary">
