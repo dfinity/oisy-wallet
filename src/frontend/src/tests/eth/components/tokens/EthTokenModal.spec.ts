abccdda5a5079dbf91f45fefa590bcd6c2d951dc
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-06-06 09:26:23 +0200

    feat(frontend): EthTokenModal update (#7171)
    # Motivation
    
    We now can update the design on EthTokenModal, as well as to switch from
    token to pageToken. Also, the old `Token` component can now be deleted.
    
    <img width="594" alt="Screenshot 2025-06-06 at 09 10 30"
    src="https://github.com/user-attachments/assets/cc2aecd0-9c49-457b-8b76-9fbe179ccb82"
    />

---
+++ src/frontend/src/tests/eth/components/tokens/EthTokenModal.spec.ts
@@ -0,0 +1,38 @@
+import EthTokenModal from '$eth/components/tokens/EthTokenModal.svelte';
+import { enabledErc20Tokens } from '$eth/derived/erc20.derived';
+import { modalStore } from '$lib/stores/modal.store';
+import { shortenWithMiddleEllipsis } from '$lib/utils/format.utils';
+import { mockValidErc20Token } from '$tests/mocks/erc20-tokens.mock';
+import { mockPage } from '$tests/mocks/page.store.mock';
+import { mockSplCustomToken } from '$tests/mocks/spl-tokens.mock';
+import { render } from '@testing-library/svelte';
+
+describe('EthTokenModal', () => {
+	beforeEach(() => {
+		mockPage.reset();
+	});
+
+	it('displays all required values', () => {
+		vi.spyOn(enabledErc20Tokens, 'subscribe').mockImplementation((fn) => {
+			fn([{ ...mockValidErc20Token, enabled: true }]);
+			return () => {};
+		});
+
+		mockPage.mock({
+			token: mockValidErc20Token.name,
+			network: mockValidErc20Token.network.id.description
+		});
+
+		const { container } = render(EthTokenModal);
+
+		modalStore.openBtcToken(mockValidErc20Token.id);
+
+		expect(container).toHaveTextContent(mockValidErc20Token.network.name);
+		expect(container).toHaveTextContent(mockValidErc20Token.name);
+		expect(container).toHaveTextContent(mockValidErc20Token.symbol);
+		expect(container).toHaveTextContent(
+			shortenWithMiddleEllipsis({ text: mockValidErc20Token.address })
+		);
+		expect(container).toHaveTextContent(`${mockSplCustomToken.decimals}`);
+	});
+});
