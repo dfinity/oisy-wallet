83724b1fdada8dc312cd39f37a935fa1a76e7fd0
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-06-10 13:32:24 +0200

    feat(frontend): tracks the click 'learn more' button event on the rewards page (#7211)
    # Motivation
    
    We want to track how ofter the `learn more` button on the `Rewards` page
    is clicked.
    
    # Changes
    
    - tracks clicks on `learn more`
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/rewards/Rewards.svelte
+++ src/frontend/src/lib/components/rewards/Rewards.svelte
@@ -2,6 +2,7 @@
 	import AllRewardsList from '$lib/components/rewards/AllRewardsList.svelte';
 	import ExternalLink from '$lib/components/ui/ExternalLink.svelte';
 	import PageTitle from '$lib/components/ui/PageTitle.svelte';
+	import { TRACK_REWARD_LEARN_MORE } from '$lib/constants/analytics.contants';
 	import { OISY_REWARDS_URL } from '$lib/constants/oisy.constants';
 	import { i18n } from '$lib/stores/i18n.store';
 </script>
@@ -15,6 +16,7 @@
 		iconVisible={false}
 		color="blue"
 		styleClass="ml-auto font-semibold"
+		trackEvent={{ name: TRACK_REWARD_LEARN_MORE }}
 	>
 		{$i18n.rewards.text.learn_more}
 	</ExternalLink>
