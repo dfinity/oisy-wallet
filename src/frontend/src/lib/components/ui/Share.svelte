04177535f2b101ca9589328f23c33ec2a06c2d06
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-06-10 13:37:06 +0200

    feat(frontend): tracks some reward campaign win events (#7204)
    # Motivation
    
    We want to track how often win modals are displayed to the user and how
    often the users are sharing those.
    
    # Changes
    
    tracks events for
    - displaying win modal
    - click on `twitter` link
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/ui/Share.svelte
+++ src/frontend/src/lib/components/ui/Share.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
 	import IconShare from '$lib/components/icons/lucide/IconShare.svelte';
 	import ExternalLink from '$lib/components/ui/ExternalLink.svelte';
+	import type { TrackEventParams } from '$lib/types/analytics';
 
 	interface Props {
 		text: string;
 		href: string;
 		styleClass?: string;
 		testId?: string;
+		trackEvent?: TrackEventParams;
 	}
 
-	let { text, href, styleClass, testId }: Props = $props();
+	let { text, href, styleClass, testId, trackEvent }: Props = $props();
 </script>
 
 <ExternalLink
 	{href}
 	{testId}
+	{trackEvent}
 	ariaLabel={text}
 	iconVisible={false}
 	asButton
