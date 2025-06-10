ed92742c0d3defa98e45ab0d49f8911f542885ba
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-06-10 10:26:18 +0200

    refactor(frontend): Specify network type for ERC20 tokens (#6755)
    # Motivation
    
    It make sense to specify the type of Ethereum network for all ERC20
    tokens.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/eth/types/erc20.ts
+++ src/frontend/src/eth/types/erc20.ts
@@ -1,9 +1,10 @@
 import type { ContractAddress } from '$eth/types/address';
+import type { EthereumNetwork } from '$eth/types/network';
 import type { Exchange } from '$lib/types/exchange';
 import type { RequiredToken, Token, TokenLinkedData, TokenMetadata } from '$lib/types/token';
 import type { Option } from '$lib/types/utils';
 
-export type Erc20Token = Erc20Contract & Token;
+export type Erc20Token = Erc20Contract & Omit<Token, 'network'> & { network: EthereumNetwork };
 
 export type RequiredErc20Token = RequiredToken<Erc20Token>;
 export type RequiredAdditionalErc20Token = Omit<RequiredErc20Token, keyof TokenLinkedData>;
