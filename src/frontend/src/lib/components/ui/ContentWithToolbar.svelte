8e3160ef7c8365d9cf6bb83136224d76c7f7ed4a
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-06-10 09:28:57 +0200

    resolves review feedback

--- src/frontend/src/lib/components/ui/ContentWithToolbar.svelte
+++ src/frontend/src/lib/components/ui/ContentWithToolbar.svelte
@@ -17,7 +17,6 @@
 </div>
 
 {@render outerContent?.()}
-<!-- <slot name="outer-content" /> -->
 
 <div
 	class="sticky bottom-0 left-0 w-full border-t border-secondary bg-surface px-3 pb-4 pt-4 md:px-6 md:pb-6 md:pt-5"
