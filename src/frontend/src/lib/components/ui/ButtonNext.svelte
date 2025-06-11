f6e5243e495a3788dcb0b2d83737268d13304272
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-06-11 11:03:56 +0200

    refactor(frontend): upgrades ButtonNext to svelte 5 (#7233)
    # Motivation
    
    Since we upgraded svelte from V4 to V5 we also want to upgrade the
    components step by step to V5.
    
    # Changes
    
    - upgrades `ButtonNext`
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/ui/ButtonNext.svelte
+++ src/frontend/src/lib/components/ui/ButtonNext.svelte
@@ -2,8 +2,12 @@
 	import Button from '$lib/components/ui/Button.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 
-	export let disabled = false;
-	export let testId: string | undefined = undefined;
+	interface Props {
+		disabled?: boolean;
+		testId?: string;
+	}
+
+	let { disabled = false, testId }: Props = $props();
 </script>
 
 <Button {disabled} {testId}>
