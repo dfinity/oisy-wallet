c4b3829b70e121cd8b9604ce4325725218e0e088
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-06-11 15:02:55 +0200

    refactor(frontend): Simplify component `SendContacts` (#7242)
    # Motivation
    
    We will start selecting the values of a contacts record by
    case-sensitive keys. That means to use a specific utils. However, before
    that, we simplify a little the possible instances where it could be
    used.
    
    For example in component `SendContacts`, we can use directly list
    `filteredNetworkContacts`, instead of iterating in it.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/send/SendContacts.svelte
+++ src/frontend/src/lib/components/send/SendContacts.svelte
@@ -42,20 +42,18 @@
 					)
 			: {}
 	);
-
-	let filteredNetworkContactsKeys = $derived(Object.keys(filteredNetworkContacts));
 </script>
 
 <div in:fade>
-	{#if nonNullish(networkContacts) && filteredNetworkContactsKeys.length > 0}
+	{#if nonNullish(networkContacts) && Object.keys(filteredNetworkContacts).length > 0}
 		<div in:fade class="flex flex-col overflow-y-hidden sm:max-h-[13.5rem]">
 			<ul class="list-none overflow-y-auto overscroll-contain">
-				{#each filteredNetworkContactsKeys as address, index (index)}
+				{#each Object.entries(filteredNetworkContacts) as [address, contact], index (index)}
 					<SendContact
-						contact={networkContacts[address]}
+						{contact}
 						{address}
 						onClick={() => {
-							selectedContact = networkContacts[address];
+							selectedContact = contact;
 							destination = address;
 							dispatch('icNext');
 						}}
