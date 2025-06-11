a0e51f5278441291fa208a445863efa9ee422130
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-06-11 09:53:48 +0200

    feat(frontend): Util to get case-safe known destinations (#7225)
    # Motivation
    
    Some networks are case-sensitive (for example Solana). So, when we lit
    the known destinations, we should use the network/address property to
    verify that we respect the case-sensitiveness.
    
    # Changes
    
    - Create generic util to get a value from a record depending on
    network's case-sensitiveness.
    - Create wrapper-utils for known destinations and for network contacts.
    - Use utils in component to send tokens.
    
    # Tests
    
    Added tests and a practical example below:
    
    ![Screenshot 2025-06-11 at 09 04
    56](https://github.com/user-attachments/assets/7e93a707-33be-423a-8f81-15ded4105ebc)
    ![Screenshot 2025-06-11 at 09 05
    02](https://github.com/user-attachments/assets/c4ee1dd0-d681-48b7-8ca0-98467877d089)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/utils/contacts.utils.ts
+++ src/frontend/src/lib/utils/contacts.utils.ts
@@ -1,6 +1,9 @@
+import type { Address } from '$lib/types/address';
 import type { ContactUi } from '$lib/types/contact';
 import type { NetworkContacts } from '$lib/types/contacts';
+import type { NetworkId } from '$lib/types/network';
 import type { TokenAccountIdTypes } from '$lib/types/token-account-id';
+import { getRecordValueByCaseSensitivity } from '$lib/utils/record.utils';
 import { isNullish } from '@dfinity/utils';
 
 export const getNetworkContacts = ({
@@ -21,3 +24,18 @@ export const getNetworkContacts = ({
 
 				return acc;
 			}, {});
+
+export const getNetworkContact = ({
+	networkContacts,
+	address,
+	networkId
+}: {
+	networkContacts: NetworkContacts;
+	address: Address;
+	networkId: NetworkId | undefined;
+}): ContactUi | undefined =>
+	getRecordValueByCaseSensitivity({
+		record: networkContacts,
+		address,
+		networkId
+	});
