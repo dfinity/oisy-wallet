5fc195b1b1f33f545b6cec4de2d64b06c0df1313
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-06-12 11:05:04 +0200

    feat(frontend): Use network case-sensitiveness for send flow alias (#7264)
    # Motivation
    
    We need to match the alias in the send flow based on network
    case-sensitiveness.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/send/SendContactName.svelte
+++ src/frontend/src/lib/components/send/SendContactName.svelte
@@ -4,6 +4,7 @@
 	import Divider from '$lib/components/common/Divider.svelte';
 	import type { Address } from '$lib/types/address';
 	import type { ContactUi } from '$lib/types/contact';
+	import { areAddressesEqual } from '$lib/utils/address.utils';
 
 	interface Props {
 		address: Address;
@@ -14,7 +15,13 @@
 	let { contact, address, children }: Props = $props();
 
 	let contactLabel = $derived(
-		contact?.addresses.find(({ address: innerAddress }) => address === innerAddress)?.label
+		contact?.addresses.find(({ address: innerAddress, addressType }) =>
+			areAddressesEqual({
+				address1: innerAddress,
+				address2: address,
+				addressType
+			})
+		)?.label
 	);
 </script>
 
