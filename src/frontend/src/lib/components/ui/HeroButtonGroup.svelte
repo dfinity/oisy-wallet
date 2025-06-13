c5ee0663732709fb8e9f504276cdf36300ff2338
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-06-13 14:55:27 +0200

    refactor(frontend): upgrades HeroButtonGroup to svelte 5 (#7293)
    # Motivation
    
    Since we upgraded svelte from V4 to V5 we also want to upgrade the
    components step by step to V5.
    
    
    # Changes
    
    - upgrades `HeroButtonGroup`
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/ui/HeroButtonGroup.svelte
+++ src/frontend/src/lib/components/ui/HeroButtonGroup.svelte
@@ -1,3 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		children: Snippet;
+	}
+
+	let { children }: Props = $props();
+</script>
+
 <div role="toolbar" class="flex flex-1 flex-row justify-between gap-2">
-	<slot />
+	{@render children()}
 </div>
