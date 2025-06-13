3b7213a77e891b42631e725d2751e6a88b49987f
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-06-13 11:50:02 +0200

    refactor(frontend): upgrades ExternalLinkIcon to svelte 5 (#7241)
    # Motivation
    
    Since we upgraded svelte from V4 to V5 we also want to upgrade the
    components step by step to V5.
    
    
    # Changes
    
    - upgrades `ExternalLinkIcon`
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/ui/ExternalLinkIcon.svelte
+++ src/frontend/src/lib/components/ui/ExternalLinkIcon.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
+	import type { Snippet } from 'svelte';
 	import type { ButtonColorStyle } from '$lib/types/style';
 
-	export let href: string;
-	export let ariaLabel: string;
-	export let colorStyle: ButtonColorStyle = 'tertiary';
+	interface Props {
+		children: Snippet;
+		onClick?: () => void;
+		href: string;
+		ariaLabel: string;
+		colorStyle?: ButtonColorStyle;
+	}
+
+	let { children, onClick, href, ariaLabel, colorStyle = 'tertiary' }: Props = $props();
 </script>
 
 <a
@@ -11,8 +18,8 @@
 	rel="external noopener noreferrer"
 	target="_blank"
 	class={`as-button ${colorStyle} icon flex flex-col text-center text-xs font-normal`}
-	on:click
+	onclick={onClick}
 	aria-label={ariaLabel}
 >
-	<slot />
+	{@render children()}
 </a>
