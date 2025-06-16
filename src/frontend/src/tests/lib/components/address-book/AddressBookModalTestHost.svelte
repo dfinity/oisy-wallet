ab86ea01dafddc6bc46c70393ed80d86452e94b5
daviddecentage <david.farina@dfinity.org>
2025-06-16 14:20:57 +0200

    update

---
+++ src/frontend/src/tests/lib/components/address-book/AddressBookModalTestHost.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { modalStore } from '$lib/stores/modal.store';
+	import AddressBookModal from '$lib/components/address-book/AddressBookModal.svelte';
+
+	let modalOpen = $derived($modalStore?.type === 'address-book');
+</script>
+
+{#if modalOpen}
+	<AddressBookModal />
+{/if}
