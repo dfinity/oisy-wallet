7ebae6ff4fd295c1e184be9722c91c2b5b48119c
CvetelinaAngelova <110659037+CvetelinaAngelova@users.noreply.github.com>
2025-06-17 12:57:55 +0300

    fix : add principle for ICP token

--- src/frontend/src/icp/derived/ic-contacts.derived.ts
+++ src/frontend/src/icp/derived/ic-contacts.derived.ts
@@ -7,29 +7,54 @@ import { tokenWithFallback } from '$lib/derived/token.derived';
 import type { NetworkContacts } from '$lib/types/contacts';
 import { isIcpAccountIdentifier } from '$lib/utils/account.utils';
 import { getNetworkContacts } from '$lib/utils/contacts.utils';
+import { Principal } from '@dfinity/principal';
 import { derived, type Readable } from 'svelte/store';
+
+const isPrincipalAddress = (address: string): boolean | undefined => {
+	try {
+		const parsed = parseIcrcv2AccountId(address);
+
+		return (
+			parsed &&
+			'WithPrincipal' in parsed &&
+			Array.isArray(parsed.WithPrincipal.subaccount) &&
+			parsed.WithPrincipal.subaccount.length === 0 &&
+			parsed.WithPrincipal.owner instanceof Principal
+		);
+	} catch {
+		return false;
+	}
+};
 
 export const icNetworkContacts: Readable<NetworkContacts> = derived(
 	[contacts, tokenWithFallback],
-	([$contacts, $tokenWithFallback]) => {
-		const isIcpToken = $tokenWithFallback.id === ICP_TOKEN_ID;
-		const isIcrcToken = isTokenIcrc({ standard: $tokenWithFallback.standard });
+	([$contacts, $token]) => {
+		const isIcp = $token.id === ICP_TOKEN_ID;
+		const isIcrc = isTokenIcrc({ standard: $token.standard });
 
 		const allIcNetworkContacts = getNetworkContacts({
 			addressType: 'Icrcv2',
 			contacts: $contacts
 		});
 
-		return Object.keys(allIcNetworkContacts).reduce<NetworkContacts>(
-			(acc, address) => ({
-				...acc,
-				...((isIcpToken && isIcpAccountIdentifier(address)) ||
-				(isIcrcToken && isIcrcAddress(address)) ||
-				parseIcrcv2AccountId(address) !== undefined
-					? { [address]: allIcNetworkContacts[address] }
-					: {})
-			}),
-			{}
-		);
+		const result: NetworkContacts = {};
+
+		for (const [address, contact] of Object.entries(allIcNetworkContacts)) {
+			let isValid = false;
+
+			if (isIcpAccountIdentifier(address) && isIcp) {
+				isValid = true;
+			} else if (isIcrcAddress(address) && isIcrc) {
+				isValid = true;
+			} else if (isPrincipalAddress(address)) {
+				isValid = true;
+			}
+
+			if (isValid) {
+				result[address] = contact;
+			}
+		}
+
+		return result;
 	}
 );
