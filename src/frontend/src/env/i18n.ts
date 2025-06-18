968a5d95e6f0c341e1183754ea75ce3b28a12565
daviddecentage <david.farina@dfinity.org>
2025-06-18 14:07:07 +0200

    update

--- src/frontend/src/env/i18n.ts
+++ src/frontend/src/env/i18n.ts
@@ -1,5 +1,8 @@
 import { LOCAL, STAGING } from '$lib/constants/app.constants';
+import { Languages } from '$lib/types/languages';
 import { parseBoolEnvVar } from '$lib/utils/env.utils';
+
+export const SUPPORTED_LANGUAGES = Object.entries(Languages);
 
 // Enabled on Staging and Local if not set
 // Todo: remove once the feature has been completed
