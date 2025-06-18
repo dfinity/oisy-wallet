37f53ee0b06761e90661276214f79563252f8afa
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-06-18 15:11:47 +0200

    refactor(frontend): Language names (#7377)
    # Motivation
    
    We refactor the language names. We dont need to translate them so we
    hardcode the labels in there own language.
    
    # Changes
    
    Removed translations
    Added map for lookin up the language name
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/types/i18n.d.ts
+++ src/frontend/src/lib/types/i18n.d.ts
@@ -2,11 +2,6 @@
  * Auto-generated definitions file ("npm run i18n")
  */
 
-interface I18nLanguages {
-	en: string;
-	de: string;
-}
-
 interface I18nCore {
 	text: {
 		cancel: string;
@@ -1092,7 +1087,6 @@ interface I18nWelcome {
 
 interface I18n {
 	lang: Languages;
-	languages: I18nLanguages;
 	core: I18nCore;
 	navigation: I18nNavigation;
 	auth: I18nAuth;
