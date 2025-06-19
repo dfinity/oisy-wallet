9edf2182abd9297149b149219a0f271afba3c8cc
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-06-19 14:03:50 +0200

    refactor(frontend): Env var i18n (#7396)
    # Motivation
    
    With parseBoolEnvVar we cannot provide a default value other than false
    so we use JSON.parse again.

--- src/frontend/src/env/i18n.ts
+++ src/frontend/src/env/i18n.ts
@@ -1,6 +1,5 @@
 import { LOCAL, STAGING } from '$lib/constants/app.constants';
 import { Languages } from '$lib/types/languages';
-import { parseBoolEnvVar } from '$lib/utils/env.utils';
 
 export const SUPPORTED_LANGUAGES = Object.entries(Languages);
 
@@ -12,5 +11,4 @@ export const LANGUAGES = {
 
 // Enabled on Staging and Local if not set
 // Todo: remove once the feature has been completed
-export const I18N_ENABLED =
-	parseBoolEnvVar(import.meta.env.VITE_I18N_ENABLED) ?? (STAGING || LOCAL);
+export const I18N_ENABLED = JSON.parse(import.meta.env.VITE_I18N_ENABLED ?? (STAGING || LOCAL));
