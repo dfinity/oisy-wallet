eeedb7cff20b1b7475e76072e5abbd036db5510f
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-06-19 20:00:48 +0200

    feat(frontend): allKnownIcrcTokensLedgerCanisterIds derived (#7385)
    # Motivation
    
    We need to detect whether an ICRC token is known to OISY to decide
    whether the "delete" button should be shown or not. The existing stores
    include customly added by user tokens, therefore we cannot rely on them.
    Instead, we implement a new derived which is a combination of all
    default + all env ICRC tokens.

--- src/frontend/src/icp/derived/icrc.derived.ts
+++ src/frontend/src/icp/derived/icrc.derived.ts
@@ -2,6 +2,8 @@ import {
 	ICRC_CHAIN_FUSION_DEFAULT_LEDGER_CANISTER_IDS,
 	ICRC_CK_TOKENS_LEDGER_CANISTER_IDS
 } from '$env/networks/networks.icrc.env';
+import { buildDip20Tokens } from '$icp/services/dip20-tokens.services';
+import { buildIcrcCustomTokens } from '$icp/services/icrc-custom-tokens.services';
 import { icrcCustomTokensStore } from '$icp/stores/icrc-custom-tokens.store';
 import { icrcDefaultTokensStore } from '$icp/stores/icrc-default-tokens.store';
 import type { LedgerCanisterIdText } from '$icp/types/canister';
@@ -13,6 +15,7 @@ import { sortIcTokens } from '$icp/utils/icrc.utils';
 import { testnetsEnabled } from '$lib/derived/testnets.derived';
 import type { CanisterIdText } from '$lib/types/canister';
 import { mapDefaultTokenToToggleable } from '$lib/utils/token.utils';
+import { parseTokenId } from '$lib/validation/token.validation';
 import { nonNullish } from '@dfinity/utils';
 import { derived, type Readable } from 'svelte/store';
 
@@ -145,4 +148,22 @@ export const enabledIcrcLedgerCanisterIdsNoCk: Readable<LedgerCanisterIdText[]> 
 			])
 		).values()
 	]
+);
+
+/**
+ * The list of all known to OISY ICRC token ledger ids.
+ * It is used to determine whether a token can be deleted or not.
+ */
+export const allKnownIcrcTokensLedgerCanisterIds: Readable<LedgerCanisterIdText[]> = derived(
+	[icrcDefaultTokens],
+	([$icrcDefaultTokens]) => {
+		const tokens = [...buildIcrcCustomTokens(), ...buildDip20Tokens()];
+		const icrcEnvTokens: IcTokenToggleable[] =
+			tokens?.map((token) => ({ ...token, id: parseTokenId(token.symbol), enabled: false })) ?? [];
+
+		return [
+			...$icrcDefaultTokens.map(({ ledgerCanisterId }) => ledgerCanisterId),
+			...icrcEnvTokens.map(({ ledgerCanisterId }) => ledgerCanisterId)
+		];
+	}
 );
