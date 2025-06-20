6d93fc9da6b23426537aa92521762a86dd4b2c36
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-06-20 09:34:41 +0200

    refactor(frontend): Resolve text (#7403)
    # Motivation
    
    For the upcoming task of making all Dapps and Rewards texts
    multilanguage, we are going to use the `resolveText` function to make a
    lookup for the translated texts.
    
    In order to make our lifes easier in the future, we refactor the
    function to require a string path, not optional as before. The only
    place this function gets called (IcTransactionLabel) can do the nullish
    check there.
    
    # Changes
    
    - Refactored resolveText to require path string
    - Adjusted usage of this function
    
    # Tests
    
    Functionally no difference
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/utils/i18n.utils.ts
+++ src/frontend/src/lib/utils/i18n.utils.ts
@@ -47,18 +47,7 @@ interface MaybeI18n extends I18n {
 	[key: string]: any;
 }
 
-export const resolveText = ({
-	i18n,
-	path
-}: {
-	i18n: MaybeI18n;
-	path: string | undefined;
-}): string | undefined => {
-	// For simplicity reason, we defer this checks within that function that way we can keep our components concise and use optional chaining within those.
-	if (isNullish(path)) {
-		return undefined;
-	}
-
+export const resolveText = ({ i18n, path }: { i18n: MaybeI18n; path: string }): string => {
 	const text = path.split('.').reduce((prev, current) => prev?.[current], i18n);
 	return nonNullish(text) && typeof text !== 'object' ? text : path;
 };
