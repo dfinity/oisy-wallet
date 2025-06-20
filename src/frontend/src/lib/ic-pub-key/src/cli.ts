e332ddf95ae564390912d1b41ac6652a799ff3b4
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-06-20 14:26:33 +0200

    ignore ic-pub-key library

--- src/frontend/src/lib/ic-pub-key/src/cli.ts
+++ src/frontend/src/lib/ic-pub-key/src/cli.ts
@@ -9,6 +9,7 @@ import {
 	PublicKeyWithChainCode as Secp256k1PublicKeyWithChainCode
 } from './ecdsa/secp256k1.js';
 
+/* istanbul ignore next */
 export const deriveEthAddress = async (user: string): Promise<string> => {
 	const pubkey = '0259761672ec7ee3bdc5eca95ba5f6a493d1133b86a76163b68af30c06fe3b75c0';
 	const chaincode = 'f666a98c7f70fe281ca8142f14eb4d1e0934a439237da83869e2cfd924b270c0';
