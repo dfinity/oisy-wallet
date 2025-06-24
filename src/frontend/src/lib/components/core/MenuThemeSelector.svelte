79256f01ef3b1c2b6469c1b4a34fe84d1a1b792b
daviddecentage <david.farina@dfinity.org>
2025-06-24 14:17:48 +0200

    update

--- src/frontend/src/lib/components/core/MenuThemeSelector.svelte
+++ src/frontend/src/lib/components/core/MenuThemeSelector.svelte
@@ -42,8 +42,8 @@
 	});
 </script>
 
-<div class="flex flex-row justify-between pl-3">
-	<span class="mr-3 flex min-w-32 flex-row items-center text-tertiary">
+<div class="flex justify-between pl-3">
+	<span class="mr-3 flex min-w-32 items-center text-tertiary">
 		<span class="mr-2 flex"><IconSunMoon /></span>
 		<span class="flex">{$i18n.settings.text.appearance}</span>
 	</span>
