f0fc68482af2f1564b22256a2656928214fbb4d9
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-06-24 15:21:37 +0200

    refactor(frontend): Theme selector (#7483)
    # Motivation
    
    We add the new MenuThemeSelector to the user menu.
    
    # Changes
    
    - Added the new MenuThemeSelector to the user menu (when logged in -
    landing page stays the same)
    - Removed ThemeSwitcherButton (when logged in - landing page stays the
    same)
    - Added min widths to MenuLanguageSelector to guarantee the with of the
    popup is wide and looking good in logged out state (when theres no theme
    selector in user menu)
    - Adjusted spacings and max width of user menu
    
    # Tests
    
    Landing page (logged out):
    
    ![image](https://github.com/user-attachments/assets/6a68494a-ee08-40b5-92df-4776e37ece44)
    
    still with switcher:
    
    ![image](https://github.com/user-attachments/assets/d6ea25c1-9188-4bf0-a5e0-3b6c7dac009b)
    
    
    Logged in:
    
    ![image](https://github.com/user-attachments/assets/46696353-eb0f-4be1-a835-9298bfdf493e)
    
    no more Switcher button:
    
    ![image](https://github.com/user-attachments/assets/ec6c4e03-4e27-40f5-8f86-49fe51cb7cab)
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/core/MenuLanguageSelector.svelte
+++ src/frontend/src/lib/components/core/MenuLanguageSelector.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="flex justify-between pl-3">
-	<span class="mr-3 flex items-center text-tertiary">
+	<span class="mr-3 flex min-w-32 items-center text-tertiary">
 		<span class="mr-1 flex"><IconLanguage /></span>
 		<span class="flex">{$i18n.core.text.language}</span>
 	</span>
