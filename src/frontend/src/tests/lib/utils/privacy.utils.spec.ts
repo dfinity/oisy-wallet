b451522e79b2c017e197f3f5610f13f644b5032a
CvetelinaAngelova <110659037+CvetelinaAngelova@users.noreply.github.com>
2025-06-25 14:50:17 +0300

    feat(frontend): add privacy mode notification (#7495)
    # Motivation
     
    Centralizing the toast logic for toggling Privacy Mode into
    privacy.utils.ts. Previously, the toast was shown directly in the Menu
    component. Now, both the Menu and Shortcut components can trigger
    Privacy Mode with toast feedback via a single utility function.
    
    # Changes
    Updated privacy.utils.ts - added optional withToast parameter to
    setPrivacyMode.
    Replaced toastsShow logic with setPrivacyMode util function.
    Added toast support when triggered by keyboard shortcut.
    Update the duration from 2 to 7 seconds
    
    # Tests
    
    Manually tested:
    Toggling Privacy Mode from the menu shows correct toast message.
    Using the keyboard shortcut toggles Privacy Mode and shows toast.
    Verified localized toast message displays as expected.
    ![Uploading Screenshot 2025-06-24 at 19.10.26.png…]()
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

---
+++ src/frontend/src/tests/lib/utils/privacy.utils.spec.ts
@@ -0,0 +1,58 @@
+import { privacyModeStore } from '$lib/stores/settings.store';
+import * as toastsStore from '$lib/stores/toasts.store';
+import { setPrivacyMode } from '$lib/utils/privacy.utils';
+import en from '$tests/mocks/i18n.mock';
+import { vi, type MockInstance } from 'vitest';
+
+describe('setPrivacyMode', () => {
+	let spyToastsShow: MockInstance;
+
+	beforeEach(() => {
+		vi.resetAllMocks();
+		spyToastsShow = vi.spyOn(toastsStore, 'toastsShow');
+	});
+
+	it('should enable privacy mode without toast', () => {
+		const spyStoreSet = vi.spyOn(privacyModeStore, 'set');
+
+		setPrivacyMode({ enabled: true });
+
+		expect(spyStoreSet).toHaveBeenCalledWith({
+			key: 'privacy-mode',
+			value: { enabled: true }
+		});
+		expect(spyToastsShow).not.toHaveBeenCalled();
+	});
+
+	it('should disable privacy mode without toast', () => {
+		const spyStoreSet = vi.spyOn(privacyModeStore, 'set');
+
+		setPrivacyMode({ enabled: false });
+
+		expect(spyStoreSet).toHaveBeenCalledWith({
+			key: 'privacy-mode',
+			value: { enabled: false }
+		});
+		expect(spyToastsShow).not.toHaveBeenCalled();
+	});
+
+	it('should enable privacy mode and show toast', () => {
+		setPrivacyMode({ enabled: true, withToast: true });
+
+		expect(spyToastsShow).toHaveBeenCalledWith({
+			text: en.navigation.text.privacy_mode_enabled,
+			level: 'info',
+			duration: 7000
+		});
+	});
+
+	it('should disable privacy mode and show toast', () => {
+		setPrivacyMode({ enabled: false, withToast: true });
+
+		expect(spyToastsShow).toHaveBeenCalledWith({
+			text: en.navigation.text.privacy_mode_disabled,
+			level: 'info',
+			duration: 7000
+		});
+	});
+});
