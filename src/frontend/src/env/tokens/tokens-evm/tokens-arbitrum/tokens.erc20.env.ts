55a0b97e1b85b0bcb909a18325f762a398a152ad
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-06-25 13:13:39 +0200

    feat(frontend): added weth token group

--- src/frontend/src/env/tokens/tokens-evm/tokens-arbitrum/tokens.erc20.env.ts
+++ src/frontend/src/env/tokens/tokens-evm/tokens-arbitrum/tokens.erc20.env.ts
@@ -1,13 +1,14 @@
 import { ARBITRUM_MAINNET_ENABLED } from '$env/networks/networks-evm/networks.evm.arbitrum.env';
 import { ARB_TOKEN } from '$env/tokens/tokens-evm/tokens-arbitrum/tokens-erc20/tokens.arb.env';
+import { BONK_TOKEN } from '$env/tokens/tokens-evm/tokens-arbitrum/tokens-erc20/tokens.bonk.env';
 import {
 	ARB_SEPOLIA_USDC_TOKEN,
 	USDC_TOKEN
 } from '$env/tokens/tokens-evm/tokens-arbitrum/tokens-erc20/tokens.usdc.env';
 import { USDT_TOKEN } from '$env/tokens/tokens-evm/tokens-arbitrum/tokens-erc20/tokens.usdt.env';
+import { WETH_TOKEN } from '$env/tokens/tokens-evm/tokens-arbitrum/tokens-erc20/tokens.weth.env';
 import type { RequiredEvmErc20Token } from '$evm/types/erc20';
 import { defineSupportedTokens } from '$lib/utils/env.tokens.utils';
-import { BONK_TOKEN } from './tokens-erc20/tokens.bonk.env';
 
 const ARBITRUM_ERC20_TOKENS_SEPOLIA: RequiredEvmErc20Token[] = [ARB_SEPOLIA_USDC_TOKEN];
 
@@ -15,7 +16,8 @@ const ARBITRUM_ERC20_TOKENS_MAINNET: RequiredEvmErc20Token[] = [
 	USDC_TOKEN,
 	USDT_TOKEN,
 	ARB_TOKEN,
-	BONK_TOKEN
+	BONK_TOKEN,
+	WETH_TOKEN
 ];
 
 export const ARBITRUM_ERC20_TOKENS: RequiredEvmErc20Token[] = defineSupportedTokens({
