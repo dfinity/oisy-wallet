11442a49767e23d05f826d63b82a75fb421a57f5
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-06-25 16:44:17 +0200

    feat(devops): Add env variable `VITE_I18N_ENABLED` to CIs (#7515)
    # Motivation
    
    We need to include `VITE_I18N_ENABLED` in the CIs.

--- src/frontend/src/env/i18n.ts
+++ src/frontend/src/env/i18n.ts
@@ -1,9 +1,9 @@
-import { LOCAL, STAGING } from '$lib/constants/app.constants';
 import { Languages } from '$lib/types/languages';
+import { parseBoolEnvVar } from '$lib/utils/env.utils';
 
 export const SUPPORTED_LANGUAGES = Object.entries(Languages);
 
-// This is the label that is shown, we dont need to translate it as we always show it in its own language
+// This is the label that is shown. We don't need to translate it as we always show it in its own language
 export const LANGUAGES = {
 	[Languages.ENGLISH]: 'English',
 	[Languages.GERMAN]: 'Deutsch'
@@ -11,6 +11,4 @@ export const LANGUAGES = {
 
 // Enabled on Staging and Local if not set
 // Todo: remove once the feature has been completed
-export const I18N_ENABLED: boolean = JSON.parse(
-	import.meta.env.VITE_I18N_ENABLED ?? (STAGING || LOCAL)
-);
+export const I18N_ENABLED = parseBoolEnvVar(import.meta.env.VITE_I18N_ENABLED);
