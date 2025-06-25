cec7dbaffa37d0747ed4877ad31c040cf59b618c
daviddecentage <david.farina@dfinity.org>
2025-06-25 14:52:44 +0200

    update

--- src/frontend/src/env/i18n.ts
+++ src/frontend/src/env/i18n.ts
@@ -11,4 +11,6 @@ export const LANGUAGES = {
 
 // Enabled on Staging and Local if not set
 // Todo: remove once the feature has been completed
-export const I18N_ENABLED = JSON.parse(import.meta.env.VITE_I18N_ENABLED ?? (STAGING || LOCAL));
+export const I18N_ENABLED: boolean = JSON.parse(
+	import.meta.env.VITE_I18N_ENABLED ?? (STAGING || LOCAL)
+);
