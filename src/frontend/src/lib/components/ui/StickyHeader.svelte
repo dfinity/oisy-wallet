d8ca09b3e745c8f8a3f60a9b0d5c11baecff0230
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-06-27 13:05:12 +0200

    feat(frontend): Sticky header (#7535)
    # Motivation
    
    On some places it might be neat to have some kind of sticky header which
    pins to the top of the page when scrolled past. This is useful when we
    want to keep some context info about a list etc.
    
    # Changes
    
    Added sticky header component
    
    # Tests
    
    
    
    https://github.com/user-attachments/assets/724ca2d8-c06a-49e5-81ad-eb73239d87f1
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/frontend/src/lib/components/ui/StickyHeader.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import { debounce, isNullish, nonNullish } from '@dfinity/utils';
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		children: Snippet;
+	}
+
+	const SPACING_TOP = 24; // since we add pt-6 we need to trigger earlier
+
+	const { children }: Props = $props();
+
+	let rootElement: HTMLElement | undefined = $state();
+	let alignmentElement: HTMLElement | undefined = $state();
+	let { originalWidth, originalHeight }: { originalWidth?: number; originalHeight?: number } =
+		$state({});
+
+	let scrolledSoon = $state(false);
+	let scrolledPast = $state(false);
+
+	const handleScroll = () => {
+		if (!rootElement) {
+			return;
+		}
+		const rect = rootElement.getBoundingClientRect();
+		scrolledSoon = rect.top <= SPACING_TOP * 4;
+		scrolledPast = rect.top <= SPACING_TOP;
+	};
+
+	const calcSizes = (force = false) => {
+		if (isNullish(rootElement) || isNullish(alignmentElement)) {
+			return;
+		}
+
+		if (isNullish(originalWidth) || force) {
+			originalWidth = rootElement.clientWidth;
+		}
+		if (isNullish(originalHeight) || force) {
+			originalHeight = rootElement.children?.[0].clientHeight;
+		}
+	};
+
+	$effect(() => {
+		calcSizes();
+	});
+</script>
+
+<svelte:window on:scroll={handleScroll} on:resize={debounce(() => calcSizes(true), 250)} />
+
+<div bind:this={rootElement} class="relative" style={`height: ${originalHeight ?? 0}px`}>
+	<div
+		class="z-2"
+		bind:this={alignmentElement}
+		class:absolute={nonNullish(originalHeight)}
+		class:bg-page={scrolledSoon}
+		class:fixed={scrolledPast}
+		class:top-0={scrolledPast}
+		class:pt-6={scrolledPast}
+		style={`width: ${originalWidth ?? 0}px`}
+	>
+		{@render children()}
+	</div>
+</div>
