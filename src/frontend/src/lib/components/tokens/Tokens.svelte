b5e20a1110d9757203a40c063a5d982e5093d38c
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-06-30 17:25:29 +0200

    feat(frontend): Enable more assets (#7513)
    # Motivation
    
    We want to display the manage token list when we filter assets, so the
    user can quickly add another token that he is looking for but may have
    disabled.
    
    - The list should reload only when filter changes
    - The list should only show disabled tokens, with the exception of
    tokens that we enable via this list, so the user can revert it easily
    right after instead of having to go to the manage modal
    
    # Changes
    
    - Added the list
    - Added filters, sorting etc for the list
    - Make titles to StickyHeaders so in long lists we always see the
    "Apply" button and have a context
    - Added translation for "Enable more assets"
    
    # Tests
    
    
    
    https://github.com/user-attachments/assets/5bba12dd-36aa-4af2-8373-79057984373c
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/tokens/Tokens.svelte
+++ src/frontend/src/lib/components/tokens/Tokens.svelte
@@ -5,22 +5,25 @@
 	import TokensList from '$lib/components/tokens/TokensList.svelte';
 	import TokensMenu from '$lib/components/tokens/TokensMenu.svelte';
 	import Header from '$lib/components/ui/Header.svelte';
+	import StickyHeader from '$lib/components/ui/StickyHeader.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 </script>
 
 <div>
-	<div class="flex w-full flex-row justify-between">
-		<div class="grow-1 relative flex flex-row justify-between">
-			<TokensFilter>
-				{#snippet overflowableContent()}
-					<Header><span class="mt-2 flex">{$i18n.tokens.text.title}</span></Header>
-				{/snippet}
-			</TokensFilter>
+	<StickyHeader>
+		<div class="flex w-full justify-between">
+			<div class="grow-1 relative flex justify-between">
+				<TokensFilter>
+					{#snippet overflowableContent()}
+						<Header><span class="mt-2 flex">{$i18n.tokens.text.title}</span></Header>
+					{/snippet}
+				</TokensFilter>
+			</div>
+			<div class="flex">
+				<TokensMenu />
+			</div>
 		</div>
-		<div class="flex">
-			<TokensMenu />
-		</div>
-	</div>
+	</StickyHeader>
 
 	<TokensList />
 
