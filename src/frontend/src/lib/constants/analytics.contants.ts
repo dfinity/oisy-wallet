b35b62989500df87d709df48a79b14cccfc05b46
daviddecentage <david.farina@dfinity.org>
2025-06-30 12:12:16 +0200

    update

--- src/frontend/src/lib/constants/analytics.contants.ts
+++ src/frontend/src/lib/constants/analytics.contants.ts
@@ -99,6 +99,10 @@ export const TRACK_COUNT_OPEN_WHY_OISY = 'open_why_oisy';
 export const TRACK_COUNT_OPEN_DOCUMENTATION = 'open_documentation';
 export const TRACK_COUNT_OPEN_CHANGELOG = 'open_changelog';
 
+// I18n
+export const TRACK_COUNT_CHANGE_LANGUAGE_SIGNED_IN = 'change_language_signed_in';
+export const TRACK_COUNT_CHANGE_LANGUAGE_SIGNED_OUT = 'change_language_signed_out';
+
 // Snapshots
 export const TRACK_SNAPSHOT_SEND_ERROR = 'snapshot_send_error';
 
--- src/frontend/src/lib/stores/i18n.store.ts
+++ src/frontend/src/lib/stores/i18n.store.ts
@@ -1,10 +1,16 @@
 import { I18N_ENABLED } from '$env/i18n';
+import {
+	TRACK_COUNT_CHANGE_LANGUAGE_SIGNED_IN,
+	TRACK_COUNT_CHANGE_LANGUAGE_SIGNED_OUT
+} from '$lib/constants/analytics.contants';
+import { authSignedIn } from '$lib/derived/auth.derived';
 import de from '$lib/i18n/de.json';
 import en from '$lib/i18n/en.json';
+import { trackEvent } from '$lib/services/analytics.services';
 import { Languages } from '$lib/types/languages';
 import { getDefaultLang, mergeWithFallback } from '$lib/utils/i18n.utils';
 import { get, set } from '$lib/utils/storage.utils';
-import { writable, type Readable } from 'svelte/store';
+import { get as getStore, writable, type Readable } from 'svelte/store';
 
 const enI18n = (): I18n => ({
 	...en,
@@ -38,6 +44,15 @@ const initI18n = (): I18nStore => {
 	const switchLang = async (lang: Languages) => {
 		const bundle = await loadLang(lang);
 		set(bundle);
+
+		trackEvent({
+			name: getStore(authSignedIn)
+				? TRACK_COUNT_CHANGE_LANGUAGE_SIGNED_IN
+				: TRACK_COUNT_CHANGE_LANGUAGE_SIGNED_OUT,
+			metadata: {
+				language: lang
+			}
+		});
 
 		saveLang(lang);
 	};
