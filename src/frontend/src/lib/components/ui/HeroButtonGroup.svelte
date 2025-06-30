405b2afd020bd665d269c19b4abfe59ec53bbb0a
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-06-30 11:56:50 +0200

    style(frontend): Hero buttons truncate and adjust for longer labels (#7544)
    # Motivation
    
    In preparation of possibly much longer texts in other languages, we need
    to adjust the hero buttons.
    
    # Changes
    
    Adjusted some styling to make text truncate and always make them
    `flex-col` to render text below icon at all times.
    
    # Tests
    
    
    ![image](https://github.com/user-attachments/assets/cab4261b-1763-4e85-beac-9421e165d13d)
    
    ![image](https://github.com/user-attachments/assets/55080ae2-0ec6-49d5-a2f0-7624e68cdc42)
    
    ![image](https://github.com/user-attachments/assets/acc506be-cc60-443c-a8f8-965c2f1c432b)

--- src/frontend/src/lib/components/ui/HeroButtonGroup.svelte
+++ src/frontend/src/lib/components/ui/HeroButtonGroup.svelte
@@ -8,6 +8,6 @@
 	let { children }: Props = $props();
 </script>
 
-<div role="toolbar" class="flex flex-1 flex-row justify-between gap-2">
+<div role="toolbar" class="flex w-full justify-between gap-2">
 	{@render children()}
 </div>
