9115c70da0283fc6748f38f748d291a5a6de9ece
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-01 15:49:28 +0200

    review

--- src/frontend/src/lib/types/custom-token.ts
+++ src/frontend/src/lib/types/custom-token.ts
@@ -4,29 +4,25 @@ import type { IcrcCustomToken } from '$icp/types/icrc-custom-token';
 import type { UserTokenState } from '$lib/types/token-toggleable';
 import type { SplToken } from '$sol/types/spl';
 
-export type CustomTokenNetworkKeys = Token extends infer T
+type CustomTokenNetworkKeys = Token extends infer T
 	? T extends Record<string, unknown>
 		? keyof T
 		: never
 	: never;
 
-type BaseIcrc = Pick<IcrcCustomToken, 'ledgerCanisterId' | 'indexCanisterId'>;
-type BaseErc20 = Pick<Erc20Token, 'address' | 'decimals' | 'symbol'> &
-	Pick<Erc20Token['network'], 'chainId'>;
-type BaseSpl = Pick<SplToken, 'address' | 'decimals' | 'symbol'>;
-
 type TokenVariant<K extends CustomTokenNetworkKeys, T> = T & { networkKey: K };
 
-export type IcrcSaveCustomToken = BaseIcrc;
-export type Erc20SaveCustomToken = BaseErc20;
-export type SplSaveCustomToken = BaseSpl;
+export type IcrcSaveCustomToken = Pick<IcrcCustomToken, 'ledgerCanisterId' | 'indexCanisterId'>;
+export type Erc20SaveCustomToken = Pick<Erc20Token, 'address' | 'decimals' | 'symbol'> &
+	Pick<Erc20Token['network'], 'chainId'>;
+export type SplSaveCustomToken = Pick<SplToken, 'address' | 'decimals' | 'symbol'>;
 
 export type SaveCustomToken = UserTokenState &
 	(IcrcSaveCustomToken | Erc20SaveCustomToken | SplSaveCustomToken);
 
 export type SaveCustomTokenWithKey = UserTokenState &
 	(
-		| TokenVariant<'Icrc', BaseIcrc>
-		| TokenVariant<'Erc20', BaseErc20>
-		| TokenVariant<'SplDevnet' | 'SplMainnet', BaseSpl>
+		| TokenVariant<'Icrc', IcrcSaveCustomToken>
+		| TokenVariant<'Erc20', Erc20SaveCustomToken>
+		| TokenVariant<'SplDevnet' | 'SplMainnet', SplSaveCustomToken>
 	);
