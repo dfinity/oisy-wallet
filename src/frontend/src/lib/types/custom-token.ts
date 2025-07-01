6da54c02f20330333f9a05ee7f7f8501954a981e
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-01 15:04:25 +0200

    feat(frontend): Add ERC20 to custom token mapper

--- src/frontend/src/lib/types/custom-token.ts
+++ src/frontend/src/lib/types/custom-token.ts
@@ -1,22 +1,32 @@
+import type { Token } from '$declarations/backend/backend.did';
+import type { Erc20Token } from '$eth/types/erc20';
 import type { IcrcCustomToken } from '$icp/types/icrc-custom-token';
 import type { UserTokenState } from '$lib/types/token-toggleable';
 import type { SplToken } from '$sol/types/spl';
 
-export type CustomTokenNetworkKeys = 'Icrc' | 'SplMainnet' | 'SplDevnet';
+export type CustomTokenNetworkKeys = Token extends infer T
+	? T extends Record<string, unknown>
+		? keyof T
+		: never
+	: never;
 
-export type IcrcSaveCustomToken = Pick<IcrcCustomToken, 'ledgerCanisterId' | 'indexCanisterId'>;
+type BaseIcrc = Pick<IcrcCustomToken, 'ledgerCanisterId' | 'indexCanisterId'>;
+type BaseErc20 = Pick<Erc20Token, 'address' | 'decimals' | 'symbol'> &
+	Pick<Erc20Token['network'], 'chainId'>;
+type BaseSpl = Pick<SplToken, 'address' | 'decimals' | 'symbol'>;
 
-export type SplSaveCustomToken = Pick<SplToken, 'address' | 'decimals' | 'symbol'>;
+type TokenVariant<K extends CustomTokenNetworkKeys, T> = T & { networkKey: K };
 
-export type SaveCustomToken = UserTokenState & (IcrcSaveCustomToken | SplSaveCustomToken);
+export type IcrcSaveCustomToken = BaseIcrc;
+export type Erc20SaveCustomToken = BaseErc20;
+export type SplSaveCustomToken = BaseSpl;
 
-type IcrcSaveCustomTokenWithKey = IcrcSaveCustomToken & {
-	networkKey: Extract<CustomTokenNetworkKeys, 'Icrc'>;
-};
-
-type SplSaveCustomTokenWithKey = SplSaveCustomToken & {
-	networkKey: Extract<CustomTokenNetworkKeys, 'SplMainnet' | 'SplDevnet'>;
-};
+export type SaveCustomToken = UserTokenState &
+	(IcrcSaveCustomToken | Erc20SaveCustomToken | SplSaveCustomToken);
 
 export type SaveCustomTokenWithKey = UserTokenState &
-	(IcrcSaveCustomTokenWithKey | SplSaveCustomTokenWithKey);
+	(
+		| TokenVariant<'Icrc', BaseIcrc>
+		| TokenVariant<'Erc20', BaseErc20>
+		| TokenVariant<'SplDevnet' | 'SplMainnet', BaseSpl>
+	);
