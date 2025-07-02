33f76db598f033f054f615341f1a7a0ad79ea15c
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-02 16:35:03 +0200

    fix(frontend): Missing total amount in Solana RPC consensus strategy (#7609)
    # Motivation
    
    In PR #7606 , I forgot to add the total number of RPC providers in the
    consensus strategy.

--- src/frontend/src/sol/canisters/sol-rpc.constants.ts
+++ src/frontend/src/sol/canisters/sol-rpc.constants.ts
@@ -6,7 +6,7 @@ import type {
 import { toNullable } from '@dfinity/utils';
 
 const SOL_RPC_CONSENSUS_STRATEGY: ConsensusStrategy = {
-	Threshold: { min: 2, total: toNullable() }
+	Threshold: { min: 2, total: toNullable(3) }
 };
 
 export const SOL_RPC_CONFIG: [] | [RpcConfig] = toNullable({
