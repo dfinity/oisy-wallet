29fda3eefd5bec57821ded20b2b5f70087b1e878
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-02 16:13:55 +0200

    fix(frontend): Do not estimate gas for ck-conversions / v1.5.6 (#7602)
    # Motivation
    
    This is a PR to release `v1.5.6` that fixes the Alias input
    auto-trimming.
    
    The PRs that were reviewed and included are:
    
    - https://github.com/dfinity/oisy-wallet/pull/7600
    - https://github.com/dfinity/oisy-wallet/pull/7601
    
    ---------
    
    Co-authored-by: Denys Karmazyn <denys.karmazyn@dfinity.org>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/eth/components/fee/EthFeeContext.svelte
+++ src/frontend/src/eth/components/fee/EthFeeContext.svelte
@@ -91,7 +91,9 @@
 				)
 			});
 
-			const estimatedGas = await estimateGas(params);
+			// We estimate gas only when it is not a ck-conversion (i.e. target network is not ICP).
+			// Otherwise, we would need to emulate the data that are provided to the minter contract address.
+			const estimatedGas = isNetworkICP(targetNetwork) ? undefined : await estimateGas(params);
 
 			if (isSupportedEthTokenId(sendTokenId) || isSupportedEvmNativeTokenId(sendTokenId)) {
 				feeStore.setFee({
