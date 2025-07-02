3d56451c4d4871c90d6d5baa2bec9b1ce7c65e0d
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-07-02 11:17:01 +0200

    feat(frontend): add AI Assistant button to desktop footer (#7599)
    # Motivation
    
    In this PR, the AI Assistant button is added to the desktop footer. On
    mobile, it needs to be rendered in another place, therefore we will add
    it there separately. Though, the button component will be re-used in
    both cases.
    
    ![Screenshot 2025-07-02 at 10 33
    54](https://github.com/user-attachments/assets/b5cb8ec4-ff74-4545-a7e7-5b406295586a)

---
+++ src/frontend/src/lib/components/ai-assistant/AiAssistantConsoleButton.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { AI_ASSISTANT_CONSOLE_ENABLED } from '$env/ai-assistant.env';
+	import IconOisy from '$lib/components/icons/IconOisy.svelte';
+	import { AI_ASSISTANT_CONSOLE_BUTTON } from '$lib/constants/test-ids.constants';
+	import { authSignedIn } from '$lib/derived/auth.derived';
+	import { i18n } from '$lib/stores/i18n.store';
+	import { replaceOisyPlaceholders } from '$lib/utils/i18n.utils';
+
+	interface Props {
+		size?: string;
+		styleClass?: string;
+	}
+	let { size = '80', styleClass }: Props = $props();
+</script>
+
+{#if $authSignedIn && AI_ASSISTANT_CONSOLE_ENABLED}
+	<button
+		aria-label={replaceOisyPlaceholders($i18n.footer.text.ai_assistant_console_button)}
+		class={`group flex items-center justify-center overflow-hidden rounded-full bg-primary duration-500 ${styleClass ?? ''}`}
+		style={`width: ${size}px; height: ${size}px;`}
+		data-tid={AI_ASSISTANT_CONSOLE_BUTTON}
+	>
+		<IconOisy {size} styleClass="group-hover:rotate-10 transition group-hover:scale-[1.10]" />
+	</button>
+{/if}
--- src/frontend/src/lib/constants/test-ids.constants.ts
+++ src/frontend/src/lib/constants/test-ids.constants.ts
@@ -246,3 +246,6 @@ export const TOKEN_MODAL_INDEX_CANISTER_ID_INPUT = 'token-modal-index-canister-i
 export const TOKEN_MODAL_INDEX_CANISTER_ID_EDIT_BUTTON =
 	'token-modal-index-canister-id-edit-button';
 export const TOKEN_MODAL_SAVE_BUTTON = 'token-modal-save-button';
+
+// AI Assistant
+export const AI_ASSISTANT_CONSOLE_BUTTON = 'ai-assistanta-console-button';
