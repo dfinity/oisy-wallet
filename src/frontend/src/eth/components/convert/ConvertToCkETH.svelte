b84a6fd434f4b0f45014318afc7801cbdd8d73fa
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-02 13:28:58 +0200

    fix(frontend): Use page token for `ConvertToCkEth` button (#7598)
    # Motivation
    
    To be more correct, we should use the store `pageToken` for the button
    `ConvertToCkEth` to find the twin token (similar to what we do for
    `ConvertToCkERC20`).
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/eth/components/convert/ConvertToCkETH.svelte
+++ src/frontend/src/eth/components/convert/ConvertToCkETH.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import { nonNullish } from '@dfinity/utils';
-	import { ETHEREUM_TOKEN } from '$env/tokens/tokens.eth.env';
+	import { isNullish, nonNullish } from '@dfinity/utils';
 	import FeeStoreContext from '$eth/components/fee/FeeStoreContext.svelte';
 	import { ethereumToken, ethereumTokenId } from '$eth/derived/token.derived';
 	import type { IcCkToken } from '$icp/types/ic-token';
@@ -8,18 +7,19 @@
 	import ConvertModal from '$lib/components/convert/ConvertModal.svelte';
 	import IconCkConvert from '$lib/components/icons/IconCkConvert.svelte';
 	import { modalConvertToTwinTokenCkEth } from '$lib/derived/modal.derived';
+	import { pageToken } from '$lib/derived/page-token.derived';
 	import { tokens } from '$lib/derived/tokens.derived';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { findTwinToken } from '$lib/utils/token.utils';
 
 	let ckEthToken: IcCkToken | undefined;
 	$: (() => {
-		if (nonNullish(ckEthToken)) {
+		if (nonNullish(ckEthToken) || isNullish($pageToken)) {
 			return;
 		}
 
 		ckEthToken = findTwinToken({
-			tokenToPair: ETHEREUM_TOKEN,
+			tokenToPair: $pageToken,
 			tokens: $tokens
 		});
 	})();
