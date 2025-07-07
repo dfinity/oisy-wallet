ba59f7bc7f7e47e08cc575cb6fce2e6784a742f3
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-07 16:52:40 +0200

    chore

--- src/frontend/src/eth/providers/infura.providers.ts
+++ src/frontend/src/eth/providers/infura.providers.ts
@@ -35,7 +35,7 @@ export class InfuraProvider {
 			return await this.estimateGas(params);
 		} catch (err: unknown) {
 			// Log the error to console for debugging purposes
-			console.error('Error estimating gas:', err);
+			console.error(`Error estimating gas for network ${this.network}:`, err);
 
 			trackEvent({
 				name: TRACK_ETH_ESTIMATE_GAS_ERROR,
