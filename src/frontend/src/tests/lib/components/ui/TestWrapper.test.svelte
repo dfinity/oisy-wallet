c13137d2be27572930630ccfb799a5feb518a863
CvetelinaAngelova <110659037+CvetelinaAngelova@users.noreply.github.com>
2025-07-08 17:40:36 +0300

    fix formating

--- src/frontend/src/tests/lib/components/ui/TestWrapper.test.svelte
+++ src/frontend/src/tests/lib/components/ui/TestWrapper.test.svelte
@@ -1,20 +1,18 @@
 <script lang="ts">
-    import CustomPopoverMenu from '$lib/components/ui/CustomPopoverMenu.svelte';
-    import { type Component } from 'svelte';
-    
-    export let title: string | undefined;
-    export let items: Array<{
-      logo: Component;
-      title: string;
-      action: () => void;
-      testId?: string;
-    }>;
-    </script>
-    
-    <CustomPopoverMenu {title} {items}>
-      <svelte:fragment slot="trigger" let:toggle let:bindTrigger>
-        <button on:click={toggle} use:bindTrigger data-testid="popover-trigger">
-          Test Trigger
-        </button>
-      </svelte:fragment>
-    </CustomPopoverMenu>
+	import CustomPopoverMenu from '$lib/components/ui/CustomPopoverMenu.svelte';
+	import { type Component } from 'svelte';
+
+	export let title: string | undefined;
+	export let items: Array<{
+		logo: Component;
+		title: string;
+		action: () => void;
+		testId?: string;
+	}>;
+</script>
+
+<CustomPopoverMenu {title} {items}>
+	<svelte:fragment slot="trigger" let:toggle let:bindTrigger>
+		<button on:click={toggle} use:bindTrigger data-testid="popover-trigger"> Test Trigger </button>
+	</svelte:fragment>
+</CustomPopoverMenu>
