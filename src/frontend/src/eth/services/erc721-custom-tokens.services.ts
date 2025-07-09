51b8d8702dfae6a35983304ca313d252c675f2d0
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-07-09 10:27:45 +0200

    implements feature

--- src/frontend/src/eth/services/erc721-custom-tokens.services.ts
+++ src/frontend/src/eth/services/erc721-custom-tokens.services.ts
@@ -2,7 +2,6 @@ import type { CustomToken } from '$declarations/backend/backend.did';
 import { ETHEREUM_NETWORK } from '$env/networks/networks.eth.env';
 import { loadCustomTokens } from '$eth/services/erc721.services';
 import { erc721CustomTokensStore } from '$eth/stores/erc721-custom-tokens.store';
-import type { Erc721CustomToken } from '$eth/types/erc721-custom-token';
 import { setManyCustomTokens } from '$lib/api/backend.api';
 import { ProgressStepsAddToken } from '$lib/enums/progress-steps';
 import type { SaveTokensParams } from '$lib/services/manage-tokens.services';
@@ -11,12 +10,7 @@ import type { TokenId } from '$lib/types/token';
 import { toCustomToken } from '$lib/utils/custom-token.utils';
 import { nonNullish } from '@dfinity/utils';
 import { get } from 'svelte/store';
-
-export type SaveErc721CustomToken = Pick<
-	Erc721CustomToken,
-	'enabled' | 'version' | 'symbol' | 'address' | 'network'
-> &
-	Partial<Pick<Erc721CustomToken, 'id'>>;
+import type { SaveErc721CustomToken } from '$eth/types/erc721-custom-token';
 
 export const saveCustomTokens = async ({
 	progress,
