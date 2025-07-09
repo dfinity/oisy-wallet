dcf8ee1c37cd9fd4875d06aa64b2d37e677d21ea
Stefan Berger <stefan.berger@inacta.ch>
2025-07-09 10:42:36 +0200

    Update tokens.spl.env.ts

--- src/frontend/src/env/tokens/tokens.spl.env.ts
+++ src/frontend/src/env/tokens/tokens.spl.env.ts
@@ -6,6 +6,7 @@ import { CRCLX_TOKEN } from '$env/tokens/tokens-spl/tokens.crclx.env';
 import { DEVNET_EURC_TOKEN, EURC_TOKEN } from '$env/tokens/tokens-spl/tokens.eurc.env';
 import { GOOGLX_TOKEN } from '$env/tokens/tokens-spl/tokens.googlx.env';
 import { JUP_TOKEN } from '$env/tokens/tokens-spl/tokens.jup.env';
+import { MCDX_TOKEN } from '$env/tokens/tokens-spl/tokens.mcdx.env';
 import { METAX_TOKEN } from '$env/tokens/tokens-spl/tokens.metax.env';
 import { MSTRX_TOKEN } from '$env/tokens/tokens-spl/tokens.mstrx.env';
 import { NVDAX_TOKEN } from '$env/tokens/tokens-spl/tokens.nvdax.env';
@@ -33,6 +34,7 @@ const SPL_TOKENS_MAINNET: RequiredSplToken[] = [
 	EURC_TOKEN,
 	GOOGLX_TOKEN,
 	JUP_TOKEN,
+	MCDX_TOKEN,
 	METAX_TOKEN,
 	MSTRX_TOKEN,
 	NVDAX_TOKEN,
