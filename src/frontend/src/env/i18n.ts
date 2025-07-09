6d403444e8eb36aec094aa19bfaa4a89c8e4b6a8
Stefan Berger <stefan.berger@inacta.ch>
2025-07-09 12:31:59 +0200

    chore(frontend): Add Portuguese support (#7617)
    # Motivation
    
    We recently added german as additional language, and now we should add
    others asap.
    
    # Changes
    
    - add Portuguese
    
    # Tests
    <img width="1443" alt="image"
    src="https://github.com/user-attachments/assets/c37f9f51-4c7e-4c4c-94bd-97ae523338c2"
    />
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/env/i18n.ts
+++ src/frontend/src/env/i18n.ts
@@ -7,6 +7,7 @@ export const SUPPORTED_LANGUAGES = Object.entries(Languages);
 export const LANGUAGES = {
 	[Languages.ENGLISH]: 'English',
 	[Languages.GERMAN]: 'Deutsch',
+	[Languages.PORTUGUESE]: 'Português',
 	[Languages.CHINESE_SIMPLIFIED]: '中文 (简体)'
 };
 
