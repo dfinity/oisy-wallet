37426eba7b61c52e043d9711c5a4ddc541904580
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-10 22:42:00 +0200

    style(frontend): Use Responsive component for mobile navigation

--- src/frontend/src/lib/components/navigation/MobileNavigationMenu.svelte
+++ src/frontend/src/lib/components/navigation/MobileNavigationMenu.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import { MOBILE_NAVIGATION_MENU } from '$lib/constants/test-ids.constants';
+	import Responsive from '$lib/components/ui/Responsive.svelte';
 </script>
 
+<Responsive down="md">
 <div
 	class="mobile-nav z-2 border-t-1 visible fixed bottom-0 left-0 right-0 flex flex-row border-tertiary bg-primary-inverted-alt md:hidden"
 	data-tid={MOBILE_NAVIGATION_MENU}
 >
 	<slot />
 </div>
+</Responsive>
