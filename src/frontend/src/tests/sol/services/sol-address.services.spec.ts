732da9857fca827e30763e8ca59eca41817ff83d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-10 16:31:09 +0200

    tests

--- src/frontend/src/tests/sol/services/sol-address.services.spec.ts
+++ src/frontend/src/tests/sol/services/sol-address.services.spec.ts
@@ -2,6 +2,7 @@ import { SOLANA_KEY_ID, SOLANA_MAINNET_NETWORK_ID } from '$env/networks/networks
 import { SOLANA_TOKEN_ID } from '$env/tokens/tokens.sol.env';
 import * as idbApi from '$lib/api/idb-addresses.api';
 import * as signerApi from '$lib/api/signer.api';
+import { deriveSolAddress } from '$lib/ic-pub-key/src/cli';
 import {
 	solAddressDevnetStore,
 	solAddressLocalnetStore,
@@ -169,6 +170,40 @@ describe('sol-address.services', () => {
 				data: mockSolAddress,
 				certified: true
 			});
+		});
+	});
+
+	describe('Temp tests for Solana Address Derivation', () => {
+		const p = 'xlmxo-5n3p7-s56nq-7wlbe-nrvij-7veys-thk7y-uuxol-wjzki-rbcqi-aqe';
+
+		it('should derive the correct address for mainnet', async () => {
+			const network = SolanaNetworks.mainnet;
+
+			const foo = await deriveSolAddress(p, network);
+			const encoder = new TextEncoder();
+			const uint8Array = encoder.encode(foo);
+			const base58StringBytes = Uint8Array.from([...foo].map((c) => c.charCodeAt(0)));
+			const decoder2 = getAddressDecoder();
+			const bar = decoder2.decode(base58StringBytes);
+
+			console.log(`SolAddress for ${network}`, foo, base58StringBytes, uint8Array, bar);
+
+			expect(foo).toBe('2EQneZBEeL3XGy3YaQAgxwxYvKq2bRPfQVpiGXgpQEfv');
+		});
+
+		it('should derive the correct address for devnet', async () => {
+			const network = SolanaNetworks.devnet;
+
+			const foo = await deriveSolAddress(p, network);
+			const encoder = new TextEncoder();
+			const uint8Array = encoder.encode(foo);
+			const base58StringBytes = Uint8Array.from([...foo].map((c) => c.charCodeAt(0)));
+			const decoder2 = getAddressDecoder();
+			const bar = decoder2.decode(base58StringBytes);
+
+			console.log(`SolAddress for ${network}`, foo, base58StringBytes, uint8Array, bar);
+
+			expect(foo).toBe('2EQneZBEeL3XGy3YaQAgxwxYvKq2bRPfQVpiGXgpQEfv');
 		});
 	});
 });
