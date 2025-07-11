7fe82af53bf9bc783829b5765362e63354588d3f
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-07-11 10:42:57 +0200

    feat(frontend): implements infura erc721 providers (#7722)
    # Motivation
    
    We want to be able to request data from erc721 contracts.
    
    # Changes
    
    - adds infura provider for erc721 contracts
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

---
+++ src/frontend/src/eth/types/erc721.ts
@@ -0,0 +1,7 @@
+import type { ContractAddress } from '$eth/types/address';
+import type { TokenMetadata } from '$lib/types/token';
+
+export type Erc721ContractAddress = ContractAddress;
+export type Erc721Contract = Erc721ContractAddress;
+
+export type Erc721Metadata = TokenMetadata;
