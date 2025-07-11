7f38045e85d706f57adf129894d0dd9210919286
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-07-11 08:29:54 +0200

    adds erc721 token standard

--- src/frontend/src/eth/types/erc721.ts
+++ src/frontend/src/eth/types/erc721.ts
@@ -1,5 +1,8 @@
 import type { ContractAddress } from '$eth/types/address';
-import type { TokenMetadata } from '$lib/types/token';
+import type { Token, TokenMetadata } from '$lib/types/token';
+import type { EthereumNetwork } from '$eth/types/network';
+
+export type Erc721Token = Erc721Contract & Omit<Token, 'network'> & { network: EthereumNetwork };
 
 export type Erc721ContractAddress = ContractAddress;
 export type Erc721Contract = Erc721ContractAddress;
