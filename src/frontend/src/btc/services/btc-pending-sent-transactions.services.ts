a8c8fa30df0603754917f7ac18382a0ffdcfd207
DecentAgeCoder <marco.oesch@dfinity.org>
2025-07-11 15:59:15 +0200

    Replace custom JSON.stringify logic with `jsonReplacer` in `console.warn` for btcPendingSentTransactions.

--- src/frontend/src/btc/services/btc-pending-sent-transactions.services.ts
+++ src/frontend/src/btc/services/btc-pending-sent-transactions.services.ts
@@ -6,7 +6,7 @@ import type { NetworkId } from '$lib/types/network';
 import type { ResultSuccess } from '$lib/types/utils';
 import { replacePlaceholders } from '$lib/utils/i18n.utils';
 import { mapNetworkIdToBitcoinNetwork, mapToSignerBitcoinNetwork } from '$lib/utils/network.utils';
-import { isNullish, nonNullish } from '@dfinity/utils';
+import { isNullish, jsonReplacer, nonNullish } from '@dfinity/utils';
 import { get } from 'svelte/store';
 
 export const loadBtcPendingSentTransactions = async ({
@@ -43,16 +43,7 @@ export const loadBtcPendingSentTransactions = async ({
 		});
 		console.warn(
 			'Storing Pending transactions: ',
-			JSON.stringify(
-				pendingTransactions,
-				(key, value) => {
-					if (value instanceof Uint8Array) {
-						return Array.from(value);
-					}
-					return value;
-				},
-				2
-			)
+			JSON.stringify(pendingTransactions, jsonReplacer, 2)
 		);
 
 		btcPendingSentTransactionsStore.setPendingTransactions({
