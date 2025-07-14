a3baaa2fa37cc272d4474fde1bb233e3683a1ca1
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-07-14 10:58:24 +0200

    implements feature

--- src/frontend/src/eth/services/erc721.services.ts
+++ src/frontend/src/eth/services/erc721.services.ts
@@ -140,7 +140,7 @@ const loadNftsForContract = async ({
 	infuraProvider: InfuraErc721Provider;
 	contractAddress: string;
 }) => {
-	const myWalletAddress = '0x29469395eaf6f95920e59f858042f0e28d98a20b';
+	const myWalletAddress = '0x29469395eaf6f95920e59f858042f0e28d98a20b'; // TODO remove this and load on wallet address
 
 	try {
 		const tokenIds = await etherscanProvider.erc721TokenInventory({
