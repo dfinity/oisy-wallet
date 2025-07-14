011d0d9163cdd0bb1cd71bc25524453c1a99a771
Stefan Berger <stefan.berger@inacta.ch>
2025-07-14 09:19:48 +0200

    import token group

--- src/frontend/src/env/tokens/tokens-evm/tokens-arbitrum/tokens-erc20/tokens.arb.env.ts
+++ src/frontend/src/env/tokens/tokens-evm/tokens-arbitrum/tokens-erc20/tokens.arb.env.ts
@@ -1,4 +1,5 @@
 import { ARBITRUM_MAINNET_NETWORK } from '$env/networks/networks-evm/networks.evm.arbitrum.env';
+import { ARB_TOKEN_GROUP } from '$env/tokens/groups/groups.arb.env';
 import arb from '$eth/assets/arb.svg';
 import type { RequiredEvmErc20Token } from '$evm/types/erc20';
 import type { TokenId } from '$lib/types/token';
