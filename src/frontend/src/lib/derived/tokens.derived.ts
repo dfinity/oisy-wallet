64d442f2d23a3d4e4b05442d4ed80c88295cc960
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-07-14 11:32:08 +0200

    feat(frontend): adds fungible tokens list (#7758)
    # Motivation
    
    Since we support `fungible` and `non-fungible` tokens, we want to have
    separate lists for those types.
    
    # Changes
    
    - adds fungible tokens list
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

--- src/frontend/src/lib/derived/tokens.derived.ts
+++ src/frontend/src/lib/derived/tokens.derived.ts
@@ -7,6 +7,7 @@ import { erc20Tokens } from '$eth/derived/erc20.derived';
 import { enabledEthereumTokens } from '$eth/derived/tokens.derived';
 import type { Erc20Token } from '$eth/types/erc20';
 import { isTokenErc20 } from '$eth/utils/erc20.utils';
+import { isTokenErc721 } from '$eth/utils/erc721.utils';
 import { isDefaultEthereumToken } from '$eth/utils/eth.utils';
 import { enabledEvmTokens } from '$evm/derived/tokens.derived';
 import { icrcChainFusionDefaultTokens, sortedIcrcTokens } from '$icp/derived/icrc.derived';
@@ -56,6 +57,10 @@ export const tokens: Readable<Token[]> = derived(
 	]
 );
 
+export const fungibleTokens: Readable<Token[]> = derived([tokens], ([$tokens]) =>
+	$tokens.filter((token) => !isTokenErc721(token))
+);
+
 export const defaultEthereumTokens: Readable<Token[]> = derived([tokens], ([$tokens]) =>
 	$tokens.filter((token) => isDefaultEthereumToken(token))
 );
@@ -76,6 +81,14 @@ export const tokensToPin: Readable<TokenToPin[]> = derived(
  * All user-enabled tokens.
  */
 export const enabledTokens: Readable<Token[]> = derived([tokens], filterEnabledTokens);
+
+/**
+ * All user-enabled fungible tokens.
+ */
+export const enabledFungibleTokens: Readable<Token[]> = derived(
+	[fungibleTokens],
+	filterEnabledTokens
+);
 
 /**
  * It isn't performant to post filter again the Erc20 tokens that are enabled, but it's code wise convenient to avoid duplication of logic.
