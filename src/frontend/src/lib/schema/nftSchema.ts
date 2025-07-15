9fd3561c8f0a95d98c986a493b1ab7766fbdbdc8
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-07-15 16:48:44 +0200

    implements feature

--- src/frontend/src/lib/schema/nftSchema.ts
+++ src/frontend/src/lib/schema/nftSchema.ts
@@ -1,5 +1,3 @@
-import { NetworkSchema } from '$lib/schema/network.schema';
-import { TokenStandardSchema } from '$lib/schema/token.schema';
 import * as z from 'zod';
 
 export const NftAttributeSchema = z.object({
@@ -18,9 +16,7 @@ export const NftSchema = z.object({
 	contract: z.object({
 		address: z.string(),
 		enabled: z.boolean(),
-		name: z.string(),
-		network: NetworkSchema,
-		standard: TokenStandardSchema
+		name: z.string()
 	})
 })
 	.merge(NftMetadataSchema);
