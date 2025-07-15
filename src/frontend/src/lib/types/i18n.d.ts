5b08b84a0faa75550e8cd176a2a08db8be19f4b4
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-07-15 09:22:16 +0200

    feat(frontend): AiAssistantForm component (#7778)
    # Motivation
    
    AiAssistantForm component:
    
    <img width="334" height="77" alt="Screenshot 2025-07-14 at 18 53 31"
    src="https://github.com/user-attachments/assets/80d9db5b-e51f-4bdf-8575-00295b68a5c5"
    />
    
    ---------
    
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

--- src/frontend/src/lib/types/i18n.d.ts
+++ src/frontend/src/lib/types/i18n.d.ts
@@ -1206,6 +1206,10 @@ interface I18nTemporal {
 	};
 }
 
+interface I18nAi_assistant {
+	text: { send_message: string; send_message_input_placeholder: string };
+}
+
 interface I18n {
 	lang: Languages;
 	core: I18nCore;
@@ -1244,4 +1248,5 @@ interface I18n {
 	earning: I18nEarning;
 	welcome: I18nWelcome;
 	temporal: I18nTemporal;
+	ai_assistant: I18nAi_assistant;
 }
