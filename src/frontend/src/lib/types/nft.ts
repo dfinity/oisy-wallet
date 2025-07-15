36aae825a120b8370095363941e13a56daff34cc
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-07-15 16:08:58 +0200

    implements feature

--- src/frontend/src/lib/types/nft.ts
+++ src/frontend/src/lib/types/nft.ts
@@ -2,4 +2,4 @@ import type { NftSchema } from '$lib/schema/nftSchema';
 import type * as z from 'zod';
 import type { Erc721CustomToken } from '$eth/types/erc721-custom-token';
 
-export type Nft = z.infer<typeof NftSchema> & Erc721CustomToken;
+export type Nft = z.infer<typeof NftSchema> & { contract: Erc721CustomToken };
