40e09725d76e41b5f953b075a0c63ab22f057af6
Stefan Berger <stefan.berger@inacta.ch>
2025-07-16 07:47:55 +0200

    Update i18n.ts

--- src/frontend/src/env/i18n.ts
+++ src/frontend/src/env/i18n.ts
@@ -5,6 +5,7 @@ export const SUPPORTED_LANGUAGES = Object.entries(Languages);
 // This is the label that is shown. We don't need to translate it as we always show it in its own language
 export const LANGUAGES = {
 	[Languages.ENGLISH]: 'English',
+	[Languages.CZECH]: 'Čeština',
 	[Languages.GERMAN]: 'Deutsch',
 	[Languages.ITALIAN]: 'Italiano',
 	[Languages.PORTUGUESE]: 'Português',
