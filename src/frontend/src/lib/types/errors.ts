6536f015f91cfbee3558ca7b3fb5a05f2c0e21ac
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-07-17 10:09:22 +0200

    implements feature

--- src/frontend/src/lib/types/errors.ts
+++ src/frontend/src/lib/types/errors.ts
@@ -12,6 +12,24 @@ export class AlreadyClaimedError extends Error {}
 
 export class InvalidCampaignError extends Error {}
 
+export class NftError extends Error {
+	constructor(private readonly _tokenUri: number, private readonly _contractAddress: string) {
+		super();
+	}
+
+	get tokenUri(): number {
+		return this._tokenUri;
+	}
+
+	get contractAddress(): string {
+		return this._contractAddress;
+	}
+}
+
+export class InvalidTokenUri extends NftError {}
+
+export class InvalidMetadataImageUrl extends NftError {}
+
 export class LoadIdbAddressError extends Error {
 	constructor(private readonly _networkId: NetworkId) {
 		super();
