c0ea9056cec87935d2d659d3db05f4a930ab49a7
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-07-17 15:43:56 +0200

    removes function for later

--- src/frontend/src/tests/lib/stores/nft.store.spec.ts
+++ src/frontend/src/tests/lib/stores/nft.store.spec.ts
@@ -1,7 +1,7 @@
 import { nftStore } from '$lib/stores/nft.store';
 import type { Nft } from '$lib/types/nft';
 import { parseNftId } from '$lib/validation/nft.validation';
-import { mockEthAddress, mockEthAddress2 } from '$tests/mocks/eth.mocks';
+import { mockEthAddress2 } from '$tests/mocks/eth.mocks';
 import { mockValidNft } from '$tests/mocks/nfts.mock';
 import { get } from 'svelte/store';
 
@@ -45,35 +45,6 @@ describe('nftStore', () => {
 			nftStore.addAll([mockValidNft, similarNft]);
 
 			expect(get(nftStore)).toEqual([mockValidNft, similarNft]);
-		});
-	});
-
-	describe('getTokenIds', () => {
-		const mockNft1 = mockValidNft;
-		const mockNft2 = { ...mockValidNft, id: parseNftId(837364) };
-
-		beforeEach(() => {
-			nftStore.addAll([mockNft1, mockNft2]);
-		});
-
-		it('should return empty list when no NFT is matching contract address', () => {
-			const tokenIds = nftStore.getTokenIds(mockEthAddress2);
-
-			expect(tokenIds).toEqual([]);
-		});
-
-		it('should return token Ids for matching contract address', () => {
-			const tokenIds = nftStore.getTokenIds(mockEthAddress);
-
-			expect(tokenIds).toEqual([mockNft1.id, mockNft2.id]);
-		});
-
-		it('should return empty array when store is empty', () => {
-			nftStore.resetAll();
-
-			const tokenIds = nftStore.getTokenIds(mockEthAddress);
-
-			expect(tokenIds).toEqual([]);
 		});
 	});
 });
