ea9ce818ec2066469dec340b4660df5a6444de37
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-17 12:34:42 +0200

    feat(frontend): Storage store will require a default value (#7830)
    # Motivation
    
    In the storage stores, we do not really need to have a nullish possible
    data. It is in fact a bit troublesome for the derived stores, since we
    need to set the fallback always.
    
    So, in this PR, we provide a default value for the storage stores, so
    that they can fallback there in case there is no value saved in local
    storage.
    
    If one wants to keep the same behaviour as before, it can always pass
    the type as nullish:
    
    `initStorageStore<SettingsData | null>`
    
    # Changes
    
    - Add compulsory default value for initializer `initStorageStore`.
    - Adapt stores to have the default value: there are only two, and both
    defaults to `false`.
    - Remove fallback from the derived stores.
    
    # Tests
    
    Adapted tests

--- src/frontend/src/tests/lib/stores/storage.store.spec.ts
+++ src/frontend/src/tests/lib/stores/storage.store.spec.ts
@@ -1,4 +1,5 @@
 import { initStorageStore, type StorageStore } from '$lib/stores/storage.store';
+import type { Option } from '$lib/types/utils';
 import { del as delStorage, get as getStorage, set as setStorage } from '$lib/utils/storage.utils';
 import { get } from 'svelte/store';
 
@@ -16,6 +17,7 @@ describe('storage.store', () => {
 		}
 
 		const mockData: MockData = { name: 'Test Data', values: [1, 2, 3] };
+		const mockData2: MockData = { name: 'Test Data 2', values: [4, 5, 6] };
 
 		const mockKey = 'mock-data';
 
@@ -28,23 +30,23 @@ describe('storage.store', () => {
 
 			vi.mocked(getStorage).mockImplementation(() => {});
 
-			mockStore = initStorageStore<MockData>({ key: mockKey });
+			mockStore = initStorageStore<MockData>({ key: mockKey, defaultValue: mockData2 });
 		});
 
 		it('should initialize with the value from storage', () => {
 			vi.mocked(getStorage).mockImplementation(() => mockData);
 
-			const store = initStorageStore<MockData>({ key: mockKey });
+			const store = initStorageStore<MockData>({ key: mockKey, defaultValue: mockData2 });
 
 			expect(get(store)).toEqual(mockData);
 		});
 
-		it('should initialize undefined if no value is stored', () => {
+		it('should initialize with default value if no value is stored', () => {
 			vi.mocked(getStorage).mockImplementation(() => undefined);
 
-			const store = initStorageStore<MockData>({ key: mockKey });
+			const store = initStorageStore<MockData>({ key: mockKey, defaultValue: mockData2 });
 
-			expect(get(store)).toBeUndefined();
+			expect(get(store)).toEqual(mockData2);
 		});
 
 		describe('set', () => {
@@ -57,12 +59,9 @@ describe('storage.store', () => {
 
 			it('should update the store', () => {
 				mockStore.set(mockParams);
+				mockStore.set({ key: mockKey, value: mockData2 });
 
-				const newValue = { name: 'Test Data 2', values: [4, 5, 6] };
-
-				mockStore.set({ key: mockKey, value: newValue });
-
-				expect(get(mockStore)).toEqual(newValue);
+				expect(get(mockStore)).toEqual(mockData2);
 			});
 		});
 
@@ -77,14 +76,39 @@ describe('storage.store', () => {
 				expect(delStorage).toHaveBeenCalledExactlyOnceWith({ key: mockKey });
 			});
 
-			it('should set the store to null', () => {
+			it('should reset the store to default value', () => {
 				mockStore.set(mockParams);
 
 				expect(get(mockStore)).toEqual(mockData);
 
 				mockStore.reset({ key: mockKey });
 
-				expect(get(mockStore)).toBeNull();
+				expect(get(mockStore)).toEqual(mockData2);
+			});
+
+			it('should reset the store to default value even if it is nullish', () => {
+				const nullStore = initStorageStore<Option<MockData>>({ key: mockKey, defaultValue: null });
+
+				nullStore.set(mockParams);
+
+				expect(get(nullStore)).toEqual(mockData);
+
+				nullStore.reset({ key: mockKey });
+
+				expect(get(nullStore)).toBeNull();
+
+				const undefinedStore = initStorageStore<Option<MockData>>({
+					key: mockKey,
+					defaultValue: undefined
+				});
+
+				undefinedStore.set(mockParams);
+
+				expect(get(undefinedStore)).toEqual(mockData);
+
+				undefinedStore.reset({ key: mockKey });
+
+				expect(get(undefinedStore)).toBeUndefined();
 			});
 		});
 	});
