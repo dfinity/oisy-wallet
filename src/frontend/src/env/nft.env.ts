e0bd24a22aab7320b78650f7d55e95d8209a1d90
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-07-17 09:21:38 +0200

    implements feature

--- src/frontend/src/env/nft.env.ts
+++ src/frontend/src/env/nft.env.ts
@@ -2,4 +2,4 @@ import { LOCAL, STAGING } from '$lib/constants/app.constants';
 
 // Enabled on Staging and Local if not set
 // TODO: remove once the feature has been completed
-export const NFTS_ENABLED = JSON.parse(process.env.NFTS_ENABLED ?? (STAGING || LOCAL)) === true;
+export const NFTS_ENABLED = JSON.parse(import.meta.env.VITE_NFTS_ENABLED ?? (STAGING || LOCAL)) === true;
