de971df3758368573e67883869a550118614ee50
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-18 00:01:24 +0200

    icon

--- src/frontend/src/lib/components/currency/MenuCurrencySelector.svelte
+++ src/frontend/src/lib/components/currency/MenuCurrencySelector.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-	import LanguageDropdown from '$lib/components/core/LanguageDropdown.svelte';
 	import CurrencyDropdown from '$lib/components/currency/CurrencyDropdown.svelte';
-	import IconLanguage from '$lib/components/icons/IconLanguage.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
+	import IconDollarSign from '$lib/components/icons/IconDollarSign.svelte';
 </script>
 
 <div class="flex justify-between pl-3">
 	<span class="mr-3 flex min-w-32 items-center text-tertiary">
-		<!--		<span class="mr-1 flex"><IconLanguage /></span> -->
+				<span class="mr-1 flex"><IconDollarSign /></span>
 		<span class="flex">{$i18n.core.text.currency}</span>
 	</span>
 
