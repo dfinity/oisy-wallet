ef90aa9cbfa606aac12103a69b6a78764ed5e952
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-07-17 10:48:53 +0200

    resolves review feedback

--- src/frontend/src/lib/stores/nft.store.ts
+++ src/frontend/src/lib/stores/nft.store.ts
@@ -34,7 +34,7 @@ const initNftStore = (): NftStore => {
 			});
 		},
 		getTokenIds: (contractAddress: string) => {
-			let tokenIds = [];
+			let tokenIds: NftId = [];
 
 			update((nfts) => {
 				if (isNullish(nfts)) {
