cd5c07e2c491823381b15861e89463b88217897c
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-17 12:52:28 +0200

    feat(frontend): Derived store for current currency and exchange rate (#7845)
    # Motivation
    
    To facilitate and simplify the code, we create derived stores based on
    `currencyStore`.
    
    # Changes
    
    - Derived store for pure currency value.
    - Derived store for the exchange rate to USD.
    
    # Tests
    
    Added tests.

---
+++ src/frontend/src/tests/lib/derived/currency.derived.spec.ts
@@ -0,0 +1,76 @@
+import { currentCurrency, currentCurrencyExchangeRate } from '$lib/derived/currency.derived';
+import { Currencies } from '$lib/enums/currencies';
+import { currencyStore } from '$lib/stores/currency.store';
+import { get } from 'svelte/store';
+
+vi.mock('idb-keyval', () => ({
+	createStore: vi.fn(() => ({
+		/* mock store implementation */
+	})),
+	set: vi.fn(),
+	get: vi.fn(),
+	del: vi.fn(),
+	update: vi.fn()
+}));
+
+describe('currency.derived', () => {
+	describe('currentCurrency', () => {
+		it('should initialize with the default currency', () => {
+			expect(get(currentCurrency)).toEqual(Currencies.USD);
+		});
+
+		it('should return the current currency from the currency store', () => {
+			expect(get(currentCurrency)).toEqual(Currencies.USD);
+
+			currencyStore.switchCurrency(Currencies.CHF);
+
+			expect(get(currentCurrency)).toEqual(Currencies.CHF);
+
+			currencyStore.switchCurrency(Currencies.EUR);
+
+			expect(get(currentCurrency)).toEqual(Currencies.EUR);
+		});
+	});
+
+	describe('currentCurrencyExchangeRate', () => {
+		beforeEach(() => {
+			currencyStore.switchCurrency(Currencies.USD);
+		});
+
+		it('should initialize with the default value', () => {
+			expect(get(currentCurrencyExchangeRate)).toEqual(1);
+		});
+
+		it('should return the current value from the currency store', () => {
+			expect(get(currentCurrencyExchangeRate)).toEqual(1);
+
+			currencyStore.setExchangeRate(101);
+
+			expect(get(currentCurrencyExchangeRate)).toEqual(101);
+
+			currencyStore.setExchangeRate(1.5);
+
+			expect(get(currentCurrencyExchangeRate)).toEqual(1.5);
+		});
+
+		it('should return null if exchange rate is not set', () => {
+			currencyStore.setExchangeRate(1.2);
+
+			expect(get(currentCurrencyExchangeRate)).toEqual(1.2);
+
+			currencyStore.setExchangeRate(null);
+
+			expect(get(currentCurrencyExchangeRate)).toBeNull();
+		});
+
+		it('should return null when the language is switched', () => {
+			currencyStore.setExchangeRate(1.2);
+
+			expect(get(currentCurrencyExchangeRate)).toEqual(1.2);
+
+			currencyStore.switchCurrency(Currencies.CHF);
+
+			expect(get(currentCurrencyExchangeRate)).toBeNull();
+		});
+	});
+});
