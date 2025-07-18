7109b443588ed4388980e53d764be1193ff94dbc
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-07-18 13:01:56 +0200

    resolves review feedback

--- src/frontend/src/lib/utils/nfts.utils.ts
+++ src/frontend/src/lib/utils/nfts.utils.ts
@@ -1,22 +1,32 @@
 import type { Erc721CustomToken } from '$eth/types/erc721-custom-token';
 import type { Nft } from '$lib/types/nft';
+import type { NetworkId } from '$lib/types/network';
+import { isNullish, nonNullish } from '@dfinity/utils';
+
+export type NftsByNetwork = Record<NetworkId, Record<string, Nft[]>>;
 
 export const getLoadedNftsByTokens = ({
-	tokens,
-	loadedNfts
-}: {
+																				tokens,
+																				loadedNfts
+																			}: {
 	tokens: Erc721CustomToken[];
 	loadedNfts: Nft[];
-}) => {
-	const nftsByToken = new Map<string, Nft[]>();
+}): NftsByNetwork => {
+	const nftsByToken: NftsByNetwork = {};
 
-	tokens.forEach((token) => nftsByToken.set(token.address.toLowerCase(), []));
+	tokens.forEach((token) => {
+		if (isNullish(nftsByToken[token.network.id])) {
+			nftsByToken[token.network.id] = {};
+		}
+		nftsByToken[token.network.id][token.address.toLowerCase()] = [];
+	});
 
 	loadedNfts.forEach((nft) => {
-		const contractAddress = nft.contract.address.toLowerCase();
-		if (nftsByToken.has(contractAddress)) {
-			const existingNfts = nftsByToken.get(contractAddress) ?? [];
-			nftsByToken.set(contractAddress, [...existingNfts, nft]);
+		const networkId = nft.contract.network.id;
+		const address = nft.contract.address.toLowerCase();
+
+		if (nonNullish(nftsByToken[networkId]) && nonNullish(nftsByToken[networkId][address])) {
+			nftsByToken[networkId][address].push(nft);
 		}
 	});
 
