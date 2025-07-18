4dbc3300976b624e483bda3e588ca53917313f36
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-07-18 14:05:31 +0200

    feat(frontend): implements nfts utils (#7880)
    # Motivation
    
    We want to be able to calculate a mapping of `tokens` and the
    corresponding `nfts` that are already loaded.
    
    # Changes
    
    - implements utils for `nfts`
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

---
+++ src/frontend/src/lib/utils/nfts.utils.ts
@@ -0,0 +1,39 @@
+import type { Erc721CustomToken } from '$eth/types/erc721-custom-token';
+import type { Nft, NftsByNetwork } from '$lib/types/nft';
+import { isNullish, nonNullish } from '@dfinity/utils';
+
+export const getLoadedNftsByTokens = ({
+	tokens,
+	loadedNfts
+}: {
+	tokens: Erc721CustomToken[];
+	loadedNfts: Nft[];
+}): NftsByNetwork => {
+	const nftsByToken: NftsByNetwork = {};
+
+	tokens.forEach(({ address, network: { id: networkId } }) => {
+		if (isNullish(nftsByToken[networkId])) {
+			nftsByToken[networkId] = {};
+		}
+		nftsByToken[networkId][address.toLowerCase()] = [];
+	});
+
+	loadedNfts.forEach((nft) => {
+		const {
+			contract: {
+				network: { id: networkId },
+				address
+			}
+		} = nft;
+		const normalizedAddress = address.toLowerCase();
+
+		if (
+			nonNullish(nftsByToken[networkId]) &&
+			nonNullish(nftsByToken[networkId][normalizedAddress])
+		) {
+			nftsByToken[networkId][normalizedAddress].push(nft);
+		}
+	});
+
+	return nftsByToken;
+};
