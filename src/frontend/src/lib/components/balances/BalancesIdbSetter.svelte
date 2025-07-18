1b409d4c2762b0cea476c6151eab6d6369fa0558
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-18 07:44:01 +0200

    lint

--- src/frontend/src/lib/components/balances/BalancesIdbSetter.svelte
+++ src/frontend/src/lib/components/balances/BalancesIdbSetter.svelte
@@ -1,19 +1,9 @@
 <script lang="ts">
 	import { type Snippet, untrack } from 'svelte';
-	import { btcTransactionsStore } from '$btc/stores/btc-transactions.store';
-	import { ethTransactionsStore } from '$eth/stores/eth-transactions.store';
-	import { icTransactionsStore } from '$icp/stores/ic-transactions.store';
 	import { setIdbBalancesStore } from '$lib/api/idb-balances.api';
-	import {
-		setIdbBtcTransactions,
-		setIdbEthTransactions,
-		setIdbIcTransactions,
-		setIdbSolTransactions
-	} from '$lib/api/idb-transactions.api';
 	import { authIdentity } from '$lib/derived/auth.derived';
 	import { enabledTokens } from '$lib/derived/tokens.derived';
 	import { balancesStore } from '$lib/stores/balances.store';
-	import { solTransactionsStore } from '$sol/stores/sol-transactions.store';
 
 	interface Props {
 		children?: Snippet;
