d98ddc89ff95d0b2455cff2bd964efd315bc2256
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-07-21 08:56:25 +0200

    displays loaded nfts

---
+++ src/frontend/src/lib/components/nfts/NftCard.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { nonNullish } from '@dfinity/utils';
+	import NetworkLogo from '$lib/components/networks/NetworkLogo.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
+	import type { Nft } from '$lib/types/nft';
+	import { replacePlaceholders } from '$lib/utils/i18n.utils';
+
+	interface Props {
+		nft: Nft;
+		testId?: string;
+	}
+
+	let { nft, testId }: Props = $props();
+</script>
+
+<div data-tid={testId}>
+	<div class="relative overflow-hidden rounded-lg">
+		{#if nonNullish(nft.imageUrl)}
+			<img
+				src={nft.imageUrl}
+				alt={replacePlaceholders($i18n.nfts.alt.image, {
+					$tokenId: nft.id.toString()
+				})}
+				class="h-48"
+				loading="lazy"
+			/>
+		{:else}
+			<div class="bg-black/16 h-48 rounded-lg"></div>
+		{/if}
+
+		<div class="absolute bottom-2 right-2">
+			<NetworkLogo network={nft.contract.network} size="xs" color="white" />
+		</div>
+	</div>
+
+	<div class="px-2 pt-2">
+		<h3 class="text-xs font-semibold text-tertiary">{nft.contract.name}</h3>
+		<span class="text-xs text-tertiary">{`#${nft.id}`}</span>
+	</div>
+</div>
