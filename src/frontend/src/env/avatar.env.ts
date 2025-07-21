b67243c32e27af15a58f132a89877a8eed91c627
CvetelinaAngelova <110659037+CvetelinaAngelova@users.noreply.github.com>
2025-07-21 17:47:06 +0300

    chore: revert changes on the release

--- src/frontend/src/env/avatar.env.ts
+++ src/frontend/src/env/avatar.env.ts
@@ -1,4 +1,8 @@
+import { LOCAL, STAGING } from '$lib/constants/app.constants';
 import { parseBoolEnvVar } from '$lib/utils/env.utils';
 
+// Enabled on Staging and Local
 // TODO: remove once the feature has been completed
-export const AVATAR_ENABLED = parseBoolEnvVar(import.meta.env.VITE_AVATAR_ENABLED);
+export const AVATAR_ENABLED = parseBoolEnvVar(
+	import.meta.env.VITE_AVATAR_ENABLED ?? (STAGING || LOCAL)
+);
