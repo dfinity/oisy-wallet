0589b63a19d548e6bb9d6e0929cd867507f58902
Stefan Berger <stefan.berger@inacta.ch>
2025-07-21 16:14:24 +0200

    Added some currencies, and sorted them by code

--- src/frontend/src/lib/enums/currency.ts
+++ src/frontend/src/lib/enums/currency.ts
@@ -1,10 +1,17 @@
 // These are three-letter ISO 4217 currency codes
 // https://www.iso.org/iso-4217-currency-codes.html
 export enum Currency {
-	USD = 'usd',
-	EUR = 'eur',
+	AED = 'aed',
+	BRL = 'brl',
 	CHF = 'chf',
+	CNY = 'cny',
+	EUR = 'eur',
 	GBP = 'gbp',
+	INR = 'inr',
 	JPY = 'jpy',
-	CNY = 'cny'
+	SAR = 'sar',
+	SGD = 'sgd',
+	USD = 'usd',
+	XAF = 'xaf',
+	XOF = 'xof'	
 }
