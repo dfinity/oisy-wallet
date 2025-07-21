7149f2bfcb11bc41b7af32a89affff2c91827e4d
CvetelinaAngelova <110659037+CvetelinaAngelova@users.noreply.github.com>
2025-07-21 17:52:12 +0300

    chore(frontend): add Avatar feature flag (#7914)
    # Motivation
    
    Add Enabled Avatar feature flag to the pipeline
    
    # Changes
    
    <!-- List the changes that have been developed -->
    
    # Tests
    
    <!-- Please provide any information or screenshots about the tests that
    have been done -->
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/env/avatar.env.ts
+++ src/frontend/src/env/avatar.env.ts
@@ -1,8 +1,4 @@
-import { LOCAL, STAGING } from '$lib/constants/app.constants';
 import { parseBoolEnvVar } from '$lib/utils/env.utils';
 
-// Enabled on Staging and Local
 // TODO: remove once the feature has been completed
-export const AVATAR_ENABLED = parseBoolEnvVar(
-	import.meta.env.VITE_AVATAR_ENABLED ?? (STAGING || LOCAL)
-);
+export const AVATAR_ENABLED = parseBoolEnvVar(import.meta.env.VITE_AVATAR_ENABLED);
