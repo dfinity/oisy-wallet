61c86a2d1452cf849a8a5944f0b8690aef4d9a32
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-21 17:12:49 +0200

    revert

--- src/frontend/src/lib/components/tokens/TokenInputCurrencyFiat.svelte
+++ src/frontend/src/lib/components/tokens/TokenInputCurrencyFiat.svelte
@@ -57,7 +57,7 @@
 				})
 				: undefined;
 
-		if (newDisplayValue !== displayValue?.toString()) {
+		if (Number(newDisplayValue) !== Number(displayValue)) {
 			displayValue = newDisplayValue;
 		}
 	};
