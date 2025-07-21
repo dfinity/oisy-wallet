c012d4b79e11921d148cc8feb2815dd2b0610b4a
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-07-21 13:05:51 +0200

    feat(frontend): display nfts (#7901)
    # Motivation
    
    If the feature flag `NFTS_ENABLED` is activated we want to be able to
    see the loaded `nfts` on the `NFTs` tab.
    
    # Changes
    
    - displays loaded `nfts`
    
    # Tests
    
    **New:**
    
    <img width="881" height="970" alt="image"
    src="https://github.com/user-attachments/assets/15deef30-8f1a-4a4b-a6d3-2e9fc67e1a82"
    />
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

---
+++ src/frontend/src/lib/components/nfts/NftCard.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { nonNullish } from '@dfinity/utils';
+	import NetworkLogo from '$lib/components/networks/NetworkLogo.svelte';
+	import Img from '$lib/components/ui/Img.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
+	import type { Nft } from '$lib/types/nft';
+	import { replacePlaceholders } from '$lib/utils/i18n.utils';
+
+	interface Props {
+		nft: Nft;
+		testId?: string;
+	}
+
+	let { nft, testId }: Props = $props();
+</script>
+
+<div data-tid={testId}>
+	<div class="relative overflow-hidden rounded-lg">
+		{#if nonNullish(nft.imageUrl)}
+			<Img
+				src={nft.imageUrl}
+				alt={replacePlaceholders($i18n.nfts.alt.image, {
+					$tokenId: nft.id.toString()
+				})}
+				testId={`${testId}-image`}
+			/>
+		{:else}
+			<div class="bg-black/16 h-48 rounded-lg" data-tid={`${testId}-placeholder`}></div>
+		{/if}
+
+		<div class="absolute bottom-2 right-2">
+			<NetworkLogo
+				network={nft.contract.network}
+				size="xs"
+				color="white"
+				testId={`${testId}-network`}
+			/>
+		</div>
+	</div>
+
+	<div class="px-2 pt-2">
+		<h3 class="text-xs font-semibold text-tertiary">{nft.contract.name}</h3>
+		<span class="text-xs text-tertiary">{`#${nft.id}`}</span>
+	</div>
+</div>
---
+++ src/frontend/src/lib/components/nfts/NftsDisplayHandler.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { nftStore } from '$lib/stores/nft.store';
+	import type { Nft } from '$lib/types/nft';
+
+	interface Props {
+		children: Snippet;
+		nfts: Nft[];
+	}
+
+	let { children, nfts = $bindable([]) }: Props = $props();
+
+	$effect(() => {
+		nfts = $nftStore ?? [];
+	});
+</script>
+
+{@render children()}
