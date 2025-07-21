8aa9e68dc2d8bcf44a3803c63eb614409cecdf73
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-21 10:03:50 +0200

    fix

--- src/frontend/src/sol/types/spl.ts
+++ src/frontend/src/sol/types/spl.ts
@@ -1,13 +1,17 @@
 import type { SolAddress } from '$lib/types/address';
 import type { RequiredToken, Token } from '$lib/types/token';
+import type { RequiredExcept } from '$lib/types/utils';
 
 export type SplTokenAddress = SolAddress;
+
+interface SplAuthorities {
+	mintAuthority?: SplTokenAddress;
+	freezeAuthority?: SplTokenAddress;
+}
 
 export type SplToken = Token & {
 	address: SplTokenAddress;
 	owner: SplTokenAddress;
-	mintAuthority?: SplTokenAddress;
-	freezeAuthority?: SplTokenAddress;
-};
+} & SplAuthorities;
 
-export type RequiredSplToken = RequiredToken<SplToken>;
+export type RequiredSplToken = RequiredExcept<RequiredToken<SplToken>, keyof SplAuthorities>;
