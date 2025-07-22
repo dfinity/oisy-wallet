b586b135d12600d78aca2cd5533662cf38042a93
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-07-22 13:26:50 +0200

    feat(frontend): displays empty nfts list (#7926)
    # Motivation
    
    If no nfts are loaded, instead of displaying an empty list we want to
    display some information.
    
    # Changes
    
    - updates empty nfts list
    
    # Tests
    
    **New:**
    
    <img width="876" height="739" alt="image"
    src="https://github.com/user-attachments/assets/ffc53f3c-8f55-4dd5-a411-98844ede69df"
    />
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

--- src/frontend/src/lib/components/nfts/NftsList.svelte
+++ src/frontend/src/lib/components/nfts/NftsList.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import EmptyNftsList from '$lib/components/nfts/EmptyNftsList.svelte';
 	import NftCard from '$lib/components/nfts/NftCard.svelte';
 	import NftsDisplayHandler from '$lib/components/nfts/NftsDisplayHandler.svelte';
 	import { NFT_CARD } from '$lib/constants/test-ids.constants';
@@ -8,9 +9,13 @@
 </script>
 
 <NftsDisplayHandler bind:nfts>
-	<div class="grid grid-cols-3 gap-4 pt-4">
-		{#each nfts as nft, index (`${nft.id}-${index}`)}
-			<NftCard {nft} testId={`${NFT_CARD}-${nft.id}`} />
-		{/each}
-	</div>
+	{#if nfts.length === 0}
+		<EmptyNftsList />
+	{:else}
+		<div class="grid grid-cols-3 gap-4 pt-4">
+			{#each nfts as nft, index (`${nft.id}-${index}`)}
+				<NftCard {nft} testId={`${NFT_CARD}-${nft.id}`} />
+			{/each}
+		</div>
+	{/if}
 </NftsDisplayHandler>
