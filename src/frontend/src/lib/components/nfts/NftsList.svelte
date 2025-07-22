5c7dcd47bb68eb44383fd3f91637fc0fbae01e5f
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-07-22 10:47:06 +0200

    filters displayed nfts

--- src/frontend/src/lib/components/nfts/NftsList.svelte
+++ src/frontend/src/lib/components/nfts/NftsList.svelte
@@ -3,14 +3,28 @@
 	import NftsDisplayHandler from '$lib/components/nfts/NftsDisplayHandler.svelte';
 	import { NFT_CARD } from '$lib/constants/test-ids.constants';
 	import type { Nft } from '$lib/types/nft';
+	import noNftsBanner from '$lib/assets/nfts/no-nfts-banner.svg'
+	import Img from '$lib/components/ui/Img.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
 
 	let nfts: Nft[] = $state([]);
 </script>
 
 <NftsDisplayHandler bind:nfts>
-	<div class="grid grid-cols-3 gap-4 pt-4">
-		{#each nfts as nft, index (`${nft.id}-${index}`)}
-			<NftCard {nft} testId={`${NFT_CARD}-${nft.id}`} />
-		{/each}
-	</div>
+	{#if nfts.length === 0}
+		<div class="py-10 px-6 flex flex-col items-center gap-5">
+			<Img src={noNftsBanner} alt={$i18n.nfts.alt.image} />
+
+			<div class="flex flex-col items-center gap-2">
+				<h5>{$i18n.nfts.text.title_empty}</h5>
+			</div>
+			<span class="text-tertiary">{$i18n.nfts.text.description_empty}</span>
+		</div>
+	{:else}
+		<div class="grid grid-cols-3 gap-4 pt-4">
+			{#each nfts as nft, index (`${nft.id}-${index}`)}
+				<NftCard {nft} testId={`${NFT_CARD}-${nft.id}`} />
+			{/each}
+		</div>
+	{/if}
 </NftsDisplayHandler>
