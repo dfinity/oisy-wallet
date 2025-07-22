7a7135b87c01c5243fdbf736abf7dce5cd9ee6d8
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-07-22 14:07:52 +0200

    only displays nfts of selected networks

--- src/frontend/src/lib/derived/network-tokens.derived.ts
+++ src/frontend/src/lib/derived/network-tokens.derived.ts
@@ -1,11 +1,12 @@
 import { exchanges } from '$lib/derived/exchange.derived';
 import { pseudoNetworkChainFusion, selectedNetwork } from '$lib/derived/network.derived';
-import { enabledFungibleTokens, tokensToPin } from '$lib/derived/tokens.derived';
+import { enabledFungibleTokens, enabledNonFungibleTokens, tokensToPin } from '$lib/derived/tokens.derived';
 import { balancesStore } from '$lib/stores/balances.store';
 import type { Token, TokenUi } from '$lib/types/token';
 import { filterTokensForSelectedNetwork } from '$lib/utils/network.utils';
 import { pinTokensWithBalanceAtTop, sortTokens } from '$lib/utils/tokens.utils';
 import { derived, type Readable } from 'svelte/store';
+import type { Erc721Token } from '$eth/types/erc721';
 
 /**
  * All user-enabled fungible tokens matching the selected network or chain fusion.
@@ -13,6 +14,14 @@ import { derived, type Readable } from 'svelte/store';
 // TODO: Create tests for this store
 export const enabledFungibleNetworkTokens: Readable<Token[]> = derived(
 	[enabledFungibleTokens, selectedNetwork, pseudoNetworkChainFusion],
+	filterTokensForSelectedNetwork
+);
+
+/**
+ * All user-enabled non-fungible tokens matching the selected network or chain fusion.
+ */
+export const enabledNonFungibleNetworkTokens: Readable<Erc721Token[]> = derived(
+	[enabledNonFungibleTokens, selectedNetwork, pseudoNetworkChainFusion],
 	filterTokensForSelectedNetwork
 );
 
