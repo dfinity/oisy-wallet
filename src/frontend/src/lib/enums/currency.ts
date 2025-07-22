1bade3ffb747fde868506f774d1ed1a20abaf748
Stefan Berger <stefan.berger@inacta.ch>
2025-07-22 06:23:05 +0200

    add some more

--- src/frontend/src/lib/enums/currency.ts
+++ src/frontend/src/lib/enums/currency.ts
@@ -3,12 +3,15 @@
 export enum Currency {
 	AED = 'aed',
 	BRL = 'brl',
+	CAD = 'cad',
 	CHF = 'chf',
 	CNY = 'cny',
 	EUR = 'eur',
 	GBP = 'gbp',
 	INR = 'inr',
 	JPY = 'jpy',
+	KRW = 'krw',
+	NGN = 'ngn',
 	SAR = 'sar',
 	SGD = 'sgd',
 	USD = 'usd'
