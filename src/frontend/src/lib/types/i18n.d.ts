cf6adb2a1092a3d0019715e9265b55931154823d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-23 10:06:58 +0200

    chore(frontend): Remove duplicate error message for importing tokens (#7950)
    # Motivation
    
    There is a duplicate message for errors in importing tokens.

--- src/frontend/src/lib/types/i18n.d.ts
+++ src/frontend/src/lib/types/i18n.d.ts
@@ -819,7 +819,6 @@ interface I18nTokens {
 		unexpected: string;
 		unexpected_hiding: string;
 		already_available: string;
-		loading_metadata: string;
 		not_toggleable: string;
 		incomplete_metadata: string;
 		duplicate_metadata: string;
