f0b67bcb281fc6512f476b8a7e911775f568ef47
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-24 16:35:49 +0200

    fix

--- src/frontend/src/tests/lib/utils/nft.utils.spec.ts
+++ src/frontend/src/tests/lib/utils/nft.utils.spec.ts
@@ -6,20 +6,18 @@ import { SUPPORTED_ETHEREUM_TOKENS } from '$env/tokens/tokens.eth.env';
 import { ICP_TOKEN } from '$env/tokens/tokens.icp.env';
 import { SUPPORTED_SOLANA_TOKENS } from '$env/tokens/tokens.sol.env';
 import { SPL_TOKENS } from '$env/tokens/tokens.spl.env';
-import { isTokenErc } from '$eth/utils/erc.utils';
+import { isTokenFungible, isTokenNonFungible } from '$lib/utils/nft.utils';
 import { MOCK_ERC1155_TOKENS } from '$tests/mocks/erc1155-tokens.mock';
 import { MOCK_ERC721_TOKENS } from '$tests/mocks/erc721-tokens.mock';
 
 describe('erc.utils', () => {
-	describe('isTokenErc', () => {
-		it.each([
-			...ERC20_TWIN_TOKENS,
-			...EVM_ERC20_TOKENS,
-			...MOCK_ERC721_TOKENS,
-			...MOCK_ERC1155_TOKENS
-		])('should return true for token $name', (token) => {
-			expect(isTokenErc(token)).toBeTruthy();
-		});
+	describe('isTokenNonFungible', () => {
+		it.each([...MOCK_ERC721_TOKENS, ...MOCK_ERC1155_TOKENS])(
+			'should return true for token $name',
+			(token) => {
+				expect(isTokenNonFungible(token)).toBeTruthy();
+			}
+		);
 
 		it.each([
 			ICP_TOKEN,
@@ -27,9 +25,33 @@ describe('erc.utils', () => {
 			...SUPPORTED_ETHEREUM_TOKENS,
 			...SUPPORTED_EVM_TOKENS,
 			...SUPPORTED_SOLANA_TOKENS,
-			...SPL_TOKENS
+			...SPL_TOKENS,
+			...ERC20_TWIN_TOKENS,
+			...EVM_ERC20_TOKENS
 		])('should return false for token $name', (token) => {
-			expect(isTokenErc(token)).toBeFalsy();
+			expect(isTokenNonFungible(token)).toBeFalsy();
+		});
+	});
+
+	describe('isTokenFungible', () => {
+		it.each([...ERC20_TWIN_TOKENS, ...EVM_ERC20_TOKENS])(
+			'should return true for token $name',
+			(token) => {
+				expect(isTokenFungible(token)).toBeTruthy();
+			}
+		);
+
+		it.each([
+			ICP_TOKEN,
+			...SUPPORTED_BITCOIN_TOKENS,
+			...SUPPORTED_ETHEREUM_TOKENS,
+			...SUPPORTED_EVM_TOKENS,
+			...SUPPORTED_SOLANA_TOKENS,
+			...SPL_TOKENS,
+			...MOCK_ERC721_TOKENS,
+			...MOCK_ERC1155_TOKENS
+		])('should return false for token $name', (token) => {
+			expect(isTokenFungible(token)).toBeFalsy();
 		});
 	});
 });
