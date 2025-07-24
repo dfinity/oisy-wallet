e120f5ab089ab8bf8b8fb13d27d99d7f160fbcad
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-24 17:47:12 +0200

    feat(frontend): Use generic transactions store for Ethereum (#7987)
    # Motivation
    
    We can finally use the existing generic transactions store for
    Ethereum/EVMs.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/eth/stores/eth-transactions.store.ts
+++ src/frontend/src/eth/stores/eth-transactions.store.ts
@@ -1,24 +1,10 @@
 import type { CertifiedStoreData } from '$lib/stores/certified.store';
-import type { TransactionsData, TransactionsStore } from '$lib/stores/transactions.store';
-import type { CertifiedData } from '$lib/types/store';
-import type { TokenId } from '$lib/types/token';
+import {
+	initTransactionsStore,
+	type CertifiedTransaction,
+	type TransactionsData
+} from '$lib/stores/transactions.store';
 import type { Transaction } from '$lib/types/transaction';
-import { nonNullish } from '@dfinity/utils';
-import { writable } from 'svelte/store';
-
-type TransactionTypes = Transaction;
-
-type CertifiedTransaction<T extends TransactionTypes> = CertifiedData<T>;
-
-interface TransactionsStoreParams<T extends TransactionTypes> {
-	tokenId: TokenId;
-	transactions: CertifiedTransaction<T>[];
-}
-
-interface EthTransactionsStore<T extends TransactionTypes>
-	extends Omit<TransactionsStore<T>, 'prepend' | 'append' | 'cleanUp'> {
-	resetAll: () => void;
-}
 
 export type EthCertifiedTransaction = CertifiedTransaction<Transaction>;
 
@@ -26,51 +12,4 @@ export type EthTransactionsData = TransactionsData<Transaction>;
 
 export type EthCertifiedTransactionsData = CertifiedStoreData<EthTransactionsData>;
 
-const initEthTransactionsStore = (): EthTransactionsStore<Transaction> => {
-	const INITIAL: EthCertifiedTransactionsData = {} as EthCertifiedTransactionsData;
-
-	const { subscribe, update, set } = writable<EthCertifiedTransactionsData>(INITIAL);
-
-	return {
-		set: ({ tokenId, transactions }: TransactionsStoreParams<Transaction>) =>
-			update((state) => ({
-				...(nonNullish(state) && state),
-				[tokenId]: transactions
-			})),
-		add: ({ tokenId, transactions }: TransactionsStoreParams<Transaction>) =>
-			update((state) => ({
-				...(nonNullish(state) && state),
-				[tokenId]: [...(state?.[tokenId] ?? []), ...transactions]
-			})),
-		update: ({
-			tokenId,
-			transaction
-		}: {
-			tokenId: TokenId;
-			transaction: CertifiedTransaction<Transaction>;
-		}) =>
-			update((state) => ({
-				...(nonNullish(state) && state),
-				[tokenId]: [
-					...(state?.[tokenId] ?? []).filter(
-						({ data: { hash } }) => hash !== transaction.data.hash
-					),
-					transaction
-				]
-			})),
-		nullify: (tokenId) =>
-			update((state) => ({
-				...(nonNullish(state) && state),
-				[tokenId]: null
-			})),
-		reset: (id: TokenId) =>
-			update((state) => ({
-				...(nonNullish(state) && state),
-				[id]: null
-			})),
-		resetAll: () => set(INITIAL),
-		subscribe
-	};
-};
-
-export const ethTransactionsStore = initEthTransactionsStore();
+export const ethTransactionsStore = initTransactionsStore<Transaction>();
