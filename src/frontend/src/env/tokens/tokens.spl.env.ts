52dba381e1af81faae785780cfdb677a871eda43
Stefan Berger <stefan.berger@inacta.ch>
2025-07-25 07:42:08 +0200

    Update tokens.spl.env.ts

--- src/frontend/src/env/tokens/tokens.spl.env.ts
+++ src/frontend/src/env/tokens/tokens.spl.env.ts
@@ -5,6 +5,7 @@ import { AMZNX_TOKEN } from '$env/tokens/tokens-spl/tokens.amznx.env';
 import { BONK_TOKEN } from '$env/tokens/tokens-spl/tokens.bonk.env';
 import { COINX_TOKEN } from '$env/tokens/tokens-spl/tokens.coinx.env';
 import { CRCLX_TOKEN } from '$env/tokens/tokens-spl/tokens.crclx.env';
+import { DFDVX_TOKEN } from '$env/tokens/tokens-spl/tokens.dfdvx.env';
 import { DEVNET_EURC_TOKEN, EURC_TOKEN } from '$env/tokens/tokens-spl/tokens.eurc.env';
 import { GLDX_TOKEN } from '$env/tokens/tokens-spl/tokens.gldx.env';
 import { GMEX_TOKEN } from '$env/tokens/tokens-spl/tokens.gmex.env';
@@ -41,6 +42,7 @@ const SPL_TOKENS_MAINNET: RequiredSplToken[] = [
 	BONK_TOKEN,
 	COINX_TOKEN,
 	CRCLX_TOKEN,
+	DFDVX_TOKEN
 	EURC_TOKEN,
 	GLDX_TOKEN,
 	GMEX_TOKEN,
