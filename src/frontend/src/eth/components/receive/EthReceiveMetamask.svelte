10cd8b57bc6248f739992f29ff26c15bfc085df0
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-25 09:52:09 +0200

    feat(frontend): Replace standard of token store with page-token store

--- src/frontend/src/eth/components/receive/EthReceiveMetamask.svelte
+++ src/frontend/src/eth/components/receive/EthReceiveMetamask.svelte
@@ -7,9 +7,9 @@
 	import Button from '$lib/components/ui/Button.svelte';
 	import { ethAddress } from '$lib/derived/address.derived';
 	import { networkEthereum } from '$lib/derived/network.derived';
-	import { tokenStandard } from '$lib/derived/token.derived';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { toastsError } from '$lib/stores/toasts.store';
+	import {pageTokenStandard} from "$lib/derived/page-token.derived";
 
 	const receiveModal = async () => {
 		if (!$metamaskAvailable) {
@@ -31,7 +31,7 @@
 	// TODO: The Metamask button currently does not support sending ERC20 tokens - it always populates an ETH transaction.
 	// We aim to fix this, but for now, the functionality is commented out.
 	let tokenStandardEth = true;
-	$: tokenStandardEth = $tokenStandard === 'ethereum';
+	$: tokenStandardEth = $pageTokenStandard === 'ethereum';
 </script>
 
 {#if $metamaskAvailable && $networkEthereum && tokenStandardEth}
