314178f7d4332037d878c5316602089ce86a1985
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-28 10:59:32 +0200

    feat(frontend): Migrate `FeeDisplay` component to Svelte 5 (#8020)
    # Motivation
    
    Migrating `FeeDisplay` (and its direct children) component to Svelte 5.

--- src/frontend/src/eth/components/fee/EthFeeDisplay.svelte
+++ src/frontend/src/eth/components/fee/EthFeeDisplay.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
 	import { nonNullish } from '@dfinity/utils';
-	import { getContext } from 'svelte';
+	import { getContext, type Snippet } from 'svelte';
 	import { ETH_FEE_CONTEXT_KEY, type EthFeeContext } from '$eth/stores/eth-fee.store';
 	import FeeDisplay from '$lib/components/fee/FeeDisplay.svelte';
+
+	interface Props {
+		label?: Snippet;
+	}
+
+	let { label }: Props = $props();
 
 	const { maxGasFee, feeSymbolStore, feeDecimalsStore, feeExchangeRateStore }: EthFeeContext =
 		getContext<EthFeeContext>(ETH_FEE_CONTEXT_KEY);
@@ -14,7 +20,6 @@
 		decimals={$feeDecimalsStore}
 		symbol={$feeSymbolStore}
 		exchangeRate={$feeExchangeRateStore}
-	>
-		<slot slot="label" name="label" />
-	</FeeDisplay>
+		{label}
+	/>
 {/if}
