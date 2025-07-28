32df847c707740742dd1acba2e816c3d81e8875d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-28 18:12:37 +0200

    feat(frontend): Track BTC send flow events (#8032)
    # Motivation
    
    We would like to understand how much is the Bitcoin send flow used.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/btc/components/send/BtcSendTokenWizard.svelte
+++ src/frontend/src/btc/components/send/BtcSendTokenWizard.svelte
@@ -9,6 +9,10 @@
 	import type { UtxosFee } from '$btc/types/btc-send';
 	import ButtonBack from '$lib/components/ui/ButtonBack.svelte';
 	import {
+		TRACK_COUNT_BTC_SEND_ERROR,
+		TRACK_COUNT_BTC_SEND_SUCCESS
+	} from '$lib/constants/analytics.contants';
+	import {
 		btcAddressMainnet,
 		btcAddressRegtest,
 		btcAddressTestnet
@@ -16,6 +20,7 @@
 	import { authIdentity } from '$lib/derived/auth.derived';
 	import { ProgressStepsSendBtc } from '$lib/enums/progress-steps';
 	import { WizardStepsSend } from '$lib/enums/wizard-steps';
+	import { trackEvent } from '$lib/services/analytics.services';
 	import { nullishSignOut } from '$lib/services/auth.services';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { SEND_CONTEXT_KEY, type SendContext } from '$lib/stores/send.store';
@@ -121,10 +126,26 @@
 				}
 			});
 
+			trackEvent({
+				name: TRACK_COUNT_BTC_SEND_SUCCESS,
+				metadata: {
+					token: $sendToken.symbol,
+					network: `${networkId.description}`
+				}
+			});
+
 			sendProgressStep = ProgressStepsSendBtc.DONE;
 
 			setTimeout(() => close(), 750);
 		} catch (err: unknown) {
+			trackEvent({
+				name: TRACK_COUNT_BTC_SEND_ERROR,
+				metadata: {
+					token: $sendToken.symbol,
+					network: `${networkId.description}`
+				}
+			});
+
 			toastsError({
 				msg: { text: $i18n.send.error.unexpected },
 				err
--- src/frontend/src/lib/constants/analytics.contants.ts
+++ src/frontend/src/lib/constants/analytics.contants.ts
@@ -11,6 +11,10 @@ export const TRACK_HELP_CONCERNED_ABOUT_SECURITY = 'help_concerned_about_securit
 export const TRACK_HELP_GOT_CONFUSED = 'help_got_confused';
 export const TRACK_HELP_OTHER = 'help_other';
 
+// Bitcoin
+export const TRACK_COUNT_BTC_SEND_SUCCESS = 'btc_send_success';
+export const TRACK_COUNT_BTC_SEND_ERROR = 'btc_send_error';
+
 // Ethereum
 export const TRACK_COUNT_ETH_SEND_SUCCESS = 'eth_send_success';
 export const TRACK_COUNT_ETH_SEND_ERROR = 'eth_send_error';
