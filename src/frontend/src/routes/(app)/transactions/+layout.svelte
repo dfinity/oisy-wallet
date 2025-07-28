13a36bdfe2310a2bbd33225a9b186e437834fe97
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-07-28 10:50:20 +0200

    feat(frontend): Migrate all layout components to Svelte 5 (#8021)
    # Motivation
    
    Migrating all layout components and pages to Svelte 5.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/routes/(app)/transactions/+layout.svelte
+++ src/frontend/src/routes/(app)/transactions/+layout.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
+	import type { Snippet } from 'svelte';
 	import Listener from '$lib/components/core/Listener.svelte';
 	import { token } from '$lib/stores/token.store';
+
+	interface Props {
+		children: Snippet;
+	}
+
+	let { children }: Props = $props();
 </script>
 
 <Listener token={$token}>
-	<slot />
+	{@render children()}
 </Listener>
--- src/frontend/src/routes/+layout.svelte
+++ src/frontend/src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { Spinner, Toasts, SystemThemeListener } from '@dfinity/gix-components';
 	import { nonNullish } from '@dfinity/utils';
-	import { onMount } from 'svelte';
+	import { onMount, type Snippet } from 'svelte';
 	import { fade } from 'svelte/transition';
 	import { browser } from '$app/environment';
 	import Banner from '$lib/components/core/Banner.svelte';
@@ -19,6 +19,12 @@
 	import '$lib/styles/global.scss';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { toastsError } from '$lib/stores/toasts.store';
+
+	interface Props {
+		children: Snippet;
+	}
+
+	let { children }: Props = $props();
 
 	/**
 	 * Init dApp
@@ -68,11 +74,14 @@
 	 * Workers
 	 */
 
-	let worker: { syncAuthIdle: (auth: AuthStoreData) => void } | undefined;
+	let worker = $state<{ syncAuthIdle: (auth: AuthStoreData) => void } | undefined>();
 
 	onMount(async () => (worker = await initAuthWorker()));
 
-	$: (worker, $authStore, (() => worker?.syncAuthIdle($authStore))());
+	$effect(() => {
+		[worker, $authStore];
+		worker?.syncAuthIdle($authStore);
+	});
 
 	/**
 	 * UI loader
@@ -80,7 +89,7 @@
 
 	// To improve the UX while the app is loading on mainnet we display a spinner which is attached statically in the index.html files.
 	// Once the authentication has been initialized we know most JavaScript resources has been loaded and therefore we can hide the spinner, the loading information.
-	$: (() => {
+	$effect(() => {
 		if (!browser) {
 			return;
 		}
@@ -92,17 +101,17 @@
 
 		const spinner = document.querySelector('body > #app-spinner');
 		spinner?.remove();
-	})();
+	});
 </script>
 
-<svelte:window on:storage={syncAuthStore} />
+<svelte:window onstorage={syncAuthStore} />
 
 {#await init()}
 	<div in:fade>
 		<Spinner />
 	</div>
 {:then _}
-	<slot />
+	{@render children()}
 {/await}
 
 <Banner />
