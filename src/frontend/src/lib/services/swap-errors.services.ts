b94bca43ef0daa45f11dff1976529170bf5ab808
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-07-30 15:57:55 +0200

    feat(frontend): adjusted swapError to have optional message and variant

--- src/frontend/src/lib/services/swap-errors.services.ts
+++ src/frontend/src/lib/services/swap-errors.services.ts
@@ -1,21 +1,27 @@
-import type { SwapErrorKey } from '$lib/types/swap';
+import type { SwapErrorCodes } from '$lib/types/swap';
 
 export const throwSwapError = ({
 	code,
-	message
+	message = undefined,
+	variant = undefined
 }: {
-	code: SwapErrorKey;
-	message: string;
+	code: SwapErrorCodes;
+	message?: string;
+	variant?: 'error' | 'warning' | 'info';
 }): never => {
-	throw new SwapError(code, message);
+	throw new SwapError(code, message, variant);
 };
 
 export class SwapError extends Error {
+	public readonly variant?: 'error' | 'warning' | 'info';
+
 	constructor(
-		public readonly code: SwapErrorKey,
-		message: string
+		public readonly code: SwapErrorCodes,
+		message?: string,
+		variant?: 'error' | 'warning' | 'info'
 	) {
 		super(message);
 		this.name = 'SwapError';
+		this.variant = variant;
 	}
 }
