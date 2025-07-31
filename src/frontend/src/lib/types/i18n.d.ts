4049794957f392a541faa52a9a168206680fc31a
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-07-31 14:36:53 +0200

    feat(frontend): adjusted SwapReview errorMessage statuses (#8104)
    # Motivation
    
    Adjusted error message handling and UI feedback in the SwapReview
    component.
    
    # Changes
    
    - Added new error messages for SwapReview
    - Introduced new button statuses in the SwapReview component

--- src/frontend/src/lib/types/i18n.d.ts
+++ src/frontend/src/lib/types/i18n.d.ts
@@ -704,6 +704,8 @@ interface I18nSwap {
 		withdraw_failed_second_part: string;
 		swap_failed_instruction_link: string;
 		manually_withdraw_success: string;
+		manually_withdraw_failed: string;
+		swap_sucess_withdraw_failed: string;
 		swap_sucess_manually_withdraw_success: string;
 	};
 }
