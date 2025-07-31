37ca13e8758982cbd6a3d1c8781e6cb814e8e38b
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-07-31 17:36:28 +0200

    feat(frontend): added new swapSucceded property to ICP Swap Error (#8112)
    # Motivation
    
    We need to track the status of swapSucceeded to determine whether to
    withdraw the destination or source token after a failed swap.
    
    # Changes
    
    Added swapSucceeded property to SwapError for improved error context
    
    # Tests
    
    Added unit tests

--- src/frontend/src/lib/services/swap-errors.services.ts
+++ src/frontend/src/lib/services/swap-errors.services.ts
@@ -3,25 +3,30 @@ import type { SwapErrorCodes } from '$lib/types/swap';
 export const throwSwapError = ({
 	code,
 	message,
-	variant
+	variant,
+	swapSucceded
 }: {
 	code: SwapErrorCodes;
 	message?: string;
 	variant?: 'error' | 'warning' | 'info';
+	swapSucceded?: boolean;
 }): never => {
-	throw new SwapError(code, message, variant);
+	throw new SwapError(code, message, variant, swapSucceded);
 };
 
 export class SwapError extends Error {
 	public readonly variant?: 'error' | 'warning' | 'info';
+	public readonly swapSucceded?: boolean;
 
 	constructor(
 		public readonly code: SwapErrorCodes,
 		message?: string,
-		variant?: 'error' | 'warning' | 'info'
+		variant?: 'error' | 'warning' | 'info',
+		swapSucceded?: boolean
 	) {
 		super(message);
 		this.name = 'SwapError';
 		this.variant = variant;
+		this.swapSucceded = swapSucceded;
 	}
 }
