c10990c227bc788ec64f15ec9c339b12f9b72cd0
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-08-02 07:59:41 +0200

    more

--- src/frontend/src/lib/utils/env.utils.ts
+++ src/frontend/src/lib/utils/env.utils.ts
@@ -1,5 +1,4 @@
 import type { OptionString } from '$lib/types/string';
-import { isEmptyString } from '@dfinity/utils';
 
 // Since the env variables are defined as `any` type, there is the risk that passing the simple variable it is accepted as object.
 // Example: `parseBoolEnvVar(import.meta.env.ENV_VARIABLE)` is type-accepted even if it should be `parseBoolEnvVar({ value: import.meta.env.ENV_VARIABLE })`
@@ -7,7 +6,7 @@ import { isEmptyString } from '@dfinity/utils';
 // eslint-disable-next-line local-rules/prefer-object-params
 export const parseBoolEnvVar = (value: OptionString, check = true): boolean => {
 	const normalised = value?.toLowerCase();
-	if (isEmptyString(normalised)) {
+	if (normalised === '') {
 		// Try to extract the source location for better debugging
 		const { stack } = new Error();
 		const callerLine = stack?.split('\n')[2] ?? '';
