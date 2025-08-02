9e43c75b091995c6bca809b9c05726415180d00a
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-08-02 07:33:08 +0200

    use env var

--- src/frontend/src/lib/components/core/Menu.svelte
+++ src/frontend/src/lib/components/core/Menu.svelte
@@ -49,6 +49,7 @@
 		isRouteSettings
 	} from '$lib/utils/nav.utils';
 	import { setPrivacyMode } from '$lib/utils/privacy.utils';
+	import { TEST_ENV_VAR } from '$env/exchange.env';
 
 	let visible = $state(false);
 	let button = $state<HTMLButtonElement | undefined>();
@@ -209,7 +210,7 @@
 		<Hr />
 
 		<div class="flex max-w-80 flex-col gap-3 pt-3">
-			{#if LOCK_SCREEN_ENABLED}
+			{#if LOCK_SCREEN_ENABLED || TEST_ENV_VAR}
 				<LockOrSignOut onHidePopover={hidePopover} />
 			{:else}
 				<SignOut on:icLogoutTriggered={hidePopover} />
