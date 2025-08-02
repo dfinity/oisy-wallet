e431564b3824b2fc638332d0434392e6bcdce3e6
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-08-02 07:27:17 +0200

    use env var

--- src/frontend/src/env/exchange.env.ts
+++ src/frontend/src/env/exchange.env.ts
@@ -3,3 +3,5 @@ import { parseBoolEnvVar } from '$lib/utils/env.utils';
 export const EXCHANGE_DISABLED = parseBoolEnvVar(import.meta.env.VITE_EXCHANGE_DISABLED);
 
 export const TEST_ENV_VAR = parseBoolEnvVar(import.meta.env.VITE_TEST_ENV_VAR);
+
+console.log('TEST_ENV_VAR', TEST_ENV_VAR);
