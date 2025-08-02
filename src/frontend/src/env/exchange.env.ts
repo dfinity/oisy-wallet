42baddcd2138228f597a697d388a1e75d2f4c06d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-08-02 07:26:29 +0200

    fix(devops): Correct import of env variable if there is no secret

--- src/frontend/src/env/exchange.env.ts
+++ src/frontend/src/env/exchange.env.ts
@@ -1,3 +1,5 @@
 import { parseBoolEnvVar } from '$lib/utils/env.utils';
 
 export const EXCHANGE_DISABLED = parseBoolEnvVar(import.meta.env.VITE_EXCHANGE_DISABLED);
+
+export const TEST_ENV_VAR = parseBoolEnvVar(import.meta.env.VITE_TEST_ENV_VAR);
