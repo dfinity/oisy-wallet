80995941c986bffa7480845279a28eef71ebb159
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-08-02 07:34:30 +0200

    another test

--- src/frontend/src/env/exchange.env.ts
+++ src/frontend/src/env/exchange.env.ts
@@ -2,6 +2,10 @@ import { parseBoolEnvVar } from '$lib/utils/env.utils';
 
 export const EXCHANGE_DISABLED = parseBoolEnvVar(import.meta.env.VITE_EXCHANGE_DISABLED);
 
-export const TEST_ENV_VAR = parseBoolEnvVar(import.meta.env.VITE_TEST_ENV_VAR);
+export const TEST_ENV_VAR = true;
 
-console.log('TEST_ENV_VAR', TEST_ENV_VAR);
+console.log(
+	'TEST_ENV_VAR',
+	import.meta.env.VITE_TEST_ENV_VAR,
+	typeof import.meta.env.VITE_TEST_ENV_VAR
+);
