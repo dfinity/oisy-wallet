c14f088f97dd9f6bb5f0c01d8dfba469aca952f2
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-08-03 07:20:16 +0200

    more

--- src/frontend/src/tests/sol/services/sol-signatures.services.integration.spec.ts
+++ src/frontend/src/tests/sol/services/sol-signatures.services.integration.spec.ts
@@ -1,4 +1,8 @@
 import { ALCHEMY_API_KEY } from '$env/rest/alchemy.env';
+import { BONK_TOKEN } from '$env/tokens/tokens-spl/tokens.bonk.env';
+import { JUP_TOKEN } from '$env/tokens/tokens-spl/tokens.jup.env';
+import { NVDAX_TOKEN } from '$env/tokens/tokens-spl/tokens.nvdax.env';
+import { POPCAT_TOKEN } from '$env/tokens/tokens-spl/tokens.popcat.env';
 import { USDC_TOKEN } from '$env/tokens/tokens-spl/tokens.usdc.env';
 import { last } from '$lib/utils/array.utils';
 import { loadSolLamportsBalance, loadTokenBalance } from '$sol/api/solana.api';
@@ -29,6 +33,26 @@ describe('sol-signatures.services integration', () => {
 				address: '7q6RDbnn2SWnvews2qYCCAMCZzntDLM8scJfUEBmEMf1',
 				ataAddress: '7xSNhASWK77oZtPyVQf1HFUXU1xxXjqkpkxVTULBmcMD',
 				token: USDC_TOKEN
+			},
+			{
+				address: '7q6RDbnn2SWnvews2qYCCAMCZzntDLM8scJfUEBmEMf1',
+				ataAddress: 'CTZFRs7fNtgEastyD6XoBsc9ySDuZTtuRvaFbH86WRb',
+				token: BONK_TOKEN
+			},
+			{
+				address: '7q6RDbnn2SWnvews2qYCCAMCZzntDLM8scJfUEBmEMf1',
+				ataAddress: '8E2dYm7NXBSb7qS3zRtxKDw7mjzPCqVKQCy2EDeujUC2',
+				token: POPCAT_TOKEN
+			},
+			{
+				address: '7q6RDbnn2SWnvews2qYCCAMCZzntDLM8scJfUEBmEMf1',
+				ataAddress: '7t8FwZRQTRaqtaeGLGij4FBsH9t9Cf918inTz3BYpuA7',
+				token: JUP_TOKEN
+			},
+			{
+				address: '5Dqoon9MdWRgwmJ839FJ2ZTpTAcc1MMprZeNyaxpaV1Q',
+				ataAddress: 'EF7heUqwgeSQ153PAdH9fR3tXn8QzbnMCdbshdpraFnA',
+				token: NVDAX_TOKEN
 			}
 		];
 
