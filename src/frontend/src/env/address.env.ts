2ab06554930f923b1d4d00f72e17c30432a02a2e
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2025-08-04 16:02:41 +0200

    chore(frontend): test

--- src/frontend/src/env/address.env.ts
+++ src/frontend/src/env/address.env.ts
@@ -1,6 +1,2 @@
-import { parseBoolEnvVar } from '$lib/utils/env.utils';
-
 // TODO: to be removed when the feature is fully implemented
-export const FRONTEND_DERIVATION_ENABLED = parseBoolEnvVar(
-	import.meta.env.VITE_FRONTEND_DERIVATION_ENABLED
-);
+export const FRONTEND_DERIVATION_ENABLED = true;
