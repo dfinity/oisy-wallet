73465bedc0555eb99cbe4d5af1ca54f899b2d9f1
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-08-05 18:28:18 +0200

    fix

--- src/frontend/src/lib/components/swap/SwapForm.svelte
+++ src/frontend/src/lib/components/swap/SwapForm.svelte
@@ -214,7 +214,7 @@
 
 				<svelte:fragment slot="amount-info">
 					{#if nonNullish($destinationToken)}
-						{#if (isNullish($swapAmountsStore) || $swapAmountsStore.swaps.length === 0) && !swapAmountsLoading}
+						{#if isNullish($swapAmountsStore) && swapAmountsLoading}
 							<div transition:slide={SLIDE_DURATION} class="text-error-primary"
 								>{$i18n.swap.text.swap_is_not_offered}</div
 							>
