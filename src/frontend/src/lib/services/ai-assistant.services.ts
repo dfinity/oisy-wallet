c2f2bfa487ae164c42e3144f1dc252f3a37caf5e
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-08-06 18:03:49 +0200

    feat(frontend): implement askLlmToFilterContacts method (#8169)
    # Motivation
    
    We need to implement a method that is going to request a semantical
    contacts filter from LLM. It filters out all sensitive contacts info
    (addresses) before sending it to AI. After LLM replies, we parse the
    response to ContactUi[].

--- src/frontend/src/lib/services/ai-assistant.services.ts
+++ src/frontend/src/lib/services/ai-assistant.services.ts
@@ -1,9 +1,25 @@
 import type { chat_message_v1 } from '$declarations/llm/llm.did';
 import { llmChat } from '$lib/api/llm.api';
-import { AI_ASSISTANT_LLM_MODEL } from '$lib/constants/ai-assistant.constants';
-import type { ChatMessageContent } from '$lib/types/ai-assistant';
+import {
+	AI_ASSISTANT_FILTER_CONTACTS_PROMPT,
+	AI_ASSISTANT_LLM_MODEL,
+	AI_ASSISTANT_SYSTEM_PROMPT,
+	AI_ASSISTANT_TOOLS
+} from '$lib/constants/ai-assistant.constants';
+import { extendedAddressContacts as extendedAddressContactsStore } from '$lib/derived/contacts.derived';
+import type {
+	AiAssistantContactUi,
+	ChatMessageContent,
+	ToolCallArgument
+} from '$lib/types/ai-assistant';
+import type { ContactUi } from '$lib/types/contact';
+import {
+	parseFromAiAssistantContacts,
+	parseToAiAssistantContacts
+} from '$lib/utils/ai-assistant.utils';
 import type { Identity } from '@dfinity/agent';
-import { fromNullable, toNullable } from '@dfinity/utils';
+import { fromNullable, jsonReplacer, toNullable } from '@dfinity/utils';
+import { get } from 'svelte/store';
 
 /**
  * Gets a text or a tool response from LLM for the provided messages array.
@@ -40,4 +56,59 @@ export const askLlm = async ({
 			results: []
 		}
 	};
+};
+
+/**
+ * Makes a call to LLM to get a semantically filtered contacts.
+ *
+ * @async
+ * @param {Object} params - The parameters required to initiate a filter contacts LLM request.
+ * @param {Identity} params.identity - The user's identity for authentication.
+ * @param {Array} params.filterParams - Array of filter arguments based on which the search will be done by LLM.
+ * @returns {Promise<ChatMessageContent>} - Resolves with an array of filtered contacts.
+ */
+export const askLlmToFilterContacts = async ({
+	identity,
+	filterParams
+}: {
+	identity: Identity;
+	filterParams: ToolCallArgument[];
+}): Promise<ContactUi[]> => {
+	const extendedAddressContacts = get(extendedAddressContactsStore);
+	const aiAssistantContacts = parseToAiAssistantContacts(extendedAddressContacts);
+
+	const {
+		message: { content }
+	} = await llmChat({
+		request: {
+			model: AI_ASSISTANT_LLM_MODEL,
+			messages: [
+				{
+					system: {
+						content: AI_ASSISTANT_SYSTEM_PROMPT
+					}
+				},
+				{
+					user: {
+						content: `
+							${AI_ASSISTANT_FILTER_CONTACTS_PROMPT}
+								
+							Contacts: ${JSON.stringify(aiAssistantContacts, jsonReplacer)}
+							Arguments: "${JSON.stringify(filterParams)}"
+						`
+					}
+				}
+			],
+			tools: toNullable(AI_ASSISTANT_TOOLS)
+		},
+		identity
+	});
+
+	const filteredAiAssistantContacts: AiAssistantContactUi[] =
+		JSON.parse(fromNullable(content) ?? '', jsonReplacer)?.contacts ?? [];
+
+	return parseFromAiAssistantContacts({
+		aiAssistantContacts: filteredAiAssistantContacts,
+		extendedAddressContacts
+	});
 };
