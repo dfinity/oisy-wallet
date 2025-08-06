7b8f9cb3046ffab60e7fd9c69d9ead02c054a49d
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-08-06 16:37:20 +0200

    introduces collection schema

--- src/frontend/src/tests/lib/components/nfts/NftCard.spec.ts
+++ src/frontend/src/tests/lib/components/nfts/NftCard.spec.ts
@@ -1,6 +1,7 @@
 import NftCard from '$lib/components/nfts/NftCard.svelte';
 import { mockValidErc1155Nft, mockValidErc721Nft } from '$tests/mocks/nfts.mock';
 import { render } from '@testing-library/svelte';
+import { assertNonNullish } from '@dfinity/utils';
 
 describe('NftCard', () => {
 	const testId = 'nft-card';
@@ -30,6 +31,8 @@ describe('NftCard', () => {
 
 		expect(balance).toBeInTheDocument();
 
+		assertNonNullish(mockValidErc1155Nft.contract?.name);
+
 		expect(getByText(mockValidErc1155Nft.contract.name)).toBeInTheDocument();
 		expect(getByText(`#${mockValidErc1155Nft.id}`)).toBeInTheDocument();
 	});
@@ -50,6 +53,8 @@ describe('NftCard', () => {
 		const networkLogo: HTMLDivElement | null = container.querySelector(networkLogoSelector);
 
 		expect(networkLogo).toBeInTheDocument();
+
+		assertNonNullish(mockValidErc721Nft.contract?.name);
 
 		expect(getByText(mockValidErc721Nft.contract.name)).toBeInTheDocument();
 		expect(getByText(`#${mockValidErc721Nft.id}`)).toBeInTheDocument();
