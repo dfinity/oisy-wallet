0de88830b2f5a9bdc5aa690422ef5aee7d5db9b7
Stefan Berger <stefan.berger@inacta.ch>
2025-08-08 16:11:33 +0200

    Adding OneSec ICP on Base

---
+++ src/frontend/src/env/tokens/tokens-evm/tokens-base/tokens-erc20/tokens.icp.env.ts
@@ -0,0 +1,26 @@
+import { BASE_NETWORK } from '$env/networks/networks-evm/networks.evm.base.env';
+import { ICP_TOKEN_GROUP } from '$env/tokens/groups/groups.icp.env';
+import icp from '$icp/assets/icp-dark.png';
+import type { RequiredEvmErc20Token } from '$evm/types/erc20';
+import type { TokenId } from '$lib/types/token';
+import { parseTokenId } from '$lib/validation/token.validation';
+
+export const ICP_DECIMALS = 8;
+
+export const ICP_SYMBOL = 'ICP';
+
+export const ICP_TOKEN_ID: TokenId = parseTokenId(ICP_SYMBOL);
+
+export const ICP_TOKEN: RequiredEvmErc20Token = {
+	id: ICP_TOKEN_ID,
+	network: BASE_NETWORK,
+	standard: 'erc20',
+	category: 'default',
+	name: 'ICP',
+	symbol: ICP_SYMBOL,
+	decimals: ICP_DECIMALS,
+	icon: icp,
+	address: '0x00f3C42833C3170159af4E92dbb451Fb3F708917',
+	exchange: 'erc20',
+	groupData: ICP_TOKEN_GROUP
+};
