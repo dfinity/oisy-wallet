81cccd708b6349b861de8de45107ab2b04a2c15d
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-08-11 17:13:20 +0200

    refactor(frontend): NFT routing (#8213)
    # Motivation
    
    We need to rearrange and extend routing for the home page in preparation
    for the new NFT pages.
    
    # Changes
    
    - Adjust selected condition for Assets main navigation item
    - Make Tokens/NFT tabs use routes instead of internal state
    - Extend routes for NFT and Token list including util functions for it
    - Add new routes and move token list route from root to subroute (and
    add redirects for `/` and `/assets`)
    - Adjust conditions for Hero header in layout
    - Renamed `Tokens` component to `Assets`
    
    # Tests
    
    Added tests for utils functions
    
    
    
    https://github.com/user-attachments/assets/2b2962b7-4b13-48e1-9706-593302f756cd
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/routes/(app)/assets/tokens/+page.svelte
+++ src/frontend/src/routes/(app)/assets/tokens/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import DappsCarousel from '$lib/components/dapps/DappsCarousel.svelte';
-	import Tokens from '$lib/components/tokens/Tokens.svelte';
+	import Assets from '$lib/components/tokens/Assets.svelte';
 	import Responsive from '$lib/components/ui/Responsive.svelte';
 </script>
 
@@ -8,4 +8,4 @@
 	<DappsCarousel wrapperStyleClass="mb-6 flex justify-center xl:hidden" />
 </Responsive>
 
-<Tokens />
+<Assets />
