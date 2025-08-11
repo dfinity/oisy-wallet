8786ec29ec3e57b7474d835d1078c523064b8ec9
Stefan Berger <stefan.berger@inacta.ch>
2025-08-11 10:05:49 +0200

    Create tokens.icp.env.ts

---
+++ src/frontend/src/env/tokens/tokens-erc20/tokens.icp.env.ts
@@ -0,0 +1,24 @@
+import { ETHEREUM_NETWORK } from '$env/networks/networks.eth.env';
+import icp from '$icp/assets/icp-dark.svg';
+import type { RequiredAdditionalErc20Token } from '$eth/types/erc20';
+import type { TokenId } from '$lib/types/token';
+import { parseTokenId } from '$lib/validation/token.validation';
+
+const ICP_DECIMALS = 8;
+
+const ICP_SYMBOL = 'ICP (OneSec)';
+
+export const ICP_TOKEN_ID: TokenId = parseTokenId(ICP_SYMBOL);
+
+export const ICP_TOKEN: RequiredAdditionalErc20Token = {
+	id: ICP_TOKEN_ID,
+	network: ETHEREUM_NETWORK,
+	standard: 'erc20',
+	category: 'default',
+	name: 'ICP',
+	symbol: ICP_SYMBOL,
+	decimals: ICP_DECIMALS,
+	icon: icp,
+	address: '0x00f3C42833C3170159af4E92dbb451Fb3F708917',
+	exchange: 'erc20'
+};
