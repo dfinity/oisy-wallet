96eebd56ad9a7a8ed46d7ef4137c5ba8137bf576
DecentAgeCoder <marco.oesch@dfinity.org>
2025-08-11 09:24:57 +0200

    Moved `invalidSendAmount` utility to BTC-specific module

--- src/frontend/src/lib/utils/input.utils.ts
+++ src/frontend/src/lib/utils/input.utils.ts
@@ -1,4 +1,3 @@
-import { BTC_MINIMUM_AMOUNT } from '$btc/constants/btc.constants';
 import type { OptionAmount } from '$lib/types/send';
 import type { OptionString } from '$lib/types/string';
 import type { Option } from '$lib/types/utils';
@@ -8,6 +7,3 @@ export const isNullishOrEmpty = (value: OptionString): value is Option<''> => is
 
 export const invalidAmount = (amount: OptionAmount): boolean =>
 	isNullish(amount) || Number(amount) < 0;
-
-export const invalidSendAmount = (amount: OptionAmount): boolean =>
-	isNullish(amount) || Number(amount) < BTC_MINIMUM_AMOUNT;
