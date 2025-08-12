c50967aa9e927841368a46d19676b3aac30026f3
daviddecentage <david.farina@dfinity.org>
2025-08-12 14:20:49 +0200

    update

--- src/frontend/src/routes/(app)/+page.ts
+++ src/frontend/src/routes/(app)/+page.ts
@@ -2,5 +2,5 @@ import { AppPath } from '$lib/constants/routes.constants';
 import { redirect } from '@sveltejs/kit';
 
 export const load = () => {
-	throw redirect(308, AppPath.Tokens); // 308 permanent redirect status code
+	throw redirect(308, AppPath.Tokens + window.location.search); // 308 permanent redirect status code
 };
