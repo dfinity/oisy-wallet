6b9f2877090076730a40b72b5ab6b8bf97896168
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-08-14 18:12:16 +0200

    feat(frontend): BreadcrumbNavigation (#8277)
    # Motivation
    
    We need a simple breadcrumb component for the upcoming NFT pages.
    
    # Changes
    
    Added component
    
    # Tests
    
    Added tests
    
    <img width="222" height="62" alt="image"
    src="https://github.com/user-attachments/assets/e4d6a3cc-f382-46b3-9657-51c1b67405e4"
    />
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/frontend/src/lib/components/ui/BreadcrumbNavigation.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	interface Props {
+		items: { label: string; url: string }[];
+	}
+
+	const { items }: Props = $props();
+</script>
+
+<div class="flex gap-2 text-xs font-bold">
+	{#each items as item, index (item.url + index)}
+		<a href={item.url} class="text-brand-primary no-underline">{item.label}</a><span>/</span>
+	{/each}
+</div>
