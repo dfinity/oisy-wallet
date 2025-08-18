07bf89fcde5bd4464b417f98451bdc540c99c14b
daviddecentage <david.farina@dfinity.org>
2025-08-18 15:19:51 +0200

    update

--- src/frontend/src/tests/lib/components/nfts/NftCollectionCard.spec.ts
+++ src/frontend/src/tests/lib/components/nfts/NftCollectionCard.spec.ts
@@ -1,11 +1,7 @@
-import { ETHEREUM_NETWORK } from '$env/networks/networks.eth.env';
 import NftCollectionCard from '$lib/components/nfts/NftCollectionCard.svelte';
 import { i18n } from '$lib/stores/i18n.store';
-import type { NftCollectionUi } from '$lib/types/nft';
-import type { TokenId } from '$lib/types/token';
 import { replacePlaceholders } from '$lib/utils/i18n.utils';
-import { mockEthAddress } from '$tests/mocks/eth.mock';
-import { mockValidErc1155Nft } from '$tests/mocks/nfts.mock';
+import { mockNftollectionUi } from '$tests/mocks/nfts.mock';
 import { assertNonNullish } from '@dfinity/utils';
 import { render } from '@testing-library/svelte';
 import { get } from 'svelte/store';
@@ -16,22 +12,10 @@ describe('NftCollectionCard', () => {
 	const getImageSelector = (index: number) => `div[data-tid="${testId}-image-${index}"]`;
 	const networkLogoSelector = `div[data-tid="${testId}-network-light-container"]`;
 
-	const mockCollection: NftCollectionUi = {
-		nfts: [mockValidErc1155Nft, mockValidErc1155Nft],
-		collection: {
-			name: 'Testcollection',
-			address: mockEthAddress,
-			network: ETHEREUM_NETWORK,
-			standard: 'erc1155',
-			symbol: 'testcollection',
-			id: 'testcollection' as unknown as TokenId
-		}
-	};
-
 	it('should render nft collection', () => {
 		const { container, getByText } = render(NftCollectionCard, {
 			props: {
-				collection: mockCollection,
+				collection: mockNftollectionUi,
 				testId
 			}
 		});
@@ -48,12 +32,12 @@ describe('NftCollectionCard', () => {
 
 		expect(networkLogo).toBeInTheDocument();
 
-		assertNonNullish(mockCollection.collection.name);
+		assertNonNullish(mockNftollectionUi.collection.name);
 
-		expect(getByText(mockCollection.collection.name)).toBeInTheDocument();
+		expect(getByText(mockNftollectionUi.collection.name)).toBeInTheDocument();
 		expect(
 			getByText(
-				`${replacePlaceholders(get(i18n).nfts.text.collection_items_count, { count: String(mockCollection.nfts.length) })}`
+				`${replacePlaceholders(get(i18n).nfts.text.collection_items_count, { count: String(mockNftollectionUi.nfts.length) })}`
 			)
 		).toBeInTheDocument();
 	});
