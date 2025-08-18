a0204fe96d15c39d47f3c411128f74542a335479
daviddecentage <david.farina@dfinity.org>
2025-08-18 15:50:44 +0200

    update

--- src/frontend/src/lib/components/nfts/NftCollectionHero.svelte
+++ src/frontend/src/lib/components/nfts/NftCollectionHero.svelte
@@ -9,6 +9,7 @@
 	import SkeletonText from '$lib/components/ui/SkeletonText.svelte';
 	import { AppPath } from '$lib/constants/routes.constants';
 	import type { Nft, NftCollection } from '$lib/types/nft';
+	import { i18n } from '$lib/stores/i18n.store';
 
 	interface Props {
 		collection?: NftCollection;
@@ -44,7 +45,7 @@
 
 		<List condensed styleClass="text-sm text-tertiary">
 			<ListItem
-				><span>Collection contract address</span>
+				><span>{$i18n.nfts.collection_address}</span>
 
 				{#if nonNullish(collection)}
 					{collection.address}
@@ -55,7 +56,7 @@
 				{/if}
 			</ListItem>
 			<ListItem>
-				<span>Network</span>
+				<span>{$i18n.networks.network}</span>
 				{#if nonNullish(collection)}
 					<NetworkWithLogo network={collection.network} />
 				{:else}
@@ -65,7 +66,7 @@
 				{/if}
 			</ListItem>
 			<ListItem>
-				<span>Token standard</span>
+				<span>{$i18n.nfts.token_standard}</span>
 				{#if nonNullish(collection)}
 					<span class="uppercase">{collection.standard}</span>
 				{:else}
