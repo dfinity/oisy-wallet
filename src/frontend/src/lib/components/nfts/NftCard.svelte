d39c4bb73ff8490e939c52e0531d47684fe4333a
daviddecentage <david.farina@dfinity.org>
2025-08-18 11:38:13 +0200

    update

--- src/frontend/src/lib/components/nfts/NftCard.svelte
+++ src/frontend/src/lib/components/nfts/NftCard.svelte
@@ -2,32 +2,29 @@
 	import { nonNullish } from '@dfinity/utils';
 	import NetworkLogo from '$lib/components/networks/NetworkLogo.svelte';
 	import Badge from '$lib/components/ui/Badge.svelte';
-	import Img from '$lib/components/ui/Img.svelte';
-	import { i18n } from '$lib/stores/i18n.store';
+	import { AppPath } from '$lib/constants/routes.constants';
 	import type { Nft } from '$lib/types/nft';
-	import { replacePlaceholders } from '$lib/utils/i18n.utils';
+	import BgImg from '$lib/components/ui/BgImg.svelte';
 
 	interface Props {
 		nft: Nft;
 		testId?: string;
+		disabled?: boolean;
 	}
 
-	let { nft, testId }: Props = $props();
+	let { nft, testId, disabled }: Props = $props();
 </script>
 
-<div data-tid={testId}>
-	<div class="relative overflow-hidden rounded-lg">
+<a
+	class="flex w-full flex-col gap-2 p-1 text-left no-underline"
+	data-tid={testId}
+	href={`${AppPath.Nfts + nft.collection.address}/${nft.id}`}
+>
+	<div class="relative aspect-square overflow-hidden rounded-xl">
 		{#if nonNullish(nft.imageUrl)}
-			<Img
-				src={nft.imageUrl}
-				alt={replacePlaceholders($i18n.nfts.alt.card.image, {
-					$tokenId: nft.id.toString()
-				})}
-				styleClass="h-48 object-contain bg-black"
-				testId={`${testId}-image`}
-			/>
+			<BgImg imageUrl={nft?.imageUrl} size="cover" />
 		{:else}
-			<div class="bg-black/16 h-48 rounded-lg" data-tid={`${testId}-placeholder`}></div>
+			<div class="bg-black/16 rounded-lg" data-tid={`${testId}-placeholder`}></div>
 		{/if}
 
 		<div class="absolute bottom-2 right-2 flex items-center gap-1">
@@ -44,8 +41,14 @@
 		</div>
 	</div>
 
-	<div class="px-2 pt-2">
-		<h3 class="truncate text-xs font-semibold text-tertiary">{nft.collection.name}</h3>
-		<span class="text-xs text-tertiary">{`#${nft.id}`}</span>
+	<div class="flex w-full flex-col gap-1">
+		<span
+			class="truncate text-sm font-bold"
+			class:text-primary={!disabled}
+			class:text-disabled={disabled}>{nft.collection.name}</span
+		>
+		<span class="text-xs" class:text-tertiary={!disabled} class:text-disabled={disabled}
+			>#{nft.id}</span
+		>
 	</div>
-</div>
+</a>
