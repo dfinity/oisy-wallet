6ebcdb1cd8e38bcb9373cfaf9f4f3f85a3902093
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-08-18 13:38:45 +0200

    refactor(frontend): NFT Card (#8295)
    # Motivation
    
    We make some slight adjustments for the NFTCard component to better fit
    into the new NFT pages.
    
    # Changes
    
    - Using new BgImg component
    - Added disabled prop
    - Made card a link
    - Adjusted minor styling of texts and card itself
    
    # Tests
    
    <img width="401" height="273" alt="image"
    src="https://github.com/user-attachments/assets/d3e9d857-1e73-43fc-91f4-d13c1bd47d07"
    />
    
    Disabled:
    <img width="441" height="233" alt="image"
    src="https://github.com/user-attachments/assets/380cb785-3bb1-4450-9d94-b817e8bf63fc"
    />
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/nfts/NftCard.svelte
+++ src/frontend/src/lib/components/nfts/NftCard.svelte
@@ -2,32 +2,34 @@
 	import { nonNullish } from '@dfinity/utils';
 	import NetworkLogo from '$lib/components/networks/NetworkLogo.svelte';
 	import Badge from '$lib/components/ui/Badge.svelte';
-	import Img from '$lib/components/ui/Img.svelte';
-	import { i18n } from '$lib/stores/i18n.store';
+	import BgImg from '$lib/components/ui/BgImg.svelte';
+	import { AppPath } from '$lib/constants/routes.constants';
 	import type { Nft } from '$lib/types/nft';
-	import { replacePlaceholders } from '$lib/utils/i18n.utils';
 
 	interface Props {
 		nft: Nft;
 		testId?: string;
+		disabled?: boolean;
 	}
 
-	let { nft, testId }: Props = $props();
+	let { nft, testId, disabled }: Props = $props();
 </script>
 
-<div data-tid={testId}>
-	<div class="relative overflow-hidden rounded-lg">
+<a
+	class="flex w-full flex-col gap-2 p-1 text-left no-underline hover:text-inherit"
+	data-tid={testId}
+	href={`${AppPath.Nfts + nft.collection.address}/${nft.id}`}
+	onclick={disabled ? (e) => e.preventDefault() : undefined}
+	class:cursor-not-allowed={disabled}
+>
+	<div
+		class="relative aspect-square overflow-hidden rounded-xl bg-primary-light"
+		class:opacity-50={disabled}
+	>
 		{#if nonNullish(nft.imageUrl)}
-			<Img
-				src={nft.imageUrl}
-				alt={replacePlaceholders($i18n.nfts.alt.card.image, {
-					$tokenId: nft.id.toString()
-				})}
-				styleClass="h-48 object-contain bg-black"
-				testId={`${testId}-image`}
-			/>
+			<BgImg imageUrl={nft?.imageUrl} size="contain" shadow="none" testId={`${testId}-image`} />
 		{:else}
-			<div class="bg-black/16 h-48 rounded-lg" data-tid={`${testId}-placeholder`}></div>
+			<div class="bg-black/16 rounded-lg" data-tid={`${testId}-placeholder`}></div>
 		{/if}
 
 		<div class="absolute bottom-2 right-2 flex items-center gap-1">
@@ -44,8 +46,14 @@
 		</div>
 	</div>
 
-	<div class="px-2 pt-2">
-		<h3 class="truncate text-xs font-semibold text-tertiary">{nft.collection.name}</h3>
-		<span class="text-xs text-tertiary">{`#${nft.id}`}</span>
+	<div class="flex w-full flex-col gap-1">
+		<span
+			class="truncate text-sm font-bold"
+			class:text-primary={!disabled}
+			class:text-disabled={disabled}>{nft.collection.name}</span
+		>
+		<span class="text-xs" class:text-tertiary={!disabled} class:text-disabled={disabled}
+			>#{nft.id}</span
+		>
 	</div>
-</div>
+</a>
