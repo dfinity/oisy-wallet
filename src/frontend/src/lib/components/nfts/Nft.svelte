e6fec6502ff104365a78865b29183467a8f3c3b1
daviddecentage <david.farina@dfinity.org>
2025-08-19 11:44:07 +0200

    update

--- src/frontend/src/lib/components/nfts/Nft.svelte
+++ src/frontend/src/lib/components/nfts/Nft.svelte
@@ -2,30 +2,39 @@
 	import { isNullish, nonNullish } from '@dfinity/utils';
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 	import NftHero from '$lib/components/nfts/NftHero.svelte';
 	import { AppPath } from '$lib/constants/routes.constants';
 	import { nftStore } from '$lib/stores/nft.store';
 	import { toastsError } from '$lib/stores/toasts.store';
 	import type { Nft } from '$lib/types/nft';
+	import { FALLBACK_TIMEOUT } from '$lib/constants/app.constants';
 
-	const [collectionId, nftId] = $derived([$page.params.collectionId, $page.params.nftId]);
+	const [networkId, collectionId, nftId] = $derived([
+		page.params.networkId,
+		page.params.collectionId,
+		page.params.nftId
+	]);
 
 	const nft: Nft | undefined = $derived(
 		($nftStore ?? []).find(
-			(nft) => String(nft.id) === nftId && nft.collection.address === collectionId
+			(nft) =>
+				String(nft.id) === nftId &&
+				nft.collection.address === collectionId &&
+				nft.collection.network.name === networkId
 		)
 	);
 
+	// redirect to assets page if nft cant be loaded within 10s
 	let timeout: NodeJS.Timeout | undefined = $state();
 
 	onMount(() => {
 		timeout = setTimeout(() => {
 			if (isNullish(nft)) {
-				goto(AppPath.Nfts);
+				goto(`${AppPath.Nfts}${page.url.search}`);
 				toastsError({ msg: { text: 'Could not load NFT' } });
 			}
-		}, 10000);
+		}, FALLBACK_TIMEOUT);
 
 		return () => {
 			if (nonNullish(timeout)) {
--- src/frontend/src/routes/(app)/nfts/[collectionId]/[nftId]/+page.ts
+++
@@ -1,7 +0,0 @@
-import { loadRouteParams, type RouteParams } from '$lib/utils/nav.utils';
-import type { LoadEvent } from '@sveltejs/kit';
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = ($event: LoadEvent): RouteParams => loadRouteParams($event);
-
-export const prerender = false;
