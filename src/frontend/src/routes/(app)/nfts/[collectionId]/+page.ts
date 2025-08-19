801701091c0ef811096ccc431668518b5087ab0d
daviddecentage <david.farina@dfinity.org>
2025-08-19 10:08:09 +0200

    update

--- src/frontend/src/routes/(app)/nfts/[collectionId]/+page.ts
+++ src/frontend/src/routes/(app)/nfts/[collectionId]/+page.ts
@@ -4,4 +4,5 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = ($event: LoadEvent): RouteParams => loadRouteParams($event);
 
+// we dont have the collection ids during build time so we need to disable prerender for dynamic routes
 export const prerender = false;
