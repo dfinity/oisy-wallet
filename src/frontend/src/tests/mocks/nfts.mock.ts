9fe175525e78778be827097561ef8c124aa0f1fb
daviddecentage <david.farina@dfinity.org>
2025-08-19 17:31:07 +0200

    update

--- src/frontend/src/tests/mocks/nfts.mock.ts
+++ src/frontend/src/tests/mocks/nfts.mock.ts
@@ -1,24 +1,9 @@
 import { ETHEREUM_NETWORK } from '$env/networks/networks.eth.env';
-import type { Nft, NftCollectionUi, NonFungibleToken } from '$lib/types/nft';
+import type { Nft, NftCollectionUi } from '$lib/types/nft';
 import type { TokenId } from '$lib/types/token';
 import { parseNftId } from '$lib/validation/nft.validation';
 import { parseTokenId } from '$lib/validation/token.validation';
 import { mockEthAddress } from '$tests/mocks/eth.mock';
-
-export const getMockNonFungibleToken = (params: {
-	addresses: string[];
-	names: string[];
-}): NonFungibleToken[] =>
-	params.addresses.map((address, index) => ({
-		address: address,
-		category: 'custom',
-		decimals: 0,
-		id: parseTokenId(String(index)),
-		name: params.names[index],
-		network: ETHEREUM_NETWORK,
-		standard: 'erc1155',
-		symbol: params.names[index]
-	}));
 
 export const mockValidErc721Nft: Nft = {
 	name: 'Beanz 123',
@@ -68,12 +53,3 @@ export const mockNftollectionUi: NftCollectionUi = {
 		id: 'testcollection' as unknown as TokenId
 	}
 };
-
-export const mockNonFungibleToken1: NonFungibleToken = getMockNonFungibleToken({
-	addresses: [mockEthAddress],
-	names: ['Nft 1']
-})[0];
-export const mockNonFungibleToken2: NonFungibleToken = getMockNonFungibleToken({
-	addresses: [mockEthAddress],
-	names: ['Nft 2']
-})[0];
