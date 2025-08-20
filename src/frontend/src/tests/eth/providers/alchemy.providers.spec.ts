cd1067d2d6bea64722b291e1ce0ca969886dde9a
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-08-20 08:32:05 +0200

    returns loaded balance with nft id

--- src/frontend/src/tests/eth/providers/alchemy.providers.spec.ts
+++ src/frontend/src/tests/eth/providers/alchemy.providers.spec.ts
@@ -8,6 +8,8 @@ import { mockValidErc1155Token } from '$tests/mocks/erc1155-tokens.mock';
 import { mockEthAddress } from '$tests/mocks/eth.mock';
 import en from '$tests/mocks/i18n.mock';
 import { Alchemy } from 'alchemy-sdk';
+import type { OwnedNft } from '$lib/types/nft';
+import { parseNftId } from '$lib/validation/nft.validation';
 
 vi.mock(import('alchemy-sdk'), async (importOriginal) => {
 	const actual = await importOriginal();
@@ -39,10 +41,18 @@ describe('alchemy.providers', () => {
 
 	describe('getNftIdsForOwner', () => {
 		const mockApiResponse = {
-			ownedNfts: [{ tokenId: '1' }, { tokenId: '2' }, { tokenId: '3' }]
+			ownedNfts: [
+				{ tokenId: '1', balance: '1' },
+				{ tokenId: '2', balance: '4' },
+				{ tokenId: '3', balance: '1' }
+			]
 		};
 
-		const expectedTokenIds = [1, 2, 3];
+		const expectedTokenIds: OwnedNft[] = [
+			{ id: parseNftId(1), balance: 1 },
+			{ id: parseNftId(2), balance: 4 },
+			{ id: parseNftId(3), balance: 1 }
+		];
 
 		beforeEach(() => {
 			vi.clearAllMocks();
