200037fe715b9e050f26930e18d5b86eae588c01
DecentAgeCoder <marco.oesch@dfinity.org>
2025-08-20 13:19:11 +0200

    Add constant for minimum BTC balance confirmations (1 confirmation)

--- src/frontend/src/btc/constants/btc.constants.ts
+++ src/frontend/src/btc/constants/btc.constants.ts
@@ -1,3 +1,8 @@
+// We want to show the latest balance.
+// Even if it's not totally confirmed (6 confirmations).
+// There is no difference between 0 and 1
+// because the bitcoin canister doesn't know about the mempool and unconfirmed transactions.
+export const BTC_BALANCE_MIN_CONFIRMATIONS = 1;
 /**
  * Minimum confirmations threshold for pending Bitcoin transaction status.
  * When block_index (and hence confirmations value) is undefined,
