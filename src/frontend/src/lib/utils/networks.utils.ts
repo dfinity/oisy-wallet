3b8eb815322b79c60cf5962980ac8f96d58b3fd4
daviddecentage <david.farina@dfinity.org>
2025-08-21 18:30:24 +0200

    update

--- src/frontend/src/lib/utils/networks.utils.ts
+++ src/frontend/src/lib/utils/networks.utils.ts
@@ -1,7 +1,20 @@
+import type { EthereumNetwork } from '$eth/types/network';
 import { LOCAL } from '$lib/constants/app.constants';
 import type { Network } from '$lib/types/network';
 import type { UserNetworks } from '$lib/types/user-networks';
+import {
+	isNetworkIdArbitrum,
+	isNetworkIdBase,
+	isNetworkIdBsc,
+	isNetworkIdEthereum,
+	isNetworkIdEvm,
+	isNetworkIdPolygon,
+	isNetworkIdSepolia,
+	isNetworkIdSolana
+} from '$lib/utils/network.utils';
 import { isUserNetworkEnabled } from '$lib/utils/user-networks.utils';
+import type { SolanaNetwork } from '$sol/types/network';
+import { nonNullish } from '@dfinity/utils';
 
 export const defineEnabledNetworks = <T extends Network>({
 	$testnetsEnabled,
@@ -22,3 +35,31 @@ export const defineEnabledNetworks = <T extends Network>({
 		...(mainnetFlag ? mainnetNetworks : []),
 		...($testnetsEnabled ? [...testnetNetworks, ...(LOCAL ? localNetworks : [])] : [])
 	].filter(({ id: networkId }) => isUserNetworkEnabled({ userNetworks: $userNetworks, networkId }));
+
+export const getContractExplorerUrl = ({
+	network,
+	contractAddress
+}: {
+	network: Network;
+	contractAddress: string;
+}): string | undefined => {
+	if (nonNullish((network as SolanaNetwork | EthereumNetwork)?.explorerUrl)) {
+		const baseUrl = (network as SolanaNetwork | EthereumNetwork)?.explorerUrl;
+		if (nonNullish(baseUrl)) {
+			if (
+				isNetworkIdEthereum(network.id) ||
+				isNetworkIdBase(network.id) ||
+				isNetworkIdEvm(network.id) ||
+				isNetworkIdSepolia(network.id) ||
+				isNetworkIdArbitrum(network.id) ||
+				isNetworkIdPolygon(network.id) ||
+				isNetworkIdBsc(network.id)
+			) {
+				return `${baseUrl}/address/${contractAddress}`;
+			}
+			if (isNetworkIdSolana(network.id)) {
+				return `${baseUrl}/account/${contractAddress}`;
+			}
+		}
+	}
+};
