773b6806359ad03013a23a0998b7816b080801f9
daviddecentage <david.farina@dfinity.org>
2025-08-25 13:15:49 +0200

    update

---
+++ src/frontend/src/lib/components/terms-of-use/TermsOfUse.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Markdown } from '@dfinity/gix-components';
+	import { i18n } from '$lib/stores/i18n.store';
+	import { replacePlaceholders } from '$lib/utils/i18n.utils.js';
+</script>
+
+<h1 class="mb-4">{$i18n.termsOfUse.text.title}</h1>
+
+<Markdown
+	text={replacePlaceholders($i18n.termsOfUse.text.body, {
+		$date: 'todo'
+	})}
+/>
