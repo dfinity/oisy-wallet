11ab0650efc2a3838810c28fceddddc2e1476922
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-08-25 17:26:53 +0200

    Update src/frontend/src/lib/components/swap/SwapAmountsContext.svelte
    Co-authored-by: Denys Karmazyn <denys.karmazyn@dfinity.org>

--- src/frontend/src/lib/components/swap/SwapAmountsContext.svelte
+++ src/frontend/src/lib/components/swap/SwapAmountsContext.svelte
@@ -18,7 +18,7 @@
 	interface Props {
 		amount: OptionAmount;
 		sourceToken?: IcToken | Erc20Token;
-		destinationToken: IcToken | Erc20Token | undefined;
+		destinationToken?: IcToken | Erc20Token;
 		slippageValue: OptionAmount;
 		children?: Snippet;
 		isSourceTokenIcrc2: boolean;
