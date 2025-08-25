a9b41718ab7bfcdcd5b5076f34d1369817c13730
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-08-25 17:35:34 +0200

    feat(frontend): added support of erc20 Tokens for SwapTokensModalList

--- src/frontend/src/lib/components/swap/SwapTokensList.svelte
+++ src/frontend/src/lib/components/swap/SwapTokensList.svelte
@@ -1,34 +1,44 @@
 <script lang="ts">
+	import { nonNullish } from '@dfinity/utils';
 	import { createEventDispatcher, getContext } from 'svelte';
-	import { ICP_TOKEN } from '$env/tokens/tokens.icp.env';
-	import type { IcTokenToggleable } from '$icp/types/ic-token-toggleable';
+	import { erc20UserTokensNotInitialized } from '$eth/derived/erc20.derived';
 	import ModalTokensList from '$lib/components/tokens/ModalTokensList.svelte';
 	import ModalTokensListItem from '$lib/components/tokens/ModalTokensListItem.svelte';
-	import ButtonCancel from '$lib/components/ui/ButtonCancel.svelte';
-	import { allKongSwapCompatibleIcrcTokens } from '$lib/derived/all-tokens.derived';
-	import { exchanges } from '$lib/derived/exchange.derived';
-	import { balancesStore } from '$lib/stores/balances.store';
 	import { i18n } from '$lib/stores/i18n.store';
+	import type { Token, TokenUi } from '$lib/types/token';
+	import type { IcTokenToggleable } from '$icp/types/ic-token-toggleable';
+	import ButtonCancel from '../ui/ButtonCancel.svelte';
+	import { SWAP_CONTEXT_KEY, type SwapContext } from '$lib/stores/swap.store';
 	import {
 		MODAL_TOKENS_LIST_CONTEXT_KEY,
 		type ModalTokensListContext
 	} from '$lib/stores/modal-tokens-list.store';
-	import { SWAP_CONTEXT_KEY, type SwapContext } from '$lib/stores/swap.store';
-	import type { Token, TokenUi } from '$lib/types/token';
 	import { pinTokensWithBalanceAtTop } from '$lib/utils/tokens.utils';
+	import { exchanges } from '$lib/derived/exchange.derived';
+	import { balancesStore } from '$lib/stores/balances.store';
+	import {
+		allCrossChainSwapTokens,
+		allKongSwapCompatibleIcrcTokens
+	} from '$lib/derived/all-tokens.derived';
+	import { ICP_TOKEN } from '$env/tokens/tokens.icp.env';
+	import { VELORA_SWAP_ENABLED } from '$env/velora-swap.env';
 
 	const { sourceToken, destinationToken } = getContext<SwapContext>(SWAP_CONTEXT_KEY);
 
 	const { setTokens } = getContext<ModalTokensListContext>(MODAL_TOKENS_LIST_CONTEXT_KEY);
 
 	const dispatch = createEventDispatcher<{
-		icSelectToken: IcTokenToggleable;
+		icSelectToken: Token;
 		icCloseTokensList: void;
 	}>();
 
-	let tokens: TokenUi<IcTokenToggleable>[] = $derived(
+	let tokens: TokenUi<Token>[] = $derived(
 		pinTokensWithBalanceAtTop({
-			$tokens: [{ ...ICP_TOKEN, enabled: true }, ...$allKongSwapCompatibleIcrcTokens].filter(
+			$tokens: [
+				{ ...ICP_TOKEN, enabled: true },
+				...$allKongSwapCompatibleIcrcTokens,
+				...(VELORA_SWAP_ENABLED ? $allCrossChainSwapTokens : [])
+			].filter(
 				(token: Token) => token.id !== $sourceToken?.id && token.id !== $destinationToken?.id
 			),
 			$exchanges,
@@ -40,18 +50,19 @@
 		setTokens(tokens);
 	});
 
-	const onIcTokenButtonClick = ({ detail: token }: CustomEvent<TokenUi<IcTokenToggleable>>) => {
+	const onIcTokenButtonClick = ({ detail: token }: CustomEvent<TokenUi<Token>>) => {
 		dispatch('icSelectToken', token);
 	};
 </script>
 
 <ModalTokensList
 	loading={false}
-	networkSelectorViewOnly={true}
+	on:icSelectNetworkFilter
 	on:icTokenButtonClick={onIcTokenButtonClick}
+	networkSelectorViewOnly={VELORA_SWAP_ENABLED ? false : true}
 >
 	{#snippet tokenListItem(token, onClick)}
-		<ModalTokensListItem {onClick} showDividers={false} {token} />
+		<ModalTokensListItem {token} {onClick} />
 	{/snippet}
 	{#snippet noResults()}
 		<p class="text-primary">
