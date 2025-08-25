471d8a80d7277b1da159083b03c93a29bb88ea87
DecentAgeCoder <marco.oesch@dfinity.org>
2025-08-25 09:47:44 +0200

    Simplify imports in `BtcSendReview` by removing unused `Readable` type and consolidating `initPendingSentTransactionsStatus`.

--- src/frontend/src/btc/components/send/BtcSendReview.svelte
+++ src/frontend/src/btc/components/send/BtcSendReview.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
 	import { isNullish, nonNullish } from '@dfinity/utils';
 	import { getContext } from 'svelte';
-	import type { Readable } from 'svelte/store';
 	import BtcReviewNetwork from '$btc/components/send/BtcReviewNetwork.svelte';
 	import BtcSendWarnings from '$btc/components/send/BtcSendWarnings.svelte';
 	import BtcUtxosFee from '$btc/components/send/BtcUtxosFee.svelte';
-	import {
-		BtcPendingSentTransactionsStatus,
-		initPendingSentTransactionsStatus
-	} from '$btc/derived/btc-pending-sent-transactions-status.derived';
+	import { BtcPendingSentTransactionsStatus } from '$btc/derived/btc-pending-sent-transactions-status.derived';
 	import type { UtxosFee } from '$btc/types/btc-send';
 	import SendReview from '$lib/components/send/SendReview.svelte';
 	import { SEND_CONTEXT_KEY, type SendContext } from '$lib/stores/send.store';
