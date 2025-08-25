e3d19d227613607bb2e02fc5538458ba3159e74b
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-08-25 20:26:16 +0200

    fix

--- src/frontend/src/lib/components/swap/SwapAmountsContext.svelte
+++ src/frontend/src/lib/components/swap/SwapAmountsContext.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import { debounce, isNullish, nonNullish } from '@dfinity/utils';
 	import { getContext, type Snippet } from 'svelte';
-	import type { Erc20Token } from '$eth/types/erc20';
-	import type { IcToken } from '$icp/types/ic-token';
 	import { SWAP_DEFAULT_SLIPPAGE_VALUE } from '$lib/constants/swap.constants';
 	import { ethAddress } from '$lib/derived/address.derived';
 	import { authIdentity } from '$lib/derived/auth.derived';
