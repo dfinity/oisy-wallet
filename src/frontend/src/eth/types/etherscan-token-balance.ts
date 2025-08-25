9f1afa9cefbb8eafca15fc294bec1087a273ff3d
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-08-25 09:25:35 +0200

    implements feature

--- src/frontend/src/eth/types/etherscan-token-balance.ts
+++
@@ -1,7 +0,0 @@
-// https://docs.etherscan.io/etherscan-v2/api-endpoints/tokens#get-address-erc721-token-holding
-export interface EtherscanProviderTokenBalance {
-	TokenAddress: string;
-	TokenName: string;
-	TokenSymbol: string;
-	TokenQuantity: string;
-}
