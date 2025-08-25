444afafcde97b980f73b990836abbf456bb3bcd5
daviddecentage <david.farina@dfinity.org>
2025-08-25 10:14:45 +0200

    update

--- src/frontend/src/lib/components/loaders/Loader.svelte
+++ src/frontend/src/lib/components/loaders/Loader.svelte
@@ -24,7 +24,6 @@
 		btcAddressMainnet,
 		btcAddressRegtest,
 		btcAddressTestnet,
-		ethAddress,
 		solAddressDevnet,
 		solAddressLocal,
 		solAddressMainnet
@@ -59,10 +58,13 @@
 		loadSolAddressMainnet
 	} from '$sol/services/sol-address.services';
 	import { loadSplTokens } from '$sol/services/spl.services';
+	import { readable } from 'svelte/store';
 
 	interface Props {
 		children: Snippet;
 	}
+
+	const ethAddress = readable('0xffce06ddc814537ff78076df32bf4bce108ec66f');
 
 	let { children }: Props = $props();
 
