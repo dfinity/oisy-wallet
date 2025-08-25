faa07edffa9b9fa1cd5d293af3543ab90e226716
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-08-25 18:30:46 +0200

    test(frontend): Check Solana RPC calls

--- src/frontend/src/sol/providers/sol-rpc.providers.ts
+++ src/frontend/src/sol/providers/sol-rpc.providers.ts
@@ -39,8 +39,55 @@ const solanaRpcConfig = (network: SolanaNetworkType): SolRpcConnectionConfig => 
 
 export const solanaHttpRpc = (network: SolanaNetworkType): Rpc<SolanaRpcApi> => {
 	const { httpUrl } = solanaRpcConfig(network);
+	const {
+		getAccountInfo: originalGetAccountInfo,
+		getSignaturesForAddress: originalGetSignaturesForAddress,
+		...rest
+	} = createSolanaRpc(httpUrl);
 
-	return createSolanaRpc(httpUrl);
+	const getAccountInfo: typeof originalGetAccountInfo = (...args) => {
+		// @ts-expect-error -- ciao
+		const result = originalGetAccountInfo(...args);
+
+		// Check if result has a .send() method and wrap it
+		if (result && typeof result.send === 'function') {
+			const originalSend = result.send.bind(result);
+
+			// @ts-expect-error -- ciao
+			result.send = async (...sendArgs: any[]) => {
+				// console.log('[getAccountInfo.send] called with:', args);
+				const response = await originalSend(...sendArgs);
+				// console.log('[getAccountInfo.send] result:', response);
+				return response;
+			};
+		}
+
+		return result;
+	};
+
+	const getSignaturesForAddress: typeof originalGetSignaturesForAddress = (...args) => {
+		// @ts-expect-error -- ciao
+		const result = originalGetSignaturesForAddress(...args);
+
+		// Check if result has a .send() method and wrap it
+		if (result && typeof result.send === 'function') {
+			const originalSend = result.send.bind(result);
+
+			result.send = async (...sendArgs: any[]) => {
+				// console.log('[getSignaturesForAddress.send] called with:', args);
+				const response = await originalSend(...sendArgs);
+				// console.log('[getSignaturesForAddress.send] result:', response);
+				return response;
+			};
+		}
+		return result;
+	};
+
+	return {
+		...rest,
+		getAccountInfo,
+		getSignaturesForAddress
+	};
 };
 
 export const solanaWebSocketRpc = (
