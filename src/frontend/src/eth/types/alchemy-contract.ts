2a26b5a6aa5642f5733fedfbbba09b5c114d9940
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-08-25 09:44:42 +0200

    implements feature

--- src/frontend/src/eth/types/alchemy-contract.ts
+++ src/frontend/src/eth/types/alchemy-contract.ts
@@ -1,8 +1,11 @@
 // https://www.alchemy.com/docs/reference/nft-api-endpoints/nft-api-endpoints/nft-ownership-endpoints/get-contracts-for-owner-v-3
-import type { NftContractForOwner } from 'alchemy-sdk';
+interface Contract {
+	address: string;
+	name?: string;
+	isSpam: boolean;
+	tokenType: string
+}
 
 export interface AlchemyProviderContracts {
-	contracts: NftContractForOwner[];
-	pageKey?: string;
-	totalCount: number;
+	contracts: Contract[];
 }
