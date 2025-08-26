695a78c92bb5af1754cc00bf7e04941a4cc4d720
daviddecentage <david.farina@dfinity.org>
2025-08-26 18:20:02 +0200

    update

--- src/frontend/src/lib/components/privacy-policy/PrivacyPolicy.svelte
+++ src/frontend/src/lib/components/privacy-policy/PrivacyPolicy.svelte
@@ -1,3 +1,10 @@
 <script lang="ts">
-	// TODO: add texts and use MarkdownWithSidebar
+	import MarkdownWithSidebar from '$lib/components/ui/MarkdownWithSidebar.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
 </script>
+
+<MarkdownWithSidebar
+	stringReplacements={{ $date: 'todo' }}
+	text={$i18n.privacy_policy.text.body}
+	title={$i18n.privacy_policy.text.title}
+/>
