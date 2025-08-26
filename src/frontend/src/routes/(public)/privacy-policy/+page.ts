1a5b8690f86c455dd3c7d15e299f6cc85fd44b93
daviddecentage <david.farina@dfinity.org>
2025-08-26 18:24:34 +0200

    update

--- src/frontend/src/routes/(public)/privacy-policy/+page.ts
+++ src/frontend/src/routes/(public)/privacy-policy/+page.ts
@@ -1,6 +1,14 @@
+import { NEW_AGREEMENTS_ENABLED } from '$env/agreements.env';
 import { resetRouteParams, type RouteParams } from '$lib/utils/nav.utils';
 import type { LoadEvent } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 // We reset the data because a public route operates without a network or token selected.
-export const load: PageLoad = (_$event: LoadEvent): RouteParams => resetRouteParams();
+export const load: PageLoad = (_$event: LoadEvent): RouteParams => {
+	if (!NEW_AGREEMENTS_ENABLED) {
+		redirect(302, '/');
+	}
+
+	return resetRouteParams();
+};
