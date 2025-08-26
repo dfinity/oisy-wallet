911fb83dd3bd26ca45a48b921aed957be4e3925f
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-08-26 09:00:26 +0200

    feat(frontend): ETH transactions skeletons do not wait for ERC20 tokens

--- src/frontend/src/eth/components/transactions/EthTransactionsSkeletons.svelte
+++ src/frontend/src/eth/components/transactions/EthTransactionsSkeletons.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-	import { erc20UserTokensNotInitialized } from '$eth/derived/erc20.derived';
 	import { ethTransactionsNotInitialized } from '$eth/derived/eth-transactions.derived';
 	import { tokenNotInitialized } from '$eth/derived/nav.derived';
 	import TransactionsSkeletons from '$lib/components/transactions/TransactionsSkeletons.svelte';
 
 	let loading: boolean;
 	$: loading =
-		($erc20UserTokensNotInitialized && $tokenNotInitialized) || $ethTransactionsNotInitialized;
+		($tokenNotInitialized) || $ethTransactionsNotInitialized;
 </script>
 
 <TransactionsSkeletons {loading}>
