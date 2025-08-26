5144f9f38227d2dd3bfd1d2fdc148e25cf4216d7
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-08-26 11:06:21 +0200

    implements feature

--- src/frontend/src/lib/components/loaders/LoaderCollections.svelte
+++ src/frontend/src/lib/components/loaders/LoaderCollections.svelte
@@ -46,7 +46,7 @@
 				}
 
 				const {
-					Erc721: { token_address: tokenAddress, chain_id: tokenChainId }
+					Erc721: { token: { token_address: tokenAddress, chain_id: tokenChainId } }
 				} = token;
 
 				return tokenAddress === contract.address && tokenChainId === network.chainId;
