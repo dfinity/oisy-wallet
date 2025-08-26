bf6907bda52bac2f1b461a658326db0a6cfcf9d0
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2025-08-26 10:26:39 +0200

    feat(frontend): AI send

--- src/frontend/src/lib/components/ai-assistant/AiAssistantToolResults.svelte
+++ src/frontend/src/lib/components/ai-assistant/AiAssistantToolResults.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { nonNullish } from '@dfinity/utils';
+	import AiAssistantReviewSendTokenTool from '$lib/components/ai-assistant/AiAssistantReviewSendTokenTool.svelte';
 	import AiAssistantShowContactsTool from '$lib/components/ai-assistant/AiAssistantShowContactsTool.svelte';
+	import SendTokenContext from '$lib/components/send/SendTokenContext.svelte';
 	import type { ToolResult } from '$lib/types/ai-assistant';
 
 	interface Props {
@@ -15,6 +17,10 @@
 	{#each results as { result, type }, index (index)}
 		{#if type === 'show_contacts' && nonNullish(result) && 'contacts' in result}
 			<AiAssistantShowContactsTool {...result} {onSendMessage} />
+		{:else if type === 'review_send_tokens' && nonNullish(result) && 'token' in result}
+			<SendTokenContext token={result.token}>
+				<AiAssistantReviewSendTokenTool {...result} />
+			</SendTokenContext>
 		{/if}
 	{/each}
 </div>
