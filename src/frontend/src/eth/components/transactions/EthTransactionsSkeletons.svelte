e829b616527ed7d1925525184c6acac1a9c06b74
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-08-26 14:06:21 +0200

    migrate to svelte 5

--- src/frontend/src/eth/components/transactions/EthTransactionsSkeletons.svelte
+++ src/frontend/src/eth/components/transactions/EthTransactionsSkeletons.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
+	import type { Snippet } from 'svelte';
 	import { ethTransactionsNotInitialized } from '$eth/derived/eth-transactions.derived';
 	import { tokenNotInitialized } from '$eth/derived/nav.derived';
 	import TransactionsSkeletons from '$lib/components/transactions/TransactionsSkeletons.svelte';
 
-	let loading: boolean;
-	$: loading = $tokenNotInitialized || $ethTransactionsNotInitialized;
+	interface Props {
+		children: Snippet;
+	}
+
+	let { children }: Props = $props();
+
+	let loading = $derived($tokenNotInitialized || $ethTransactionsNotInitialized);
 </script>
 
 <TransactionsSkeletons {loading}>
-	<slot />
+	{@render children()}
 </TransactionsSkeletons>
