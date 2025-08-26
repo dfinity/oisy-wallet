93fa1a851e02ac36d109f46f17cb83e81fbebc5d
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-08-26 14:48:47 +0200

    implements feature

--- src/frontend/src/eth/types/erc1155.ts
+++ src/frontend/src/eth/types/erc1155.ts
@@ -8,7 +8,7 @@ type Erc1155Standard = Extract<TokenStandard, 'erc1155'>;
 export type Erc1155Token = Erc1155Contract &
 	Omit<Token, 'network' | 'standard'> & { network: EthereumNetwork; standard: Erc1155Standard, state?: TokenState };
 
-export type RequiredErc1155Token = RequiredToken<Erc1155Token>;
+export type RequiredErc1155Token = RequiredToken<Omit<Erc1155Token, 'state'>>;
 
 export type Erc1155ContractAddress = ContractAddress;
 export type Erc1155Contract = Erc1155ContractAddress;
