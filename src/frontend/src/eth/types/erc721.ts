b6332a5af7c2a706e6ae6cb6c415a401eb617037
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-08-26 14:51:01 +0200

    implements feature

--- src/frontend/src/eth/types/erc721.ts
+++ src/frontend/src/eth/types/erc721.ts
@@ -8,7 +8,7 @@ type Erc721Standard = Extract<TokenStandard, 'erc721'>;
 export type Erc721Token = Erc721Contract &
 	Omit<Token, 'network' | 'standard'> & { network: EthereumNetwork; standard: Erc721Standard, state?: TokenState };
 
-export type RequiredErc721Token = RequiredToken<Erc721Token>;
+export type RequiredErc721Token = RequiredToken<Omit<Erc721Token, 'state'>>;
 
 export type Erc721ContractAddress = ContractAddress;
 export type Erc721Contract = Erc721ContractAddress;
