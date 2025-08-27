d3972f461290b59d38227bdd028404307a4ebfd4
daviddecentage <david.farina@dfinity.org>
2025-08-27 14:43:14 +0200

    update

--- src/frontend/src/lib/components/privacy-policy/PrivacyPolicy.svelte
+++ src/frontend/src/lib/components/privacy-policy/PrivacyPolicy.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import MarkdownWithSidebar from '$lib/components/ui/MarkdownWithSidebar.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
+	import { getAgreementLastUpdated } from '$lib/utils/agreements.utils';
 </script>
 
 <MarkdownWithSidebar
-	stringReplacements={{ $date: '26.08.2025' }}
+	stringReplacements={{ $date: getAgreementLastUpdated({ type: 'privacyPolicy', $i18n }) }}
 	text={$i18n.privacy_policy.text.body}
 	title={$i18n.privacy_policy.text.title}
 />
