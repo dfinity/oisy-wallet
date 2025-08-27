9b6ef6834ada280daf6d1e79b69f6a3fb5d8c42c
daviddecentage <david.farina@dfinity.org>
2025-08-27 14:52:39 +0200

    update

--- src/frontend/src/lib/components/terms-of-use/TermsOfUse.svelte
+++ src/frontend/src/lib/components/terms-of-use/TermsOfUse.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	import MarkdownWithSidebar from '$lib/components/ui/MarkdownWithSidebar.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
-
-	// TODO: insert date from build env variable?
+	import { getAgreementLastUpdated } from '$lib/utils/agreements.utils';
 </script>
 
 <MarkdownWithSidebar
-	stringReplacements={{ $date: '26.08.2025' }}
-	text={$i18n.termsOfUse.text.body}
-	title={$i18n.termsOfUse.text.title}
+	stringReplacements={{ $date: getAgreementLastUpdated({ type: 'termsOfUse', $i18n }) }}
+	text={$i18n.terms_of_use.text.body}
+	title={$i18n.terms_of_use.text.title}
 />
