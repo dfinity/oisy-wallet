417c80b10b15b881b421648c76d70a74a313315b
daviddecentage <david.farina@dfinity.org>
2025-08-27 13:32:34 +0200

    update

--- src/frontend/src/env/agreements.env.ts
+++ src/frontend/src/env/agreements.env.ts
@@ -1,5 +1,26 @@
+import agreementsJson from '$env/agreements.json';
+import { EnvAgreementsSchema } from '$env/schema/env-agreements.schema';
+import type { EnvAgreement } from '$env/types/env-agreements';
+import { MILLISECONDS_IN_SECOND } from '$lib/constants/app.constants';
+import { i18n } from '$lib/stores/i18n.store';
 import { parseBoolEnvVar } from '$lib/utils/env.utils';
+import { formatSecondsToDate } from '$lib/utils/format.utils';
+import { get } from 'svelte/store';
+import * as z from 'zod/v4';
 
 export const NEW_AGREEMENTS_ENABLED = parseBoolEnvVar(
 	import.meta.env.VITE_FRONTEND_NEW_AGREEMENTS_ENABLED
 );
+
+export const getAgreementLastUpdated = (type: keyof EnvAgreement) => {
+	const { data } = z.safeParse(EnvAgreementsSchema, agreementsJson);
+
+	return formatSecondsToDate({
+		seconds: data[type]?.lastUpdatedTimestamp / MILLISECONDS_IN_SECOND,
+		language: get(i18n).lang,
+		formatOptions: {
+			minute: undefined,
+			hour: undefined
+		}
+	});
+};
