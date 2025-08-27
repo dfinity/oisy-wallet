88f87d8455823ff358bdc70792a0c98d7da385b5
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-08-27 11:33:58 +0200

    feat(frontend): Terms and privacy links (#8504)
    # Motivation
    
    Same as the LicenseLink we add link components for the new Terms of Use
    and Privacy Policy.
    
    # Changes
    
    Added components
    Added texts
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/frontend/src/lib/components/terms-of-use/TermsOfUseLink.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { i18n } from '$lib/stores/i18n.store';
+	import { replaceOisyPlaceholders } from '$lib/utils/i18n.utils';
+
+	interface Props {
+		noUnderline?: boolean;
+		testId?: string;
+	}
+
+	let { noUnderline = false, testId }: Props = $props();
+</script>
+
+<a
+	class:no-underline={noUnderline}
+	aria-label={replaceOisyPlaceholders($i18n.terms_of_use.alt.terms_of_use)}
+	data-tid={testId}
+	href="/terms-of-use"
+>
+	{$i18n.terms_of_use.text.terms_of_use}
+</a>
