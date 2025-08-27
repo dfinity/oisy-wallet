9f5eebafa1c26ddefa5fdeffce46a74ad5c2d252
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-08-27 15:03:42 +0200

    feat(frontend): Terms of use (#8505)
    # Motivation
    
    We add the new Terms of Use
    
    # Changes
    
    - Added conditional redirect in case the feature is not enabled
    - Added english draft version
    - Render it in the respective component
    
    ---------
    
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/types/i18n.d.ts
+++ src/frontend/src/lib/types/i18n.d.ts
@@ -1239,7 +1239,7 @@ interface I18nLicense_agreement {
 }
 
 interface I18nTerms_of_use {
-	text: { terms_of_use: string };
+	text: { terms_of_use: string; title: string; body: string };
 	alt: { terms_of_use: string };
 }
 
