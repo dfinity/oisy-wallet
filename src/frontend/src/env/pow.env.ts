a84ebe52d6e0b86da20ad4ffada06fa0c0ebcdf2
DecentAgeCoder <marco.oesch@dfinity.org>
2025-08-28 14:35:18 +0200

    Refactored `POW_FEATURE_ENABLED` to use dynamic environment parsing via `parseBoolEnvVar` for improved configurability.

--- src/frontend/src/env/pow.env.ts
+++ src/frontend/src/env/pow.env.ts
@@ -1,4 +1,4 @@
-// import { parseBoolEnvVar } from '$lib/utils/env.utils';
+import { parseBoolEnvVar } from '$lib/utils/env.utils';
 
 // Enables the POW Protector on the frontend. Remember to also enable/disable the feature in the backend.
-export const POW_FEATURE_ENABLED = true; // parseBoolEnvVar(import.meta.env.VITE_POW_FEATURE_ENABLED);
+export const POW_FEATURE_ENABLED = parseBoolEnvVar(import.meta.env.VITE_POW_FEATURE_ENABLED);
