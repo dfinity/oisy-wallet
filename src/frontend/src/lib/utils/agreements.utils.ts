0a250dea691f012b68fcfa3d0da79695b33b65bb
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-08-29 12:02:12 +0200

    refactor(frontend): Move `transformAgreementsJsonBigint` to specific module (#8556)
    # Motivation
    
    Util `transformAgreementsJsonBigint` is used to set agreementsData, that
    by itself it will be used inside the `agreements.utils` module, risking
    a looped called.
    
    To avoid it, we move it to a different module, specific for it.

--- src/frontend/src/lib/utils/agreements.utils.ts
+++ src/frontend/src/lib/utils/agreements.utils.ts
@@ -3,27 +3,6 @@ import type { EnvAgreements } from '$env/types/env-agreements';
 import { MILLISECONDS_IN_SECOND } from '$lib/constants/app.constants';
 import { formatSecondsToDate } from '$lib/utils/format.utils';
 
-export const transformAgreementsJsonBigint = (
-	json: Record<string, { lastUpdatedTimestamp: { __bigint__: string }; lastUpdatedDate: string }>
-): EnvAgreements => {
-	const res: Record<string, { lastUpdatedTimestamp: bigint; lastUpdatedDate: string }> = {};
-	Object.entries(json).forEach(
-		([
-			key,
-			{
-				lastUpdatedTimestamp: { __bigint__ },
-				...rest
-			}
-		]) => {
-			res[key] = {
-				...rest,
-				lastUpdatedTimestamp: BigInt(__bigint__)
-			};
-		}
-	);
-	return res as EnvAgreements;
-};
-
 export const getAgreementLastUpdated = ({
 	type,
 	$i18n
