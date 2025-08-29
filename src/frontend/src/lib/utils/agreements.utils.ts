2fd84b16ecccffea4bcfd6de312e8534e78b66e3
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-08-29 09:52:49 +0200

    feat(frontend): Derived store for user agreements

--- src/frontend/src/lib/utils/agreements.utils.ts
+++ src/frontend/src/lib/utils/agreements.utils.ts
@@ -1,8 +1,11 @@
+import type { UserAgreement as BackendUserAgreement } from '$declarations/backend/backend.did';
 import agreementsJson from '$env/agreements.json';
 import { EnvAgreementsSchema } from '$env/schema/env-agreements.schema';
 import type { EnvAgreement } from '$env/types/env-agreements';
 import { MILLISECONDS_IN_SECOND } from '$lib/constants/app.constants';
+import type { AgreementData } from '$lib/types/user-agreements';
 import { formatSecondsToDate } from '$lib/utils/format.utils';
+import { fromNullable } from '@dfinity/utils';
 import * as z from 'zod/v4';
 
 const parseAgreementsJson = () => z.parse(EnvAgreementsSchema, agreementsJson);
@@ -22,3 +25,9 @@ export const getAgreementLastUpdated = ({
 			hour: undefined
 		}
 	});
+
+export const mapUserAgreement = (backendUserAgreement: BackendUserAgreement): AgreementData => ({
+	accepted: fromNullable(backendUserAgreement.accepted),
+	lastAcceptedTimestamp: fromNullable(backendUserAgreement.last_accepted_at_ns),
+	lastUpdatedTimestamp: fromNullable(backendUserAgreement.last_updated_at_ms)
+});
