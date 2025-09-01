36a395b7025fd0bc4c3e3a5eda66b94ce8ad134a
DecentAgeCoder <marco.oesch@dfinity.org>
2025-09-01 10:47:15 +0200

    Add Blockstream API URLs for testnet and regtest environments

--- src/frontend/src/env/rest/blockstream.env.ts
+++ src/frontend/src/env/rest/blockstream.env.ts
@@ -5,3 +5,13 @@ export const BLOCKSTREAM_API_URL = safeParse({
 	schema: UrlSchema,
 	value: 'https://blockstream.info/api'
 });
+
+export const BLOCKSTREAM_TESTNET_API_URL = safeParse({
+	schema: UrlSchema,
+	value: 'https://blockstream.info/testnet/api'
+});
+
+export const BLOCKSTREAM_REGTEST_API_URL = safeParse({
+	schema: UrlSchema,
+	value: 'http://localhost:3000/api'
+});
