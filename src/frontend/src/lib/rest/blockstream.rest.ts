e9470a9ceb4e52d2d7e3449466106527fe80f147
DecentAgeCoder <marco.oesch@dfinity.org>
2025-09-01 10:47:34 +0200

    Extend `loadBtcTransactionsData` to support `bitcoinNetwork` parameter and update its usage

--- src/frontend/src/lib/rest/blockstream.rest.ts
+++ src/frontend/src/lib/rest/blockstream.rest.ts
@@ -1,5 +1,9 @@
-import { BLOCKSTREAM_API_URL } from '$env/rest/blockstream.env';
-
+import {
+	BLOCKSTREAM_API_URL,
+	BLOCKSTREAM_REGTEST_API_URL,
+	BLOCKSTREAM_TESTNET_API_URL
+} from '$env/rest/blockstream.env';
+import type { BitcoinNetwork } from '@dfinity/ckbtc';
 /**
  * Get latest mined BTC block height. Used for calculating the number of confirmations for a BTC transaction.
  *
@@ -7,13 +11,33 @@ import { BLOCKSTREAM_API_URL } from '$env/rest/blockstream.env';
  * - https://github.com/Blockstream/esplora/blob/master/API.md#get-blockstipheight
  *
  */
-export const btcLatestBlockHeight = (): Promise<number> =>
+export const btcLatestBlockHeight = ({
+	bitcoinNetwork
+}: {
+	bitcoinNetwork: BitcoinNetwork;
+}): Promise<number> =>
 	fetchBlockstreamApi<number>({
-		endpointPath: `blocks/tip/height`
+		endpointPath: `blocks/tip/height`,
+		bitcoinNetwork
 	});
 
-const fetchBlockstreamApi = async <T>({ endpointPath }: { endpointPath: string }): Promise<T> => {
-	const response = await fetch(`${BLOCKSTREAM_API_URL}/${endpointPath}`);
+const fetchBlockstreamApi = async <T>({
+	endpointPath,
+	bitcoinNetwork
+}: {
+	endpointPath: string;
+	bitcoinNetwork: BitcoinNetwork;
+}): Promise<T> => {
+	let baseUrl;
+	if (bitcoinNetwork === 'testnet') {
+		baseUrl = BLOCKSTREAM_TESTNET_API_URL;
+	} else if (bitcoinNetwork === 'regtest') {
+		baseUrl = BLOCKSTREAM_REGTEST_API_URL;
+	} else {
+		baseUrl = BLOCKSTREAM_API_URL;
+	}
+
+	const response = await fetch(`${baseUrl}/${endpointPath}`);
 
 	if (!response.ok) {
 		throw new Error('Blockstream API response not ok.');
