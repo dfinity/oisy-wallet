cbcdba95a4df51cd499b16497b4770cab6feb766
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-09-03 15:27:47 +0200

    implements feature

---
+++ src/frontend/src/tests/lib/components/nfts/NftList.spec.ts
@@ -0,0 +1,45 @@
+import { mockValidErc1155Nft } from '$tests/mocks/nfts.mock';
+import { parseNftId } from '$lib/validation/nft.validation';
+import NftList from '$lib/components/nfts/NftList.svelte';
+import { render } from '@testing-library/svelte';
+import { assertNonNullish } from '@dfinity/utils';
+import { AppPath } from '$lib/constants/routes.constants';
+
+describe('NftList', () => {
+	const mockNfts = [
+		{ ...mockValidErc1155Nft, name: 'Null', id: parseNftId(0) },
+		{ ...mockValidErc1155Nft, name: 'Eins', id: parseNftId(1) },
+		{ ...mockValidErc1155Nft, name: 'Zwei', id: parseNftId(2) }
+	];
+
+	it('should render a list of nfts', () => {
+		const title = 'Nfts';
+
+		const {container, getByText} = render(NftList, {
+			props: {
+				title,
+				nfts: mockNfts,
+			}
+		})
+
+		expect(getByText(title)).toBeInTheDocument();
+
+		const grid = container.querySelector('.grid');
+
+		assertNonNullish(grid);
+
+		const links = grid.querySelectorAll('a');
+
+		assertNonNullish(links);
+
+		for (let i = 0; i < links.length; i++) {
+			expect(links.item(i).getAttribute('href')).toContain(
+				`${AppPath.Nfts}${mockValidErc1155Nft.collection.network.name}-${mockValidErc1155Nft.collection.address}`
+			);
+
+			const img = links.item(i).querySelector('.bg-secondary-alt');
+
+			assertNonNullish(img);
+		}
+	});
+});
