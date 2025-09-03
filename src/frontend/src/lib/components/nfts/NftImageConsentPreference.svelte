3f7df30f152193d2e9449e3d167380f926c03d56
daviddecentage <david.farina@dfinity.org>
2025-09-03 12:17:44 +0200

    update

--- src/frontend/src/lib/components/nfts/NftImageConsentPreference.svelte
+++ src/frontend/src/lib/components/nfts/NftImageConsentPreference.svelte
@@ -11,7 +11,7 @@
 	import { getAllowMediaForNft } from '$lib/utils/nfts.utils';
 
 	interface Props {
-		nft?: Nft;
+		nft: Nft;
 	}
 
 	const { nft }: Props = $props();
@@ -33,33 +33,31 @@
 	};
 </script>
 
-{#if nonNullish(nft)}
-	<span class="flex items-center text-right">
-		{#if hasConsent}
-			{$i18n.nfts.text.media_enabled}
-		{:else}
-			{$i18n.nfts.text.media_disabled}
-		{/if}
-		<Responsive up="md">
-			<Button
-				colorStyle="secondary-light"
-				onclick={openConsentModal}
-				styleClass="w-auto p-0 grow-0 text-nowrap hover:bg-inherit ml-2"
-				transparent>{$i18n.nfts.text.review_preference}</Button
-			>
-		</Responsive>
-		<Responsive down="sm">
-			<ButtonIcon
-				ariaLabel={$i18n.nfts.text.review_preference}
-				colorStyle="secondary-light"
-				onclick={openConsentModal}
-				styleClass="w-auto p-0 grow-0 text-nowrap hover:bg-inherit ml-2 h-auto"
-				transparent
-			>
-				{#snippet icon()}
-					<IconPenLine />
-				{/snippet}
-			</ButtonIcon>
-		</Responsive>
-	</span>
-{/if}
+<span class="flex items-center text-right">
+	{#if hasConsent}
+		{$i18n.nfts.text.media_enabled}
+	{:else}
+		{$i18n.nfts.text.media_disabled}
+	{/if}
+	<Responsive up="md">
+		<Button
+			colorStyle="secondary-light"
+			onclick={openConsentModal}
+			styleClass="w-auto p-0 grow-0 text-nowrap hover:bg-inherit ml-2"
+			transparent>{$i18n.nfts.text.review_preference}</Button
+		>
+	</Responsive>
+	<Responsive down="sm">
+		<ButtonIcon
+			ariaLabel={$i18n.nfts.text.review_preference}
+			colorStyle="secondary-light"
+			onclick={openConsentModal}
+			styleClass="w-auto p-0 grow-0 text-nowrap hover:bg-inherit ml-2 h-auto"
+			transparent
+		>
+			{#snippet icon()}
+				<IconPenLine />
+			{/snippet}
+		</ButtonIcon>
+	</Responsive>
+</span>
