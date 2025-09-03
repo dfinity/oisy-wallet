5a9f413ec717a63aa7946764c3a3c1f7c57fcac5
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-09-03 12:31:46 +0200

    resolves review feedback

--- src/frontend/src/lib/components/nfts/NftsShowHiddenToggle.svelte
+++ src/frontend/src/lib/components/nfts/NftsShowHiddenToggle.svelte
@@ -4,7 +4,7 @@
 	import { i18n } from '$lib/stores/i18n.store';
 	import { showHiddenStore } from '$lib/stores/settings.store';
 
-	let checked = $derived($showHidden);
+	const checked = $derived($showHidden);
 
 	const toggleHidden = () =>
 		showHiddenStore.set({ key: 'show-hidden', value: { enabled: !checked } });
@@ -12,4 +12,4 @@
 
 <svelte:window on:oisyToggleShowHidden={toggleHidden} />
 
-<Toggle ariaLabel={$i18n.nfts.text.show_hidden} bind:checked on:nnsToggle={toggleHidden} />
+<Toggle ariaLabel={$i18n.nfts.text.show_hidden} {checked} on:nnsToggle={toggleHidden} />
