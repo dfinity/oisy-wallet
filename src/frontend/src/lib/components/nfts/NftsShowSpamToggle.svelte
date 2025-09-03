b95d5da278ee088bac5ccb6aea9c0e84775a415b
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-09-03 12:32:25 +0200

    resolves review feedback

--- src/frontend/src/lib/components/nfts/NftsShowSpamToggle.svelte
+++ src/frontend/src/lib/components/nfts/NftsShowSpamToggle.svelte
@@ -4,11 +4,11 @@
 	import { i18n } from '$lib/stores/i18n.store';
 	import { showSpamStore } from '$lib/stores/settings.store';
 
-	let checked = $derived($showSpam);
+	const checked = $derived($showSpam);
 
 	const toggleShow = () => showSpamStore.set({ key: 'show-spam', value: { enabled: !checked } });
 </script>
 
 <svelte:window on:oisyToggleShowSpam={toggleShow} />
 
-<Toggle ariaLabel={$i18n.nfts.text.show_spam} bind:checked on:nnsToggle={toggleShow} />
+<Toggle ariaLabel={$i18n.nfts.text.show_spam} {checked} on:nnsToggle={toggleShow} />
