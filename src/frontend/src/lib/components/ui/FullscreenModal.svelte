f7837919a3c90433bf14ed67a2b28b78327aa861
daviddecentage <david.farina@dfinity.org>
2025-09-03 16:47:56 +0200

    update

---
+++ src/frontend/src/lib/components/ui/FullscreenModal.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Backdrop } from '@dfinity/gix-components';
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		children: Snippet;
+		open: boolean;
+	}
+
+	let { children, open = $bindable(false) }: Props = $props();
+</script>
+
+{#if open}
+	<Backdrop on:nnsClose={() => (open = false)} />
+
+	{@render children()}
+{/if}
