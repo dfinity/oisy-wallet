8860095bac1634833126b52e5b39eb8d075656e1
BonomoAlessandro <64775378+BonomoAlessandro@users.noreply.github.com>
2025-09-08 15:19:48 +0200

    feat(frontend): implements getNftsByOwner alchemy api endpoint (#8723)
    # Motivation
    
    We want to have a new `getNftsByOwner` alchemy api function to be able
    to load the nft metadata.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

--- src/frontend/src/eth/types/alchemy-nfts.ts
+++ src/frontend/src/eth/types/alchemy-nfts.ts
@@ -1,8 +1,41 @@
 // https://www.alchemy.com/docs/reference/nft-api-endpoints/nft-api-endpoints/nft-ownership-endpoints/get-nf-ts-for-owner-v-3
 interface OwnedNft {
 	balance: string;
-	contractAddress: string;
+	description?: string;
+	image?: Image;
+	name?: string;
+	raw: { metadata: Metadata };
 	tokenId: string;
+	contract: Contract;
+}
+
+interface Contract {
+	address: string;
+	isSpam?: boolean;
+	name?: string;
+	openSeaMetadata?: {
+		bannerImageUrl?: string;
+		description?: string;
+	};
+	symbol?: string;
+	tokenType: string;
+}
+
+interface Image {
+	cachedUrl?: string;
+	contentType?: string;
+	originalUrl?: string;
+	pngUrl?: string;
+	thumbnailUrl?: string;
+}
+
+interface Metadata {
+	attributes?: Attribute[];
+}
+
+interface Attribute {
+	trait_type: string;
+	value: string;
 }
 
 export interface AlchemyProviderOwnedNfts {
