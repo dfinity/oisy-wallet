74a57a75c9db8f78d644ad047dd077723cbbc534
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-09-08 17:51:10 +0200

    Update src/frontend/src/tests/icp/services/index-canister.services.spec.ts

--- src/frontend/src/tests/icp/services/index-canister.services.spec.ts
+++ src/frontend/src/tests/icp/services/index-canister.services.spec.ts
@@ -3,7 +3,7 @@ import { getBlocks } from '$icp/api/icrc-ledger.api';
 import { isIndexCanisterAwake } from '$icp/services/index-canister.services';
 import { mockIndexCanisterId, mockLedgerCanisterId } from '$tests/mocks/ic-tokens.mock';
 import { mockIdentity } from '$tests/mocks/identity.mock';
-import type { IcrcStatus } from '@dfinity/ledger-icrc';
+import type { IcrcNgStatus } from '@dfinity/ledger-icrc';
 import type { IcrcGetBlocksResult } from '@dfinity/ledger-icrc';
 
 vi.mock('$icp/api/icrc-index-ng.api', () => ({
