b50a72b1c1a902c112ebe1eb7e518374a8020dfc
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-09-08 10:48:06 +0200

    tests

--- src/frontend/src/icp/api/icrc-ledger.api.ts
+++ src/frontend/src/icp/api/icrc-ledger.api.ts
@@ -7,12 +7,14 @@ import type { Identity } from '@dfinity/agent';
 import type { Allowance } from '@dfinity/ledger-icp/dist/candid/ledger';
 import {
 	IcrcLedgerCanister,
+	type GetBlocksParams,
 	type IcrcAccount,
 	type IcrcBlockIndex,
 	type IcrcSubaccount,
 	type IcrcTokenMetadataResponse,
 	type IcrcTokens
 } from '@dfinity/ledger-icrc';
+import type { GetBlocksResult } from '@dfinity/ledger-icrc/dist/candid/icrc_ledger';
 import { Principal } from '@dfinity/principal';
 import { assertNonNullish, toNullable, type QueryParams } from '@dfinity/utils';
 
@@ -209,6 +211,22 @@ const toAccount = ({
 	owner,
 	subaccount: toNullable(subaccount)
 });
+
+export const getBlocks = async ({
+	certified = true,
+	identity,
+	ledgerCanisterId,
+	...rest
+}: {
+	identity: OptionIdentity;
+	ledgerCanisterId: CanisterIdText;
+} & GetBlocksParams): Promise<GetBlocksResult> => {
+	assertNonNullish(identity);
+
+	const { getBlocks } = await ledgerCanister({ identity, ledgerCanisterId });
+
+	return getBlocks({ certified, ...rest });
+};
 
 const ledgerCanister = async ({
 	identity,
