6d4209b3ba618d01db89c211f2344b34616ddc81
daviddecentage <david.farina@dfinity.org>
2025-09-09 19:03:54 +0200

    update

--- src/frontend/src/lib/components/send/Send.svelte
+++ src/frontend/src/lib/components/send/Send.svelte
@@ -3,10 +3,20 @@
 	import SendModal from '$lib/components/send/SendModal.svelte';
 	import { modalSend } from '$lib/derived/modal.derived';
 	import { modalStore } from '$lib/stores/modal.store';
+	import SendButton from '$lib/components/send/SendButton.svelte';
 
-	export let isTransactionsPage: boolean;
+	interface Props {
+		isTransactionsPage: boolean;
+	}
+
+	const { isTransactionsPage }: Props = $props();
 </script>
 
 <SendButtonWithModal isOpen={$modalSend} open={modalStore.openSend}>
-	<SendModal slot="modal" {isTransactionsPage} on:nnsClose />
+	{#snippet button(onclick)}
+		<SendButton {onclick} />
+	{/snippet}
+	{#snippet modal()}
+		<SendModal {isTransactionsPage} on:nnsClose />
+	{/snippet}
 </SendButtonWithModal>
