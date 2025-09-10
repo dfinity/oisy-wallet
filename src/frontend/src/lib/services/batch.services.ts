6caeb1784c010867735cb2770299aa0cbd1b0933
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-09-10 10:15:01 +0200

    implements createBatches function

--- src/frontend/src/lib/services/batch.services.ts
+++ src/frontend/src/lib/services/batch.services.ts
@@ -12,3 +12,8 @@ export const batch = async function* <T>({
 		await new Promise((resolve) => setTimeout(resolve, 1000));
 	}
 };
+
+export const createBatches = <T>({ items, batchSize }: { items: T[]; batchSize: number }): T[][] =>
+	Array.from({ length: Math.ceil(items.length / batchSize) }, (_, index) =>
+		items.slice(index * batchSize, (index + 1) * batchSize)
+	);
