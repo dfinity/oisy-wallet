5c6315f3b73805864f5e93617284870e5590fbff
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-09-10 09:55:23 +0200

    refactor(frontend): Use ERC tokens utils to check the standard (#8773)
    # Motivation
    
    When possible we would prefer to use the utils `isTokenErc1155`,
    `isTokenErc721` and `isTokenErc20`.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/eth/utils/erc1155.utils.ts
+++ src/frontend/src/eth/utils/erc1155.utils.ts
@@ -2,7 +2,7 @@ import type { Erc1155Token } from '$eth/types/erc1155';
 import type { Erc1155CustomToken } from '$eth/types/erc1155-custom-token';
 import type { Token } from '$lib/types/token';
 
-export const isTokenErc1155CustomToken = (token: Token): token is Erc1155CustomToken =>
-	token.standard === 'erc1155' && 'enabled' in token;
-
 export const isTokenErc1155 = (token: Token): token is Erc1155Token => token.standard === 'erc1155';
+
+export const isTokenErc1155CustomToken = (token: Token): token is Erc1155CustomToken =>
+	isTokenErc1155(token) && 'enabled' in token;
