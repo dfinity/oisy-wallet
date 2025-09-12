2a372abf4039998982e98b2cf6794bd13c27fbc2
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-09-13 00:57:26 +0200

    refactor(frontend): Migrate Svelte page store to page state

--- src/frontend/src/lib/derived/nav.derived.ts
+++ src/frontend/src/lib/derived/nav.derived.ts
@@ -1,54 +1,17 @@
-import { page } from '$app/stores';
+import { page } from '$app/state';
 import type { OptionString } from '$lib/types/string';
 import { derived, type Readable } from 'svelte/store';
 
-export const routeToken: Readable<OptionString> = derived(
-	[page],
-	([
-		{
-			data: { token }
-		}
-	]) => token
-);
+export const routeToken: Readable<OptionString> = derived([], () => page.data.token);
 
-export const routeNetwork: Readable<OptionString> = derived(
-	[page],
-	([
-		{
-			data: { network }
-		}
-	]) => network
-);
+export const routeNetwork: Readable<OptionString> = derived([], () => page.data.network);
 
 // TODO: replace with routeNetwork when the NFT network ID is in `data` instead of `params`
 // TODO: confirm that is the ID and not the name of the network that is being passed
-export const routeNftNetwork: Readable<OptionString> = derived(
-	[page],
-	([
-		{
-			params: { networkId }
-		}
-	]) => networkId
-);
+export const routeNftNetwork: Readable<OptionString> = derived([], () => page.params.networkId);
 
 // TODO: replace with `data` instead of `params`
-export const routeCollection: Readable<OptionString> = derived(
-	[page],
-	([
-		{
-			// TODO: collectionId --> collection
-			params: { collectionId }
-		}
-	]) => collectionId
-);
+export const routeCollection: Readable<OptionString> = derived([], () => page.params.collectionId);
 
 // TODO: replace with `data` instead of `params`
-export const routeNft: Readable<OptionString> = derived(
-	[page],
-	([
-		{
-			// TODO: nftId --> nft
-			params: { nftId }
-		}
-	]) => nftId
-);
+export const routeNft: Readable<OptionString> = derived([], () => page.params.nftId);
