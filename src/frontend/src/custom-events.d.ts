7d249c4d0912450f6a4e7aaa18aee77ae6ac3bce
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-09-15 11:14:17 +0200

    feat(frontend): Move the custom OISY events to a specific module

---
+++ src/frontend/src/custom-events.d.ts
@@ -0,0 +1,35 @@
+// TODO: try to provide the correct types to the events
+declare module 'svelte/elements' {
+	interface HTMLAttributes<T> {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		onoisyTriggerWallet?: (event: CustomEvent<any>) => void;
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		onoisyReceive?: (event: CustomEvent<any>) => void;
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		onoisyReceiveCkBTC?: (event: CustomEvent<any>) => void;
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		onoisyCkBtcUpdateBalance?: (event: CustomEvent<any>) => void;
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		onoisyCkBtcMinterInfoStatus?: (event: CustomEvent<any>) => void;
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		onoisyCkEthMinterInfoStatus?: (event: CustomEvent<any>) => void;
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		onoisyCkEthereumPendingTransactions?: (event: CustomEvent<any>) => void;
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		onoisyIndexCanisterBalanceOutOfSync?: (event: CustomEvent<any>) => void;
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		onoisyValidateAddresses?: (event: CustomEvent<any>) => void;
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		onoisyRefreshUserProfile?: (event: CustomEvent<any>) => void;
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		onoisyToggleZeroBalances?: (event: CustomEvent<any>) => void;
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		onoisyRefreshContacts?: (event: CustomEvent<any>) => void;
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		onoisyToggleShowHidden?: (event: CustomEvent<any>) => void;
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		onoisyToggleShowSpam?: (event: CustomEvent<any>) => void;
+	}
+}
+
+export {};
