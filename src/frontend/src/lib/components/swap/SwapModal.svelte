5d2c4af5c9fa2b581f1aeadec52a3a4b45fde99a
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-09-15 14:37:19 +0200

    fix

--- src/frontend/src/lib/components/swap/SwapModal.svelte
+++ src/frontend/src/lib/components/swap/SwapModal.svelte
@@ -27,7 +27,6 @@
 	import type { SwapMappedResult, SwapSelectTokenType } from '$lib/types/swap';
 	import type { Token } from '$lib/types/token';
 	import { closeModal } from '$lib/utils/modal.utils';
-	import { OISY_URL } from '$lib/constants/oisy.constants';
 
 	const { setSourceToken, setDestinationToken } = setContext<SwapContext>(
 		SWAP_CONTEXT_KEY,
