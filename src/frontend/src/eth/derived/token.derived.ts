d0e009d6d37acc3bed4685f4a126683eed607355
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-09-15 15:52:07 +0200

    more

--- src/frontend/src/eth/derived/token.derived.ts
+++ src/frontend/src/eth/derived/token.derived.ts
@@ -1,21 +1,18 @@
 import { selectedEthereumNetwork } from '$eth/derived/network.derived';
 import { enabledEthereumTokens } from '$eth/derived/tokens.derived';
-import { evmNativeToken } from '$evm/derived/token.derived';
 import { DEFAULT_ETHEREUM_TOKEN } from '$lib/constants/tokens.constants';
-import type { RequiredToken, TokenId } from '$lib/types/token';
+import type { RequiredTokenWithLinkedData, TokenId } from '$lib/types/token';
 import { derived, type Readable } from 'svelte/store';
 
 /**
  * Native token - i.e. not ERC20 - for the selected Ethereum/EVM network.
  */
-export const nativeEthereumToken: Readable<RequiredToken> = derived(
-	[enabledEthereumTokens, selectedEthereumNetwork, evmNativeToken],
-	([$enabledEthereumTokens, $selectedEthereumNetwork, $evmNativeToken]) =>
+export const nativeEthereumToken: Readable<RequiredTokenWithLinkedData> = derived(
+	[enabledEthereumTokens, selectedEthereumNetwork],
+	([$enabledEthereumTokens, $selectedEthereumNetwork]) =>
 		$enabledEthereumTokens.find(
 			({ network: { id: networkId } }) => $selectedEthereumNetwork?.id === networkId
-		) ??
-		$evmNativeToken ??
-		DEFAULT_ETHEREUM_TOKEN
+		) ?? DEFAULT_ETHEREUM_TOKEN
 );
 
 export const nativeEthereumTokenId: Readable<TokenId> = derived(
