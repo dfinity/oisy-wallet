679e8665f9998e1e39411c141fd196258c2468b6
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-09-15 12:56:32 +0200

    feat(frontend): Check for Ethereum tokens in EthFeeStoreContext

--- src/frontend/src/eth/components/fee/EthFeeStoreContext.svelte
+++ src/frontend/src/eth/components/fee/EthFeeStoreContext.svelte
@@ -9,6 +9,7 @@
 	} from '$eth/stores/eth-fee.store';
 	import { exchanges } from '$lib/derived/exchange.derived';
 	import type { Token, TokenId } from '$lib/types/token';
+	import { isNetworkIdEthereum, isNetworkIdEvm } from '$lib/utils/network.utils';
 
 	interface Props {
 		token: Token;
@@ -27,13 +28,36 @@
 
 	const feeExchangeRateStore = writable<number | undefined>(undefined);
 
+	let networkId = $derived(token.network.id);
+
+	let isEthNetwork = $derived(isNetworkIdEthereum(networkId) || isNetworkIdEvm(networkId));
+
+	const reset = () => {
+		feeSymbolStore.set(undefined);
+		feeTokenIdStore.set(undefined);
+		feeDecimalsStore.set(undefined);
+		feeExchangeRateStore.set(undefined);
+	};
+
 	$effect(() => {
+		if (!isEthNetwork) {
+			reset();
+
+			return;
+		}
+
 		feeSymbolStore.set(token.symbol);
 		feeTokenIdStore.set(token.id);
 		feeDecimalsStore.set(token.decimals);
 	});
 
 	$effect(() => {
+		if (!isEthNetwork) {
+			reset();
+
+			return;
+		}
+
 		feeExchangeRateStore.set($exchanges?.[token.id]?.usd);
 	});
 
