09588656c01a47bfe3e3c510f27d6c22aa1eb0dd
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-09-16 10:54:38 +0200

    implements feature

--- src/frontend/src/lib/components/rewards/RewardNetworkBonus.svelte
+++ src/frontend/src/lib/components/rewards/RewardNetworkBonus.svelte
@@ -17,6 +17,7 @@
 	import networkBonusDisabled8 from '$lib/assets/rewards/network-bonus-disabled-8.svg';
 	import Img from '$lib/components/ui/Img.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
+	import { REWARDS_NETWORK_MULTIPLIER_IMAGE } from '$lib/constants/test-ids.constants';
 
 	interface Props {
 		isEligible: boolean;
@@ -43,4 +44,4 @@
 	);
 </script>
 
-<Img alt={$i18n.rewards.text.network_bonus} src={networkBonusSource} />
+<Img alt={$i18n.rewards.text.network_bonus} src={networkBonusSource} testId={REWARDS_NETWORK_MULTIPLIER_IMAGE} />
