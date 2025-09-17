0b2de18e3d4f2879391f1440335798050d05401b
AlessandroBonomo <bonomo.alessandro@yahoo.de>
2025-09-17 15:24:02 +0200

    displays network bonus multiplier info

--- src/frontend/src/lib/components/rewards/RewardsRequirements.svelte
+++ src/frontend/src/lib/components/rewards/RewardsRequirements.svelte
@@ -6,15 +6,22 @@
 	import { REWARDS_REQUIREMENTS_STATUS } from '$lib/constants/test-ids.constants';
 	import { i18n } from '$lib/stores/i18n.store';
 	import type { CampaignCriterion } from '$lib/types/reward';
+	import IconHelp from '$lib/components/icons/lucide/IconHelp.svelte';
+	import ExternalLink from '$lib/components/ui/ExternalLink.svelte';
+	import { slide } from 'svelte/transition';
+	import type { RewardCampaignDescription } from '$env/types/env-reward';
 
 	interface Props {
 		isEligible: boolean;
 		hasNetworkBonus: boolean;
 		networkBonusMultiplier: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 		criteria: CampaignCriterion[];
+		reward: RewardCampaignDescription;
 	}
 
-	let { isEligible, hasNetworkBonus, networkBonusMultiplier, criteria }: Props = $props();
+	let { isEligible, hasNetworkBonus, networkBonusMultiplier, criteria, reward }: Props = $props();
+
+	let infoExpanded = $state(false);
 </script>
 
 {#if criteria.length > 0}
@@ -32,9 +39,25 @@
 
 			{#if hasNetworkBonus && nonNullish(networkBonusMultiplier)}
 				<NetworkBonusImage disabled={!isEligible} multiplier={networkBonusMultiplier} />
+
+				<button class="p-0.5 text-tertiary" onclick={() => (infoExpanded = !infoExpanded)}>
+					<IconHelp size="18" />
+				</button>
 			{/if}
 		</div>
+
+		{#if infoExpanded}
+			<span class="mt-1 w-full text-sm text-tertiary" transition:slide>
+				{$i18n.rewards.requirements.network_bonus_info}
+				<ExternalLink
+					ariaLabel={$i18n.rewards.text.learn_more}
+					href={reward.learnMoreHref}
+					iconVisible={false}>{$i18n.rewards.text.learn_more}</ExternalLink
+				>
+			</span>
+		{/if}
 	</div>
+
 	<ul class="list-none">
 		{#each criteria as criterion, i (criterion)}
 			<li class="flex gap-2 pt-1">
