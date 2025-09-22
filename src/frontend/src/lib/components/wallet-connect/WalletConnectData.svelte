caa5dc611d20d93957cf4807eb1dba97cc70bad8
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-09-22 13:39:32 +0200

    style(frontend): Use common style for WalletConnectData

--- src/frontend/src/lib/components/wallet-connect/WalletConnectData.svelte
+++ src/frontend/src/lib/components/wallet-connect/WalletConnectData.svelte
@@ -3,22 +3,28 @@
 	import Copy from '$lib/components/ui/Copy.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { shortenWithMiddleEllipsis } from '$lib/utils/format.utils';
+	import ModalValue from '$lib/components/ui/ModalValue.svelte';
 
 	interface Props {
 		data: string | undefined;
 		label: string;
 	}
-	let { data, label }: Props = $props();
+
+	let { data, label: labelStr }: Props = $props();
 </script>
 
 {#if nonNullish(data)}
-	<label class="font-bold" for="data">{label}:</label>
+	<ModalValue>
+		{#snippet label()}{labelStr}{/snippet}
 
-	<div id="data" class="mb-4 flex items-center gap-1 font-normal">
-		{shortenWithMiddleEllipsis({ text: data })}<Copy
-			inline
-			text={$i18n.wallet_connect.text.raw_copied}
-			value={data}
-		/>
-	</div>
+		{#snippet mainValue()}
+			<div id="data" class="mb-4 flex items-center gap-1 font-normal">
+				{shortenWithMiddleEllipsis({ text: data })}<Copy
+					inline
+					text={$i18n.wallet_connect.text.raw_copied}
+					value={data}
+				/>
+			</div>
+		{/snippet}
+	</ModalValue>
 {/if}
