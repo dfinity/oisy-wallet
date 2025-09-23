4321f008cc48bed10e572cd265b25c2315d70a93
DecentAgeCoder <marco.oesch@dfinity.org>
2025-09-23 12:21:47 +0200

    Remove testnet API URL and rename `blockchain-transactions.utils.ts` to `btc-transactions.utils.ts`.

--- src/frontend/src/env/rest/blockchain.env.ts
+++ src/frontend/src/env/rest/blockchain.env.ts
@@ -5,8 +5,3 @@ export const BLOCKCHAIN_API_URL = safeParse({
 	schema: UrlSchema,
 	value: 'https://blockchain.info'
 });
-
-export const BLOCKCHAIN_TESTNET_API_URL = safeParse({
-	schema: UrlSchema,
-	value: 'https://testnet.blockchain.info'
-});
