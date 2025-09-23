ba2c5dea2b31e2def7c7e42aa7b57a9a25cc1986
DecentAgeCoder <marco.oesch@dfinity.org>
2025-09-23 13:15:45 +0200

    Integrate network-aware Blockstream API with support for multiple Bitcoin networks

--- src/frontend/src/env/rest/blockstream.env.ts
+++ src/frontend/src/env/rest/blockstream.env.ts
@@ -5,3 +5,10 @@ export const BLOCKSTREAM_API_URL = safeParse({
 	schema: UrlSchema,
 	value: 'https://blockstream.info/api'
 });
+
+export const BLOCKSTREAM_TESTNET_API_URL = safeParse({
+	schema: UrlSchema,
+	value: 'https://blockstream.info/testnet/api'
+});
+
+export const BLOCKSTREAM_REGTEST_API_URL = 'http://localhost:3000/api';
