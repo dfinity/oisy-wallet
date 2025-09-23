40033af84f5fbbf69b04ab58b394321033e7825a
daviddecentage <david.farina@dfinity.org>
2025-09-23 16:39:07 +0200

    update

--- src/frontend/src/lib/components/ui/RadioBox.svelte
+++ src/frontend/src/lib/components/ui/RadioBox.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
+	import Radio from '$lib/components/ui/Radio.svelte';
 
 	interface Props {
-		selected: boolean;
+		checked: boolean;
 		disabled?: boolean;
 		onChange: () => void;
 		name?: string;
@@ -10,61 +11,12 @@
 		label: Snippet;
 	}
 
-	const { selected, disabled = false, onChange, name, id, label }: Props = $props();
+	const { checked, disabled = false, onChange, id, label }: Props = $props();
 </script>
 
 <label
-	class="flex cursor-pointer items-center gap-3 rounded-lg border border-brand-subtle-20 bg-brand-subtle-10 p-3"
+	class="flex w-full cursor-pointer items-center gap-3 rounded-xl border border-brand-subtle-20 bg-brand-subtle-10 p-3"
 >
-	<input
-		{id}
-		{name}
-		class:selected
-		checked={selected}
-		{disabled}
-		onchange={onChange}
-		type="radio"
-	/>
+	<Radio {checked} {disabled} {onChange} inputId={id} />
 	{@render label()}
 </label>
-
-<style lang="scss">
-	input[type='radio'] {
-		appearance: none;
-		margin: 0;
-		width: 20px;
-		height: 20px;
-		border-radius: var(--border-radius-lg);
-		cursor: pointer;
-		position: relative;
-		box-sizing: border-box;
-		background: var(--input-background);
-		color: var(--color-background-brand-primary);
-		border: var(--input-border-size) solid var(--input-border-color);
-		transition:
-			color var(--animation-time-short) ease-out,
-			background var(--animation-time-short) ease-out,
-			border var(--animation-time-short) ease-in;
-
-		&:after {
-			content: '';
-			position: absolute;
-			top: 50%;
-			left: 50%;
-			transform: translate(-50%, -50%);
-			width: 8px;
-			height: 8px;
-			border-radius: 50%;
-			background: var(--input-background);
-		}
-
-		&:checked {
-			background: var(--color-background-brand-primary);
-			border: var(--input-border-size) solid var(--color-background-brand-primary);
-		}
-
-		&:hover {
-			border: var(--input-border-size) solid var(--color-background-brand-primary);
-		}
-	}
-</style>
