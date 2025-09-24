7360821f1f4c94d83b6474c65d4f46b7cce4d46d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-09-24 14:01:56 +0200

    Update src/frontend/src/btc/components/send/BtcSendForm.svelte

--- src/frontend/src/btc/components/send/BtcSendForm.svelte
+++ src/frontend/src/btc/components/send/BtcSendForm.svelte
@@ -41,7 +41,7 @@
 	{selectedContact}
 >
 	{#snippet sendAmount()}
-		<BtcSendAmount slot="amount" bind:amount bind:amountError on:icTokensList />
+		<BtcSendAmount  bind:amount bind:amountError on:icTokensList />
 	{/snippet}
 
 	<!--	TODO: calculate and display transaction fee	-->
