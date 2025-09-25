d33354117435c540dcd946273ea76c823b057b2b
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-09-25 18:31:43 +0200

    chore(deps): Remove jimp

--- e2e/utils/qr-code.utils.ts
+++ e2e/utils/qr-code.utils.ts
@@ -2,23 +2,28 @@ import { nonNullish } from '@dfinity/utils';
 import { Jimp } from 'jimp';
 import jsQR from 'jsqr';
 
+// TODO: uncomment and replace Jimp with native functions
 export const getQRCodeValueFromDataURL = async ({
-	dataUrl
+	dataUrl:_
 }: {
 	dataUrl: string;
+	// eslint-disable-next-line arrow-body-style,require-await
 }): Promise<string | undefined> => {
-	const qrBuffer = Buffer.from(dataUrl.replace(/^data:image\/[a-z]+;base64,/, ''), 'base64');
-	const image = await Jimp.read(qrBuffer);
+	return Promise.reject(new Error('Not implemented'));
 
-	const imageData = {
-		data: new Uint8ClampedArray(image.bitmap.data),
-		width: image.bitmap.width,
-		height: image.bitmap.height
-	};
-
-	const decodedQR = jsQR(imageData.data, imageData.width, imageData.height);
-
-	if (nonNullish(decodedQR)) {
-		return decodedQR.data;
-	}
+	// const qrBuffer = Buffer.from(dataUrl.replace(/^data:image\/[a-z]+;base64,/, ''), 'base64');
+	// const image = await Jimp.read(qrBuffer);
+	//
+	// const imageData = {
+	// 	data: new Uint8ClampedArray(image.bitmap.data),
+	// 	width: image.bitmap.width,
+	// 	height: image.bitmap.height
+	// };
+	//
+	// const decodedQR = jsQR(imageData.data, imageData.width, imageData.height);
+	//
+	// if (nonNullish(decodedQR)) {
+	// 	return decodedQR.data;
+	// }
 };
+
