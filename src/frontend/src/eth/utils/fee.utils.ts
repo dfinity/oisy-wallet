54836750c7241e22edd58650af2d5e27fbef88b4
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-09-25 18:07:56 +0200

    fix

--- src/frontend/src/eth/utils/fee.utils.ts
+++ src/frontend/src/eth/utils/fee.utils.ts
@@ -5,8 +5,10 @@ import { isNullish } from '@dfinity/utils';
 export const maxGasFee = ({
 	maxFeePerGas,
 	gas: estimatedGasFee
-}: TransactionFeeData): bigint | undefined =>
-	isNullish(maxFeePerGas) ? undefined : maxFeePerGas * estimatedGasFee;
+}: TransactionFeeData): bigint | undefined => {
+	console.log({ maxFeePerGas, estimatedGasFee }, 'maxGasFee');
+	return isNullish(maxFeePerGas) ? undefined : maxFeePerGas * estimatedGasFee;
+};
 
 export const minGasFee = ({
 	maxPriorityFeePerGas,
