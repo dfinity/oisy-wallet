dc01c82b76f89be5d1f5c7f70484fd9343186e9a
CvetelinaAngelova <110659037+CvetelinaAngelova@users.noreply.github.com>
2025-09-25 17:31:42 +0300

    add amount to the ICP

--- src/frontend/src/icp/types/ic-transaction.ts
+++ src/frontend/src/icp/types/ic-transaction.ts
@@ -2,7 +2,7 @@ import type { Event } from '$declarations/xtc_ledger/xtc_ledger.did';
 import type { Dip20TransactionWithId } from '$icp/types/api';
 import type { icpTransactionTypes } from '$lib/schema/transaction.schema';
 import type { TransactionId, TransactionType } from '$lib/types/transaction';
-import type { Transaction, TransactionWithId } from '@dfinity/ledger-icp';
+import type { Tokens, Transaction, TransactionWithId } from '@dfinity/ledger-icp';
 import type {
 	IcrcTransaction as IcrcTransactionCandid,
 	IcrcTransactionWithId
@@ -40,6 +40,7 @@ export interface IcTransactionUi {
 	type: IcTransactionType;
 	// e.g. BTC Received
 	typeLabel?: string;
+	fee?: Tokens;
 	from?: string;
 	// e.g. From: BTC Network
 	fromLabel?: string;
