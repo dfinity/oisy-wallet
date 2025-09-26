2cacbc8967c03f4fe53e1ac106b6afea89fe3961
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-09-26 15:06:34 +0200

    more

--- src/frontend/src/lib/services/auth-broadcast.services.ts
+++ src/frontend/src/lib/services/auth-broadcast.services.ts
@@ -1,5 +1,4 @@
 import { OISY_URL } from '$lib/constants/oisy.constants';
-import { authStore } from '$lib/stores/auth.store';
 
 export const AUTH_BROADCAST_CHANNEL = 'oisy_auth_channel';
 
@@ -9,23 +8,19 @@ export const AUTH_BROADCAST_MESSAGE_LOGIN_SUCCESS = 'authClientLoginSuccess';
 // there could be a mismatch of the cached delegation chain vs the identity key of the `authClient` object.
 // This causes the `authClient` to be unable to correctly sign calls, raising Trust Errors.
 // To mitigate this, we use a `BroadcastChannel` to notify other tabs when a login has occurred, so that they can sync their `authClient` object.
-class AuthBroadcastChannel {
+export class AuthBroadcastChannel {
 	readonly #bc: BroadcastChannel;
 
 	constructor() {
 		this.#bc = new BroadcastChannel(AUTH_BROADCAST_CHANNEL);
 	}
 
-	private onLoginSuccess = (handler: () => Promise<void>) => {
+	onLoginSuccess = (handler: () => Promise<void>) => {
 		this.#bc.onmessage = async ($event) => {
 			if ($event.origin === OISY_URL && $event.data === AUTH_BROADCAST_MESSAGE_LOGIN_SUCCESS) {
 				await handler();
 			}
 		};
-	};
-
-	init = () => {
-		this.onLoginSuccess(authStore.forceSync);
 	};
 
 	close = () => {
@@ -36,19 +31,3 @@ class AuthBroadcastChannel {
 		this.#bc.postMessage(AUTH_BROADCAST_MESSAGE_LOGIN_SUCCESS);
 	};
 }
-
-export const initAuthBroadcastChannel = (): AuthBroadcastChannel => {
-	const bc = new AuthBroadcastChannel();
-
-	bc.init();
-
-	return bc;
-};
-
-export const broadcastAuthClientLoginSuccess = () => {
-	const bc = new AuthBroadcastChannel();
-
-	bc.postLoginSuccess();
-
-	bc.close();
-};
--- src/frontend/src/routes/+layout.svelte
+++ src/frontend/src/routes/+layout.svelte
@@ -14,7 +14,10 @@
 	} from '$lib/constants/analytics.contants';
 	import { isLocked } from '$lib/derived/locked.derived';
 	import { initPlausibleAnalytics, trackEvent } from '$lib/services/analytics.services';
-	import { initAuthBroadcastChannel } from '$lib/services/auth-broadcast.services';
+	import {
+		AuthBroadcastChannel,
+		initAuthBroadcastChannel
+	} from '$lib/services/auth-broadcast.services';
 	import { displayAndCleanLogoutMsg } from '$lib/services/auth.services';
 	import { initAuthWorker } from '$lib/services/worker.auth.services';
 	import { authStore, type AuthStoreData } from '$lib/stores/auth.store';
@@ -111,7 +114,9 @@
 	});
 
 	const openBc = () => {
-		const bc = initAuthBroadcastChannel();
+		const bc = new AuthBroadcastChannel();
+
+		bc.onLoginSuccess(authStore.forceSync);
 
 		return () => {
 			bc?.close();
