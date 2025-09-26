2bd5b2955dcea1484206b702dcb41c4a3e62b057
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-09-26 12:06:12 +0200

    fix(frontend): Wrong place for nullish assertion in overwriteStoredIdentityKey

--- src/frontend/src/lib/stores/auth.store.ts
+++ src/frontend/src/lib/stores/auth.store.ts
@@ -85,11 +85,11 @@ const initAuthStore = (): AuthStore => {
 	 * TODO: Remove this when `authClient` will handle it by itself during login.
 	 */
 	const overwriteStoredIdentityKey = async () => {
-		// In the unlikely event of an error while setting a value in IndexedDB,
-		// we log out the user and refresh the page to prevent potential conflicts.
-		assertNonNullish(authClient);
-
 		try {
+			// In the unlikely event of an error while setting a value in IndexedDB,
+			// we log out the user and refresh the page to prevent potential conflicts.
+			assertNonNullish(authClient);
+
 			const key = authClient['_key'];
 
 			await authClientStorage.set(KEY_STORAGE_KEY, (key as ECDSAKeyIdentity).getKeyPair());
