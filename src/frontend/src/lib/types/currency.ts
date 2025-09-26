b69c590206cbbed31fd4e862b82a0bfcaa240ceb
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-09-26 09:53:59 +0200

    build(deps): Bump `zod` to v4 (#9146)
    # Motivation
    
    In an effort to keep up with to the changes in `zod`, we can bump it to
    v4: until now the code was already migrated to use the new major
    version, using the v3-subpackage `zod/v4`:
    
    ```ts
    import * as z from 'zod/v4';
    ```
    
    Now, all this imports can simply be:
    
    ```ts
    import * as z from 'zod';
    ```
    
    # Note
    
    Before merging, we require to wait for `oisy-wallet-signer` to have the
    latest `zod` too:
    
    - https://github.com/dfinity/oisy-wallet-signer/pull/665
    
    # Changes
    
    - Bump all dependencies related to zod:
    
    ```bash
    npm rm \
      zod \
      @dfinity/oisy-wallet-signer \
      @dfinity/zod-schemas \
      @velora-dex/sdk \
      @reown/walletkit \
      @walletconnect/auth-client \
    && npm i \
      zod@^4.0.0 \
      @dfinity/oisy-wallet-signer \
      @dfinity/zod-schemas \
      @velora-dex/sdk \
      @reown/walletkit \
      @walletconnect/auth-client
    ```
    
    - Adapt all the imports.
    
    
    # Tests
    
    There was only one test failing for util `parseMetadataResourceUrl`, but
    it is just an hedge-case test, and now it is fixed.

--- src/frontend/src/lib/types/currency.ts
+++ src/frontend/src/lib/types/currency.ts
@@ -1,5 +1,5 @@
 import type { CurrencyDataSchema, CurrencyExchangeDataSchema } from '$lib/schema/currency.schema';
-import type * as z from 'zod/v4';
+import type * as z from 'zod';
 
 export type CurrencyData = z.infer<typeof CurrencyDataSchema>;
 
