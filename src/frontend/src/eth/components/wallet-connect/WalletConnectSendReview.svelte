29d14bbcd001de532cea39b9b8c291e08c09c681
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-09-29 09:26:38 +0200

    refactor(frontend): Use SendReview component in WalletConnect flow

--- src/frontend/src/eth/components/wallet-connect/WalletConnectSendReview.svelte
+++ src/frontend/src/eth/components/wallet-connect/WalletConnectSendReview.svelte
@@ -1,18 +1,15 @@
 <script lang="ts">
+	import { Html } from '@dfinity/gix-components';
 	import { nonNullish } from '@dfinity/utils';
-	import { getContext } from 'svelte';
 	import EthFeeDisplay from '$eth/components/fee/EthFeeDisplay.svelte';
-	import SendReviewNetwork from '$eth/components/send/SendReviewNetwork.svelte';
 	import type { EthereumNetwork } from '$eth/types/network';
 	import { decodeErc20AbiDataValue } from '$eth/utils/transactions.utils';
-	import SendData from '$lib/components/send/SendData.svelte';
+	import ReviewNetwork from '$lib/components/send/ReviewNetwork.svelte';
+	import SendReview from '$lib/components/send/SendReview.svelte';
 	import ContentWithToolbar from '$lib/components/ui/ContentWithToolbar.svelte';
 	import WalletConnectActions from '$lib/components/wallet-connect/WalletConnectActions.svelte';
 	import WalletConnectData from '$lib/components/wallet-connect/WalletConnectData.svelte';
-	import { ethAddress } from '$lib/derived/address.derived';
-	import { balance } from '$lib/derived/balances.derived';
 	import { i18n } from '$lib/stores/i18n.store';
-	import { SEND_CONTEXT_KEY, type SendContext } from '$lib/stores/send.store';
 	import type { Network } from '$lib/types/network';
 	import { formatToken } from '$lib/utils/format.utils';
 
@@ -41,26 +38,28 @@
 	let amountDisplay = $derived(
 		erc20Approve && nonNullish(data) ? decodeErc20AbiDataValue({ data }) : amount
 	);
-
-	const { sendToken } = getContext<SendContext>(SEND_CONTEXT_KEY);
 </script>
 
 <ContentWithToolbar>
-	<SendData
-		amount={formatToken({ value: amountDisplay })}
-		balance={$balance}
-		{destination}
-		source={$ethAddress ?? ''}
-		token={$sendToken}
-	>
-		<WalletConnectData {data} label={$i18n.wallet_connect.text.hex_data} />
+	<SendReview amount={formatToken({ value: amountDisplay })} {destination}>
+		{#snippet info()}
+			<WalletConnectData {data} label={$i18n.wallet_connect.text.hex_data} />
+		{/snippet}
 
-		<EthFeeDisplay slot="fee" />
+		{#snippet fee()}
+			<EthFeeDisplay>
+				{#snippet label()}
+					<Html text={$i18n.fee.text.max_fee_eth} />
+				{/snippet}
+			</EthFeeDisplay>
+		{/snippet}
 
-		<SendReviewNetwork slot="network" {sourceNetwork} {targetNetwork} token={$sendToken} />
-	</SendData>
+		{#snippet network()}
+			<ReviewNetwork destinationNetworkId={targetNetwork?.id} {sourceNetwork} />
+		{/snippet}
 
-	{#snippet toolbar()}
-		<WalletConnectActions {onApprove} {onReject} />
-	{/snippet}
+		{#snippet replaceToolbar()}
+			<WalletConnectActions {onApprove} {onReject} />
+		{/snippet}
+	</SendReview>
 </ContentWithToolbar>
