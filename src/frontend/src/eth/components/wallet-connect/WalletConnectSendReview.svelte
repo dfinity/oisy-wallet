831e2f1872eee11d79faab2b4e03d4c3e06f3547
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-09-29 10:10:41 +0200

    refactor(frontend): Replace deprecated network component in WalletConnect modal (#9237)
    # Motivation
    
    Component `SendReviewNetwork` is deprecated and we currently use the
    generic component `ReviewNetwork`. To keep the modals consistent, we use
    the same for the WalletConnect review modal.
    
    ### Before
    
    <img width="659" height="740" alt="Screenshot 2025-09-29 at 09 37 12"
    src="https://github.com/user-attachments/assets/c4c28b40-7bde-4295-b942-8beff2c0885b"
    />
    
    ### After
    
    <img width="663" height="662" alt="Screenshot 2025-09-29 at 09 42 16"
    src="https://github.com/user-attachments/assets/d5c64619-5314-4ed5-b320-f213d2e68a5e"
    />

--- src/frontend/src/eth/components/wallet-connect/WalletConnectSendReview.svelte
+++ src/frontend/src/eth/components/wallet-connect/WalletConnectSendReview.svelte
@@ -3,9 +3,9 @@
 	import { nonNullish } from '@dfinity/utils';
 	import { getContext } from 'svelte';
 	import EthFeeDisplay from '$eth/components/fee/EthFeeDisplay.svelte';
-	import SendReviewNetwork from '$eth/components/send/SendReviewNetwork.svelte';
 	import type { EthereumNetwork } from '$eth/types/network';
 	import { decodeErc20AbiDataValue } from '$eth/utils/transactions.utils';
+	import ReviewNetwork from '$lib/components/send/ReviewNetwork.svelte';
 	import SendData from '$lib/components/send/SendData.svelte';
 	import ContentWithToolbar from '$lib/components/ui/ContentWithToolbar.svelte';
 	import WalletConnectActions from '$lib/components/wallet-connect/WalletConnectActions.svelte';
@@ -62,7 +62,7 @@
 			{/snippet}
 		</EthFeeDisplay>
 
-		<SendReviewNetwork slot="network" {sourceNetwork} {targetNetwork} token={$sendToken} />
+		<ReviewNetwork slot="network" destinationNetworkId={targetNetwork?.id} {sourceNetwork} />
 	</SendData>
 
 	{#snippet toolbar()}
