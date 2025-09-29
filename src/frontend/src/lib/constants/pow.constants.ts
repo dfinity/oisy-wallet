547eb015552c1b33ea19c4395f3253a4423b18de
DecentAgeCoder <marco.oesch@dfinity.org>
2025-09-29 17:26:22 +0200

    Increased `POW_MIN_CYCLES_THRESHOLD` to 10_000_000_000.

--- src/frontend/src/lib/constants/pow.constants.ts
+++ src/frontend/src/lib/constants/pow.constants.ts
@@ -2,4 +2,4 @@
 export const POW_CHALLENGE_INTERVAL_MILLIS = 60_000;
 
 // Minimum cycles threshold that users must have available for signer operations
-export const POW_MIN_CYCLES_THRESHOLD = 1_000_000_000n;
+export const POW_MIN_CYCLES_THRESHOLD = 10_000_000_000n;
