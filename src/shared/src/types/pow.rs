359fc044d852293f48b0d7549a997a020c0e5ddf
DecentAgeCoder <marco.oesch@dfinity.org>
2025-09-30 13:34:16 +0200

    Increased CYCLES_PER_DIFFICULTY

--- src/shared/src/types/pow.rs
+++ src/shared/src/types/pow.rs
@@ -48,7 +48,7 @@ pub const MAX_DIFFICULTY: u32 = 5_000_000;
 // `1_000_000` will grant `10_000 * 1_000_000 = 10,000,000,000` cycles.
 // It is important to find the correct balance here (also taking the cycles used to execute the PoW
 // code itself into account)
-pub const CYCLES_PER_DIFFICULTY: u64 = 1_458_500;
+pub const CYCLES_PER_DIFFICULTY: u64 = 1_468_500;
 
 // ---------------------------------------------------------------------------------------------
 // - Error-structures and -enums
