1bc7985bc1e38dd5c0207015c52af91d58e5f136
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-09-30 16:38:21 +0200

    feat(frontend): Show nfts manage tokens (#9285)
    # Motivation
    
    We want to display only Nfts on the manage tokens modal if were on the
    Nft routes, and only tokens if not.
    
    # Changes
    
    - Extended filtering inside of modal token list context
    - Added labels for modal, buttons etc to correctly display "Import Nft
    collection" etc
    - Pass if it should render Nfts or not to the context
    
    # Tests
    
    Added test to ensure the filter prop in the ModalListContext works
    
    <img width="646" height="815" alt="image"
    src="https://github.com/user-attachments/assets/a63d6c01-e55d-41a4-a1a4-38ef0eee6043"
    />
    
    <img width="646" height="815" alt="image"
    src="https://github.com/user-attachments/assets/5b60c004-5e4e-4f1f-98fa-767db678826a"
    />
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/manage/ManageTokens.svelte
+++ src/frontend/src/lib/components/manage/ManageTokens.svelte
@@ -24,7 +24,11 @@
 	import type { Token } from '$lib/types/token';
 	import { pinEnabledTokensAtTop, sortTokens } from '$lib/utils/tokens.utils';
 
-	let { initialSearch, infoElement }: { initialSearch?: string; infoElement?: Snippet } = $props();
+	let {
+		initialSearch,
+		infoElement,
+		isNftsPage
+	}: { initialSearch?: string; infoElement?: Snippet; isNftsPage?: boolean } = $props();
 
 	const dispatch = createEventDispatcher();
 
@@ -56,7 +60,8 @@
 			filterZeroBalance: false,
 			filterNetwork: $selectedNetwork,
 			filterQuery: nonNullish(initialSearch) ? initialSearch : '',
-			sortByBalance: false
+			sortByBalance: false,
+			filterNfts: isNftsPage
 		})
 	);
 
@@ -151,7 +156,10 @@
 				colorStyle="secondary-light"
 				disabled={$pseudoNetworkICPTestnet}
 				onclick={() => dispatch('icAddToken')}
-				><IconPlus /> {$i18n.tokens.manage.text.import_token}</Button
+				><IconPlus />
+				{isNftsPage
+					? $i18n.tokens.manage.text.import_nft
+					: $i18n.tokens.manage.text.import_token}</Button
 			>
 			<Button disabled={saveDisabled} onclick={save} testId={MANAGE_TOKENS_MODAL_SAVE}>
 				{$i18n.core.text.save}
