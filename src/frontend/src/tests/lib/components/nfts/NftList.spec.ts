1462bbb63462c5e07cf649a9f87d0f8f4bcb3e2f
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-09-30 15:10:05 +0200

    feat(lint): Enable rule no-relative-imports

--- src/frontend/src/tests/lib/components/nfts/NftList.spec.ts
+++ src/frontend/src/tests/lib/components/nfts/NftList.spec.ts
@@ -1,9 +1,9 @@
 import { AppPath } from '$lib/constants/routes.constants';
 import { parseNftId } from '$lib/validation/nft.validation';
+import NftListTestHost from '$tests/lib/components/nfts/NftListTestHost.svelte';
 import { mockValidErc1155Nft } from '$tests/mocks/nfts.mock';
 import { assertNonNullish } from '@dfinity/utils';
 import { render } from '@testing-library/svelte';
-import NftListTestHost from './NftListTestHost.svelte';
 
 describe('NftList', () => {
 	const mockNfts = [
