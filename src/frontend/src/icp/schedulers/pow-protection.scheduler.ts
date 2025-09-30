1457b109873c4c5b72be9b6ecec3fd81970686d4
DecentAgeCoder <marco.oesch@dfinity.org>
2025-09-30 11:25:09 +0200

    Added `CYCLES_GRANTED` progress state in POW scheduler to signal granted cycles during the challenge process.

--- src/frontend/src/icp/schedulers/pow-protection.scheduler.ts
+++ src/frontend/src/icp/schedulers/pow-protection.scheduler.ts
@@ -76,7 +76,9 @@ export class PowProtectionScheduler implements Scheduler<PostMessageDataRequest>
 				identity,
 				request: { nonce }
 			});
-
+			this.postMessagePowProgress({
+				progress: 'CYCLES_GRANTED'
+			});
 			if (allowSigningResponse?.challenge_completion[0]?.next_allowance_ms !== undefined) {
 				this.postMessagePowNextAllowance({
 					nextAllowanceMs: allowSigningResponse.challenge_completion[0].next_allowance_ms
