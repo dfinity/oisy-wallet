542450e438cc8f6cd6b7d76cd0b2da5f2f849848
DecentAgeCoder <marco.oesch@dfinity.org>
2025-09-30 10:44:00 +0200

    Increased `MAX_CHECK_ATTEMPTS` in `PowProtector` from 40 to 50 to allow more retries.

--- src/frontend/src/lib/components/pow/PowProtector.svelte
+++ src/frontend/src/lib/components/pow/PowProtector.svelte
@@ -29,7 +29,7 @@
 	let checkAttempts = $state(0);
 	let powWorker: PowProtectorWorkerInitResult | undefined;
 
-	const MAX_CHECK_ATTEMPTS = 40;
+	const MAX_CHECK_ATTEMPTS = 50;
 	const CHECK_INTERVAL_MS = 5000;
 
 	// Initialize with default value, but it will be reactively updated from the store
