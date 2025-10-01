e2065347341f134109004a9c193cee51cb3c11b6
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-10-01 19:34:05 +0200

    feat(frontend): Automatically accepts agreements if never visioned

--- src/frontend/src/lib/components/guard/AgreementsGuard.svelte
+++ src/frontend/src/lib/components/guard/AgreementsGuard.svelte
@@ -1,23 +1,41 @@
 <script lang="ts">
-	import type { Snippet } from 'svelte';
+	import { onMount, type Snippet } from 'svelte';
 	import { fade } from 'svelte/transition';
 	import AcceptAgreementsModal from '$lib/components/agreements/AcceptAgreementsModal.svelte';
+	import { authIdentity } from '$lib/derived/auth.derived';
 	import {
+		agreementsToAccept,
 		hasAcceptedAllLatestAgreements,
 		hasOutdatedAgreements,
 		noAgreementVisionedYet
 	} from '$lib/derived/user-agreements.derived';
+	import { userProfileVersion } from '$lib/derived/user-profile.derived';
+	import { acceptAgreements } from '$lib/services/user-agreements.services';
 
 	interface Props {
 		children: Snippet;
 	}
 
 	let { children }: Props = $props();
+
+	$effect(() => {
+		if ($hasAcceptedAllLatestAgreements) {
+			return;
+		}
+
+		if ($noAgreementVisionedYet) {
+			acceptAgreements({
+				identity: $authIdentity,
+				agreementsToAccept: $agreementsToAccept,
+				currentUserVersion: $userProfileVersion
+			});
+		}
+	});
 </script>
 
-{#if $hasAcceptedAllLatestAgreements}
+{#if $hasAcceptedAllLatestAgreements || $noAgreementVisionedYet}
 	{@render children()}
-{:else if $noAgreementVisionedYet || $hasOutdatedAgreements}
+{:else if $hasOutdatedAgreements}
 	<div in:fade={{ delay: 0, duration: 250 }}>
 		<AcceptAgreementsModal />
 	</div>
