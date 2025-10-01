5e8adedb6411a77393e39e267a7ce51335af0d5c
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-10-01 18:35:42 +0200

    feat(frontend): Remove ERC20 dependency from the send tokens list (#9305)
    # Motivation
    
    The send tokens list sets the loading state based on the ERC20 tokens
    store being initialized, however it is not coherent, since it is used
    for all networks and it has no direct dependency on ERC20 tokens.
    
    Furthermore, it means that if the Ethereum/EVM networks are disabled, we
    would be waiting for the store to load infinitely.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/send/SendTokensList.svelte
+++ src/frontend/src/lib/components/send/SendTokensList.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { nonNullish } from '@dfinity/utils';
 	import { createEventDispatcher } from 'svelte';
-	import { erc20UserTokensNotInitialized } from '$eth/derived/erc20.derived';
 	import ModalTokensList from '$lib/components/tokens/ModalTokensList.svelte';
 	import ModalTokensListItem from '$lib/components/tokens/ModalTokensListItem.svelte';
 	import ButtonCloseModal from '$lib/components/ui/ButtonCloseModal.svelte';
@@ -11,16 +10,13 @@
 
 	const dispatch = createEventDispatcher();
 
-	let loading: boolean;
-	$: loading = $erc20UserTokensNotInitialized;
-
 	const onIcTokenButtonClick = ({ detail: token }: CustomEvent<Token>) => {
 		dispatch('icSendToken', token);
 	};
 </script>
 
 <ModalTokensList
-	{loading}
+	loading={false}
 	networkSelectorViewOnly={nonNullish($selectedNetwork)}
 	on:icSelectNetworkFilter
 	on:icTokenButtonClick={onIcTokenButtonClick}
