31defbb0442bf4a235f59a647707fdfaea7b377f
DecentAgeCoder <marco.oesch@dfinity.org>
2025-10-01 20:22:16 +0200

    Lowered `POW_MIN_CYCLES_THRESHOLD` in `pow.constants.ts` to optimize usability while maintaining security.

--- src/frontend/src/lib/constants/pow.constants.ts
+++ src/frontend/src/lib/constants/pow.constants.ts
@@ -2,7 +2,7 @@
 export const POW_CHALLENGE_INTERVAL_MILLIS = 60_000;
 
 // Minimum cycles threshold that users must have available for signer operations
-export const POW_MIN_CYCLES_THRESHOLD = 1_000_000_000n;
+export const POW_MIN_CYCLES_THRESHOLD = 300_000_000n;
 
 // Maximum number of attempts to check for sufficient cycles before signing out the user
 export const MAX_CHECK_ATTEMPTS = 100;
