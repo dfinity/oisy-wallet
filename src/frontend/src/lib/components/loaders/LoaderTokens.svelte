3f205ca369b510798683411c1a8c7ecfb03ebc1e
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-10-01 14:26:34 +0200

    refactor(frontend): Spin-off LoaderTokens from Loader component

---
+++ src/frontend/src/lib/components/loaders/LoaderTokens.svelte
@@ -0,0 +1,116 @@
+<script lang="ts">
+	import { debounce, nonNullish } from '@dfinity/utils';
+	import type { Snippet } from 'svelte';
+	import { NFTS_ENABLED } from '$env/nft.env';
+	import {
+		erc1155CustomTokensInitialized,
+		erc1155CustomTokensNotInitialized
+	} from '$eth/derived/erc1155.derived';
+	import { erc20UserTokensNotInitialized } from '$eth/derived/erc20.derived';
+	import {
+		erc721CustomTokensInitialized,
+		erc721CustomTokensNotInitialized
+	} from '$eth/derived/erc721.derived';
+	import { loadErc1155Tokens } from '$eth/services/erc1155.services';
+	import { loadErc20Tokens } from '$eth/services/erc20.services';
+	import { loadErc721Tokens } from '$eth/services/erc721.services';
+	import { loadIcrcTokens } from '$icp/services/icrc.services';
+	import LoaderCollections from '$lib/components/loaders/LoaderCollections.svelte';
+	import LoaderNfts from '$lib/components/loaders/LoaderNfts.svelte';
+	import { LOCAL } from '$lib/constants/app.constants';
+	import { ethAddress } from '$lib/derived/address.derived';
+	import { authIdentity } from '$lib/derived/auth.derived';
+	import { enabledNonFungibleNetworkTokens } from '$lib/derived/network-tokens.derived';
+	import {
+		networkEthereumEnabled,
+		networkEvmMainnetEnabled,
+		networkEvmTestnetEnabled,
+		networkSepoliaEnabled,
+		networkSolanaDevnetEnabled,
+		networkSolanaLocalEnabled,
+		networkSolanaMainnetEnabled
+	} from '$lib/derived/networks.derived';
+	import { testnetsEnabled } from '$lib/derived/testnets.derived';
+	import { loadNfts } from '$lib/services/nft.services';
+	import { nftStore } from '$lib/stores/nft.store';
+	import { splCustomTokensNotInitialized } from '$sol/derived/spl.derived';
+	import { loadSplTokens } from '$sol/services/spl.services';
+
+	interface Props {
+		children: Snippet;
+	}
+
+	let { children }: Props = $props();
+
+	$effect(() => {
+		loadIcrcTokens({ identity: $authIdentity });
+	});
+
+	let loadErc = $derived(
+		$networkEthereumEnabled ||
+			$networkEvmMainnetEnabled ||
+			($testnetsEnabled && ($networkSepoliaEnabled || $networkEvmTestnetEnabled))
+	);
+
+	let loadErc20 = $derived(loadErc && $erc20UserTokensNotInitialized);
+
+	let loadErc721 = $derived(loadErc && $erc721CustomTokensNotInitialized);
+
+	let loadErc1155 = $derived(loadErc && $erc1155CustomTokensNotInitialized);
+
+	let loadSpl = $derived(
+		($networkSolanaMainnetEnabled ||
+			($testnetsEnabled &&
+				($networkSolanaDevnetEnabled || (LOCAL && $networkSolanaLocalEnabled)))) &&
+			$splCustomTokensNotInitialized
+	);
+
+	$effect(() => {
+		if (loadErc20) {
+			loadErc20Tokens({ identity: $authIdentity });
+		}
+	});
+
+	$effect(() => {
+		if (loadErc721) {
+			loadErc721Tokens({ identity: $authIdentity });
+		}
+	});
+
+	$effect(() => {
+		if (loadErc1155) {
+			loadErc1155Tokens({ identity: $authIdentity });
+		}
+	});
+
+	$effect(() => {
+		if (loadSpl) {
+			loadSplTokens({ identity: $authIdentity });
+		}
+	});
+
+	const debounceLoadNfts = debounce(async () => {
+		await loadNfts({
+			tokens: $enabledNonFungibleNetworkTokens ?? [],
+			loadedNfts: $nftStore ?? [],
+			walletAddress: $ethAddress
+		});
+	}, 1000);
+
+	$effect(() => {
+		if (
+			NFTS_ENABLED &&
+			($erc721CustomTokensInitialized || $erc1155CustomTokensInitialized) &&
+			nonNullish($ethAddress) &&
+			$enabledNonFungibleNetworkTokens.length > 0
+		) {
+			debounceLoadNfts();
+		}
+	});
+</script>
+
+<LoaderCollections>
+	<LoaderNfts>
+		{@render children()}
+	</LoaderNfts>
+</LoaderCollections>
