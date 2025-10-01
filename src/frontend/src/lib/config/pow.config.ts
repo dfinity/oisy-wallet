029041d8125b5bf93aa91ffa86031b9ba0c14379
DecentAgeCoder <marco.oesch@dfinity.org>
2025-10-01 13:57:32 +0200

    Add `pow.config.ts` with shared `powProtectorSteps` configuration for progress step management in PowProtector

---
+++ src/frontend/src/lib/config/pow.config.ts
@@ -0,0 +1,24 @@
+import { ProgressStepsPowProtectorLoader } from '$lib/enums/progress-steps';
+import type { StaticStep, WizardStepsParams } from '$lib/types/steps';
+import type { NonEmptyArray } from '$lib/types/utils';
+import type { ProgressStep } from '@dfinity/gix-components';
+
+export const powProtectorSteps = ({
+	i18n
+}: WizardStepsParams): NonEmptyArray<ProgressStep | StaticStep> => [
+	{
+		step: ProgressStepsPowProtectorLoader.REQUEST_CHALLENGE,
+		text: i18n.pow_protector.text.request_challenge,
+		state: 'completed'
+	} as ProgressStep,
+	{
+		step: ProgressStepsPowProtectorLoader.SOLVE_CHALLENGE,
+		text: i18n.pow_protector.text.solve_challenge,
+		state: 'completed'
+	} as ProgressStep,
+	{
+		step: ProgressStepsPowProtectorLoader.GRANT_CYCLES,
+		text: i18n.pow_protector.text.grant_cycles,
+		state: 'completed'
+	} as ProgressStep
+];
