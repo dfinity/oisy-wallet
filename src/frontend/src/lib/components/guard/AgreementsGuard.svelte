e6f6870d8308f7c78d897e54c4d55a7c686632a9
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-10-02 14:22:37 +0200

    fix(frontend): Remove concurrency of accepting agreements

--- src/frontend/src/lib/components/guard/AgreementsGuard.svelte
+++ src/frontend/src/lib/components/guard/AgreementsGuard.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { queryAndUpdate } from '@dfinity/utils';
 	import type { Snippet } from 'svelte';
 	import { fade } from 'svelte/transition';
 	import AcceptAgreementsModal from '$lib/components/agreements/AcceptAgreementsModal.svelte';
@@ -18,16 +19,49 @@
 
 	let { children }: Props = $props();
 
+	let accepting = false;
+
 	$effect(() => {
 		if ($hasAcceptedAllLatestAgreements) {
 			return;
 		}
 
 		if ($noAgreementVisionedYet) {
-			acceptAgreements({
+			if (accepting) {
+				return;
+			}
+
+			// As additional safe measure we do not start accepting agreements if there are no agreements to accept.
+			// This allows the `$effect` rune to be re-triggered again if `agreementsToAccept` changes,
+			// since the `accepting` variable is still set to `false`.
+			if (Object.keys($agreementsToAccept).length === 0) {
+				return;
+			}
+
+            // The $effect rune cannot await async functions (in this case acceptAgreements).
+            // That causes the `accepting` flag to be set to `false` before the async function is finished.
+            // To avoid it, we use the service queryAndUpdate,
+            // which handles the state of the request internally and has callbacks in case of awaited success or failure.
+			queryAndUpdate({
+				request: async ({ identity }) => {
+					accepting = true;
+
+					await acceptAgreements({
+						identity,
+						agreementsToAccept: $agreementsToAccept,
+						currentUserVersion: $userProfileVersion
+					});
+				},
+				onLoad: ({ certified }) => {
+					if (certified) {
+						accepting = false;
+					}
+				},
+				onUpdateError: () => {
+					accepting = false;
+				},
 				identity: $authIdentity,
-				agreementsToAccept: $agreementsToAccept,
-				currentUserVersion: $userProfileVersion
+				strategy: 'update'
 			});
 		}
 	});
