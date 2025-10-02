87ee409792c0d88620537ad6b82fe5f3e81b6c6a
daviddecentage <david.farina@dfinity.org>
2025-10-02 16:27:27 +0200

    update

--- src/frontend/src/tests/icp/components/transactions/IcTransactionModal.spec.ts
+++ src/frontend/src/tests/icp/components/transactions/IcTransactionModal.spec.ts
@@ -1,11 +1,7 @@
 import { ICP_TOKEN } from '$env/tokens/tokens.icp.env';
 import IcTransactionModal from '$icp/components/transactions/IcTransactionModal.svelte';
 import { i18n } from '$lib/stores/i18n.store';
-import {
-	formatNanosecondsToDate,
-	formatToken,
-	shortenWithMiddleEllipsis
-} from '$lib/utils/format.utils';
+import { formatToken, shortenWithMiddleEllipsis } from '$lib/utils/format.utils';
 import { createMockIcTransactionsUi } from '$tests/mocks/ic-transactions.mock';
 import { render } from '@testing-library/svelte';
 import { get } from 'svelte/store';
@@ -81,22 +77,5 @@ describe('IcTransactionModal', () => {
 
 		expect(getByText(get(i18n).fee.text.fee)).toBeInTheDocument();
 		expect(getByText(formattedAmount)).toBeInTheDocument();
-	});
-
-	it('should display the spender expiration if approve transaction', () => {
-		const { getByText } = render(IcTransactionModal, {
-			transaction: {
-				...mockIcTransactionUi,
-				approveExpiresAt: 1754910013783000000n,
-				approveSpender: 'a498bfac62d235242a60c2141bdad25c7924b62676af755286fa3b8eead72889',
-				type: 'approve'
-			},
-			token: ICP_TOKEN
-		});
-
-		expect(getByText(get(i18n).transaction.text.expiration)).toBeInTheDocument();
-		expect(
-			getByText(formatNanosecondsToDate({ nanoseconds: 1754910013783000000n }))
-		).toBeInTheDocument();
 	});
 });
