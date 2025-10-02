0f8e96e156a10deb1d72d25cdf0b8676670bb7bd
DecentAgeCoder <marco.oesch@dfinity.org>
2025-10-02 23:59:12 +0200

    Update PoW constants to introduce `POW_ZERO_CYCLES_THRESHOLD` and improve naming consistency

--- src/frontend/src/lib/constants/pow.constants.ts
+++ src/frontend/src/lib/constants/pow.constants.ts
@@ -1,11 +1,15 @@
 // The interval in milliseconds between calls to the PoW-protected allowSigning function
 export const POW_CHALLENGE_INTERVAL_MILLIS = 60_000;
 
-// Minimum cycles threshold that users must have available for signer operations
-export const POW_MIN_CYCLES_THRESHOLD = 1_000_000_000n;
+// Minimum cycles allowance required for signer operations before triggering a PoW challenge.
+export const POW_MIN_CYCLES_THRESHOLD = 100_000_000_000n;
+
+// Threshold below which cycles are considered effectively zero due to fragmented costs, triggering the PoW protector modal.
+// Setting the same value as POW_MIN_CYCLES_THRESHOLD here will ensure that the modal is triggered when the user's cycles fall below this threshold.
+export const POW_ZERO_CYCLES_THRESHOLD = 100_000_000_000n; // 26_153_846_153n
 
 // Maximum number of attempts to check for sufficient cycles before signing out the user
-export const MAX_CHECK_ATTEMPTS = 100;
+export const POW_MAX_CHECK_ATTEMPTS = 100;
 
 // Interval in milliseconds between cycle availability checks
-export const CHECK_INTERVAL_MS = 7000;
+export const POW_CHECK_INTERVAL_MS = 7000;
