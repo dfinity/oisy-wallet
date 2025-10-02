dbe1d1541dc0f420ef34a995acf2b4cbaa50af9e
DecentAgeCoder <marco.oesch@dfinity.org>
2025-10-02 12:24:36 +0200

    Lowered `POW_MIN_CYCLES_THRESHOLD` and `POW_ZERO_CYCLES_THRESHOLD` to `100_000_000n` for consistent cycle handling and modal triggering logic.

--- src/frontend/src/lib/constants/pow.constants.ts
+++ src/frontend/src/lib/constants/pow.constants.ts
@@ -2,11 +2,11 @@
 export const POW_CHALLENGE_INTERVAL_MILLIS = 60_000;
 
 // Minimum cycles allowance required for signer operations before triggering a PoW challenge.
-export const POW_MIN_CYCLES_THRESHOLD = 3_300_000_000_000n;
+export const POW_MIN_CYCLES_THRESHOLD = 100_000_000n;
 
 // Threshold below which cycles are considered effectively zero due to fragmented costs, triggering the PoW protector modal.
 // Setting the same value as POW_MIN_CYCLES_THRESHOLD here will ensure that the modal is triggered when the user's cycles fall below this threshold.
-export const POW_ZERO_CYCLES_THRESHOLD = 3_300_000_000_000n; // 26_153_846_153n
+export const POW_ZERO_CYCLES_THRESHOLD = 100_000_000n; // 26_153_846_153n
 
 // Maximum number of attempts to check for sufficient cycles before signing out the user
 export const POW_MAX_CHECK_ATTEMPTS = 100;
