9caf080c9d0ad8b6a8ee2532b01f048e82258f4e
daviddecentage <david.farina@dfinity.org>
2025-10-02 14:14:56 +0200

    update

--- src/frontend/src/tests/sol/components/transactions/SolTransaction.spec.ts
+++ src/frontend/src/tests/sol/components/transactions/SolTransaction.spec.ts
@@ -1,20 +1,20 @@
-import { ETHEREUM_TOKEN } from '$env/tokens/tokens.eth.env';
-import EthTransaction from '$eth/components/transactions/EthTransaction.svelte';
+import { SOLANA_TOKEN } from '$env/tokens/tokens.sol.env';
 import { EIGHT_DECIMALS } from '$lib/constants/app.constants';
 import { formatToken } from '$lib/utils/format.utils';
 import { getTokenDisplaySymbol } from '$lib/utils/token.utils';
-import { createMockEthTransactionsUi } from '$tests/mocks/eth-transactions.mock';
+import SolTransaction from '$sol/components/transactions/SolTransaction.svelte';
+import { createMockSolTransactionsUi } from '$tests/mocks/sol-transactions.mock';
 import { assertNonNullish } from '@dfinity/utils';
 import { render } from '@testing-library/svelte';
 
-describe('EthTransaction', () => {
-	const [mockTrx] = createMockEthTransactionsUi(1);
+describe('SolTransaction', () => {
+	const [mockTrx] = createMockSolTransactionsUi(1);
 
 	it('should render correct amount for send transactions', () => {
-		const { container } = render(EthTransaction, {
+		const { container } = render(SolTransaction, {
 			props: {
 				transaction: { ...mockTrx, value: 123450000000000n, type: 'send' },
-				token: ETHEREUM_TOKEN
+				token: SOLANA_TOKEN
 			}
 		});
 
@@ -26,17 +26,17 @@ describe('EthTransaction', () => {
 			`${formatToken({
 				value: -123450000000000n,
 				displayDecimals: EIGHT_DECIMALS,
-				unitName: ETHEREUM_TOKEN.decimals,
+				unitName: SOLANA_TOKEN.decimals,
 				showPlusSign: false
-			})} ${getTokenDisplaySymbol(ETHEREUM_TOKEN)}`
+			})} ${getTokenDisplaySymbol(SOLANA_TOKEN)}`
 		);
 	});
 
 	it('should render correct amount for receive transactions', () => {
-		const { container } = render(EthTransaction, {
+		const { container } = render(SolTransaction, {
 			props: {
 				transaction: { ...mockTrx, value: 123450000000000n, type: 'receive' },
-				token: ETHEREUM_TOKEN
+				token: SOLANA_TOKEN
 			}
 		});
 
@@ -48,9 +48,9 @@ describe('EthTransaction', () => {
 			`${formatToken({
 				value: 123450000000000n,
 				displayDecimals: EIGHT_DECIMALS,
-				unitName: ETHEREUM_TOKEN.decimals,
+				unitName: SOLANA_TOKEN.decimals,
 				showPlusSign: true
-			})} ${getTokenDisplaySymbol(ETHEREUM_TOKEN)}`
+			})} ${getTokenDisplaySymbol(SOLANA_TOKEN)}`
 		);
 	});
 });
