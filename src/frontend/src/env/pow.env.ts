b0ce996a76284ff0613ae8fcf3f3abf9171a3b62
DecentAgeCoder <marco.oesch@dfinity.org>
2025-10-03 09:38:37 +0200

    Updated `POW_FEATURE_ENABLED` default value, added `build:dev` command, set `BACKEND_CANISTER_ID` to static value, and introduced frontend development build logic in `dfx.json`.

--- src/frontend/src/env/pow.env.ts
+++ src/frontend/src/env/pow.env.ts
@@ -1,4 +1,4 @@
 // Enables the POW Protector on the frontend. Remember to also enable/disable the feature in the backend.
 import { parseBoolEnvVar } from '$lib/utils/env.utils';
 
-export const POW_FEATURE_ENABLED = parseBoolEnvVar(import.meta.env.VITE_POW_FEATURE_ENABLED);
+export const POW_FEATURE_ENABLED = parseBoolEnvVar(import.meta.env.VITE_POW_FEATURE_ENABLED, true);
