589df6e7efe94430ce8cc94e83c0a7492f00efc9
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-10-03 23:02:30 +0200

    feat(frontend): Add optional contact to WalletConnect modal

--- src/frontend/src/lib/components/send/SendSource.svelte
+++ src/frontend/src/lib/components/send/SendSource.svelte
@@ -1,16 +1,26 @@
 <script lang="ts">
 	import { nonNullish } from '@dfinity/utils';
+	import ContactWithAvatar from '$lib/components/contact/ContactWithAvatar.svelte';
 	import ExchangeAmountDisplay from '$lib/components/exchange/ExchangeAmountDisplay.svelte';
 	import WalletConnectModalValue from '$lib/components/wallet-connect/WalletConnectModalValue.svelte';
 	import { ZERO } from '$lib/constants/app.constants';
+	import { contacts } from '$lib/derived/contacts.derived';
 	import { i18n } from '$lib/stores/i18n.store';
 	import type { OptionBalance } from '$lib/types/balance';
+	import type { ContactAddressUi, ContactUi } from '$lib/types/contact';
 	import type { OptionToken } from '$lib/types/token';
+	import { filterAddressFromContact, getContactForAddress } from '$lib/utils/contact.utils';
 
 	export let token: OptionToken;
 	export let balance: OptionBalance;
 	export let source: string;
 	export let exchangeRate: number | undefined = undefined;
+
+	let contact: ContactUi | undefined;
+	$: contact = getContactForAddress({ addressString: source, contactList: $contacts });
+
+	let contactAddress: ContactAddressUi | undefined;
+	$: contactAddress = filterAddressFromContact({ contact, address: source });
 </script>
 
 <WalletConnectModalValue label={$i18n.send.text.balance} ref="balance">
@@ -27,5 +37,10 @@
 </WalletConnectModalValue>
 
 <WalletConnectModalValue label={$i18n.send.text.source} ref="source">
-	{source}
+	<div class="flex flex-col gap-1">
+		{source}
+		{#if nonNullish(contact)}
+			<ContactWithAvatar {contact} {contactAddress} />
+		{/if}
+	</div>
 </WalletConnectModalValue>
