b332b3cd55c9f31c523e5a5af6353b91f4d56a60
DecentAgeCoder <marco.oesch@dfinity.org>
2025-10-03 11:43:35 +0200

    Refactor `PowProtector.svelte` to use `$state` for `checkInterval` variable type definition

--- src/frontend/src/lib/components/pow/PowProtector.svelte
+++ src/frontend/src/lib/components/pow/PowProtector.svelte
@@ -25,7 +25,7 @@
 
 	// Use let with $state() for variables that need to be reassigned
 	let cyclesAllowanceSpent = $state<boolean>(false);
-	let checkInterval: ReturnType<typeof setInterval> | undefined;
+	let checkInterval = $state<NodeJS.Timeout | undefined>();
 	let checkAttempts = $state<number>(0);
 	let powWorker: PowProtectorWorkerInitResult | undefined;
 
