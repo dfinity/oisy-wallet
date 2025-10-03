4397fb7cd20862ea83562430b1a53ac11aae6105
DecentAgeCoder <79855464+DecentAgeCoder@users.noreply.github.com>
2025-10-03 14:02:37 +0200

    feat(frontend): adding PowProtector component (#9299)
    # Motivation
    
    This PR finalizes the PoW protection system by introducing the user
    interface component.
    The complete feature, including all related changes, is tracked in PR
    #7409.
    
    <img width="558" height="647" alt="image"
    src="https://github.com/user-attachments/assets/54474eed-5f81-4d27-ab87-da2cc26f8dd9"
    />
    
    
    # Changes
    - adds PowProtector modal
    - adds support for dynamic progress steps
    - includes localization updates for multiple languages.
    
    # Tests
    - Introduced `PowProtector.spec.ts `to test `PowProtector.svelte`
    
    ---------
    
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/types/i18n.d.ts
+++ src/frontend/src/lib/types/i18n.d.ts
@@ -1251,6 +1251,16 @@ interface I18nAddress {
 	qr: { title: string };
 }
 
+interface I18nPow_protector {
+	text: {
+		title: string;
+		description: string;
+		request_challenge: string;
+		solve_challenge: string;
+		grant_cycles: string;
+	};
+}
+
 interface I18nSigner {
 	sign_in: { text: { access_your_wallet: string; open_or_create: string } };
 	idle: { text: { waiting: string }; alt: { img_placeholder: string } };
@@ -1436,6 +1446,7 @@ interface I18n {
 	address_book: I18nAddress_book;
 	contact: I18nContact;
 	address: I18nAddress;
+	pow_protector: I18nPow_protector;
 	signer: I18nSigner;
 	carousel: I18nCarousel;
 	agreements: I18nAgreements;
