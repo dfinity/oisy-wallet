669071d2e76bb253bec12285dffe850d26c7c0d9
daviddecentage <david.farina@dfinity.org>
2025-10-03 18:24:52 +0200

    update

--- src/frontend/src/lib/components/ui/ConfirmButtonWithModal.svelte
+++ src/frontend/src/lib/components/ui/ConfirmButtonWithModal.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import ConfirmModal from '$lib/components/ui/ConfirmModal.svelte';
 	import type { Snippet } from 'svelte';
 	import { Modal } from '@dfinity/gix-components';
 	import ButtonCancel from '$lib/components/ui/ButtonCancel.svelte';
@@ -10,9 +9,10 @@
 		button: Snippet<[onclick: () => void]>;
 		onConfirm: () => void;
 		children: Snippet;
+		testId?: string;
 	}
 
-	const { title: innerTitle, button, onConfirm, children }: Props = $props();
+	const { title: innerTitle, button, onConfirm, children, testId }: Props = $props();
 
 	let open = $state(false);
 
@@ -26,7 +26,7 @@
 
 {@render button(() => (open = true))}
 
-<Modal visible={open} onClose={onCancelHandler} role="alert">
+<Modal visible={open} onClose={onCancelHandler} role="alert" {testId}>
 	{#snippet title()}
 		<div class="p-3">{@render innerTitle()}</div>
 	{/snippet}
@@ -35,8 +35,8 @@
 
 	{#snippet footer()}
 		<div class="my-3 flex w-full justify-between gap-3">
-			<ButtonCancel onclick={onCancelHandler} />
-			<Button onclick={onConfirmHandler}>Confirm</Button>
+			<ButtonCancel onclick={onCancelHandler} testId={`${testId}-cancel`} />
+			<Button onclick={onConfirmHandler} testId={`${testId}-confirm`}>Confirm</Button>
 		</div>
 	{/snippet}
 </Modal>
