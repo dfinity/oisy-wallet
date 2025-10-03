f30d2fc83fe6f05498c27bda345d589123c87620
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-10-03 17:11:51 +0200

    feat(frontend): Specific component for values in WalletConnect modals

---
+++ src/frontend/src/lib/components/wallet-connect/WalletConnectModalValue.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		label: string;
+		ref: string;
+		children: Snippet;
+	}
+
+	let { label, ref, children }: Props = $props();
+</script>
+
+<label class="font-bold" for={ref}>{label}</label>
+
+<div id={ref} class="mb-4 flex items-center gap-1 font-normal">
+	{@render children()}
+</div>
