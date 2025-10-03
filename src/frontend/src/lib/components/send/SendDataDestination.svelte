90f79a767ca8b772b2e5e1b8931efb803e0c0108
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-10-03 17:24:33 +0200

    more

--- src/frontend/src/lib/components/send/SendDataDestination.svelte
+++ src/frontend/src/lib/components/send/SendDataDestination.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import WalletConnectModalValue from '$lib/components/wallet-connect/WalletConnectModalValue.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
-	import { shortenWithMiddleEllipsis } from '$lib/utils/format.utils';
 
 	interface Props {
 		destination: string;
@@ -11,5 +10,5 @@
 </script>
 
 <WalletConnectModalValue label={$i18n.send.text.destination} ref="destination">
-	{shortenWithMiddleEllipsis({ text: destination })}
+	{destination}
 </WalletConnectModalValue>
