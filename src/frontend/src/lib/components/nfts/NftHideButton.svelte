8c31fd777bfa70690e40f4bbb004b937eb62b545
daviddecentage <david.farina@dfinity.org>
2025-10-07 16:29:25 +0200

    update

--- src/frontend/src/lib/components/nfts/NftHideButton.svelte
+++ src/frontend/src/lib/components/nfts/NftHideButton.svelte
@@ -7,36 +7,18 @@
 	import { authIdentity } from '$lib/derived/auth.derived';
 	import {
 		NFT_COLLECTION_ACTION_HIDE,
-		NFT_COLLECTION_ACTION_NOT_SPAM,
-		NFT_COLLECTION_ACTION_SPAM,
 		NFT_COLLECTION_ACTION_UNHIDE
 	} from '$lib/constants/test-ids.constants';
-	import IconAlertOctagon from '$lib/components/icons/lucide/IconAlertOctagon.svelte';
 	import { updateNftSection } from '$lib/services/nft.services';
 	import IconEye from '$lib/components/icons/lucide/IconEye.svelte';
 	import IconEyeOff from '$lib/components/icons/lucide/IconEyeOff.svelte';
-	import ConfirmButtonWithModal from '$lib/components/ui/ConfirmButtonWithModal.svelte';
-	import { findNftsByToken } from '$lib/utils/nfts.utils';
-	import { nftStore } from '$lib/stores/nft.store';
 
 	interface Props {
 		token: NonFungibleToken;
 	}
 
 	let { token }: Props = $props();
-
-	const hasMultipleNfts = $derived(
-		nonNullish($nftStore) ? findNftsByToken({ nfts: $nftStore, token }).length > 1 : false
-	);
 </script>
-
-{#snippet hideButton(onclick: () => void)}
-	<NftActionButton label={$i18n.nfts.text.hide} {onclick} testId={NFT_COLLECTION_ACTION_HIDE}>
-		{#snippet icon()}
-			<IconEyeOff size="18" />
-		{/snippet}
-	</NftActionButton>
-{/snippet}
 
 {#if token.section !== CustomTokenSection.SPAM}
 	{#if nonNullish(token.section) && token.section === CustomTokenSection.HIDDEN}
@@ -50,28 +32,15 @@
 				<IconEye size="18" />
 			{/snippet}
 		</NftActionButton>
-	{:else if hasMultipleNfts}
-		<ConfirmButtonWithModal
-			onConfirm={() =>
-				updateNftSection({ section: CustomTokenSection.HIDDEN, token, $authIdentity })}
-			button={hideButton}
-		>
-			<div class="flex w-full flex-col items-center text-center">
-				<span
-					class="m-3 inline-flex aspect-square w-[56px] rounded-full bg-brand-primary p-3 text-white"
-				>
-					<IconEyeOff size="32" />
-				</span>
-				<h4 class="my-3">Move collection to spam?</h4>
-				<p class="text-sm"
-					>This will move all NFTs of this collection to spam. They'll be hidden from your main
-					view, but you can always find and restore them later.</p
-				>
-			</div>
-		</ConfirmButtonWithModal>
 	{:else}
-		{@render hideButton(() =>
-			updateNftSection({ section: CustomTokenSection.HIDDEN, token, $authIdentity })
-		)}
+		<NftActionButton
+			label={$i18n.nfts.text.hide}
+			onclick={() => updateNftSection({ section: CustomTokenSection.HIDDEN, token, $authIdentity })}
+			testId={NFT_COLLECTION_ACTION_HIDE}
+		>
+			{#snippet icon()}
+				<IconEyeOff size="18" />
+			{/snippet}
+		</NftActionButton>
 	{/if}
 {/if}
--- src/frontend/src/lib/components/nfts/NftSpamButton.svelte
+++ src/frontend/src/lib/components/nfts/NftSpamButton.svelte
@@ -11,28 +11,13 @@
 	} from '$lib/constants/test-ids.constants';
 	import IconAlertOctagon from '$lib/components/icons/lucide/IconAlertOctagon.svelte';
 	import { updateNftSection } from '$lib/services/nft.services';
-	import ConfirmButtonWithModal from '$lib/components/ui/ConfirmButtonWithModal.svelte';
-	import { findNftsByToken } from '$lib/utils/nfts.utils';
-	import { nftStore } from '$lib/stores/nft.store';
 
 	interface Props {
 		token: NonFungibleToken;
 	}
 
 	let { token }: Props = $props();
-
-	const hasMultipleNfts = $derived(
-		nonNullish($nftStore) ? findNftsByToken({ nfts: $nftStore, token }).length > 1 : false
-	);
 </script>
-
-{#snippet spamButton(onclick: () => void)}
-	<NftActionButton label={$i18n.nfts.text.spam} {onclick} testId={NFT_COLLECTION_ACTION_SPAM}>
-		{#snippet icon()}
-			<IconAlertOctagon size="18" />
-		{/snippet}
-	</NftActionButton>
-{/snippet}
 
 {#if nonNullish(token.section) && token.section === CustomTokenSection.SPAM}
 	<NftActionButton
@@ -44,26 +29,14 @@
 			<IconAlertOctagon size="18" />
 		{/snippet}
 	</NftActionButton>
-{:else if hasMultipleNfts}
-	<ConfirmButtonWithModal
-		onConfirm={() => updateNftSection({ section: CustomTokenSection.SPAM, token, $authIdentity })}
-		button={spamButton}
-	>
-		<div class="flex w-full flex-col items-center text-center">
-			<span
-				class="m-3 inline-flex aspect-square w-[56px] rounded-full bg-warning-primary p-3 text-white"
-			>
-				<IconAlertOctagon size="32" />
-			</span>
-			<h4 class="my-3">Move collection to spam?</h4>
-			<p class="text-sm"
-				>This will move all NFTs of this collection to spam. They'll be hidden from your main view,
-				but you can always find and restore them later.</p
-			>
-		</div>
-	</ConfirmButtonWithModal>
 {:else}
-	{@render spamButton(() =>
-		updateNftSection({ section: CustomTokenSection.SPAM, token, $authIdentity })
-	)}
+	<NftActionButton
+		label={$i18n.nfts.text.spam}
+		onclick={() => updateNftSection({ section: CustomTokenSection.SPAM, token, $authIdentity })}
+		testId={NFT_COLLECTION_ACTION_SPAM}
+	>
+		{#snippet icon()}
+			<IconAlertOctagon size="18" />
+		{/snippet}
+	</NftActionButton>
 {/if}
