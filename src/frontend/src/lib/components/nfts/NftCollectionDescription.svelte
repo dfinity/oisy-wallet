1a0688584db31864a0b67a22bdebe2f7b1bbd93f
daviddecentage <david.farina@dfinity.org>
2025-10-07 23:18:01 +0200

    update

--- src/frontend/src/lib/components/nfts/NftCollectionDescription.svelte
+++ src/frontend/src/lib/components/nfts/NftCollectionDescription.svelte
@@ -12,6 +12,7 @@
 	import { i18n } from '$lib/stores/i18n.store';
 	import type { NftCollection } from '$lib/types/nft';
 	import { findNonFungibleToken, getAllowMediaForNft } from '$lib/utils/nfts.utils';
+	import BgImg from '$lib/components/ui/BgImg.svelte';
 
 	interface Props {
 		collection?: NftCollection;
@@ -42,23 +43,26 @@
 
 {#if nonNullish(collection)}
 	<div class="my-5 flex flex-col rounded-lg bg-primary p-5">
-		<div class="flex flex-row gap-3">
-			<div class="mb-2 flex flex-col gap-3">
+		<div class="mb-6 flex flex-row justify-between gap-3">
+			<div class=" flex flex-col items-start gap-3">
 				<h5>{collection.name}</h5>
 
 				<p class="mb-0 text-sm">{collection.description}</p>
 
-				<Button
-					link
-					onclick={() => goto(`${AppPath.Nfts}${collection.network.name}-${collection.address}`)}
-					styleClass="inline-block mb-3 text-sm"
-					>{$i18n.nfts.text.go_to_collection}<IconExpand axis="y" expanded /></Button
-				>
+				<span>
+					<Button
+						link
+						paddingSmall
+						onclick={() => goto(`${AppPath.Nfts}${collection.network.name}-${collection.address}`)}
+						styleClass="inline-block text-sm"
+						>{$i18n.nfts.text.go_to_collection}<IconExpand axis="y" /></Button
+					>
+				</span>
 			</div>
 
 			{#if nonNullish(collection.bannerImageUrl) && hasConsent}
-				<div class="max-h-30 flex min-w-6 overflow-hidden rounded-lg">
-					<Img src={collection.bannerImageUrl} />
+				<div class="flex aspect-auto h-32 w-32 overflow-hidden rounded-lg">
+					<BgImg imageUrl={collection.bannerImageUrl} size="cover" />
 				</div>
 			{/if}
 		</div>
