56febaa9be7ef2a20e765cd76f80e90dbedd533e
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-10-09 16:26:25 +0200

    chore

--- src/frontend/src/lib/types/signer.ts
+++ src/frontend/src/lib/types/signer.ts
@@ -2,5 +2,3 @@ import type { SignerMasterPubKeysSchema } from '$lib/schema/signer.schema';
 import type * as z from 'zod';
 
 export type SignerMasterPubKeys = z.infer<typeof SignerMasterPubKeysSchema>;
-
-export type SignerRootKeyName = keyof SignerMasterPubKeys;
