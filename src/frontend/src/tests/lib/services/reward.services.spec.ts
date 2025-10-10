ec2b4f82978ff75db4c7128635be3aa7b7f6bcf2
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-10-10 17:12:20 +0200

    refactor(frontend): Refer to new path for rewards bindings

--- src/frontend/src/tests/lib/services/reward.services.spec.ts
+++ src/frontend/src/tests/lib/services/reward.services.spec.ts
@@ -5,7 +5,7 @@ import type {
 	ReferrerInfo,
 	RewardInfo,
 	UserData
-} from '$declarations/rewards/rewards.did';
+} from '$declarations/rewards/declarations/rewards.did';
 import { ICP_TOKEN } from '$env/tokens/tokens.icp.env';
 import * as rewardApi from '$lib/api/reward.api';
 import { ZERO } from '$lib/constants/app.constants';
