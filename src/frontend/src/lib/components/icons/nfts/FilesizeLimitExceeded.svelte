2a6d439c90ded33574acc5a56fabf06eafde087e
daviddecentage <david.farina@dfinity.org>
2025-10-10 10:12:31 +0200

    update

--- src/frontend/src/lib/components/icons/nfts/FilesizeLimitExceeded.svelte
+++ src/frontend/src/lib/components/icons/nfts/FilesizeLimitExceeded.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { NFT_PLACEHOLDER_FILESIZE } from '$lib/constants/test-ids.constants';
+
 	interface Props {
 		size?: string;
 	}
@@ -12,6 +14,7 @@
 	viewBox="0 0 200 200"
 	fill="none"
 	xmlns="http://www.w3.org/2000/svg"
+	data-tid={NFT_PLACEHOLDER_FILESIZE}
 >
 	<path
 		d="M0 12C0 5.37258 5.37258 0 12 0H188C194.627 0 200 5.37258 200 12V188C200 194.627 194.627 200 188 200H12C5.37258 200 0 194.627 0 188V12Z"
