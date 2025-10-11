f5d013ab0fc1f01ff63eb073fd18b1a914a2efa3
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-10-11 16:02:26 +0200

    refactor(frontend): Refer to new path for `kong_backend` bindings (#9564)
    # Motivation
    
    In PR https://github.com/dfinity/oisy-wallet/pull/9521, we are going to
    introduce the use of library
    [`@icp-sdk/bindgen`](https://js.icp.build/bindgen/latest/) that helps us
    creating the bindings, substituting effectively `dfx generate`.
    
    However, since the new library saves the generated files in a
    sub-directory /declarations, we need to adapt our code. And the change
    is quite through a lot of files.
    
    To easy this transition, in this PR we just change the imports that
    refers to `kong_backend` bindings to the new path.

--- src/frontend/src/lib/types/swap.ts
+++ src/frontend/src/lib/types/swap.ts
@@ -1,4 +1,4 @@
-import type { SwapAmountsReply } from '$declarations/kong_backend/kong_backend.did';
+import type { SwapAmountsReply } from '$declarations/kong_backend/declarations/kong_backend.did';
 import type { Erc20Token } from '$eth/types/erc20';
 import type { EthereumNetwork } from '$eth/types/network';
 import type { ProgressStep } from '$eth/types/send';
