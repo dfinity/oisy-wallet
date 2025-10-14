9b9170516bf32c7d36c8c4baacf001f4388c7ff3
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-10-14 17:06:30 +0200

    feat(frontend): Queue for workers

--- src/frontend/src/lib/workers/workers.ts
+++ src/frontend/src/lib/workers/workers.ts
@@ -1,69 +1,6 @@
-import { onBtcWalletMessage } from '$btc/workers/btc-wallet.worker';
-import { onBtcStatusesMessage } from '$icp/workers/btc-statuses.worker';
-import { onCkBtcMinterInfoMessage } from '$icp/workers/ckbtc-minter-info.worker';
-import { onCkBtcUpdateBalanceMessage } from '$icp/workers/ckbtc-update-balance.worker';
-import { onCkEthMinterInfoMessage } from '$icp/workers/cketh-minter-info.worker';
-import { onDip20WalletMessage } from '$icp/workers/dip20-wallet.worker';
-import { onIcpWalletMessage } from '$icp/workers/icp-wallet.worker';
-import { onIcrcWalletMessage } from '$icp/workers/icrc-wallet.worker';
-import { onPowProtectionMessage } from '$icp/workers/pow-protection.worker';
-import type {
-	PostMessage,
-	PostMessageDataRequest,
-	PostMessageRequest,
-	PostMessageRequestMap
-} from '$lib/types/post-message';
-import { onAuthMessage } from '$lib/workers/auth.worker';
-import { onExchangeMessage } from '$lib/workers/exchange.worker';
-import { onSolWalletMessage } from '$sol/workers/sol-wallet.worker';
+import { enqueue } from '$lib/services/worker.services';
+import type { PostMessage, PostMessageDataRequest } from '$lib/types/post-message';
 import { isNullish } from '@dfinity/utils';
-
-export const messageToHandler: PostMessageRequestMap = {
-	startIdleTimer: onAuthMessage,
-	stopIdleTimer: onAuthMessage,
-
-	startExchangeTimer: onExchangeMessage,
-	stopExchangeTimer: onExchangeMessage,
-
-	startPowProtectionTimer: onPowProtectionMessage,
-	triggerPowProtectionTimer: onPowProtectionMessage,
-	stopPowProtectionTimer: onPowProtectionMessage,
-
-	startIcpWalletTimer: onIcpWalletMessage,
-	stopIcpWalletTimer: onIcpWalletMessage,
-	triggerIcpWalletTimer: onIcpWalletMessage,
-
-	startIcrcWalletTimer: onIcrcWalletMessage,
-	stopIcrcWalletTimer: onIcrcWalletMessage,
-	triggerIcrcWalletTimer: onIcrcWalletMessage,
-
-	startDip20WalletTimer: onDip20WalletMessage,
-	stopDip20WalletTimer: onDip20WalletMessage,
-	triggerDip20WalletTimer: onDip20WalletMessage,
-
-	startBtcWalletTimer: onBtcWalletMessage,
-	stopBtcWalletTimer: onBtcWalletMessage,
-	triggerBtcWalletTimer: onBtcWalletMessage,
-
-	startSolWalletTimer: onSolWalletMessage,
-	stopSolWalletTimer: onSolWalletMessage,
-	triggerSolWalletTimer: onSolWalletMessage,
-
-	startBtcStatusesTimer: onBtcStatusesMessage,
-	stopBtcStatusesTimer: onBtcStatusesMessage,
-	triggerBtcStatusesTimer: onBtcStatusesMessage,
-
-	startCkBTCUpdateBalanceTimer: onCkBtcUpdateBalanceMessage,
-	stopCkBTCUpdateBalanceTimer: onCkBtcUpdateBalanceMessage,
-
-	startCkEthMinterInfoTimer: onCkEthMinterInfoMessage,
-	stopCkEthMinterInfoTimer: onCkEthMinterInfoMessage,
-	triggerCkEthMinterInfoTimer: onCkEthMinterInfoMessage,
-
-	startCkBtcMinterInfoTimer: onCkBtcMinterInfoMessage,
-	stopCkBtcMinterInfoTimer: onCkBtcMinterInfoMessage,
-	triggerCkBtcMinterInfoTimer: onCkBtcMinterInfoMessage
-};
 
 onmessage = async (ev: MessageEvent<PostMessage<PostMessageDataRequest>>) => {
 	const msg = ev.data?.msg;
@@ -71,15 +8,5 @@ onmessage = async (ev: MessageEvent<PostMessage<PostMessageDataRequest>>) => {
 		return;
 	}
 
-	const handler = messageToHandler[msg as PostMessageRequest];
-	if (isNullish(handler)) {
-		return;
-	}
-
-	try {
-		await handler(ev);
-	} catch (_: unknown) {
-		// Silent catch, we don't want the worker to crash
-		// Any errors are handled in the main thread or in the worker itself
-	}
+	await enqueue(ev);
 };
