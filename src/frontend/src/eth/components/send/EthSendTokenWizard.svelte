aefcda54fe9fe910b4feaca80ff5a40327352bcb
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-10-15 11:24:02 +0200

    feat(frontend): updated trackEvents for nft send

--- src/frontend/src/eth/components/send/EthSendTokenWizard.svelte
+++ src/frontend/src/eth/components/send/EthSendTokenWizard.svelte
@@ -25,8 +25,7 @@
 	import ButtonBack from '$lib/components/ui/ButtonBack.svelte';
 	import InProgressWizard from '$lib/components/ui/InProgressWizard.svelte';
 	import {
-		TRACK_COUNT_ETH_NFT_SEND_ERROR,
-		TRACK_COUNT_ETH_NFT_SEND_SUCCESS,
+		TRACK_NFT_SEND,
 		TRACK_COUNT_ETH_SEND_ERROR,
 		TRACK_COUNT_ETH_SEND_SUCCESS
 	} from '$lib/constants/analytics.constants';
@@ -45,6 +44,8 @@
 	import type { Token, TokenId } from '$lib/types/token';
 	import { invalidAmount, isNullishOrEmpty } from '$lib/utils/input.utils';
 	import { parseToken } from '$lib/utils/parse.utils';
+	import type { address } from '@solana/kit';
+	import { error } from 'console';
 
 	/**
 	 * Send context store
@@ -199,24 +200,29 @@
 			});
 
 			trackEvent({
-				name: TRACK_COUNT_ETH_NFT_SEND_SUCCESS,
+				name: TRACK_NFT_SEND,
 				metadata: {
+					resultStatus: 'success',
 					token: $sendToken.symbol,
-					collection: nft.collection.name ?? nft.collection.address,
+					collection: nft.collection.name ?? '',
+					address: nft.collection.address,
 					tokenId: String(nft.id),
-					network: sourceNetwork.id.description ?? `${$sendToken.network.id.description}`
+					network: sourceNetwork.name
 				}
 			});
 
 			setTimeout(() => close(), 750);
 		} catch (err: unknown) {
 			trackEvent({
-				name: TRACK_COUNT_ETH_NFT_SEND_ERROR,
+				name: TRACK_NFT_SEND,
 				metadata: {
+					resultStatus: 'error',
 					token: $sendToken.symbol,
-					collection: nft.collection.name ?? nft.collection.address,
+					collection: nft.collection.name ?? '',
+					address: nft.collection.address,
 					tokenId: String(nft.id),
-					network: sourceNetwork.id.description ?? `${$sendToken.network.id.description}`
+					network: sourceNetwork.name,
+					error: (err as Error).message
 				}
 			});
 
