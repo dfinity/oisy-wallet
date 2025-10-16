ea0ba80c06d99b9b940ce7c1bfe798bf51928790
daviddecentage <david.farina@dfinity.org>
2025-10-16 14:40:03 +0200

    update

--- src/frontend/src/lib/components/ui/Button.svelte
+++ src/frontend/src/lib/components/ui/Button.svelte
@@ -52,6 +52,7 @@
 <button
 	class={`${colorStyle} flex text-center ${styleClass}`}
 	class:animate-pulse={loading}
+	class:cursor-not-allowed={disabled || loading}
 	class:duration-500={loading}
 	class:ease-in-out={loading}
 	class:flex-1={!inlineLink}
