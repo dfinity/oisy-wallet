a6d2889853676af48931a847c585ff4bb99dbed4
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-10-16 11:27:40 +0200

    refactor(frontend): Migrate `SettingsModal` to Svelte 5 (#9697)
    # Motivation
    
    Migrating component `SettingsModal` (and its direct children) to Svelte
    5.

--- src/frontend/src/lib/components/settings/SettingsModalEnabledNetworks.svelte
+++ src/frontend/src/lib/components/settings/SettingsModalEnabledNetworks.svelte
@@ -37,8 +37,8 @@
 	const enabledNetworks = { ...$userNetworks };
 	const enabledNetworksInitial = { ...enabledNetworks };
 
-	let enabledTestnet = $testnetsEnabled;
-	const enabledTestnetInitial = enabledTestnet;
+	let enabledTestnet = $state($testnetsEnabled);
+	const enabledTestnetInitial = $testnetsEnabled;
 
 	const checkModified = ({
 		enabledTestnet,
@@ -58,8 +58,8 @@
 
 		return testnetModified || networkModified;
 	};
-	let isModified: boolean;
-	$: isModified = checkModified({ enabledTestnet, enabledNetworks });
+
+	let isModified = $derived(checkModified({ enabledTestnet, enabledNetworks }));
 
 	const toggleTestnets = () => {
 		enabledTestnet = !enabledTestnet;
@@ -72,7 +72,7 @@
 		};
 	};
 
-	let saveLoading = false;
+	let saveLoading = $state(false);
 
 	const save = async () => {
 		if (isNullish($authIdentity)) {
