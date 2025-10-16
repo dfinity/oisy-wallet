4f7c6bc9925dc77b562dc54a63b6b7877f16b3e0
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2025-10-16 18:01:01 +0200

    feat(frontend): fix

--- src/frontend/src/lib/constants/analytics.constants.ts
+++ src/frontend/src/lib/constants/analytics.constants.ts
@@ -178,12 +178,12 @@ export const TRACK_BUY_TOKEN = 'buy_token';
 export const TRACK_OPEN_ASSETS_TAB = 'open_assets_tab';
 export const TRACK_NFT_SETTINGS_CHANGE = 'nft_settings_change';
 export const TRACK_NFT_SORT_CHANGE = 'nft_sort_change';
-
-// Stake
-export const TRACK_COUNT_STAKE_SUCCESS = 'stake_success';
-export const TRACK_COUNT_STAKE_ERROR = 'stake_error';
 export const TRACK_NFT_SEND = 'nft_send';
 export const TRACK_NFT_CONSENT_GIVEN = 'nft_consent_given';
 export const TRACK_NFT_OPEN = 'nft_open';
 export const TRACK_NFT_OPEN_CONSENT_MODAL = 'nft_open_consent_modal';
 export const TRACK_NFT_SPAM_HIDE_ACTION = 'nft_spam_hide_action';
+
+// Stake
+export const TRACK_COUNT_STAKE_SUCCESS = 'stake_success';
+export const TRACK_COUNT_STAKE_ERROR = 'stake_error';
