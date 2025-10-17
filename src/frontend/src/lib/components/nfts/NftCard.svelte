b931c365d699dc12bf5d3937e956da4133b6c577
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-10-17 13:56:00 +0200

    chore(frontend): Reduce conditional class in Nft card (#9728)
    # Motivation
    
    In order to help preventing a nuclear meltdown on iphones we reduce a
    conditional class in NftCard.
    
    # Changes
    
    Reduced two conditions to one
    
    # Tests
    
    <img width="199" height="238" alt="image"
    src="https://github.com/user-attachments/assets/2e2f44d5-4196-4ae7-8520-5907c18fd51b"
    />

--- src/frontend/src/lib/components/nfts/NftCard.svelte
+++ src/frontend/src/lib/components/nfts/NftCard.svelte
@@ -112,14 +112,8 @@
 		</span>
 	</span>
 
-	<span class="flex w-full flex-col gap-1 px-2 pb-2">
-		<span
-			class="truncate text-sm font-bold"
-			class:text-disabled={disabled}
-			class:text-primary={!disabled}>{nft.name}</span
-		>
-		<span class="text-xs" class:text-disabled={disabled} class:text-tertiary={!disabled}
-			>#{nft.id}</span
-		>
+	<span class="flex w-full flex-col gap-1 px-2 pb-2" class:text-disabled={disabled}>
+		<span class="truncate text-sm font-bold" class:text-primary={!disabled}>{nft.name}</span>
+		<span class="text-xs" class:text-tertiary={!disabled}>#{nft.id}</span>
 	</span>
 </button>
