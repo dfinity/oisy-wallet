001985f30e11ed075eda7c64926be4ff1bd7a13f
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-10-20 14:37:35 +0200

    refactor(frontend): refactored spam hide nft buttons events (#9769)
    # Motivation
    
    WE aim to refactor plausible events for nfts.
    
    # Changes
    
    Refactored events for hide/spam buttons for nfts
    
    # Tests
    
    Updated tests
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/enums/plausible.ts
+++ src/frontend/src/lib/enums/plausible.ts
@@ -1,11 +1,16 @@
 export enum PLAUSIBLE_EVENTS {
-	VIEW_OPEN = 'view_open'
+	VIEW_OPEN = 'view_open',
+	NFT_CATEGORIZE = 'nfft_categorize'
 }
 
 export enum PLAUSIBLE_EVENT_CONTEXTS {
+	NFT = 'nft',
 	ASSETS_TAB = 'assets_tab'
 }
 
 export enum PLAUSIBLE_EVENT_SOURCES {
-	HOMEPAGE = 'homepage'
+	HOMEPAGE = 'homepage',
+	NFT_COLLECTION = 'nft-collection-page',
+	NFT_MEDIA_REVIEW = 'media-review',
+	NFT_PAGE = 'nft-page'
 }
