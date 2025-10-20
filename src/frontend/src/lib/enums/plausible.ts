f0584625b09b37a6cec9fa67eb3c788a64f40742
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-10-20 16:28:15 +0200

    Update src/frontend/src/lib/enums/plausible.ts

--- src/frontend/src/lib/enums/plausible.ts
+++ src/frontend/src/lib/enums/plausible.ts
@@ -1,7 +1,7 @@
 export enum PLAUSIBLE_EVENTS {
 	VIEW_OPEN = 'view_open',
 	NFT_CATEGORIZE = 'nfft_categorize',
-	PAGE_OPEN = 'page_open'
+	PAGE_OPEN = 'page_open',
 	NFT_MEDIA_CONSENT = 'nft_media_consent'
 }
 
