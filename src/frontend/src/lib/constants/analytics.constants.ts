0ab055307f978acd92eba60cf03febd050487361
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-10-20 15:32:06 +0200

    fix

--- src/frontend/src/lib/constants/analytics.constants.ts
+++ src/frontend/src/lib/constants/analytics.constants.ts
@@ -187,29 +187,3 @@ export const TRACK_NFT_SPAM_HIDE_ACTION = 'nft_spam_hide_action';
 // Stake
 export const TRACK_COUNT_STAKE_SUCCESS = 'stake_success';
 export const TRACK_COUNT_STAKE_ERROR = 'stake_error';
-
-// New Plausible Events
-export enum PLAUSIBLE_EVENTS {
-	VIEW_OPEN = 'view_open',
-	NFT_CATEGORIZE = 'nfft_categorize',
-	LIST_SETTINGS_CHANGE = 'list_settings_change'
-}
-
-export enum PLAUSIBLE_EVENT_CONTEXTS {
-	NFT = 'nft',
-	ASSETS_TAB = 'assets_tab'
-}
-
-export enum PLAUSIBLE_EVENT_SOURCES {
-	HOME_PAGE = 'home-page',
-	NFT_COLLECTION = 'nft-collection-page',
-	NFT_MEDIA_REVIEW = 'media-review',
-	NFT_PAGE = 'nft-page'
-}
-
-export enum PLAUSIBLE_EVENT_EVENTS_KEYS {
-	GROUP = 'group',
-	VISIBILITY = 'visibility',
-	SORT_ASC = 'sort_asc',
-	SORT_DESC = 'sort_desc'
-}
