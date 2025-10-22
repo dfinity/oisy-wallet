a2f8dd0b6be88e9017eb0b701d7b8a6507c5f62a
Stefan Berger <sberger@bluewin.ch>
2025-10-22 11:41:02 +0200

    Update Banner.svelte

--- src/frontend/src/lib/components/core/Banner.svelte
+++ src/frontend/src/lib/components/core/Banner.svelte
@@ -38,18 +38,6 @@
 <!-- TODO remove this WarningBanner again as soon a solution is found for enabling display type standalone  -->
 {#if pwaBannerVisible}
 	<div
-		class="fixed left-[50%] top-6 z-10 flex min-w-80 -translate-x-[50%] justify-between gap-4 rounded-lg bg-primary"
-	>
-		<WarningBanner>
-			<span class="w-full px-2">
-				<Html text={replaceOisyPlaceholders($i18n.core.warning.standalone_mode)} />
-			</span>
-			<button aria-label={$i18n.core.text.close} onclick={closePwaBanner}>
-				<IconClose />
-			</button>
-		</WarningBanner>
-	</div>
-	<div
 			class="fixed left-[50%] top-6 z-10 flex min-w-80 -translate-x-[50%] justify-between gap-4 rounded-lg bg-primary"
 		>
 		<div
