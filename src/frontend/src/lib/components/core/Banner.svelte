2947a7f9c5361967248225901dd5169eb2a40597
Stefan Berger <sberger@bluewin.ch>
2025-10-22 12:46:56 +0200

    Update Banner.svelte

--- src/frontend/src/lib/components/core/Banner.svelte
+++ src/frontend/src/lib/components/core/Banner.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { Html, IconClose, IconWarning } from '@dfinity/gix-components';
-	import IconCircleCheck from '$lib/components/icons/lucide/IconCircleCheck.svelte';
+	import IconInfo from '$lib/components/icons/lucide/IconInfo.svelte';
 	import WarningBanner from '$lib/components/ui/WarningBanner.svelte';
 	import { BETA, STAGING } from '$lib/constants/app.constants';
 	import { i18n } from '$lib/stores/i18n.store';
@@ -36,12 +36,12 @@
 <!-- TODO remove this WarningBanner again as soon a solution is found for enabling display type standalone  -->
 {#if pwaBannerVisible}
 	<div
-		class="fixed left-[50%] top-6 z-10 flex min-w-80 -translate-x-[50%] justify-between gap-4 rounded-lg bg-success-light"
+		class="fixed left-[50%] top-6 z-10 flex min-w-80 -translate-x-[50%] justify-between gap-4 rounded-lg bg-primary"
 	>
 		<div
 			class="border-info-solid bg-info-subtle-10 text-info-primary inline-flex w-full items-center justify-center gap-2 rounded-lg border px-6 py-2 text-xs font-normal sm:w-fit md:text-base"
 		>
-			<IconCircleCheck></IconCircleCheck>
+			<IconInfo></IconInfo>
 			<span class="w-full px-2"><Html text={$i18n.core.warning.standalone_mode} /></span>
 			<button aria-label={$i18n.core.text.close} onclick={closePwaBanner}>
 				<IconClose />
