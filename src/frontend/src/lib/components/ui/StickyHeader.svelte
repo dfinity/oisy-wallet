aba52623c881bf7f03467fb1e04e4deef5000b00
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-10-23 14:55:30 +0200

    more

--- src/frontend/src/lib/components/ui/StickyHeader.svelte
+++ src/frontend/src/lib/components/ui/StickyHeader.svelte
@@ -28,7 +28,7 @@
 <svelte:window onscroll={handleScroll} />
 
 <div bind:this={rootElement}>
-	<div class="z-3 sticky top-0 whitespace-nowrap px-1 pt-6" class:bg-page={scrolledSoon} class:pt-6={scrolledSoon}>
+	<div class="z-3 sticky top-0 whitespace-nowrap px-1" class:bg-page={scrolledSoon} class:pt-6={scrolledSoon}>
 		{@render header()}
 	</div>
 
