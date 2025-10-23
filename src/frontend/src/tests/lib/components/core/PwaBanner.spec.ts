7d4ec2b34ac8513cd9c2a88d54e6dacd84068aea
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-10-23 11:50:45 +0200

    feat(frontend): Use Info banner instead of Warning for PWA Message (#9903)
    # Motivation
    
    We prefer to show the PWA message as info instead of a warning, to be
    less impactful.
    
    # Changes
    
    - Create new component `InfoBanner`, similar to `WarningBanner`.
    - Replace the banner component in `PwaBanner`.
    
    # Tests
    
    ### Before
    
    <img width="605" height="596" alt="Screenshot 2025-10-22 at 14 49 03"
    src="https://github.com/user-attachments/assets/0d9f780d-c675-4402-a2df-f625de70701d"
    />
    
    ### After
    
    <img width="659" height="649" alt="Screenshot 2025-10-23 at 09 30 37"
    src="https://github.com/user-attachments/assets/4a92ca99-b8c9-45e5-903d-981f76fd9fcc"
    />

--- src/frontend/src/tests/lib/components/core/PwaBanner.spec.ts
+++ src/frontend/src/tests/lib/components/core/PwaBanner.spec.ts
@@ -1,7 +1,7 @@
 import PwaBanner from '$lib/components/core/PwaBanner.svelte';
 import {
-	PWA_WARNING_BANNER_CLOSE_BUTTON_TEST_ID,
-	PWA_WARNING_BANNER_TEST_ID
+	PWA_INFO_BANNER_CLOSE_BUTTON_TEST_ID,
+	PWA_INFO_BANNER_TEST_ID
 } from '$lib/constants/test-ids.constants';
 import * as deviceUtils from '$lib/utils/device.utils';
 import { fireEvent, render } from '@testing-library/svelte';
@@ -18,28 +18,28 @@ describe('PwaBanner', () => {
 
 		const { queryByTestId } = render(PwaBanner);
 
-		expect(queryByTestId(PWA_WARNING_BANNER_TEST_ID)).toBeNull();
-		expect(queryByTestId(PWA_WARNING_BANNER_TEST_ID)).not.toBeInTheDocument();
+		expect(queryByTestId(PWA_INFO_BANNER_TEST_ID)).toBeNull();
+		expect(queryByTestId(PWA_INFO_BANNER_TEST_ID)).not.toBeInTheDocument();
 	});
 
 	it('should show the banner for PWA environment', () => {
 		const { getByTestId } = render(PwaBanner);
 
-		expect(getByTestId(PWA_WARNING_BANNER_TEST_ID)).toBeDefined();
-		expect(getByTestId(PWA_WARNING_BANNER_TEST_ID)).toBeInTheDocument();
+		expect(getByTestId(PWA_INFO_BANNER_TEST_ID)).toBeDefined();
+		expect(getByTestId(PWA_INFO_BANNER_TEST_ID)).toBeInTheDocument();
 	});
 
 	it('should hide the banner after clicking the close button', async () => {
 		const { getByTestId, queryByTestId } = render(PwaBanner);
 
-		expect(getByTestId(PWA_WARNING_BANNER_TEST_ID)).toBeDefined();
-		expect(getByTestId(PWA_WARNING_BANNER_TEST_ID)).toBeInTheDocument();
+		expect(getByTestId(PWA_INFO_BANNER_TEST_ID)).toBeDefined();
+		expect(getByTestId(PWA_INFO_BANNER_TEST_ID)).toBeInTheDocument();
 
-		expect(getByTestId(PWA_WARNING_BANNER_CLOSE_BUTTON_TEST_ID)).toBeInTheDocument();
+		expect(getByTestId(PWA_INFO_BANNER_CLOSE_BUTTON_TEST_ID)).toBeInTheDocument();
 
-		await fireEvent.click(getByTestId(PWA_WARNING_BANNER_CLOSE_BUTTON_TEST_ID));
+		await fireEvent.click(getByTestId(PWA_INFO_BANNER_CLOSE_BUTTON_TEST_ID));
 
-		expect(queryByTestId(PWA_WARNING_BANNER_TEST_ID)).toBeNull();
-		expect(queryByTestId(PWA_WARNING_BANNER_TEST_ID)).not.toBeInTheDocument();
+		expect(queryByTestId(PWA_INFO_BANNER_TEST_ID)).toBeNull();
+		expect(queryByTestId(PWA_INFO_BANNER_TEST_ID)).not.toBeInTheDocument();
 	});
 });
