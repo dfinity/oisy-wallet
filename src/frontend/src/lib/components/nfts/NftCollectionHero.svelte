c9f9feb21d5c06a16812ce2f7fb7149f85279225
daviddecentage <david.farina@dfinity.org>
2025-10-24 16:22:58 +0200

    update

--- src/frontend/src/lib/components/nfts/NftCollectionHero.svelte
+++ src/frontend/src/lib/components/nfts/NftCollectionHero.svelte
@@ -13,6 +13,9 @@
 	import { PLAUSIBLE_EVENT_SOURCES } from '$lib/enums/plausible';
 	import { i18n } from '$lib/stores/i18n.store';
 	import type { Nft, NonFungibleToken } from '$lib/types/nft';
+	import { nftsUrl } from '$lib/utils/nav.utils';
+	import { getContext } from 'svelte';
+	import { NFT_PAGES_CONTEXT_KEY, type NftPagesContext } from '$lib/stores/nft-pages.store';
 
 	interface Props {
 		token?: NonFungibleToken;
@@ -21,7 +24,12 @@
 
 	const { token, nfts }: Props = $props();
 
-	const breadcrumbItems = $derived([{ label: $i18n.navigation.text.tokens, url: AppPath.Nfts }]);
+	const { store } = getContext<NftPagesContext>(NFT_PAGES_CONTEXT_KEY);
+	const originSelectedNetwork = $derived($store?.originSelectedNetwork ?? undefined);
+
+	const breadcrumbItems = $derived([
+		{ label: $i18n.navigation.text.tokens, url: nftsUrl({ originSelectedNetwork }) }
+	]);
 
 	const firstNft = $derived(nfts?.[0]);
 	const bannerUrl = $derived(nonNullish(firstNft) ? firstNft.collection.bannerImageUrl : undefined);
