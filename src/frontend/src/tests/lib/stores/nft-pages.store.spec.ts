c7aca40f193b8958f887c2e8057ef1c0ac1c5168
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-10-24 16:20:47 +0200

    feat(frontend): Adds asset tab to nft pages store (#9949)
    # Motivation
    
    In order to persist which tab the user was on on the assets page, we
    store it in the Nft page context.
    
    # Changes
    
    Added new assetsTab field
    
    # Tests
    
    Extended tests
    
    ---------
    
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

--- src/frontend/src/tests/lib/stores/nft-pages.store.spec.ts
+++ src/frontend/src/tests/lib/stores/nft-pages.store.spec.ts
@@ -1,12 +1,13 @@
+import { TokenTypes } from '$lib/enums/token-types';
 import { initNftPagesStore } from '$lib/stores/nft-pages.store';
 import type { NetworkId } from '$lib/types/network';
 import { get } from 'svelte/store';
 
 describe('nft-pages.store', () => {
-	it('initializes with originSelectedNetwork undefined', () => {
+	it('initializes with originSelectedNetwork and assetsTab undefined', () => {
 		const store = initNftPagesStore();
 
-		expect(get(store)).toEqual({ originSelectedNetwork: undefined });
+		expect(get(store)).toEqual({ originSelectedNetwork: undefined, assetsTab: undefined });
 	});
 
 	it('updates originSelectedNetwork when setOriginSelectedNetwork is called', () => {
@@ -15,6 +16,14 @@ describe('nft-pages.store', () => {
 		const mockNetworkId = { description: 'network-123' } as unknown as NetworkId;
 		store.setOriginSelectedNetwork(mockNetworkId);
 
-		expect(get(store)).toEqual({ originSelectedNetwork: mockNetworkId });
+		expect(get(store)?.originSelectedNetwork).toEqual(mockNetworkId);
+	});
+
+	it('updates assetsTab when setAssetsTab is called', () => {
+		const store = initNftPagesStore();
+
+		store.setAssetsTab(TokenTypes.TOKENS);
+
+		expect(get(store)?.assetsTab).toEqual(TokenTypes.TOKENS);
 	});
 });
