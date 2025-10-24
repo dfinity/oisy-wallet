e287c9455cc3a832f1dfe97119e89b3e4b64a767
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-10-24 11:42:34 +0200

    feat(frontend): Nft pages store for context (#9937)
    # Motivation
    
    Adding a store for Nft page data that will be used in a context #9934 
    
    # Changes
    
    Added store
    
    # Tests
    
    Added tests
    
    ---------
    
    Co-authored-by: Denys Karmazyn <denys.karmazyn@dfinity.org>

---
+++ src/frontend/src/lib/stores/nft-pages.store.ts
@@ -0,0 +1,31 @@
+import type { OptionNetworkId } from '$lib/types/network';
+import type { Option } from '$lib/types/utils';
+import { writable, type Readable } from 'svelte/store';
+
+export type NftPagesStoreData = Option<{
+	originSelectedNetwork: OptionNetworkId;
+}>;
+
+export interface NftPagesStore extends Readable<NftPagesStoreData> {
+	setOriginSelectedNetwork: (networkId: OptionNetworkId) => void;
+}
+
+export const initNftPagesStore = (): NftPagesStore => {
+	const { subscribe, set } = writable<NftPagesStoreData>({
+		originSelectedNetwork: undefined
+	});
+
+	return {
+		subscribe,
+
+		setOriginSelectedNetwork: (networkId: OptionNetworkId) => {
+			set({ originSelectedNetwork: networkId });
+		}
+	};
+};
+
+export interface NftPagesContext {
+	store: NftPagesStore;
+}
+
+export const NFT_PAGES_CONTEXT_KEY = Symbol('nft-pages');
