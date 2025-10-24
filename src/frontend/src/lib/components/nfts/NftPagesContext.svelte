f61851a99472b6f6efe2ce047906283b7d3aa640
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-10-24 12:45:32 +0200

    feat(frontend): Nft pages context (#9934)
    # Motivation
    
    We need an easy way to propagate values across all Nft pages, so we add
    a context.
    
    # Changes
    
    Added store for the context
    Added context component
    
    # Tests
    
    Added tests
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/frontend/src/lib/components/nfts/NftPagesContext.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { nonNullish } from '@dfinity/utils';
+	import { setContext, type Snippet } from 'svelte';
+	import { selectedNetwork } from '$lib/derived/network.derived';
+	import {
+		initNftPagesStore,
+		NFT_PAGES_CONTEXT_KEY,
+		type NftPagesContext
+	} from '$lib/stores/nft-pages.store';
+
+	interface Props {
+		children: Snippet;
+	}
+
+	const { children }: Props = $props();
+
+	const { store } = setContext<NftPagesContext>(NFT_PAGES_CONTEXT_KEY, {
+		store: initNftPagesStore()
+	});
+
+	$effect(() => {
+		if (nonNullish($selectedNetwork?.id)) {
+			store.setOriginSelectedNetwork($selectedNetwork.id);
+		}
+	});
+</script>
+
+{@render children()}
