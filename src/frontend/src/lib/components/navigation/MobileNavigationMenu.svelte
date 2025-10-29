e4440bdcceaebd76667afe0534389878c7dc3a00
daviddecentage <david.farina@dfinity.org>
2025-10-29 22:28:36 +0100

    update

--- src/frontend/src/lib/components/navigation/MobileNavigationMenu.svelte
+++ src/frontend/src/lib/components/navigation/MobileNavigationMenu.svelte
@@ -2,6 +2,7 @@
 	import type { Snippet } from 'svelte';
 	import Responsive from '$lib/components/ui/Responsive.svelte';
 	import { MOBILE_NAVIGATION_MENU } from '$lib/constants/test-ids.constants';
+	import { bottomSheetOpenStore } from '$lib/stores/ui.store';
 
 	interface Props {
 		children: Snippet;
@@ -14,6 +15,7 @@
 	<div
 		class="mobile-nav visible fixed right-0 bottom-0 left-0 z-3 flex flex-row border-t-1 border-tertiary bg-primary-inverted-alt md:hidden"
 		data-tid={MOBILE_NAVIGATION_MENU}
+		class:hidden={$bottomSheetOpenStore}
 	>
 		{@render children()}
 	</div>
