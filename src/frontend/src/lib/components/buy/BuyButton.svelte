cf99277cdf7f6824da70295f811a6d44a1ed69d6
daviddecentage <david.farina@dfinity.org>
2025-10-29 18:30:07 +0100

    update

--- src/frontend/src/lib/components/buy/BuyButton.svelte
+++ src/frontend/src/lib/components/buy/BuyButton.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import { ONRAMPER_API_KEY } from '$env/rest/onramper.env';
 	import ButtonHero from '$lib/components/hero/ButtonHero.svelte';
 	import IconlyBuy from '$lib/components/icons/iconly/IconlyBuy.svelte';
 	import { isBusy } from '$lib/derived/busy.derived';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { isNullishOrEmpty } from '$lib/utils/input.utils';
+	import { isRouteNfts } from '$lib/utils/nav.utils';
 
 	interface Props {
 		onclick: () => void;
@@ -15,7 +17,7 @@
 
 <ButtonHero
 	ariaLabel={$i18n.send.text.send}
-	disabled={$isBusy || isNullishOrEmpty(ONRAMPER_API_KEY)}
+	disabled={$isBusy || isNullishOrEmpty(ONRAMPER_API_KEY) || isRouteNfts($page)}
 	{onclick}
 >
 	{#snippet icon()}
