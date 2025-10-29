d33d4024685de289281db8070e073649ac47a65b
daviddecentage <david.farina@dfinity.org>
2025-10-29 22:17:01 +0100

    update

--- src/frontend/src/lib/components/buy/BuyButton.svelte
+++ src/frontend/src/lib/components/buy/BuyButton.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-	import { page } from '$app/stores';
 	import { ONRAMPER_API_KEY } from '$env/rest/onramper.env';
 	import ButtonHero from '$lib/components/hero/ButtonHero.svelte';
 	import IconlyBuy from '$lib/components/icons/iconly/IconlyBuy.svelte';
 	import { isBusy } from '$lib/derived/busy.derived';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { isNullishOrEmpty } from '$lib/utils/input.utils';
-	import { isRouteNfts } from '$lib/utils/nav.utils';
 	import { HERO_CONTEXT_KEY, type HeroContext } from '$lib/stores/hero.store';
 	import { getContext } from 'svelte';
 
