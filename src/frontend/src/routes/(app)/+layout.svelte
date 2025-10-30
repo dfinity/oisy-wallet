dfinity/oisy-wallet

commit 5dd82e41f91bec227d19a80bf4486ee7e561ca7b

style(frontend): Fix overflow x landing page (#10049)
# Motivation

Currently we can scroll sideways on the landing page as the image
overflows to the right of the screen on mobile.

# Changes

Prevent overflow-x in the layout. This will also affect the logged in
application, but sideways scrolling the main layout shouldnt be allowed
there either.

# Tests

Before:
<img width="402" height="683" alt="image"
src="https://github.com/user-attachments/assets/b0bbcf38-009d-4477-89da-45180fbe4da3"
/>
<img width="402" height="683" alt="image"
src="https://github.com/user-attachments/assets/b05c9f12-ea25-4b35-b7e7-7ac9fb7259d4"
/>


After:
<img width="402" height="683" alt="image"
src="https://github.com/user-attachments/assets/d75fe48a-f4ca-4a45-b2f3-44afa3825dfc"
/>

---------

Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

src/frontend/src/routes/(app)/+layout.svelte
@@ -65,7 +65,7 @@
 {:else}
 	<div class:h-dvh={$authNotSignedIn}>
 		<div
-			class="relative flex flex-col pb-5 md:pb-0"
+			class="relative flex flex-col overflow-x-hidden pb-5 md:pb-0"
 			class:h-full={$authSignedIn}
 			class:min-h-[100dvh]={$authNotSignedIn}
 		>

commit 354dce010b3a2a9368ab6c645823396cbd365771

fix(frontend): Sticky headers (#10081)
# Motivation

The sticky headers got broken by #10049. Since we only use sticky
headers in the signed in part of the app and the overflow happens on the
landing page we can conditionally set the overflow-x-hidden.

# Changes

Adjusted class depending on signedIn state

# Tests



https://github.com/user-attachments/assets/0d5fbf2a-97bb-49fc-9cd5-302c99c3eb78

---------

Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

src/frontend/src/routes/(app)/+layout.svelte
@@ -65,9 +65,10 @@
 {:else}
 	<div class:h-dvh={$authNotSignedIn}>
 		<div
-			class="relative flex flex-col overflow-x-hidden pb-5 md:pb-0"
+			class="relative flex flex-col pb-5 md:pb-0"
 			class:h-full={$authSignedIn}
 			class:min-h-[100dvh]={$authNotSignedIn}
+			class:overflow-x-hidden={$authNotSignedIn}
 		>
 			<Header />
 