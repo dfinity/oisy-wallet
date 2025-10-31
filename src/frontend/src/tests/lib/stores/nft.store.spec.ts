ec48c9776dec4f0e75b75a93752c0ad2f03c2ba7
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-10-31 11:49:46 +0100

    fix(frontend): Refresh NFTs by network (#10104)
    # Motivation
    
    In `LoaderNFts` component, when we refresh the NFTs per collection, we
    do it by network, adding it to the existing store (removing duplicates
    too).
    
    However, if we send out an NFT, when we refresh, we will not receive it
    from the provider, so, we will not remove it from the store.
    
    To fix this, we can simply add the NFTs by network, completely removing
    the ones already saved for that specific network.
    
    # Changes
    
    - Add new method `setAllByNetwork` in NFT store.
    - Use the new method in `LoaderNfts`.
    
    # Tests
    
    Added tests and a practical one:
    
    
    
    https://github.com/user-attachments/assets/060de98a-8517-4380-ad55-a4c0f584d4e3
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/tests/lib/stores/nft.store.spec.ts
+++ src/frontend/src/tests/lib/stores/nft.store.spec.ts
@@ -1,3 +1,4 @@
+import { ARBITRUM_MAINNET_NETWORK } from '$env/networks/networks-evm/networks.evm.arbitrum.env';
 import { nftStore } from '$lib/stores/nft.store';
 import type { Nft } from '$lib/types/nft';
 import { parseNftId } from '$lib/validation/nft.validation';
@@ -74,6 +75,45 @@ describe('nftStore', () => {
 			nftStore.addAll([similarNft]);
 
 			expect(get(nftStore)).toEqual([similarNft]);
+		});
+	});
+
+	describe('setAllByNetwork', () => {
+		const mockNft1 = mockValidErc721Nft;
+		const mockNft2 = { ...mockValidErc721Nft, id: parseNftId('837364') };
+		const mockNfts = [mockNft1, mockNft2];
+		const mockNetwordId = mockNft1.collection.network.id;
+		const mockParams = { networkId: mockNetwordId, nfts: mockNfts };
+
+		it('should return the new NFTs if the store is nullish', () => {
+			nftStore.resetAll();
+
+			nftStore.setAllByNetwork(mockParams);
+
+			expect(get(nftStore)).toEqual(mockNfts);
+		});
+
+		it('should add new NFTs if the store is not nullish', () => {
+			const existingNft = {
+				...mockValidErc721Nft,
+				collection: { ...mockValidErc721Nft.collection, network: ARBITRUM_MAINNET_NETWORK }
+			};
+
+			nftStore.addAll([existingNft]);
+
+			nftStore.setAllByNetwork(mockParams);
+
+			expect(get(nftStore)).toEqual([existingNft, ...mockNfts]);
+		});
+
+		it('should replace only NFTs from the specified network', () => {
+			nftStore.addAll(mockNfts);
+
+			const newNft = { ...mockValidErc721Nft, id: parseNftId('123456') };
+
+			nftStore.setAllByNetwork({ ...mockParams, nfts: [newNft] });
+
+			expect(get(nftStore)).toEqual([newNft]);
 		});
 	});
 
