3f5c39becce5da4aff8bf4b8d87f0ec6da5091a1
David Dal Busco <david.dalbusco@dfinity.org>
2025-11-03 16:22:51 +0100

    feat(frontend): scope auth client provider with a class (#10153)
    # Motivation
    
    In an upcoming PR, we would like to transform the custom storage from a
    variable declared at the root of a module to an instance pattern. That's
    why, in a first step, we refactor the various auth-client related
    functions to include them in a class pattern.
    
    In this PR, the various functions get reexported by the module,
    therefore it should not introduce any other changes that those scoped to
    the module.

--- src/frontend/src/lib/providers/auth-client.providers.ts
+++ src/frontend/src/lib/providers/auth-client.providers.ts
@@ -1,41 +1,58 @@
 import type { Identity } from '@dfinity/agent';
 import { AuthClient, IdbStorage, KEY_STORAGE_KEY } from '@dfinity/auth-client';
 
-// We use a dedicated storage for the auth client to better manage it, e.g. clear it for a new login
-// @see src/lib/utils/auth.utils.ts safeCreateAuthClient
-export const authClientStorage = new IdbStorage();
+export class AuthClientProvider {
+	// We use a dedicated storage for the auth client to better manage it, e.g. clear it for a new login
+	readonly #storage: IdbStorage;
 
-export const createAuthClient = (): Promise<AuthClient> =>
-	AuthClient.create({
-		storage: authClientStorage,
-		idleOptions: {
-			disableIdle: true,
-			disableDefaultIdleCallback: true
-		}
-	});
-
-/**
- * Since icp-js-core persists identity keys in IndexedDB by default,
- * they could be tampered with and affect the next login.
- * To ensure each session starts clean and safe, we clear the stored keys before creating a new AuthClient.
- */
-export const safeCreateAuthClient = async (): Promise<AuthClient> => {
-	await authClientStorage.remove(KEY_STORAGE_KEY);
-	return await createAuthClient();
-};
-
-/**
- * In certain features, we want to execute jobs with the authenticated identity without getting it from the auth.store.
- * This is notably useful for Web Workers who do not have access to the window.
- */
-export const loadIdentity = async (): Promise<Identity | undefined> => {
-	const authClient = await createAuthClient();
-	const authenticated = await authClient.isAuthenticated();
-
-	// Not authenticated, therefore, we provide no identity as a result
-	if (!authenticated) {
-		return undefined;
+	constructor() {
+		this.#storage = new IdbStorage();
 	}
 
-	return authClient.getIdentity();
-};
+	createAuthClient = (): Promise<AuthClient> =>
+		AuthClient.create({
+			storage: this.#storage,
+			idleOptions: {
+				disableIdle: true,
+				disableDefaultIdleCallback: true
+			}
+		});
+
+	/**
+	 * Since icp-js-core persists identity keys in IndexedDB by default,
+	 * they could be tampered with and affect the next login.
+	 * To ensure each session starts clean and safe, we clear the stored keys before creating a new AuthClient.
+	 */
+	safeCreateAuthClient = async (): Promise<AuthClient> => {
+		await this.#storage.remove(KEY_STORAGE_KEY);
+		return await this.createAuthClient();
+	};
+
+	/**
+	 * In certain features, we want to execute jobs with the authenticated identity without getting it from the auth.store.
+	 * This is notably useful for Web Workers who do not have access to the window.
+	 */
+	loadIdentity = async (): Promise<Identity | undefined> => {
+		const authClient = await this.createAuthClient();
+		const authenticated = await authClient.isAuthenticated();
+
+		// Not authenticated, therefore, we provide no identity as a result
+		if (!authenticated) {
+			return undefined;
+		}
+
+		return authClient.getIdentity();
+	};
+
+	get storage(): IdbStorage {
+		return this.#storage;
+	}
+}
+
+const {
+	storage: authClientStorage,
+	safeCreateAuthClient,
+	loadIdentity,
+	createAuthClient
+} = new AuthClientProvider();
+export { authClientStorage, createAuthClient, loadIdentity, safeCreateAuthClient };
