5734f5e7518130d17187eb3214d84af9a979fbe9
daviddecentage <david.farina@dfinity.org>
2025-11-04 16:14:38 +0100

    update

--- src/frontend/src/routes/+layout.svelte
+++ src/frontend/src/routes/+layout.svelte
@@ -150,25 +150,27 @@
 
 	let scrollY = 0;
 
-	function lockBodyScroll() {
+	const lockBodyScroll = () => {
 		scrollY = window.scrollY;
 		document.body.style.position = 'fixed';
 		document.body.style.top = `-${scrollY}px`;
 		document.body.style.width = '100%';
-	}
+	};
 
-	function unlockBodyScroll() {
+	const unlockBodyScroll = () => {
 		document.body.style.position = '';
 		document.body.style.top = '';
 		document.body.style.width = '';
 		window.scrollTo(0, scrollY);
-	}
+	};
 
 	$effect(() => {
-		if ($modalStore?.type) {
-			lockBodyScroll();
-		} else {
-			unlockBodyScroll();
+		if (isIos()) {
+			if ($modalStore?.type) {
+				lockBodyScroll();
+			} else {
+				unlockBodyScroll();
+			}
 		}
 	});
 </script>
