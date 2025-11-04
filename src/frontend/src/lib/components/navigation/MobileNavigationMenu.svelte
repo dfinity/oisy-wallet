66d33b5e34db82448ebfc29904fa965431f551ac
daviddecentage <david.farina@dfinity.org>
2025-11-04 13:30:39 +0100

    update

--- src/frontend/src/lib/components/navigation/MobileNavigationMenu.svelte
+++ src/frontend/src/lib/components/navigation/MobileNavigationMenu.svelte
@@ -13,10 +13,11 @@
 
 <Responsive down="md">
 	<div
-		class="mobile-nav visible fixed right-0 bottom-0 left-0 z-3 flex flex-row border-t-1 border-tertiary bg-primary-inverted-alt md:hidden"
+		class="mobile-nav-wrapper visible fixed right-0 bottom-0 left-0 z-3 flex flex-row border-t-1 border-tertiary bg-primary-inverted-alt md:hidden"
 		class:hidden={$bottomSheetOpenStore}
-		data-tid={MOBILE_NAVIGATION_MENU}
 	>
-		{@render children()}
+		<div class="mobile-nav flex flex-row" data-tid={MOBILE_NAVIGATION_MENU}>
+			{@render children()}
+		</div>
 	</div>
 </Responsive>
