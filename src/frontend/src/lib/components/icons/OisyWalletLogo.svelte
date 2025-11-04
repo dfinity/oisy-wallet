8f6debc97a37b74fa30f3aea48dc398147aa4f8c
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-04 10:15:11 +0100

    Update src/frontend/src/lib/components/icons/OisyWalletLogo.svelte

--- src/frontend/src/lib/components/icons/OisyWalletLogo.svelte
+++ src/frontend/src/lib/components/icons/OisyWalletLogo.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import SeasonalIconAstronautHelmet from '$lib/components/core/SeasonalIconAstronautHelmet.svelte';
 	import SeasonalOisyLogoLarge from '$lib/components/core/SeasonalOisyLogoLarge.svelte';
-	import OisyLogoLarge from '$lib/components/icons/OisyLogoLarge.svelte';
 	import { OISY_NAME } from '$lib/constants/oisy.constants';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { replacePlaceholders } from '$lib/utils/i18n.utils';
