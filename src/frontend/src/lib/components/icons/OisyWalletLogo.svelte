c0c1227729e9c71d42b972e88f39246f8430b056
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-04 10:15:06 +0100

    Update src/frontend/src/lib/components/icons/OisyWalletLogo.svelte

--- src/frontend/src/lib/components/icons/OisyWalletLogo.svelte
+++ src/frontend/src/lib/components/icons/OisyWalletLogo.svelte
@@ -6,7 +6,6 @@
 	import { i18n } from '$lib/stores/i18n.store';
 	import { replacePlaceholders } from '$lib/utils/i18n.utils';
 
-	let ariaLabel = $derived(replacePlaceholders($i18n.core.alt.logo, { $name: OISY_NAME }));
 </script>
 
 <div class="mr-3">
