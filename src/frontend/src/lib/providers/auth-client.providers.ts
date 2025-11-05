e48f93c9e02046d8b89c269e85c2f06cb5d6a5e0
David Dal Busco <david.dalbusco@outlook.com>
2025-11-05 16:05:31 +0100

    chore: migrate import

--- src/frontend/src/lib/providers/auth-client.providers.ts
+++ src/frontend/src/lib/providers/auth-client.providers.ts
@@ -1,5 +1,5 @@
 import type { Identity } from '@dfinity/agent';
-import { AuthClient, IdbStorage, KEY_STORAGE_KEY } from '@dfinity/auth-client';
+import { AuthClient, IdbStorage, KEY_STORAGE_KEY } from '@icp-sdk/auth/client';
 import { isNullish } from '@dfinity/utils';
 
 export class AuthClientProvider {
