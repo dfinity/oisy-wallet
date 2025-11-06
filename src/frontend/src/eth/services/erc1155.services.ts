23bca16041823302f64510bddf5eaf1e30655e9d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-06 16:15:10 +0100

    feat(frontend): Do not stop loading ERC1155 custom tokens on single error

--- src/frontend/src/eth/services/erc1155.services.ts
+++ src/frontend/src/eth/services/erc1155.services.ts
@@ -166,9 +166,22 @@ const loadCustomTokensWithMetadata = async (
 			}
 		);
 
-	const customTokens = await Promise.all(customTokenPromises);
+	const customTokens = await Promise.allSettled(customTokenPromises);
 
-	return customTokens.filter(nonNullish);
+	return customTokens.reduce<Erc1155CustomToken[]>((acc, result) => {
+		if (result.status === 'fulfilled' && nonNullish(result.value)) {
+			acc.push(result.value);
+		}
+
+		if (result.status === 'rejected') {
+			toastsError({
+				msg: { text: get(i18n).init.error.erc1155_custom_tokens },
+				err: result.reason
+			});
+		}
+
+		return acc;
+	}, []);
 };
 
 const loadCustomTokenData = ({
