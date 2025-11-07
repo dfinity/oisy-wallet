0034ac76f18ba7a12d32562d34321aa82cea269d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-07 12:25:46 +0100

    build(frontend): Bump gix-components to next

--- src/frontend/src/lib/components/ui/Input.svelte
+++ src/frontend/src/lib/components/ui/Input.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-	import { Input as GixInput } from '@dfinity/gix-components';
+	import { Input as GixInput, type InputProps as GixInputProps } from '@dfinity/gix-components';
 	import { nonNullish, notEmptyString } from '@dfinity/utils';
-	import type { ComponentProps, Snippet } from 'svelte';
+	import type { Snippet } from 'svelte';
 	import { fade } from 'svelte/transition';
 	import ButtonPaste from '$lib/components/ui/ButtonPaste.svelte';
 	import ButtonReset from '$lib/components/ui/ButtonReset.svelte';
 
-	type GixInputProps = ComponentProps<GixInput>;
-	interface InputProps {
+	export interface InputProps extends GixInputProps {
 		showResetButton?: boolean;
 		resetButtonAriaLabel?: string;
 		showPasteButton?: boolean;
@@ -21,32 +20,32 @@
 		resetButtonAriaLabel,
 		showPasteButton,
 		value = $bindable(),
-		innerEnd,
+		innerEnd: innerEndProp,
 		...props
-	}: InputProps & GixInputProps = $props();
+	}: InputProps = $props();
 </script>
 
 <div
 	style={`--input-padding-inner-end: calc(var(--padding-2x) + ${endWidth}px)`}
 	class="base-input"
 >
-	<GixInput {...props} bind:value on:nnsInput>
-		<svelte:fragment slot="inner-end">
+	<GixInput {...props} bind:value>
+		{#snippet innerEnd()}
 			<div class="flex items-center pl-2" bind:clientWidth={endWidth}>
 				{#if nonNullish(value) && notEmptyString(value.toString()) && showResetButton}
 					<div transition:fade>
 						<ButtonReset ariaLabel={resetButtonAriaLabel} onclick={() => (value = undefined)} />
 					</div>
-					{#if nonNullish(innerEnd) || showPasteButton}
+					{#if nonNullish(innerEndProp) || showPasteButton}
 						<div class="self-stretch border-r-1 border-black/20" transition:fade></div>
 					{/if}
 				{/if}
 				{#if showPasteButton}
 					<ButtonPaste onpaste={(text) => (value = text)} />
 				{/if}
-				{@render innerEnd?.()}
+				{@render innerEndProp?.()}
 			</div>
-		</svelte:fragment>
+		{/snippet}
 	</GixInput>
 </div>
 
