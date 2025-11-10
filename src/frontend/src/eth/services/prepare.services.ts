a3868f85af7ff9fe7de94a95cb30a9d0667813a5
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-10 12:46:13 +0100

    refactor(frontend): Extract ETH transaction prepare service

---
+++ src/frontend/src/eth/services/prepare.services.ts
@@ -0,0 +1,43 @@
+import type { EthSignTransactionRequest } from '$declarations/signer/declarations/signer.did';
+import type { NetworkChainId } from '$eth/types/network';
+import { i18n } from '$lib/stores/i18n.store';
+import type { TransferParams } from '$lib/types/send';
+import { isNullish } from '@dfinity/utils';
+import { get } from 'svelte/store';
+
+export const prepare = ({
+	maxPriorityFeePerGas: max_priority_fee_per_gas,
+	maxFeePerGas: max_fee_per_gas,
+	nonce,
+	gas,
+	chainId: chain_id,
+	data,
+	to,
+	amount
+}: Omit<TransferParams, 'amount' | 'from'> &
+	NetworkChainId & {
+		nonce: number;
+		gas: bigint;
+		amount: bigint;
+	}): EthSignTransactionRequest => {
+	if (isNullish(data)) {
+		const {
+			send: {
+				error: { erc20_data_undefined }
+			}
+		} = get(i18n);
+
+		throw new Error(erc20_data_undefined);
+	}
+
+	return {
+		to,
+		chain_id,
+		nonce: BigInt(nonce),
+		gas,
+		max_fee_per_gas,
+		max_priority_fee_per_gas,
+		value: amount,
+		data: [data]
+	};
+};
