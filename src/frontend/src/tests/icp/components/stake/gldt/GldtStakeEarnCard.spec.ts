287441b53c9ba05219ad6fe5ce36266bb689160c
Denys Karmazyn <denys.karmazyn@dfinity.org>
2025-11-12 09:06:20 +0100

    feat(frontend): update GldtStakeEarnCard component (#10277)
    # Motivation
    
    We need to update the GldtStakeEarnCard component - we now calculate
    user's total available balance and calculate how many GLDT token can be
    bought with this amount of tokens.

--- src/frontend/src/tests/icp/components/stake/gldt/GldtStakeEarnCard.spec.ts
+++ src/frontend/src/tests/icp/components/stake/gldt/GldtStakeEarnCard.spec.ts
@@ -1,5 +1,10 @@
 import { GLDT_LEDGER_CANISTER_ID } from '$env/networks/networks.icrc.env';
 import GldtStakeEarnCard from '$icp/components/stake/gldt/GldtStakeEarnCard.svelte';
+import {
+	GLDT_STAKE_CONTEXT_KEY,
+	initGldtStakeStore,
+	type GldtStakeContext
+} from '$icp/stores/gldt-stake.store';
 import { icrcCustomTokensStore } from '$icp/stores/icrc-custom-tokens.store';
 import { replacePlaceholders } from '$lib/utils/i18n.utils';
 import en from '$tests/mocks/i18n.mock';
@@ -7,6 +12,9 @@ import { mockIcrcCustomToken } from '$tests/mocks/icrc-custom-tokens.mock';
 import { render } from '@testing-library/svelte';
 
 describe('GldtStakeEarnCard', () => {
+	const mockContext = () =>
+		new Map<symbol, GldtStakeContext>([[GLDT_STAKE_CONTEXT_KEY, { store: initGldtStakeStore() }]]);
+
 	beforeEach(() => {
 		icrcCustomTokensStore.resetAll();
 
@@ -17,7 +25,9 @@ describe('GldtStakeEarnCard', () => {
 	});
 
 	it('should display correct values if GLDT token is not available', () => {
-		const { container } = render(GldtStakeEarnCard);
+		const { container } = render(GldtStakeEarnCard, {
+			context: mockContext()
+		});
 
 		expect(container).toHaveTextContent(
 			replacePlaceholders(en.stake.text.enable_token_button, { $token_symbol: 'GLDT' })
@@ -36,7 +46,8 @@ describe('GldtStakeEarnCard', () => {
 					ledgerCanisterId: GLDT_LEDGER_CANISTER_ID,
 					symbol: 'GLDT'
 				}
-			}
+			},
+			context: mockContext()
 		});
 
 		expect(container).toHaveTextContent(
