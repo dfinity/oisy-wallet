8f34278a2cced6afed528109b81be7eb90aeb40c
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-12 13:31:12 +0100

    test(frontend): Tests for `BuyButton` (#10293)
    # Motivation
    
    Tests for the `BuyButton` in the `Hero`.

--- src/frontend/src/lib/components/buy/BuyButton.svelte
+++ src/frontend/src/lib/components/buy/BuyButton.svelte
@@ -3,6 +3,7 @@
 	import { ONRAMPER_API_KEY } from '$env/rest/onramper.env';
 	import ButtonHero from '$lib/components/hero/ButtonHero.svelte';
 	import IconlyBuy from '$lib/components/icons/iconly/IconlyBuy.svelte';
+	import { BUY_TOKENS_MODAL_OPEN_BUTTON } from '$lib/constants/test-ids.constants';
 	import { isBusy } from '$lib/derived/busy.derived';
 	import { HERO_CONTEXT_KEY, type HeroContext } from '$lib/stores/hero.store';
 	import { i18n } from '$lib/stores/i18n.store';
@@ -21,6 +22,7 @@
 	ariaLabel={$i18n.send.text.send}
 	disabled={$isBusy || isNullishOrEmpty(ONRAMPER_API_KEY) || $inflowActionsDisabled}
 	{onclick}
+	testId={BUY_TOKENS_MODAL_OPEN_BUTTON}
 >
 	{#snippet icon()}
 		<IconlyBuy size="24" />
