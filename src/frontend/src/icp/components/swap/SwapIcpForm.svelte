d61b660797a6a1d6e350f63644cb1de122ab8e40
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-11-13 12:40:37 +0100

    test

--- src/frontend/src/icp/components/swap/SwapIcpForm.svelte
+++ src/frontend/src/icp/components/swap/SwapIcpForm.svelte
@@ -1,24 +1,14 @@
 <script lang="ts">
-	import { Html } from '@dfinity/gix-components';
 	import { nonNullish } from '@dfinity/utils';
 	import { getContext } from 'svelte';
 	import SwapFees from '$lib/components/swap/SwapFees.svelte';
 	import SwapForm from '$lib/components/swap/SwapForm.svelte';
 	import SwapProvider from '$lib/components/swap/SwapProvider.svelte';
 	import Hr from '$lib/components/ui/Hr.svelte';
-	import MessageBox from '$lib/components/ui/MessageBox.svelte';
 	import { ZERO } from '$lib/constants/app.constants';
-	import { i18n } from '$lib/stores/i18n.store';
-	import {
-		SWAP_AMOUNTS_CONTEXT_KEY,
-		type SwapAmountsContext as SwapAmountsContextType
-	} from '$lib/stores/swap-amounts.store';
 	import { SWAP_CONTEXT_KEY, type SwapContext } from '$lib/stores/swap.store';
 	import type { OptionAmount } from '$lib/types/send';
-	import { SwapProvider as SwapProviderType } from '$lib/types/swap';
 	import type { TokenActionErrorType } from '$lib/types/token-action';
-	import { replacePlaceholders } from '$lib/utils/i18n.utils';
-	import { calculateValueDifference } from '$lib/utils/swap.utils';
 	import { validateUserAmount } from '$lib/utils/user-amount.utils';
 
 	interface Props {
@@ -48,26 +38,12 @@
 	const { sourceToken, destinationToken, sourceTokenBalance, isSourceTokenIcrc2 } =
 		getContext<SwapContext>(SWAP_CONTEXT_KEY);
 
-	const { store: swapAmountsStore } = getContext<SwapAmountsContextType>(SWAP_AMOUNTS_CONTEXT_KEY);
-
-	const { sourceTokenExchangeRate, destinationTokenExchangeRate } =
-		getContext<SwapContext>(SWAP_CONTEXT_KEY);
-
 	let errorType = $state<TokenActionErrorType | undefined>();
 
 	let totalFee = $derived(
 		nonNullish($isSourceTokenIcrc2)
 			? (sourceTokenFee ?? ZERO) * ($isSourceTokenIcrc2 ? 2n : 1n)
 			: undefined
-	);
-
-	let valueDifference = $derived(
-		calculateValueDifference({
-			swapAmount,
-			receiveAmount,
-			sourceTokenExchangeRate: $sourceTokenExchangeRate,
-			destinationTokenExchangeRate: $destinationTokenExchangeRate
-		})
 	);
 
 	const customValidate = (userAmount: bigint): TokenActionErrorType =>
@@ -81,19 +57,11 @@
 				})
 			: undefined;
 
-	let isSlippageExceeded = $derived(
-		nonNullish(slippageValue) &&
-			nonNullish(valueDifference) &&
-			$swapAmountsStore?.selectedProvider?.provider === SwapProviderType.KONG_SWAP
-			? valueDifference < 0 && Math.abs(valueDifference) > Number(slippageValue)
-			: false
-	);
 </script>
 
 <SwapForm
 	{errorType}
 	fee={totalFee}
-	{isSlippageExceeded}
 	{isSwapAmountsLoading}
 	{onClose}
 	onCustomValidate={customValidate}
@@ -103,18 +71,6 @@
 	bind:receiveAmount
 	bind:slippageValue
 >
-	{#snippet swapAdditionalInfo()}
-		{#if isSlippageExceeded && nonNullish(slippageValue) && nonNullish(valueDifference)}
-			<MessageBox styleClass="sm:text-sm mt-4">
-				<Html
-					text={replacePlaceholders($i18n.swap.error.swap_slippage_exceeded_exchange_rate, {
-						$valueDifference: valueDifference.toFixed(2),
-						$maxSlippage: `${slippageValue}`
-					})}
-				/>
-			</MessageBox>
-		{/if}
-	{/snippet}
 	{#snippet swapDetails()}
 		{#if nonNullish($destinationToken) && nonNullish($sourceToken)}
 			<Hr spacing="md" />
