7bef6cb63ab39410950219c162db197898c01f09
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-11-14 10:33:31 +0100

    fix(frontend): Missing rewards requirement prop for earnings card (#10322)
    # Motivation
    
    While extending the reward requirements component in #10314 I forgot to
    set the new prop to truncate the requirement criterias.
    
    # Changes
    
    Added prop
    
    # Tests
    
    <img width="263" height="242" alt="image"
    src="https://github.com/user-attachments/assets/47e1a98e-857f-4956-a8fc-c397a1ea736f"
    />

--- src/frontend/src/lib/components/rewards/RewardsRequirements.svelte
+++ src/frontend/src/lib/components/rewards/RewardsRequirements.svelte
@@ -77,7 +77,11 @@
 	<ul class="list-none">
 		{#each criteria as criterion, i (criterion)}
 			<li class="flex gap-2 pt-1">
-				<RewardRequirement {criterion} testId={`${REWARDS_REQUIREMENTS_STATUS}-${i}`} />
+				<RewardRequirement
+					{criterion}
+					testId={`${REWARDS_REQUIREMENTS_STATUS}-${i}`}
+					truncate={type === 'earnings-card'}
+				/>
 			</li>
 		{/each}
 	</ul>
