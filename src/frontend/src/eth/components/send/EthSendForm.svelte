08110e8aac74040a72a6164cb9c91babe1668446
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-17 09:20:36 +0100

    more

--- src/frontend/src/eth/components/send/EthSendForm.svelte
+++ src/frontend/src/eth/components/send/EthSendForm.svelte
@@ -4,9 +4,11 @@
 	import { getContext, type Snippet } from 'svelte';
 	import EthFeeDisplay from '$eth/components/fee/EthFeeDisplay.svelte';
 	import EthSendAmount from '$eth/components/send/EthSendAmount.svelte';
+	import { infuraProviders } from '$eth/providers/infura.providers';
 	import { ETH_FEE_CONTEXT_KEY, type EthFeeContext } from '$eth/stores/eth-fee.store';
 	import SendFeeInfo from '$lib/components/send/SendFeeInfo.svelte';
 	import SendForm from '$lib/components/send/SendForm.svelte';
+	import { ethAddress } from '$lib/derived/address.derived';
 	import { i18n } from '$lib/stores/i18n.store';
 	import type { ContactUi } from '$lib/types/contact';
 	import type { OptionAmount } from '$lib/types/send';
@@ -46,6 +48,32 @@
 
 	const { feeSymbolStore, feeDecimalsStore, feeTokenIdStore }: EthFeeContext =
 		getContext<EthFeeContext>(ETH_FEE_CONTEXT_KEY);
+
+	let highestNonce = $state<number | undefined>();
+
+	let onChainNonce = $state<number | undefined>();
+
+	const updateHighestNonce = async () => {
+		const {
+			network: { id: networkId }
+		} = nativeEthereumToken;
+
+		const { getTransactionCount, getTransactionCount2 } = infuraProviders(networkId);
+
+		if (isNullish($ethAddress)) {
+			return;
+		}
+
+		highestNonce = await getTransactionCount($ethAddress);
+
+		onChainNonce = await getTransactionCount2($ethAddress);
+	};
+
+	$effect(() => {
+		[nativeEthereumToken, $ethAddress];
+
+		updateHighestNonce();
+	});
 </script>
 
 <SendForm
@@ -68,8 +96,20 @@
 			{/snippet}
 		</EthFeeDisplay>
 
-		<span>Custom Nonce</span>
-		<input min="0" placeholder="Custom Nonce" type="number" bind:value={customNonce} />
+		<div>
+			<span>Custom Nonce</span>
+			<input min="0" placeholder="Custom Nonce" type="number" bind:value={customNonce} />
+		</div>
+
+		<div>
+			<span>Highest nonce</span>
+			<span>{highestNonce}</span>
+		</div>
+
+		<div>
+			<span>On-chain nonce</span>
+			<span>{onChainNonce}</span>
+		</div>
 	{/snippet}
 
 	{#snippet info()}
