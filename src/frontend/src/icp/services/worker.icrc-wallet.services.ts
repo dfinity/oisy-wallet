b40fd96a3289f05116c6d49c02d9871fee0b0967
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-18 13:01:05 +0100

    more

--- src/frontend/src/icp/services/worker.icrc-wallet.services.ts
+++ src/frontend/src/icp/services/worker.icrc-wallet.services.ts
@@ -39,6 +39,8 @@ export class IcrcWalletWorker extends AppWorker implements WalletWorker {
 			>) => {
 				const { msg, ref, data } = dataMsg;
 
+				// This is an additional guard because it may happen that the worker is initialised as a singleton.
+				// In this case, we need to check if we should treat the message or if the message was intended for another worker.
 				if (ref !== this.ledgerCanisterId) {
 					return;
 				}
