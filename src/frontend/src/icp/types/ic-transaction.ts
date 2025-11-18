ae41bd38e77b1352f3cd159bd2e7ff0b62cff52e
David Dal Busco <david.dalbusco@outlook.com>
2025-11-18 18:22:18 +0100

    feat: adapt interface for canister ng

--- src/frontend/src/icp/types/ic-transaction.ts
+++ src/frontend/src/icp/types/ic-transaction.ts
@@ -3,10 +3,7 @@ import type { Dip20TransactionWithId } from '$icp/types/api';
 import type { icpTransactionTypes } from '$lib/schema/transaction.schema';
 import type { TransactionId, TransactionType } from '$lib/types/transaction';
 import type { Transaction, TransactionWithId } from '@dfinity/ledger-icp';
-import type {
-	IcrcTransaction as IcrcTransactionCandid,
-	IcrcTransactionWithId
-} from '@dfinity/ledger-icrc';
+import type { IcrcIndexNgTransaction, IcrcIndexNgTransactionWithId } from '@dfinity/ledger-icrc';
 
 export interface IcTransactionAddOnsInfo {
 	transferToSelf?: 'send' | 'receive';
@@ -17,8 +14,8 @@ export type IcpTransaction = { transaction: Transaction & IcTransactionAddOnsInf
 	'id'
 >;
 export type IcrcTransaction = {
-	transaction: IcrcTransactionCandid & IcTransactionAddOnsInfo;
-} & Pick<IcrcTransactionWithId, 'id'>;
+	transaction: IcrcIndexNgTransaction & IcTransactionAddOnsInfo;
+} & Pick<IcrcIndexNgTransactionWithId, 'id'>;
 export type Dip20Transaction = { transaction: Event & IcTransactionAddOnsInfo } & Pick<
 	Dip20TransactionWithId,
 	'id'
