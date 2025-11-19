0809223014aa3d73f3ddcd391c9e56d89edecc0f
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-11-19 16:24:47 +0100

    fix

--- src/frontend/src/lib/components/scanner/PayHero.svelte
+++ src/frontend/src/lib/components/scanner/PayHero.svelte
@@ -3,9 +3,10 @@
 	import IconOpenCryptoPay from '$lib/components/icons/IconOpenCryptoPay.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { replacePlaceholders } from '$lib/utils/i18n.utils';
+	import type { Amount } from '$lib/types/send';
 
 	interface Props {
-		amount: string | number;
+		amount: Amount;
 		asset: string;
 		receipt?: string;
 	}
@@ -14,7 +15,7 @@
 </script>
 
 <div
-	class="bg-pos-0 mb-6 flex h-full w-full flex-col content-center items-center justify-center rounded-[24px] bg-brand-subtle-10 pt-6 text-center text-primary-inverted transition-all duration-500 ease-in-out md:rounded-[28px]"
+	class="bg-pos-0 mb-6 flex h-full w-full flex-col content-center items-center justify-center rounded-[24px] bg-brand-subtle-10 pt-6 text-center text-primary-inverted duration-500 ease-in-out md:rounded-[28px]"
 >
 	{#if nonNullish(receipt)}
 		<p class="mb-3 font-bold text-secondary">
