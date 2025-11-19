4ebad1f37c50b61c93b1e742431cfbcb6300112c
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-11-19 16:11:23 +0100

    fix

--- src/frontend/src/lib/components/scanner/PayHero.svelte
+++ src/frontend/src/lib/components/scanner/PayHero.svelte
@@ -2,11 +2,12 @@
 	import IconOpenCryptoPay from '$lib/components/icons/IconOpenCryptoPay.svelte';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { replacePlaceholders } from '$lib/utils/i18n.utils';
+	import { nonNullish } from '@dfinity/utils';
 
 	interface Props {
 		amount: string | number;
 		asset: string;
-		receipt: string;
+		receipt?: string;
 	}
 
 	let { amount, asset, receipt }: Props = $props();
@@ -15,11 +16,14 @@
 <div
 	class="bg-pos-0 mb-6 flex h-full w-full flex-col content-center items-center justify-center rounded-[24px] bg-brand-subtle-10 pt-6 text-center text-primary-inverted transition-all duration-500 ease-in-out md:rounded-[28px]"
 >
-	<p class="mb-3 font-bold text-secondary">
-		{replacePlaceholders($i18n.scanner.text.pay_to, {
-			$receipt: receipt
-		})}
-	</p>
+	{#if nonNullish(receipt)}
+		<p class="mb-3 font-bold text-secondary">
+			{replacePlaceholders($i18n.scanner.text.pay_to, {
+				$receipt: receipt
+			})}
+		</p>
+	{/if}
+	
 	<div class="mb-6 flex flex-col gap-1 text-secondary">
 		<output
 			class="inline-flex w-full flex-row justify-center gap-3 text-4xl font-bold break-words md:text-5xl"
