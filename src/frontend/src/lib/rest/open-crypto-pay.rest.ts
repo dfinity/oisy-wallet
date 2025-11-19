2191d54970f76fc555575272430ed7b3a56948a8
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-11-19 14:35:04 +0100

    feat(frontend): added open crypto pay rest

---
+++ src/frontend/src/lib/rest/open-crypto-pay.rest.ts
@@ -0,0 +1,12 @@
+export const fetchOpenCryptoPay = async <T>(apiUrl: string): Promise<T | null> => {
+	const response = await fetch(apiUrl, {
+		method: 'GET',
+		headers: { 'Content-Type': 'application/json' }
+	});
+
+	if (!response.ok) {
+		throw new Error('Fetching OpenCryptoPay failed.');
+	}
+
+	return response.json();
+};
---
+++ src/frontend/src/lib/types/open-crypto-pay.ts
@@ -0,0 +1,63 @@
+export interface Address {
+	street: string;
+	houseNumber: string;
+	city: string;
+	zip: string;
+	country: string;
+}
+
+export interface Recipient {
+	name: string;
+	address: Address;
+	phone: string;
+	mail: string;
+	website: string;
+	registrationNumber: string;
+	storeType: string;
+	merchantCategory: string;
+	goodsType: string;
+	goodsCategory: string;
+}
+
+export interface Quote {
+	id: string;
+	expiration: string;
+	payment: string;
+}
+
+export interface RequestedAmount {
+	asset: string;
+	amount: number;
+}
+
+export interface Asset {
+	asset: string;
+	amount: string;
+}
+
+export interface TransferAmount {
+	method: string;
+	minFee: number;
+	assets: Asset[];
+	available: boolean;
+}
+
+export interface OpenCryptoPayResponse {
+	id: string;
+	externalId: string;
+	mode: string;
+	tag: string;
+	callback: string;
+	minSendable: number;
+	maxSendable: number;
+	metadata: string;
+	displayName: string;
+	standard: string;
+	possibleStandards: string[];
+	displayQr: boolean;
+	recipient: Recipient;
+	route: string;
+	quote: Quote;
+	requestedAmount: RequestedAmount;
+	transferAmounts: TransferAmount[];
+}
