4559a9775af4192c7a8790ec678a04d8ecfb85f1
daviddecentage <david.farina@dfinity.org>
2025-11-19 18:03:22 +0100

    update

--- src/frontend/src/lib/components/stake/StakeContentCard.svelte
+++ src/frontend/src/lib/components/stake/StakeContentCard.svelte
@@ -18,18 +18,17 @@
 	class:border={!primaryStyle}
 	class:border-0={primaryStyle}
 >
-
 	<div
-		class="mb-8 flex flex-col justify-center text-center"
+		class="flex flex-col justify-center text-center"
 		class:gap-2={!primaryStyle}
 		class:gap-4={primaryStyle}
 	>
 		{@render content()}
 	</div>
 
-{#if nonNullish(buttons)}
-	<div class="flex w-full flex-col gap-2">
-		{@render buttons()}
-	</div>
+	{#if nonNullish(buttons)}
+		<div class="mt-8 flex w-full flex-col gap-2">
+			{@render buttons()}
+		</div>
 	{/if}
 </div>
