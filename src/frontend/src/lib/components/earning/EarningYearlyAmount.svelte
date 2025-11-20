0868c31dfae7c7a7a885f86f0ad440331cde734b
daviddecentage <david.farina@dfinity.org>
2025-11-20 17:18:28 +0100

    update

--- src/frontend/src/lib/components/earning/EarningYearlyAmount.svelte
+++ src/frontend/src/lib/components/earning/EarningYearlyAmount.svelte
@@ -7,35 +7,46 @@
 	import { i18n } from '$lib/stores/i18n.store';
 	import { formatCurrency } from '$lib/utils/format.utils';
 	import { replacePlaceholders } from '$lib/utils/i18n.utils';
+	import type { Snippet } from 'svelte';
 
 	interface Props {
 		value?: number;
 		showPlusSign?: boolean;
 		formatPositiveAmount?: boolean;
+		fallback?: Snippet;
 	}
 
-	const { value, showPlusSign = false, formatPositiveAmount = false }: Props = $props();
+	const { value, showPlusSign = false, formatPositiveAmount = false, fallback }: Props = $props();
+
+	const formattedCurrency = $derived(
+		nonNullish(value)
+			? formatCurrency({
+					value,
+					currency: $currentCurrency,
+					exchangeRate: $currencyExchangeStore,
+					language: $currentLanguage
+				})
+			: undefined
+	);
 
 	const yearlyAmount = $derived(
-		nonNullish(value)
+		nonNullish(formattedCurrency)
 			? replacePlaceholders($i18n.stake.text.active_earning_per_year, {
-					$amount: `${formatCurrency({
-						value,
-						currency: $currentCurrency,
-						exchangeRate: $currencyExchangeStore,
-						language: $currentLanguage
-					})}`
+					$amount: `${formattedCurrency}`
 				})
 			: undefined
 	);
 </script>
 
-{#if nonNullish(yearlyAmount) && nonNullish(value)}
+{#if nonNullish(yearlyAmount)}
 	<span
 		class:text-brand-primary={!formatPositiveAmount}
-		class:text-success-primary={formatPositiveAmount && value > 0}
+		class:text-success-primary={formatPositiveAmount && nonNullish(value) && value > 0}
+		class:text-tertiary={value === 0}
 		in:fade
 	>
 		{`${showPlusSign ? '+' : ''}${yearlyAmount}`}
 	</span>
+{:else if nonNullish(fallback)}
+	{@render fallback()}
 {/if}
