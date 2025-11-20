4681eee2e58badb5c3567b7e8b43c8a5f614e5ee
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-20 22:44:50 +0100

    Update src/frontend/src/icp/api/ext-v2-token.api.ts

--- src/frontend/src/icp/api/ext-v2-token.api.ts
+++ src/frontend/src/icp/api/ext-v2-token.api.ts
@@ -17,8 +17,7 @@ import { Principal } from '@icp-sdk/core/principal';
  */
 export const transactions = async ({
 	identity,
-	certified,
-	canisterId,
+	certified,	canisterId,
 	...rest
 }: CanisterApiFunctionParamsWithCanisterId<QueryParams>): Promise<Transaction[]> => {
 	const { transactions } = await extV2TokenCanister({
