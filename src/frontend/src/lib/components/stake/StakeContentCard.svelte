39700cba6e4f779e47ed8c9caa45607131cb4740
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-11-20 17:15:04 +0100

    Update StakeContentCard.svelte

--- src/frontend/src/lib/components/stake/StakeContentCard.svelte
+++ src/frontend/src/lib/components/stake/StakeContentCard.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { nonNullish } from '@dfinity/utils';
 	import type { Snippet } from 'svelte';
-	import { nonNullish } from '@dfinity/utils';
 
 	interface Props {
 		content: Snippet;
