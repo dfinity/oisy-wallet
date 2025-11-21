cd76a317e346a0ad95fa112ccf76458c474150e2
daviddecentage <80264282+daviddecentage@users.noreply.github.com>
2025-11-21 09:46:29 +0100

    feat(frontend): Earning potential card (#10452)
    # Motivation
    
    We want to display a summary of all earning methods potential.
    
    # Changes
    
    Added component
    
    # Tests
    
    Added tests
    
    <img width="271" height="145" alt="image"
    src="https://github.com/user-attachments/assets/a04c2fea-45fa-44e6-9649-0b8f310f68bf"
    />
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/frontend/src/lib/components/earning/EarningPotentialCard.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import EarningYearlyAmount from '$lib/components/earning/EarningYearlyAmount.svelte';
+	import StakeContentCard from '$lib/components/stake/StakeContentCard.svelte';
+	import SkeletonText from '$lib/components/ui/SkeletonText.svelte';
+	import { currentCurrency } from '$lib/derived/currency.derived';
+	import { highestApyEarningData } from '$lib/derived/earning.derived';
+	import { currentLanguage } from '$lib/derived/i18n.derived';
+	import { enabledMainnetFungibleTokensUsdBalance } from '$lib/derived/tokens.derived';
+	import { currencyExchangeStore } from '$lib/stores/currency-exchange.store';
+	import { i18n } from '$lib/stores/i18n.store';
+	import { formatCurrency } from '$lib/utils/format.utils';
+
+	const highestApy = $derived(
+		!isNaN(Number($highestApyEarningData?.apy)) ? Number($highestApyEarningData?.apy) : 0
+	);
+</script>
+
+<StakeContentCard>
+	{#snippet content()}
+		<div class="text-sm">{$i18n.stake.text.earning_potential}</div>
+
+		<div class="my-1 text-lg font-bold sm:text-xl">
+			<EarningYearlyAmount
+				showPlusSign={$enabledMainnetFungibleTokensUsdBalance > 0 && highestApy > 0}
+				value={($enabledMainnetFungibleTokensUsdBalance * highestApy) / 100}
+			>
+				{#snippet fallback()}
+					<SkeletonText />
+				{/snippet}
+			</EarningYearlyAmount>
+		</div>
+
+		<div class="flex justify-center gap-2 text-sm font-bold sm:text-base">
+			{formatCurrency({
+				value: $enabledMainnetFungibleTokensUsdBalance,
+				currency: $currentCurrency,
+				exchangeRate: $currencyExchangeStore,
+				language: $currentLanguage
+			})}
+		</div>
+	{/snippet}
+</StakeContentCard>
