937095d52dee5c3b64506c231b02f96747205ce2
daviddecentage <david.farina@dfinity.org>
2025-11-21 10:08:00 +0100

    update

---
+++ src/frontend/src/lib/components/earning/EarningHeader.svelte
@@ -0,0 +1,29 @@
+<script>
+	import StakeContentSection from '$lib/components/stake/StakeContentSection.svelte';
+	import EarningPotentialCard from '$lib/components/earning/EarningPotentialCard.svelte';
+	import EarningPositionCard from '$lib/components/earning/EarningPositionCard.svelte';
+	import { i18n } from '$lib/stores/i18n.store.js';
+	import ExternalLink from '$lib/components/ui/ExternalLink.svelte';
+	import { OISY_DOCS_URL } from '$lib/constants/oisy.constants.js';
+</script>
+
+<StakeContentSection>
+	{#snippet title()}
+		<h3 class="w-full text-center">
+			{$i18n.earning.text.header_title}
+		</h3>
+	{/snippet}
+	{#snippet content()}
+		<p class="w-full text-center">
+			{$i18n.earning.text.header_description}
+			<ExternalLink iconAsLast href={OISY_DOCS_URL} ariaLabel={$i18n.core.alt.learn_more}>
+				{$i18n.core.text.learn_more}
+			</ExternalLink>
+		</p>
+
+		<div class="flex w-full grid-cols-2 gap-3">
+			<EarningPotentialCard />
+			<EarningPositionCard />
+		</div>
+	{/snippet}
+</StakeContentSection>
---
+++ src/frontend/src/tests/lib/components/earning/EarningHeader.spec.ts
@@ -0,0 +1,28 @@
+import EarningHeader from '$lib/components/earning/EarningHeader.svelte';
+import { i18n } from '$lib/stores/i18n.store';
+import { render, screen } from '@testing-library/svelte';
+import { get } from 'svelte/store';
+
+describe('EarningHeader', () => {
+	it('renders header title', () => {
+		render(EarningHeader);
+		expect(screen.getByText(get(i18n).earning.text.header_title)).toBeInTheDocument();
+	});
+
+	it('renders header description', () => {
+		render(EarningHeader);
+		expect(screen.getByText(get(i18n).earning.text.header_description)).toBeInTheDocument();
+	});
+
+	it('renders the potential card', () => {
+		render(EarningHeader);
+
+		expect(screen.getByText(get(i18n).stake.text.earning_potential)).toBeInTheDocument();
+	});
+
+	it('renders the position card', () => {
+		render(EarningHeader);
+
+		expect(screen.getByText(get(i18n).stake.text.active_earning)).toBeInTheDocument();
+	});
+});
