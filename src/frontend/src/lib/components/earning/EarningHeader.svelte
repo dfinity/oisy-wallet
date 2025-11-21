3cd0fbe8d16d36edb13cbb47a2c3ddcf130e9f96
daviddecentage <david.farina@dfinity.org>
2025-11-21 10:41:44 +0100

    update

--- src/frontend/src/lib/components/earning/EarningHeader.svelte
+++ src/frontend/src/lib/components/earning/EarningHeader.svelte
@@ -3,7 +3,7 @@
 	import EarningPotentialCard from '$lib/components/earning/EarningPotentialCard.svelte';
 	import StakeContentSection from '$lib/components/stake/StakeContentSection.svelte';
 	import ExternalLink from '$lib/components/ui/ExternalLink.svelte';
-	import { OISY_DOCS_URL } from '$lib/constants/oisy.constants.js';
+	import { OISY_DOCS_URL } from '$lib/constants/oisy.constants';
 	import { i18n } from '$lib/stores/i18n.store';
 </script>
 
