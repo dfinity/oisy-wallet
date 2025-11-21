0e8edf2c8f28a147d5cb019000e1ca4fb7797194
daviddecentage <david.farina@dfinity.org>
2025-11-21 17:23:33 +0100

    update

---
+++ src/frontend/src/tests/lib/components/ui/CollapsibleList.spec.ts
@@ -0,0 +1,59 @@
+import CollapsibleList from '$lib/components/ui/CollapsibleList.svelte';
+import { createMockSnippet } from '$tests/mocks/snippet.mock';
+import { assertNonNullish } from '@dfinity/utils';
+import { fireEvent, render } from '@testing-library/svelte';
+
+describe('CollapsibleList', () => {
+	it('should render one item initally', () => {
+		const { queryByTestId } = render(CollapsibleList, {
+			props: {
+				items: [createMockSnippet('item1'), createMockSnippet('item2'), createMockSnippet('item3')]
+			}
+		});
+
+		expect(queryByTestId('item1')).toBeInTheDocument();
+		expect(queryByTestId('item2')).not.toBeInTheDocument();
+		expect(queryByTestId('item3')).not.toBeInTheDocument();
+	});
+
+	it('should render all items when expand button is clicked', async () => {
+		const { queryByTestId, getByRole } = render(CollapsibleList, {
+			props: {
+				items: [createMockSnippet('item1'), createMockSnippet('item2'), createMockSnippet('item3')]
+			}
+		});
+
+		const button = getByRole('button');
+
+		assertNonNullish(button);
+
+		await fireEvent.click(button);
+
+		expect(queryByTestId('item1')).toBeInTheDocument();
+		expect(queryByTestId('item2')).toBeInTheDocument();
+		expect(queryByTestId('item3')).toBeInTheDocument();
+	});
+
+	it('should render collapse the list to one item again if button is clicked again', async () => {
+		const { queryByTestId, getByRole } = render(CollapsibleList, {
+			props: {
+				items: [createMockSnippet('item1'), createMockSnippet('item2'), createMockSnippet('item3')]
+			}
+		});
+
+		const button = getByRole('button');
+
+		assertNonNullish(button);
+
+		await fireEvent.click(button);
+
+		expect(queryByTestId('item1')).toBeInTheDocument();
+		expect(queryByTestId('item2')).toBeInTheDocument();
+		expect(queryByTestId('item3')).toBeInTheDocument();
+
+		await fireEvent.click(button);
+
+		expect(queryByTestId('item2')).not.toBeInTheDocument();
+		expect(queryByTestId('item3')).not.toBeInTheDocument();
+	});
+});
