5c47b4da6099547982ecf5ff84541080db0358d4
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-24 09:27:24 +0100

    feat(frontend): API service to call balance method of EXTv2 token canister (#10472)
    # Motivation
    
    We add the service for the API call of EXT v2 token to get the balance.

--- src/frontend/src/icp/api/ext-v2-token.api.ts
+++ src/frontend/src/icp/api/ext-v2-token.api.ts
@@ -1,7 +1,12 @@
-import type { Transaction } from '$declarations/ext_v2_token/ext_v2_token.did';
+import type {
+	Balance,
+	TokenIdentifier,
+	Transaction
+} from '$declarations/ext_v2_token/ext_v2_token.did';
 import { ExtV2TokenCanister } from '$icp/canisters/ext-v2-token.canister';
+import { ZERO } from '$lib/constants/app.constants';
 import type { CanisterApiFunctionParamsWithCanisterId } from '$lib/types/canister';
-import { assertNonNullish, type QueryParams } from '@dfinity/utils';
+import { assertNonNullish, isNullish, type QueryParams } from '@dfinity/utils';
 import { Principal } from '@icp-sdk/core/principal';
 
 /**
@@ -29,6 +34,41 @@ export const transactions = async ({
 	});
 
 	return await transactions({ certified });
+};
+
+/**
+ * Returns the user's balance for a specific token of the collection.
+ *
+ * @link https://github.com/Toniq-Labs/ext-v2-token/blob/main/API-REFERENCE.md#balance-1
+ *
+ * @param {Object} params - The parameters for fetching the balance.
+ * @param {boolean} [params.certified=true] - Whether the data should be certified.
+ * @param {OptionIdentity} params.identity - The identity to use for the request.
+ * @param {CanisterIdText} params.canisterId - The canister ID of the EXT v2 token.
+ * @param {TokenIdentifier} params.tokenIdentifier - The token identifier as string.
+ * @param {QueryParams} params.rest - Additional query parameters.
+ * @returns {Promise<Balance>} The user's balance for the specified token.
+ */
+export const balance = async ({
+	identity,
+	certified,
+	canisterId,
+	tokenIdentifier,
+	...rest
+}: CanisterApiFunctionParamsWithCanisterId<
+	{ tokenIdentifier: TokenIdentifier } & QueryParams
+>): Promise<Balance> => {
+	if (isNullish(identity)) {
+		return ZERO;
+	}
+
+	const { balance } = await extV2TokenCanister({
+		identity,
+		canisterId,
+		...rest
+	});
+
+	return await balance({ tokenIdentifier, account: identity.getPrincipal(), certified });
 };
 
 const extV2TokenCanister = async ({
