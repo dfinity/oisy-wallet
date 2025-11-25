9d1ed38e90b1c938b95c3a487f70e07dd78f9dfa
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-25 10:12:28 +0100

    revert

--- src/frontend/src/icp/services/ext.services.ts
+++ src/frontend/src/icp/services/ext.services.ts
@@ -52,6 +52,7 @@ const loadCustomTokensWithMetadata = async (
 				version: versionNullable,
 				section: sectionNullable,
 				allow_external_content_source: allowExternalContentSourceNullable
+				// eslint-disable-next-line require-await -- We are going to add an async function to fetch the metadata
 			}) => {
 				const version = fromNullable(versionNullable);
 				const section = fromNullable(sectionNullable);
