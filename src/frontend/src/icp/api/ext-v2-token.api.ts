1bffef93de9bdd2523e12ec2a90b867d662f6393
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-25 11:19:28 +0100

    feat(frontend): API method for EXT canister getTokensByOwner

--- src/frontend/src/icp/api/ext-v2-token.api.ts
+++ src/frontend/src/icp/api/ext-v2-token.api.ts
@@ -1,9 +1,11 @@
 import type {
 	Balance,
 	TokenIdentifier,
+	TokenIndex,
 	Transaction
 } from '$declarations/ext_v2_token/ext_v2_token.did';
 import { ExtV2TokenCanister } from '$icp/canisters/ext-v2-token.canister';
+import { getIcrcAccount } from '$icp/utils/icrc-account.utils';
 import { ZERO } from '$lib/constants/app.constants';
 import type { CanisterApiFunctionParamsWithCanisterId } from '$lib/types/canister';
 import { assertNonNullish, isNullish, type QueryParams } from '@dfinity/utils';
@@ -22,8 +24,8 @@ import { Principal } from '@icp-sdk/core/principal';
  * @returns {Promise<Transaction[]>} The array of all collection transactions, not filtered by caller.
  */
 export const transactions = async ({
-	identity,
 	certified,
+	identity,
 	canisterId,
 	...rest
 }: CanisterApiFunctionParamsWithCanisterId<QueryParams>): Promise<Transaction[]> => {
@@ -50,8 +52,8 @@ export const transactions = async ({
  * @returns {Promise<Balance>} The user's balance for the specified token.
  */
 export const balance = async ({
-	identity,
 	certified,
+	identity,
 	canisterId,
 	tokenIdentifier,
 	...rest
@@ -69,6 +71,44 @@ export const balance = async ({
 	});
 
 	return await balance({ tokenIdentifier, account: identity.getPrincipal(), certified });
+};
+
+/**
+ * Get the list of collection's tokens owned by a specific user.
+ *
+ * The metadata returned with each token is not the metadata that we need.
+ * Please use the method `ext_metadata` instead.
+ *
+ * @link https://github.com/Toniq-Labs/ext-v2-token/blob/main/API-REFERENCE.md#tokens_ext
+ *
+ * @param {Object} params - The parameters for fetching the tokens.
+ * @param {Principal} params.owner - The principal to use for the request.
+ * @param {OptionIdentity} params.identity - The identity to use for the request.
+ * @param {CanisterIdText} params.canisterId - The canister ID of the EXT v2 token.
+ * @param {boolean} [params.certified=true] - Whether the data should be certified.
+ * @returns {Promise<TokenIndex[]>} The list of token indices owned by the user.
+ * @throws CanisterInternalError if the token identifier is invalid.
+ */
+export const getTokensByOwner = async ({
+	identity,
+	owner,
+	certified,
+	canisterId,
+	...rest
+}: CanisterApiFunctionParamsWithCanisterId<{ owner: Principal } & QueryParams>): Promise<
+	TokenIndex[]
+> => {
+	if (isNullish(identity)) {
+		return [];
+	}
+
+	const { getTokensByOwner } = await extV2TokenCanister({
+		identity,
+		canisterId,
+		...rest
+	});
+
+	return await getTokensByOwner({ certified, ...getIcrcAccount(owner) });
 };
 
 const extV2TokenCanister = async ({
