65bd65e624c02ae2b92703073092e6570e529773
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-25 17:43:58 +0100

    feat(frontend): Fetch EXT NFT collections in LoaderCollections

--- src/frontend/src/lib/components/loaders/LoaderCollections.svelte
+++ src/frontend/src/lib/components/loaders/LoaderCollections.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
 	import { isNullish } from '@dfinity/utils';
 	import { get } from 'svelte/store';
+	import type { TokenIndex } from '$declarations/ext_v2_token/ext_v2_token.did';
 	import { NFTS_ENABLED } from '$env/nft.env';
+	import { EXT_BUILTIN_TOKENS } from '$env/tokens/tokens-ext/tokens.ext.env';
 	import { enabledEthereumNetworks } from '$eth/derived/networks.derived';
 	import { alchemyProviders } from '$eth/providers/alchemy.providers';
 	import { saveErcCustomTokens } from '$eth/services/erc-custom-tokens.services';
 	import type { EthereumNetwork } from '$eth/types/network';
 	import { enabledEvmNetworks } from '$evm/derived/networks.derived';
+	import { getTokensByOwner } from '$icp/api/ext-v2-token.api';
 	import { listCustomTokens } from '$lib/api/backend.api';
 	import IntervalLoader from '$lib/components/core/IntervalLoader.svelte';
 	import { COLLECTION_TIMER_INTERVAL_MILLIS } from '$lib/constants/app.constants';
 	import { ethAddress } from '$lib/derived/address.derived';
 	import { authIdentity } from '$lib/derived/auth.derived';
 	import { i18n } from '$lib/stores/i18n.store';
+	import type { CanisterIdText } from '$lib/types/canister';
 	import type { OwnedContract } from '$lib/types/nft';
 
 	const loadContracts = async (network: EthereumNetwork): Promise<OwnedContract[]> => {
@@ -53,12 +57,48 @@
 		}
 	};
 
+	const loadExtTokens = async () => {
+		if (isNullish($authIdentity)) {
+			return;
+		}
+
+		const tokens = await EXT_BUILTIN_TOKENS.reduce<Promise<Record<CanisterIdText, TokenIndex[]>>>(
+			async (acc, { canisterId }) => {
+				if (isNullish($authIdentity)) {
+					return await acc;
+				}
+
+				try {
+					const tokens = await getTokensByOwner({
+						identity: $authIdentity,
+						owner: $authIdentity.getPrincipal(),
+						canisterId
+					});
+
+					return tokens.length > 0 ? {...(await acc), [canisterId]: tokens} : await acc;
+				} catch (error: unknown) {
+					console.warn(`Error fetching tokens from canister ${canisterId}:`, error);
+
+					return await acc;
+				}
+			},
+			Promise.resolve({})
+		);
+
+		console.log('Loaded tokens from external canisters:', tokens);
+
+		// await saveExtCustomTokens({
+		// 	identity: $authIdentity,
+		// 	tokens: tokens as NonEmptyArray<SaveExtCustomToken>
+		// });
+	};
+
 	const onLoad = async () => {
 		if (!NFTS_ENABLED) {
 			return;
 		}
 
-		await Promise.all([loadErcTokens()]);
+		await Promise.all([loadErcTokens(), loadExtTokens()]);
 	};
 </script>
 
