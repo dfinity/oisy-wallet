bd59cf29abb7060ff2283eed763b731d2256b6cc
daviddecentage <david.farina@dfinity.org>
2025-11-25 15:31:56 +0100

    update

---
+++ src/frontend/src/icp/components/stake/gldt/GldtInfoBox.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import StakeContentSection from '$lib/components/stake/StakeContentSection.svelte';
+</script>
+
+<StakeContentSection>
+	{#snippet title()}
+		test
+	{/snippet}
+	{#snippet content()}
+		test
+	{/snippet}
+</StakeContentSection>
