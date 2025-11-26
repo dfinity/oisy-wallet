603b4f4021416c9bb9f2fd2c0eb8defc2e2da46f
daviddecentage <david.farina@dfinity.org>
2025-11-26 13:00:47 +0100

    update

---
+++ src/frontend/src/lib/components/ui/FactBox.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { nonNullish } from '@dfinity/utils';
+
+	interface Props {
+		icon?: Snippet;
+		title?: Snippet;
+		description?: Snippet;
+	}
+
+	const { icon, title, description }: Props = $props();
+</script>
+
+<div class="flex w-full flex-col items-center">
+	{@render icon?.()}
+	{#if nonNullish(title)}
+		<span class="my-3 text-lg font-bold">
+			{@render title()}
+		</span>
+	{/if}
+	{#if nonNullish(description)}
+		<p class="text-tertiary">{@render description()}</p>
+	{/if}
+</div>
