84c46355bf55c3ca189f7c0e5d817832bd43ef85
daviddecentage <david.farina@dfinity.org>
2025-11-26 11:33:35 +0100

    update

---
+++ src/frontend/src/tests/icp/components/stake/gldt/GldtInfoBox.spec.ts
@@ -0,0 +1,60 @@
+import GldtInfoBox from '$icp/components/stake/gldt/GldtInfoBox.svelte';
+import { render, screen } from '@testing-library/svelte';
+import { get, writable } from 'svelte/store';
+
+import { GLDT_STAKE_CONTEXT_KEY } from '$icp/stores/gldt-stake.store';
+import { i18n } from '$lib/stores/i18n.store';
+import { formatStakeApyNumber } from '$lib/utils/format.utils';
+import { replacePlaceholders } from '$lib/utils/i18n.utils';
+
+const renderWithStakeContext = (apy?: number) => {
+	const mockStore = writable({
+		apy,
+		position: { staked: 10n }
+	});
+
+	const mockContext = new Map([
+		[GLDT_STAKE_CONTEXT_KEY, { store: { subscribe: mockStore.subscribe } }]
+	]);
+
+	return render(GldtInfoBox, { context: mockContext });
+};
+
+describe('GldtInfoBox', () => {
+	it('renders info title and description', () => {
+		renderWithStakeContext(5);
+
+		expect(screen.getByText(get(i18n).stake.info.gldt.title)).toBeInTheDocument();
+
+		expect(screen.getByText(get(i18n).stake.info.gldt.description)).toBeInTheDocument();
+	});
+
+	it('renders fact titles and descriptions', () => {
+		renderWithStakeContext(5);
+
+		expect(screen.getByText(get(i18n).stake.info.gldt.fact_1_title)).toBeInTheDocument();
+
+		expect(screen.getByText(get(i18n).stake.info.gldt.fact_2_title)).toBeInTheDocument();
+
+		expect(screen.getByText(get(i18n).stake.info.gldt.fact_3_title)).toBeInTheDocument();
+	});
+
+	it('renders APY replaced text when APY exists', () => {
+		const apy = 7;
+		renderWithStakeContext(apy);
+
+		const expectedText = replacePlaceholders(get(i18n).stake.info.gldt.fact_3_description, {
+			$apy: `${formatStakeApyNumber(apy)}`
+		});
+
+		expect(screen.getByText(expectedText)).toBeInTheDocument();
+	});
+
+	it('renders fallback description when APY is missing', () => {
+		renderWithStakeContext(undefined);
+
+		expect(
+			screen.getByText(get(i18n).stake.info.gldt.fact_3_description_fallback)
+		).toBeInTheDocument();
+	});
+});
