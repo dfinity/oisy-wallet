7d7d5e59979ac8e764711d000da6e402e6665d14
David Dal Busco <david.dalbusco@outlook.com>
2025-11-26 12:10:04 +0100

    chore(frontend): update lint dependencies and disable rules
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -35,7 +35,9 @@ export default [
 	// TODO: re-enable this rule when we fix all the warnings that it causes.
 	{
 		rules: {
-			'svelte/no-navigation-without-resolve': 'off'
+			'svelte/no-navigation-without-resolve': 'off',
+			'vitest/no-conditional-expect': 'off',
+			'vitest/no-disabled-tests': 'off'
 		}
 	},
 
