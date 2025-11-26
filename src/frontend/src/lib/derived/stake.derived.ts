81cbf5eb683a85c2091d4eecc33cbc00cedce415
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-26 14:36:03 +0100

    more

--- src/frontend/src/lib/derived/stake.derived.ts
+++ src/frontend/src/lib/derived/stake.derived.ts
@@ -1,17 +1,17 @@
 import { isGLDTToken } from '$icp-eth/utils/token.utils';
 import { gldtStakeStore } from '$icp/stores/gldt-stake.store';
 import { ZERO } from '$lib/constants/app.constants';
-import { enabledFungibleTokensUi } from '$lib/derived/tokens.derived';
+import { enabledTokens } from '$lib/derived/tokens.derived';
 import type { StakeBalances } from '$lib/types/stake-balance';
 import { nonNullish } from '@dfinity/utils';
 import { derived, type Readable } from 'svelte/store';
 
 export const stakeBalances: Readable<StakeBalances> = derived(
-	[enabledFungibleTokensUi, gldtStakeStore],
-	([$enabledFungibleTokensUi, $gldtStakeStore]) => {
+	[enabledTokens, gldtStakeStore],
+	([$enabledTokens, $gldtStakeStore]) => {
 		const balances = {} as StakeBalances;
 
-		const gldtToken = $enabledFungibleTokensUi.find(isGLDTToken);
+		const gldtToken = $enabledTokens.find(isGLDTToken);
 
 		if (nonNullish(gldtToken)) {
 			const claimable = $gldtStakeStore?.position?.claimable_rewards?.find(([tokenSymbol]) =>
