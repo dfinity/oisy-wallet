a162d4774c5caa58c5ba4fea90b1f31215ecd261
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-26 12:49:22 +0100

    revert

--- src/frontend/src/env/earning.ts
+++ src/frontend/src/env/earning.ts
@@ -1,2 +1,4 @@
+import { parseBoolEnvVar } from '$lib/utils/env.utils';
+
 // TODO: remove once the feature has been completed
-export const EARNING_ENABLED = true;
+export const EARNING_ENABLED = parseBoolEnvVar(import.meta.env.VITE_EARNING_ENABLED);
