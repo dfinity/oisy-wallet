9d49c9b143061187d91ee15923e66fdaf5c38dc8
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-27 17:35:49 +0100

    feat(frontend): Add method to load EXT NFTs in `LoaderNfts` (#10536)
    # Motivation
    
    We add the service to load EXT NFTs in the component `LoaderNfts`.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/lib/components/loaders/LoaderNfts.svelte
+++ src/frontend/src/lib/components/loaders/LoaderNfts.svelte
@@ -4,14 +4,17 @@
 	import { NFTS_ENABLED } from '$env/nft.env';
 	import { loadNftsByNetwork } from '$eth/services/nft.services';
 	import type { EthNonFungibleToken } from '$eth/types/nft';
+	import { loadNfts as loadExtNfts } from '$icp/services/nft.services';
+	import type { IcNonFungibleToken } from '$icp/types/nft';
 	import IntervalLoader from '$lib/components/core/IntervalLoader.svelte';
 	import { NFT_TIMER_INTERVAL_MILLIS } from '$lib/constants/app.constants';
 	import { ethAddress } from '$lib/derived/address.derived';
+	import { authIdentity } from '$lib/derived/auth.derived';
 	import { enabledNonFungibleTokens } from '$lib/derived/tokens.derived';
 	import { nftStore } from '$lib/stores/nft.store';
 	import type { NetworkId } from '$lib/types/network';
 	import type { Nft, NonFungibleToken } from '$lib/types/nft';
-	import { isNetworkIdEthereum, isNetworkIdEvm } from '$lib/utils/network.utils';
+	import { isNetworkIdEthereum, isNetworkIdEvm, isNetworkIdICP } from '$lib/utils/network.utils';
 	import { getTokensByNetwork } from '$lib/utils/nft.utils';
 
 	const loadNfts = async ({
@@ -27,6 +30,14 @@
 				// For now, it is acceptable to cast it since we checked before if the network is Ethereum or EVM.
 				tokens: tokens as EthNonFungibleToken[],
 				walletAddress: $ethAddress
+			});
+		}
+
+		if (isNetworkIdICP(networkId)) {
+			return await loadExtNfts({
+				// For now, it is acceptable to cast it since we checked before if the network is ICP.
+				tokens: tokens as IcNonFungibleToken[],
+				identity: $authIdentity
 			});
 		}
 
