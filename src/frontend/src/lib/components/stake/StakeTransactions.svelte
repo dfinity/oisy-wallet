8a0e64393d32c96a49d8f2f9ad5d7afeac952146
daviddecentage <david.farina@dfinity.org>
2025-11-27 12:16:12 +0100

    update

--- src/frontend/src/lib/components/stake/StakeTransactions.svelte
+++ src/frontend/src/lib/components/stake/StakeTransactions.svelte
@@ -15,10 +15,32 @@
 
 	const { transactions }: Props = $props();
 
-	const getTimestamp = (timestampNanoseconds?: bigint) =>
+	const getTimestamp = (timestampNanoseconds?: bigint | number) =>
 		nonNullish(timestampNanoseconds)
-			? Number(timestampNanoseconds / NANO_SECONDS_IN_SECOND)
+			? Number(BigInt(timestampNanoseconds) / NANO_SECONDS_IN_SECOND)
 			: undefined;
+
+	const getTo = (transaction: StakingTransactionsUiWithToken): string | undefined => {
+		return nonNullish(transaction.to) && transaction.to.length > 0
+			? (transaction.to[0] as string)
+			: nonNullish(transaction.to) && transaction.to.length === 0
+				? (transaction.to as string)
+				: undefined;
+	};
+
+	const getStatus = (transaction: StakingTransactionsUiWithToken) => {
+		if ('status' in transaction && nonNullish(transaction.status)) {
+			return transaction.status === 'confirmed' ||
+				transaction.status === 'executed' ||
+				transaction.status === 'finalized' ||
+				transaction.status === 'processed'
+				? 'confirmed'
+				: transaction.status === 'pending'
+					? 'pending'
+					: 'unconfirmed';
+		}
+		return 'unconfirmed';
+	};
 </script>
 
 <StakeContentSection>
@@ -27,30 +49,26 @@
 	{/snippet}
 	{#snippet content()}
 		{#each transactions as transaction}
-			{#if 'status' in transaction && transaction.status === 'confirmed'}
-				<Transaction
-					type={transaction.type}
-					token={transaction.token}
-					status={'confirmed'}
-					iconType="token"
-					timestamp={getTimestamp(transaction.timestamp)}
-					from={transaction.from}
-					to={(nonNullish(transaction.to) && transaction.to.length > 0
-						? transaction.to[0]
-						: transaction.to) as string | undefined}
-					displayAmount={transaction.value}
-				>
-					{#if 'incoming' in transaction && transaction?.incoming}
-						{#if transaction.isReward}
-							Reward claimed
-						{:else}
-							Unstaked
-						{/if}
+			<Transaction
+				type={transaction.type}
+				token={transaction.token}
+				status={getStatus(transaction)}
+				iconType="token"
+				timestamp={getTimestamp(transaction.timestamp)}
+				from={transaction.from}
+				to={getTo(transaction)}
+				displayAmount={transaction.value}
+			>
+				{#if 'incoming' in transaction && transaction?.incoming}
+					{#if transaction.isReward}
+						Reward claimed
 					{:else}
-						Staked
+						Unstaked
 					{/if}
-				</Transaction>
-			{/if}
+				{:else}
+					Staked
+				{/if}
+			</Transaction>
 		{/each}
 	{/snippet}
 </StakeContentSection>
