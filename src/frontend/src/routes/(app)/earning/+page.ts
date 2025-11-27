f5022a0dbb7a0b74abf673816120f6fe2cb4e190
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-27 16:34:06 +0100

    more

--- src/frontend/src/routes/(app)/earning/+page.ts
+++ src/frontend/src/routes/(app)/earning/+page.ts
@@ -1,6 +1,6 @@
 import { loadRouteParams, type RouteParams } from '$lib/utils/nav.utils';
 import type { LoadEvent } from '@sveltejs/kit';
 // eslint-disable-next-line local-rules/no-relative-imports
-import type { PageLoad } from '../../../../../../.svelte-kit/types/src/frontend';
+import type { PageLoad } from './$types';
 
 export const load: PageLoad = ($event: LoadEvent): RouteParams => loadRouteParams($event);
