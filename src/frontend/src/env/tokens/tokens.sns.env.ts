9308eac7c571f99a723e4321d5a82b0d1ad7f367
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-28 15:46:37 +0100

    Update src/frontend/src/env/tokens/tokens.sns.env.ts

--- src/frontend/src/env/tokens/tokens.sns.env.ts
+++ src/frontend/src/env/tokens/tokens.sns.env.ts
@@ -13,3 +13,10 @@ export const SNS_BUILTIN_TOKENS: IcTokenWithoutIdExtended[] = buildIcrcCustomTok
 
 export const SNS_BUILTIN_TOKENS_INDEXED: Record<LedgerCanisterIdText, IcTokenWithoutIdExtended> =
 	buildIndexedIcTokens(SNS_BUILTIN_TOKENS);
+	
+	
+	
+
+export const GOLDAO_LEDGER_CANISTER_ID: LedgerCanisterIdText =
+	SNS_BUILTIN_TOKENS.find((t) => t.symbol === 'GOLDAO')?.ledgerCanisterId ??
+	'tyyy3-4aaaa-aaaaq-aab7a-cai';
