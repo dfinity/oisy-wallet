258c1d023124fbe09c968012668854f4cbd3b6a5
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-11-28 09:21:08 +0100

    feat(frontend): More precise checks for `isVCHFToken` and `isVEURToken` (#10606)
    # Motivation
    
    When trying to compare an ICRC token, it is safer to always compare the
    Ledger canister ID too.

--- src/frontend/src/icp-eth/utils/token.utils.ts
+++ src/frontend/src/icp-eth/utils/token.utils.ts
@@ -1,4 +1,8 @@
-import { GLDT_LEDGER_CANISTER_ID } from '$env/networks/networks.icrc.env';
+import {
+	GLDT_LEDGER_CANISTER_ID,
+	VCHF_LEDGER_CANISTER_ID,
+	VEUR_LEDGER_CANISTER_ID
+} from '$env/networks/networks.icrc.env';
 import { isTokenIcrc } from '$icp/utils/icrc.utils';
 import type { Token } from '$lib/types/token';
 
@@ -7,6 +11,12 @@ export const isGLDTToken = (token: Token): boolean =>
 	token.symbol === 'GLDT' &&
 	token.ledgerCanisterId === GLDT_LEDGER_CANISTER_ID;
 
-export const isVCHFToken = (token: Token): boolean => isTokenIcrc(token) && token.symbol === 'VCHF';
+export const isVCHFToken = (token: Token): boolean =>
+	isTokenIcrc(token) &&
+	token.symbol === 'VCHF' &&
+	token.ledgerCanisterId === VCHF_LEDGER_CANISTER_ID;
 
-export const isVEURToken = (token: Token): boolean => isTokenIcrc(token) && token.symbol === 'VEUR';
+export const isVEURToken = (token: Token): boolean =>
+	isTokenIcrc(token) &&
+	token.symbol === 'VEUR' &&
+	token.ledgerCanisterId === VEUR_LEDGER_CANISTER_ID;
