b5b4c1c59a4d927eacbf2a1c1337d83829523776
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-12-01 11:06:38 +0100

    more

--- src/frontend/src/eth/services/nft-send.services.ts
+++ src/frontend/src/eth/services/nft-send.services.ts
@@ -1,10 +1,7 @@
 import { transferErc1155, transferErc721 } from '$eth/services/nft-transfer.services';
 import { isTokenErc1155 } from '$eth/utils/erc1155.utils';
 import { isTokenErc721 } from '$eth/utils/erc721.utils';
-import type { ProgressStepsSend } from '$lib/enums/progress-steps';
-import type { OptionIdentity } from '$lib/types/identity';
-import type { NftId, NonFungibleToken } from '$lib/types/nft';
-import type { TransferParams } from '$lib/types/send';
+import type { SendNftCommonParams, TransferParams } from '$lib/types/send';
 import { isNetworkIdEthereum, isNetworkIdEvm } from '$lib/utils/network.utils';
 import { isNullish } from '@dfinity/utils';
 
@@ -18,14 +15,10 @@ export const sendNft = async ({
 	maxFeePerGas,
 	maxPriorityFeePerGas,
 	progress
-}: {
-	token: NonFungibleToken;
-	tokenId: NftId;
-	identity: OptionIdentity;
-	progress?: (step: ProgressStepsSend) => void;
-} & (Pick<TransferParams, 'from' | 'to' | 'maxFeePerGas' | 'maxPriorityFeePerGas'> & {
-	gas: bigint;
-})) => {
+}: SendNftCommonParams &
+	Pick<TransferParams, 'from' | 'to' | 'maxFeePerGas' | 'maxPriorityFeePerGas'> & {
+		gas: bigint;
+	}) => {
 	if (isNullish(identity)) {
 		return;
 	}
