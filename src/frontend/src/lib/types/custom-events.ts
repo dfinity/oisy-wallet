f8406240fed5cac61fbf07892be00cad1d78b06f
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-12-02 09:26:11 +0100

    feat(frontend): Define types for custom events (#10645)
    # Motivation
    
    Overdue TODO, we decide to be type-safe in the custom events.

--- src/frontend/src/lib/types/custom-events.ts
+++ src/frontend/src/lib/types/custom-events.ts
@@ -1,3 +1,11 @@
+import type { SyncState } from '$lib/types/sync';
+
+export interface NoParamEvent {}
+
+export type OisySyncStatusEvent = SyncState;
+
+export type OisyIndexCanisterBalanceOutOfSyncEvent = boolean;
+
 export interface OisyReloadCollectionsEvent {
 	callback?: () => void;
 }
