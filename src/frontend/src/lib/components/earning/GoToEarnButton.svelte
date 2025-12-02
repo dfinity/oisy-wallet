be5aaaa0175b134f9dcaa45f62437e4e75625845
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-12-02 14:27:56 +0100

    review

--- src/frontend/src/lib/components/earning/GoToEarnButton.svelte
+++ src/frontend/src/lib/components/earning/GoToEarnButton.svelte
@@ -2,6 +2,7 @@
 	import type { NavigationTarget } from '@sveltejs/kit';
 	import { afterNavigate, goto } from '$app/navigation';
 	import IconArrowRight from '$lib/components/icons/IconArrowRight.svelte';
+	import Button from '$lib/components/ui/Button.svelte';
 	import { AppPath } from '$lib/constants/routes.constants';
 	import { EARNING_GOTO_BUTTON } from '$lib/constants/test-ids.constants';
 	import { networkId } from '$lib/derived/network.derived';
@@ -24,11 +25,13 @@
 	);
 </script>
 
-<button
-	class="success gap-1 px-12 font-semibold"
-	data-tid={EARNING_GOTO_BUTTON}
+<Button
+	colorStyle="success"
 	onclick={() => goto(path)}
+	styleClass="gap-1 flex-none px-12 font-semibold"
+	testId={EARNING_GOTO_BUTTON}
+	type="button"
 >
 	{$i18n.earning.text.go_to_earn}
 	<IconArrowRight />
-</button>
+</Button>
