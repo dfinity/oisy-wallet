0598e5d218477e70b6bb38fcc5811ad3bd074a04
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-12-03 16:41:46 +0100

    more

--- src/frontend/src/icp/components/receive/IcReceiveIcp.svelte
+++ src/frontend/src/icp/components/receive/IcReceiveIcp.svelte
@@ -1,19 +1,27 @@
 <script lang="ts">
 	import { getContext } from 'svelte';
+	import { page } from '$app/state';
 	import IcReceiveInfoIcp from '$icp/components/receive/IcReceiveInfoIcp.svelte';
 	import {
 		RECEIVE_TOKEN_CONTEXT_KEY,
 		type ReceiveTokenContext
 	} from '$icp/stores/receive-token.store';
+	import { isTokenIcp } from '$icp/utils/icrc.utils';
 	import ReceiveAddressModal from '$lib/components/receive/ReceiveAddressModal.svelte';
 	import ReceiveButtonWithModal from '$lib/components/receive/ReceiveButtonWithModal.svelte';
 	import { modalIcpReceive } from '$lib/derived/modal.derived';
 	import { modalStore } from '$lib/stores/modal.store';
+	import {isRouteNfts, isRouteTokens} from '$lib/utils/nav.utils';
 
-	const { open, close } = getContext<ReceiveTokenContext>(RECEIVE_TOKEN_CONTEXT_KEY);
+	const { tokenStandard, open, close } = getContext<ReceiveTokenContext>(RECEIVE_TOKEN_CONTEXT_KEY);
 
 	const openReceive = (modalId: symbol) => {
-		modalStore.openIcpReceive(modalId);
+		if (isTokenIcp({ standard: $tokenStandard }) || isRouteTokens(page) || isRouteNfts(page)) {
+			modalStore.openIcpReceive(modalId);
+			return;
+		}
+
+		modalStore.openEthReceive(modalId);
 	};
 
 	const openModal = async (modalId: symbol) => await open(async () => await openReceive(modalId));
