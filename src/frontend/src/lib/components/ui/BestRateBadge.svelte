f9365b9c1f237da5ac0dd513c5e62fa6f2efbc53
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-12-04 14:52:05 +0100

    feat(frontend): added new NestRateBadge component

---
+++ src/frontend/src/lib/components/ui/BestRateBadge.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import IconStar from '$lib/components/icons/IconStar.svelte';
+	import Badge from '$lib/components/ui/Badge.svelte';
+	import Responsive from '$lib/components/ui/Responsive.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
+</script>
+
+<Badge styleClass="mt-1" variant="success" width="w-fit">
+	<span class="flex items-center gap-1">
+		<IconStar />
+		<Responsive up="md">{$i18n.swap.text.best_rate}</Responsive>
+	</span>
+</Badge>
