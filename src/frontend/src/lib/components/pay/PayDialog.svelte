327f1e2d585cc4fdf2140ee41e9850a45a4264af
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-12-04 14:38:15 +0100

    tests

--- src/frontend/src/lib/components/pay/PayDialog.svelte
+++ src/frontend/src/lib/components/pay/PayDialog.svelte
@@ -8,6 +8,7 @@
 	import ContentWithToolbar from '$lib/components/ui/ContentWithToolbar.svelte';
 	import ImgBanner from '$lib/components/ui/ImgBanner.svelte';
 	import Responsive from '$lib/components/ui/Responsive.svelte';
+	import {PAY_DIALOG, PAY_DIALOG_BANNER, PAY_DIALOG_PAY_BUTTON} from '$lib/constants/test-ids.constants';
 	import { modalPayDialogOpen } from '$lib/derived/modal.derived';
 	import { i18n } from '$lib/stores/i18n.store';
 	import { modalStore } from '$lib/stores/modal.store';
@@ -35,11 +36,12 @@
 		alt={$i18n.pay.text.dialog_title}
 		src={CoverPayDialog}
 		styleClass="max-h-56 rounded-lg"
+		testId={PAY_DIALOG_BANNER}
 	/>
 {/snippet}
 
 {#snippet footer()}
-	<Button fullWidth onclick={openScanner}>
+	<Button fullWidth onclick={openScanner} testId={PAY_DIALOG_PAY_BUTTON}>
 		<div class="flex flex-row items-center gap-2">
 			<IconScanLine />
 			{replaceOisyPlaceholders($i18n.pay.text.dialog_button)}
@@ -48,7 +50,7 @@
 {/snippet}
 
 <Responsive up="md">
-	<Modal onClose={close}>
+	<Modal onClose={close} testId={PAY_DIALOG}>
 		{#snippet title()}
 			<div class="flex items-center gap-3 font-bold text-primary">
 				{replaceOisyPlaceholders($i18n.pay.text.dialog_title)}
@@ -66,7 +68,7 @@
 </Responsive>
 
 <Responsive down="sm">
-	<BottomSheet {footer} onClose={close} bind:visible={open}>
+	<BottomSheet {footer} onClose={close} testId={PAY_DIALOG} bind:visible={open}>
 		{#snippet content()}
 			{@render banner()}
 
