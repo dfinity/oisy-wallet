c23d0e8f6d8f4e669e9d23cf764ea5c07a60e869
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-12-04 14:04:08 +0100

    Update src/frontend/src/lib/components/scanner/EmptyTokenList.svelte
    Co-authored-by: Antonio Ventilii <antonio.ventilii@dfinity.org>

--- src/frontend/src/lib/components/scanner/EmptyTokenList.svelte
+++ src/frontend/src/lib/components/scanner/EmptyTokenList.svelte
@@ -7,9 +7,12 @@
 
 <div class="mb-12 flex flex-col items-center justify-center gap-4 text-center">
 	<IconOisyMate />
+	
 	<h3>{$i18n.scanner.text.no_supported_tokens}</h3>
-	<p>{$i18n.scanner.text.supported_tokens} </p>
-	<Button colorStyle="secondary-light" onclick={() => goto('/')}
-		>{$i18n.scanner.text.go_to_assets}</Button
-	>
+	
+	<p>{$i18n.scanner.text.supported_tokens}</p>
+	
+	<Button colorStyle="secondary-light" onclick={() => goto(AppPath.Tokens)}>
+	{$i18n.scanner.text.go_to_assets}
+	</Button>
 </div>
