13c8297e727db756619349724e6f0be0281193d8
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-12-05 13:28:00 +0100

    feat(frontend): payment modal with tokenlist steps

--- src/frontend/src/lib/components/scanner/ScannerWizard.svelte
+++ src/frontend/src/lib/components/scanner/ScannerWizard.svelte
@@ -12,6 +12,10 @@
 		PAY_CONTEXT_KEY,
 		type PayContext
 	} from '$lib/stores/open-crypto-pay.store';
+	import { isNullish } from '@dfinity/utils';
+	import { goToWizardStep } from '$lib/utils/wizard-modal.utils';
+	import TokensList from '$lib/components/scanner/open-crypto-pay/OpenCryptoPayTokensList.svelte';
+	import BottomSheetTokensList from '$lib/components/scanner/open-crypto-pay/BottomSheetTokensList.svelte';
 
 	let steps = $derived<WizardSteps<WizardStepsScanner>>(scannerWizardSteps({ i18n: $i18n }));
 
@@ -23,10 +27,28 @@
 
 	setContext<PayContext>(PAY_CONTEXT_KEY, initPayContext());
 
+	const goToStep = (stepName: WizardStepsScanner) => {
+		if (isNullish(modal)) {
+			return;
+		}
+
+		goToWizardStep({
+			modal,
+			steps,
+			stepName
+		});
+	};
+
 	let isTokenSelecting = $state<boolean>(false);
 </script>
 
-<WizardModal bind:this={modal} {onClose} {steps} bind:currentStep>
+<WizardModal
+	bind:this={modal}
+	{onClose}
+	{steps}
+	bind:currentStep
+	disablePointerEvents={currentStep?.name === WizardStepsScanner.TOKENS_LIST}
+>
 	{#snippet title()}
 		{currentStep?.title}
 	{/snippet}
@@ -35,7 +57,21 @@
 		{#if currentStep?.name === WizardStepsScanner.SCAN}
 			<ScannerCode onNext={() => modal?.next()} />
 		{:else if currentStep?.name === WizardStepsScanner.PAY}
-			<OpenCryptoPay onSelectToken={() => {}} bind:isTokenSelecting />
+			<OpenCryptoPay
+				onSelectToken={() => goToStep(WizardStepsScanner.TOKENS_LIST)}
+				bind:isTokenSelecting
+			/>
+		{:else if currentStep?.name === WizardStepsScanner.TOKENS_LIST && !isTokenSelecting}
+			<TokensList onClose={() => goToStep(WizardStepsScanner.PAY)} />
 		{/if}
 	{/key}
 </WizardModal>
+
+{#if isTokenSelecting}
+	<BottomSheetTokensList
+		onClose={() => {
+			goToStep(WizardStepsScanner.PAY);
+		}}
+		bind:visible={isTokenSelecting}
+	/>
+{/if}
