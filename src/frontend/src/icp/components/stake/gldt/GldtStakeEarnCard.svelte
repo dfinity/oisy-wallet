6a4a26c3f26c0e4ceb1312a0f19bddf54eb4d005
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2025-12-05 17:17:45 +0100

    refactor(frontend): re-use EarningYearlyAmount component

--- src/frontend/src/icp/components/stake/gldt/GldtStakeEarnCard.svelte
+++ src/frontend/src/icp/components/stake/gldt/GldtStakeEarnCard.svelte
@@ -9,6 +9,7 @@
 	import type { IcToken } from '$icp/types/ic-token';
 	import { setCustomToken } from '$icp-eth/services/custom-token.services';
 	import { isGLDTToken } from '$icp-eth/utils/token.utils';
+	import EarningYearlyAmount from '$lib/components/earning/EarningYearlyAmount.svelte';
 	import GetTokenModal from '$lib/components/get-token/GetTokenModal.svelte';
 	import StakeContentCard from '$lib/components/stake/StakeContentCard.svelte';
 	import StakeModal from '$lib/components/stake/StakeModal.svelte';
@@ -85,23 +86,12 @@
 	{#snippet content()}
 		<div class="text-sm">{$i18n.stake.text.earning_potential}</div>
 
-		<div
-			class="my-1 text-lg font-bold whitespace-nowrap sm:text-xl"
-			class:text-brand-primary-alt={$enabledMainnetFungibleTokensUsdBalance > 0}
-			class:text-tertiary={$enabledMainnetFungibleTokensUsdBalance === 0}
-		>
-			{`${$enabledMainnetFungibleTokensUsdBalance > 0 && currentApy > 0 ? '+ ' : ''}`}{replacePlaceholders(
-				$i18n.stake.text.active_earning_per_year,
-				{
-					$amount: `${formatCurrency({
-						value: ($enabledMainnetFungibleTokensUsdBalance * currentApy) / 100,
-						currency: $currentCurrency,
-						exchangeRate: $currencyExchangeStore,
-						language: $currentLanguage
-					})}`
-				}
-			)}
-		</div>
+		<EarningYearlyAmount
+			showAsNeutral
+			showPlusSign={$enabledMainnetFungibleTokensUsdBalance > 0 && currentApy > 0}
+			styleClass="my-1 text-lg font-bold sm:text-xl"
+			value={($enabledMainnetFungibleTokensUsdBalance * currentApy) / 100}
+		/>
 
 		{#if $icrcCustomTokensNotInitialized || nonNullish(gldtToken)}
 			<div class="flex items-center justify-center gap-2 text-sm sm:text-base">
