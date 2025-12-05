f835ac15f42500cdf228121efeb43538b463b544
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-12-05 15:34:37 +0100

    more

--- src/frontend/src/env/tokens/tokens.ic.env.ts
+++ src/frontend/src/env/tokens/tokens.ic.env.ts
@@ -1,12 +1,11 @@
 import { DIP20_BUILTIN_TOKENS } from '$env/tokens/tokens.dip20.env';
 import { SNS_BUILTIN_TOKENS } from '$env/tokens/tokens.sns.env';
-import type { IcTokenExtended } from '$icp/types/icrc-custom-token';
+import type { IcToken } from '$icp/types/ic-token';
 import { parseTokenId } from '$lib/validation/token.validation';
 
-export const IC_BUILTIN_TOKENS: IcTokenExtended[] = [
-	...SNS_BUILTIN_TOKENS,
-	...DIP20_BUILTIN_TOKENS
-].map((token) => ({
-	...token,
-	id: parseTokenId(token.symbol)
-}));
+export const IC_BUILTIN_TOKENS: IcToken[] = [...SNS_BUILTIN_TOKENS, ...DIP20_BUILTIN_TOKENS].map(
+	(token) => ({
+		...token,
+		id: parseTokenId(token.symbol)
+	})
+);
