ddaff00e4a4bac6e4aa58c7c01b9fc754587176c
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-12-11 16:09:11 +0100

    feat(frontend): added paymentSucceeded component

---
+++ src/frontend/src/lib/components/scanner/open-crypto-pay/PaymentSucceeded.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/Button.svelte';
+	import ContentWithToolbar from '$lib/components/ui/ContentWithToolbar.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
+	import PaymentStatusHero from './PaymentStatusHero.svelte';
+
+	export let onClose: () => void;
+</script>
+
+<ContentWithToolbar styleClass="flex flex-col w-full">
+	<PaymentStatusHero status="success" />
+	{#snippet toolbar()}
+		<Button fullWidth onclick={onClose}>{$i18n.core.text.done}</Button>
+	{/snippet}
+</ContentWithToolbar>
