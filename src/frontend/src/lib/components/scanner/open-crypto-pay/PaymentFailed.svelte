4134c1afb46598d77c4ddaa8af0ad0f6b29d3d76
roman-nazaruk <roman.nazaruk@dfinity.org>
2025-12-11 16:05:58 +0100

    feat(frontend): added paymentFailed component

---
+++ src/frontend/src/lib/components/scanner/open-crypto-pay/PaymentFailed.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/Button.svelte';
+	import ContentWithToolbar from '$lib/components/ui/ContentWithToolbar.svelte';
+	import PaymentStatusHero from '$lib/components/scanner/open-crypto-pay/PaymentStatusHero.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
+
+	export let onClose: () => void;
+</script>
+
+<ContentWithToolbar styleClass="flex flex-col w-full">
+	<PaymentStatusHero status="failure" />
+	{#snippet toolbar()}
+		<Button fullWidth onclick={onClose}>{$i18n.scanner.text.try_again}</Button>
+	{/snippet}
+</ContentWithToolbar>
