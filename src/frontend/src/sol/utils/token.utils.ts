dfce2f54b592dfa459005c0c95851393b8535d90
Antonio Ventilii <antonio.ventilii@dfinity.org>
2025-12-16 05:05:44 -0300

    feat(frontend): Util to check standard of Solana native tokens (#10899)
    # Motivation
    
    It is useful to have a method to check if a token is native Solana.

--- src/frontend/src/sol/utils/token.utils.ts
+++ src/frontend/src/sol/utils/token.utils.ts
@@ -1,8 +1,11 @@
 import { TRUMP_SYMBOL } from '$env/tokens/tokens-spl/tokens.trump.env';
 import type { Token } from '$lib/types/token';
+import { isTokenSpl } from '$sol/utils/spl.utils';
+
+export const isTokenSolanaNative = (token: Token): boolean => token.standard === 'solana';
 
 export const isSolanaToken = (token: Token): boolean =>
-	token.standard === 'solana' || token.standard === 'spl';
+	isTokenSolanaNative(token) || isTokenSpl(token);
 
 export const isTrumpToken = (token: Token): boolean =>
 	isSolanaToken(token) && token.symbol === TRUMP_SYMBOL;
