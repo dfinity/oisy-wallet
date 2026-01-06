5c74921e6d72a989dcca155dc4e5d4b62ec51666
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-06 16:37:47 +0100

    feat(frontend): DIP721 method to check the NFT owned

--- src/frontend/src/tests/icp/canisters/dip721.canister.spec.ts
+++ src/frontend/src/tests/icp/canisters/dip721.canister.spec.ts
@@ -171,4 +171,149 @@ describe('dip721.canister', () => {
 			expect(service.balanceOf).toHaveBeenCalledExactlyOnceWith(mockPrincipal);
 		});
 	});
+
+	describe('getTokensByOwner', () => {
+		const mockParams = { principal: mockPrincipal, certified };
+
+		const mockIdentifiers = [12345n, 54321n];
+
+		beforeEach(() => {
+			vi.clearAllMocks();
+		});
+
+		it('should correctly call the ownerTokenIdentifiers method', async () => {
+			service.ownerTokenIdentifiers.mockResolvedValue({ Ok: mockIdentifiers });
+
+			const { getTokensByOwner } = await createDip721Canister({ serviceOverride: service });
+
+			const res = await getTokensByOwner(mockParams);
+
+			expect(res).toEqual(mockIdentifiers);
+			expect(service.ownerTokenIdentifiers).toHaveBeenCalledExactlyOnceWith(mockPrincipal);
+		});
+
+		it('should handle unauthorized operator error', async () => {
+			service.ownerTokenIdentifiers.mockResolvedValue({ Err: { UnauthorizedOperator: null } });
+
+			const { getTokensByOwner } = await createDip721Canister({ serviceOverride: service });
+
+			await expect(getTokensByOwner(mockParams)).rejects.toThrowError(
+				new CanisterInternalError('Unauthorized operator')
+			);
+
+			expect(service.ownerTokenIdentifiers).toHaveBeenCalledExactlyOnceWith(mockPrincipal);
+		});
+
+		it('should handle self transfer error', async () => {
+			service.ownerTokenIdentifiers.mockResolvedValue({ Err: { SelfTransfer: null } });
+
+			const { getTokensByOwner } = await createDip721Canister({ serviceOverride: service });
+
+			await expect(getTokensByOwner(mockParams)).rejects.toThrowError(
+				new CanisterInternalError('Cannot transfer NFT to self')
+			);
+
+			expect(service.ownerTokenIdentifiers).toHaveBeenCalledExactlyOnceWith(mockPrincipal);
+		});
+
+		it('should handle token not found error', async () => {
+			service.ownerTokenIdentifiers.mockResolvedValue({ Err: { TokenNotFound: null } });
+
+			const { getTokensByOwner } = await createDip721Canister({ serviceOverride: service });
+
+			await expect(getTokensByOwner(mockParams)).rejects.toThrowError(
+				new CanisterInternalError('NFT token not found')
+			);
+
+			expect(service.ownerTokenIdentifiers).toHaveBeenCalledExactlyOnceWith(mockPrincipal);
+		});
+
+		it('should handle unauthorized owner error', async () => {
+			service.ownerTokenIdentifiers.mockResolvedValue({ Err: { UnauthorizedOwner: null } });
+
+			const { getTokensByOwner } = await createDip721Canister({ serviceOverride: service });
+
+			await expect(getTokensByOwner(mockParams)).rejects.toThrowError(
+				new CanisterInternalError('Unauthorized owner for the NFT')
+			);
+
+			expect(service.ownerTokenIdentifiers).toHaveBeenCalledExactlyOnceWith(mockPrincipal);
+		});
+
+		it('should handle self approve error', async () => {
+			service.ownerTokenIdentifiers.mockResolvedValue({ Err: { SelfApprove: null } });
+
+			const { getTokensByOwner } = await createDip721Canister({ serviceOverride: service });
+
+			await expect(getTokensByOwner(mockParams)).rejects.toThrowError(
+				new CanisterInternalError('Cannot approve self for the NFT')
+			);
+
+			expect(service.ownerTokenIdentifiers).toHaveBeenCalledExactlyOnceWith(mockPrincipal);
+		});
+
+		it('should handle operator not found error', async () => {
+			service.ownerTokenIdentifiers.mockResolvedValue({ Err: { OperatorNotFound: null } });
+
+			const { getTokensByOwner } = await createDip721Canister({ serviceOverride: service });
+
+			await expect(getTokensByOwner(mockParams)).rejects.toThrowError(
+				new CanisterInternalError('Operator not found for the NFT')
+			);
+
+			expect(service.ownerTokenIdentifiers).toHaveBeenCalledExactlyOnceWith(mockPrincipal);
+		});
+
+		it('should handle existed NFT error', async () => {
+			service.ownerTokenIdentifiers.mockResolvedValue({ Err: { ExistedNFT: null } });
+
+			const { getTokensByOwner } = await createDip721Canister({ serviceOverride: service });
+
+			await expect(getTokensByOwner(mockParams)).rejects.toThrowError(
+				new CanisterInternalError('NFT already exists')
+			);
+
+			expect(service.ownerTokenIdentifiers).toHaveBeenCalledExactlyOnceWith(mockPrincipal);
+		});
+
+		it('should handle owner not found error', async () => {
+			service.ownerTokenIdentifiers.mockResolvedValue({ Err: { OwnerNotFound: null } });
+
+			const { getTokensByOwner } = await createDip721Canister({ serviceOverride: service });
+
+			await expect(getTokensByOwner(mockParams)).rejects.toThrowError(
+				new CanisterInternalError('Owner not found for the NFT')
+			);
+
+			expect(service.ownerTokenIdentifiers).toHaveBeenCalledExactlyOnceWith(mockPrincipal);
+		});
+
+		it('should handle a generic canister error', async () => {
+			// @ts-expect-error we test this on purpose
+			service.ownerTokenIdentifiers.mockResolvedValue({ Err: { CanisterError: null } });
+
+			const { getTokensByOwner } = await createDip721Canister({
+				serviceOverride: service
+			});
+
+			await expect(getTokensByOwner(mockParams)).rejects.toThrowError(
+				new CanisterInternalError('Unknown Dip721CanisterError')
+			);
+
+			expect(service.ownerTokenIdentifiers).toHaveBeenCalledExactlyOnceWith(mockPrincipal);
+		});
+
+		it('should throw an error if ownerTokenIdentifiers throws', async () => {
+			const mockError = new Error('Test response error');
+			service.ownerTokenIdentifiers.mockRejectedValue(mockError);
+
+			const { getTokensByOwner } = await createDip721Canister({ serviceOverride: service });
+
+			const res = getTokensByOwner(mockParams);
+
+			await expect(res).rejects.toThrowError(mockError);
+
+			expect(service.ownerTokenIdentifiers).toHaveBeenCalledExactlyOnceWith(mockPrincipal);
+		});
+	});
 });
