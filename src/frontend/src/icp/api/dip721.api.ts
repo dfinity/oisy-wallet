1b975eb8f4c8d243ba5995897e0e0ec50412d52d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-06 16:07:44 +0100

    feat(frontend): API method to fetch balance of DIP721 NFTs

---
+++ src/frontend/src/icp/api/dip721.api.ts
@@ -0,0 +1,43 @@
+import type { Balance } from '$declarations/ext_v2_token/ext_v2_token.did';
+import { Dip721Canister } from '$icp/canisters/dip721.canister';
+import { ZERO } from '$lib/constants/app.constants';
+import type { CanisterApiFunctionParamsWithCanisterId } from '$lib/types/canister';
+import { assertNonNullish, isNullish, type QueryParams } from '@dfinity/utils';
+import { Principal } from '@icp-sdk/core/principal';
+
+/**
+ * Returns the count of NFTs owned by the principal.
+ *
+ * @link https://github.com/Psychedelic/DIP721/blob/develop/spec.md#balanceof
+ */
+export const balance = async ({
+	certified,
+	identity,
+	canisterId,
+	...rest
+}: CanisterApiFunctionParamsWithCanisterId<QueryParams>): Promise<Balance> => {
+	if (isNullish(identity)) {
+		return ZERO;
+	}
+
+	const { balance } = await dip721Canister({
+		identity,
+		canisterId,
+		...rest
+	});
+
+	return await balance({ principal: identity.getPrincipal(), certified });
+};
+
+const dip721Canister = async ({
+	identity,
+	nullishIdentityErrorMessage,
+	canisterId
+}: CanisterApiFunctionParamsWithCanisterId): Promise<Dip721Canister> => {
+	assertNonNullish(identity, nullishIdentityErrorMessage);
+
+	return await Dip721Canister.create({
+		identity,
+		canisterId: Principal.fromText(canisterId)
+	});
+};
