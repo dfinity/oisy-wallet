9528395a9bf117f0c84b9e0f3e4ac59b57c098a3
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-06 09:58:40 +0100

    chore(build): Fix spelling mistake in description of build files (#10969)
    # Motivation
    
    Repeated word in the build files.

--- scripts/build.xtc_ledger.sh
+++ scripts/build.xtc_ledger.sh
@@ -7,7 +7,7 @@ print_help() {
 
 	- The Wasm and Candid files are downloaded.
 
-	The files are installed at at the locations defined for 'xtc_ledger' in 'dfx.json'.
+	The files are installed at the locations defined for 'xtc_ledger' in 'dfx.json'.
 	EOF
 }
 
