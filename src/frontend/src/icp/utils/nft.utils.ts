47425445264e01bf6002ba127f94c07925c703f9
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-07 16:54:02 +0100

    feat(frontend): Util to map a DIPT721 NFT to OISY NFT (#11015)
    # Motivation
    
    We require an util to normalize the DIP721 NFT to the rest.
    
    For now, it is a simple mapping, since we still don't have a method to
    fetch the metadata.

--- src/frontend/src/icp/utils/nft.utils.ts
+++ src/frontend/src/icp/utils/nft.utils.ts
@@ -1,7 +1,9 @@
 import type { TokenIndex } from '$declarations/ext_v2_token/ext_v2_token.did';
 import { getExtMetadata } from '$icp/services/ext-metadata.services';
+import type { Dip721Token } from '$icp/types/dip721-token';
 import type { ExtToken } from '$icp/types/ext-token';
 import { extIndexToIdentifier, parseExtTokenIndex } from '$icp/utils/ext.utils';
+import { NftMediaStatusEnum } from '$lib/schema/nft.schema';
 import type { Nft, NftCollection } from '$lib/types/nft';
 import { getMediaStatusOrCache } from '$lib/utils/nfts.utils';
 import { parseNftId } from '$lib/validation/nft.validation';
@@ -54,5 +56,24 @@ export const mapExtNft = async ({
 		thumbnailUrl,
 		mediaStatus,
 		collection: mapExtCollection(token)
+	};
+};
+
+const mapDip721Collection = ({ canisterId, ...rest }: Dip721Token): NftCollection => ({
+	...rest,
+	address: canisterId
+});
+
+// TODO: Fetch metadata of the NFT
+export const mapDip721Nft = ({ index, token }: { index: bigint; token: Dip721Token }): Nft => {
+	const mediaStatus = {
+		image: NftMediaStatusEnum.INVALID_DATA,
+		thumbnail: NftMediaStatusEnum.INVALID_DATA
+	};
+
+	return {
+		id: parseNftId(index.toString()),
+		mediaStatus,
+		collection: mapDip721Collection(token)
 	};
 };
