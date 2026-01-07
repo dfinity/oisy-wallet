cd1a68551d13325fda9cecdb7d2dde62cd3a26f8
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-07 10:28:35 +0100

    feat(build): Add minting account to additional ICRC tokens list

--- src/frontend/src/tests/icp/utils/map-icrc-data.spec.ts
+++ src/frontend/src/tests/icp/utils/map-icrc-data.spec.ts
@@ -1,16 +1,20 @@
+import { getIcrcAccount } from '$icp/utils/icrc-account.utils';
 import { mapIcrcData } from '$icp/utils/map-icrc-data';
 import * as appConstants from '$lib/constants/app.constants';
+import { mockPrincipal } from '$tests/mocks/identity.mock';
+import { decodeIcrcAccount, encodeIcrcAccount } from '@icp-sdk/canisters/ledger/icrc';
 
 describe('mapIcrcData', () => {
 	const token = {
 		TESTTOKEN: {
 			ledgerCanisterId: 'dummy-ledger-id',
-			indexCanisterId: 'dummy-canister-id'
+			indexCanisterId: 'dummy-canister-id',
+			mintingAccount: encodeIcrcAccount(getIcrcAccount(mockPrincipal))
 		}
 	};
 
 	const expected = {
-		TESTTOKEN: token.TESTTOKEN
+		TESTTOKEN: { ...token.TESTTOKEN, mintingAccount: decodeIcrcAccount(mockPrincipal.toText()) }
 	};
 
 	const envs = [
@@ -20,7 +24,7 @@ describe('mapIcrcData', () => {
 		{ env: 'LOCAL', expected: {} }
 	];
 
-	describe.each(envs)('when %s is true', ({ env, expected }) => {
+	describe.each(envs)('when $env is true', ({ env, expected }) => {
 		beforeEach(() => {
 			vi.resetAllMocks();
 
