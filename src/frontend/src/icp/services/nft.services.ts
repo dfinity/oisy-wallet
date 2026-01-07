b38dbda8397ed2c86534f3f04805f99af0ce82ef
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-07 18:12:26 +0100

    feat(frontend): Integrate DIP721 in service to load IC NFTs

--- src/frontend/src/icp/services/nft.services.ts
+++ src/frontend/src/icp/services/nft.services.ts
@@ -1,7 +1,9 @@
+import { getTokensByOwner as getDip721TokensByOwner } from '$icp/api/dip721.api';
 import { getTokensByOwner as getExtTokensByOwner } from '$icp/api/ext-v2-token.api';
 import type { IcNonFungibleToken } from '$icp/types/nft';
+import { isTokenDip721 } from '$icp/utils/dip721.utils';
 import { isTokenExt } from '$icp/utils/ext.utils';
-import { mapExtNft } from '$icp/utils/nft.utils';
+import { mapDip721Nft, mapExtNft } from '$icp/utils/nft.utils';
 import type { OptionIdentity } from '$lib/types/identity';
 import type { Nft } from '$lib/types/nft';
 import type { Token } from '$lib/types/token';
@@ -36,6 +38,34 @@ const loadExtNfts = async ({
 	}
 };
 
+const loadDip721Nfts = async ({
+	token,
+	identity
+}: {
+	token: IcNonFungibleToken;
+	identity: Identity;
+}) => {
+	const { canisterId } = token;
+
+	const owner = identity.getPrincipal();
+
+	try {
+		const tokenIndices = await getDip721TokensByOwner({
+			identity,
+			owner,
+			canisterId
+		});
+
+		const promises = tokenIndices.map(async (index) => await mapDip721Nft({ index, token }));
+
+		return await Promise.all(promises);
+	} catch (error: unknown) {
+		console.warn(`Error loading DIP721 tokens from collection ${canisterId}:`, error);
+
+		return [];
+	}
+};
+
 export const loadNfts = async ({
 	tokens,
 	identity
@@ -50,6 +80,10 @@ export const loadNfts = async ({
 	const nftPromises = tokens.map(async (token) => {
 		if (isTokenExt(token)) {
 			return await loadExtNfts({ token, identity });
+		}
+
+		if (isTokenDip721(token)) {
+			return await loadDip721Nfts({ token, identity });
 		}
 
 		assertNever(token, `Unsupported NFT IC token ${(token as Token).standard.code}`);
