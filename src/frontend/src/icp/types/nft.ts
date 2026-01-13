f07eb38368680ecac7571147518c19246df35021
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-13 14:50:54 +0100

    feat(frontend): Util to check if a custom token is pseudo-standard ICPunks (#11093)
    # Motivation
    
    Like for other standard, we need an util to check if a custom token is
    ICPunks-like.

--- src/frontend/src/icp/types/nft.ts
+++ src/frontend/src/icp/types/nft.ts
@@ -1,4 +1,5 @@
 import type { Dip721Token } from '$icp/types/dip721-token';
 import type { ExtToken } from '$icp/types/ext-token';
+import type { IcPunksToken } from '$icp/types/icpunks-token';
 
-export type IcNonFungibleToken = ExtToken | Dip721Token;
+export type IcNonFungibleToken = ExtToken | Dip721Token | IcPunksToken;
