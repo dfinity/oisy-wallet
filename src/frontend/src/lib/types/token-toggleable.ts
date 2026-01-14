835f4b17b331a58c9d3e090bc678bc4197cccff3
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-14 10:08:19 +0100

    more

--- src/frontend/src/lib/types/token-toggleable.ts
+++ src/frontend/src/lib/types/token-toggleable.ts
@@ -1,13 +1,14 @@
 import type { CustomToken } from '$declarations/backend/backend.did';
+import type { NonFungibleTokenAppearance } from '$lib/types/nft-ui';
 import type { Token } from '$lib/types/token';
 
 // Type pick and omit fields to make the reader aware that we are redefining the two fields we are interested in.
 export type CustomTokenState = Omit<
 	Pick<CustomToken, 'version' | 'enabled'>,
-	'version' | 'enabled'
+	'version' | 'enabled' | 'section' | 'allowExternalContentSource'
 > & {
 	version?: bigint;
 	enabled: boolean;
-};
+} & NonFungibleTokenAppearance;
 
 export type TokenToggleable<T extends Token> = T & CustomTokenState;
