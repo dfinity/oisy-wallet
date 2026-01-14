bd06f111c586f5fe1cbc0a43d8f7a7fab35289a9
Stefan Berger <stefan.berger@dfinity.org>
2026-01-14 09:41:17 +0100

    Update auth.ts

--- src/frontend/src/lib/types/auth.ts
+++ src/frontend/src/lib/types/auth.ts
@@ -1,5 +1,5 @@
 export enum InternetIdentityDomain {
-	VERSION_1_0_LEGACY = 'identity.ic0.app',
-	VERSION_1_0 = 'identity.internetcomputer.org',
+	VERSION_1_0_LEGACY = 'beta.identity.ic0.app',
+	VERSION_1_0 = 'beta.identity.internetcomputer.org',
 	VERSION_2_0 = 'beta.id.ai'
 }
