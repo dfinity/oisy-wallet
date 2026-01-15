d2d3675a979219759b73a2e2fcf757b5bd5f8435
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-15 14:08:50 +0100

    more

--- src/backend/tests/it/stats.rs
+++ src/backend/tests/it/stats.rs
@@ -1,14 +1,37 @@
 //! `PocketIc` tests for the `stats()` API.
+
+use std::sync::LazyLock;
 use candid::Principal;
 use shared::types::{user_profile::OisyUser, Stats};
-
+use shared::types::custom_token::{CustomToken, IcrcToken, Token};
 use crate::{
-    user_token::{ANOTHER_TOKEN, MOCK_TOKEN},
     utils::{
         mock::USER_1,
         pocketic::{controller, BackendBuilder, PicCanisterTrait},
     },
 };
+
+static ICRC_TOKEN: LazyLock<IcrcToken> = LazyLock::new(|| IcrcToken {
+    ledger_id: Principal::from_text("ddsp7-7iaaa-aaaaq-aacqq-cai").unwrap(),
+    index_id: Some(Principal::from_text("dnqcx-eyaaa-aaaaq-aacrq-cai").unwrap()),
+});
+static USER_TOKEN: LazyLock<CustomToken> = LazyLock::new(|| CustomToken {
+    token: Token::Icrc(crate::custom_token::ICRC_TOKEN.clone()),
+    enabled: true,
+    version: None,
+    section: None,
+    allow_external_content_source: None,
+});
+static ANOTHER_USER_TOKEN: LazyLock<CustomToken> = LazyLock::new(|| CustomToken {
+    token: Token::Icrc(IcrcToken {
+        ledger_id: Principal::from_text("uf2wh-taaaa-aaaaq-aabna-cai").unwrap(),
+        index_id: Some(Principal::from_text("ux4b6-7qaaa-aaaaq-aaboa-cai").unwrap()),
+    }),
+    enabled: true,
+    version: None,
+    section: None,
+    allow_external_content_source: None,
+});
 
 #[test]
 fn stats_returns_correct_number_of_users() {
@@ -17,11 +40,11 @@ fn stats_returns_correct_number_of_users() {
     // Create five users.
     let expected_users: Vec<OisyUser> = pic_setup.create_users(1..=5);
     // Create three users with tokens.
-    let user_tokens = vec![MOCK_TOKEN.clone(), ANOTHER_TOKEN.clone()];
+    let user_tokens = vec![USER_TOKEN.clone(), ANOTHER_USER_TOKEN.clone()];
     const NUM_USERS_WITH_TOKENS: usize = 3;
     for user in &expected_users[0..NUM_USERS_WITH_TOKENS] {
         pic_setup
-            .update::<()>(user.principal, "set_many_user_tokens", &user_tokens)
+            .update::<()>(user.principal, "set_many_custom_tokens", &user_tokens)
             .expect("Test setup error: Failed to set user tokens");
     }
     // That should give us these stats:
