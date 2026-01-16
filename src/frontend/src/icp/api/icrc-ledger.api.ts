d9c10ad27a87d01bbccab3efe0be57734afb557a
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-16 11:39:58 +0100

    chore(frontend): Remove default update call when fetching ICRC metadata (#11142)
    # Motivation
    
    There is no need to enforce a default of update call when fetching ICRC
    metadata, since the parameters are already passed in all the consumers.

--- src/frontend/src/icp/api/icrc-ledger.api.ts
+++ src/frontend/src/icp/api/icrc-ledger.api.ts
@@ -27,7 +27,7 @@ import { Principal } from '@icp-sdk/core/principal';
  * @returns {Promise<IcrcTokenMetadataResponse>} The metadata response for the ICRC token.
  */
 export const metadata = async ({
-	certified = true,
+	certified,
 	identity,
 	...rest
 }: {
