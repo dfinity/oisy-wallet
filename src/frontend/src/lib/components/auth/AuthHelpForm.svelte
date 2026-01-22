fa56e171d93953894d0e2ca9dd513225581e70ee
Stefan Berger <stefan.berger@dfinity.org>
2026-01-22 13:22:58 +0100

    Change Buttons and Links

--- src/frontend/src/lib/components/auth/AuthHelpForm.svelte
+++ src/frontend/src/lib/components/auth/AuthHelpForm.svelte
@@ -48,20 +48,20 @@
 	<div>
 		<div class="grid gap-2">
 			<Button
-				ariaLabel={$i18n.auth.help.text.login_page_looks_different}
+				ariaLabel={$i18n.auth.help.text.issue_with_login_page}
 				colorStyle="secondary-light"
 				fullWidth
 				onclick={() => {
 					trackEvent({
 						name: PLAUSIBLE_EVENTS.SIGN_IN_CANCELLED_HELP,
-						metadata: { event_key: trackingEventKey, event_value: 'login_page_looks_different' }
+						metadata: { event_key: trackingEventKey, event_value: 'issue_with_login_page' }
 					});
 					onOpenNewIdentityHelp();
 				}}
 				testId={HELP_AUTH_NEW_IDENTITY_VERSION_BUTTON}
 				type="button"
 			>
-				{$i18n.auth.help.text.login_page_looks_different}
+				{$i18n.auth.help.text.issue_with_login_page}
 			</Button>
 
 			<Button
@@ -84,65 +84,7 @@
 				{$i18n.auth.help.text.use_identity_number}
 			</Button>
 
-			<Button
-				ariaLabel={$i18n.auth.help.text.lost_identity_number}
-				colorStyle="secondary-light"
-				fullWidth
-				onclick={() => {
-					trackEvent({
-						name: PLAUSIBLE_EVENTS.SIGN_IN_CANCELLED_HELP,
-						metadata: {
-							event_key: trackingEventKey,
-							event_value: 'lost_identity_number'
-						}
-					});
-					onOpenLegacyIdentityHelp();
-				}}
-				testId={HELP_AUTH_LOST_IDENTITY_BUTTON}
-				type="button"
-			>
-				{$i18n.auth.help.text.lost_identity_number}
-			</Button>
-
 			<div class="mt-6 mb-2 font-bold">{$i18n.auth.help.text.useful_links}</div>
-
-			<div>
-				<ExternalLink
-					ariaLabel={$i18n.auth.help.text.switch_to_new_internet_identity}
-					href={OISY_INTERNET_IDENTITY_VERSION_2_0_DOCS_URL}
-					iconAsLast
-					styleClass="font-semibold"
-					testId={HELP_AUTH_SWITCH_TO_NEW_INTERNET_IDENTITY_LINK}
-					trackEvent={{
-						name: PLAUSIBLE_EVENTS.SIGN_IN_CANCELLED_HELP,
-						metadata: {
-							event_key: trackingEventKey,
-							event_value: 'switch_to_new_internet_identity'
-						}
-					}}
-				>
-					{replaceOisyPlaceholders($i18n.auth.help.text.switch_to_new_internet_identity)}
-				</ExternalLink>
-			</div>
-
-			<div>
-				<ExternalLink
-					ariaLabel={replaceOisyPlaceholders($i18n.auth.help.text.internet_identity_help_center)}
-					href={OISY_INTERNET_IDENTITY_HELP_CENTER_URL}
-					iconAsLast
-					styleClass="font-semibold"
-					testId={HELP_AUTH_INTERNET_IDENTITY_HELP_CENTER_LINK}
-					trackEvent={{
-						name: PLAUSIBLE_EVENTS.SIGN_IN_CANCELLED_HELP,
-						metadata: {
-							event_key: trackingEventKey,
-							event_value: 'internet_identity_help_center'
-						}
-					}}
-				>
-					{replaceOisyPlaceholders($i18n.auth.help.text.internet_identity_help_center)}
-				</ExternalLink>
-			</div>
 
 			<div>
 				<ExternalLink
@@ -165,20 +107,39 @@
 
 			<div>
 				<ExternalLink
-					ariaLabel={$i18n.auth.help.text.private_key}
-					href={OISY_FAQ_URL_WHO_CONTROLS_THE_PRIVATE_KEY}
+					ariaLabel={$i18n.auth.help.text.logging_into_oisy}
+					href={OISY_LOGGING_INTO_OISY_URL}
 					iconAsLast
 					styleClass="font-semibold"
-					testId={HELP_AUTH_PRIVATE_KEY_LINK}
+					testId={HELP_AUTH_LOGGING_INTO_OISY_LINK}
 					trackEvent={{
 						name: PLAUSIBLE_EVENTS.SIGN_IN_CANCELLED_HELP,
 						metadata: {
 							event_key: trackingEventKey,
-							event_value: 'private_key'
+							event_value: 'logging_into_oisy'
 						}
 					}}
 				>
-					{$i18n.auth.help.text.private_key}
+					{replaceOisyPlaceholders($i18n.auth.help.text.logging_into_oisy)}
+				</ExternalLink>
+			</div>
+
+			<div>
+				<ExternalLink
+					ariaLabel={$i18n.auth.help.text.creating_a_wallet}
+					href={OISY_CREATING_A_WALLET_URL}
+					iconAsLast
+					styleClass="font-semibold"
+					testId={HELP_AUTH_CREATING_A_WALLET_LINK}
+					trackEvent={{
+						name: PLAUSIBLE_EVENTS.SIGN_IN_CANCELLED_HELP,
+						metadata: {
+							event_key: trackingEventKey,
+							event_value: 'creating_a_wallet'
+						}
+					}}
+				>
+					{replaceOisyPlaceholders($i18n.auth.help.text.creating_a_wallet)}
 				</ExternalLink>
 			</div>
 
