ccff09b1e80e5359a7f65129989fa4a731163d8a
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-23 10:41:46 +0100

    refactor(frontend): Reduce loop in service to load ERC721 tokens (#11270)
    # Motivation
    
    We can refactor the code for the service that loads ERC721 tokens to
    reduce a loop.
    
    NOTE: tests and results do not change.

--- src/frontend/src/eth/services/erc721.services.ts
+++ src/frontend/src/eth/services/erc721.services.ts
@@ -67,12 +67,6 @@ export const loadCustomTokens = ({
 		identity
 	});
 
-const loadErc721CustomTokens = async (params: LoadCustomTokenParams): Promise<CustomToken[]> =>
-	await loadNetworkCustomTokens({
-		...params,
-		filterTokens: ({ token }) => 'Erc721' in token
-	});
-
 const safeLoadMetadata = async ({
 	networkId,
 	address
@@ -98,70 +92,80 @@ const safeLoadMetadata = async ({
 	}
 };
 
+type CustomTokenErc721Variant = Omit<CustomToken, 'token'> & { token: { Erc721: ErcToken } };
+
+const filterErc721CustomToken = (
+	customToken: CustomToken
+): customToken is CustomTokenErc721Variant => 'Erc721' in customToken.token;
+
+const mapErc721CustomToken = async ({
+	token,
+	enabled,
+	version: versionNullable,
+	section: sectionNullable,
+	allow_external_content_source: allowExternalContentSourceNullable
+}: CustomTokenErc721Variant): Promise<Erc721CustomToken | undefined> => {
+	const version = fromNullable(versionNullable);
+	const section = fromNullable(sectionNullable);
+	const mappedSection = nonNullish(section) ? mapTokenSection(section) : undefined;
+	const allowExternalContentSource = fromNullable(allowExternalContentSourceNullable);
+
+	const {
+		Erc721: { token_address: tokenAddress, chain_id: tokenChainId }
+	} = token;
+
+	const network = [...SUPPORTED_ETHEREUM_NETWORKS, ...SUPPORTED_EVM_NETWORKS].find(
+		({ chainId }) => tokenChainId === chainId
+	);
+
+	// This should not happen because we filter the chain_id in the previous filter, but we need it to be type safe
+	assertNonNullish(
+		network,
+		`Inconsistency in network data: no network found for chainId ${tokenChainId} in custom token, even though it is in the environment`
+	);
+
+	const metadata = await safeLoadMetadata({ networkId: network.id, address: tokenAddress });
+
+	if (isNullish(metadata)) {
+		return;
+	}
+
+	return {
+		...{
+			id: parseCustomTokenId({ identifier: tokenAddress, chainId: network.chainId }),
+			name: tokenAddress,
+			address: tokenAddress,
+			network,
+			symbol: metadata.symbol ?? '', // The symbol is used with the amount, no issue with having it empty for NFTs
+			decimals: 0, // Erc721 contracts don't have decimals, but to avoid unexpected behavior, we set it to 0
+			standard: { code: 'erc721' as const },
+			category: 'custom' as const,
+			enabled,
+			version,
+			...(nonNullish(mappedSection) && {
+				section: mappedSection
+			}),
+			allowExternalContentSource
+		},
+		...metadata
+	};
+};
+
 const loadCustomTokensWithMetadata = async (
 	params: LoadCustomTokenParams
 ): Promise<Erc721CustomToken[]> => {
-	const erc721CustomTokens: CustomToken[] = await loadErc721CustomTokens(params);
+	const backendCustomTokens: CustomToken[] = await loadNetworkCustomTokens(params);
 
-	const customTokenPromises = erc721CustomTokens
-		.filter(
-			(customToken): customToken is CustomToken & { token: { Erc721: ErcToken } } =>
-				'Erc721' in customToken.token
-		)
-		.map(
-			async ({
-				token,
-				enabled,
-				version: versionNullable,
-				section: sectionNullable,
-				allow_external_content_source: allowExternalContentSourceNullable
-			}) => {
-				const version = fromNullable(versionNullable);
-				const section = fromNullable(sectionNullable);
-				const mappedSection = nonNullish(section) ? mapTokenSection(section) : undefined;
-				const allowExternalContentSource = fromNullable(allowExternalContentSourceNullable);
-
-				const {
-					Erc721: { token_address: tokenAddress, chain_id: tokenChainId }
-				} = token;
-
-				const network = [...SUPPORTED_ETHEREUM_NETWORKS, ...SUPPORTED_EVM_NETWORKS].find(
-					({ chainId }) => tokenChainId === chainId
-				);
-
-				// This should not happen because we filter the chain_id in the previous filter, but we need it to be type safe
-				assertNonNullish(
-					network,
-					`Inconsistency in network data: no network found for chainId ${tokenChainId} in custom token, even though it is in the environment`
-				);
-
-				const metadata = await safeLoadMetadata({ networkId: network.id, address: tokenAddress });
-
-				if (isNullish(metadata)) {
-					return;
-				}
-
-				return {
-					...{
-						id: parseCustomTokenId({ identifier: tokenAddress, chainId: network.chainId }),
-						name: tokenAddress,
-						address: tokenAddress,
-						network,
-						symbol: metadata.symbol ?? '', // The symbol is used with the amount, no issue with having it empty for NFTs
-						decimals: 0, // Erc721 contracts don't have decimals, but to avoid unexpected behavior, we set it to 0
-						standard: { code: 'erc721' as const },
-						category: 'custom' as const,
-						enabled,
-						version,
-						...(nonNullish(mappedSection) && {
-							section: mappedSection
-						}),
-						allowExternalContentSource
-					},
-					...metadata
-				};
+	const customTokenPromises = backendCustomTokens.reduce<Promise<Erc721CustomToken | undefined>[]>(
+		(acc, token) => {
+			if (filterErc721CustomToken(token)) {
+				acc.push(mapErc721CustomToken(token));
 			}
-		);
+
+			return acc;
+		},
+		[]
+	);
 
 	const customTokens = await Promise.allSettled(customTokenPromises);
 
