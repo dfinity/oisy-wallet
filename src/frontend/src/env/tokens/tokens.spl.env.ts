cf8baeb0bbe227d5f31ba8c7079f10f9dc2e8b67
Stefan Berger <stefan.berger@dfinity.org>
2026-01-24 22:49:08 +0100

    add SOL token to list

--- src/frontend/src/env/tokens/tokens.spl.env.ts
+++ src/frontend/src/env/tokens/tokens.spl.env.ts
@@ -39,6 +39,7 @@ import { PGX_TOKEN } from '$env/tokens/tokens-spl/tokens.pgx.env';
 import { POPCAT_TOKEN } from '$env/tokens/tokens-spl/tokens.popcat.env';
 import { QQQX_TOKEN } from '$env/tokens/tokens-spl/tokens.qqqx.env';
 import { RAY_TOKEN } from '$env/tokens/tokens-spl/tokens.ray.env';
+import { SLVON_TOKEN } from '$env/tokens/tokens-spl/tokens.slvon.env';
 import { SPX_TOKEN } from '$env/tokens/tokens-spl/tokens.spx.env';
 import { SPYX_TOKEN } from '$env/tokens/tokens-spl/tokens.spyx.env';
 import { TQQQX_TOKEN } from '$env/tokens/tokens-spl/tokens.tqqqx.env';
@@ -93,6 +94,7 @@ const SPL_TOKENS_MAINNET: RequiredSplToken[] = [
 	POPCAT_TOKEN,
 	QQQX_TOKEN,
 	RAY_TOKEN,
+	SLVON_TOKEN,
 	SPX_TOKEN,
 	SPYX_TOKEN,
 	TQQQX_TOKEN,
