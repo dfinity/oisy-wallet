992705b6aad0b0043755dd86abecdc11122c77b6
Stefan Berger <stefan.berger@dfinity.org>
2026-01-24 22:43:38 +0100

    add group config

---
+++ src/frontend/src/env/tokens/groups/groups.slvon.env.ts
@@ -0,0 +1,14 @@
+import slvon from '$eth/assets/slvon.webp';
+import type { TokenGroupData, TokenGroupId } from '$lib/types/token-group';
+import { parseTokenGroupId } from '$lib/validation/token-group.validation';
+
+const SLVON_TOKEN_GROUP_SYMBOL = 'SLVon';
+
+export const SLVON_TOKEN_GROUP_ID: TokenGroupId = parseTokenGroupId(SLVON_TOKEN_GROUP_SYMBOL);
+
+export const SLVON_TOKEN_GROUP: TokenGroupData = {
+	id: SLVON_TOKEN_GROUP_ID,
+	icon: slvon,
+	name: 'iShares Silver Trust (Ondo Tokenized Stock)',
+	symbol: SLVON_TOKEN_GROUP_SYMBOL
+};
