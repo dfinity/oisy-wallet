372af937c4ff628da3555178fddd00378ad46f4e
Stefan Berger <stefan.berger@dfinity.org>
2026-01-25 12:23:46 +0100

    use ishares icon

--- src/frontend/src/env/tokens/groups/groups.slvon.env.ts
+++ src/frontend/src/env/tokens/groups/groups.slvon.env.ts
@@ -1,4 +1,4 @@
-import slvon from '$eth/assets/slvon.webp';
+import ishares from '$eth/assets/ishares.webp';
 import type { TokenGroupData, TokenGroupId } from '$lib/types/token-group';
 import { parseTokenGroupId } from '$lib/validation/token-group.validation';
 
@@ -8,7 +8,7 @@ export const SLVON_TOKEN_GROUP_ID: TokenGroupId = parseTokenGroupId(SLVON_TOKEN_
 
 export const SLVON_TOKEN_GROUP: TokenGroupData = {
 	id: SLVON_TOKEN_GROUP_ID,
-	icon: slvon,
+	icon: ishares,
 	name: 'iShares Silver Trust (Ondo Tokenized Stock)',
 	symbol: SLVON_TOKEN_GROUP_SYMBOL
 };
