96c729d6ba54dce90d64da99c341221301d3622e
Stefan Berger <stefan.berger@dfinity.org>
2026-01-25 07:50:35 +0100

    add BSC token to list

--- src/frontend/src/env/tokens/tokens-evm/tokens-bsc/tokens.bep20.env.ts
+++ src/frontend/src/env/tokens/tokens-evm/tokens-bsc/tokens.bep20.env.ts
@@ -1,11 +1,12 @@
 import { BSC_MAINNET_ENABLED } from '$env/networks/networks-evm/networks.evm.bsc.env';
+import { COPXON_TOKEN } from '$env/tokens/tokens-evm/tokens-bsc/tokens-bep20/tokens.copxon.env';
 import { NVDAON_TOKEN } from '$env/tokens/tokens-evm/tokens-bsc/tokens-bep20/tokens.nvdaon.env';
 import { USDC_TOKEN } from '$env/tokens/tokens-evm/tokens-bsc/tokens-bep20/tokens.usdc.env';
 import { USDT_TOKEN } from '$env/tokens/tokens-evm/tokens-bsc/tokens-bep20/tokens.usdt.env';
 import type { RequiredEvmBep20Token } from '$evm/types/bep20';
 import { defineSupportedTokens } from '$lib/utils/env.tokens.utils';
 
-const BSC_BEP20_TOKENS_MAINNET: RequiredEvmBep20Token[] = [NVDAON_TOKEN, USDC_TOKEN, USDT_TOKEN];
+const BSC_BEP20_TOKENS_MAINNET: RequiredEvmBep20Token[] = [COPXON_TOKEN, NVDAON_TOKEN, USDC_TOKEN, USDT_TOKEN];
 
 export const BSC_BEP20_TOKENS: RequiredEvmBep20Token[] = defineSupportedTokens({
 	mainnetFlag: BSC_MAINNET_ENABLED,
