dd84217292251006519c1b71f2efd40554010a5f
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-28 17:32:11 +0100

    lint

--- src/frontend/src/lib/components/wallet-connect/WalletConnect.svelte
+++ src/frontend/src/lib/components/wallet-connect/WalletConnect.svelte
@@ -2,7 +2,6 @@
 	import WalletConnectSend from '$lib/components/wallet-connect/WalletConnectSend.svelte';
 	import WalletConnectSession from '$lib/components/wallet-connect/WalletConnectSession.svelte';
 	import WalletConnectSign from '$lib/components/wallet-connect/WalletConnectSign.svelte';
-	import type { OptionWalletConnectListener } from '$lib/types/wallet-connect';
 </script>
 
 <WalletConnectSession />
