ddab1e8794be489f9b3ca33752cc8b593dd936c9
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-30 10:53:16 +0100

    refactor(frontend): Use WalletConnect `proposal` store instead of passing it down (#11355)
    # Motivation
    
    In component `WalletConnectReview` we can really use the WalletConnect
    `proposal` store, instead of passing the prop down.
    
    That also allows us to reuse the entire WalletConnect wizard.

--- src/frontend/src/lib/components/wallet-connect/WalletConnectSessionWizard.svelte
+++ src/frontend/src/lib/components/wallet-connect/WalletConnectSessionWizard.svelte
@@ -1,25 +1,22 @@
 <script lang="ts">
 	import type { WizardStep } from '@dfinity/gix-components';
-	import type { WalletKitTypes } from '@reown/walletkit';
 	import WalletConnectForm from '$lib/components/wallet-connect/WalletConnectForm.svelte';
 	import WalletConnectReview from '$lib/components/wallet-connect/WalletConnectReview.svelte';
 	import { WizardStepsWalletConnect } from '$lib/enums/wizard-steps';
-	import type { Option } from '$lib/types/utils';
 
 	interface Props {
-		proposal: Option<WalletKitTypes.SessionProposal>;
 		currentStep: WizardStep<WizardStepsWalletConnect> | undefined;
 		onConnect: (uri: string) => void;
 		onApprove: () => void;
 		onReject: () => void;
 	}
 
-	let { proposal, currentStep, onConnect, onApprove, onReject }: Props = $props();
+	let { currentStep, onConnect, onApprove, onReject }: Props = $props();
 </script>
 
 {#key currentStep?.name}
 	{#if currentStep?.name === WizardStepsWalletConnect.REVIEW}
-		<WalletConnectReview {onApprove} onCancel={onReject} {onReject} {proposal} />
+		<WalletConnectReview {onApprove} onCancel={onReject} {onReject} />
 	{:else if currentStep?.name === WizardStepsWalletConnect.CONNECT}
 		<WalletConnectForm {onConnect} />
 	{/if}
