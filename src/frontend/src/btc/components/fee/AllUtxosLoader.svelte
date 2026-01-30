3990a5acf9e3d28f6ec693ce5b3e39bb1589a2cc
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-30 09:44:44 +0100

    format

--- src/frontend/src/btc/components/fee/AllUtxosLoader.svelte
+++ src/frontend/src/btc/components/fee/AllUtxosLoader.svelte
@@ -3,11 +3,14 @@
 	import { type Snippet, untrack } from 'svelte';
 	import { CONFIRMED_BTC_TRANSACTION_MIN_CONFIRMATIONS } from '$btc/constants/btc.constants';
 	import { allUtxosStore } from '$btc/stores/all-utxos.store';
+	import {
+		BITCOIN_CANISTER_IDS,
+		IC_CKBTC_MINTER_CANISTER_ID
+	} from '$env/tokens/tokens-icp/tokens.icp.ck.btc.env';
 	import { getUtxosQuery } from '$icp/api/bitcoin.api';
 	import { authIdentity } from '$lib/derived/auth.derived';
 	import type { NetworkId } from '$lib/types/network';
 	import { mapNetworkIdToBitcoinNetwork } from '$lib/utils/network.utils';
-	import {BITCOIN_CANISTER_IDS, IC_CKBTC_MINTER_CANISTER_ID} from "$env/tokens/tokens-icp/tokens.icp.ck.btc.env";
 
 	interface Props {
 		source?: string;
