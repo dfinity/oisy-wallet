b93054accad505cd5835a1e7c68ec7f6ad1baeb1
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-30 15:30:45 +0100

    refactor(frontend): Split account utils to avoid loop imports (#11375)
    # Motivation
    
    The account utils are causing a bit of a loop imports since they are
    nested used in the constructions of both ERC20 tokens and ICRC tokens.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- src/frontend/src/eth/schema/address.schema.ts
+++ src/frontend/src/eth/schema/address.schema.ts
@@ -1,5 +1,6 @@
 import { AddressSchema } from '$lib/schema/address.schema';
-import { isEthAddress } from '$lib/utils/account.utils';
+
+import { isEthAddress } from '$eth/utils/account.utils';
 
 export const EthAddressSchema = AddressSchema.refine((val) => isEthAddress(val));
 
