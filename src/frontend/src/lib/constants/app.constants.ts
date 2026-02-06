1df560d8afd8d05094d2962d7ebf6d54a66579c9
Stefan Berger <stefan.berger@dfinity.org>
2026-02-06 20:57:17 +0100

    keep constants

--- src/frontend/src/lib/constants/app.constants.ts
+++ src/frontend/src/lib/constants/app.constants.ts
@@ -143,7 +143,11 @@ export const AUTH_DERIVATION_ORIGIN =
 			? 'https://tewsx-xaaaa-aaaad-aadia-cai.icp0.io'
 			: undefined;
 
-export const VC_POPUP_WIDTH = 576;
+export const AUTH_POPUP_WIDTH = 576;
+// we need to temporarily increase the height so II 2.0 in "guided mode" fits the popup
+// TODO: revert to 625 after II provides a fix on their end
+export const AUTH_POPUP_HEIGHT = 826;
+export const VC_POPUP_WIDTH = AUTH_POPUP_WIDTH;
 // Screen to allow credential presentation is longer than the authentication screen.
 export const VC_POPUP_HEIGHT = 900;
 
