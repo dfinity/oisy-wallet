a07236bf5c39dd7f5abd300a277aad2c5d5f6a51
Stefan Berger <stefan.berger@dfinity.org>
2026-02-11 19:15:55 +0100

    Update token.utils.ts

--- src/frontend/src/eth/utils/token.utils.ts
+++ src/frontend/src/eth/utils/token.utils.ts
@@ -1,3 +1,4 @@
+import { ETHEREUM_NETWORK_ID } from '$env/networks/networks.eth.env';
 import { ERC20_TWIN_TOKENS_IDS } from '$env/tokens/tokens.erc20.env';
 import { ERC20_ICP_ADDRESS, ERC20_ICP_SYMBOL } from '$eth/constants/erc20-icp.constants';
 import { isTokenErc20 } from '$eth/utils/erc20.utils';
@@ -14,7 +15,8 @@ import { zeroPadValue } from 'ethers/utils';
 export const isErc20Icp = (token: OptionToken): boolean =>
 	token?.symbol === ERC20_ICP_SYMBOL &&
 	isTokenErc20(token) &&
-	token?.address.toLowerCase() === ERC20_ICP_ADDRESS.toLowerCase();
+	token?.address.toLowerCase() === ERC20_ICP_ADDRESS.toLowerCase() &&
+	token?.network.id === ETHEREUM_NETWORK_ID;
 // isTokenErc20(token) && token?.address === ERC20_ICP_ADDRESS && token?.network.id === ETHEREUM_NETWORK_ID; TEST
 
 export const isSupportedErc20TwinTokenId = (tokenId: TokenId): boolean =>
