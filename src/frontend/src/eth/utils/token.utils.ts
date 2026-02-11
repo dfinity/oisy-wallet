8467140a1024964518b4b0dcfd024c25f57e1535
Stefan Berger <stefan.berger@dfinity.org>
2026-02-11 16:30:31 +0100

    Refactor isErc20Icp function to remove symbol check

--- src/frontend/src/eth/utils/token.utils.ts
+++ src/frontend/src/eth/utils/token.utils.ts
@@ -1,5 +1,5 @@
 import { ERC20_TWIN_TOKENS_IDS } from '$env/tokens/tokens.erc20.env';
-import { ERC20_ICP_ADDRESS, ERC20_ICP_SYMBOL } from '$eth/constants/erc20-icp.constants';
+import { ERC20_ICP_ADDRESS } from '$eth/constants/erc20-icp.constants';
 import { isTokenErc20 } from '$eth/utils/erc20.utils';
 import { isSupportedEthTokenId } from '$eth/utils/eth.utils';
 import { isSupportedEvmNativeTokenId } from '$evm/utils/native-token.utils';
@@ -12,9 +12,7 @@ import { formatToken } from '$lib/utils/format.utils';
 import { zeroPadValue } from 'ethers/utils';
 
 export const isErc20Icp = (token: OptionToken): boolean =>
-	token?.symbol === ERC20_ICP_SYMBOL &&
-	isTokenErc20(token) &&
-	token?.address.toLowerCase() === ERC20_ICP_ADDRESS.toLowerCase();
+	isTokenErc20(token) &&	token?.address.toLowerCase() === ERC20_ICP_ADDRESS.toLowerCase();
 // isTokenErc20(token) && token?.address === ERC20_ICP_ADDRESS && token?.network.id === ETHEREUM_NETWORK_ID; TEST
 
 export const isSupportedErc20TwinTokenId = (tokenId: TokenId): boolean =>
