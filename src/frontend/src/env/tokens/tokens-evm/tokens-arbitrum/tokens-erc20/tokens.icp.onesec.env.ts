4a39bef4c692c401e7459428de5593456252c754
Stefan Berger <stefan.berger@dfinity.org>
2026-02-13 21:26:50 +0100

    Create tokens.icp.onesec.env.ts

---
+++ src/frontend/src/env/tokens/tokens-evm/tokens-arbitrum/tokens-erc20/tokens.icp.onesec.env.ts
@@ -0,0 +1,25 @@
+import { ARBITRUM_MAINNET_NETWORK } from '$env/networks/networks-evm/networks.evm.arbitrum.env';
+import { ICP_ONESEC_TOKEN_GROUP } from '$env/tokens/groups/groups.icp.onesec.env';
+import icpOnesec from '$icp/assets/icp-onesec.webp';
+import type { RequiredEvmErc20Token } from '$evm/types/erc20';
+import type { TokenId } from '$lib/types/token';
+import { parseTokenId } from '$lib/validation/token.validation';
+
+const ICP_ONESEC_DECIMALS = 8;
+
+const ICP_ONESEC_SYMBOL = 'ICP';
+
+export const ICP_ONESEC_TOKEN_ID: TokenId = parseTokenId(ICP_ONESEC_SYMBOL);
+
+export const ICP_ONESEC_TOKEN: RequiredEvmErc20Token = {
+	id: ICP_ONESEC_TOKEN_ID,
+	network: ARBITRUM_MAINNET_NETWORK,
+	standard: { code: 'erc20' },
+	category: 'default',
+	name: 'ICP (Onesec)',
+	symbol: ICP_ONESEC_SYMBOL,
+	decimals: ICP_ONESEC_DECIMALS,
+	icon: icpOnesec,
+	address: '0x00f3C42833C3170159af4E92dbb451Fb3F708917',
+	groupData: ICP_ONESEC_TOKEN_GROUP
+};
