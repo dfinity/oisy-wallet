196d28e5be250134ccb9fd3e9dd4c0fda02439ee
Stefan Berger <stefan.berger@dfinity.org>
2026-02-13 18:16:17 +0100

    Create groups.icp.onesec.env.ts

---
+++ src/frontend/src/env/tokens/groups/groups.icp.onesec.env.ts
@@ -0,0 +1,14 @@
+import icp from '$icp/assets/icp-onesec.webp';
+import type { TokenGroupData, TokenGroupId } from '$lib/types/token-group';
+import { parseTokenGroupId } from '$lib/validation/token-group.validation';
+
+const ICP_TOKEN_GROUP_SYMBOL = 'ICP';
+
+export const ICP_TOKEN_GROUP_ID: TokenGroupId = parseTokenGroupId(ICP_TOKEN_GROUP_SYMBOL);
+
+export const ICP_TOKEN_GROUP: TokenGroupData = {
+	id: ICP_TOKEN_GROUP_ID,
+	icon: icp,
+	name: 'ICP (Onesec)',
+	symbol: ICP_TOKEN_GROUP_SYMBOL
+};
