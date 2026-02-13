dc9439ab05bbeab97755956d952c37393e59a46d
Stefan Berger <stefan.berger@dfinity.org>
2026-02-13 18:21:09 +0100

    Create tokens.icp.onesec.env.ts

---
+++ src/frontend/src/env/tokens/tokens-erc20/tokens.icp.onesec.env.ts
@@ -0,0 +1,26 @@
+import { ETHEREUM_NETWORK } from '$env/networks/networks.eth.env';
+import { ICP_ONESEC_TOKEN_GROUP } from '$env/tokens/groups/groups.icp.onesec.env';
+import type { RequiredAdditionalErc20Token } from '$eth/types/erc20';
+import icp from '$icp/assets/icp-onesec.webp';
+import type { TokenId } from '$lib/types/token';
+import { parseTokenId } from '$lib/validation/token.validation';
+
+const ICP_DECIMALS = 8;
+
+const ICP_SYMBOL = 'ICP•OneSec';
+
+export const ICP_ONESEC_TOKEN_ID: TokenId = parseTokenId(ICP_SYMBOL);
+
+export const ICP_ONESEC_TOKEN: RequiredAdditionalErc20Token = {
+	id: ICP_ONESEC_TOKEN_ID,
+	network: ETHEREUM_NETWORK,
+	standard: 'erc20',
+	category: 'default',
+	name: 'ICP (Onesec)',
+	symbol: ICP_SYMBOL,
+	decimals: ICP_DECIMALS,
+	icon: icp,
+	address: '0x00f3C42833C3170159af4E92dbb451Fb3F708917',
+	exchange: 'erc20',
+	groupData: ICP_ONESEC_TOKEN_GROUP
+};
