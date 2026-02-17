41065b25f0b8c4005772627431587f076ec78e1d
Stefan Berger <stefan.berger@dfinity.org>
2026-02-17 21:43:30 +0100

    Create tokens.cbbtc.env.ts

---
+++ src/frontend/src/env/tokens/tokens-erc20/tokens.cbbtc.env.ts
@@ -0,0 +1,28 @@
+import { ETHEREUM_NETWORK } from '$env/networks/networks.eth.env';
+import { CBTC_TOKEN_GROUP } from '$env/tokens/groups/groups.cbbtc.env';
+import type { RequiredErc20Token } from '$eth/types/erc20';
+import cbbtc from '$icp-eth/assets/cbbtc.webp';
+import type { TokenId } from '$lib/types/token';
+import { parseTokenId } from '$lib/validation/token.validation';
+
+export const CBBTC_DECIMALS = 8;
+
+export const CBTC_SYMBOL = 'cbBTC';
+
+export const CBBTC_TOKEN_ID: TokenId = parseTokenId(CBBTC_SYMBOL);
+
+export const CBBTC_TOKEN: RequiredErc20Token = {
+	id: CBBTC_TOKEN_ID,
+	network: ETHEREUM_NETWORK,
+	standard: { code: 'erc20' },
+	category: 'default',
+	name: 'Coinbase Wrapped BTC',
+	symbol: CBBTC_SYMBOL,
+	decimals: CBBTC_DECIMALS,
+	icon: cbbtc,
+	address: '0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf',
+	groupData: CBBTC_TOKEN_GROUP,
+	buy: {
+		onramperId: 'cbbtc_ethereum'
+	}
+};
