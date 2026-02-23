0cea00064443a886ff1a2fb1ffc8ade24a4c615a
Stefan Berger <stefan.berger@dfinity.org>
2026-02-23 16:56:58 +0100

    Update tokens.erc20.env.ts

--- src/frontend/src/env/tokens/tokens-evm/tokens-base/tokens.erc20.env.ts
+++ src/frontend/src/env/tokens/tokens-evm/tokens-base/tokens.erc20.env.ts
@@ -4,7 +4,7 @@ import {
 	EURC_TOKEN,
 	SEPOLIA_EURC_TOKEN
 } from '$env/tokens/tokens-evm/tokens-base/tokens-erc20/tokens.eurc.env';
-import { ICP_ONESEC_TOKEN } from '$env/tokens/tokens-evm/tokens-base/tokens-erc20/tokens.icp.onesec.env';
+import { ICP_TOKEN } from '$env/tokens/tokens-evm/tokens-base/tokens-erc20/tokens.icp.env';
 import { SPX_TOKEN } from '$env/tokens/tokens-evm/tokens-base/tokens-erc20/tokens.spx.env';
 import {
 	SEPOLIA_USDC_TOKEN,
@@ -20,7 +20,7 @@ const BASE_ERC20_TOKENS_SEPOLIA: RequiredEvmErc20Token[] = [SEPOLIA_USDC_TOKEN, 
 const BASE_ERC20_TOKENS_MAINNET: RequiredEvmErc20Token[] = [
 	CBBTC_TOKEN,
 	EURC_TOKEN,
-	ICP_ONESEC_TOKEN,
+	ICP_TOKEN,
 	SPX_TOKEN,
 	USDC_TOKEN,
 	WETH_TOKEN,
