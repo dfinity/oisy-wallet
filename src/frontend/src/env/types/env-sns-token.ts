7f8ec039b5cdc8bcdedcc8cd1fb1e346ac046c58
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-02-27 08:39:14 +0100

    revert

--- src/frontend/src/env/types/env-sns-token.ts
+++ src/frontend/src/env/types/env-sns-token.ts
@@ -1,4 +1,9 @@
 import type { EnvSnsTokenSchema } from '$env/schema/env-sns-token.schema';
+import type { EnvIcrcTokenMetadataWithIcon } from '$env/types/env-icrc-token';
 import type * as z from 'zod';
 
 export type EnvSnsToken = z.infer<typeof EnvSnsTokenSchema>;
+
+export type EnvSnsTokenWithIcon = Omit<EnvSnsToken, 'metadata'> & {
+	metadata: EnvIcrcTokenMetadataWithIcon;
+};
