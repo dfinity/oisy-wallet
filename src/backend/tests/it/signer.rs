f196fb3f857b61ceb1daf9e03342b0a84c22fc65
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-03-02 17:07:38 +0100

    import

--- src/backend/tests/it/signer.rs
+++ src/backend/tests/it/signer.rs
@@ -11,15 +11,27 @@ use shared::types::{
         },
         GetAllowedCyclesError, GetAllowedCyclesResponse,
     },
+    user_profile::UserProfile,
     Stats,
 };
 
-use crate::{
-    utils::{
-        mock::VC_HOLDER,
-        pocketic::{controller, pic_canister::PicCanisterTrait, setup, BackendBuilder},
-    },
+use crate::utils::{
+    mock::VC_HOLDER,
+    pocketic::{controller, pic_canister::PicCanisterTrait, setup, BackendBuilder, PicBackend},
 };
+
+pub fn call_create_user_profile(
+    pic_setup: &PicBackend,
+    caller: Principal,
+) -> Result<UserProfile, String> {
+    let result = pic_setup.update::<UserProfile>(caller, "create_user_profile", ());
+    assert!(
+        result.is_ok(),
+        "Expected Ok, but got Err: {}",
+        result.unwrap_err()
+    );
+    result
+}
 
 #[test]
 fn test_topup_cannot_be_called_if_not_controller() {
