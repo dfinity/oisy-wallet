f38297b94b3e106692576e9586915324a87ae98a
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2026-03-03 14:07:21 +0100

    refactor(frontend): remove GLDT stake UI components

--- src/frontend/src/routes/(app)/earn/gold/+page.ts
+++
@@ -1,6 +0,0 @@
-import { loadRouteParams, type RouteParams } from '$lib/utils/nav.utils';
-import type { LoadEvent } from '@sveltejs/kit';
-// eslint-disable-next-line local-rules/no-relative-imports
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = ($event: LoadEvent): RouteParams => loadRouteParams($event);
