3f691df0daa85ab1899361e329d03e683590df2f
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-03-03 09:51:52 +0100

    more

--- src/backend/src/types/maps.rs
+++ src/backend/src/types/maps.rs
@@ -1,48 +1,42 @@
 use std::collections::HashMap;
 
-use candid::{CandidType, Deserialize, Principal};
 use ic_stable_structures::{
     memory_manager::VirtualMemory, DefaultMemoryImpl, StableBTreeMap, StableCell,
 };
 use shared::types::{
-    backend_config::Config,
-    bitcoin::StoredPendingTransaction,
-    contact::StoredContacts,
-    custom_token::{CustomToken, CustomTokenId},
-    exchange::ExchangeRate,
-    pow::StoredChallenge,
-    token::UserToken,
-    user_profile::StoredUserProfile,
-    Timestamp,
+    backend_config::Config, bitcoin::StoredPendingTransaction, contact::StoredContacts,
+    custom_token::CustomToken, pow::StoredChallenge, token::UserToken,
+    user_profile::StoredUserProfile, Timestamp,
 };
 
+use crate::types::storable::{Candid, StoredPrincipal, StoredTokenId};
+
 pub type VMem = VirtualMemory<DefaultMemoryImpl>;
+
 pub type ConfigCell = StableCell<Option<Candid<Config>>, VMem>;
+
 pub type UserTokenMap = StableBTreeMap<StoredPrincipal, Candid<Vec<UserToken>>, VMem>;
+
 pub type CustomTokenMap = StableBTreeMap<StoredPrincipal, Candid<Vec<CustomToken>>, VMem>;
+
 /// Map of (`updated_timestamp`, `user_principal`) to `UserProfile`
 pub type UserProfileMap =
     StableBTreeMap<(Timestamp, StoredPrincipal), Candid<StoredUserProfile>, VMem>;
+
 /// Map of `user_principal` to `updated_timestamp` (in `UserProfile`)
 pub type UserProfileUpdatedMap = StableBTreeMap<StoredPrincipal, Timestamp, VMem>;
-pub type PowChallengeMap = StableBTreeMap<StoredPrincipal, Candid<StoredChallenge>, VMem>;
-#[derive(Default)]
-pub struct Candid<T>(pub T)
-where
-    T: CandidType + for<'de> Deserialize<'de>;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub struct StoredPrincipal(pub Principal);
+pub type PowChallengeMap = StableBTreeMap<StoredPrincipal, Candid<StoredChallenge>, VMem>;
+
 // Define a new type for the contact storage
 pub type ContactMap = StableBTreeMap<StoredPrincipal, Candid<StoredContacts>, VMem>;
 
 pub type PendingTransactionsMap = HashMap<String, Vec<StoredPendingTransaction>>;
+
 pub type BtcUserPendingTransactionsMap =
     StableBTreeMap<StoredPrincipal, Candid<PendingTransactionsMap>, VMem>;
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub struct StoredTokenId(pub CustomTokenId);
-
 pub type TokenActivityMap = StableBTreeMap<StoredTokenId, Timestamp, VMem>;
+
 
 pub type ExchangeRateMap = StableBTreeMap<StoredTokenId, Candid<ExchangeRate>, VMem>;
