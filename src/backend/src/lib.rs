c006a0a1bb1e162e35c2fa37b7afc2ea419cf19d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-03-03 17:40:05 +0100

    refactor(backend): Move admin methods to specific rust module (#11785)
    # Motivation
    
    In an effort to re-organize the backend codebase, we move the admin
    methods to a specific module.

--- src/backend/src/lib.rs
+++ src/backend/src/lib.rs
@@ -1,9 +1,7 @@
 use candid::Principal;
-use ic_cdk::{api::time, export_candid, init, post_upgrade, query, update};
-use serde_bytes::ByteBuf;
+use ic_cdk::{api::time, export_candid, init, post_upgrade};
 use shared::{
     http::{HttpRequest, HttpResponse},
-    metrics::get_metrics,
     std_canister_status,
     types::{
         agreement::UpdateUserAgreementsRequest,
@@ -33,7 +31,6 @@ use shared::{
 };
 
 use crate::{
-    guards::caller_is_allowed,
     state::{mutate_state, read_config, read_state, set_config},
     types::storable::{Candid, StoredPrincipal},
 };
@@ -89,65 +86,6 @@ pub fn post_upgrade(arg: Option<Arg>) {
     bitcoin::api::init_fee_percentiles_cache();
 
     housekeeping::start_periodic_housekeeping_timers();
-}
-
-/// Gets the canister configuration.
-#[query(guard = "caller_is_allowed")]
-#[must_use]
-pub fn config() -> Config {
-    read_config(Clone::clone)
-}
-
-/// Processes external HTTP requests.
-#[query]
-#[allow(clippy::needless_pass_by_value)]
-#[must_use]
-pub fn http_request(request: HttpRequest) -> HttpResponse {
-    let path = request
-        .url
-        .split('?')
-        .next()
-        .unwrap_or_else(|| unreachable!("Even splitting an empty string yields one entry"));
-    match path {
-        "/metrics" => get_metrics(),
-        _ => HttpResponse {
-            status_code: 404,
-            headers: vec![],
-            body: ByteBuf::from(String::from("Not found.")),
-        },
-    }
-}
-
-/// API method to get cycle balance and burn rate.
-#[update]
-pub async fn get_canister_status() -> std_canister_status::CanisterStatusResultV2 {
-    std_canister_status::get_canister_status_v2().await
-}
-
-/// Gets statistics about the canister.
-///
-/// Note: This is a private method, restricted to authorized users, as some stats may not be
-/// suitable for public consumption.
-#[query(guard = "caller_is_allowed")]
-#[must_use]
-pub fn stats() -> Stats {
-    read_state(|s| Stats::from(s))
-}
-
-/// Gets account creation timestamps.
-#[query(guard = "caller_is_allowed")]
-#[must_use]
-pub fn get_account_creation_timestamps() -> Vec<(Principal, Timestamp)> {
-    read_state(|s| {
-        s.user_profile
-            .iter()
-            .map(|entry| {
-                let (_updated, StoredPrincipal(principal)) = *entry.key();
-                let user = entry.value();
-                (principal, user.created_timestamp)
-            })
-            .collect()
-    })
 }
 
 export_candid!();
