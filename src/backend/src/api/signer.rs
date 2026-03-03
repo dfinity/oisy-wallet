b4db5bb76811333a688dde3e5fe9f811c3ad9ca4
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-03-03 13:51:59 +0100

    rebase

--- src/backend/src/api/signer.rs
+++ src/backend/src/api/signer.rs
@@ -10,9 +10,8 @@ use shared::types::{
 
 use crate::{
     guards::{caller_is_controller, caller_is_not_anonymous},
-    signer,
     housekeeping::ALLOW_SIGNING_RATE_LIMITER,
-    pow, rate_limiter, signer,
+    rate_limiter, signer,
 };
 
 /// Adds cycles to the cycles ledger, if it is below a certain threshold.
@@ -58,10 +57,10 @@ pub async fn get_allowed_cycles() -> GetAllowedCyclesResult {
 #[update(guard = "caller_is_not_anonymous")]
 pub async fn allow_signing() -> AllowSigningResult {
     async fn inner() -> Result<AllowSigningResponse, AllowSigningError> {
-              ALLOW_SIGNING_RATE_LIMITER
+        ALLOW_SIGNING_RATE_LIMITER
             .with(rate_limiter::RateLimiter::check_caller)
             .map_err(AllowSigningError::RateLimited)?;
-      
+
         // Passing None to apply the old cycle calculation logic
         signer::allow_signing(None).await?;
 
