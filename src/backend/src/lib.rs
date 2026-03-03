c3634fabb89adbdfe0eff999de6088061806c3f0
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-03-03 16:21:19 +0100

    refactor(backend): Move candid unittests in library

--- src/backend/src/lib.rs
+++ src/backend/src/lib.rs
@@ -55,9 +55,6 @@ mod token_activity;
 mod types;
 mod user_profile;
 
-#[cfg(test)]
-mod tests;
-
 #[cfg(feature = "canbench-rs")]
 mod benchmark;
 
@@ -251,3 +248,38 @@ pub fn get_account_creation_timestamps() -> Vec<(Principal, Timestamp)> {
 }
 
 export_candid!();
+
+#[cfg(test)]
+mod tests {
+    use std::path::{Path, PathBuf};
+
+    use candid_parser::utils::{service_compatible, CandidSource};
+
+     use super::*;
+
+    /// Determines the workspace directory when running tests.
+    fn workspace_dir() -> PathBuf {
+        let output = std::process::Command::new(env!("CARGO"))
+            .arg("locate-project")
+            .arg("--workspace")
+            .arg("--message-format=plain")
+            .output()
+            .unwrap()
+            .stdout;
+        let cargo_path = Path::new(std::str::from_utf8(&output).unwrap().trim());
+        cargo_path.parent().unwrap().to_path_buf()
+    }
+
+    /// Checks candid interface type compatibility with production.
+    #[test]
+    #[ignore] // Not run unless requested explicitly
+    fn check_candid_interface_compatibility() {
+        let canister_interface = __export_service();
+        let prod_interface_file = workspace_dir().join("target/ic/candid/backend.ic.did");
+        service_compatible(
+            CandidSource::Text(&canister_interface),
+            CandidSource::File(&prod_interface_file.as_path()),
+        )
+        .expect("The proposed canister interface is not compatible with the production interface");
+    }
+}
