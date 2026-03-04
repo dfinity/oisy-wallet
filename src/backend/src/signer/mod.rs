8700fff5d887efe55bba2fd11536484cdc26f722
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-03-04 14:27:13 +0100

    more

--- src/backend/src/signer/mod.rs
+++ src/backend/src/signer/mod.rs
@@ -1,7 +1,7 @@
 mod canister_ids;
 mod service;
 
-pub(crate) use service::SUFFICIENT_CYCLES_THRESHOLD;
-pub use service::{
+pub(crate) use service::{
     allow_signing, btc_principal_to_p2wpkh_address, get_allowed_cycles, top_up_cycles_ledger,
+    SUFFICIENT_CYCLES_THRESHOLD,
 };
