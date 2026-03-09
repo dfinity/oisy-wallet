7511212144af830d95edcc708e4462420685a0ed
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-03-09 13:55:25 +0100

    feat(frontend): Use built-in contacts in util mapAddressToName

--- src/frontend/src/eth/components/transactions/EthTransactionModal.svelte
+++ src/frontend/src/eth/components/transactions/EthTransactionModal.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { Modal } from '@dfinity/gix-components';
 	import { nonNullish, notEmptyString } from '@dfinity/utils';
-	import { ETHEREUM_TOKEN_ID, SEPOLIA_TOKEN_ID } from '$env/tokens/tokens.eth.env';
 	import EthTransactionStatus from '$eth/components/transactions/EthTransactionStatus.svelte';
 	import { ercFungibleTokens } from '$eth/derived/erc-fungible.derived';
 	import { erc20Tokens } from '$eth/derived/erc20.derived';
@@ -13,8 +12,6 @@
 		isMaxUint256,
 		mapAddressToName
 	} from '$eth/utils/transactions.utils';
-	import { ckEthMinterInfoStore } from '$icp-eth/stores/cketh.store';
-	import type { OptionCertifiedMinterInfo } from '$icp-eth/types/cketh-minter';
 	import List from '$lib/components/common/List.svelte';
 	import ListItem from '$lib/components/common/ListItem.svelte';
 	import ModalHero from '$lib/components/common/ModalHero.svelte';
@@ -39,10 +36,10 @@
 		shortenWithMiddleEllipsis
 	} from '$lib/utils/format.utils';
 	import { replacePlaceholders } from '$lib/utils/i18n.utils';
-	import { isNetworkIdSepolia } from '$lib/utils/network.utils';
 	import { isTokenNonFungible } from '$lib/utils/nft.utils';
 	import { findNft } from '$lib/utils/nfts.utils';
 	import { parseNftId } from '$lib/validation/nft.validation';
+	import {ckMinterBuiltInContacts} from "$icp-eth/derived/ck-minter-contacts.derived";
 
 	interface Props {
 		transaction: EthTransactionUi;
@@ -97,11 +94,7 @@
 		notEmptyString(to) ? `${explorerBaseUrl}/address/${to}` : undefined
 	);
 
-	let ckMinterInfo: OptionCertifiedMinterInfo = $derived(
-		$ckEthMinterInfoStore?.[
-			isNetworkIdSepolia(token?.network.id) ? SEPOLIA_TOKEN_ID : ETHEREUM_TOKEN_ID
-		]
-	);
+
 
 	let approveSpenderExplorerUrl = $derived(
 		nonNullish(approveSpender) ? `${explorerBaseUrl}/address/${approveSpender}` : undefined
@@ -113,7 +106,7 @@
 					address: from,
 					networkId: token.network.id,
 					erc20Tokens: $erc20Tokens,
-					ckMinterInfo
+						builtInContacts: $ckMinterBuiltInContacts
 				}) ?? from)
 			: from
 	);
@@ -124,7 +117,7 @@
 					address: to,
 					networkId: token.network.id,
 					erc20Tokens: $erc20Tokens,
-					ckMinterInfo
+						builtInContacts: $ckMinterBuiltInContacts
 				}) ?? to)
 			: to
 	);
@@ -135,7 +128,7 @@
 					address: approveSpender,
 					networkId: token.network.id,
 					erc20Tokens: $erc20Tokens,
-					ckMinterInfo
+					builtInContacts: $ckMinterBuiltInContacts
 				}) ?? undefined)
 			: undefined
 	);
