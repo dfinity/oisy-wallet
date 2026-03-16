1d1132b2a98979c2593914d7173cb7aff67f9515
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-03-16 14:05:40 +0100

    feat(frontend): Hide balance skeletons for disabled tokens in Swap modal

--- src/frontend/src/lib/components/swap/SwapTokensList.svelte
+++ src/frontend/src/lib/components/swap/SwapTokensList.svelte
@@ -18,6 +18,7 @@
 	} from '$lib/stores/modal-tokens-list.store';
 	import { SWAP_CONTEXT_KEY, type SwapContext } from '$lib/stores/swap.store';
 	import type { Token } from '$lib/types/token';
+	import type { TokenToggleable } from '$lib/types/token-toggleable';
 	import type { TokenUi } from '$lib/types/token-ui';
 	import { mapTokenUi } from '$lib/utils/token.utils';
 	import { sortTokens } from '$lib/utils/tokens.utils';
@@ -34,14 +35,17 @@
 
 	const { setTokens } = getContext<ModalTokensListContext>(MODAL_TOKENS_LIST_CONTEXT_KEY);
 
-	let tokensUi: TokenUi[] = $derived(
+	let tokensUi: TokenToggleable<TokenUi>[] = $derived(
 		[
 			{ ...ICP_TOKEN, enabled: true },
 			...$allSortedIcrcTokens,
 			...(VELORA_SWAP_ENABLED ? $allCrossChainSwapTokens : [])
 		]
-			.filter((token: Token) => token.id !== $sourceToken?.id && token.id !== $destinationToken?.id)
-			.map((token: Token) =>
+			.filter(
+				(token: TokenToggleable<Token>) =>
+					token.id !== $sourceToken?.id && token.id !== $destinationToken?.id
+			)
+			.map((token: TokenToggleable<Token>) =>
 				mapTokenUi({
 					token,
 					$balances: $balancesStore,
