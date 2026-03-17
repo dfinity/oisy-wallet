db4871e70896877c5818f428a5b6ba2cef165140
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-03-17 17:56:27 +0100

    more

---
+++ src/frontend/src/lib/components/tokens/TokensAssetTypeToggle.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Toggle } from '@dfinity/gix-components';
+	import { tokenCategoryFilterEnabled } from '$lib/derived/settings.derived';
+	import { i18n } from '$lib/stores/i18n.store';
+	import { tokenCategoryFilterEnabledStore } from '$lib/stores/settings.store';
+
+	let checked = $derived($tokenCategoryFilterEnabled);
+
+	const toggleHide = () =>
+		tokenCategoryFilterEnabledStore.set({
+			key: 'token-category-filter-enabled',
+			value: { enabled: !checked }
+		});
+</script>
+
+<svelte:window onoisyToggleAssetTypes={toggleHide} />
+
+<Toggle ariaLabel={$i18n.tokens.text.hide_asset_types} bind:checked on:nnsToggle={toggleHide} />
