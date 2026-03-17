a962697212bd96c6eff426527f8cde77e1e681c9
Denys Karmazyn <denys.karmazyn@dfinity.org>
2026-03-17 11:35:52 +0100

    feat(frontend): implement HarvestStakeFees (#11986)
    # Motivation
    
    The next component we need to implement is `HarvestStakeFees` - it will
    be used in both staking and unstaking flows.

---
+++ src/frontend/src/eth/components/stake/harvest-autopilot/HarvestStakeFees.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import EthFeeDisplay from '$eth/components/fee/EthFeeDisplay.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
+</script>
+
+<EthFeeDisplay>
+	{#snippet label()}
+		{$i18n.fee.text.network_fee}
+	{/snippet}
+</EthFeeDisplay>
