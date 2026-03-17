4b8ba76582bac82a768fa30ef8e2c43c01802985
Stefan Berger <stefan.berger@dfinity.org>
2026-03-17 16:55:48 +0100

    Update tokens.wbtc.env.ts

--- src/frontend/src/env/tokens/tokens-evm/tokens-arbitrum/tokens-erc20/tokens.wbtc.env.ts
+++ src/frontend/src/env/tokens/tokens-evm/tokens-arbitrum/tokens-erc20/tokens.wbtc.env.ts
@@ -2,6 +2,7 @@ import { ARBITRUM_MAINNET_NETWORK } from '$env/networks/networks-evm/networks.ev
 import { WBTC_TOKEN_GROUP } from '$env/tokens/groups/groups.wbtc.env';
 import wbtc from '$eth/assets/wbtc.webp';
 import type { RequiredEvmErc20Token } from '$evm/types/erc20';
+import { TokenCategoryTagValue, TokenTagType } from '$lib/enums/token-tag';
 import type { TokenId } from '$lib/types/token';
 import { parseTokenId } from '$lib/validation/token.validation';
 
