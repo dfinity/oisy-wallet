a7b36f358f824df2cba75791ee315f55386763f9
Denys Karmazyn <denys.karmazyn@dfinity.org>
2026-03-17 08:53:31 +0100

    refactor(frontend): remove GLDT stakig/unstaking (#11969)
    # Motivation
    
    We continue cleaning up GLDT stuff from the codebase. In the next step,
    we delete all things related to the staking/unstaking process.

--- src/frontend/src/lib/components/earning/StakeContext.svelte
+++ src/frontend/src/lib/components/earning/StakeContext.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import { setContext, type Snippet } from 'svelte';
-	import GldtStakeContext from '$icp/components/stake/gldt/GldtStakeContext.svelte';
 	import {
 		GLDT_STAKE_CONTEXT_KEY,
-		type GldtStakeContext as GldtStakeContextType,
+		type GldtStakeContext,
 		gldtStakeStore
 	} from '$icp/stores/gldt-stake.store';
 
@@ -13,11 +12,9 @@
 
 	let { children }: Props = $props();
 
-	setContext<GldtStakeContextType>(GLDT_STAKE_CONTEXT_KEY, {
+	setContext<GldtStakeContext>(GLDT_STAKE_CONTEXT_KEY, {
 		store: gldtStakeStore
 	});
 </script>
 
-<GldtStakeContext>
-	{@render children()}
-</GldtStakeContext>
+{@render children()}
