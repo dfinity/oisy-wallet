6d200227b8412ea84b2ee750d9b7eb025dbf9dee
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-03-18 09:27:08 +0100

    style(frontend): Remove icon from token list menu toggle (#12013)
    # Motivation
    
    Visually, we prefer to remove the icon for the toggle inside the token
    menu.
    
    <img width="754" height="412" alt="Screenshot 2026-03-17 at 18 50 14"
    src="https://github.com/user-attachments/assets/f674eee9-be27-465a-984a-c8aea0c7b22c"
    />

--- src/frontend/src/lib/components/tokens/TokensMenu.svelte
+++ src/frontend/src/lib/components/tokens/TokensMenu.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import List from '$lib/components/common/List.svelte';
 	import ListItem from '$lib/components/common/ListItem.svelte';
-	import IconHide from '$lib/components/icons/IconHide.svelte';
 	import IconManage from '$lib/components/icons/lucide/IconManage.svelte';
 	import TokensZeroBalanceToggle from '$lib/components/tokens/TokensZeroBalanceToggle.svelte';
 	import ButtonIcon from '$lib/components/ui/ButtonIcon.svelte';
@@ -49,9 +48,6 @@
 		<List condensed noPadding>
 			<ListItem>
 				<LogoButton fullWidth onClick={toggleHideZeros}>
-					{#snippet logo()}
-						<IconHide />
-					{/snippet}
 					{#snippet title()}
 						<span class="text-sm font-normal">{$i18n.tokens.text.hide_zero_balances}</span>
 					{/snippet}
