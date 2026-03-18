15336b8104c9c8518861e587cef5a5c86b6b6cdf
Denys Karmazyn <denys.karmazyn@dfinity.org>
2026-03-18 09:54:30 +0100

    feat(frontend): update stake/unstake modals with Harvest autopilots (#12019)
    # Motivation
    
    We can now update existing Stake and Unstake modal components with
    Harvest autopilots.

--- src/frontend/src/tests/lib/components/stake/StakeModal.spec.ts
+++ src/frontend/src/tests/lib/components/stake/StakeModal.spec.ts
@@ -1,5 +1,7 @@
+import { BAUTOPILOT_USDC_TOKEN } from '$env/tokens/tokens-evm/tokens-base/tokens-erc4626/tokens.bautopilot_usdc.env';
 import StakeModal from '$lib/components/stake/StakeModal.svelte';
 import type { Token } from '$lib/types/token';
+import type { Vault } from '$lib/types/vaults';
 import en from '$tests/mocks/i18n.mock';
 import { mockValidIcrcToken } from '$tests/mocks/ic-tokens.mock';
 import { render } from '@testing-library/svelte';
@@ -17,5 +19,21 @@ describe('StakeModal', () => {
 		});
 
 		expect(container).toHaveTextContent(en.stake.text.unsupported_token_staking);
+	});
+
+	it('should not display unsupported message when vault is harvest autopilot', () => {
+		const mockVault: Vault = {
+			token: { ...BAUTOPILOT_USDC_TOKEN, enabled: true },
+			apy: '5.5'
+		};
+
+		const { container } = render(StakeModal, {
+			props: {
+				token: BAUTOPILOT_USDC_TOKEN as Token,
+				vault: mockVault
+			}
+		});
+
+		expect(container).not.toHaveTextContent(en.stake.text.unsupported_token_staking);
 	});
 });
