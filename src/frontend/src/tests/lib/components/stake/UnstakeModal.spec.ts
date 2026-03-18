5e2e4d4af5ebd4c82be506af83d5ec9afbf157b7
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2026-03-18 09:52:33 +0100

    feat(frontend): add new modals for Harvest stake/unstake

--- src/frontend/src/tests/lib/components/stake/UnstakeModal.spec.ts
+++ src/frontend/src/tests/lib/components/stake/UnstakeModal.spec.ts
@@ -4,6 +4,12 @@ import en from '$tests/mocks/i18n.mock';
 import { mockValidIcrcToken } from '$tests/mocks/ic-tokens.mock';
 import { render } from '@testing-library/svelte';
 
+vi.mock('$eth/services/eth-listener.services', () => ({
+	initMinedTransactionsListener: vi.fn(() => ({
+		disconnect: vi.fn()
+	}))
+}));
+
 describe('UnstakeModal', () => {
 	beforeEach(() => {
 		vi.resetAllMocks();
