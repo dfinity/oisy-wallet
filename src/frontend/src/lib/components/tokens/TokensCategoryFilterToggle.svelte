742ee93beb4da10c4a69ea6ddd6353b98d388c19
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-03-18 16:38:53 +0100

    more

--- src/frontend/src/lib/components/tokens/TokensCategoryFilterToggle.svelte
+++ src/frontend/src/lib/components/tokens/TokensCategoryFilterToggle.svelte
@@ -2,15 +2,21 @@
 	import { Toggle } from '@dfinity/gix-components';
 	import { hideTokenCategoryFilter } from '$lib/derived/settings.derived';
 	import { i18n } from '$lib/stores/i18n.store';
-	import { hideTokenCategoryFilterStore } from '$lib/stores/settings.store';
+	import {
+		hideTokenCategoryFilterStore,
+		tokenCategoryFilterStore
+	} from '$lib/stores/settings.store';
 
 	let checked = $derived($hideTokenCategoryFilter);
 
-	const toggleHide = () =>
+	const toggleHide = () => {
 		hideTokenCategoryFilterStore.set({
 			key: 'hide-token-category-filter',
 			value: { enabled: !checked }
 		});
+
+		tokenCategoryFilterStore.reset({ key: 'token-category-filter' })
+	};
 </script>
 
 <svelte:window onoisyToggleTokenCategoryFilter={toggleHide} />
