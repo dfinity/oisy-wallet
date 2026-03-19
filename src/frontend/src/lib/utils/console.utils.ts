b06b409b64a9d6378230c919e364b4689ab2ec71
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-03-19 15:25:26 +0100

    feat(frontend): Add more error verbosity in debug mode for STAGING

--- src/frontend/src/lib/utils/console.utils.ts
+++ src/frontend/src/lib/utils/console.utils.ts
@@ -1,9 +1,20 @@
+import { LOCAL, STAGING } from '$lib/constants/app.constants';
 import { formatIcCallError } from '$lib/utils/error.utils';
+
+const VERBOSE = LOCAL || STAGING;
 
 export const consoleError = (...args: unknown[]): void => {
 	console.error(...args.map((arg) => formatIcCallError({ err: arg })));
+
+	if (VERBOSE) {
+		console.debug('[verbose]', ...args);
+	}
 };
 
 export const consoleWarn = (...args: unknown[]): void => {
 	console.warn(...args.map((arg) => formatIcCallError({ err: arg })));
+
+	if (VERBOSE) {
+		console.debug('[verbose]', ...args);
+	}
 };
