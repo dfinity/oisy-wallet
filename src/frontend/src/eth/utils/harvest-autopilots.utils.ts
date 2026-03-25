e7c50d29d82531756f31460c8e321435d5097806
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2026-03-25 10:14:08 +0100

    refactor(frontend): harvest autopilot tracking metadata as util

--- src/frontend/src/eth/utils/harvest-autopilots.utils.ts
+++ src/frontend/src/eth/utils/harvest-autopilots.utils.ts
@@ -1,11 +1,17 @@
 import { HARVEST_AUTOPILOT_ADDRESSES } from '$eth/constants/harvest-autopilots.constants';
 import type { EthCertifiedTransaction } from '$eth/stores/eth-transactions.store';
 import type { OptionEthAddress } from '$eth/types/address';
+import type { Erc20Token } from '$eth/types/erc20';
 import type { Erc20CustomToken } from '$eth/types/erc20-custom-token';
 import type { Erc4626Token } from '$eth/types/erc4626';
 import type { Erc4626CustomToken } from '$eth/types/erc4626-custom-token';
 import { isTokenErc4626 } from '$eth/utils/erc4626.utils';
 import { mapEthTransactionUi } from '$eth/utils/transactions.utils';
+import {
+	PLAUSIBLE_EVENT_CONTEXTS,
+	PLAUSIBLE_EVENT_SOURCES,
+	PLAUSIBLE_EVENT_SUBCONTEXT_EARN
+} from '$lib/enums/plausible';
 import type { Token } from '$lib/types/token';
 import type { TokenUi } from '$lib/types/token-ui';
 import type { StakingTransactionsUiWithToken } from '$lib/types/transaction-ui';
@@ -66,3 +72,26 @@ export const getHarvestAutopilotVaultTransactions = ({
 
 	return result;
 };
+
+export const getHarvestAutopilotBaseTrackingMetadata = ({
+	assetToken,
+	vaultToken
+}: {
+	assetToken: Erc20Token;
+	vaultToken: Erc4626Token;
+}) => ({
+	event_context: PLAUSIBLE_EVENT_CONTEXTS.EARN,
+	event_subcontext: PLAUSIBLE_EVENT_SUBCONTEXT_EARN.HARVEST_AUTOPILOT,
+	source_location: PLAUSIBLE_EVENT_SOURCES.HARVEST_AUTOPILOT,
+	source_sublocation: vaultToken.name,
+	token_network: assetToken.network.name,
+	token_address: assetToken.address,
+	token_standard: assetToken.standard.code,
+	token_symbol: assetToken.symbol,
+	token_name: assetToken.name,
+	token2_network: vaultToken.network.name,
+	token2_address: vaultToken.address,
+	token2_standard: vaultToken.standard.code,
+	token2_symbol: vaultToken.symbol,
+	token2_name: vaultToken.name
+});
