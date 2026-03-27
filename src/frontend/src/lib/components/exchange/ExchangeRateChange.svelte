a9cf4092fb98629f072963e32a22d224b7cae7b3
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-03-27 09:32:28 +0100

    size

--- src/frontend/src/lib/components/exchange/ExchangeRateChange.svelte
+++ src/frontend/src/lib/components/exchange/ExchangeRateChange.svelte
@@ -51,7 +51,7 @@
 		class:text-success-primary={exchangeRateChangeSign === 'positive'}
 		class:text-tertiary={exchangeRateChangeSign === 'zero'}
 	>
-		<span class="inline-block scale-125 transform" class:rotate-180={exchangeRateChangeSign === 'positive'}>
+		<span class="inline-block scale-150 transform" class:rotate-180={exchangeRateChangeSign === 'positive'}>
 			{exchangeRateChangeSymbol}
 		</span>
 		{formattedExchangeRateChange}
