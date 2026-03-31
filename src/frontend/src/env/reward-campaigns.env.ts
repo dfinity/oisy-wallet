9a05287a9b40acfa2e0a60bf6f0d6b841c217f67
Stefan Berger <stefan.berger@dfinity.org>
2026-03-31 10:09:29 +0200

    Change FEATURED_REWARD_CAROUSEL_SLIDE_ID to empty string

--- src/frontend/src/env/reward-campaigns.env.ts
+++ src/frontend/src/env/reward-campaigns.env.ts
@@ -14,4 +14,4 @@ export const SPRINKLES_SEASON_1_EPISODE_5_ID = 'sprinkles_s1e5';
 export const SPRINKLES_SEASON_1_EPISODE_6_ID = 'sprinkles_s1e6';
 
 // TODO for v2.0: set it to SPRINKLES_SEASON_1_EPISODE_6_ID
-export const FEATURED_REWARD_CAROUSEL_SLIDE_ID = null;
+export const FEATURED_REWARD_CAROUSEL_SLIDE_ID = '';
