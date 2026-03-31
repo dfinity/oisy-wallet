d730576825250108fb3222b691cb240abb6ded30
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-03-31 09:09:33 +0200

    style(frontend): Add icon to Swap value difference

--- src/frontend/src/lib/components/swap/SwapValueDifference.svelte
+++ src/frontend/src/lib/components/swap/SwapValueDifference.svelte
@@ -27,16 +27,33 @@
 			destinationTokenExchangeRate: $destinationTokenExchangeRate
 		})
 	);
+
+	let isError = $derived(
+		nonNullish(valueDifference) && valueDifference <= SWAP_VALUE_DIFFERENCE_ERROR_VALUE
+	);
+
+	let isWarning = $derived(
+		nonNullish(valueDifference) &&
+			valueDifference <= SWAP_VALUE_DIFFERENCE_WARNING_VALUE &&
+			valueDifference > SWAP_VALUE_DIFFERENCE_ERROR_VALUE
+	);
+
+	let isSuccess = $derived(
+		nonNullish(valueDifference) && valueDifference > SWAP_VALUE_DIFFERENCE_WARNING_VALUE
+	);
 </script>
 
 {#if nonNullish(valueDifference)}
 	<span
-		class="underline"
-		class:text-error-primary={valueDifference <= SWAP_VALUE_DIFFERENCE_ERROR_VALUE}
-		class:text-success-primary={valueDifference > SWAP_VALUE_DIFFERENCE_WARNING_VALUE}
-		class:text-warning-primary={valueDifference <= SWAP_VALUE_DIFFERENCE_WARNING_VALUE &&
-			valueDifference > SWAP_VALUE_DIFFERENCE_ERROR_VALUE}
+		class="gap-2"
+		class:text-error-primary={isError}
+		class:text-success-primary={isSuccess}
+		class:text-warning-primary={isWarning}
+		class:font-bold={isWarning || isError}
 	>
 		{`${valueDifference > 0 ? '+' : ''}${valueDifference.toFixed(2)}`}%
+		{#if isWarning || isError}
+			⚠
+		{/if}
 	</span>
 {/if}
