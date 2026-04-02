422c150200ca83aa30127ecac3218d5f1920857a
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-04-02 16:51:10 +0200

    feat(frontend): Service to accept providers agreements

---
+++ src/frontend/src/lib/services/provider-agreements.services.ts
@@ -0,0 +1,31 @@
+import { updateProviderAgreements } from '$lib/api/backend.api';
+import type { SaveProviderAgreements } from '$lib/types/api';
+import type { CanisterApiFunctionParams } from '$lib/types/canister';
+import type { NullishIdentity } from '$lib/types/identity';
+import { emit } from '$lib/utils/events.utils';
+import { assertNonNullish, nowInBigIntNanoSeconds } from '@dfinity/utils';
+
+export const acceptProviderAgreement = async ({
+	identity,
+	currentUserVersion
+}: {
+	identity: NullishIdentity;
+	currentUserVersion: CanisterApiFunctionParams<SaveProviderAgreements>['currentUserVersion'];
+}): Promise<void> => {
+	assertNonNullish(identity, 'Identity is required to save a provider agreement.');
+
+	await updateProviderAgreements({
+		identity,
+		providerAgreements: {
+			'NearIntents-Swap': {
+				accepted: true,
+				lastAcceptedTimestamp: nowInBigIntNanoSeconds(),
+				lastUpdatedTimestamp: nowInBigIntNanoSeconds(),
+				textSha256: undefined
+			}
+		},
+		currentUserVersion
+	});
+
+	emit({ message: 'oisyRefreshUserProfile' });
+};
