55529cebf50659243b88c4afe307293996088382
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-04-02 06:53:50 +0200

    chore(frontend): Remove Proof of Uniqueness feature

--- src/frontend/src/lib/constants/credentials.constants.ts
+++ src/frontend/src/lib/constants/credentials.constants.ts
@@ -1,5 +1,3 @@
 // This credential type is defined by the issuer Decide AI
-import { parseBoolEnvVar } from '$lib/utils/env.utils';
 
 export const POUH_CREDENTIAL_TYPE = 'ProofOfUniqueness';
-export const POUH_ENABLED = parseBoolEnvVar(import.meta.env.VITE_POUH_ENABLED);
