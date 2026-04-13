4e2ecdd05c9b6586ca50f235f9305d56cc11ac9b
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-04-13 07:02:42 +0200

    chore(backend): Remove unnecessary Verifiable Credentials mock

--- src/backend/tests/it/utils/mock.rs
+++ src/backend/tests/it/utils/mock.rs
@@ -5,7 +5,6 @@ pub const CONTROLLER: &str = "l3lfs-gak7g-xrbil-j4v4h-aztjn-4jyki-wprso-m27h3-ib
 /// A normal user, without any special permissions.
 pub const USER_1: &str = "7blps-itamd-lzszp-7lbda-4nngn-fev5u-2jvpn-6y3ap-eunp7-kz57e-fqe";
 
-pub const VC_HOLDER: &str = "7eboi-tyuys-aqm4c-w2l7i-vgucm-xvawx-lemzx-6kq2g-f53u7-yvfh2-nae";
 pub const ISSUER_CANISTER_ID: &str = "qdiif-2iaaa-aaaap-ahjaq-cai";
 pub const ISSUER_ORIGIN: &str = "https://dummy-issuer.vc/";
 pub const VC_DERIVATION_ORIGIN: &str = "https://l7rua-raaaa-aaaap-ahh6a-cai.ic0.app";
