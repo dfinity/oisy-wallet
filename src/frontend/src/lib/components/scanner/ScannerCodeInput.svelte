43cc644079808917048246f2910b6b57bbc51c89
Denys Karmazyn <denys.karmazyn@dfinity.org>
2026-04-14 17:41:31 +0200

    feat(frontend): update ScannerCodeInput component (#12423)
    # Motivation
    
    The next step of the Universal Scanner epic is to update
    ScannerCodeInput component.

--- src/frontend/src/lib/components/scanner/ScannerCodeInput.svelte
+++ src/frontend/src/lib/components/scanner/ScannerCodeInput.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-	import { notEmptyString } from '@dfinity/utils';
+	import { nonNullish, notEmptyString } from '@dfinity/utils';
+	import type { Snippet } from 'svelte';
 	import { slide } from 'svelte/transition';
-	import InputText from '$lib/components/ui/InputText.svelte';
+	import InputTextWithAction from '$lib/components/ui/InputTextWithAction.svelte';
 	import { SLIDE_DURATION } from '$lib/constants/transition.constants';
+	import { isDesktop } from '$lib/utils/device.utils';
 
 	interface Props {
 		value: string;
@@ -10,20 +12,30 @@
 		label: string;
 		placeholder: string;
 		name: string;
+		styleClass?: string;
+		children?: Snippet;
 	}
 
-	let { value = $bindable(''), error, label, placeholder, name }: Props = $props();
+	let {
+		value = $bindable(''),
+		error,
+		label,
+		placeholder,
+		name,
+		styleClass = '',
+		children
+	}: Props = $props();
 
 	let isError = $derived(notEmptyString(error));
 
 	let borderColor = $derived(isError ? 'var(--color-border-error-solid)' : 'inherit');
 </script>
 
-<div>
-	<p class="mt-2 mb-1 md:text-center">{label}</p>
+<div class={`${styleClass} p-3`}>
+	<span class="text-sm font-bold">{label}</span>
 
 	<div style={`--input-custom-border-color: ${borderColor}`}>
-		<InputText {name} {placeholder} bind:value />
+		<InputTextWithAction {name} autofocus={isDesktop()} {placeholder} bind:value />
 
 		{#if isError}
 			<p class="text-md mb-0 pt-2 text-error-primary" transition:slide={SLIDE_DURATION}>
@@ -31,4 +43,10 @@
 			</p>
 		{/if}
 	</div>
+
+	{#if nonNullish(children)}
+		<div class="mt-3">
+			{@render children()}
+		</div>
+	{/if}
 </div>
