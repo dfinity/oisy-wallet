ae99fad6cf4d889ef168d5d17638c59180934c68
Stefan Berger <stefan.berger@dfinity.org>
2026-04-15 09:58:38 +0200

    Add CHAT token configuration for ERC20

---
+++ src/frontend/src/env/tokens/tokens-erc20/tokens.chat.env.ts
@@ -0,0 +1,27 @@
+import { ETHEREUM_NETWORK } from '$env/networks/networks.eth.env';
+import { CHAT_TOKEN_GROUP } from '$env/tokens/groups/groups.chat.env';
+import type { RequiredAdditionalErc20Token } from '$eth/types/erc20';
+import { TokenCategoryTagValue, TokenTagType } from '$lib/enums/token-tag';
+import type { TokenId } from '$lib/types/token';
+import { parseTokenId } from '$lib/validation/token.validation';
+import chat from '/icons/icrc/7pail-xaaaa-aaaas-aabmq-cai.png';
+
+const CHAT_DECIMALS = 8;
+
+const CHAT_SYMBOL = 'CHAT';
+
+export const CHAT_TOKEN_ID: TokenId = parseTokenId(CHAT_SYMBOL);
+
+export const CHAT_TOKEN: RequiredAdditionalErc20Token = {
+	id: CHAT_TOKEN_ID,
+	network: ETHEREUM_NETWORK,
+	standard: { code: 'erc20' },
+	category: 'default',
+	tags: [{ type: TokenTagType.CATEGORY, value: TokenCategoryTagValue.CRYPTO }],
+	name: 'CHAT',
+	symbol: CHAT_SYMBOL,
+	decimals: CHAT_DECIMALS,
+	icon: chat,
+	address: '0xDb95092C454235E7e666c4E226dBBbCdeb499d25',
+	groupData: CHAT_TOKEN_GROUP
+};
