483826c054d990070f5dff68dffa605de342bb88
Denys Karmazyn <denys.karmazyn@dfinity.org>
2026-04-16 10:02:38 +0200

    feat(frontend): display ScanInfo as BottomSheet on mobile (#12451)
    # Motivation
    
    We want to display ScanInfo as BottomSheet on mobile screens, similar to
    how we do it in other modals.

---
+++ src/frontend/src/lib/components/scanner/ScannerCodeInfoButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import IconHelpCircle from '$lib/components/icons/IconHelpCircle.svelte';
+	import Button from '$lib/components/ui/Button.svelte';
+	import { i18n } from '$lib/stores/i18n.store';
+	import { replaceOisyPlaceholders } from '$lib/utils/i18n.utils';
+
+	interface Props {
+		onclick: () => void;
+	}
+
+	let { onclick }: Props = $props();
+</script>
+
+<Button fullWidth link {onclick} styleClass="text-secondary bg-surface py-4 rounded-none">
+	<span>{replaceOisyPlaceholders($i18n.scanner.text.what_is_scan)}</span>
+
+	<IconHelpCircle />
+</Button>
