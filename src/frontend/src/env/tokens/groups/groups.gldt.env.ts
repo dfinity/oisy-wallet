0863b5d7e0e8402d01e9acd4c82afc91ab27dba6
Stefan Berger <sberger@bluewin.ch>
2026-04-22 12:32:05 +0200

    Merge branch 'main' into feat/icrc-send-memo-sonnet-4_6

--- src/frontend/src/env/tokens/groups/groups.gldt.env.ts
+++ src/frontend/src/env/tokens/groups/groups.gldt.env.ts
@@ -1,6 +1,5 @@
 import type { TokenGroupData, TokenGroupId } from '$lib/types/token-group';
 import { parseTokenGroupId } from '$lib/validation/token-group.validation';
-import gldt from '/icons/icrc/6c7su-kiaaa-aaaar-qaira-cai.png';
 
 const GLDT_TOKEN_GROUP_SYMBOL = 'GLDT';
 
@@ -8,7 +7,7 @@ export const GLDT_TOKEN_GROUP_ID: TokenGroupId = parseTokenGroupId(GLDT_TOKEN_GR
 
 export const GLDT_TOKEN_GROUP: TokenGroupData = {
 	id: GLDT_TOKEN_GROUP_ID,
-	icon: gldt,
+	icon: '/icons/icrc/6c7su-kiaaa-aaaar-qaira-cai.png',
 	name: 'GLDT',
 	symbol: GLDT_TOKEN_GROUP_SYMBOL
 };
--- src/frontend/src/env/tokens/tokens-evm/tokens-arbitrum/tokens-erc20/tokens.gldt.env.ts
+++ src/frontend/src/env/tokens/tokens-evm/tokens-arbitrum/tokens-erc20/tokens.gldt.env.ts
@@ -4,7 +4,6 @@ import type { RequiredEvmErc20Token } from '$evm/types/erc20';
 import { TokenCategoryTagValue, TokenTagType } from '$lib/enums/token-tag';
 import type { TokenId } from '$lib/types/token';
 import { parseTokenId } from '$lib/validation/token.validation';
-import gldt from '/icons/icrc/6c7su-kiaaa-aaaar-qaira-cai.png';
 
 export const GLDT_DECIMALS = 8;
 
@@ -21,7 +20,7 @@ export const GLDT_TOKEN: RequiredEvmErc20Token = {
 	name: 'GLDT',
 	symbol: GLDT_SYMBOL,
 	decimals: GLDT_DECIMALS,
-	icon: gldt,
+	icon: '/icons/icrc/6c7su-kiaaa-aaaar-qaira-cai.png',
 	address: '0x86856814e74456893Cfc8946BedcBb472b5fA856',
 	groupData: GLDT_TOKEN_GROUP
 };
