65c3f11209856b596aa2ea4a418b99e0ad6598c6
Antonio Ventilii <antonioventilii@gmail.com>
2026-05-07 10:26:27 +0200

    Update src/frontend/src/lib/components/transactions/filter/TransactionsFilterToolbar.svelte

--- src/frontend/src/lib/components/transactions/filter/TransactionsFilterToolbar.svelte
+++ src/frontend/src/lib/components/transactions/filter/TransactionsFilterToolbar.svelte
@@ -7,6 +7,9 @@
 
 <div class="mb-4 flex flex-wrap items-center gap-2" data-tid={TRANSACTIONS_FILTER_TOOLBAR}>
 	<TransactionsFilterTypes />
-	<TransactionsFilterTokens />
+
+<TransactionsFilterTokens />
+
 	<TransactionsFilterContacts />
+	
 </div>
