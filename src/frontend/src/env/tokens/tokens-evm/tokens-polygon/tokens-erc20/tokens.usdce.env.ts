e1907cf5225205e87dfca633616dbae3c828c70a
Stefan Berger <stefan.berger@dfinity.org>
2026-05-07 16:16:18 +0200

    Import POLYGON_MAINNET_NETWORK in tokens.usdce.env.ts

--- src/frontend/src/env/tokens/tokens-evm/tokens-polygon/tokens-erc20/tokens.usdce.env.ts
+++ src/frontend/src/env/tokens/tokens-evm/tokens-polygon/tokens-erc20/tokens.usdce.env.ts
@@ -1,3 +1,4 @@
+import {POLYGON_MAINNET_NETWORK} from '$env/networks/networks-evm/networks.evm.polygon.env';
 import { USDCE_TOKEN_GROUP } from '$env/tokens/groups/groups.usdce.env';
 import usdce from '$eth/assets/usdc.svg';
 import type { RequiredEvmErc20Token } from '$evm/types/erc20';
