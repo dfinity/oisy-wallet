2f3f00e197b4df04b4d2d14e5cf24c60b45e5307
Stefan Berger <stefan.berger@dfinity.org>
2026-05-11 13:26:42 +0200

    Add TODO comment for OnRamper functionality
    Added a TODO comment regarding the OnRamper functionality.

--- src/frontend/src/env/rest/onramper.env.ts
+++ src/frontend/src/env/rest/onramper.env.ts
@@ -1,5 +1,6 @@
 import { LOCAL, STAGING } from '$lib/constants/app.constants';
 
+// TODO: Enable (or remove this flag?) when OnRamper is functional again
 export const ONRAMPER_ENABLED: boolean = false;
 
 const ONRAMPER_ENV: 'dev' | 'prod' = LOCAL || STAGING ? 'dev' : 'prod';
