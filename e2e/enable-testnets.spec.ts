c38d55dc5d86504510198b8488f16a9a52c79ed4
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-05-16 11:18:24 +0200

    test(e2e): re-enable enable-testnets specs
    Un-skip the 8 testnet-enablement cases (BTC Testnet/Regtest,
    SepoliaETH, SOL Devnet/Local, SepoliaBASE, BSC Testnet, POL Amoy).
    Each navigates to settings, enables one testnet, opens the network
    switcher, and snapshots the resulting homepage centered on the new
    token card.
    
    Builds on the homepage logged-in canary (#12817) and the settings spec
    (#12819). 8 snapshot baselines per browser/OS will be auto-generated
    on first CI run.

--- e2e/enable-testnets.spec.ts
+++ e2e/enable-testnets.spec.ts
@@ -6,8 +6,7 @@ TestnetCases.forEach(({ networkSymbol, tokenSymbol }) => {
 		await page.clock.install();
 	});
 
-	// TODO: E2E tests are failing and/or take too much time, we need to fix them slowly, so we skip them for now
-	testWithII.skip(`should enable ${networkSymbol} network`, async ({ page, iiPage, isMobile }) => {
+	testWithII(`should enable ${networkSymbol} network`, async ({ page, iiPage, isMobile }) => {
 		const testnetsPage = new TestnetsPage({ page, iiPage, isMobile });
 		await testnetsPage.waitForReady();
 		await testnetsPage.enableTestnets({ networkSymbol, tokenSymbol });
