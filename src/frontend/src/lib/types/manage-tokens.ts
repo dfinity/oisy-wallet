7c28ea5f7d702aa7da994365f2f2165e6c9f1412
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-05-20 16:55:16 +0200

    feat(frontend): support initial manage-token wizard state

--- src/frontend/src/lib/types/manage-tokens.ts
+++ src/frontend/src/lib/types/manage-tokens.ts
@@ -1,4 +1,11 @@
+import type { AddTokenData } from '$icp-eth/types/add-token';
+import type { WizardStepsManageTokens } from '$lib/enums/wizard-steps';
+import type { Network } from '$lib/types/network';
+
 export interface ManageTokensData {
-	initialSearch: string;
+	initialSearch?: string;
 	message?: string;
+	initialNetwork?: Network;
+	initialTokenData?: Partial<AddTokenData>;
+	initialStep?: WizardStepsManageTokens;
 }
