ac2a566ce99dd6d7fcf1f195847b8aa639bc28aa
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2026-07-01 12:27:42 +0200

    feat(frontend): implement Borrowings tab

---
+++ src/frontend/src/lib/components/liquidium/LiquidiumProviderTag.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Badge from '$lib/components/ui/Badge.svelte';
+	import { lendBorrowProvidersConfig } from '$lib/config/lend-borrow.config';
+	import { LendBorrowProvider } from '$lib/types/lend-borrow';
+
+	const { name } = lendBorrowProvidersConfig[LendBorrowProvider.LIQUIDIUM];
+</script>
+
+<span class="hidden sm:inline-block">
+	<Badge styleClass="ml-2 mb-1" variant="transparent" width="w-fit">
+		{name}
+	</Badge>
+</span>
