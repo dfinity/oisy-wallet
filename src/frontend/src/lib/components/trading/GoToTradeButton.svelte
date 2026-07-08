9dab404d2a6ef934b0d72086965e5628bae9d4b1
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2026-07-08 10:30:38 +0200

    refactor(frontend): reusable GoToButton component

--- src/frontend/src/lib/components/trading/GoToTradeButton.svelte
+++ src/frontend/src/lib/components/trading/GoToTradeButton.svelte
@@ -1,19 +1,13 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import IconArrowRight from '$lib/components/icons/IconArrowRight.svelte';
-	import Button from '$lib/components/ui/Button.svelte';
+	import GoToButton from '$lib/components/common/GoToButton.svelte';
 	import { AppPath } from '$lib/constants/routes.constants';
 	import { TRADING_GOTO_BUTTON } from '$lib/constants/test-ids.constants';
 	import { i18n } from '$lib/stores/i18n.store';
 </script>
 
-<Button
-	colorStyle="success"
+<GoToButton
+	label={$i18n.trading.text.go_to_trade}
 	onclick={() => goto(AppPath.ProvidersOisyTrade)}
-	styleClass="gap-1 flex-none px-12 font-semibold"
 	testId={TRADING_GOTO_BUTTON}
-	type="button"
->
-	{$i18n.trading.text.go_to_trade}
-	<IconArrowRight />
-</Button>
+/>
