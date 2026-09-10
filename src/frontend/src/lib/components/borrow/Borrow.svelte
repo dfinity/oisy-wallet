5ffd52ae749f88824e739d1331ec66cdd245483c
DenysKarmazynDFINITY <denys.karmazyn@dfinity.org>
2026-09-10 16:30:07 +0200

    feat(frontend): add empty state to the Borrow page

--- src/frontend/src/lib/components/borrow/Borrow.svelte
+++ src/frontend/src/lib/components/borrow/Borrow.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { goto } from '$app/navigation';
-	import { anyLendBorrowProviderEnabled } from '$env/lend-borrow';
 	import AllBorrowOpportunityCardList from '$lib/components/borrow/AllBorrowOpportunityCardList.svelte';
 	import BorrowHeader from '$lib/components/borrow/BorrowHeader.svelte';
 	import StakeContentSection from '$lib/components/stake/StakeContentSection.svelte';
-	import { AppPath } from '$lib/constants/routes.constants';
 	import {
 		PLAUSIBLE_EVENT_CONTEXTS,
 		PLAUSIBLE_EVENT_VALUES,
@@ -15,11 +12,6 @@
 	import { i18n } from '$lib/stores/i18n.store';
 
 	onMount(() => {
-		if (!anyLendBorrowProviderEnabled) {
-			goto(AppPath.Earn);
-			return;
-		}
-
 		trackEvent({
 			name: PLAUSIBLE_EVENTS.PAGE_OPEN,
 			metadata: {
@@ -30,16 +22,14 @@
 	});
 </script>
 
-{#if anyLendBorrowProviderEnabled}
-	<div class="flex flex-col gap-6 pb-6">
-		<BorrowHeader />
-		<StakeContentSection>
-			{#snippet title()}
-				<h4>{$i18n.borrow.text.borrowing_options}</h4>
-			{/snippet}
-			{#snippet content()}
-				<AllBorrowOpportunityCardList />
-			{/snippet}
-		</StakeContentSection>
-	</div>
-{/if}
+<div class="flex flex-col gap-6 pb-6">
+	<BorrowHeader />
+	<StakeContentSection>
+		{#snippet title()}
+			<h4>{$i18n.borrow.text.borrowing_options}</h4>
+		{/snippet}
+		{#snippet content()}
+			<AllBorrowOpportunityCardList />
+		{/snippet}
+	</StakeContentSection>
+</div>
